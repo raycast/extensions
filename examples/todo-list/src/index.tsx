2b268f3ff1529b3253657b1123c876382b1b28a1
Thomas Paul Mann <hi@thomaspaulmann.com>
2021-10-11 17:13:05 +0100

    Add todo list example

---
+++ examples/todo-list/src/index.tsx
@@ -0,0 +1,110 @@
+import { ActionPanel, Form, Icon, List, PushAction, SubmitFormAction, useNavigation } from "@raycast/api";
+import { useState } from "react";
+
+interface Todo {
+  title: string;
+  isCompleted: boolean;
+}
+
+export default function Command() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  function handleCreate(todo: Todo) {
+    const newTodos = [...todos, todo];
+    setTodos(newTodos);
+  }
+
+  function handleToggle(index: number) {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    setTodos(newTodos);
+  }
+
+  function handleDelete(index: number) {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
+
+  return (
+    <List
+      actions={
+        <ActionPanel>
+          <CreateTodoAction onCreate={handleCreate} />
+        </ActionPanel>
+      }
+    >
+      {todos.map((todo, index) => (
+        <List.Item
+          key={index}
+          icon={todo.isCompleted ? Icon.Checkmark : Icon.Circle}
+          title={todo.title}
+          actions={
+            <ActionPanel>
+              <ActionPanel.Section>
+                <ToggleTodoAction todo={todo} index={index} onToggle={handleToggle} />
+              </ActionPanel.Section>
+              <ActionPanel.Section>
+                <CreateTodoAction onCreate={handleCreate} />
+                <DeleteTodoAction index={index} onDelete={handleDelete} />
+              </ActionPanel.Section>
+            </ActionPanel>
+          }
+        />
+      ))}
+    </List>
+  );
+}
+
+function CreateTodoAction(props: { onCreate: (todo: Todo) => void }) {
+  return (
+    <PushAction
+      icon={Icon.Pencil}
+      title="Create Todo"
+      shortcut={{ modifiers: ["cmd"], key: "n" }}
+      target={<CreateTodoForm onCreate={props.onCreate} />}
+    />
+  );
+}
+
+function ToggleTodoAction(props: { todo: Todo; index: number; onToggle: (index: number) => void }) {
+  return (
+    <ActionPanel.Item
+      icon={props.todo.isCompleted ? Icon.Circle : Icon.Checkmark}
+      title={props.todo.isCompleted ? "Uncomplete Todo" : "Complete Todo"}
+      onAction={() => props.onToggle(props.index)}
+    />
+  );
+}
+
+function DeleteTodoAction(props: { index: number; onDelete: (index: number) => void }) {
+  return (
+    <ActionPanel.Item
+      icon={Icon.Trash}
+      title="Delete Todo"
+      shortcut={{ modifiers: ["ctrl"], key: "x" }}
+      onAction={() => props.onDelete(props.index)}
+    />
+  );
+}
+
+function CreateTodoForm(props: { onCreate: (todo: Todo) => void }) {
+  const { pop } = useNavigation();
+
+  function handleSubmit(values: { title: string }) {
+    props.onCreate({ title: values.title, isCompleted: false });
+    pop();
+  }
+
+  return (
+    <Form
+      actions={
+        <ActionPanel>
+          <SubmitFormAction title="Create Todo" onSubmit={handleSubmit} />
+        </ActionPanel>
+      }
+    >
+      <Form.TextField id="title" title="Title" />
+    </Form>
+  );
+}
