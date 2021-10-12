2df3a2a74d1e736c98c6622c28514429027f1895
Thomas Paul Mann <hi@thomaspaulmann.com>
2021-10-12 20:57:24 +0100

    Add extension to search recent projects in VS Code

---
+++ extensions/visual-studio-code-recent-projects/src/index.jsx
@@ -0,0 +1,63 @@
+import { ActionPanel, CopyToClipboardAction, List, OpenAction, ShowInFinderAction } from "@raycast/api";
+import { existsSync, readFileSync } from "fs";
+import { homedir } from "os";
+import { basename, dirname } from "path";
+
+const STORAGE = `${homedir()}/Library/Application Support/Code/storage.json`;
+
+function getRecentEntries() {
+  const json = JSON.parse(readFileSync(STORAGE).toString());
+  return json.openedPathsList.entries;
+}
+
+export default function Command() {
+  const folders = [];
+  const files = [];
+
+  const recentEntries = getRecentEntries();
+  recentEntries.forEach((entry) => {
+    if (entry.folderUri && existsSync(new URL(entry.folderUri))) {
+      folders.push(<ProjectListItem key={entry.folderUri} uri={entry.folderUri} />);
+    } else if (entry.fileUri && existsSync(new URL(entry.fileUri))) {
+      files.push(<ProjectListItem key={entry.fileUri} uri={entry.fileUri} />);
+    }
+  });
+
+  return (
+    <List searchBarPlaceholder="Search recent projects...">
+      <List.Section title="Folders">{folders}</List.Section>
+      <List.Section title="Files">{files}</List.Section>
+    </List>
+  );
+}
+
+function ProjectListItem(props) {
+  const name = decodeURI(basename(props.uri));
+  const path = decodeURI(props.uri.replace(`file://${homedir()}`, "~"));
+  const subtitle = dirname(path);
+  const keywords = path.split("/");
+  return (
+    <List.Item
+      title={name}
+      subtitle={subtitle}
+      icon={{ fileIcon: path }}
+      keywords={keywords}
+      actions={
+        <ActionPanel>
+          <ActionPanel.Section>
+            <OpenAction title="Open in Code" icon="code.png" target={props.uri} application="Visual Studio Code" />
+            <ShowInFinderAction path={props.uri} />
+          </ActionPanel.Section>
+          <ActionPanel.Section>
+            <CopyToClipboardAction title="Copy Name" content={name} shortcut={{ modifiers: ["cmd"], key: "." }} />
+            <CopyToClipboardAction
+              title="Copy Path"
+              content={path}
+              shortcut={{ modifiers: ["cmd", "shift"], key: "." }}
+            />
+          </ActionPanel.Section>
+        </ActionPanel>
+      }
+    />
+  );
+}
