08ef0f502c3941fefef9632102cf00e04995940d
Sven Tiigi <sven.tiigi@gmail.com>
2021-10-14 19:24:52 +0200

    Added documentation

--- extensions/xcode/src/shared/group-by.ts
+++ extensions/xcode/src/shared/group-by.ts
@@ -1,13 +1,30 @@
-export function groupBy<K, V>(list: Array<V>, keyGetter: (input: V) => K): Map<K, Array<V>> {
-  const map = new Map<K, Array<V>>();
-  list.forEach((item) => {
-    const key = keyGetter(item);
-    const collection = map.get(key);
-    if (!collection) {
-      map.set(key, [item]);
+/**
+ * Group a given array by a given key into a Map
+ * Source: https://stackoverflow.com/a/38327540
+ * @param list The list
+ * @param keyProvider The key provider that is used for grouping
+ */
+export function groupBy<Key, Value>(
+  list: Array<Value>,
+  keyProvider: (input: Value) => Key
+): Map<Key, Array<Value>> {
+  // Initialize the grouping Map
+  const map = new Map<Key, Array<Value>>();
+  // For each value in the list
+  for (const value of list) {
+    // Retrieve the key from the key provider
+    const key = keyProvider(value);
+    // Retrieve the current values from the grouping Map
+    const values = map.get(key);
+    // Check if values is falsy
+    if (!values) {
+      // Set value as array to grouping Map
+      map.set(key, [value]);
     } else {
-      collection.push(item);
+      // Push value to collection
+      values.push(value);
     }
-  });
+  }
+  // Return map
   return map;
 }
