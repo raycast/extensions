ed26f87dd8bf6e4938c623eab5e3b442a1c7c3b9
Federico Zivolo <federico.zivolo@gmail.com>
2021-10-14 14:18:15 +0200

    extension(emoji): add recently used list

--- extensions/emoji/src/emoji.tsx
+++ extensions/emoji/src/emoji.tsx
@@ -7,7 +7,8 @@ import {
   setLocalStorageItem,
 } from "@raycast/api";
 
-import { useState, useEffect, useRef, ReactElement } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
+import type { ReactElement, SetStateAction, Dispatch } from "react";
 import { createEmojiList } from "generate-emoji-list";
 
 type Category = { category: string; emojis: Emoji[] };
@@ -16,27 +17,20 @@ type Emoji = {
   description: string;
 };
 
-export default function Main(): ReactElement {
-  const [searchText, setSearchText] = useState<string>("");
-  const [list, setList] = useState<Category[]>([]);
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const useStateFromLocalStorage = <T, _ = void>(key: string, initialValue: T): [T, Dispatch<SetStateAction<T>>] => {
+  const [state, setState] = useState<T>(initialValue);
   const didUnmount = useRef<boolean>(false);
 
   useEffect(() => {
     (async () => {
-      const cachedList = await getLocalStorageItem("emoji-list");
+      const cache = await getLocalStorageItem(key);
 
-      if (didUnmount.current) return;
-
-      if (typeof cachedList === "string") {
-        setList(JSON.parse(cachedList));
-      }
-
-      createEmojiList().then((list: Category[]) => {
-        setLocalStorageItem("emoji-list", JSON.stringify(list));
+      if (typeof cache === "string") {
         if (!didUnmount.current) {
-          setList(list);
+          setState(JSON.parse(cache));
         }
-      });
+      }
     })();
 
     return () => {
@@ -44,23 +38,66 @@ export default function Main(): ReactElement {
     };
   }, []);
 
+  const setStateAndLocalStorage = useCallback((updater) => {
+    setState((state) => {
+      const newValue = typeof updater === "function" ? updater(state) : updater;
+      setLocalStorageItem(key, JSON.stringify(newValue));
+      return newValue;
+    });
+  }, []);
+
+  return [state, setStateAndLocalStorage];
+};
+
+export default function Main(): ReactElement {
+  const [searchText, setSearchText] = useState<string>("");
+  const [list, setList] = useStateFromLocalStorage<Category[]>("emoji-list", []);
+  const didUnmount = useRef<boolean>(false);
+
+  useEffect(() => {
+    createEmojiList().then((list: Category[]) => {
+      if (!didUnmount.current) {
+        setList(list);
+      }
+    });
+
+    return () => {
+      didUnmount.current = true;
+    };
+  }, []);
+
+  const [recentlyUsed, setRecentlyUsed] = useStateFromLocalStorage<Emoji[]>("recently-used", []);
+  const addToRecentlyUsed = useCallback((emoji: Emoji) => {
+    setRecentlyUsed((list) =>
+      list.find((x) => x.description === emoji.description) ? list : [emoji, ...list].slice(0, 10)
+    );
+  }, []);
+
   return (
     <List onSearchTextChange={setSearchText} isLoading={list.length === 0}>
-      {list.map((category: Category) => (
+      {[{ category: "Recently Used", emojis: recentlyUsed }, ...list].map((category: Category) => (
         <List.Section title={category.category} key={category.category}>
           {category.emojis
             .filter((emoji) => emoji.description.includes(searchText))
             .map((emoji) => (
               <List.Item
-                key={emoji.description}
-                id={emoji.description}
+                key={`${category.category}${emoji.description}`}
+                id={`${category.category}${emoji.description}`}
                 icon={emoji.emoji}
                 title={emoji.description}
                 actions={
                   <ActionPanel>
                     <ActionPanel.Section>
-                      <PasteAction title="Paste Emoji to curent window" content={emoji.emoji} />
-                      <CopyToClipboardAction title="Copy Emoji to Clipboard" content={emoji.emoji} />
+                      <PasteAction
+                        title="Paste Emoji to Curent Window"
+                        content={emoji.emoji}
+                        onPaste={() => addToRecentlyUsed(emoji)}
+                      />
+                      <CopyToClipboardAction
+                        title="Copy Emoji to Clipboard"
+                        content={emoji.emoji}
+                        onCopy={() => addToRecentlyUsed(emoji)}
+                      />
                     </ActionPanel.Section>
                   </ActionPanel>
                 }
