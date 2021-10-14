f6334d6c750bfd286a8b3e6d35704fa60cd492ac
Sven Tiigi <sven.tiigi@gmail.com>
2021-10-14 08:47:33 +0200

    Merged XcodeProjects loading in just one useEffect execution

--- extensions/xcode-projects/src/projects-command.ts
+++ extensions/xcode-projects/src/projects-command.ts
@@ -8,41 +8,38 @@ import { XcodeProjectService } from "./services/xcode-project.service";
  * Xcode projects command
  */
 export default () => {
-    // Initialize XcodeProjectService
-    const xcodeProjectService = new XcodeProjectService();
-    // Use XcodeProject State
-    const [xcodeProjects, setXcodeProjects] = useState<XcodeProject[] | undefined>(undefined);
-    // Use Effect to load cached XcodeProjects
-    useEffect(() => {
-        // Retrieve cached XcodeProjects
-        xcodeProjectService
-          .cachedXcodeProjects()
-          .then(cachedXcodeProjects => {
-            // Check if no XcodeProjects have been set
-            if (!xcodeProjects) {
-              // Set cached XcodeProjects
-              setXcodeProjects(cachedXcodeProjects);
-            }
-          })
-          .catch(console.error);
-    }, []);
-    // Use Effect to load XcodeProjects
-    useEffect(() => {
-        // Retrieve XcodeProjects
-        xcodeProjectService
-          .xcodeProjects()
-          .then(setXcodeProjects)
-          .catch(error => {
-              // Log Error
-              console.error(error);
-              // Show Toast
-              return showToast(
-                ToastStyle.Failure,
-                "An error occurred while finding Xcode Projects",
-                error
-              );
-          });
-    }, []);
-    // Return XcodeProject List
-    return xcodeProjectList(xcodeProjects);
+  // Initialize XcodeProjectService
+  const xcodeProjectService = new XcodeProjectService();
+  // Use XcodeProject State
+  const [xcodeProjects, setXcodeProjects] = useState<XcodeProject[] | undefined>(undefined);
+  // Use Effect
+  useEffect(() => {
+    // Retrieve cached XcodeProjects
+    xcodeProjectService
+      .cachedXcodeProjects()
+      .then(cachedXcodeProjects => {
+        // Check if no XcodeProjects have been set
+        if (!xcodeProjects) {
+          // Set cached XcodeProjects
+          setXcodeProjects(cachedXcodeProjects);
+        }
+      })
+      .catch(console.error);
+    // Retrieve most recent XcodeProjects
+    xcodeProjectService
+      .xcodeProjects()
+      .then(setXcodeProjects)
+      .catch(error => {
+        // Log Error
+        console.error(error);
+        // Show Toast
+        return showToast(
+          ToastStyle.Failure,
+          "An error occurred while finding Xcode Projects",
+          error
+        );
+      });
+  }, []);
+  // Return XcodeProject List
+  return xcodeProjectList(xcodeProjects);
 };
