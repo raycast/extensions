7c28cb77b71de867783412975e3050195b826ac7
Matt Gleich <git@mattglei.ch>
2021-10-15 10:30:24 -0400

    feat: apply custom prettier and change description
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- extensions/vercast/src/index.tsx
+++ extensions/vercast/src/index.tsx
@@ -8,58 +8,63 @@ import {
   preferences,
   showToast,
   ToastStyle,
-} from "@raycast/api";
-import { useEffect, useState } from "react";
-import useInterval from "./use-interval";
-import { Deployment, DeploymentState, fetchDeployments, fetchUsername } from "./vercel";
+} from '@raycast/api'
+import { useEffect, useState } from 'react'
+import useInterval from './use-interval'
+import {
+  Deployment,
+  DeploymentState,
+  fetchDeployments,
+  fetchUsername,
+} from './vercel'
 
-render(<Main />);
+render(<Main />)
 
 function Main(): JSX.Element {
-  const token = String(preferences.token.value);
+  const token = String(preferences.token.value)
   if (token.length !== 24) {
-    showToast(ToastStyle.Failure, "Invalid token detected");
-    throw new Error("Invalid token length detected");
+    showToast(ToastStyle.Failure, 'Invalid token detected')
+    throw new Error('Invalid token length detected')
   }
 
-  const [username, setUsername] = useState("");
-  const [deployments, setDeployments] = useState<Deployment[]>();
+  const [username, setUsername] = useState('')
+  const [deployments, setDeployments] = useState<Deployment[]>()
 
   useEffect(() => {
-    const call = async () => setUsername(await fetchUsername());
-    if (username === "") {
-      call();
+    const call = async () => setUsername(await fetchUsername())
+    if (username === '') {
+      call()
     }
-  });
+  })
   useEffect(() => {
-    const call = async () => setDeployments(await fetchDeployments(username));
+    const call = async () => setDeployments(await fetchDeployments(username))
     if (!deployments) {
-      call();
+      call()
     }
-  });
+  })
 
   useInterval(async () => {
-    setDeployments(await fetchDeployments(username));
-  }, 1000);
+    setDeployments(await fetchDeployments(username))
+  }, 1000)
 
   return (
     <List isLoading={!deployments}>
       {deployments?.map((d) => {
-        let iconSource = Icon.Globe;
-        let iconTintColor = Color.PrimaryText;
+        let iconSource = Icon.Globe
+        let iconTintColor = Color.PrimaryText
         switch (d.state) {
           case DeploymentState.ready:
-            iconSource = Icon.Checkmark;
-            iconTintColor = Color.Green;
-            break;
+            iconSource = Icon.Checkmark
+            iconTintColor = Color.Green
+            break
           case DeploymentState.deploying:
-            iconSource = Icon.Hammer;
-            iconTintColor = Color.Yellow;
-            break;
+            iconSource = Icon.Hammer
+            iconTintColor = Color.Yellow
+            break
           case DeploymentState.failed:
-            iconSource = Icon.XmarkCircle;
-            iconTintColor = Color.Red;
-            break;
+            iconSource = Icon.XmarkCircle
+            iconTintColor = Color.Red
+            break
         }
         return (
           <List.Item
@@ -75,8 +80,8 @@ function Main(): JSX.Element {
               </ActionPanel>
             }
           />
-        );
+        )
       })}
     </List>
-  );
+  )
 }
--- extensions/vercast/src/vercel.ts
+++ extensions/vercast/src/vercel.ts
@@ -1,12 +1,12 @@
-import { preferences, showToast, ToastStyle } from "@raycast/api";
-import fetch, { Headers } from "node-fetch";
-import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
+import { preferences, showToast, ToastStyle } from '@raycast/api'
+import fetch, { Headers } from 'node-fetch'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
 
 const headers = new Headers({
-  Authorization: "Bearer " + preferences.token.value,
-});
-const apiURL = "https://api.vercel.com/";
+  Authorization: 'Bearer ' + preferences.token.value,
+})
+const apiURL = 'https://api.vercel.com/'
 
 export enum DeploymentState {
   ready,
@@ -15,73 +15,76 @@ export enum DeploymentState {
 }
 
 export interface Deployment {
-  project: string;
-  state: DeploymentState;
-  time: string;
-  id: string;
-  url: string;
-  domain: string;
+  project: string
+  state: DeploymentState
+  time: string
+  id: string
+  url: string
+  domain: string
 }
 
 export async function fetchUsername(): Promise<string> {
   try {
-    const response = await fetch(apiURL + "www/user", {
-      method: "get",
+    const response = await fetch(apiURL + 'www/user', {
+      method: 'get',
       headers: headers,
-    });
-    const json = await response.json();
-    return json.user.username;
+    })
+    const json = await response.json()
+    return json.user.username
   } catch (err) {
-    console.error(err);
-    showToast(ToastStyle.Failure, "Failed to fetch username");
-    throw new Error("Failed to fetch username");
+    console.error(err)
+    showToast(ToastStyle.Failure, 'Failed to fetch username')
+    throw new Error('Failed to fetch username')
   }
-  return Promise.resolve("");
+  return Promise.resolve('')
 }
 
-export async function fetchDeployments(username: string): Promise<Deployment[]> {
-  dayjs.extend(relativeTime);
+export async function fetchDeployments(
+  username: string
+): Promise<Deployment[]> {
+  dayjs.extend(relativeTime)
 
   try {
-    const response = await fetch(apiURL + "v8/projects", {
-      method: "get",
+    const response = await fetch(apiURL + 'v8/projects', {
+      method: 'get',
       headers: headers,
-    });
-    const json = await response.json();
-    const deployments: Deployment[] = [];
+    })
+    const json = await response.json()
+    const deployments: Deployment[] = []
     for (const project of json.projects) {
       for (const deployment of project.latestDeployments) {
         if (deployment.creator.username === username) {
-          let state: DeploymentState;
+          let state: DeploymentState
           switch (deployment.readyState.toUpperCase()) {
-            case "READY":
-              state = DeploymentState.ready;
-              break;
-            case "BUILDING":
-            case "QUEUED":
-              state = DeploymentState.deploying;
-              break;
+            case 'READY':
+              state = DeploymentState.ready
+              break
+            case 'BUILDING':
+            case 'QUEUED':
+              state = DeploymentState.deploying
+              break
             default:
-              state = DeploymentState.failed;
-              break;
+              state = DeploymentState.failed
+              break
           }
           deployments.push({
             project: project.name,
             state: state,
             time: dayjs(deployment.createdAt).fromNow(),
             id: deployment.id,
-            url: `https://vercel.com/${username}/${project.name}/${deployment.id.replace("dpl_", "")}`,
+            url: `https://vercel.com/${username}/${
+              project.name
+            }/${deployment.id.replace('dpl_', '')}`,
             domain: deployment.alias[0],
-          });
-          break;
+          })
+          break
         }
       }
     }
-    return deployments;
+    return deployments
   } catch (err) {
-    console.error(err);
-    showToast(ToastStyle.Failure, "Failed to fetch deployments");
-    throw new Error("Failed to fetch deployments");
+    console.error(err)
+    showToast(ToastStyle.Failure, 'Failed to fetch deployments')
+    throw new Error('Failed to fetch deployments')
   }
-  return Promise.resolve([]);
 }
