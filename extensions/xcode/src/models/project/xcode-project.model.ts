94fd07222ce815b1f3a4699e824a22e22b62e6b9
Sven Tiigi <sven.tiigi@gmail.com>
2021-10-15 22:25:49 +0200

    Add Xcode Extension (#14)
    * Added xcode-projects Extension
    
    * Updated command name to xcode projects
    
    * Refactored command-icon to icon
    
    * Updated wording
    
    * Updated SearchBar Placeholder
    
    * Added support for Swift Playgrounds
    
    * Added accessory title to List Item
    
    * Merged XcodeProjects loading in just one useEffect execution
    
    * Renamed xcode-projects extension to xcode and added releases command
    
    * Added async version of exec
    
    * Added Simulators command
    
    Allowing to boot and shutdown Xcode Simulators
    
    * Wrap XcodeSimulatorService in state instead of global const
    
    * Added documentation
    
    * Added Xcode Simulator Icon
    
    * Addded Apps command
    
    * Added check if Cache is not empty
    
    * On error set empty applications
    
    * Retrieve Bundle Identifier from Info.plist instead of retrieving it async via defaults read
    
    * Refactored push action to initialize component only when needed
    
    * Fixed Navigation bug
    
    By method injecting the Navigation to the components
    
    * Added main extension icon
    
    * Added keywords
    
    * Updated icon to proper size 512x512 according to review guidelines
    
    * Added Create Swift Package Command
    
    * Set empty XcodeReleases on error in case no previous releases are available
    
    * Set empty XcodeProjects in case of an error if no previous projects are available
    
    * Send empty array of XcodeSimulators in case of an error
    
    * Added Clear Derived Data command
    
    * Updated command order
    
    * Removed trailing slash
    
    * Log error
    
    * Added Clear SPM cache command
    
    * Updated naming convention for commands
    
    * Moved models in distinct child directories and updated import paths
    
    Improves the overview of the models
    
    * Added tildify dependency
    
    * tildify Xcode project path
    
    * Updated title and subtitle of commands according to suggestions and guidelines
    
    * Fixed author name with raycast username
    
    * Updated description

---
+++ extensions/xcode/src/models/project/xcode-project.model.ts
@@ -0,0 +1,23 @@
+import { XcodeProjectType } from "./xcode-project-type.model";
+
+/**
+ * A XcodeProject
+ */
+export interface XcodeProject {
+  /**
+   * The name of the XcodeProject
+   */
+  name: string;
+  /**
+   * The XcodeProjectType
+   */
+  type: XcodeProjectType;
+  /**
+   * The file path
+   */
+  filePath: string;
+  /**
+   * The keywords
+   */
+  keywords: string[]
+}
---
+++ extensions/xcode/src/services/xcode-simulator.service.ts
@@ -0,0 +1,152 @@
+import { XcodeSimulator } from "../models/simulator/xcode-simulator.model";
+import { execAsync, ExecAsyncOutput } from "../shared/exec-async";
+import { BehaviorSubject, Observable } from "rxjs";
+import { showToast, ToastStyle } from "@raycast/api";
+
+/**
+ * XcodeSimulatorService
+ */
+export class XcodeSimulatorService {
+
+  /**
+   * The XcodeSimulators BehaviorSubject
+   */
+  private xcodeSimulatorsSubject = new BehaviorSubject<XcodeSimulator[] | undefined>(undefined);
+
+  /**
+   * The XcodeSimulators Observable
+   */
+  get xcodeSimulators(): Observable<XcodeSimulator[] | undefined> {
+    // Refresh XcodeSimulators
+    this.refreshXcodeSimulators();
+    // Return Subject as Observable
+    return this.xcodeSimulatorsSubject.asObservable();
+  }
+
+  /**
+   * Retrieve all installed XcodeSimulators
+   */
+  async getXcodeSimulators(): Promise<XcodeSimulator[]> {
+    // Execute command
+    const output = await execAsync(
+      "xcrun simctl list -j -v devices"
+    );
+    // Parse stdout as JSON
+    const devicesResponseJSON = JSON.parse(output.stdout);
+    // Check if JSON or devices within the JSON are not available
+    if (!devicesResponseJSON || !devicesResponseJSON.devices) {
+      // Throw Error
+      throw new Error("No Devices have been found");
+    }
+    // Initialize XcodeSimulators
+    const simulators: XcodeSimulator[] = [];
+    // For each DeviceGroup
+    for (const deviceGroup in devicesResponseJSON.devices) {
+      // Initialize runtime components from DeviceGroup
+      const runtimeComponents = deviceGroup
+        .substring(deviceGroup.lastIndexOf(".") + 1)
+        .split("-");
+      // Initialize runtime string
+      const runtime = [
+        runtimeComponents.shift(),
+        runtimeComponents.join(".")
+      ].join(" ");
+      // Push Simulators in DeviceGroup
+      simulators.push(
+        ...devicesResponseJSON
+          .devices[deviceGroup]
+          .map((simulator: XcodeSimulator) => {
+            simulator.runtime = runtime;
+            return simulator;
+          })
+      );
+    }
+    // Return XcodeSimulators
+    return simulators;
+  }
+
+  /**
+   * Boot XcodeSimulator
+   * @param xcodeSimulator The XcodeSimulator to boot
+   */
+  async boot(
+    xcodeSimulator: XcodeSimulator
+  ): Promise<void> {
+    // Update to pending state
+    this.updateToPendingStateIfNeeded(xcodeSimulator);
+    // Boot Simulator
+    await execAsync(
+      `xcrun simctl boot ${xcodeSimulator.udid}`
+    );
+    // Refresh XcodeSimulators
+    this.refreshXcodeSimulators();
+  }
+
+  /**
+   * Shutdown XcodeSimulator
+   * @param xcodeSimulator The XcodeSimulator to shutdown
+   */
+  async shutdown(
+    xcodeSimulator: XcodeSimulator
+  ): Promise<void> {
+    // Update to pending state
+    this.updateToPendingStateIfNeeded(xcodeSimulator);
+    // Shutdown Simulator
+    await execAsync(
+      `xcrun simctl shutdown ${xcodeSimulator.udid}`
+    );
+    // Refresh XcodeSimulators
+    this.refreshXcodeSimulators();
+  }
+
+  /**
+   * Refresh XcodeSimulators
+   */
+  private async refreshXcodeSimulators() {
+    try {
+      // Retrieve XcodeSimulators
+      const simulators = await this.getXcodeSimulators();
+      // Send XcodeSimulators to Subject
+      this.xcodeSimulatorsSubject.next(simulators);
+    } catch (error) {
+      // Send empty XcodeSimulators to Subject
+      this.xcodeSimulatorsSubject.next([]);
+      // Show failure Toast
+      await showToast(
+        ToastStyle.Failure,
+        "An error occurred while retrieving the Xcode Simulators",
+        (error as Error).message
+      );
+    }
+  }
+
+  /**
+   * Update a given XcodeSimulator to pending state if needed
+   * @param xcodeSimulator The XcodeSimulator
+   */
+  private updateToPendingStateIfNeeded(
+    xcodeSimulator: XcodeSimulator
+  ) {
+    // Retrieve the current XcodeSimulators
+    const xcodeSimulators = this.xcodeSimulatorsSubject.value;
+    // Check if XcodeSimulators are not available
+    if (!xcodeSimulators) {
+      // Return out of function
+      return;
+    }
+    // Emit updated XcodeSimulators
+    this.xcodeSimulatorsSubject.next(
+      xcodeSimulators
+        .map(currentXcodeSimulator => {
+          // Check if current XcodeSimulator matches the given XcodeSimulator
+          if (currentXcodeSimulator.udid === xcodeSimulator.udid) {
+            // Update state to pending
+            currentXcodeSimulator.state = "Pending";
+          }
+          // Return current XcodeSimulator
+          return currentXcodeSimulator;
+        })
+    );
+  }
+
+}
