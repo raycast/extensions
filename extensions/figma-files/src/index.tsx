a55a8eda740d18f402acc7a338028d448a7719cf
Thomas Paul Mann <hi@thomaspaulmann.com>
2021-10-16 13:41:17 +0100

    Format code

--- extensions/figma-files/src/index.tsx
+++ extensions/figma-files/src/index.tsx
@@ -1,23 +1,23 @@
-import { ActionPanel, List, OpenInBrowserAction, showToast, ToastStyle, getPreferenceValues } from "@raycast/api";
-import { useState, useEffect } from "react";
-import fetch from "node-fetch";
+import { ActionPanel, List, OpenInBrowserAction, showToast, ToastStyle, getPreferenceValues } from "@raycast/api"
+import { useState, useEffect } from "react"
+import fetch from "node-fetch"
 
 export default function FileList() {
   const [state, setState] = useState<{ projectFiles: ProjectFiles[] }>({
     projectFiles: [],
-  });
+  })
 
   useEffect(() => {
     async function fetch() {
-      const projectFiles = await fetchFiles();
+      const projectFiles = await fetchFiles()
 
       setState((oldState) => ({
         ...oldState,
         projectFiles,
-      }));
+      }))
     }
-    fetch();
-  }, []);
+    fetch()
+  }, [])
 
   return (
     <List isLoading={state.projectFiles.length === 0} searchBarPlaceholder="Filter files by name...">
@@ -29,11 +29,11 @@ export default function FileList() {
         </List.Section>
       ))}
     </List>
-  );
+  )
 }
 
 function FileListItem(props: { file: File }) {
-  const { file } = props;
+  const { file } = props
 
   return (
     <List.Item
@@ -48,11 +48,11 @@ function FileListItem(props: { file: File }) {
         </ActionPanel>
       }
     />
-  );
+  )
 }
 
 async function fetchTeamProjects(): Promise<TeamProjects> {
-  const { PERSONAL_ACCESS_TOKEN, TEAM_ID } = getPreferenceValues();
+  const { PERSONAL_ACCESS_TOKEN, TEAM_ID } = getPreferenceValues()
   try {
     const response = await fetch(`https://api.figma.com/v1/teams/${TEAM_ID}/projects`, {
       method: "GET",
@@ -60,20 +60,20 @@ async function fetchTeamProjects(): Promise<TeamProjects> {
         "Content-Type": "application/json",
         "X-Figma-Token": PERSONAL_ACCESS_TOKEN,
       },
-    });
+    })
 
-    const json = (await response.json()) as TeamProjects;
-    return json;
+    const json = (await response.json()) as TeamProjects
+    return json
   } catch (error) {
-    console.error(error);
-    showToast(ToastStyle.Failure, "Could not load team");
-    return Promise.resolve({ name: "No team found", projects: [] });
+    console.error(error)
+    showToast(ToastStyle.Failure, "Could not load team")
+    return Promise.resolve({ name: "No team found", projects: [] })
   }
 }
 
 async function fetchFiles(): Promise<ProjectFiles[]> {
-  const { PERSONAL_ACCESS_TOKEN } = getPreferenceValues();
-  const teamProjects = await fetchTeamProjects();
+  const { PERSONAL_ACCESS_TOKEN } = getPreferenceValues()
+  const teamProjects = await fetchTeamProjects()
   const projects = teamProjects.projects.map(async (project) => {
     try {
       const response = await fetch(`https://api.figma.com/v1/projects/${project.id}/files`, {
@@ -82,38 +82,38 @@ async function fetchFiles(): Promise<ProjectFiles[]> {
           "Content-Type": "application/json",
           "X-Figma-Token": PERSONAL_ACCESS_TOKEN,
         },
-      });
+      })
 
-      const json = (await response.json()) as ProjectFiles;
-      return { name: project.name, files: json.files as File[] };
+      const json = (await response.json()) as ProjectFiles
+      return { name: project.name, files: json.files as File[] }
     } catch (error) {
-      console.error(error);
-      showToast(ToastStyle.Failure, "Could not load files");
-      return Promise.resolve([]);
+      console.error(error)
+      showToast(ToastStyle.Failure, "Could not load files")
+      return Promise.resolve([])
     }
-  });
+  })
 
-  return Promise.all(projects) as Promise<ProjectFiles[]>;
+  return Promise.all(projects) as Promise<ProjectFiles[]>
 }
 
 type Project = {
-  id: string;
-  name: string;
-};
+  id: string
+  name: string
+}
 
 type TeamProjects = {
-  name: string;
-  projects: Project[];
-};
+  name: string
+  projects: Project[]
+}
 
 type ProjectFiles = {
-  files: File[];
-  name: string;
-};
+  files: File[]
+  name: string
+}
 
 type File = {
-  key: string;
-  last_modified: string;
-  name: string;
-  thumbnail_url: string;
-};
+  key: string
+  last_modified: string
+  name: string
+  thumbnail_url: string
+}
