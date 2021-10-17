77957bf5d35884e97b73948a3012c45056929ec8
es183923 <72744903+es183923@users.noreply.github.com>
2021-10-17 16:18:35 -0400

    add action to copy json for colored icon

--- extensions/api-icon-list/src/index.tsx
+++ extensions/api-icon-list/src/index.tsx
@@ -1,9 +1,10 @@
-import { ActionPanel, Color, CopyToClipboardAction, Icon, List } from "@raycast/api";
-import { useState } from "react";
+import { ActionPanel, ActionPanelSection, Color, CopyToClipboardAction, Icon, List } from "@raycast/api";
+import { SetStateAction, useState } from "react";
 
 function enumKeys<O extends object, K extends keyof O = keyof O>(obj: O): K[] {
   return Object.keys(obj).filter(k => Number.isNaN(+k)) as K[];
 }
+
 
 export default function Command() {
   const [color, setColor] = useState<Color>();
@@ -14,25 +15,31 @@ export default function Command() {
         icon={{ source: Icon.Dot, tintColor: Color[colorName] }}
         title={colorName}
         key={colorName}
-        onAction={() => setColor(Color[colorName])}
+        onAction={() => setColor(colorName as SetStateAction<Color | undefined>)}
       />
     );
   }
-
   const items: Element[] = [];
   for (const iconName of enumKeys(Icon)) {
     items.push(
       <List.Item
         title={iconName}
-        icon={{ source: Icon[iconName], tintColor: color }}
+        icon={{ source: Icon[iconName], tintColor: (color ? Color[color] : undefined) }}
         id={iconName}
         key={iconName}
         actions={
           <ActionPanel>
-            <CopyToClipboardAction title="Copy Icon Name" content={iconName} />
-            <ActionPanel.Submenu icon={Icon.Dot} title="Change Color">
-              {actionPanelItems}
-            </ActionPanel.Submenu>
+            <ActionPanelSection title="Copy">
+              <CopyToClipboardAction title="Copy Icon Name" content={iconName} />
+              {
+                color && <CopyToClipboardAction title="Copy JSON for Colored Icon" content={`{ source: Icon.${iconName}, tintColor: Color.${color}}`} />
+              }
+            </ActionPanelSection>
+            <ActionPanelSection title="Color">
+              <ActionPanel.Submenu icon={Icon.Dot} title="Change Color">
+                {actionPanelItems}
+              </ActionPanel.Submenu>
+            </ActionPanelSection>
           </ActionPanel>
         }
       />
