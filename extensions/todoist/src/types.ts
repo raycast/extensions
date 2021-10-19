010d16a184008b2dec714852f6a3e6efd3ed81c9
Thomas Lombart <t.lombart97@gmail.com>
2021-10-19 10:57:20 +0200

    feat: add Todoist extension (#57)
    * feat: add Todoist extension
    
    * Update extensions/todoist/package.json
    
    * Apply suggestions from code review
    
    Co-authored-by: Thomas Paul Mann <hi@thomaspaulmann.com>

---
+++ extensions/todoist/src/types.ts
@@ -0,0 +1,51 @@
+export enum ViewMode {
+  project,
+  date,
+}
+
+export interface SectionWithTasks {
+  name: string;
+  order?: number;
+  tasks: Task[];
+}
+
+export interface Project {
+  favorite: boolean;
+  inbox_project?: boolean;
+  id: number;
+  name: string;
+  url: string;
+}
+
+export interface Section {
+  id: number;
+  name: string;
+  order: number;
+  project_id: number;
+}
+
+export interface DueDate {
+  recurring: boolean;
+  string: string;
+  date: string;
+}
+
+export interface Task {
+  id: number;
+  content: string;
+  description: string;
+  url: string;
+  due?: DueDate;
+  priority: number;
+  section_id: number;
+  project_id: number;
+  order: number;
+}
+
+export type TaskPayload = Partial<{
+  title: string;
+  description: string;
+  project_id: number;
+  priority: number;
+  due_date: string;
+}>;
