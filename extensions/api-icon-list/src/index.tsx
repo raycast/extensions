03c01420ef3b43e9aa6fbbd48100f74091d333f5
es183923 <72744903+es183923@users.noreply.github.com>
2021-10-19 17:18:42 -0400

    Update api-icon-list extension (#79)
    * Update api-icon-list extension
    
    - can change icon colors now
    - directly iterates through `Icon` and `Color` enums, so new icons and colors will automatically show
    
    * lint
    
    * add action to copy json for colored icon
    
    * fix error

--- extensions/api-icon-list/src/index.tsx
+++ extensions/api-icon-list/src/index.tsx
@@ -1,84 +1,53 @@
-import { ActionPanel, CopyToClipboardAction, Icon, List, render } from "@raycast/api";
+import { ActionPanel, ActionPanelSection, Color, CopyToClipboardAction, Icon, List } from "@raycast/api";
+import { SetStateAction, useState } from "react";
 
-type IconItem = [string, Icon];
+function enumKeys<O extends object, K extends keyof O = keyof O>(obj: O): K[] {
+  return Object.keys(obj).filter(k => Number.isNaN(+k)) as K[];
+}
 
 export default function Command() {
-  const iconsList: IconItem[] = [
-    ["ArrowClockwise", Icon.ArrowClockwise],
-    ["ArrowRight", Icon.ArrowRight],
-    ["Binoculars", Icon.Binoculars],
-    ["Bubble", Icon.Bubble],
-    ["Calendar", Icon.Calendar],
-    ["Checkmark", Icon.Checkmark],
-    ["ChevronDown", Icon.ChevronDown],
-    ["ChevronUp", Icon.ChevronUp],
-    ["Circle", Icon.Circle],
-    ["Clipboard", Icon.Clipboard],
-    ["Clock", Icon.Clock],
-    ["Desktop", Icon.Desktop],
-    ["Document", Icon.Document],
-    ["Dot", Icon.Dot],
-    ["Download", Icon.Download],
-    ["Envelope", Icon.Envelope],
-    ["ExclamationMark", Icon.ExclamationMark],
-    ["Eye", Icon.Eye],
-    ["EyeSlash", Icon.EyeSlash],
-    ["Finder", Icon.Finder],
-    ["Gear", Icon.Gear],
-    ["Globe", Icon.Globe],
-    ["Hammer", Icon.Hammer],
-    ["LevelMeter", Icon.LevelMeter],
-    ["Link", Icon.Link],
-    ["List", Icon.List],
-    ["MagnifyingGlass", Icon.MagnifyingGlass],
-    ["MemoryChip", Icon.MemoryChip],
-    ["Message", Icon.Message],
-    ["Pencil", Icon.Pencil],
-    ["Person", Icon.Person],
-    ["Phone", Icon.Phone],
-    ["Pin", Icon.Pin],
-    ["Plus", Icon.Plus],
-    ["Sidebar", Icon.Sidebar],
-    ["SpeakerArrowDown", Icon.SpeakerArrowDown],
-    ["SpeakerArrowUp", Icon.SpeakerArrowUp],
-    ["SpeakerSlash", Icon.SpeakerSlash],
-    ["Star", Icon.Star],
-    ["Text", Icon.Text],
-    ["TextDocument", Icon.TextDocument],
-    ["QuesionMark", Icon.QuestionMark],
-    ["Terminal", Icon.Terminal],
-    ["Trash", Icon.Trash],
-    ["Upload", Icon.Upload],
-    ["Video", Icon.Video],
-    ["Window", Icon.Window],
-    ["XmarkCircle", Icon.XmarkCircle],
-  ];
+  const [color, setColor] = useState<Color>();
+  const actionPanelItems: Element[] = [];
+  for (const colorName of enumKeys(Color)) {
+    actionPanelItems.push(
+      <ActionPanel.Item
+        icon={{ source: Icon.Dot, tintColor: Color[colorName] }}
+        title={colorName}
+        key={colorName}
+        onAction={() => setColor(colorName as SetStateAction<Color | undefined>)}
+      />
+    );
+  }
+  const items: Element[] = [];
+  for (const iconName of enumKeys(Icon)) {
+    items.push(
+      <List.Item
+        title={iconName}
+        icon={{ source: Icon[iconName], tintColor: color }}
+        id={iconName}
+        key={iconName}
+        actions={
+          <ActionPanel>
+            <ActionPanelSection title="Copy">
+              <CopyToClipboardAction title="Copy Icon Name" content={iconName} />
+              {
+                color && <CopyToClipboardAction title="Copy JSON for Colored Icon" content={`{ source: Icon.${iconName}, tintColor: Color.${color}}`} />
+              }
+            </ActionPanelSection>
+            <ActionPanelSection title="Color">
+              <ActionPanel.Submenu icon={Icon.Dot} title="Change Color">
+                {actionPanelItems}
+              </ActionPanel.Submenu>
+            </ActionPanelSection>
+          </ActionPanel>
+        }
+      />
+    );
+  }
 
-  return <IconsList icons={iconsList} />;
-}
-
-function IconsList(props: { icons: IconItem[] }) {
   return (
     <List searchBarPlaceholder="Filter icons by name...">
-      {props.icons.map((icon) => (
-        <IconListItem key={icon[0]} icon={icon} />
-      ))}
+      {items}
     </List>
-  );
-}
-
-function IconListItem(props: { icon: IconItem }) {
-  const icon = props.icon;
-  return (
-    <List.Item
-      id={icon[0]}
-      title={icon[0]}
-      icon={icon[1]}
-      actions={
-        <ActionPanel>
-          <CopyToClipboardAction title="Copy Icon Name" content={`Icon.${icon[0]}`} />
-        </ActionPanel>
-      }
-    />
   );
 }
