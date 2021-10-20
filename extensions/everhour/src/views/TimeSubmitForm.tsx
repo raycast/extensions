bcadd01d34fa7e12137fea44c48dbb9eda3dc0fd
Faris Aziz <farisaziz12@gmail.com>
2021-10-20 23:15:44 +0200

    refactor(time-submit-form): use dropdown

--- extensions/everhour/src/views/TimeSubmitForm.tsx
+++ extensions/everhour/src/views/TimeSubmitForm.tsx
@@ -1,25 +1,49 @@
 import { Form, ActionPanel, SubmitFormAction, showToast, ToastStyle, useNavigation } from "@raycast/api";
 import { submitTaskHours } from "../api";
+import { createResolvedToast } from "../utils";
 
-export function TimeSubmitForm({ taskId, fetchTasks }: { taskId: string; fetchTasks: () => Promise<void> }) {
+const timeOptions = [
+  "0.10",
+  "0.15",
+  "0.25",
+  "0.5",
+  "1",
+  "1.5",
+  "2",
+  "2.5",
+  "3",
+  "3.5",
+  "4",
+  "4.5",
+  "5",
+  "5.5",
+  "6",
+  "6.5",
+  "7",
+  "7.5",
+  "8",
+];
+
+export function TimeSubmitForm({ taskId, refreshRecords }: { taskId: string; refreshRecords: () => Promise<void> }) {
   const { pop } = useNavigation();
 
   const handleSubmit = async ({ hours }: { hours: string }) => {
+    const toast = await showToast(ToastStyle.Animated, "Adding Time");
     try {
       const { taskName } = await submitTaskHours(taskId, hours);
 
       if (taskName) {
-        await fetchTasks();
+        await refreshRecords();
         pop();
-        await showToast(
-          ToastStyle.Success,
+        createResolvedToast(
+          toast,
           `Added ${hours} ${parseInt(hours) === 1 ? "hour" : "hours"} to ${taskName}`
-        );
+        ).success();
       } else {
-        await showToast(ToastStyle.Failure, "Failed to add time");
+        createResolvedToast(toast, "Failed to add time").error();
       }
     } catch (error) {
-      await showToast(ToastStyle.Failure, "Failed to add time");
+      createResolvedToast(toast, "Failed to add time").error();
     }
   };
 
@@ -31,7 +55,11 @@ export function TimeSubmitForm({ taskId, fetchTasks }: { taskId: string; fetchTa
         </ActionPanel>
       }
     >
-      <Form.TextField id="hours" title="Hours" placeholder="0.25" />
+      <Form.Dropdown id="hours" title="Hours" defaultValue="0.25">
+        {timeOptions.map((option) => (
+          <Form.Dropdown.Item value={option} title={option} icon="⏱" />
+        ))}
+      </Form.Dropdown>
     </Form>
   );
 }
