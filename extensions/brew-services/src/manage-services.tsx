519085e4f5961f9ff264e91e43d61ccc56c83224
es183923 <72744903+es183923@users.noreply.github.com>
2021-10-20 16:07:05 -0400

    Update manage-services.tsx

--- extensions/brew-services/src/manage-services.tsx
+++ extensions/brew-services/src/manage-services.tsx
@@ -1,6 +1,5 @@
-import { List } from "@raycast/api";
 import { useEffect, useState } from "react";
-import { createActions, createIcon, getServices, serviceType } from "./services";
+import { BrewItemList, getServices, serviceType } from "./services";
 
 export default function Command() {
   const [services, setServices] = useState<serviceType[]>();
@@ -8,21 +7,5 @@ export default function Command() {
     getServices().then((service) => setServices(service));
   });
 
-  return (
-    <List isLoading={!services} searchBarPlaceholder="Search for services...">
-      {
-        (services ?? []).map(d => (
-          <List.Item
-            id={d.name}
-            key={d.name}
-            title={d.name}
-            subtitle={d.status}
-            accessoryTitle={d.user}
-            icon={createIcon(d.status)}
-            actions={createActions(d)}
-          />
-        ))
-      }
-    </List>
-  );
+  return <BrewItemList services={services} />;
 }
