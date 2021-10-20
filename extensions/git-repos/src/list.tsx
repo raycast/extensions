bec6e5bcfea7702972dc7b3db7478620c4b2cec1
Dean Moore <dmoore@verkadesg.com>
2021-10-20 13:09:03 -0400

    Changed the wrong shortcut in previous commit

--- extensions/git-repos/src/list.tsx
+++ extensions/git-repos/src/list.tsx
@@ -42,7 +42,7 @@ export default function Main(): ReactElement {
                 <ActionPanel.Section>
                   <OpenAction title="Open in VSCode" icon={{fileIcon: "/Applications/Visual Studio Code.app"}} target={repo.fullPath} application="Visual Studio Code" />
                   <OpenAction title="Open in Terminal" icon={{fileIcon: "/Applications/iTerm.app"}} target={repo.fullPath} application="iTerm" />
-                  <OpenWithAction path={repo.fullPath} shortcut={{ modifiers: ["shift", "cmd"], key: "o" }} />
+                  <OpenWithAction path={repo.fullPath} shortcut={{ modifiers: ["cmd"], key: "o" }} />
                   <OpenAction title="Open in Finder" icon={{fileIcon: "/System/Library/CoreServices/Finder.app"}} target={repo.fullPath} application="Finder" shortcut={{ modifiers: ["cmd"], key: "f" }} />
                 </ActionPanel.Section>
                 <ActionPanel.Section>
@@ -51,10 +51,10 @@ export default function Main(): ReactElement {
                       let shortcut = undefined as KeyboardShortcut | undefined
                       switch (remote.name) {
                         case "origin":
-                          shortcut = { modifiers: ["cmd"], key: "o" }
+                          shortcut = { modifiers: ["shift", "cmd"], key: "o" }
                           break
                         case "upstream":
-                          shortcut = { modifiers: ["cmd"], key: "u" }
+                          shortcut = { modifiers: ["shift", "cmd"], key: "u" }
                           break
 
                         default:
