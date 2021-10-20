ae706c96cfc4c1565bb0900c3b295770521a7ee6
Felipe Turcheti <fturcheti@gmail.com>
2021-10-20 15:25:19 -0300

    New extension: Open Finder Selection With…

---
+++ extensions/open-finder-selection-with/src/index.tsx
@@ -0,0 +1,62 @@
+import { getApplications, Application, List, ActionPanel, ActionPanelItem } from "@raycast/api";
+import { useState, useEffect } from "react";
+import { execSync } from "child_process";
+
+export default function ApplicationsList() {
+  const [applications, setApplications] = useState<Application[]>([]);
+
+  useEffect(() => {
+    async function fetchApplications() {
+      setApplications(await getApplications());
+    }
+
+    fetchApplications();
+  }, []);
+
+  return (
+    <List
+      isLoading={applications.length === 0}
+      searchBarPlaceholder="Filter applications by name...">
+      {applications.map((app) => (
+        <ApplicationsListItem key={app.bundleId} application={app} />
+      ))}
+    </List>
+  );
+}
+
+
+function ApplicationsListItem(props: { application: Application }) {
+  const app = props.application;
+
+  const applescript = `
+  on run
+    tell application "Finder"
+      set theItems to selection
+    end tell
+
+    repeat with itemRef in theItems
+      set theItem to POSIX path of (itemRef as string)
+      set command to "open -b ${app.bundleId} " & quoted form of theItem
+      do shell script command
+    end repeat
+  end run
+  `;
+
+  return (
+    <List.Item
+      key={app.bundleId}
+      title={app.name}
+      icon={{ fileIcon: app.path }}
+      actions={
+        <ActionPanel>
+          <ActionPanelItem
+            title={`Open with ${app.name}`}
+            onAction={() => {
+              execSync(`osascript -e '${applescript}'`);
+            }}
+          />
+        </ActionPanel>
+      }
+    />
+  );
+}
