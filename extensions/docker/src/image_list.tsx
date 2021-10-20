e45958b32f2956e6b9708771a6d1acd12e95704b
Priit Haamer <priit@haamer.ee>
2021-10-20 14:35:27 +0300

    feat(docker): Remove image command

--- extensions/docker/src/image_list.tsx
+++ extensions/docker/src/image_list.tsx
@@ -1,36 +1,61 @@
-import { ActionPanel, Color, Detail, Icon, List, PushAction } from '@raycast/api';
+import { ActionPanel, Color, Detail, Icon, List, PushAction, showToast, ToastStyle } from '@raycast/api';
 import { useEffect, useMemo, useState } from 'react';
 import Dockerode, { ImageInfo } from '@priithaamer/dockerode';
 import ImageDetail from './image_detail';
 import { formatBytes, imageTitle } from './docker/image';
 
-export default function ImageList() {
-  const docker = useMemo(() => new Dockerode(), []);
-  const [images, setImages] = useState<{ images: ImageInfo[] }>({ images: [] });
+const useDocker = (docker: Dockerode) => {
+  const [isLoading, setLoading] = useState(false);
+  const [images, setImages] = useState<ImageInfo[]>([]);
   const [error, setError] = useState<Error>();
 
-  useEffect(() => {
-    async function fetchImages() {
-      try {
-        const images = await docker.listImages();
-        setImages({ images });
-      } catch (error) {
-        if (error instanceof Error) {
-          setError(error);
+  const removeImage = async (image: ImageInfo) => {
+    setLoading(true);
+    await docker.getImage(image.Id).remove();
+    const images = await docker.listImages();
+    setImages(images);
+    setLoading(false);
+  };
+
+  return {
+    isLoading,
+    images,
+    error,
+    removeImage,
+    useImages: () =>
+      useEffect(() => {
+        async function fetchImages() {
+          setLoading(true);
+          try {
+            const images = await docker.listImages();
+            setImages(images);
+          } catch (error) {
+            if (error instanceof Error) {
+              setError(error);
+            }
+          } finally {
+            setLoading(false);
+          }
         }
-      }
-    }
-    fetchImages();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+        fetchImages();
+      }, []),
+  };
+};
+
+export default function ImageList() {
+  const docker = useMemo(() => new Dockerode(), []);
+
+  const { isLoading, images, useImages, error, removeImage } = useDocker(docker);
+
+  useImages();
 
   if (error) {
     return <Detail markdown={`## Error connecting to Docker\n\n${error.message}\n`} />;
   }
 
   return (
-    <List>
-      {images.images.map((image) => (
+    <List isLoading={isLoading}>
+      {images.map((image) => (
         <List.Item
           key={image.Id}
           title={imageTitle(image)}
@@ -43,6 +68,15 @@ export default function ImageList() {
                 icon={{ source: Icon.Binoculars }}
                 shortcut={{ modifiers: ['cmd'], key: 'i' }}
                 target={<ImageDetail imageId={image.Id} />}
+              />
+              <ActionPanel.Item
+                title="Remove Image"
+                icon={{ source: Icon.Trash }}
+                shortcut={{ modifiers: ['cmd', 'shift'], key: 'x' }}
+                onAction={async () => {
+                  await removeImage(image);
+                  await showToast(ToastStyle.Success, `Image ${imageTitle(image)} removed`);
+                }}
               />
             </ActionPanel>
           }
