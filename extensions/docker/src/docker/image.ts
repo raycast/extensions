6c5bddcd41e3858ce48b6da07ff05a0490857d73
Priit Haamer <priit@haamer.ee>
2021-10-20 10:13:16 +0300

    refactor(docker): extract image helper functions

---
+++ extensions/docker/src/docker/image.ts
@@ -0,0 +1,14 @@
+import { ImageInfo, ImageInspectInfo } from '@priithaamer/dockerode';
+
+export const imageTitle = (image: ImageInfo | ImageInspectInfo) => image.RepoTags.join(' ');
+
+export const formatBytes = (bytes: number, decimals = 2) => {
+  if (bytes === 0) {
+    return '0 Bytes';
+  }
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+};
--- extensions/docker/src/image_list.tsx
+++ extensions/docker/src/image_list.tsx
@@ -1,13 +1,14 @@
 import { Color, Detail, Icon, List } from '@raycast/api';
 import { useEffect, useState } from 'react';
 import Dockerode, { ImageInfo } from '@priithaamer/dockerode';
+import { formatBytes } from './docker/image';
 
 export default function ImageList() {
   const [images, setImages] = useState<{ images: ImageInfo[] }>({ images: [] });
   const [error, setError] = useState<Error>();
 
   useEffect(() => {
-    async function fetchContainers() {
+    async function fetchImages() {
       try {
         const docker = new Dockerode();
         const images = await docker.listImages();
@@ -18,7 +19,7 @@ export default function ImageList() {
         }
       }
     }
-    fetchContainers();
+    fetchImages();
   }, []);
 
   if (error) {
@@ -38,14 +39,3 @@ export default function ImageList() {
     </List>
   );
 }
-
-const formatBytes = (bytes: number, decimals = 2) => {
-  if (bytes === 0) {
-    return '0 Bytes';
-  }
-  const k = 1024;
-  const dm = decimals < 0 ? 0 : decimals;
-  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-  const i = Math.floor(Math.log(bytes) / Math.log(k));
-  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
-};
