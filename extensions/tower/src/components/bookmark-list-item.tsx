e5a0d150cf24b15fa2e230eb3cae26e79e27282e
Thomas van der Westen <tdwesten@gmail.com>
2021-10-21 22:17:18 +0200

    Clean claen clean

---
+++ extensions/tower/src/components/bookmark-list-item.tsx
@@ -0,0 +1,44 @@
+import { ActionPanel, CopyToClipboardAction, Icon, List, OpenAction, ShowInFinderAction } from "@raycast/api";
+import Bookmark from "../dtos/bookmark-dto";
+import OpenBookMarkAction from "./open-bookmark-action";
+
+export default function BookmarkListItem(props: { bookmark: Bookmark }) {
+  const bookmark: Bookmark = props.bookmark;
+
+  return (
+    <List.Item
+      id={bookmark.RepositoryIdentifier}
+      title={bookmark.Name}
+      subtitle={bookmark.getFolder}
+      icon={Icon.Circle}
+      accessoryTitle={""}
+      actions={
+        <ActionPanel>
+          <ActionPanel.Section>
+            <OpenBookMarkAction bookmark={bookmark} />
+            <ShowInFinderAction path={bookmark.getPath} />
+            <OpenAction
+              title="Open in Code"
+              icon="vscode-icon.png"
+              target={bookmark.getPath}
+              application="Visual Studio Code"
+              shortcut={{ modifiers: ["cmd", "shift"], key: "o" }}
+            />
+          </ActionPanel.Section>
+          <ActionPanel.Section>
+            <CopyToClipboardAction
+              title="Copy Name"
+              content={bookmark.Name}
+              shortcut={{ modifiers: ["cmd"], key: "." }}
+            />
+            <CopyToClipboardAction
+              title="Copy Path"
+              content={bookmark.getPath}
+              shortcut={{ modifiers: ["cmd", "shift"], key: "." }}
+            />
+          </ActionPanel.Section>
+        </ActionPanel>
+      }
+    />
+  );
+}
