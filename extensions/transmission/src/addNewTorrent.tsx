be09b31f296bfd9a2731dc3b407c3de96734982e
Federico Zivolo <federico.zivolo@gmail.com>
2021-10-21 16:18:30 +0200

    add quick paths option

--- extensions/transmission/src/addNewTorrent.tsx
+++ extensions/transmission/src/addNewTorrent.tsx
@@ -1,8 +1,19 @@
-import { Form, ActionPanel, SubmitFormAction, showToast, ToastStyle, popToRoot } from "@raycast/api";
-import { useCallback, useMemo } from "react";
+import {
+  Form,
+  ActionPanel,
+  SubmitFormAction,
+  showToast,
+  ToastStyle,
+  popToRoot,
+  getPreferenceValues,
+} from "@raycast/api";
+import { useState, useCallback, useMemo } from "react";
 import { createClient } from "./modules/client";
 
+const preferences = getPreferenceValues();
+
 export default function AddNewTorrent() {
+  const [downloadDir, setDownloadDir] = useState("");
   const transmission = useMemo(() => createClient(), []);
 
   const handleSubmit = useCallback(async (values: { url: string; downloadDir: string }) => {
@@ -28,7 +39,16 @@ export default function AddNewTorrent() {
       }
     >
       <Form.TextField id="url" placeholder="Magnet Link" />
-      <Form.TextField id="downloadDir" placeholder="Download Directory" />
+      <Form.Dropdown id="quickPath" onChange={setDownloadDir}>
+        <Form.Dropdown.Item value="" title="Choose an option, or type below" />
+        {preferences.quickPaths
+          .split(",")
+          .map((path: string) => path.trim())
+          .map((path: string, index: number) => (
+            <Form.Dropdown.Item key={index} value={path} title={path} />
+          ))}
+      </Form.Dropdown>
+      <Form.TextField id="downloadDir" placeholder="Download Directory" value={downloadDir} onChange={setDownloadDir} />
     </Form>
   );
 }
