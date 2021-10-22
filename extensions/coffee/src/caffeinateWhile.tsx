4f854c231cea1813c72750af15faf1d6ee1b6481
Max Schmidt <max.schmidt@outlook.de>
2021-10-22 15:41:31 +0200

    add 'caffeinate while' feature
    Signed-off-by: Max Schmidt <max.schmidt@outlook.de>

---
+++ extensions/coffee/src/caffeinateWhile.tsx
@@ -0,0 +1,54 @@
+import { Form, ActionPanel, SubmitFormAction } from "@raycast/api";
+import { useEffect, useState } from "react";
+import { runAppleScript } from "run-applescript";
+import Caffeinate from "./caffeinate";
+
+interface Process {
+  [key: string]: string;
+}
+
+const CaffeinateWhile = () => {
+  const [loading, setLoading] = useState(true);
+  const [processes, setProcesses] = useState<Process[]>([]);
+  useEffect(() => {
+    (async () => {
+      const ids = (
+        await runAppleScript(
+          `tell application "System Events" to get the unix id of every process whose background only is false`
+        )
+      ).split(", ");
+      const names = (
+        await runAppleScript(
+          `tell application "System Events" to get the name of every process whose background only is false`
+        )
+      ).split(", ");
+      const arr: Process[] = names.map((value, index) => ({ [value]: ids[index] }));
+      setProcesses(arr);
+      setLoading(false);
+    })();
+  }, []);
+  return (
+    <Form
+      isLoading={loading}
+      actions={
+        <ActionPanel>
+          <SubmitFormAction
+            title="Caffeinate"
+            onSubmit={(data) => {
+              Caffeinate(" -w " + data.process);
+            }}
+          />
+        </ActionPanel>
+      }
+    >
+      <Form.Dropdown id="process" title="App">
+        {processes.map((process) => {
+          const key = Object.keys(process)[0];
+          return <Form.Dropdown.Item key={key} value={process[key]} title={key} />;
+        })}
+      </Form.Dropdown>
+    </Form>
+  );
+};
+
+export default CaffeinateWhile;
