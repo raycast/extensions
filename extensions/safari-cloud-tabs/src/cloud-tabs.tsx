b74880060538c54bfe5a8bb58b48e92df82fd7c4
Loris Guignard <loris.guignard@gmail.com>
2021-10-23 01:14:54 +0200

    Switch to sql.js with WASM

--- extensions/safari-cloud-tabs/src/cloud-tabs.tsx
+++ extensions/safari-cloud-tabs/src/cloud-tabs.tsx
@@ -1,21 +1,33 @@
 import { List, ActionPanel, OpenInBrowserAction, environment } from '@raycast/api';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import os from 'os';
+import path from 'path';
 import { promisify } from 'util';
-import { exec } from 'child_process';
-import csvToJson from 'csvtojson';
+import { readFile } from 'fs';
 import _ from 'lodash';
+import initSqlJs, { Database, ParamsObject } from 'sql.js';
 
-const asyncExec = promisify(exec);
+const asyncReadFile = promisify(readFile);
 const cloudTabsDbPath = `${os.homedir()}/Library/Safari/CloudTabs.db`;
 
-const executeQuery = async (query: string) => {
-  const { stdout, stderr } = await asyncExec(`echo "${query}" | sqlite3 "${cloudTabsDbPath}" -header -csv`);
-  if (stderr) {
-    throw new Error(stderr);
+const loadDb = async (): Promise<Database> => {
+  const fileBuffer = await asyncReadFile(cloudTabsDbPath);
+  const SQL = await initSqlJs({
+    locateFile: () => path.join(environment.assetsPath, 'sql-wasm.wasm'),
+  });
+
+  return new SQL.Database(fileBuffer);
+};
+
+const executeQuery = async (db: Database, query: string): Promise<ParamsObject[]> => {
+  const results = [];
+  const stmt = db.prepare(query);
+  while (stmt.step()) {
+    results.push(stmt.getAsObject());
   }
 
-  return csvToJson().fromString(stdout);
+  stmt.free();
+  return results;
 };
 
 interface Tab {
@@ -25,9 +37,6 @@ interface Tab {
   title: string;
   url: string;
   position: string;
-  is_showing_reader: string;
-  is_pinned: string;
-  reader_scroll_position_page_index: string;
 }
 
 const formatTitle = (title: string) => _.truncate(title, { length: 75 });
@@ -39,19 +48,22 @@ const getFaviconUrl = (url: string) => {
 
 export default function Command() {
   const [tabs, setTabs] = useState<Tab[]>();
-  useEffect(() => {
-    const getTabs = async () => {
-      const tabs = await executeQuery(`
-        SELECT * FROM cloud_tabs t
-        INNER JOIN cloud_tab_devices d
-        ON t.device_uuid = d.device_uuid
-      `);
 
-      setTabs(tabs);
-    };
+  const fetchTabs = useCallback(async () => {
+    const db = await loadDb();
+    const tabs = (await executeQuery(
+      db,
+      `SELECT t.tab_uuid, d.device_uuid, d.device_name, t.title, t.url, t.position
+       FROM cloud_tabs t
+       INNER JOIN cloud_tab_devices d ON t.device_uuid = d.device_uuid`
+    )) as unknown as Tab[];
 
-    getTabs();
+    setTabs(tabs);
   }, []);
+
+  useEffect(() => {
+    fetchTabs();
+  }, [fetchTabs]);
 
   const groupedTabs = _.groupBy(tabs, 'device_uuid');
 
