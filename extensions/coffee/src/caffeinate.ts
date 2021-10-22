cd8e09cbe56e4671fcd3e61d5be476cfde6ea1d2
Max Schmidt <35741000+mooxl@users.noreply.github.com>
2021-10-22 18:03:48 +0200

    add 'caffeinate while' feature (#177)
    Signed-off-by: Max Schmidt <max.schmidt@outlook.de>

--- extensions/coffee/src/caffeinate.ts
+++ extensions/coffee/src/caffeinate.ts
@@ -1,12 +1,14 @@
 import { showHUD } from "@raycast/api";
 import { runAppleScript } from "run-applescript";
 
-export default async function main() {
+const Caffeinate = async (pid?: string) => {
   try {
     await runAppleScript(`do shell script "pgrep caffeinate"`);
     await showHUD("Your Mac is already caffeinated");
   } catch (_) {
-    runAppleScript('do shell script "caffeinate -di"');
+    runAppleScript(`do shell script "caffeinate -di${pid}"`);
     await showHUD("Your Mac is caffeinated");
   }
-}
+};
+
+export default Caffeinate;
