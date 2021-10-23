f262dd51c6ee2278f05cc4f8e0c14e10739aae6d
Felipe Turcheti <fturcheti@gmail.com>
2021-10-23 17:07:41 -0300

    Open With App: Code cleanup
    - Cleaner getFinderSelection logic
    - showHUD with warning when invoking the command with nothing selected in Finder

--- extensions/open-with-app/src/index.tsx
+++ extensions/open-with-app/src/index.tsx
@@ -5,9 +5,11 @@ import {
   Application,
   getApplications,
   closeMainWindow,
+  showHUD,
 } from "@raycast/api";
 import { useState, useEffect } from "react";
 import { runAppleScript } from "run-applescript";
+import { execSync } from "child_process";
 
 export default function ApplicationsList() {
   const [applications, setApplications] = useState<Application[]>([]);
@@ -24,43 +26,60 @@ export default function ApplicationsList() {
     <List
       isLoading={applications.length === 0}
       searchBarPlaceholder="Filter applications by name...">
-      {applications.map((app) => (
-        <ApplicationsListItem key={app.bundleId} application={app} />
+      {applications.map((application) => (
+        <ApplicationsListItem key={application.bundleId} application={application} />
       ))}
     </List>
   );
 }
 
+async function getFinderSelection():Promise<string[]> {
+  // The applescript below returns a string with a list
+  // of the items selected in Finder separated by commas
+  const applescript = `
+  tell application "Finder"
+    set theItems to selection
+  end tell
+
+  set itemsPaths to {}
+
+  repeat with itemRef in theItems
+    set theItem to POSIX path of (itemRef as string)
+    set end of itemsPaths to the theItem
+  end repeat
+
+  return itemsPaths
+  `;
+
+  const response = await runAppleScript(applescript);
+
+  return (response === '')
+    ? []
+    : response.split(',').map((item) => item.trim());
+}
 
 function ApplicationsListItem(props: { application: Application }) {
-  const app = props.application;
-
-  const applescript = `
-  on run
-    tell application "Finder"
-      set theItems to selection
-    end tell
-
-    repeat with itemRef in theItems
-      set theItem to POSIX path of (itemRef as string)
-      set command to "open -b ${app.bundleId} " & quoted form of theItem
-      do shell script command
-    end repeat
-  end run
-  `;
+  const application = props.application;
 
   return (
     <List.Item
-      key={app.bundleId}
-      title={app.name}
-      icon={{ fileIcon: app.path }}
+      key={application.bundleId}
+      title={application.name}
+      icon={{ fileIcon: application.path }}
       actions={
         <ActionPanel>
           <ActionPanelItem
-            title={`Open with ${app.name}`}
-            onAction={() => {
+            title={`Open with ${application.name}`}
+            onAction={async () => {
               closeMainWindow();
-              runAppleScript(applescript);
+              const selectedItems = await getFinderSelection();
+              if (selectedItems.length === 0) {
+                await showHUD(`⚠️  No Finder selection to open.`);
+              } else {
+                selectedItems.forEach((item) => {
+                  execSync(`open -b ${application.bundleId} "${item}"`);
+                })
+              }
             }}
           />
         </ActionPanel>
