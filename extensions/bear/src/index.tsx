d8eb0e94753431db5d9183caddb94585bd468e22
Harry Marr <harry.marr@gmail.com>
2021-10-22 23:39:06 -0400

    Bear extension

---
+++ extensions/bear/src/index.tsx
@@ -0,0 +1,41 @@
+import { ActionPanel, List, OpenAction, PushAction, showToast, ToastStyle } from "@raycast/api";
+import { useEffect, useState } from "react";
+import { formatDistance } from "date-fns";
+import { Note } from "./bear-db";
+import { useBearDb } from "./hooks";
+import PreviewNote from "./preview-note";
+
+export default function main() {
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [db, error] = useBearDb();
+  const [notes, setNotes] = useState<Note[]>();
+  useEffect(() => {
+    if (db != null) {
+      setNotes(db.getNotes(searchQuery));
+    }
+  }, [db, searchQuery]);
+
+  if (error) {
+    showToast(ToastStyle.Failure, "Something went wrong", error.message);
+  }
+
+  return (
+    <List navigationTitle="Search Bear notes" isLoading={!db} onSearchTextChange={setSearchQuery}>
+      {notes?.map((note) => (
+        <List.Item
+          key={note.id}
+          title={note.title}
+          subtitle={note.tags.map((t) => `#${t}`).join(" ")}
+          icon={{ source: "command-icon.png" }}
+          actions={
+            <ActionPanel>
+              <OpenAction title="Open note in Bear" target={`bear://x-callback-url/open-note?id=${note.id}`} />
+              <PushAction title="Show note preview" target={<PreviewNote note={note} />} />
+            </ActionPanel>
+          }
+          accessoryTitle={`modified ${formatDistance(note.modifiedAt, new Date(), { addSuffix: true })}`}
+        />
+      ))}
+    </List>
+  );
+}
