83e13714521806d36744993584e92e86e618bb54
Garrett Tolbert <garrett1.tolbert@gmail.com>
2021-10-23 15:36:34 -0500

    refactor: removed duplicate interface

--- extensions/avery/src/twitter.ts
+++ extensions/avery/src/twitter.ts
@@ -1,12 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import Twit from "twit";
-
-interface Preferences {
-  access_token_secret: string;
-  consumer_key: string;
-  consumer_secret: string;
-  access_token: string;
-}
+import { Preferences } from "./types";
 
 interface Response {
   id: string;
