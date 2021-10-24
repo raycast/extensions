4ed92f30705d16c0eb21dfd18e7a12eba0491aa9
Markus Langer <markus.langer@gis-ag.com>
2021-10-24 21:06:38 +0200

    Add Visual Studio Code Project Manager extension

---
+++ extensions/visual-studio-code-project-manager/src/search-project-manager-projects.tsx
@@ -0,0 +1,138 @@
+import {
+  ActionPanel,
+  CopyToClipboardAction,
+  environment,
+  getPreferenceValues,
+  List,
+  OpenAction,
+  OpenWithAction,
+  ShowInFinderAction,
+  showToast,
+  ToastStyle,
+  TrashAction,
+} from "@raycast/api";
+import { existsSync, readFileSync } from "fs";
+import { homedir } from "os";
+import { dirname } from "path";
+import { ReactElement } from "react";
+import tildify from "tildify";
+import { Preferences, ProjectEntry } from "./types";
+
+const STORAGE = `${homedir()}/Library/Application Support/Code/User/globalStorage/alefragnani.project-manager/projects.json`;
+
+const preferences: Preferences = getPreferenceValues();
+
+function getProjectEntries(): ProjectEntry[] {
+  const storageFile = preferences.projectManagerDataPath || STORAGE;
+  if (!existsSync(storageFile)) {
+    showToast(
+      ToastStyle.Failure,
+      "Projects.json file not found",
+      `File 'projects.json' not found in path: ${storageFile}`
+    );
+    return [];
+  }
+  return JSON.parse(readFileSync(storageFile).toString());
+}
+
+function getSortedProjects(projects: ProjectEntry[]): ProjectEntry[] {
+  const projectsToSort = [...projects];
+  return projectsToSort.sort((a, b) => a.name.localeCompare(b.name));
+}
+
+function getProjectsGroupedByTag(projects: ProjectEntry[]): Map<string, ProjectEntry[]> {
+  const groupedProjects = new Map<string, ProjectEntry[]>();
+
+  projects.forEach((project: ProjectEntry) => {
+    const tags = project.tags.length > 0 ? project.tags : ["[no tags]"];
+    tags.forEach((tag) => {
+      const projects: ProjectEntry[] = [];
+      if (groupedProjects.has(tag)) {
+        projects.push(...(groupedProjects.get(tag) || []));
+      }
+      projects.push(project);
+      groupedProjects.set(tag, projects);
+    });
+  });
+
+  return new Map([...groupedProjects.entries()].sort());
+}
+
+function getProjectsGroupedByTagAsElements(projectEntries: ProjectEntry[]): ReactElement[] {
+  const projectsGrouped = getProjectsGroupedByTag(projectEntries);
+  const elements: ReactElement[] = [];
+  projectsGrouped.forEach((value, key) => {
+    elements.push(
+      <List.Section key={key} title={key}>
+        {value?.map((project) => (
+          <ProjectListItem key={project.rootPath} {...project} />
+        ))}
+      </List.Section>
+    );
+  });
+  return elements;
+}
+
+export default function Command() {
+  const elements: ReactElement[] = [];
+  const projectEntries = getProjectEntries();
+  const sortedProjects = getSortedProjects(projectEntries);
+
+  if (preferences.groupProjectsByTag) {
+    const groupedProjects = getProjectsGroupedByTagAsElements(sortedProjects);
+    elements.push(...groupedProjects);
+  } else {
+    sortedProjects.forEach((project) => {
+      elements.push(<ProjectListItem key={project.rootPath} {...project} />);
+    });
+  }
+
+  return <List searchBarPlaceholder="Search projects ...">{elements}</List>;
+}
+
+function ProjectListItem({ name, rootPath, tags }: ProjectEntry) {
+  const path = rootPath;
+  const prettyPath = tildify(path);
+  const subtitle = dirname(prettyPath);
+  const keywords = tags;
+  return (
+    <List.Item
+      title={name}
+      subtitle={subtitle}
+      icon={{ fileIcon: path }}
+      keywords={keywords}
+      accessoryTitle={tags.join(", ")}
+      actions={
+        <ActionPanel>
+          <ActionPanel.Section>
+            <OpenAction title="Open in Code" icon="icon.png" target={path} application="Visual Studio Code" />
+            <ShowInFinderAction path={path} />
+            <OpenWithAction path={path} shortcut={{ modifiers: ["cmd"], key: "o" }} />
+          </ActionPanel.Section>
+          <ActionPanel.Section>
+            <CopyToClipboardAction title="Copy Name" content={name} shortcut={{ modifiers: ["cmd"], key: "." }} />
+            <CopyToClipboardAction
+              title="Copy Path"
+              content={prettyPath}
+              shortcut={{ modifiers: ["cmd", "shift"], key: "." }}
+            />
+          </ActionPanel.Section>
+          <ActionPanel.Section>
+            <TrashAction paths={[path]} shortcut={{ modifiers: ["ctrl"], key: "x" }} />
+          </ActionPanel.Section>
+          <DevelopmentActionSection />
+        </ActionPanel>
+      }
+    />
+  );
+}
+
+function DevelopmentActionSection() {
+  return environment.isDevelopment ? (
+    <ActionPanel.Section title="Development">
+      <OpenAction title="Open Storage File in Code" icon="icon.png" target={STORAGE} application="Visual Studio Code" />
+      <ShowInFinderAction title="Show Storage File in Finder" path={STORAGE} />
+      <CopyToClipboardAction title="Copy Storage File Path" content={STORAGE} />
+    </ActionPanel.Section>
+  ) : null;
+}
