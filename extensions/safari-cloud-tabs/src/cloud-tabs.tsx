130a08fa24b8c6acada8a7d5fcf34f9a57a9d366
Loris Guignard <loris.guignard@gmail.com>
2021-10-24 13:14:18 +0200

    Add markdown message in case of permission error

--- extensions/safari-cloud-tabs/src/cloud-tabs.tsx
+++ extensions/safari-cloud-tabs/src/cloud-tabs.tsx
@@ -1,19 +1,12 @@
-import { List, ActionPanel, OpenInBrowserAction, environment, OpenAction } from '@raycast/api';
+import { List, ActionPanel, Icon, environment, OpenAction, Detail } from '@raycast/api';
 import { useState, useEffect, useCallback } from 'react';
 import os from 'os';
 import path from 'path';
-import { execSync } from 'child_process';
 import { promisify } from 'util';
 import { readFile } from 'fs';
 import _ from 'lodash';
 import initSqlJs, { Database, ParamsObject } from 'sql.js';
 import execa from 'execa';
-
-// TODO
-// - add try/catch + instructions if missing permissions
-// - current device on top
-// - position
-// - (?) show active tab
 
 const asyncReadFile = promisify(readFile);
 
@@ -37,6 +30,17 @@ const loadDb = async (): Promise<Database> => {
   return new SQL.Database(fileBuffer);
 };
 
+// @TODO: This screen should be handled by Raycast itself (https://github.com/raycast/extensions/issues/101)
+const permissionErrorMarkdown = `## Raycast needs full disk access in order to display your Safari Cloub Tabs.
+
+![Full Disk Access Preferences Pane](https://i.imgur.com/3SAUwrx.png)
+
+1. Open the **Security & Privacy** Preferences pane and select the **Privacy** tab
+2. Select **Full Disk Access** from the list of services
+3. Click the lock icon in the bottom left corner to unlock the interface
+4. Enter your macOS administrator password
+5. Drag and Drop the icon for the **Raycast** application into the list as seen above`;
+
 const executeQuery = async (db: Database, query: string): Promise<ParamsObject[]> => {
   const results = [];
   const stmt = db.prepare(query);
@@ -52,7 +56,7 @@ interface Tab {
   uuid: string;
   title: string;
   url: string;
-  position: string;
+  position: string; // @TODO handle value to sort tabs
   device_uuid: string;
   device_name: string;
 }
@@ -72,39 +76,52 @@ const getFaviconUrl = (url: string) => {
 };
 
 export default function Command() {
+  const [hasPermissionError, setHasPermissionError] = useState(false);
   const [devices, setDevices] = useState<Device[]>();
 
   const fetchDevices = useCallback(async () => {
-    const db = await loadDb();
-    const currentDeviceName = getCurrentDeviceName();
-    const tabs = (await executeQuery(
-      db,
-      `SELECT t.tab_uuid as uuid, d.device_uuid, d.device_name, t.title, t.url, t.position
-       FROM cloud_tabs t
-       INNER JOIN cloud_tab_devices d ON t.device_uuid = d.device_uuid`
-    )) as unknown as Tab[];
+    try {
+      const db = await loadDb();
+      const currentDeviceName = getCurrentDeviceName();
+      const tabs = (await executeQuery(
+        db,
+        `SELECT t.tab_uuid as uuid, d.device_uuid, d.device_name, t.title, t.url, t.position
+         FROM cloud_tabs t
+         INNER JOIN cloud_tab_devices d ON t.device_uuid = d.device_uuid`
+      )) as unknown as Tab[];
 
-    const devices = _.chain(tabs)
-      .groupBy('device_uuid')
-      .reduce((devices: Device[], tabs: Tab[], device_uuid: string) => {
-        devices.push({
-          uuid: device_uuid,
-          name: tabs[0].device_name,
-          is_current: currentDeviceName === tabs[0].device_name,
-          tabs,
-        });
+      const devices = _.chain(tabs)
+        .groupBy('device_uuid')
+        .reduce((devices: Device[], tabs: Tab[], device_uuid: string) => {
+          devices.push({
+            uuid: device_uuid,
+            name: tabs[0].device_name,
+            is_current: currentDeviceName === tabs[0].device_name,
+            tabs,
+          });
 
-        return devices;
-      }, [])
-      .orderBy('is_current', 'desc')
-      .value();
+          return devices;
+        }, [])
+        .orderBy('is_current', 'desc')
+        .value();
 
-    setDevices(devices);
+      setDevices(devices);
+    } catch (err) {
+      if (err instanceof Error && err.message.includes('operation not permitted')) {
+        return setHasPermissionError(true);
+      }
+
+      throw err;
+    }
   }, []);
 
   useEffect(() => {
     fetchDevices();
   }, [fetchDevices]);
+
+  if (hasPermissionError) {
+    return <Detail markdown={permissionErrorMarkdown} />;
+  }
 
   return (
     <List isLoading={!devices}>
@@ -119,9 +136,7 @@ export default function Command() {
               icon={getFaviconUrl(tab.url)}
               actions={
                 <ActionPanel>
-                  <OpenAction title="Open in Safari" target={tab.url} application="Safari" />
-                  <OpenInBrowserAction url={tab.url} />
-                  {environment.isDevelopment && <ActionPanel.Item title="Log" onAction={() => console.log(tab)} />}
+                  <OpenAction title="Open in Safari" target={tab.url} application="Safari" icon={Icon.Globe} />
                 </ActionPanel>
               }
             />
