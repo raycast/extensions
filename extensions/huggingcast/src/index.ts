7557c1d7bc70cb85dc8be6875e974569e268fa00
Ragnor Comerford <hello@ragnor.co>
2021-10-25 16:35:45 +0400

    Clean up

--- extensions/huggingcast/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { showHUD, copyTextToClipboard } from "@raycast/api";
-
-export default async function main() {
-  const now = new Date();
-  await copyTextToClipboard(now.toLocaleDateString());
-  await showHUD("Copied date to clipboard");
-};
