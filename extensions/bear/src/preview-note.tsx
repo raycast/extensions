bdd0110ab03b6790bb7c95be7d5fcb5c5b3b0444
Luca Schultz <45899273+lucaschultz@users.noreply.github.com>
2021-10-27 22:59:19 +0200

    Add clickable file links in preview

--- extensions/bear/src/preview-note.tsx
+++ extensions/bear/src/preview-note.tsx
@@ -1,10 +1,37 @@
 import { Detail } from "@raycast/api";
+import { homedir } from "os";
 import { Note } from "./bear-db";
 import NoteActions from "./note-actions";
 
+const BEAR_LOCAL_FILES_PATH = homedir() + '/Library/Group Containers/9K33E3U3T4.net.shinyfrog.bear/Application Data/Local Files';
+
+export function formatBearAttachments(text: string, forPreview = true): string {
+  let result = text;
+  const matches = result.matchAll(/\[(?<type>file|image):(?<path>.+)\]/g);
+  for (const match of matches) {
+    if (match.groups === undefined) {
+      return result;
+    }
+    let matchReplacement = "";
+    if (match.groups.type === "image" && !forPreview) {
+      const imagePath = `${BEAR_LOCAL_FILES_PATH}/Note Images/${match.groups.path}`;
+      matchReplacement = `![](${imagePath})`;
+    } else {
+      const fileLink = encodeURI(
+        `file://${BEAR_LOCAL_FILES_PATH}/${match.groups.type === "file" ? "Note Files" : "Note Images"}/${
+          match.groups.path
+        }`
+      );
+      matchReplacement = `[Show attached ${match.groups.type}](${fileLink})`;
+    }
+    result = result.replace(match[0], matchReplacement);
+  }
+  return result;
+}
+
 export default function PreviewNote({ note }: { note: Note }) {
   return (<Detail
-    markdown={note.text}
+    markdown={formatBearAttachments(note.text)}
     navigationTitle={note.title}
     actions={<NoteActions isNotePreview={true} note={ note }/>}
   />);
