dc59e24e09ac67bfabec7d0a2ab5f991b53b9727
Luca Schultz <45899273+lucaschultz@users.noreply.github.com>
2021-10-27 22:45:56 +0200

    Add actions and navigation title to note preview

--- extensions/bear/src/preview-note.tsx
+++ extensions/bear/src/preview-note.tsx
@@ -1,6 +1,11 @@
 import { Detail } from "@raycast/api";
 import { Note } from "./bear-db";
+import NoteActions from "./note-actions";
 
 export default function PreviewNote({ note }: { note: Note }) {
-  return <Detail markdown={note.text}></Detail>;
+  return (<Detail
+    markdown={note.text}
+    navigationTitle={note.title}
+    actions={<NoteActions isNotePreview={true} note={ note }/>}
+  />);
 }
