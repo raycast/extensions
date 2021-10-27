575e25f5b06dae07d162336cf238b7f16f9b5f54
Luca Schultz <45899273+lucaschultz@users.noreply.github.com>
2021-10-27 22:51:09 +0200

    Improve tag and title formatting
    Untitled notes are now called untitled notes, subtags aren't displayed by themselves and tags with spaces in them wrapped in #

--- extensions/bear/src/note-item.tsx
+++ extensions/bear/src/note-item.tsx
@@ -3,17 +3,37 @@ import { formatDistanceToNowStrict } from "date-fns";
 import { Note } from "./bear-db";
 import NoteActions from "./note-actions";
 
+/**
+ * Get a nicely formatted string of tags from an array of tags
+ *
+ * @param tagStr - A `SqlVallue` containing the string of comma seperated tags
+ * @returns
+ */
+function formatTags(tags: string[]): string {
+  if (tags.length === 0) {
+    return "Not Tagged";
+  }
+  const formattedTags = [];
+  for (const tag of tags) {
+    // Only format tag if tah is not subtag
+    if (tags.filter((t) => t.includes(tag)).length < 2) {
+      // Format tags with spaces like Bear (two hashtags)
+      formattedTags.push(tag.includes(" ") ? `#${tag}#` : `#${tag}`);
+    }
+  }
+  return formattedTags.join(" ");
+}
+
 export default function NoteItem({ note }: { note: Note }) {
   return (
     <List.Item
       key={note.id}
-      title={note.title}
-      subtitle={note.tags.map((t) => `#${t}`).join(" ")}
+      title={note.title === "" ? "Untitled Note" : note.title}
+      subtitle={formatTags(note.tags)}
       icon={{ source: "command-icon.png" }}
       keywords={[note.id]}
-      actions={<NoteActions isNotePreview={false} note={ note }/>}
+      actions={<NoteActions isNotePreview={false} note={note} />}
       accessoryTitle={`edited ${formatDistanceToNowStrict(note.modifiedAt, { addSuffix: true })}`}
     />
   );
 }
-
