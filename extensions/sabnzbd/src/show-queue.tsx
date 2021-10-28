9c824a37dbfeea8780b48ae1bc7db4477335f426
Jesse Claven <jesse.claven@me.com>
2021-10-28 21:28:50 +0100

    Add actions for queue slots

--- extensions/sabnzbd/src/show-queue.tsx
+++ extensions/sabnzbd/src/show-queue.tsx
@@ -1,7 +1,7 @@
-import { List, showToast, ToastStyle, preferences, Icon, ImageLike } from "@raycast/api";
+import { List, showToast, ToastStyle, preferences, Icon, ImageLike, ActionPanel } from "@raycast/api";
 import { useState, useEffect } from "react";
 import fetch from "node-fetch";
-import { Client, Queue, QueueSlot } from "sabnzbd-api";
+import { Client, Queue, QueueSlot, Results } from "sabnzbd-api";
 
 export default function SlotList() {
   const [state, setState] = useState<{ slots: QueueSlot[] }>({ slots: [] });
@@ -32,20 +32,54 @@ function SlotListItem(props: { slot: QueueSlot }) {
 
   let icon: ImageLike;
 
+  let actions: any;
+
   switch (slot.status) {
     case "Paused":
       icon = Icon.ChevronDown;
+
+      actions = (
+        <ActionPanel>
+          <ActionPanel.Item title={"Resume"} onAction={() => onResume(slot)} />
+          <ActionPanel.Item title={"Delete"} onAction={() => onDelete(slot)} />
+        </ActionPanel>
+      );
+
       break;
 
     case "Downloading":
       icon = Icon.ChevronUp;
+
+      actions = (
+        <ActionPanel>
+          <ActionPanel.Item title={"Pause"} onAction={() => onPause(slot)} />
+          <ActionPanel.Item title={"Delete"} onAction={() => onDelete(slot)} />
+        </ActionPanel>
+      );
+
       break;
 
     case "Queued":
       icon = Icon.Dot;
+
+      actions = (
+        <ActionPanel>
+          <ActionPanel.Item title={"Resume"} onAction={() => onResume(slot)} />
+          <ActionPanel.Item title={"Delete"} onAction={() => onDelete(slot)} />
+        </ActionPanel>
+      );
+
       break;
 
     default:
+      actions = (
+        <ActionPanel>
+          <ActionPanel.Item title={"Delete"} onAction={() => onDelete(slot)} />
+        </ActionPanel>
+      );
+
+      console.log(`Unknown slot status: ${slot.status}`);
+
       icon = Icon.QuestionMark;
   }
 
@@ -57,7 +91,58 @@ function SlotListItem(props: { slot: QueueSlot }) {
     timeleft = "";
   }
 
-  return <List.Item id={slot.nzo_id} key={slot.nzo_id} title={slot.filename} subtitle={timeleft} icon={icon} />;
+  return (
+    <List.Item
+      id={slot.nzo_id}
+      key={slot.nzo_id}
+      title={slot.filename}
+      subtitle={timeleft}
+      icon={icon}
+      actions={actions}
+    />
+  );
+}
+
+async function onDelete(slot: QueueSlot) {
+  console.log("Delete slot");
+
+  let client = new Client(preferences.url.value as string, preferences.apiToken.value as string);
+
+  try {
+    const results = (await client.jobDelete(slot.nzo_id)) as Results;
+    showToast(ToastStyle.Success, "Deleted job");
+  } catch (error) {
+    console.error(error);
+    showToast(ToastStyle.Failure, "Could not delete job");
+  }
+}
+
+async function onPause(slot: QueueSlot) {
+  console.log("Pause slot");
+
+  let client = new Client(preferences.url.value as string, preferences.apiToken.value as string);
+
+  try {
+    const results = (await client.jobPause(slot.nzo_id)) as Results;
+    showToast(ToastStyle.Success, "Paused job");
+  } catch (error) {
+    console.error(error);
+    showToast(ToastStyle.Failure, "Could not pause job");
+  }
+}
+
+async function onResume(slot: QueueSlot) {
+  console.log("Resume slot");
+
+  let client = new Client(preferences.url.value as string, preferences.apiToken.value as string);
+
+  try {
+    const results = (await client.jobResume(slot.nzo_id)) as Results;
+    showToast(ToastStyle.Success, "Resumed job");
+  } catch (error) {
+    console.error(error);
+    showToast(ToastStyle.Failure, "Could not resume job");
+  }
 }
 
 async function fetchSlots(): Promise<QueueSlot[]> {
