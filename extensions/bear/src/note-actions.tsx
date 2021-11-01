3c3f383eb3f957f5259a368299a2500097f1a3b2
Luca Schultz <45899273+lucaschultz@users.noreply.github.com>
2021-11-01 18:27:02 +0100

    Add more sensible way to deal with encrypted notes
    Sergey managed to get the search for encrypted notes working in 376ad6de , however this leads to problems since some of the actions rely on having access to the text of the note and some functions of the official Bear URL scheme are not available for encrypted notes.
    
    I removed actions (which won't work with encrypted notes) from the actions panel if an encrypted note is selected in Raycast.

--- extensions/bear/src/note-actions.tsx
+++ extensions/bear/src/note-actions.tsx
@@ -45,66 +45,84 @@ export default function NoteActions({ isNotePreview, note }: { isNotePreview: bo
             target={`bear://x-callback-url/open-note?id=${note.id}&edit=yes`}
             icon={Icon.Sidebar}
           />
-          <OpenAction
-            title="Open in New Bear Window"
-            target={`bear://x-callback-url/open-note?id=${note.id}&new_window=yes&edit=yes`}
-            icon={Icon.Window}
-          />
-        </ActionPanel.Section>
-        <ActionPanel.Section title="Edit">
-        <PushAction
-            title="Add Text"
-            icon={Icon.Plus}
-            shortcut={{ modifiers: ["cmd"], key: 't' }}
-            target={<AddText note={note}/>}
-          />
-          <OpenAction
-            title="Move to Archive"
-            target={`bear://x-callback-url/archive?id=${note.id}&show_window=yes`}
-            icon={{ source: Icon.List, tintColor: Color.Orange }}
-            shortcut={{ modifiers: ["cmd", "shift"], key:"backspace"}}
-          />
-          <OpenAction
-            title="Move to Trash"
-            target={`bear://x-callback-url/trash?id=${note.id}&show_window=yes`}
-            icon={{ source: Icon.Trash, tintColor: Color.Red }}
-            shortcut={{ modifiers: ["cmd"], key:"backspace"}}
-          />
-        </ActionPanel.Section>
-        <ActionPanel.Section title="Show in Raycast">
-          { isNotePreview ? null : <NotePreviewAction note={ note }/> }
-          <PushAction
-            title="Show Note Links"
-            target={<NoteLinks note={note} />}
-            icon={Icon.Link}
-            shortcut={{ modifiers: ["cmd"], key: "l" }}
+          {note.encrypted ?
+            null :
+            <OpenAction
+              title="Open in New Bear Window"
+              target={`bear://x-callback-url/open-note?id=${note.id}&new_window=yes&edit=yes`}
+              icon={Icon.Window}
             />
+          }
         </ActionPanel.Section>
+        {note.encrypted ?
+          null :
+          <ActionPanel.Section title="Edit">
+            <PushAction
+              title="Add Text"
+              icon={Icon.Plus}
+              shortcut={{ modifiers: ["cmd"], key: 't' }}
+              target={<AddText note={note}/>}
+            />
+            <OpenAction
+              title="Move to Archive"
+              target={`bear://x-callback-url/archive?id=${note.id}&show_window=yes`}
+              icon={{ source: Icon.List, tintColor: Color.Orange }}
+              shortcut={{ modifiers: ["cmd", "shift"], key:"backspace"}}
+            />
+            <OpenAction
+              title="Move to Trash"
+              target={`bear://x-callback-url/trash?id=${note.id}&show_window=yes`}
+              icon={{ source: Icon.Trash, tintColor: Color.Red }}
+              shortcut={{ modifiers: ["cmd"], key:"backspace"}}
+            />
+          </ActionPanel.Section>
+        }
+        {note.encrypted ?
+          null :
+          <ActionPanel.Section title="Show in Raycast">
+            { isNotePreview ? null : <NotePreviewAction note={ note }/> }
+            <PushAction
+              title="Show Note Links"
+              target={<NoteLinks note={note} />}
+              icon={Icon.Link}
+              shortcut={{ modifiers: ["cmd"], key: "l" }}
+              />
+          </ActionPanel.Section>
+        }
         <ActionPanel.Section title="Copy">
-          <CopyToClipboardAction
-            title="Copy Markdown"
-            content={formatBearAttachments(note.text, false)}
-            icon={Icon.Clipboard}
-            shortcut={{ modifiers: ["cmd"], key: "c" }}
+        { note.encrypted ? 
+          null :
+            <CopyToClipboardAction
+              title="Copy Markdown"
+              content={formatBearAttachments(note.text, false)}
+              icon={Icon.Clipboard}
+              shortcut={{ modifiers: ["cmd"], key: "c" }}
           />
-          <CopyToClipboardAction
-            title="Copy HTML"
-            content={renderMarkdown(note.text)}
-            icon={Icon.Terminal}
-            shortcut={{ modifiers: ["cmd", "shift"], key: "c" }}
+        }
+        { note.encrypted ? 
+            null :
+            <CopyToClipboardAction
+              title="Copy HTML"
+              content={renderMarkdown(note.text)}
+              icon={Icon.Terminal}
+              shortcut={{ modifiers: ["cmd", "shift"], key: "c" }}
           />
+        }
           <CopyToClipboardAction
             title="Copy Link to Note"
             content={`bear://x-callback-url/open-note?id=${note.id}`}
             icon={Icon.Link}
             shortcut={{ modifiers: ["cmd", "opt"], key: "c" }}
           />
-          <CopyToClipboardAction
-            title="Copy Unique Identifier"
-            content={`note.id`}
-            icon={Icon.QuestionMark}
-            shortcut={{ modifiers: ["cmd", "opt", 'shift'], key: "c" }}
+          {note.encrypted ? 
+            null :
+            <CopyToClipboardAction
+              title="Copy Unique Identifier"
+              content={`note.id`}
+              icon={Icon.QuestionMark}
+              shortcut={{ modifiers: ["cmd", "opt", 'shift'], key: "c" }}
             />
+          }
         </ActionPanel.Section>
         <ActionPanel.Section title="Create">
           <PushAction
