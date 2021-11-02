b4a80bc906f7b28dc0f82a7f1df8aaa88c849dbd
Achille Lacoin <achille.lacoin@dailymotion.com>
2021-11-02 21:35:23 +0100

    Bitwarden Extension: Add Support for Apple Silicon Macs, Add Lock Vault command (#263)
    * Add support apple silicon macs
    
    * Update pref title
    
    * search bw cli in brew directories by default
    
    * extract PATH to a global var
    
    * fix @pernielsentikaer comments
    
    * Update troubleshooting guide
    
    * Add ability to lock the vault
    
    * Group favorites to the top
    
    * Update extensions/bitwarden/src/hooks.ts
    
    Co-authored-by: Petr Nikolaev <petr@raycast.com>

--- extensions/bitwarden/src/components.tsx
+++ extensions/bitwarden/src/components.tsx
@@ -6,19 +6,16 @@ import {
   SubmitFormAction,
   Detail,
   CopyToClipboardAction,
-  getPreferenceValues,
 } from "@raycast/api";
 import { Bitwarden } from "./api";
 
 export function TroubleshootingGuide(): JSX.Element {
   showToast(ToastStyle.Failure, "Bitwarden CLI not found");
-  const { path } = getPreferenceValues();
   const markdown = `# The Bitwarden CLI was not found
 ## Please check that:
 
 1. The Bitwarden CLI is [correctly installed](https://bitwarden.com/help/article/cli/#download-and-install)
-1. The path of the installation matches the Bitwarden CLI Installation Path extension setting
-  > Currently set to: \`${path}\`
+1. If you did not install bitwarden using brew, please check that path of the installation matches the \`Bitwarden CLI Installation Path\` extension setting
 `;
 
   return (
