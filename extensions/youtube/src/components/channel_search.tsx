b07c6698d788f32fc0540c969d743757d04f4307
Michael Aigner <tonka3100@gmail.com>
2021-11-02 22:07:08 +0100

    add recent search list

--- extensions/youtube/src/components/channel_search.tsx
+++ extensions/youtube/src/components/channel_search.tsx
@@ -3,9 +3,15 @@ import { useState } from "react";
 import { getErrorMessage } from "../lib/utils";
 import { Channel, searchChannels, useRefresher } from "../lib/youtubeapi";
 import { ChannelListItem } from "./channel";
+import { RecentSearchesList, useRecentSearch } from "./search";
 
 export function SearchChannelList() {
   const [searchText, setSearchText] = useState<string>();
+  const {
+    data: rc,
+    appendRecentSearches,
+    clearAllRecentSearches,
+  } = useRecentSearch("recent_channel_searches", setSearchText);
   const { data, error, isLoading } = useRefresher<Channel[] | undefined>(async () => {
     if (searchText) {
       return await searchChannels(searchText);
@@ -13,14 +19,25 @@ export function SearchChannelList() {
     return undefined;
   }, [searchText]);
   if (error) {
-    showToast(ToastStyle.Failure, "Could not search videos", getErrorMessage(error));
+    showToast(ToastStyle.Failure, "Could not search channels", getErrorMessage(error));
   }
-
-  return (
-    <List isLoading={isLoading} onSearchTextChange={setSearchText} throttle={true}>
-      {data?.map((c) => (
-        <ChannelListItem key={c.id} channel={c} />
-      ))}
-    </List>
-  );
+  if (data) {
+    return (
+      <List isLoading={isLoading} onSearchTextChange={setSearchText} throttle={true}>
+        {data?.map((c) => (
+          <ChannelListItem key={c.id} channel={c} />
+        ))}
+      </List>
+    );
+  } else {
+    const isLoadingTotal = !searchText ? rc === undefined : true;
+    return (
+      <RecentSearchesList
+        recentSearches={rc}
+        isLoading={isLoadingTotal}
+        setRootSearchText={appendRecentSearches}
+        clearAll={clearAllRecentSearches}
+      />
+    );
+  }
 }
