dd49867b03e817b9f0e7a021c4ad4daca903669b
George <george@mukuru.com>
2021-11-05 02:17:48 +0400

    Open JetBrains Projects: Add better feedback when cli tools are missing (#268)
    * Add better feedback when cli tools are missing
    
    * Autodiscover files based on '.history.json' files
    
    * Tools location preference and locator
    
    * Error handling for history file
    
    * UX tweaks

--- extensions/jetbrains/src/util.ts
+++ extensions/jetbrains/src/util.ts
@@ -1,20 +1,48 @@
-import {preferences} from "@raycast/api";
-import {lstat} from "fs/promises";
-import fg from "fast-glob";
-import {basename} from "path";
+import {preferences} from "@raycast/api"
+import {lstat, readFile, stat} from "fs/promises"
+import fg from "fast-glob"
+import {basename, dirname} from "path"
+import {parseStringPromise} from "xml2js"
+import {homedir} from "os"
+import Fuse from "fuse.js"
 
-export const preferredApp = String(preferences["app"].value) ?? String(preferences["app"].default);
+export const JetBrainsIcon = "jb.png"
+
+export const preferredApp = String(preferences["app"].value || preferences["app"].default)
+export const bin = String(preferences["bin"].value || preferences["bin"].default).replace('~', homedir())
 
 export interface file {
-  title: string;
-  isDir: boolean;
-  icon: string;
-  lastModifiedAt: Date;
-  path: string;
-  app?: file;
+  title: string
+  icon: string
+  path: string
+  isDir: boolean
+  lastModifiedAt: Date
 }
 
-export function getFiles(dir: string): Promise<Array<file>> {
+export interface recentEntry {
+  title: string
+  icon: string
+  path: string
+  dirname: string
+  parts: string
+  opened: number
+  app: string
+  exists?: boolean
+  filter?: number
+}
+
+export interface AppHistory {
+  title: string
+  url: string|false
+  tool: string|false
+  icon: string
+  xmlFiles: file[]
+  entries?: recentEntry[]
+  fused?: Fuse<recentEntry>
+}
+
+
+export function getFiles(dir: string | string[]): Promise<Array<file>> {
   return fg(dir).then(async (result) => await Promise.all(result.map(
     (path) => lstat(path).then(stat => ({
       title: path.split("/").reverse()[0],
@@ -26,47 +54,69 @@ export function getFiles(dir: string): Promise<Array<file>> {
   )))
 }
 
-export function listAnd(entries: IterableIterator<string> | Array<string>): string {
-  const arr = Array.from(entries);
-  const last = arr.pop();
-  return last ? `${arr.join(", ")} and ${last}` : "";
-}
-
-const rightTools = new Map<string, file>();
-const wrongTools = new Map<string, Array<file>>();
-
-export function getRightTool(tools: Array<file>, app: string): file {
-  if (!rightTools.has(app)) {
-    rightTools.set(app, tools.find((tool) => app.match(new RegExp(basename(tool.path), "i"))) ?? tools[0]);
-  }
-  return rightTools.get(app) ?? tools[0];
-}
-
-export function getOtherTools(tools: Array<file>, app: string, rightTool: file): Array<file> {
-  if (!wrongTools.has(app)) {
-    wrongTools.set(
-      app,
-      tools.filter((tool) => tool.title !== rightTool.title)
-    );
-  }
-  return wrongTools.get(app) ?? tools;
-}
-
 export const createUniqueArray = <T>(s: string, values: Array<T>): Array<T> => {
   if (values.length == 0) {
-    return values;
+    return values
   }
-  const check = (values[0] as Record<string, unknown>)[s];
+  const check = (values[0] as Record<string, unknown>)[s]
   if (typeof check !== "string") {
-    throw new Error("Not a string type");
+    throw new Error("Not a string type")
   }
-  const set = new Set<string>();
+  const set = new Set<string>()
   return values.reduce((arr: Array<T>, next) => {
-    const check = String((next as Record<string, unknown>)[s]);
+    const check = String((next as Record<string, unknown>)[s])
     if (!set.has(check)) {
-      set.add(check);
-      return [...arr, next];
+      set.add(check)
+      return [...arr, next]
     }
-    return arr;
-  }, [] as Array<T>);
-};
+    return arr
+  }, [] as Array<T>)
+}
+
+interface xmlJson {
+  _attr: {
+    [key: string]: string
+  }
+  value: Array<xmlJson>
+  RecentProjectMetaInfo: Array<xmlJson>
+  option: Array<xmlJson>
+}
+
+export async function getRecentEntries(xmlFile: file, app: AppHistory): Promise<Array<recentEntry>> {
+  return readFile(xmlFile.path)
+    .then((xmlFile) =>
+      parseStringPromise(xmlFile, {attrkey: "_attr"}).then((result) => {
+        return (result.application.component[0].option[0].map[0].entry ?? [])
+          .map(
+            // convert xmlJson object to array of recentEntries
+            (recentEntry: xmlJson): recentEntry => {
+              const projectOpenTimestamp = (recentEntry.value[0].RecentProjectMetaInfo[0].option ?? []).find(
+                (recentOption: xmlJson) => recentOption._attr.name === "projectOpenTimestamp"
+              )
+              const path = recentEntry._attr.key.replace("$USER_HOME$", homedir())
+              return {
+                title: basename(path),
+                icon: app.icon ?? JetBrainsIcon,
+                path: path,
+                dirname: dirname(path)
+                  .replace(homedir(), "~")
+                  .replace("/Volumes", ""),
+                opened: Number(projectOpenTimestamp?._attr.value ?? 0),
+                parts: path.substr(1).split("/").reverse().slice(1).join(" ← "),
+                app: app.title,
+              }
+            }
+          )
+          // check if the file actually exists to prevent breaking open with actions
+          .map(async (recent: recentEntry) => {
+            return {
+              ...recent,
+              exists: await stat(recent.path)
+                .then(() => true)
+                .catch(() => false),
+            } as recentEntry
+          })
+      })
+    )
+    .then(async (entries) => await Promise.all(entries))
+}
