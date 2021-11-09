3f7dfb22370c317205159e5bac2563846a577df7
Thiago Holanda <unnamedd@gmail.com>
2021-11-09 21:24:07 +0100

    Add prop parentName into GroupSection element

--- extensions/script-commands/src/components/GroupSection.tsx
+++ extensions/script-commands/src/components/GroupSection.tsx
@@ -5,18 +5,22 @@ import { ScriptCommandItem } from "@components";
 import { Group, ScriptCommand } from "@models";
 
 type Props = { 
-  group: Group 
+  group: Group, 
+  parentName?: string
 }
 
-export function GroupSection({ group }: Props) {
+export function GroupSection({ group, parentName }: Props) {
   const key = `${group.name}-${group.path}`
 
   group.scriptCommands.sort((left: ScriptCommand, right: ScriptCommand) => {
     return (left.title > right.title) ? 1 : -1
   })
 
+  const title = parentName ?? group.name
+  const subtitle = parentName != null ? group.name : ""
+
   return (
-    <List.Section key={key} title={group.name}>
+    <List.Section key={key} title={ title } subtitle={ subtitle }>
       {group.scriptCommands.map((scriptCommand) => (
         <ScriptCommandItem scriptCommand={scriptCommand} />
       ))}
