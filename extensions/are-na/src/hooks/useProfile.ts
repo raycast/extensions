4e4908399ec680695540dd6fd262e31e989d741a
Talor Anderson <ta.anders17@gmail.com>
2021-11-09 21:47:26 -0800

    better types for /me

--- extensions/are-na/src/hooks/useProfile.ts
+++ extensions/are-na/src/hooks/useProfile.ts
@@ -1,12 +1,12 @@
 import { useCallback } from "react";
 import { useAsync } from "react-async";
-import { User } from "../types/arena";
+import { Me } from "../types/arena";
 import { api } from "../util/api";
 import { useToken } from "./useToken";
 
 export const useProfile = () => {
   const accessToken = useToken();
 
-  const promiseFn = useCallback(() => api(accessToken)<User>("GET", "/me"), []);
-  return useAsync<User>(promiseFn);
+  const promiseFn = useCallback(() => api(accessToken)<Me>("GET", "/me"), []);
+  return useAsync<Me>(promiseFn);
 };
--- extensions/are-na/src/types/arena.ts
+++ extensions/are-na/src/types/arena.ts
@@ -87,6 +87,44 @@ export interface User {
   class: string;
   initials: string;
 }
+
+export interface AvatarImage {
+  thumb: string;
+  display: string;
+}
+
+export interface Metadata {
+  description?: any;
+}
+
+export interface Me {
+  id: number;
+  created_at: Date;
+  slug: string;
+  username: string;
+  first_name: string;
+  last_name: string;
+  full_name: string;
+  avatar: string;
+  avatar_image: AvatarImage;
+  channel_count: number;
+  following_count: number;
+  profile_id: number;
+  follower_count: number;
+  initials: string;
+  can_index: boolean;
+  metadata: Metadata;
+  is_premium: boolean;
+  is_lifetime_premium: boolean;
+  is_supporter: boolean;
+  is_exceeding_connections_limit: boolean;
+  is_confirmed: boolean;
+  is_pending_reconfirmation: boolean;
+  is_pending_confirmation: boolean;
+  badge: string;
+  base_class: string;
+  class: string;
+}
 /**
  Attributes
 id 	(Integer) 	The internal ID of the block
