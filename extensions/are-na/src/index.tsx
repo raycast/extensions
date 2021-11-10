450ba484e4d8f6b632df028f4d2ef142a7a0b646
Talor Anderson <ta.anders17@gmail.com>
2021-11-09 21:47:16 -0800

    clean up profile view

--- extensions/are-na/src/index.tsx
+++ extensions/are-na/src/index.tsx
@@ -1,25 +1,35 @@
-import { Detail, showToast, ToastStyle } from "@raycast/api";
+import { ActionPanel, Detail, OpenInBrowserAction, showToast, ToastStyle } from "@raycast/api";
 import { useEffect } from "react";
 import { useProfile } from "./hooks/useProfile";
-import { prefs } from "./util/preferences";
+import { Me } from "./types/arena";
+
+const genMarkdown = (user: Me) => `
+# ${user.username}${user.is_premium ? " ++" : ""}
+[https://are.na/${user.slug}](https://are.na/${user.slug})
+
+[${user.follower_count} followers](https://are.na/${user.slug}/followers) / 
+[${user.following_count} following](https://are.na/${user.slug}/following)
+`;
 
 export default function Command() {
   const { data: user, error } = useProfile();
 
   useEffect(() => {
-    if (error) showToast(ToastStyle.Failure, "An Error Occured.", (error as any).message);
+    if (error) showToast(ToastStyle.Failure, "An Error Occured.", error.message);
   }, [error]);
   return (
     <Detail
       isLoading={!user}
-      markdown={`
-# Hello world!
-debug:
-\`\`\`
-${JSON.stringify(user, null, 2)}
-\`\`\`
-
-`}
+      markdown={user ? genMarkdown(user) : ""}
+      actions={
+        user ? (
+          <ActionPanel>
+            <OpenInBrowserAction url={`https://are.na/${user.slug}`} title={"Open in Browser"} />
+            <OpenInBrowserAction url={`https://are.na/${user.slug}/followers`} title="Go to Followers" />
+            <OpenInBrowserAction url={`https://are.na/${user.slug}/following`} title="Go to Following" />
+          </ActionPanel>
+        ) : null
+      }
     />
   );
 }
