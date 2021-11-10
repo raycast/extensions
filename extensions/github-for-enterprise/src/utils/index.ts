ca7b58be462ffe8377ff8f9b4de015dda2879a3a
Kinbaum <mkinbaum@gmail.com>
2021-11-10 03:42:57 -0500

    GitHub for Enterprise (#261)
    * chore: initial commit of GitHub for Enterprise
    
    * feat: add icons for dark ui
    
    * fix: package-lock error
    
    * fix: dimenstions for icons
    
    * docs: adding readme file to get started
    
    * chore: updating package dependencies
    
    * chore: updating package dependencies

---
+++ extensions/github-for-enterprise/src/utils/index.ts
@@ -0,0 +1,42 @@
+import { getPreferenceValues } from '@raycast/api';
+import { request } from 'graphql-request';
+
+export async function fetcher({ document, variables = {}, headers = {} }: any) {
+    const { graphqlEndpoint, username, token } = getPreferenceValues();
+    
+    return request(graphqlEndpoint, document, {
+        username,
+        ...variables
+    }, {
+        Authorization: `Bearer ${token}`,
+        ...headers
+    });
+}
+
+export const groupBy = <T, K extends keyof T>(arr: T[], key: K) => {
+    return arr.reduce((r, a) => {
+        const v = a[key];
+        (r as any)[v] = [ ...(r as any)[v] || [], a ];
+        return r;
+    }, {} as Record<string, T[]>);
+};
+
+export const partition = <T>(arr: T[], predicate: (item: T) => boolean) => {
+    const pass = [] as T[];
+    const fail = [] as T[];
+
+    for (const item of arr) {
+        if (predicate(item)) {
+            pass.push(item);
+        } else {
+            fail.push(item);
+        }
+    }
+
+    return [ pass, fail ];
+};
+
+export const plural = (count: number = 0, text: string, returnCount: boolean = true) => {
+    const str = text + (count === 0 || count > 1 ? 's' : '');
+    return returnCount ? count + ' ' + str : str;
+};
