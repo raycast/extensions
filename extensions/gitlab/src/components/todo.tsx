c1caa73469f7c1d044e818b360d94e1eb069a19a
Daishi Kato <dai-shi@users.noreply.github.com>
2021-11-12 17:59:54 +0900

    refactor(gitlab): avoid stale value (#328)
    * refactor(gitlab): avoid stale value
    
    * a fix

--- extensions/gitlab/src/components/todo.tsx
+++ extensions/gitlab/src/components/todo.tsx
@@ -82,11 +82,13 @@ export function useSearch(query: string | undefined): {
   const [error, setError] = useState<string>();
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  let cancel = false;
-
   useEffect(() => {
+    // FIXME In the future version, we don't need didUnmount checking
+    // https://github.com/facebook/react/pull/22114
+    let didUnmount = false;
+
     async function fetchData() {
-      if (query === null || cancel) {
+      if (query === null || didUnmount) {
         return;
       }
 
@@ -96,15 +98,15 @@ export function useSearch(query: string | undefined): {
       try {
         const glTodos = await gitlab.getTodos({ search: query || "" });
 
-        if (!cancel) {
+        if (!didUnmount) {
           setTodos(glTodos);
         }
       } catch (e: any) {
-        if (!cancel) {
+        if (!didUnmount) {
           setError(e.message);
         }
       } finally {
-        if (!cancel) {
+        if (!didUnmount) {
           setIsLoading(false);
         }
       }
@@ -113,7 +115,7 @@ export function useSearch(query: string | undefined): {
     fetchData();
 
     return () => {
-      cancel = true;
+      didUnmount = true;
     };
   }, [query]);
 
