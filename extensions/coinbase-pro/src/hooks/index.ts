58a550bb676ad659c549a37be816ab0afdaf6706
Faris Aziz <farisaziz12@gmail.com>
2021-11-12 23:03:33 +0100

    feat(use-fetch): add is loading and error

--- extensions/coinbase-pro/src/hooks/index.ts
+++ extensions/coinbase-pro/src/hooks/index.ts
@@ -11,12 +11,14 @@ export const useFetch = (fetchFunc: () => any, options: any = {}, deps: any[] = 
     refreshInterval = null,
   } = options;
   const [state, setState] = useState<any>(defaultValue);
+  const [error, setError] = useState<any>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
     let intervalId: NodeJS.Timeout;
     (async () => {
       if (!shouldExecute) return;
-
+      setIsLoading(true);
       let toast;
       if (shouldShowToast) {
         toast = await showToast(ToastStyle.Animated, `Fetching ${capitalize(name)}`);
@@ -24,8 +26,10 @@ export const useFetch = (fetchFunc: () => any, options: any = {}, deps: any[] = 
       try {
         if (refreshInterval) {
           intervalId = setInterval(async () => {
+            setIsLoading(true);
             const data = await fetchFunc();
             if (data) setState(data);
+            setIsLoading(false);
           }, refreshInterval);
         }
 
@@ -33,13 +37,16 @@ export const useFetch = (fetchFunc: () => any, options: any = {}, deps: any[] = 
         if (data) setState(data);
 
         if (shouldShowToast) createResolvedToast(toast, `${capitalize(name)} Fetched`).success();
+        setIsLoading(false);
       } catch (error) {
+        setIsLoading(false);
         const message = (error as string) || "";
+        setError(message);
         if (shouldShowToast) createResolvedToast(toast, `Failed to fetch ${name.toLowerCase()}`, message).error();
       }
     })();
     return () => clearInterval(intervalId);
   }, deps);
 
-  return state;
+  return { data: state, error, isLoading };
 };
