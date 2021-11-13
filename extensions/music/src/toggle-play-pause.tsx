50b1bcda13b47c95d0d7ae10c9243dbab168b832
Rawnly <fedevitale99@gmail.com>
2021-11-13 18:03:08 +0100

    feat: music extension

---
+++ extensions/music/src/toggle-play-pause.tsx
@@ -0,0 +1,8 @@
+import { closeMainWindow } from '@raycast/api'
+import * as playerControls from './util/controls'
+
+
+export default async () => {
+	await closeMainWindow()
+	await playerControls.togglePlay()
+}
