14d64cf3692a91d376e1cc2c04806bcd6a999f3d
Thiago Holanda <unnamedd@gmail.com>
2021-11-18 13:30:11 +0100

    Refactor Data Manager use

--- extensions/script-commands/src/Commands/ScriptCommandsList.tsx
+++ extensions/script-commands/src/Commands/ScriptCommandsList.tsx
@@ -1,69 +1,26 @@
 import { 
-  useState, 
-  useEffect 
-} from "react"
-
-import { 
-  DataManager 
-} from "@managers"
-
-import { 
-  Main 
-} from "@models"
-
-import { 
   MainContent 
 } from "@components"
 
-import { 
-  ViewState 
-} from "@types"
+import {
+   ApplicationProvider
+ } from "@providers"
+import { useScriptCommands } from "Providers/DataManagerProvider"
 
-const dataManager = DataManager.shared()
+export function ScriptCommandsList(): JSX.Element {
+  const { main } = useScriptCommands()  
 
-type Props = {
-  onAction?: () => void
-}
-
-export function ScriptCommandsList({ onAction }: Props): JSX.Element {
-  const [state, setState] = useState<ViewState>({ needsReload: true })
-
-  const [content, setContent] = useState<Main>({ 
-    groups: [],
-    totalScriptCommands: 0
-  })
-
-  useEffect(() => {
-    async function fetch() {
-      const response = await dataManager.fetchCommands()
-
-      setContent(oldState => ({
-        ...oldState,
-        groups: response.groups,
-        totalScriptCommands: response.totalScriptCommands
-      }))
-    }
-
-    fetch()
-  }, [state])
+  console.log(`Called List: ${main.groups.length} - TSC: ${main.totalScriptCommands}`)
 
   return (
-    <MainContent 
-      navigationTitle="Search Command"
-      placeholder={`Search for your Script Command among of ${content.totalScriptCommands} items`}
-      isLoading={content.groups.length == 0} 
-      groups={content.groups} 
-      showSearchListAction={ false }
-      onAction={ 
-        () => {
-          setState(oldState => ({
-            ...oldState, 
-            needsReload: true 
-          }))
-
-          if (onAction != undefined)
-            onAction()          
-      }}
-    />
+    <ApplicationProvider>
+      <MainContent 
+        navigationTitle="Search Command"
+        placeholder={`Search for your Script Command among of ${main.totalScriptCommands} items`}
+        isLoading={main.groups.length == 0} 
+        groups={main.groups} 
+        showSearchListAction={ false }
+      />
+    </ApplicationProvider>
   )
 }
---
+++ extensions/script-commands/src/Providers/index.ts
@@ -0,0 +1,5 @@
+export { 
+  useDataManager,
+  ApplicationProvider,
+  ApplicationContext
+} from "./DataManagerProvider"
