598962b99adaf41935e8404ca78e1ccca1f7c388
Henri <HenriChabrand@users.noreply.github.com>
2021-11-18 14:14:16 +0100

    Update extensions/notion/src/search-page.tsx
    Co-authored-by: Per Nielsen Tikær <pnt@klean.dk>

--- extensions/notion/src/search-page.tsx
+++ extensions/notion/src/search-page.tsx
@@ -181,22 +181,6 @@ function validateForm(values: FormValues): boolean {
 }
 
 
-function notionColorToTintColor (notionColor: string): Color {
-   const colorMapper = {
-    'default': Color.PrimaryText,
-    'gray': Color.PrimaryText,
-    'brown': Color.Brown,
-    'red': Color.Red,
-    'blue': Color.Blue,
-    'green': Color.Green,
-    'yellow': Color.Yellow,
-    'orange': Color.Orange,
-    'purple': Color.Purple,
-    'pink': Color.Magenta
-  } as Record<string,Color>
-
-  return colorMapper[notionColor] 
-}
 
 async function storeLastEditedPages(pages: Page[]) {
   const data = JSON.stringify(pages)
