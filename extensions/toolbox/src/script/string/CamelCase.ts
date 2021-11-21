bb560eed70374948ed5d15ec1d30be6445ef7496
kang <doyouing@gmail.com>
2021-11-22 00:46:05 +0900

    Revise the phrase.

--- extensions/toolbox/src/script/string/CamelCase.ts
+++ extensions/toolbox/src/script/string/CamelCase.ts
@@ -2,7 +2,7 @@ import { Script } from "../type";
 import * as _ from "lodash";
 
 export const CamelCase: Script = {
-  info: { title: "CamelCase", desc: "Converts Your Text To CamelCase", type: "all", example: "ray cast" },
+  info: { title: "Camel Case", desc: "Converts Your Text To Camel Case", type: "all", example: "ray cast" },
   run(input) {
     return _.camelCase(input);
   },
