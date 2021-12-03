100a613f7e77edd7a1e7974abb71e9a4b4c0d10b
Valentin Polo <valentin@hey.com>
2021-12-03 17:39:15 +0100

    Add Algolia extension

---
+++ extensions/algolia/src/index.tsx
@@ -0,0 +1,54 @@
+import { getPreferenceValues, List, ActionPanel, OpenInBrowserAction } from "@raycast/api";
+import { useEffect, useMemo, useState } from "react";
+import algoliasearch from "algoliasearch/lite";
+
+export default function main() {
+  const preferences = getPreferenceValues();
+
+  const algoliaClient = useMemo(() => {
+    return algoliasearch(preferences.appId, preferences.apiKey);
+  }, [preferences.appId, preferences.apiKey]);
+
+  const algoliaIndex = useMemo(() => {
+    return algoliaClient.initIndex(preferences.indexName);
+  }, [algoliaClient, preferences.indexName]);
+
+  const [searchResults, setSearchResults] = useState<any[] | undefined>();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const search = async (query = "") => {
+    setIsLoading(true);
+
+    return await algoliaIndex.search(query).then((res) => {
+      setIsLoading(false);
+      return res.hits;
+    });
+  };
+
+  useEffect(() => {
+    (async () => setSearchResults(await search()))();
+  }, []);
+
+  return (
+    <List
+      throttle={true}
+      isLoading={isLoading || searchResults === undefined}
+      onSearchTextChange={async (query) => setSearchResults(await search(query))}
+    >
+      {searchResults?.map((result) => (
+        <List.Item
+          key={result.objectID}
+          title={result[preferences.mainAttribute]}
+          subtitle={preferences.secondaryAttribute ? result[preferences.secondaryAttribute] : undefined}
+          actions={
+            preferences.urlAttribute ? (
+              <ActionPanel title={result.name}>
+                <OpenInBrowserAction url={result.url} title="Open in Browser" />
+              </ActionPanel>
+            ) : undefined
+          }
+        />
+      ))}
+    </List>
+  );
+}
