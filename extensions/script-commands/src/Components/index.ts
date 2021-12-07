876f3898b374d0435ecab5d2986019b21d1e04ca
Thiago Holanda <unnamedd@gmail.com>
2021-12-07 05:05:38 +0100

    Reorganized Components/index.ts to reflect the new structure of the components

--- extensions/script-commands/src/Components/index.ts
+++ extensions/script-commands/src/Components/index.ts
@@ -1,10 +1,104 @@
+// #####################################################
+// ACTION ITEMS
+
+export { 
+  ViewSourceCodeActionItem 
+} from "./Action/Items/ViewSourceCodeActionItem"
+
+export { 
+  ViewReadmeActionItem 
+} from "./Action/Items/ViewReadmeActionItem"
+
+export { 
+  ClearFilterActionItem 
+} from "./Action/Items/ClearFilterActionItem"
+
+export { 
+  LanguageActionItem 
+} from "./Action/Items/LanguageActionItem"
+
+export { 
+  AuthorActionItem 
+} from "./Action/Items/AuthorActionItem"
+
+export { 
+  UninstallActionItem
+} from "./Action/Items/UninstallActionItem"
+
+export { 
+  SetupActionItem
+} from "./Action/Items/SetupActionItem"
+
+export { 
+  InstallActionItem
+} from "./Action/Items/InstallActionItem"
+
+export { 
+  ConfirmChangeActionItem
+} from "./Action/Items/ConfirmChangeActionItem"
+
+// #####################################################
+// SUBMENUS
+
+export { 
+  TypeActionSubmenu 
+} from "./Action/Submenus/TypeActionSubmenu"
+
+export { 
+  LanguagesActionSubmenu 
+} from "./Action/Submenus/LanguagesActionSubmenu"
+
+// #####################################################
+// PANELS
+
 export { 
   AuthorsActionPanel
-} from "./ActionPanels/AuthorsActionPanel"
+} from "./Action/Panels/AuthorsActionPanel"
 
 export { 
   FiltersActionPanel 
-} from "./ActionPanels/FiltersActionPanel"
+} from "./Action/Panels/FiltersActionPanel"
+
+export { 
+  ManagementActionPanel 
+} from "./Action/Panels/ManagementActionPanel"
+
+export {
+  ReadmeActionPanel
+} from "./Action/Panels/ReadmeActionPanel"  
+
+export { 
+  ViewsActionPanel
+} from "./Action/Panels/ViewsActionPanel"
+
+// #####################################################
+// DETAILS
+
+export { 
+  ReadmeDetail
+} from "./Details/ReadmeDetail"  
+
+export { 
+  SourceCodeDetail 
+} from "./Details/SourceCodeDetail"  
+
+// #####################################################
+// LIST ITEMS
+
+export { 
+  ScriptCommandItem
+} from "./ListItems/ScriptCommandItem"
+
+// #####################################################
+// COMMON
+
+export { 
+  StoreToast, 
+} from "./Common/StoreToast"
+
+export { 
+  FilterToast,
+} from "./Common/FilterToast"
 
 export { 
   GroupSection 
@@ -13,31 +107,3 @@ export {
 export { 
   MainContent 
 } from "./Contents/MainContent"
-
-export { 
-  ManagementActionPanel 
-} from "./ActionPanels/ManagementActionPanel"
-
-export {
-  ReadmeActionPanel
-} from "./ActionPanels/ReadmeActionPanel"
-
-export { 
-  ReadmeDetail
-} from "./Details/ReadmeDetail"
-
-export { 
-  SourceCodeDetail 
-} from "./Details/SourceCodeDetail"
-
-export { 
-  ScriptCommandItem
-} from "./ListItems/ScriptCommandItem"
-
-export { 
-  StoreToast
-} from "./Common/Toast"
-
-export { 
-  ViewsActionPanel
-} from "./ActionPanels/ViewsActionPanel"
