d40183300d424472c0c927234e5a43e778ed35c3
jradtilbrook <jradtilbrook@hey.com>
2021-12-17 09:30:09 +0800

    Refactor to one function

--- extensions/bitwarden/src/search.tsx
+++ extensions/bitwarden/src/search.tsx
@@ -63,8 +63,14 @@ function ItemList(props: {
     }
   }
 
-  function loadTotp(sessionToken: string) {
-    return (id: string) => bitwardenApi.getTotp(id, sessionToken)
+  async function copyTotp(sessionToken: string | undefined, id: string) {
+    if (sessionToken) {
+        const totp = await bitwardenApi.getTotp(id, sessionToken)
+        copyTextToClipboard(totp);
+        closeMainWindow({ clearRootSearch: true });
+      } else {
+        showToast(ToastStyle.Failure, "Failed to fetch TOTP.");
+      }
   }
 
   useEffect(() => {
@@ -97,7 +103,8 @@ function ItemList(props: {
             item={item}
             folder={item.folderId ? folderMap[item.folderId] : undefined}
             refreshItems={refreshItems}
-            getTotp={sessionToken ? loadTotp(sessionToken) : undefined}
+            sessionToken={sessionToken}
+            copyTotp={copyTotp}
           />
         ))}
       </List.Section>
@@ -108,7 +115,8 @@ function ItemList(props: {
           item={item}
           folder={item.folderId ? folderMap[item.folderId] : undefined}
           refreshItems={refreshItems}
-          getTotp={sessionToken ? loadTotp(sessionToken) : undefined}
+          sessionToken={sessionToken}
+          copyTotp={copyTotp}
         />
       ))}
       </List.Section>
@@ -127,8 +135,8 @@ function getIcon(item: Item) {
   }[item.type];
 }
 
-function ItemListItem(props: { item: Item; folder: Folder | undefined; refreshItems?: () => void, getTotp?: (id: string) => Promise<string> }) {
-  const { item, folder, refreshItems, getTotp } = props;
+function ItemListItem(props: { item: Item; folder: Folder | undefined; refreshItems?: () => void, sessionToken: string | undefined, copyTotp: (sessionToken: string | undefined, id: string) => void }) {
+  const { item, folder, refreshItems, sessionToken, copyTotp } = props;
   const { name, notes, identity, login, secureNote, fields, passwordHistory, card } = item;
   const accessoryIcons = [];
 
@@ -153,16 +161,6 @@ function ItemListItem(props: { item: Item; folder: Folder | undefined; refreshIt
 
   const tree = treeify.asTree(cleanItem, true, false);
 
-  async function copyTotp(id: string) {
-      if (getTotp) {
-        const totp = await getTotp(id);
-        copyTextToClipboard(totp);
-        closeMainWindow({ clearRootSearch: true });
-      } else {
-        showToast(ToastStyle.Failure, "Failed to fetch TOTP.");
-      }
-  }
-
   return (
     <List.Item
       id={item.id}
@@ -174,7 +172,7 @@ function ItemListItem(props: { item: Item; folder: Folder | undefined; refreshIt
       actions={
         <ActionPanel>
           {item.login?.password ? <CopyToClipboardAction title="Copy Password" content={item.login.password} /> : null}
-          {item.login?.totp ? <ActionPanel.Item title="Copy TOTP" icon={Icon.Clipboard} onAction={() => copyTotp(item.id)} /> : null}
+          {item.login?.totp ? <ActionPanel.Item title="Copy TOTP" icon={Icon.Clipboard} onAction={() => copyTotp(sessionToken, item.id)} /> : null}
           {item.notes ? (
             <PushAction
               title="Show Secure Note"
