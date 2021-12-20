5dff8bf3be371bcc0e59ecb11a0267868dcc0ce7
poblouin <pblouin@wealthsimple.com>
2021-12-20 08:11:49 -0500

    Fix indentation

--- extensions/music/src/love-and-add-to-library.tsx
+++ extensions/music/src/love-and-add-to-library.tsx
@@ -3,6 +3,6 @@ import * as playerControls from './util/controls'
 
 export default async () => {
 	await closeMainWindow()
-    await playerControls.love()
+	await playerControls.love()
 	await playerControls.addToLibrary()
 }
