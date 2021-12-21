efe4dae55256897dfecb08e4b88b64af1c849f2b
Timur Badretdinov <destinerx@gmail.com>
2021-12-21 19:58:45 +0300

    Add Coingecko extension (#556)
    * Add Coingecko extension
    
    * Fix typo in manifest
    
    Co-authored-by: fe9lix <fe9lix@googlemail.com>
    
    * Use "includes" instead of "startsWith" to filter
    
    Co-authored-by: fe9lix <fe9lix@googlemail.com>

---
+++ extensions/coingecko/src/storage.ts
@@ -0,0 +1,31 @@
+import { getLocalStorageItem, setLocalStorageItem } from '@raycast/api';
+
+const KEY_FAVORITES = 'favorites';
+
+export async function getFavorites(): Promise<string[]> {
+  const favoriteString = await getLocalStorageItem(KEY_FAVORITES) || '[]';
+  return JSON.parse(favoriteString.toString());
+}
+
+export async function addFavorite(id: string) {
+  const favorites = await getFavorites();
+  if (favorites.includes(id)) {
+    return;
+  }
+  favorites.push(id);
+  await setFavorites(favorites);
+}
+
+export async function removeFavorite(id: string) {
+  const favorites = await getFavorites();
+  if (!favorites.includes(id)) {
+    return;
+  }
+  favorites.splice(favorites.indexOf(id), 1);
+  await setFavorites(favorites);
+}
+
+async function setFavorites(favorites: string[]) {
+  const newFavoriteString = JSON.stringify(favorites);
+  await setLocalStorageItem(KEY_FAVORITES, newFavoriteString);
+}
