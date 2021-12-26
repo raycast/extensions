1487cf3f9c19b2f03900dc9868609ee7340ba354
ciiiii <allen.ccccnm@gmail.com>
2021-12-26 18:09:19 +0800

    Define index command

---
+++ extensions/clash/src/index.tsx
@@ -0,0 +1,60 @@
+import { ActionPanel, ImageLike, List, PushAction } from "@raycast/api";
+import Commponents from "./components";
+
+type CommponentT = {
+  title: string;
+  icon?: ImageLike;
+  node: JSX.Element;
+};
+
+const commponents: Array<CommponentT> = [
+  {
+    title: "Overview",
+    icon: "overview.png",
+    node: <Commponents.Overview />,
+  },
+  {
+    title: "Proxies",
+    icon: "proxies.png",
+    node: <Commponents.Proxies />,
+  },
+  {
+    title: "Rules",
+    icon: "rules.png",
+    node: <Commponents.Rules />,
+  },
+  {
+    title: "Conns",
+    icon: "conns.png",
+    node: <Commponents.Conns />,
+  },
+  {
+    title: "Logs",
+    icon: "logs.png",
+    node: <Commponents.Logs />,
+  },
+  {
+    title: "Backends",
+    icon: "backends.png",
+    node: <Commponents.Backends />,
+  },
+];
+
+export default function Command() {
+  return (
+    <List>
+      {commponents.map((commponent, index) => (
+        <List.Item
+          key={index}
+          title={commponent.title}
+          icon={commponent.icon}
+          actions={
+            <ActionPanel>
+              <PushAction title={`Show ${commponent.title}`} target={commponent.node} />
+            </ActionPanel>
+          }
+        />
+      ))}
+    </List>
+  );
+}
