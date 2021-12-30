911888fef6ce3d386dac0bc08c393eea682bb516
ciiiii <allen.ccccnm@gmail.com>
2021-12-30 19:25:03 +0800

    Add "Clash" Extension (#570)
    * Init clash extension
    
    * Define index command
    
    * Add util libs
    
    * Add useWebsocket wrapper
    
    * Define http requests
    
    * Define Websocket requests
    
    * Add commponents
    
    * Add image assets
    
    * Show empty list when no available backend
    
    * Improve error handle
    
    * Modify Extensions and Commands Naming
    
    * Add error toast in main list

---
+++ extensions/clash/src/components/rules.tsx
@@ -0,0 +1,34 @@
+import { List } from "@raycast/api";
+import { useEffect, useState } from "react";
+import { ErrorHandler } from "../utils/error";
+import { GetRules } from "./client/http";
+import { RuleT } from "./types";
+
+// TODO: ruleProviders /providers/rules
+export default function Rules(): JSX.Element {
+  const [loading, setLoading] = useState(true);
+  const [rules, setRules] = useState([] as Array<RuleT>);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const data = await GetRules();
+        setRules(data);
+      } catch (e) {
+        ErrorHandler(e)
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <List isLoading={loading}>
+      {rules.map((rule, index) => (
+        <List.Item key={index} title={rule.payload} subtitle={rule.type} accessoryTitle={rule.proxy} />
+      ))}
+    </List>
+  );
+}
