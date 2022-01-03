0acd0a37666e1a4494b343cdd4f88ec0746b1542
Robin Klein <robin@klein.im>
2022-01-03 22:55:36 +0100

    Added Preferences for Streamlink Support

--- extensions/twitch/src/interfaces/Preferences.ts
+++ extensions/twitch/src/interfaces/Preferences.ts
@@ -1,4 +1,7 @@
 export default interface Preferences {
     clientId: string,
-    authorization: string
+    authorization: string,
+    streamlink?: string,
+    player?: string,
+    quality?: string,
 }
