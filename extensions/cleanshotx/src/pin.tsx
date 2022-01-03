d8996b46f817efc018f28bff2992425bdddd84a4
Aayush9029 <aayushpokharel36@gmail.com>
2022-01-03 16:16:12 -0500

    added / removed spaces

--- extensions/cleanshotx/src/pin.tsx
+++ extensions/cleanshotx/src/pin.tsx
@@ -1,5 +1,5 @@
 import open from "open";
-import {closeMainWindow } from "@raycast/api";
+import { closeMainWindow } from "@raycast/api";
 
 export default async() => {
     const url = "cleanshot://pin";
