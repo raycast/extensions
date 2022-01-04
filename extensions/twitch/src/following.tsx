0fa8f0e2e1bb9b3224892fa50e25a675f58be7e2
Robin Klein <robin@klein.im>
2022-01-04 16:41:13 +0100

    Implemented new Action Component

--- extensions/twitch/src/following.tsx
+++ extensions/twitch/src/following.tsx
@@ -6,13 +6,13 @@ import { exec } from "child_process";
 import Item from "./interfaces/FollowingItem";
 import Preferences from "./interfaces/preferences";
 import User from "./interfaces/user";
+import Action from "./utils";
 
 export default function main() {
   const preferences: Preferences = getPreferenceValues();
   const clientId = preferences.clientId;
   const authorization = preferences.authorization;
   const streamlinkLocation = preferences.streamlink || "/opt/homebrew/bin/streamlink";
-  const playerLocation = preferences.player || "";
   const quality = preferences.quality || "best";
 
   const [loading, setLoading] = React.useState(false);
@@ -58,18 +58,7 @@ export default function main() {
     <List isLoading={loading} searchBarPlaceholder="Search for a Streamer on Twitch" navigationTitle="Search a Channel" onSearchTextChange={(text) => setQuery(text)}>
       {items.map((item: Item) => {
         return <ListItem key={item.id} icon={{ source: item.thumbnail_url.replace("{width}", "320").replace("{height}", "180"), mask: ImageMask.RoundedRectangle }} accessoryIcon={{ tintColor: item.type == "live" ? "green" : "red", source: item.type == "live" ? "checkmark-circle-16" : "xmark-circle-16" }} accessoryTitle={`${item.type == "live" ? item.game_name : "Offline"}`} id={item.id} title={item.title} subtitle={item.user_name} actions={
-          <ActionPanel>
-            <OpenAction title="Open Channel" target={`https://twitch.tv/${item.user_name}`} />
-            <OpenAction title="Open Stream in Streamlink" target="streamlink" onOpen={(target) => {
-              if (item.type != "live") { showToast(ToastStyle.Failure, "This streamer is offline!"); return; }
-
-              exec(`${streamlinkLocation} https://twitch.tv/${item.user_name} ${quality} ${playerLocation ? `--player ${playerLocation}` : ""}`, (err, stdout, stderr) => {
-                if (err) {
-                  showToast(ToastStyle.Failure, "Error at starting Streamlink");
-                }
-              });
-            }} shortcut={{ modifiers: ["opt"], key: "enter" }} />
-          </ActionPanel>
+          <Action live={item.type == "live"} name={item.user_name} />
         } />
       })}
     </List>
--- extensions/twitch/src/index.tsx
+++ extensions/twitch/src/index.tsx
@@ -1,18 +1,15 @@
 import { ActionPanel, confirmAlert, Detail, getPreferenceValues, ImageMask, List, ListItem, OpenAction, showHUD, showToast, ToastStyle } from "@raycast/api";
 import React from "react";
 import fetch from 'node-fetch';
-import { exec } from "child_process";
 
 import Item from "./interfaces/Item";
 import Preferences from "./interfaces/preferences";
+import Action from './utils';
 
 export default function main() {
   const preferences: Preferences = getPreferenceValues();
   const clientId = preferences.clientId;
   const authorization = preferences.authorization;
-  const streamlinkLocation = preferences.streamlink || "/opt/homebrew/bin/streamlink";
-  const playerLocation = preferences.player || "";
-  const quality = preferences.quality || "best";
 
   const [loading, setLoading] = React.useState(false);
   const [query, setQuery] = React.useState<string>("");
@@ -41,18 +38,7 @@ export default function main() {
     <List isLoading={loading} searchBarPlaceholder="Search for a Streamer on Twitch" navigationTitle="Search a Channel" onSearchTextChange={(text) => setQuery(text)}>
       {items.map((item: Item) => {
         return <ListItem key={item.id} icon={{ source: item.thumbnail_url, mask: ImageMask.Circle }} accessoryIcon={{ tintColor: item.is_live ? "green" : "red", source: item.is_live ? "checkmark-circle-16" : "xmark-circle-16" }} accessoryTitle={`${item.is_live ? item.game_name : "Offline"}`} id={item.id} title={item.title} subtitle={item.display_name} actions={
-          <ActionPanel>
-            <OpenAction title="Open Channel" target={`https://twitch.tv/${item.broadcaster_login}`} />
-            <OpenAction title="Open Stream in Streamlink" target="streamlink" onOpen={(target) => {
-              if (!item.is_live) { showToast(ToastStyle.Failure, "This streamer is offline!"); return; }
-
-              exec(`${streamlinkLocation} https://twitch.tv/${item.broadcaster_login} ${quality} ${playerLocation ? `--player ${playerLocation}` : ""}`, (err, stdout, stderr) => {
-                if (err) {
-                  showToast(ToastStyle.Failure, "Error at starting Streamlink");
-                }
-              });
-            }} shortcut={{ modifiers: ["opt"], key: "enter" }} />
-          </ActionPanel>
+          <Action live={item.is_live} name={item.broadcaster_login} />
         } />
       })}
     </List>
