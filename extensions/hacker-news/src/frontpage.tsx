97e1dadaa6152dc189628dfcebfeffb191af77a1
Yukai Huang <yukaihuangtw@gmail.com>
2022-01-07 12:13:56 +0800

    refactor: extract hooks and component from frontpage

--- extensions/hacker-news/src/frontpage.tsx
+++ extensions/hacker-news/src/frontpage.tsx
@@ -1,29 +1,10 @@
-import { ActionPanel, CopyToClipboardAction, List, OpenInBrowserAction, showToast, ToastStyle } from "@raycast/api";
-import { useEffect, useState } from "react";
-import Parser from "rss-parser";
+import { List, showToast, ToastStyle } from "@raycast/api";
 
-const parser = new Parser();
-
-interface State {
-  items?: Parser.Item[];
-  error?: Error;
-}
+import { StoryListItem } from "./StoryListItem";
+import useHNRss from "./useHNRss";
 
 export default function Command() {
-  const [state, setState] = useState<State>({});
-
-  useEffect(() => {
-    async function fetchStories() {
-      try {
-        const feed = await parser.parseURL("https://hnrss.org/frontpage?count=25");
-        setState({ items: feed.items });
-      } catch (error) {
-        setState({ error: error instanceof Error ? error : new Error("Something went wrong") });
-      }
-    }
-
-    fetchStories();
-  }, []);
+  const state = useHNRss("frontpage");
 
   if (state.error) {
     showToast(ToastStyle.Failure, "Failed loading stories", state.error.message);
@@ -36,67 +17,4 @@ export default function Command() {
       ))}
     </List>
   );
-}
-
-function StoryListItem(props: { item: Parser.Item; index: number }) {
-  const icon = getIcon(props.index + 1);
-  const points = getPoints(props.item);
-  const comments = getComments(props.item);
-
-  return (
-    <List.Item
-      icon={icon}
-      title={props.item.title ?? "No title"}
-      subtitle={props.item.creator}
-      accessoryTitle={`👍  ${points}    💬  ${comments}`}
-      actions={<Actions item={props.item} />}
-    />
-  );
-}
-
-function Actions(props: { item: Parser.Item }) {
-  return (
-    <ActionPanel title={props.item.title}>
-      <ActionPanel.Section>
-        {props.item.link && <OpenInBrowserAction url={props.item.link} />}
-        {props.item.guid && <OpenInBrowserAction url={props.item.guid} title="Open Comments in Browser" />}
-      </ActionPanel.Section>
-      <ActionPanel.Section>
-        {props.item.link && (
-          <CopyToClipboardAction
-            content={props.item.link}
-            title="Copy Link"
-            shortcut={{ modifiers: ["cmd"], key: "." }}
-          />
-        )}
-      </ActionPanel.Section>
-    </ActionPanel>
-  );
-}
-
-const iconToEmojiMap = new Map<number, string>([
-  [1, "1️⃣"],
-  [2, "2️⃣"],
-  [3, "3️⃣"],
-  [4, "4️⃣"],
-  [5, "5️⃣"],
-  [6, "6️⃣"],
-  [7, "7️⃣"],
-  [8, "8️⃣"],
-  [9, "9️⃣"],
-  [10, "🔟"],
-]);
-
-function getIcon(index: number) {
-  return iconToEmojiMap.get(index) ?? "⏺";
-}
-
-function getPoints(item: Parser.Item) {
-  const matches = item.contentSnippet?.match(/(?<=Points:\s*)(\d+)/g);
-  return matches?.[0];
-}
-
-function getComments(item: Parser.Item) {
-  const matches = item.contentSnippet?.match(/(?<=Comments:\s*)(\d+)/g);
-  return matches?.[0];
 }
