16ac39a864f6132efab64eb93344dacfed55b4ee
Taf Hyseni <tafhyseni@gmail.com>
2022-01-11 23:47:11 +0100

    Added Versions and transparent livewire icon

--- extensions/laravel-livewire/src/index.tsx
+++ extensions/laravel-livewire/src/index.tsx
@@ -1,16 +1,28 @@
-import { ActionPanel, List, OpenInBrowserAction, CopyToClipboardAction } from "@raycast/api";
-
-import documentation from "./links";
+import {
+  ActionPanel,
+  List,
+  OpenInBrowserAction,
+  CopyToClipboardAction,
+  getPreferenceValues,
+  showToast, ToastStyle
+} from "@raycast/api";
 
 export default function SearchDocumentation() {
+
+  const getPreference = getPreferenceValues();
+  const documentation = {
+    "1.x": require("./documentation/1.x.json"),
+    "2.x": require("./documentation/2.x.json")
+  }
+  
   return (
     <List>
-      {Object.entries(documentation).map(([section, items]) => (
+      {Object.entries(documentation[getPreference.livewireVersion]).map(([section, items]) => (
         <List.Section title={section} key={section}>
           {items.map((item) => (
             <List.Item
               key={item.url}
-              icon="livewire-icon.png"
+              icon="livewire-icon-transparent.png"
               title={item.title}
               keywords={[item.title, section]}
               actions={
