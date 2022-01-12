41db0917317a347754f0fc565c8e084bdd8a21db
Samuel Nitsche <mail@samynitsche.de>
2022-01-12 09:17:47 +0100

    Handle cases where Tunnelblick isn't installed

--- extensions/tunnelblick/src/index.tsx
+++ extensions/tunnelblick/src/index.tsx
@@ -4,35 +4,52 @@ import { runAppleScript, runAppleScriptSync } from "run-applescript";
 import { Connection } from "./types/connection";
 
 async function getVpnConnections(): Promise<Connection[]> {
-  const result = await runAppleScript(`
-    tell application "Tunnelblick" to get configurations
-  `);
+  try {
+    const result = await runAppleScript(`
+      tell application "Tunnelblick" to get configurations
+    `);
 
-  const connections = result.split(",").map((connection) => {
-    const name = connection.replace("configuration ", "").trim();
-    const status = runAppleScriptSync(`
+    const connections = result.split(",").map((connection) => {
+      const name = connection.replace("configuration ", "").trim();
+      const status = runAppleScriptSync(`
       tell application "Tunnelblick" to get state of first configuration where name = "${name}"
     `);
 
-    return {
-      name,
-      status,
-    };
-  });
+      return {
+        name,
+        status,
+      };
+    });
 
-  return connections;
+    return connections;
+  } catch {
+    return new Promise((resolve, reject) =>
+      reject("Couln't get VPN connections. Make sure you have Tunnelblick installed.")
+    );
+  }
 }
 
 export default function Command() {
   const [isLoading, setIsLoading] = useState(true);
   const [connections, setConnections] = useState<Connection[]>([]);
+  const [error, setError] = useState<Error | null>(null);
 
   useEffect(() => {
-    getVpnConnections().then((connections: Connection[]) => {
-      setConnections(connections);
-      setIsLoading(false);
-    });
+    getVpnConnections()
+      .then((connections: Connection[]) => {
+        setConnections(connections);
+      })
+      .catch((error) => {
+        setError(new Error(error));
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, []);
+
+  if (error) {
+    showToast(ToastStyle.Failure, "Something went wrong", error.message);
+  }
 
   return (
     <List isLoading={isLoading}>
