d2cc654e8107a466324f687571334a816eeb0df5
Danyel Alkeddah <14123621+danyelkeddah@users.noreply.github.com>
2022-01-17 12:53:02 +0300

    add algolia support

--- extensions/spatie-documentation/src/laravel-permission.tsx
+++ extensions/spatie-documentation/src/laravel-permission.tsx
@@ -1,42 +1,165 @@
-import { ActionPanel, CopyToClipboardAction, getPreferenceValues, List, OpenInBrowserAction } from "@raycast/api";
+import {
+  ActionPanel,
+  CopyToClipboardAction,
+  getPreferenceValues,
+  List,
+  OpenInBrowserAction,
+  showToast,
+  ToastStyle,
+} from "@raycast/api";
+import { useEffect, useMemo, useState } from "react";
+import algoliaSearch from "algoliasearch/lite";
+import striptags from "striptags";
+
+type docList = {
+  [versions: string]: {
+    title: string;
+    url: string;
+  }[];
+};
+
+const documentation: { [key: string]: docList } = {
+  v2: require("./documentation/laravel-permission/v2.json"),
+  v3: require("./documentation/laravel-permission/v3.json"),
+  v4: require("./documentation/laravel-permission/v4.json"),
+  v5: require("./documentation/laravel-permission/v5.json"),
+};
+
+const APPID = "BH4D9OD16A";
+const APIKEY = "7a1f56fb06bd42e657e82bdafe86cef3";
+const INDEX = "spatie_be";
+
+type KeyValueHierarchy = {
+  [key: string]: string;
+};
+
+type SpatieLaravelPermissionDocsHit = {
+  url: string;
+  hierarchy: KeyValueHierarchy;
+  objectID: string;
+  _highlightResult: {
+    content:
+      | {
+          value: string;
+          matchlevel: string;
+          fullyHighlighted: boolean;
+          matchedWords: string[];
+        }
+      | undefined;
+    hierarchy: {
+      [key: string]: {
+        value: string;
+        matchLevel: string;
+        matchedWords: string[];
+      };
+    };
+  };
+};
 
 export default function SearchDocumentation() {
   const getPreference = getPreferenceValues();
+  const algoliaClient = useMemo(() => {
+    return algoliaSearch(APPID, APIKEY);
+  }, [APPID, APIKEY]);
 
-  type docList = {
-    [versions: string]: {
-      title: string;
-      url: string;
-    }[];
+  const algoliaIndex = useMemo(() => {
+    return algoliaClient.initIndex(INDEX);
+  }, [algoliaClient, INDEX]);
+
+  const [searchResults, setSearchResults] = useState<any[] | undefined>();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const hierarchyToArray = (hierarchy: KeyValueHierarchy) => {
+    return Object.values(hierarchy)
+      .filter((hierarchyEntry: string | unknown) => hierarchyEntry)
+      .map((hierarchyEntry: string) => hierarchyEntry.replace("&amp;", "&"));
   };
 
-  const documentation: { [key: string]: docList } = {
-    v2: require("./documentation/laravel-permission/v2.json"),
-    v3: require("./documentation/laravel-permission/v3.json"),
-    v4: require("./documentation/laravel-permission/v4.json"),
-    v5: require("./documentation/laravel-permission/v5.json"),
+  const getTitle = (hit: SpatieLaravelPermissionDocsHit): string => {
+    return hierarchyToArray(hit.hierarchy).pop() || "";
   };
 
+  const getSubTitle = (hit: SpatieLaravelPermissionDocsHit): string => {
+    const highlightResult = striptags(hit._highlightResult?.content?.value || "");
+    if (highlightResult) {
+      return highlightResult;
+    }
+
+    const hierarchy = hierarchyToArray(hit.hierarchy) || [];
+    hierarchy.pop();
+    return hierarchy.join(" > ");
+  };
+
+  const search = async (query = "") => {
+    if (query === "") {
+      return;
+    }
+    setIsLoading(true);
+
+    return await algoliaIndex
+      .search(query, {
+        hitsPerPage: 11,
+        facetFilters: ["version:" + getPreference.spatieLaravelPermissionVersion, "project:laravel-permission"],
+      })
+      .then((res) => {
+        setIsLoading(false);
+        return res.hits;
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        showToast(ToastStyle.Failure, "Error searching Spatie Laravel Permission Documentation", err.message);
+        return [];
+      });
+  };
+
+  useEffect(() => {
+    (async () => setSearchResults(await search()))();
+  }, []);
+
+  const currentDocs = documentation[getPreference.spatieLaravelPermissionVersion];
   return (
-    <List>
-      {Object.entries(documentation[getPreference.spatieLaravelPermissionVersion]).map(([section, items]) => (
-        <List.Section title={section} key={section}>
-          {items.map((item) => (
-            <List.Item
-              key={item.url}
-              icon="spatie-icon.png"
-              title={item.title}
-              keywords={[item.title, section]}
-              actions={
-                <ActionPanel>
-                  <OpenInBrowserAction url={item.url} />
-                  <CopyToClipboardAction title="Copy URL" content={item.url} />
-                </ActionPanel>
-              }
-            />
-          ))}
-        </List.Section>
-      ))}
+    <List
+      throttle={true}
+      isLoading={isLoading}
+      onSearchTextChange={async (query) => setSearchResults(await search(query))}
+    >
+      {searchResults?.map((hit: SpatieLaravelPermissionDocsHit) => {
+        return (
+          <List.Item
+            key={hit.objectID}
+            title={getTitle(hit)}
+            subtitle={getSubTitle(hit)}
+            icon="spatie-icon.png"
+            actions={
+              <ActionPanel title={hit.url}>
+                <OpenInBrowserAction url={hit.url} title="Open in Browser" />
+                <CopyToClipboardAction content={hit.url} title="Copy URL" />
+              </ActionPanel>
+            }
+          />
+        );
+      }) ||
+        Object.entries(currentDocs).map(([section, items]: Array<any>) => {
+          return (
+            <List.Section title={section} key={section}>
+              {items.map((item: any) => {
+                return (
+                  <List.Item
+                    key={item.url}
+                    title={item.title}
+                    icon="spatie-icon.png"
+                    actions={
+                      <ActionPanel title={item.url}>
+                        <OpenInBrowserAction url={item.url} title="Open in Browser" />
+                        <CopyToClipboardAction content={item.url} title="Copy URL" />
+                      </ActionPanel>
+                    }
+                  />
+                );
+              })}
+            </List.Section>
+          );
+        })}
     </List>
   );
 }
