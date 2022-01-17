fce835ebffe36f7c2db91d439231475120e72980
Martin Reitschmied <martin.reitschmied@sushi.dev>
2022-01-17 10:37:01 +0100

    Prettier for 8ball

--- extensions/8ball/src/index.tsx
+++ extensions/8ball/src/index.tsx
@@ -1,32 +1,30 @@
 import { Toast, ToastStyle } from "@raycast/api";
 import { setTimeout } from "timers/promises";
 
-
 export default async () => {
-
-  const answers : string[] = [
-    "Nah, don't do it." ,
-    "Nope" ,
+  const answers: string[] = [
+    "Nah, don't do it.",
+    "Nope",
     "Never!!",
-    "Definitely not." ,
-    "Anything but that." ,
-    "No." ,
-    "NO!" ,
-    "You know what to do." ,
-    "You have to ask?!" ,
-    "Ask me another time." ,
-    "I really don't know.." ,
+    "Definitely not.",
+    "Anything but that.",
+    "No.",
+    "NO!",
+    "You know what to do.",
+    "You have to ask?!",
+    "Ask me another time.",
+    "I really don't know..",
     "Sorry, I don't care.",
-    "Yes." ,
-    "YES!" ,
-    "YEAH!!" ,
-    "Of course!" ,
-    "Yesss." ,
+    "Yes.",
+    "YES!",
+    "YEAH!!",
+    "Of course!",
+    "Yesss.",
     "Yup.",
-    "I'm sure." ,
-    "Certainly." ,
-    "Why not?" ,
-    "Can't think of any reason why not." ,
+    "I'm sure.",
+    "Certainly.",
+    "Why not?",
+    "Can't think of any reason why not.",
   ];
 
   const randomElement = answers[Math.floor(Math.random() * answers.length)];
@@ -38,5 +36,4 @@ export default async () => {
 
   toast.style = ToastStyle.Success;
   toast.title = randomElement;
-
-}
+};
