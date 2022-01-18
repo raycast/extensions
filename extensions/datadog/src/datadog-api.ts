3aba54378224be574b237c378639c0f45d64d732
Vitaliy Kudryk <kudryk.v@protonmail.com>
2022-01-18 21:01:02 +0200

    🔥 monitors api

--- extensions/datadog/src/datadog-api.ts
+++ extensions/datadog/src/datadog-api.ts
@@ -8,5 +8,3 @@ const APP_KEY = getPreferenceValues()["app-key"];
 const configuration = v1.createConfiguration({ authMethods: { apiKeyAuth: API_KEY, appKeyAuth: APP_KEY } });
 
 export const dashboardsApi = new v1.DashboardsApi(configuration);
-export const monitorsApi = new v1.MonitorsApi(configuration);
-
