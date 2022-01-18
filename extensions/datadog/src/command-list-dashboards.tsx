856ad0d5a17c9d6452bad50ec37f56baa1d39785
Vitaliy Kudryk <kudryk.v@protonmail.com>
2022-01-18 21:09:39 +0200

    ♻️ break into multiple files

--- extensions/datadog/src/command-list-dashboards.tsx
+++ extensions/datadog/src/command-list-dashboards.tsx
@@ -1,27 +1,13 @@
-import { ActionPanel, List, OpenInBrowserAction } from "@raycast/api";
-import { useEffect, useState } from "react";
-import { dashboardsApi } from "./datadog-api";
-import {
-  DashboardSummaryDefinition
-} from "@datadog/datadog-api-client/dist/packages/datadog-api-client-v1/models/DashboardSummaryDefinition";
-import { showError } from "./util";
+import {ActionPanel, List, OpenInBrowserAction} from "@raycast/api";
+import {useDashboards} from "./useDashboards";
 
 
 // noinspection JSUnusedGlobalSymbols
 export default function Command() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [dashboards, setDashboards] = useState<DashboardSummaryDefinition[]>();
+  const {dashboards, dashboardsAreLoading} = useDashboards();
 
-  useEffect(() => {
-    dashboardsApi
-      .listDashboards()
-      .then(summary => setDashboards(summary.dashboards))
-      .then(() => setIsLoading(false))
-      .catch(showError);
-  }, []);
-
-  return <List isLoading={isLoading}>
-    {dashboards?.map(dashboard => <List.Item
+  return <List isLoading={dashboardsAreLoading}>
+    {dashboards.map(dashboard => <List.Item
       key={dashboard.id}
       icon={{source: {light: "icon@light.png", dark: "icon@dark.png"}}}
       title={dashboard.title || "No title"}
@@ -29,7 +15,7 @@ export default function Command() {
       accessoryTitle={dashboard.authorHandle}
       actions={
         <ActionPanel>
-          <OpenInBrowserAction url={`https://app.datadoghq.com${dashboard.url}`} />
+          <OpenInBrowserAction url={`https://app.datadoghq.com${dashboard.url}`}/>
         </ActionPanel>
       }
     />)}
