57ddc6fe80e1dc7151aa163960e735fbdedd9fe3
Vitaliy Kudryk <kudryk.v@protonmail.com>
2022-01-19 12:56:28 +0200

    🚨 prettier

--- extensions/datadog/src/datadog-api.ts
+++ extensions/datadog/src/datadog-api.ts
@@ -1,30 +1,29 @@
-import {getPreferenceValues} from "@raycast/api";
-import {v1} from "@datadog/datadog-api-client";
-import fetch, {Response} from "node-fetch";
-import {APM} from "./types";
+import { getPreferenceValues } from "@raycast/api";
+import { v1 } from "@datadog/datadog-api-client";
+import fetch, { Response } from "node-fetch";
+import { APM } from "./types";
 
 const API_KEY = getPreferenceValues()["api-key"];
 const APP_KEY = getPreferenceValues()["app-key"];
 
-const configuration = v1.createConfiguration({authMethods: {apiKeyAuth: API_KEY, appKeyAuth: APP_KEY}});
+const configuration = v1.createConfiguration({ authMethods: { apiKeyAuth: API_KEY, appKeyAuth: APP_KEY } });
 
 export const dashboardsApi = new v1.DashboardsApi(configuration);
 
-export const apiAPM = ({env}: { env: string }): Promise<APM[]> =>
+export const apiAPM = ({ env }: { env: string }): Promise<APM[]> =>
   fetch(`https://api.datadoghq.com/api/v1/service_dependencies?env=${env}`, params)
     .then(parseResponseToJSON)
     .then(json => json as Record<string, { calls: string[] }>)
     .then(rec => Object.entries(rec).sort(([l], [r]) => (l < r ? -1 : 1)))
-    .then(entries => entries.map(([name, {calls}]) => ({env, name, calls} as APM)));
+    .then(entries => entries.map(([name, { calls }]) => ({ env, name, calls } as APM)));
 
 const parseResponseToJSON = (resp: Response) =>
-  resp.json()
-    .then(json => {
-      if (resp.ok) return json;
+  resp.json().then(json => {
+    if (resp.ok) return json;
 
-      // @ts-ignore
-      throw new Error((json.errors).join(", "));
-    });
+    // @ts-ignore
+    throw new Error(json.errors.join(", "));
+  });
 
 const params = {
   headers: {
