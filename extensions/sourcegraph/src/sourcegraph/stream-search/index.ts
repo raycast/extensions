39a2c631ee61920b321f358cf9f091874ef3fdf7
Robert Lin <robert@bobheadxi.dev>
2022-01-18 22:46:09 -0800

    Publish raycast-sourcegraph@44ccb1d
    Version: https://github.com/bobheadxi/raycast-sourcegraph/commit/44ccb1d

--- extensions/sourcegraph/src/sourcegraph/stream-search/index.ts
+++ extensions/sourcegraph/src/sourcegraph/stream-search/index.ts
@@ -81,17 +81,15 @@ export async function performSearch(
       };
 
       handlers.onResults(
-        event.data.map(
-          (match): SearchResult => {
-            const url = `${src.instance}${getMatchUrl(match)}`;
-            if (match.type === "commit") {
-              // Commit stuff comes already markdown-formatted?? so strip formatting
-              match.label = stripMarkdown.processSync(match.label)?.value.toString().split(`› `).pop() || "";
-              match.detail = stripMarkdown.processSync(match.detail)?.value.toString();
-            }
-            return { url, match };
+        event.data.map((match): SearchResult => {
+          const url = `${src.instance}${getMatchUrl(match)}`;
+          if (match.type === "commit") {
+            // Commit stuff comes already markdown-formatted?? so strip formatting
+            match.label = stripMarkdown.processSync(match.label)?.value.toString().split(`› `).pop() || "";
+            match.detail = stripMarkdown.processSync(match.detail)?.value.toString();
           }
-        )
+          return { url, match };
+        })
       );
     });
 
