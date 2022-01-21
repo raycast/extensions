e359e7ff0574c07b8f260b64ce135348eefa7324
praveenjuge <praveen@skcript.com>
2022-01-21 18:11:36 +0530

    Add popToRoot to new group
    Signed-off-by: praveenjuge <praveen@skcript.com>

--- extensions/ulysses/src/new-group.tsx
+++ extensions/ulysses/src/new-group.tsx
@@ -1,15 +1,21 @@
-import { ActionPanel, Form, SubmitFormAction } from "@raycast/api";
+import { ActionPanel, Form, SubmitFormAction, popToRoot } from "@raycast/api";
 import { openUlyssesCallback } from "./utils";
 
+interface Values {
+  group_name: string;
+}
+
 export default function NewGroup() {
+  async function handleSubmit(values: Values) {
+    await openUlyssesCallback("ulysses://x-callback-url/new-group?name=" + values.group_name);
+    popToRoot();
+  }
+
   return (
     <Form
       actions={
         <ActionPanel>
-          <SubmitFormAction
-            title="Create Group"
-            onSubmit={(values) => openUlyssesCallback("ulysses://x-callback-url/new-group?name=" + values.group_name)}
-          />
+          <SubmitFormAction title="Create Group" onSubmit={handleSubmit} />
         </ActionPanel>
       }
     >
