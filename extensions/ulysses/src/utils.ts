754bdd5c76fd12f5b8d0cb0d4de7b1cad12aff48
praveenjuge <praveen@skcript.com>
2022-01-21 10:05:24 +0530

    Add setapp to app list and add detail to description
    Signed-off-by: praveenjuge <praveen@skcript.com>

--- extensions/ulysses/src/utils.ts
+++ extensions/ulysses/src/utils.ts
@@ -3,12 +3,14 @@ import open from "open";
 
 async function isUlyssesInstalled() {
   const applications = await getApplications();
-  return applications.some(({ bundleId }) => bundleId === "com.ulyssesapp.mac");
+  return applications.some(({ bundleId }) => (
+    bundleId === "com.ulyssesapp.mac" || bundleId === "com.soulmen.ulysses-setapp"
+  ));
 }
 
 export async function openUlyssesCallback(url: string) {
   if (!(await isUlyssesInstalled())) {
-    await showToast(ToastStyle.Failure, "Ulysses is not installed", "https://ulysses.app/");
+    await showToast(ToastStyle.Failure, "Ulysses is not installed.", "Install it from: https://ulysses.app");
     return;
   } else {
     await closeMainWindow();
