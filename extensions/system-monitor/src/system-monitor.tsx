cb755f93e890c1504cb57150c5daae2243dea564
Hossam Mourad <ihos4m@gmail.com>
2022-01-22 14:29:01 -0500

    add the system-monitor extension

---
+++ extensions/system-monitor/src/system-monitor.tsx
@@ -0,0 +1,22 @@
+import { Detail } from "@raycast/api";
+import { cpuUsage, freemem, freememPercentage } from "os-utils";
+import { useState } from "react";
+
+export default function SystemMonitor() {
+  const [cpu, setcpu] = useState(0);
+  const [freeMemPercentage, setFreeMemPercentage] = useState(0);
+  const [freeMem, setFreeMem] = useState(0);
+
+  cpuUsage((v) => {
+    setcpu(Math.round(v * 100));
+    setFreeMemPercentage(Math.round(freememPercentage() * 100));
+    setFreeMem(Math.round(freemem() / 1024));
+  });
+
+  const markdown = `
+  * 🖥️ CPU Usage: **${cpu}%**
+  * 📝 Free Memory: **${freeMemPercentage}% (~ ${freeMem} GB)**
+  `;
+
+  return <Detail markdown={!cpu ? "Calculating..." : markdown} />;
+}
