953b5cccdf8520d4e7ef3745e5c749faaeaa05c4
pomdtr <achille.lacoin@dailymotion.com>
2022-01-25 00:42:22 +0100

    update file content script

--- extensions/pipe-commands/assets/commands/copy-file-content.sh
+++ extensions/pipe-commands/assets/commands/copy-file-content.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# @raycast.title Copy to tmp
+# @raycast.title Copy File Content
 # @raycast.mode silent
 # @raycast.packageName File Actions
 # @raycast.argument1 {"type": "file", "placeholder": "query", "percentEncoded": false}
 
-cp "$1" /tmp/
-echo "Copied $1 to /tmp/" >&2
+pbcopy < "$1"
+echo "Content of $(basename "$1") copied to clipboard!" >&2
