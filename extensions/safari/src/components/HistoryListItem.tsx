a283b9977e36cac262ef0fab620704f8ba9b944f
Thomas Paul Mann <hello@thomaspaulmann.com>
2022-01-27 15:24:35 +0000

    Add command to search history (and refactor the rest) (#719)
    * Add command to search history (and refactor the rest)
    
    * Sync command descriptions of Safari extension
    
    * Please linter
    
    * Add lint to scripts
    
    * Rename commands to make then unified
    
    * Unify copy actions

---
+++ extensions/safari/src/components/HistoryListItem.tsx
@@ -0,0 +1,38 @@
+import { ActionPanel, List, OpenInBrowserAction } from "@raycast/api";
+import { HistoryItem } from "../types";
+import { getFaviconUrl, getUrlDomain } from "../utils";
+import CopyMarkdownLinkAction from "./CopyMarkdownLinkAction";
+import CopyTitleAction from "./CopyTitleAction";
+import CopyUrlAction from "./CopyUrlAction";
+import SearchInBrowserAction from "./SearchInBrowserAction";
+
+const Actions = (props: { entry: HistoryItem; searchText?: string }) => {
+  return (
+    <ActionPanel>
+      <ActionPanel.Section>
+        <OpenInBrowserAction url={props.entry.url} />
+        <SearchInBrowserAction searchText={props.searchText} />
+      </ActionPanel.Section>
+      <ActionPanel.Section>
+        <CopyUrlAction url={props.entry.url} />
+        <CopyTitleAction title={props.entry.title} />
+        <CopyMarkdownLinkAction title={props.entry.title} url={props.entry.url} />
+      </ActionPanel.Section>
+    </ActionPanel>
+  );
+};
+
+const HistoryListItem = (props: { entry: HistoryItem; searchText?: string }) => {
+  const domain = getUrlDomain(props.entry.url);
+
+  return props.entry.title ? (
+    <List.Item
+      title={props.entry.title}
+      icon={getFaviconUrl(domain)}
+      accessoryTitle={domain}
+      actions={<Actions entry={props.entry} searchText={props.searchText} />}
+    />
+  ) : null;
+};
+
+export default HistoryListItem;
