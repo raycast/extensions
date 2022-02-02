281613e51044ce1602037931eedada976bf5de11
Dima Grossman <dima@grossman.io>
2022-02-02 14:34:45 +0200

    Update extensions/ssh-manager/src/create.tsx
    Co-authored-by: Per Nielsen Tikær <pnt@klean.dk>

--- extensions/ssh-manager/src/create.tsx
+++ extensions/ssh-manager/src/create.tsx
@@ -24,7 +24,7 @@ export default function Main() {
         </ActionPanel>
       }
     >
-      <Form.TextField id="name" title="Connection name" />
+      <Form.TextField id="name" title="Connection Name" />
       <Form.TextField id="address" title="Server Address" placeholder={"A resolvable DNS name or IP"} />
       <Form.TextField id="user" title="Username" placeholder={"A username to authenticate with"} />
       <Form.TextField
