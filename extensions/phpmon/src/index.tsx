b6952907252917fdc97963f5dd97a860c12be0e0
Nico Verbruggen <mail@nicoverbruggen.be>
2022-02-03 21:19:11 +0100

    Add missing .eslintrc.json, ran Prettier again

--- extensions/phpmon/src/index.tsx
+++ extensions/phpmon/src/index.tsx
@@ -1,4 +1,9 @@
-import { ActionPanel, List, OpenInBrowserAction, PushAction } from "@raycast/api";
+import {
+  ActionPanel,
+  List,
+  OpenInBrowserAction,
+  PushAction,
+} from "@raycast/api";
 import { existsSync } from "fs";
 
 export default function CommonActionsCommand() {
@@ -6,7 +11,8 @@ export default function CommonActionsCommand() {
     {
       id: "list",
       title: "Show Linked & Parked Domains",
-      subtitle: "Opens the window displaying the list of domains registered with Valet.",
+      subtitle:
+        "Opens the window displaying the list of domains registered with Valet.",
       accessoryTitle: null,
       url: "phpmon://list",
     },
@@ -41,7 +47,8 @@ export default function CommonActionsCommand() {
     {
       id: "services/restart/all",
       title: "Restart All Services",
-      subtitle: "Attempts to restart all Valet services (php, nginx and dnsmasq).",
+      subtitle:
+        "Attempts to restart all Valet services (php, nginx and dnsmasq).",
       accessoryTitle: null,
       url: "phpmon://services/restart/all",
     },
@@ -62,7 +69,8 @@ export default function CommonActionsCommand() {
     {
       id: "services/restart/php",
       title: "Restart Service: php",
-      subtitle: "Attempts to restart the service that corresponds to the linked PHP version.",
+      subtitle:
+        "Attempts to restart the service that corresponds to the linked PHP version.",
       accessoryTitle: null,
       url: "phpmon://services/restart/php",
     },
@@ -84,7 +92,10 @@ export default function CommonActionsCommand() {
         subtitle="Choose a new version of PHP to switch to."
         actions={
           <ActionPanel>
-            <PushAction title="Continue to Select a Version" target={<SwitchForm />} />
+            <PushAction
+              title="Continue to Select a Version"
+              target={<SwitchForm />}
+            />
           </ActionPanel>
         }
       />
@@ -96,7 +107,11 @@ export default function CommonActionsCommand() {
           subtitle={item.subtitle}
           actions={
             <ActionPanel>
-              <OpenInBrowserAction title="Execute with PHP Monitor" url={item.url} icon="list-icon.png" />
+              <OpenInBrowserAction
+                title="Execute with PHP Monitor"
+                url={item.url}
+                icon="list-icon.png"
+              />
             </ActionPanel>
           }
         />
@@ -107,14 +122,29 @@ export default function CommonActionsCommand() {
 
 function SwitchForm() {
   function items() {
-    const brewPath = existsSync("/opt/homebrew/bin/brew") ? "/opt/homebrew" : "/usr/local";
+    const brewPath = existsSync("/opt/homebrew/bin/brew")
+      ? "/opt/homebrew"
+      : "/usr/local";
 
     return versions.reverse().filter(function (version) {
       return existsSync(brewPath + "/opt/php@" + version);
     });
   }
 
-  const versions = ["5.6", "7.0", "7.1", "7.2", "7.3", "7.4", "8.0", "8.1", "8.2", "8.3", "8.4", "9.0"];
+  const versions = [
+    "5.6",
+    "7.0",
+    "7.1",
+    "7.2",
+    "7.3",
+    "7.4",
+    "8.0",
+    "8.1",
+    "8.2",
+    "8.3",
+    "8.4",
+    "9.0",
+  ];
 
   return (
     <List isLoading={false} searchBarPlaceholder="Filter by title...">
@@ -123,10 +153,18 @@ function SwitchForm() {
           key={"switch-" + version}
           icon="list-icon.png"
           title={"Switch to PHP " + version}
-          subtitle={"Switch to PHP " + version + " and get notified once the switch is done."}
+          subtitle={
+            "Switch to PHP " +
+            version +
+            " and get notified once the switch is done."
+          }
           actions={
             <ActionPanel>
-              <OpenInBrowserAction title={"Switch Now"} url={"phpmon://switch/php/" + version} icon="list-icon.png" />
+              <OpenInBrowserAction
+                title={"Switch Now"}
+                url={"phpmon://switch/php/" + version}
+                icon="list-icon.png"
+              />
             </ActionPanel>
           }
         />
