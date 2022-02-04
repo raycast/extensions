e5807d2f1fd32a6b5657b8f420ea140114350a19
zkytech <zhangkunyuan@hotmail.com>
2022-02-04 15:33:15 +0800

    1:encrypt entries cache file;2:add some comment

--- extensions/keepassxc/src/utils/keepassLoader.ts
+++ extensions/keepassxc/src/utils/keepassLoader.ts
@@ -3,6 +3,7 @@ import { promisify } from "util";
 import fs from "fs";
 import path from "path";
 import child_process from "child_process";
+import CryptoJS from "crypto-js";
 const stat = promisify(fs.stat);
 const exec = promisify(child_process.exec);
 
@@ -14,27 +15,54 @@ interface Preference {
 
 const preferences: Preference = getPreferenceValues();
 
-const dbPassword = preferences.dbPassword;
-const cacheFile = path.join("/tmp/", "kpass_db.cache");
+// this file is used to cache entries (without password) in the database
+const cacheFile = path.join("/tmp/", "jijhggkjbuylkjoiy.cache");
+// keepass database file path
 const database = preferences.database;
+// password for keepass database
+const dbPassword = preferences.dbPassword;
+// keepass-cli executable path
 const keepassxcCli = path.join(preferences.keepassxcRootPath, "Contents/MacOS/keepassxc-cli");
 
+const key = CryptoJS.enc.Utf8.parse("iusadgaksjdfglasidgj");
+const iv = CryptoJS.enc.Utf8.parse("asiudhlashdklasjhdjkljhbh");
+
+const decrypt = (word: string) => {
+  const encryptedHexStr = CryptoJS.enc.Hex.parse(word);
+  const srcs = CryptoJS.enc.Base64.stringify(encryptedHexStr);
+  const decrypt = CryptoJS.AES.decrypt(srcs, key, { iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 });
+  const decryptedStr = decrypt.toString(CryptoJS.enc.Utf8);
+  return decryptedStr.toString();
+};
+
+const encrypt = (word: string) => {
+  const srcs = CryptoJS.enc.Utf8.parse(word);
+  const encrypted = CryptoJS.AES.encrypt(srcs, key, { iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 });
+  return encrypted.ciphertext.toString().toUpperCase();
+};
+
+/**
+ * load entries from database with keepassxc-cli
+ * @returns all entries in keepass database
+ */
 const loadEntries = async () => {
+  // a cache file is used to boost response speed of loading entries by avoid calling keepassxc-cli every time (which may take 2 ~ 3 seconds)
   if (!fs.existsSync(cacheFile)) {
     const { stdout } = await exec(`echo "${dbPassword}" | "${keepassxcCli}" locate "${database}" / -q`);
-    fs.writeFileSync(cacheFile, stdout.toString());
+    fs.writeFileSync(cacheFile, encrypt(stdout.toString()));
   }
   const lastModifiedTime = (await stat(cacheFile)).mtime.getTime();
   const currTime = new Date().getTime();
   let data = "";
   if (currTime - lastModifiedTime <= 1000 * 60 && fs.existsSync(cacheFile)) {
-    data = fs.readFileSync(cacheFile, "utf8");
+    // load entries from cache
+    data = decrypt(fs.readFileSync(cacheFile, "utf8"));
   } else {
+    // call keepassxc-cli to refresh cache file if it is more than 1 minute since last modified
     const { stdout } = await exec(`echo "${dbPassword}" | "${keepassxcCli}" locate "${database}" / -q`);
+    fs.writeFileSync(cacheFile, encrypt(stdout.toString()));
     data = stdout.toString();
-    fs.writeFileSync(cacheFile, data);
   }
-
   return data
     .split("\n")
     .map((f: string) => f.trim())
