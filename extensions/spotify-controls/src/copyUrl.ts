89c75d353d8b512369084f69fec3200da74142a0
Ron Myers <ronmyers@outlook.com>
2022-02-05 17:40:47 -0400

    Spotify Controls: Adds `Shuffle Play` + change in behavior
    - all commands, except `current`, now ensure Spotify is running
    - `current` has handling for Spotify not being open

--- extensions/spotify-controls/src/copyUrl.ts
+++ extensions/spotify-controls/src/copyUrl.ts
@@ -1,16 +1,16 @@
 import { showToast, ToastStyle } from "@raycast/api";
 import { runAppleScript } from "run-applescript";
+import { buildScriptEnsuringSpotifyIsRunning } from "./utils";
 
 export default async () => {
-  const script = `
-    tell application "Spotify"
-        set spotifyURL to spotify url of the current track
-    end tell
-
+  const script = buildScriptEnsuringSpotifyIsRunning(`
+    set spotifyURL to spotify url of the current track
+    
     set AppleScript's text item delimiters to ":"
     set idPart to third text item of spotifyURL
 
-    set the clipboard to ("https://open.spotify.com/track/" & idPart)`;
+    set the clipboard to ("https://open.spotify.com/track/" & idPart)
+  `);
 
   try {
     await runAppleScript(script);
