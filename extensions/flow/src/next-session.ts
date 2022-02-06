fa602b8e5a0d4aa16a705ed2c03dcca987acb9d8
vimtor <victor@vimtor.io>
2022-02-06 18:28:58 +0100

    Make next session skip break

--- extensions/flow/src/next-session.ts
+++ extensions/flow/src/next-session.ts
@@ -2,12 +2,16 @@ import { showHUD, showToast, ToastStyle } from "@raycast/api";
 import { runAppleScript } from "run-applescript";
 import { isFlowInstalled } from "./utils";
 
-export default async function skipSession() {
+export default async function nextSession() {
   if (!(await isFlowInstalled())) {
-    await showToast(ToastStyle.Failure, "Flow is not installed", "https://flowapp.info/");
+    await showToast(ToastStyle.Failure, "Flow is not installed", "Install it from: https://flowapp.info/download");
     return;
   }
+  const phase = await runAppleScript('tell application "Flow" to getPhase');
   await runAppleScript('tell application "Flow" to skip');
+  if (phase === "Flow") {
+    await runAppleScript('tell application "Flow" to skip');
+  }
   await runAppleScript('tell application "Flow" to start');
-  await showHUD("Session skipped");
+  await showHUD("Next session started");
 }
