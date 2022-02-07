36daba6e7f3e63d2e19b6af04a4731ee18b5cc6b
Ron Myers <ron.myers@hey.com>
2022-02-07 10:50:47 -0400

    Spotify Controls: Adds `Shuffle Play` + change in behavior (#813)
    - all commands, except `current`, now ensure Spotify is running
    - `current` has handling for Spotify not being open

--- extensions/spotify-controls/src/repeat.ts
+++ extensions/spotify-controls/src/repeat.ts
@@ -1,14 +1,14 @@
 import { showToast, ToastStyle } from "@raycast/api";
 import { runAppleScript } from "run-applescript";
+import { buildScriptEnsuringSpotifyIsRunning } from "./utils";
 
 export default async () => {
-  const script = `
+  const script = buildScriptEnsuringSpotifyIsRunning(`
     property repeatEnabled : false
-    tell application "Spotify"
-      set repeatEnabled to repeating
-      set repeating to not repeatEnabled
-      return not repeatEnabled
-    end tell`;
+    set repeatEnabled to repeating
+    set repeating to not repeatEnabled
+    return not repeatEnabled
+  `);
   try {
     const result = await runAppleScript(script);
     await showToast(ToastStyle.Success, result === "true" ? "Repeat On" : "Repeat Off");
--- extensions/spotify-controls/src/shuffle.ts
+++ extensions/spotify-controls/src/shuffle.ts
@@ -1,14 +1,14 @@
 import { showToast, ToastStyle } from "@raycast/api";
 import { runAppleScript } from "run-applescript";
+import { buildScriptEnsuringSpotifyIsRunning } from "./utils";
 
 export default async () => {
-  const script = `
+  const script = buildScriptEnsuringSpotifyIsRunning(`
     property shuffleEnabled : false
-    tell application "Spotify"
-      set shuffleEnabled to shuffling
-      set shuffling to not shuffleEnabled
-      return not shuffleEnabled
-    end tell`;
+    set shuffleEnabled to shuffling
+    set shuffling to not shuffleEnabled
+    return not shuffleEnabled
+  `);
   try {
     const result = await runAppleScript(script);
     await showToast(ToastStyle.Success, result === "true" ? "Shuffle On" : "Shuffle Off");
