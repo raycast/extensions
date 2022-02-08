9aa09642d30f0f4144c24cc4c4cd7d44b9abe3e5
Abdulbaki Dursun <eggsydev@gmail.com>
2022-02-08 17:49:38 +0300

    refactor: use new setting and add types

--- extensions/unsplash/src/functions/setWallpaper.ts
+++ extensions/unsplash/src/functions/setWallpaper.ts
@@ -11,7 +11,7 @@ interface SetWallpaperProps {
 export const setWallpaper = async ({ url, id, useHud = false }: SetWallpaperProps) => {
   const toast = useHud && (await showToast(ToastStyle.Animated, "Downloading and setting wallpaper..."));
 
-  const { downloadSize } = getPreferenceValues<UnsplashPreferences>();
+  const { downloadSize, applyTo } = getPreferenceValues<UnsplashPreferences>();
   const selectedPath = environment.supportPath;
 
   const fixedPathName = selectedPath.endsWith("/")
@@ -36,7 +36,7 @@ export const setWallpaper = async ({ url, id, useHud = false }: SetWallpaperProp
 
       try
         tell application "System Events"
-          tell current desktop
+          tell ${applyTo} desktop
             set picture to (x as text)
             return "ok"
           end tell
