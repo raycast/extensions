a58f2b46f5a3e6dd8f56bd18a59d9ec01224bfc8
Robert Lin <robert@bobheadxi.dev>
2022-02-07 20:32:54 -0800

    Publish raycast-sourcegraph@2f31f12
    Version: https://github.com/bobheadxi/raycast-sourcegraph/commit/2f31f12

--- extensions/sourcegraph/src/sourcegraph/stream-search/index.ts
+++ extensions/sourcegraph/src/sourcegraph/stream-search/index.ts
@@ -56,7 +56,7 @@ export async function performSearch(
     ["display", "1500"],
   ];
   const parameterEncoded = parameters.map(([k, v]) => k + "=" + encodeURIComponent(v)).join("&");
-  const requestURL = `${src.instance}/search/stream?${parameterEncoded}`;
+  const requestURL = `${src.instance}/.api/search/stream?${parameterEncoded}`;
   const stream = src.token
     ? new EventSource(requestURL, { headers: { Authorization: `token ${src.token}` } })
     : new EventSource(requestURL);
@@ -83,10 +83,18 @@ export async function performSearch(
       handlers.onResults(
         event.data.map((match): SearchResult => {
           const url = `${src.instance}${getMatchUrl(match)}`;
-          if (match.type === "commit") {
-            // Commit stuff comes already markdown-formatted?? so strip formatting
-            match.label = stripMarkdown.processSync(match.label)?.value.toString().split(`› `).pop() || "";
-            match.detail = stripMarkdown.processSync(match.detail)?.value.toString();
+          switch (match.type) {
+            case "commit":
+              // Commit stuff comes already markdown-formatted?? so strip formatting
+              match.label = stripMarkdown.processSync(match.label)?.value.toString().split(`› `).pop() || "";
+              match.detail = stripMarkdown.processSync(match.detail)?.value.toString();
+              break;
+            case "content":
+              // Line number appears 0-indexed, for ease of use increment it so links
+              // aren't off by 1.
+              match.lineMatches.forEach((l) => {
+                l.lineNumber += 1;
+              });
           }
           return { url, match };
         })
