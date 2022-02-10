8b41bc40fecfd6da71572d39296f43ba849510a2
mathieudutour <mathieu@dutour.me>
2022-02-10 17:18:47 +0100

    Add codemods

---
+++ migrations/1.28.0/detail.ts
@@ -0,0 +1,31 @@
+import { JSCodeshift, Collection } from "jscodeshift";
+import { isVariableDeclared, removeImport, addImport } from "../utils";
+
+export default function ActionPanelTransform(
+  j: JSCodeshift,
+  root: Collection<any>
+) {
+  let needToAddImport = false;
+
+  for (let name of [["DetailProps", "Props"]]) {
+    root
+      .find(j.TSTypeReference, {
+        typeName: {
+          name: name[0],
+        },
+      })
+      .replaceWith((p) => {
+        needToAddImport = true;
+        return j.tsTypeReference(
+          j.tsQualifiedName(j.identifier("Detail"), j.identifier(name[1])),
+          p.node.typeParameters
+        );
+      });
+
+    removeImport(j, root, name[0]);
+  }
+
+  if (needToAddImport) {
+    addImport(j, root, "Detail");
+  }
+}
