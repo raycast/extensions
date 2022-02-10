8b83811de45fe099e72c86d38a600f8da435bfca
Thomas Paul Mann <hello@thomaspaulmann.com>
2022-02-10 16:06:12 +0000

    Update extensions/unicode-symbols/src/index.tsx

--- extensions/unicode-symbols/src/index.tsx
+++ extensions/unicode-symbols/src/index.tsx
@@ -69,7 +69,7 @@ export default function Command() {
                 <ActionPanel>
                   <ActionPanel.Section>
                     <PasteAction
-                      title="Paste Character to Active App"
+                      title="Paste Character in Active App"
                       content={item.value}
                       onPaste={() => addToRecentlyUsedItems(item)}
                     />
