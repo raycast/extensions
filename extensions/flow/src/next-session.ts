198d4e7eec93387268d07bfbc175273bfbf04145
vimtor <victor@vimtor.io>
2022-02-12 20:01:59 +0100

    Add loading toast to Flow commands

--- extensions/flow/src/next-session.ts
+++ extensions/flow/src/next-session.ts
@@ -1,12 +1,22 @@
-import { showHUD, showToast, ToastStyle } from "@raycast/api";
+import { showHUD, Toast, ToastStyle } from "@raycast/api";
 import { runAppleScript } from "run-applescript";
 import { isFlowInstalled } from "./utils";
 
 export default async function nextSession() {
+    const toast = new Toast({
+    title: "Starting next session",
+    style: ToastStyle.Animated
+  });
+  
+  toast.show();
+  
   if (!(await isFlowInstalled())) {
-    await showToast(ToastStyle.Failure, "Flow is not installed", "Install it from: https://flowapp.info/download");
+    toast.title = "Flow not installed";
+    toast.message = "Install it from: https://flowapp.info/download"
+    toast.style = ToastStyle.Failure;
     return;
   }
+
   const phase = await runAppleScript('tell application "Flow" to getPhase');
   await runAppleScript('tell application "Flow" to skip');
   if (phase === "Flow") {
