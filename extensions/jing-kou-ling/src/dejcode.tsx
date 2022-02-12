4d785f7beeb4d02f17bd3e3b3ece926c13ffb715
陈柘 <zhe@mac-mini.lan>
2022-02-13 00:38:04 +0800

    generate extensions

---
+++ extensions/jing-kou-ling/src/dejcode.tsx
@@ -0,0 +1,88 @@
+import { ActionPanel, Action, List, showToast, Toast, Detail } from "@raycast/api";
+import { useState, useEffect, useRef, useCallback } from "react";
+import axios, { Axios } from "axios"
+
+export default function DeCode() {
+    const { state, decode }: { state: { result: IDcodeResult, isLoading: boolean }, decode: (encodeText: string) => Promise<Toast | void> } = useDecode()
+    console.log(state);
+
+    return (
+        <List
+            isLoading={state.isLoading}
+            onSearchTextChange={decode}
+            searchBarPlaceholder="粘贴需要解析的京东口令"
+            throttle
+        >
+            <List.Section
+                title="解析结果"
+            >
+                {
+                    Object.keys(state.result).map(key => (
+                        < DecodeItem key={key} title={key} value={state.result[key]} />
+                    ))
+                }
+            </List.Section>
+        </List>
+    )
+}
+
+function DecodeItem({ title, value }: { title: string, value: string | number }) {
+    return (
+        <List.Item
+            title={title}
+            subtitle={`${value}`}
+            actions={
+                <ActionPanel>
+                    <Action.CopyToClipboard title="复制到剪贴板" content={`${value}`} />
+                </ActionPanel>
+            }
+        />
+    )
+}
+
+
+function useDecode() {
+    const [state, setState] = useState({ result: {}, isLoading: false })
+    const cancelRef = useRef<AbortController | null>(null)
+    async function decode(encodeText: string): Promise<void | Toast> {
+        cancelRef.current?.abort()
+        cancelRef.current = new AbortController()
+        setState((oldState) => ({
+            ...oldState,
+            isLoading: true
+        }))
+        try {
+            const { data } = await axios.post("https://api.jds.codes/jd/jcommand", { "code": encodeText })
+            console.log(data);
+            if (data.code === 200 && data.data) {
+                setState({ result: data.data, isLoading: false })
+            } else {
+                setState({ result: {}, isLoading: false })
+                return showToast({ style: Toast.Style.Failure, title: data.msg })
+            }
+
+        } catch (error) {
+            console.log(error);
+            setState({ result: {}, isLoading: false })
+            return showToast({ style: Toast.Style.Failure, title: "发生错误" })
+        }
+    }
+    useEffect(() => {
+        return () => {
+            cancelRef.current?.abort();
+        };
+    }, []);
+
+    return {
+        state,
+        decode
+    }
+}
+
+interface IDcodeResult {
+    img?: string,
+    headImg?: string,
+    title?: string,
+    userName?: string,
+    jumpUrl?: string
+}
---
+++ extensions/jing-kou-ling/src/enjcode.tsx
@@ -0,0 +1,49 @@
+import { Form, ActionPanel, Action, showToast, Toast } from "@raycast/api";
+import axios from "axios"
+import { useState } from "react";
+
+export default function Command() {
+
+    const [formLoading, setFormLoading] = useState(false)
+
+    async function handleSubmit(values: { title: string, url: string }) {
+
+        if (!values.url) {
+            return showToast({ title: "参数不完整", message: "请输入需要生成口令的链接提交", style: Toast.Style.Failure })
+        }
+        if (!values.title) {
+            values.title = "动物园欢迎您"
+        }
+        setFormLoading(true)
+        const { data } = await axios.post("https://api.jds.codes/jd/gencode", { ...values })
+        console.log(data)
+        if (data.code === 200) {
+            setFormLoading(false)
+            showToast({ title: "生成成功", message: "口令已复制到剪贴板", style: Toast.Style.Success })
+        } else {
+            setFormLoading(false)
+            showToast({ title: "生成失败", message: data.msg, style: Toast.Style.Failure })
+        }
+
+    }
+
+    return (
+        <>
+            <Form
+                actions={
+                    <ActionPanel>
+                        <Action.SubmitForm onSubmit={handleSubmit} title="生成" />
+                    </ActionPanel>
+                }
+                isLoading={formLoading}
+            >
+                <Form.Description
+                    title="生成京口令"
+                    text="生成自己的京口令"
+                />
+                <Form.TextField id="title" title="口令标题" placeholder="请输入你需要生成的口令的标题" />
+                <Form.TextField id="url" title="口令链接" placeholder="请输入你需要生成的口令的的链接" />
+            </Form>
+        </>
+    );
+}
