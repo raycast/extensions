dd27581ed16a47f798703c3657b5cf82a2194d31
pomdtr <achille.lacoin@dailymotion.com>
2022-02-14 23:40:13 +0100

    add support for piping a file from the clipboard

--- extensions/pipe-commands/src/pipe-clipboard-to-command.tsx
+++ extensions/pipe-commands/src/pipe-clipboard-to-command.tsx
@@ -1,15 +1,29 @@
 import { showToast, Toast } from "@raycast/api";
 import { spawnSync } from "child_process";
-import { PipeCommands } from "./pipe-to-command";
+import { existsSync } from "fs";
+import { PipeCommands, PipeInput } from "./pipe-to-command";
 
-function getClipboardContent(): string {
-  return spawnSync("pbpaste", { encoding: "utf8", maxBuffer: 10 * 1024 * 1024 }).stdout;
+const applescript = `
+if ((clipboard info) as string) contains "«class furl»" then
+    set output to (POSIX path of (the clipboard as «class furl»))
+else
+    set output to the clipboard
+end if
+`;
+
+function getClipboardContent(): PipeInput {
+  const content = spawnSync("osascript", ["-e", applescript], {
+    encoding: "utf8",
+    maxBuffer: 10 * 1024 * 1024,
+  }).stdout.trim();
+  const type = existsSync(content) ? "file" : "text";
+  return { type, content };
 }
 
 export default function PipeClipboardToCommand() {
   try {
-    const content = getClipboardContent();
-    return <PipeCommands input={{ type: "text", content }} />;
+    const input = getClipboardContent();
+    return <PipeCommands input={input} />;
   } catch (e: unknown) {
     showToast(Toast.Style.Failure, (e as Error).message);
   }
