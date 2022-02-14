238a8937c58deeb73842af663b4f1a653a4152d7
Taylor <tayyiorrobinson@gmail.com>
2022-02-14 16:27:42 +0000

    🎨roblox-games: run prettier

--- extensions/roblox-games/src/index.tsx
+++ extensions/roblox-games/src/index.tsx
@@ -1,14 +1,14 @@
 import {
-	ActionPanel,
-	Detail,
-	getPreferenceValues,
-	Icon,
-	List,
-	OpenAction,
-	OpenInBrowserAction,
-	showToast,
-	SubmitFormAction,
-	ToastStyle
+  ActionPanel,
+  Detail,
+  getPreferenceValues,
+  Icon,
+  List,
+  OpenAction,
+  OpenInBrowserAction,
+  showToast,
+  SubmitFormAction,
+  ToastStyle,
 } from "@raycast/api";
 import { spawnSync } from "child_process";
 import { existsSync } from "fs";
@@ -19,151 +19,145 @@ import { getIcon } from "./icons";
 import performSearch from "./search";
 
 async function launchGame(gameId: number) {
-	const toast = await showToast(ToastStyle.Animated, "Launching game...", "Requesting ticket...");
-	const ticket = await getGameLaunchTicket();
-	toast.message = "Launching game...";
-	spawnSync("open", [
-		"-b",
-		"com.roblox.RobloxPlayer",
-		"--args",
-		"-ticket",
-		ticket,
-		"-scriptURL",
-		"https://assetgame.roblox.com/game/PlaceLauncher.ashx?request=RequestGame&browserTrackerId=133769&isPlayTogetherGame=false&placeId=" +
-			gameId,
-		"-browserTrackerId",
-		"133769",
-	]);
-	toast.hide();
+  const toast = await showToast(ToastStyle.Animated, "Launching game...", "Requesting ticket...");
+  const ticket = await getGameLaunchTicket();
+  toast.message = "Launching game...";
+  spawnSync("open", [
+    "-b",
+    "com.roblox.RobloxPlayer",
+    "--args",
+    "-ticket",
+    ticket,
+    "-scriptURL",
+    "https://assetgame.roblox.com/game/PlaceLauncher.ashx?request=RequestGame&browserTrackerId=133769&isPlayTogetherGame=false&placeId=" +
+      gameId,
+    "-browserTrackerId",
+    "133769",
+  ]);
+  toast.hide();
 }
 
 export default function Command() {
-	if (!studioConfigExists()) {
-		if (existsSync("/Applications/RobloxStudio.app/Contents/MacOS/RobloxStudio")) {
-			return (
-				<Detail
-					markdown={`# No account found.
+  if (!studioConfigExists()) {
+    if (existsSync("/Applications/RobloxStudio.app/Contents/MacOS/RobloxStudio")) {
+      return (
+        <Detail
+          markdown={`# No account found.
 
 In order to use this extension, you must be logged in to Roblox Studio.
 
 Click below to open RobloxStudio.`}
-					actions={
-						<ActionPanel title={`Open application`}>
-							<OpenAction title={`Open RobloxStudio`} target="/Applications/RobloxStudio.app" />
-						</ActionPanel>
-					}
-				></Detail>
-			);
-		} else {
-			return (
-				<Detail
-					markdown={`# Roblox Studio not installed.
+          actions={
+            <ActionPanel title={`Open application`}>
+              <OpenAction title={`Open RobloxStudio`} target="/Applications/RobloxStudio.app" />
+            </ActionPanel>
+          }
+        ></Detail>
+      );
+    } else {
+      return (
+        <Detail
+          markdown={`# Roblox Studio not installed.
 
 In order to use this extension, you must be logged in to Roblox Studio.
 
 Click below to download`}
-					actions={
-						<ActionPanel title="Download RobloxStudio">
-							<OpenInBrowserAction
-								title={`Open in browser`}
-								url="https://setup.rbxcdn.com/mac/RobloxStudio.dmg"
-							/>
-						</ActionPanel>
-					}
-				></Detail>
-			);
-		}
-	}
+          actions={
+            <ActionPanel title="Download RobloxStudio">
+              <OpenInBrowserAction title={`Open in browser`} url="https://setup.rbxcdn.com/mac/RobloxStudio.dmg" />
+            </ActionPanel>
+          }
+        ></Detail>
+      );
+    }
+  }
 
-	const { state, search } = useSearch();
-	return (
-		<List isLoading={state.isLoading} onSearchTextChange={search} searchBarPlaceholder="Search by name..." throttle>
-			<List.Section title="Results" subtitle={state.results.length + ""}>
-				{state.results.map((searchResult) => (
-					<SearchListItem key={searchResult.id} searchResult={searchResult} />
-				))}
-			</List.Section>
-		</List>
-	);
+  const { state, search } = useSearch();
+  return (
+    <List isLoading={state.isLoading} onSearchTextChange={search} searchBarPlaceholder="Search by name..." throttle>
+      <List.Section title="Results" subtitle={state.results.length + ""}>
+        {state.results.map((searchResult) => (
+          <SearchListItem key={searchResult.id} searchResult={searchResult} />
+        ))}
+      </List.Section>
+    </List>
+  );
 }
 
 function SearchListItem({ searchResult }: { searchResult: Game }) {
-	return (
-		<List.Item
-			title={searchResult.name}
-			subtitle={searchResult.gameDescription}
-			icon={getIcon("GameIcon:" + searchResult.universeId)}
-			accessoryTitle={
-				getPreferenceValues().accessory == "author"
-					? searchResult.creatorName
-					: searchResult.playerCount.toLocaleString()
-			}
-			accessoryIcon={
-				getPreferenceValues().accessory == "author"
-					? getIcon(
-							(searchResult.creatorType == "Group" ? "GroupIcon:" : "AvatarHeadShot:") +
-								searchResult.creatorId,
-					  )
-					: Icon.Person
-			}
-			actions={
-				<ActionPanel>
-					<ActionPanel.Section>
-						<SubmitFormAction
-							title="Play Game"
-							onSubmit={() => {
-								launchGame(searchResult.placeId);
-							}}
-							icon={Icon.ArrowRight}
-						/>
-						<OpenInBrowserAction
-							title="Open in browser"
-							url={`https://roblox.com/games/${searchResult.placeId}`}
-							shortcut={{ modifiers: ["cmd"], key: "." }}
-						/>
-					</ActionPanel.Section>
-				</ActionPanel>
-			}
-		/>
-	);
+  return (
+    <List.Item
+      title={searchResult.name}
+      subtitle={searchResult.gameDescription}
+      icon={getIcon("GameIcon:" + searchResult.universeId)}
+      accessoryTitle={
+        getPreferenceValues().accessory == "author"
+          ? searchResult.creatorName
+          : searchResult.playerCount.toLocaleString()
+      }
+      accessoryIcon={
+        getPreferenceValues().accessory == "author"
+          ? getIcon((searchResult.creatorType == "Group" ? "GroupIcon:" : "AvatarHeadShot:") + searchResult.creatorId)
+          : Icon.Person
+      }
+      actions={
+        <ActionPanel>
+          <ActionPanel.Section>
+            <SubmitFormAction
+              title="Play Game"
+              onSubmit={() => {
+                launchGame(searchResult.placeId);
+              }}
+              icon={Icon.ArrowRight}
+            />
+            <OpenInBrowserAction
+              title="Open in browser"
+              url={`https://roblox.com/games/${searchResult.placeId}`}
+              shortcut={{ modifiers: ["cmd"], key: "." }}
+            />
+          </ActionPanel.Section>
+        </ActionPanel>
+      }
+    />
+  );
 }
 
 function useSearch() {
-	const [state, setState] = useState<SearchState>({ results: [], isLoading: true });
-	const cancelRef = useRef<AbortController | null>(null);
+  const [state, setState] = useState<SearchState>({ results: [], isLoading: true });
+  const cancelRef = useRef<AbortController | null>(null);
 
-	useEffect(() => {
-		search("");
-		return () => {
-			cancelRef.current?.abort();
-		};
-	}, []);
+  useEffect(() => {
+    search("");
+    return () => {
+      cancelRef.current?.abort();
+    };
+  }, []);
 
-	async function search(searchText: string) {
-		cancelRef.current?.abort();
-		cancelRef.current = new AbortController();
-		try {
-			setState((oldState) => ({
-				...oldState,
-				isLoading: true,
-			}));
-			const results = await performSearch(searchText, cancelRef.current.signal);
-			setState((oldState) => ({
-				...oldState,
-				results: results,
-				isLoading: false,
-			}));
-		} catch (error) {
-			if (error instanceof AbortError) {
-				return;
-			}
-			console.error("search error", error);
-			showToast(ToastStyle.Failure, "Could not perform search", String(error));
-		}
-	}
+  async function search(searchText: string) {
+    cancelRef.current?.abort();
+    cancelRef.current = new AbortController();
+    try {
+      setState((oldState) => ({
+        ...oldState,
+        isLoading: true,
+      }));
+      const results = await performSearch(searchText, cancelRef.current.signal);
+      setState((oldState) => ({
+        ...oldState,
+        results: results,
+        isLoading: false,
+      }));
+    } catch (error) {
+      if (error instanceof AbortError) {
+        return;
+      }
+      console.error("search error", error);
+      showToast(ToastStyle.Failure, "Could not perform search", String(error));
+    }
+  }
 
-	return {
-		state: state,
-		search: search,
-	};
+  return {
+    state: state,
+    search: search,
+  };
 }
--- extensions/roblox-games/src/types.d.ts
+++ extensions/roblox-games/src/types.d.ts
@@ -1,24 +1,24 @@
 interface SearchState {
-	results: SearchResult[];
-	isLoading: boolean;
+  results: SearchResult[];
+  isLoading: boolean;
 }
 
 interface Game {
-	creatorId: number;
-	creatorName: string;
-	creatorType: string;
-	totalUpVotes: number;
-	totalDownVotes: number;
-	universeId: number;
-	name: string;
-	placeId: number;
-	playerCount: number;
-	imageToken: string;
-	isSponsored: boolean;
-	nativeAdData: string;
-	isShowSponsoredLabel: boolean;
-	price?: number;
-	analyticsIdentifier?: any;
-	gameDescription: string;
-	genre: string;
+  creatorId: number;
+  creatorName: string;
+  creatorType: string;
+  totalUpVotes: number;
+  totalDownVotes: number;
+  universeId: number;
+  name: string;
+  placeId: number;
+  playerCount: number;
+  imageToken: string;
+  isSponsored: boolean;
+  nativeAdData: string;
+  isShowSponsoredLabel: boolean;
+  price?: number;
+  analyticsIdentifier?: any;
+  gameDescription: string;
+  genre: string;
 }
