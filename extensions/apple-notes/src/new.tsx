24c8f3a3b797f44438f6c3dc44774c0211413c5a
Thomas Moeskops <thomasmoeskops@brightfish.nl>
2022-02-18 11:53:46 +0100

    Codestyle: changed quotation

--- extensions/apple-notes/src/new.tsx
+++ extensions/apple-notes/src/new.tsx
@@ -4,9 +4,9 @@ import { runAppleScript } from "run-applescript";
 export default async () => {
   await closeMainWindow();
   await runAppleScript(
-    "tell application \"Notes\" to activate \n" +
-      "tell application \"System Events\" to tell process \"Notes\" to ¬\n" +
-      "click menu item \"New Note\" of menu 1 of ¬\n" +
-      "menu bar item \"File\" of menu bar 1"
+    'tell application "Notes" to activate \n' +
+      'tell application "System Events" to tell process "Notes" to ¬\n' +
+      'click menu item "New Note" of menu 1 of ¬\n' +
+      'menu bar item "File" of menu bar 1'
   );
 };
