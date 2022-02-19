e1afce26a62ac738b4f64035182ef30f32b30bbb
Sandro Mosca <sandro.mosca.dev@gmail.com>
2022-02-19 15:44:53 +0100

    Run prettier

--- extensions/amazing-marvin-inbox/src/index.tsx
+++ extensions/amazing-marvin-inbox/src/index.tsx
@@ -6,31 +6,30 @@ interface FormInputs {
 }
 
 interface Preferences {
-  apiKey: string
+  apiKey: string;
 }
 
 export default function () {
-  const preferences = getPreferenceValues<Preferences>()
+  const preferences = getPreferenceValues<Preferences>();
 
   async function handleSubmit(values: FormInputs) {
     showToast(Toast.Style.Animated, "Creating the task");
 
-    if (values.taskdescription == "")
-      return showToast(Toast.Style.Failure, "Couldn't create an empty task")
+    if (values.taskdescription == "") return showToast(Toast.Style.Failure, "Couldn't create an empty task");
 
     try {
       await got.post("https://serv.amazingmarvin.com/api/addTask", {
         json: {
-          title: values.taskdescription
+          title: values.taskdescription,
         },
         headers: {
-          'X-Api-Token': preferences.apiKey
-        }
-      })
+          "X-Api-Token": preferences.apiKey,
+        },
+      });
 
       showToast(Toast.Style.Success, "Task created");
     } catch (error) {
-      showToast(Toast.Style.Failure, `Couldn't create the task: ${error.message}`)
+      showToast(Toast.Style.Failure, `Couldn't create the task: ${error}`);
     }
   }
 
