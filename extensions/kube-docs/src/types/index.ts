ebc43bd67a52f0cdd71481385ae37837583124df
Rui Silva <rui.s@tamanna.com>
2022-02-20 14:19:14 +0000

    feat(kube-docs): implemented kubernetes docs extension

---
+++ extensions/kube-docs/src/types/index.ts
@@ -0,0 +1,54 @@
+export type FetcherArgs = {
+  query: string;
+  offset?: number;
+}
+
+export type DocsResponse = {
+  __type: string;
+  instrumentation: {
+    __type: string;
+    pingUrlBase: string;
+    pageLoadPingUrl: string;
+  };
+  queryContext: {
+    originalQuery: string;
+  };
+  webPages?: {
+    webSearchUrl: string;
+    webSearchUrlPingSuffix: string;
+    totalEstimatedMatches: number;
+    value: DocsItem[];
+  };
+  rankingResponse: {
+    mainline?: {
+      items: {
+        answerType: string;
+        resultIndex: number;
+        value: {
+          id: string;
+        }
+      }
+    }
+  }
+};
+
+export type DocsItem = {
+  id: string;
+  name: string;
+  url: string;
+  urlPingSuffix: string;
+  datePublished: string;
+  datePublishedDisplayText: string;
+  isFamilyFriendly: boolean;
+  displayUrl: string;
+  snippet: string;
+  dateLastCrawled: string;
+  openGraphImage: {
+    contentUrl: string;
+    width: number;
+    height: number;
+  };
+  fixedPosition: boolean;
+  language: string;
+  isNavigational: boolean;
+};
