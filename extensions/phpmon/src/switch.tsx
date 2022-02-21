6a9666ffecde5ed5aa8c6bd79900e432c9231046
Nico Verbruggen <mail@nicoverbruggen.be>
2022-02-21 22:45:00 +0100

    Ran linter again

--- extensions/phpmon/src/switch.tsx
+++ extensions/phpmon/src/switch.tsx
@@ -3,9 +3,24 @@ import { existsSync } from "fs";
 import { execSync } from "child_process";
 
 export default function SwitchCommand() {
-  const versions = ["5.6", "7.0", "7.1", "7.2", "7.3", "7.4", "8.0", "8.1", "8.2", "8.3", "8.4", "9.0"];
+  const versions = [
+    "5.6",
+    "7.0",
+    "7.1",
+    "7.2",
+    "7.3",
+    "7.4",
+    "8.0",
+    "8.1",
+    "8.2",
+    "8.3",
+    "8.4",
+    "9.0",
+  ];
 
-  const brewPath = existsSync("/opt/homebrew/bin/brew") ? "/opt/homebrew" : "/usr/local";
+  const brewPath = existsSync("/opt/homebrew/bin/brew")
+    ? "/opt/homebrew"
+    : "/usr/local";
 
   function items() {
     return versions.reverse().filter(function (version) {
@@ -16,7 +31,10 @@ export default function SwitchCommand() {
   let current: string | null = null;
 
   if (existsSync(brewPath + "/bin/php-config")) {
-    current = String(execSync("/opt/homebrew/bin/php-config --version")).slice(0, 3);
+    current = String(execSync("/opt/homebrew/bin/php-config --version")).slice(
+      0,
+      3
+    );
   }
 
   return (
@@ -28,8 +46,12 @@ export default function SwitchCommand() {
           title={"Switch to PHP " + version}
           subtitle={
             version != current
-              ? "Switch to PHP " + version + " and get notified once the switch is done."
-              : "Attempt to switch to PHP " + version + ", even though this is already linked."
+              ? "Switch to PHP " +
+                version +
+                " and get notified once the switch is done."
+              : "Attempt to switch to PHP " +
+                version +
+                ", even though this is already linked."
           }
           accessoryTitle={version == current ? "Active" : ""}
           accessoryIcon={version == current ? Icon.Checkmark : ""}
