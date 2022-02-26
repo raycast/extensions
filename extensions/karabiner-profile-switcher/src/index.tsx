269529feb28f20948a52e81169f8de0138bbcd93
Luca Schultz <45899273+lucaschultz@users.noreply.github.com>
2022-02-26 16:11:26 +0100

    Add „Karabiner Profile Switcher“ extension

---
+++ extensions/karabiner-profile-switcher/src/index.tsx
@@ -0,0 +1,89 @@
+import {
+  ActionPanel,
+  Action,
+  Icon,
+  List,
+  showToast,
+  Toast,
+  Color,
+  showHUD,
+  popToRoot,
+  closeMainWindow,
+  open,
+  Clipboard,
+} from "@raycast/api";
+import { useEffect, useState } from "react";
+import { ILayout } from "./model/interfaces";
+import { isKarabinerCliAvailable, KarabinerManager } from "./model/KarabinerManager";
+
+function toastErrorOptions(message: string, error: string): Toast.Options {
+  return {
+    title: "An Error Occured",
+    message,
+    style: Toast.Style.Failure,
+    primaryAction: {
+      title: "Copy Error",
+      onAction: () => Clipboard.copy(`${error}`),
+      shortcut: { modifiers: ["cmd", "shift"], key: "c" },
+    },
+  };
+}
+
+async function handleLayoutSwitch(profile: ILayout) {
+  try {
+    await profile.activate();
+    await Promise.all([showHUD(`Activated ${profile.title}`), popToRoot(), closeMainWindow()]);
+  } catch (e) {
+    await showToast(toastErrorOptions(`Couldn't activate ${profile.title}`, `${e}`));
+  }
+}
+
+export default function Command() {
+  const [profiles, setProfiles] = useState<ILayout[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setProfiles(await KarabinerManager.getAll());
+      } catch (e) {
+        if (!(await isKarabinerCliAvailable())) {
+          const options: Toast.Options = {
+            ...toastErrorOptions("Karabiner Elements CLI Not Available", `${e}`),
+            secondaryAction: {
+              title: "Get Karabiner",
+              onAction: () => open("https://karabiner-elements.pqrs.org"),
+              shortcut: { modifiers: ["cmd"], key: "o" },
+            },
+          };
+          await showToast(options);
+        } else {
+          await showToast(toastErrorOptions("Couldn't Get Profiles", `${e}`));
+        }
+      }
+    })();
+  }, []);
+
+  return (
+    <List isLoading={profiles.length === 0} searchBarPlaceholder="Search available layout...">
+      {profiles.map((source) => (
+        <List.Item
+          key={source.id}
+          icon={source.active ? { source: Icon.Checkmark, tintColor: Color.Green } : Icon.Circle}
+          title={source.title}
+          accessoryTitle={source.active ? "Current Profile" : ""}
+          keywords={[source.id]}
+          actions={
+            <ActionPanel>
+              <Action icon={Icon.Checkmark} title="Activate" onAction={() => handleLayoutSwitch(source)} />
+              <Action
+                icon={Icon.Gear}
+                title="Open Karabiner Elements"
+                onAction={() => open("/System/Volumes/Data/Applications/Karabiner-Elements.app")}
+              />
+            </ActionPanel>
+          }
+        />
+      ))}
+    </List>
+  );
+}
