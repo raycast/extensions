27c8c307a4808bf1ea5f92e2ea7fc24f5234668b
vimtor <victor@vimtor.io>
2022-02-28 17:22:24 +0100

    Escape Wikipedia featured and random page URLs

--- extensions/wikipedia/src/random-page.tsx
+++ extensions/wikipedia/src/random-page.tsx
@@ -3,6 +3,6 @@ import { closeMainWindow, open } from "@raycast/api";
 
 export default async function () {
   const pageTitle = await getRandomPageTitle();
-  await open(`https://wikipedia.org/wiki/${pageTitle}`);
+  await open(`https://wikipedia.org/wiki/${pageTitle.replaceAll(" ", "_")}`);
   await closeMainWindow({ clearRootSearch: true });
 }
