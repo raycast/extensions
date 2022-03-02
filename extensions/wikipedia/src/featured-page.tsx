aa59f2b31fdb1524d0c05557fe561be22b2835e3
Víctor Navarro <victor@vimtor.io>
2022-03-02 14:52:25 +0100

    Escape Wikipedia featured and random page URLs (#1007)
    * Escape Wikipedia featured and random page URLs
    
    * Encode URI component for Wikipedia page title
    
    * Also replace underscores
    
    * Remove HTML tags from Wikipedia titles
    
    * Prettier
    
    Co-authored-by: Mathieu Dutour <mathieu@dutour.me>

--- extensions/wikipedia/src/featured-page.tsx
+++ extensions/wikipedia/src/featured-page.tsx
@@ -1,8 +1,8 @@
-import { getTodayFeaturedPageTitle } from "./wikipedia";
+import { encodeTitle, getTodayFeaturedPageTitle } from "./wikipedia";
 import { closeMainWindow, open } from "@raycast/api";
 
 export default async function () {
   const pageTitle = await getTodayFeaturedPageTitle();
-  await open(`https://wikipedia.org/wiki/${pageTitle}`);
+  await open(`https://wikipedia.org/wiki/${encodeTitle(pageTitle)}`);
   await closeMainWindow({ clearRootSearch: true });
 }
