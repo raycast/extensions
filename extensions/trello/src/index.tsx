bb5075e4cfa7e4214c8fae6820d74e6ef99c07c1
Chris Chinchilla <chris@chrischinchilla.com>
2022-03-05 12:34:32 +0100

    Prettier fixes

--- extensions/trello/src/index.tsx
+++ extensions/trello/src/index.tsx
@@ -28,8 +28,8 @@ export default function PackageList() {
     <List isLoading={loading} searchBarPlaceholder={`Filter todos`} throttle>
       {results?.length
         ? results.map((result) => {
-          return <TodoListItem key={result.id} result={result} />;
-        })
+            return <TodoListItem key={result.id} result={result} />;
+          })
         : null}
     </List>
   );
