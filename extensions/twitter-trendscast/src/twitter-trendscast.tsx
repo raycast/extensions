d3831034cb538d3a395fd08cb875d9e565bfb5c4
pradeep <pradeepboorgu@gmail.com>
2022-03-07 21:50:16 +0530

    Remove URLs that are behaving weird

--- extensions/twitter-trendscast/src/twitter-trendscast.tsx
+++ extensions/twitter-trendscast/src/twitter-trendscast.tsx
@@ -11,28 +11,26 @@ class TwitterTrend {
 
 export default function Command() {
   const twitterTrends = [
-    new TwitterTrend("1", "globe.png", "What's happening around you?", "https://twitter.com/explore/tabs/for-you"),
-    new TwitterTrend("2", "trends.png", "What's trending around you?", "https://twitter.com/explore/tabs/trending"),
     new TwitterTrend(
-      "3",
+      "1",
       "bolt.png",
       "Top tweets of the day from people you follow",
       "https://twitter.com/search?q=min_faves%3A50%20filter%3Afollows&src=typed_query&f=top"
     ),
     new TwitterTrend(
-      "4",
+      "2",
       "flame.png",
       "Popular tweets of the day from people you follow",
       "https://twitter.com/search?q=min_faves:25%20min_retweets:25%20filter:follows&src=typed_query&f=top"
     ),
     new TwitterTrend(
-      "5",
+      "3",
       "newspaper.png",
       "Latest media tweets of the day from people you follow",
       "https://twitter.com/search?q=filter%3Amedia%20filter%3Afollows&src=typed_query&f=top"
     ),
     new TwitterTrend(
-      "6",
+      "4",
       "checkmark.png",
       "Tweets from the verified users",
       "https://twitter.com/search?q=filter%3Averified&src=typed_query&f=top"
