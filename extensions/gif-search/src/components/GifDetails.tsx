c3c2fef56e28d4267b1e01465552f2e59c9ab729
josephschmitt <joe@compass.com>
2022-03-08 01:21:45 -0500

    Add actions to details page

--- extensions/gif-search/src/components/GifDetails.tsx
+++ extensions/gif-search/src/components/GifDetails.tsx
@@ -1,14 +1,39 @@
-import { Detail } from "@raycast/api";
+import path from "path";
+
+import { Action, ActionPanel, Detail } from "@raycast/api";
 
 import { useDecodedGif } from "../hooks/useDecodedGif";
 import { useGifFrames } from "../hooks/useGifFrames";
 import type { IGif } from "../models/gif";
 
 export function GifDetails(props: { item: IGif; index: number }) {
-  const { frames = [], isDecoded } = useDecodedGif(props.item.gif_url, props.item.slug);
+  const { gif_url, slug, title, url } = props.item;
+
+  const { frames = [], isDecoded } = useDecodedGif(gif_url, slug);
   const { currentFrame } = useGifFrames(0, frames, 60);
 
   const md = `<img src="${currentFrame}">`;
 
-  return <Detail isLoading={!isDecoded} navigationTitle={props.item.title} markdown={md} />;
+  return (
+    <Detail
+      isLoading={!isDecoded}
+      navigationTitle={title}
+      markdown={md}
+      actions={
+        <ActionPanel title={slug}>
+          <Action.CopyToClipboard
+            title="Copy GIF URL to Clipboard"
+            content={gif_url}
+            shortcut={{ modifiers: ["cmd", "shift"], key: "c" }}
+          />
+          <Action.CopyToClipboard
+            title="Copy Page URL to Clipboard"
+            content={url}
+            shortcut={{ modifiers: ["opt", "shift"], key: "c" }}
+          />
+          <Action.OpenInBrowser url={url} />
+        </ActionPanel>
+      }
+    />
+  );
 }
