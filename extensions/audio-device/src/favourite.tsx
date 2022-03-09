3af3f346b1aa963447e9d7a2204da49c14987e31
Michael Barrett <mike182uk@gmail.com>
2022-03-09 08:00:59 +0000

    make favourite command no-view

--- extensions/audio-device/src/favourite.tsx
+++ extensions/audio-device/src/favourite.tsx
@@ -1,15 +1,13 @@
-import { getPreferenceValues, popToRoot, showHUD, showToast, Toast } from "@raycast/api";
+import { getPreferenceValues, showHUD, showToast, Toast } from "@raycast/api";
 import { setOutputDevice } from "./utils";
 
 const preferences = getPreferenceValues();
 if (preferences.favourite != null && preferences.favourite !== "") {
   setOutputDevice(preferences.favourite);
-  popToRoot();
   showHUD(`Active audio device set to ${preferences.favourite}`);
 } else {
   showToast({
     style: Toast.Style.Failure,
     title: "No favorite device specified",
   });
-  popToRoot();
 }
