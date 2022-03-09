d2497927927d138e257290769c15c9c030661ac6
gebeto <slavik.nychkalo@gmail.com>
2022-03-09 16:20:57 +0200

    update dependencies, add translation form

---
+++ extensions/google-translate/src/hooks.ts
@@ -0,0 +1,14 @@
+import React from 'react';
+import { getPreferenceValues } from "@raycast/api";
+import { LanguageCode } from "./languages";
+
+
+export type TranslatePreferences = {
+  lang1: LanguageCode;
+  lang2: LanguageCode;
+};
+
+
+export const usePreferences = () => {
+  return React.useMemo(() => getPreferenceValues<TranslatePreferences>(), []);
+}
