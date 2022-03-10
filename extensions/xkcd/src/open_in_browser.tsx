eb462f775f9926b3b433777fcab9af468fb5ef87
maggie-j-liu <maggie.j.liu@gmail.com>
2022-03-09 22:19:45 -0800

    remove unused assets

--- extensions/xkcd/src/open_in_browser.tsx
+++ extensions/xkcd/src/open_in_browser.tsx
@@ -1,16 +1,11 @@
-import { Action } from "@raycast/api";
+import { Action, Icon } from "@raycast/api";
 import { useAtom } from "jotai";
 import { currentComicAtom } from "./atoms";
 import { BASE_URL } from "./xkcd";
 
 const OpenComicInBrowser = () => {
   const [currentComic] = useAtom(currentComicAtom);
-  return (
-    <Action.OpenInBrowser
-      icon={{ source: { light: "globe.png", dark: "globe@dark.png" } }}
-      url={`${BASE_URL}/${currentComic}/`}
-    />
-  );
+  return <Action.OpenInBrowser icon={Icon.Globe} url={`${BASE_URL}/${currentComic}/`} />;
 };
 
 export default OpenComicInBrowser;
