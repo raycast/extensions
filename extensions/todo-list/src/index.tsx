1157cec9a03a0c44bfe90d9385c8d24ad2297869
maggie-j-liu <maggie.j.liu@gmail.com>
2022-03-10 18:17:42 -0800

    implement editing and opening urls

--- extensions/todo-list/src/index.tsx
+++ extensions/todo-list/src/index.tsx
@@ -1,20 +1,18 @@
-import { ActionPanel, clearSearchBar, environment, List, showToast, ToastStyle } from "@raycast/api";
+import { environment, List } from "@raycast/api";
 import { useEffect, useState } from "react";
 import fs from "fs/promises";
-import { searchModeAtom, todoAtom } from "./atoms";
+import { editingAtom, searchBarTextAtom, searchModeAtom, todoAtom } from "./atoms";
 import { useAtom } from "jotai";
 import { DEFAULT_SECTIONS, TODO_FILE } from "./config";
-import _ from "lodash";
-import { insertIntoSection, compare } from "./utils";
-import DeleteAllAction from "./delete_all";
 import TodoSection from "./todo_section";
-import SearchModeAction from "./search_mode_action";
+import ListActions from "./list_actions";
 
 export default function TodoList() {
-  const [todoSections, setTodoSections] = useAtom(todoAtom);
-  const [newTodoText, setNewTodoText] = useState("");
-  const [searchMode, setSearchMode] = useAtom(searchModeAtom);
+  const [, setTodoSections] = useAtom(todoAtom);
+  const [searchMode] = useAtom(searchModeAtom);
   const [loading, setLoading] = useState(true);
+  const [searchBarText, setSearchBarText] = useAtom(searchBarTextAtom);
+  const [editing] = useAtom(editingAtom);
   useEffect(() => {
     (async () => {
       try {
@@ -50,37 +48,15 @@ export default function TodoList() {
     })();
   }, []);
 
-  const addTodo = async () => {
-    if (newTodoText.length === 0) {
-      await showToast(ToastStyle.Failure, "Empty todo", "Todo items cannot be empty.");
-      return;
-    }
-    todoSections.todo = [
-      ...insertIntoSection(
-        todoSections.todo,
-        {
-          title: newTodoText,
-          completed: false,
-          timeAdded: Date.now(),
-        },
-        compare
-      ),
-    ];
-    await clearSearchBar();
-    setTodoSections(_.cloneDeep(todoSections));
-  };
   return (
     <List
+      navigationTitle={`Manage Todo List${editing !== false ? " • Editing" : searchMode ? " • Searching" : ""}`}
       key={searchMode ? "search" : "nosearch"}
       isLoading={loading}
-      actions={
-        <ActionPanel>
-          {!searchMode && <ActionPanel.Item title="Create Todo" onAction={() => addTodo()} />}
-          <SearchModeAction />
-          <DeleteAllAction />
-        </ActionPanel>
-      }
-      onSearchTextChange={searchMode ? undefined : (text: string) => setNewTodoText(text.trimEnd())}
+      actions={<ListActions />}
+      enableFiltering={searchMode}
+      searchText={searchBarText}
+      onSearchTextChange={(text: string) => setSearchBarText(text)}
       searchBarPlaceholder={searchMode ? "Search todos" : "Type and hit enter to add an item to your list"}
     >
       <TodoSection sectionKey="pinned" />
