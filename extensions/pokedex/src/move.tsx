e06a66c1168ee43bb691049eea4af1d5a040a895
Anh Thang Bui <buianhthang89@gmail.com>
2022-03-12 23:38:48 +0700

    feat: use svg icon, update type filter

--- extensions/pokedex/src/move.tsx
+++ extensions/pokedex/src/move.tsx
@@ -1,14 +1,19 @@
 import { useMemo, useState } from "react";
-import { List } from "@raycast/api";
+import { List, getPreferenceValues } from "@raycast/api";
+import json2md from "json2md";
 import groupBy from "lodash.groupby";
+import TypeDropdown from "./components/type_dropdown";
 
 import moves from "./statics/moves.json";
 
+const preference = getPreferenceValues();
+
 export default function Move() {
   const [searchText, setSearchText] = useState<string>("");
+  const [type, setType] = useState<string>("all");
 
   const generations = useMemo(() => {
-    const listing = searchText
+    let listing = searchText
       ? moves.filter(
           (move) =>
             move.name.toLowerCase().includes(searchText.toLowerCase()) ||
@@ -16,11 +21,22 @@ export default function Move() {
         )
       : moves;
 
+    if (type != "all") {
+      listing = listing.filter((p) => p.type === type);
+    }
+
     return groupBy(listing, "generation");
-  }, [searchText]);
+  }, [searchText, type]);
 
   return (
-    <List throttle onSearchTextChange={setSearchText}>
+    <List
+      throttle
+      onSearchTextChange={setSearchText}
+      isShowingDetail={preference.showPreview}
+      searchBarAccessory={
+        <TypeDropdown command="Move" onSelectType={setType} />
+      }
+    >
       {Object.entries(generations).map(([generation, moves]) => {
         return (
           <List.Section key={generation} title={generation}>
@@ -29,10 +45,27 @@ export default function Move() {
                 <List.Item
                   key={idx}
                   title={move.name}
-                  subtitle={move.short_effect}
+                  subtitle={
+                    preference.showPreview ? undefined : move.short_effect
+                  }
                   icon={`moves/${move.damage_class || "status"}.png`}
                   accessoryTitle={move.type}
-                  accessoryIcon={`types/${move.type}.png`}
+                  accessoryIcon={`types/${move.type.toLowerCase()}.svg`}
+                  detail={
+                    preference.showPreview ? (
+                      <List.Item.Detail
+                        markdown={json2md([
+                          {
+                            h1: move.name,
+                          },
+                          { p: move.short_effect },
+                          { p: `**Power**: ${move.power || "-"}` },
+                          { p: `**Accuracy**: ${move.accuracy || "-"}` },
+                          { p: `**PP**: ${move.pp || "-"}` },
+                        ])}
+                      />
+                    ) : undefined
+                  }
                 />
               );
             })}
