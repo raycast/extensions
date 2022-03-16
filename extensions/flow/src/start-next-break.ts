b41c6bd38400caef076d5ac5531b693e6469aaa8
vimtor <victor@vimtor.io>
2022-03-16 12:16:18 +0100

    Add "Start Next Break" command

---
+++ extensions/flow/src/start-next-break.ts
@@ -0,0 +1,26 @@
+import { showHUD, Toast, ToastStyle } from "@raycast/api";
+import { getCurrentPhase, isFlowInstalled, resetTimer, skipSession } from "./utils";
+
+export default async function() {
+  const toast = new Toast({
+    title: "Starting next session",
+    style: ToastStyle.Animated
+  });
+
+  toast.show();
+
+  if (!(await isFlowInstalled())) {
+    toast.title = "Flow not installed";
+    toast.message = "Install it from: https://flowapp.info/download";
+    toast.style = ToastStyle.Failure;
+    return;
+  }
+
+  const phase = await getCurrentPhase();
+  if (phase === "Flow") {
+    await skipSession();
+  } else {
+    await resetTimer();
+  }
+  await showHUD("Break session started");
+}
