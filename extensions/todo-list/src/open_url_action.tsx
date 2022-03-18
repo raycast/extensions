dbb807cacdb88a48385efb2095ff0594dbe87a01
maggie-j-liu <maggie.j.liu@gmail.com>
2022-03-17 22:50:22 -0700

    better method

--- extensions/todo-list/src/open_url_action.tsx
+++ extensions/todo-list/src/open_url_action.tsx
@@ -2,17 +2,22 @@ import { Action, Keyboard } from "@raycast/api";
 import { URL } from "url";
 
 const OpenUrlAction = ({ url, shortcut, title }: { url: string; shortcut?: Keyboard.Shortcut; title?: string }) => {
-  const resolvedURL = url.includes(":") ? url : `http://${url}`;
-  const hostname = new URL(resolvedURL).hostname;
+  let hostname;
+  let resolvedUrl = url;
+  try {
+    hostname = new URL(url).hostname;
+  } catch (e) {
+    try {
+      resolvedUrl = `http://${url}`;
+      hostname = new URL(resolvedUrl).hostname;
+    } catch (e) {
+      return null;
+    }
+  }
   const faviconUrl = `https://www.google.com/s2/favicons?sz=64&domain=${hostname}`;
   return (
     <>
-      <Action.OpenInBrowser
-        icon={{ source: faviconUrl }}
-        shortcut={shortcut}
-        title={title ?? resolvedURL}
-        url={resolvedURL}
-      />
+      <Action.OpenInBrowser icon={{ source: faviconUrl }} shortcut={shortcut} title={title ?? url} url={resolvedUrl} />
     </>
   );
 };
