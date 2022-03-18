c059b979266175407f1818c94781b8c009cb1b6a
maggie-j-liu <maggie.j.liu@gmail.com>
2022-03-17 18:40:15 -0700

    fix: add scheme to url if there is none

--- extensions/todo-list/src/open_url_action.tsx
+++ extensions/todo-list/src/open_url_action.tsx
@@ -2,11 +2,17 @@ import { Action, Keyboard } from "@raycast/api";
 import { URL } from "url";
 
 const OpenUrlAction = ({ url, shortcut, title }: { url: string; shortcut?: Keyboard.Shortcut; title?: string }) => {
-  const hostname = new URL(url).hostname;
+  const resolvedURL = url.includes(":") ? url : `http://${url}`;
+  const hostname = new URL(resolvedURL).hostname;
   const faviconUrl = `https://www.google.com/s2/favicons?sz=64&domain=${hostname}`;
   return (
     <>
-      <Action.OpenInBrowser icon={{ source: faviconUrl }} shortcut={shortcut} title={title ?? url} url={url} />
+      <Action.OpenInBrowser
+        icon={{ source: faviconUrl }}
+        shortcut={shortcut}
+        title={title ?? resolvedURL}
+        url={resolvedURL}
+      />
     </>
   );
 };
