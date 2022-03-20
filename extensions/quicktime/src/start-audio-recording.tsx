c5bee90520a2e95c516f8e994dcccbd8cc17d1c5
pradeep <pradeepboorgu@gmail.com>
2022-03-20 17:05:27 +0530

    fix using prettier

--- extensions/quicktime/src/start-audio-recording.tsx
+++ extensions/quicktime/src/start-audio-recording.tsx
@@ -3,7 +3,9 @@ import { runAppleScript } from "run-applescript";
 
 export default async () => {
   await closeMainWindow();
-  await runAppleScript('tell application "System Events" \n tell application "QuickTime Player" to activate frontmost \n tell application "QuickTime Player" to start (new audio recording) \n end tell');
+  await runAppleScript(
+    'tell application "System Events" \n tell application "QuickTime Player" to activate frontmost \n tell application "QuickTime Player" to start (new audio recording) \n end tell'
+  );
 };
 
 /*
