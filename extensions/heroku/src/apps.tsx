56021f3f9848a741f766c4e05854cc51cb05042d
Yukai Huang <yukaihuangtw@gmail.com>
2022-03-21 22:43:27 +0800

    feat: polish apps

--- extensions/heroku/src/apps.tsx
+++ extensions/heroku/src/apps.tsx
@@ -1,24 +1,31 @@
-import { List } from "@raycast/api";
-import useSWR from 'swr'
-import heroku from './heroku'
+import { Action, ActionPanel, Icon, List } from "@raycast/api";
+import useSWR from "swr";
+import heroku, { simplifyCustomResponse } from "./heroku";
 
 export default function Command() {
-  const { data, error } = useSWR('apps', () => heroku.requests.getApps({}).then(res => {
-    if (res.hasFailed) {
-      throw res.error
-    } else {
-      return res.data
-    }
-  }))
+  const { data, error } = useSWR("apps", () => heroku.requests.getApps({}).then(simplifyCustomResponse));
 
   if (!data) {
-    return <List isLoading />
+    return <List isLoading />;
   }
-  
-  return <List>
-    {data.map(app => <List.Item 
-      title={app.name}
-      key={app.id}
-    />)}
-  </List>
+
+  return (
+    <List>
+      {data.map((app) => (
+        <List.Item
+          title={app.name}
+          key={app.id}
+          icon={Icon.Desktop}
+          accessoryTitle={app.updated_at ? `Updated at ${new Date(app.updated_at).toLocaleString()}` : ""}
+          subtitle={app.stack.name}
+          accessoryIcon={Icon.Calendar}
+          actions={
+            <ActionPanel>
+              <Action.OpenInBrowser url={app.web_url} title="Open App in Browser" />
+            </ActionPanel>
+          }
+        />
+      ))}
+    </List>
+  );
 }
