ee7cf1735a1bc39629d8a3137bcb722ac30ab94a
Timur Badretdinov <destinerx@gmail.com>
2022-03-22 19:41:01 +0400

    Clean up path to svg convertion

--- extensions/icons/src/utils.ts
+++ extensions/icons/src/utils.ts
@@ -1,22 +1,11 @@
 import { encode } from 'js-base64';
 
 function toSvg(path: string, width: number, height: number): string {
-  return `
-  <svg width="${width}" height="${height}" viewBox="0 0 ${width} ${height}" xmlns="http://www.w3.org/2000/svg">
-    <style>
-      path {
-        fill: white;
-      }
-    </style>
-    ${path}
-  </svg>`;
+  return `<svg width="${width}" height="${height}" viewBox="0 0 ${width} ${height}" xmlns="http://www.w3.org/2000/svg">${path}</svg>`;
 }
 
 function toBase64(svg: string): string {
   return `data:image/svg+xml;base64,${encode(svg)}`;
 }
 
-export {
-  toSvg,
-  toBase64,
-};
+export { toSvg, toBase64 };
