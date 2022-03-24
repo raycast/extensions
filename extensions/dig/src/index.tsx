7515b402fe232020fdff1c12821ac6bbd2d9687d
Daniel Bahl <db@v5.dk>
2022-03-24 19:52:33 +0100

    version 1 of dig

---
+++ extensions/dig/src/index.tsx
@@ -0,0 +1,166 @@
+import {
+  ActionPanel,
+  CopyToClipboardAction,
+  Detail,
+  Icon,
+  List,
+  OpenInBrowserAction,
+  showToast,
+  ToastStyle,
+  useNavigation,
+} from "@raycast/api";
+import { useState, useEffect } from "react";
+var execSync = require('child_process').execSync;
+
+export default function DigSearchResultsList() {
+  const [query, setQuery] = useState<null | string>(null);
+  const [state, setState] = useState<Result[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { push } = useNavigation();
+
+  useEffect(() => {
+    async function fetch() {
+      if (!query) {
+        setState([]);
+        return;
+      }
+      setIsLoading(true);
+      const results = await digByQuery(query);
+      setState(results);
+      setIsLoading(false);
+    }
+    fetch();
+  }, [query]);
+
+  return (
+    <List
+      isLoading={isLoading}
+      searchBarPlaceholder="Type a valid Hostname (ex. raycast.com or raycast.com mx)"
+      onSearchTextChange={(text) => setQuery(text)}
+      throttle
+    >
+      {state.map((result, idx) => (
+        <List.Item
+          id={idx.toString()}
+          key={idx}
+          title={result.title}
+          icon="icon.png"
+          accessories={[{ text: result.summary }]}
+          actions={
+            <ActionPanel>
+              <CopyToClipboardAction
+                title="Copy Destination"
+                content={result.summary}
+              />
+              <OpenInBrowserAction url={result.url} />
+              <ActionPanel.Item
+                title="Show NS-record Details"
+                icon={Icon.Sidebar}
+                onAction={() => push(<Details {...result} />)}
+                shortcut={{ modifiers: ["cmd"], key: "e" }}
+              />
+            </ActionPanel>
+          }
+        />
+      ))}
+    </List>
+  );
+}
+
+function Details(props: Result) {
+  const { title, summary, url } = props;
+
+  return (
+    <Detail
+      markdown={`# ${title}\n## Destination:\n\`\`\`\n${summary}\n\`\`\``}
+      actions={
+        <ActionPanel>
+          <OpenInBrowserAction url={url} />
+          <CopyToClipboardAction title="Copy Destination" content={summary} />
+        </ActionPanel>
+      }
+    />
+  );
+}
+
+type digResponse = {
+  documents: Array<{
+    title: string;
+    mdn_url: string;
+    summary: string;
+  }>;
+};
+
+type Result = {
+  title: string;
+  url: string;
+  summary: string;
+};
+
+function getNSEntry(cmdLine: string) {
+  var n = cmdLine.split(" ");
+  return n[n.length - 1];
+
+}
+
+function hasWhiteSpace(s: string) {
+  return s.indexOf(' ') >= 0;
+}
+
+async function digByQuery(query: string): Promise<Result[]> {
+  try {
+
+  // Prepare:
+  var str = query.trim();
+
+  // Check if string have options:
+  if(hasWhiteSpace(str)) {
+    
+    var queryArr = str.split(" ");
+    let query = queryArr[0].trim();
+    let option = queryArr[1].trim();
+    var cmd = "host -t " + option + " " + query;
+
+  } else {
+    var cmd = "host " + query;
+  }
+  
+  // Define execOptions:
+  var options = {
+    encoding: 'utf8'
+  };
+    
+  // Execute command:
+  let execReturnData = (execSync(cmd, options));
+  
+  // Split lines into array:
+  let execReturnDataArr = execReturnData.split("\n");
+  
+  // Prepare Output:
+  var x = [];
+    
+  // Loop stdout lines:
+  for(const val of execReturnDataArr) {
+
+    // Grab summary:
+    var sum = getNSEntry(val);
+  
+    // If not empty push into x arr:
+    if(val && sum) {
+      x.push( {
+            title: val,
+            summary: sum,
+            url: "https://www.nslookup.io/dns-records/" + query
+        } 
+      );
+    }
+  }
+  
+    // Return x arr
+    return x;
+
+  } catch (e) {
+    showToast(ToastStyle.Failure, `Could not resolve domain`);
+    return Promise.resolve([]);
+  }
+}
