80de367d598e2d800771b791cf31f46b38243ece
Michael Aigner <tonka3100@gmail.com>
2022-03-28 10:33:33 +0200

    add date to commits

--- extensions/gitlab/src/components/commits/item.tsx
+++ extensions/gitlab/src/components/commits/item.tsx
@@ -1,5 +1,6 @@
-import { ImageLike, ImageMask, Color, List, ActionPanel } from "@raycast/api";
+import { Color, List, ActionPanel, Image } from "@raycast/api";
 import { GitLabIcons } from "../../icons";
+import { toDateString } from "../../utils";
 import { GitLabOpenInBrowserAction } from "../actions";
 import { getCIJobStatusIcon } from "../jobs";
 import { Commit } from "./list";
@@ -9,16 +10,17 @@ export function CommitListItem(props: { commit: Commit; projectID: number }): JS
   const commit = props.commit;
   const projectID = props.projectID;
   const { commitStatus: status } = useCommitStatus(projectID, commit.id);
-  const icon: ImageLike = status?.author?.avatar_url
-    ? { source: status.author.avatar_url, mask: ImageMask.Circle }
+  const icon: Image.ImageLike = status?.author?.avatar_url
+    ? { source: status.author.avatar_url, mask: Image.Mask.Circle }
     : { source: GitLabIcons.commit, tintColor: Color.Green };
-  const statusIcon: ImageLike | undefined = status?.status ? getCIJobStatusIcon(status.status) : undefined;
+  const statusIcon: Image.ImageLike | undefined = status?.status ? getCIJobStatusIcon(status.status) : undefined;
   return (
     <List.Item
       key={commit.id}
       title={commit.title}
       icon={icon}
       accessoryIcon={statusIcon}
+      accessoryTitle={toDateString(commit.created_at)}
       actions={
         <ActionPanel>
           <GitLabOpenInBrowserAction url={commit.web_url} />
