5afee7c27e706b3fbfc047a75f921b29741b3f1a
Mike Ozornin <mike.ozornin@gmail.com>
2022-04-02 03:27:31 +0300

    eslint config

--- extensions/typographer/src/index.ts
+++ extensions/typographer/src/index.ts
@@ -1,14 +1,14 @@
-import { getSelectedText, Clipboard, showHUD, getPreferenceValues  } from "@raycast/api";
+import { getSelectedText, Clipboard, showHUD, getPreferenceValues } from "@raycast/api";
 
-const Typograf = require('typograf');
+import Typograf from 'typograf';
 
 export default async function main() {
-  const {LANGUAGE} = getPreferenceValues();
-  const tp = new Typograf({locale: [LANGUAGE]});
+  const { LANGUAGE } = getPreferenceValues();
+  const tp = new Typograf({ locale: [LANGUAGE] });
 
   const selectedText = await getSelectedText();
   const transformedText = tp.execute(selectedText);
-  
+
   await Clipboard.paste(transformedText);
   await showHUD("✓");
 }
