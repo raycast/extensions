cf3d7868e7c9a965bf6f85e2f34c640a8b1d13a0
nametake <nametake.kyarabuki@gmail.com>
2022-04-03 11:06:20 +0900

    Resolved peferences paths

--- extensions/google-drive-for-desktop/src/index.tsx
+++ extensions/google-drive-for-desktop/src/index.tsx
@@ -1,5 +1,6 @@
 import { getPreferenceValues, List } from "@raycast/api";
 import fs from "fs";
+import path from "path";
 
 interface Preferences {
   rootPath: string;
@@ -8,6 +9,10 @@ interface Preferences {
 
 export default function SearchGoogleDriveForDesktopFile() {
   const preferences = getPreferenceValues<Preferences>();
+
+  const rootPath = path.resolve(preferences.rootPath);
+  const exludePaths = preferences.excludePaths?.split(",").map((p) => path.resolve(p)) ?? [];
+
   const files = fs.readdirSync(preferences.rootPath, { withFileTypes: true });
   return (
     <List>
