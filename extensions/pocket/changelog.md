e8712b92c38706c373c7ac3d98e0d45872cbabe2
vimtor <victor@vimtor.io>
2022-04-03 23:10:24 +0200

    Add "Create Bookmark from Clipboard" command

--- extensions/pocket/changelog.md
+++ extensions/pocket/changelog.md
@@ -3,7 +3,9 @@
 ## [Filters & Commands] - 2022-03-19
 - Add `All`, `Unread` and `Archived` filters for the `Search Bookmarks` command
 - Add new preference to select default read state filter
-- Add `Latest Bookmark` command to open the latest unread bookmark
+- Rename `Random Bookmark` command to `Open Random Bookmark`
+- Add `Open Latest Bookmark` command to open the latest unread bookmark
+- Add `Create Bookmark from Clipboard` command to create a bookmark from the clipboard contents
 
 ## [Categories & Screenshots] - 2022-03-12
 Add categories and screenshots for the store
--- extensions/pocket/src/utils/api.ts
+++ extensions/pocket/src/utils/api.ts
@@ -38,6 +38,36 @@ interface FetchBookmarksResponse {
   list: Record<string, RawBookmark>;
 }
 
+interface CreateBookmarkRequest {
+  url: string;
+}
+
+function formatBookmark(bookmark: RawBookmark): Bookmark {
+  return {
+    id: bookmark.item_id,
+    title: bookmark.resolved_title || bookmark.given_title,
+    originalUrl: bookmark.resolved_url || bookmark.given_url,
+    pocketUrl: `https://getpocket.com/read/${bookmark.item_id}`,
+    type: bookmark.is_article === "0" ? "video" : "article",
+    favorite: bookmark.favorite === "1",
+    tags: bookmark.tags ? Object.keys(bookmark.tags) : [],
+    author: bookmark.authors ? Object.values(bookmark.authors)[0]?.name : "",
+    updatedAt: new Date(parseInt(`${bookmark.time_added}000`))
+  };
+}
+
+export async function createBookmark({ url }: CreateBookmarkRequest) {
+  const response = await api.post("v3/add", {
+    json: {
+      consumer_key: consumerKey,
+      access_token: accessToken,
+      url: encodeURI(url)
+    }
+  });
+  const result = JSON.parse(response.body);
+  return formatBookmark(result.item);
+}
+
 export async function sendAction({ id, action }: SendActionRequest) {
   await api.post("v3/send", {
     json: {
@@ -47,10 +77,10 @@ export async function sendAction({ id, action }: SendActionRequest) {
         {
           action,
           item_id: id,
-          time: Math.floor(new Date().getTime() / 1000),
-        },
-      ],
-    },
+          time: Math.floor(new Date().getTime() / 1000)
+        }
+      ]
+    }
   });
 }
 
@@ -62,20 +92,10 @@ export async function fetchBookmarks({ state, count }: FetchBookmarksRequest): P
       detailType: "complete",
       sort: "newest",
       count,
-      state,
-    },
+      state
+    }
   });
   const result = JSON.parse(response.body) as FetchBookmarksResponse;
-  const bookmarks: Array<Bookmark> = Object.values(result.list).map((bookmark) => ({
-    id: bookmark.item_id,
-    title: bookmark.resolved_title || bookmark.given_title,
-    originalUrl: bookmark.resolved_url || bookmark.given_url,
-    pocketUrl: `https://getpocket.com/read/${bookmark.item_id}`,
-    type: bookmark.is_article === "0" ? "video" : "article",
-    favorite: bookmark.favorite === "1",
-    tags: bookmark.tags ? Object.keys(bookmark.tags) : [],
-    author: bookmark.authors ? Object.values(bookmark.authors)[0]?.name : "",
-    updatedAt: new Date(parseInt(`${bookmark.time_added}000`)),
-  }));
+  const bookmarks: Array<Bookmark> = Object.values(result.list).map(formatBookmark);
   return bookmarks.sort((a, b) => b.updatedAt.getTime() - a.updatedAt.getTime());
 }
