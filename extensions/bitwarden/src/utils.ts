9e0cbd6387e768a80a6a380b31664b0e9a04eafd
Achille Lacoin <achille.lacoin@dailymotion.com>
2022-04-04 07:06:23 +0200

    [Bitwarden] remove detailled view, fix login flow (#1242)
    * remove detailled view, fix login flow
    
    * fix login and prefs issues
    
    * Convert `Lock Vault` to an action of the `Search Vault` command
    
    * Update Changelog
    
    * add shortcut for logout, add empty view for the list
    
    * add a toast while the vault is getting locked
    
    * remove exit calls

--- extensions/bitwarden/src/utils.ts
+++ extensions/bitwarden/src/utils.ts
@@ -6,19 +6,6 @@ export function codeBlock(content: string): string {
   return "```\n" + content + "\n```";
 }
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types,@typescript-eslint/no-explicit-any
-export function filterNullishPropertiesFromObject(obj: any): any {
-  if (!obj) return obj;
-  const noNullish: Record<string, unknown> = {};
-  for (const key in obj) {
-    if (Object.hasOwnProperty.call(obj, key) && (obj[key] ?? false)) {
-      noNullish[key] = obj[key];
-    }
-  }
-
-  return noNullish;
-}
-
 export function faviconUrl(url: string): string {
   try {
     const domain = new URL(url).hostname;
