131d73bc5096bbb831940570d98f9bae935223dc
Markus Langer <dev@m-la.de>
2022-04-05 18:13:03 +0200

    [Visual Studio Code - Project Manager] - add VSCode Insiders option (#1207)
    * add VSCode Insiders option
    
    * fix description
    
    * use build for storage path
    
    Co-authored-by: Markus Langer <markus.langer@gis-ag.com>

--- extensions/visual-studio-code-project-manager/src/search-project-manager-projects.tsx
+++ extensions/visual-studio-code-project-manager/src/search-project-manager-projects.tsx
@@ -18,9 +18,7 @@ import config from "parse-git-config";
 import { dirname } from "path";
 import { ReactElement } from "react";
 import tildify from "tildify";
-import { CachedProjectEntry, Preferences, ProjectEntry } from "./types";
-
-const STORAGE = `${homedir()}/Library/Application Support/Code/User/globalStorage/alefragnani.project-manager`;
+import { CachedProjectEntry, Preferences, ProjectEntry, VSCodeBuild } from "./types";
 
 const preferences: Preferences = getPreferenceValues();
 
@@ -29,6 +27,15 @@ const gitClientInstalled = existsSync(gitClientPath);
 
 const terminalPath = preferences.terminalAppPath || "";
 const terminalInstalled = existsSync(terminalPath);
+
+const build: VSCodeBuild = preferences.build;
+const appKeyMapping = {
+  Code: "com.microsoft.VSCode",
+  "Code - Insiders": "com.microsoft.VSCodeInsiders",
+} as const;
+const appKey: string = appKeyMapping[build] ?? appKeyMapping.Code;
+
+const STORAGE = `${homedir()}/Library/Application Support/${build}/User/globalStorage/alefragnani.project-manager`;
 
 function getProjectEntries(): ProjectEntry[] {
   const storagePath = getPreferencesPath() || STORAGE;
@@ -145,7 +152,7 @@ function ProjectListItem({ name, rootPath, tags }: ProjectEntry) {
       actions={
         <ActionPanel>
           <ActionPanel.Section>
-            <OpenAction title="Open in Code" icon="command-icon.png" target={path} application="Visual Studio Code" />
+            <OpenAction title={`Open in ${build}`} icon="command-icon.png" target={path} application={appKey} />
             {terminalInstalled && (
               <ActionPanel.Item
                 title="Open in Terminal"
