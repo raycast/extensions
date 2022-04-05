b7c21229f5fca0474591ecfce039675ff8ef7ef4
kkange <doyouing@gmail.com>
2022-04-05 22:20:35 +0900

    Improve ToolBox extension (#472)
    * Add "Google Trands" extension
    
    * icon change
    
    * npm update
    
    * icon name change
    
    * Change icons using guide templates
    
    * Separate settings by command.
    
    * Realtime trends size change
    
    * Fixing folder typos.
    
    * Modify Execution Type
    
    * Changing the script error handling method.
    
    * Add format json script
    
    * Raycast api version up.
    
    * Add "vkbeautify" format library
    
    * util file cleanup
    
    * Add format script
    
    * Add libraries.
    
    * Change to one or more types.
    
    * Add and change scripts
    
    * Add and change scripts
    
    * Add License file
    
    * changed it to lowercase letters
    
    * Update the library.
    
    * Change the sql format library.
    
    * Add action option after copy
    
    * Change setting name
    
    * change script word
    
    * Api update, add categories, modify description
    
    * Migrated the raycast api
    
    * Add Change Logs, Screenshots

--- extensions/toolbox/src/script/html/base64.ts
+++ extensions/toolbox/src/script/html/base64.ts
@@ -1,14 +1,15 @@
 import { Script } from "../type";
+import { encode, decode } from "js-base64";
 
 export const base64Encode: Script = {
   info: {
     title: "Base64 Encode",
     desc: "Encode your text into Base64",
-    type: "all",
+    type: ["list", "form", "clipboard"],
     example: "https://www.raycast.com",
   },
   run(input) {
-    return Buffer.from(input, "binary").toString("base64");
+    return encode(input);
   },
 };
 
@@ -16,10 +17,10 @@ export const base64Decode: Script = {
   info: {
     title: "Base64 Decode",
     desc: "Decode your text from Base64",
-    type: "all",
+    type: ["list", "form", "clipboard"],
     example: "aHR0cHM6Ly93d3cucmF5Y2FzdC5jb20=",
   },
   run(input) {
-    return Buffer.from(input, "base64").toString("binary");
+    return decode(input);
   },
 };
