b96772c5d4cbdf61483aed408841fdf1a2d907f2
Sorin Muntean <me@sorinmuntean.ro>
2022-04-05 23:08:15 +0300

    cleanup

--- extensions/password-generator/src/generate-password.tsx
+++ extensions/password-generator/src/generate-password.tsx
@@ -1,39 +1,37 @@
-import { useState, Fragment } from "react";
+import { useState } from "react";
 import { ActionPanel, Action, Form, Detail, showToast, Clipboard, Toast } from "@raycast/api";
-
 import { generatePassword } from "./helpers/helpers";
 
+interface Form {
+  length: string;
+  useNumbers: 1 | 0;
+  useChars: 1 | 0;
+}
+
 export default function Command() {
-  type Password = string | null | undefined;
-  const [password, setPassword] = useState<Password>();
-  const [error, isError] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
 
-  let errorMessage = "";
-
-  const handleGeneratePassword = (values: any) => {
-    const length = values.lengthinput;
+  const handleGeneratePassword = (values: Form) => {
+    const length = values.length;
     const lengthNumber = parseInt(length, 10);
 
-    const useNumbers = values.usenumbers === 1 ? true : false;
-    const useChars = values.usechars === 1 ? true : false;
-
-    if (Number.isFinite(lengthNumber) && lengthNumber > 4 && lengthNumber < 65) {
-      const generatedPassword = generatePassword(lengthNumber, useNumbers, useChars);
-      setPassword(generatedPassword);
-      values = {};
-      Clipboard.copy(generatedPassword);
-      showToast(Toast.Style.Success, "Copied Password", generatedPassword);
-    } else {
-      isError(true);
-      setPassword("Error");
-      if (lengthNumber < 5) {
-        errorMessage = "Password length must be greater than 4";
-      } else if (lengthNumber > 64) {
-        errorMessage = "Password length must be less than 65";
-      } else if (!Number.isFinite(lengthNumber)) {
-        errorMessage = "Password length must be a number";
-      }
+    const useNumbers = Boolean(values.useNumbers);
+    const useChars = Boolean(values.useChars);
+    if (!Number.isFinite(lengthNumber)) {
+      setError("Password length must be a number");
+      return;
     }
+    if (lengthNumber < 5) {
+      setError("Password length must be greater than 4");
+      return;
+    }
+    if (lengthNumber > 64) {
+      setError("Password length must be less than 65");
+    }
+
+    const generatedPassword = generatePassword(lengthNumber, useNumbers, useChars);
+    Clipboard.copy(generatedPassword);
+    showToast(Toast.Style.Success, "Copied Password", generatedPassword);
   };
 
   return (
@@ -42,29 +40,19 @@ export default function Command() {
         navigationTitle="Password Generator"
         actions={
           <ActionPanel>
-            <Action.SubmitForm title="Generate Password" onSubmit={(values) => handleGeneratePassword(values)} />
+            <Action.SubmitForm title="Generate Password" onSubmit={(values: Form) => handleGeneratePassword(values)} />
           </ActionPanel>
         }
       >
         <Form.TextField
-          id="lengthinput"
+          id="length"
           title="Enter password length (number of characters):"
           placeholder="Enter a number between 5 and 64"
         />
-        <Form.Checkbox id="usenumbers" label="Use numbers?" defaultValue={true} />
-        <Form.Checkbox id="usechars" label="Use special characters?" defaultValue={true} />
+        <Form.Checkbox id="useNumbers" label="Use numbers?" defaultValue={true} />
+        <Form.Checkbox id="useChars" label="Use special characters?" defaultValue={true} />
       </Form>
-      {password && !error && (
-        <Detail
-          markdown={`### Generated Password copied to clipboard!`}
-          actions={
-            <ActionPanel>
-              <Action.CopyToClipboard content={password} />
-            </ActionPanel>
-          }
-        />
-      )}
-      {error && <Detail markdown={`### Error: please enter a valid number between 5 and 64`} />}
+      {error && <Detail markdown={`### Error: ${error}`} />}
     </>
   );
 }
