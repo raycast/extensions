f4a7311ff9c1bc2aa10ad37ad61ef153e17f59d2
Sorin Muntean <me@sorinmuntean.ro>
2022-04-07 13:00:47 +0300

    show loading indicators while data is being fetched

--- extensions/parcel-tracker/src/components/my/ManageMain.tsx
+++ extensions/parcel-tracker/src/components/my/ManageMain.tsx
@@ -1,5 +1,5 @@
 import { List, LocalStorage, showToast, Toast } from "@raycast/api";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { getVendors } from "../../api/api";
 import { IVendorData } from "../../model/vendorData";
 import PackageItem from "./PackageItem";
@@ -8,74 +8,82 @@ export interface IItems {
   [key: string]: string;
 }
 
+interface State {
+  loading: boolean;
+  packages: IItems;
+  vendors: IVendorData[];
+}
+
 export default function ManageMain() {
-  const [packages, setPackages] = useState<IItems>({});
-  const [vendors, setVendors] = useState<IVendorData[]>(new Array<IVendorData>());
+  const [state, setState] = useState<State>({ loading: true, packages: {}, vendors: [] });
 
   useEffect(() => {
-    LocalStorage.allItems().then((response) => {
-      setPackages(response);
-    });
-    getVendors().then((response) => setVendors(response.data));
+    (async () => {
+      try {
+        const packages = await LocalStorage.allItems();
+        const { data: vendors } = await getVendors();
+        setState({ loading: false, packages, vendors });
+      } catch (e) {
+        setState((previous) => ({ ...previous, loading: false }));
+      }
+    })();
   }, []);
 
   const handleRemove = (itemKey: string) => {
     LocalStorage.removeItem(itemKey).then(() => {
       showToast({ style: Toast.Style.Success, title: "Removed." });
-      const newPackages = { ...packages };
+      const newPackages = { ...state.packages };
       delete newPackages[itemKey];
-      setPackages(newPackages);
+      setState((previous) => ({ ...previous, packages: newPackages }));
     });
   };
 
   const filterByComplete = (isComplete: boolean): Array<string> => {
-    return Object.keys(packages).filter((itemKey) => getIsComplete(itemKey) == isComplete);
+    return Object.keys(state.packages).filter((itemKey) => getIsComplete(itemKey) == isComplete);
   };
 
   const getItemName = (itemKey: string): string => {
-    return packages[itemKey].split("//")[0];
+    return state.packages[itemKey].split("//")[0];
   };
 
   const getIsComplete = (itemKey: string): boolean => {
-    return packages[itemKey].split("//")[1] === "Y";
+    return state.packages[itemKey].split("//")[1] === "Y";
   };
 
   const findVendorByCode = (itemKey: string): IVendorData | null => {
     const code = itemKey.split("-")[0];
-    return vendors.find((v) => v.code === code) || null;
+    return state.vendors.find((v) => v.code === code) || null;
   };
 
   return (
-    <List>
+    <List isLoading={state.loading}>
       <List.Section title="Delivery NOT completed" key="notCompleted">
-        {packages &&
-          filterByComplete(false).map((itemKey) => {
-            return (
-              <PackageItem
-                key={itemKey}
-                itemKey={itemKey}
-                itemName={getItemName(itemKey)}
-                isComplete={getIsComplete(itemKey)}
-                handleRemove={handleRemove}
-                vendor={findVendorByCode(itemKey)}
-              />
-            );
-          })}
+        {filterByComplete(false).map((itemKey) => {
+          return (
+            <PackageItem
+              key={itemKey}
+              itemKey={itemKey}
+              itemName={getItemName(itemKey)}
+              isComplete={getIsComplete(itemKey)}
+              handleRemove={handleRemove}
+              vendor={findVendorByCode(itemKey)}
+            />
+          );
+        })}
       </List.Section>
       <List.Section title="Delivery completed" key="completed">
-        {packages &&
-          filterByComplete(true).map((itemKey) => {
-            return (
-              <PackageItem
-                key={itemKey}
-                itemKey={itemKey}
-                itemName={getItemName(itemKey)}
-                isComplete={getIsComplete(itemKey)}
-                handleRemove={handleRemove}
-                vendor={findVendorByCode(itemKey)}
-              />
-            );
-          })}
+        {filterByComplete(true).map((itemKey) => {
+          return (
+            <PackageItem
+              key={itemKey}
+              itemKey={itemKey}
+              itemName={getItemName(itemKey)}
+              isComplete={getIsComplete(itemKey)}
+              handleRemove={handleRemove}
+              vendor={findVendorByCode(itemKey)}
+            />
+          );
+        })}
       </List.Section>
     </List>
   );
--- extensions/parcel-tracker/src/components/track/TrackMain.tsx
+++ extensions/parcel-tracker/src/components/track/TrackMain.tsx
@@ -1,31 +1,42 @@
 import { Action, ActionPanel, Icon, List } from "@raycast/api";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Track from "./Track";
 import { getVendors } from "../../api/api";
 import { IVendorData } from "../../model/vendorData";
 
+interface State {
+  loading: boolean;
+  vendors: IVendorData[];
+}
+
 export default function TrackMain() {
-  const [vendors, setVendors] = useState<IVendorData[]>();
+  const [state, setState] = useState<State>({ loading: true, vendors: [] });
 
   useEffect(() => {
-    getVendors().then((response) => setVendors(response.data));
+    (async () => {
+      try {
+        const { data: vendors } = await getVendors();
+        setState({ loading: false, vendors });
+      } catch (e) {
+        setState((previous) => ({ ...previous, loading: false }));
+      }
+    })();
   }, []);
 
   return (
-    <List searchBarPlaceholder="Choose delivery vendor">
-      {vendors &&
-        vendors.map((vendor) => (
-          <List.Item
-            key={vendor.code}
-            icon={Icon.Circle}
-            title={vendor.name}
-            actions={
-              <ActionPanel>
-                <Action.Push title="Next" target={<Track vendorKey={vendor.code} vendorName={vendor.name} />} />
-              </ActionPanel>
-            }
-          />
-        ))}
+    <List isLoading={state.loading} searchBarPlaceholder="Choose delivery vendor">
+      {state.vendors.map((vendor) => (
+        <List.Item
+          key={vendor.code}
+          icon={Icon.Circle}
+          title={vendor.name}
+          actions={
+            <ActionPanel>
+              <Action.Push title="Next" target={<Track vendorKey={vendor.code} vendorName={vendor.name} />} />
+            </ActionPanel>
+          }
+        />
+      ))}
     </List>
   );
 }
