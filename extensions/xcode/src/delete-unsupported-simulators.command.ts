874175404071dec90a286bf4c353acb32bfe62ab
pradeep <pradeepboorgu@gmail.com>
2022-04-08 14:03:09 +0530

    added new command

---
+++ extensions/xcode/src/delete-unsupported-simulators.command.ts
@@ -0,0 +1,27 @@
+import { showToast, ToastStyle } from "@raycast/api";
+import { XcodeCleanupService } from "./services/xcode-cleanup.service";
+
+/**
+ * Xcode clear Swift Package Manager (SPM) cache command
+ */
+export default async () => {
+  // Initialize XcodeCleanupService
+  const xcodeCleanupService = new XcodeCleanupService();
+  // Show loading Toast
+  const loadingToast = await showToast(ToastStyle.Animated, "Please wait");
+  try {
+    // Clear Swift Package Manager cache
+    await xcodeCleanupService.deleteUnavailableSimulators();
+    // Hide loading Toast
+    await loadingToast.hide();
+    // Show success Toast
+    await showToast(ToastStyle.Success, "Unsupported simulators successfully deleted");
+  } catch (error) {
+    // Log error
+    console.error(error);
+    // Hide loading Toast
+    await loadingToast.hide();
+    // Show failure Toast
+    await showToast(ToastStyle.Failure, "An error occurred while trying to delete unsupported simulators");
+  }
+};
