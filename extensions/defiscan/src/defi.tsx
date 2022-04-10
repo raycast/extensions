9cb0d0272b11f81fba7d7da66014f0f5b5f3a006
Adrian <322735+adrian-schnell@users.noreply.github.com>
2022-04-10 17:13:49 +0200

    added some statistics as detail view

--- extensions/defiscan/src/defi.tsx
+++ extensions/defiscan/src/defi.tsx
@@ -1,24 +1,29 @@
 import { ActionPanel, Action, List, Icon, Color } from "@raycast/api";
 import { useState } from "react";
+import { useStats } from "./useStats";
 
 const baseUrl = "https://defiscan.live/";
 
 export default function Command() {
   const [valueText, setValueText] = useState("");
+  const { status, statsSummaryMarkdown } = useStats();
 
   const items = (
     <List.Section title="Search">
       {valueText.length >= 1 &&
         valueText.length < 10 &&
-        searchOption("Block ID / Hash", Color.Magenta, "blocks", valueText)}
-      {valueText.length == 64 && searchOption("Vault", Color.Magenta, "vaults", valueText)}
-      {valueText.length >= 30 && searchOption("Address", Color.Magenta, "address", valueText)}
-      {valueText.length == 64 && searchOption("Transaction", Color.Magenta, "transactions", valueText)}
+        searchOption("Block ID / Hash", Color.Magenta, "blocks", valueText, statsSummaryMarkdown)}
+      {valueText.length == 64 && searchOption("Vault", Color.Magenta, "vaults", valueText, statsSummaryMarkdown)}
+      {valueText.length >= 30 && searchOption("Address", Color.Magenta, "address", valueText, statsSummaryMarkdown)}
+      {valueText.length == 64 &&
+        searchOption("Transaction", Color.Magenta, "transactions", valueText, statsSummaryMarkdown)}
     </List.Section>
   );
 
   return (
     <List
+      isLoading={status === "loading"}
+      isShowingDetail
       enableFiltering={false}
       navigationTitle="Search on DeFiScan.live"
       searchBarPlaceholder="Search by block id/hash, address, trx id or vault id"
@@ -30,24 +35,27 @@ export default function Command() {
         <List.Item
           title="DEX"
           icon={{ source: Icon.Link, tintColor: Color.Magenta }}
+          detail={<List.Item.Detail markdown={statsSummaryMarkdown} />}
           actions={
             <ActionPanel title="list DEX">
-              <Action.OpenInBrowser url={baseUrl + "dex"} shortcut={{ modifiers: ["cmd"], key: "d" }} />
+              <Action.OpenInBrowser url={baseUrl + "dex"} />
             </ActionPanel>
           }
         />
         <List.Item
           title="Vaults"
           icon={{ source: Icon.Link, tintColor: Color.Magenta }}
+          detail={<List.Item.Detail markdown={statsSummaryMarkdown} />}
           actions={
             <ActionPanel title="list Vaults">
-              <Action.OpenInBrowser url={baseUrl + "vaults"} shortcut={{ modifiers: ["cmd"], key: "v" }} />
+              <Action.OpenInBrowser url={baseUrl + "vaults"} />
             </ActionPanel>
           }
         />
         <List.Item
           title="Blocks"
           icon={{ source: Icon.Link, tintColor: Color.Magenta }}
+          detail={<List.Item.Detail markdown={statsSummaryMarkdown} />}
           actions={
             <ActionPanel title="list Blocks">
               <Action.OpenInBrowser url={baseUrl + "blocks"} />
@@ -57,6 +65,7 @@ export default function Command() {
         <List.Item
           title="Masternodes"
           icon={{ source: Icon.Link, tintColor: Color.Magenta }}
+          detail={<List.Item.Detail markdown={statsSummaryMarkdown} />}
           actions={
             <ActionPanel title="list Masternodes">
               <Action.OpenInBrowser url={baseUrl + "masternodes"} />
@@ -66,6 +75,7 @@ export default function Command() {
         <List.Item
           title="Oracles"
           icon={{ source: Icon.Link, tintColor: Color.Magenta }}
+          detail={<List.Item.Detail markdown={statsSummaryMarkdown} />}
           actions={
             <ActionPanel title="list Oracles">
               <Action.OpenInBrowser url={baseUrl + "oracles"} />
@@ -75,6 +85,7 @@ export default function Command() {
         <List.Item
           title="Auctions"
           icon={{ source: Icon.Link, tintColor: Color.Magenta }}
+          detail={<List.Item.Detail markdown={statsSummaryMarkdown} />}
           actions={
             <ActionPanel title="list Auctions">
               <Action.OpenInBrowser url={baseUrl + "auctions"} />
@@ -86,7 +97,7 @@ export default function Command() {
   );
 }
 
-function searchOption(title: string, color: Color, link: string, value: string) {
+function searchOption(title: string, color: Color, link: string, value: string, statsSummaryMarkdown: string) {
   return (
     <List.Item
       title={title}
@@ -96,6 +107,7 @@ function searchOption(title: string, color: Color, link: string, value: string) 
           <Action.OpenInBrowser url={baseUrl + link + "/" + value} />
         </ActionPanel>
       }
+      detail={<List.Item.Detail markdown={statsSummaryMarkdown} />}
     />
   );
 }
---
+++ extensions/defiscan/src/models/masternode.tsx
@@ -0,0 +1,5 @@
+export type Masternode = {
+  total: number;
+  frozen5yr: number;
+  frozen10yr: number;
+};
