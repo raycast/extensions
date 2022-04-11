7df0a0058e1c53bc6db3dcbda0eb0b0f6cb09a29
Alberto Xamin <alberto.xamin@datadoghq.com>
2022-04-11 18:14:23 +0200

    add regex to handle most markup cases

--- extensions/finnish-dictionary/src/common.ts
+++ extensions/finnish-dictionary/src/common.ts
@@ -1,11 +1,36 @@
+const regexWord = /\[\[([A-Za-zäöëå ]+)#(Finnish|English)\|\1\]\]/gmi;
+const regexWords = /\[\[w:(.*?)\|.*?\]\]/gm;
+const regexCategory = /\(\[\[.*#([a-zäöëå]+)\|\1.*?\]\]\)/gm;
+const regexLinks =
+  /([a-zäöëå]+)\[\[Category:(Finnish|English) redlinks\]\]\[\[Category:(Finnish|English) redlinks\/m\]\]/gm;
+const regexGeneralCategory = /\[\[([a-zäöëå]+).*?\]\]\[\[Category:[a-z]+:.*?\]\]/gm;
+const regexWikipedia = /(\[\[)wikipedia:(.*?)\|(.*?)(\]\])/gm;
+const regexAppendix = /(\[\[)Appendix(.*?)(\]\])/gm;
+const regexNotImportantCategories = /\[\[Category:.*?\]\]/gm;
+const regexStrike = /<strike>(.*?)<\/strike>/gm;
+const regexBrackets = /(\[\[)(.*?)(\]\])/gm;
+
 export function resToDetail(word: string, srcLang: string, destLang: string, res: any) {
+  const cleanMarkup = (markup: string) => {
+    return markup
+      .replace(regexWord, "$1")
+      .replace(regexWords, "$1")
+      .replace(regexCategory, "_$1_")
+      .replace(regexGeneralCategory, "$1")
+      .replace(regexLinks, `[$1](https://redfoxsanakirja.fi/fi/sanakirja/-/s/${srcLang}/${destLang}/$1)`)
+      .replace(regexWikipedia, "$2")
+      .replace(regexAppendix, "")
+      .replace(regexNotImportantCategories, "")
+      .replace(regexStrike, "~~$1~~")
+      .replace(regexBrackets, "**$2**");
+  };
   let detail = "## " + word + "\n\n";
   try {
     if (!res.translations.empty) {
       for (const x of res.translations.entryGroups) {
         detail += `##### ${x.gategory}\n---\n`;
         for (const y of x.entries) {
-          detail += `1. ${y.text.replace("[[", "(").replace("]]", ")")} ${y.context ? "_" + y.context + "_" : ""}\n\n`;
+          detail += `1. ${cleanMarkup(y.text)} ${y.context ? "_" + y.context + "_" : ""}\n\n`;
         }
       }
     }
@@ -13,23 +38,16 @@ export function resToDetail(word: string, srcLang: string, destLang: string, res
       for (const x of res.definitions.entryGroups) {
         detail += `##### ${x.gategory}\n---\n`;
         for (const y of x.entries) {
-          detail += `${y.text}\n\n`;
+          detail += `${cleanMarkup(y.text)}\n\n`;
         }
       }
     }
     if (res.definitionsInDestLanguage && !res.definitionsInDestLanguage.empty) {
       detail += `##### DEFINITIONS IN DEST LANGUAGE\n---\n`;
-      const regexWord = /\[\[([a-zäöëå]+)#(Finnish|English)\|\1\]\]/gm;
-      const regexCategory = /\(\[\[.*#([a-zäöëå]+)\|\1.*?\]\]\)/gm;
-      const regexLinks =
-        /([a-zäöëå]+)\[\[Category:(Finnish|English) redlinks\]\]\[\[Category:(Finnish|English) redlinks\/m\]\]/gm;
       for (const x of res.definitionsInDestLanguage.entryGroups) {
         detail += `* ${x.gategory}\n`;
         for (const y of x.entries) {
-          detail += `\t1. ${y.text
-            .replace(regexWord, "$1")
-            .replace(regexCategory, "_$1_")
-            .replace(regexLinks, `[$1](https://redfoxsanakirja.fi/fi/sanakirja/-/s/${srcLang}/${destLang}/$1)`)}\n`;
+          detail += `\t1. ${cleanMarkup(y.text)}\n`;
         }
         detail += `\n`;
       }
