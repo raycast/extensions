ab2b41086d82ca001cd1ddd7603709903541e45a
pomdtr <achille.lacoin@dailymotion.com>
2022-04-12 00:54:15 +0200

    remove confusing nbValues Option

--- extensions/send-to-ifttt-webhook/src/index.tsx
+++ extensions/send-to-ifttt-webhook/src/index.tsx
@@ -3,12 +3,12 @@ import fetch from "node-fetch";
 
 interface CommandForm {
   value1: string;
-  value2?: string;
-  value3?: string;
+  value2: string;
+  value3: string;
 }
 
 export default function Command() {
-  const { eventName, webhooksKey, nbValues } = getPreferenceValues<{
+  const { eventName, webhooksKey } = getPreferenceValues<{
     eventName: string;
     webhooksKey: string;
     nbValues: string;
@@ -38,13 +38,9 @@ export default function Command() {
       }
     >
       <Form.Description title="Event Name" text={eventName} />
-      {range(parseInt(nbValues), 1).map((i) => (
-        <Form.TextArea key={i} id={`value${i}`} title={`Value ${i}`} placeholder="Enter text" />
-      ))}
+      <Form.TextArea key="1" id={"value1"} title={"Value 1"} placeholder="Enter text" />
+      <Form.TextArea key="2" id={"value2"} title={"Value 2"} placeholder="Enter text" />
+      <Form.TextArea key="3" id={"value3"} title={"Value 3"} placeholder="Enter text" />
     </Form>
   );
-}
-
-function range(size: number, startAt = 0) {
-  return [...Array(size).keys()].map((i) => i + startAt);
 }
