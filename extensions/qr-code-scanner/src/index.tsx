1872426c6572889b6e0b3a236763f5535e3cf855
StevenRCE0 <40051361+StevenRCE0@users.noreply.github.com>
2022-04-12 10:02:24 +0800

    Update extension QR Code Scanner.

--- extensions/qr-code-scanner/src/index.tsx
+++ extensions/qr-code-scanner/src/index.tsx
@@ -2,16 +2,21 @@ import {
   closeMainWindow,
   confirmAlert,
   copyTextToClipboard,
+  getPreferenceValues,
   popToRoot,
   showHUD,
   showToast,
-  ToastStyle,
+  ToastStyle
 } from "@raycast/api";
 import { exec } from "child_process";
 import open from "open";
 import { read as decodeImage } from "jimp";
 import { randomInt } from "crypto";
 import jsQR from "jsqr";
+
+interface Preferences {
+  captureMode: "area" | "fullscreen";
+}
 
 function qrDecode(filepath: string, callback: (data: string | boolean) => void) {
   decodeImage(filepath, (err, image) => {
@@ -24,8 +29,9 @@ function qrDecode(filepath: string, callback: (data: string | boolean) => void) 
       return;
     }
     const result = jsQR(new Uint8ClampedArray(image.bitmap.data.buffer), image.bitmap.width, image.bitmap.height, {
-      inversionAttempts: "attemptBoth",
+      inversionAttempts: "attemptBoth"
     });
+    exec(`rm ${filepath}`);
     if (result) {
       const decoder = new TextDecoder("shift-jis");
       const code = decoder.decode(Uint8Array.from(result?.binaryData).buffer);
@@ -36,12 +42,27 @@ function qrDecode(filepath: string, callback: (data: string | boolean) => void) 
   });
 }
 
-function trigger(randName: string) {
+function trigger(randName: string, mode: Preferences["captureMode"], displayNumber = 1) {
   closeMainWindow();
 
-  exec("/usr/sbin/screencapture -i /tmp/shotTemp" + randName + ".jpg", function () {
+  function captureScreenCommand(): string {
+    switch (mode) {
+      case "fullscreen":
+        return `/usr/sbin/screencapture -D ${displayNumber.toString()} /tmp/shotTemp${randName}.jpg`;
+      default:
+        return `/usr/sbin/screencapture -i /tmp/shotTemp${randName}.jpg`;
+    }
+  }
+
+  exec(captureScreenCommand(), function(exception) {
+    if (exception && exception.message.indexOf("Invalid display specified") > -1) {
+      showHUD("No QR Code Found on Any Screen :(");
+      popToRoot();
+    }
     qrDecode("/tmp/shotTemp" + randName + ".jpg", (data: string | boolean) => {
-      if (data === false) {
+      if (!data && mode === "fullscreen") {
+        trigger(randName, mode, displayNumber + 1);
+      } else if (!data) {
         showHUD("No QR Code Found :(");
         popToRoot();
       } else if (typeof data === "string") {
@@ -57,12 +78,12 @@ function trigger(randName: string) {
                   closeMainWindow();
                   popToRoot();
                 });
-              },
+              }
             },
             dismissAction: {
               title: "Cancel",
-              onAction: () => popToRoot(),
-            },
+              onAction: () => popToRoot()
+            }
           });
         } else {
           showHUD("Copied: " + (data.length > 20 ? data.substring(0, 30) + "..." : data));
@@ -75,5 +96,6 @@ function trigger(randName: string) {
 
 export default async function main() {
   const randName = randomInt(100, 999).toString();
-  trigger(randName);
+  const preferences = getPreferenceValues<Preferences>();
+  trigger(randName, preferences.captureMode);
 }
