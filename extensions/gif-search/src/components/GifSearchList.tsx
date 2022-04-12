7b0a62f841370db690c6640c4ff1fe9ccca68695
Joe Schmitt <joe@compass.com>
2022-04-11 22:12:14 -0400

    Add logos to search service dropdown items

--- extensions/gif-search/src/components/GifSearchList.tsx
+++ extensions/gif-search/src/components/GifSearchList.tsx
@@ -1,4 +1,4 @@
-import { List, Image } from "@raycast/api";
+import { List, Image, Icon, Color } from "@raycast/api";
 import { GIF_SERVICE } from "../preferences";
 import { GifListSection, GifListSectionProps } from "./GifListSection";
 
@@ -23,12 +23,28 @@ export function GifSearchList(props: GifListProps) {
         props.showDropdown && (
           <List.Dropdown tooltip="" storeValue={true} onChange={props.onDropdownChange}>
             <List.Dropdown.Section>
-              <List.Dropdown.Item title="Giphy" value={GIF_SERVICE.GIPHY} />
-              <List.Dropdown.Item title="Tenor" value={GIF_SERVICE.TENOR} />
-              <List.Dropdown.Item title="Finer Gifs Club" value={GIF_SERVICE.FINER_GIFS} />
+              <List.Dropdown.Item
+                title="Giphy"
+                value={GIF_SERVICE.GIPHY}
+                icon={{ source: "giphy-logo-square-180.png" }}
+              />
+              <List.Dropdown.Item
+                title="Tenor"
+                value={GIF_SERVICE.TENOR}
+                icon={{ source: "tenor-logo-square-180.png" }}
+              />
+              <List.Dropdown.Item
+                title="Finer Gifs Club"
+                value={GIF_SERVICE.FINER_GIFS}
+                icon={{ source: "finergifs-logo.svg", tintColor: Color.PrimaryText }}
+              />
             </List.Dropdown.Section>
             <List.Dropdown.Section>
-              <List.Dropdown.Item title="Favorites" value={GIF_SERVICE.FAVORITES} />
+              <List.Dropdown.Item
+                title="Favorites"
+                value={GIF_SERVICE.FAVORITES}
+                icon={{ source: Icon.Star, tintColor: Color.Yellow }}
+              />
             </List.Dropdown.Section>
           </List.Dropdown>
         )
