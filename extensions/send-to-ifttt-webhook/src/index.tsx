25254beb9ed7c6eeb9e70e20ce95ac7f76dcd021
pomdtr <achille.lacoin@dailymotion.com>
2022-04-13 17:26:51 +0200

    allow to specify up to five ifttt webhooks

--- extensions/send-to-ifttt-webhook/src/index.tsx
+++ extensions/send-to-ifttt-webhook/src/index.tsx
@@ -1,26 +1,32 @@
-import { Form, ActionPanel, Action, showToast, getPreferenceValues, showHUD, popToRoot } from "@raycast/api";
+import { Form, ActionPanel, Action, showToast, getPreferenceValues, showHUD, popToRoot, List } from "@raycast/api";
 import fetch from "node-fetch";
 
 interface CommandForm {
+  eventName: string;
   value1: string;
   value2: string;
   value3: string;
 }
 
 export default function Command() {
-  const { eventName, webhooksKey } = getPreferenceValues<{
-    eventName: string;
+  const { eventName1, eventName2, eventName3, eventName4, eventName5, webhooksKey } = getPreferenceValues<{
+    eventName1: string;
+    eventName2: string;
+    eventName3: string;
+    eventName4: string;
+    eventName5: string;
     webhooksKey: string;
     nbValues: string;
   }>();
   async function handleSubmit(values: CommandForm) {
+    const { eventName, value1, value2, value3 } = values;
     try {
       await fetch(`https://maker.ifttt.com/trigger/${eventName}/with/key/${webhooksKey}`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(values),
+        body: JSON.stringify({ value1, value2, value3 }),
       });
       await showHUD("Sent to IFTTT 🚀");
       await popToRoot();
@@ -37,7 +43,11 @@ export default function Command() {
         </ActionPanel>
       }
     >
-      <Form.Description title="Event Name" text={eventName} />
+      <Form.Dropdown title="Event Name" id="eventName" storeValue>
+        {[eventName1, eventName2, eventName3, eventName4, eventName5].map((eventName, index) =>
+          eventName ? <List.Dropdown.Item key={index} title={eventName} value={eventName} /> : null
+        )}
+      </Form.Dropdown>
       <Form.TextArea key="1" id={"value1"} title={"Value 1"} placeholder="Enter text" />
       <Form.TextArea key="2" id={"value2"} title={"Value 2"} placeholder="Enter text" />
       <Form.TextArea key="3" id={"value3"} title={"Value 3"} placeholder="Enter text" />
