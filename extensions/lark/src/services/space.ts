93522837a9d58df147d015b6faf4b620819f8ba6
htoo <ht.anglenx@gmail.com>
2022-04-13 17:52:58 +0800

    fix(Lark): fix wikidoc icons bug
    Fix the problem that wiki document icons are sometimes displayed incorrectly.

--- extensions/lark/src/services/space.ts
+++ extensions/lark/src/services/space.ts
@@ -223,6 +223,7 @@ export async function searchDocs(params: SearchDocsParams): Promise<SearchDocsRe
         title: trimTagsAndDecodeEntities(objEntity.title),
         preview: trimTagsAndDecodeEntities(objEntity.preview),
         url: computeRedirectedUrl(objEntity),
+        type: computeType(objEntity),
       };
     });
     return data;
@@ -262,6 +263,14 @@ const computeRedirectedUrl = (objEntity: ObjEntity) => {
     .replace(/\/space\/bitable\//, '/base/')
     .replace(/\/space\/mindnote\//, '/mindnotes/')
     .replace(/\/space\//, '/');
+};
+
+const computeType = (objEntity: ObjEntity) => {
+  if (objEntity.wiki_infos) {
+    return NodeType.Wik;
+  }
+
+  return objEntity.type;
 };
 
 const CACHE_DOCS_RECENT_LIST = 'CACHE_DOCS_RECENT_LIST';
