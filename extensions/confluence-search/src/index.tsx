d18489b1df7325f5d204858aa4731a75c5b36c63
Mike Ozornin <mike.ozornin@gmail.com>
2022-04-14 10:58:19 +0300

    Fix: switch from basic (it's deprecated) and remove unnecessary /wiki slug

--- extensions/confluence-search/src/index.tsx
+++ extensions/confluence-search/src/index.tsx
@@ -1,23 +1,23 @@
 import {
   ActionPanel,
   CopyToClipboardAction,
-  List,
-  OpenInBrowserAction,
-  ImageMask,
   getPreferenceValues,
   Icon,
+  ImageMask,
+  List,
+  OpenInBrowserAction,
   showToast,
   ToastStyle,
 } from "@raycast/api";
-import { useState, useEffect, useRef, useCallback } from "react";
 import fetch, { AbortError, RequestInit, Response } from "node-fetch";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 const prefs: { instance: string; user: string; token: string } = getPreferenceValues();
 export const confluenceUrl = `https://${prefs.instance}`;
 
 const headers = {
   Accept: "application/json",
-  Authorization: "Basic " + Buffer.from(`${prefs.user}:${prefs.token}`).toString("base64"),
+  Authorization: `Bearer ${prefs.token}`,
 };
 
 export default function Command() {
@@ -74,7 +74,7 @@ async function searchConfluence(searchText: string, signal: AbortSignal) {
     method: "get",
     signal: signal,
   };
-  const apiUrl = `${confluenceUrl}/wiki/rest/api/search?cql=title~"${searchText}*"&expand=content.version`;
+  const apiUrl = `${confluenceUrl}/rest/api/search?cql=title~"${searchText}*"&expand=content.version`;
   return fetch(apiUrl, init).then((response) => {
     return parseResponse(response);
   });
@@ -109,7 +109,7 @@ function SearchListItem({ searchResult }: { searchResult: SearchResult }) {
       actions={
         <ActionPanel>
           <ActionPanel.Section>
-            <OpenInBrowserAction title="Open in Browser" url={confluenceUrl + "/wiki" + searchResult.url} />
+            <OpenInBrowserAction title="Open in Browser" url={confluenceUrl + searchResult.url} />
             <CopyToClipboardAction
               title="Copy URL"
               content={confluenceUrl + "/wiki" + searchResult.url}
