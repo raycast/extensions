3e77a9a24d914a539730b05d6025c3888a13409d
Dominik Grzedzielski <gie.dominik@gmail.com>
2022-04-16 13:36:09 +0200

    fix(bundlephobia-search): lint

--- extensions/bundlephobia-search/src/api.ts
+++ extensions/bundlephobia-search/src/api.ts
@@ -6,7 +6,9 @@ type SearchPackageResponse = Array<{ package: Package }>;
 type SizeDataResponse = SizeData;
 
 export const searchPackage = async (q: string) => {
-  const { data } = await axios.get<SearchPackageResponse>(`https://api.npms.io/v2/search/suggestions?q=${q}`);
+  const { data } = await axios.get<SearchPackageResponse>(
+    `https://api.npms.io/v2/search/suggestions?q=${q}`
+  );
 
   return data.map((p) => p.package);
 };
