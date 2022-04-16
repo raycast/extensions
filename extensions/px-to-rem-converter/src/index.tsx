71dbca7867c76ac2048826323c41ec01a15defdc
raulanatol <raul@natol.es>
2022-04-16 13:05:40 +0100

    Add extension: PX to REM converter

---
+++ extensions/px-to-rem-converter/src/index.tsx
@@ -0,0 +1,45 @@
+import { Action, ActionPanel, List } from "@raycast/api";
+import React, { useState } from "react";
+
+const BASE_FONT_PIXELS = 16;
+
+const toREM = (px: number): string =>
+  (px / BASE_FONT_PIXELS) + "rem";
+
+const toPX = (rem: number) =>
+  (rem * BASE_FONT_PIXELS) + "px";
+
+export default function Command() {
+  const [rem, setREM] = useState<string>(toREM(BASE_FONT_PIXELS));
+  const [px, setPX] = useState<string>(BASE_FONT_PIXELS + "px");
+
+  const handleOnTextChange = (value = "") => {
+    const input = Number(value.trim().split(" ")[0]);
+    if (!isNaN(input)) {
+      setREM(toREM(input));
+      setPX(toPX(input));
+    }
+  };
+
+  return (
+    <List onSearchTextChange={handleOnTextChange}
+          enableFiltering={false}
+          navigationTitle="PX to REM"
+          searchBarPlaceholder="Search your PX">
+      <List.Section title="to REM">
+        <List.Item title={rem} actions={
+          <ActionPanel title="Copy">
+            <Action.CopyToClipboard title="Copy to clipboard" content={rem} />
+          </ActionPanel>
+        } />
+      </List.Section>
+      <List.Section title="to PX">
+        <List.Item title={px} actions={
+          <ActionPanel title="Copy">
+            <Action.CopyToClipboard title="Copy to clipboard" content={px} />
+          </ActionPanel>
+        } />
+      </List.Section>
+    </List>
+  );
+}
