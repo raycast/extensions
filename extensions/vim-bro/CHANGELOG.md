eea471bebbda810a2e86c1bfc837994d18314b95
Ajay Prem Shankar <ajaypremshankar@gmail.com>
2022-04-19 19:11:15 +0530

    Revert vim-bro

--- extensions/vim-bro/CHANGELOG.md
+++ extensions/vim-bro/CHANGELOG.md
@@ -1,12 +1,5 @@
 # Vim-bro Changelog
 
-<<<<<<< HEAD
-## [Added support to search vim commands] - 2022-03-30
-- Better keyword search
-
-## [Added vim-bro] - 2022-03-20
-Initial version code
-=======
 ## [Added better search with multiple keywords] - 2022-03-30
 
 - Vim-bro can now shows search based on priority defined by number of keyword match.
@@ -18,4 +11,3 @@ Initial version code
 ## [Added vim-bro] - 2022-03-20
 
 Initial version code
->>>>>>> 6c89e1bc02a5e20cf24fdc9edd3f2a642a2b0031
--- extensions/vim-bro/src/index.tsx
+++ extensions/vim-bro/src/index.tsx
@@ -1,21 +1,8 @@
 import { List } from "@raycast/api";
 import { useEffect, useState } from "react";
 import commandsRaw from "./commands.json";
-<<<<<<< HEAD
-
-type Command = {
-  kbd: string;
-  text: string;
-};
-
-type CommandGroup = {
-  key: string;
-  commands: Command[];
-};
-=======
 import { Command, CommandGroup } from "./types";
 import { searchKeywordInCommandGroups } from "./utils";
->>>>>>> 6c89e1bc02a5e20cf24fdc9edd3f2a642a2b0031
 
 export default function CommandSearch() {
   const commandGroups = commandsRaw as CommandGroup[];
@@ -24,46 +11,14 @@ export default function CommandSearch() {
   const [filteredList, setFilteredList] = useState(commandGroups);
 
   useEffect(() => {
-<<<<<<< HEAD
-    const filteredCommandGroups: CommandGroup[] = [];
-
-    const notMatchedCommandGroups: CommandGroup[] = [];
-
-    commandGroups.forEach((group: CommandGroup) => {
-      if (group.key.toLowerCase().includes(searchText.toLowerCase())) {
-        filteredCommandGroups.push(group);
-      } else {
-        notMatchedCommandGroups.push(group);
-      }
-    });
-
-    notMatchedCommandGroups.forEach((group: CommandGroup) => {
-      const list: Command[] = [];
-      group.commands.forEach((item: Command) => {
-        if (item.text.toLowerCase().includes(searchText.toLowerCase())) {
-          list.push(item);
-        } else if (item.kbd.toLowerCase().includes(searchText.toLowerCase())) {
-          list.push(item);
-        }
-      });
-
-      filteredCommandGroups.push({ key: group.key, commands: list });
-    });
-    setFilteredList(filteredCommandGroups);
-=======
     setFilteredList(searchKeywordInCommandGroups(searchText, commandGroups));
->>>>>>> 6c89e1bc02a5e20cf24fdc9edd3f2a642a2b0031
   }, [searchText]);
 
   return (
     <List
       enableFiltering={false}
       onSearchTextChange={setSearchText}
-<<<<<<< HEAD
-      navigationTitle="Search vim commands"
-=======
       navigationTitle="Search Vim Commands"
->>>>>>> 6c89e1bc02a5e20cf24fdc9edd3f2a642a2b0031
       searchBarPlaceholder="Learn new command by searching it here."
     >
       {filteredList.map((commandGroup: CommandGroup) => {
@@ -75,13 +30,8 @@ export default function CommandSearch() {
               return (
                 <List.Item
                   key={command.kbd}
-<<<<<<< HEAD
-                  title={command.text[0].toUpperCase() + command.text.slice(1)}
-                  subtitle={command.kbd}
-=======
                   title={command.kbd}
                   subtitle={command.text[0].toUpperCase() + command.text.slice(1)}
->>>>>>> 6c89e1bc02a5e20cf24fdc9edd3f2a642a2b0031
                 />
               );
             })}
