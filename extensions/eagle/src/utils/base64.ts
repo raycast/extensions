7c1e0de8757284e4db3a39ee19f423644a98f518
Yukai Huang <yukaihuangtw@gmail.com>
2022-04-22 21:15:58 +0800

    fix: remember to close file

--- extensions/eagle/src/utils/base64.ts
+++ extensions/eagle/src/utils/base64.ts
@@ -4,5 +4,6 @@ export async function fromPathToBase64Url(path: string, ext: string) {
   const file = await open(path, "r");
   const buffer = await readFile(file);
   const base64Url = `data:image/${ext};base64,${buffer.toString("base64")}`;
+  await file.close();
   return base64Url;
 }
