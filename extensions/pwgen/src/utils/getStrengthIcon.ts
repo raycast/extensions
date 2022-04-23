af466b1ed4033dc28aa9be815715c654a70bc918
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2022-04-23 15:25:04 +0200

    refactor: use emoji rather than color

--- extensions/pwgen/src/utils/getStrengthIcon.ts
+++ extensions/pwgen/src/utils/getStrengthIcon.ts
@@ -1,24 +1,49 @@
-import { Color, Icon, Image } from '@raycast/api'
+import { Color, Image } from '@raycast/api'
 
-import { STRENGTH_HORRIBLE, STRENGTH_STRONG, STRENGTH_WEAK } from './constants'
+export const getStrengthIcon = (strength: number): { source: Image.Source; tintColor?: Color.ColorLike } => {
+	const entropy = [
+		{
+			bits: 28,
+			color: '#e44759',
+			icon: '💀',
+			name: 'very_weak',
+		},
+		{
+			bits: 35,
+			color: '#f8b32d',
+			icon: '😢',
+			name: 'weak',
+		},
+		{
+			bits: 60,
+			color: '#67a4d3',
+			icon: '😐',
+			name: 'reasonable',
+		},
+		{
+			bits: 128,
+			color: '#33ccb1',
+			icon: '😎',
+			name: 'strong',
+		},
+		{
+			bits: Infinity,
+			color: '#33cc33',
+			icon: '🤩',
+			name: 'very_strong',
+		},
+	]
 
-export const getStrengthIcon = (strength: number): { source: Image.Source; tintColor: Color.ColorLike } => {
-	let tintColor = Color.Orange
+	let source = entropy[0].icon
 
-	if (strength <= STRENGTH_HORRIBLE) {
-		tintColor = Color.Purple
-	}
-
-	if (strength > STRENGTH_HORRIBLE && strength <= STRENGTH_WEAK) {
-		tintColor = Color.Red
-	}
-
-	if (strength >= STRENGTH_STRONG) {
-		tintColor = Color.Green
+	for (const element of entropy) {
+		if (strength < element.bits) {
+			source = element.icon
+			break
+		}
 	}
 
 	return {
-		source: Icon.Circle,
-		tintColor,
+		source,
 	}
 }
