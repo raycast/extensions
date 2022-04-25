de81e45e3e18d753cc6d1628b1b33b2f1a7e3bda
Thomas Moeskops <thomasmoeskops@brightfish.nl>
2022-04-25 17:00:05 +0200

    Bugfix Apple Notes 'New Note' AS

--- extensions/apple-notes/src/new.tsx
+++ extensions/apple-notes/src/new.tsx
@@ -4,9 +4,9 @@ import { runAppleScript } from "run-applescript";
 export default async () => {
   await closeMainWindow();
   await runAppleScript(
-    'tell application "Notes" to activate \n' +
+      'tell application "Notes" to activate \n' +
       'tell application "System Events" to tell process "Notes" to ¬\n' +
-      'click menu item "New Note" of menu 1 of ¬\n' +
-      'menu bar item "File" of menu bar 1'
+      'click menu item 1 of menu 1 of ¬\n' +
+      'menu bar item 3 of menu bar 1'
   );
 };
