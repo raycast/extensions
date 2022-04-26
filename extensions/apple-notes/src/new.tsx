f0d1edec209921934f4cc22d3a016782915ce11f
Per Nielsen Tikær <per@raycast.com>
2022-04-26 08:27:18 +0200

    Update new.tsx

--- extensions/apple-notes/src/new.tsx
+++ extensions/apple-notes/src/new.tsx
@@ -3,10 +3,10 @@ import { runAppleScript } from "run-applescript";
 
 export default async () => {
   await closeMainWindow();
-  await runAppleScript(
-    'tell application "Notes" to activate \n' +
-      'tell application "System Events" to tell process "Notes" to ¬\n' +
-      "click menu item 1 of menu 1 of ¬\n" +
-      "menu bar item 3 of menu bar 1"
-  );
+  await runAppleScript(`
+    tell application "Notes" to activate
+    tell application "System Events" to tell process "Notes" to ¬
+    click menu item 1 of menu 1 of ¬
+    menu bar item 3 of menu bar 1
+  `);
 };
