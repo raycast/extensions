806cc4804c82b908ce295d2abbf4843af6f88dc1
Per Nielsen Tikær <per@raycast.com>
2022-04-26 13:46:47 +0200

    Change metadata and improved the code

--- extensions/dig/src/index.tsx
+++ extensions/dig/src/index.tsx
@@ -1,44 +1,77 @@
-import {
-  ActionPanel,
-  Detail,
-  Icon,
-  List,
-  Action,
-  showToast,
-  Toast,
-  useNavigation,
-} from "@raycast/api";
-import { useState, useEffect } from "react";
-const { execSync } = require("child_process");
+import { ActionPanel, Detail, Icon, List, Action, useNavigation } from "@raycast/api";
+import { useState, useEffect, useRef } from "react";
+import { spawn } from "child_process";
+import { STATUS_CODES } from "http";
+
+interface Result {
+  title: string;
+  url: string;
+  summary: string;
+}
+
+interface State {
+  result: Result[];
+  isLoading: boolean;
+  query: string | null;
+  validDomain: boolean | string;
+}
 
 export default function DigSearchResultsList() {
-  const [query, setQuery] = useState<null | string>(null);
-  const [state, setState] = useState<Result[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [state, setState] = useState<State>({ result: [], isLoading: false, query: null, validDomain: false });
   const { push } = useNavigation();
+  const cancelRef = useRef<AbortController | null>(null);
 
   useEffect(() => {
     async function fetch() {
-      if (!query) {
-        setState([]);
+      cancelRef.current?.abort();
+      cancelRef.current = new AbortController();
+
+      if (!state.query) {
+        setState((previous) => ({ ...previous, result: [] }));
         return;
       }
-      setIsLoading(true);
-      const results = await digByQuery(query);
-      setState(results);
-      setIsLoading(false);
+
+      // Only run if domain is found
+      const queryArr = state.query.split(" ");
+      const domainMatch = queryArr[0].match(
+        /[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)?/gi
+      );
+
+      if (!domainMatch) {
+        setState((previous) => ({ ...previous, result: [] }));
+        return;
+      }
+
+      setState((previous) => ({ ...previous, isLoading: true, validDomain: !!domainMatch }));
+
+      try {
+        const results = await digByQuery(state.query, cancelRef.current.signal);
+        setState((previous) => ({ ...previous, result: results, isLoading: false }));
+      } catch (err) {
+        if ((err as Error).name === "AbortError") {
+          return;
+        }
+      } finally {
+        setState((previous) => ({ ...previous, isLoading: false }));
+      }
     }
     fetch();
-  }, [query]);
+
+    return function () {
+      cancelRef.current?.abort();
+    };
+  }, [state.query]);
 
   return (
     <List
-      isLoading={isLoading}
+      isLoading={state.isLoading}
       searchBarPlaceholder="Type a valid Hostname (ex. raycast.com or raycast.com mx)"
-      onSearchTextChange={(text) => setQuery(text)}
+      onSearchTextChange={(text) => setState((previous) => ({ ...previous, query: text }))}
       throttle
     >
-      {state.map((result, idx) => (
+      <ListWithEmptyView validDomain={state.validDomain} query={state.query} />
+
+      {state.result.map((result, idx) => (
         <List.Item
           id={idx.toString()}
           key={idx}
@@ -71,18 +104,23 @@ function Details(props: Result) {
       markdown={`# ${title}\n## Destination:\n\`\`\`\n${summary}\n\`\`\``}
       actions={
         <ActionPanel>
-          <OpenInBrowserAction url={url} />
-          <CopyToClipboardAction title="Copy Destination" content={summary} />
+          <Action.OpenInBrowser url={url} />
+          <Action.CopyToClipboard title="Copy Destination" content={summary} />
         </ActionPanel>
       }
     />
   );
 }
 
-type Result = {
-  title: string;
-  url: string;
-  summary: string;
+export const ListWithEmptyView = (props: any) => {
+  const { validDomain, query } = props;
+
+  return (
+    <List.EmptyView
+      title={query && !!validDomain ? "No records found" : "Type hostname to search"}
+      icon={Icon.QuestionMark}
+    />
+  );
 };
 
 function getNSEntry(cmdLine: string) {
@@ -94,36 +132,38 @@ function hasWhiteSpace(s: string) {
   return s.indexOf(" ") >= 0;
 }
 
-async function digByQuery(query: string): Promise<Result[]> {
+async function digByQuery(query: string, signal: AbortSignal): Promise<Result[]> {
   try {
-    // Prepare:
     const str = query.trim();
-    let cmd: string;
+    const params: string[] = [];
+    const output = [];
+    const queryArr = str.split(" ");
+    let data = "";
 
-    // Check if string have options:
-    if (hasWhiteSpace(str)) {
-      const queryArr = str.split(" ");
+    // Check if string have options
+    if (queryArr.length > 1) {
       const query = queryArr[0].trim();
       const option = queryArr[1].trim();
-     cmd = "host -t " + option + " " + query;
+
+      params.push("-t", option, query);
     } else {
-     cmd = "host " + query;
+      params.push(str);
     }
 
-    // Define execOptions:
-    const options = {
-      encoding: "utf8",
-    };
+    const child = spawn("host", params);
+    child.stdout.setEncoding("utf-8");
 
-    // Execute command:
+    for await (const chunk of child.stdout) {
+      data += chunk;
+    }
 
-    const execReturnData = execSync(cmd, options);
+    // Check for errors in data
+    if (data.includes("not found")) {
+      return Promise.resolve([]);
+    }
 
     // Split lines into array:
-    const execReturnDataArr = execReturnData.split("\n");
-
-    // Prepare Output:
-    const x = [];
+    const execReturnDataArr = data.split("\n");
 
     // Loop stdout lines:
     for (const val of execReturnDataArr) {
@@ -132,7 +172,7 @@ async function digByQuery(query: string): Promise<Result[]> {
 
       // If not empty push into x arr:
       if (val && sum) {
-        x.push({
+        output.push({
           title: val,
           summary: sum,
           url: "https://www.nslookup.io/dns-records/" + query,
@@ -141,9 +181,8 @@ async function digByQuery(query: string): Promise<Result[]> {
     }
 
     // Return x arr
-    return x;
+    return output;
   } catch (e) {
-    showToast(Toast.Style.Failure, `Could not resolve domain`);
     return Promise.resolve([]);
   }
 }
