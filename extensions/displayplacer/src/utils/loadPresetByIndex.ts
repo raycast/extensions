19f16250fea2605f8ede7a221d8a4e1feb32783a
Eric <37158449+eluce2@users.noreply.github.com>
2022-04-25 19:04:44 -0700

    add error checking to load preset by number

--- extensions/displayplacer/src/utils/loadPresetByIndex.ts
+++ extensions/displayplacer/src/utils/loadPresetByIndex.ts
@@ -1,11 +1,22 @@
-import { LocalStorage } from "@raycast/api";
-import { switchSettings } from "./displayplacer";
+import { LocalStorage, showHUD } from "@raycast/api";
+import { listScreenInfo, switchSettings } from "./displayplacer";
 
 export default async function loadPresetByIndex(index: number) {
+  try {
+    listScreenInfo();
+  } catch (e) {
+    console.error(e);
+    showHUD("Error: Cannot load displayplacer utility. Ensure it's installed on your system.");
+    return;
+  }
+
   const myFavs = await LocalStorage.getItem("favorites");
   if (!myFavs) return;
   const favorites: Favorite[] = JSON.parse(myFavs.toString());
   const fav = favorites[index];
-  if (!fav) return;
+  if (!fav) {
+    showHUD("Favorite not found");
+    return;
+  }
   switchSettings(fav);
 }
