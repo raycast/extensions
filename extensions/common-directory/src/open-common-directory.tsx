aad690a9276877823577e48139d83c8a494085c1
Per Nielsen Tikær <per@raycast.com>
2022-04-27 14:05:20 +0200

    Update open-common-directory.tsx

--- extensions/common-directory/src/open-common-directory.tsx
+++ extensions/common-directory/src/open-common-directory.tsx
@@ -54,8 +54,8 @@ export default function OpenCommonDirectory() {
       {(commonDirectory.length === 0 && showOpenDirectory && openDirectory.length === 0) ||
       (commonDirectory.length === 0 && !showOpenDirectory) ? (
         <List.EmptyView
-          title={"No directory. Please add first"}
-          description={"You can always add directories from the Action Panel"}
+          title={"No directories found"}
+          description={"You can always add directories directly from the Action Panel"}
           actions={
             <ActionPanel>
               <Action.Push
@@ -158,7 +158,7 @@ function DirectoryItem(props: {
 
           <CopyFileActions directory={directory} />
 
-          <ActionPanel.Section title={"Directory Action"}>
+          <ActionPanel.Section title={"Directory Actions"}>
             <Action.Push
               title={"Add Directory"}
               icon={Icon.Plus}
