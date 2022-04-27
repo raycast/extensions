38a296d6bdd370132168ee6f8dcaf7b68f1d8167
koinzhang <koinzhang@gmail.com>
2022-04-27 16:38:19 +0800

    Update Quick Access

--- extensions/quick-access/src/search-pinned-directories.tsx
+++ extensions/quick-access/src/search-pinned-directories.tsx
@@ -87,7 +87,7 @@ export default function Command() {
                 <List.Section
                   key={directory.directory.id}
                   title={directory.directory.name}
-                  subtitle={showDetail ? "" : parse(directory.directory.path).dir}
+                  subtitle={showDetail ? directory.files.length + "" : parse(directory.directory.path).dir}
                 >
                   {directory.files.map(
                     (fileValue) =>
@@ -269,17 +269,15 @@ export async function upRank(index: number, setRefresh: React.Dispatch<React.Set
   const moreHighRank = directories.filter((value) => {
     return value.path !== directories[index].path && value.rank >= directories[index].rank;
   });
-  if (moreHighRank.length == 0) {
-    return directories;
+  if (moreHighRank.length !== 0) {
+    let allRank = 0;
+    directories.forEach((value) => [(allRank = allRank + value.rank)]);
+    directories[index].rank = Math.floor((directories[index].rank + 1 - directories[index].rank / allRank) * 100) / 100;
   }
-  let allRank = 0;
-  directories.forEach((value) => [(allRank = allRank + value.rank)]);
-  directories[index].rank = Math.floor((directories[index].rank + 1 - directories[index].rank / allRank) * 100) / 100;
   directories.sort(function (a, b) {
     return b.rank - a.rank;
   });
-  await LocalStorage.setItem(LocalStorageKey.LOCAL_PIN_DIRECTORY, JSON.stringify(directories));
 
+  await LocalStorage.setItem(LocalStorageKey.LOCAL_PIN_DIRECTORY, JSON.stringify(directories));
   setRefresh(refreshNumber());
-  return directories;
 }
