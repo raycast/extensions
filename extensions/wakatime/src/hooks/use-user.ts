968665fe5173914127dcd0e9101e8b1fe255384c
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-01 19:27:30 +0100

    ✨feat (hooks): add Toast message

--- extensions/wakatime/src/hooks/use-user.ts
+++ extensions/wakatime/src/hooks/use-user.ts
@@ -1,10 +1,30 @@
 import { useEffect, useState } from "react";
+import { showToast, Toast } from "@raycast/api";
 
 import { getUser } from "../utils";
 
 export function useUser() {
   const [data, setData] = useState<WakaTime.User>();
-  useEffect(() => void getUser().then(setData), []);
+
+  useEffect(() => {
+    async function getData() {
+      const toast = await showToast(Toast.Style.Animated, "Loading User");
+
+      try {
+        const user = await getUser();
+        setData(user);
+
+        toast.style = Toast.Style.Success;
+        toast.title = "Done!";
+      } catch (err) {
+        toast.style = Toast.Style.Failure;
+        toast.title = "Failed Loading User";
+        toast.message = (err as Record<string, string>).message;
+      }
+    }
+
+    getData();
+  }, []);
 
   return data;
 }
