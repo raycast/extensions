52c1c77d5691189402a8f8e0bb74be94526dae03
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-01 20:01:38 +0100

    ✨feat (dashboard): add `User` ListItem

--- extensions/wakatime/src/dashboard.tsx
+++ extensions/wakatime/src/dashboard.tsx
@@ -1,5 +1,22 @@
-import { Detail } from "@raycast/api";
+import { Icon, Image, List } from "@raycast/api";
+
+import { useUser } from "./hooks";
 
 export default function Command() {
-  return <Detail />;
+  const { data, isLoading } = useUser();
+
+  return (
+    <List isLoading={isLoading}>
+      {data !== undefined && (
+        <List.Section title="Profile">
+          <List.Item
+            subtitle={data.username}
+            title={data.display_name}
+            accessories={[{ icon: Icon.Envelope, tooltip: data.public_email }]}
+            icon={data.photo_public ? { source: data.photo, mask: Image.Mask.Circle } : Icon.Person}
+          />
+        </List.Section>
+      )}
+    </List>
+  );
 }
