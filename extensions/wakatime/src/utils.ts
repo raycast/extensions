c98b57da05b64c32ba16a540e9d15249e2b996cc
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-02 23:06:49 +0100

    📝docs: add JSDoc comments

--- extensions/wakatime/src/utils.ts
+++ extensions/wakatime/src/utils.ts
@@ -4,11 +4,21 @@ import { getPreferenceValues } from "@raycast/api";
 
 const URL = "https://wakatime.com/api/v1";
 
+/**
+ * It fetches the current user from the WakaTime API and returns the response as a User object
+ * @returns WakaTime.User
+ */
 export async function getUser() {
   const response = await fetch(`${URL}/users/current`, setAuthHeader());
   return (await response.json()) as WakaTime.User;
 }
 
+/**
+ * It fetches the user's summary data from the WakaTime API and returns it as a `WakaTime.Summary` object
+ * @param {string} key - The key of the summary you want to get.
+ * @param {Date} start - The start date of the summary.
+ * @returns WakaTime.Summary
+ */
 export async function getSummary(key: string, start: Date) {
   const response = await fetch(
     `${URL}/users/current/summaries?start=${format(start, "yyyy-MM-dd")}&end=${format(
@@ -20,16 +30,30 @@ export async function getSummary(key: string, start: Date) {
   return (await response.json()) as WakaTime.Summary;
 }
 
+/**
+ * It fetches the leaderboard data from the WakaTime API and returns it as a `WakaTime.LeaderBoard` object
+ * @param {string} [id] - The id of the leaderboard you want to get. If you don't pass in an id, it will return the public global leaderboard.
+ * @returns WakaTime.LeaderBoard
+ */
 export async function getLeaderBoard(id?: string) {
   const response = await fetch(`${URL}/${id ? `users/current/leaderboards/${id}` : "leaders"}`, setAuthHeader());
   return (await response.json()) as WakaTime.LeaderBoard;
 }
 
+/**
+ * It fetches the user's private leaderboards from the WakaTime API and returns it as a `WakaTime.PrivateLeaderBoards` object
+ * @returns `WakaTime.PrivateLeaderBoards`
+ */
 export async function getPrivateLeaderBoards() {
   const response = await fetch(`${URL}/users/current/leaderboards`, setAuthHeader());
   return (await response.json()) as WakaTime.PrivateLeaderBoards;
 }
 
+/**
+ * It takes a number of seconds and returns a string representing the duration in hours, minutes, and seconds
+ * @param {number} seconds - The number of seconds to convert to a duration.
+ * @returns A string that represents the duration of a given number of seconds.
+ */
 export function getDuration(seconds: number) {
   const getAmount = (rate: number, unit: string) => {
     const num = Math.floor(seconds / rate);
@@ -46,6 +70,12 @@ export function getDuration(seconds: number) {
   return duration || "0 sec";
 }
 
+/**
+ * It takes a WakaTime summary object and a key, and returns an array of the top 5 items in that key, sorted by total time
+ * @param obj - is the data from a WakaTime.Summary.
+ * @param key - key to get data from
+ * @returns An array of tuples, where the first element is the name of the item and the second element is the total seconds spent on that item.
+ */
 export function cumulateSummaryDuration(
   { data }: WakaTime.Summary,
   key: keyof Omit<typeof data[0], "grand_total" | "range">
@@ -66,6 +96,12 @@ export function cumulateSummaryDuration(
   return obj.slice(0, 5);
 }
 
+/**
+ * It takes a country code, converts it to uppercase, splits it into an array of characters, maps each
+ * character to its corresponding code point, and then returns a string of the code points
+ * @param {string} countryCode - The country code of the country you want to get the flag emoji for.
+ * @returns An emoji for the country code passed in.
+ */
 export function getFlagEmoji(countryCode: string) {
   const codePoints = countryCode
     .toUpperCase()
@@ -75,6 +111,10 @@ export function getFlagEmoji(countryCode: string) {
   return String.fromCodePoint(...codePoints);
 }
 
+/**
+ * It takes the API key from the user's preferences, validates it, and returns an object with the API key in the Authorization header
+ * @returns An object with a header property that contains the Authorization header.
+ */
 function setAuthHeader() {
   const API_KEY_REGEX = /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
   const { apiKey } = getPreferenceValues<Preferences>();
