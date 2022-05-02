3d52404b10ecab520df1263334eecef6f6080dac
mkitane <mehdikitane@gmail.com>
2022-05-02 22:39:59 +0200

    Add detail of the snippet to the view

---
+++ extensions/masscode-assistant/src/components/snippet-content.tsx
@@ -0,0 +1,33 @@
+import { List } from "@raycast/api";
+import type { Snippet } from "../types";
+
+const SnippetContent = ({ snippet, selectedFragment }: { snippet: Snippet; selectedFragment: number }) => {
+
+  const getMarkdown = () => {
+  	const title = snippet.name;
+  	const language = snippet.content[selectedFragment].language;
+  	const text = snippet.content[selectedFragment].value;
+
+    if (language == "plain_text" || language == "markdown") {
+      return `
+# ${title}
+
+${text}
+      `;
+    }
+
+    return `
+# ${title}
+
+\`\`\`${language}
+${text}
+\`\`\`
+    `;
+  }
+
+
+  return <List.Item.Detail markdown={getMarkdown()} />
+}
+
+
+export default SnippetContent
