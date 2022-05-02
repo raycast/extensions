9e62bbb9da8e5e1fa4531716a43f0cb107291333
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-02 02:30:45 +0100

    ✨feat (summary): add `Yesterday` range

--- extensions/wakatime/src/hooks/use-summary.ts
+++ extensions/wakatime/src/hooks/use-summary.ts
@@ -4,9 +4,10 @@ import { useEffect, useState } from "react";
 import { getSummary } from "../utils";
 
 export function useSummary(range: Range | "all" = "all", id = "current") {
-  const rangeOrder: Range[] = ["Today", "Last 7 Days", "Last 30 Days", "Last 6 Months", "Last Year"];
+  const rangeOrder: Range[] = ["Today", "Yesterday", "Last 7 Days", "Last 30 Days", "Last 6 Months", "Last Year"];
   const ranges: Record<Range, Date> = {
     Today: new Date(),
+    Yesterday: sub(new Date(), { days: 1 }),
     "Last 7 Days": sub(new Date(), { days: 7 }),
     "Last 30 Days": sub(new Date(), { days: 30 }),
     "Last 6 Months": sub(new Date(), { months: 6 }),
@@ -38,4 +39,4 @@ export function useSummary(range: Range | "all" = "all", id = "current") {
   return { data, isLoading };
 }
 
-type Range = "Today" | "Last 7 Days" | "Last 30 Days" | "Last 6 Months" | "Last Year";
+type Range = "Today" | "Yesterday" | "Last 7 Days" | "Last 30 Days" | "Last 6 Months" | "Last Year";
