95281c21646544f5bdba4850111c17387b16d371
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-02 02:29:55 +0100

    🐛fix (summary): use today as `Today` range

--- extensions/wakatime/src/hooks/use-summary.ts
+++ extensions/wakatime/src/hooks/use-summary.ts
@@ -6,7 +6,7 @@ import { getSummary } from "../utils";
 export function useSummary(range: Range | "all" = "all", id = "current") {
   const rangeOrder: Range[] = ["Today", "Last 7 Days", "Last 30 Days", "Last 6 Months", "Last Year"];
   const ranges: Record<Range, Date> = {
-    Today: sub(new Date(), { days: 1 }),
+    Today: new Date(),
     "Last 7 Days": sub(new Date(), { days: 7 }),
     "Last 30 Days": sub(new Date(), { days: 30 }),
     "Last 6 Months": sub(new Date(), { months: 6 }),
