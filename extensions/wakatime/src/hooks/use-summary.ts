1fc63f98be1239110352f43bae673c87dea66e2f
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-03 11:19:56 +0100

    ✨feat (summary): remove `Last 6 Months` and `Last Year` ranges

--- extensions/wakatime/src/hooks/use-summary.ts
+++ extensions/wakatime/src/hooks/use-summary.ts
@@ -5,14 +5,12 @@ import { useEffect, useState } from "react";
 import { getSummary } from "../utils";
 
 export function useSummary() {
-  const rangeOrder: Range[] = ["Today", "Yesterday", "Last 7 Days", "Last 30 Days", "Last 6 Months", "Last Year"];
+  const rangeOrder: Range[] = ["Today", "Yesterday", "Last 7 Days", "Last 30 Days"];
   const ranges: Record<Range, Date> = {
     Today: new Date(),
     Yesterday: sub(new Date(), { days: 1 }),
     "Last 7 Days": sub(new Date(), { days: 7 }),
     "Last 30 Days": sub(new Date(), { days: 30 }),
-    "Last 6 Months": sub(new Date(), { months: 6 }),
-    "Last Year": sub(new Date(), { years: 1 }),
   };
   const [isLoading, setIsLoading] = useState<boolean>();
   const [data, setData] = useState<Array<readonly [string, WakaTime.Summary]>>();
@@ -47,4 +45,4 @@ export function useSummary() {
   return { data, isLoading };
 }
 
-type Range = "Today" | "Yesterday" | "Last 7 Days" | "Last 30 Days" | "Last 6 Months" | "Last Year";
+type Range = "Today" | "Yesterday" | "Last 7 Days" | "Last 30 Days";
