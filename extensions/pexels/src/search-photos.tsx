5ffbeb1e2957facf7619d694877eae9f4d0a999a
koinzhang <koinzhang@gmail.com>
2022-05-05 21:11:18 +0800

    Update Pexels extension
    - Add Average Color and Description for photos
    - Update Empty-View

--- extensions/pexels/src/search-photos.tsx
+++ extensions/pexels/src/search-photos.tsx
@@ -1,19 +1,21 @@
-import { ActionPanel, List } from "@raycast/api";
+import { List } from "@raycast/api";
 import React, { useState } from "react";
 import { searchPhotos } from "./hooks/hooks";
-import { ActionToPexels, PhotosListItem } from "./utils/ui-component";
+import { PhotosListItem } from "./components/photos-list-item";
+import { PexelsEmptyView } from "./components/pexels-empty-view";
+import { isEmpty } from "./utils/common-utils";
 
 export default function SearchPhotos() {
-  const [startSearch, setStartSearch] = useState<string>("");
+  const [searchContent, setSearchContent] = useState<string>("");
   const [page, setPage] = useState<number>(1);
-  const { pexelsPhotos, loading } = searchPhotos(startSearch.trim(), page);
+  const { pexelsPhotos, loading } = searchPhotos(searchContent.trim(), page);
 
   return (
     <List
       isShowingDetail={pexelsPhotos?.photos.length !== 0}
       isLoading={loading}
       searchBarPlaceholder={"Search photos"}
-      onSearchTextChange={setStartSearch}
+      onSearchTextChange={setSearchContent}
       onSelectionChange={(id) => {
         if (typeof id !== "undefined") {
           const _id = pexelsPhotos?.photos.length - 1 + "_" + pexelsPhotos?.photos[pexelsPhotos.photos.length - 1]?.id;
@@ -24,19 +26,10 @@ export default function SearchPhotos() {
       }}
       throttle={true}
     >
-      {pexelsPhotos?.photos.length === 0 ? (
-        <List.EmptyView
-          title={"Welcome to Pexels"}
-          icon={"empty-view-icon.png"}
-          actions={
-            <ActionPanel>
-              <ActionToPexels />
-            </ActionPanel>
-          }
-        />
-      ) : (
-        pexelsPhotos?.photos.map((value, index) => <PhotosListItem key={index} pexelsPhoto={value} index={index} />)
-      )}
+      <PexelsEmptyView title={isEmpty(searchContent) ? "Welcome to Pexels" : "No Photos"} />
+      {pexelsPhotos?.photos.map((value, index) => (
+        <PhotosListItem key={index} pexelsPhoto={value} index={index} />
+      ))}
     </List>
   );
 }
