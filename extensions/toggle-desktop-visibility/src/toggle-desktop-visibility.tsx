28b32b6400900fdadc5afd66e0bc1defc8acbb51
Sorin Muntean <me@sorinmuntean.ro>
2022-05-10 11:55:09 +0200

    rename commands & extension folder

--- extensions/toggle-desktop-visibility/src/toggle-desktop-visibility.tsx
+++ extensions/toggle-desktop-visibility/src/toggle-desktop-visibility.tsx
@@ -1,21 +1,22 @@
 import { spawnSync } from "child_process";
 import { showHUD } from "@raycast/api";
 
-function toggleDesktopState(isHidden: boolean): string {
-  if (isHidden) {
-    spawnSync("defaults", ["write", "com.apple.finder", "CreateDesktop", "true"]);
-    spawnSync("killall", ["Finder"]);
-    return "Desktop Visible 🙉";
-  }
+function hideDesktop(): string {
   spawnSync("defaults", ["write", "com.apple.finder", "CreateDesktop", "false"]);
   spawnSync("killall", ["Finder"]);
   return "Desktop Hidden 🙈";
+}
+
+function showDesktop(): string {
+  spawnSync("defaults", ["write", "com.apple.finder", "CreateDesktop", "true"]);
+  spawnSync("killall", ["Finder"]);
+  return "Desktop Visible 🙉";
 }
 
 export default async function () {
   const { stdout } = spawnSync("defaults", ["read", "com.apple.finder", "CreateDesktop"], {
     encoding: "utf-8",
   });
-
-  await showHUD(toggleDesktopState(stdout.trim() === "false"));
+  const message = stdout.trim() === "false" ? showDesktop() : hideDesktop();
+  await showHUD(message);
 }
