b4efffff90ac0a2adc57974e2bb78a75ef234971
Zander Martineau <hi@zander.wtf>
2022-05-10 19:46:30 +0100

    Add AbortController to fetch call

--- extensions/search-npm/src/utils/fetchPackages.ts
+++ extensions/search-npm/src/utils/fetchPackages.ts
@@ -2,12 +2,17 @@ import { showToast, Toast } from '@raycast/api'
 import fetch from 'node-fetch'
 import { NpmsFetchResponse } from '../npmsResponse.model'
 
+let controller
+
 export const fetchPackages = async (
   searchTerm = '',
 ): Promise<NpmsFetchResponse> => {
+  controller = new AbortController()
+  const signal = controller.signal
   try {
     const response = await fetch(
       `https://api.npms.io/v2/search/suggestions?q=${searchTerm}`,
+      { signal },
     )
     const json = await response.json()
     return json as NpmsFetchResponse
