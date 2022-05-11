b3f998614d30d6deadf5194894eec517380188e6
Per Nielsen Tikær <per@raycast.com>
2022-05-11 09:14:16 +0200

    Update fetchPackages.ts

--- extensions/search-npm/src/utils/fetchPackages.ts
+++ extensions/search-npm/src/utils/fetchPackages.ts
@@ -4,9 +4,8 @@ import { NpmsFetchResponse } from '../npmsResponse.model'
 
 export const fetchPackages = async (
   searchTerm = '',
-  signal: AbortSignal
+  signal: AbortSignal,
 ): Promise<NpmsFetchResponse> => {
-
   try {
     const response = await fetch(
       `https://api.npms.io/v2/search/suggestions?q=${searchTerm}`,
@@ -15,11 +14,10 @@ export const fetchPackages = async (
     const json = await response.json()
     return json as NpmsFetchResponse
   } catch (error: any) {
-
-    if (error.name === "AbortError") {
-      return [];
+    if (error.name === 'AbortError') {
+      return []
     }
-       
+
     console.error(error)
     showToast(Toast.Style.Failure, 'Could not fetch packages')
     return Promise.resolve([])
