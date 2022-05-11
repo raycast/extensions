80088116fb3997233fdd31e1d52723deefd6b23a
Per Nielsen Tikær <per@raycast.com>
2022-05-11 14:25:18 +0200

    Update index.tsx

--- extensions/fibonacci-sequence/src/index.tsx
+++ extensions/fibonacci-sequence/src/index.tsx
@@ -1,27 +1,29 @@
 import { ActionPanel, Action, List } from "@raycast/api";
 
 const fibonacci = (n: number) => {
-    let n1 = 1, n2 = 1, nextTerm;
-    const a: Array<number> = []
-    
-    for (let i = 1; i <= n; i++) {
-        a.push(n1);
-        nextTerm = n1 + n2;
-        n1 = n2;
-        n2 = nextTerm;
-    }
-    
-    return [...new Set(a)];
-}
+  let n1 = 1,
+    n2 = 1,
+    nextTerm;
+  const a: Array<number> = [];
+
+  for (let i = 1; i <= n; i++) {
+    a.push(n1);
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
+  }
+
+  return [...new Set(a)];
+};
 
 const items = () => {
   return fibonacci(16).map((i) => {
     return {
       id: i,
       title: i,
-    }
-  })
-}
+    };
+  });
+};
 
 export default function Command() {
   return (
@@ -32,8 +34,9 @@ export default function Command() {
           icon="list-icon.png"
           title={`${item.title}`}
           actions={
-            <ActionPanel>
+            <ActionPanel title="Actions">
               <Action.CopyToClipboard content={item.title} />
+              <Action.Paste content={item.title} />
             </ActionPanel>
           }
         />
