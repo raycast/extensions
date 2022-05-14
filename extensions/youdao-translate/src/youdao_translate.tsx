f3ef61a827012697d5fa306cee2cd722bbcb373d
Phf <leopan.me@gmail.com>
2022-05-14 23:18:32 +0800

    Update Youdao Translate

--- extensions/youdao-translate/src/youdao_translate.tsx
+++ extensions/youdao-translate/src/youdao_translate.tsx
@@ -267,19 +267,17 @@ function Translate(props: { content: string | undefined; from_language: string; 
         {translate_result.basic && translate_result.basic.phonetic ? (
           <List.Section title="Phonetic">
             <List.Item
-            key="0"
-            title={`/ ${translate_result.basic.phonetic} /`}
-            icon={{ source: Icon.Dot, tintColor: Color.Green }}
-            actions={
-              <TranslateResultActionPanel
-              copy_content={translate_result.basic.phonetic}
-              url={
-                translate_result.webdict && translate_result.webdict.url
-                ? translate_result.webdict.url
-                : undefined
+              key="0"
+              title={`/ ${translate_result.basic.phonetic} /`}
+              icon={{ source: Icon.Dot, tintColor: Color.Green }}
+              actions={
+                <TranslateResultActionPanel
+                  copy_content={translate_result.basic.phonetic}
+                  url={
+                    translate_result.webdict && translate_result.webdict.url ? translate_result.webdict.url : undefined
+                  }
+                />
               }
-              />
-            }
             />
           </List.Section>
         ) : null}
