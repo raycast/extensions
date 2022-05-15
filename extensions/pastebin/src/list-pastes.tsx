21240cf34389eed57660159043098492121a4d9f
vimtor <victor@vimtor.io>
2022-05-15 17:31:55 +0200

    Add tooltips and better icons for Pastebin extension

--- extensions/pastebin/src/list-pastes.tsx
+++ extensions/pastebin/src/list-pastes.tsx
@@ -1,5 +1,5 @@
 import { Action, ActionPanel, Alert, Color, confirmAlert, Icon, List } from "@raycast/api";
-import { ParsedPaste } from "pastebin-api";
+import { ParsedPaste, Publicity } from "pastebin-api";
 import { usePaste, usePastes } from "./utils/hooks";
 import { useState } from "react";
 import dateFormat, { masks } from "dateformat";
@@ -12,6 +12,21 @@ function formatDate(date: Date) {
   return dateFormat(date, masks.shortDate);
 }
 
+const publicityIcons: Record<Publicity, { value: Icon; tooltip: string }> = {
+  [Publicity.Public]: {
+    value: Icon.Globe,
+    tooltip: "Public",
+  },
+  [Publicity.Private]: {
+    value: Icon.Link,
+    tooltip: "Private",
+  },
+  [Publicity.Unlisted]: {
+    value: Icon.EyeSlash,
+    tooltip: "Unlisted",
+  },
+};
+
 export default function ListPastes() {
   const [selectedId, setSelectedId] = useState<string>();
   const { pastes, loading: listLoading, removePasting } = usePastes();
@@ -23,10 +38,10 @@ export default function ListPastes() {
         <List.Item
           key={paste.paste_key}
           id={paste.paste_key}
-          title={paste.paste_title}
-          icon={Icon.Clipboard}
+          title={paste.paste_title || "Untitled"}
+          icon={publicityIcons[paste.paste_private]}
           detail={<List.Item.Detail isLoading={detailLoading} markdown={formatPaste(paste, detail)} />}
-          accessories={[{ text: formatDate(new Date(paste.paste_date * 1000)) }]}
+          accessories={[{ text: formatDate(new Date(paste.paste_date * 1000)), tooltip: "Creation Date" }]}
           actions={
             <ActionPanel>
               <Action.OpenInBrowser url={paste.paste_url} />
