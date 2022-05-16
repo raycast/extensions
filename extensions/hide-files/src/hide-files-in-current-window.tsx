ec137a0f6093d41fec54316eb15aa18ce7f50a39
koinzhang <koinzhang@gmail.com>
2022-05-16 16:33:13 +0800

    Update Toast text

--- extensions/hide-files/src/hide-files-in-current-window.tsx
+++ extensions/hide-files/src/hide-files-in-current-window.tsx
@@ -1,11 +1,18 @@
-import { showHUD } from "@raycast/api";
+import { closeMainWindow, showHUD } from "@raycast/api";
 import { spawn } from "child_process";
 import { getFilesInDirectory, getFocusFinderPath } from "./utils/common-utils";
 import { putFileOnHidePanel } from "./utils/hide-files-utils";
+import { homedir } from "os";
 
 export default async () => {
-  await showHUD("Hiding files in current window...");
+  closeMainWindow({ clearRootSearch: false });
   const finderPath = await getFocusFinderPath();
+  const desktopPath = `${homedir()}/Desktop/`;
+  if (finderPath === desktopPath) {
+    await showHUD("Hiding desktop files...");
+  } else {
+    await showHUD("Hiding files in current window...");
+  }
   spawn("chflags", ["hidden", `${finderPath.replace(" ", `" "`)}*`], { shell: true });
 
   //add files to hide panel
--- extensions/hide-files/src/unhide-files-in-current-window.tsx
+++ extensions/hide-files/src/unhide-files-in-current-window.tsx
@@ -1,11 +1,18 @@
-import { showHUD } from "@raycast/api";
+import { closeMainWindow, showHUD } from "@raycast/api";
 import { spawn } from "child_process";
 import { getFocusFinderPath } from "./utils/common-utils";
 import { removeFilesFromPanel } from "./utils/hide-files-utils";
+import { homedir } from "os";
 
 export default async () => {
-  await showHUD("Unhiding Current window files...");
+  closeMainWindow({ clearRootSearch: false });
   const finderPath = await getFocusFinderPath();
+  const desktopPath = `${homedir()}/Desktop/`;
+  if (finderPath === desktopPath) {
+    await showHUD("Unhiding desktop files...");
+  } else {
+    await showHUD("Unhiding files in current window...");
+  }
   spawn("chflags", ["nohidden", `${finderPath.replace(" ", `" "`)}*`], { shell: true });
 
   //remove files from hide panel
