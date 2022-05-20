6d0bdafaea14c5b5d130093c56a03eedd69d2bdd
koinzhang <koinzhang@gmail.com>
2022-05-20 13:20:14 +0800

    Update empty view icon

--- extensions/raycast-wallpaper/src/components/raycast-wallpaper-empty-view.tsx
+++ extensions/raycast-wallpaper/src/components/raycast-wallpaper-empty-view.tsx
@@ -2,5 +2,5 @@ import { List } from "@raycast/api";
 import React from "react";
 
 export function RaycastWallpaperEmptyView() {
-  return <List.EmptyView title={"No pictures"} />;
+  return <List.EmptyView icon={{ source: "raycast-empty-view-icon.svg" }} title={"No pictures"} />;
 }
