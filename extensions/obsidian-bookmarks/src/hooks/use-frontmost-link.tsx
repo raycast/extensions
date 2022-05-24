91f99c0eedf50de16880320b984745b67681d38d
Drew Hays <drewshays@gmail.com>
2022-05-24 09:58:07 -0700

    New extension: Obsidian Bookmarks (#1719)
    * initial commit of obsidian-bookmarks extension
    
    * fix tag count
    
    * rename screenshots
    
    * allow default tags in preferences, fix tag "slug"
    
    * rename form actions
    
    * show error if vault or bookmarks path is inaccessible
    
    * Align frontmatter keys with @viticci's Shortcut
    
    This update aligns the keys used in the YAML frontmatter for any saved
    bookmarks with Federico Viticci's shortcut that he published in the
    [MacStories] article [My Obsidian Setup, Part 5: Appending Text and
    Webpage Links to Specific Sections of My 'Dashboard' Note][article]
    
    I didn't notice this shortcut existed until after I'd started this
    extension, but they feel spiritually similar and I'd like to make sure
    they're compatible.
    
    [MacStories]: https://macstories.net/
    [article]: https://club.macstories.net/posts/shortcuts-essentials
    
    * fix build error
    
    * Chore: corrected date
    
    Co-authored-by: Per Nielsen Tikær <per@raycast.com>

---
+++ extensions/obsidian-bookmarks/src/hooks/use-frontmost-link.tsx
@@ -0,0 +1,73 @@
+import { useEffect, useState } from "react";
+import { runJxa } from "run-jxa";
+
+export interface Link {
+  title: string;
+  url: string;
+}
+
+export function isLink(val: unknown): val is Link {
+  if (val == null || typeof val !== "object") return false;
+  const link = val as Link;
+  return typeof link.title === "string" && typeof link.url === "string";
+}
+
+export async function getFrontmostLink(): Promise<Link | null> {
+  const result = await runJxa(`
+    const chrome = new Set(["com.google.Chrome", "com.google.Chrome.beta", "com.google.Chrome.canary"]);
+    const safari = new Set(["com.apple.Safari", "com.apple.SafariTechPreview"]);
+
+    function getFrontmostChromeLink(bundleId) {
+      const tab = Application(bundleId).windows[0].activeTab();
+      return {url: tab.url(), title: tab.title()};
+    }
+
+    function getFrontmostSafariLink(bundleId) {
+      const tab = Application(bundleId).documents[0];
+      return {url: tab.url(), title: tab.name()};
+    }
+
+
+    function getFrontmostApp() {
+      const apps = Application("System Events")
+        .applicationProcesses
+        .where({frontmost: true})
+      return apps[0].bundleIdentifier();
+    }
+
+    function getFrontmostLink() {
+      const app = getFrontmostApp();
+      if (chrome.has(app)) {
+        return getFrontmostChromeLink(app);
+      } else if (safari.has(app)) {
+        return getFrontmostSafariLink(app);
+      } else {
+        return null;
+      }
+    }
+
+    return getFrontmostLink();
+  `);
+
+  if (result == null) return null;
+  if (isLink(result)) return result;
+  throw new Error(`Unknown link format: ${JSON.stringify(result)}`);
+}
+
+export type FrontmostLinkHook = { link: Link | null | undefined; loading: boolean };
+export default function useFrontmostLink(): FrontmostLinkHook {
+  const [loading, setLoading] = useState(true);
+  const [link, setLink] = useState<Link | null | undefined>();
+
+  useEffect(() => {
+    const fetch = async () => {
+      const frontLink = await getFrontmostLink();
+      setLink(frontLink);
+      setLoading(false);
+    };
+
+    fetch();
+  }, []);
+
+  return { link, loading };
+}
