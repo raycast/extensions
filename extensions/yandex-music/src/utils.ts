10bcd021cc1de848131dc075078f09644d63a439
Aleksandr Strijnev <alexstrnik@yandex-team.ru>
2022-05-24 14:44:57 +0300

    fix: safari uniq syntax && handle case when 'Allow JavaScript from Apple Events' turned off

--- extensions/yandex-music/src/utils.ts
+++ extensions/yandex-music/src/utils.ts
@@ -1,16 +1,41 @@
 import { runAppleScript } from "run-applescript";
-import { Application, getPreferenceValues } from "@raycast/api";
+import { Application, getPreferenceValues, showHUD } from "@raycast/api";
+
+// May work in other browser too, but it's confirmed to work only in these
+type SupportedBrowsers = "Safari" | "Chrome" | "Yandex";
+
+interface OsaError {
+  stderr: string;
+}
+
+function runJS(browser: SupportedBrowsers | string, code: string): string {
+  if (browser === "Safari") {
+    return `tell t to do javascript "${code}"`;
+  } else {
+    return `tell t to execute javascript "${code}"`;
+  }
+}
 
 export async function runJSInYandexMusicTab(code: string) {
   const browser = getPreferenceValues<{ browser: Application }>().browser;
 
-  await runAppleScript(`
-        tell application "${browser.name}"
-            repeat with w in (every window)		
-                repeat with t in (every tab whose URL contains "music.yandex.ru") of w			
-                    tell t to execute javascript "${code}"	
-                end repeat	
-            end repeat
-        end tell
-    `);
+  try {
+    await runAppleScript(`
+            tell application "${browser.name}"
+                repeat with w in (every window)		
+                    repeat with t in (every tab whose URL contains "music.yandex.ru") of w			
+                        ${runJS(browser.name, code)}
+                    end repeat	
+                end repeat
+            end tell
+        `);
+  } catch (e) {
+    const message = (e as OsaError).stderr;
+
+    if (message.includes("Allow JavaScript from Apple Events")) {
+      showHUD(
+        `You must enable the 'Allow JavaScript from Apple Events' option in ${browser.name}'s Develop menu to use extension`
+      );
+    }
+  }
 }
