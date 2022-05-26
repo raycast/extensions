3f5c6e7f2e2b8dd45c2cf033dc63df320530766e
Vitaliy Kudryk <kudryk.v@protonmail.com>
2022-05-26 18:44:08 +0300

    account detail

--- extensions/recurly/src/components/AccountDetail.tsx
+++ extensions/recurly/src/components/AccountDetail.tsx
@@ -1,72 +1,80 @@
 import {List} from "@raycast/api";
 import {Account} from "recurly";
 
-// noinspection JSUnusedGlobalSymbols
+const Label = List.Item.Detail.Metadata.Label;
+
 export default function AccountDetail({account}: { account: Account }) {
-  return <List.Item.Detail markdown={accountMarkdown(account)}/>
+  return <List.Item.Detail
+    metadata={
+      <List.Item.Detail.Metadata>
+        <Label title="Account State" text={account.state || ''} icon={account.state || ''} />
+
+        <List.Item.Detail.Metadata.Separator />
+
+        <Label title="Account Information" />
+        <Label title="Account Code" text={account.code || ''} />
+        <Label title="ID" text={account.id || ''} />
+        <Label title="Email" text={account.email || ''} />
+        <Label title="Tax Exempt?" text={account.taxExempt ? 'Yes' : 'No'} />
+
+        <List.Item.Detail.Metadata.Separator />
+
+        <Label title="Billing Info" />
+        <Label title="Full Name" text={formatBillingName(account)} />
+        <Label title="Credit Card" text={formatCreditCard(account)} />
+        <Label title="Expiration" text={formatBillingExpiration(account)} />
+        <Label title="Address" text={formatBillingAddress(account)} />
+        <Label title="IP Address" text={formatIPAddress(account)} />
+
+        <List.Item.Detail.Metadata.Separator />
+
+        <Label title="Created at" text={format(account.createdAt)} />
+        <Label title="Updated at" text={format(account.updatedAt)} />
+
+        <List.Item.Detail.Metadata.Separator />
+
+        {formatCustomFields(account)}
+      </List.Item.Detail.Metadata>
+    }
+  />
 }
 
-const accountMarkdown = (account: Account) => `
-# ${formatSubscriptionEmoji(account)} ${formatTitle(account)}
----
+const formatIPAddress = (account: Account) =>
+  `${account.billingInfo?.updatedBy?.ip} (${account.billingInfo?.updatedBy?.country})`;
 
-${account.email || 'No email'}, ${account.code || 'code not set'}, ${account.id}
+const formatBillingAddress = (account: Account) => {
+  return [
+      account.billingInfo?.address?.country,
+      account.billingInfo?.address?.region,
+      account.billingInfo?.address?.city,
+      account.billingInfo?.address?.street1,
+      account.billingInfo?.address?.postalCode,
+      account.billingInfo?.address?.street2,
+    ].filter(item => item && item !== 'N/A').join(', ')
+};
 
-${formatName(account)}
 
-Created at ${localeDateTime(account.createdAt)}
+const formatBillingExpiration = (account: Account) => {
+  return `${account.billingInfo?.paymentMethod?.expMonth}/${account.billingInfo?.paymentMethod?.expYear}`
+};
 
-Last updated at ${localeDateTime(account.updatedAt)}
-
-${formatBillingInfo(account)}
-
----
-
-${formatCustomFields(account)}`
-
-const formatTitle = (account: Account) =>
-  account.email || account.code || account.id
-
-const localeDateTime = (date: Date | null | undefined) =>
+const format = (date: Date | null | undefined) =>
   date
     ? `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`
     : undefined;
 
-const formatName = (account: Account) =>
-  account.firstName && account.lastName
-    ? `${account.firstName} ${account.lastName}`
-    : account.firstName
-      ? account.firstName
-      : account.lastName
-        ? account.lastName
-        : 'Name not set';
+const formatBillingName = (account: Account) =>
+  [account.billingInfo?.firstName, account.billingInfo?.lastName]
+    .filter(value => value)
+    .join(' ');
+
+const formatCreditCard = (account: Account) =>
+  `${account.billingInfo?.paymentMethod?.cardType} ${account.billingInfo?.paymentMethod?.lastFour}`
 
 const formatCustomFields = (account: Account) =>
-  account.customFields && account.customFields.length > 0
-    ? account.customFields.map(({name, value}) => `${name}: ${value}`).join("\n\n")
-    : 'No custom fields set';
+  account.customFields && account.customFields.length > 0 &&
+    account.customFields.map(({name, value}) => <Label title={name || ''} text={value || '<no value>'} />);
 
-const formatBillingInfo = (account: Account) =>
-  account.billingInfo
-    ? `## Billing info
-
-${account.billingInfo.firstName} ${account.billingInfo.lastName}
-
-Credit card ${account.billingInfo.paymentMethod?.cardType}
-
-Expiration ${account.billingInfo.paymentMethod?.expMonth} / ${account.billingInfo.paymentMethod?.expYear}
-
-Address ${[
-      account.billingInfo.address?.country,
-      account.billingInfo.address?.region,
-      account.billingInfo.address?.city,
-      account.billingInfo.address?.street1,
-      account.billingInfo.address?.postalCode,
-      account.billingInfo.address?.street2,
-    ].filter(item => item && item !== 'N/A').join(', ')}
-    
-IP ${account.billingInfo.updatedBy?.ip} (${account.billingInfo.updatedBy?.country})`
-    : 'No billing info set';
 
 export const formatSubscriptionEmoji = (account: Account) =>
   account.hasActiveSubscription
