66f150d0f73779ab6a30ee32cbdb516e9de0e330
Raymond Li <raymond.li@aloha.software>
2022-05-30 10:38:13 +0800

    Open Signal when user interact with the extension (#1802)
    To Fix open chat feature doesn't work when Signal is not opened

--- extensions/signal/src/utils/use-signal-chats.ts
+++ extensions/signal/src/utils/use-signal-chats.ts
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
 import { SignalChat } from "./types";
 import { getStoredSignalChats, saveStoredSignalChats } from "./local-storage";
+import osascript from "osascript-tag";
+import { showToast, Toast } from "@raycast/api";
 
 export function useSignalChats() {
   const [chats, setChats] = useState<Array<SignalChat>>();
+  const [ready, setReady] = useState<boolean>();
 
   useEffect(() => {
     getStoredSignalChats().then((chats) => {
@@ -11,9 +14,41 @@ export function useSignalChats() {
     });
   }, []);
 
+  const executeJxa = async (script: string) => {
+    try {
+      const result = await osascript.jxa({ parse: true })`${script}`;
+      return result;
+    } catch (err: unknown) {
+      if (typeof err === "string") {
+        const message = err.replace("execution error: Error: ", "");
+        console.log(err);
+        showToast(Toast.Style.Failure, "Something went wrong", message);
+      }
+    }
+  };
+
+  const openSingal = async () => {
+    const result = executeJxa(`
+    const app = Application("Signal");
+var running = app.running();
+if (!running){
+   app.activate();
+   delay(5);
+}
+return running;
+    `);
+
+    result.then((value) => {
+      setReady(value == true);
+      return console.log(ready);
+    });
+  };
+
+  openSingal();
+
   return {
     chats: chats || [],
-    isLoading: chats === undefined,
+    isLoading: chats === undefined || !ready,
     updateChats: async (chats: Array<SignalChat>) => {
       setChats(chats);
       await saveStoredSignalChats(chats);
