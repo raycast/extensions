564a56356ca5b801666ddd005d19808815be7d46
Shaked Lokits <shaked.lokits@gmail.com>
2022-06-04 15:30:08 +0300

    Added open URL.

--- extensions/football/src/components/match.tsx
+++ extensions/football/src/components/match.tsx
@@ -1,5 +1,6 @@
-import { List, Icon, Color } from '@raycast/api';
+import { List, Icon, Color, Action, ActionPanel } from '@raycast/api';
 import { format, parseISO, isPast } from 'date-fns';
+import { URL } from 'node:url';
 import { useMatches } from '../hooks/useFootballData';
 import { useGroupedMatches } from '../hooks/useGroupedMatches';
 import { Match, Team } from '../shared/types';
@@ -34,26 +35,42 @@ export const MatchDetails = ({ match }: { match: Match }) => (
 );
 
 export const MatchItem = ({ match, team }: { match: Match; team?: Team }) => {
-  const getMatchTitle = (match: Match) =>
+  const matchDate = parseISO(match.utcDate);
+  const getMatchTitle = () =>
     match.homeTeam.id === team?.id
       ? `${match.homeTeam.shortName ?? 'TBD'} ${getScoreValue(match.score.fullTime.home)} - ` +
         `${getScoreValue(match.score.fullTime.away)} ${match.awayTeam.shortName ?? 'TBD'}`
       : `${match.awayTeam.shortName ?? 'TBD'} ${getScoreValue(match.score.fullTime.away)} - ` +
         `${getScoreValue(match.score.fullTime.home)} ${match.homeTeam.shortName ?? 'TBD'}`;
 
+  const getSearchURL = () => {
+    const url = new URL('https://google.com/search');
+    url.searchParams.set(
+      'q',
+      `${match.homeTeam.shortName} ${match.awayTeam.shortName} ${format(matchDate, 'dd MMMM yyyy')}`
+    );
+
+    return url.toString();
+  };
+
   return (
     <List.Item
-      title={getMatchTitle(match)}
+      title={getMatchTitle()}
       key={match.id}
       accessories={[
-        { text: format(parseISO(match.utcDate), 'dd.MM.yyyy hh:mm') },
+        { text: format(matchDate, 'dd.MM.yyyy hh:mm') },
         {
           icon: {
             source: Icon.Calendar,
-            tintColor: isPast(parseISO(match.utcDate)) ? Color.Green : Color.SecondaryText,
+            tintColor: isPast(matchDate) ? Color.Green : Color.SecondaryText,
           },
         },
       ]}
+      actions={
+        <ActionPanel>
+          <Action.OpenInBrowser title="Search Match Details" url={getSearchURL()} />
+        </ActionPanel>
+      }
     />
   );
 };
@@ -63,7 +80,11 @@ export const MatchList = ({ team }: { team: Team }) => {
   const groupedMatches = useGroupedMatches(matches);
 
   return (
-    <List isLoading={loadingMatches}>
+    <List
+      isLoading={loadingMatches}
+      navigationTitle={`${team.shortName}'s Matches`}
+      searchBarPlaceholder="Search matches"
+    >
       <List.Section title="Past Matches">
         {groupedMatches.pastMatches.map((match) => (
           <MatchItem key={match.id} match={match} team={team} />
