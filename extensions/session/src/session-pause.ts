ae0d82d5965fdb68050d1af9dc42ab362bf7db47
James Lyons <jamesjlyons29@gmail.com>
2022-06-03 17:12:05 -0700

    Add Session HUD messages

--- extensions/session/src/session-pause.ts
+++ extensions/session/src/session-pause.ts
@@ -1,8 +1,9 @@
 import open from "open";
-import { closeMainWindow } from "@raycast/api";
+import { closeMainWindow, showHUD } from "@raycast/api";
 
 export default async () => {
   const url = "session:///pause";
   open(url);
   await closeMainWindow();
+  await showHUD("Session paused ⏲️");
 };
--- extensions/session/src/session-start-new.ts
+++ extensions/session/src/session-start-new.ts
@@ -1,8 +1,9 @@
 import open from "open";
-import { closeMainWindow } from "@raycast/api";
+import { closeMainWindow, showHUD } from "@raycast/api";
 
 export default async () => {
   const url = "session:///start";
   open(url);
   await closeMainWindow();
+  await showHUD("New session started ⏲️");
 };
