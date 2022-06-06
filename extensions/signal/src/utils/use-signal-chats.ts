7331ac984664dcc592b2c83562dc00f91cdaabff
Raymond Li <leeraymond78@users.noreply.github.com>
2022-06-06 11:08:46 +0800

    Check if Signal not open then open and go into the chart automatically

--- extensions/signal/src/utils/use-signal-chats.ts
+++ extensions/signal/src/utils/use-signal-chats.ts
@@ -1,12 +1,9 @@
 import { useEffect, useState } from "react";
 import { SignalChat } from "./types";
 import { getStoredSignalChats, saveStoredSignalChats } from "./local-storage";
-import osascript from "osascript-tag";
-import { showToast, Toast } from "@raycast/api";
 
 export function useSignalChats() {
   const [chats, setChats] = useState<Array<SignalChat>>();
-  const [ready, setReady] = useState<boolean>();
 
   useEffect(() => {
     getStoredSignalChats().then((chats) => {
@@ -14,41 +11,9 @@ export function useSignalChats() {
     });
   }, []);
 
-  const executeJxa = async (script: string) => {
-    try {
-      const result = await osascript.jxa({ parse: true })`${script}`;
-      return result;
-    } catch (err: unknown) {
-      if (typeof err === "string") {
-        const message = err.replace("execution error: Error: ", "");
-        console.log(err);
-        showToast(Toast.Style.Failure, "Something went wrong", message);
-      }
-    }
-  };
-
-  const openSingal = async () => {
-    const result = executeJxa(`
-    const app = Application("Signal");
-var running = app.running();
-if (!running){
-   app.activate();
-   delay(5);
-}
-return running;
-    `);
-
-    result.then((value) => {
-      setReady(value == true);
-      return console.log(ready);
-    });
-  };
-
-  openSingal();
-
   return {
     chats: chats || [],
-    isLoading: chats === undefined || !ready,
+    isLoading: chats === undefined,
     updateChats: async (chats: Array<SignalChat>) => {
       setChats(chats);
       await saveStoredSignalChats(chats);
