dd09f4e4870e4c70023b2233863664aec83d17d0
koinzhang <36128970+koinzhang@users.noreply.github.com>
2022-06-09 00:53:34 +0800

    Refactor code

--- extensions/raycast-wallpaper/src/components/raycast-wallpaper-grid.tsx
+++ extensions/raycast-wallpaper/src/components/raycast-wallpaper-grid.tsx
@@ -1,21 +1,9 @@
-import {
-  Action,
-  ActionPanel,
-  Alert,
-  confirmAlert,
-  getPreferenceValues,
-  Grid,
-  Icon,
-  showToast,
-  Toast,
-} from "@raycast/api";
-import { buildIconCachePath, checkIconCache, deleteCache, downloadPicture, setWallpaper } from "../utils/common-utils";
-import { RAYCAST_WALLPAPER } from "../utils/constants";
+import { getPreferenceValues, Grid } from "@raycast/api";
 import React from "react";
 import { RaycastWallpaper } from "../types/types";
-import { ActionOpenPreferences } from "./action-open-preferences";
 import { RaycastWallpaperEmptyView } from "./raycast-wallpaper-empty-view";
 import { Preferences } from "../types/preferences";
+import { ActionOnRaycastWallpaper } from "./action-on-raycast-wallpaper";
 
 export function RaycastWallpaperGrid(props: { raycastWallpapers: RaycastWallpaper[] }) {
   const preferences = getPreferenceValues<Preferences>();
@@ -28,69 +16,14 @@ export function RaycastWallpaperGrid(props: { raycastWallpapers: RaycastWallpape
       searchBarPlaceholder={"Search pictures"}
     >
       <RaycastWallpaperEmptyView layout={preferences.layout} />
-      {raycastWallpapers.map((value, index, array) => {
+      {raycastWallpapers.map((value, index) => {
         return (
           <Grid.Item
             id={index + ""}
             key={index + value.title}
-            content={{ source: checkIconCache(value) ? buildIconCachePath(value) : value.url }}
+            content={value.url.replace(".png", "-preview.png")}
             title={value.title}
-            actions={
-              <ActionPanel>
-                <Action
-                  icon={Icon.Desktop}
-                  title={"Set Desktop Wallpaper"}
-                  onAction={() => {
-                    setWallpaper(value).then(() => "");
-                  }}
-                />
-                <Action
-                  icon={Icon.Download}
-                  title={"Download Picture"}
-                  onAction={async () => {
-                    await downloadPicture(value);
-                  }}
-                />
-                <ActionPanel.Section>
-                  <Action
-                    icon={Icon.TwoArrowsClockwise}
-                    title={"Set Random Wallpaper"}
-                    shortcut={{ modifiers: ["cmd"], key: "r" }}
-                    onAction={() => {
-                      const randomImage = array[Math.floor(Math.random() * array.length)];
-                      setWallpaper(randomImage).then(() => "");
-                    }}
-                  />
-                  <Action.OpenInBrowser
-                    title={"Go to Raycast Wallpaper"}
-                    shortcut={{ modifiers: ["shift", "cmd"], key: "g" }}
-                    url={RAYCAST_WALLPAPER}
-                  />
-                  <Action
-                    icon={Icon.Trash}
-                    title={"Clear Pictures Cache"}
-                    shortcut={{ modifiers: ["shift", "cmd"], key: "backspace" }}
-                    onAction={async () => {
-                      const options: Alert.Options = {
-                        icon: Icon.Trash,
-                        title: "Are you sure?",
-                        message: "Next time you enter the command, the pictures will be re-cached.",
-                        primaryAction: {
-                          title: "Confirm",
-                          onAction: () => {
-                            deleteCache();
-                            showToast(Toast.Style.Success, "Clear cache success!");
-                          },
-                        },
-                      };
-                      await confirmAlert(options);
-                    }}
-                  />
-                </ActionPanel.Section>
-
-                <ActionOpenPreferences />
-              </ActionPanel>
-            }
+            actions={<ActionOnRaycastWallpaper index={index} raycastWallpapers={raycastWallpapers} />}
           />
         );
       })}
--- extensions/raycast-wallpaper/src/hooks/hooks.ts
+++ extensions/raycast-wallpaper/src/hooks/hooks.ts
@@ -1,9 +1,10 @@
 import { useCallback, useEffect, useState } from "react";
-import fetch, { AbortError } from "node-fetch";
 import { LocalStorageKey, RAYCAST_WALLPAPER_LIST_URL } from "../utils/constants";
 import { RaycastWallpaper } from "../types/types";
-import { cacheIconPreview, cachePicture, checkCache, checkIconCache } from "../utils/common-utils";
-import { LocalStorage } from "@raycast/api";
+import { LocalStorage, showToast, Toast } from "@raycast/api";
+import { cachePicture, checkCache } from "../utils/common-utils";
+import axios from "axios";
+import Style = Toast.Style;
 
 export const getRaycastWallpaperList = () => {
   const [raycastWallpapers, setRaycastWallpapers] = useState<RaycastWallpaper[]>([]);
@@ -15,38 +16,33 @@ export const getRaycastWallpaperList = () => {
       const _wallpaperList =
         typeof _localStorage === "undefined" ? [] : (JSON.parse(_localStorage) as RaycastWallpaper[]);
 
-      if (_wallpaperList.length === 0) {
-        fetch(RAYCAST_WALLPAPER_LIST_URL)
-          .then((response) => response.json())
-          .then((data) => {
-            //cache wallpaper list
-            const _raycastWallpaper = data as RaycastWallpaper[];
-            setRaycastWallpapers(_raycastWallpaper);
+      setRaycastWallpapers(_wallpaperList);
+      //cache picture
+      await axios({
+        method: "GET",
+        url: RAYCAST_WALLPAPER_LIST_URL,
+        params: {
+          format: "json",
+        },
+      })
+        .then((axiosRes) => {
+          const _raycastWallpaper = axiosRes.data as RaycastWallpaper[];
+          setRaycastWallpapers(_raycastWallpaper);
 
-            //cache list
-            LocalStorage.setItem(LocalStorageKey.WALLPAPER_LIST_CACHE, JSON.stringify(_raycastWallpaper));
+          //cache list
+          LocalStorage.setItem(LocalStorageKey.WALLPAPER_LIST_CACHE, JSON.stringify(_raycastWallpaper));
 
-            //cache picture
-            _raycastWallpaper.forEach((value) => {
-              if (!checkCache(value)) {
-                cachePicture(value);
-              } else {
-                if (!checkIconCache(value)) {
-                  cacheIconPreview(value);
-                }
-              }
-            });
+          _raycastWallpaper.forEach((value) => {
+            if (!checkCache(value)) {
+              cachePicture(value);
+            }
           });
-      } else {
-        setRaycastWallpapers(_wallpaperList);
-      }
+        })
+        .catch((error) => {
+          showToast(Style.Failure, String(error));
+        });
     } catch (e) {
-      if (e instanceof AbortError) {
-        console.log("Abort");
-      } else {
-        console.log(e);
-      }
-      return;
+      await showToast(Style.Failure, String(e));
     }
   }, []);
 
