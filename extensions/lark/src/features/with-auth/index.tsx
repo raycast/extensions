288e748ab76475241b9e5abb4b54af85790cc4dc
Per Nielsen Tikær <per@raycast.com>
2022-06-08 11:15:34 +0200

    Update index.tsx

--- extensions/lark/src/features/with-auth/index.tsx
+++ extensions/lark/src/features/with-auth/index.tsx
@@ -1,16 +1,10 @@
-import { Detail } from "@raycast/api";
-import { useEffect, useState } from "react";
-import { parse } from "tough-cookie";
-import {
-  checkAuthState,
-  isAuthenticated,
-  setAuthData,
-} from "../../services/shared";
-import { QRLogin } from "./qr-login";
+import { Detail } from '@raycast/api';
+import { useEffect, useState } from 'react';
+import { parse } from 'tough-cookie';
+import { checkAuthState, isAuthenticated, setAuthData } from '../../services/shared';
+import { QRLogin } from './qr-login';
 
-const AuthGuard: React.FC<{ component: React.FC }> = ({
-  component: Component,
-}) => {
+const AuthGuard: React.FC<{ component: React.FC }> = ({ component: Component }) => {
   const [checked, setChecked] = useState(isAuthenticated);
   const [, refresh] = useState(0);
 
@@ -22,7 +16,7 @@ const AuthGuard: React.FC<{ component: React.FC }> = ({
     for (const item of cookies) {
       const cookie = parse(item);
       if (!cookie) return;
-      if (cookie.key === "session") {
+      if (cookie.key === 'session') {
         await setAuthData(tenantDomain, cookie.value);
         refresh(Math.random());
         break;
@@ -30,15 +24,7 @@ const AuthGuard: React.FC<{ component: React.FC }> = ({
     }
   };
 
-  return checked ? (
-    isAuthenticated ? (
-      <Component />
-    ) : (
-      <QRLogin onConfirm={handleLogin} />
-    )
-  ) : (
-    <Detail markdown="" />
-  );
+  return checked ? isAuthenticated ? <Component /> : <QRLogin onConfirm={handleLogin} /> : <Detail markdown="" />;
 };
 
 export const withAuth =
