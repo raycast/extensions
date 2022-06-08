b677d1181c65812ff8e11b805ab02807b140a0fc
koinzhang <36128970+koinzhang@users.noreply.github.com>
2022-06-08 18:30:40 +0800

    Update Placeholder extension

--- extensions/placeholder/src/search-placeholder-images.tsx
+++ extensions/placeholder/src/search-placeholder-images.tsx
@@ -1,27 +1,22 @@
-import { Action, ActionPanel, getPreferenceValues, Icon, List } from "@raycast/api";
+import { getPreferenceValues, Grid, List } from "@raycast/api";
 import React, { useState } from "react";
 import { getPlaceholderImages } from "./hooks/hooks";
 import { Preferences } from "./types/preferences";
 import { PlaceholderEmptyView } from "./components/placeholder-empty-view";
-import { ActionOpenPreferences } from "./components/action-open-preferences";
-import StylizePlaceholderImage from "./stylize-placeholder-image";
-import { PicsumImageAction } from "./components/picsum-image-action";
-import { setWallpaper } from "./utils/common-utils";
+import { ActionOnPlaceholderImage } from "./components/action-on-placeholder-image";
 
 export default function SearchPlaceholderImages() {
-  const { primaryAction, perPage } = getPreferenceValues<Preferences>();
-
+  const preferences = getPreferenceValues<Preferences>();
   const [page, setPage] = useState<number>(1);
+  const { picsumImages, isLoading } = getPlaceholderImages(page, parseInt(preferences.perPage));
 
-  const { picsumImages, isLoading } = getPlaceholderImages(page, parseInt(perPage));
-
-  return (
+  return preferences.layout === "List" ? (
     <List
       isShowingDetail={picsumImages.length !== 0 && !isLoading}
       isLoading={isLoading}
       searchBarPlaceholder={"Search images"}
     >
-      <PlaceholderEmptyView />
+      <PlaceholderEmptyView layout={preferences.layout} />
 
       {picsumImages.map((value) => {
         return (
@@ -47,68 +42,28 @@ export default function SearchPlaceholderImages() {
               />
             }
             actions={
-              <ActionPanel>
-                <Action.Push
-                  icon={{
-                    source: {
-                      light: "stylize-placeholder-icon.png",
-                      dark: "stylize-placeholder-icon@dark.png",
-                    },
-                  }}
-                  title={"Stylize Image"}
-                  target={<StylizePlaceholderImage id={value.id} width={value.width} height={value.height} />}
-                />
-                <Action
-                  icon={Icon.Desktop}
-                  title={"Set Desktop Wallpaper"}
-                  onAction={() => {
-                    setWallpaper(value.download_url, "wallpaper-" + Date.now()).then();
-                  }}
-                />
-                <ActionPanel.Section>
-                  <Action
-                    icon={Icon.ChevronUp}
-                    title={"Previous Page"}
-                    shortcut={{ modifiers: ["cmd"], key: "pageUp" }}
-                    onAction={() => {
-                      if (page > 1) {
-                        setPage(page - 1);
-                      }
-                    }}
-                  />
-                  <Action
-                    icon={Icon.ChevronDown}
-                    title={"Next Page"}
-                    shortcut={{ modifiers: ["cmd"], key: "pageDown" }}
-                    onAction={() => setPage(page + 1)}
-                  />
-                </ActionPanel.Section>
-                <ActionPanel.Section>
-                  <PicsumImageAction
-                    imageURL={value.download_url}
-                    size={value.width + "x" + value.height}
-                    primaryAction={primaryAction}
-                  />
-                </ActionPanel.Section>
-                <ActionPanel.Section>
-                  <Action.OpenInBrowser shortcut={{ modifiers: ["cmd"], key: "o" }} url={value.download_url} />
-                  <Action.OpenInBrowser
-                    title={"Open in Unsplash"}
-                    shortcut={{ modifiers: ["cmd"], key: "u" }}
-                    url={value.url}
-                  />
-                  <Action.CopyToClipboard
-                    shortcut={{ modifiers: ["shift", "cmd"], key: "." }}
-                    title={"Copy Unsplash URL"}
-                    content={value.url}
-                  />
-                </ActionPanel.Section>
-                <ActionOpenPreferences />
-              </ActionPanel>
+              <ActionOnPlaceholderImage picsumImage={value} preferences={preferences} page={page} setPage={setPage} />
             }
           />
         );
       })}
     </List>
+  ) : (
+    <Grid isLoading={isLoading} searchBarPlaceholder={"Search images"}>
+      <PlaceholderEmptyView layout={preferences.layout} />
+
+      {picsumImages.map((value) => {
+        return (
+          <Grid.Item
+            key={value.url}
+            content={value.download_url}
+            title={value.author}
+            actions={
+              <ActionOnPlaceholderImage picsumImage={value} preferences={preferences} page={page} setPage={setPage} />
+            }
+          />
+        );
+      })}
+    </Grid>
   );
 }
