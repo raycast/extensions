f9edc1c026ba29531e1524446ce4277ab61a90b5
Emil Małańczak <kdddmalanczak@gmail.com>
2022-06-09 22:06:57 +0200

    refactor: 💡 connect 1st and 2nd step to improve UX experience

--- extensions/mantine/src/index.tsx
+++ extensions/mantine/src/index.tsx
@@ -1,12 +1,18 @@
 import { Action, ActionPanel, Icon, List, showToast, Toast } from "@raycast/api";
 import { useEffect, useState } from "react";
+import { DocumentsList } from "./components/DocumentsList";
 import { SectionList } from "./components/SectionList";
 import { DocumentationSection, getDocumentation } from "./helpers/getDocumentation";
 import { uppercaseFirst } from "./utils/uppercaseFirst";
 
+type Documentation = {
+  sections: DocumentationSection[];
+  changelog: DocumentationSection["data"];
+};
+
 export default function Command() {
   const [status, setStatus] = useState<"loading" | "error" | "initial" | "ready">("initial");
-  const [documentation, setDocumentation] = useState<DocumentationSection[]>();
+  const [documentation, setDocumentation] = useState<Documentation>();
 
   const getDocs = async () => {
     setStatus("loading");
@@ -19,7 +25,24 @@ export default function Command() {
     try {
       const docs = await getDocumentation();
 
-      setDocumentation(docs);
+      const dividedDocs = docs.reduce(
+        (acc, doc) =>
+          doc.section === "changelog"
+            ? {
+                ...acc,
+                changelog: doc.data.reverse(),
+              }
+            : {
+                ...acc,
+                sections: [...acc.sections, doc],
+              },
+        {
+          sections: [],
+          changelog: [],
+        } as Documentation
+      );
+
+      setDocumentation(dividedDocs);
 
       toast.style = Toast.Style.Success;
       toast.title = "Documentation loaded";
@@ -44,24 +67,28 @@ export default function Command() {
   return (
     <>
       <List isLoading={documentation === undefined || status === "loading"}>
-        {documentation && documentation?.length > 0 ? (
+        {documentation && documentation?.sections?.length > 0 ? (
           <>
-            {documentation.map(({ section, data }) => (
+            {documentation.sections.map(({ section, data }) => (
+              <List.Section key={section} title={uppercaseFirst(section)}>
+                <DocumentsList documentation={data} />
+              </List.Section>
+            ))}
+            <List.Section title="Changelog">
               <List.Item
+                title="Changelog"
                 icon={Icon.Dot}
-                title={uppercaseFirst(section)}
-                key={section}
                 actions={
                   <ActionPanel>
                     <Action.Push
                       icon={Icon.List}
                       title="Open"
-                      target={<SectionList data={data} title={uppercaseFirst(section)} />}
+                      target={<SectionList data={documentation.changelog} title="Changelog" />}
                     />
                   </ActionPanel>
                 }
               />
-            ))}
+            </List.Section>
           </>
         ) : (
           <List.EmptyView title="Loading mantine" description="wait until documentation will load" icon="logo.png" />
