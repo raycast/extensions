b7b679e0c7043bf4d94ad9fba1e1cfb6f92a0125
Alberto Xamin <alberto.xamin@datadoghq.com>
2022-06-10 14:23:01 +0200

    add pagination

--- extensions/datadog/src/command-list-dashboards.tsx
+++ extensions/datadog/src/command-list-dashboards.tsx
@@ -1,13 +1,53 @@
-import { ActionPanel, List, OpenInBrowserAction } from "@raycast/api";
-import { useDashboards } from "./useDashboards";
+import { ActionPanel, List, OpenInBrowserAction, getPreferenceValues } from "@raycast/api";
 import { linkDomain } from "./util";
+import fetch from "node-fetch";
+import { DashboardSummaryDefinition } from "@datadog/datadog-api-client/dist/packages/datadog-api-client-v1/models/DashboardSummaryDefinition";
+import { useEffect, useState } from "react";
 
-// noinspection JSUnusedGlobalSymbols
+type State = {
+  dashboardsAreLoading: boolean;
+  dashboards: DashboardSummaryDefinition[];
+};
+
+type DashboardSearchApiResponse = {
+  total: number;
+  dashboards: DashboardSummaryDefinition[];
+};
+
 export default function CommandListDashboards() {
-  const { dashboards, dashboardsAreLoading } = useDashboards();
+  const [{ dashboardsAreLoading, dashboards }, setState] = useState<State>({
+    dashboards: [],
+    dashboardsAreLoading: true,
+  });
+  const search = (query: string) => {
+    console.log("searching for dashboards", query);
+    fetch(
+      encodeURI(
+        `https://app.datadoghq.com/api/v1/dashboard_search?with_suggested=true&query=${query}&start=0&count=50&sort=`
+      ),
+      {
+        headers: {
+          "DD-API-KEY": getPreferenceValues()["api-key"],
+          "DD-APPLICATION-KEY": getPreferenceValues()["app-key"],
+        },
+      }
+    )
+      .then(res => res.json())
+      .then(data => {
+        setState(prev => ({ ...prev, dashboards: (data as DashboardSearchApiResponse).dashboards }));
+      })
+      .catch(err => {
+        console.error(err);
+      })
+      .finally(() => setState(prev => ({ ...prev, dashboardsAreLoading: false })));
+  };
+
+  useEffect(() => {
+    search("");
+  }, []);
 
   return (
-    <List isLoading={dashboardsAreLoading}>
+    <List isLoading={dashboardsAreLoading} onSearchTextChange={search}>
       {dashboards.map(dashboard => (
         <List.Item
           key={dashboard.id}
