9500f3287ac481a9ccd5feb69c1e5071e0ff1b11
koinzhang <36128970+koinzhang@users.noreply.github.com>
2022-06-13 17:07:22 +0800

    Update action-on-open-link-app.tsx

--- extensions/open-link-in-specific-browser/src/components/action-on-open-link-app.tsx
+++ extensions/open-link-in-specific-browser/src/components/action-on-open-link-app.tsx
@@ -48,54 +48,57 @@ export function ActionOnOpenLinkApp(props: {
       <ActionPanel.Section>
         {!isCustom && <ActionAddOpenLinkApp curApp={openLinkApplication} setRefresh={setRefresh} />}
         {isCustom && (
-          <Action
-            title={"Remove Application"}
-            icon={Icon.Trash}
-            shortcut={{ modifiers: ["ctrl"], key: "x" }}
-            onAction={async () => {
-              const _openLinkApplications = [...openLinkApplications];
-              _openLinkApplications.splice(index, 1);
-              await LocalStorage.setItem(LocalStorageKey.CUSTOM_APPS, JSON.stringify(_openLinkApplications));
-              setRefresh(Date.now());
-            }}
-          />
+          <>
+            <Action
+              title={"Remove Application"}
+              icon={Icon.Trash}
+              shortcut={{ modifiers: ["ctrl"], key: "x" }}
+              onAction={async () => {
+                const _openLinkApplications = [...openLinkApplications];
+                _openLinkApplications.splice(index, 1);
+                await LocalStorage.setItem(LocalStorageKey.CUSTOM_APPS, JSON.stringify(_openLinkApplications));
+                setRefresh(Date.now());
+              }}
+            />
+
+            <Action
+              title={`Reset ${openLinkApplication.name} Rank`}
+              icon={Icon.ArrowClockwise}
+              shortcut={{ modifiers: ["cmd"], key: "r" }}
+              onAction={async () => {
+                await alertDialog(
+                  Icon.Trash,
+                  `Reset ${openLinkApplication.name} Rank`,
+                  `Are you sure you want to reset ${openLinkApplication.name} rank?`,
+                  "Reset",
+                  async () => {
+                    await clearRank(openLinkApplication, openLinkApplications);
+                    setRefresh(Date.now());
+                    await showToast(Toast.Style.Success, `Rank of ${openLinkApplication.name} Reset!`);
+                  }
+                );
+              }}
+            />
+            <Action
+              title={"Reset All Rank"}
+              icon={Icon.ExclamationMark}
+              shortcut={{ modifiers: ["shift", "cmd"], key: "r" }}
+              onAction={async () => {
+                await alertDialog(
+                  Icon.Trash,
+                  `Reset All Rank`,
+                  `Are you sure you want to reset all rank?`,
+                  "Reset All",
+                  async () => {
+                    await clearAllRank(openLinkApplications);
+                    setRefresh(Date.now());
+                    await showToast(Toast.Style.Success, "Rank of All Reset!");
+                  }
+                );
+              }}
+            />
+          </>
         )}
-        <Action
-          title={`Reset ${openLinkApplication.name} Rank`}
-          icon={Icon.ArrowClockwise}
-          shortcut={{ modifiers: ["cmd"], key: "r" }}
-          onAction={async () => {
-            await alertDialog(
-              Icon.Trash,
-              `Reset ${openLinkApplication.name} Rank`,
-              `Are you sure you want to reset ${openLinkApplication.name} rank?`,
-              "Reset",
-              async () => {
-                await clearRank(openLinkApplication, openLinkApplications);
-                setRefresh(Date.now());
-                await showToast(Toast.Style.Success, `Rank of ${openLinkApplication.name} Reset!`);
-              }
-            );
-          }}
-        />
-        <Action
-          title={"Reset All Rank"}
-          icon={Icon.ExclamationMark}
-          shortcut={{ modifiers: ["shift", "cmd"], key: "r" }}
-          onAction={async () => {
-            await alertDialog(
-              Icon.Trash,
-              `Reset All Rank`,
-              `Are you sure you want to reset all rank?`,
-              "Reset All",
-              async () => {
-                await clearAllRank(openLinkApplications);
-                setRefresh(Date.now());
-                await showToast(Toast.Style.Success, "Rank of All Reset!");
-              }
-            );
-          }}
-        />
       </ActionPanel.Section>
       <ActionOpenPreferences />
     </ActionPanel>
