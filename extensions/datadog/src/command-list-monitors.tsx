79f56d9668a0f17a2ce2807e735a8849483ac6c9
Alberto Xamin <alberto.xamin@datadoghq.com>
2022-06-13 15:28:13 +0200

    use pagination for monitors

--- extensions/datadog/src/command-list-monitors.tsx
+++ extensions/datadog/src/command-list-monitors.tsx
@@ -9,8 +9,10 @@ import {
   WARN,
 } from "@datadog/datadog-api-client/dist/packages/datadog-api-client-v1/models/MonitorOverallStates";
 import { ActionPanel, List, OpenInBrowserAction } from "@raycast/api";
-import { useMonitors } from "./useMonitors";
-import { linkDomain } from "./util";
+import { useEffect, useState } from "react";
+import { monitorsApi } from "./datadog-api";
+import { MonitorSearchResponse } from "@datadog/datadog-api-client/dist/packages/datadog-api-client-v1/models/MonitorSearchResponse";
+import { showError, linkDomain } from "./util";
 
 const statusIcon = (status: MonitorOverallStates | undefined) => {
   const icon = (name: string, themable = false) => {
@@ -41,27 +43,55 @@ const statusIcon = (status: MonitorOverallStates | undefined) => {
   return { light: "", dark: "" };
 };
 
+type State = {
+  monitorsAreLoading: boolean;
+  response?: MonitorSearchResponse;
+};
+
 // noinspection JSUnusedGlobalSymbols
 export default function CommandListMonitors() {
-  const { monitors, monitorsAreLoading } = useMonitors();
+  const [{ monitorsAreLoading, response }, setState] = useState<State>({
+    response: undefined,
+    monitorsAreLoading: true,
+  });
 
+  const search = (query: string) => {
+    console.log("searching for monitors", query);
+    setState(prev => ({ ...prev, monitorsAreLoading: true }));
+    monitorsApi
+      .searchMonitors({ query: "", page: 0, perPage: 50 })
+      .then(x => setState(prev => ({ ...prev, response: x, monitorsAreLoading: false })))
+      .catch(showError);
+  };
+
+  useEffect(() => {
+    search("");
+  }, []);
+
+  const getCountSummary = () => {
+    if (!response) return "";
+    return ", " + response!.counts!.status!.map(x => `${x.count} ${x.name}`).join(", ");
+  };
   return (
-    <List isLoading={monitorsAreLoading}>
-      {monitors.map(monitor => (
-        <List.Item
-          key={monitor.id}
-          icon={{ source: { light: "icon@light.png", dark: "icon@dark.png" } }}
-          title={monitor.name || monitor.query}
-          subtitle={monitor.tags?.join(", ")}
-          accessoryTitle={monitor.overallState}
-          accessoryIcon={{ source: statusIcon(monitor.overallState) }}
-          actions={
-            <ActionPanel>
-              <OpenInBrowserAction url={`https://${linkDomain()}/monitors/${monitor.id}`} />
-            </ActionPanel>
-          }
-        />
-      ))}
+    <List isLoading={monitorsAreLoading} onSearchTextChange={search}>
+      <List.Section title={`Available monitors ${response?.metadata?.totalCount}${getCountSummary()}`}>
+        {response
+          ? response!.monitors!.map(monitor => (
+              <List.Item
+                key={monitor.id}
+                title={monitor.name || monitor.query!}
+                subtitle={monitor.tags?.join(", ")}
+                accessoryTitle={monitor.status}
+                accessoryIcon={{ source: statusIcon(monitor.status) }}
+                actions={
+                  <ActionPanel>
+                    <OpenInBrowserAction url={`https://${linkDomain()}/monitors/${monitor.id}`} />
+                  </ActionPanel>
+                }
+              />
+            ))
+          : null}
+      </List.Section>
     </List>
   );
 }
