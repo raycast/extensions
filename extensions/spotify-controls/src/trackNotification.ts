1a856f35ce7ca1caf38e2dd62043ff4d7c7119a0
alejgarciarodriguez <alejgarciarodriguez@gmail.com>
2022-06-13 21:58:15 +0200

    display notification when track changed

---
+++ extensions/spotify-controls/src/trackNotification.ts
@@ -0,0 +1,42 @@
+import {getPreferenceValues} from "@raycast/api";
+import {PreferenceValues} from "@raycast/api/types/api/environment/preferences";
+import {runAppleScriptSilently} from "./utils";
+
+export async function showPreviousTrackNotification() {
+    const preferences = getPreferenceValues<PreferenceValues>();
+    if (false === preferences.previousTrackNotificationEnabled) {
+        return;
+    }
+
+    await showNotification();
+}
+
+export async function showNextTrackNotification() {
+    const preferences = getPreferenceValues<PreferenceValues>();
+    if (false === preferences.previousTrackNotificationEnabled) {
+        return;
+    }
+
+    await showNotification();
+}
+
+async function showNotification() {
+    const command = `
+      if application "Spotify" is not running then
+          return
+      end if
+    
+      property currentTrackName : "Unknown Track"
+      property currentTrackArtist : "Unknown Artist"
+    
+      tell application "Spotify"
+          try
+              set currentTrackName to name of the current track
+              set currentTrackArtist to artist of the current track
+          end try
+      end tell
+    
+      display notification with title currentTrackArtist subtitle currentTrackName`;
+
+    await runAppleScriptSilently(command);
+}
