169f7afb350789212c774cd9d7cbe7f10b57b84d
Per Nielsen Tikær <per@raycast.com>
2022-06-14 13:41:36 +0200

    Update utils.ts

--- extensions/pipe-commands/src/utils.ts
+++ extensions/pipe-commands/src/utils.ts
@@ -15,7 +15,9 @@ export function parseMetadatas(script: string): ScriptMetadatas {
   const matches = [...script.matchAll(metadataRegex)];
   for (const match of matches) {
     const metadataTitle = match[1];
-    metadatas[metadataTitle] = ["argument1", "needsConfirmation"].includes(metadataTitle) ? JSON.parse(match[2]) : match[2];
+    metadatas[metadataTitle] = ["argument1", "needsConfirmation"].includes(metadataTitle)
+      ? JSON.parse(match[2])
+      : match[2];
   }
 
   return metadatas as unknown as ScriptMetadatas;
