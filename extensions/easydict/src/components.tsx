305d842f2a597e018d43bee093a82f83b9cb4c28
tisfeng <tisfeng@gmail.com>
2022-06-15 18:01:14 +0800

    style: change copy action title to "Copy Text"

--- extensions/easydict/src/components.tsx
+++ extensions/easydict/src/components.tsx
@@ -160,7 +160,7 @@ export class ListActionPanel extends Component<ListItemActionPanelItem> {
             onCopy={() => {
               console.log("copy: ", this.props.copyText);
             }}
-            title={`Copy Text:  ${this.props.copyText}`}
+            title={`Copy Text`}
             content={this.props.copyText || ""}
           />
           {this.props.isInstalledEudic && (
