94c715bb2f7ca3f31aba8d8aef3ebdde07f0fd60
pradeep <pradeepboorgu@gmail.com>
2022-06-15 16:01:31 +0530

    improve

--- extensions/zen-mode/src/deactivate.tsx
+++ extensions/zen-mode/src/deactivate.tsx
@@ -1,9 +1,14 @@
 import open from "open";
 import { closeMainWindow, showHUD } from "@raycast/api";
+import { isZenModeInstalled } from "./checkInstall";
 
 export default async () => {
-  const url = "zenmode://deactivate";
-  open(url);
-  await closeMainWindow();
-  await showHUD("Zen Mode - Deactivated");
+  if (await isZenModeInstalled()) {
+    const url = "zenmode://deactivate";
+    open(url);
+    await closeMainWindow();
+    await showHUD("Zen Mode - Deactivated");
+  } else {
+    await showHUD("Zen Mode app is not installed");
+  }
 };
