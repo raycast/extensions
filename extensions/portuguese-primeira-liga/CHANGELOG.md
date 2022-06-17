bc38c4703d15c50e1d2638e342af1c06400de889
danielsequeira <daniel@raycast.com>
2022-06-17 18:10:09 +0200

    Added Newspapers command and Show Details action

--- extensions/portuguese-primeira-liga/CHANGELOG.md
+++ extensions/portuguese-primeira-liga/CHANGELOG.md
@@ -1,5 +1,10 @@
 # Portuguese Primeira Liga Changelog
 
+## [New Additions] - 2022-06-17
+
+- Added Newspapers command to view today's newspapers covers
+- Added Show Details action on Standing command
+
 ## [Initial Version] - 2022-06-16
 
 Added Standings
--- extensions/portuguese-primeira-liga/src/standings.tsx
+++ extensions/portuguese-primeira-liga/src/standings.tsx
@@ -1,11 +1,13 @@
-import { Color, Image, List } from "@raycast/api";
+import { Action, ActionPanel, Icon, Image, List } from "@raycast/api";
+import { useState } from "react";
 import useStandings from "./hooks/useStandings";
 
 export default function GetStandings() {
   const standings = useStandings();
+  const [showDetails, setShowDetails] = useState<boolean>(false);
 
   return (
-    <List throttle isLoading={!standings}>
+    <List throttle isLoading={!standings} isShowingDetail={showDetails}>
       {standings?.map((table) => {
         return (
           <List.Item
@@ -17,15 +19,33 @@ export default function GetStandings() {
               fallback: "default.png",
             }}
             subtitle={`${table.team.tla}`}
-            accessories={[
-              { text: `W: ${table.won}` },
-              { text: `D: ${table.draw}` },
-              { text: `L: ${table.lost}` },
-              { text: `GF: ${table.goalsFor}` },
-              { text: `GA: ${table.goalsAgainst}` },
-              { text: `GD: ${table.goalDifference}` },
-              { text: `Points: ${table.points}` },
-            ]}
+            accessories={[{ text: `Points: ${table.points}` }]}
+            detail={
+              <List.Item.Detail
+                metadata={
+                  <List.Item.Detail.Metadata>
+                    <List.Item.Detail.Metadata.Label title="Details" />
+                    <List.Item.Detail.Metadata.Label title="Played Games" text={table.playedGames.toString()} />
+                    <List.Item.Detail.Metadata.Label title="Form" text={table.form} />
+                    <List.Item.Detail.Metadata.Label title="Won" text={table.won.toString()} />
+                    <List.Item.Detail.Metadata.Label title="Drawn" text={table.draw.toString()} />
+                    <List.Item.Detail.Metadata.Label title="Lost" text={table.lost.toString()} />
+                    <List.Item.Detail.Metadata.Label title="Goals For" text={table.goalsFor.toString()} />
+                    <List.Item.Detail.Metadata.Label title="Goals Against" text={table.goalsAgainst.toString()} />
+                    <List.Item.Detail.Metadata.Label title="Goals Difference" text={table.goalDifference.toString()} />
+                  </List.Item.Detail.Metadata>
+                }
+              />
+            }
+            actions={
+              <ActionPanel>
+                <Action
+                  title={showDetails ? "Hide Details" : "Show Details"}
+                  icon={Icon.Sidebar}
+                  onAction={() => setShowDetails(!showDetails)}
+                />
+              </ActionPanel>
+            }
           />
         );
       })}
