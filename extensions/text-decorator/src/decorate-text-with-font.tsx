656bb22b3fc0c18f8f3c24ef6c6c337fbe3c7fca
koinzhang <36128970+koinzhang@users.noreply.github.com>
2022-06-20 18:26:23 +0800

    Update decorate-text-with-font.tsx

--- extensions/text-decorator/src/decorate-text-with-font.tsx
+++ extensions/text-decorator/src/decorate-text-with-font.tsx
@@ -1,13 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import utd from "unicode-text-decorator";
-import { Action, ActionPanel, Color, getPreferenceValues, Grid, Icon, Image, List, LocalStorage } from "@raycast/api";
+import { Color, getPreferenceValues, Grid, List } from "@raycast/api";
 import React, { useState } from "react";
 import { EmptyView } from "./components/empty-view";
-import { fontFamily, LocalStorageKey } from "./utils/constants";
+import { fontFamily } from "./utils/constants";
 import { getStarTextFont } from "./hooks/hooks";
-import { decorateText } from "./decorate-text";
-import { ActionOpenPreferences } from "./components/action-open-preferences";
 import { Preferences } from "./types/preferences";
 import { ActionOnFont } from "./components/action-on-font";
 
@@ -18,41 +16,46 @@ export default function DecorateTextWithFont() {
   const { starTextFont } = getStarTextFont(refresh);
 
   return itemLayout === "List" ? (
-    <List searchBarPlaceholder={"Search fonts"}>
+    <List searchBarPlaceholder={"Search fonts, *font is the default font of 𝐃𝐞𝐜𝐨𝐫𝐚𝐭𝐞 𝐓𝐞𝐱𝐭 command"}>
       <EmptyView layout={itemLayout} />
       {fontFamily.map((value) => {
         return (
           <List.Item
             key={value.value}
             icon={{ source: "list-icons/" + value.icon }}
-            title={value.title}
-            subtitle={utd.decorate("Unicode", value.value, { fallback: true })}
-            accessories={[
+            title={
               starTextFont === value.value
-                ? {
-                    icon: { source: Icon.Star, tintColor: Color.Yellow },
-                    tooltip: "Default font of 𝐃𝐞𝐜𝐨𝐫𝐚𝐭𝐞 𝐓𝐞𝐱𝐭 command",
-                  }
-                : {},
-            ]}
+                ? "*" + utd.decorate(value.title, fontFamily[0].value, { fallback: true })
+                : value.title
+            }
+            subtitle={utd.decorate("Unicode", value.value, { fallback: true })}
             actions={<ActionOnFont font={value} setRefresh={setRefresh} />}
           />
         );
       })}
     </List>
   ) : (
-    <Grid searchBarPlaceholder={"Search fonts"} itemSize={itemSize as Grid.ItemSize}>
+    <Grid
+      searchBarPlaceholder={"Search fonts, *font is the default font of 𝐃𝐞𝐜𝐨𝐫𝐚𝐭𝐞 𝐓𝐞𝐱𝐭 command"}
+      itemSize={itemSize as Grid.ItemSize}
+    >
       <EmptyView layout={itemLayout} />
       {fontFamily.map((value) => {
         return (
           <Grid.Item
             key={value.value}
             content={{
-              source: "grid-icons/" + value.icon,
-              tintColor: starTextFont === value.value ? Color.Yellow : undefined,
-              tooltip: starTextFont === value.value ? "Default font of 𝐃𝐞𝐜𝐨𝐫𝐚𝐭𝐞 𝐓𝐞𝐱𝐭 command" : undefined,
+              value: {
+                source: "grid-icons/" + value.icon,
+                tintColor: starTextFont === value.value ? Color.Yellow : undefined,
+              },
+              tooltip: starTextFont === value.value ? "Default font of 𝐃𝐞𝐜𝐨𝐫𝐚𝐭𝐞 𝐓𝐞𝐱𝐭 command" : "",
             }}
-            title={value.title}
+            title={
+              starTextFont === value.value
+                ? "*" + utd.decorate(value.title, fontFamily[0].value, { fallback: true })
+                : value.title
+            }
             actions={<ActionOnFont font={value} setRefresh={setRefresh} />}
           />
         );
