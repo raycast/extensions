ef8d1c63264e4a317114e372c85c689209d7c47a
Per Nielsen Tikær <per@raycast.com>
2022-06-21 14:23:33 +0200

    Update checkInstall.ts

--- extensions/spaces/src/checkInstall.ts
+++ extensions/spaces/src/checkInstall.ts
@@ -2,7 +2,6 @@ import { getApplications, showToast, Toast, open, showHUD } from "@raycast/api";
 
 export async function isSpacesInstalled() {
   const applications = await getApplications();
-  //   console.log(applications);
 
   return applications.some(({ bundleId }) => bundleId === "com.pradeepb28.spacesformacos");
 }
