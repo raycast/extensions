79488b05fb478516b0743438c7aa47b626a511a0
Ethan Chan <metakirby5@gmail.com>
2022-06-29 02:34:30 +0900

    Implement.

--- extensions/dotween-eases/src/index.tsx
+++ extensions/dotween-eases/src/index.tsx
@@ -1,43 +1,73 @@
-import { useState } from "react";
-import { ActionPanel, Action, Icon, Grid, Color } from "@raycast/api";
+import { ActionPanel, Action, Grid, Detail, environment } from "@raycast/api";
+import { join } from "path";
+
+const eases = [
+  "Unset",
+  "Linear",
+  "InSine",
+  "OutSine",
+  "InOutSine",
+  "InQuad",
+  "OutQuad",
+  "InOutQuad",
+  "InCubic",
+  "OutCubic",
+  "InOutCubic",
+  "InQuart",
+  "OutQuart",
+  "InOutQuart",
+  "InQuint",
+  "OutQuint",
+  "InOutQuint",
+  "InExpo",
+  "OutExpo",
+  "InOutExpo",
+  "InCirc",
+  "OutCirc",
+  "InOutCirc",
+  "InElastic",
+  "OutElastic",
+  "InOutElastic",
+  "InBack",
+  "OutBack",
+  "InOutback",
+  "InBounce",
+  "OutBounce",
+  "InOutBounce",
+  "Flash",
+  "InFlash",
+  "OutFlash",
+  "InOutFlash",
+  "INTERNAL_Zero",
+];
 
 export default function Command() {
-  const [itemSize, setItemSize] = useState<Grid.ItemSize>(Grid.ItemSize.Medium);
-  const [isLoading, setIsLoading] = useState(true);
   return (
-    <Grid
-      itemSize={itemSize}
-      inset={Grid.Inset.Large}
-      isLoading={isLoading}
-      searchBarAccessory={
-        <Grid.Dropdown
-          tooltip="Grid Item Size"
-          storeValue
-          onChange={(newValue) => {
-            setItemSize(newValue as Grid.ItemSize);
-            setIsLoading(false);
-          }}
-        >
-          <Grid.Dropdown.Item title="Large" value={Grid.ItemSize.Large} />
-          <Grid.Dropdown.Item title="Medium" value={Grid.ItemSize.Medium} />
-          <Grid.Dropdown.Item title="Small" value={Grid.ItemSize.Small} />
-        </Grid.Dropdown>
-      }
-    >
-      {!isLoading &&
-        Object.entries(Icon).map(([name, icon]) => (
+    <Grid itemSize={Grid.ItemSize.Large}>
+      {eases.map((ease) => {
+        const copyAction = <Action.CopyToClipboard content={`Ease.${ease}`} />;
+        return (
           <Grid.Item
-            key={name}
-            content={{ value: { source: icon, tintColor: Color.PrimaryText }, tooltip: name }}
-            title={name}
-            subtitle={icon}
+            key={ease}
+            content={`${ease}.gif`}
+            title={ease}
             actions={
               <ActionPanel>
-                <Action.CopyToClipboard content={icon} />
+                <Action.Push
+                  title="View"
+                  target={
+                    <Detail
+                      markdown={`![](file://${join(environment.assetsPath, ease)}.gif)`}
+                      actions={<ActionPanel>{copyAction}</ActionPanel>}
+                    />
+                  }
+                />
+                {copyAction}
               </ActionPanel>
             }
           />
-        ))}
+        );
+      })}
     </Grid>
   );
 }
