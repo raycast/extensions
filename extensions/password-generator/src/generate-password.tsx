2094bda00116bcc6203befbbbfbb49bd62ec7690
Joshua Michaels <joshuaiz@me.com>
2022-06-28 13:53:01 -0500

    numbers + chars fix

--- extensions/password-generator/src/generate-password.tsx
+++ extensions/password-generator/src/generate-password.tsx
@@ -31,6 +31,42 @@ const handleGeneratePassword = (values: Form) => {
   showToast(Toast.Style.Success, "Copied Password", generatedPassword);
 };
 export default function Command() {
+  type Password = string | null | undefined;
+  const [password, setPassword] = useState<Password>();
+  const [error, isError] = useState<boolean>(false);
+
+  let errorMessage = "";
+
+  const handleGeneratePassword = (values: any) => {
+    // console.log(values);
+
+    const length = values.lengthinput;
+    const lengthNumber = parseInt(length, 10);
+    // console.log("lengthNumber", lengthNumber);
+
+    const useNumbers = values.usenumbers;
+    const useChars = values.usechars;
+
+    if (Number.isFinite(lengthNumber) && lengthNumber > 4 && lengthNumber < 65) {
+      const generatedPassword = generatePassword(lengthNumber, useNumbers, useChars);
+
+      setPassword(generatedPassword);
+      values = {};
+      copyTextToClipboard(generatedPassword);
+      showToast(ToastStyle.Success, "Copied Password", generatedPassword);
+    } else {
+      isError(true);
+      setPassword("Error");
+      if (lengthNumber < 5) {
+        errorMessage = "Password length must be greater than 4";
+      } else if (lengthNumber > 64) {
+        errorMessage = "Password length must be less than 65";
+      } else if (!Number.isFinite(lengthNumber)) {
+        errorMessage = "Password length must be a number";
+      }
+    }
+  };
+
   return (
     <Form
       navigationTitle="Password Generator"
--- extensions/password-generator/src/helpers/helpers.ts
+++ extensions/password-generator/src/helpers/helpers.ts
@@ -1,14 +1,15 @@
 export function generatePassword(len: number, useNumbers: boolean, useChars: boolean): string {
-  let charset = "abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ";
-  if (useNumbers) {
-    charset += "23456789";
-  }
-  if (useChars) {
-    charset += "!@#$*^&%";
-  }
-  let retVal = "";
-  for (let i = 0, n = charset.length; i < len; ++i) {
-    retVal += charset.charAt(Math.floor(Math.random() * n));
-  }
-  return retVal;
+    let charset = "abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ";
+    if (useNumbers) {
+        charset += "23456789";
+    }
+    if (useChars) {
+        charset += "!@#$*^&%";
+    }
+    let retVal = "";
+    for (let i = 0, n = charset.length; i < len; ++i) {
+        // console.log('charset', charset);
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
 }
