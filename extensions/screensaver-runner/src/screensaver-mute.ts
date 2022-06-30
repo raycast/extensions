f83861d3f9e85a690b04b910508e0a97b36e6c18
Fran Grgić <grgicfran@gmail.com>
2022-06-30 14:55:04 +0200

    Surround scripts with try/catch

--- extensions/screensaver-runner/src/screensaver-mute.ts
+++ extensions/screensaver-runner/src/screensaver-mute.ts
@@ -1,7 +1,16 @@
+import { showToast } from '@raycast/api';
 import { runAppleScript } from 'run-applescript';
 import { runScreensaver, muteSystem } from './scripts';
 
 export default async () => {
-  runAppleScript(runScreensaver);
-  runAppleScript(muteSystem);
+  try {
+    await runAppleScript(runScreensaver);
+  } catch (e) {
+    showToast({ title: 'Error running screensaver' });
+  }
+  try {
+    runAppleScript(muteSystem);
+  } catch (e) {
+    showToast({ title: 'Failed to mute the system volume' });
+  }
 };
