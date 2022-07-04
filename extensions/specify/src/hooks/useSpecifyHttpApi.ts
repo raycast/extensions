87f09e8ce30209dd6a3b1cac3f8a813a8fd7d126
Allan Michay <allan.michay@gmail.com>
2022-07-04 18:03:09 +0200

    specify: display error message on wrong token

--- extensions/specify/src/hooks/useSpecifyHttpApi.ts
+++ extensions/specify/src/hooks/useSpecifyHttpApi.ts
@@ -1,8 +1,17 @@
-import { getPreferenceValues } from '@raycast/api';
+import { popToRoot, getPreferenceValues, showToast, Toast } from '@raycast/api';
 import { createSpecifyApiAxiosInstance } from '../fetchers/fetcher';
 import { Color, Bitmap, Vector } from '../types/tokens.types';
 import { Repository } from '../types/repositories.types';
 import { naturalSort } from '../utils/naturalSort.utils';
+
+const showUnauthorizedErrorToast = async () => {
+  await showToast({
+    title: 'Specify API Error',
+    message: 'Try again after checking your Specify token.',
+    style: Toast.Style.Failure,
+  });
+  await popToRoot({ clearSearchBar: true });
+};
 
 export const useSpecifyHttpApi = () => {
   const { personalAccessToken }: { personalAccessToken: string } = getPreferenceValues();
@@ -10,9 +19,14 @@ export const useSpecifyHttpApi = () => {
   const axiosInstance = createSpecifyApiAxiosInstance(personalAccessToken);
 
   const getRepositories = async () => {
-    const { data } = await axiosInstance.get<Array<Repository>>('/repositories');
+    try {
+      const { data } = await axiosInstance.get<Array<Repository>>('/repositories');
 
-    return data?.length ? naturalSort<Repository>(data, { key: 'name' }) : [];
+      return data?.length ? naturalSort<Repository>(data, { key: 'name' }) : [];
+    } catch (error) {
+      await showUnauthorizedErrorToast();
+      return [];
+    }
   };
 
   const getColors = async (namespace: string, repositoryName: string) => {
@@ -29,12 +43,18 @@ export const useSpecifyHttpApi = () => {
         },
       ],
     };
-    const { data } = await axiosInstance.post<Array<Color> | null>(
-      `/repository/${namespace}/${repositoryName}/design-tokens`,
-      payload
-    );
 
-    return data?.length ? naturalSort<Color>(data, { key: 'name' }) : [];
+    try {
+      const { data } = await axiosInstance.post<Array<Color> | null>(
+        `/repository/${namespace}/${repositoryName}/design-tokens`,
+        payload
+      );
+
+      return data?.length ? naturalSort<Color>(data, { key: 'name' }) : [];
+    } catch (error) {
+      await showUnauthorizedErrorToast();
+      return [];
+    }
   };
 
   const getBitmaps = async (namespace: string, repositoryName: string) => {
@@ -51,12 +71,17 @@ export const useSpecifyHttpApi = () => {
         },
       ],
     };
-    const { data } = await axiosInstance.post<Array<Bitmap> | null>(
-      `/repository/${namespace}/${repositoryName}/design-tokens`,
-      payload
-    );
+    try {
+      const { data } = await axiosInstance.post<Array<Bitmap> | null>(
+        `/repository/${namespace}/${repositoryName}/design-tokens`,
+        payload
+      );
 
-    return data?.length ? naturalSort<Bitmap>(data, { key: 'name' }) : [];
+      return data?.length ? naturalSort<Bitmap>(data, { key: 'name' }) : [];
+    } catch (error) {
+      await showUnauthorizedErrorToast();
+      return [];
+    }
   };
 
   const getVectors = async (namespace: string, repositoryName: string) => {
@@ -73,12 +98,17 @@ export const useSpecifyHttpApi = () => {
         },
       ],
     };
-    const { data } = await axiosInstance.post<Array<Vector> | null>(
-      `/repository/${namespace}/${repositoryName}/design-tokens`,
-      payload
-    );
+    try {
+      const { data } = await axiosInstance.post<Array<Vector> | null>(
+        `/repository/${namespace}/${repositoryName}/design-tokens`,
+        payload
+      );
 
-    return data?.length ? naturalSort<Vector>(data, { key: 'name' }) : [];
+      return data?.length ? naturalSort<Vector>(data, { key: 'name' }) : [];
+    } catch (error) {
+      await showUnauthorizedErrorToast();
+      return [];
+    }
   };
 
   return {
