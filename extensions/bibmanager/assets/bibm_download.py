1969c99a3c40dc6511591239890d4e66b9cd9b68
Aaron David Schneider <aaron.schneider@nbi.ku.dk>
2022-07-04 15:20:47 +0200

    improve error handling

--- extensions/bibmanager/assets/bibm_download.py
+++ extensions/bibmanager/assets/bibm_download.py
@@ -1,6 +1,16 @@
 import bibmanager.bib_manager as bibm
 import bibmanager.pdf_manager as pdf
+import bibmanager.utils as u
 import sys
+import signal
+
+
+TIMEOUT = 120
+
+def handler(signum, frame):
+    print("out of time")
+    raise Exception
+
 
 if __name__ == "__main__":
     key = sys.argv[1]
@@ -8,7 +18,13 @@ if __name__ == "__main__":
     bibm.load()
     bib = bibm.find(key=key)
     if bib is not None and bib.bibcode is not None:
-        pdf.fetch(bib.bibcode, replace=False)
+        signal.signal(signal.SIGALRM, handler)
+        signal.alarm(TIMEOUT)
+        try:
+            pdf.fetch(bib.bibcode, replace=False)
+        except Exception:
+            raise TimeoutError()
+        signal.alarm(0)
     else:
         raise ValueError('Could not fetch pdf')
 
@@ -16,3 +32,7 @@ if __name__ == "__main__":
     bib = bibm.find(key=key)
     if bib.pdf is None:
         raise ValueError('Could not fetch pdf')
+    else:
+        pdf = u.BM_PDF() + bib.pdf
+        sys.stdout.write("PDF: "+ pdf)
+        sys.stdout.flush()
