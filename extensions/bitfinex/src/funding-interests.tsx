0fd201b9ebab141d124215b31dc9e05e1828a9fb
Yukai Huang <yukaihuangtw@gmail.com>
2022-07-07 21:33:55 +0800

    feat: show total interests & balance

--- extensions/bitfinex/src/funding-interests.tsx
+++ extensions/bitfinex/src/funding-interests.tsx
@@ -1,4 +1,5 @@
 import { List } from "@raycast/api";
+import { useMemo } from "react";
 import Bitfinex from "./api";
 import useSWR from "swr";
 ("swr");
@@ -14,25 +15,42 @@ export default function FundingOffers() {
       }) as Promise<any[]>
   );
 
+  const totalInterest = useMemo(() => {
+    return data.reduce((total, interest) => {
+      return total + interest.amount;
+    }, 0);
+  }, [data]);
+
+  const currentBalance = useMemo(() => {
+    return Number(data?.[0]?.balance ?? 0).toFixed(2);
+  }, [data]);
+
   return (
     <List isLoading={isValidating}>
-      {data?.map((interest) => {
-        const amount = Number(interest.amount).toFixed(2);
-        const roi = Number((interest.amount / (interest.balance - interest.amount)) * 365 * 100).toFixed(2);
+      <List.Section title="Interests Summary">
+        <List.Item title="Total Interests" accessories={[{ text: `${totalInterest.toFixed(2)} USD` }]} />
+        <List.Item title="Balance" accessories={[{ text: `${currentBalance} USD` }]} />
+      </List.Section>
 
-        return (
-          <List.Item
-            key={interest.id}
-            title={`$${amount} ${interest.currency}`}
-            subtitle={`${roi}%`}
-            accessories={[
-              {
-                date: new Date(interest.mts),
-              },
-            ]}
-          />
-        );
-      })}
+      <List.Section title="Daily Interests">
+        {data?.map((interest) => {
+          const amount = Number(interest.amount).toFixed(2);
+          const roi = Number((interest.amount / (interest.balance - interest.amount)) * 365 * 100).toFixed(2);
+
+          return (
+            <List.Item
+              key={interest.id}
+              title={`$${amount} ${interest.currency}`}
+              subtitle={`${roi}%`}
+              accessories={[
+                {
+                  date: new Date(interest.mts),
+                },
+              ]}
+            />
+          );
+        })}
+      </List.Section>
     </List>
   );
 }
