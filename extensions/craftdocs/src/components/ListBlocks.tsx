3555aa2296e9f8afe01bdae54ced2abbf94674b1
Vitaliy Kudryk <contact@bgnfu7re.me>
2022-07-08 19:47:27 +0300

    update icons 💄

--- extensions/craftdocs/src/components/ListBlocks.tsx
+++ extensions/craftdocs/src/components/ListBlocks.tsx
@@ -1,5 +1,5 @@
-import { Action, ActionPanel, List } from "@raycast/api";
-import { Block } from "../hooks/useSearch";
+import {Action, ActionPanel, Color, List} from "@raycast/api";
+import {Block} from "../hooks/useSearch";
 import Config from "../Config";
 import CreateDocumentItem from "./CreateDocumentItem";
 
@@ -31,7 +31,11 @@ export default function ListBlocks(params: ListBlocksParams) {
 
 const BlockItem = ({ block }: { block: Block }) => (
   <List.Item
-    icon={block.entityType === "document" ? "📄" : "📑"}
+    icon={
+      block.entityType === "document"
+        ? {source: "doc_richtext.png", tintColor: Color.PrimaryText}
+        : {source: "doc_on_doc.png", tintColor: Color.PrimaryText}
+    }
     subtitle={block.content}
     title={block.documentName || block.content}
     actions={
