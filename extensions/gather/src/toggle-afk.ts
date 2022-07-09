d894fc10bd99bcfd2e6760c16e8cbde5f47eec8a
Per Nielsen Tikær <per@raycast.com>
2022-07-09 07:17:17 +0200

    Added installation check

--- extensions/gather/src/toggle-afk.ts
+++ extensions/gather/src/toggle-afk.ts
@@ -1,16 +1,36 @@
-import { closeMainWindow } from "@raycast/api";
+import { closeMainWindow, open, showToast, Toast } from "@raycast/api";
 import { runAppleScript } from "run-applescript";
+import { gatherInstalled } from "./utils";
 
 export default async () => {
-  await closeMainWindow();
-  await runAppleScript('activate application "Gather"');
+  const installed = await gatherInstalled();
 
-  // Toggle microphone
-  await runAppleScript('tell application "System Events" to key code 9 using {shift down, command down}');
+  if (!installed) {
+    const options: Toast.Options = {
+      style: Toast.Style.Failure,
+      title: "Gather is not installed.",
+      message: "Install it from: https://www.gather.town",
+      primaryAction: {
+        title: "Go to https://www.gather.town",
+        onAction: (toast) => {
+          open("https://www.gather.town");
+          toast.hide();
+        },
+      },
+    };
 
-  // Toggle camera
-  await runAppleScript('tell application "System Events" to key code 0 using {shift down, command down}');
+    await showToast(options);
+  } else {
+    await closeMainWindow();
+    await runAppleScript('activate application "Gather"');
 
-  // Toggle availability status
-  await runAppleScript('tell application "System Events" to key code 32 using command down');
+    // Toggle microphone
+    await runAppleScript('tell application "System Events" to key code 9 using {shift down, command down}');
+
+    // Toggle camera
+    await runAppleScript('tell application "System Events" to key code 0 using {shift down, command down}');
+
+    // Toggle availability status
+    await runAppleScript('tell application "System Events" to key code 32 using command down');
+  }
 };
