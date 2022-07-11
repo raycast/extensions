acd88e52ad0db0808bb5f33761c07d8c36698e8f
mathieudutour <mathieu@dutour.me>
2022-07-11 16:32:01 +0200

    use local file

--- extensions/apple-notes/src/errors.tsx
+++ extensions/apple-notes/src/errors.tsx
@@ -1,4 +1,6 @@
-import { ActionPanel, Detail, Icon, Action } from "@raycast/api";
+import { ActionPanel, Detail, Icon, Action, environment } from "@raycast/api";
+import path from "path";
+import fileUrl from "file-url";
 
 export class PermissionError extends Error {
   constructor(message: string) {
@@ -14,7 +16,7 @@ export const isPermissionError = (error: unknown) => {
 // @TODO: This screen should be handled by Raycast itself (https://github.com/raycast/extensions/issues/101)
 const permissionErrorMarkdown = `## Raycast needs full disk access in order to display your Apple Notes.
 
-![Full Disk Access Preferences Pane](https://i.imgur.com/3SAUwrx.png)
+![Full Disk Access Preferences Pane](${fileUrl(path.join(environment.assetsPath, "full-disk-access.png"))})
 
 1. Open the **Security & Privacy** Preferences pane and select the **Privacy** tab
 2. Select **Full Disk Access** from the list of services
