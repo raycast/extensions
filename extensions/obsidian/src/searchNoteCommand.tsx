cc2cba2919bf3b8a2949046535e25401e815e942
marcjulianschwarz <info@marc-julian.de>
2022-07-12 12:08:01 +0200

    ran prettier

--- extensions/obsidian/src/searchNoteCommand.tsx
+++ extensions/obsidian/src/searchNoteCommand.tsx
@@ -15,9 +15,11 @@ export default function Command() {
   } else if (vaults.length === 0) {
     return <NoVaultFoundMessage />;
   } else if (vaults.length > 1) {
-    return <VaultSelection vaults={vaults} target={(vault: Vault) => <NoteListObsidian vault={vault} showTitle={true}/>} />;
+    return (
+      <VaultSelection vaults={vaults} target={(vault: Vault) => <NoteListObsidian vault={vault} showTitle={true} />} />
+    );
   } else if (vaults.length == 1) {
-    return <NoteListObsidian vault={vaults[0]} showTitle={false}/>;
+    return <NoteListObsidian vault={vaults[0]} showTitle={false} />;
   } else {
     noVaultPathsToast();
   }
