711c5d9d36f3ed872fc29616945d33b137802c3f
Andres Morelos <andresmorelos123@gmail.com>
2022-07-12 11:18:41 -0500

    Updating Numi Icon & Checking Api Status.

--- extensions/numi/src/index.tsx
+++ extensions/numi/src/index.tsx
@@ -1,4 +1,4 @@
-import { List, ActionPanel, Action, showToast, Toast } from "@raycast/api";
+import { List, ActionPanel, Action, showToast, Toast, Detail } from "@raycast/api";
 import { useState, useEffect } from "react";
 import { query } from "./services/requests";
 
@@ -9,14 +9,12 @@ interface State {
 }
 
 export default function Command() {
-  const [state, setState] = useState<State>({ isLoading: false });
+  const [apiStatus, setApiStatus] = useState<boolean>(false);
+  const [state, setState] = useState<State>({ isLoading: true });
   const [toast, setToast] = useState<Toast | undefined>(undefined);
-  useEffect(() => {
-    if (!state.isLoading) {
-      return;
-    }
+  const INTERVAL_TIME = 5000;
 
-    const q = state.query?.trim() ?? "";
+  const queryOnNumi = (q: string | undefined) => {
     query(q)
       .then((results) => {
         if (toast) {
@@ -25,6 +23,10 @@ export default function Command() {
         setState((oldState) => ({ ...oldState, results, isLoading: false }));
       })
       .catch((err) => {
+        if (err.message.includes("ECONNREFUSED")) {
+          setApiStatus(false);
+        }
+
         showToast({
           style: Toast.Style.Failure,
           title: err.message,
@@ -32,32 +34,67 @@ export default function Command() {
           .then((toast) => setToast)
           .catch((err) => console.error("Error Creating Toast"));
       });
+  };
+
+  const checkApiStatus = () => {
+    query("1")
+      .then(() => setApiStatus(true))
+      .catch((err) => {
+        if (err.message.includes("ECONNREFUSED")) {
+          setApiStatus(false);
+        }
+      });
+  };
+
+  useEffect(() => {
+    if (!apiStatus) {
+      checkApiStatus();
+    }
+    const interval = setInterval(() => {
+      if (!apiStatus) {
+        checkApiStatus();
+      }
+    }, INTERVAL_TIME);
+
+    return clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    const q = state.query?.trim() ?? "";
+    if (!state.isLoading) {
+      return;
+    }
+    queryOnNumi(q);
   }, [state]);
 
-  return (
-    <List
-      searchBarPlaceholder="Enter text to query"
-      onSearchTextChange={(searchValue) => {
-        setState((oldState) => ({ ...oldState, query: searchValue, isLoading: true }));
-      }}
-      isLoading={state.isLoading}
-    >
-      {state.results &&
-        state.results.map((result, index) => {
-          if (result) {
-            return (
-              <List.Item
-                key={index}
-                title={result}
-                actions={
-                  <ActionPanel>
-                    <Action.CopyToClipboard title="Copy" content={result} />
-                  </ActionPanel>
-                }
-              />
-            );
-          }
-        })}
-    </List>
-  );
+  if (apiStatus) {
+    return (
+      <List
+        searchBarPlaceholder="Enter text to query"
+        onSearchTextChange={(searchValue) => {
+          setState((oldState) => ({ ...oldState, query: searchValue, isLoading: true }));
+        }}
+        isLoading={state.isLoading}
+      >
+        {state.results &&
+          state.results.map((result, index) => {
+            if (result) {
+              return (
+                <List.Item
+                  key={index}
+                  title={result}
+                  actions={
+                    <ActionPanel>
+                      <Action.CopyToClipboard title="Copy" content={result} />
+                    </ActionPanel>
+                  }
+                />
+              );
+            }
+          })}
+      </List>
+    );
+  } else {
+    return <Detail markdown="# Numi is not running" />;
+  }
 }
