54dcf24619cfb67dd70581f3011a546e22df2cc2
Navtoj Chahal <github@navtoj.com>
2022-07-13 08:39:48 -0700

    Added "Tailwind CSS Colors" extension.

---
+++ extensions/tailwindcss-colors/src/backup.tsx
@@ -0,0 +1,43 @@
+import { Action, ActionPanel, Color, Grid, Icon } from "@raycast/api";
+import { useState } from "react";
+
+export default function Command() {
+	const [itemSize, setItemSize] = useState<Grid.ItemSize>(Grid.ItemSize.Small);
+	const [isLoading, setIsLoading] = useState(true);
+	return (
+		<Grid
+			itemSize={itemSize}
+			inset={Grid.Inset.Large}
+			isLoading={isLoading}
+			searchBarAccessory={
+				<Grid.Dropdown
+					tooltip="Grid Item Size"
+					storeValue
+					onChange={(newValue) => {
+						setItemSize(newValue as Grid.ItemSize);
+						setIsLoading(false);
+					}}
+				>
+					<Grid.Dropdown.Item title="Large" value={Grid.ItemSize.Large} />
+					<Grid.Dropdown.Item title="Medium" value={Grid.ItemSize.Medium} />
+					<Grid.Dropdown.Item title="Small" value={Grid.ItemSize.Small} />
+				</Grid.Dropdown>
+			}
+		>
+			{!isLoading &&
+				Object.entries(Icon).map(([name, icon]) => (
+					<Grid.Item
+						key={name}
+						content={{ value: { source: icon, tintColor: Color.PrimaryText }, tooltip: name }}
+						title={name}
+						subtitle={icon}
+						actions={
+							<ActionPanel>
+								<Action.CopyToClipboard content={icon} />
+							</ActionPanel>
+						}
+					/>
+				))}
+		</Grid>
+	);
+}
