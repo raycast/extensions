625e1bf1450dfba54711eca7ab0b8511cc35f4ab
Per Nielsen Tikær <per@raycast.com>
2022-07-14 05:36:37 +0200

    Prettier

--- extensions/passphrase-generator/src/ListItem.tsx
+++ extensions/passphrase-generator/src/ListItem.tsx
@@ -1,25 +1,29 @@
-import { Action, ActionPanel, List } from '@raycast/api'
-import type { PasswordGenerator } from './utils/types'
-import { getStrengthIcon } from './utils/getStrengthIcon'
+import { Action, ActionPanel, List } from "@raycast/api";
+import type { PasswordGenerator } from "./utils/types";
+import { getStrengthIcon } from "./utils/getStrengthIcon";
 
 type Props = {
-	generator: PasswordGenerator
-	generatePasswords: () => void
-}
+  generator: PasswordGenerator;
+  generatePasswords: () => void;
+};
 
 const ListItem = ({ generator, generatePasswords }: Props): JSX.Element => (
-	<List.Item
-		key={generator.id}
-		title={generator.password}
-		subtitle={`Length: ${generator.password.length}`}
-		accessories={[{ text: generator.title }]}
-		icon={getStrengthIcon(generator.strength)}
-		actions={
-			<ActionPanel>
-				<Action.CopyToClipboard title="Copy password" content={generator.password} onCopy={generatePasswords} />
-			</ActionPanel>
-		}
-	/>
-)
+  <List.Item
+    key={generator.id}
+    title={generator.password}
+    subtitle={`Length: ${generator.password.length}`}
+    accessories={[{ text: generator.title }]}
+    icon={getStrengthIcon(generator.strength)}
+    actions={
+      <ActionPanel>
+        <Action.CopyToClipboard
+          title="Copy password"
+          content={generator.password}
+          onCopy={generatePasswords}
+        />
+      </ActionPanel>
+    }
+  />
+);
 
-export default ListItem
+export default ListItem;
