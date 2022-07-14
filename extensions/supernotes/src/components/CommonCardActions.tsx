3f3b0952c8bbe6b42266c3dd34dcb0fa8ce771c1
acnebs <connor.cn@gmail.com>
2022-07-15 01:22:15 +0800

    Pop to root if junking a card from the <CardDetail /> view

--- extensions/supernotes/src/components/CommonCardActions.tsx
+++ extensions/supernotes/src/components/CommonCardActions.tsx
@@ -1,4 +1,4 @@
-import { Action, Icon } from "@raycast/api";
+import { Action, Icon, popToRoot } from "@raycast/api";
 
 import useJunk from "hooks/useJunk";
 import { useRecentRemover } from "hooks/useRecent";
@@ -11,21 +11,23 @@ interface CommonCardActionsProps {
 
 const CommonCardActions = ({ card, removeFromList }: CommonCardActionsProps) => {
   const { found, removeFromRecents } = useRecentRemover(card);
-  const removeFromRecentsAndList = () => {
+  const remove = () => {
     removeFromRecents();
-    removeFromList && removeFromList(card.data.id);
+    if (removeFromList) {
+      removeFromList(card.data.id);
+    } else {
+      popToRoot();
+    }
   };
 
-  const { junk } = useJunk(removeFromRecentsAndList);
+  const { junk } = useJunk(remove);
 
   return (
     <>
       <Action.CopyToClipboard title="Copy Markdown" icon={Icon.Clipboard} content={card.data.markup} />
       <Action.CopyToClipboard title="Copy HTML" icon={Icon.Clipboard} content={card.data.html} />
       <Action title="Junk Card" icon={Icon.Trash} onAction={() => junk(card.data.id)} />
-      {found && (
-        <Action title="Remove from Recently Viewed" icon={Icon.XmarkCircle} onAction={removeFromRecentsAndList} />
-      )}
+      {found && <Action title="Remove from Recently Viewed" icon={Icon.XmarkCircle} onAction={remove} />}
     </>
   );
 };
