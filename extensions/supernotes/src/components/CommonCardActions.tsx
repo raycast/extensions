a8c8f48247c37baf4b0745df586d3e6ba3cad6ee
pomdtr <achille.lacoin@dailymotion.com>
2022-07-15 14:23:25 +0200

    add shortcuts to common card actions

--- extensions/supernotes/src/components/CommonCardActions.tsx
+++ extensions/supernotes/src/components/CommonCardActions.tsx
@@ -25,10 +25,10 @@ const CommonCardActions = ({ card, removeFromList }: CommonCardActionsProps) => 
 
   return (
     <>
-      <Action.CopyToClipboard title="Copy Markdown" icon={Icon.Clipboard} content={card.data.markup} />
-      <Action.CopyToClipboard title="Copy HTML" icon={Icon.Clipboard} content={card.data.html} />
       <Action.OpenInBrowser url={`${SUPERNOTES_VIEW_URL}/${card.data.id}/`}/>
-      <Action title="Junk Card" icon={Icon.Trash} onAction={() => junk(card.data.id)} />
+      <Action.CopyToClipboard title="Copy Markdown" icon={Icon.Clipboard} content={card.data.markup} shortcut={{modifiers: ["cmd"], key: "."}}/>
+      <Action.CopyToClipboard title="Copy HTML" icon={Icon.Clipboard} content={card.data.html} shortcut={{modifiers: ["cmd", "shift"], key: "."}}/>
+      <Action title="Junk Card" icon={Icon.Trash} onAction={() => junk(card.data.id)} shortcut={{modifiers: ["ctrl"], key: "x"}}/>
       {found && <Action title="Remove from Recently Viewed" icon={Icon.XmarkCircle} onAction={remove} />}
     </>
   );
