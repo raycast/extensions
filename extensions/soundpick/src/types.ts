b282e54f8d877b923f60156bdc3731ad0755c5fb
Robin Saleh-Jan <robinsalehjan@users.noreply.github.com>
2022-07-16 15:32:25 +0200

    Add `AppleScriptParser` interface to `types.ts`

--- extensions/soundpick/src/types.ts
+++ extensions/soundpick/src/types.ts
@@ -1,5 +1,10 @@
 export type AppleScriptResponse = string
 
+export interface AppleScriptParser {
+    parse(input: AppleScriptResponse): Array<SoundOutputDevice>
+    parseDeviceNames(input: AppleScriptResponse): Array<string>
+    parseDeviceConnections(input: AppleScriptResponse): Array<boolean>
+}
 
 export interface SoundOutputDevice {
     name: string
