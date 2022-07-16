2f957d12bdf9fc0c8ed124d1f9aaa48e1f04af74
Robin Saleh-Jan <robinsalehjan@users.noreply.github.com>
2022-07-16 12:31:56 +0200

    Implement `AppleScriptParser` class

---
+++ extensions/soundpick/src/apple-script-parser.ts
@@ -0,0 +1,74 @@
+import { stringToBool } from "./utils";
+import { AppleScriptResponse, SoundOutputDevice } from "./types";
+
+export default class AppleScriptParser implements AppleScriptParser {
+  parse(input: AppleScriptResponse): Array<SoundOutputDevice> {
+    const deviceNames = this.parseDeviceNames(input)
+    const deviceConnections = this.parseDeviceConnections(input)
+
+    let response = Array<SoundOutputDevice>()
+
+    if (deviceNames.length != deviceConnections.length) {
+      console.log(`[ERROR]: the number of devices ${deviceNames.length} is not equal to the number of connections ${deviceConnections.length}`)
+      return response
+    }
+
+    for (let index = 0; index < deviceNames.length; index++) {
+      let deviceName = deviceNames[index]
+      let deviceConnection = deviceConnections[index]
+      response.push(<SoundOutputDevice>{name: deviceName, isConnected: deviceConnection})
+    }
+
+    console.log(`[INFO]: Returning response: ${JSON.stringify(response)}`)
+    return response
+  }
+  
+  parseDeviceNames(input: AppleScriptResponse): Array<string> {
+    const splittedInput = this.splitInput(input)
+    
+    const deviceNames = splittedInput.filter(
+      (value: string, index: number) => {
+        const isDeviceNameIndex = index % 2 === 0
+        const sanitizedValue = value.trim()
+        if (isDeviceNameIndex) {
+          return sanitizedValue
+        }
+      })
+      
+      console.log(`[INFO]: Parsed device names: ${deviceNames} in response`)
+      return deviceNames
+    }
+
+    parseDeviceConnections(input: AppleScriptResponse): Array<boolean> {
+      const splittedInput = this.splitInput(input)
+
+      const deviceConnections: Array<boolean> = splittedInput
+      .filter((_, index) => { 
+        const isDeviceConnectionIndex = index % 2 === 1 
+        return isDeviceConnectionIndex
+      })
+      .map((value: string) => {
+        const sanitizedValue = value.trim()
+        return stringToBool(sanitizedValue) 
+      })
+      
+      console.log(`[INFO]: Parsed connection states: ${deviceConnections} in response`)
+      return deviceConnections
+    }
+
+    splitInput(input: AppleScriptResponse): Array<string> {
+      // Input is a comma-separated string
+      // for example: "Macbook Pro Speakers, true, AirPods Pro, false"
+      // the first value is name of the device 
+      // second value is connection status of that device
+      const splittedInput = input.split(",")
+      
+      if (splittedInput.length % 2 != 0) {
+        console.log(`[ERROR]: The number of elements in the apple script response is not equal to an even number : ${splittedInput.length}`)
+        console.log(`[ERROR]: Return a empty array`)
+        return Array<string>();
+      }
+      
+      return splittedInput
+    }
+}
