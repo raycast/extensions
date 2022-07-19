d5e5e45c87851a8cb612f7451fc9f2bc3dde3968
Aayush <43297314+Aayush9029@users.noreply.github.com>
2022-07-19 14:26:50 -0400

    man lint

--- extensions/xqc/src/interface/vodModel.ts
+++ extensions/xqc/src/interface/vodModel.ts
@@ -1,43 +1,42 @@
 export interface VODModel {
-    total: number;
-    limit: number;
-    skip: number;
-    data: SingleVod[];
+  total: number;
+  limit: number;
+  skip: number;
+  data: SingleVod[];
 }
 
 export interface SingleVod {
-    id: string;
-    chapters: Chapter[];
-    title: string;
-    duration: string;
-    date: string;
-    thumbnail_url: string;
-    youtube: Youtube[];
-    stream_id: string;
-    drive: Drive[];
-    createdAt: Date;
-    updatedAt: Date;
+  id: string;
+  chapters: Chapter[];
+  title: string;
+  duration: string;
+  date: string;
+  thumbnail_url: string;
+  youtube: Youtube[];
+  stream_id: string;
+  drive: Drive[];
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 export interface Chapter {
-    gameId: string;
-    name: string;
-    image: string;
-    duration: string;
-    start: number;
-    end: number;
+  gameId: string;
+  name: string;
+  image: string;
+  duration: string;
+  start: number;
+  end: number;
 }
 
 export interface Drive {
-    id: string;
-    type: string;
+  id: string;
+  type: string;
 }
 
-
 export interface Youtube {
-    id: string;
-    type: string;
-    duration: number;
-    part: number;
-    thumbnail_url: string;
+  id: string;
+  type: string;
+  duration: number;
+  part: number;
+  thumbnail_url: string;
 }
