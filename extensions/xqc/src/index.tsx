bfa42622cd5a6a0bfe10076fdabdccae20da9d6e
Aayush <43297314+Aayush9029@users.noreply.github.com>
2022-07-19 14:26:35 -0400

    man lint

--- extensions/xqc/src/index.tsx
+++ extensions/xqc/src/index.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import { ActionPanel, Action, Detail, List, Color } from "@raycast/api";
-import { VODModel, SingleVod } from "./interface/vodModel"
+import { VODModel, SingleVod } from "./interface/vodModel";
 
-import fetch from "node-fetch"
+import fetch from "node-fetch";
 
 export default function Command() {
   const [vods, setVods] = useState<SingleVod[]>([]);
@@ -10,9 +10,9 @@ export default function Command() {
 
   const fetchVods = async () => {
     const response = await fetch("https://api.xqc.wtf/vods?$limit=20&$sort[createdAt]=-1");
-    const data = await response.json() as VODModel;
+    const data = (await response.json()) as VODModel;
     setVods(data.data);
-  }
+  };
 
   useEffect(() => {
     setIsLoading(true);
@@ -21,59 +21,54 @@ export default function Command() {
   }, []);
 
   return (
-    <List
-      enableFiltering={false}
-      navigationTitle="Search Vods"
-    >
-      {
-        vods.map((vod) => (
-          <List.Item key={vod.id}
-            id={vod.id}
-            icon={vod.thumbnail_url}
-            title={vod.title}
-            subtitle={vod.date}
-            actions={
-              <ActionPanel>
-                <Action.Push title="Details" target={<VodDetail vod={vod} />} />
-              </ActionPanel>
-            }
-          ></List.Item>
-        ))
-      }
-
+    <List enableFiltering={false} navigationTitle="Search Vods">
+      {vods.map((vod) => (
+        <List.Item
+          key={vod.id}
+          id={vod.id}
+          icon={vod.thumbnail_url}
+          title={vod.title}
+          subtitle={vod.date}
+          actions={
+            <ActionPanel>
+              <Action.Push title="Details" target={<VodDetail vod={vod} />} />
+            </ActionPanel>
+          }
+        ></List.Item>
+      ))}
     </List>
   );
 }
-
 
 function VodDetail(props: { vod: SingleVod }) {
   return (
     <Detail
       key={props.vod.id}
       markdown={`
-      ${props.vod.youtube.map((youtube) => (`
+      ${props.vod.youtube
+        .map(
+          (youtube) => `
 ![](${youtube.thumbnail_url})
 
         Part ${youtube.part}
         Duration: ${Math.round(youtube.duration / 60 / 60)} hours
         Link: https://www.youtube.com/watch?v=${youtube.id}
 
-        `)).join("\n")}
+        `
+        )
+        .join("\n")}
       `}
       navigationTitle={props.vod.title}
       metadata={
         <Detail.Metadata>
-          {
-            props.vod.youtube.map((youtube) => (
-              <Detail.Metadata.Link
-                title={`Youtube Link: ${Math.round(youtube.duration / 60 / 60)} hours`}
-                target={`https://www.youtube.com/watch?v=${youtube.id}`}
-                text={`Part ${youtube.part}`}
-                key={youtube.id}
-              />
-
-            ))
-          }
+          {props.vod.youtube.map((youtube) => (
+            <Detail.Metadata.Link
+              title={`Youtube Link: ${Math.round(youtube.duration / 60 / 60)} hours`}
+              target={`https://www.youtube.com/watch?v=${youtube.id}`}
+              text={`Part ${youtube.part}`}
+              key={youtube.id}
+            />
+          ))}
         </Detail.Metadata>
       }
     />
