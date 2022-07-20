ad9d2ab2c7bd33e382637c210ba88bd5a0e73588
Vitaliy Kudryk <contact@bgnfu7re.me>
2022-07-20 22:24:18 +0300

    form with some validations ✨

--- extensions/recurly/src/components/TenantConfigurationForm.tsx
+++ extensions/recurly/src/components/TenantConfigurationForm.tsx
@@ -1,5 +1,6 @@
 import { Action, ActionPanel, Form } from "@raycast/api";
 import { TenantConfiguration } from "../TenantConfiguration";
+import {useState} from "react";
 
 export type TenantConfigurationFormProps = {
   tenant?: TenantConfiguration;
@@ -11,6 +12,14 @@ const emptyTenant: TenantConfiguration = { name: "", subdomain: "", apiKey: "" }
 export default function TenantConfigurationForm({ tenant = emptyTenant, onSubmit }: TenantConfigurationFormProps) {
   const { name, subdomain, apiKey } = tenant;
 
+  const [nameError, setNameError] = useState<string | undefined>();
+  const [subdomainError, setSubdomainError] = useState<string | undefined>();
+  const [apiKeyError, setApiKeyError] = useState<string | undefined>();
+
+  const unsetNameError = () => setNameError(undefined);
+  const unsetSubdomainError = () => setSubdomainError(undefined);
+  const unsetApiKeyError = () => setApiKeyError(undefined);
+
   return (
     <Form
       actions={
@@ -20,9 +29,30 @@ export default function TenantConfigurationForm({ tenant = emptyTenant, onSubmit
       }
     >
       <Form.Description text="Add a new tenant here. The name should be unique." />
-      <Form.TextField id="name" title="Name" defaultValue={name} />
-      <Form.TextField id="subdomain" title="Subdomain" defaultValue={subdomain} />
-      <Form.PasswordField id="apiKey" title="API Key" defaultValue={apiKey} />
+      <Form.TextField
+        id="name"
+        title="Name"
+        error={nameError}
+        onChange={unsetNameError}
+        onBlur={event => event.target.value?.length === 0 && setNameError("Name is required.")}
+        defaultValue={name}
+      />
+      <Form.TextField
+        id="subdomain"
+        title="Subdomain"
+        error={subdomainError}
+        onChange={unsetSubdomainError}
+        onBlur={event => event.target.value?.length === 0 && setSubdomainError("Subdomain is required.")}
+        defaultValue={subdomain}
+      />
+      <Form.PasswordField
+        id="apiKey"
+        title="API Key"
+        error={apiKeyError}
+        onChange={unsetApiKeyError}
+        onBlur={event => event.target.value?.length === 0 && setApiKeyError("API Key is required.")}
+        defaultValue={apiKey}
+      />
     </Form>
   );
 }
