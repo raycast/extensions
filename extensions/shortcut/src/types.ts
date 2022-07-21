0975988c4c265cc25836cb5895b7ae7da118e759
Yukai Huang <yukaihuangtw@gmail.com>
2022-07-21 19:30:57 +0800

    style: fix linting

--- extensions/shortcut/src/types.ts
+++ extensions/shortcut/src/types.ts
@@ -1,3 +1,3 @@
 export type Preference = {
   apiToken: string;
-}
+};
--- extensions/shortcut/src/utils/shortcut.ts
+++ extensions/shortcut/src/utils/shortcut.ts
@@ -1,7 +1,6 @@
-import { ShortcutClient } from '@useshortcut/client';
-import { getPreferenceValues } from '@raycast/api'
-import { Preference } from '../types';
-
+import { ShortcutClient } from "@useshortcut/client";
+import { getPreferenceValues } from "@raycast/api";
+import { Preference } from "../types";
 
 const preference = getPreferenceValues<Preference>();
 
