9a5cb017753c296414ea6c204aaa82e87c549848
Vitaliy Kudryk <contact@bgnfu7re.me>
2022-07-23 12:21:53 +0300

    pullSearch 🔊

--- extensions/github-pull-notifications/src/integration/pullSearch.ts
+++ extensions/github-pull-notifications/src/integration/pullSearch.ts
@@ -3,8 +3,8 @@ import octokit from "./octokit";
 import { mapPullSearchResultToShort } from "./mappers";
 
 export const pullSearch = (query: string): Promise<PullSearchResultShort[]> => Promise.resolve()
-  .then(() => console.debug(`pullSearch for ${query}`))
+  .then(() => console.debug(`pullSearch: ${query}`))
   .then(() => octokit.rest.search.issuesAndPullRequests({ per_page: 100, q: `is:pr ${query}` }))
   .then(res => res.data.items || [])
   .then(items => items.map(mapPullSearchResultToShort))
-  .finally(() => console.debug(`pullSearch for ${query} done`));
+  .finally(() => console.debug(`pullSearch: ${query} done`));
