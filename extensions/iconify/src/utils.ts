4b3c7bd77c4b90444584d982a55152c49ab07907
Kid <44045911+kidonng@users.noreply.github.com>
2022-07-23 06:52:30 +0000

    Lint

--- extensions/iconify/src/utils.ts
+++ extensions/iconify/src/utils.ts
@@ -3,7 +3,9 @@ function toSvg(path: string, width: number, height: number): string {
 }
 
 function toBase64(svg: string): string {
-  return `data:image/svg+xml;base64,${Buffer.from(svg, 'utf8').toString('base64')}`;
+  return `data:image/svg+xml;base64,${Buffer.from(svg, 'utf8').toString(
+    'base64',
+  )}`;
 }
 
 export { toSvg, toBase64 };
