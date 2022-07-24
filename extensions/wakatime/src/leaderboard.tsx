e7629fab85c78a9e35928aef1b1d313f04550c01
Ademola Adedeji <a.mola.dev@gmail.com>
2022-07-24 19:21:50 +0100

    ✨ feat(leaderboard): add dropdown to List for page numbers

--- extensions/wakatime/src/leaderboard.tsx
+++ extensions/wakatime/src/leaderboard.tsx
@@ -5,11 +5,26 @@ import { LeaderBoardItem } from "./components";
 import { useState } from "react";
 
 export default function Command({ id }: { id?: string }) {
-  const { data, isLoading } = useLeaderBoard(id);
+  const [page, setPage] = useState<string>();
   const [showDetail, setShowDetail] = useState(false);
 
+  const { data, isLoading } = useLeaderBoard({ id, page: page == undefined ? undefined : +page });
+
   return (
-    <List isLoading={isLoading} isShowingDetail={showDetail} selectedItemId={data?.current_user.user.id}>
+    <List
+      isLoading={isLoading}
+      isShowingDetail={showDetail}
+      selectedItemId={data?.current_user.user.id}
+      searchBarAccessory={
+        data && (
+          <List.Dropdown tooltip="Page" value={page ?? String(data.current_user.page)} onChange={setPage}>
+            {Array.from({ length: data.total_pages }).map((_, idx) => (
+              <List.Dropdown.Item key={idx} title={`Page ${idx + 1}`} value={String(idx + 1)} />
+            ))}
+          </List.Dropdown>
+        )
+      }
+    >
       {data?.data.map((item, idx) => (
         <LeaderBoardItem key={idx} {...item} {...{ showDetail, setShowDetail }} />
       ))}
