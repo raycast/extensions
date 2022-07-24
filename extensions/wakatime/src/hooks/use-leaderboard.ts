28c46d80edba148dfbf121d1daae74e35686ac7e
Ademola Adedeji <a.mola.dev@gmail.com>
2022-07-24 20:45:12 +0100

    ♻️ refactor: use `useBase` hook to write `useLeaderboard`

--- extensions/wakatime/src/hooks/use-leaderboard.ts
+++ extensions/wakatime/src/hooks/use-leaderboard.ts
@@ -1,37 +1,18 @@
-import { useEffect, useState } from "react";
-import { showToast, Toast } from "@raycast/api";
-
+import { useBase } from "./base";
 import { getLeaderBoard } from "../utils";
 
 export function useLeaderBoard({ id, page }: { id?: string; page?: number } = {}) {
-  const [data, setData] = useState<WakaTime.LeaderBoard>();
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const result = useBase({
+    handler: () => getLeaderBoard({ id, page }),
+    toasts: {
+      loading: { title: "Loading Leaderboard" },
+      success: { title: "Done!!" },
+      error: (err) => ({
+        title: "Failed fetching data!",
+        message: err.message,
+      }),
+    },
+  });
 
-  useEffect(() => {
-    async function getData() {
-      setData(undefined);
-      setIsLoading(true);
-      const toast = await showToast(Toast.Style.Animated, "Loading Leaderboard");
-
-      try {
-        const data = await getLeaderBoard({ id, page });
-
-        if (!data.ok) throw new Error(data.error);
-        setData(data);
-
-        toast.style = Toast.Style.Success;
-        toast.title = "Success";
-      } catch (error) {
-        toast.style = Toast.Style.Failure;
-        toast.title = "Error Loading Leaderboard";
-        toast.message = (error as Record<string, string>).message;
-      }
-
-      setIsLoading(false);
-    }
-
-    getData();
-  }, [id, page]);
-
-  return { data, isLoading };
+  return result;
 }
