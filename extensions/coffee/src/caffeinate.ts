b29b1d1d4602ffba5aba0e58a9a7a8b4476a9510
GastroGeek <gastrogeek@gmail.com>
2022-07-26 16:46:08 +0100

    save a line

--- extensions/coffee/src/caffeinate.ts
+++ extensions/coffee/src/caffeinate.ts
@@ -2,13 +2,11 @@ import { showHUD } from "@raycast/api";
 import { runAppleScript } from "run-applescript";
 
 const Caffeinate = async (args?: string) => {
-  const validStringCaffeinateArgs = typeof args === "string";
-
   try {
     await runAppleScript(`do shell script "pgrep caffeinate"`);
     await showHUD("Your Mac is already caffeinated");
   } catch (_) {
-    runAppleScript(`do shell script "caffeinate -di${validStringCaffeinateArgs ? ` ${args}` : ""}"`);
+    runAppleScript(`do shell script "caffeinate -di${typeof args === "string" ? ` ${args}` : ""}"`);
     await showHUD("Your Mac is caffeinated");
   }
 };
