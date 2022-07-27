7a8be04dc6489c07513dba1a19a150eed6dcb907
tisfeng <tisfeng@gmail.com>
2022-07-27 22:05:44 +0800

    style: clean up comments

--- extensions/easydict/src/google.ts
+++ extensions/easydict/src/google.ts
@@ -2,7 +2,7 @@
  * @author: tisfeng
  * @createTime: 2022-07-22 23:27
  * @lastEditor: tisfeng
- * @lastEditTime: 2022-07-24 16:22
+ * @lastEditTime: 2022-07-27 22:05
  * @fileName: google.ts
  *
  * Copyright (c) 2022 by tisfeng, All Rights Reserved.
@@ -122,37 +122,6 @@ async function googleCrawlerTranslate(
       return Promise.reject(errorInfo);
     });
 }
-
-/**
-<body>
-    <div class="root-container">
-        <div class="header">
-            <div class="logo-image"></div>
-            <div class="logo-text">翻译</div>
-        </div>
-        <div class="languages-container">
-            <div class="sl-and-tl"><a href="./m?sl=auto&amp;tl=zh-CN&amp;q=good&amp;mui=sl&amp;hl=zh-CN">检测语言</a> → <a
-                    href="./m?sl=auto&amp;tl=zh-CN&amp;q=good&amp;mui=tl&amp;hl=zh-CN">中文（简体）</a></div>
-        </div>
-        <div class="input-container">
-            <form action="/m"><input type="hidden" name="sl" value="auto"><input type="hidden" name="tl"
-                    value="zh-CN"><input type="hidden" name="hl" value="zh-CN"><input type="text" aria-label="原文"
-                    name="q" class="input-field" maxlength="2048" value="good">
-                <div class="translate-button-container"><input type="submit" value="翻译" class="translate-button"></div>
-            </form>
-        </div>
-        <div class="result-container">好的</div>
-        <div class="links-container">
-            <ul>
-                <li><a href="https://www.google.com/m?hl=zh-CN">Google 主页</a></li>
-                <li><a href="https://www.google.com/tools/feedback/survey/xhtml?productId=95112&hl=zh-CN">发送反馈</a></li>
-                <li><a href="https://www.google.com/intl/zh-CN/policies">隐私权及使用条款</a></li>
-                <li><a href="./full">切换为完整网站</a></li>
-            </ul>
-        </div>
-    </div>
-</body>
-*/
 
 /**
  * Get current ip address
