1a02d845b0c7f5d6db98bd882d49dab201d7e592
Yukai Huang <yukaihuangtw@gmail.com>
2022-07-29 15:05:11 +0800

    feat: simplify workspace command

--- extensions/hackmd/src/list-my-notes.tsx
+++
@@ -1,9 +0,0 @@
-import { useCachedPromise } from "@raycast/utils";
-import NotesList from "./components/NotesList";
-import api from "./lib/api";
-
-export default function ListMyNotes() {
-  const { isLoading, data, mutate } = useCachedPromise(() => api.getNoteList());
-
-  return <NotesList notes={data} isLoading={isLoading} mutate={mutate} />;
-}
