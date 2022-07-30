eafee3f95544cf53baa6b106e57bd5b04c687417
marcjulianschwarz <info@marc-julian.de>
2022-07-30 13:02:42 +0200

    fix search being case sensitive

--- extensions/obsidian/src/utils/search.tsx
+++ extensions/obsidian/src/utils/search.tsx
@@ -5,11 +5,16 @@ export function filterNotes(notes: Note[], input: string, byContent: boolean) {
     return notes;
   }
 
+  input = input.toLowerCase();
+
   if (byContent) {
     return notes.filter(
-      (note) => note.content.includes(input) || note.title.includes(input) || note.path.includes(input)
+      (note) =>
+        note.content.toLowerCase().includes(input) ||
+        note.title.toLowerCase().includes(input) ||
+        note.path.toLowerCase().includes(input)
     );
   } else {
-    return notes.filter((note) => note.title.toLowerCase().includes(input.toLowerCase()));
+    return notes.filter((note) => note.title.toLowerCase().includes(input));
   }
 }
