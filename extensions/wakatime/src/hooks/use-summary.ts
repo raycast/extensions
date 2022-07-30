2bacb08f2ce998ea7c28850a1d17a641b938ed74
Ademola Adedeji <a.mola.dev@gmail.com>
2022-07-30 16:09:40 +0100

    💥 breaking: show summary when all requests are finished

--- extensions/wakatime/src/hooks/use-summary.ts
+++ extensions/wakatime/src/hooks/use-summary.ts
@@ -5,33 +5,28 @@ import { useEffect, useState } from "react";
 import { getSummary } from "../utils";
 
 export function useSummary() {
-  const rangeOrder: Range[] = ["Today", "Yesterday", "Last 7 Days", "Last 30 Days"];
-  const ranges: Record<Range, Date> = {
-    Today: new Date(),
-    Yesterday: sub(new Date(), { days: 1 }),
-    "Last 7 Days": sub(new Date(), { days: 7 }),
-    "Last 30 Days": sub(new Date(), { days: 30 }),
-  };
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [data, setData] = useState<Array<readonly [string, WakaTime.Summary]>>();
 
   useEffect(() => {
+    const ranges: Array<[Range, Date]> = [
+      ["Today", new Date()],
+      ["Yesterday", sub(new Date(), { days: 1 })],
+      ["Last 7 Days", sub(new Date(), { days: 7 })],
+      ["Last 30 Days", sub(new Date(), { days: 30 })],
+    ];
+
     async function getData() {
       setIsLoading(true);
 
       try {
-        await Promise.all(
-          rangeOrder.map(async (range, idx) => {
-            const summary = await getSummary(range, ranges[range]);
-            if (!summary.ok) throw new Error(summary.error);
+        const summaries = ranges.map(async ([key, date]) => {
+          const summary = await getSummary(key, date);
+          if (summary.ok) return [key, summary] as const;
+        });
 
-            setData((data = []) => {
-              const d = [...data];
-              d.splice(idx, 0, [range, summary] as const);
-              return d;
-            });
-          })
-        );
+        const data = await Promise.all(summaries);
+        setData(data.filter(Boolean) as NonNullable<typeof data[number]>[]);
       } catch (error) {
         await showToast(Toast.Style.Failure, "Error Loading Summary", (error as Error).message);
       }
@@ -39,7 +34,7 @@ export function useSummary() {
       setIsLoading(false);
     }
 
-    getData();
+    void getData();
   }, []);
 
   return { data, isLoading };
