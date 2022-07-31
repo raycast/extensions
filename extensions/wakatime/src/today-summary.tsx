cc85d34563b158addc6536d17f990693d3e52ccf
Fatpandac <i@fatpandac.com>
2022-07-31 11:50:35 +0800

    fix: add preference

--- extensions/wakatime/src/today-summary.tsx
+++ extensions/wakatime/src/today-summary.tsx
@@ -1,21 +1,23 @@
 import { useSummary } from "./hooks";
 
-import { MenuBarExtra } from "@raycast/api";
+import { MenuBarExtra, getPreferenceValues } from "@raycast/api";
 
 export default function Command() {
   const summary = useSummary();
+  const preference = getPreferenceValues();
+  const asksSentence = preference.showAsksSentence;
 
-  return (
-    <MenuBarExtra
-      icon="../assets/icon.png"
-      title={`${summary.data
-        ?.map(
-          ([key, range]) =>
-            key === "Today" &&
-            (range.cummulative_total.seconds === 0 ? "Are we coding today?" : `Today: ${range.cummulative_total.text}`)
-        )
-        .filter((item) => item)}`}
-      isLoading={summary.isLoading}
-    />
-  );
+  function getTitle(): string {
+    return `${summary.data
+      ?.map(
+        ([key, range]) =>
+          key === "Today" &&
+          (range.cummulative_total.seconds === 0 && asksSentence
+            ? asksSentence
+            : `Today: ${range.cummulative_total.text}`)
+      )
+      .filter((item) => item)}`;
+  }
+
+  return <MenuBarExtra icon="../assets/icon.png" title={getTitle()} isLoading={summary.isLoading} />;
 }
