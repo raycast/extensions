e1c85faa3968aeb8aa8aa56234be98a6e51bae36
404 <shijin@bilibili.com>
2022-08-03 22:48:53 +0800

    refine

--- extensions/itranslate/src/itranslate.tsx
+++ extensions/itranslate/src/itranslate.tsx
@@ -19,11 +19,6 @@ import {
 import { TranslateError } from "./TranslateError";
 import { TranslateResult } from "./TranslateResult";
 
-<<<<<<< HEAD
-let delayFetchTranslateAPITimer: NodeJS.Timeout;
-
-=======
->>>>>>> ec6c15a36eef7f23c36af480407eb8961efd0693
 export default function Command() {
   const preferences: IPreferences = getPreferenceValues<IPreferences>();
   const langSecond: ILangItem = getLang(preferences.langSecond);
@@ -44,16 +39,8 @@ export default function Command() {
   }, []);
 
   useEffect(() => {
-<<<<<<< HEAD
-    clearTimeout(delayFetchTranslateAPITimer);
-    if (inputState.trim().length > 0) {
-      delayFetchTranslateAPITimer = setTimeout(() => {
-        translate(currentTargetLang);
-      }, 900);
-=======
     if (inputState.trim().length > 0) {
       translate(currentTargetLang);
->>>>>>> ec6c15a36eef7f23c36af480407eb8961efd0693
     } else {
       updateTransResultsState([]);
       updateShowDetail(false);
@@ -149,10 +136,7 @@ export default function Command() {
   return (
     <List
       isLoading={isLoadingState}
-<<<<<<< HEAD
-=======
       throttle={true}
->>>>>>> ec6c15a36eef7f23c36af480407eb8961efd0693
       isShowingDetail={isShowDetail}
       searchText={inputState}
       searchBarPlaceholder={"Translate text"}
