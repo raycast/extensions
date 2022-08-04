28081f77ce8ac4955021082ab2cdcd2d2a8df4d2
Mathieu Dutour <mathieu@dutour.me>
2022-08-04 11:54:52 +0200

    Open source Google Workspace and Zoom (#2476)

---
+++ extensions/zoom/src/components/withZoomAuth.tsx
@@ -0,0 +1,36 @@
+import { Detail } from "@raycast/api";
+import { useEffect, useState } from "react";
+import { authorize, client } from "../api/oauth";
+
+let token: string | null = null;
+
+export function withZoomAuth(component: JSX.Element) {
+  const [x, forceRerender] = useState(0);
+
+  useEffect(() => {
+    (async function () {
+      const tokens = await client.getTokens();
+
+      const accessToken = tokens?.accessToken || (await authorize());
+
+      token = accessToken;
+
+      forceRerender(x + 1);
+    })();
+  }, []);
+
+  if (!token) {
+    // Using the <List /> component makes the placeholder buggy
+    return <Detail isLoading />;
+  }
+
+  return component;
+}
+
+export function getOAuthToken(): string {
+  if (!token) {
+    throw new Error("getOAuthToken must be used when authenticated");
+  }
+
+  return token;
+}
