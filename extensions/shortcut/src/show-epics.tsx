89465033f479c6c99bab96bf06f262929378c444
Yukai Huang <yukaihuangtw@gmail.com>
2022-08-04 13:24:24 +0800

    refactor: extract EpicListItem

--- extensions/shortcut/src/show-epics.tsx
+++ extensions/shortcut/src/show-epics.tsx
@@ -1,54 +1,17 @@
-import { Action, ActionPanel, Icon, List } from "@raycast/api";
-import { getProgressIcon } from "@raycast/utils";
-import EpicStories from "./components/EpicStories";
-import { getOwnersAccessoryItems } from "./helpers/storyHelpers";
-import { useEpics, useMemberMap } from "./hooks";
+import { List } from "@raycast/api";
+import EpicListItem from "./components/EpicListItem";
+import { useEpics } from "./hooks";
 
 export default function ShowEpics() {
   const { data: epics, isLoading } = useEpics();
-  const memberMap = useMemberMap();
 
   return (
     <List isLoading={isLoading}>
       {epics
         ?.sort((a, b) => a.position - b.position)
-        .map((epic) => {
-          const owners = epic.owner_ids.map((ownerId) => memberMap?.[ownerId]);
-          const progress = epic.stats.num_stories_done / epic.stats.num_stories_total;
-
-          return (
-            <List.Item
-              title={epic.name}
-              subtitle={`#${epic.id}`}
-              key={epic.id}
-              icon={Icon.Tag}
-              actions={
-                <ActionPanel>
-                  <Action.Push icon={Icon.List} title="View Epic Stories" target={<EpicStories epicId={epic.id} />} />
-                  <Action.OpenInBrowser url={epic.app_url} />
-                </ActionPanel>
-              }
-              accessories={
-                [
-                  {
-                    date: new Date(epic.created_at!),
-                  },
-                  ...getOwnersAccessoryItems(owners),
-
-                  isNaN(progress)
-                    ? {
-                        icon: getProgressIcon(progress, "#58b1e4"),
-                        tooltip: "No progress",
-                      }
-                    : {
-                        icon: getProgressIcon(progress, "#58b1e4"),
-                        tooltip: progress === 0 ? "Not started" : `${Math.round(progress * 100)}%`,
-                      },
-                ].filter(Boolean) as List.Item.Accessory[]
-              }
-            />
-          );
-        })}
+        .map((epic) => (
+          <EpicListItem epic={epic} />
+        ))}
     </List>
   );
 }
