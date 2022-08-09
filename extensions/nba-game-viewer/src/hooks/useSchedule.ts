1039e7da0d70c5b5598338436ec21949af839c43
Thomas Paul Mann <hello@thomaspaulmann.com>
2022-08-09 20:37:09 +0100

    NBA: Convert schedule to `@raycast/utils`

--- extensions/nba-game-viewer/src/hooks/useSchedule.ts
+++ extensions/nba-game-viewer/src/hooks/useSchedule.ts
@@ -1,96 +1,68 @@
 import getSchedule from "../utils/getSchedule";
-import { useState, useEffect } from "react";
+import { useCallback } from "react";
 import { Day, Game, Competitor } from "../types/schedule.types";
 import convertDate from "../utils/convertDate";
-import { Cache } from "@raycast/api";
+import { useCachedPromise } from "@raycast/utils";
 
-const cache = new Cache();
+const useSchedule = () => {
+  const fetchGames = useCallback(async () => {
+    const currentDate = new Date();
 
-const useSchedule = (): {
-  schedule: Day[];
-  loading: boolean;
-  error: boolean;
-} => {
-  const [schedule, setSchedule] = useState<Array<Day>>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<boolean>(false);
+    const data = await getSchedule({
+      year: currentDate.getUTCFullYear(),
+      month: currentDate.getUTCMonth() + 1,
+      day: currentDate.getUTCDate(),
+    });
 
-  useEffect(() => {
-    const getGames = async () => {
-      const currentDate = new Date();
-
-      let data: any = null;
-
-      const cachedData = cache.get("schedule");
-      if (cachedData) {
-        const cachedSchedule = JSON.parse(cachedData);
-        setSchedule(cachedSchedule);
+    Object.keys(data).map((key) => {
+      if (!Object.prototype.hasOwnProperty.call(data[key], "games")) {
+        delete data[key];
       }
+    });
 
-      try {
-        data = await getSchedule({
-          year: currentDate.getUTCFullYear(),
-          month: currentDate.getUTCMonth() + 1,
-          day: currentDate.getUTCDate(),
-        });
-      } catch (error) {
-        setError(true);
-        return error;
-      }
+    const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-      Object.keys(data).map((key) => {
-        if (!Object.prototype.hasOwnProperty.call(data[key], "games")) {
-          delete data[key];
-        }
-      });
+    const scheduledGames: Array<Day> = Object.keys(data).map((key) => {
+      return {
+        date: `${
+          weekdays[new Date(data[key].games[0]?.date.toLocaleString("en-US")).getUTCDay() - 1]
+        }  —  ${convertDate(key)}`,
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        games: data[key].games.map((game: any): Game => {
+          return {
+            id: game.id,
+            name: game.name,
+            shortName: game.shortName,
+            time: new Date(game.date).toLocaleTimeString("en-US", { timeStyle: "short" }).toString(),
+            competitors: game.competitions[0].competitors
+              // eslint-disable-next-line @typescript-eslint/no-explicit-any
+              .map((competitor: any): Competitor => {
+                return {
+                  displayName: competitor.team.displayName,
+                  shortName: competitor.team.shortDisplayName,
+                  logo: competitor.team.logo,
+                  home: competitor.homeAway,
+                };
+              })
+              .sort((a: Competitor) => {
+                return a.home === "home" ? -1 : 1;
+              }),
+            status: {
+              period: game.competitions[0].status.period,
+              clock: game.competitions[0].status.clock,
+              completed: game.competitions[0].status.type.completed,
+              inProgress: game.competitions[0].status.type.description === "In Progress",
+            },
+            stream: game.links[0].href,
+          };
+        }),
+      };
+    });
 
-      const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-      const scheduledGames: Array<Day> = Object.keys(data).map((key) => {
-        return {
-          date: `${
-            weekdays[new Date(data[key].games[0].date.toLocaleString("en-US")).getUTCDay() - 1]
-          }  —  ${convertDate(key)}`,
-          games: data[key].games.map((game: any): Game => {
-            return {
-              id: game.id,
-              name: game.name,
-              shortName: game.shortName,
-              time: new Date(game.date).toLocaleTimeString("en-US", { timeStyle: "short" }).toString(),
-              competitors: game.competitions[0].competitors
-                .map((competitor: any): Competitor => {
-                  return {
-                    displayName: competitor.team.displayName,
-                    shortName: competitor.team.shortDisplayName,
-                    logo: competitor.team.logo,
-                    home: competitor.homeAway,
-                  };
-                })
-                .sort((a: Competitor) => {
-                  return a.home === "home" ? -1 : 1;
-                }),
-              status: {
-                period: game.competitions[0].status.period,
-                clock: game.competitions[0].status.clock,
-                completed: game.competitions[0].status.type.completed,
-                inProgress: game.competitions[0].status.type.description === "In Progress",
-              },
-              stream: game.links[0].href,
-            };
-          }),
-        };
-      });
-
-      setSchedule(scheduledGames);
-      setLoading(false);
-
-      cache.set("schedule", JSON.stringify(scheduledGames));
-    };
-
-    getGames();
+    return scheduledGames;
   }, []);
 
-  return { schedule, loading, error };
+  return useCachedPromise(fetchGames);
 };
 
 export default useSchedule;
--- extensions/nba-game-viewer/src/views/schedule.tsx
+++ extensions/nba-game-viewer/src/views/schedule.tsx
@@ -1,19 +1,14 @@
-import { List, Toast, showToast } from "@raycast/api";
+import { List } from "@raycast/api";
 import { Day } from "../types/schedule.types";
 import useSchedule from "../hooks/useSchedule";
 import DayComponent from "../components/Day";
 
 const Schedue = () => {
-  const data = useSchedule();
-
-  if (data.error) {
-    showToast(Toast.Style.Failure, "Failed to get schedule");
-    data.loading = false;
-  }
+  const { data, isLoading } = useSchedule();
 
   return (
-    <List isLoading={data.loading}>
-      {data.schedule.map((day: Day) => (
+    <List isLoading={isLoading}>
+      {data?.map((day: Day) => (
         <DayComponent key={day.date} day={day} />
       ))}
     </List>
