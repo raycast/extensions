4370d6d5abc85e5fc92f8cfaad5c115a853d10dd
AndreasElia <andreaselia@live.co.uk>
2022-08-09 15:19:02 +0100

    update avatar for feedback

--- extensions/abstract-api/src/avatars.tsx
+++ extensions/abstract-api/src/avatars.tsx
@@ -8,54 +8,34 @@ interface Preferences {
   avatarsApiKey: string;
 }
 
-interface CommandForm {
-  name: string;
-}
-
 export default function Command() {
   const preferences = getPreferenceValues<Preferences>();
   const [name, setName] = useState("");
-  const [url, setUrl] = useState("");
 
-  async function handleSubmit(values: CommandForm) {
-    if (values.name == "") {
+  function getUrl() {
+    const baseUrl = "https://avatars.abstractapi.com/v1";
+    const urlFriendlyName = encodeURIComponent(name);
+
+    return `${baseUrl}/?api_key=${preferences.avatarsApiKey}&name=${urlFriendlyName}&image_format=png`;
+  }
+
+  async function submitAndOpen() {
+    if (name == "") {
       showToast(Toast.Style.Failure, "Error", "Name is required");
       return;
     }
 
-    const toast = await showToast({
-      style: Toast.Style.Animated,
-      title: "Retrieving avatar...",
-    });
-
-    const baseUrl = "https://avatars.abstractapi.com/v1";
-    const name = encodeURIComponent(values.name);
-    const url = `${baseUrl}/?api_key=${preferences.avatarsApiKey}&name=${name}&image_format=png`;
-
-    axios
-      .get(url)
-      .then(() => {
-        toast.style = Toast.Style.Success;
-        toast.title = "Avatar retrieved successfully";
-
-        setName(name);
-        setUrl(url);
-      })
-      .catch((error) => {
-        toast.style = Toast.Style.Failure;
-        toast.title = "Unable to retrieve avatar";
-        toast.message = error.response.data.error.message ?? "";
-      });
+    open(getUrl());
   }
 
-  async function download() {
+  async function submitAndDownload() {
     const toast = await showToast({
       style: Toast.Style.Animated,
       title: "Saving avatar...",
     });
 
     await axios
-      .get(url, { responseType: "stream" })
+      .get(getUrl(), { responseType: "stream" })
       .then((response) => {
         const filename = name.split(" ").join("_");
         response.data.pipe(fs.createWriteStream(`${homedir()}/Desktop/${filename}.png`));
@@ -74,17 +54,17 @@ export default function Command() {
     <Form
       actions={
         <ActionPanel>
-          <Action.SubmitForm title="Generate Avatar" onSubmit={handleSubmit} icon={Icon.Pencil} />
-          {url ? (
-            <>
-              <Action title="Download" onAction={download} icon={Icon.Download} />
-              <Action.OpenInBrowser title="Open in Browser" url={url} />
-            </>
-          ) : null}
+          <Action.SubmitForm title="Download" onSubmit={submitAndDownload} icon={Icon.Download} />
+          <Action.SubmitForm title="Open in Browser" onSubmit={submitAndOpen} icon={Icon.Globe} />
         </ActionPanel>
       }
     >
-      <Form.TextField id="name" title="Name" placeholder="Enter name" />
+      <Form.TextField
+        id="name"
+        title="Name"
+        placeholder="Enter name"
+        onChange={(value) => setName(value)}
+      />
     </Form>
   );
 }
