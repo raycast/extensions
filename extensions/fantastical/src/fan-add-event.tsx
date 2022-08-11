09278150e5c54783d0a97c9c8d1ec6344ac0f577
Devah Schaefers <73009338+devahschaefers@users.noreply.github.com>
2022-08-11 07:33:19 +0200

    Add new Fantastical extension (#2464)
    * added fantastical extension
    
    * delet extension as it was added as a git submodule
    
    * added fantastical extension
    
    * fixed readme
    
    * changed folder for readme pictures
    
    * fixed readme
    
    * Added action to install Fanstatical
    
    * Added open
    
    * Update CHANGELOG.md
    
    Co-authored-by: Per Nielsen Tikær <per@raycast.com>
    Co-authored-by: raycastbot <bot@raycast.com>

---
+++ extensions/fantastical/src/fan-add-event.tsx
@@ -0,0 +1,30 @@
+import { Action, ActionPanel, showToast, Toast, open } from "@raycast/api";
+import { runAppleScript } from "run-applescript";
+import { isInstalled } from "./isInstalled";
+interface Arguments {
+  add: string;
+}
+
+export default async (props: { arguments: Arguments }) => {
+  const args = props.arguments;
+  if (await isInstalled()) {
+    await runAppleScript(`
+        tell application "Fantastical" 
+            \n parse sentence "${args.add}" \n
+        end tell`);
+  } else {
+    const options: Toast.Options = {
+      style: Toast.Style.Failure,
+      title: "Fantastical is not installed",
+      message: "Please first install Fantastical to use this extension.",
+      primaryAction: {
+        title: "Install Fantastical",
+        onAction: () => {
+          open("https://fantastical.app/");
+        },
+      },
+    };
+
+    showToast(options);
+  }
+};
