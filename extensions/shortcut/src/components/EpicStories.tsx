56d771bb4f5df05fdfcfde50d3a0e544828e6877
Yukai Huang <yukaihuangtw@gmail.com>
2022-08-11 15:07:37 +0800

    fix: pass error prop into StoriesList

--- extensions/shortcut/src/components/EpicStories.tsx
+++ extensions/shortcut/src/components/EpicStories.tsx
@@ -2,7 +2,7 @@ import { useEpicStories } from "../hooks";
 import StoriesList from "./StoriesList";
 
 export default function EpicStories({ epicId }: { epicId: number }) {
-  const { data: stories, isLoading, mutate } = useEpicStories(epicId);
+  const { data: stories, isLoading, mutate, error } = useEpicStories(epicId);
 
-  return <StoriesList isLoading={isLoading} stories={stories} mutate={mutate} />;
+  return <StoriesList isLoading={isLoading} stories={stories} mutate={mutate} error={error} />;
 }
--- extensions/shortcut/src/components/IterationStories.tsx
+++ extensions/shortcut/src/components/IterationStories.tsx
@@ -2,7 +2,7 @@ import { useIterationStories } from "../hooks";
 import StoriesList from "./StoriesList";
 
 export function IterationStories({ iterationId }: { iterationId: number }) {
-  const { data: stories, isLoading, mutate } = useIterationStories(iterationId);
+  const { data: stories, isLoading, mutate, error } = useIterationStories(iterationId);
 
-  return <StoriesList stories={stories} isLoading={isLoading} mutate={mutate} />;
+  return <StoriesList stories={stories} isLoading={isLoading} mutate={mutate} error={error} />;
 }
