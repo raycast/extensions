c89ee92960ceb50be2b414c29b14d7b37f762c46
Aayush <43297314+Aayush9029@users.noreply.github.com>
2022-08-13 16:32:03 -0400

    Add files via upload

---
+++ extensions/brew/CHANGELOG.md
@@ -0,0 +1,50 @@
+# Brew Changelog
+
+## [Improvements] - 2022-08-13
+
+- Add a copy install command action
+- Add run install command in terminal action
+
+## [Improvements] - 2022-08-10
+
+- Add a "Custom Brew Path" preference for users with brew installed in a non-standard path
+- The extension will now prompt for user's login password if brew requires authorization when installed or uninstalling casks
+- Improved reporting of fetch errors
+- Add a search bar filter for the installable type (formula or cask).
+
+## [Improvements] - 2022-06-06
+
+- Removed --dry-run flag when running the Upgrade command so the casks and formulas actually upgraded
+
+## [Search] - 2022-03-15
+
+- Improve search accuracy (now includes the description)
+- Fix an issue loading installed formulae where user has a large number installed
+- Upgrade to latest @raycast/api
+
+## [Improvements] - 2022-01-17
+
+- Improve reliability of `outdated` command
+- Add action to copy formula/cask name
+- Add cask name & tap to cask details
+- Add Toast action to cancel current action
+- Add Toast action to copy error log after failure
+
+## [Upgrades] - 2021-12-01
+
+- Add `upgrade` command
+- Add greedy upgrade preference
+
+## [Improvements] - 2021-11-19
+
+- Improve discovery of brew prefix
+- Update Cask.installed correctly after installation
+- Fix installed state after uninstalling search result
+- Fix cache check after installing/uninstalling cask
+- Add uninstall action to outdated action panel
+
+## [Casks] - 2021-11-04
+
+- Add support for searching and managing casks
+
+## [Initial Version] - 2021-10-26
---
+++ extensions/brew/src/components/actionPanels.tsx
@@ -0,0 +1,230 @@
+import { Action, ActionPanel, Icon } from "@raycast/api";
+import { brewIsInstalled, brewInstallPath } from "../brew";
+import { Cask, Formula, OutdatedCask, OutdatedFormula } from "../brew";
+import { FormulaInfo } from "./formulaInfo";
+import { CaskInfo } from "./caskInfo";
+import * as Actions from "./actions";
+import { runCommandInTerminal } from "./runInTerminal";
+
+export function CaskActionPanel(props: {
+  cask: Cask;
+  showDetails: boolean;
+  onAction: (result: boolean) => void;
+}): JSX.Element {
+  const cask = props.cask;
+
+  function installedActionPanel() {
+    return (
+      <ActionPanel>
+        <ActionPanel.Section>
+          {props.showDetails && (
+            <Action.Push
+              title="Show Details"
+              icon={Icon.Document}
+              target={<CaskInfo cask={cask} onAction={props.onAction} />}
+            />
+          )}
+          <Action.ShowInFinder path={brewInstallPath(cask)} />
+          <Action.CopyToClipboard
+            title="Copy Cask Name"
+            content={cask.token}
+            shortcut={{ modifiers: ["cmd", "shift"], key: "c" }}
+          />
+          <Action.CopyToClipboard title="Copy Tap Name" content={cask.tap} />
+        </ActionPanel.Section>
+        <ActionPanel.Section>
+          <Action.CopyToClipboard
+            title="Copy Uninstall Command"
+            content={`brew uninstall --cask ${cask.name}`}
+            shortcut={{ modifiers: ["cmd", "opt"], key: "c" }}
+          />
+          <Action
+            title="Run In Terminal"
+            icon={Icon.Terminal}
+            onAction={() => {
+              runCommandInTerminal(`brew uninstall --cask ${cask.name}`);
+            }}
+          />
+        </ActionPanel.Section>
+        <ActionPanel.Section>
+          <Action.OpenInBrowser url={cask.homepage} />
+          <Action.CopyToClipboard title="Copy URL" content={cask.homepage} />
+        </ActionPanel.Section>
+        <ActionPanel.Section>
+          {cask.outdated && <Actions.FormulaUpgradeAction formula={cask} onAction={props.onAction} />}
+          <Actions.FormulaUninstallAction formula={cask} onAction={props.onAction} />
+        </ActionPanel.Section>
+      </ActionPanel>
+    );
+  }
+
+  function uninstalledActionPanel() {
+    return (
+      <ActionPanel>
+        <ActionPanel.Section>
+          {props.showDetails && (
+            <Action.Push
+              title="Show Details"
+              icon={Icon.Document}
+              target={<CaskInfo cask={cask} onAction={props.onAction} />}
+            />
+          )}
+          <Actions.FormulaInstallAction formula={cask} onAction={props.onAction} />
+          <Action.CopyToClipboard
+            title="Copy Cask Name"
+            content={cask.token}
+            shortcut={{ modifiers: ["cmd", "shift"], key: "c" }}
+          />
+          <Action.CopyToClipboard title="Copy Tap Name" content={cask.tap} />
+        </ActionPanel.Section>
+        <ActionPanel.Section>
+          <Action.CopyToClipboard
+            title="Copy Install Command"
+            content={`brew install --cask ${cask.name}`}
+            shortcut={{ modifiers: ["cmd", "opt"], key: "c" }}
+          />
+          <Action
+            title="Run In Terminal"
+            icon={Icon.Terminal}
+            onAction={() => {
+              runCommandInTerminal(`brew install --cask ${cask.name}`);
+            }}
+          />
+        </ActionPanel.Section>
+        <ActionPanel.Section>
+          <Action.OpenInBrowser url={cask.homepage} />
+          <Action.CopyToClipboard title="Copy URL" content={cask.homepage} />
+        </ActionPanel.Section>
+      </ActionPanel>
+    );
+  }
+
+  if (brewIsInstalled(props.cask)) {
+    return installedActionPanel();
+  } else {
+    return uninstalledActionPanel();
+  }
+}
+
+export function FormulaActionPanel(props: {
+  formula: Formula;
+  showDetails: boolean;
+  onAction: (result: boolean) => void;
+}): JSX.Element {
+  const formula = props.formula;
+
+  function installedActionPanel() {
+    return (
+      <ActionPanel>
+        <ActionPanel.Section>
+          {props.showDetails && (
+            <Action.Push
+              title="Show Details"
+              icon={Icon.Document}
+              target={<FormulaInfo formula={formula} onAction={props.onAction} />}
+            />
+          )}
+          <Action.ShowInFinder path={brewInstallPath(formula)} />
+          <Action.CopyToClipboard
+            title="Copy Formula Name"
+            content={formula.name}
+            shortcut={{ modifiers: ["cmd", "shift"], key: "c" }}
+          />
+        </ActionPanel.Section>
+        <ActionPanel.Section>
+          <Action.CopyToClipboard
+            title="Copy Uninstall Command"
+            content={`brew uninstall ${formula.name}`}
+            shortcut={{ modifiers: ["cmd", "opt"], key: "c" }}
+          />
+          <Action
+            title="Run In Terminal"
+            icon={Icon.Terminal}
+            onAction={() => {
+              runCommandInTerminal(`brew uninstall ${formula.name}`);
+            }}
+          />
+        </ActionPanel.Section>
+        <ActionPanel.Section>
+          <Action.OpenInBrowser url={formula.homepage} />
+          <Action.CopyToClipboard title="Copy URL" content={formula.homepage} />
+        </ActionPanel.Section>
+        <ActionPanel.Section>
+          <Actions.FormulaPinAction formula={formula} onAction={props.onAction} />
+          {formula.outdated && <Actions.FormulaUpgradeAction formula={formula} onAction={props.onAction} />}
+          <Actions.FormulaUninstallAction formula={formula} onAction={props.onAction} />
+        </ActionPanel.Section>
+      </ActionPanel>
+    );
+  }
+
+  function uninstalledActionPanel() {
+    return (
+      <ActionPanel>
+        <ActionPanel.Section>
+          {props.showDetails && (
+            <Action.Push
+              title="Show Details"
+              icon={Icon.Document}
+              target={<FormulaInfo formula={formula} onAction={props.onAction} />}
+            />
+          )}
+          <Actions.FormulaInstallAction formula={formula} onAction={props.onAction} />
+          <Action.CopyToClipboard
+            title="Copy Formula Name"
+            content={formula.name}
+            shortcut={{ modifiers: ["cmd", "shift"], key: "c" }}
+          />
+        </ActionPanel.Section>
+        <ActionPanel.Section>
+          <Action.CopyToClipboard
+            title="Copy Install Command"
+            content={`brew install ${formula.name}`}
+            shortcut={{ modifiers: ["cmd", "opt"], key: "c" }}
+          />
+          <Action
+            title="Run In Terminal"
+            icon={Icon.Terminal}
+            onAction={() => {
+              runCommandInTerminal(`brew install ${formula.name}`);
+            }}
+          />
+        </ActionPanel.Section>
+        <ActionPanel.Section>
+          <Action.OpenInBrowser url={formula.homepage} />
+          <Action.CopyToClipboard title="Copy URL" content={formula.homepage} />
+        </ActionPanel.Section>
+      </ActionPanel>
+    );
+  }
+
+  if (brewIsInstalled(props.formula)) {
+    return installedActionPanel();
+  } else {
+    return uninstalledActionPanel();
+  }
+}
+
+export function OutdatedActionPanel(props: {
+  outdated: OutdatedCask | OutdatedFormula;
+  onAction: (result: boolean) => void;
+}): JSX.Element {
+  const outdated = props.outdated;
+
+  function isPinable(o: OutdatedCask | OutdatedFormula): o is OutdatedFormula {
+    return (o as OutdatedFormula).pinned != undefined ? true : false;
+  }
+
+  return (
+    <ActionPanel>
+      <ActionPanel.Section>
+        <Actions.FormulaUpgradeAction formula={outdated} onAction={props.onAction} />
+        <Actions.FormulaUpgradeAllAction onAction={props.onAction} />
+        {isPinable(outdated) && <Actions.FormulaPinAction formula={outdated} onAction={props.onAction} />}
+      </ActionPanel.Section>
+      <ActionPanel.Section>
+        <Actions.FormulaUninstallAction formula={outdated} onAction={props.onAction} />
+      </ActionPanel.Section>
+    </ActionPanel>
+  );
+}
