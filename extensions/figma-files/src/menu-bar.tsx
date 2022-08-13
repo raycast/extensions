be3d4474448703e2bd1df94d2f37a22b7af14647
Michael Schultz <michaelwschultz@gmail.com>
2022-08-13 09:26:27 -0700

    Adds recent files to menubar

--- extensions/figma-files/src/menu-bar.tsx
+++ extensions/figma-files/src/menu-bar.tsx
@@ -1,9 +1,12 @@
 import { MenuBarExtra, open } from "@raycast/api";
 import { useProjectFiles } from "./hooks/useProjectFiles";
 import { useFigmaApp } from "./hooks/useFigmaApp";
+import { useVisitedFiles } from "./hooks/useVisitedFiles";
 
 export default function Command() {
   const { projectFiles, isLoading: isLoadingProjectFiles, hasError } = useProjectFiles();
+  const { files: visitedFiles, visitFile, isLoading: isLoadingVisitedFiles } = useVisitedFiles();
+
   const desktopApp = useFigmaApp();
 
   let url = "figma://file/";
@@ -12,20 +15,41 @@ export default function Command() {
   }
 
   return (
-    <MenuBarExtra icon="figma-menubar-icon.png" tooltip="Your Design Files">
+    <MenuBarExtra icon="figma-menubar-icon.png" tooltip="Figma files">
       {hasError && <MenuBarExtra.Item title="Error" />}
-      {!hasError &&
+      {visitedFiles && (
+        <>
+          <MenuBarExtra.Submenu
+            key="recent-files"
+            title="Recent"
+            children={visitedFiles?.map((file) => (
+              <MenuBarExtra.Item
+                key={file.key + "-recent-file"}
+                title={file.name}
+                onAction={() => (open(url + file.key), visitFile(file))}
+              />
+            ))}
+          />
+          <MenuBarExtra.Separator />
+        </>
+      )}
+      {!isLoadingVisitedFiles &&
         projectFiles &&
         projectFiles?.map((project) => (
           <MenuBarExtra.Submenu
             key={project.name}
             title={project.name}
             children={project.files?.map((file) => (
-              <MenuBarExtra.Item key={file.key} title={file.name} onAction={() => open(url + file.key)} />
+              <MenuBarExtra.Item
+                key={file.key}
+                title={file.name}
+                onAction={() => (open(url + file.key), visitFile(file))}
+              />
             ))}
           />
         ))}
-      {isLoadingProjectFiles && !projectFiles && <MenuBarExtra.Item title="Loading..." />}
+      {(isLoadingProjectFiles || isLoadingVisitedFiles) && !projectFiles && <MenuBarExtra.Item title="Loading..." />}
+      {!isLoadingProjectFiles && !projectFiles && <MenuBarExtra.Item title="No projects found" />}
     </MenuBarExtra>
   );
 }
