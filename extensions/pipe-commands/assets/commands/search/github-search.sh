346a7e3bc6989aee2621bd8b0f9ec8f771dcc31a
Achille Lacoin <achille.lacoin@dailymotion.com>
2022-08-15 14:25:23 +0200

    [Pipe Commands] Improve Compatibility with Script Commands (#1980)
    * Improve Compatibility with Script Commands
    
    * fix styling issues
    
    * fix issues with custom Pipe Command Folder
    
    * remove the icon selection from the creation form
    
    * Add support for the description field in the form
    
    * fix missing metadatas in pipe command creation form
    
    * remove the pipe command creation form
    The script command creation form can be used instead :)
    
    * add ability to pipe selected files in finder to a command
    
    * improve file handling
    
    * fix needsConfirmation
    
    * remove file handling
    
    * deprecate `paste` and `copy` modes for better compatibility
    
    * add some documentation on the interaction of script/pipe command interaction
    
    * Update utils.ts
    
    * Update types.ts
    
    * Update CHANGELOG.md
    
    * Update CHANGELOG.md
    
    * drop support for fullOutput and compact modes
    
    * add a notification to signify that the output was copied to the clipboard
    
    * default to just running the script
    
    * introduce the "pipe" mode
    
    * untildify currentDirectoryPath
    
    * fix image title
    
    * update manage commands screen
    
    * remove argument1 requirement for pipe commands, use stdin instead
    
    * update schema
    
    * migration and prettier
    
    * use distinct default icons for pipe and silent commands
    
    * filter silent commands without argument
    
    * improve boolean logic
    
    * remove manage pipe commands command
    
    * remove codeblock util
    
    * some naming changes
    
    * Update CHANGELOG.md
    
    Co-authored-by: Per Nielsen Tikær <per@raycast.com>
    Co-authored-by: raycastbot <bot@raycast.com>

--- extensions/pipe-commands/assets/commands/search/github-search.sh
+++ extensions/pipe-commands/assets/commands/search/github-search.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# @raycast.schemaVersion 1
 # @raycast.title Github Search
 # @raycast.packageName Web Searches
-# @raycast.icon Globe
+# @raycast.icon ./icons/github.png
+# @raycast.iconDark ./icons/github-dark.png
 # @raycast.mode silent
 # @raycast.argument1 {"type": "text", "percentEncoded": true}
 
