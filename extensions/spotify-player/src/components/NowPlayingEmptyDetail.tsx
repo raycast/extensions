d3c5c21b1ff13e8062f7a5e7703412d51ad6ec76
Daniel Cannon <daniel@danielcannon.co.uk>
2022-08-15 10:33:36 +0100

    [Spotify-Player] Introduce "Now Playing" (view) command (#2518)
    * Add now playing view command
    
    * Update changelog/assets
    
    * Rename menu bar again
    
    * Add missing import
    
    * Add missing await
    
    * Spotify: Rename menu bar command
    
    * Spotify: Remove subtitle udpating from menu bar command
    
    * Spotify: Now playing command clean up
    
    * Improve handling when Spotify application is not running
    
    * Remove updateCommandMetadata from now playing view command
    
    * Ensure application name is still dynamic
    
    * Spotify: Move the subtitle updating back for the menu bar command
    
    Co-authored-by: mattisssa <kolevanartem@gmail.com>

--- extensions/spotify-player/src/components/NowPlayingEmptyDetail.tsx
+++ extensions/spotify-player/src/components/NowPlayingEmptyDetail.tsx
@@ -1,16 +1,24 @@
-import { Icon, List } from "@raycast/api";
+import { Detail, Icon } from "@raycast/api";
 
-export default function NowPlayingEmptyDetail({ text }: { text: string }): JSX.Element {
+export default function NowPlayingEmptyDetail({
+  title,
+  showLoadingImage,
+}: {
+  showLoadingImage?: boolean;
+  title: string;
+}): JSX.Element {
   return (
-    <List.Item.Detail
-      markdown={`<img src="${Icon.Hourglass}" alt="${text}" width="160" height="160" />`}
+    <Detail
+      markdown={
+        showLoadingImage ? `<img src="${Icon.Hourglass}" alt="${title}" width="275" height="275" />` : `# ${title}`
+      }
       metadata={
-        <List.Item.Detail.Metadata>
-          <List.Item.Detail.Metadata.Label title="Track" text={text} icon={Icon.Hourglass} />
-          <List.Item.Detail.Metadata.Label title="Artist" text="Unknown" />
-          <List.Item.Detail.Metadata.Label title="Album" text="Unknown" />
-          <List.Item.Detail.Metadata.Label title="Duration" text="00:00 | 00:00" />
-        </List.Item.Detail.Metadata>
+        <Detail.Metadata>
+          <Detail.Metadata.Label title="Track" text={title} icon={Icon.Hourglass} />
+          <Detail.Metadata.Label title="Artist" text="Unknown" />
+          <Detail.Metadata.Label title="Album" text="Unknown" />
+          <Detail.Metadata.Label title="Duration" text="00:00 | 00:00" />
+        </Detail.Metadata>
       }
     />
   );
