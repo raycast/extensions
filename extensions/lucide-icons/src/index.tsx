84ba005e8d13f2f0745287ed4ad9208dfed51d6b
Sorin Muntean <me@sorinmuntean.ro>
2022-08-16 15:13:05 +0200

    allow changing color & size

--- extensions/lucide-icons/src/index.tsx
+++ extensions/lucide-icons/src/index.tsx
@@ -1,5 +1,6 @@
 import { useState, useMemo } from "react";
-import { ActionPanel, Action, Grid, environment } from "@raycast/api";
+import { ActionPanel, Action, Grid, environment, Color, Icon } from "@raycast/api";
+import { useCachedState } from "@raycast/utils";
 import { readdirSync, readFileSync } from "fs";
 import { join } from "path";
 
@@ -21,45 +22,43 @@ export default function Command() {
       ]),
     []
   );
-  const [itemSize, setItemSize] = useState<Grid.ItemSize>(Grid.ItemSize.Medium);
-  const [isLoading, setIsLoading] = useState(true);
+  const [color, setColor] = useCachedState<Color>("color", Color.PrimaryText);
+  const [itemSize, setItemSize] = useCachedState<Grid.ItemSize>("size", Grid.ItemSize.Medium);
 
   return (
-    <Grid
-      itemSize={itemSize}
-      inset={Grid.Inset.Large}
-      isLoading={isLoading}
-      searchBarAccessory={
-        <Grid.Dropdown
-          tooltip="Grid Item Size"
-          storeValue
-          onChange={(newValue) => {
-            setItemSize(newValue as Grid.ItemSize);
-            setIsLoading(false);
-          }}
-        >
-          <Grid.Dropdown.Item title="Large" value={Grid.ItemSize.Large} />
-          <Grid.Dropdown.Item title="Medium" value={Grid.ItemSize.Medium} />
-          <Grid.Dropdown.Item title="Small" value={Grid.ItemSize.Small} />
-        </Grid.Dropdown>
-      }
-    >
-      {!isLoading &&
-        files.map(([path, name]) => (
-          <Grid.Item
-            key={name}
-            content={path}
-            title={name}
-            actions={
-              <ActionPanel>
-                <Action.OpenInBrowser url={`https://lucide.dev/icon/${name}`} />
-                <Action.CopyToClipboard content={name} title="Copy Name to Clipboard" />
-                <Action.CopyToClipboard content={readFileSync(path, "utf8")} title="Copy SVG to Clipboard" />
-                <Action.CopyToClipboard content={`<${toPascalCase(name)} />`} title="Copy Component to Clipboard" />
-              </ActionPanel>
-            }
-          />
-        ))}
+    <Grid itemSize={itemSize} inset={Grid.Inset.Large}>
+      {files.map(([path, name]) => (
+        <Grid.Item
+          key={name}
+          content={{ source: path, tintColor: color }}
+          title={name}
+          actions={
+            <ActionPanel>
+              <Action.OpenInBrowser url={`https://lucide.dev/icon/${name}`} />
+              <Action.CopyToClipboard content={name} title="Copy Name to Clipboard" />
+              <Action.CopyToClipboard content={readFileSync(path, "utf8")} title="Copy SVG to Clipboard" />
+              <Action.CopyToClipboard content={`<${toPascalCase(name)} />`} title="Copy Component to Clipboard" />
+              <ActionPanel.Section title="Preferences">
+                <ActionPanel.Submenu title="Change Size…" icon={Icon.MagnifyingGlass}>
+                  {Object.entries(Grid.ItemSize).map(([key, size]) => (
+                    <Action key={size} title={key} onAction={() => setItemSize(size)} />
+                  ))}
+                </ActionPanel.Submenu>
+                <ActionPanel.Submenu title="Change Color…" icon={Icon.EyeDropper}>
+                  {Object.entries(Color).map(([key, color]) => (
+                    <Action
+                      key={color}
+                      title={key}
+                      icon={{ source: Icon.Circle, tintColor: color }}
+                      onAction={() => setColor(color)}
+                    />
+                  ))}
+                </ActionPanel.Submenu>
+              </ActionPanel.Section>
+            </ActionPanel>
+          }
+        />
+      ))}
     </Grid>
   );
 }
