d0f61eeaeb3f6217a087db1fa718f45edf8ee8b3
404 <shijin@bilibili.com>
2022-08-16 13:47:37 +0800

    feat(iHost): phase1

--- extensions/ihosts/src/index.tsx
+++ extensions/ihosts/src/index.tsx
@@ -1,20 +1,31 @@
-import { ActionPanel, Detail, List, Action } from "@raycast/api";
+import { ActionPanel, Detail, List, Action, Icon, Color } from "@raycast/api";
 import fs from "node:fs";
 
 export default function Command() {
-  const hosts = fs.readFileSync("/etc/hosts", "utf8");
-  console.log(hosts);
   return (
     <List>
       <List.Item
-        icon="list-icon.png"
-        title="Greeting"
-        actions={
-          <ActionPanel>
-            <Action.Push title="Show Details" target={<Detail markdown="# Hey! 👋" />} />
-          </ActionPanel>
-        }
+        title={"System Hosts"}
+        icon={{ source: Icon.ComputerChip, tintColor: Color.Blue }}
+        actions={systemHostsActions(false)}
       />
+      <List.Item title={"Uat"} icon={{ source: Icon.CheckCircle, tintColor: Color.Green }} />
+      <List.Item title={"Pre"} icon={{ source: Icon.Circle, tintColor: Color.SecondaryText }} />
+      <List.Item title={"Prod"} icon={{ source: Icon.CheckCircle, tintColor: Color.Green }} />
     </List>
+  );
+}
+
+function systemHostsDetail() {
+  const systemHostsMd = fs.readFileSync("/etc/hosts", "utf8");
+  return <Detail markdown={systemHostsMd} actions={systemHostsActions(true)} />;
+}
+
+function systemHostsActions(isDetail: boolean) {
+  return (
+    <ActionPanel>
+      {!isDetail && <Action.Push title="Show Detail" target={systemHostsDetail()} />}
+      <Action.OpenWith path={"/etc/hosts"} />
+    </ActionPanel>
   );
 }
