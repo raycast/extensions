ffa3070e6e8663c654f16985749f8a82cf7e03a1
yug2005 <yug.sahu@mail.utoronto.ca>
2022-08-20 14:11:31 -0400

    New command: share with mail

---
+++ extensions/mail/src/share-with-mail.tsx
@@ -0,0 +1,1 @@
+import { ComposeMessage } from "./components/compose";
--- extensions/mail/src/utils/cache.ts
+++ extensions/mail/src/utils/cache.ts
@@ -2,7 +2,8 @@ import { Cache } from "@raycast/api";
 import { Account, Message } from "../types/types"; 
 
 export enum ExpirationTime {
-  Hour = 60 * 60 * 1000,
+  Minute = 60 * 1000,
+  Hour = 60 * Minute,
   Day = 24 * Hour,
   Week = 7 * Day,
 }
@@ -33,7 +34,7 @@ export const getMessages = (account: string, mailbox: string): Message[] => {
   const key = `${account}-${mailbox}`;
   if (messages.has(key)) {
     const { time, data } = JSON.parse(messages.get(key)!);
-    if (!isCacheExpired(time, ExpirationTime.Week)) {
+    if (!isCacheExpired(time, ExpirationTime.Hour)) {
       return data;
     }
   }
