77e409286efbd4587cb7744a43898a635529e22d
Mohamed Ibrahim <mibrahim.dev@gmail.com>
2022-08-21 02:34:45 +0200

    feat: update ESLint

--- extensions/android/src/list-emulators.tsx
+++ extensions/android/src/list-emulators.tsx
@@ -1,75 +1,86 @@
-import { Action, ActionPanel, getPreferenceValues, Icon, List, popToRoot, PreferenceValues, showToast, Toast } from "@raycast/api";
+import {
+  Action,
+  ActionPanel,
+  getPreferenceValues,
+  Icon,
+  List,
+  popToRoot,
+  PreferenceValues,
+  showToast,
+  Toast,
+} from "@raycast/api";
 import { useEffect, useState } from "react";
-const { exec } = require('child_process');
-
-
+const { exec } = require("child_process");
 
 export default function Command() {
-
-  const [items, setItems] = useState<string[]>(() => [])
-  const [loading, setLoading] = useState(true)
+  const [items, setItems] = useState<string[]>(() => []);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-
     async function listDir() {
+      exec(
+        `emulator -list-avds`,
+        (err: string, stdout: string, stderr: string) => {
+          console.log(err);
+          console.log(stdout);
+          console.log(stderr);
 
-      exec(`emulator -list-avds`, (err: string, stdout: string, stderr: string) => {
-        console.log(err);
-        console.log(stdout);
-        console.log(stderr);
+          //error
+          if (err != null) {
+            showToast(
+              Toast.Style.Failure,
+              "Make sure you have `emulator` command available globally"
+            );
+          }
 
-        //error
-        if (err != null) {
-          showToast(Toast.Style.Failure, "Make sure you have `emulator` command available globally")
+          //Success
+          if (stdout != null) {
+            const avds = stdout.split("\n").filter((p: string) => p.trim());
+            setItems(avds);
+          }
+
+          setLoading(false);
         }
-
-        //Success
-        if (stdout != null) {
-          const avds = stdout.split("\n").filter((p: string) => p.trim());
-          setItems(avds)
-        }
-
-        setLoading(false)
-      })
-
+      );
     }
 
-    listDir()
-
-  }, [])
+    listDir();
+  }, []);
 
   return (
     <List isLoading={loading}>
-      {
-        items?.map((emulator: string, index) => (
-
-          <List.Item
-            icon={{ source: "android-emulator.png" }}
-            key={index}
-            title={emulator}
-            accessories={[
-              { icon: Icon.Mobile },
-            ]}
-            actions={<ActionPanel>
-
-              <Action title="Open Emulator" onAction={() => openEmultror(emulator)} />
-
-            </ActionPanel>}
-          />
-        ))
-      }
-    </List>);
+      {items?.map((emulator: string, index) => (
+        <List.Item
+          icon={{ source: "android-emulator.png" }}
+          key={index}
+          title={emulator}
+          accessories={[{ icon: Icon.Mobile }]}
+          actions={
+            <ActionPanel>
+              <Action
+                title="Open Emulator"
+                onAction={() => openEmultror(emulator)}
+              />
+            </ActionPanel>
+          }
+        />
+      ))}
+    </List>
+  );
 }
 
 function openEmultror(emulator: string): void {
-  exec(`emulator -avd ${emulator}`, (err: string, stdout: string, stderr: string) => {
-    console.log(err);
-    console.log(stdout);
-    console.log(stderr);
+  exec(
+    `emulator -avd ${emulator}`,
+    (err: string, stdout: string, stderr: string) => {
+      console.log(err);
+      console.log(stdout);
+      console.log(stderr);
 
-    if (stderr != null) {
-      showToast(Toast.Style.Failure, stderr)
+      if (stderr != null) {
+        showToast(Toast.Style.Failure, stderr);
+      }
+      popToRoot;
     }
-    popToRoot
-  })
+  );
 }
