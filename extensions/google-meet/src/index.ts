d7ea7a82c2ce6fd42da7930c85cf53865958906c
Vitor Gomes <vitorgomes2007@hotmail.com>
2022-08-28 00:09:16 -0300

    feat: copy created meet url to clipboard

--- extensions/google-meet/src/index.ts
+++ extensions/google-meet/src/index.ts
@@ -1,5 +1,24 @@
-import { open } from "@raycast/api";
+import { getMeetTab, openMeetTab } from "./helpers";
+
+import { showHUD, Clipboard, showToast, Toast } from "@raycast/api";
 
 export default async function main() {
-  await open("https://meet.google.com/new");
+  try {
+    await showToast({
+      style: Toast.Style.Animated,
+      title: "Creating meeting...",
+    });
+
+    await openMeetTab();
+
+    const meetTab = await getMeetTab();
+
+    await Clipboard.copy(meetTab);
+    await showHUD("Copied meet link to clipboard");
+  } catch (err) {
+    await showToast({
+      style: Toast.Style.Failure,
+      title: "Couldn't copy to clipboard",
+    });
+  }
 }
