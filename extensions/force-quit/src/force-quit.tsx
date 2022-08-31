3c8edd7b1b15e04f2fe52e442a0a14ac959ff5a6
dannymout <dannymout@users.noreply.github.com>
2022-08-31 01:02:19 -0400

    ran prettier

--- extensions/force-quit/src/force-quit.tsx
+++ extensions/force-quit/src/force-quit.tsx
@@ -3,21 +3,21 @@ import { List, Action, ActionPanel, closeMainWindow } from "@raycast/api";
 import { exec } from "child_process";
 
 function getOpenApps() {
-    const apps = runAppleScriptSync(`
+  const apps = runAppleScriptSync(`
         tell application "System Events"
             set appList to the name of (every application process whose background only is false and name is not "Force Quit Application")
             return appList
         end tell
-    `)
+    `);
 
-    return apps.split(', ')
+  return apps.split(", ");
 }
 
 function forceQuit(app: string) {
   exec(`killall '${app}'`);
 }
 
-const apps = getOpenApps()
+const apps = getOpenApps();
 
 export default function Command() {
   return (
