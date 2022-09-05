69889438501cae72925c0ab4814473793f054372
Alberto Romero <aromeronavia@gmail.com>
2022-09-05 15:49:34 -0500

    Rename command to today's focus instead of today's frog

--- extensions/todoist/src/today-focus.tsx
+++ extensions/todoist/src/today-focus.tsx
@@ -5,18 +5,18 @@ import { SWRKeys } from "./types";
 
 const MAX_TASK_LENGTH = 45;
 
-export default function TodayFrog() {
+export default function TodayFocus() {
   const { data: tasks, error: getTasksError } = useSWR(SWRKeys.tasks, () =>
     todoist.getTasks({ filter: "today|overdue" })
   );
   const { data: labels, error: getLabelsError } = useSWR(SWRKeys.labels, () => todoist.getLabels());
 
   const preferences = getPreferenceValues();
-  const frogLabel = labels?.find((label) => label.name === preferences.frogLabelName);
-  const frogTask = tasks?.find((task) => frogLabel && task.labelIds.includes(frogLabel?.id));
+  const focusLabel = labels?.find((label) => label.name === preferences.focusLabelName);
+  const focusTask = tasks?.find((task) => focusLabel && task.labelIds.includes(focusLabel?.id));
 
   const getTitle = () => {
-    if (frogTask) return "🐸 " + frogTask.content;
+    if (focusTask) return "🎯 " + focusTask.content;
     if (tasks && tasks.length > 0) return tasks[0].content.substring(0, MAX_TASK_LENGTH);
     if (tasks && tasks.length === 0) return "🎉 No tasks left for today";
 
