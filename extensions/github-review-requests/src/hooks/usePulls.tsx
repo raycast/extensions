5d5d5cdc5f96f9b9c99e92074574b380dd95b0ab
Vitaliy Kudryk <contact@bgnfu7re.me>
2022-09-06 08:50:33 +0300

    prettier 🚨

--- extensions/github-review-requests/src/hooks/usePulls.tsx
+++ extensions/github-review-requests/src/hooks/usePulls.tsx
@@ -5,7 +5,7 @@ import { getLogin } from "../integration/getLogin";
 import { isActionUserInitiated } from "../util";
 import { processPulls } from "../flows/processPulls";
 import { PullRequestShort } from "../types";
-import {Icon, MenuBarExtra} from "@raycast/api";
+import { Icon, MenuBarExtra } from "@raycast/api";
 
 const usePulls = () => {
   const { isPullStoreLoading, updatedPulls, recentlyVisitedPulls, hiddenPulls, visitPull, updatePulls } =
@@ -58,12 +58,16 @@ const usePulls = () => {
     updatedPulls,
     recentlyVisitedPulls,
 
-    Refresh: isPullStoreLoading ? () => null : () => <MenuBarExtra.Item
-      title="Force Refresh"
-      onAction={runPullIteration}
-      icon={Icon.RotateClockwise}
-      shortcut={{ key: "r", modifiers: ["cmd"] }}
-    />,
+    Refresh: isPullStoreLoading
+      ? () => null
+      : () => (
+          <MenuBarExtra.Item
+            title="Force Refresh"
+            onAction={runPullIteration}
+            icon={Icon.RotateClockwise}
+            shortcut={{ key: "r", modifiers: ["cmd"] }}
+          />
+        ),
 
     visitPull,
   };
