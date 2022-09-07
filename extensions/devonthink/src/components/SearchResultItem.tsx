00a6111cb80701d467642d1c30727e0d26aa30a3
Vitaliy Kudryk <contact@bgnfu7re.me>
2022-09-07 14:34:06 +0300

    configure properties 🚧✨

--- extensions/devonthink/src/components/SearchResultItem.tsx
+++ extensions/devonthink/src/components/SearchResultItem.tsx
@@ -1,6 +1,6 @@
 import {Action, ActionPanel, getPreferenceValues, List} from "@raycast/api";
 import {SearchResult} from "../hooks/useSearch";
-import {Preferences} from "../types/Preferences";
+import {Preferences, PropertyKey} from "../types/Preferences";
 
 const SearchResultItem = ({result}: { result: SearchResult }) => (
   <List.Item
@@ -25,66 +25,66 @@ export default SearchResultItem;
 
 const searchResultMetadataItems = (result: SearchResult) => {
   return [
-    <MyLabel key="uuid" title="UUID" text={result.uuid}/>,
-    <MyLabel key="name" title="Name" text={result.name}/>,
-    <MyLabel key="path" title="Path" text={result.path}/>,
+    <MyLabel key="uuid" propKey="propertyUUID" title="UUID" text={result.uuid}/>,
+    <MyLabel key="name" propKey="propertyName" title="Name" text={result.name}/>,
+    <MyLabel key="path" propKey="propertyPath" title="Path" text={result.path}/>,
     <TagList key="tags" title="Tags">{result.tags.map((tag) => <Tag key={tag} text={tag}/>)}</TagList>,
-    <MyLabel key="geolocation" title="Geolocation" text={result.geolocation}/>,
-    <MyLabel key="date" title="Date" text={result.date}/>,
-    <MyLabel key="additionDate" title="Addition Date" text={result.additionDate}/>,
-    <MyLabel key="modificationDate" title="Modification Date" text={result.modificationDate}/>,
-    <MyLink key="url" title="URL" text={result.url} target={result.url}/>,
-    <MyLabel key="type" title="Type" text={result.type}/>,
-    <MyLabel key="contentHash" title="Content Hash" text={result.contentHash}/>,
-    <MyLabel key="location" title="Location" text={result.location}/>,
-    <MyLabel key="filename" title="Filename" text={result.filename}/>,
-    <MyLabel key="aliases" title="Aliases" text={result.aliases}/>,
-    <MyLink key="referenceURL" title="Reference URL" text="URL" target={result.referenceURL}/>,
-    <MyLabel key="comment" title="Comment" text={result.comment}/>,
-    <MyLabel key="tagType" title="Tag Type" text={result.tagType}/>,
-    <MyLabel key="locationGroup" title="Location Group" text={result.locationGroup}/>,
-    <MyLabel key="kind" title="Kind" text={result.kind}/>,
-    <MyLabel key="mimeType" title="MIME Type" text={result.mimeType}/>,
-    <MyLabel key="openingDate" title="Opening Date" text={result.openingDate}/>,
-    <MyLabel key="attachedScript" title="Attached Script" text={result.attachedScript}/>,
-    <MyLabel key="creationDate" title="Creation Date" text={result.creationDate}/>,
-    <MyLabel key="class" title="Class" text={result.class}/>,
+    <MyLabel key="geolocation" propKey="propertyGeolocation" title="Geolocation" text={result.geolocation}/>,
+    <MyLabel key="date" propKey="propertyDate" title="Date" text={result.date}/>,
+    <MyLabel key="additionDate" propKey="propertyAdditionDate" title="Addition Date" text={result.additionDate}/>,
+    <MyLabel key="modificationDate" propKey="propertyModificationDate" title="Modification Date" text={result.modificationDate}/>,
+    <MyLink key="url" propKey="propertyUrl" title="URL" text={result.url} target={result.url}/>,
+    <MyLabel key="type" propKey="propertyType" title="Type" text={result.type}/>,
+    <MyLabel key="contentHash" propKey="propertyContentHash" title="Content Hash" text={result.contentHash}/>,
+    <MyLabel key="location" propKey="propertyLocation" title="Location" text={result.location}/>,
+    <MyLabel key="filename" propKey="propertyFilename" title="Filename" text={result.filename}/>,
+    <MyLabel key="aliases" propKey="propertyAliases" title="Aliases" text={result.aliases}/>,
+    <MyLink key="referenceURL" propKey="propertyReferenceURL" title="Reference URL" text="URL" target={result.referenceURL}/>,
+    <MyLabel key="comment" propKey="propertyComment" title="Comment" text={result.comment}/>,
+    <MyLabel key="tagType" propKey="propertyTagType" title="Tag Type" text={result.tagType}/>,
+    <MyLabel key="locationGroup" propKey="propertyLocationGroup" title="Location Group" text={result.locationGroup}/>,
+    <MyLabel key="kind" propKey="propertyKind" title="Kind" text={result.kind}/>,
+    <MyLabel key="mimeType" propKey="propertyMimeType" title="MIME Type" text={result.mimeType}/>,
+    <MyLabel key="openingDate" propKey="propertyOpeningDate" title="Opening Date" text={result.openingDate}/>,
+    <MyLabel key="attachedScript" propKey="propertyAttachedScript" title="Attached Script" text={result.attachedScript}/>,
+    <MyLabel key="creationDate" propKey="propertyCreationDate" title="Creation Date" text={result.creationDate}/>,
+    <MyLabel key="class" propKey="propertyClass" title="Class" text={result.class}/>,
 
-    <MyLabel key="id" title="ID" text={"" + result.id}/>,
-    <MyLabel key="score" title="Score" text={"" + result.score}/>,
-    <MyLabel key="numberOfHits" title="Number of Hits" text={"" + result.numberOfHits}/>,
-    <MyLabel key="altitude" title="Altitude" text={"" + result.altitude}/>,
-    <MyLabel key="latitude" title="Latitude" text={"" + result.latitude}/>,
-    <MyLabel key="longitude" title="Longitude" text={"" + result.longitude}/>,
-    <MyLabel key="interval" title="Interval" text={"" + result.interval}/>,
-    <MyLabel key="numberOfReplicants" title="Number of Replicants" text={"" + result.numberOfReplicants}/>,
-    <MyLabel key="batesNumber" title="Bates Number" text={"" + result.batesNumber}/>,
-    <MyLabel key="annotationCount" title="Annotation Count" text={"" + result.annotationCount}/>,
-    <MyLabel key="characterCount" title="Character Count" text={"" + result.characterCount}/>,
-    <MyLabel key="rating" title="Rating" text={"" + result.rating}/>,
-    <MyLabel key="pageCount" title="Page Count" text={"" + result.pageCount}/>,
-    <MyLabel key="wordCount" title="Word Count" text={"" + result.wordCount}/>,
-    <MyLabel key="size" title="Size" text={"" + result.size}/>,
-    <MyLabel key="attachmentCount" title="Attachment Count" text={"" + result.attachmentCount}/>,
-    <MyLabel key="numberOfDuplicates" title="Number of Duplicates" text={"" + result.numberOfDuplicates}/>,
-    <MyLabel key="height" title="Height" text={"" + result.height}/>,
-    <MyLabel key="duration" title="Duration" text={"" + result.duration}/>,
-    <MyLabel key="dpi" title="DPI" text={"" + result.dpi}/>,
-    <MyLabel key="width" title="Width" text={"" + result.width}/>,
-    <MyLabel key="label" title="Label" text={"" + result.label}/>,
+    <MyLabel key="id" propKey="propertyID" title="ID" text={"" + result.id}/>,
+    <MyLabel key="score" propKey="propertyScore" title="Score" text={"" + result.score}/>,
+    <MyLabel key="numberOfHits" propKey="propertyNumberOfHits" title="Number of Hits" text={"" + result.numberOfHits}/>,
+    <MyLabel key="altitude" propKey="propertyAltitude" title="Altitude" text={"" + result.altitude}/>,
+    <MyLabel key="latitude" propKey="propertyLatitude" title="Latitude" text={"" + result.latitude}/>,
+    <MyLabel key="longitude" propKey="propertyLongitude" title="Longitude" text={"" + result.longitude}/>,
+    <MyLabel key="interval" propKey="propertyInterval" title="Interval" text={"" + result.interval}/>,
+    <MyLabel key="numberOfReplicants" propKey="propertyNumberOfReplicants" title="Number of Replicants" text={"" + result.numberOfReplicants}/>,
+    <MyLabel key="batesNumber" propKey="propertyBatesNumber" title="Bates Number" text={"" + result.batesNumber}/>,
+    <MyLabel key="annotationCount" propKey="propertyAnnotationCount" title="Annotation Count" text={"" + result.annotationCount}/>,
+    <MyLabel key="characterCount" propKey="propertyCharacterCount" title="Character Count" text={"" + result.characterCount}/>,
+    <MyLabel key="rating" propKey="propertyRating" title="Rating" text={"" + result.rating}/>,
+    <MyLabel key="pageCount" propKey="propertyPageCount" title="Page Count" text={"" + result.pageCount}/>,
+    <MyLabel key="wordCount" propKey="propertyWordCount" title="Word Count" text={"" + result.wordCount}/>,
+    <MyLabel key="size" propKey="propertySize" title="Size" text={"" + result.size}/>,
+    <MyLabel key="attachmentCount" propKey="propertyAttachmentCount" title="Attachment Count" text={"" + result.attachmentCount}/>,
+    <MyLabel key="numberOfDuplicates" propKey="propertyNumberOfDuplicates" title="Number of Duplicates" text={"" + result.numberOfDuplicates}/>,
+    <MyLabel key="height" propKey="propertyHeight" title="Height" text={"" + result.height}/>,
+    <MyLabel key="duration" propKey="propertyDuration" title="Duration" text={"" + result.duration}/>,
+    <MyLabel key="dpi" propKey="propertyDpi" title="DPI" text={"" + result.dpi}/>,
+    <MyLabel key="width" propKey="propertyWidth" title="Width" text={"" + result.width}/>,
+    <MyLabel key="label" propKey="propertyLabel" title="Label" text={"" + result.label}/>,
 
-    <MyLabel key="state" title="State" text={result.state ? "yes" : "no"}/>,
-    <MyLabel key="excludeFromTagging" title="Exclude from Tagging" text={result.excludeFromTagging ? "yes" : "no"}/>,
-    <MyLabel key="stateVisibility" title="State Visibility" text={result.stateVisibility ? "yes" : "no"}/>,
-    <MyLabel key="excludeFromWikiLinking" title="Exclude from Wiki Linking"
+    <MyLabel key="state" propKey="propertyState" title="State" text={result.state ? "yes" : "no"}/>,
+    <MyLabel key="excludeFromTagging" propKey="propertyExcludeFromTagging" title="Exclude from Tagging" text={result.excludeFromTagging ? "yes" : "no"}/>,
+    <MyLabel key="stateVisibility" propKey="propertyStateVisibility" title="State Visibility" text={result.stateVisibility ? "yes" : "no"}/>,
+    <MyLabel key="excludeFromWikiLinking" propKey="propertyExcludeFromWikiLinking" title="Exclude from Wiki Linking"
              text={result.excludeFromWikiLinking ? "yes" : "no"}/>,
-    <MyLabel key="excludeFromClassification" title="Exclude from Classification"
+    <MyLabel key="excludeFromClassification" propKey="propertyExcludeFromClassification" title="Exclude from Classification"
              text={result.excludeFromClassification ? "yes" : "no"}/>,
-    <MyLabel key="locking" title="Locking" text={result.locking ? "yes" : "no"}/>,
-    <MyLabel key="unread" title="Unread" text={result.unread ? "yes" : "no"}/>,
-    <MyLabel key="indexed" title="Indexed" text={result.indexed ? "yes" : "no"}/>,
-    <MyLabel key="excludeFromSeeAlso" title="Exclude from See Also" text={result.excludeFromSeeAlso ? "yes" : "no"}/>,
-    <MyLabel key="excludeFromSearch" title="Exclude from Search" text={result.excludeFromSearch ? "yes" : "no"}/>,
+    <MyLabel key="locking" propKey="propertyLocking" title="Locking" text={result.locking ? "yes" : "no"}/>,
+    <MyLabel key="unread" propKey="propertyUnread" title="Unread" text={result.unread ? "yes" : "no"}/>,
+    <MyLabel key="indexed" propKey="propertyIndexed" title="Indexed" text={result.indexed ? "yes" : "no"}/>,
+    <MyLabel key="excludeFromSeeAlso" propKey="propertyExcludeFromSeeAlso" title="Exclude from See Also" text={result.excludeFromSeeAlso ? "yes" : "no"}/>,
+    <MyLabel key="excludeFromSearch" propKey="propertyExcludeFromSearch" title="Exclude from Search" text={result.excludeFromSearch ? "yes" : "no"}/>,
   ];
 }
 
@@ -95,17 +95,23 @@ const Link = List.Item.Detail.Metadata.Link;
 
 const preferences: Preferences = getPreferenceValues();
 
-const MyLabel = ({title, text}: { title: string, text?: string }) => (
-  preferences.hideEmptySearchResultItemProperties && !text
-    ? null
-    : <Label title={title} text={text}/>
-)
+const MyLabel = ({propKey, title, text}: { propKey: PropertyKey, title: string, text?: string }) => {
+  if (!preferences[propKey]) {
+    return null;
+  }
 
-const MyLink = ({title, text, target}: { title: string, text: string, target?: string }) => {
+  return (
+    preferences.hideEmptySearchResultItemProperties && !text
+      ? null
+      : <Label title={title} text={text}/>
+  );
+}
+
+const MyLink = ({propKey, title, text, target}: { propKey: PropertyKey, title: string, text: string, target?: string }) => {
   if (!preferences.hideEmptySearchResultItemProperties) {
     return target
       ? <Link title={title} text={text} target={target}/>
-      : <MyLabel title={title} text="No URL" />;
+      : <MyLabel propKey={propKey} title={title} text="No URL" />;
   }
 
   if (!target) {
