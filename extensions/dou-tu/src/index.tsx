bb65c89335b0acfb6b456d13947d644947d0896d
gaoyang <xz@lgysh.cn>
2022-09-09 09:52:04 +0800

    run Prettier

--- extensions/dou-tu/src/index.tsx
+++ extensions/dou-tu/src/index.tsx
@@ -1,93 +1,95 @@
-import { useEffect, useState } from 'react'
-import { Grid } from '@raycast/api'
-import clipboardService from './services/clipboardService'
-import sourcesService from './services/sourcesService'
-import { IDoutuImage } from './services/sources'
-import { v4 as uuidv4 } from 'uuid'
+import { useEffect, useState } from "react";
+import { Grid } from "@raycast/api";
+import clipboardService from "./services/clipboardService";
+import sourcesService from "./services/sourcesService";
+import { IDoutuImage } from "./services/sources";
+import { v4 as uuidv4 } from "uuid";
 
-let currentKeyword = ''
-let currentPageIndex = 1
-let awaitRequest = false
+let currentKeyword = "";
+let currentPageIndex = 1;
+let awaitRequest = false;
 
 export default function Command() {
-  const [isLoading, setIsLoading] = useState(true)
-  const [isEnd, setIsEnd] = useState(true)
-  const [selectedItemId, setSelectedItemId] = useState('placeholder_1')
-  const [list, setList] = useState<IDoutuImage[]>([])
+  const [isLoading, setIsLoading] = useState(true);
+  const [isEnd, setIsEnd] = useState(true);
+  const [selectedItemId, setSelectedItemId] = useState("placeholder_1");
+  const [list, setList] = useState<IDoutuImage[]>([]);
 
   useEffect(() => {
-    more()
-  }, [])
+    more();
+  }, []);
 
   const more = async () => {
-    setIsLoading(true)
-    awaitRequest = true
-    const placeholderItem = { id: `placeholder_${currentPageIndex}_${uuidv4()}`, url: '' }
-    const result = await sourcesService.get(currentKeyword, currentPageIndex++)
-    setIsEnd(result.isEnd)
+    setIsLoading(true);
+    awaitRequest = true;
+    const placeholderItem = { id: `placeholder_${currentPageIndex}_${uuidv4()}`, url: "" };
+    const result = await sourcesService.get(currentKeyword, currentPageIndex++);
+    setIsEnd(result.isEnd);
     if (result.images.length === 0) {
-      currentPageIndex = -1
-      setList([])
+      currentPageIndex = -1;
+      setList([]);
     } else {
-      setList([...(currentPageIndex === 2 ? [] : list), placeholderItem, ...result.images])
-      setSelectedItemId(placeholderItem.id)
+      setList([...(currentPageIndex === 2 ? [] : list), placeholderItem, ...result.images]);
+      setSelectedItemId(placeholderItem.id);
     }
-    setIsLoading(false)
-  }
+    setIsLoading(false);
+  };
 
   const searchBarAccessory = (
     <Grid.Dropdown
       tooltip="Select Source"
       storeValue={true}
-      onChange={sourceName => {
-        if (sourceName === sourcesService.getSource()?.name) return
-        sourcesService.changeSource(sourceName)
-        currentPageIndex = 1
-        more()
-      }}>
+      onChange={(sourceName) => {
+        if (sourceName === sourcesService.getSource()?.name) return;
+        sourcesService.changeSource(sourceName);
+        currentPageIndex = 1;
+        more();
+      }}
+    >
       <Grid.Dropdown.Section title="Emoji Categories">
         {sourcesService.sources.map((source, index) => (
           <Grid.Dropdown.Item key={index} title={source.name} value={source.name} />
         ))}
       </Grid.Dropdown.Section>
     </Grid.Dropdown>
-  )
+  );
 
   return (
     <Grid
       throttle={true}
       isLoading={isLoading}
       selectedItemId={selectedItemId}
-      onSearchTextChange={keyword => {
-        currentKeyword = keyword
-        currentPageIndex = 1
-        more()
+      onSearchTextChange={(keyword) => {
+        currentKeyword = keyword;
+        currentPageIndex = 1;
+        more();
       }}
-      onSelectionChange={async id => {
-        if (!id) return
-        if (id.startsWith('placeholder_')) {
-          awaitRequest = false
-          return
+      onSelectionChange={async (id) => {
+        if (!id) return;
+        if (id.startsWith("placeholder_")) {
+          awaitRequest = false;
+          return;
         }
-        if (awaitRequest) return
-        if (id === 'more') {
-          currentPageIndex > 0 && more()
-          return
+        if (awaitRequest) return;
+        if (id === "more") {
+          currentPageIndex > 0 && more();
+          return;
         }
-        const item = list.find(o => o.id === id)
-        item && clipboardService.imageToClipboard(item.url)
+        const item = list.find((o) => o.id === id);
+        item && clipboardService.imageToClipboard(item.url);
       }}
-      searchBarAccessory={searchBarAccessory}>
+      searchBarAccessory={searchBarAccessory}
+    >
       {list.map((item, index) =>
-        item.id.startsWith('placeholder_') ? (
+        item.id.startsWith("placeholder_") ? (
           <Grid.Item
             key={index}
             id={item.id.toString()}
             content={{
-              tooltip: 'Click images copy',
+              tooltip: "Click images copy",
               value: {
-                source: 'click.png'
-              }
+                source: "click.png",
+              },
             }}
           />
         ) : (
@@ -97,8 +99,8 @@ export default function Command() {
       {isEnd ? (
         <></>
       ) : (
-        <Grid.Item key="more" id="more" content={{ tooltip: 'Click more', value: { source: 'more.png' } }} />
+        <Grid.Item key="more" id="more" content={{ tooltip: "Click more", value: { source: "more.png" } }} />
       )}
     </Grid>
-  )
+  );
 }
--- extensions/dou-tu/src/services/clipboardService.ts
+++ extensions/dou-tu/src/services/clipboardService.ts
@@ -1,29 +1,29 @@
-import { environment, showHUD, showToast, Toast } from '@raycast/api'
-import fetch from 'node-fetch'
-import fs from 'fs'
-import proc from 'child_process'
+import { environment, showHUD, showToast, Toast } from "@raycast/api";
+import fetch from "node-fetch";
+import fs from "fs";
+import proc from "child_process";
 
 export default {
   imageToClipboard: async (imageUrl: string) => {
     const toast = await showToast({
       style: Toast.Style.Animated,
-      title: 'Waiting',
-      message: 'Being copied to the clipboard...'
-    })
-    const tempDir = `${environment.assetsPath}/temp`
-    if (!fs.existsSync(tempDir)) await fs.mkdirSync(tempDir)
+      title: "Waiting",
+      message: "Being copied to the clipboard...",
+    });
+    const tempDir = `${environment.assetsPath}/temp`;
+    if (!fs.existsSync(tempDir)) await fs.mkdirSync(tempDir);
     else {
-      const files = fs.readdirSync(tempDir)
-      if (files.length >= 10) files.forEach(file => fs.unlinkSync(`${tempDir}/${file}`))
+      const files = fs.readdirSync(tempDir);
+      if (files.length >= 10) files.forEach((file) => fs.unlinkSync(`${tempDir}/${file}`));
     }
 
-    const fileName = imageUrl.substring(imageUrl.lastIndexOf('/') + 1)
-    const response = await fetch(imageUrl)
-    const data = await response.arrayBuffer()
-    const filePath = `${tempDir}/${fileName}`
-    await fs.writeFileSync(filePath, new Uint8Array(data), 'binary')
-    proc.exec(`osascript -e 'set the clipboard to (read (POSIX file "${filePath}") as JPEG picture)'`)
-    toast.hide()
-    showHUD('Already copied to clipboard.(Command + V)')
-  }
-}
+    const fileName = imageUrl.substring(imageUrl.lastIndexOf("/") + 1);
+    const response = await fetch(imageUrl);
+    const data = await response.arrayBuffer();
+    const filePath = `${tempDir}/${fileName}`;
+    await fs.writeFileSync(filePath, new Uint8Array(data), "binary");
+    proc.exec(`osascript -e 'set the clipboard to (read (POSIX file "${filePath}") as JPEG picture)'`);
+    toast.hide();
+    showHUD("Already copied to clipboard.(Command + V)");
+  },
+};
