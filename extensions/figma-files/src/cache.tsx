0325ee6fd5ca1660df02cc4464748a1fe6302602
Michael Schultz <michaelwschultz@gmail.com>
2022-09-12 05:54:41 -0700

    [Figma] new layout, menubar command, and updates depreciated APIs (#2563)
    * bump raycast version and replace deprecated apis
    
    * Adds detail view for larger thumbnails
    
    * updates changelog and adds menubar app
    
    * adds ability to open files in app from menu bar
    
    * Adds recent files to menubar
    
    * remove detail toggle, add dark icon, fix flicker on open button
    
    * Update screenshots, readme, menubar icons
    
    * Update extensions/figma-files/src/menu-bar.tsx
    
    Co-authored-by: Sorin Muntean <sxn@users.noreply.github.com>
    
    * Replaces List with Grid
    
    * fix visitedFiles bug and lint project
    
    * adds update interval to menu-bar command
    
    * fix linting errors
    
    * await `visitFile` in menu bar command
    
    * ran npm audit fix
    
    * Adds missing text to grid item if thumbnail is missing
    
    * Updated more deps to fix type error but no luck
    
    * Update CHANGELOG.md
    
    Co-authored-by: Sorin Muntean <sxn@users.noreply.github.com>
    Co-authored-by: Sorin Muntean <me@sorinmuntean.ro>
    Co-authored-by: Per Nielsen Tikær <per@raycast.com>

--- extensions/figma-files/src/cache.tsx
+++ extensions/figma-files/src/cache.tsx
@@ -1,4 +1,4 @@
-import { setLocalStorageItem, getLocalStorageItem, removeLocalStorageItem } from "@raycast/api";
+import { LocalStorage } from "@raycast/api";
 import type { File, ProjectFiles, Node } from "./types";
 
 const PROJECT_FILES_CACHE_KEY = "PROJECT_FILES";
@@ -6,24 +6,24 @@ const PAGES_CACHE_KEY = "PAGES";
 
 export async function storeFiles(projectFiles: ProjectFiles[]) {
   const data = JSON.stringify(projectFiles);
-  await setLocalStorageItem(PROJECT_FILES_CACHE_KEY, data);
+  await LocalStorage.setItem(PROJECT_FILES_CACHE_KEY, data);
 }
 
 export async function loadFiles() {
-  const data: string | undefined = await getLocalStorageItem(PROJECT_FILES_CACHE_KEY);
+  const data: string | undefined = await LocalStorage.getItem(PROJECT_FILES_CACHE_KEY);
   return data !== undefined ? JSON.parse(data) : undefined;
 }
 
 export async function clearFiles() {
-  return await removeLocalStorageItem(PROJECT_FILES_CACHE_KEY);
+  return await LocalStorage.removeItem(PROJECT_FILES_CACHE_KEY);
 }
 
 export async function storePages(pages: Node[], file: File) {
   const data = JSON.stringify(pages);
-  await setLocalStorageItem(`${PAGES_CACHE_KEY}-${file.key}`, data);
+  await LocalStorage.setItem(`${PAGES_CACHE_KEY}-${file.key}`, data);
 }
 
 export async function loadPages(file: File) {
-  const data: string | undefined = await getLocalStorageItem(`${PAGES_CACHE_KEY}-${file.key}`);
+  const data: string | undefined = await LocalStorage.getItem(`${PAGES_CACHE_KEY}-${file.key}`);
   return data !== undefined ? JSON.parse(data) : undefined;
 }
--- extensions/figma-files/src/components/FileGridItem.tsx
+++ extensions/figma-files/src/components/FileGridItem.tsx
@@ -1,31 +1,24 @@
-import { ActionPanel, List, CopyToClipboardAction } from "@raycast/api";
-import TimeAgo from "javascript-time-ago";
-import en from "javascript-time-ago/locale/en.json";
+import { Action, ActionPanel, Grid } from "@raycast/api";
 
 import type { File } from "../types";
 import DevelopmentActionSection from "./DevelopmentActionSection";
 import { OpenProjectFileAction } from "./OpenProjectFileAction";
 import { OpenPageSubmenuAction } from "./OpenPageSubmenuAction";
 
-TimeAgo.addDefaultLocale(en);
-const timeAgo = new TimeAgo("en-US");
-
-export default function FileListItem(props: { file: File; extraKey?: string; onVisit: (file: File) => void }) {
+export default function FileGridItem(props: { file: File; extraKey?: string; onVisit: (file: File) => void }) {
   const { file, extraKey, onVisit } = props;
-
-  const accessoryTitle = String(timeAgo.format(new Date(file.last_modified)));
   const fileIdentifier = extraKey ? `${file.key}-${extraKey}` : file.key;
+
   return (
-    <List.Item
+    <Grid.Item
       id={fileIdentifier}
       title={file.name}
-      icon={file.thumbnail_url}
-      accessoryTitle={accessoryTitle}
+      content={file.thumbnail_url ?? "Missing thumbnail"}
       actions={
         <ActionPanel>
           <ActionPanel.Section>
             <OpenProjectFileAction file={props.file} onVisit={onVisit} />
-            <CopyToClipboardAction content={`https://figma.com/file/${file.key}`} />
+            <Action.CopyToClipboard content={`https://figma.com/file/${file.key}`} />
           </ActionPanel.Section>
           <ActionPanel.Section>
             <OpenPageSubmenuAction file={props.file} onVisit={onVisit} />
