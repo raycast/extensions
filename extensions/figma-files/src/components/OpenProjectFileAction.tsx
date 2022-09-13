adffac72529bd68e529349e9a90b5386e724bf40
Timo Bechtel <hi@timobechtel.com>
2022-09-13 23:42:07 +0200

    fix(figma): always opening desktop app
    fixes issue where figma file search would always try to open files in the desktop app, even when the
    app is not installed

--- extensions/figma-files/src/components/OpenProjectFileAction.tsx
+++ extensions/figma-files/src/components/OpenProjectFileAction.tsx
@@ -1,18 +1,17 @@
 import { Action } from "@raycast/api";
-import { useCallback } from "react";
 import { useFigmaApp } from "../hooks/useFigmaApp";
 
 import type { File } from "../types";
 
 export function OpenProjectFileAction(props: { file: File; onVisit: (file: File) => void }) {
-  const desktopApp = useCallback(() => useFigmaApp(), []);
+  const desktopApp = useFigmaApp();
 
   return desktopApp ? (
     <Action.Open
       icon="command-icon.png"
       title="Open in Figma"
       target={`figma://file/${props.file.key}`}
-      application={desktopApp()}
+      application={desktopApp}
       onOpen={() => props.onVisit(props.file)}
     />
   ) : (
