00c86ab8bf40e01b69e567c1b9eff4281ec89b95
Michael Wang <hi@michael.wang>
2022-09-14 14:46:19 -0700

    run lint

--- extensions/github-codespaces/src/active-codespaces.tsx
+++ extensions/github-codespaces/src/active-codespaces.tsx
@@ -4,72 +4,99 @@ import { personalAccessToken, preferredEditor } from "./preferences";
 import { Codespaces } from "./types";
 
 export default function Command() {
-	const { data, isLoading } = useFetch<Codespaces>("https://api.github.com/user/codespaces", {
-		headers: {
-			Accept: "application/vnd.github+json",
-			Authorization: `Bearer ${personalAccessToken}`,
-		},
-	});
+  const { data, isLoading, revalidate } = useFetch<Codespaces>(
+    "https://api.github.com/user/codespaces",
+    {
+      headers: {
+        Accept: "application/vnd.github+json",
+        Authorization: `Bearer ${personalAccessToken}`,
+      },
+    }
+  );
 
-	if (!data) {
-		return null;
-	}
+  if (!data) {
+    return (
+      <MenuBarExtra
+        icon={{ source: { light: "github.png", dark: "github@dark.png" } }}
+        isLoading={true}
+      />
+    );
+  }
 
-	const activeCodespaces = data.codespaces.filter((codespace) => codespace.state === "Available");
-	const recentCodespaces = data.codespaces
-		.filter((codespace) => codespace.state === "Shutdown")
-		.sort((a, b) => new Date(b.last_used_at).getTime() - new Date(a.last_used_at).getTime())
-		.slice(0, 5);
+  const activeCodespaces = data.codespaces.filter(
+    (codespace) => codespace.state === "Available"
+  );
+  const recentCodespaces = data.codespaces
+    .filter((codespace) => codespace.state === "Shutdown")
+    .sort(
+      (a, b) =>
+        new Date(b.last_used_at).getTime() - new Date(a.last_used_at).getTime()
+    )
+    .slice(0, 5);
 
-	return (
-		<MenuBarExtra
-			isLoading={isLoading}
-			icon={{ source: { light: "github.png", dark: "github@dark.png" } }}
-			title={activeCodespaces.length ? `${activeCodespaces.length} active` : undefined}
-			tooltip="Active Codespaces"
-		>
-			<MenuBarExtra.Item title="Active" />
-			{activeCodespaces.map((codespace) => (
-				<MenuBarExtra.Item
-					key={codespace.id}
-					icon={{
-						source: Icon.Dot,
-						tintColor: "green",
-					}}
-					title={`${codespace.display_name || codespace.name} • ${codespace.repository.owner.login}/${
-						codespace.repository.name
-					}`}
-					onAction={() => {
-						if (preferredEditor === "web") {
-							open(codespace.web_url);
-						} else {
-							open(`vscode://github.codespaces/connect?name=${codespace.name}&windowId=_blank`);
-						}
-					}}
-					tooltip={`Launch in ${preferredEditor === "web" ? "web editor" : "VS Code"}`}
-				/>
-			))}
-			<MenuBarExtra.Item title="Recent" />
-			{recentCodespaces.map((codespace) => (
-				<MenuBarExtra.Item
-					key={codespace.id}
-					title={`${codespace.display_name || codespace.name} • ${codespace.repository.owner.login}/${
-						codespace.repository.name
-					}`}
-					icon={{
-						source: Icon.Dot,
-						tintColor: "grey",
-					}}
-					onAction={() => {
-						if (preferredEditor === "web") {
-							open(codespace.web_url);
-						} else {
-							open(`vscode://github.codespaces/connect?name=${codespace.name}&windowId=_blank`);
-						}
-					}}
-					tooltip={`Launch in ${preferredEditor === "web" ? "web editor" : "VS Code"}`}
-				/>
-			))}
-		</MenuBarExtra>
-	);
+  return (
+    <MenuBarExtra
+      isLoading={isLoading}
+      icon={{ source: { light: "github.png", dark: "github@dark.png" } }}
+      title={
+        activeCodespaces.length
+          ? `${activeCodespaces.length} active`
+          : undefined
+      }
+      tooltip="Active Codespaces"
+    >
+      <MenuBarExtra.Item
+        title={activeCodespaces.length ? "Active" : "No active codespaces"}
+      />
+      {activeCodespaces.map((codespace) => (
+        <MenuBarExtra.Item
+          key={codespace.id}
+          icon={{
+            source: Icon.Dot,
+            tintColor: "green",
+          }}
+          title={`${codespace.display_name || codespace.name} • ${
+            codespace.repository.owner.login
+          }/${codespace.repository.name}`}
+          onAction={() => {
+            if (preferredEditor === "web") {
+              open(codespace.web_url);
+            } else {
+              open(
+                `vscode://github.codespaces/connect?name=${codespace.name}&windowId=_blank`
+              );
+            }
+          }}
+          tooltip={`Launch in ${
+            preferredEditor === "web" ? "web editor" : "VS Code"
+          }`}
+        />
+      ))}
+      <MenuBarExtra.Item title="Recent" />
+      {recentCodespaces.map((codespace) => (
+        <MenuBarExtra.Item
+          key={codespace.id}
+          title={`${codespace.display_name || codespace.name} • ${
+            codespace.repository.owner.login
+          }/${codespace.repository.name}`}
+          icon={{
+            source: Icon.Dot,
+            tintColor: "grey",
+          }}
+          onAction={() => {
+            if (preferredEditor === "web") {
+              open(codespace.web_url);
+            } else {
+              open(
+                `vscode://github.codespaces/connect?name=${codespace.name}&windowId=_blank`
+              );
+            }
+          }}
+          tooltip={`Launch in ${
+            preferredEditor === "web" ? "web editor" : "VS Code"
+          }`}
+        />
+      ))}
+    </MenuBarExtra>
+  );
 }
--- extensions/github-codespaces/src/open-codespace.tsx
+++ extensions/github-codespaces/src/open-codespace.tsx
@@ -6,76 +6,106 @@ import { Codespaces } from "./types";
 import CodespaceActions from "./actions/CodespaceActions";
 
 export default function Command() {
-	const { data, isLoading, revalidate } = useFetch<Codespaces>("https://api.github.com/user/codespaces", {
-		headers: {
-			Accept: "application/vnd.github+json",
-			Authorization: `Bearer ${personalAccessToken}`,
-		},
-	});
+  const { data, isLoading, revalidate } = useFetch<Codespaces>(
+    "https://api.github.com/user/codespaces",
+    {
+      headers: {
+        Accept: "application/vnd.github+json",
+        Authorization: `Bearer ${personalAccessToken}`,
+      },
+    }
+  );
 
-	const handleRevalidate = revalidate;
+  const handleRevalidate = revalidate;
 
-	return (
-		<List isLoading={isLoading}>
-			{data?.codespaces
-				.sort((a, b) => new Date(b.last_used_at).getTime() - new Date(a.last_used_at).getTime())
-				.map((codespace) => {
-					const gitStatus = {
-						icon: Icon.ArrowUp,
-						text: `${codespace.git_status.ahead}${codespace.git_status.has_uncommitted_changes ? "+" : ""}`,
-						tooltip: codespace.git_status.has_uncommitted_changes
-							? codespace.git_status.ahead
-								? `You have ${codespace.git_status.ahead} unpushed commits as well as other uncommitted changes`
-								: `You have uncommitted changes`
-							: codespace.git_status.has_unpushed_changes
-							? `You have ${codespace.git_status.ahead} unpushed commits`
-							: undefined,
-					};
-					return (
-						<List.Item
-							key={codespace.id}
-							icon={match(codespace.state)
-								.with(P.union("Unknown"), () => Icon.QuestionMarkCircle)
-								.with(
-									P.union("Queued", "Provisioning", "Awaiting", "Moved", "ShuttingDown", "Exporting", "Updating", "Rebuilding"),
-									() => Icon.Clock
-								)
-								.with(P.union("Available", "Created", "Starting", "Shutdown"), () =>
-									getAvatarIcon(`${codespace.repository.name.toUpperCase()}`)
-								)
-								.with(P.union("Unavailable", "Deleted", "Archived", "Failed"), () => Icon.XMarkCircle)
-								.exhaustive()}
-							title={codespace.display_name || codespace.name}
-							keywords={[
-								`${codespace.repository.owner.login}/${codespace.repository.name}`,
-								codespace.name,
-								codespace.repository.name,
-							]}
-							subtitle={`${codespace.repository.owner.login}/${codespace.repository.name}`}
-							accessories={[
-								gitStatus,
-								{
-									icon: Icon.ComputerChip,
-									tooltip: codespace.machine?.display_name,
-									text: `${codespace.machine?.cpus}-core`,
-								},
-								codespace.state === "Available"
-									? {
-											icon: {
-												source: Icon.Dot,
-												tintColor: "green",
-											},
-											text: "Active",
-									  }
-									: {
-											date: new Date(codespace.last_used_at),
-											tooltip: `Last used at: ${new Date(codespace.last_used_at).toLocaleString()}`,
-									  },
-							]}
-							actions={<CodespaceActions codespace={codespace} onRevalidate={handleRevalidate} />}
-						/>
-					);
-				})}
-		</List>
-	);
+  return (
+    <List isLoading={isLoading}>
+      {data?.codespaces
+        .sort(
+          (a, b) =>
+            new Date(b.last_used_at).getTime() -
+            new Date(a.last_used_at).getTime()
+        )
+        .map((codespace) => {
+          const gitStatus = {
+            icon: Icon.ArrowUp,
+            text: `${codespace.git_status.ahead}${
+              codespace.git_status.has_uncommitted_changes ? "+" : ""
+            }`,
+            tooltip: codespace.git_status.has_uncommitted_changes
+              ? codespace.git_status.ahead
+                ? `You have ${codespace.git_status.ahead} unpushed commits as well as other uncommitted changes`
+                : `You have uncommitted changes`
+              : codespace.git_status.has_unpushed_changes
+              ? `You have ${codespace.git_status.ahead} unpushed commits`
+              : undefined,
+          };
+          return (
+            <List.Item
+              key={codespace.id}
+              icon={match(codespace.state)
+                .with(P.union("Unknown"), () => Icon.QuestionMarkCircle)
+                .with(
+                  P.union(
+                    "Queued",
+                    "Provisioning",
+                    "Awaiting",
+                    "Moved",
+                    "ShuttingDown",
+                    "Exporting",
+                    "Updating",
+                    "Rebuilding"
+                  ),
+                  () => Icon.Clock
+                )
+                .with(
+                  P.union("Available", "Created", "Starting", "Shutdown"),
+                  () =>
+                    getAvatarIcon(`${codespace.repository.name.toUpperCase()}`)
+                )
+                .with(
+                  P.union("Unavailable", "Deleted", "Archived", "Failed"),
+                  () => Icon.XMarkCircle
+                )
+                .exhaustive()}
+              title={codespace.display_name || codespace.name}
+              keywords={[
+                `${codespace.repository.owner.login}/${codespace.repository.name}`,
+                codespace.name,
+                codespace.repository.name,
+              ]}
+              subtitle={`${codespace.repository.owner.login}/${codespace.repository.name}`}
+              accessories={[
+                gitStatus,
+                {
+                  icon: Icon.ComputerChip,
+                  tooltip: codespace.machine?.display_name,
+                  text: `${codespace.machine?.cpus}-core`,
+                },
+                codespace.state === "Available"
+                  ? {
+                      icon: {
+                        source: Icon.Dot,
+                        tintColor: "green",
+                      },
+                      text: "Active",
+                    }
+                  : {
+                      date: new Date(codespace.last_used_at),
+                      tooltip: `Last used at: ${new Date(
+                        codespace.last_used_at
+                      ).toLocaleString()}`,
+                    },
+              ]}
+              actions={
+                <CodespaceActions
+                  codespace={codespace}
+                  onRevalidate={handleRevalidate}
+                />
+              }
+            />
+          );
+        })}
+    </List>
+  );
 }
