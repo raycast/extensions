f8ff696975080afc3c6c336d6b2b9e0dc9bd8a96
Ilya Radchenko <knownasilya@gmail.com>
2022-09-15 09:32:20 -0400

    feat: add details and comments for my tasks

--- extensions/teamgantt/src/my-tasks.tsx
+++ extensions/teamgantt/src/my-tasks.tsx
@@ -1,10 +1,12 @@
-import { List, getPreferenceValues, Icon, ActionPanel, Action } from '@raycast/api';
+import { List, getPreferenceValues, Icon, ActionPanel, Action, useNavigation } from '@raycast/api';
 import { useEffect, useState } from 'react';
 import { MyPreferences, Task, TimeBlock, User } from './types';
 import { authenticate, findTasks, getCurrentTimeBlock, getCurrentUser, punchIn, punchOut } from './api';
 import { groupTasksByProject } from './utils';
+import TaskDetails from './task-details';
 
 export default function Command() {
+  const { push } = useNavigation();
   const [idToken, setIdToken] = useState<string>();
   const [isLoading, setIsLoading] = useState(true);
   const [tasks, setTasks] = useState<Task[]>([]);
@@ -64,15 +66,33 @@ export default function Command() {
                 title={task.name}
                 subtitle={task.parent_group_name}
                 keywords={keywords}
+                accessoryTitle={keywords.join(', ')}
+                accessoryIcon={task.is_starred ? Icon.Star : undefined}
                 actions={
                   <ActionPanel title={`'${task.name}' actions`}>
-                    <Action
-                      title="Punch Out"
-                      onAction={() => timeBlock && punchOut(timeBlock.id, undefined, idToken)}
-                    />
+                    <ActionPanel.Section title="Selected Task">
+                      <Action
+                        title="View Details"
+                        icon={Icon.ArrowRight}
+                        onAction={() => push(<TaskDetails task={task} idToken={idToken} />)}
+                      />
 
-                    <Action title="Punch In" onAction={() => punchIn(task.id, idToken)} />
-                    {isPunchedIn && <Action.OpenInBrowser url={`https://app.teamgantt.com/my-tasks/edit/${task.id}`} />}
+                      <Action.OpenInBrowser
+                        url={`https://app.teamgantt.com/projects/list/edit/${task.id}?ids=${task.project_id}`}
+                      />
+                    </ActionPanel.Section>
+
+                    <ActionPanel.Section title="TimeSheets">
+                      <Action
+                        title="Punch Out"
+                        onAction={() => timeBlock && punchOut(timeBlock.id, undefined, idToken)}
+                      />
+
+                      <Action title="Punch In" onAction={() => punchIn(task.id, idToken)} />
+                      {isPunchedIn && (
+                        <Action.OpenInBrowser url={`https://app.teamgantt.com/my-tasks/edit/${task.id}`} />
+                      )}
+                    </ActionPanel.Section>
                   </ActionPanel>
                 }
               />
