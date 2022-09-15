0528d386bef8b3b51f61d80c9f87fc5182d8a351
Ilya Radchenko <knownasilya@gmail.com>
2022-09-15 00:00:35 -0400

    feat: show details in comments

--- extensions/teamgantt/src/comments.tsx
+++ extensions/teamgantt/src/comments.tsx
@@ -9,12 +9,24 @@ export default function Comments({ comments }: Props) {
   if (!comments.length) {
     return <Detail navigationTitle="Comments" markdown="No comments" />;
   }
+
+  const sortedComments = comments.sort((a, b) => (a.added_date > b.added_date ? -1 : 1));
+
   return (
-    <List navigationTitle="Comments">
-      {comments.map((comment) => {
+    <List navigationTitle="Comments" isShowingDetail={true}>
+      {sortedComments.map((comment) => {
         const addedBy = comment.added_by;
         const name = `${addedBy.first_name}${addedBy.last_name ?? ''}`;
-        return <List.Item title={name} subtitle={comment.added_date} icon={comment.is_read ? undefined : Icon.Dot} />;
+
+        return (
+          <List.Item
+            key={comment.added_date}
+            title={name}
+            subtitle={comment.added_date}
+            icon={comment.is_read ? undefined : Icon.Dot}
+            detail={<List.Item.Detail markdown={comment.message} />}
+          />
+        );
       })}
     </List>
   );
