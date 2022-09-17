fcffa334096dca2f8fd0dfb2407977e07f49bd9e
ran <skyran1278@gmail.com>
2022-09-17 19:10:15 +0800

    New Extension: Stretchly

---
+++ extensions/stretchly/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Stretchly Changelog
+
+## [Added Stretchly] - 2022-09-17
+
+- add command to call stretchly mini and long break
---
+++ extensions/stretchly/src/skip-to-next-long-break.tsx
@@ -0,0 +1,15 @@
+import { showHUD } from "@raycast/api";
+import { exec } from "child_process";
+import * as util from "util";
+
+const promiseExec = util.promisify(exec);
+
+export default async function main() {
+  try {
+    const { stdout, stderr } = await promiseExec("/Applications/Stretchly.app/Contents/MacOS/stretchly long");
+    console.log("stdout:", stdout);
+    console.log("stderr:", stderr);
+  } catch (error) {
+    await showHUD(`${error}`); // should contain code (exit code) and signal (that caused the termination).
+  }
+}
