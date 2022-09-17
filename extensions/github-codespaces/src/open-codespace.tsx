19a7f93bd593e0d98511a23ab4cfbc51aee36158
Michael Wang <hi@michael.wang>
2022-09-16 17:06:39 -0700

    add sorting

--- extensions/github-codespaces/src/open-codespace.tsx
+++ extensions/github-codespaces/src/open-codespace.tsx
@@ -1,9 +1,15 @@
-import { Icon, List } from "@raycast/api";
-import { getAvatarIcon, useFetch } from "@raycast/utils";
-import { match, P } from "ts-pattern";
+import { useState } from "react";
+import { List } from "@raycast/api";
+import { useFetch } from "@raycast/utils";
 import { personalAccessToken } from "./preferences";
 import { Codespaces } from "./types";
-import CodespaceActions from "./actions/CodespaceActions";
+import SortBy, { Criteria } from "./views/SortBy";
+import CodespaceItem from "./views/CodespaceItem";
+import {
+  groupByCompute,
+  groupByOwner,
+  groupByRepository,
+} from "./utils/groupBy";
 
 export default function Command() {
   const { data, isLoading, revalidate } = useFetch<Codespaces>(
@@ -15,98 +21,80 @@ export default function Command() {
       },
     }
   );
+  const [criteria, setCriteria] = useState<Criteria>("date");
 
   const handleRevalidate = revalidate;
+  const handleSortByChange = setCriteria;
 
-  return (
-    <List isLoading={isLoading}>
-      {data?.codespaces
+  if (!data?.codespaces) {
+    return <></>;
+  }
+
+  const ListByCriteria = {
+    date: () =>
+      data?.codespaces
         .sort(
           (a, b) =>
             new Date(b.last_used_at).getTime() -
             new Date(a.last_used_at).getTime()
         )
-        .map((codespace) => {
-          const gitStatus = {
-            icon: Icon.ArrowUp,
-            text: `${codespace.git_status.ahead}${
-              codespace.git_status.has_uncommitted_changes ? "+" : ""
-            }`,
-            tooltip: codespace.git_status.has_uncommitted_changes
-              ? codespace.git_status.ahead
-                ? `You have ${codespace.git_status.ahead} unpushed commits as well as other uncommitted changes`
-                : `You have uncommitted changes`
-              : codespace.git_status.has_unpushed_changes
-              ? `You have ${codespace.git_status.ahead} unpushed commits`
-              : undefined,
-          };
-          return (
-            <List.Item
+        .map((codespace) => (
+          <CodespaceItem
+            key={codespace.id}
+            codespace={codespace}
+            onRevalidate={handleRevalidate}
+          />
+        )),
+    repo: () => {
+      const groupedCodespaces = groupByRepository(data.codespaces);
+      return Object.keys(groupedCodespaces).map((group) => (
+        <List.Section key={group} title={group}>
+          {groupedCodespaces[group].map((codespace) => (
+            <CodespaceItem
               key={codespace.id}
-              icon={match(codespace.state)
-                .with(P.union("Unknown"), () => Icon.QuestionMarkCircle)
-                .with(
-                  P.union(
-                    "Queued",
-                    "Provisioning",
-                    "Awaiting",
-                    "Moved",
-                    "ShuttingDown",
-                    "Exporting",
-                    "Updating",
-                    "Rebuilding"
-                  ),
-                  () => Icon.Clock
-                )
-                .with(
-                  P.union("Available", "Created", "Starting", "Shutdown"),
-                  () =>
-                    getAvatarIcon(`${codespace.repository.name.toUpperCase()}`)
-                )
-                .with(
-                  P.union("Unavailable", "Deleted", "Archived", "Failed"),
-                  () => Icon.XMarkCircle
-                )
-                .exhaustive()}
-              title={codespace.display_name || codespace.name}
-              keywords={[
-                `${codespace.repository.owner.login}/${codespace.repository.name}`,
-                codespace.name,
-                codespace.repository.name,
-                codespace.git_status.ref || "",
-              ]}
-              subtitle={`${codespace.repository.owner.login}/${codespace.repository.name} • ${codespace.git_status.ref}`}
-              accessories={[
-                gitStatus,
-                {
-                  icon: Icon.ComputerChip,
-                  tooltip: codespace.machine?.display_name,
-                  text: `${codespace.machine?.cpus}-core`,
-                },
-                codespace.state === "Available"
-                  ? {
-                      icon: {
-                        source: Icon.Dot,
-                        tintColor: "green",
-                      },
-                      text: "Active",
-                    }
-                  : {
-                      date: new Date(codespace.last_used_at),
-                      tooltip: `Last used at: ${new Date(
-                        codespace.last_used_at
-                      ).toLocaleString()}`,
-                    },
-              ]}
-              actions={
-                <CodespaceActions
-                  codespace={codespace}
-                  onRevalidate={handleRevalidate}
-                />
-              }
+              codespace={codespace}
+              onRevalidate={handleRevalidate}
             />
-          );
-        })}
+          ))}
+        </List.Section>
+      ));
+    },
+    owner: () => {
+      const groupedCodespaces = groupByOwner(data.codespaces);
+      return Object.keys(groupedCodespaces).map((group) => (
+        <List.Section key={group} title={group}>
+          {groupedCodespaces[group].map((codespace) => (
+            <CodespaceItem
+              key={codespace.id}
+              codespace={codespace}
+              onRevalidate={handleRevalidate}
+            />
+          ))}
+        </List.Section>
+      ));
+    },
+    compute: () => {
+      const groupedCodespaces = groupByCompute(data.codespaces);
+      return Object.keys(groupedCodespaces).map((group) => (
+        <List.Section key={group} title={group}>
+          {groupedCodespaces[group].map((codespace) => (
+            <CodespaceItem
+              key={codespace.id}
+              codespace={codespace}
+              onRevalidate={handleRevalidate}
+            />
+          ))}
+        </List.Section>
+      ));
+    },
+  };
+
+  return (
+    <List
+      searchBarAccessory={<SortBy onSortByChange={handleSortByChange} />}
+      isLoading={isLoading}
+    >
+      {ListByCriteria[criteria]()}
     </List>
   );
 }
