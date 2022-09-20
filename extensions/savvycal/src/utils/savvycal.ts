9eb49a9ce29d2b0b351dfc168d2f2c783fc89c05
Per Nielsen Tikær <per@raycast.com>
2022-09-20 08:04:25 +0200

    Added new icon

--- extensions/savvycal/src/utils/savvycal.ts
+++ extensions/savvycal/src/utils/savvycal.ts
@@ -9,4 +9,8 @@ export const SAVVYCAL_BASE_URL = "https://savvycal.com";
 export const SAVVYCAL_API_BASE_URL = "https://api.savvycal.com";
 export const greenIcon = { source: Icon.CircleFilled, tintColor: Color.Green };
 export const redIcon = { source: Icon.CircleFilled, tintColor: Color.Red };
+export const clockIcon = {
+  source: Icon.Clock,
+  tintColor: Color.SecondaryText,
+};
 export const savvycalIcon = getFavicon("https://savvycal.com");
