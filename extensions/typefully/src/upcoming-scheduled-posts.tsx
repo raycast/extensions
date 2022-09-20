ee6ec55e884461a859b0684ddc5381c9b6320845
Per Nielsen Tikær <per@raycast.com>
2022-09-20 14:08:21 +0200

    Linted file

--- extensions/typefully/src/upcoming-scheduled-posts.tsx
+++ extensions/typefully/src/upcoming-scheduled-posts.tsx
@@ -2,61 +2,62 @@ import got from "got";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import { getPreferenceValues, MenuBarExtra, open } from "@raycast/api";
-import { useCachedPromise } from "@raycast/utils"
-import { Draft } from './interfaces/draft';
-import { Preferences } from './interfaces/preferences';
+import { useCachedPromise } from "@raycast/utils";
+import { Draft } from "./interfaces/draft";
+import { Preferences } from "./interfaces/preferences";
 
 dayjs.extend(relativeTime);
 
 interface ScheduledPostItemProps {
-    draft: Draft
+  draft: Draft;
 }
-
 
 const ScheduledPostItem = (props: ScheduledPostItemProps) => {
-    const maxTitleLength = 24;
-    const timeUntilPost = dayjs().to(dayjs(props.draft.scheduled_date));
+  const maxTitleLength = 24;
+  const timeUntilPost = dayjs().to(dayjs(props.draft.scheduled_date));
 
-    return <MenuBarExtra.Item
-        key={props.draft.id}
-        title={`${props.draft.text_first_tweet.slice(0, maxTitleLength - timeUntilPost.length)}... (${timeUntilPost})`}
-        onAction={() => open(`https://typefully.com/?d=${props.draft.id}`)}
-    />;
-}
+  return (
+    <MenuBarExtra.Item
+      key={props.draft.id}
+      title={`${props.draft.text_first_tweet.slice(0, maxTitleLength - timeUntilPost.length)}... (${timeUntilPost})`}
+      onAction={() => open(`https://typefully.com/?d=${props.draft.id}`)}
+    />
+  );
+};
 
 export default function Command() {
-    const preferences = getPreferenceValues<Preferences>();
-    const { isLoading, data } = useCachedPromise(
-        async (url: string): Promise<Array<Draft>> => {
-            const drafts: Array<Draft> = await got.get(url, {
-                headers: {
-                    "X-API-KEY": `Bearer ${preferences.token}`
-                },
-                responseType: 'json'
-            }).json();
+  const preferences = getPreferenceValues<Preferences>();
+  const { isLoading, data } = useCachedPromise(
+    async (url: string): Promise<Array<Draft>> => {
+      const drafts: Array<Draft> = await got
+        .get(url, {
+          headers: {
+            "X-API-KEY": `Bearer ${preferences.token}`,
+          },
+          responseType: "json",
+        })
+        .json();
 
-            const sortedDraftsByDate = drafts.sort((a: Draft, b: Draft) => {
-                return new Date(a.scheduled_date).getTime() - new Date(b.scheduled_date).getTime();
-            });
+      const sortedDraftsByDate = drafts.sort((a: Draft, b: Draft) => {
+        return new Date(a.scheduled_date).getTime() - new Date(b.scheduled_date).getTime();
+      });
 
-            return sortedDraftsByDate;
-        },
-        ["https://api.typefully.com/v1/drafts/recently-scheduled"],
-        {
-            keepPreviousData: true,
-        }
-    );
+      return sortedDraftsByDate;
+    },
+    ["https://api.typefully.com/v1/drafts/recently-scheduled"],
+    {
+      keepPreviousData: true,
+    }
+  );
 
-    return (
-        <MenuBarExtra icon="https://typefully.com/icon/favicon.ico" tooltip="Your Pull Requests">
-            <MenuBarExtra.Item title="Upcoming Scheduled Posts" />
-            {
-                isLoading ? (
-                    <MenuBarExtra.Item title="Loading..." />
-                ) : (data || []).map((draft) => (
-                    <ScheduledPostItem key={draft.id} draft={draft} />
-                ))
-            }
-        </MenuBarExtra>
-    );
+  return (
+    <MenuBarExtra icon="https://typefully.com/icon/favicon.ico" tooltip="Your Pull Requests">
+      <MenuBarExtra.Item title="Upcoming Scheduled Posts" />
+      {isLoading ? (
+        <MenuBarExtra.Item title="Loading..." />
+      ) : (
+        (data || []).map((draft) => <ScheduledPostItem key={draft.id} draft={draft} />)
+      )}
+    </MenuBarExtra>
+  );
 }
