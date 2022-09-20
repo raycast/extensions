318559493e99efdea3900829ecb03bdbd238533b
Per Nielsen Tikær <per@raycast.com>
2022-09-20 08:06:22 +0200

    Added durations and tooltip

--- extensions/savvycal/src/components/Link.tsx
+++ extensions/savvycal/src/components/Link.tsx
@@ -3,18 +3,32 @@ import { SchedulingLink } from "@/utils/types";
 import {
   SAVVYCAL_BASE_URL,
   greenIcon,
+  clockIcon,
   redIcon,
   savvycalIcon,
 } from "../utils/savvycal";
 import LinkActions from "./LinkActions";
 
 export default function Link(link: SchedulingLink) {
+  const isActive = link.state == "active";
+  const durations = link.durations.join(", ");
+
   return (
     <List.Item
       title={link.name}
       subtitle={SAVVYCAL_BASE_URL + "/" + link.scope.slug + "/" + link.slug}
       icon={savvycalIcon}
-      accessories={[{ icon: link.state == "active" ? greenIcon : redIcon }]} //TODO: there's probably a way to make this reactive
+      accessories={[
+        {
+          text: durations,
+          icon: clockIcon,
+          tooltip: "Available durations (In minutes)",
+        },
+        {
+          tooltip: isActive ? "Enabled" : "Disabled",
+          icon: isActive ? greenIcon : redIcon,
+        },
+      ]} //TODO: there's probably a way to make this reactive
       actions={<LinkActions {...link} />}
     />
   );
