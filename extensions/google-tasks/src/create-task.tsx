1dc6042c9f14fbf2cd145600fae390e99db8f7a2
elliotdes <elliotdesimons@gmail.com>
2022-09-21 21:08:28 +0100

    add handleCreate for create task view

--- extensions/google-tasks/src/create-task.tsx
+++ extensions/google-tasks/src/create-task.tsx
@@ -1,6 +1,23 @@
 import CreateTaskForm from "./components/CreateTaskForm";
 import { createTask } from "./api/endpoints";
+import { useCallback } from "react";
+import { showToast, Toast } from "@raycast/api";
+import { TaskForm } from "./types";
 
 export default function Command() {
-  return <CreateTaskForm onCreate={createTask} />;
+  const handleCreate = useCallback(
+    (listId: string, taskToCreate: TaskForm) => {
+      (async () => {
+        try {
+          await createTask(listId, taskToCreate);
+        } catch (error) {
+          console.error(error);
+          showToast({ style: Toast.Style.Failure, title: String(error) });
+        }
+      })();
+    },
+    [createTask]
+  );
+
+  return <CreateTaskForm onCreate={handleCreate} />;
 }
