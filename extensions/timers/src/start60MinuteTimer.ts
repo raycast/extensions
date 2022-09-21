f01315c7e6f05316a4711d6a1b7b451bda5f7807
ThatNerdSquared <72814106+ThatNerdSquared@users.noreply.github.com>
2022-09-21 12:16:07 -0700

    feat(Timers): cleanup, support API changes, persistent timers, stopwatches (#2858)
    * feat: Timers for Raycast v1.1
    
    * feat: add stopwatch copying, show timer data in MenuBarExtra
    
    * Update package.json
    
    Co-authored-by: Per Nielsen Tikær <per@raycast.com>

--- extensions/timers/src/start60MinuteTimer.ts
+++ extensions/timers/src/start60MinuteTimer.ts
@@ -1,8 +1,7 @@
-import { closeMainWindow, showHUD } from "@raycast/api";
+import { closeMainWindow } from "@raycast/api";
 import { startTimer } from "./timerUtils";
 
 export default async () => {
   await closeMainWindow();
-  await startTimer(60 * 60);
-  await showHUD("Timer started for 60 minutes! 🎉");
+  startTimer(60 * 60, "1 Hour Timer");
 };
--- extensions/timers/src/types.ts
+++ extensions/timers/src/types.ts
@@ -5,6 +5,13 @@ export interface Timer {
   originalFile: string;
 }
 
+export interface Stopwatch {
+  name: string;
+  timeStarted: Date;
+  timeElapsed: number;
+  originalFile: string;
+}
+
 export interface Values {
   hours: string;
   minutes: string;
@@ -20,4 +27,31 @@ export interface CustomTimer {
 
 export interface Preferences {
   selectedSound: string;
+  ringContinuously: boolean;
+  copyOnSwStop: boolean;
+}
+
+export interface CTInlineArgs {
+  hours: string;
+  minutes: string;
+  seconds: string;
+}
+
+export interface SWInlineArgs {
+  name: string;
+}
+
+export interface InputField {
+  id: keyof CTInlineArgs;
+  title: string;
+  placeholder: string;
+  err: string | undefined;
+  drop: () => void;
+  validator: (event: RayFormEvent) => void;
+}
+
+export interface RayFormEvent {
+  target: {
+    value?: string | undefined;
+  };
 }
