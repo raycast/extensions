f9c5767e19d66c2e38ffd83a7ea0c8bfa809fc6c
Per Nielsen Tikær <per@raycast.com>
2022-09-22 22:34:40 +0200

    Simplyfied the code with useForm

--- extensions/codesnap/src/index.tsx
+++ extensions/codesnap/src/index.tsx
@@ -1,78 +1,46 @@
-import { Form, ActionPanel, Action, Clipboard } from "@raycast/api";
-import { useState, useEffect } from "react";
-
+import { Form, ActionPanel, Action, Clipboard, open, Icon } from "@raycast/api";
+import { FormValidation, useForm } from "@raycast/utils";
+import { useEffect } from "react";
+interface FormValues {
+  name: string;
+  code: string;
+}
 export default function Command() {
-  const [nameError, setNameError] = useState<string | undefined>();
-  const [codeError, setCodeError] = useState<string | undefined>();
-  const [name, setName] = useState<string>("CodeSnap Raycast Snippet");
-  const [code, setCode] = useState<string>("");
-  const [url, setUrl] = useState<string>("");
-
+  const { handleSubmit, itemProps, setValue } = useForm<FormValues>({
+    onSubmit(values) {
+      open(
+        `https://codesnap.dev/editor?code=${encodeURIComponent(values.code)}&name=${encodeURIComponent(
+          values.name
+        )}&tag=Raycast`
+      );
+    },
+    initialValues: {
+      name: "CodeSnap Raycast Snippet",
+    },
+    validation: {
+      name: FormValidation.Required,
+      code: FormValidation.Required,
+    },
+  });
   useEffect(() => {
     Clipboard.readText().then((text) => {
-      setCode(text ?? "");
+      if (!text) {
+        return;
+      }
+      setValue("code", text);
     });
-  });
-
-  useEffect(() => {
-    setUrl(`https://codesnap.dev/editor?code=${encodeURIComponent(code)}&name=${encodeURIComponent(name)}&tag=Raycast`);
-  }, [name, code]);
-
-  function dropNameErrorIfNeeded() {
-    if (nameError && nameError.length > 0) {
-      setNameError(undefined);
-    }
-  }
-
-  function dropCodeErrorIfNeeded() {
-    if (codeError && codeError.length > 0) {
-      setCodeError(undefined);
-    }
-  }
-
+  }, []);
   return (
     <Form
       actions={
         <ActionPanel>
-          <Action.OpenInBrowser url={url} />
+          <Action.SubmitForm title="Create Snippet" icon={Icon.Image} onSubmit={handleSubmit} />
         </ActionPanel>
       }
     >
       <Form.Description text="Prepare your code snippet" />
-      <Form.TextField
-        id="name"
-        title="Name of your code snippet"
-        error={nameError}
-        value={name}
-        placeholder="Enter name"
-        onChange={(newValue) => {
-          setName(newValue);
-          dropNameErrorIfNeeded();
-        }}
-        onBlur={(event) => {
-          const value = event.target.value;
-          if (!value || value.length === 0) {
-            setNameError("The field shouldn't be empty!");
-          }
-        }}
-      />
-      <Form.TextArea
-        id="code"
-        title="Your code"
-        error={codeError}
-        placeholder="Paste your code here"
-        value={code}
-        onChange={(newValue) => {
-          setCode(newValue);
-          dropCodeErrorIfNeeded();
-        }}
-        onBlur={(event) => {
-          const value = event.target.value;
-          if (!value || value.length === 0) {
-            setCodeError("The field shouldn't be empty!");
-          }
-        }}
-      />
+      <Form.TextField title="Name of your code snippet" placeholder="Enter name" {...itemProps.name} />
+      <Form.TextArea title="Your code" placeholder="Paste your code here" {...itemProps.code} />
     </Form>
   );
 }
