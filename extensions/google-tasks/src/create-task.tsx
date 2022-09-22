2e78d9e9d643463a726ae485eda8351fda54709a
elliotdes <elliotdesimons@gmail.com>
2022-09-22 11:49:38 +0100

    Add authorize before create view

--- extensions/google-tasks/src/create-task.tsx
+++ extensions/google-tasks/src/create-task.tsx
@@ -3,12 +3,14 @@ import { createTask } from "./api/endpoints";
 import { useCallback } from "react";
 import { showToast, Toast } from "@raycast/api";
 import { TaskForm } from "./types";
+import * as google from "./api/oauth";
 
 export default function Command() {
   const handleCreate = useCallback(
     (listId: string, taskToCreate: TaskForm) => {
       (async () => {
         try {
+          await google.authorize();
           await createTask(listId, taskToCreate);
         } catch (error) {
           console.error(error);
