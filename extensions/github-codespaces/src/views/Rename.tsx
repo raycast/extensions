7e260a02a76af64fdf735d889e66d66f4b67aed2
Michael Wang <mkwng@github.com>
2022-09-22 20:22:41 +0000

    prevent empty name

--- extensions/github-codespaces/src/views/Rename.tsx
+++ extensions/github-codespaces/src/views/Rename.tsx
@@ -4,48 +4,48 @@ import handleRename from "../methods/handleRename";
 import { useState } from "react";
 
 const Rename = ({
-  codespace,
-  onRevalidate,
+	codespace,
+	onRevalidate,
 }: {
-  codespace: Codespace;
-  onRevalidate: () => void;
+	codespace: Codespace;
+	onRevalidate: () => void;
 }) => {
-  const [nameError, setNameError] = useState<string | undefined>();
-  const { pop } = useNavigation();
-  return (
-    <Form
-      actions={
-        <ActionPanel>
-          <Action.SubmitForm
-            title="Rename"
-            onSubmit={({ name }) =>
-              handleRename({ codespace, name }).finally(() => {
-                pop();
-                onRevalidate();
-              })
-            }
-          />
-        </ActionPanel>
-      }
-    >
-      <Form.TextField
-        id="name"
-        title="Name"
-        defaultValue={codespace.display_name || ""}
-        placeholder="Select a display name for this codespace"
-        error={nameError}
-        onBlur={(e) => {
-          if (e.target.value?.length == 0) {
-            setNameError("Name is required");
-          } else {
-            if (nameError && nameError.length > 0) {
-              setNameError(undefined);
-            }
-          }
-        }}
-      />
-    </Form>
-  );
+	const [nameError, setNameError] = useState<string | undefined>();
+	const { pop } = useNavigation();
+	return (
+		<Form
+			actions={
+				<ActionPanel>
+					<Action.SubmitForm
+						title="Rename"
+						onSubmit={({ name }) =>
+							handleRename({ codespace, name }).finally(() => {
+								pop();
+								onRevalidate();
+							})
+						}
+					/>
+				</ActionPanel>
+			}
+		>
+			<Form.TextField
+				id="name"
+				title="Name"
+				defaultValue={codespace.display_name || ""}
+				placeholder="Select a display name for this codespace"
+				error={nameError}
+				onChange={(v) => {
+					if (v?.length == 0) {
+						setNameError("Name is required");
+					} else {
+						if (nameError && nameError.length > 0) {
+							setNameError(undefined);
+						}
+					}
+				}}
+			/>
+		</Form>
+	);
 };
 
 export default Rename;
