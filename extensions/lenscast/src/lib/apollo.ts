f2a03410cf8fb6ef800fbc2f1b666e9d6df794b7
Pedro Duarte <oi@ped.ro>
2022-09-23 10:12:08 +0200

    Update apollo.ts

--- extensions/lenscast/src/lib/apollo.ts
+++ extensions/lenscast/src/lib/apollo.ts
@@ -4,7 +4,6 @@ import fetch from "cross-fetch";
 export const apolloClient = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({
-    // uri: "https://api-mumbai.lens.dev/",
     uri: "https://api.lens.dev/",
     fetch,
   }),
