65c76c8a8b7c437ed620ea81d6e469e9b7643c64
Kasper Mikiewicz <IderedPL@gmail.com>
2022-09-26 00:03:38 +0200

    feat(transform): add CSS to JS

--- extensions/transform/src/lib/types.d.ts
+++ extensions/transform/src/lib/types.d.ts
@@ -4,6 +4,7 @@ declare module "gofmt.js";
 declare module "@walmartlabs/json-to-simple-graphql-schema/lib";
 declare module "alterschema";
 declare module "html2pug";
+declare module "postcss-js";
 
 type OptionsComponentProps = {
   setOptions: React.Dispatch<React.SetStateAction<Record<string, string>>>;
