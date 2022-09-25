9acbd2d212a4624b573faf6fa715ce1d23a21440
Kasper Mikiewicz <IderedPL@gmail.com>
2022-09-25 14:30:32 +0200

    feat(transform): add JS Object to JSON

--- extensions/transform/src/transformers/index.ts
+++ extensions/transform/src/transformers/index.ts
@@ -1,15 +1,6 @@
-// String
-export * from "./string-to-regex";
-// HTML
 export * from "./html-to-markdown";
 export * from "./html-to-pug";
-// Markdown
-export * from "./markdown-to-html";
-export * from "./markdown-to-pug";
-// Pug
-export * from "./pug-to-html";
-export * from "./pug-to-markdown";
-// JSON
+export * from "./js-object-to-json";
 export * from "./json-to-big-query";
 export * from "./json-to-go-bson";
 export * from "./json-to-go-struct";
@@ -20,9 +11,12 @@ export * from "./json-to-kotlin";
 export * from "./json-to-toml";
 export * from "./json-to-typescript";
 export * from "./json-to-yaml";
-// TOML
+export * from "./markdown-to-html";
+export * from "./markdown-to-pug";
+export * from "./pug-to-html";
+export * from "./pug-to-markdown";
+export * from "./string-to-regex";
 export * from "./toml-to-json";
 export * from "./toml-to-yaml";
-// YAML
 export * from "./yaml-to-json";
 export * from "./yaml-to-toml";
