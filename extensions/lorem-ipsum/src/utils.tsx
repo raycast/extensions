c4a2368f13fe2c7a83bce7904dbc61e476f2cdb8
GastroGeek <gastrogeek@gmail.com>
2022-09-26 14:44:10 +0100

    update api/deps, allow support for multiple sentences/paragraphs

--- extensions/lorem-ipsum/src/utils.tsx
+++ extensions/lorem-ipsum/src/utils.tsx
@@ -1,5 +1,8 @@
-import { closeMainWindow, copyTextToClipboard, pasteText, showHUD } from "@raycast/api";
+import { closeMainWindow, Clipboard, showHUD } from "@raycast/api";
 import { LoremIpsum } from "lorem-ipsum";
+
+// don't want to cause a heap error, so cap it 😱
+const LOREM_IPSUM_MAX_NUMBER = 10000;
 
 const generator = new LoremIpsum({
   sentencesPerParagraph: {
@@ -15,27 +18,63 @@ const generator = new LoremIpsum({
 // generator.generateWords(1);
 // generator.generateParagraphs(7);
 
-export const generateParagraph = () => {
-  return generator.generateSentences(10);
+export const generateParagraphs = (count: number) => {
+  return Array.from(Array(count))
+    .map(() => generator.generateSentences(6))
+    .join("\r\n\r\n"); // newline + seperator line
 };
 
-export const generateSentence = () => {
-  return generator.generateSentences(1);
+export const generateSentences = (count: number) => {
+  return generator.generateSentences(count);
 };
 
 export const notify = () => {
   showHUD("Copied to clipboard");
 };
 
+export const safeLoremIpsumNumberArg = async (arg: string | undefined) => {
+  if (!arg) {
+    arg = "1";
+  }
+
+  try {
+    const parseableNumber = parseInt(arg, 10);
+
+    // number and valid/within range?
+    if (isNaN(parseableNumber) || parseableNumber > LOREM_IPSUM_MAX_NUMBER) {
+      return {
+        error: {
+          message: `Please enter a valid integer number, no more than ${LOREM_IPSUM_MAX_NUMBER}`,
+        },
+        safeLoremIpsumNumber: null,
+      };
+    }
+
+    // all good
+    return {
+      error: null,
+      safeLoremIpsumNumber: parseableNumber,
+    };
+  } catch (e) {
+    // generic error
+    return {
+      error: {
+        message: "Something went wrong",
+      },
+      safeLoremIpsumNumber: null,
+    };
+  }
+};
+
 export const preformAction = async (action: string, output: string) => {
   switch (action) {
     case "clipboard":
-      await copyTextToClipboard(output);
+      await Clipboard.copy(output);
       await notify();
       break;
 
     case "paste":
-      await pasteText(output);
+      await Clipboard.paste(output);
       break;
   }
 
