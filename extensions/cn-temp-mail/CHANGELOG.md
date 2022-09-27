589b2c7d0269b67ea35fa2382268fd2e68a6d9cd
Santosh Maharjan <immortalsantee@gmail.com>
2022-09-27 16:02:14 +0545

    Prettier issue fixed to upload to the store.

--- extensions/cn-temp-mail/CHANGELOG.md
+++ extensions/cn-temp-mail/CHANGELOG.md
@@ -1,3 +1,6 @@
 # CN Temp Mail Changelog
 
+## [Changes] - 2022-09-27
+-  Prettier fixed.
+
 ## [Initial Version] - 2022-09-26
--- extensions/cn-temp-mail/src/index.tsx
+++ extensions/cn-temp-mail/src/index.tsx
@@ -1,37 +1,34 @@
 import { randomUUID } from "crypto";
-import {Form, showHUD, Clipboard, open, ActionPanel, Action, showToast, popToRoot} from "@raycast/api";
+import { Form, showHUD, Clipboard, open, ActionPanel, Action, showToast, popToRoot } from "@raycast/api";
 
 type Values = {
   textfield: string;
 };
 
 export default function Command() {
-    async function handleSubmit(values: Values) {
-        const username = values.textfield;
-        const url = `https://maildrop.cc/inbox/${username}`;
-        const email = `${username}@maildrop.cc`;
+  async function handleSubmit(values: Values) {
+    const username = values.textfield;
+    const url = `https://maildrop.cc/inbox/${username}`;
+    const email = `${username}@maildrop.cc`;
 
-        showToast({title: "Email created", message: `${email} email copied to clipboard.`}).then();
+    showToast({ title: "Email created", message: `${email} email copied to clipboard.` }).then();
 
-        await open(url);
-        await Clipboard.copy(email);
-        await showHUD(`${email} email copied to clipboard.`);
-        await popToRoot();
-    }
+    await open(url);
+    await Clipboard.copy(email);
+    await showHUD(`${email} email copied to clipboard.`);
+    await popToRoot();
+  }
 
-    return (
-        <Form actions={
-            <ActionPanel>
-                <Action.SubmitForm onSubmit={handleSubmit}/>
-            </ActionPanel>
-        }>
-            <Form.Description text="Enter name to get temp mail."/>
-            <Form.TextField
-                id="textfield"
-                title="Username"
-                placeholder="Enter username"
-                defaultValue={randomUUID()}
-            />
-        </Form>
-    );
+  return (
+    <Form
+      actions={
+        <ActionPanel>
+          <Action.SubmitForm onSubmit={handleSubmit} />
+        </ActionPanel>
+      }
+    >
+      <Form.Description text="Enter name to get temp mail." />
+      <Form.TextField id="textfield" title="Username" placeholder="Enter username" defaultValue={randomUUID()} />
+    </Form>
+  );
 }
