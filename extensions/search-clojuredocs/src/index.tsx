b7523056e674d3190236ca572f651ca692166631
Daniel Blancas <edblancas@gmail.com>
2022-10-01 11:49:26 -0500

    use metadata

--- extensions/search-clojuredocs/src/index.tsx
+++ extensions/search-clojuredocs/src/index.tsx
@@ -56,18 +56,33 @@ const CljDetail = ({ res }: { res: DocInfo }) => {
   const mark = `
    # ${res.ns}/${res.name}
 
-   ~~~
-   ${res.arglists.length == 0 ? "No args" : res.arglists?.join("\n")}
-   ~~~
+   ---
+
+   ${res.arglists.length == 0 ? 
+    "No args" : 
+    res.arglists?.map((arg) => "`(" + res.name + " " + arg + ")`").join(" ")}
 
    ~~~
    ${res.doc ?? "No doc"}
    ~~~
-   
-   ${res.examples == null ? "" : "### " + res.examples.length + " Example(s)"}
-   ~~~
-   ${res.examples == null ? "No examples" : res.examples?.join("\n~~~\n~~~\n")}
-   ~~~
+
+   ### ${res.examples == null ? "0" : res.examples.length} Examples
+
+   ${res.examples == null ? 
+    "`No examples`" : 
+    res.examples?.map((e) => "~~~\n" + e + "\n~~~").join("\n")}
+
+   ### See also
+
+   ${res["see-alsos"] == null ? 
+    "None" : 
+    res["see-alsos"]?.map((arg) => "`(" + res.name + " " + arg + ")`").join(" ")}
+
+   ### ${res.notes == null ? "0" : res.notes.length} Note(s)
+
+   ${res.notes == null ? 
+    "`No notes`" : 
+    res.notes?.map((n) => "~~~\n" + n + "\n~~~").join("\n")}
   `;
 
   return (
@@ -75,6 +90,19 @@ const CljDetail = ({ res }: { res: DocInfo }) => {
       <Detail
         navigationTitle={res.ns + "/" + res.name}
         markdown={mark}
+        metadata={
+          <Detail.Metadata>
+            <Detail.Metadata.Label title="Available since" text={res.added} />
+            <Detail.Metadata.Link title="Library" text={res.ns} target={res["library-url"]} />
+            <Detail.Metadata.Separator />
+            <Detail.Metadata.TagList title="See also">
+              {(data || []).map((result) => (
+                <SearchListItem key={result.ns + "/" + result.name} searchResult={result} />
+              ))}
+              <Detail.Metadata.TagList.Item text="Electric" color={"#eed535"} />
+            </Detail.Metadata.TagList>
+          </Detail.Metadata>
+        }
         actions={
           <ActionPanel>
             <Action.OpenInBrowser url={"https://clojuredocs.org" + res.href} />
