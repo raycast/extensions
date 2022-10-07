98ee7a397fd2b9f8dcb4d9f8daf89d55f51b3aa1
Abdullah Mzaien <xa300600@hotmail.com>
2022-10-07 17:41:26 +0300

    chore: update `@raycast/utils` + handling errors

--- extensions/adhan-time/src/adhan.tsx
+++ extensions/adhan-time/src/adhan.tsx
@@ -1,4 +1,4 @@
-import { MenuBarExtra, getPreferenceValues, Icon, openCommandPreferences, Cache } from "@raycast/api";
+import { MenuBarExtra, getPreferenceValues, Icon, openCommandPreferences, showToast, Toast } from "@raycast/api";
 import { useFetch } from "@raycast/utils";
 export interface Preferences {
   country: string;
@@ -20,21 +20,33 @@ export interface Data {
 }
 export default function Command() {
   const userPreference: Preferences = getPreferenceValues();
-  const { isLoading, data } = useFetch<Data>(
-    `https://api.aladhan.com/v1/timingsByCity?city=${userPreference.city}&country=${userPreference.country}&method=${userPreference.calculation_methods}`,
-    { keepPreviousData: true }
+  const { isLoading, data, error } = useFetch<Data>(
+    `https://api.aladhan.com/v1/timingsByCity?city=${encodeURI(userPreference.city)}&country=${encodeURI(
+      userPreference.country
+    )}&method=${encodeURI(userPreference.calculation_methods)}`,
+    {
+      keepPreviousData: true,
+      onError: (error: Error) => {
+        showToast({
+          style: Toast.Style.Failure,
+          title: `${error} Check your preferences`,
+          message: `Country ${userPreference.country} City ${userPreference.city}`,
+          primaryAction: {
+            title: "Open Preferences",
+            onAction: () => openCommandPreferences(),
+          },
+        });
+      },
+    }
   );
-  const cache = new Cache();
-  cache.set("timings", JSON.stringify(data));
-  const cachedPrayerTiming = JSON.parse(cache.get("timings")!);
   return (
     <MenuBarExtra icon={Icon.Clock} title="Prayer times" tooltip="Prayer times" isLoading={isLoading}>
-      <MenuBarExtra.Item title={`Fajr: ${cachedPrayerTiming?.data.timings.Fajr}`} icon={Icon.Sunrise} />
-      <MenuBarExtra.Item title={`Sunrise: ${cachedPrayerTiming?.data.timings.Sunrise}`} icon={Icon.Sunrise} />
-      <MenuBarExtra.Item title={`Dhuhr: ${cachedPrayerTiming?.data.timings.Dhuhr}`} icon={Icon.Sun} />
-      <MenuBarExtra.Item title={`Asr: ${cachedPrayerTiming?.data.timings.Asr}`} icon={Icon.Sun} />
-      <MenuBarExtra.Item title={`Maghrib: ${cachedPrayerTiming?.data.timings.Maghrib}`} icon={Icon.Moon} />
-      <MenuBarExtra.Item title={`Isha: ${cachedPrayerTiming?.data.timings.Isha}`} icon={Icon.Moon} />
+      <MenuBarExtra.Item title={`Fajr: ${data?.data.timings.Fajr}`} icon={Icon.Sunrise} />
+      <MenuBarExtra.Item title={`Sunrise: ${data?.data.timings.Sunrise}`} icon={Icon.Sunrise} />
+      <MenuBarExtra.Item title={`Dhuhr: ${data?.data.timings.Dhuhr}`} icon={Icon.Sun} />
+      <MenuBarExtra.Item title={`Asr: ${data?.data.timings.Asr}`} icon={Icon.Sun} />
+      <MenuBarExtra.Item title={`Maghrib: ${data?.data.timings.Maghrib}`} icon={Icon.Moon} />
+      <MenuBarExtra.Item title={`Isha: ${data?.data.timings.Isha}`} icon={Icon.Moon} />
       <MenuBarExtra.Separator />
       <MenuBarExtra.Item
         title="Change city or country"
