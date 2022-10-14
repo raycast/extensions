d721217d61c81e5ca200ab62547908709e012a0e
koin <36128970+koinzhang@users.noreply.github.com>
2022-10-14 17:05:24 +0800

    [Fix bug]Update extension: Append Clipboard (#3220)
    * [Fix bug]Update extension: Append Clipboard
    
    * Update CHANGELOG.md
    
    Co-authored-by: raycastbot <bot@raycast.com>

--- extensions/append-clipboard/CHANGELOG.md
+++ extensions/append-clipboard/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Append Clipboard Changelog
 
+## [Fix bug] - 2022-10-14
+
+- Fix the bug that the clipboard is undefined
+
 ## [Add new command] - 2022-10-12
 
 - Add new command: Prepend Text to Clipboard
--- extensions/append-clipboard/src/append-text-to-clipboard.tsx
+++ extensions/append-clipboard/src/append-text-to-clipboard.tsx
@@ -1,12 +1,5 @@
-import { Clipboard, closeMainWindow, getPreferenceValues, getSelectedText, showHUD } from "@raycast/api";
-import { Preferences } from "./types/preferences";
+import { getAppendedText } from "./utils/common-util";
 
 export default async () => {
-  const { appendSeparator } = await getPreferenceValues<Preferences>();
-  await closeMainWindow({ clearRootSearch: false });
-  const appendText = await getSelectedText();
-  const clipBoardText = await Clipboard.readText();
-  const finalText = clipBoardText + appendSeparator + appendText;
-  await Clipboard.copy(finalText);
-  await showHUD(`✅ Appended text to clipboard`);
+  await getAppendedText();
 };
