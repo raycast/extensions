c7fea6d679c035bb76db34600d2f2f37e0e39a5d
Per Nielsen Tikær <per@raycast.com>
2022-10-14 06:07:39 +0200

    Update tc-no-generate.ts

--- extensions/tc-no-generator/src/tc-no-generate.ts
+++ extensions/tc-no-generator/src/tc-no-generate.ts
@@ -6,11 +6,5 @@ export default async () => {
   await Clipboard.copy(no);
 
   await LocalStorage.setItem("last-generate-tc", no);
-
-  await showToast({
-    title: "Turkish Identification Number",
-    message: `Copied ${no} to clipboard`,
-  });
-
-  await showHUD("Copied to clipboard");
+  await showHUD(`✅ Copied ${no} to clipboard`);
 };
