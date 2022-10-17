b971ede191a1627e5599e86fd57b176e052e5041
Per Nielsen Tikær <per@raycast.com>
2022-10-17 07:56:03 +0300

    Update index.tsx

--- extensions/crunchbase/src/index.tsx
+++ extensions/crunchbase/src/index.tsx
@@ -82,7 +82,7 @@ export default function Command() {
                   ) : null}
                 </ActionPanel.Section>
                 <ActionPanel.Section>
-                  <Action title="Toggle Detail" icon={Icon.Sidebar} onAction={() => setShowingDetail(!showingDetail)} />
+                  <Action title="Toggle Details" icon={Icon.Sidebar} onAction={() => setShowingDetail(!showingDetail)} />
                 </ActionPanel.Section>
               </ActionPanel>
             }
