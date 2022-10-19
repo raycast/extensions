305fb71e98311f39eeecd43ecd725ad114b18e2d
Michael Wang <mkwng@github.com>
2022-10-19 15:03:56 -0700

    add ssh command

--- extensions/github-codespaces/src/actions/CodespaceActions.tsx
+++ extensions/github-codespaces/src/actions/CodespaceActions.tsx
@@ -1,4 +1,4 @@
-import { Action, ActionPanel, Icon } from "@raycast/api";
+import { Action, ActionPanel, Clipboard, Icon } from "@raycast/api";
 import { preferredEditor } from "../preferences";
 import { Client, clientNames, Codespace } from "../types";
 import handleDelete from "../methods/handleDelete";
@@ -8,70 +8,69 @@ import { handleStop } from "../methods/handleStop";
 import { launchEditor } from "../utils/launchEditor";
 
 function CodespaceActions({
-  codespace,
-  onRevalidate,
+	codespace,
+	onRevalidate,
 }: {
-  codespace: Codespace;
-  onRevalidate: () => void;
+	codespace: Codespace;
+	onRevalidate: () => void;
 }) {
-  return (
-    <ActionPanel>
-      <ActionPanel.Section title="Open">
-        <Action
-          title={`Open in ${clientNames[preferredEditor]}`}
-          onAction={() => launchEditor({ codespace })}
-        />
-        {Object.keys(clientNames).map((c) => {
-          if (c === preferredEditor) return null;
-          return (
-            <Action
-              key={c}
-              title={`Open in ${clientNames[c as Client]}`}
-              onAction={() => launchEditor({ codespace, client: c as Client })}
-            />
-          );
-        })}
-      </ActionPanel.Section>
-      {!codespace.pending_operation && (
-        <>
-          <ActionPanel.Section title="Update">
-            {codespace.state === "Available" && (
-              <Action
-                title="Stop"
-                icon={Icon.Stop}
-                onAction={() => handleStop({ codespace }).finally(onRevalidate)}
-              />
-            )}
-            <Action.Push
-              title="Rename"
-              icon={Icon.Pencil}
-              shortcut={{ modifiers: ["cmd", "shift"], key: "r" }}
-              target={
-                <Rename codespace={codespace} onRevalidate={onRevalidate} />
-              }
-            />
-            <Action.Push
-              title="Change compute"
-              icon={Icon.ComputerChip}
-              target={
-                <ChangeCompute
-                  codespace={codespace}
-                  onRevalidate={onRevalidate}
-                />
-              }
-            />
-            <Action
-              title="Delete"
-              icon={Icon.Trash}
-              style={Action.Style.Destructive}
-              shortcut={{ modifiers: ["cmd", "shift"], key: "backspace" }}
-              onAction={() => handleDelete({ codespace }).finally(onRevalidate)}
-            />
-          </ActionPanel.Section>
-        </>
-      )}
-    </ActionPanel>
-  );
+	return (
+		<ActionPanel>
+			<ActionPanel.Section title="Open">
+				<Action
+					title={`Open in ${clientNames[preferredEditor]}`}
+					onAction={() => launchEditor({ codespace })}
+				/>
+				{Object.keys(clientNames).map((c) => {
+					if (c === preferredEditor) return null;
+					return (
+						<Action
+							key={c}
+							title={`Open in ${clientNames[c as Client]}`}
+							onAction={() => launchEditor({ codespace, client: c as Client })}
+						/>
+					);
+				})}
+				<Action.CopyToClipboard
+					title="Copy SSH command"
+					content={`gh codespace ssh -c ${codespace.name}`}
+					shortcut={{ modifiers: ["cmd", "shift"], key: "c" }}
+					icon={Icon.Terminal}
+				/>
+			</ActionPanel.Section>
+			{!codespace.pending_operation && (
+				<>
+					<ActionPanel.Section title="Update">
+						{codespace.state === "Available" && (
+							<Action
+								title="Stop"
+								icon={Icon.Stop}
+								onAction={() => handleStop({ codespace }).finally(onRevalidate)}
+							/>
+						)}
+						<Action.Push
+							title="Rename"
+							icon={Icon.Pencil}
+							shortcut={{ modifiers: ["cmd", "shift"], key: "r" }}
+							target={<Rename codespace={codespace} onRevalidate={onRevalidate} />}
+						/>
+						<Action.Push
+							title="Change compute"
+							icon={Icon.ComputerChip}
+							target={<ChangeCompute codespace={codespace} onRevalidate={onRevalidate} />}
+						/>
+						<Action
+							title="Delete"
+							icon={Icon.Trash}
+							style={Action.Style.Destructive}
+							shortcut={{ modifiers: ["cmd", "shift"], key: "backspace" }}
+							onAction={() => handleDelete({ codespace }).finally(onRevalidate)}
+						/>
+					</ActionPanel.Section>
+				</>
+			)}
+		</ActionPanel>
+	);
 }
 
 export default CodespaceActions;
