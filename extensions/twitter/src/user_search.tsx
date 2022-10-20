56945439c5e85c2b05519b2bb30485b089804e40
Willam W <ww8668@gmail.com>
2022-10-20 20:30:39 +0800

    Update User Search root to handle new SearchListV2

--- extensions/twitter/src/user_search.tsx
+++ extensions/twitter/src/user_search.tsx
@@ -1,11 +1,13 @@
 import { ReactElement } from "react";
 import { useV2 } from "./common";
 import { UserList } from "./v1/components/user_search";
-import { SearchUserListV2 } from "./v2/components/user_search";
+import { SearchListV2, SEARCH_TYPE } from "./v2/components/search";
+
+const searchBarPlaceholder = "Search Users by Name or Handle (e.g. @tonka_2000 or Michael Aigner)";
 
 export default function UserSearchRoot(): ReactElement {
   if (useV2()) {
-    return <SearchUserListV2 />;
+    return <SearchListV2 searchBarPlaceholder={searchBarPlaceholder} searchType={SEARCH_TYPE.USER} />;
   } else {
     return <UserList />;
   }
