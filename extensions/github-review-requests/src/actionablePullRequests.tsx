93895ec68b59862dd2f8364d5744a78ba7d91c52
Adrian Cooney <cooney.adrian@gmail.com>
2022-10-24 11:55:56 +0100

    Remove "All Good" from menubar

--- extensions/github-review-requests/src/actionablePullRequests.tsx
+++ extensions/github-review-requests/src/actionablePullRequests.tsx
@@ -50,7 +50,9 @@ const actionablePullRequests = () => {
 export default actionablePullRequests;
 
 const getTitle = (updatedPulls: PullRequestShort[]) =>
-  updatedPulls.length === 0 ? `All good` : `${updatedPulls.length} PR${updatedPulls.length > 1 ? "s" : ""}`;
+  updatedPulls.length > 0
+    ? `${updatedPulls.length} PR${updatedPulls.length > 1 ? "s" : ""}`
+    : undefined;
 
 const getIcon = (updatedPulls: PullRequestShort[]) => ({
   source: "icon.png",
