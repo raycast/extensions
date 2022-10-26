35817a207a41236fe3633b42bca392914af06f5a
Michael Aigner <tonka3100@gmail.com>
2022-10-26 15:02:21 +0200

    add open issues cmd

--- extensions/github-app/src/issues.tsx
+++ extensions/github-app/src/issues.tsx
@@ -1,18 +1,65 @@
-import { getPreferenceValues, List, showToast, Toast } from "@raycast/api";
-import { Octokit, App } from "octokit";
+import { Action, ActionPanel, getPreferenceValues, Image, List, showToast, Toast } from "@raycast/api";
+import { Octokit } from "octokit";
 import { useEffect, useState } from "react";
 import { getErrorMessage } from "./utils";
 
+interface Repo {
+  id: number;
+  name: string;
+  full_name: string;
+}
+
+interface User {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  html_url: string;
+  type: string;
+}
+
 interface Issue {
-  id: string;
+  id: number;
+  number: number;
+  url: string;
+  title: string;
+  html_url: string;
+  body_text: string;
+  repository?: Repo;
+  //milestone
+  user: User;
+  updated_at: string;
+  created_at: string;
+  // labels[]
 }
 
 export default function IssuesCommand(): JSX.Element {
-  const { issues, error, isLoading} = useIssues("");
-  if(error){
-    showToast({ style: Toast.Style.Failure, message: error, title: "Could not fetch Issues"});
+  const { issues, error, isLoading } = useIssues("");
+  if (error) {
+    showToast({ style: Toast.Style.Failure, message: error, title: "Could not fetch Issues" });
   }
-  return <List />;
+  return (
+    <List isLoading={isLoading}>
+      {issues?.map((i) => (
+        <List.Item
+          key={i.id.toString()}
+          title={i.title}
+          subtitle={`#${i.number}`}
+          icon={{ source: i.user?.avatar_url, mask: Image.Mask.Circle }}
+          accessories={[
+            { date: new Date(i.updated_at) },
+            { icon: { source: i.user?.avatar_url, mask: Image.Mask.Circle }, tooltip: i.user.login },
+          ]}
+          actions={
+            <ActionPanel>
+              <Action.OpenInBrowser url={i.html_url} />
+            </ActionPanel>
+          }
+        />
+      ))}
+    </List>
+  );
 }
 
 function useIssues(query: string | undefined): {
@@ -23,7 +70,7 @@ function useIssues(query: string | undefined): {
   const [issues, setIssues] = useState<Issue[]>();
   const [error, setError] = useState<string>();
   const [isLoading, setIsLoading] = useState(true);
-  
+
   useEffect(() => {
     let cancel = false;
     async function fetchData() {
@@ -34,31 +81,35 @@ function useIssues(query: string | undefined): {
         const pat = (prefs.pat as string) || undefined;
 
         const octokit = new Octokit({ auth: pat });
-        /*const d = await octokit.rest.repos.listForAuthenticatedUser();
-        for(const p of d.data){
-          console.log(`${p.name} ${p.full_name}`);
-        }*/
-        const d = await octokit.rest.issues.list();
-        console.log(d.data);
-        //console.log(d.data);
-        //await octokit.rest.repos.get({user})
-        //octokit.rest.issues.list();
-        
+        const d = await octokit.rest.search.issuesAndPullRequests({ q: "type:issue author:@me is:open sort:updated" });
+        const data: Issue[] | undefined = d.data?.items?.map((p) => ({
+          id: p.id,
+          number: p.number,
+          title: p.title,
+          url: p.url,
+          html_url: p.html_url,
+          body_text: p.body_text,
+          repository: p.repository as Repo | undefined,
+          user: p.user as User | undefined,
+          created_at: p.created_at,
+          updated_at: p.updated_at,
+        }));
+        if (!cancel) {
+          setIssues(data);
+        }
       } catch (error) {
         setError(getErrorMessage(error));
-      }
-      finally{
-        if(!cancel){
+      } finally {
+        if (!cancel) {
           setIsLoading(false);
         }
       }
-      
-    };
+    }
     fetchData();
 
     return () => {
       cancel = true;
-    }
+    };
   }, [query]);
 
   return { issues, error, isLoading };
