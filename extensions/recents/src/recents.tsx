c78c6e6c157c4ae9477081597c07bdbeee130981
GastroGeek <gastrogeek@gmail.com>
2022-10-26 11:26:23 +0100

    ensure that if limit pref is blank, that it still defaults to 1000

--- extensions/recents/src/recents.tsx
+++ extensions/recents/src/recents.tsx
@@ -54,7 +54,7 @@ const getRecents = async (scope: string | undefined, callback: (result: Spotligh
       query: queryParts.query,
       directories: queryParts.directories,
       attributes: ["kMDItemDisplayName", "kMDItemKind", "kMDItemLastUsedDate"],
-      limit,
+      limit: limit || 1000,
     });
 
     query.output.on("data", (data: SpotlightResult) => {
