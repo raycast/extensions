2852bd1cca295ffa82482975a8ab8464a801cacc
PLa <tanate.mea@ascendcorp.com>
2022-10-27 21:39:48 +0700

    feat: add functionality

--- extensions/jira2git/src/transform.tsx
+++ extensions/jira2git/src/transform.tsx
@@ -1,6 +1,16 @@
-import { showHUD, Clipboard } from "@raycast/api";
+import { showHUD, Clipboard, showToast, Toast } from "@raycast/api";
+import { ErrorText } from "./utils/exception";
+import { jiraFetchObject } from "./utils/jira";
 
 const defaultPrefix = "feature";
+
+interface Issue {
+  id: string;
+  key: string;
+  fields: {
+    summary: string;
+  };
+}
 
 interface TransformProps {
   arguments: {
@@ -9,20 +19,52 @@ interface TransformProps {
   };
 }
 
-const getCardTitle = () => {
-  return "";
+const getIssueDetail = async (id: string) => {
+  const result = await jiraFetchObject<Issue>(
+    `/rest/api/2/issue/${id}`,
+    {},
+    { 400: ErrorText("Invalid ID", "Unknown issue") }
+  );
+  return result;
 };
 
-const transformUrlToCode = (url: string) => {
-  // https://jira.net/browse/W220037-6
-  return url;
+const getIssueTitle = async (id: string) => {
+  const issue = await getIssueDetail(id);
+  const summary = issue.fields.summary;
+  const symbolRegex = /[^\w\s]/gi;
+  const title = summary
+    .replace(symbolRegex, " ")
+    .trim()
+    .split(" ")
+    .filter((word) => word)
+    .map((word) => word.toLowerCase())
+    .join("-");
+  return title;
 };
 
-export default async function Transform(props: TransformProps) {
-  const { prefix = defaultPrefix, url } = props.arguments;
-  const code = transformUrlToCode(url);
-  const title = getCardTitle();
-  const branchName = `${prefix}/${code}-${title}`;
+const transformUrlToIssueId = (url: string) => {
+  const issueIdRegex = /([A-Z][A-Z0-9]+-[0-9]+)/g;
+  const issueId = url.match(issueIdRegex);
+  return issueId?.length ? issueId[0] : "";
+};
+
+const TransformCommand = async (props: TransformProps) => {
+  const { prefix, url } = props.arguments;
+  const issueId = transformUrlToIssueId(url);
+
+  if (!issueId) {
+    await showToast({
+      style: Toast.Style.Failure,
+      title: "Invalid URL format",
+      message: "Please try with a new URL",
+    });
+    return;
+  }
+
+  const title = await getIssueTitle(issueId);
+  const branchName = `${prefix || defaultPrefix}/${issueId}-${title}`;
   await Clipboard.copy(branchName);
   await showHUD("Copied branch name to clipboard");
-}
+};
+
+export default TransformCommand;
