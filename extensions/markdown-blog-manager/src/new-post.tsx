ec0d4edeea11d0829a53c3d97ec792c4b5f2c327
Per Nielsen Tikær <per@raycast.com>
2022-11-01 12:38:31 +0100

    Update new-post.tsx

--- extensions/markdown-blog-manager/src/new-post.tsx
+++ extensions/markdown-blog-manager/src/new-post.tsx
@@ -7,13 +7,12 @@ import { fillTemplateVariables } from './utils/templates';
 import { FormValidation, useForm } from '@raycast/utils';
 import { ValidatePreferences } from './utils/preferences';
 
-
 export default function Command() {
 	return (
 		<ValidatePreferences>
 			<NewPost />
 		</ValidatePreferences>
-	);
+	); 
 }
 
 export function NewPost() {
@@ -50,34 +49,26 @@ export function NewPost() {
 
 					return 'Post already exists';
 				}
-			}
-
+			},
 		},
 		initialValues: {
 			summary: 'A new draft has been created.',
 			extension: 'md',
-		}
+		},
 	});
-
 
 	useEffect(() => {
 		const subdirectory = itemProps.subdirectory.value || '';
 		const title = itemProps.title.value || '';
 		const summary = itemProps.summary.value || '';
 		setValue('content', fillTemplateVariables(subdirectory, title, summary));
-	}, [
-		itemProps.subdirectory.value,
-		itemProps.title.value,
-		itemProps.summary.value
-	]);
-
-
+	}, [itemProps.subdirectory.value, itemProps.title.value, itemProps.summary.value]);
 
 	useEffect(() => {
 		if (!itemProps.title.value) {
 			return;
 		}
-		setValue('slug', titleToSlug(itemProps.title.value))
+		setValue('slug', titleToSlug(itemProps.title.value));
 	}, [itemProps.title.value]);
 
 	return (
@@ -91,31 +82,21 @@ export function NewPost() {
 			}
 		>
 			<Form.TextField title="Title" placeholder="Post Title" {...itemProps.title} />
-			<Form.TextField
-				title="Summary"
-				{...itemProps.summary}
-			/>
+			<Form.TextField title="Summary" {...itemProps.summary} />
 			<Form.Dropdown title="Subdirectory" {...itemProps.subdirectory}>
 				<Form.Dropdown.Item value="" title="None" />
 				{subdirectories().map((subdirectory) => (
 					<Form.Dropdown.Item key={subdirectory} value={subdirectory} title={subdirectory} />
 				))}
 			</Form.Dropdown>
-			<Form.TextField
-				title="Post Slug"
-				{...itemProps.slug}
-			/>
+			<Form.TextField title="Post Slug" {...itemProps.slug} />
 			<Form.Dropdown title="File Extension" {...itemProps.extension}>
 				<Form.Dropdown.Item value="md" title=".md" />
 				<Form.Dropdown.Item value="mdx" title=".mdx" />
 			</Form.Dropdown>
 			<Form.Separator />
 			<Form.Description text="A new markdown file will be created in your drafts folder. with the following content:" />
-			<Form.TextArea
-				title="Preview"
-				enableMarkdown={true}
-				{...itemProps.content}
-			/>
+			<Form.TextArea title="Preview" enableMarkdown={true} {...itemProps.content} />
 		</Form>
 	);
 }
