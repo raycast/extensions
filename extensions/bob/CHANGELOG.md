a41c2097c64ce92015c94d20ca014df43e1ec6c2
Wei GENG <vigeng@qq.com>
2022-11-02 02:05:03 +0800

    [Bob]: New extension for Bob
    Here is a new extension for Bob.
    Main features are:
    * Show input window
    * Translate from clipboard
    
    fix [PR #3380](https://github.com/raycast/extensions/pull/3380)

---
+++ extensions/bob/CHANGELOG.md
@@ -0,0 +1,6 @@
+# Bob Changelog
+
+## [New commands] - 2022-11-1
+
+- Show input window
+- Translate from clipboard
---
+++ extensions/bob/src/transClipboard.tsx
@@ -0,0 +1,20 @@
+import {buildScriptEnsuringBobIsRunning, isEmpty, runAppleScriptSilently} from "./utils";
+import {Clipboard, showHUD} from "@raycast/api";
+
+
+export default async () => {
+  const clipboardText = await Clipboard.readText();
+  if (isEmpty(clipboardText)) {
+    await showHUD("No text in clipboard");
+  } else {
+    const script = buildScriptEnsuringBobIsRunning(
+        `tell application "Bob"
+    launch
+    translate "` + clipboardText + ` "
+end tell`
+    );
+    await runAppleScriptSilently(script);
+    await showHUD("Bob started");
+  }
+
+};
