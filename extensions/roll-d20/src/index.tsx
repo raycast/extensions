a40a07b71a8f9e7e8f91500527d3119b63aec8d9
Per Nielsen Tikær <per@raycast.com>
2022-11-04 09:36:31 +0100

    Update index.tsx

--- extensions/roll-d20/src/index.tsx
+++ extensions/roll-d20/src/index.tsx
@@ -3,5 +3,5 @@ import { showHUD } from "@raycast/api";
 export default async function Command() {
   const d20Roll: string = Math.ceil(Math.random() * 20).toString();
 
-  await showHUD("Your roll: " + d20Roll);
+  await showHUD("🎲 Your roll: " + d20Roll);
 }
