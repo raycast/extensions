4f88ff1c78bef0d3bf3d8d8caf5fa4a30d4f1f23
Sven Tiigi <sven.tiigi@gmail.com>
2022-11-05 09:24:38 +0100

    Use Action.Push instead of manual navigation push

--- extensions/xcode/src/components/swift-package-index-list/swift-package-index-list-item.component.tsx
+++ extensions/xcode/src/components/swift-package-index-list/swift-package-index-list-item.component.tsx
@@ -1,5 +1,5 @@
 import { SwiftPackageIndexSearchResult } from "../../models/swift-package-index/swift-package-index-search-result.model";
-import { Action, ActionPanel, Icon, List, useNavigation } from "@raycast/api";
+import { Action, ActionPanel, Icon, List } from "@raycast/api";
 import { SwiftPackageIndexListItemDetail } from "./swift-package-index-list-item-detail.component";
 import { XcodeAddSwiftPackage } from "../xcode-add-swift-package/xcode-add-swift-package.component";
 
@@ -10,7 +10,6 @@ export function SwiftPackageIndexListItem(props: {
   searchResult: SwiftPackageIndexSearchResult;
   isAddToXcodeActionVisible: boolean;
 }): JSX.Element {
-  const navigation = useNavigation();
   return (
     <List.Item
       id={props.searchResult.id}
@@ -21,11 +20,10 @@ export function SwiftPackageIndexListItem(props: {
         <ActionPanel>
           <Action.OpenInBrowser url={props.searchResult.url} />
           {props.isAddToXcodeActionVisible ? (
-            <Action
-              key="add-to-xcode-project"
+            <Action.Push
               title="Add to Xcode Project"
               icon={Icon.Plus}
-              onAction={() => navigation.push(<XcodeAddSwiftPackage url={props.searchResult.url} />)}
+              target={<XcodeAddSwiftPackage url={props.searchResult.url} />}
             />
           ) : undefined}
           <Action.CopyToClipboard content={props.searchResult.url} shortcut={{ modifiers: ["cmd"], key: "c" }} />
