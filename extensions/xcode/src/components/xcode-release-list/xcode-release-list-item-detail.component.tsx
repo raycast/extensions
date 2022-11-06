ec68c433a61c38e4ce6b1dda9699289f5bb72b64
Sven Tiigi <sven.tiigi@gmail.com>
2022-11-06 10:49:10 +0100

    Added section titles

--- extensions/xcode/src/components/xcode-release-list/xcode-release-list-item-detail.component.tsx
+++ extensions/xcode/src/components/xcode-release-list/xcode-release-list-item-detail.component.tsx
@@ -9,6 +9,7 @@ export function XcodeReleaseListItemDetail(props: { release: XcodeRelease }): JS
     <List.Item.Detail
       metadata={
         <List.Item.Detail.Metadata>
+          <List.Item.Detail.Metadata.Label title="About" />
           <List.Item.Detail.Metadata.Label title="Version" text={props.release.versionNumber} />
           {props.release.beta ? (
             <List.Item.Detail.Metadata.Label title="Beta" text={String(props.release.beta)} />
@@ -17,11 +18,12 @@ export function XcodeReleaseListItemDetail(props: { release: XcodeRelease }): JS
             <List.Item.Detail.Metadata.Label title="Release Candidate" text={String(props.release.releaseCandidate)} />
           ) : undefined}
           <List.Item.Detail.Metadata.Label title="Build" text={props.release.buildNumber} />
-          <List.Item.Detail.Metadata.Label title="Date" text={props.release.date.toLocaleDateString()} />
-          <List.Item.Detail.Metadata.Separator />
+          <List.Item.Detail.Metadata.Label title="Released" text={props.release.date.toLocaleDateString()} />
           {props.release.swiftVersion ? (
             <List.Item.Detail.Metadata.Label title="Swift" text={props.release.swiftVersion} />
           ) : undefined}
+          <List.Item.Detail.Metadata.Separator />
+          <List.Item.Detail.Metadata.Label title="SDKs" />
           {props.release.sdks
             .filter((sdk) => sdk.version)
             .map((sdk) => (
