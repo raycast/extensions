f3f4c44cca82f0558262e6a25e733714d9bdb667
Abdullah Mzaien <xa300600@hotmail.com>
2022-11-08 14:26:37 +0300

    update(Adhan-time): Add search view  (#3419)
    * feat: add adhan time extension
    
    * feat: add adhan time extension
    
    * Update README.md
    
    * Update package.json
    
    * chore: update raycast packages
    
    * chore: update `@raycast/utils` + handling errors
    
    * cs
    
    * feat: adding view mode + code refactor
    
    * chore: reverse menu-bar command name
    
    * Update CHANGELOG.md
    
    Co-authored-by: Per Nielsen Tikær <per@raycast.com>
    Co-authored-by: Sorin Muntean <me@sorinmuntean.ro>

---
+++ extensions/adhan-time/src/prayer-types.ts
@@ -0,0 +1,23 @@
+export interface Preferences {
+  country: string;
+  city: string;
+  calculation_methods: string;
+}
+export type Prayers = {
+  Fajr: string;
+  Sunrise: string;
+  Dhuhr: string;
+  Asr: string;
+  Sunset: string;
+  Maghrib: string;
+  Isha: string;
+  Imsak: string;
+  Midnight: string;
+  Firstthird: string;
+  Lastthird: string;
+};
+export type PrayerType = {
+  data: {
+    timings: Prayers;
+  };
+};
