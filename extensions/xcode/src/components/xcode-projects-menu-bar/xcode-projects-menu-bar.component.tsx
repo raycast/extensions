abfa5fd36a59e4e0e5477388beb1afb6ca35c12e
Sven Tiigi <sven.tiigi@gmail.com>
2022-11-08 06:56:11 +0100

    [Xcode] Add "Open Developer Documentation" command & Maintenance Updates (#3434)
    * Updated Raycast Dependencies and bumped Version
    
    * Omit return statement in command component export
    
    * Use question mark notation instead of union type notation
    
    * Use Action.Push instead of manual navigation push
    
    * Reformat code
    
    * Use system title and icon of Action.OpenInBrowser
    
    * Omit return statements when mapping state models to components
    
    * Added „Open Developer Documentation“ command
    
    * Open folder on right click
    
    * Added Icon for favorite and recent section
    
    * Improved EmptyView if documentation entries are loading
    
    * Bumped version to 1.9.0
    
    * Improved EmptyView if Swift Package Index entries are loading
    
    * Added Xcode Simulator Application List Item Detail and added Launch Action
    
    * Added XcodeReleaseListItemDetail component
    
    * Improved Apple Developer Documentation List component
    
    * Added section titles
    
    * Added missing keys
    
    * Removed unused keys
    
    * Renamed „Installed Simulator Apps“ command to „Search Recent Builds“
    
    * Use Promise.allSettled instead of Promise.all
    
    * Removed custom search bar placeholders
    
    * Updated CHANGELOG.md

--- extensions/xcode/src/components/xcode-projects-menu-bar/xcode-projects-menu-bar.component.tsx
+++ extensions/xcode/src/components/xcode-projects-menu-bar/xcode-projects-menu-bar.component.tsx
@@ -1,4 +1,4 @@
-import { MenuBarExtra } from "@raycast/api";
+import { Icon, MenuBarExtra } from "@raycast/api";
 import { usePromise } from "@raycast/utils";
 import { XcodeProjectsMenuBarItem } from "./xcode-projects-menu-bar-item.component";
 import { XcodeProjectMenuBarService } from "../../services/xcode-project-menu-bar.service";
@@ -14,12 +14,13 @@ export function XcodeProjectsMenuBar(): JSX.Element {
       icon={{ source: { light: "xcode-menu-bar-icon-black.png", dark: "xcode-menu-bar-icon-white.png" } }}
       tooltip="Show Recent Xcode Projects"
     >
-      {menuBarList.data?.favoriteXcodeProjects.length ? <MenuBarExtra.Item title="Favorites" /> : null}
+      {menuBarList.data?.favoriteXcodeProjects.length ? <MenuBarExtra.Item icon={Icon.Star} title="Favorites" /> : null}
       {menuBarList.data?.favoriteXcodeProjects.length ? <MenuBarExtra.Separator /> : null}
-      {menuBarList.data?.favoriteXcodeProjects.map((xcodeProject) => {
-        return <XcodeProjectsMenuBarItem key={xcodeProject.filePath} project={xcodeProject} />;
-      })}
+      {menuBarList.data?.favoriteXcodeProjects.map((xcodeProject) => (
+        <XcodeProjectsMenuBarItem key={xcodeProject.filePath} project={xcodeProject} />
+      ))}
       <MenuBarExtra.Item
+        icon={Icon.Clock}
         title={
           menuBarList.isLoading
             ? "Loading..."
@@ -29,9 +30,9 @@ export function XcodeProjectsMenuBar(): JSX.Element {
         }
       />
       {menuBarList.data?.recentXcodeProjects.length ? <MenuBarExtra.Separator /> : null}
-      {menuBarList.data?.recentXcodeProjects.map((xcodeProject) => {
-        return <XcodeProjectsMenuBarItem key={xcodeProject.filePath} project={xcodeProject} />;
-      })}
+      {menuBarList.data?.recentXcodeProjects.map((xcodeProject) => (
+        <XcodeProjectsMenuBarItem key={xcodeProject.filePath} project={xcodeProject} />
+      ))}
     </MenuBarExtra>
   );
 }
