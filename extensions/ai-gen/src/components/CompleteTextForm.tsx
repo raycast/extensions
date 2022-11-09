99ca92b80f831740bdc6f3fe20dfb11387f5a4b3
Joe Schmitt <joe@compass.com>
2022-11-09 00:16:58 -0500

    Fix advanced options not saving if show advanced turned back off (#3444)
    Refactors the validation so that changes to advanced options stick even if you hide them again before submitting

--- extensions/ai-gen/src/components/CompleteTextForm.tsx
+++ extensions/ai-gen/src/components/CompleteTextForm.tsx
@@ -15,6 +15,21 @@ export interface CompleteTextValues {
   frequency_penalty?: string;
   presence_penalty?: string;
 }
+export interface CompleteTextErrors {
+  prompt?: string;
+  model?: string;
+  temperature?: string;
+  max_tokens?: string;
+  top_p?: string;
+  frequency_penalty?: string;
+  presence_penalty?: string;
+}
+
+interface ValidateFieldOpt {
+  id: keyof CompleteTextErrors;
+  min?: number;
+  max?: number;
+}
 
 const DEFAULT_MODEL = PREFERRED_MODELS[0].id;
 const DEFAULT_TEMP = "0.7";
@@ -29,21 +44,32 @@ export function CompleteTextForm(props: { draftValues?: CompleteTextValues }) {
   const { draftValues } = props;
   const { enableDrafts, storeValue } = getPreferenceValues();
 
-  const [tokenCount, setTokenCount] = useState<number>();
+  const [values, setValues] = useState<CompleteTextValues>({
+    prompt: draftValues?.prompt ?? "",
+    model: DEFAULT_MODEL,
+    temperature: DEFAULT_TEMP,
+    max_tokens: DEFAULT_MAX_TOKENS,
+    top_p: DEFAULT_TOP_P,
+    frequency_penalty: DEFAULT_FREQUENCY_PENALTY,
+    presence_penalty: DEFAULT_PRESENCE_PENALTY,
+  });
+  function setFormValue<T>(k: string, v: T) {
+    return setValues({ ...values, ...{ [k]: v } });
+  }
 
-  const [promptError, setPromptError] = useState<string | undefined>();
+  const [errors, setErrors] = useState<CompleteTextErrors>();
+  function setFormError(k: string, v: string | undefined) {
+    return setErrors({ ...errors, ...{ [k]: v } });
+  }
+
+  const [tokenCount, setTokenCount] = useState<number>();
   const [showAdvanced, setShowAdvanced] = useState<boolean>();
   const [model, setModel] = useState(PREFERRED_MODELS[0]);
-
-  const [temperatureError, setTemperatureError] = useState<string | undefined>();
-  const [maxTokensError, setMaxTokensError] = useState<string | undefined>();
-  const [topPError, setTopPError] = useState<string | undefined>();
-  const [frequencyError, setFequencyError] = useState<string | undefined>();
-  const [presenceError, setPresenceError] = useState<string | undefined>();
 
   const { push } = useNavigation();
 
   function onModelChange(newValue: string) {
+    setFormValue("model", newValue);
     const newModel = PREFERRED_MODELS.find(({ id }) => id == newValue);
     newModel && setModel(newModel);
   }
@@ -59,83 +85,38 @@ export function CompleteTextForm(props: { draftValues?: CompleteTextValues }) {
     setTokenCount(len);
 
     if (!len || len > model.max) {
-      setPromptError(`A prompt is required and must have fewer than ${model.max} tokens`);
+      setFormError("prompt", `A prompt is required and must have fewer than ${model.max} tokens`);
       return false;
     }
 
-    if (promptError?.length) {
-      setPromptError(undefined);
+    if (errors?.prompt?.length) {
+      setFormError("prompt", undefined);
     }
 
     return true;
   }
 
-  function validateTemperature(value?: string, opt: { min?: number; max?: number } = {}) {
-    const valid = validateRange(value, opt);
+  function validateRange(value?: string, opt?: ValidateFieldOpt) {
+    const num = toNum(value);
+    const { id, min = 0, max = 1 } = opt || ({} as ValidateFieldOpt);
 
-    if (!valid) {
-      setTemperatureError(`Value must be betwee ${opt.min} and ${opt.max}`);
+    if (!id) {
       return false;
     }
 
-    if (temperatureError?.length) {
-      setTemperatureError(undefined);
-    }
-  }
-
-  function validateMaxTokens(value?: string, opt: { min?: number; max?: number } = {}) {
-    const valid = validateRange(value, opt);
-
-    if (!valid) {
-      setMaxTokensError(`Value must be betwee ${opt.min} and ${opt.max}`);
+    if (num < min || num > max) {
+      setFormError?.(id, `Value must be betwee ${min} and ${max}`);
       return false;
     }
 
-    if (maxTokensError?.length) {
-      setMaxTokensError(undefined);
+    if (errors?.[id]?.length) {
+      setFormError?.(id, undefined);
     }
+
+    return true;
   }
 
-  function validateTopP(value?: string, opt: { min?: number; max?: number } = {}) {
-    const valid = validateRange(value, opt);
-
-    if (!valid) {
-      setTopPError(`Value must be betwee ${opt.min} and ${opt.max}`);
-      return false;
-    }
-
-    if (topPError?.length) {
-      setTopPError(undefined);
-    }
-  }
-
-  function validateFrequency(value?: string, opt: { min?: number; max?: number } = {}) {
-    const valid = validateRange(value, opt);
-
-    if (!valid) {
-      setFequencyError(`Value must be betwee ${opt.min} and ${opt.max}`);
-      return false;
-    }
-
-    if (frequencyError?.length) {
-      setFequencyError(undefined);
-    }
-  }
-
-  function validatePresence(value?: string, opt: { min?: number; max?: number } = {}) {
-    const valid = validateRange(value, opt);
-
-    if (!valid) {
-      setPresenceError(`Value must be betwee ${opt.min} and ${opt.max}`);
-      return false;
-    }
-
-    if (presenceError?.length) {
-      setPresenceError(undefined);
-    }
-  }
-
-  function onSubmit(values: CompleteTextValues) {
+  function onSubmit() {
     const {
       prompt,
       showAdvanced,
@@ -179,12 +160,14 @@ export function CompleteTextForm(props: { draftValues?: CompleteTextValues }) {
         title={`Prompt\n${tokenCount}/${model.max} tokens`}
         placeholder="The prompt(s) to generate completions for"
         info={`Learn more about prompt design https://beta.openai.com/docs/guides/completion/prompt-design`}
-        error={promptError}
+        error={errors?.prompt}
+        onChange={(value) => {
+          setFormValue("prompt", value);
+          validatePrompt(value);
+        }}
         onBlur={(event) => validatePrompt(event.target.value)}
-        onChange={(value) => validatePrompt(value)}
         autoFocus={true}
         storeValue={!draftValues?.prompt && storeValue}
-        defaultValue={draftValues?.prompt}
       />
       <Form.Checkbox
         id="showAdvanced"
@@ -199,8 +182,8 @@ export function CompleteTextForm(props: { draftValues?: CompleteTextValues }) {
             id="model"
             title="Model"
             info={model.description}
+            value={values?.model}
             onChange={onModelChange}
-            defaultValue={DEFAULT_MODEL}
           >
             {PREFERRED_MODELS.map((model) => (
               <Form.Dropdown.Item key={model.id} title={model.id} value={model.id} />
@@ -210,41 +193,61 @@ export function CompleteTextForm(props: { draftValues?: CompleteTextValues }) {
             id="temperature"
             title="Temperature"
             info="Controls randomness: Lowering results in less random completions. As the temperature approaches zero, the model will become deterministic and repetitive."
-            defaultValue={DEFAULT_TEMP}
-            error={temperatureError}
-            onBlur={(event) => validateTemperature(event.target.value)}
+            error={errors?.temperature}
+            value={values?.temperature}
+            onChange={(v: string) => {
+              setFormValue("temperature", v);
+              validateRange(v, { id: "temperature" });
+            }}
+            onBlur={(event) => validateRange(event.target.value, { id: "temperature" })}
           />
           <Form.TextField
             id="max_tokens"
             title="Maximum length"
             info={`The maximum number of tokens to generate. The "${model.id}" model can use up to ${model.max} tokens shared between prompt and completion. (One token is roughly 4 characters for normal English text)`}
-            defaultValue={DEFAULT_MAX_TOKENS}
-            error={maxTokensError}
-            onBlur={(event) => validateMaxTokens(event.target.value, { min: 0, max: model.max })}
+            error={errors?.max_tokens}
+            value={values?.max_tokens}
+            onChange={(v: string) => {
+              setFormValue("max_tokens", v);
+              validateRange(v, { id: "max_tokens", max: model.max });
+            }}
+            onBlur={(event) => validateRange(event.target.value, { id: "max_tokens", max: model.max })}
           />
           <Form.TextField
             id="top_p"
             title="Top P"
             info="Controls diversity via nucleus sampling: 0.5 means half of all likelihood-weighted options are considered."
-            defaultValue={DEFAULT_TOP_P}
-            error={topPError}
-            onBlur={(event) => validateTopP(event.target.value)}
+            error={errors?.top_p}
+            value={values?.top_p}
+            onChange={(v: string) => {
+              setFormValue("top_p", v);
+              validateRange(v, { id: "top_p" });
+            }}
+            onBlur={(event) => validateRange(event.target.value, { id: "top_p" })}
           />
           <Form.TextField
             id="frequency_penalty"
             title="Frequency penalty"
             info="How much to penalize new tokens based on their existing frequency in the text so far. Decreases the model's likelihood to repeat the same line verbatim."
-            defaultValue={DEFAULT_FREQUENCY_PENALTY}
-            error={frequencyError}
-            onBlur={(event) => validateFrequency(event.target.value, { min: 0, max: 2 })}
+            error={errors?.frequency_penalty}
+            value={values?.frequency_penalty}
+            onChange={(v: string) => {
+              setFormValue("frequency_penalty", v);
+              validateRange(v, { id: "frequency_penalty", max: 2 });
+            }}
+            onBlur={(event) => validateRange(event.target.value, { id: "frequency_penalty", max: 2 })}
           />
           <Form.TextField
             id="presence_penalty"
             title="Presence penalty"
             info="How much to penalize new tokens based on whether they appear in the text so far. Increases the model's likelihood to talk about new topics."
-            defaultValue={DEFAULT_PRESENCE_PENALTY}
-            error={presenceError}
-            onBlur={(event) => validatePresence(event.target.value, { min: 0, max: 2 })}
+            error={errors?.presence_penalty}
+            value={values?.presence_penalty}
+            onChange={(v: string) => {
+              setFormValue("presence_penalty", v);
+              validateRange(v, { id: "presence_penalty", max: 2 });
+            }}
+            onBlur={(event) => validateRange(event.target.value, { id: "presence_penalty", max: 2 })}
           />
         </Fragment>
       ) : null}
@@ -254,15 +257,4 @@ export function CompleteTextForm(props: { draftValues?: CompleteTextValues }) {
 
 function toNum(str?: string) {
   return parseFloat(str ?? "0");
-}
-
-function validateRange(value?: string, opt: { min?: number; max?: number } = {}) {
-  const num = toNum(value);
-  const { min = 0, max = 1 } = opt;
-
-  if (num < min || num > max) {
-    return false;
-  }
-
-  return true;
 }
