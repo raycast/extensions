f2d0e6ce7a29a7420fddf6c94e12721b4f86cfbe
Mustafa Afat <59372593+afatmustafa@users.noreply.github.com>
2022-11-10 11:07:05 +0300

    Update index.tsx

--- extensions/random-laravel-tips-from-laravel-daily/src/index.tsx
+++ extensions/random-laravel-tips-from-laravel-daily/src/index.tsx
@@ -5,15 +5,7 @@ export default function Command() {
     try {
         const {isLoading, data, revalidate}: any = useFetch("https://laraveldaily.com/api/v1/tips?count=1&format=markdown");
         const first = data.data[0];
-        const markdown = `
-# ${first.name}
----
-${first.description}
----
-![](${first.original_image})
----
-Credit: [Laravel Daily](https://laraveldaily.com/)
-`;
+        const markdown = `\n# ${first.name}\n---\n${first.description}\n---\n![](${first.original_image})\n---\nCredit: [Laravel Daily](https://laraveldaily.com/)\n`;
 
         return (
             <Detail
