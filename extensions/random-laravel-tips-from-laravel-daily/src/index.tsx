ba1d2d0910e238fd747ea61d100d1dc4af09528f
Mustafa Afat <59372593+afatmustafa@users.noreply.github.com>
2022-11-10 11:53:02 +0300

    Eslint Prettier

--- extensions/random-laravel-tips-from-laravel-daily/src/index.tsx
+++ extensions/random-laravel-tips-from-laravel-daily/src/index.tsx
@@ -1,9 +1,11 @@
-import {Action, ActionPanel, Detail} from "@raycast/api";
-import {useFetch} from "@raycast/utils";
+import { Action, ActionPanel, Detail } from "@raycast/api";
+import { useFetch } from "@raycast/utils";
 
 export default function Command() {
     try {
-        const {isLoading, data, revalidate}: any = useFetch("https://laraveldaily.com/api/v1/tips?count=1&format=markdown");
+        const { isLoading, data, revalidate }: any = useFetch(
+            "https://laraveldaily.com/api/v1/tips?count=1&format=markdown"
+        );
         const tipTitle = data.data[0].name;
         const tipDescription = data.data[0].description;
         const tipImage = data.data[0].original_image;
@@ -23,13 +25,12 @@ Credit: [Laravel Daily](https://laraveldaily.com/)
                 markdown={markdown}
                 actions={
                     <ActionPanel>
-                        <Action title="Get Another Tip" onAction={() => revalidate()}/>
+                        <Action title="Get Another Tip" onAction={() => revalidate()} />
                     </ActionPanel>
                 }
             />
         );
     } catch (e) {
-        console.log((e as Error).message)
+        console.log((e as Error).message);
     }
-
 }
