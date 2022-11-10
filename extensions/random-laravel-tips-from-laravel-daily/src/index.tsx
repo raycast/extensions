15d66505e32358e6e78754c50b4200c28182d322
Mustafa Afat <59372593+afatmustafa@users.noreply.github.com>
2022-11-10 11:56:53 +0300

    Prettier Eslint

--- extensions/random-laravel-tips-from-laravel-daily/src/index.tsx
+++ extensions/random-laravel-tips-from-laravel-daily/src/index.tsx
@@ -2,15 +2,15 @@ import { Action, ActionPanel, Detail } from "@raycast/api";
 import { useFetch } from "@raycast/utils";
 
 export default function Command() {
-    try {
-        const { isLoading, data, revalidate }: any = useFetch(
-            "https://laraveldaily.com/api/v1/tips?count=1&format=markdown"
-        );
-        const tipTitle = data.data[0].name;
-        const tipDescription = data.data[0].description;
-        const tipImage = data.data[0].original_image;
+  try {
+    const { isLoading, data, revalidate }: any = useFetch(
+      "https://laraveldaily.com/api/v1/tips?count=1&format=markdown"
+    );
+    const tipTitle = data.data[0].name;
+    const tipDescription = data.data[0].description;
+    const tipImage = data.data[0].original_image;
 
-        const markdown = `
+    const markdown = `
 # ${tipTitle}
 ---
 ${tipDescription}
@@ -19,18 +19,18 @@ ${tipDescription}
 ---
 Credit: [Laravel Daily](https://laraveldaily.com/)
 `;
-        return (
-            <Detail
-                isLoading={isLoading}
-                markdown={markdown}
-                actions={
-                    <ActionPanel>
-                        <Action title="Get Another Tip" onAction={() => revalidate()} />
-                    </ActionPanel>
-                }
-            />
-        );
-    } catch (e) {
-        console.log((e as Error).message);
-    }
+    return (
+      <Detail
+        isLoading={isLoading}
+        markdown={markdown}
+        actions={
+          <ActionPanel>
+            <Action title="Get Another Tip" onAction={() => revalidate()} />
+          </ActionPanel>
+        }
+      />
+    );
+  } catch (e) {
+    console.log((e as Error).message);
+  }
 }
