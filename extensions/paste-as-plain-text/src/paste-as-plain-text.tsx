42f1538e8faba64a773319e89ac2f1e8999124bc
Joe Pantazidis <joe.pantazidis@outlook.de>
2022-11-11 10:05:33 +0100

    refactor: use preference option to clean line breaks
    instead of a new command

--- extensions/paste-as-plain-text/src/paste-as-plain-text.tsx
+++ extensions/paste-as-plain-text/src/paste-as-plain-text.tsx
@@ -1,12 +1,15 @@
-import { Clipboard, showHUD } from "@raycast/api";
-import { isEmpty } from "./utils";
+import { Clipboard, showHUD, getPreferenceValues } from "@raycast/api";
+import { Preferences } from "./types";
+import { isEmpty, trim } from "./utils";
 
 export default async () => {
+  const preferences = getPreferenceValues<Preferences>();
   const clipboardText = await Clipboard.readText();
   if (isEmpty(clipboardText)) {
     await showHUD("No text in clipboard");
   } else {
-    await Clipboard.paste(clipboardText + "");
+    const text = preferences.cleanLineBreaks ? trim(clipboardText) : clipboardText + "";
+    await Clipboard.paste(text);
     await showHUD("Paste as Plain Text");
   }
 };
