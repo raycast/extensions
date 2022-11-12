8883e2c31452c9ba29a04357e6f10c8272b8c2e3
Anish De <63192115+AnishDe12020@users.noreply.github.com>
2022-11-12 16:41:28 +0530

    feat: feat: add slidev (english) docsearch

--- extensions/docsearch/src/algolia/apiData.ts
+++ extensions/docsearch/src/algolia/apiData.ts
@@ -215,6 +215,17 @@ export default <IAPIData[]>[
   {
     name: "Slidev",
     icon: "../assets/logo/slidev.png",
+    apiKey: "1ff173fe73b20edc962c1c24c0b1c160",
+    appId: "LCBV6MIFS6",
+    indexName: "slidev",
+    homepage: "https://sli.dev/",
+    searchParameters: {
+      facetFilters: ["language:en"],
+    },
+  },
+  {
+    name: "Slidev",
+    icon: "../assets/logo/slidev.png",
     apiKey: "1a5c5a504139c58f428974c78c55291d",
     appId: "BH4D9OD16A",
     indexName: "slidev",
