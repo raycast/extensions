005a18a64cdfecc3225c2c793d2cc620ecf3818e
Anish De <63192115+AnishDe12020@users.noreply.github.com>
2022-11-12 16:52:24 +0530

    chore: change apollo icon for something more visible in all color modes

--- extensions/docsearch/src/algolia/apiData.ts
+++ extensions/docsearch/src/algolia/apiData.ts
@@ -3,7 +3,7 @@ import type { IAPIData } from "./types";
 export default <IAPIData[]>[
   {
     name: "Apollo",
-    icon: "../assets/apollo.ico",
+    icon: "../assets/logo/apollo.png",
     apiKey: "ef1ac4e3c439bc17a8e7572700691efc",
     appId: "Q0CJ63JM7B",
     indexName: "docs",
