28a1fa94ef72f124e537c9d80d2f997f588e5a3f
Jakub Soboczyński <teziovsky@gmail.com>
2022-11-13 13:05:41 +0100

    feat(actions): add keys to each element

--- extensions/react-icons/src/utils/actions.tsx
+++ extensions/react-icons/src/utils/actions.tsx
@@ -3,12 +3,14 @@ import { renderToStaticMarkup } from "react-dom/server";
 import { ActionFunction, Actions } from "../types";
 
 export const actions: Record<Actions, ActionFunction> = {
-  copyJSX: (name, _) => <Action.CopyToClipboard title="Copy JSX" content={`<${name} />`} />,
-  copySVG: (_, IconComponent) => (
-    <Action.CopyToClipboard title="Copy SVG" content={renderToStaticMarkup(<IconComponent />)} />
+  copyJSX: (name, _) => <Action.CopyToClipboard key={name} title="Copy JSX" content={`<${name} />`} />,
+  copySVG: (name, IconComponent) => (
+    <Action.CopyToClipboard key={name} title="Copy SVG" content={renderToStaticMarkup(<IconComponent />)} />
   ),
-  copyName: (name, _) => <Action.CopyToClipboard title="Copy Name" content={name} />,
-  pasteJSX: (name, _) => <Action.Paste title="Paste JSX" content={`<${name} />`} />,
-  pasteSVG: (_, IconComponent) => <Action.Paste title="Paste SVG" content={renderToStaticMarkup(<IconComponent />)} />,
-  pasteName: (name, _) => <Action.Paste title="Paste Name" content={name} />,
+  copyName: (name, _) => <Action.CopyToClipboard key={name} title="Copy Name" content={name} />,
+  pasteJSX: (name, _) => <Action.Paste key={name} title="Paste JSX" content={`<${name} />`} />,
+  pasteSVG: (name, IconComponent) => (
+    <Action.Paste key={name} title="Paste SVG" content={renderToStaticMarkup(<IconComponent />)} />
+  ),
+  pasteName: (name, _) => <Action.Paste key={name} title="Paste Name" content={name} />,
 };
