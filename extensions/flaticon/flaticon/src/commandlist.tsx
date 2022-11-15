6d1fc0f309847af62e3043d3c5419b83cc12681e
Vitaliy Kudryk <contact@bgnfu7re.me>
2022-11-15 10:58:11 +0200

    create command 🚧

---
+++ extensions/flaticon/flaticon/src/commandlist.tsx
@@ -0,0 +1,8 @@
+import {Color, Grid} from "@raycast/api";
+
+// noinspection JSUnusedGlobalSymbols
+export default function CommandList() {
+  return <Grid>
+    <Grid.Item content={{color: Color.Blue}}></Grid.Item>
+  </Grid>
+}
