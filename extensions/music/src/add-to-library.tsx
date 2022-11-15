9ff520ff561011196dcc22eb4d617fd3eb069c23
Peter <95014170+peterdev22@users.noreply.github.com>
2022-11-15 18:46:13 +1030

    SF Symbols are now enums

--- extensions/music/src/add-to-library.tsx
+++ extensions/music/src/add-to-library.tsx
@@ -1,9 +1,10 @@
 import { pipe } from "fp-ts/lib/function";
 
+import { SFSymbols } from "./util/models";
 import * as music from "./util/scripts";
 import { handleTaskEitherError } from "./util/utils";
 
 export default pipe(
   music.currentTrack.addToLibrary,
-  handleTaskEitherError("✗ Failed to Add to Library", "􀈄 Added to library")
+  handleTaskEitherError(SFSymbols.WARNING + " Failed to Add to Library", SFSymbols.ADD_TO_LIBRARY + " Added to library")
 )();
--- extensions/music/src/toggle-play-pause.tsx
+++ extensions/music/src/toggle-play-pause.tsx
@@ -1,9 +1,13 @@
 import { pipe } from "fp-ts/lib/function";
 
+import { SFSymbols } from "./util/models";
 import * as music from "./util/scripts";
 import { handleTaskEitherError } from "./util/utils";
 
 export default pipe(
   music.player.togglePlay,
-  handleTaskEitherError("✗ Failed to toggle play/pause", "􀊈 Successfully played/paused")
+  handleTaskEitherError(
+    SFSymbols.WARNING + " Failed to toggle play/pause",
+    SFSymbols.PLAYPAUSE + " Successfully played/paused"
+  )
 )();
