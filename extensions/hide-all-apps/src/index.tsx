a2677e46abe243946df99a039a59eeadde4e8436
Pedro Duarte <oi@ped.ro>
2022-11-17 10:40:53 +0100

    [Hide All Apps]: Update Apple Script

--- extensions/hide-all-apps/src/index.tsx
+++ extensions/hide-all-apps/src/index.tsx
@@ -10,9 +10,10 @@ export default async function Command() {
 
   // https://gist.github.com/Teraflopst/e1863cfff5f0deb46ec2cabe9984305b
   await runAppleScript(`
-tell application "Finder"
-    set visible of every process whose visible is true and name is not "Finder" to false
-    set the collapsed of windows to true
+activate application "Finder"
+tell application "System Events"
+    set visible of processes where name is not "Finder" to false
 end tell
+tell application "Finder" to set collapsed of windows to true
   `);
 }
