d8573735d76976c738734313b6eb987ac71ee41d
Yukai Huang <yukaihuangtw@gmail.com>
2022-11-23 21:00:37 +0800

    feat: render blocks as markdown

--- extensions/curator-bio/lib/types.ts
+++ extensions/curator-bio/lib/types.ts
@@ -2,3 +2,106 @@ export type Preference = {
   email: string;
   password: string;
 };
+
+export interface Item {
+  id: string;
+  customId: string;
+  type: "image" | "url";
+  url: string;
+  cover: string;
+  coverFitSize: boolean;
+  title: string;
+  subtitle: string;
+  tags: string[];
+  collections: CollectionsItem[];
+  userId: string;
+  user: User;
+  meta: any[];
+  content: Content;
+  createdAt: string;
+  updatedAt: string;
+  status: string;
+  settings: Settings;
+}
+export interface CollectionsItem {
+  name: string;
+  icon: string;
+  items: string[];
+  childs: any[];
+  views: number;
+  subs: number;
+  customId: string;
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+}
+export interface User {
+  id: string;
+  customId: string;
+  name: string;
+  avatar: string;
+  about: string;
+}
+export interface Content {
+  time: number;
+  blocks: BlocksItem[];
+  version: string;
+  html: string;
+}
+export type BlocksItem = {
+  id: string;
+} & FileType;
+
+type FileType =
+  | {
+      type: "linkTool";
+      data: LinkData;
+    }
+  | {
+      type: "image";
+      data: ImageData;
+    }
+  | {
+      type: "embed";
+      data: EmbedData;
+    };
+
+export interface LinkData {
+  link: string;
+  meta: LinkMeta;
+}
+export interface LinkMeta {
+  title: string;
+  description: string;
+  image: Image;
+}
+export interface Image {
+  url: string;
+}
+
+export interface ImageData {
+  file: File;
+  caption: string;
+  stretched: boolean;
+  withBorder: boolean;
+  withBackground: boolean;
+}
+export interface File {
+  url: string;
+}
+export interface Settings {
+  link: Link;
+}
+export interface Link {
+  enabled: boolean;
+  value: string;
+}
+
+interface EmbedData {
+  service: string;
+  source: string;
+  embed: string;
+  width: number;
+  height: number;
+  caption: string;
+}
--- extensions/curator-bio/src/feed.tsx
+++ extensions/curator-bio/src/feed.tsx
@@ -1,6 +1,71 @@
-import { Action, ActionPanel, Form, Grid, List } from "@raycast/api";
-import { useState } from "react";
+import { Action, ActionPanel, List } from "@raycast/api";
+import { useMemo, useState } from "react";
 import { useMe, useSubscriptions } from "../lib/hooks";
+import { BlocksItem, Item, ImageData, LinkData } from "../lib/types";
+
+const blocksToMarkdownRenderer = (blocks: BlocksItem[] = []): string => {
+  return blocks
+    .map((block) => {
+      const data = block.data;
+      switch (block.type) {
+        case "linkTool": {
+          const {
+            meta: { image, title: _title, description: _description },
+            link,
+          } = data as LinkData;
+          const description = _description.length > 150 ? _description.slice(0, 150) + "..." : _description;
+          const title = _title.length > 100 ? _title.slice(0, 100) + "..." : _title;
+
+          return `### [${title}](${link})
+
+> ${description}
+${image?.url && `\n![${_title}](${image?.url})`}
+          `;
+        }
+
+        case "image": {
+          const {
+            file: { url },
+            caption,
+          } = data as ImageData;
+
+          return `![${caption}](${url})`;
+        }
+
+        default:
+          console.warn(`Unknown block type: ${block.type}`);
+          return "";
+      }
+    })
+    .join("\n");
+};
+
+const ItemDetail = ({ item }: { item: Item }) => {
+  const link = item.settings?.link?.value;
+
+  const blocksMarkdown = useMemo(() => {
+    return blocksToMarkdownRenderer(item.content.blocks);
+  }, [item.content.blocks]);
+
+  const markdown = useMemo(() => {
+    return `# ${item.title}
+
+## ${item.subtitle}
+
+${blocksMarkdown}
+`;
+  }, [blocksMarkdown, item.subtitle, item.title]);
+
+  return (
+    <List.Item
+      key={item.id}
+      title={item.title}
+      subtitle={item.subtitle}
+      actions={<ActionPanel>{link && <Action.OpenInBrowser title="Open in Browser" url={link} />}</ActionPanel>}
+      detail={<List.Item.Detail markdown={markdown} />}
+    />
+  );
+};
 
 export default function Login() {
   const { isLoading: isLogging, data: userResponse, error } = useMe();
@@ -12,25 +77,11 @@ export default function Login() {
 
   const isListLoading = isLoading || isLogging;
 
-  console.log(data);
-
   return (
-    <Grid isLoading={isListLoading}>
-      {data.map((item: any) => {
-        const link = item.settings?.link?.enabled ? item.settings?.link?.value : null;
-
-        return (
-          <Grid.Item
-            key={item.id}
-            content={{
-              source: item.cover,
-            }}
-            title={item.title}
-            subtitle={item.subtitle}
-            actions={<ActionPanel>{link && <Action.OpenInBrowser title="Open in Browser" url={link} />}</ActionPanel>}
-          />
-        );
-      })}
-    </Grid>
+    <List isLoading={isListLoading} isShowingDetail={true}>
+      {data.map((item: any) => (
+        <ItemDetail key={item.id} item={item} />
+      ))}
+    </List>
   );
 }
