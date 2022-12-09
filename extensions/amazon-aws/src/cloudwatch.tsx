0ba5d8071c4ec5db4480a726c409011e43c7242f
JonathanWbn <jwieben@hey.com>
2022-12-09 19:53:50 +0100

    feat: add cloudwatch command

---
+++ extensions/amazon-aws/src/cloudwatch.tsx
@@ -0,0 +1,69 @@
+import { CloudWatchLogsClient, DescribeLogGroupsCommand, LogGroup } from "@aws-sdk/client-cloudwatch-logs";
+import { Action, ActionPanel, Icon, List } from "@raycast/api";
+import { useCachedPromise } from "@raycast/utils";
+import { useState } from "react";
+import AWSProfileDropdown, { AWS_URL_BASE } from "./aws-profile-dropdown";
+
+export default function CloudWatch() {
+  const [search, setSearch] = useState<string>("");
+  const { data: logGroups, error, isLoading, revalidate } = useCachedPromise(fetchLogGroups, [search]);
+
+  return (
+    <List
+      isLoading={isLoading}
+      searchBarPlaceholder="Filter log groups by name..."
+      searchBarAccessory={<AWSProfileDropdown onProfileSelected={revalidate} />}
+      searchText={search}
+      onSearchTextChange={setSearch}
+    >
+      {search.length < 4 ? (
+        <List.EmptyView
+          title="Missing Search Query"
+          icon={Icon.MagnifyingGlass}
+          description="The search will begin when at least 4 characters are provided."
+        />
+      ) : error ? (
+        <List.EmptyView title={error.name} description={error.message} icon={Icon.Warning} />
+      ) : (
+        logGroups?.map((logGroup) => <LogGroup key={logGroup.logGroupName} logGroup={logGroup} />)
+      )}
+    </List>
+  );
+}
+
+function LogGroup({ logGroup }: { logGroup: LogGroup }) {
+  return (
+    <List.Item
+      icon={Icon.Document}
+      title={logGroup.logGroupName || ""}
+      actions={
+        <ActionPanel>
+          <Action.OpenInBrowser
+            title="Open CloudWatch log group"
+            url={`${AWS_URL_BASE}/cloudwatch/home?region=${
+              process.env.AWS_REGION
+            }#logsV2:log-groups/log-group/${encodeURIComponent(logGroup.logGroupName || "")}`}
+          />
+          <Action.CopyToClipboard title="Copy Log Group Name" content={logGroup.logGroupName || ""} />
+        </ActionPanel>
+      }
+    />
+  );
+}
+
+async function fetchLogGroups(search: string, token?: string, accLogGroups?: LogGroup[]): Promise<LogGroup[]> {
+  if (search.length < 4) return [];
+  if (!process.env.AWS_PROFILE) return [];
+
+  const { nextToken, logGroups } = await new CloudWatchLogsClient({}).send(
+    new DescribeLogGroupsCommand({ nextToken: token, logGroupNamePattern: search || undefined })
+  );
+
+  const combinedLogGroups = [...(accLogGroups || []), ...(logGroups || [])];
+
+  if (nextToken) {
+    return fetchLogGroups(search, nextToken, combinedLogGroups);
+  }
+
+  return combinedLogGroups;
+}
