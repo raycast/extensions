adda6e77ea31ad6eef9c5850a850bbb3053c11c8
Yukai Huang <yukaihuangtw@gmail.com>
2022-12-09 14:00:20 +0800

    feat: add form validation to create offer form

--- extensions/bitfinex/src/components/CreateOfferForm.tsx
+++ extensions/bitfinex/src/components/CreateOfferForm.tsx
@@ -3,6 +3,14 @@ import { FundingOffer } from "bfx-api-node-models";
 import LendingRates from "../lending-rates";
 import Bitfinex, { handleAPIError } from "../lib/api";
 import { getCurrency } from "../lib/preference";
+import { useForm, FormValidation } from "@raycast/utils";
+
+type CreateOfferFormValues = {
+  symbol: string;
+  amount: string;
+  rate: string;
+  period: string;
+};
 
 export function CreateOfferForm({
   mutateBalanceInfo,
@@ -14,31 +22,73 @@ export function CreateOfferForm({
   const rest = Bitfinex.rest();
   const { pop } = useNavigation();
 
-  const onSubmit = async (values: any) => {
-    try {
-      const newOffer = new FundingOffer({
-        type: "LIMIT",
-        symbol: values.symbol,
-        rate: parseFloat(values.rate) / 100 / 365,
-        amount: parseFloat(values.amount),
-        period: parseInt(values.period, 10),
-      });
-      await rest.submitFundingOffer(newOffer);
-    } catch (e: any) {
-      handleAPIError("Create offer failed", e);
-    }
+  const { handleSubmit, itemProps } = useForm<CreateOfferFormValues>({
+    async onSubmit(values) {
+      try {
+        const newOffer = new FundingOffer({
+          type: "LIMIT",
+          symbol: values.symbol,
+          rate: parseFloat(values.rate) / 100 / 365,
+          amount: parseFloat(values.amount),
+          period: parseInt(values.period, 10),
+        });
+        await rest.submitFundingOffer(newOffer);
+      } catch (e: any) {
+        handleAPIError("Create offer failed", e);
+      }
 
-    mutateFundingInfo();
-    mutateBalanceInfo();
+      mutateFundingInfo();
+      mutateBalanceInfo();
 
-    pop();
-  };
+      pop();
+    },
+    validation: {
+      symbol: FormValidation.Required,
+      amount: (value) => {
+        if (!value) {
+          return "Amount is required";
+        }
+
+        const amount = parseFloat(value);
+        if (isNaN(amount)) {
+          return "Amount must be a number";
+        }
+        if (amount < 150) {
+          return "Amount must be greater than 150";
+        }
+      },
+      rate: (value) => {
+        if (!value) {
+          return "Rate is required";
+        }
+
+        const rate = parseFloat(value);
+        if (isNaN(rate)) {
+          return "Rate must be a number";
+        }
+      },
+      period: (value) => {
+        if (!value) {
+          return "Period is required";
+        }
+
+        const period = parseInt(value, 10);
+        if (isNaN(period)) {
+          return "Period must be a number";
+        }
+
+        if (period < 1 || period > 120) {
+          return "Period must be between 1 and 120";
+        }
+      },
+    },
+  });
 
   return (
     <Form
       actions={
         <ActionPanel>
-          <Action.SubmitForm title="Create Offer" onSubmit={onSubmit} icon={Icon.PlusCircle} />
+          <Action.SubmitForm title="Create Offer" onSubmit={handleSubmit} icon={Icon.PlusCircle} />
           <Action.Push
             title="Show Lending Rates"
             target={<LendingRates />}
@@ -51,16 +101,16 @@ export function CreateOfferForm({
         </ActionPanel>
       }
     >
-      <Form.TextField id="symbol" title="Symbol" defaultValue={getCurrency()} />
+      <Form.TextField title="Symbol" defaultValue={getCurrency()} {...itemProps.symbol} />
 
-      <Form.TextField id="amount" title="Amount" defaultValue="100" />
+      <Form.TextField title="Amount" defaultValue="150" {...itemProps.amount} />
       <Form.TextField
-        id="rate"
         title="Rate (in APR)"
         defaultValue="18"
         info="Press CMD+Shift+L to see recent lending rates."
+        {...itemProps.rate}
       />
-      <Form.TextField id="period" title="Period (in days)" defaultValue="7" />
+      <Form.TextField title="Period (in days)" defaultValue="7" {...itemProps.period} />
     </Form>
   );
 }
