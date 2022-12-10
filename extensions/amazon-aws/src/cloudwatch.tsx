c59853a1b5aa26b1e205e34cbf88e602c58b0163
JonathanWbn <jwieben@hey.com>
2022-12-10 09:17:42 +0100

    chore: consistent casing

--- extensions/amazon-aws/src/cloudwatch.tsx
+++ extensions/amazon-aws/src/cloudwatch.tsx
@@ -39,7 +39,7 @@ function LogGroup({ logGroup }: { logGroup: LogGroup }) {
       actions={
         <ActionPanel>
           <Action.OpenInBrowser
-            title="Open CloudWatch log group"
+            title="Open CloudWatch Log Group"
             url={`${AWS_URL_BASE}/cloudwatch/home?region=${
               process.env.AWS_REGION
             }#logsV2:log-groups/log-group/${encodeURIComponent(logGroup.logGroupName || "")}`}
