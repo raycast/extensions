f66ec9343c767fefa2af4bf8c3c231c6b2ecbaf4
crisboarna <cristian.boarna@gmail.com>
2022-12-13 09:52:43 +0200

    feat(browser history): add Chrome, Firefox, Safari, Edge, Brave, Vivaldi history search (#3791)
    * feat(browser history): added chrome, firefox, safari history search
    
    * feat(browser history): added extra store pictures
    
    * feat(browser history): added extra store pictures correct size
    
    * feat(browser history): updated readme
    
    * fix(browser-history): removed action panel title
    
    * feat(browser history): added edge, brave, vivaldi
    
    * feat(browser history): replaced wasm with useSQL
    
    * feat(browser history): removed iml ignore
    
    * Update CHANGELOG.md
    
    Co-authored-by: Per Nielsen Tikær <per@raycast.com>

---
+++ extensions/browser-history/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Browser History Changelog
+
+## [Initial Version] - 2022-12-13
+
+- Added Chrome, Firefox, Safari, Edge, Brave and Vivaldi history search support.
---
+++ extensions/browser-history/src/util/index.ts
@@ -0,0 +1,77 @@
+import fs from "fs";
+import path from "path";
+import { SupportedBrowsers } from "../interfaces";
+
+const userLibraryDirectoryPath = () => {
+  if (!process.env.HOME) {
+    throw new Error("$HOME environment variable is not set.");
+  }
+
+  return path.join(process.env.HOME, "Library");
+};
+
+const getProfileName = (userDirectoryPath: string, browser: SupportedBrowsers) => {
+  let profiles;
+  switch (browser) {
+    case SupportedBrowsers.Firefox:
+      profiles = fs.readdirSync(userDirectoryPath);
+      return profiles.filter((profile) => profile.endsWith(".default-release"))[0];
+    default:
+      return "Default";
+  }
+};
+
+export const getHistoryDbPath = (browser: SupportedBrowsers) => {
+  const userDataDirectory = userLibraryDirectoryPath();
+  let profilePath, profileName;
+
+  switch (browser) {
+    case SupportedBrowsers.Chrome:
+      profilePath = path.join(userDataDirectory, "Application Support", "Google", "Chrome");
+      profileName = getProfileName(profilePath, browser);
+      return path.join(profilePath, profileName, "History");
+    case SupportedBrowsers.Firefox:
+      profilePath = path.join(userDataDirectory, "Application Support", "Firefox", "Profiles");
+      profileName = getProfileName(profilePath, browser);
+      return path.join(profilePath, profileName, "places.sqlite");
+    case SupportedBrowsers.Safari:
+      return path.join(userDataDirectory, "Safari", "History.db");
+    case SupportedBrowsers.Edge:
+      return path.join(userDataDirectory, "Application Support", "Microsoft Edge", "Default", "History");
+    case SupportedBrowsers.Brave:
+      return path.join(
+        userDataDirectory,
+        "Application Support",
+        "BraveSoftware",
+        "Brave-Browser",
+        "Default",
+        "History"
+      );
+    case SupportedBrowsers.Vivaldi:
+      return path.join(userDataDirectory, "Application Support", "Vivaldi", "Default", "History");
+    default:
+      throw new Error("Unsupported browser.");
+  }
+};
+
+export const getHistoryTable = (browser: SupportedBrowsers): string => {
+  switch (browser) {
+    case SupportedBrowsers.Firefox:
+      return "moz_places";
+    case SupportedBrowsers.Safari:
+      return "history_items";
+    default:
+      return "urls";
+  }
+};
+
+export const getHistoryDateColumn = (browser: SupportedBrowsers): string => {
+  switch (browser) {
+    case SupportedBrowsers.Firefox:
+      return "last_visit_date";
+    case SupportedBrowsers.Safari:
+      return "visit_time";
+    default:
+      return "last_visit_time";
+  }
+};
