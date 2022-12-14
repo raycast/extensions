8bf544ee83c119ee512de9f791f679a6d67e31c0
Zach Fuller <zach5990@gmail.com>
2022-12-13 20:56:11 -0600

    style(youtube-music): replace play and pause commands with play/pause

--- extensions/youtube-music/src/play.tsx
+++
@@ -1,8 +0,0 @@
-import { closeMainWindow } from "@raycast/api";
-import { runJSInYouTubeMusicTab } from "./utils";
-
-export default async () => {
-  if (await runJSInYouTubeMusicTab("document.querySelector('#play-pause-button').click();")) {
-    await closeMainWindow();
-  }
-};
