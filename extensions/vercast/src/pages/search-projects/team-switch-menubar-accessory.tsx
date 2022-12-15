848150e7deb7e4ab6cb2e591eb5c2cc04fb2e8ff
Bruce Giovando <bgiovand@gmail.com>
2022-12-14 19:12:19 -0800

    fixed lint issue

--- extensions/vercast/src/pages/search-projects/team-switch-menubar-accessory.tsx
+++ extensions/vercast/src/pages/search-projects/team-switch-menubar-accessory.tsx
@@ -1,11 +1,7 @@
 import { MenuBarExtra, Icon, Color } from "@raycast/api";
 import useVercel from "../../hooks/use-vercel-info";
 
-const MenuBarTeamAccessory = ({
-  onTeamChange,
-}: {
-  onTeamChange: () => void;
-}) => {
+const MenuBarTeamAccessory = ({ onTeamChange }: { onTeamChange: () => void }) => {
   const { user, selectedTeam, teams, updateSelectedTeam } = useVercel();
 
   const onChange = async (teamIdOrUsername: string) => {
@@ -16,36 +12,30 @@ const MenuBarTeamAccessory = ({
   const selectedID = selectedTeam?.id ?? user?.username;
 
   return (
-    <MenuBarExtra.Submenu
-      title={`${selectedTeam?.name ?? user?.name ?? "None"}`}
-      icon={Icon.TwoPeople}
-    >
+    <MenuBarExtra.Submenu title={`${selectedTeam?.name ?? user?.name ?? "None"}`} icon={Icon.TwoPeople}>
       <MenuBarExtra.Item title="Personal Account" />
       {user && (
         <MenuBarExtra.Item
           title={user.name || user.username}
           icon={{
             source: Icon.Person,
-            tintColor:
-              user.username == selectedID ? Color.Green : Color.PrimaryText,
+            tintColor: user.username == selectedID ? Color.Green : Color.PrimaryText,
           }}
           onAction={(newValue) => onChange(user.username)}
         />
       )}
-      {teams?.length && (<MenuBarExtra.Item title="Teams" />)}
-      {
-        teams?.map((team) => (
-          <MenuBarExtra.Item
-            key={team.id}
-            title={team.name}
-            icon={{
-              source: Icon.TwoPeople,
-              tintColor:
-                team.id == selectedID ? Color.Green : Color.PrimaryText,
-            }}
-            onAction={(newValue) => onChange(team.id)}
-          />
-        ))}
+      {teams?.length && <MenuBarExtra.Item title="Teams" />}
+      {teams?.map((team) => (
+        <MenuBarExtra.Item
+          key={team.id}
+          title={team.name}
+          icon={{
+            source: Icon.TwoPeople,
+            tintColor: team.id == selectedID ? Color.Green : Color.PrimaryText,
+          }}
+          onAction={(newValue) => onChange(team.id)}
+        />
+      ))}
     </MenuBarExtra.Submenu>
   );
 };
