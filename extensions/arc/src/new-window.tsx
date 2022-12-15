f289762ac8ec062fe97646cfd750d5ce775491e0
Zach Fuller <zach5990@gmail.com>
2022-12-14 20:23:07 -0600

    Address feedback

--- extensions/arc/src/new-window.tsx
+++ extensions/arc/src/new-window.tsx
@@ -4,7 +4,6 @@ import { createNewArcWindow } from "./utils";
 export default async function Command() {
   try {
     await createNewArcWindow();
-    await showHUD("✅ Window created");
   } catch {
     await showHUD("❌ Couldn't create window");
   }
