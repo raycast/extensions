b00f02ff2209e4b3ac963b2d4b0c536e30cff484
PeterZhu <zyxchelsea11@gmail.com>
2022-12-21 14:51:51 +0800

    run Prettier to do some code formatting

--- extensions/show-desktop/src/show-desktop.tsx
+++ extensions/show-desktop/src/show-desktop.tsx
@@ -1,16 +1,16 @@
-// Create by Peter Zhu
+// Create by Peter Zhu 2022-12-21
 
 import { closeMainWindow, clearSearchBar, popToRoot, Detail } from "@raycast/api";
 import { runAppleScript } from "run-applescript";
 
 export default function Command() {
-    clearSearchBar();
-    closeMainWindow({ clearRootSearch: true });
-    popToRoot({ clearSearchBar: true });
-    runAppleScript(`
+  clearSearchBar();
+  closeMainWindow({ clearRootSearch: true });
+  popToRoot({ clearSearchBar: true });
+  runAppleScript(`
     tell application "Finder" to activate
     tell application "System Events" to keystroke "h" using {command down, option down}
     tell application "Finder" to activate
     tell application "System Events" to keystroke "m" using {command down, option down}
-    `);  
+    `);
 }
