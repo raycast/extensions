3f05542c18fc585168bd24a54bdad7bfb83145fc
mathieudutour <mathieu@dutour.me>
2022-12-21 17:11:46 +0100

    Add playground extension

---
+++ extensions/playground/src/run-playground.tsx
@@ -0,0 +1,30 @@
+import { Form, ActionPanel, Action, Color } from "@raycast/api";
+
+export default function Command() {
+  return (
+    <Form
+      actions={
+        <ActionPanel>
+          <Action.SubmitForm
+            onSubmit={(values) => {
+              console.log(values);
+            }}
+          />
+        </ActionPanel>
+      }
+    >
+      <Form.TagPicker id="test" title="tag picker" storeValue>
+        <Form.TagPicker.Item
+          value="orange"
+          title="orange"
+          icon={{
+            source: `icon.png`,
+            tintColor: Color.PrimaryText,
+          }}
+        />
+        <Form.TagPicker.Item value="green" title="green" />
+        <Form.TagPicker.Item value="blue" title="blue" />
+      </Form.TagPicker>
+    </Form>
+  );
+}
