11e2a3a3cf8db113728a5597b1c89bb115ecb1f8
Mathieu Dutour <mathieu@dutour.me>
2022-12-23 10:33:39 +0100

    Update index.tsx

--- extensions/test67/src/index.tsx
+++ extensions/test67/src/index.tsx
@@ -1,5 +1,5 @@
 import { Detail } from "@raycast/api";
 
 export default function Command() {
-  return <Detail markdown="# Hello World" />;
+  return <Detail markdown="# Hello Worldoiha wdih" />;
 }
