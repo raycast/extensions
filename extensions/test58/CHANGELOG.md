e16ad91435d1e85a76ea11606dfec6d2c489c629
mathieudutour <mathieu@dutour.me>
2022-12-23 10:17:31 +0100

    first commit :tada:

---
+++ extensions/test58/CHANGELOG.md
@@ -0,0 +1,3 @@
+# test58 Changelog
+
+## [Initial Version] - 2022-12-23
---
+++ extensions/test58/src/index.tsx
@@ -0,0 +1,5 @@
+import { Detail } from "@raycast/api";
+
+export default function Command() {
+  return <Detail markdown="# Hello World" />;
+}
