82976a9755d124ccba284f272cca55da4023ec66
Charles <thexrayone@icloud.com>
2022-12-27 22:08:48 -0500

    feat: :construction: start iterating on transaction detail view metadata

--- extensions/raynab/src/components/transactions/transactionDetails.tsx
+++ extensions/raynab/src/components/transactions/transactionDetails.tsx
@@ -1,4 +1,4 @@
-import { Action, ActionPanel, Detail } from '@raycast/api';
+import { Action, ActionPanel, Color, Detail } from '@raycast/api';
 import { CurrencyFormat, TransactionDetail } from '@srcTypes';
 import dayjs from 'dayjs';
 
@@ -12,21 +12,38 @@ import { OpenInYnabAction } from '@components/actions';
 export function TransactionDetails({ transaction }: { transaction: TransactionDetail }) {
   const [activeBudgetCurrency] = useLocalStorage<CurrencyFormat | null>('activeBudgetCurrency', null);
 
-  const markdown = `
-  # ${transaction.amount > 0 ? 'Inflow to' : 'Outflow from'} ${transaction.account_name}
+  const hasSubtransactions = transaction.subtransactions.length > 0;
 
-  - **Amount**: ${formatToReadablePrice({
-    amount: transaction.amount,
-    currency: activeBudgetCurrency,
-  })}
-  - **Payee**: ${transaction.payee_name ?? 'Not Specified'}
-  - **Date**: ${dayjs(transaction.date).format('LL')}
-  - **Category**: ${transaction.category_name ?? 'Not Specified'}
+  const markdown = `
+  # ${transaction.payee_name}
   `;
+
   return (
     <Detail
       navigationTitle="Transaction Details"
       markdown={markdown}
+      metadata={
+        <Detail.Metadata>
+          <Detail.Metadata.Label
+            title="Amount"
+            text={formatToReadablePrice({ amount: transaction.amount, currency: activeBudgetCurrency })}
+          />
+          <Detail.Metadata.Label title="Account" text={transaction.account_name} />
+          <Detail.Metadata.Label title="Date" text={dayjs(transaction.date).format('LL')} />
+          <Detail.Metadata.TagList title={hasSubtransactions ? 'Categories' : 'Category'}>
+            {hasSubtransactions ? (
+              transaction.subtransactions.map((transaction) => (
+                <Detail.Metadata.TagList.Item
+                  key={transaction.id}
+                  text={transaction.category_name ?? 'Not Specified'}
+                />
+              ))
+            ) : (
+              <Detail.Metadata.TagList.Item text={transaction.category_name ?? 'Not Specified'} color={Color.Green} />
+            )}
+          </Detail.Metadata.TagList>
+        </Detail.Metadata>
+      }
       actions={
         <ActionPanel>
           <Action.CopyToClipboard
