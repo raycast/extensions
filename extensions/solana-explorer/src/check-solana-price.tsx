e1dee15eaab2b53280ca7e907dde9393ce2b0734
Steve Simkins <73185218+stevedsimkins@users.noreply.github.com>
2023-01-02 10:29:19 -0500

    Removed console.log

--- extensions/solana-explorer/src/check-solana-price.tsx
+++ extensions/solana-explorer/src/check-solana-price.tsx
@@ -13,7 +13,6 @@ const fetchPrice = () => {
         const res = await axios.get("https://price.jup.ag/v3/price?ids=SOL");
         const data = res.data;
         const priceData = Math.round(data.data.SOL.price * 100) / 100;
-        console.log(priceData);
         setState({
           price: `$${priceData}`,
           isLoading: false,
