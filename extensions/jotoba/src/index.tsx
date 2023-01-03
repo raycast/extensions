2940c4c1d0bbda6368857f595c960398eb18aea1
Per Nielsen Tikær <per@raycast.com>
2023-01-03 07:12:50 +0100

    Removed description

--- extensions/jotoba/src/index.tsx
+++ extensions/jotoba/src/index.tsx
@@ -53,11 +53,7 @@ export default function Command(props: { arguments: SearchArguments }) {
         </>
       )}
       {state.searchText === "" && (
-        <List.EmptyView
-          icon={{ source: "JotoHead.svg" }}
-          title={"Type something to start your search."}
-          description={"The Jotoba Project is made possible by JojiiOfficial and Yukaru.\n Raycast extension by clnhs."}
-        />
+        <List.EmptyView icon={{ source: "JotoHead.svg" }} title="Type something to start your search." />
       )}
     </List>
   );
