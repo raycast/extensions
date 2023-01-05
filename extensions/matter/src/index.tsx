93d90ad4f9b4e999fe23027fba04fa6aa7cc60bd
Per Nielsen Tikær <per@raycast.com>
2023-01-05 13:25:09 +0100

    Update

--- extensions/matter/src/index.tsx
+++ extensions/matter/src/index.tsx
@@ -36,25 +36,23 @@ export default function Command() {
   }, []);
 
   return (
-    <>
-      <List isLoading={loading}>
-        {state.items?.feed.map((item: any) => (
-          <List.Item
-            key={item.id}
-            icon={{
-              source: item.content.photo_thumbnail_url ? item.content.photo_thumbnail_url : "",
-              mask: Image.Mask.Circle,
-            }}
-            title={item.content.title}
-            actions={<Actions item={item} />}
-            accessories={[
-              {
-                text: item.content.article?.word_count ? item.content.article?.word_count.toString() + " words" : "",
-              },
-            ]}
-          />
-        ))}
-      </List>
-    </>
+    <List isLoading={loading}>
+      {state.items?.feed.map((item: any) => (
+        <List.Item
+          key={item.id}
+          icon={{
+            source: item.content.photo_thumbnail_url ? item.content.photo_thumbnail_url : "",
+            mask: Image.Mask.Circle,
+          }}
+          title={item.content.title}
+          actions={<Actions item={item} />}
+          accessories={[
+            {
+              text: item.content.article?.word_count ? item.content.article?.word_count.toString() + " words" : "",
+            },
+          ]}
+        />
+      ))}
+    </List>
   );
 }
