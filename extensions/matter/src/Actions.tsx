8f1bea9a32c5a9514ff41d62c01955047db914c4
Per Nielsen Tikær <per@raycast.com>
2023-01-05 13:21:03 +0100

    Update Actions.tsx

--- extensions/matter/src/Actions.tsx
+++ extensions/matter/src/Actions.tsx
@@ -3,18 +3,14 @@ import { ActionPanel, Action } from "@raycast/api";
 export function Actions(props: any) {
   return (
     <ActionPanel title={props.item.content.title}>
-      <ActionPanel.Section>
-        {props.item.content.url && <Action.OpenInBrowser url={props.item.content.url} />}
-      </ActionPanel.Section>
-      <ActionPanel.Section>
-        {props.item.content.url && (
-          <Action.CopyToClipboard
-            content={props.item.content.url}
-            title="Copy Link"
-            shortcut={{ modifiers: ["cmd"], key: "." }}
-          />
-        )}
-      </ActionPanel.Section>
+      {props.item.content.url && <Action.OpenInBrowser url={props.item.content.url} />}
+      {props.item.content.url && (
+        <Action.CopyToClipboard
+          content={props.item.content.url}
+          title="Copy Link"
+          shortcut={{ modifiers: ["cmd"], key: "." }}
+        />
+      )}
     </ActionPanel>
   );
 }
