20b5c781ce5de0344177824185befc646f8f2b2c
Dennis Eilander <eilander.dennis@gmail.com>
2023-01-11 22:16:44 +0100

    Fix codestyle

--- extensions/laravel-valet/src/components/Site.tsx
+++ extensions/laravel-valet/src/components/Site.tsx
@@ -1,4 +1,4 @@
-import {Action, ActionPanel, Color, Icon, List} from "@raycast/api";
+import { Action, ActionPanel, Color, Icon, List } from "@raycast/api";
 import { site } from "../utils";
 
 interface SiteProps {
@@ -8,14 +8,14 @@ interface SiteProps {
 export function Site({ site: site }: SiteProps): JSX.Element {
   return (
     <List.Item
-      title={{value: site.url.replace(site.secured ? 'https://' : 'http://', ''), tooltip: site.url}}
+      title={{ value: site.url.replace(site.secured ? "https://" : "http://", ""), tooltip: site.url }}
       subtitle={site.prettyPath}
       icon={Icon.Folder}
       accessories={[
         {
-            icon: site.secured ? Icon.Lock : '',
-            tag: site.secured ? { color: Color.Green, value: "secured" } : '',
-            tooltip: site.secured ? "Site is secured using the `valet secure` command" : ""
+          icon: site.secured ? Icon.Lock : "",
+          tag: site.secured ? { color: Color.Green, value: "secured" } : "",
+          tooltip: site.secured ? "Site is secured using the `valet secure` command" : "",
         },
       ]}
       actions={
@@ -23,7 +23,11 @@ export function Site({ site: site }: SiteProps): JSX.Element {
           <Action.OpenInBrowser url={site.url} icon={Icon.Globe} />
           <Action.ShowInFinder path={site.path} />
           <Action.OpenWith path={site.path} shortcut={{ modifiers: ["cmd"], key: "o" }} />
-          <Action.CopyToClipboard title="Copy site Path" content={site.path} shortcut={{ modifiers: ["cmd"], key: "p" }} />
+          <Action.CopyToClipboard
+            title="Copy site Path"
+            content={site.path}
+            shortcut={{ modifiers: ["cmd"], key: "p" }}
+          />
         </ActionPanel>
       }
     />
