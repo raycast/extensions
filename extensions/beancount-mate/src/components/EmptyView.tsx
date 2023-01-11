0309d60790fa79b757cd303b6841e566e17b072d
iwfan <i.wangfancn@gmail.com>
2023-01-11 22:44:29 +0800

    [beancount-meta] Chore: remove useless comment

--- extensions/beancount-mate/src/components/EmptyView.tsx
+++ extensions/beancount-mate/src/components/EmptyView.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { List } from "@raycast/api";
 
 const EmptyView: React.FC = () => {
-  // https://cdn-icons-png.flaticon.com/512/4160/4160724.png
   return (
     <List.EmptyView
       icon={"https://cdn-icons-png.flaticon.com/512/4160/4160766.png"}
