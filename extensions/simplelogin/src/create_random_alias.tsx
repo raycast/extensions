313dd239f87e3cc3d12688cf50008c4143e11b7d
Adrian <322735+adrian-schnell@users.noreply.github.com>
2023-01-12 08:51:51 +0100

    add loading animation for random alias command

--- extensions/simplelogin/src/create_random_alias.tsx
+++ extensions/simplelogin/src/create_random_alias.tsx
@@ -1,7 +1,11 @@
 import { createRandomAlias } from "./api/simplelogin_api";
-import { showHUD, Clipboard } from "@raycast/api";
+import { showHUD, Clipboard, showToast, Toast, popToRoot } from "@raycast/api";
 
 export default async function Command() {
+  await showToast({
+    style: Toast.Style.Animated,
+    title: "Generating random alias...",
+  });
   const alias = await createRandomAlias();
   if (alias == null) {
     showHUD("Failed to create random alias");
