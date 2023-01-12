3d73b309584b946d42ec598b142a94df8f8c6e09
Adrian <322735+adrian-schnell@users.noreply.github.com>
2023-01-12 11:32:52 +0100

    add sections for the list

--- extensions/simplelogin/src/list_alias.tsx
+++ extensions/simplelogin/src/list_alias.tsx
@@ -1,6 +1,6 @@
 import { loadAllAliases, updateAliasPinnedStatus, deleteAlias, toggleAliasState } from "./api/simplelogin_api";
 import { useEffect, useState } from "react";
-import { Action, ActionPanel, Icon, List, confirmAlert, showToast, Toast, Alert, Color } from "@raycast/api";
+import { Action, ActionPanel, Icon, List, confirmAlert, showToast, Toast, Alert } from "@raycast/api";
 import { AliasResponse } from "./models/alias";
 import moment from "moment";
 
@@ -22,7 +22,7 @@ export default function Command() {
     }, 1000);
   }, [aliases]);
 
-  function onDrinkTypeChange(newValue: string) {
+  function filterDropdownList(newValue: string) {
     if (newValue == "all") {
       setFilteredAlias(aliases);
     } else if (newValue == "pinned") {
@@ -79,6 +79,86 @@ export default function Command() {
     });
   }
 
+  function aliasListItem(alias: AliasResponse) {
+    return (
+      <List.Item
+        key={alias.id}
+        title={alias.email}
+        icon={{ source: alias.pinned ? Icon.Pin : Icon.PinDisabled }}
+        detail={
+          <List.Item.Detail
+            metadata={
+              <List.Item.Detail.Metadata>
+                <List.Item.Detail.Metadata.Label title="Name" text={alias.name ?? "n/a"} />
+                <List.Item.Detail.Metadata.Label
+                  title="Note"
+                  text={alias.note != null && alias.note?.length > 0 ? alias.note : "---"}
+                />
+                <List.Item.Detail.Metadata.Separator />
+                <List.Item.Detail.Metadata.Label title="Metadata" />
+                <List.Item.Detail.Metadata.Label
+                  title="Pinned"
+                  text={alias.pinned ? "Yes" : "No"}
+                  icon={alias.pinned ? Icon.Pin : Icon.PinDisabled}
+                />
+                <List.Item.Detail.Metadata.Label
+                  title="Enabled"
+                  text={alias.enabled ? "Yes" : "No"}
+                  icon={alias.enabled ? Icon.Checkmark : Icon.XMarkCircle}
+                />
+                <List.Item.Detail.Metadata.Label
+                  title="Created"
+                  text={moment(alias.creation_date).format("HH:mm DD.MM.YY")}
+                  icon={Icon.Calendar}
+                />
+                <List.Item.Detail.Metadata.Label
+                  title="Mails forwarded"
+                  text={"" + alias.nb_forward}
+                  icon={Icon.ArrowClockwise}
+                />
+                <List.Item.Detail.Metadata.Label title="Mails replied" text={"" + alias.nb_reply} icon={Icon.Forward} />
+                <List.Item.Detail.Metadata.Label title="Mails blocked" text={"" + alias.nb_block} icon={Icon.Eraser} />
+                <List.Item.Detail.Metadata.Separator />
+                <List.Item.Detail.Metadata.Label title="Contained in mailbox" />
+                <>
+                  {alias.mailboxes.length > 0 &&
+                    alias.mailboxes.map((mailbox) => (
+                      <List.Item.Detail.Metadata.Label title="E-mail" text={mailbox.email} key={mailbox.id} />
+                    ))}
+                </>
+              </List.Item.Detail.Metadata>
+            }
+          />
+        }
+        actions={
+          <ActionPanel>
+            <Action.CopyToClipboard content={alias.email} shortcut={{ modifiers: ["cmd"], key: "." }} />
+            <>
+              {alias.pinned ? (
+                <Action title="Unpin Alias" onAction={() => updatePinnedStatus(alias, false)} icon={Icon.PinDisabled} />
+              ) : (
+                <Action title="Pin Alias" onAction={() => updatePinnedStatus(alias, true)} icon={Icon.Pin} />
+              )}
+            </>
+            <Action
+              title={alias.enabled ? "Disable Alias" : "Enable Alias"}
+              onAction={() => toggleAliasStatePrompt(alias, !alias.enabled)}
+              icon={!alias.enabled ? Icon.Eye : Icon.EyeDisabled}
+              shortcut={{ modifiers: ["cmd"], key: "t" }}
+            />
+            <Action
+              title="Delete Alias"
+              style={Action.Style.Destructive}
+              onAction={() => deleteAliasPrompt(alias)}
+              icon={Icon.DeleteDocument}
+              shortcut={{ modifiers: ["cmd"], key: "delete" }}
+            />
+          </ActionPanel>
+        }
+      />
+    );
+  }
+
   return (
     <List
       isLoading={isLoading}
@@ -90,7 +170,7 @@ export default function Command() {
             <List.Dropdown
               tooltip="Filter Aliases"
               onChange={(newValue) => {
-                onDrinkTypeChange(newValue);
+                filterDropdownList(newValue);
               }}
             >
               <List.Dropdown.Item title="Show All" value="all" key="all" icon={Icon.Globe} />
@@ -103,101 +183,28 @@ export default function Command() {
         </>
       }
     >
-      <>
-        {filteredAlias != undefined &&
-          filteredAlias.length > 0 &&
-          filteredAlias.map((alias) => {
-            return (
-              <List.Item
-                key={alias.id}
-                title={alias.email}
-                icon={{ source: Icon.Minus, tintColor: alias.pinned ? Color.Yellow : Color.PrimaryText }}
-                detail={
-                  <List.Item.Detail
-                    metadata={
-                      <List.Item.Detail.Metadata>
-                        <List.Item.Detail.Metadata.Label title="Name" text={alias.name ?? "n/a"} />
-                        <List.Item.Detail.Metadata.Label
-                          title="Note"
-                          text={alias.note != null && alias.note?.length > 0 ? alias.note : "---"}
-                        />
-                        <List.Item.Detail.Metadata.Separator />
-                        <List.Item.Detail.Metadata.Label title="Metadata" />
-                        <List.Item.Detail.Metadata.Label
-                          title="Pinned"
-                          text={alias.pinned ? "Yes" : "No"}
-                          icon={alias.pinned ? Icon.Pin : Icon.PinDisabled}
-                        />
-                        <List.Item.Detail.Metadata.Label
-                          title="Enabled"
-                          text={alias.enabled ? "Yes" : "No"}
-                          icon={alias.enabled ? Icon.Checkmark : Icon.XMarkCircle}
-                        />
-                        <List.Item.Detail.Metadata.Label
-                          title="Created"
-                          text={moment(alias.creation_date).format("HH:mm DD.MM.YY")}
-                          icon={Icon.Calendar}
-                        />
-                        <List.Item.Detail.Metadata.Label
-                          title="Mails forwarded"
-                          text={"" + alias.nb_forward}
-                          icon={Icon.ArrowClockwise}
-                        />
-                        <List.Item.Detail.Metadata.Label
-                          title="Mails replied"
-                          text={"" + alias.nb_reply}
-                          icon={Icon.Forward}
-                        />
-                        <List.Item.Detail.Metadata.Label
-                          title="Mails blocked"
-                          text={"" + alias.nb_block}
-                          icon={Icon.Eraser}
-                        />
-                        <List.Item.Detail.Metadata.Separator />
-                        <List.Item.Detail.Metadata.Label title="Contained in mailbox" />
-                        <>
-                          {alias.mailboxes.length > 0 &&
-                            alias.mailboxes.map((mailbox) => (
-                              <List.Item.Detail.Metadata.Label title="E-mail" text={mailbox.email} key={mailbox.id} />
-                            ))}
-                        </>
-                      </List.Item.Detail.Metadata>
-                    }
-                  />
-                }
-                actions={
-                  <ActionPanel>
-                    <Action.CopyToClipboard content={alias.email} shortcut={{ modifiers: ["cmd"], key: "." }} />
-                    <>
-                      {alias.pinned ? (
-                        <Action
-                          title="Unpin Alias"
-                          onAction={() => updatePinnedStatus(alias, false)}
-                          icon={Icon.PinDisabled}
-                        />
-                      ) : (
-                        <Action title="Pin Alias" onAction={() => updatePinnedStatus(alias, true)} icon={Icon.Pin} />
-                      )}
-                    </>
-                    <Action
-                      title={alias.enabled ? "Disable Alias" : "Enable Alias"}
-                      onAction={() => toggleAliasStatePrompt(alias, !alias.enabled)}
-                      icon={!alias.enabled ? Icon.Eye : Icon.EyeDisabled}
-                      shortcut={{ modifiers: ["cmd"], key: "t" }}
-                    />
-                    <Action
-                      title="Delete Alias"
-                      style={Action.Style.Destructive}
-                      onAction={() => deleteAliasPrompt(alias)}
-                      icon={Icon.DeleteDocument}
-                      shortcut={{ modifiers: ["cmd"], key: "delete" }}
-                    />
-                  </ActionPanel>
-                }
-              />
-            );
-          })}
-      </>
+      <List.Section title="Pinned Aliases">
+        <>
+          {filteredAlias != undefined &&
+            filteredAlias.length > 0 &&
+            filteredAlias
+              .filter((alias) => alias.pinned)
+              .map((alias) => {
+                return aliasListItem(alias);
+              })}
+        </>
+      </List.Section>
+      <List.Section title="Not Pinned Aliases">
+        <>
+          {filteredAlias != undefined &&
+            filteredAlias.length > 0 &&
+            filteredAlias
+              .filter((alias) => !alias.pinned)
+              .map((alias) => {
+                return aliasListItem(alias);
+              })}
+        </>
+      </List.Section>
       <List.EmptyView icon={{ source: "simplelogin_icon.png" }} title="No alias found" />
     </List>
   );
