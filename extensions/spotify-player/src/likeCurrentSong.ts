7b9518090e35d1eff73441d325dcb5a5717d99d0
Sorin Muntean <me@sorinmuntean.ro>
2023-01-13 17:32:06 +0100

    Spotify Player: Allow no-view commands (`Like Current Song`, `Dislike Current Song`, `Just Play` and `Star Radio`) to initialize authorization.

--- extensions/spotify-player/src/likeCurrentSong.ts
+++ extensions/spotify-player/src/likeCurrentSong.ts
@@ -1,26 +1,20 @@
 import { showHUD, showToast, Toast } from "@raycast/api";
 import { likeCurrentlyPlayingTrack } from "./spotify/client";
-import { isAuthorized } from "./spotify/oauth";
 
-export default async () => {
-  const authorized = await isAuthorized();
-  if (!authorized) {
-    showHUD("⚠️ Please open any view-based command and authorize to perform the command.");
-    return;
-  }
+export default async function Command() {
   const toast = await showToast({ style: Toast.Style.Animated, title: "Liking Song" });
   try {
     const response = await likeCurrentlyPlayingTrack();
     if (response?.result) {
-      const title = `${response.result.artist} – ${response.result.name}`;
-      showHUD(`💚 ${title}`);
+      const title = `${response.result.artist} - ${response.result.name}`;
+      await showHUD(`💚 ${title}`);
     } else if (response?.error) {
       toast.style = Toast.Style.Failure;
       toast.title = response.error;
     }
   } catch (err) {
     toast.style = Toast.Style.Failure;
-    toast.title = "Failed to like the song";
+    toast.title = "Failed to like song";
     toast.message = (err as unknown as Error).message;
   }
-};
+}
