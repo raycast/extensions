28e7481e9044faa2d59b24c587be5ea902bfed34
Greg <gskril@gmail.com>
2023-01-16 23:08:52 +0200

    Render markdown links

--- extensions/searchcaster/src/profiles.tsx
+++ extensions/searchcaster/src/profiles.tsx
@@ -2,6 +2,8 @@ import { Action, ActionPanel, Image, List } from "@raycast/api";
 import { Profile, ProfileActionsProps } from "./types";
 import { useProfileSearch, useFarcasterInstalled, truncateAddress } from "./utils";
 import { useState } from "react";
+import Linkify from "linkify-it";
+import tlds from "tlds";
 
 export default function Command() {
   const [searchText, setSearchText] = useState<string>();
@@ -54,19 +56,31 @@ function Actions({ profile, farcasterInstalled }: ProfileActionsProps) {
 }
 
 function ProfileDetails({ profile }: { profile: Profile }) {
+  const linkify = Linkify();
+  linkify.tlds(tlds);
+
+  // Find all links in bio and replace them with markdown links
+  const bio = profile.body.bio;
+  const markdown = linkify.test(bio)
+    ? linkify.match(bio)?.reduce((acc, match) => {
+        return acc.replace(match.raw, `[${match.raw}](${match.url})`);
+      }, bio)
+    : bio;
+
   return (
     <List.Item.Detail
-      markdown={profile.body.bio}
+      markdown={markdown}
       metadata={
         <List.Item.Detail.Metadata>
           <List.Item.Detail.Metadata.Label
             title="Name"
             text={profile.body.displayName}
-            icon={{
-              source: profile.body.avatarUrl,
-              mask: Image.Mask.RoundedRectangle,
-              fallback: "https://explorer.farcaster.xyz/avatar.png",
-            }}
+            icon={
+              profile.body.avatarUrl && {
+                source: profile.body.avatarUrl,
+                mask: Image.Mask.RoundedRectangle,
+              }
+            }
           />
           <List.Item.Detail.Metadata.Label title="Username" text={profile.body.username} />
           {profile.body.registeredAt && (
