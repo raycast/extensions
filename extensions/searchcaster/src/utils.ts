60bd9a0e0e773a0da1ad953af7037863e168918c
Greg <gskril@gmail.com>
2023-01-16 21:26:50 +0200

    Add filter by username when searching casts

--- extensions/searchcaster/src/utils.ts
+++ extensions/searchcaster/src/utils.ts
@@ -4,9 +4,16 @@ import { useFetch } from "@raycast/utils";
 import { useEffect, useState } from "react";
 
 export function useCastSearch(query: string) {
-  const { data, isLoading } = useFetch<Response>(
-    "https://searchcaster.xyz/api/search?" + new URLSearchParams({ text: query, count: "100" })
-  );
+  const searchParams = new URLSearchParams({ text: query, count: "100" });
+
+  // If the query contains from:${username} then we should filter casts by that username
+  if (query.includes("from:")) {
+    const username = query.split("from:")[1].trim();
+    searchParams.append("username", username);
+    searchParams.set("text", query.replace(`from:${username}`, "").trim());
+  }
+
+  const { data, isLoading } = useFetch<Response>("https://searchcaster.xyz/api/search?" + searchParams);
 
   return { data, isLoading };
 }
