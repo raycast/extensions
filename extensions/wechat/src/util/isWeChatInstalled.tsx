5c120ad9aa4b177c783f401f6ad661ae58b202c8
Lee <44310445+lx200916@users.noreply.github.com>
2023-01-19 04:38:22 +0800

    Fix isWeChatTweakInstalled (#4302)

--- extensions/wechat/src/util/isWeChatInstalled.tsx
+++ extensions/wechat/src/util/isWeChatInstalled.tsx
@@ -2,7 +2,9 @@ import * as fs from "fs"
 
 export const isWeChatInstalled = (): boolean => {
   try {
-    return fs.existsSync("/Applications/WeChat.app" || "/Applications/微信.app")
+    return ["/Applications/WeChat.app", "/Applications/微信.app"].some(
+      fs.existsSync
+    )
   } catch (e) {
     console.error(String(e))
     return false
