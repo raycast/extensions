23b9d8e249b6149571cf4180465b65606c3f858c
khasbilegt <khasbilegt.ts@gmail.com>
2023-01-19 08:37:24 +0800

    make guide nicer

--- extensions/1password/src/v8/components/Guide.tsx
+++ extensions/1password/src/v8/components/Guide.tsx
@@ -1,18 +1,23 @@
-import { Action, ActionPanel, Detail, Icon, openExtensionPreferences } from "@raycast/api";
+import { Action, ActionPanel, Detail, Icon, environment, openExtensionPreferences } from "@raycast/api";
+import { join } from "path";
+
+const binary = join(environment.assetsPath, "binary-instruction.png");
+const settings = join(environment.assetsPath, "1password-settings.png");
 
 const INSTRUCTION = `
-## ✅ 1Password CLI 2
+# You will need the following to run the extension
 
-### You can install it on Mac using brew or manually download and extract it. If you installed it manually you have to specify the installation path in the extension preference section.
+## ✅ 1Password CLI
 
-1. [Download 1Password CLI for your platform and architecture](https://app-updates.agilebits.com/product_history/CLI2). You can [verify its authenticity](https://developer.1password.com/docs/cli/verify).
-2. Install 1Password CLI in the default location: \`/usr/local/bin\`.
-3. To verify the installation, check the version number: \`op --version\`
+### This extension utilizes the 1Password CLI tool and, it will be installed automatically when you run your first command. But in case there's an issue, check the following.
+![Terminal Instruction](${binary})
 
 ## ✅ Enable Command-Line Interface (CLI)
 
 ### To turn on the app integration and set up 1Password to authenticate with biometrics:
-1. Open 1Password.
+![1Password Settings](${settings})
+
+1. Open [1Password](onepassword://settings).
 2. Select the account or collection at the top of the sidebar and choose Preferences > Security.
 3. Check [Touch ID](https://support.1password.com/touch-id-mac/).
 4. Select Developer in the sidebar.
@@ -27,8 +32,8 @@ export function Guide() {
       markdown={INSTRUCTION}
       actions={
         <ActionPanel>
-          <Action icon={Icon.Gear} title="Open Extension Preferences" onAction={openExtensionPreferences} />
           <Action.Open title="Open 1Password Settings" target="onepassword://settings" />
+          <Action icon={Icon.Gear} title="Open Extension Preferences" onAction={openExtensionPreferences} />
         </ActionPanel>
       }
     />
