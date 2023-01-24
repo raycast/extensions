8f394f42c36a1f37d551d83f15b28848ec954f6d
Martin Piala <pialamar@gmail.com>
2023-01-24 10:26:25 +0000

    add the option to quit the app

--- extensions/quit-applications/src/index.tsx
+++ extensions/quit-applications/src/index.tsx
@@ -13,6 +13,50 @@ function quitApp(app: string) {
   return runAppleScript(`tell application "${app}" to quit`);
 }
 
+function restartApp(app: string) {
+  return runAppleScript(`tell application "${app}"
+                            repeat while its running
+                              quit
+                              delay 0.5
+	                          end repeat
+	                          activate
+                        end tell`);
+}
+
+function quitAppWithToast(app: string): boolean {
+  try {
+    quitApp(app);
+    showToast({
+      style: Toast.Style.Success,
+      title: `Quit ${app}`,
+    });
+    return true;
+  } catch {
+    showToast({
+      style: Toast.Style.Failure,
+      title: `Unable to quit ${app}`,
+    });
+    return false;
+  }
+}
+
+function restartAppWithToast(app: string): boolean {
+  try {
+    restartApp(app);
+    showToast({
+      style: Toast.Style.Success,
+      title: `Restarted ${app}`,
+    });
+    return true;
+  } catch {
+    showToast({
+      style: Toast.Style.Failure,
+      title: `Unable to restart ${app}`,
+    });
+    return false;
+  }
+}
+
 interface AppListState {
   apps: string[];
   isLoading: boolean;
@@ -46,20 +90,15 @@ class AppList extends React.Component<Record<string, never>, AppListState> {
                 <Action
                   title="Quit"
                   onAction={() => {
-                    try {
-                      quitApp(app);
-                      showToast({
-                        style: Toast.Style.Success,
-                        title: `Quit ${app}`,
-                      });
+                    const success = quitAppWithToast(app);
+                    if (success) {
                       this.setState({ apps: this.state.apps.filter((a) => a !== app) });
-                    } catch {
-                      showToast({
-                        style: Toast.Style.Failure,
-                        title: `Unable to quit ${app}`,
-                      });
                     }
                   }}
+                />
+                <Action
+                  title="Restart"
+                  onAction={() => { restartAppWithToast(app) }}
                 />
               </ActionPanel>
             }
