45d158eb77f8d33643c39e022a02fa1274170aa6
mathieudutour <mathieu@dutour.me>
2023-01-25 14:14:22 +0100

    Issue bot: Add more logs

--- scripts/issue-bot.js
+++ scripts/issue-bot.js
@@ -81,7 +81,7 @@ module.exports = async ({ github, context, core }) => {
           state: "closed",
         });
       } else {
-        console.log("didn't find the right comment");
+        console.log(`didn't find the right comment in ${context.payload.comment.body}`);
       }
     } else {
       console.log(`${context.payload.comment.user.login} is not an owner`);
