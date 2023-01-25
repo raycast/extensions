62c3649f0ef9df3dc627d18d2b2094066323f5d2
Per Nielsen Tikær <per@raycast.com>
2023-01-25 12:33:36 +0100

    Fixes and tweaks

--- extensions/internet-radio/src/menubar-radio.tsx
+++ extensions/internet-radio/src/menubar-radio.tsx
@@ -1,11 +1,11 @@
 import { Icon, Image, launchCommand, LaunchType, LocalStorage, MenuBarExtra } from "@raycast/api";
-import { getFavicon } from "@raycast/utils";
+import { getFavicon, useCachedState } from "@raycast/utils";
 import { useEffect, useState } from "react";
 import { getAllStations, loadDefaults, playStation } from "./utils";
 
 export default function Command() {
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
-  const [currentStationName, setCurrentStationName] = useState<string>("");
+  const [currentStationName, setCurrentStationName] = useCachedState<string>("radio-title");
   const [stations, setStations] = useState<{ [stationName: string]: { [key: string]: string | string[] } }>();
 
   // Load default stations, if necessary
@@ -35,6 +35,7 @@ export default function Command() {
         key={stationName}
         icon={websiteIcon}
         title={stationName}
+        subtitle={stationName == currentStationName ? "Currently Playing" : ""}
         onAction={async () => {
           await playStation(stationName, stationData.stream as unknown as string);
           setIsPlaying(true);
@@ -51,35 +52,36 @@ export default function Command() {
       icon={isPlaying ? Icon.Livestream : Icon.LivestreamDisabled}
       tooltip={`Currently playing station: ${currentStationName}`}
     >
-      <MenuBarExtra.Item title="Controls" />
+      <MenuBarExtra.Section title="Controls">
+        {isPlaying ? (
+          <MenuBarExtra.Item
+            icon={Icon.Stop}
+            title="Stop Playback"
+            onAction={async () => await launchCommand({ name: "stop-playback", type: LaunchType.Background })}
+            shortcut={{ modifiers: ["cmd"], key: "s" }}
+            tooltip="Stop playback of the currently playing station"
+          />
+        ) : null}
 
-      {isPlaying ? (
         <MenuBarExtra.Item
-          icon={Icon.Stop}
-          title="Stop Playback"
-          onAction={async () => await launchCommand({ name: "stop-playback", type: LaunchType.Background })}
-          shortcut={{ modifiers: ["cmd"], key: "s" }}
-          tooltip="Stop playback of the currently playing station"
+          icon={Icon.ArrowCounterClockwise}
+          title="Play Last Station"
+          onAction={async () => await launchCommand({ name: "play-last-station", type: LaunchType.Background })}
+          shortcut={{ modifiers: ["cmd"], key: "l" }}
+          tooltip="Resume playback of the last stopped station"
         />
-      ) : null}
 
-      <MenuBarExtra.Item
-        icon={Icon.ArrowCounterClockwise}
-        title="Play Last Station"
-        onAction={async () => await launchCommand({ name: "play-last-station", type: LaunchType.Background })}
-        shortcut={{ modifiers: ["cmd"], key: "l" }}
-        tooltip="Resume playback of the last stopped station"
-      />
-
-      <MenuBarExtra.Item
-        icon={Icon.Shuffle}
-        title="Play Random Station"
-        onAction={async () => await launchCommand({ name: "play-random-station", type: LaunchType.Background })}
-        shortcut={{ modifiers: ["cmd"], key: "r" }}
-        tooltip="Start playing a randomly selected station"
-      />
-
-      <MenuBarExtra.Submenu title="Select Station">{menuItems}</MenuBarExtra.Submenu>
+        <MenuBarExtra.Item
+          icon={Icon.Shuffle}
+          title="Play Random Station"
+          onAction={async () => await launchCommand({ name: "play-random-station", type: LaunchType.Background })}
+          shortcut={{ modifiers: ["cmd"], key: "r" }}
+          tooltip="Start playing a randomly selected station"
+        />
+      </MenuBarExtra.Section>
+      <MenuBarExtra.Section title="Stations">
+        <MenuBarExtra.Submenu title="Select Station">{menuItems}</MenuBarExtra.Submenu>
+      </MenuBarExtra.Section>
     </MenuBarExtra>
   );
 }
