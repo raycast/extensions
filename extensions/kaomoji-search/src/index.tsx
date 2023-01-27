fe7cb7191b7be08c14205795f856c8dc362f3b76
Yukai Huang <yukaihuangtw@gmail.com>
2023-01-27 12:52:38 +0800

    feat: grid support

--- extensions/kaomoji-search/src/index.tsx
+++ extensions/kaomoji-search/src/index.tsx
@@ -1,4 +1,4 @@
-import { ActionPanel, List, showToast, Action, Toast } from "@raycast/api";
+import { ActionPanel, List, showToast, Action, Toast, getPreferenceValues, Grid } from "@raycast/api";
 import { useState, useEffect, useRef } from "react";
 import { AbortError } from "node-fetch";
 import { lib } from "asciilib";
@@ -6,16 +6,29 @@ import { nanoid } from "nanoid";
 
 export default function Command() {
   const { state, search } = useSearch();
+  const { displayMode } = getPreferenceValues<{ displayMode: "list" | "grid" }>();
 
-  return (
-    <List isLoading={state.isLoading} onSearchTextChange={search} searchBarPlaceholder="Search by name..." throttle>
-      <List.Section title="Results" subtitle={state.results.length + ""}>
-        {state.results.map((searchResult) => (
-          <SearchListItem key={searchResult.id} searchResult={searchResult} />
-        ))}
-      </List.Section>
-    </List>
-  );
+  if (displayMode === "grid") {
+    return (
+      <Grid isLoading={state.isLoading} onSearchTextChange={search} searchBarPlaceholder="Search by name..." throttle>
+        <Grid.Section title="Results" subtitle={state.results.length + ""}>
+          {state.results.map((searchResult) => (
+            <SearchGridItem key={searchResult.id} searchResult={searchResult} />
+          ))}
+        </Grid.Section>
+      </Grid>
+    );
+  } else {
+    return (
+      <List isLoading={state.isLoading} onSearchTextChange={search} searchBarPlaceholder="Search by name..." throttle>
+        <List.Section title="Results" subtitle={state.results.length + ""}>
+          {state.results.map((searchResult) => (
+            <SearchListItem key={searchResult.id} searchResult={searchResult} />
+          ))}
+        </List.Section>
+      </List>
+    );
+  }
 }
 
 function SearchListItem({ searchResult }: { searchResult: SearchResult }) {
@@ -27,6 +40,46 @@ function SearchListItem({ searchResult }: { searchResult: SearchResult }) {
           text: searchResult.description,
         },
       ]}
+      actions={
+        <ActionPanel>
+          <ActionPanel.Section>
+            <Action.Paste title="Paste in Active App" content={searchResult.name} />
+            <Action.CopyToClipboard content={searchResult.name} />
+          </ActionPanel.Section>
+        </ActionPanel>
+      }
+    />
+  );
+}
+
+function toHTMLEntities(str: string) {
+  return str.replace(/./gm, function (s) {
+    return "&#" + s.charCodeAt(0) + ";";
+  });
+}
+
+function getSvgWithKaomoji(kaomoji: string, dark = false) {
+  return `<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 90 90">
+  <text dominant-baseline="middle" x="45" y="45" text-anchor="middle" fill="${dark ? "#fff" : "#000"}" font-size="8px">
+    ${toHTMLEntities(kaomoji)}
+  </text>
+</svg>`;
+}
+
+function getBase64SvgUrl(kaomoji: string, dark = false) {
+  return "data:image/svg+xml;base64," + Buffer.from(getSvgWithKaomoji(kaomoji, dark)).toString("base64");
+}
+
+function SearchGridItem({ searchResult }: { searchResult: SearchResult }) {
+  return (
+    <Grid.Item
+      content={{
+        source: {
+          dark: getBase64SvgUrl(searchResult.name, true),
+          light: getBase64SvgUrl(searchResult.name, false),
+        },
+      }}
+      title={searchResult.description}
       actions={
         <ActionPanel>
           <ActionPanel.Section>
