dd93be9b48462b159828fdcd5cdca1d119170b5a
Chris Chinchilla <chris@chrischinchilla.com>
2023-01-30 14:21:22 +0100

    Draft of submit

--- extensions/trello/src/utils/fetchBoards.ts
+++ extensions/trello/src/utils/fetchBoards.ts
@@ -10,9 +10,9 @@ export const returnBoards = async () => {
     const response = await fetch(
       `https://api.trello.com/1/members/me/boards?key=${apitoken}&token=${token}&organization=true`
     );
-
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const json: any = await response.json();
+    console.log(json);
     return json as Board[];
   } catch (error) {
     showToast(Toast.Style.Failure, "An error occured", "Could not fetch todos, check your credentials");
