7b2b47e6d695d01dea901aec0406a1878aefa808
iwfan <i.wangfancn@gmail.com>
2023-01-30 13:09:30 +0800

    [BeancountMeta] fix code style check

--- extensions/beancount-mate/src/components/ErrorView.tsx
+++ extensions/beancount-mate/src/components/ErrorView.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 import { List } from "@raycast/api";
 
 const ErrorView: React.FC = () => {
   return (
     <List.EmptyView
-      icon={'https://cdn-icons-png.flaticon.com/512/4160/4160755.png'}
+      icon={"https://cdn-icons-png.flaticon.com/512/4160/4160755.png"}
       title="Your typings is invalid"
       description="If you are not familiar with Costflow, Open the syntax Doc ⬇️ "
     />
-  )
-}
+  );
+};
 
-export default ErrorView
+export default ErrorView;
