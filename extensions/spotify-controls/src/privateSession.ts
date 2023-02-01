c6dd475dd999d75a60552ea74a3c56d76ebc7232
dmacdermott <damianmacdermott@gmail.com>
2023-02-01 10:42:27 +0900

    spotify-controls: fix requested changes

--- extensions/spotify-controls/src/privateSession.ts
+++ extensions/spotify-controls/src/privateSession.ts
@@ -1,13 +1,13 @@
 import { showToast, Toast } from "@raycast/api";
 import { runAppleScript } from "run-applescript";
-import { buildScriptEnsuringSpotifyIsRunning, runAppleScriptSilently } from "./utils";
+import { buildScriptEnsuringSpotifyIsRunning } from "./utils";
 
 export default async () => {
   const script = buildScriptEnsuringSpotifyIsRunning(`
   tell application "System Events"
 	  tell process "Spotify"
-    click menu item "Private Session" of menu 1 of menu bar item "Spotify" of menu bar 1
-    set privateSession to value of attribute "AXMenuItemMarkChar" of menu item "Private Session" of menu 1 of menu bar item "Spotify" of menu bar 1
+    click menu item 5 of menu 1 of menu bar item "Spotify" of menu bar 1
+    set privateSession to value of attribute "AXMenuItemMarkChar" of (menu item 5) of menu 1 of menu bar item "Spotify" of menu bar 1
 	  end tell
   end tell
   return privateSession
@@ -15,12 +15,12 @@ export default async () => {
   try {
     const result = await runAppleScript(script);
     await showToast({
-      title: result === "✓" ? "Private Session Off" : "Private Session On",
+      title: result === "✓" ? "Private session turned off" : "Private session turned on",
     });
   } catch (_) {
     await showToast({
       style: Toast.Style.Failure,
-      title: "Failed toggling shuffle",
+      title: "Failed toggling private session",
     });
   }
 };
