a3a8ccbb9dc6a1c6be43fd80ed2eae72ac51110f
Kory Smith <kor54e@gmail.com>
2023-02-03 15:41:21 -0500

    fixes background refresh and re-adds it

--- extensions/pinboard/src/refreshCache.tsx
+++ extensions/pinboard/src/refreshCache.tsx
@@ -1,9 +1,57 @@
-import { Detail } from "@raycast/api";
-import { usePromise } from "@raycast/utils";
-import { refreshCache } from "./api";
+import { Cache, getPreferenceValues } from "@raycast/api";
+import { LastUpdated, PinboardBookmark, transformBookmark } from "./api";
+import fetch from "node-fetch";
 
-export default function RefreshCache() {
-  const { data } = usePromise(refreshCache, []);
+async function refreshCache() {
+  const apiBasePath = "https://api.pinboard.in/v1";
+  const allPostsEndpoint = `${apiBasePath}/posts/all`;
+  const lastUpdatedEndpoint = `${apiBasePath}/posts/update`;
 
-  return <Detail markdown={data} />;
+  const { apiToken } = getPreferenceValues();
+
+  const params = new URLSearchParams({ auth_token: apiToken, format: "json" });
+
+  const pinboardCache = new Cache({
+    namespace: "pinboard",
+  });
+
+  const isFirstInit = pinboardCache.isEmpty;
+  console.debug({ isFirstInit });
+
+  // Get the last updated time from the server and from the cache. We always want these values.
+  const cachedLastUpdated = pinboardCache.get("lastUpdated");
+  const serverLastUpdated = (await fetch(`${lastUpdatedEndpoint}?${params.toString()}`).then((res) => {
+    if (!res.ok) {
+      return { update_time: "na" };
+    } else {
+      return res.json();
+    }
+  })) as LastUpdated;
+
+  const shouldRefresh = cachedLastUpdated !== serverLastUpdated?.update_time;
+  console.debug({ shouldRefresh, cachedLastUpdated, serverLastUpdated });
+
+  const serverBookmarks = (await fetch(`${allPostsEndpoint}?${params.toString()}`).then((res) => {
+    if (!res.ok) {
+      return Promise.reject(res.statusText);
+    } else {
+      return res.json();
+    }
+  })) as PinboardBookmark[];
+
+  const transformedServerBookmarks = serverBookmarks.map((post) => transformBookmark(post));
+
+  if (shouldRefresh && serverBookmarks && serverLastUpdated) {
+    console.debug("Refreshing cache...");
+    pinboardCache.set("lastUpdated", serverLastUpdated.update_time);
+    console.debug("Updated lastUpdated cache");
+    pinboardCache.set("bookmarks", JSON.stringify(transformedServerBookmarks));
+    console.debug("Updated bookmarks cache");
+    return `Successfully updated cache! There are now ${Object.keys(serverBookmarks).length} items`;
+  }
+  return "There was no need for an update, so I didn't update";
+}
+
+export default async function Command() {
+  await refreshCache();
 }
