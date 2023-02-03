754a3aa14979027796de8b160f1747f5eb31201d
Kory Smith <kor54e@gmail.com>
2023-02-03 14:31:21 -0500

    removes background refresh because it wasn't working

--- extensions/pinboard/src/refreshCache.tsx
+++
@@ -1,9 +0,0 @@
-import { Detail } from "@raycast/api";
-import { usePromise } from "@raycast/utils";
-import { refreshCache } from "./api";
-
-export default function RefreshCache() {
-  const { data } = usePromise(refreshCache, []);
-
-  return <Detail markdown={data} />;
-}
