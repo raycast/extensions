e627a13f21f4e41b423a671ddcdcc9064cece7c3
Devah Schaefers <73009338+devahschaefers@users.noreply.github.com>
2023-02-03 07:52:27 -0800

    [Fantastical] Fixed bug for getting the name of an app & added two new commands which allow you to quickly add events without opening the app (#4567)
    * Added quick add commands and fixed bugs in searching for the app
    
    * changed file names for consistency
    
    * fixed bug for getting name of app
    
    * fixed linting issues

--- extensions/fantastical/src/fan-add-reminder-editable.tsx
+++ extensions/fantastical/src/fan-add-reminder-editable.tsx
@@ -1,6 +1,6 @@
-import { showToast, Toast, open } from "@raycast/api";
+import { showToast, Toast } from "@raycast/api";
 import { runAppleScript } from "run-applescript";
-import { isInstalled } from "./isInstalled";
+import { getName } from "./getName";
 
 interface Arguments {
   add: string;
@@ -8,24 +8,17 @@ interface Arguments {
 
 export default async (props: { arguments: Arguments }) => {
   const args = props.arguments;
-  if (await isInstalled()) {
-    const text = `tell application "Fantastical" 
+  const name = await getName();
+  if (name !== undefined) {
+    const text = `tell application "${name}" 
                         \n parse sentence "Todo ${args.add}" 
                     \n end tell`;
     await runAppleScript(text);
   } else {
-    const options: Toast.Options = {
-      style: Toast.Style.Failure,
+    await showToast({
       title: "Fantastical is not installed",
+      style: Toast.Style.Failure,
       message: "Please first install Fantastical to use this extension.",
-      primaryAction: {
-        title: "Install Fantastical",
-        onAction: () => {
-          open("https://fantastical.app/");
-        },
-      },
-    };
-
-    showToast(options);
+    });
   }
 };
