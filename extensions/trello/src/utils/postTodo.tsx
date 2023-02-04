33eec1fb4ef02c1ce2d43bb78a0536bda4b13a1e
Brittany Joiner <brittanyjoiner15@gmail.com>
2023-02-03 21:59:42 -0600

    add pop to root to go back on form

--- extensions/trello/src/utils/postTodo.tsx
+++ extensions/trello/src/utils/postTodo.tsx
@@ -1,5 +1,5 @@
 import fetch from "node-fetch";
-import { getPreferenceValues, showToast, Toast } from "@raycast/api";
+import { getPreferenceValues, popToRoot, showToast, Toast } from "@raycast/api";
 import { Preferences } from "@raycast/api/types/core/preferences";
 
 export const postTodo = async (values) => {
@@ -11,6 +11,7 @@ export const postTodo = async (values) => {
             `https://api.trello.com/1/cards?key=${apitoken}&token=${token}&name=${values.name}&idList=${values.idList}`, { method: "POST" }
         );
         showToast({ title: "Success", message: "Your to do was created" });
+        popToRoot();
     } catch (error) {
         showToast(Toast.Style.Failure, "An error occured", "Could not fetch todos, check your credentials");
         return Promise.resolve([]);
