81129f8c87a6cdbac52622c5d47afb001d6b95a3
Per Nielsen Tikær <per@raycast.com>
2023-02-08 21:07:55 +0100

    Spiced u the code a bit

--- extensions/chatgpt3-prompt/src/index.tsx
+++ extensions/chatgpt3-prompt/src/index.tsx
@@ -1,32 +1,69 @@
-import { ActionPanel, Detail, List, Action } from "@raycast/api";
+import { ActionPanel, Detail, List, Action, getPreferenceValues } from "@raycast/api";
 import data from "./result.json";
 
+type CommandPreferences = {
+  primaryAction: "copy" | "paste";
+};
+
 export default function Command() {
+  const preferences: CommandPreferences = getPreferenceValues();
+
   return (
     <List>
-      {data.map((item, index) => (
-        <List.Item
-          key={item.title + index}
-          icon="list-icon.png"
-          title={item.title}
-          actions={
-            <ActionPanel>
-              <Action.Push
-                title={item.title}
-                target={
-                  <Detail
-                    markdown={item.items
-                      .map((item) => {
-                        return `${item.content}`;
-                      })
-                      .join("\n")}
-                  />
-                }
-              />
-            </ActionPanel>
-          }
-        />
-      ))}
+      {data.map((item, index) => {
+        const content = item.items
+          .map((item) => {
+            return `${item.content}`;
+          })
+          .join("\n");
+
+        return (
+          <List.Item
+            key={item.title + index}
+            icon="list-icon.png"
+            title={item.title}
+            actions={
+              <ActionPanel>
+                {preferences.primaryAction === "copy" ? (
+                  <>
+                    <Action.CopyToClipboard title="Copy Prompt" content={content} />
+                    <Action.Paste title="Paste Prompt in Active App" content={content} />
+                  </>
+                ) : (
+                  <>
+                    <Action.Paste title="Paste Prompt in Active App" content={content} />
+                    <Action.CopyToClipboard title="Copy Prompt" content={content} />
+                  </>
+                )}
+                <Action.Push
+                  title="Show Prompt"
+                  shortcut={{ modifiers: ["cmd"], key: "d" }}
+                  target={
+                    <Detail
+                      markdown={content}
+                      actions={
+                        <ActionPanel>
+                          {preferences.primaryAction === "copy" ? (
+                            <>
+                              <Action.CopyToClipboard title="Copy Prompt" content={content} />
+                              <Action.Paste title="Paste Prompt in Active App" content={content} />
+                            </>
+                          ) : (
+                            <>
+                              <Action.Paste title="Paste Prompt in Active App" content={content} />
+                              <Action.CopyToClipboard title="Copy Prompt" content={content} />
+                            </>
+                          )}
+                        </ActionPanel>
+                      }
+                    />
+                  }
+                />
+              </ActionPanel>
+            }
+          />
+        );
+      })}
     </List>
   );
 }
