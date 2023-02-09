0272b567275faadb60f6586135eb71f2022d0362
Ryon Huedepohl <13386411+RyonHuedepohl@users.noreply.github.com>
2023-02-08 20:52:54 -0800

    Added error message if Reflect isn't installed

--- extensions/reflect/src/search.tsx
+++ extensions/reflect/src/search.tsx
@@ -1,7 +1,28 @@
-import { closeMainWindow } from "@raycast/api";
+import { Toast, closeMainWindow, open } from "@raycast/api";
 import { runAppleScript } from "run-applescript";
+import { checkReflect, reflectDownload } from "./util";
 
 export default async () => {
+  const reflectInstalled = await checkReflect();
+  if (!reflectInstalled) {
+    const toast = new Toast({
+      title: "Reflect not found",
+      style: Toast.Style.Failure,
+    });
+
+    toast.show();
+    toast.message = "Press ⌘ + D to download";
+    toast.primaryAction = {
+      title: "Download",
+      shortcut: {
+        modifiers: ["cmd"],
+        key: "d",
+      },
+      onAction: async () => await open(reflectDownload),
+    };
+    return false;
+  }
+
   await closeMainWindow();
   await runAppleScript(`
     tell application "Reflect" to activate
