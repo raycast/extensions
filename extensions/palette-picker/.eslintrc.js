1424362a47a3824af2db4003e265c2a5bc4ee78f
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-02-09 14:39:38 +0100

    readme and changelog

---
+++ extensions/palette-picker/.eslintrc.js
@@ -0,0 +1,42 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  extends: ["eslint:recommended", "prettier"],
+  overrides: [
+    {
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+      files: ["**/*.ts", "**/*.tsx"],
+      parserOptions: {
+        ecmaVersion: "latest",
+        tsconfigRootDir: __dirname,
+        project: [
+          "./tsconfig.json",
+          "./apps/*/tsconfig.json",
+          "./packages/*/tsconfig.json",
+        ],
+      },
+      rules: {
+        "@typescript-eslint/no-unused-vars": [
+          "error",
+          {
+            argsIgnorePattern: "^_",
+            varsIgnorePattern: "^_",
+            caughtErrorsIgnorePattern: "^_",
+          },
+        ],
+      },
+    },
+  ],
+  root: true,
+  reportUnusedDisableDirectives: true,
+  ignorePatterns: [
+    ".eslintrc.js",
+    "**/*.config.js",
+    "**/*.config.cjs",
+    "packages/config/**",
+  ],
+};
+
+module.exports = config;
