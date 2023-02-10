09c2cdcab0f13a67f024da7ed8668ce0bff3325e
maximilianzuern <max.zuern@gmail.com>
2023-02-10 22:12:53 +0100

    fixed issues

--- extensions/Splitwise/src/splitwise.tsx
+++ extensions/Splitwise/src/splitwise.tsx
@@ -1,14 +1,4 @@
-import { 
-  Action, 
-  ActionPanel,
-  Icon, 
-  Image, 
-  List, 
-  Form,
-  showToast,
-  Toast,
-  Color,
-} from "@raycast/api";
+import { Action, ActionPanel, Icon, Image, List, Form, showToast, Toast, Color } from "@raycast/api";
 import { useFetch } from "@raycast/utils";
 import got from "got";
 import { useState } from "react";
@@ -17,59 +7,72 @@ import { personalAccessToken } from "./preferences";
 import { Friend, GetFriends } from "./types";
 
 export default function Command() {
-  const { data, isLoading } = useFetch<GetFriends>(
-    "https://secure.splitwise.com/api/v3.0/get_friends",
-    {
-      headers: { Accept: "application/json", Authorization: `Bearer ${personalAccessToken}` },
-    }
-  );
-  
+  const { data, isLoading } = useFetch<GetFriends>("https://secure.splitwise.com/api/v3.0/get_friends", {
+    headers: { Accept: "application/json", Authorization: `Bearer ${personalAccessToken}` },
+  });
+
   return (
-  <List
-  searchBarPlaceholder="Search your favorite friend" 
-  isLoading={isLoading}
-  >
-    {data?.friends
-    .sort((a, b) => new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime())
-    .map((Friend) => (
-    <List.Item 
-    key={Friend.id}
-    icon={{ source: Friend.picture.small ?? Icon.PersonCircle, mask: Image.Mask.Circle }}
-    title={[Friend.first_name, Friend.last_name].join(" ")}
-    accessories={[
-      // return the amount and currency code if they are present, if not, don't show anything
-      { text: {
-        value: `${Friend.balance[0]?.amount && Friend.balance[0]?.currency_code ? `${Friend.balance[0].amount} ${Friend.balance[0].currency_code}` : ''}`, 
-        color: Number(Friend.balance[0]?.amount) < 0 ? Color.Red : Color.Green}, 
-      },
-      {icon: `${Friend.balance[0]?.amount && Friend.balance[0]?.currency_code ? Icon.Coins: ''}`},
-      { date: new Date(Friend.updated_at), tooltip: `Last interaction: ${new Date(Friend.updated_at).toLocaleString()}` } 
-    ]}
-    actions={
-      <ActionPanel>
-        <Action.Push icon={Icon.Wallet} title="Add Expense" target={<FillForm Friend={Friend} />} />
-      </ActionPanel>
-      }
-    />
-    ))}
+    <List searchBarPlaceholder="Search your favorite friend" isLoading={isLoading}>
+      {data?.friends
+        .sort((a, b) => new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime())
+        .map((Friend) => (
+          <List.Item
+            key={Friend.id}
+            icon={{ source: Friend.picture.small ?? Icon.PersonCircle, mask: Image.Mask.Circle }}
+            title={[Friend.first_name, Friend.last_name].join(" ")}
+            accessories={[
+              // return the amount and currency code if they are present, if not, don't show anything
+              {
+                text: {
+                  value: `${
+                    Friend.balance[0]?.amount && Friend.balance[0]?.currency_code
+                      ? `${Friend.balance[0].amount} ${Friend.balance[0].currency_code}`
+                      : ""
+                  }`,
+                  color: Number(Friend.balance[0]?.amount) < 0 ? Color.Red : Color.Green,
+                },
+              },
+              { icon: `${Friend.balance[0]?.amount && Friend.balance[0]?.currency_code ? Icon.Coins : ""}` },
+              {
+                date: new Date(Friend.updated_at),
+                tooltip: `Last interaction: ${new Date(Friend.updated_at).toLocaleString()}`,
+              },
+            ]}
+            actions={
+              <ActionPanel>
+                <Action.Push icon={Icon.Wallet} title="Add Expense" target={<FillForm Friend={Friend} />} />
+              </ActionPanel>
+            }
+          />
+        ))}
     </List>
-    );
+  );
 }
 
-function FillForm(props: { Friend: Friend} ) {
+function FillForm(props: { Friend: Friend }) {
   const [input, setDescription] = useState<string>("");
-  
+
   return (
     <Form
-    navigationTitle="Add Expense"
-    actions={
-      <ActionPanel>
-         <ShareSecretAction input={input} Friend={props.Friend}/>
-      </ActionPanel>
-    }
+      navigationTitle="Add Expense"
+      actions={
+        <ActionPanel>
+          <ShareSecretAction input={input} Friend={props.Friend} />
+        </ActionPanel>
+      }
     >
       <Form.Description title="Friend" text={`${props.Friend.first_name} ${props.Friend.last_name}`} />
-      <Form.TextArea id="input" title="Natural Language Input" value={input} onChange={setDescription} placeholder={`I owe ${props.Friend.first_name} 12.82 for cinema...` + "\n" + `${props.Friend.first_name} paid 23 bucks for pizza`} />
+      <Form.TextArea
+        id="input"
+        title="Natural Language Input"
+        value={input}
+        onChange={setDescription}
+        placeholder={
+          `I owe ${props.Friend.first_name} 12.82 for cinema...` +
+          "\n" +
+          `${props.Friend.first_name} paid 23 bucks for pizza`
+        }
+      />
     </Form>
   );
 }
@@ -84,7 +87,7 @@ interface Body {
   };
 }
 
-function ShareSecretAction(props: { input: string; Friend: Friend;}) {
+function ShareSecretAction(props: { input: string; Friend: Friend }) {
   async function handleSubmit() {
     const toast = await showToast({
       style: Toast.Style.Animated,
@@ -95,9 +98,9 @@ function ShareSecretAction(props: { input: string; Friend: Friend;}) {
       const { body }: { body: Body } = await got.post("https://secure.splitwise.com/api/v3.0/parse_sentence", {
         headers: { Accept: "application/json", Authorization: `Bearer ${personalAccessToken}` },
         json: {
-          "input": `${props.input}`,
-          "friend_id": props.Friend.id,
-          "autosave": true
+          input: `${props.input}`,
+          friend_id: props.Friend.id,
+          autosave: true,
         },
         responseType: "json",
       });
@@ -109,10 +112,10 @@ function ShareSecretAction(props: { input: string; Friend: Friend;}) {
       } else {
         toast.style = Toast.Style.Failure;
         toast.title = "D'oh! Invalid input!";
-        
+
         let errorString = "";
-        let response_error = body.expense.errors;
-        for (let key in response_error) {
+        const response_error = body.expense.errors;
+        for (const key in response_error) {
           errorString += key + ": " + response_error[key] + "\n";
         }
         toast.message = `${errorString}`;
@@ -126,4 +129,3 @@ function ShareSecretAction(props: { input: string; Friend: Friend;}) {
 
   return <Action.SubmitForm icon={Icon.Wallet} title="Add Expense" onSubmit={handleSubmit} />;
 }
-
--- extensions/Splitwise/src/types.ts
+++ extensions/Splitwise/src/types.ts
@@ -1,31 +1,31 @@
 export type GetFriends = {
-    friends: Friend[];
-}
+  friends: Friend[];
+};
 
 export type Friend = {
-    id:                  number;
-    first_name:          string;
-    last_name:           string;
-    email:               string;
-    registration_status: string;
-    picture:             Picture;
-    groups:              Group[];
-    balance:             Balance[];
-    updated_at:          Date;
-}
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  registration_status: string;
+  picture: Picture;
+  groups: Group[];
+  balance: Balance[];
+  updated_at: Date;
+};
 
 export type Balance = {
-    currency_code: string;
-    amount:        string;
-}
+  currency_code: string;
+  amount: string;
+};
 
 export type Group = {
-    group_id: number;
-    balance:  Balance[];
-}
+  group_id: number;
+  balance: Balance[];
+};
 
-export type Picture =  {
-    small:  string;
-    medium: string;
-    large:  string;
-}
+export type Picture = {
+  small: string;
+  medium: string;
+  large: string;
+};
