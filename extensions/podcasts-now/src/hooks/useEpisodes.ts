b1b9231ddaf3db1a5ed2af988fb7a6b55a4a8d2c
Ray Hao <cloudy9101@gmail.com>
2023-02-11 00:16:50 +1300

    remove useless code

--- extensions/podcasts-now/src/hooks/useEpisodes.ts
+++ extensions/podcasts-now/src/hooks/useEpisodes.ts
@@ -1,28 +1,6 @@
 import { useCachedPromise } from "@raycast/utils";
-import { useEffect, useState } from "react";
-import { Episode, getEpisodes } from "../feed-parser";
-
-export const useEpisodesFetch = (feed: string) => {
-  const [data, setData] = useState<Episode[]>();
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState<Error>();
-
-  useEffect(() => {
-    (async () => {
-      setIsLoading(true);
-      try {
-        const episodes = await getEpisodes(feed);
-        setData(episodes);
-      } catch (err) {
-        setError(err as Error);
-      }
-      setIsLoading(false);
-    })();
-  }, [feed]);
-
-  return { data, error, isLoading };
-};
+import { getEpisodes } from "../feed-parser";
 
 export const useEpisodes = (feed: string) => {
-  return useCachedPromise(getEpisodes, [feed]);
+  return useCachedPromise<typeof getEpisodes>(getEpisodes, [feed]);
 };
