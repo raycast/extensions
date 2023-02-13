b9d79826a6b9589f550c1f05f2d744e0ee4a1f0d
Jakub Soboczyński <teziovsky@gmail.com>
2023-02-13 19:00:04 +0100

    feat: update useUsers hook

--- extensions/height/src/hooks/useUsers.ts
+++ extensions/height/src/hooks/useUsers.ts
@@ -1,15 +1,31 @@
 import { useFetch } from "@raycast/utils";
+import { useMemo } from "react";
 import { ApiHeaders, ApiUrls } from "../api/helpers";
 import { UserObject } from "../types/user";
 import { ApiResponse } from "../types/utils";
 
-export default function useUsers() {
-  const { data, error, isLoading, mutate } = useFetch<ApiResponse<UserObject>>(ApiUrls.users, {
+type Props = {
+  options?: Parameters<typeof useFetch<ApiResponse<UserObject[]>>>[1];
+};
+
+export default function useUsers({ options }: Props = {}) {
+  const { data, error, isLoading, mutate } = useFetch<ApiResponse<UserObject[]>>(ApiUrls.users, {
     headers: ApiHeaders,
+    ...options,
   });
+
+  const { users, bots } = useMemo(() => {
+    const users = data?.list?.filter((user) => !user.deleted && !user?.botType);
+
+    const bots = data?.list?.filter((user) => !user.deleted && Boolean(user?.botType));
+
+    return { users, bots };
+  }, [data]);
 
   return {
     usersData: data?.list,
+    users,
+    bots,
     usersError: error,
     usersIsLoading: isLoading,
     usersMutate: mutate,
