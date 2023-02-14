870a4eafe42bc1a7d3af89b85b3f3b2d6457c999
Chris Chinchilla <chris@chrischinchilla.com>
2023-02-14 13:07:25 +0000

    [Trello] Add creating Trello todo to extension (#4699)
    * Draft of Trello extension
    
    * Review changes
    
    * Fix dates
    
    * Refactor
    
    * Attempt to "fix" TS errors
    
    * Add lock file
    
    * Fix prettier issues that didn't show in VSC…
    
    * Update extensions/trello/src/index.tsx
    
    Co-authored-by: Per Nielsen Tikær <pnt@klean.dk>
    
    * Update extensions/trello/src/index.tsx
    
    Co-authored-by: Per Nielsen Tikær <pnt@klean.dk>
    
    * Update extensions/trello/src/index.tsx
    
    Co-authored-by: Per Nielsen Tikær <pnt@klean.dk>
    
    * Prettier
    
    * Apply fixes, update read me, and change placeholder to default
    
    * Prettier fix
    
    * Update extensions/trello/src/index.tsx
    
    Co-authored-by: Per Nielsen Tikær <pnt@klean.dk>
    
    * Update extensions/trello/package.json
    
    Co-authored-by: Per Nielsen Tikær <pnt@klean.dk>
    
    * Update dependencies
    
    * Split functionality
    
    * Update readme
    
    * Prettier fixes
    
    * Update readme
    
    * Update extensions/trello/package.json
    
    Co-authored-by: Per Nielsen Tikær <per@tikaer.dk>
    
    * Update extensions/trello/src/utils/fetchTodos.tsx
    
    Co-authored-by: Per Nielsen Tikær <per@tikaer.dk>
    
    * Update extensions/trello/src/utils/fetchTodos.tsx
    
    Co-authored-by: Per Nielsen Tikær <per@tikaer.dk>
    
    * First commit: Added vim bro to the extensions for review.
    
    * Removed unused image file
    
    * Fixed lint and folder structure
    
    * Fixed linting issues
    
    * Added more commands
    
    * Added filtering to the list. Keeping matched lists at the top.
    
    * Converted commands file to an array for better type handling.
    
    * Added readme.
    
    * Added search boards command. Some refactoring.
    
    * Removed vim bro from trello branch
    
    * Self review fixes.
    
    * Fixed prettier failure.
    
    * Update Raycast API
    
    * add form layout
    
    * Start
    
    * Draft of submit
    
    * add pop to root to go back on form
    
    * Update dependencies
    
    * Updated lockfile
    
    * Prettier errors
    
    * Update Raycast API
    
    * Updated code
    
    * Update package
    
    ---------
    
    Co-authored-by: Per Nielsen Tikær <pnt@klean.dk>
    Co-authored-by: Per Nielsen Tikær <per@tikaer.dk>
    Co-authored-by: Ajay Prem Shankar <ajaypremshankar@gmail.com>
    Co-authored-by: Brittany Joiner <brittanyjoiner15@gmail.com>
    Co-authored-by: Per Nielsen Tikær <per@raycast.com>

--- extensions/trello/src/utils/fetchLists.tsx
+++ extensions/trello/src/utils/fetchLists.tsx
@@ -1,17 +1,14 @@
 import { getPreferenceValues, showToast, Toast } from "@raycast/api";
 import fetch from "node-fetch";
-import { Board } from "../Board";
+import { List } from "../List";
 import { preferences } from "./types";
 
-export const returnBoards = async () => {
+export const returnLists = async (boardId: string) => {
   const { token, apitoken } = getPreferenceValues<preferences>();
-
   try {
-    const response = await fetch(
-      `https://api.trello.com/1/members/me/boards?key=${apitoken}&token=${token}&organization=true`
-    );
+    const response = await fetch(`https://api.trello.com/1/boards/${boardId}/lists?key=${apitoken}&token=${token}`);
 
-    return (await response.json()) as Board[];
+    return (await response.json()) as List[];
   } catch (error) {
     showToast(Toast.Style.Failure, "An error occured", "Could not fetch todos, check your credentials");
     return Promise.resolve([]);
