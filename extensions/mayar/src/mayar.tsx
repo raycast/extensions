80efde042e2de14006eb7a49f975c9fed63f1047
GITHUBFULLNAME <rf.moerdowo@gmail.com>
2023-02-15 12:57:38 +0700

    double quote

--- extensions/mayar/src/mayar.tsx
+++ extensions/mayar/src/mayar.tsx
@@ -1,6 +1,6 @@
 import { List, getPreferenceValues } from "@raycast/api";
 import { useEffect, useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 interface Balance {
   balanceActive: number;
@@ -15,15 +15,15 @@ interface Data {
 }
 
 function formatRp(value: number): string {
-  const formatter = new Intl.NumberFormat('id-ID', {
-    style: 'currency',
-    currency: 'IDR',
+  const formatter = new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
   });
   return formatter.format(value);
 }
 
 const fetchData = async (token: string): Promise<Data[]> => {
-  const response = await axios.get<Data[]>('https://api.mayar.id/hl/v1/balance', {
+  const response = await axios.get<Data[]>("https://api.mayar.id/hl/v1/balance", {
     headers: {
       Authorization: `Bearer ${token}`,
     },
@@ -37,7 +37,7 @@ export default function Command() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const token = preferences['required-password'];
+    const token = preferences["required-password"];
     fetchData(token)
       .then((result) => {
         setData(result);
@@ -51,23 +51,23 @@ export default function Command() {
   return (
     <List isLoading={loading}>
       <List.Item
-          icon={{ source: "wallet.png" }}
-          key='balance'
-          title='Balance'
-          accessories={[{ text: loading? 'Loading...' : formatRp(data?.data.balance) }]}
-        />
-        <List.Item
-          icon={{ source: "active.png" }}
-          key='balance-active'
-          title='Balance Active'
-          accessories={[{ text: loading? 'Loading...' : formatRp(data?.data.balanceActive) }]}
-        />
-        <List.Item
-          icon={{ source: "pending.png" }}
-          key='balance-pending'
-          title='Balance Pending'
-          accessories={[{ text: loading? 'Loading...' : formatRp(data?.data.balancePending) }]}
-        />
+        icon={{ source: "wallet.png" }}
+        key="balance"
+        title="Balance"
+        accessories={[{ text: loading ? "Loading..." : formatRp(data?.data.balance) }]}
+      />
+      <List.Item
+        icon={{ source: "active.png" }}
+        key="balance-active"
+        title="Balance Active"
+        accessories={[{ text: loading ? "Loading..." : formatRp(data?.data.balanceActive) }]}
+      />
+      <List.Item
+        icon={{ source: "pending.png" }}
+        key="balance-pending"
+        title="Balance Pending"
+        accessories={[{ text: loading ? "Loading..." : formatRp(data?.data.balancePending) }]}
+      />
     </List>
   );
 }
