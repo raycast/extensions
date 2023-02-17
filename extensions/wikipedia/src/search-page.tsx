00d1f592e3ff583c40b22cc0e2fbef5f371ec5e1
vimtor <victor@vimtor.io>
2023-02-17 09:41:39 +0100

    Add copy paste and search preferences

--- extensions/wikipedia/src/search-page.tsx
+++ extensions/wikipedia/src/search-page.tsx
@@ -1,8 +1,10 @@
-import { Action, ActionPanel, Icon, List } from "@raycast/api";
-import { usePromise } from "@raycast/utils";
+import { Action, ActionPanel, Icon, List, getPreferenceValues } from "@raycast/api";
+import { useCachedPromise, usePromise } from "@raycast/utils";
 import { useState } from "react";
 import ShowDetailsPage from "./show-details-page";
 import { findPagesByTitle, getPageData } from "./wikipedia";
+
+const preferences = getPreferenceValues();
 
 export default function SearchPage() {
   const [search, setSearch] = useState("");
@@ -17,32 +19,32 @@ export default function SearchPage() {
       searchBarPlaceholder="Search pages by name..."
       searchBarAccessory={
         <List.Dropdown tooltip="Language" storeValue onChange={setLanguage}>
-          <List.Dropdown.Item title="English" value="en" />
-          <List.Dropdown.Item title="German" value="de" />
-          <List.Dropdown.Item title="French" value="fr" />
-          <List.Dropdown.Item title="Japanese" value="ja" />
-          <List.Dropdown.Item title="Spanish" value="es" />
-          <List.Dropdown.Item title="Russian" value="ru" />
-          <List.Dropdown.Item title="Portuguese" value="pt" />
-          <List.Dropdown.Item title="Italian" value="it" />
-          <List.Dropdown.Item title="Chinese" value="zh" />
-          <List.Dropdown.Item title="Persian" value="fa" />
-          <List.Dropdown.Item title="Arabic" value="ar" />
-          <List.Dropdown.Item title="Polish" value="pl" />
-          <List.Dropdown.Item title="Dutch" value="nl" />
-          <List.Dropdown.Item title="Turkish" value="tr" />
-          <List.Dropdown.Item title="Greek" value="el" />
+          <List.Dropdown.Item icon="🇺🇸" title="English" value="en" />
+          <List.Dropdown.Item icon="🇩🇪" title="German" value="de" />
+          <List.Dropdown.Item icon="🇫🇷" title="French" value="fr" />
+          <List.Dropdown.Item icon="🇯🇵" title="Japanese" value="ja" />
+          <List.Dropdown.Item icon="🇪🇸" title="Spanish" value="es" />
+          <List.Dropdown.Item icon="🇷🇺" title="Russian" value="ru" />
+          <List.Dropdown.Item icon="🇵🇹" title="Portuguese" value="pt" />
+          <List.Dropdown.Item icon="🇮🇹" title="Italian" value="it" />
+          <List.Dropdown.Item icon="🇨🇳" title="Chinese" value="zh" />
+          <List.Dropdown.Item icon="🇮🇷" title="Persian" value="fa" />
+          <List.Dropdown.Item icon="🇦🇪" title="Arabic" value="ar" />
+          <List.Dropdown.Item icon="🇵🇱" title="Polish" value="pl" />
+          <List.Dropdown.Item icon="🇸🇽" title="Dutch" value="nl" />
+          <List.Dropdown.Item icon="🇹🇷" title="Turkish" value="tr" />
+          <List.Dropdown.Item icon="🇬🇷" title="Greek" value="el" />
         </List.Dropdown>
       }
     >
       {data?.language === language &&
-        data?.results.map((title) => <PageItem key={title} title={title} language={language} />)}
+        data?.results.map((title: string) => <PageItem key={title} title={title} language={language} />)}
     </List>
   );
 }
 
 function PageItem({ title, language }: { title: string; language: string }) {
-  const { data: page } = usePromise(getPageData, [title, language]);
+  const { data: page } = useCachedPromise(getPageData, [title, language]);
 
   return (
     <List.Item
@@ -52,13 +54,38 @@ function PageItem({ title, language }: { title: string; language: string }) {
       subtitle={page?.description}
       actions={
         <ActionPanel>
-          <Action.OpenInBrowser url={page?.content_urls.desktop.page || ""} />
-          {page && <Action.Push icon={Icon.Window} title={"Show Details"} target={<ShowDetailsPage page={page} />} />}
-          <Action.CopyToClipboard
-            shortcut={{ modifiers: ["cmd"], key: "." }}
-            title="Copy URL"
-            content={page?.content_urls.desktop.page || ""}
-          />
+          {preferences.defaultAction === "browser" ? (
+            <>
+              <Action.OpenInBrowser url={page?.content_urls.desktop.page || ""} />
+              <Action.Push
+                icon={Icon.Window}
+                title="Show Details"
+                target={<ShowDetailsPage title={title} language={language} />}
+              />
+            </>
+          ) : (
+            <>
+              <Action.Push
+                icon={Icon.Window}
+                title="Show Details"
+                target={<ShowDetailsPage title={title} language={language} />}
+              />
+              <Action.OpenInBrowser url={page?.content_urls.desktop.page || ""} />
+            </>
+          )}
+          <ActionPanel.Section>
+            <Action.CopyToClipboard
+              shortcut={{ modifiers: ["cmd"], key: "." }}
+              title="Copy URL"
+              content={page?.content_urls.desktop.page || ""}
+            />
+            <Action.CopyToClipboard shortcut={{ modifiers: ["cmd"], key: "," }} title="Copy Title" content={title} />
+            <Action.CopyToClipboard
+              shortcut={{ modifiers: ["cmd", "shift"], key: "," }}
+              title="Copy Description"
+              content={page?.description}
+            />
+          </ActionPanel.Section>
         </ActionPanel>
       }
     />
--- extensions/wikipedia/src/show-details-page.tsx
+++ extensions/wikipedia/src/show-details-page.tsx
@@ -1,31 +1,43 @@
 import { Action, ActionPanel, Detail } from "@raycast/api";
-import { PageSummary } from "./wikipedia";
+import { getPageData } from "./wikipedia";
+import { useCachedPromise } from "@raycast/utils";
 
-function ShowDetailsPage({ page }: { page: PageSummary }) {
+function ShowDetailsPage({ title, language }: { title: string; language: string }) {
+  const { data: page } = useCachedPromise(getPageData, [title, language]);
+
   const markdown = `
-  
   # ${page.title}
   
-  ###### ${page.description ? page.description : ""}
+  ${page.description ? page.description : ""}
+
+
 
   ---
 
-  ${page.extract} [read more](${page.content_urls.desktop.page})\n\n
+  ${page.extract} [Read More](${page.content_urls.desktop.page})\n\n
   ![](${page.thumbnail?.source})
   `;
 
   return (
     <Detail
-      navigationTitle={page.title}
+      navigationTitle={title}
       markdown={markdown}
       actions={
         <ActionPanel>
           <Action.OpenInBrowser url={page.content_urls.desktop.page} />
-          <Action.CopyToClipboard
-            shortcut={{ modifiers: ["cmd"], key: "." }}
-            title="Copy URL"
-            content={page.content_urls.desktop.page}
-          />
+          <ActionPanel.Section>
+            <Action.CopyToClipboard
+              shortcut={{ modifiers: ["cmd"], key: "." }}
+              title="Copy URL"
+              content={page.content_urls.desktop.page}
+            />
+            <Action.CopyToClipboard shortcut={{ modifiers: ["cmd"], key: "," }} title="Copy Title" content={title} />
+            <Action.CopyToClipboard
+              shortcut={{ modifiers: ["cmd", "shift"], key: "," }}
+              title="Copy Description"
+              content={page?.description}
+            />
+          </ActionPanel.Section>
         </ActionPanel>
       }
     />
