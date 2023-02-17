e41ea0f74b99d77718557375d2257ff27fe809cf
denizyoldas <daksu30@gmail.com>
2023-02-17 23:45:00 +0300

    (feat): Remove result.json connect the api and readme update
    ## delete:
    - result.json
    
    ## add:
    - axios
    - csv-parse
    
    ## update:
    - index.tsx

--- extensions/chatgpt3-prompt/src/index.tsx
+++ extensions/chatgpt3-prompt/src/index.tsx
@@ -1,58 +1,75 @@
-import { ActionPanel, Detail, List, Action, getPreferenceValues, Icon } from "@raycast/api";
-import data from "./result.json";
+import { ActionPanel, Detail, List, Action, getPreferenceValues } from "@raycast/api";
+import { parse } from "csv-parse";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 type CommandPreferences = {
   primaryAction: "copy" | "paste";
 };
 
+type Data = {
+  act: string;
+  prompt: string;
+};
+
 export default function Command() {
+  const [data, setData] = useState<Data[]>([]);
   const preferences: CommandPreferences = getPreferenceValues();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://raw.githubusercontent.com/f/awesome-chatgpt-prompts/main/prompts.csv"
+        );
+        parse(response.data, { columns: true }, (err, records) => {
+          setData(records);
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <List>
       {data.map((item, index) => {
-        const content = item.items
-          .map((item) => {
-            return `${item.content}`;
-          })
-          .join("\n");
-
         return (
           <List.Item
-            key={item.title + index}
+            key={item.act + index}
             icon="list-icon.png"
-            title={item.title}
+            title={item.act}
             actions={
               <ActionPanel>
                 {preferences.primaryAction === "copy" ? (
                   <>
-                    <Action.CopyToClipboard title="Copy Prompt" content={content} />
-                    <Action.Paste title="Paste Prompt in Active App" content={content} />
+                    <Action.CopyToClipboard title="Copy Prompt" content={item.prompt} />
+                    <Action.Paste title="Paste Prompt in Active App" content={item.prompt} />
                   </>
                 ) : (
                   <>
-                    <Action.Paste title="Paste Prompt in Active App" content={content} />
-                    <Action.CopyToClipboard title="Copy Prompt" content={content} />
+                    <Action.Paste title="Paste Prompt in Active App" content={item.prompt} />
+                    <Action.CopyToClipboard title="Copy Prompt" content={item.prompt} />
                   </>
                 )}
                 <Action.Push
                   title="Show Prompt"
-                  icon={Icon.AppWindowList}
                   shortcut={{ modifiers: ["cmd"], key: "d" }}
                   target={
                     <Detail
-                      markdown={content}
+                      markdown={item.prompt}
                       actions={
                         <ActionPanel>
                           {preferences.primaryAction === "copy" ? (
                             <>
-                              <Action.CopyToClipboard title="Copy Prompt" content={content} />
-                              <Action.Paste title="Paste Prompt in Active App" content={content} />
+                              <Action.CopyToClipboard title="Copy Prompt" content={item.prompt} />
+                              <Action.Paste title="Paste Prompt in Active App" content={item.prompt} />
                             </>
                           ) : (
                             <>
-                              <Action.Paste title="Paste Prompt in Active App" content={content} />
-                              <Action.CopyToClipboard title="Copy Prompt" content={content} />
+                              <Action.Paste title="Paste Prompt in Active App" content={item.prompt} />
+                              <Action.CopyToClipboard title="Copy Prompt" content={item.prompt} />
                             </>
                           )}
                         </ActionPanel>
