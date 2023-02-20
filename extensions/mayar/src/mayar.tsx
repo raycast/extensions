815ae6ea26e3af470fe28beef20a754c335bc8e1
Frianto Moerdowo <rf.moerdowo@gmail.com>
2023-02-20 13:02:49 +0000

    [Extensions Update] Mayar Extensions with Menu Bar (#4860)
    * Mayar menu bar for check balance and recent transactions
    
    * remove single quote
    
    * rerunt test
    
    * change object access
    
    * typescript fix
    
    * fix ts
    
    * Update the code
    
    * Removed unused logo
    
    ---------
    
    Co-authored-by: Per Nielsen Tikær <per@raycast.com>

--- extensions/mayar/src/mayar.tsx
+++ extensions/mayar/src/mayar.tsx
@@ -2,16 +2,10 @@ import { List, getPreferenceValues } from "@raycast/api";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-interface Balance {
-  balanceActive: number;
-  balancePending: number;
-  balance: number;
-}
-
 interface Data {
   statusCode: number;
   messages: string;
-  data: Balance;
+  data: any;
 }
 
 function formatRp(value: number): string {
@@ -22,8 +16,8 @@ function formatRp(value: number): string {
   return formatter.format(value);
 }
 
-const fetchData = async (token: string): Promise<Data[]> => {
-  const response = await axios.get<Data[]>("https://api.mayar.id/hl/v1/balance", {
+const fetchData = async (token: string): Promise<Data> => {
+  const response = await axios.get<Data>("https://api.mayar.id/hl/v1/balance", {
     headers: {
       Authorization: `Bearer ${token}`,
     },
