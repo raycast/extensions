ebd64f4a195a67b06fa4ef7b2b1dc96f0562b1ba
Emil Sharier <emil.sharier.dev@gmail.com>
2023-02-22 12:44:03 +0530

    Adding test suites, conditional check for OS version

--- extensions/change-scroll-direction/src/changeScrollDirection.ts
+++ extensions/change-scroll-direction/src/changeScrollDirection.ts
@@ -1,9 +1,10 @@
 import { closeMainWindow, showHUD } from "@raycast/api";
 import { runAppleScript } from "run-applescript";
+import { fetchAppleScript } from "./scripts";
 
 export default async function main() {
   try {
-    await runAppleScriptSilently(script);
+    await runAppleScriptSilently();
     await showHUD("Scroll direction changed");
   } catch (err) {
     await showHUD("Couldn't change scroll direction...");
@@ -20,25 +21,8 @@ export default async function main() {
  * @throws An error when the AppleScript fails to run
  * @returns A promise that is resolved when the AppleScript finished running
  */
-async function runAppleScriptSilently(appleScript: string) {
+async function runAppleScriptSilently() {
   await closeMainWindow();
+  const appleScript = fetchAppleScript();
   await runAppleScript(appleScript);
 }
-
-const script = `
-  tell application "System Settings"
-  activate
-  end tell
-  delay 0.1
-
-  tell application "System Events"
-  tell process "System Settings"
-      click menu item "Trackpad" of menu "View" of menu bar 1
-      delay 0.25
-      click radio button 2 of tab group 1 of group 1 of group 2 of splitter group 1 of group 1 of window 1
-      click checkbox "Natural scrolling" of group 1 of scroll area 1 of group 1 of group 2 of splitter group 1 of group 1 of window 1
-      tell application "System Settings" to quit
-  end tell
-  end tell
-return 1
-`;
