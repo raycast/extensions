1cc4df0b2cc90027d18e3fbe94ee3f206b6b8384
Dennis Muensterer <dennismuensterer@gmail.com>
2023-02-24 00:35:50 +0100

    Revert "Fix git mistake"
    This reverts commit 78f1dab34881cb5f169883d225f560720bc7546e.

--- extensions/bettertouchtool/node_modules/@typescript-eslint/type-utils/dist/getTypeArguments.d.ts
+++
@@ -1,3 +0,0 @@
-import type * as ts from 'typescript';
-export declare function getTypeArguments(type: ts.TypeReference, checker: ts.TypeChecker): readonly ts.Type[];
-//# sourceMappingURL=getTypeArguments.d.ts.map
