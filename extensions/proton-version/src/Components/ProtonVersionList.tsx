817c8cb07f12ea5988d455469f6048babfd268f5
Flavien Bonvin <flavien.bonvin@pm.me>
2023-02-26 16:17:51 +0100

    Add proton-version extension
    - Add commands for mail, drive, calendar and account
    - Initial commit

---
+++ extensions/proton-version/src/Components/ProtonVersionList.tsx
@@ -0,0 +1,49 @@
+import { Action, ActionPanel, List, Icon } from "@raycast/api";
+import { GITHUB_HOME, PROTON_GITHUB_RELEASE_CHANGE, PROTON_GITHUB_RELEASE_TAG } from "../constants";
+import { getAssetFromProduct } from "../helpers/getAssetFromProduct";
+import { getFormattedDate } from "../helpers/getFormattedDate";
+import { getKeywordsFromProduct } from "../helpers/getKeywordsFromProduct";
+import { getProductNameFromProduct } from "../helpers/getProductNameFromProduct";
+import { getProductUrlFromProduct } from "../helpers/getProductUrlFromProduct";
+import { ProtonEnv, ProtonProduct, Version } from "../interface";
+
+interface Props {
+  data?: Version;
+  product: ProtonProduct;
+  environment: ProtonEnv;
+}
+
+const ProtonVersionList = ({ data, product, environment }: Props) => {
+  const title = `${data?.version} ${environment === "beta" ? "β" : ""}` ?? "Error while loading data";
+  const githubTagUrl = PROTON_GITHUB_RELEASE_TAG(product, data?.version ?? "");
+  const githubChangeUrl = PROTON_GITHUB_RELEASE_CHANGE(product, data?.version ?? "");
+
+  return (
+    <List.Item
+      title={title}
+      icon={{ source: getAssetFromProduct(product) }}
+      subtitle={`Deployed ${getFormattedDate(data?.date)} GMT`}
+      keywords={getKeywordsFromProduct(product)}
+      actions={
+        <ActionPanel>
+          <ActionPanel.Section title="GitHub">
+            <Action.OpenInBrowser title="ProtonMail/WebClients" url={GITHUB_HOME} icon={Icon.House} />
+            <Action.OpenInBrowser title="Release link" url={githubTagUrl} icon={Icon.ArrowRight} />
+            <Action.OpenInBrowser title="Compare changes" url={githubChangeUrl} icon={Icon.ArrowCounterClockwise} />
+            <Action.Paste title="Copy version number" content={data?.version ?? ""} />
+          </ActionPanel.Section>
+          <ActionPanel.Section title="Access product">
+            <Action.OpenInBrowser
+              title={`Access ${getProductNameFromProduct(product)}`}
+              url={getProductUrlFromProduct(product)}
+              icon={Icon.House}
+              shortcut={{ modifiers: ["cmd"], key: "p" }}
+            />
+          </ActionPanel.Section>
+        </ActionPanel>
+      }
+    />
+  );
+};
+
+export default ProtonVersionList;
