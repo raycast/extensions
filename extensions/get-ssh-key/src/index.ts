ed6f4d2d91b2ce929454c72add00b46dc3515e2f
Cheng Tang <theon.tang@lego.com>
2023-02-28 09:01:05 +0800

    add get ssh key

---
+++ extensions/get-ssh-key/src/index.ts
@@ -0,0 +1,7 @@
+import { showHUD } from "@raycast/api";
+import { exec } from "child_process";
+
+export default async function main() {
+  await exec('cat ~/.ssh/id_*.pub | pbcopy');
+  await showHUD("Copied ssh to clipboard");
+}
