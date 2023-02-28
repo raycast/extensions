fc04fa1f636541c8c33b996524ce898e4c91a826
Per Nielsen Tikær <per@raycast.com>
2023-02-28 15:13:21 +0100

    Update index.ts

--- extensions/get-ssh-key/src/index.ts
+++ extensions/get-ssh-key/src/index.ts
@@ -3,5 +3,5 @@ import { exec } from "child_process";
 
 export default async function main() {
   await exec("cat ~/.ssh/id_*.pub | pbcopy");
-  await showHUD("Copied ssh to clipboard");
+  await showHUD("Copied ssh key to clipboard");
 }
