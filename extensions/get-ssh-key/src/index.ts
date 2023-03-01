684e68c330323d7fef4d2d1833b46eb0af0cb1a1
Cheng Tang <theon.tang@lego.com>
2023-03-01 20:30:03 +0800

    fix lint

--- extensions/get-ssh-key/src/index.ts
+++ extensions/get-ssh-key/src/index.ts
@@ -2,6 +2,6 @@ import { showHUD } from "@raycast/api";
 import { exec } from "child_process";
 
 export default async function main() {
-  await exec("cat $(find ~/.ssh/ -maxdepth 1 -name \"*.pub\" -print -quit) | pbcopy");
+  await exec('cat $(find ~/.ssh/ -maxdepth 1 -name "*.pub" -print -quit) | pbcopy');
   await showHUD("Copied ssh key to clipboard");
 }
