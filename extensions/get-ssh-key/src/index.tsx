b3e733fb30d9c5bc49bd8cc34edce0056aca02ee
Per Nielsen Tikær <per@raycast.com>
2023-03-02 15:48:46 +0100

    Update index.tsx

--- extensions/get-ssh-key/src/index.tsx
+++ extensions/get-ssh-key/src/index.tsx
@@ -6,16 +6,12 @@ import path from "path";
 export default async function Command() {
   const homeDir = os.homedir();
   const sshKeyRootDir = path.join(homeDir, ".ssh");
-  const publicKeys = fs
-    .readdirSync(sshKeyRootDir)
-    .filter((file) => file.endsWith(".pub"));
+  const publicKeys = fs.readdirSync(sshKeyRootDir).filter((file) => file.endsWith(".pub"));
 
   if (publicKeys.length === 0) {
     await showHUD("❌ No ssh keys found");
   } else if (publicKeys.length === 1) {
-    await Clipboard.copy(
-      fs.readFileSync(path.join(sshKeyRootDir, publicKeys[0]), "utf8")
-    );
+    await Clipboard.copy(fs.readFileSync(path.join(sshKeyRootDir, publicKeys[0]), "utf8"));
     await showHUD("✅ Copied ssh key to clipboard");
   } else {
     await launchCommand({ name: "list", type: LaunchType.UserInitiated });
