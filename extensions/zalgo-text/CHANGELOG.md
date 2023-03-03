e3b2c0833b3cb0f7327447d72c0adddce4a0fee1
Peter Miles <kincaidpeter@gmail.com>
2023-03-02 18:33:37 -0800

    Add zalgo-text extension
    - feat: add initial generator
    - first commit

---
+++ extensions/zalgo-text/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Zalgo Text Generator Changelog
+
+## [Initial Version] - 2023-03-02
+
+- Added initial extension
---
+++ extensions/zalgo-text/src/index.tsx
@@ -0,0 +1,30 @@
+import { Form, ActionPanel, Action, showToast, Clipboard } from "@raycast/api";
+import zalgo from "to-zalgo";
+
+type Values = {
+  textfield: string;
+};
+
+export default function Command() {
+  async function handleSubmit(values: Values) {
+    const zalgodText = zalgo(values.textfield);
+
+    Clipboard.copy(zalgodText);
+    await showToast({ title: "Copied", message: zalgodText });
+  }
+
+  return (
+    <>
+      <Form
+        actions={
+          <ActionPanel>
+            <Action.SubmitForm onSubmit={handleSubmit} />
+          </ActionPanel>
+        }
+      >
+        <Form.Description text="Provide the text you'd like to convert to Zalgo" />
+        <Form.TextField id="textfield" title="Text field" placeholder="Enter text" defaultValue="Raycast is cool!" />
+      </Form>
+    </>
+  );
+}
