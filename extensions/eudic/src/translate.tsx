3db91477ac24cf680f5d704fc708b76798535aa7
Shi Bin <15519900807@qq.com>
2023-03-04 22:57:00 +0800

    fix: format code with Prettier

--- extensions/eudic/src/translate.tsx
+++ extensions/eudic/src/translate.tsx
@@ -2,20 +2,18 @@ import { closeMainWindow, LaunchProps } from "@raycast/api";
 import { runAppleScript } from "run-applescript";
 
 interface EudicArguments {
-    words: string;
+  words: string;
 }
 
 export default async function Command(props: LaunchProps<{ arguments: EudicArguments }>) {
-    const { words } = props.arguments;
-    await closeMainWindow();
-    await runAppleScript(`
+  const { words } = props.arguments;
+  await closeMainWindow();
+  await runAppleScript(`
         do shell script "open -b com.eusoft.eudic"
         do shell script "open -b com.eusoft.eudic"
 
         tell application id "com.eusoft.eudic"
             activate
             show dic with word "${words}"
-        end tell`
-    );
+        end tell`);
 }
-
