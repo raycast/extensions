c7ac502e81412eedf7c8c4f6c8a34998c8b8e4d3
Ex10si0n <yan.ex10si0n@icloud.com>
2023-03-04 16:31:47 +0800

    update: code reformat

--- extensions/ask-chatgpt/src/api.ts
+++ extensions/ask-chatgpt/src/api.ts
@@ -1,20 +1,19 @@
 import { Configuration, OpenAIApi } from "openai";
-import {getPreferenceValues } from "@raycast/api";
-import {ChatResponse, Preferences} from "./type";
+import { getPreferenceValues } from "@raycast/api";
+import { ChatResponse, Preferences } from "./type";
 
 export async function fetchChatResponse(prompt: string): Promise<string> {
-    const preferences: Preferences = getPreferenceValues();
-    const configuration = new Configuration({
-        apiKey: preferences.openaiApiToken
-    });
-    const openai = new OpenAIApi(configuration);
-    const completion = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: [{role: "user", content: prompt}],
-    });
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const responseData: ChatResponse = completion.data;
-    return responseData.choices[0].message.content;
+  const preferences: Preferences = getPreferenceValues();
+  const configuration = new Configuration({
+    apiKey: preferences.openaiApiToken,
+  });
+  const openai = new OpenAIApi(configuration);
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: prompt }],
+  });
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const responseData: ChatResponse = completion.data;
+  return responseData.choices[0].message.content;
 }
-
--- extensions/ask-chatgpt/src/chat.tsx
+++ extensions/ask-chatgpt/src/chat.tsx
@@ -1,35 +1,38 @@
-import {Detail, List, LaunchProps} from '@raycast/api'
-import {useState, useEffect} from "react";
-import {fetchChatResponse} from "./api";
+import { Detail, List, LaunchProps } from "@raycast/api";
+import { useState, useEffect } from "react";
+import { fetchChatResponse } from "./api";
 
 interface PromptText {
-    prompt: string;
+  prompt: string;
 }
 
 export default function askGPT(props: LaunchProps<{ arguments: PromptText }>) {
-    const [response, setResponse] = useState("");
-    const { prompt } = props.arguments;
+  const [response, setResponse] = useState("");
+  const { prompt } = props.arguments;
 
-    useEffect(() => {
-        async function callApi() {
-            try {
-                const strResponse = await fetchChatResponse(prompt);
-                setResponse(strResponse);
-            } catch (err) {
-                console.log(err)
-            }
-        }
-        callApi();
-    }, [])
-    return (
-        <Detail
-            markdown={response}
-            metadata={
-            <List.Item.Detail.Metadata>
-                <List.Item.Detail.Metadata.Label title="Prompt" text={prompt} />
-                <List.Item.Detail.Metadata.Label title="Date" text={new Date().toLocaleString()} />
-            </List.Item.Detail.Metadata>
-            }
-        />
-    )
+  useEffect(() => {
+    async function callApi() {
+      try {
+        const strResponse = await fetchChatResponse(prompt);
+        setResponse(strResponse);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    callApi();
+  }, []);
+  return (
+    <Detail
+      markdown={response}
+      metadata={
+        <List.Item.Detail.Metadata>
+          <List.Item.Detail.Metadata.Label title="Prompt" text={prompt} />
+          <List.Item.Detail.Metadata.Label
+            title="Date"
+            text={new Date().toLocaleString()}
+          />
+        </List.Item.Detail.Metadata>
+      }
+    />
+  );
 }
