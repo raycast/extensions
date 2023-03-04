26eb062167630ccddc82a23ab341b2157f815417
Basix <basix@basix.tech>
2023-03-04 09:52:29 +0900

    Fix useMe lint

--- extensions/mastodon/src/hooks/masto.ts
+++ extensions/mastodon/src/hooks/masto.ts
@@ -24,5 +24,8 @@ export function useInstance(masto: mastodon.Client | undefined) {
 }
 
 export function useMe(masto: mastodon.Client | undefined) {
-  return useCachedPromise(async (masto: mastodon.Client | undefined) => masto?.v1.accounts.verifyCredentials(), [masto]);
+  return useCachedPromise(
+    async (masto: mastodon.Client | undefined) => masto?.v1.accounts.verifyCredentials(),
+    [masto]
+  );
 }
