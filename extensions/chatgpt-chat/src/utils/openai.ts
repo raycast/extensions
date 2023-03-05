40a637e0be3a613fe3ca9ae566d8bc328e1a384a
Erik Davtyan <erikdavtyan@gmail.com>
2023-03-06 03:23:09 +0400

    Remove unnecessary chat context to save on prompt tokens

--- extensions/chatgpt-chat/src/utils/openai.ts
+++ extensions/chatgpt-chat/src/utils/openai.ts
@@ -8,12 +8,6 @@ export interface Conversation {
 
 export type MessageWithId = ChatCompletionRequestMessage & { id: string };
 
-const CONTEXT: MessageWithId = {
-  role: "system",
-  content:
-    "The following is a conversation with an AI chatbot called Raycast ChatGPT. The chatbot is a Raycast extention and helps users connect to ChatGPT from their local machines",
-  id: "0",
-};
 
 export class Conversation extends EventEmitter {
   private static instance: Conversation;
@@ -54,7 +48,7 @@ export class Conversation extends EventEmitter {
 
     const completion = await this.openai.createChatCompletion({
       model: "gpt-3.5-turbo",
-      messages: [CONTEXT, ...this.messages].map(({ id, ...m }) => m),
+      messages: this.messages.map(({ id, ...m }) => m),
     });
 
     const message = completion.data.choices[0].message;
