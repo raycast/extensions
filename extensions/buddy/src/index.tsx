39c411d80d0c8e01bb84b4a3ae40631b766aade5
Mark de Vries <mark@lamalama.nl>
2023-03-05 23:44:42 +0100

    Update buddy extension
    - Merge branch 'contributions/merge-1678056255887769000'
    - Pull contributions
    - add better error handling
    - add error handling
    - add icon and subtitle
    - add project pipelines

--- extensions/buddy/src/index.tsx
+++ extensions/buddy/src/index.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
-import { ActionPanel, List, Action, getPreferenceValues, showToast, Toast } from "@raycast/api";
+import { ActionPanel, List, Action, getPreferenceValues, showToast, Toast, useNavigation, Icon } from "@raycast/api";
 import fetch from "node-fetch";
 import { BUDDY_API_URL } from "./config";
+import { PipelinesList } from "./components/PipelinesList";
 
 export default function Command() {
+  const { push } = useNavigation();
   const [error, setError] = useState<Error>();
   const [projects, setProjects] = useState<IProject[]>([]);
   const [workspaces, setWorkspaces] = useState<IWorkspace[]>([]);
@@ -17,12 +19,17 @@ export default function Command() {
       headers: { Authorization: `Bearer ${ACCESS_TOKEN}` },
     });
 
-    let data: { workspaces: IWorkspace[] } = { workspaces: [] };
+    let data: { workspaces: IWorkspace[]; errors: IError[] } = { workspaces: [], errors: [] };
     try {
       data = (await response.json()) as WorkspacesResponse;
     } catch (e) {
       setError(new Error("while fetching your workspaces"));
     } finally {
+      if (data.errors) {
+        data.errors.forEach((error) => setError(error));
+        setLoading(false);
+      }
+
       setWorkspaces(data.workspaces);
     }
   }
@@ -32,12 +39,16 @@ export default function Command() {
       headers: { Authorization: `Bearer ${ACCESS_TOKEN}` },
     });
 
-    let data: { projects: IProject[] } = { projects: [] };
+    let data: { projects: IProject[]; errors: IError[] } = { projects: [], errors: [] };
     try {
       data = (await response.json()) as ProjectsResponse;
     } catch (e) {
       setError(new Error("while fetching your projects"));
     } finally {
+      if (data.errors) {
+        data.errors.forEach((error) => setError(error));
+      }
+
       setProjects(data.projects);
       setLoading(false);
     }
@@ -75,27 +86,39 @@ export default function Command() {
             setWorkspace(workspace);
           }}
         >
-          {workspaces.map((workspace, index) => {
-            return <List.Dropdown.Item key={`workspace-${index}`} title={workspace.name} value={workspace.domain} />;
-          })}
+          {workspaces && workspaces.length > 0
+            ? workspaces.map((workspace, index) => {
+                return (
+                  <List.Dropdown.Item key={`workspace-${index}`} title={workspace.name} value={workspace.domain} />
+                );
+              })
+            : ""}
         </List.Dropdown>
       }
     >
-      {projects.map((project, index) => {
-        return (
-          <List.Item
-            key={`project-${index}`}
-            title={project.name}
-            actions={
-              <ActionPanel title="Buddy">
-                <Action.OpenInBrowser title="Pipelines" url={`${project.html_url}/pipelines`} />
-                <Action.OpenInBrowser title="Sandboxes" url={`${project.html_url}/sb`} />
-                <Action.OpenInBrowser title="Code" url={`${project.html_url}/`} />
-              </ActionPanel>
-            }
-          />
-        );
-      })}
+      {projects && projects.length > 0 ? (
+        projects.map((project, index) => {
+          return (
+            <List.Item
+              key={`project-${index}`}
+              title={project.name}
+              actions={
+                <ActionPanel title="Buddy">
+                  <Action
+                    title="Pipelines"
+                    onAction={() => push(<PipelinesList domain={workspace} name={project.name} />)}
+                  />
+                  <Action.OpenInBrowser title="Open Pipelines" url={`${project.html_url}/pipelines`} />
+                  <Action.OpenInBrowser title="Open Sandboxes" url={`${project.html_url}/sb`} />
+                  <Action.OpenInBrowser title="Open Code" url={`${project.html_url}/`} />
+                </ActionPanel>
+              }
+            />
+          );
+        })
+      ) : (
+        <List.EmptyView icon={Icon.QuestionMark} description="No Projects Found" />
+      )}
     </List>
   );
 }
@@ -104,13 +127,20 @@ type ProjectsResponse = {
   project: string;
   html_url: string;
   projects: IProject[];
+  errors: IError[];
 };
 
 type WorkspacesResponse = {
   project: string;
   html_url: string;
   workspaces: IWorkspace[];
+  errors: IError[];
 };
+
+interface IError {
+  message: string;
+  name: string;
+}
 
 interface IProject {
   url: string;
