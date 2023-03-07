25806981ef53915c81ae0f9be2e85a346c617b61
Kevin Dai <78251524+daikaiyu@users.noreply.github.com>
2023-03-07 23:36:43 +0800

    feat & refactor: new command

--- extensions/v2ex-viewer/src/viewNotifications.tsx
+++ extensions/v2ex-viewer/src/viewNotifications.tsx
@@ -1,39 +1,64 @@
 import { List, Icon } from "@raycast/api";
-import { getUnixFromNow } from "./utils/time";
-import { useNotifications } from "./api/hooks";
+import { getUnixFromNow } from "@/utils/time";
+import { useFetch } from "@raycast/utils";
+import { getToken } from "@/utils/preference";
+import { Response, Notification } from "@/types/v2ex";
+import { showLoadingToast, showFailedToast, showSuccessfulToast } from "./utils/toast";
 
-const getTopicTitle = (text: string) => {
-  const regex = /<a href="\/t\/.*>(?<topicTitle>.*)<\/a>/gm;
+const getDetail = (text: string) => {
+  const regex = text.includes("›")
+    ? /(?<behavior>[^ ]+) › <a[^<>]+?>(?<topicTitle>[^<>]+)/gm
+    : /[^>]+?>(?<topicTitle>[^<>]+)<\/a> \W(?<behavior>[^<>]+)/gm;
   const match = regex.exec(text);
   try {
-    return match?.groups?.topicTitle;
+    return match?.groups;
   } catch (error) {
-    return text;
+    return null;
   }
 };
-export default function Command() {
-  const { notifications, previousLatestId } = useNotifications();
 
+export default function Command() {
+  const token = getToken();
+  const notifications = useFetch<Response<Notification[]>>("https://www.v2ex.com/api/v2/notifications", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    execute: !!token,
+    keepPreviousData: true,
+    onWillExecute: () => {
+      showLoadingToast({ message: "/notifications" });
+    },
+    onError: (error) => {
+      showFailedToast({ message: error.message || "" });
+    },
+    onData: (data) => {
+      showSuccessfulToast({ message: data.message || "" });
+    },
+  });
   return (
-    <List isShowingDetail>
-      {notifications.map(({ created, id, text, member, payload }) => {
-        const accessories = [
-          { text: getUnixFromNow(created) },
-          {
-            icon: id > previousLatestId ? Icon.Dot : null,
-          },
-        ];
-        return (
-          <List.Section title={getTopicTitle(text)}>
-            <List.Item
-              icon={text.includes("›") ? Icon.Heart : Icon.Message}
-              title={member.username}
-              accessories={accessories}
-              detail={<List.Item.Detail markdown={payload} />}
-            />
-          </List.Section>
-        );
-      })}
+    <List>
+      {notifications.data?.result &&
+        notifications.data.result.map(({ created, text, member, payload }) => {
+          const { topicTitle, behavior } = getDetail(text) || {};
+          if (text.includes("›")) {
+            return (
+              <List.Item
+                title={"❤️   " + member.username}
+                subtitle={behavior + " › " + topicTitle}
+                accessories={[{ tag: getUnixFromNow(created) }]}
+              />
+            );
+          }
+          return (
+            <List.Section title={topicTitle}>
+              <List.Item
+                title={"✏️   " + member.username}
+                subtitle={behavior + " : " + payload}
+                accessories={[{ tag: getUnixFromNow(created) }]}
+              />
+            </List.Section>
+          );
+        })}
     </List>
   );
 }
