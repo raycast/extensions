6537a985ec27caa68e6be89aeac32b9b17ed7d8f
MBearo <wuweiqi@baidu.com>
2023-03-07 16:25:10 +0800

    update prettier

--- extensions/codegeex-explanation/src/index.tsx
+++ extensions/codegeex-explanation/src/index.tsx
@@ -1,45 +1,43 @@
 import { Detail, getSelectedText, getPreferenceValues } from "@raycast/api";
 import { useState, useEffect, useMemo } from "react";
-import axios from 'axios'
+import axios from "axios";
 const usePreferences = () => {
   return useMemo(() => getPreferenceValues(), []);
 };
 export default function Command() {
-  const [result, setResult] = useState<string>('')
-  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [result, setResult] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const preferences = usePreferences();
   const onSearchTextChange = async (e: string) => {
     const res = await axios({
-      method: 'post',
-      url: 'https://wudao.aminer.cn/os/api/api/v2/multilingual_code_explain',
+      method: "post",
+      url: "https://wudao.aminer.cn/os/api/api/v2/multilingual_code_explain",
       data: {
-        'n': 1,
-        'prompt': e,
-        'lang': preferences.language,
-        'locale': preferences.comment,
-        'apikey': preferences.apikey,
-        'apisecret': preferences.apisecret,
-      }
-    })
+        n: 1,
+        prompt: e,
+        lang: preferences.language,
+        locale: preferences.comment,
+        apikey: preferences.apikey,
+        apisecret: preferences.apisecret,
+      },
+    });
     setResult(`\`\`\`js
-${res.data.result.output.code.join('')}
+${res.data.result.output.code.join("")}
 \`\`\`
-`)
-    setIsLoading(false)
-  }
+`);
+    setIsLoading(false);
+  };
   const readSelectedText = async () => {
     try {
       const selectedText = await getSelectedText();
-      await onSearchTextChange(selectedText)
+      await onSearchTextChange(selectedText);
     } catch (error) {
-      setResult('Please select some text')
+      setResult("Please select some text");
     }
-  }
+  };
   useEffect(() => {
-    readSelectedText()
-  }, [])
+    readSelectedText();
+  }, []);
 
-  return (
-    <Detail markdown={result} isLoading={isLoading} />
-  )
+  return <Detail markdown={result} isLoading={isLoading} />;
 }
