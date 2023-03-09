072b5597d133594bf3fa02777e962ca3af1dc7db
Per Nielsen Tikær <per@raycast.com>
2023-03-09 11:31:07 +0100

    Updated the code

--- extensions/typer/src/command1.ts
+++ extensions/typer/src/command1.ts
@@ -1,8 +1,14 @@
-import { Clipboard, getPreferenceValues } from "@raycast/api";
+import { Clipboard, getPreferenceValues, openCommandPreferences, showHUD, updateCommandMetadata } from "@raycast/api";
 export default async function main() {
   const values = getPreferenceValues();
   const value = values["typercommand1"];
   if (value) {
+    await updateCommandMetadata({ subtitle: `Pasting: ${value}` });
+
     Clipboard.paste(value);
+    return await showHUD("✅ Paste Successful");
   }
+
+  await openCommandPreferences();
+  return await showHUD("❌ Please set a text to paste");
 }
