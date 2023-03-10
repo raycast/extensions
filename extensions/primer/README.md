d413ba695fb8e72615f6da2556a39b212a9b7afc
Cole Bemis <colebemis@github.com>
2023-03-10 11:35:56 -0700

    Update primer extension
    - Copy react component name to clipboard
    - Add react props view
    - Create a useComponentData hook
    - Update README.md

--- extensions/primer/README.md
+++ extensions/primer/README.md
@@ -4,7 +4,7 @@ Search and navigate the [Primer](https://primer.style) ecosystem with [Raycast](
 
 ![Screenshot](https://user-images.githubusercontent.com/4608155/224273440-d5367cb7-9b4a-4a93-b9a2-8a03b48a0233.png)
 
-## Local development
+## Run locally
 
 ```sh
 # Clone the repository
--- extensions/primer/src/index.tsx
+++ extensions/primer/src/index.tsx
@@ -1,6 +1,13 @@
-import { Action, ActionPanel, Color, Icon, List } from "@raycast/api";
-import { useFetch } from "@raycast/utils";
-import { z } from "zod";
+import {
+  Action,
+  ActionPanel,
+  Color,
+  Icon,
+  List,
+  useNavigation,
+} from "@raycast/api";
+import { ReactPropsView } from "./react-props-view";
+import { useComponentData } from "./use-component-data";
 
 const statusColors: Record<string, Color> = {
   alpha: Color.Orange,
@@ -9,33 +16,9 @@ const statusColors: Record<string, Color> = {
   deprecated: Color.Red,
 };
 
-const componentSchema = z.object({
-  id: z.string(),
-  name: z.string(),
-  description: z.union([z.string(), z.null()]),
-  implementations: z.object({
-    react: z.union([
-      z.object({
-        status: z.string(),
-        a11yReviewed: z.boolean(),
-      }),
-      z.null(),
-    ]),
-  }),
-});
-
-const componentsSchema = z.object({
-  components: z.array(componentSchema),
-});
-
 export default function Command() {
-  // TODO: Replace this with primer.style/design URL after merging https://github.com/primer/design/pull/425
-  const { isLoading, data } = useFetch(
-    "https://primer-5dd8f1e892-26441320.drafts.github.io/components.json"
-  );
-
-  const { components = [] } = data ? componentsSchema.parse(data) : {};
-
+  const { isLoading, components } = useComponentData();
+  const { push } = useNavigation();
   return (
     <List isLoading={isLoading} isShowingDetail>
       <List.Section title="Components">
@@ -51,11 +34,36 @@ export default function Command() {
                   url={`https://primer.style/design/components/${component.id}`}
                 />
                 {component.implementations.react ? (
-                  <Action.OpenInBrowser
-                    icon={Icon.Book}
-                    title="Open React Documentation"
-                    url={`https://primer.style/design/components/${component.id}/react`}
-                  />
+                  <ActionPanel.Section title="React">
+                    <Action.OpenInBrowser
+                      icon={Icon.Book}
+                      title="Open React Documentation"
+                      url={`https://primer.style/design/components/${component.id}/react`}
+                    />
+                    <Action.CopyToClipboard
+                      icon={Icon.Clipboard}
+                      title="Copy React Component Name"
+                      content={component.implementations.react.name}
+                    />
+                    <Action.OpenInBrowser
+                      icon={Icon.Document}
+                      title="Open React Source Code"
+                      url={`https://github.com/primer/react/blob/main/src/${component.implementations.react.name}`}
+                    />
+                    <Action
+                      icon={Icon.List}
+                      title="View React Props"
+                      onAction={() =>
+                        push(
+                          <ReactPropsView
+                            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- We know this is defined because of the conditional above
+                            component={component.implementations.react!}
+                            docsUrl={`https://primer.style/design/components/${component.id}/react#props`}
+                          />
+                        )
+                      }
+                    />
+                  </ActionPanel.Section>
                 ) : null}
               </ActionPanel>
             }
@@ -104,7 +112,6 @@ export default function Command() {
                               : Color.SecondaryText,
                           }}
                         />
-                        {/* )} */}
                       </>
                     ) : (
                       <List.Item.Detail.Metadata.Label
