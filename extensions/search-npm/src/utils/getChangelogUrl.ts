e2665d3b7593de9b6faf82d4bd71ef2e6f846012
Luis <luis.fgbo@gmail.com>
2023-03-11 00:41:51 +0000

    feat(search-npm): run prettier against extension ts files

--- extensions/search-npm/src/utils/getChangelogUrl.ts
+++ extensions/search-npm/src/utils/getChangelogUrl.ts
@@ -1,6 +1,10 @@
-export const getChangeLogUrl = (type: 'github' | 'gitlab' | undefined, owner: string | null, name: string | null ) => {
-  if(type === 'github' && owner && name) {
-    return `https://github.com/${owner}/${name}/releases`;
+export const getChangeLogUrl = (
+  type: 'github' | 'gitlab' | undefined,
+  owner: string | null,
+  name: string | null,
+) => {
+  if (type === 'github' && owner && name) {
+    return `https://github.com/${owner}/${name}/releases`
   }
-  return null;
+  return null
 }
