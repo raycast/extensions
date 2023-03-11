c3e5df1b5fac0277e2a7c706b63ead086ba582a3
rawnly <rawnly@users.noreply.github.com>
2023-03-12 00:14:22 +0100

    feat: fixed `Library` localization

--- extensions/music/src/util/scripts/current-track.ts
+++ extensions/music/src/util/scripts/current-track.ts
@@ -3,6 +3,7 @@ import * as R from "fp-ts/Reader";
 import * as RTE from "fp-ts/ReaderTaskEither";
 import * as TE from "fp-ts/TaskEither";
 
+import { getLibraryName } from "./general";
 import { createQueryString, parseQueryString, runScript, tell } from "../apple-script";
 import { STAR_VALUE } from "../costants";
 import { ScriptError, Track } from "../models";
@@ -11,9 +12,13 @@ export const reveal = tell("Music", "reveal current track");
 export const love = tell("Music", "set loved of current track to true");
 export const dislike = tell("Music", "set disliked of current track to true");
 export const addToLibrary = pipe(
-  tell("Music", 'duplicate current track to source "Library"'),
-  // added a fallback script
-  TE.orElse(() => tell("Music", 'duplicate current track to library playlist "Library"'))
+  tell("Music", `duplicate current track to source 1`),
+  TE.orElseFirst(() =>
+    pipe(
+      getLibraryName,
+      TE.chain((name) => tell("Music", `duplicate current track to library playlist "${name}"`))
+    )
+  )
 );
 
 export const setCurrentTrackRating: RTE.ReaderTaskEither<number, ScriptError, string> = pipe(
@@ -33,27 +38,32 @@ export const getCurrentTrackRating = pipe(
  * @param playlist - The name of the target playlist
  */
 export const addToPlaylist = (playlist: string) =>
-  tell(
-    "Music",
-    `
+  pipe(
+    getLibraryName,
+    TE.chain((libraryName) =>
+      runScript(
+        `
 tell application "Music"
 	set theName to name of current track
 	set theArtist to artist of current track
 	set theAlbum to album of the current track
-	set existingTracks to get tracks of source "Library" whose name is theName and artist is theArtist and album is theAlbum
+	set existingTracks to get tracks of source 1 whose name is theName and artist is theArtist and album is theAlbum
 	
 	if (count of existingTracks) = 0 then
-		set theCount to count of tracks of source "Library"
-		duplicate current track to source "Library"
-		repeat while theCount = (count of tracks of source "Library")
+		set theCount to count of tracks of source 1
+		duplicate current track to source 1
+
+		repeat while theCount = (count of tracks of source 1)
 			delay 1
 		end repeat
 	end if
 	
-	set theTrack to first track of source "Library" whose name is theName and artist is theArtist and album is theAlbum
-	duplicate theTrack to playlist "${playlist}"
+	set theTrack to first track of source 1 whose name is theName and artist is theArtist and album is theAlbum
+	duplicate theTrack to playlist ${[libraryName, "Music"].includes(playlist) ? 1 : `"${playlist}"`}
 end tell
 `
+      )
+    )
   );
 
 export const getCurrentTrack = (): TE.TaskEither<Error, Readonly<Track>> => {
