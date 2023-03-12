ee58b75e5e3be2ba12f2f64120dcc5b436195d0c
Michael Li <michael.li1324@gmail.com>
2023-03-12 01:28:15 -0800

    [NBA Viewer] Fix 'View Standings' command and polish view

--- extensions/nba-game-viewer/src/utils/getStandings.ts
+++ extensions/nba-game-viewer/src/utils/getStandings.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
+import { StandingsResponse } from "../types/standings.types";
 
 type GetStandingsArgs = {
   year: string;
   group: string;
 };
 
-const getStandings = async ({ year, group }: GetStandingsArgs) => {
+const getStandings = async ({ year, group }: GetStandingsArgs): Promise<StandingsResponse> => {
   const groupId = group === "league" ? 1 : group === "conference" ? 2 : 3;
   const baseUrl = `https://site.web.api.espn.com/apis/v2/sports/basketball/nba/standings`;
   const params = {
