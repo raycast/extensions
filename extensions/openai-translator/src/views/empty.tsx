fc3052004efd8639470d7fdf0a669da4c153ac1a
Tiou Lims <dourokinga@gmail.com>
2023-03-12 20:06:05 +0800

    fix empty.tsx code style

--- extensions/openai-translator/src/views/empty.tsx
+++ extensions/openai-translator/src/views/empty.tsx
@@ -3,7 +3,9 @@ import { Icon, List } from "@raycast/api";
 export const EmptyView = () => (
   <List.EmptyView
     title="Type anything!"
-    description={"Type your text and hit the enter key\n⌘+P to change language. Using *Switch to Translate ...* to switch from/to."}
+    description={
+      "Type your text and hit the enter key\n⌘+P to change language. Using *Switch to Translate ...* to switch from/to."
+    }
     icon={Icon.QuestionMark}
   />
 );
