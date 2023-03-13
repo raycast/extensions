4eabc4f617f94358f778c92e24999f4749aba73c
Jakub Soboczyński <teziovsky@gmail.com>
2023-03-13 20:32:47 +0100

    chore: remove console.logs from oauth

--- extensions/height/src/api/oauth.ts
+++ extensions/height/src/api/oauth.ts
@@ -10,11 +10,9 @@ const client = new OAuth.PKCEClient({
   providerId: "height",
   description: "Connect your Height account…",
 });
-console.log("🚀 ~ file: oauth.ts:13 ~ client:", client);
 
 export async function authorize() {
   const tokenSet = await client.getTokens();
-  console.log("🚀 ~ file: oauth.ts:17 ~ authorize ~ tokenSet:", tokenSet);
 
   if (tokenSet?.accessToken) {
     if (tokenSet?.refreshToken && tokenSet?.isExpired()) {
@@ -32,13 +30,10 @@ export async function authorize() {
     clientId,
     scope: "api",
   });
-  console.log("🚀 ~ file: oauth.ts:36 ~ authorize ~ authRequest:", authRequest);
 
   const { authorizationCode } = await client.authorize(authRequest);
-  console.log("🚀 ~ file: oauth.ts:38 ~ authorize ~ authorizationCode:", authorizationCode);
 
   const tokens = await fetchTokens(authRequest, authorizationCode);
-  console.log("🚀 ~ file: oauth.ts:41 ~ authorize ~ tokens:", tokens);
   await client.setTokens(tokens);
 
   return tokens.access_token;
@@ -51,19 +46,18 @@ async function fetchTokens(authRequest: OAuth.AuthorizationRequest, authCode: st
     body: JSON.stringify({
       code: authCode,
       client_id: clientId,
+      code_verifier: authRequest.codeVerifier,
       redirect_uri: authRequest.redirectURI,
       grant_type: "authorization_code",
       scope: ["api"],
-      code_verifier: authRequest.codeVerifier,
     }),
   });
-  console.log("🚀 ~ file: oauth.ts:60 ~ fetchTokens ~ response:", response);
+
   if (!response.ok) {
     console.error("Fetch tokens error: ", await response.text());
     throw new Error(response.statusText);
   }
 
-  console.log("🚀 ~ file: oauth.ts:66 ~ fetchTokens ~ await response.json():", await response.json());
   return (await response.json()) as OAuth.TokenResponse;
 }
 
@@ -78,14 +72,13 @@ async function refreshTokens(refreshToken: string): Promise<OAuth.TokenResponse>
       scope: ["api"],
     }),
   });
-  console.log("🚀 ~ file: oauth.ts:81 ~ refreshTokens ~ response:", response);
+
   if (!response.ok) {
     console.error("refresh tokens error:", await response.text());
     throw new Error(response.statusText);
   }
 
   const tokenResponse = (await response.json()) as OAuth.TokenResponse;
-  console.log("🚀 ~ file: oauth.ts:88 ~ refreshTokens ~ tokenResponse:", tokenResponse);
   tokenResponse.refresh_token = tokenResponse.refresh_token ?? refreshToken;
 
   return tokenResponse;
