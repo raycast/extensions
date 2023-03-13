a9448492b37055fe39dd9c389deda4bc8f786370
Feyli <mathiscom64@gmail.com>
2023-03-13 13:31:20 +0100

    Initial commit

--- extensions/discord-timestamps/src/timestamp.tsx
+++ extensions/discord-timestamps/src/timestamp.tsx
@@ -1,10 +1,44 @@
-import { Action, ActionPanel, List, getPreferenceValues } from "@raycast/api";
-import { useState } from "react";
-import { TimestampType, generateTimestamp, prettyPreview } from "./timestamps";
+import {
+  Action,
+  ActionPanel,
+  List,
+  Detail,
+  Clipboard,
+  Toast,
+  Icon,
+  getPreferenceValues,
+  showToast,
+  getFrontmostApplication,
+} from "@raycast/api";
+import { usePromise } from "@raycast/utils";
+import { useState, useEffect, useRef } from "react";
+import { generateTimestamp, prettyPreview, TimestampType, validateSnowflake } from "./timestamps";
+import Style = Toast.Style;
+
+interface frontmostApp {
+  path: string;
+  name: string;
+}
 
 export default function Command() {
-  const [date, setDate] = useState<Date>(new Date());
   const { defaultAction } = getPreferenceValues();
+  const { data }: { data?: frontmostApp } = usePromise(getFrontmostApplication);
+  const [date, setDate] = useState<Date>(new Date());
+  const isCurrentRef = useRef<boolean>(true);
+
+  useEffect(() => {
+    const timeToNextSecond = 1000 - date.getMilliseconds();
+    setTimeout(() => {
+      if (isCurrentRef.current) {
+        setDate(new Date());
+      }
+      setInterval(() => {
+        if (isCurrentRef.current) {
+          setDate(new Date());
+        }
+      }, 1000);
+    }, timeToNextSecond);
+  }, []);
 
   function Actions(props: { type: TimestampType }) {
     const { type } = props;
@@ -13,7 +47,11 @@ export default function Command() {
       <ActionPanel>
         {defaultAction === "paste" ? (
           <>
-            <Action.Paste content={`${generateTimestamp(date, type)}`} />
+            <Action.Paste
+              content={`${generateTimestamp(date, type)}`}
+              title={"Paste to " + data?.path.split("/").pop()?.split(".").shift() || data?.name || "Active App"}
+              icon={{ fileIcon: data?.path || "" }}
+            />
             <Action.CopyToClipboard content={`${generateTimestamp(date, type)}`} />
           </>
         ) : (
@@ -28,22 +66,84 @@ export default function Command() {
 
   return (
     <List navigationTitle="Generate Discord Timestamp">
-      <List.Section title="Set Date">
+      <List.Section title="Input Date">
         <List.Item
-          title={"Change Date"}
+          title={"Set Date From Input"}
           actions={
             <ActionPanel>
               <Action.PickDate
                 title="Set Date"
-                onChange={(date) => {
-                  if (date) setDate(date);
+                onChange={async (date) => {
+                  if (date) {
+                    isCurrentRef.current = false;
+                    setDate(date);
+                    await showToast({
+                      style: Style.Success,
+                      title: "Success",
+                      message: `Date Set To ` + date.toLocaleString(),
+                    });
+                  }
+                }}
+              />
+            </ActionPanel>
+          }
+        />
+        <List.Item
+          title={"Set Date From Snowflake In Clipboard"}
+          actions={
+            <ActionPanel>
+              <Action
+                title="Retrieve From Clipboard"
+                icon={{ source: Icon.Clipboard }}
+                onAction={() => {
+                  Clipboard.readText().then(async (text) => {
+                    if (text) {
+                      try {
+                        isCurrentRef.current = false;
+                        setDate(validateSnowflake(text));
+                        await showToast({
+                          style: Style.Success,
+                          title: "Success",
+                          message: `Date Set To ` + date.toLocaleString(),
+                        });
+                      } catch (e: unknown) {
+                        await showToast({
+                          style: Style.Failure,
+                          title: "Invalid Snowflake",
+                          message: e instanceof Error ? e.message : String(e),
+                        });
+                      }
+                    } else {
+                      await showToast({
+                        style: Style.Failure,
+                        title: "Invalid Snowflake",
+                        message: "The clipboard is empty",
+                      });
+                    }
+                  });
+                }}
+              />
+              <Action.Push title={"What Is A Snowflake"} icon={{ source: Icon.Info }} target={<SnowflakeGuide />} />
+            </ActionPanel>
+          }
+        />
+        <List.Item
+          title={"Set Date From Current Time"}
+          actions={
+            <ActionPanel>
+              <Action
+                title="Set Date To Current Time"
+                icon={{ source: Icon.Clock }}
+                onAction={async () => {
+                  setDate(new Date());
+                  isCurrentRef.current = true;
                 }}
               />
             </ActionPanel>
           }
         />
       </List.Section>
-      <List.Section title="Timestamps">
+      <List.Section title="Timestamps (shown in your local timezone)">
         <List.Item title={"Short Time"} subtitle={`${prettyPreview(date, "t")}`} actions={<Actions type={"t"} />} />
         <List.Item title={"Long Time"} subtitle={`${prettyPreview(date, "T")}`} actions={<Actions type={"T"} />} />
         <List.Item title={"Short Date"} subtitle={`${prettyPreview(date, "d")}`} actions={<Actions type={"d"} />} />
@@ -58,5 +158,24 @@ export default function Command() {
         <List.Item title={"Epoch Time"} subtitle={`${prettyPreview(date, "E")}`} actions={<Actions type={"E"} />} />
       </List.Section>
     </List>
+  );
+}
+
+function SnowflakeGuide() {
+  return (
+    <Detail
+      markdown={
+        "# ❄️ What is a snowflake?\n" +
+        "A snowflake is a unique ID that Discord uses to identify messages, users, and other objects. Every snowflake includes the exact creation date and time of the object it's linked to.\n" +
+        "## How do I get a snowflake?\n" +
+        "1. Enable developer mode in Discord.\n" +
+        "2. Right click on a message, user, or other object.\n" +
+        '3. Click "Copy ID".\n' +
+        "## How do I enable developer mode?\n" +
+        "1. Go to your Discord settings.\n" +
+        '2. Scroll down to the "Advanced" section.\n' +
+        '3. Enable "Developer Mode".\n'
+      }
+    />
   );
 }
