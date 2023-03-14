2645c7341b226b5ccc987dbdd6c04dee6d7ebf2e
Per Nielsen Tikær <per@raycast.com>
2023-03-14 11:31:44 +0100

    Update

--- extensions/proton-version/src/Components/ProtonVersionList.tsx
+++ extensions/proton-version/src/Components/ProtonVersionList.tsx
@@ -1,7 +1,6 @@
 import { Action, ActionPanel, List, Icon } from "@raycast/api";
 import { GITHUB_HOME, PROTON_GITHUB_RELEASE_CHANGE, PROTON_GITHUB_RELEASE_TAG } from "../constants";
 import { getAssetFromProduct } from "../helpers/getAssetFromProduct";
-import { getFormattedDate } from "../helpers/getFormattedDate";
 import { getKeywordsFromProduct } from "../helpers/getKeywordsFromProduct";
 import { getProductNameFromProduct } from "../helpers/getProductNameFromProduct";
 import { getProductUrlFromProduct } from "../helpers/getProductUrlFromProduct";
@@ -28,20 +27,22 @@ const ProtonVersionList = ({ data, product, environment }: Props) => {
   const title = `${data.version} ${environment === "beta" ? "β" : ""}`;
   const githubTagUrl = PROTON_GITHUB_RELEASE_TAG(product, data.version);
   const githubChangeUrl = PROTON_GITHUB_RELEASE_CHANGE(product, data.version);
+  const date = new Date(data.date);
 
   return (
     <List.Item
-      title={title}
+      title={getProductNameFromProduct(product)}
       icon={{ source: getAssetFromProduct(product) }}
-      subtitle={`Deployed ${getFormattedDate(data.date)} GMT`}
+      subtitle={title}
+      accessories={[{ date: date, tooltip: `Deployed Date: ${date.toLocaleString()}` }]}
       keywords={getKeywordsFromProduct(product)}
       actions={
         <ActionPanel>
           <ActionPanel.Section title="GitHub">
             <Action.OpenInBrowser title="ProtonMail/WebClients" url={GITHUB_HOME} icon={Icon.House} />
-            <Action.OpenInBrowser title="Release link" url={githubTagUrl} icon={Icon.ArrowRight} />
-            <Action.OpenInBrowser title="Compare changes" url={githubChangeUrl} icon={Icon.ArrowCounterClockwise} />
-            <Action.Paste title="Copy version number" content={data.version ?? ""} />
+            <Action.OpenInBrowser title="Release Link" url={githubTagUrl} icon={Icon.ArrowRight} />
+            <Action.OpenInBrowser title="Compare Changes" url={githubChangeUrl} icon={Icon.ArrowCounterClockwise} />
+            <Action.Paste title="Copy Version Number" content={data.version ?? ""} />
           </ActionPanel.Section>
           <ActionPanel.Section title="Access product">
             <Action.OpenInBrowser
--- extensions/proton-version/src/helpers/getFormattedDate.ts
+++
@@ -1,9 +0,0 @@
-import { format } from "date-fns";
-
-export const getFormattedDate = (dateString?: string) => {
-  if (!dateString) {
-    return "Error while formatting the date";
-  }
-  const date = new Date(dateString);
-  return format(date, "MMM dd @hh:mm");
-};
