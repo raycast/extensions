b0cbc36373fa958294076666d32c47b0e1fb70fd
Jackson Salopek <jackson.salopek@gmail.com>
2023-03-17 18:53:56 -0400

    Broke applescript out into functions for error-handling, resolved launch error if app was not installed

--- extensions/tradingview-controls/src/tvc-open-symbol.tsx
+++ extensions/tradingview-controls/src/tvc-open-symbol.tsx
@@ -1,5 +1,7 @@
-import { LaunchProps, closeMainWindow } from "@raycast/api";
+import { LaunchProps, closeMainWindow, showToast, Toast } from "@raycast/api";
 import { runAppleScript } from "run-applescript";
+import { handleApplication } from "./handle-application";
+import { launchAndFocusApplication } from "./launch-and-focus-application";
 import { TVCLogger } from "./logger";
 import { TVCSymbolArg } from "./types";
 
@@ -24,41 +26,61 @@ export async function TVCOpenSymbol(
   // Get the symbol from the arguments.
   const { symbol } = props.arguments;
 
-  // Close the main window to prevent it from stealing focus.
-  await closeMainWindow();
+  // Check if TradingView is installed.
+  // If it's not, show an error and exit.
+  const { isInstalled, isRunning } = await handleApplication();
 
-  // Check if TradingView is running and open it if it's not.
-  // Then, activate the app and open the symbol.
-  TVCLogger.log("Running AppleScript...", { context });
-  await runAppleScript(`
-  on is_running(appName)
-    tell application "System Events" to (name of processes) contains appName
-  end is_running
-  set tvIsRunning to is_running("TradingView")
-  if not tvIsRunning then
-    tell application "TradingView" to launch
-    repeat until is_running("TradingView")
-      delay 0.1
-    end repeat
-    delay 4
-  end if
-  tell application "TradingView" to activate
-  delay 1
-  set symbol to "${symbol}"
-  tell application "System Events"
-    if symbol starts with "B" or symbol starts with "b" then
-      key code 11
-      delay 0.01
-      keystroke text 2 thru -1 of symbol
-      keystroke return
-    else
-      keystroke symbol
-      keystroke return
-    end if
-  end tell`);
-  TVCLogger.log("AppleScript executed.", { context });
+  if (!isInstalled) {
+    TVCLogger.error("TradingView is not installed.", { context });
+    TVCLogger.error("Please install it and try again.", { context });
+    showToast({
+      style: Toast.Style.Failure,
+      title: "TradingView is not installed.",
+      message: "Please install it and try again.",
+    });
+  } else {
+    // Close the main window to prevent it from stealing focus.
+    await closeMainWindow();
 
-  TVCLogger.log("Symbol opened in TradingView.", { context });
+    // Check if TradingView is running and open it if it's not.
+    if (!isRunning) {
+      TVCLogger.log("TradingView is not running.", { context });
+      // try to activate again
+      const retryStatus = await launchAndFocusApplication();
+      if (!retryStatus) {
+        TVCLogger.error("TradingView did not launch in time.", { context });
+        TVCLogger.error(
+          "Please try opening the TradingView app manually, then trying again.",
+          { context }
+        );
+        showToast({
+          style: Toast.Style.Failure,
+          title: "TradingView did not launch in time.",
+          message:
+            "Please try opening the TradingView app manually, then trying again.",
+        });
+      }
+    }
+
+    // Then, activate the app and open the symbol.
+    TVCLogger.log("Running AppleScript...", { context });
+    await runAppleScript(`
+      set symbol to "${symbol}"
+      tell application "System Events"
+        if symbol starts with "B" or symbol starts with "b" then
+          key code 11
+          delay 0.01
+          keystroke text 2 thru -1 of symbol
+          keystroke return
+        else
+          keystroke symbol
+          keystroke return
+        end if
+      end tell`);
+    TVCLogger.log("AppleScript executed.", { context });
+
+    TVCLogger.log("Symbol opened in TradingView.", { context });
+  }
 }
 
 export default TVCOpenSymbol;
