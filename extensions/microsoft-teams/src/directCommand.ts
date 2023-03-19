373da71df4b5eefa71fbbb7c67ef863d524d28ad
Sven Wiegand <sven.wiegand@orgavision.com>
2023-03-19 19:30:17 +0100

    Added command icons and leave call command.

--- extensions/microsoft-teams/src/directCommand.ts
+++ extensions/microsoft-teams/src/directCommand.ts
@@ -3,13 +3,15 @@ import { showHUD } from "@raycast/api";
 
 export async function directCommand(
   action: MeetingAction,
-  hudTitle: (msg: UpdateMessage) => string,
+  hudTitle?: (msg: UpdateMessage) => string,
   hudErrorTitle = "No Meeting"
 ) {
   const client = await meetingClient();
   try {
     const state = await client.sendActionAndRequestMeetingState(action);
-    await showHUD(hudTitle(state));
+    if (hudTitle) {
+      await showHUD(hudTitle(state));
+    }
   } catch (error) {
     await showHUD(hudErrorTitle);
   }
