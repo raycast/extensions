4188ca304f3fe74f389d2da9010b133f1ee63850
Thomas Lombart <lombart.thomas@icloud.com>
2023-03-21 11:33:53 +0100

    Jira Beta: Revert sprint state changes

--- extensions/jira/src/api/sprints.ts
+++ extensions/jira/src/api/sprints.ts
@@ -5,14 +5,6 @@ type SprintResult = {
   displayName: string;
 };
 
-type DetailedSprint = {
-  id: string;
-  name: string;
-  state: "active" | "future" | "closed";
-  startDate: string;
-  endDate: string;
-};
-
 type GetSprintsParams = {
   fieldName: string;
   fieldValue: string;
@@ -23,32 +15,5 @@ export async function getSprints({ fieldName, fieldValue }: GetSprintsParams) {
     params: { fieldName, fieldValue },
   });
 
-  const sprints = response?.results || [];
-
-  const allSprints = await Promise.all(
-    sprints.map((sprint) => {
-      try {
-        return request<DetailedSprint>(`/sprint/${sprint.value}`, { useAgileApi: true });
-      } catch (error) {
-        return null;
-      }
-    })
-  );
-
-  const filteredSprints = allSprints.filter((sprint) => sprint !== null) as DetailedSprint[];
-
-  // show active sprints first, then future sprints, then closed sprints
-  filteredSprints.sort((sprintA, sprintB) => {
-    const stateOrder = { active: 0, future: 1, closed: 2 };
-
-    if (stateOrder[sprintA.state] !== stateOrder[sprintB.state]) {
-      return stateOrder[sprintA.state] - stateOrder[sprintB.state];
-    } else if (sprintA.startDate !== sprintB.startDate) {
-      return sprintA.startDate.localeCompare(sprintB.startDate);
-    } else {
-      return sprintA.name.localeCompare(sprintB.name);
-    }
-  });
-
-  return filteredSprints;
+  return response?.results;
 }
