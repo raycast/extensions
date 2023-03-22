b57891c3363df429207da392d27fe60992333d74
Abiel Zulio M <abielzm@gmail.com>
2023-03-23 05:26:42 +0700

    feat: support `gpt-4`

--- extensions/chatgpt/src/hooks/useModel.tsx
+++ extensions/chatgpt/src/hooks/useModel.tsx
@@ -17,7 +17,14 @@ export function useModel(): ModelHook {
   const [data, setData] = useState<Model[]>([]);
   const [isLoading, setLoading] = useState<boolean>(true);
 
-  const option: Model["option"][] = ["gpt-3.5-turbo", "gpt-3.5-turbo-0301"];
+  const option: Model["option"][] = [
+    "gpt-3.5-turbo",
+    "gpt-3.5-turbo-0301",
+    "gpt-4",
+    "gpt-4-0314",
+    "gpt-4-32k",
+    "gpt-4-32k-0314",
+  ];
 
   useEffect(() => {
     (async () => {
--- extensions/chatgpt/src/type.ts
+++ extensions/chatgpt/src/type.ts
@@ -33,7 +33,7 @@ export interface Model {
   created_at: string;
   name: string;
   prompt: string;
-  option: "gpt-3.5-turbo" | "gpt-3.5-turbo-0301" | string;
+  option: "gpt-3.5-turbo" | "gpt-3.5-turbo-0301" | "gpt-4" | "gpt-4-0314" | "gpt-4-32k" | "gpt-4-32k-0314" | string;
   temperature: number;
   pinned: boolean;
 }
