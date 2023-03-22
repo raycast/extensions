6ed1de50df8297b7a5d737108d3c04446ef25dea
Olga Kozh <olga.goi@gmail.com>
2023-03-22 07:07:52 +0100

    ensure raycast window is closed

--- extensions/google-chrome/src/new-window.tsx
+++ extensions/google-chrome/src/new-window.tsx
@@ -1,8 +1,9 @@
-import { showHUD } from "@raycast/api";
+import { closeMainWindow, showHUD } from "@raycast/api";
 import { createNewWindow } from "./actions";
 
 export default async function Command() {
   try {
+    await closeMainWindow();
     await createNewWindow();
   } catch {
     await showHUD("❌ Failed opening a new Google Chrome window");
