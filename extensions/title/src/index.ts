eab89e901f8f22145affee299c41d5d0911f2725
mrohmer <rohmer.matze@gmail.com>
2023-03-23 09:29:50 +0100

    Update title extension
    - fix: upscale screenshots
    - fix: run prettier

--- extensions/title/src/index.ts
+++ extensions/title/src/index.ts
@@ -1,16 +1,16 @@
-import {Clipboard, LaunchProps, showToast, Toast} from "@raycast/api";
-import title from 'title';
+import { Clipboard, LaunchProps, showToast, Toast } from "@raycast/api";
+import title from "title";
 
 interface TitleArguments {
-  text?: string
+  text?: string;
 }
 
-export default async function main(props: LaunchProps<Record<'arguments', TitleArguments>>) {
+export default async function main(props: LaunchProps<Record<"arguments", TitleArguments>>) {
   const text = await getText(props.arguments?.text);
 
   if (!text) {
     await showToast({
-      title: 'Input & Clipboard Text Is Empty',
+      title: "Input & Clipboard Text Is Empty",
       style: Toast.Style.Failure,
     });
     return;
@@ -20,14 +20,14 @@ export default async function main(props: LaunchProps<Record<'arguments', TitleA
   await Clipboard.copy(result);
   await showToast({
     title: result,
-    message: 'Copied to Clipboard',
+    message: "Copied to Clipboard",
     style: Toast.Style.Success,
   });
 }
 
-const getText = async (argument: string|undefined): Promise<string|undefined> => {
+const getText = async (argument: string | undefined): Promise<string | undefined> => {
   if (argument?.trim()) {
     return argument.trim();
   }
   return Clipboard.readText();
-}
+};
