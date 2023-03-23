114f5f0f910c0db7361a7e5d6af015a3b6fe8d54
Per Nielsen Tikær <per@raycast.com>
2023-03-23 22:11:34 +0100

    Update type.ts

--- extensions/chatgpt/src/type.ts
+++ extensions/chatgpt/src/type.ts
@@ -34,7 +34,7 @@ export interface Model {
   name: string;
   prompt: string;
   option: "gpt-3.5-turbo" | "gpt-3.5-turbo-0301" | "gpt-4" | "gpt-4-0314" | "gpt-4-32k" | "gpt-4-32k-0314" | string;
-  temperature: number;
+  temperature: string;
   pinned: boolean;
 }
 
