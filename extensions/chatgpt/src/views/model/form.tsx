a7478b55dac7eb92b55875c7e3a0ce80d06c471e
Per Nielsen Tikær <per@raycast.com>
2023-03-23 22:11:00 +0100

    Update form.tsx

--- extensions/chatgpt/src/views/model/form.tsx
+++ extensions/chatgpt/src/views/model/form.tsx
@@ -1,18 +1,17 @@
-import { Action, ActionPanel, Form, showToast, Toast, useNavigation } from "@raycast/api";
+import { Action, ActionPanel, Form, Icon, showToast, Toast, useNavigation } from "@raycast/api";
 import { useForm, FormValidation } from "@raycast/utils";
-import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { Model, ModelHook } from "../../type";
 
 export const ModelForm = (props: { model?: Model; use: { models: ModelHook }; name?: string }) => {
-  const { use } = props;
+  const { use, model } = props;
   const { pop } = useNavigation();
 
   const { handleSubmit, itemProps } = useForm<Model>({
     onSubmit: async (model) => {
       let updatedModel: Model = { ...model, updated_at: new Date().toISOString() };
       if (typeof updatedModel.temperature === "string") {
-        updatedModel = { ...updatedModel, temperature: Number(updatedModel.temperature) };
+        updatedModel = { ...updatedModel, temperature: updatedModel.temperature };
       }
       if (props.model) {
         const toast = await showToast({
@@ -27,7 +26,11 @@ export const ModelForm = (props: { model?: Model; use: { models: ModelHook }; na
           title: "Save your model...",
           style: Toast.Style.Animated,
         });
-        use.models.add({ ...updatedModel, id: data.id, created_at: data.created_at });
+        use.models.add({
+          ...updatedModel,
+          id: uuidv4(),
+          created_at: new Date().toISOString(),
+        });
         await showToast({
           title: "Model saved",
           style: Toast.Style.Animated,
@@ -38,31 +41,28 @@ export const ModelForm = (props: { model?: Model; use: { models: ModelHook }; na
     validation: {
       name: FormValidation.Required,
       temperature: (value) => {
-        if (value) {
-          if (value < 0) {
-            return "Minimal value is 0";
-          } else if (value > 2) {
-            return "Maximal value is 2";
+        if (value !== undefined && value !== null) {
+          const numValue = Number(value);
+          if (!isNaN(numValue)) {
+            if (numValue < 0) {
+              return "Minimal value is 0";
+            } else if (numValue > 2) {
+              return "Maximal value is 2";
+            }
           }
         } else {
           return FormValidation.Required;
         }
       },
     },
+    initialValues: {
+      name: model?.name ?? "",
+      temperature: model?.temperature.toString() ?? "1",
+      option: model?.option ?? "gpt-3.5-turbo",
+      prompt: model?.prompt ?? "",
+      pinned: model?.pinned ?? false,
+    },
   });
-
-  const [data, setData] = useState<Model>(
-    props?.model ?? {
-      id: uuidv4(),
-      updated_at: "",
-      created_at: new Date().toISOString(),
-      name: props?.name ?? "",
-      prompt: "",
-      option: "gpt-3.5-turbo",
-      temperature: 1,
-      pinned: false,
-    }
-  );
 
   const MODEL_OPTIONS = use.models.option;
 
@@ -70,53 +70,23 @@ export const ModelForm = (props: { model?: Model; use: { models: ModelHook }; na
     <Form
       actions={
         <ActionPanel>
-          <Action.SubmitForm title="Submit" onSubmit={handleSubmit} />
+          <Action.SubmitForm title="Submit" icon={Icon.SaveDocument} onSubmit={handleSubmit} />
         </ActionPanel>
       }
     >
-      <Form.TextField
-        title="Name"
-        placeholder="Name your model"
-        defaultValue={data.name}
-        onChange={(value) => setData({ ...data, name: value })}
-        {...itemProps.name}
-      />
-      <Form.TextArea
-        id="prompt"
-        title="Prompt"
-        placeholder="Describe your prompt"
-        defaultValue={data.prompt}
-        onChange={(value) => setData({ ...data, prompt: value })}
-      />
+      <Form.TextField title="Name" placeholder="Name your model" {...itemProps.name} />
+      <Form.TextArea title="Prompt" placeholder="Describe your prompt" {...itemProps.prompt} />
       <Form.TextField
         title="Temperature"
         placeholder="Set your sampling temperature (0 - 2)"
-        defaultValue={data.temperature.toLocaleString()}
-        onChange={(value) => {
-          setData({ ...data, temperature: Number(value) });
-        }}
         {...itemProps.temperature}
       />
-      <Form.Dropdown
-        id="option"
-        title="Model"
-        placeholder="Choose model option"
-        defaultValue={data.option}
-        onChange={(value) => setData({ ...data, option: value, updated_at: new Date().toISOString() })}
-      >
+      <Form.Dropdown title="Model" placeholder="Choose model option" {...itemProps.option}>
         {MODEL_OPTIONS.map((option) => (
           <Form.Dropdown.Item value={option} title={option} key={option} />
         ))}
       </Form.Dropdown>
-      {data.id !== "default" && (
-        <Form.Checkbox
-          id="pinned"
-          title="Pinned"
-          label="Pin model"
-          defaultValue={data.pinned}
-          onChange={(value) => setData({ ...data, pinned: value })}
-        />
-      )}
+      {model?.id !== "default" && <Form.Checkbox title="Pinned" label="Pin model" {...itemProps.pinned} />}
     </Form>
   );
 };
