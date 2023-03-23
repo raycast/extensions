5b9192655761127302d8e2d9bb5caa73b6de09eb
Abiel Zulio M <abielzm@gmail.com>
2023-03-23 21:28:35 +0700

    fix: initial `isLoading` state

--- extensions/chatgpt/src/hooks/useConversations.tsx
+++ extensions/chatgpt/src/hooks/useConversations.tsx
@@ -15,8 +15,8 @@ export function useConversations(): ConversationsHook {
 
       if (storedConversations) {
         setData((previous) => [...previous, ...JSON.parse(storedConversations)]);
-        setLoading(false);
       }
+      setLoading(false);
     })();
   }, []);
 
