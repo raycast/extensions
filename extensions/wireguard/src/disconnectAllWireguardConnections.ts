766b8ae9a153e46f43c04c093c30bd848f69d684
sbugzu <sbugzu_at_protonmail.com>
2023-03-24 19:54:47 +0800

    clear root search when close main window

--- extensions/wireguard/src/disconnectAllWireguardConnections.ts
+++ extensions/wireguard/src/disconnectAllWireguardConnections.ts
@@ -10,6 +10,6 @@ export default async () => {
       await disconnectVPNByName(VPNName);
     }
   } else {
-    await closeMainWindow();
+    await closeMainWindow({ clearRootSearch: true });
   }
 };
