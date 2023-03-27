30331b797766dcb85fbff77ab073f8b9555ecfe2
Pedro Duarte <oi@ped.ro>
2023-03-27 17:08:57 +0200

    Delete searchTracks.ts

--- extensions/spotify-player/src/api/searchTracks.ts
+++
@@ -1,7 +0,0 @@
-import { getSpotifyClient } from "../helpers/withSpotifyClient";
-
-export async function searchTracks(query: string, limit: number) {
-  const { spotifyClient } = getSpotifyClient();
-  const response = await spotifyClient.search(query, ["track"], { limit });
-  return response.tracks;
-}
