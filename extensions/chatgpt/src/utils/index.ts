f22fe0994206ba31005304cd943c94b30c548ef7
Abiel Zulio M <abielzm@gmail.com>
2023-04-03 13:11:41 +0700

    Revert "feat: handle overlimit token"
    This reverts commit 82c3a489dbe6febef99818730ccfc90abc0bd15a.

--- extensions/chatgpt/src/utils/index.ts
+++ extensions/chatgpt/src/utils/index.ts
@@ -1,38 +1,8 @@
 import { Chat, Message } from "../type";
 
-function countOpenAITokens(text: string): number {
-  // 100 tokens ~= 75 words
-  const words = text.split(" ").length;
-  const openAITokens = Math.ceil(words / 75) * 100;
-  return openAITokens;
-}
-
-function limitConversationLength(chats: Chat[]) {
-  // https://help.openai.com/en/articles/4936856-what-are-tokens-and-how-to-count-them
-  const maxTokens = 3750;
-  const newChats: Chat[] = [];
-  let tokens = 0;
-
-  for (const chat of chats) {
-    const questionTokens = countOpenAITokens(chat.question);
-    const answerTokens = countOpenAITokens(chat.answer);
-
-    tokens = tokens + questionTokens + answerTokens;
-
-    if (tokens > maxTokens) {
-      break;
-    }
-
-    newChats.push(chat);
-  }
-
-  return newChats;
-}
-
 export function chatTransfomer(chat: Chat[], prompt: string): Message[] {
   const messages: Message[] = [{ role: "system", content: prompt }];
-  const limitedChat = limitConversationLength(chat);
-  limitedChat.forEach(({ question, answer }) => {
+  chat.forEach(({ question, answer }) => {
     messages.push({ role: "user", content: question });
     messages.push({
       role: "assistant",
