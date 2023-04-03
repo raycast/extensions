3378b7f36aa146c5164dec89d8c1f65b2b5d270d
justspacedog <JustPod@googlemail.com>
2023-04-03 22:07:01 +0200

    initial commit

--- extensions/piratebay/src/categories.ts
+++ extensions/piratebay/src/categories.ts
@@ -1,206 +1,206 @@
 export const categories = [
-	{
-		value: '0',
-		title: 'All'
-	},
-	{
-		value: '100',
-		title: 'Audio'
-	},
-	{
-		value: '101',
-		title: '	Music'
-	},
-	{
-		value: '102',
-		title: '	Audio books'
-	},
-	{
-		value: '103',
-		title: '	Sound clips'
-	},
-	{
-		value: '104',
-		title: '	FLAC'
-	},
-	{
-		value: '199',
-		title: '	Other Audio'
-	},
-	{
-		value: '200',
-		title: 'Video'
-	},
-	{
-		value: '201',
-		title: '	Movies'
-	},
-	{
-		value: '202',
-		title: '	Movies DVDR'
-	},
-	{
-		value: '203',
-		title: '	Music videos'
-	},
-	{
-		value: '204',
-		title: '	Movie clips'
-	},
-	{
-		value: '205',
-		title: '	TV shows'
-	},
-	{
-		value: '206',
-		title: '	Handheld Videos'
-	},
-	{
-		value: '207',
-		title: '	HD - Movies'
-	},
-	{
-		value: '208',
-		title: '	HD - TV shows'
-	},
-	{
-		value: '209',
-		title: '	3D'
-	},
-	{
-		value: '299',
-		title: '	Other Movies'
-	},
-	{
-		value: '300',
-		title: 'Applications'
-	},
-	{
-		value: '301',
-		title: '	Windows'
-	},
-	{
-		value: '302',
-		title: '	Mac Apps'
-	},
-	{
-		value: '303',
-		title: '	UNIX'
-	},
-	{
-		value: '304',
-		title: '	Handheld Apps'
-	},
-	{
-		value: '305',
-		title: '	iOS (iPad/iPhone) Apps'
-	},
-	{
-		value: '306',
-		title: '	Android'
-	},
-	{
-		value: '399',
-		title: '	Other OS'
-	},
-	{
-		value: '400',
-		title: 'Games'
-	},
-	{
-		value: '401',
-		title: '	PC'
-	},
-	{
-		value: '402',
-		title: '	Mac Games'
-	},
-	{
-		value: '403',
-		title: '	PSx'
-	},
-	{
-		value: '404',
-		title: '	XBOX360'
-	},
-	{
-		value: '405',
-		title: '	Wii'
-	},
-	{
-		value: '406',
-		title: '	Handheld Games'
-	},
-	{
-		value: '407',
-		title: '	iOS (iPad/iPhone) Games'
-	},
-	{
-		value: '408',
-		title: '	Android'
-	},
-	{
-		value: '499',
-		title: '	Other Games'
-	},
-	{
-		value: '500',
-		title: 'Porn'
-	},
-	{
-		value: '501',
-		title: '	Movies (Porn)'
-	},
-	{
-		value: '502',
-		title: '	Movies DVDR (Porn)'
-	},
-	{
-		value: '503',
-		title: '	Pictures (Porn)'
-	},
-	{
-		value: '504',
-		title: '	Games (Porn)'
-	},
-	{
-		value: '505',
-		title: '	HD - Movies (Porn)'
-	},
-	{
-		value: '506',
-		title: '	Movie clips (Porn)'
-	},
-	{
-		value: '599',
-		title: '	Other Porn'
-	},
-	{
-		value: '600',
-		title: 'Other'
-	},
-	{
-		value: '601',
-		title: '	E-books'
-	},
-	{
-		value: '602',
-		title: '	Comics'
-	},
-	{
-		value: '603',
-		title: '	Pictures'
-	},
-	{
-		value: '604',
-		title: '	Covers'
-	},
-	{
-		value: '605',
-		title: '	Physibles'
-	},
-	{
-		value: '699',
-		title: '	Other Other'
-	},
+  {
+    value: "0",
+    title: "All",
+  },
+  {
+    value: "100",
+    title: "Audio",
+  },
+  {
+    value: "101",
+    title: "	Music",
+  },
+  {
+    value: "102",
+    title: "	Audio books",
+  },
+  {
+    value: "103",
+    title: "	Sound clips",
+  },
+  {
+    value: "104",
+    title: "	FLAC",
+  },
+  {
+    value: "199",
+    title: "	Other Audio",
+  },
+  {
+    value: "200",
+    title: "Video",
+  },
+  {
+    value: "201",
+    title: "	Movies",
+  },
+  {
+    value: "202",
+    title: "	Movies DVDR",
+  },
+  {
+    value: "203",
+    title: "	Music videos",
+  },
+  {
+    value: "204",
+    title: "	Movie clips",
+  },
+  {
+    value: "205",
+    title: "	TV shows",
+  },
+  {
+    value: "206",
+    title: "	Handheld Videos",
+  },
+  {
+    value: "207",
+    title: "	HD - Movies",
+  },
+  {
+    value: "208",
+    title: "	HD - TV shows",
+  },
+  {
+    value: "209",
+    title: "	3D",
+  },
+  {
+    value: "299",
+    title: "	Other Movies",
+  },
+  {
+    value: "300",
+    title: "Applications",
+  },
+  {
+    value: "301",
+    title: "	Windows",
+  },
+  {
+    value: "302",
+    title: "	Mac Apps",
+  },
+  {
+    value: "303",
+    title: "	UNIX",
+  },
+  {
+    value: "304",
+    title: "	Handheld Apps",
+  },
+  {
+    value: "305",
+    title: "	iOS (iPad/iPhone) Apps",
+  },
+  {
+    value: "306",
+    title: "	Android",
+  },
+  {
+    value: "399",
+    title: "	Other OS",
+  },
+  {
+    value: "400",
+    title: "Games",
+  },
+  {
+    value: "401",
+    title: "	PC",
+  },
+  {
+    value: "402",
+    title: "	Mac Games",
+  },
+  {
+    value: "403",
+    title: "	PSx",
+  },
+  {
+    value: "404",
+    title: "	XBOX360",
+  },
+  {
+    value: "405",
+    title: "	Wii",
+  },
+  {
+    value: "406",
+    title: "	Handheld Games",
+  },
+  {
+    value: "407",
+    title: "	iOS (iPad/iPhone) Games",
+  },
+  {
+    value: "408",
+    title: "	Android",
+  },
+  {
+    value: "499",
+    title: "	Other Games",
+  },
+  {
+    value: "500",
+    title: "Porn",
+  },
+  {
+    value: "501",
+    title: "	Movies (Porn)",
+  },
+  {
+    value: "502",
+    title: "	Movies DVDR (Porn)",
+  },
+  {
+    value: "503",
+    title: "	Pictures (Porn)",
+  },
+  {
+    value: "504",
+    title: "	Games (Porn)",
+  },
+  {
+    value: "505",
+    title: "	HD - Movies (Porn)",
+  },
+  {
+    value: "506",
+    title: "	Movie clips (Porn)",
+  },
+  {
+    value: "599",
+    title: "	Other Porn",
+  },
+  {
+    value: "600",
+    title: "Other",
+  },
+  {
+    value: "601",
+    title: "	E-books",
+  },
+  {
+    value: "602",
+    title: "	Comics",
+  },
+  {
+    value: "603",
+    title: "	Pictures",
+  },
+  {
+    value: "604",
+    title: "	Covers",
+  },
+  {
+    value: "605",
+    title: "	Physibles",
+  },
+  {
+    value: "699",
+    title: "	Other Other",
+  },
 ];
