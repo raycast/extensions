f3161fa2c7d48fd0836f0b8202381b6eb67c9784
Till Krüss <till@kruss.io>
2023-04-08 15:08:43 -0700

    Update wordpress-docs extension
    - apply lint
    - fix screenshot dimensions
    - import code
    - Initial commit

--- extensions/wordpress-docs/src/index.tsx
+++ extensions/wordpress-docs/src/index.tsx
@@ -1,27 +1,22 @@
-import { useState } from 'react'
-import { XMLParser } from 'fast-xml-parser'
-import { useFetch, Response } from '@raycast/utils'
-import { ActionPanel, Action, List, Icon } from '@raycast/api'
+import { useState } from "react";
+import { XMLParser } from "fast-xml-parser";
+import { useFetch, Response } from "@raycast/utils";
+import { ActionPanel, Action, List, Icon } from "@raycast/api";
 
 // TODO: create preference to select Browser?
 
 export default function Command() {
-  const [searchText, setSearchText] = useState('')
+  const [searchText, setSearchText] = useState("");
 
   if (searchText.length === 0) {
     // TODO: show dict.cc `ListWithEmptyView`
   }
 
-  const query = searchText.length === 0
-    ? 'foobar'
-    : searchText
+  const query = searchText.length === 0 ? "foobar" : searchText;
 
-  const { data, isLoading } = useFetch(
-    `https://developer.wordpress.org/search/${query}/feed/rss/`,
-    {
-      parseResponse: parseFetchResponse
-    }
-  )
+  const { data, isLoading } = useFetch(`https://developer.wordpress.org/search/${query}/feed/rss/`, {
+    parseResponse: parseFetchResponse,
+  });
 
   return (
     <List
@@ -36,7 +31,7 @@ export default function Command() {
         ))}
       </List.Section>
     </List>
-  )
+  );
 }
 
 function SearchListItem({ searchResult }: { searchResult: SearchResult }) {
@@ -53,59 +48,64 @@ function SearchListItem({ searchResult }: { searchResult: SearchResult }) {
         </ActionPanel>
       }
     />
-  )
+  );
 }
 
 async function parseFetchResponse(response: Response) {
-  const data = await response.text()
+  const data = await response.text();
 
-  if (! response.ok) {
-    throw new Error(response.statusText)
+  if (!response.ok) {
+    throw new Error(response.statusText);
   }
 
-  const xml = new XMLParser()
-  const result = xml.parse(data)
+  const xml = new XMLParser();
+  const result = xml.parse(data);
 
   if (response.redirected) {
-    const parts = response.url.split('/').filter(part => part).reverse()
+    const parts = response.url
+      .split("/")
+      .filter((part) => part)
+      .reverse();
 
-    return [{
-      name: parts[0],
-      description: '',
-      type: parts[1].slice(0, -1),
-      url: response.url,
-    } as SearchResult]
+    return [
+      {
+        name: parts[0],
+        description: "",
+        type: parts[1].slice(0, -1),
+        url: response.url,
+      } as SearchResult,
+    ];
   }
 
-  if (! result.rss || ! result.rss.channel || ! result.rss.channel.item) {
-    return null
+  if (!result.rss || !result.rss.channel || !result.rss.channel.item) {
+    return null;
   }
 
   return result.rss.channel.item.map((match: RssItem) => {
-    console.log(match)
-    const text = match.description.replace(/(<([^>]+)>)/gi, '')
-    const type = text.substring(0, text.indexOf(':')).trim()
-    const description = text.substring(text.indexOf(':') + 1).trim()
+    console.log(match);
+    const text = match.description.replace(/(<([^>]+)>)/gi, "");
+    const type = text.substring(0, text.indexOf(":")).trim();
+    const description = text.substring(text.indexOf(":") + 1).trim();
 
     return {
       name: match.title,
       description: description,
       type: type,
       url: match.link,
-    } as SearchResult
-  })
+    } as SearchResult;
+  });
 }
 
 interface SearchResult {
-  name: string
-  description: string
-  type: string
-  url: string
+  name: string;
+  description: string;
+  type: string;
+  url: string;
 }
 
 interface RssItem {
-  title: string
-  description: string
-  link: string
-  url: string
+  title: string;
+  description: string;
+  link: string;
+  url: string;
 }
