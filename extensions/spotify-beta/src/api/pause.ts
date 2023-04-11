57cd815f470d62ced58ee8ff135668489fd860c8
Pedro Duarte <oi@ped.ro>
2023-04-11 21:17:15 +0200

    Revert "Update pause.ts"
    This reverts commit 81cf1eb7fa6700dddb863b1dc669dd66cec9bab9.

--- extensions/spotify-beta/src/api/pause.ts
+++ extensions/spotify-beta/src/api/pause.ts
@@ -2,35 +2,18 @@ import { runAppleScript } from "run-applescript";
 import { buildScriptEnsuringSpotifyIsRunning } from "../helpers/applescript";
 import { getErrorMessage } from "../helpers/getError";
 import { getSpotifyClient } from "../helpers/withSpotifyClient";
-import { checkSpotifyApp } from "../helpers/isSpotifyInstalled";
 
 export async function pause() {
   const { spotifyClient } = getSpotifyClient();
-
-  async function checkIfSpotifyIsRunning() {
-    const response = await runAppleScript(`
-    if application "Spotify" is running then
-        return "Spotify is running"
-      else
-        return "Spotify is not running"
-      end if
-    `);
-    return response === "Spotify is running";
-  }
-
   try {
     await spotifyClient.putMePlayerPause();
   } catch (err) {
     const error = getErrorMessage(err);
 
     if (error?.toLocaleLowerCase().includes("restricted device")) {
-      const isSpotifyInstalled = await checkSpotifyApp();
-      const isSpotifyRunning = await checkIfSpotifyIsRunning();
-      if (isSpotifyInstalled && isSpotifyRunning) {
-        const script = buildScriptEnsuringSpotifyIsRunning("pause");
-        await runAppleScript(script);
-        return;
-      }
+      const script = buildScriptEnsuringSpotifyIsRunning("pause");
+      await runAppleScript(script);
+      return;
     }
     console.log("pause.ts Error: ", error);
     throw new Error(error);
