3caaf47cf3739190dca7fa869645de495eacac6c
billy <zygboom@163.com>
2023-04-12 18:58:42 +0800

    Update ray-so extension
    - update
    - 更新依赖
    - update
    - 新增打开文件夹功能
    - 终极版

--- extensions/ray-so/src/generate-image.tsx
+++ extensions/ray-so/src/generate-image.tsx
@@ -1,12 +1,18 @@
-import { getSelectedText, showToast, ToastStyle, showHUD, getPreferenceValues } from "@raycast/api";
+import { getSelectedText, showToast, Toast, showHUD, getPreferenceValues } from "@raycast/api";
 import { encodeURI } from "js-base64";
-import open from "open";
+import axios from "axios";
+import fs from "fs";
+import { getNowTime } from "./utils/date";
+import { exec } from "child_process";
 
 interface Preferences {
   theme: string;
   padding: number;
   darkMode: boolean;
   background: boolean;
+  raysoUrl: string;
+  SystemDirectory: string;
+  OpenDirectory: boolean;
 }
 
 export default async () => {
@@ -16,16 +22,44 @@ export default async () => {
   try {
     selectedText = await getSelectedText();
   } catch (e) {
-    await showHUD(
-      "❌ Screenshot generation failed. Please make sure you've selected the text you want to take a screenshot of."
-    );
+    await showHUD("截图生成失败。请确保您已选择要截屏的文本");
     return;
   }
 
   const base64Text = encodeURI(selectedText);
 
-  await showToast(ToastStyle.Animated, "Generating screenshot");
+  await showToast(Toast.Style.Animated, "Generating screenshot");
+  const url = preferences.raysoUrl;
+  const data = {
+    theme: preferences.theme,
+    background: preferences.background,
+    darkMode: preferences.darkMode,
+    padding: preferences.padding,
+    code: base64Text,
+  };
+  await axios
+    .post(url, data, {
+      responseType: "arraybuffer",
+    })
+    .then(async (res) => {
+      if (res.status === 200) {
+        const fileName = `rayso_${getNowTime()}.png`;
+      const filePath = `${preferences.SystemDirectory}/${fileName}`;
+      fs.writeFileSync(filePath, res.data);
 
-  const url = `https://ray.so/#theme=${preferences.theme}&background=${preferences.background}&darkMode=${preferences.darkMode}&padding=${preferences.padding}&code=${base64Text}`;
-  open(url);
+      if (preferences.OpenDirectory) {
+        const fileDir = preferences.SystemDirectory.replace(/ /g, "\\ ");
+        exec(`open ${fileDir}`);
+      }
+      await showToast(Toast.Style.Success, "截图生成成功");
+      await showHUD("截图生成成功");
+      } else {
+        await showToast(Toast.Style.Failure, res.data);
+        await showHUD(res.data);
+      }
+    })
+    .catch(async (e) => {
+      await showToast(Toast.Style.Failure, e);
+      await showHUD(e);
+    });
 };
---
+++ extensions/ray-so/src/utils/date.ts
@@ -0,0 +1,13 @@
+/**
+ * 获取当前时间，格式为 YYYY-MM-DD_HH-mm-ss
+ */
+export function getNowTime() {
+    const date = new Date();
+    const year = date.getFullYear().toString();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    const hour = date.getHours().toString().padStart(2, '0');
+    const minute = date.getMinutes().toString().padStart(2, '0');
+    const second = date.getSeconds().toString().padStart(2, '0');
+    return `${year}-${month}-${day}_${hour}-${minute}-${second}`;
+}
