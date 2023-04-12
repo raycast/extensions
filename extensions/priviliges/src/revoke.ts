9efdafc4c5bafd5df13a7889196f7d161a68dedc
ph00lt0 <15004290+ph00lt0@users.noreply.github.com>
2023-04-12 17:19:19 +0000

    import fix

--- extensions/priviliges/src/revoke.ts
+++ extensions/priviliges/src/revoke.ts
@@ -1,5 +1,5 @@
 import { showHUD } from "@raycast/api";
-const {exec} = require('child_process');
+import { exec } from "child_process";
 
 export default async function main() {
   exec('/Applications/Privileges.app/Contents/Resources/PrivilegesCLI --remove');
