96524f986b8186636de9e511fd8ec0c85b5d5c26
AndreasElia <andreaselia@live.co.uk>
2023-04-12 10:59:30 +0100

    code cleanup and selected text console error fix

--- extensions/wayback-machine/src/save.ts
+++ extensions/wayback-machine/src/save.ts
@@ -5,38 +5,45 @@ type WaybackArguments = {
   url: string;
 };
 
+const urlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/;
+
 export default async function main(props: LaunchProps<{ arguments: WaybackArguments }>) {
-  const { url } = props.arguments;
-  let selectedText: string | undefined;
-
-  try {
-    selectedText = await getSelectedText();
-  } catch (error) {
-    console.error(error);
-  }
-
-  const toast = await showToast({
-    style: Toast.Style.Animated,
-    title: "Saving to Wayback Machine",
-  });
-
-  const urlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/;
-  const webpageUrl: string | undefined = url || selectedText;
-  if (webpageUrl === undefined || !urlRegex.test(webpageUrl)) {
-    toast.style = Toast.Style.Failure;
-    toast.title = "No domain found";
+  if (props.arguments.url && urlRegex.test(props.arguments.url)) {
+    await savePage(props.arguments.url);
     return;
   }
 
   try {
+    const selectedText = await getSelectedText();
+
+    if (!urlRegex.test(selectedText)) {
+      await showToast({ style: Toast.Style.Failure, title: "No domain found" });
+      return;
+    }
+
+    await savePage(selectedText);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function savePage(webpageUrl: string) {
+  const toast = await showToast({ style: Toast.Style.Animated, title: "Saving to Wayback Machine" });
+
+  try {
     const res = await fetch(`https://web.archive.org/save/${webpageUrl}`);
+
     if (res.status >= 400) {
+      console.log(await res.text());
       toast.style = Toast.Style.Failure;
       toast.title = "Failed to save to Wayback Machine";
+      return;
     }
+
     toast.style = Toast.Style.Success;
     toast.title = "Saved to Wayback Machine";
   } catch (err) {
+    console.log(err);
     toast.style = Toast.Style.Failure;
     toast.title = "Failed to save to Wayback Machine";
   }
