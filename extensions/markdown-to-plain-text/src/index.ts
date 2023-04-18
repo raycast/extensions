511e4f7abc5c154abd8dc71738533b0a638d376f
john <john@digitalgd.com.cn>
2023-04-19 03:36:35 +0800

    chore: run prettier

--- extensions/markdown-to-plain-text/src/index.ts
+++ extensions/markdown-to-plain-text/src/index.ts
@@ -2,13 +2,12 @@ import { Clipboard } from "@raycast/api";
 import MarkdownIt from "markdown-it";
 import plainText from "markdown-it-plain-text";
 
-
 export default async function main() {
-  const { text } = await Clipboard.read()
+  const { text } = await Clipboard.read();
   const md = new MarkdownIt();
   md.use(plainText);
   md.render(text);
   // Here are plain text result
-  const result = (md as any).plainText
+  const result = (md as any).plainText;
   await Clipboard.paste(result);
 }
