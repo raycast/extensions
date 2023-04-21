66341454633daf25005ee602b5d91ed71eba660a
sevichecc <91365763+Sevichecc@users.noreply.github.com>
2023-04-21 22:54:36 +0800

    Update miniflux extension
    - feat: add input validation
    - docs: update Changelog
    - refactr: Add AdvanceOptions component
    - docs: Add README
    - fix: favicon invalid
    - style: lint the code
    - feat: add background refresh
    - feat: add background refresh
    - feat: add Open Miniflux Command
    - feat: add Actions to FeedInDetail
    - feat: add SaveToReadwise Command
    - fix: use latest entries/'s cache in search command
    - style: change the name of Command
    - feat: refresh all feeds
    - style: change the kaomoji
    - feat: add subscribe
    - refactor: cache category and icon
    - refactor: use getFavicon to replace getIconForFeed Api
    - feat: when fetch the original content, mark as read
    - style: lint code
    - feat: add kaomoji
    - feat: frendly toast
    - perf: cache after setIcon
    - refactor: remove isMounted check in useEntryIcon
    - feat: filter recent entries by categories
    - feat: filter recent entries by categories
    - refactor: change useCallback to useMemo
    - refactor: use default export to export api
    - perf: cache recent entries
    - feat: cache search result
    - chore: move pnpm to npm
    - docs: update icon
    - chore: update dependenies
    - docs: change the icon
    - feat: show how many search result has been found
    - feat: Add getCategories api
    - feat: add some icon and styling
    - refactor: api
    - refactor: api
    - refactor: api refactor
    - feat: update bookmark & add some toasts
    - refactor: add some hooks
    - feat: init read feeds function
    - refactor: load latest feed before search
    - feat: search in miniflux
    - init
    - Initial commit

--- extensions/miniflux/src/add-subscription.tsx
+++ extensions/miniflux/src/add-subscription.tsx
@@ -1,91 +1,88 @@
-import { Form, Action, ActionPanel, Icon, popToRoot, showToast, Toast } from "@raycast/api";
-import { useCategories } from "./utils/useCategories";
+import { Form, Action, ActionPanel, showToast, Toast } from "@raycast/api";
 import { useState, useCallback } from "react";
-import { DiscoverRequest, CreateFeedRequest, DiscoveredFeed, MinifluxApiError } from "./utils/types";
+import { DiscoveredFeed, MinifluxApiError } from "./utils/types";
 import { useErrorHandler } from "./utils/useErrorHandler";
+import { useCategories } from "./utils/useCategories";
 import apiServer from "./utils/api";
-import AdvanceOptions from "./components/AdvanceOptions";
+import CreateFeedForm from "./components/CreateFeedForm";
+
+type FromType = {
+  url: string;
+  category_id: string;
+};
 
 export default function AddSubscription() {
   const categories = useCategories();
-  const [enableAdvance, setEnableAdvance] = useState(false);
-  const [feeds, setFeeds] = useState<DiscoveredFeed[]>();
-  const [haveDiscovered, setHaveDiscovered] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
   const handleError = useErrorHandler();
 
-  const discoverFeeds = useCallback(async (url: DiscoverRequest) => {
-    setHaveDiscovered(true);
-    setIsLoading(true);
-    showToast(Toast.Style.Animated, "Finding Feeds...〜(＞＜)〜");
+  const [feeds, setFeeds] = useState<DiscoveredFeed[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [urlError, setUrlError] = useState<string | undefined>();
+  const [url, setUrl] = useState("");
+  const [categoryId, setCategoryId] = useState<string>("");
 
-    const results = await apiServer.discoverFeed(url);
+  const discoverFeeds = useCallback(
+    async ({ url, category_id }: FromType) => {
+      if (urlError) return;
+      try {
+        setIsLoading(true);
+        showToast(Toast.Style.Animated, "Finding Feeds...〜(＞＜)〜");
+        const results = await apiServer.discoverFeed({ url });
 
-    showToast(Toast.Style.Success, `${results.length} feeds have been found ＼(＾▽＾)／`);
-    setIsLoading(false);
-    setFeeds(results);
-  }, []);
-
-  const createFeeds = useCallback(async (values: CreateFeedRequest) => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { enableAdvance: _, ...remainingSettings } = values;
-    setIsLoading(true);
-    try {
-      showToast(Toast.Style.Animated, "Subscribing to the feed...__φ(．．;)");
-
-      await apiServer.createFeed({ ...remainingSettings, category_id: values.category_id * 1 });
-
-      setIsLoading(false);
-      showToast(Toast.Style.Animated, "Subscribed! ＼(≧▽≦)／");
-    } catch (error) {
-      handleError(error as MinifluxApiError);
-      setIsLoading(false);
-    } finally {
-      setTimeout(() => popToRoot(), 3000);
-    }
-  }, []);
-
-  if (haveDiscovered) {
-    return (
-      <Form
-        isLoading={isLoading}
-        actions={
-          <ActionPanel>
-            <Action.SubmitForm title="Subscribe" onSubmit={createFeeds} />
-          </ActionPanel>
+        if (results.length === 0) {
+          showToast(Toast.Style.Success, `no feeds found ＼(＾▽＾)／`);
+        } else {
+          showToast(Toast.Style.Success, `${results.length} feeds found ＼(＾▽＾)／`);
+          setFeeds(results);
         }
-      >
-        <Form.Dropdown title="Choose a Feed" id="feed_url">
-          {feeds?.map(({ url, title, type }) => (
-            <Form.Dropdown.Item key={title} value={url} title={`${url} (${type})`} icon={Icon.Livestream} />
-          ))}
-        </Form.Dropdown>
-        <Form.Dropdown id="category_id" title="Category" storeValue>
-          {categories.map(({ id, title }) => (
-            <Form.Dropdown.Item key={id} title={title} value={id.toString()} />
-          ))}
-        </Form.Dropdown>
-        <Form.Checkbox
-          id="enableAdvance"
-          label="Enable Advanced Options"
-          value={enableAdvance}
-          onChange={setEnableAdvance}
-          storeValue
-        />
-        {enableAdvance && <AdvanceOptions />}
-      </Form>
-    );
-  }
+
+        setIsLoading(false);
+        setCategoryId(category_id);
+      } catch (error) {
+        handleError(error as MinifluxApiError);
+        setIsLoading(false);
+      }
+    },
+    [urlError]
+  );
+
+  const handleUrl = () => {
+    if (url.length > 0) {
+      setUrlError(undefined);
+    } else {
+      setUrlError("The field should not be empty!");
+    }
+  };
 
   return (
-    <Form
-      actions={
-        <ActionPanel>
-          <Action.SubmitForm title="Find a Feed" onSubmit={discoverFeeds} />
-        </ActionPanel>
-      }
-    >
-      <Form.TextField id="url" title="URL" placeholder="https://domain.tld/" />
-    </Form>
+    <>
+      {feeds.length !== 0 ? (
+        <CreateFeedForm feeds={feeds} categoryId={categoryId} />
+      ) : (
+        <Form
+          isLoading={isLoading}
+          actions={
+            <ActionPanel>
+              <Action.SubmitForm title="Find a Feed" onSubmit={discoverFeeds} />
+            </ActionPanel>
+          }
+        >
+          <Form.TextField
+            id="url"
+            title="URL"
+            value={url}
+            placeholder="https://domain.tld/"
+            error={urlError}
+            onChange={setUrl}
+            onBlur={handleUrl}
+          />
+          <Form.Dropdown id="category_id" title="Category" storeValue>
+            {categories.map(({ id, title }) => (
+              <Form.Dropdown.Item key={id} title={title} value={id.toString()} />
+            ))}
+          </Form.Dropdown>
+        </Form>
+      )}
+    </>
   );
 }
