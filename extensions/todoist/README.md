049c1d2505f02a0cb2da3ddfc1e958533e84b423
Thomas Lombart <lombart.thomas@icloud.com>
2023-04-21 09:11:50 +0200

    Todoist: Revamp extension

--- extensions/todoist/README.md
+++ extensions/todoist/README.md
@@ -9,8 +9,3 @@ Bring Todoist to Raycast so that you can manage your tasks easily. No more conte
 Before using the command, you need to retrieve your Todoist token located in the [integration settings view](https://todoist.com/app/settings/integrations) under the section called **API token**.
 
 Then, copy it and paste it either into the extension's Welcome screen or in the extension's preferences under **Todoist Token**.
-
-## Limitations
-
-- Tasks in the "Today" and "Upcoming" views are not ordered the same as in your Todoist app because of a limitation from the [Todoist REST API](https://developer.todoist.com/rest/v1/#overview). The [Sync API](https://developer.todoist.com/sync/v8/) supports it but is not adapted for a Raycast extension.
-- It's not possible to get tasks only assigned to you when sharing projects because of a limitation from the Todoist REST API.
---
+++ extensions/todoist/src/components/TodayTasks.tsx
@@ -0,0 +1,44 @@
+import { isBefore, isSameDay } from "date-fns";
+import { useMemo } from "react";
+
+import { getToday } from "../helpers/dates";
+import { groupByDueDates } from "../helpers/groupBy";
+import { getTasksForTodayOrUpcomingView } from "../helpers/tasks";
+import { QuickLinkView } from "../home";
+import useCachedData from "../hooks/useCachedData";
+import useViewTasks from "../hooks/useViewTasks";
+
+import TaskListSections from "./TaskListSections";
+import TodayEmptyView from "./TodayEmptyView";
+
+type TodayTasksProps = { quickLinkView: QuickLinkView };
+
+export default function TodayTasks({ quickLinkView }: TodayTasksProps) {
+  const [data] = useCachedData();
+
+  const tasks = useMemo(() => {
+    if (!data) return [];
+
+    return getTasksForTodayOrUpcomingView(data.items, data.user.id).filter((t) => {
+      if (!t.due) return false;
+
+      return isBefore(new Date(t.due.date), getToday()) || isSameDay(new Date(t.due.date), getToday());
+    });
+  }, [data]);
+
+  let sections = [];
+
+  const {
+    sections: groupedSections,
+    viewProps,
+    sortedTasks,
+  } = useViewTasks("todoist.today", { tasks, data, optionsToExclude: ["date"] });
+
+  sections = viewProps.groupBy?.value === "default" ? groupByDueDates(sortedTasks) : groupedSections;
+
+  if (tasks.length === 0) {
+    return <TodayEmptyView quickLinkView={quickLinkView} />;
+  }
+
+  return <TaskListSections sections={sections} viewProps={viewProps} quickLinkView={quickLinkView} />;
+}
