4d4bac82b08630672e0d3bd4c12bcbe2cfe6f698
urwrstkn8mare <42769125+urwrstkn8mare@users.noreply.github.com>
2023-04-24 01:33:43 +0800

    give option to run commands in iTerm

--- extensions/brew/src/components/runInTerminal.ts
+++ extensions/brew/src/components/runInTerminal.ts
@@ -1,10 +1,16 @@
 import { closeMainWindow } from "@raycast/api";
 import { runAppleScript } from "run-applescript";
+import { preferences } from "../preferences";
 
 export function runCommandInTerminal(command: string): void {
   runAppleScript(`
-    tell application "Terminal"
-      do script "${command}"
+    tell application "${preferences.useIterm2 ? "iTerm" : "Terminal"}"
+      ${
+        preferences.useIterm2
+          ? "set newWindow to create window with default profile command"
+          : "do shell script \"open -a 'Terminal'\"\ndo script"
+      } "bash -c '${command}; read -n 1 -s -r -p \\"Press any key to exit - will not quit\\" ; echo' ; exit" in selected tab of the front window
+
     end tell
   `);
   closeMainWindow();
--- extensions/brew/src/preferences.ts
+++ extensions/brew/src/preferences.ts
@@ -3,4 +3,5 @@ import { getPreferenceValues } from "@raycast/api";
 export const preferences: {
   greedyUpgrades: boolean;
   customBrewPath?: string;
+  useIterm2: boolean;
 } = getPreferenceValues();
