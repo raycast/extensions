278d88469c20e14513f320307741eb7a2a2b9c3a
urwrstkn8mare <42769125+urwrstkn8mare@users.noreply.github.com>
2023-04-24 12:50:24 +0800

    Add option to uninstall casks with `--zap`

--- extensions/brew/src/preferences.ts
+++ extensions/brew/src/preferences.ts
@@ -3,4 +3,5 @@ import { getPreferenceValues } from "@raycast/api";
 export const preferences: {
   greedyUpgrades: boolean;
   customBrewPath?: string;
+  zapCask: boolean;
 } = getPreferenceValues();
