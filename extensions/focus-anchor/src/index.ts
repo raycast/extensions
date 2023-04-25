e75fd8e7dbe2a982233884e1114c4e3005244f57
Ashley Hindle <hey@ashleyhindle.com>
2023-04-25 16:28:11 +0100

    Prettier files

--- extensions/focus-anchor/src/index.ts
+++ extensions/focus-anchor/src/index.ts
@@ -4,10 +4,13 @@ interface FocusArguments {
   commitment: string;
 }
 
-const encode = (str: string):string => Buffer.from(str, 'binary').toString('base64');
+const encode = (str: string): string => Buffer.from(str, "binary").toString("base64");
 
 export default async function main(props: LaunchProps<{ arguments: FocusArguments }>) {
   const { commitment } = props.arguments;
 
-  await open("focusanchor://setCommitment/" + encode(commitment)).catch((error) => { console.error(error); showHUD("Focus Anchor must be installed"); });
+  await open("focusanchor://setCommitment/" + encode(commitment)).catch((error) => {
+    console.error(error);
+    showHUD("Focus Anchor must be installed");
+  });
 }
