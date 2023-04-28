8f6a1a03fe7d7f6d237d9d47997045e4733bb676
Lucas Mezêncio <lucas.mezencio@gmail.com>
2023-04-28 14:17:43 +0100

    Use `original_timezone` in order to be able to make date operations

--- extensions/world-clock/src/components/time-info-detail.tsx
+++ extensions/world-clock/src/components/time-info-detail.tsx
@@ -17,7 +17,7 @@ export function TimeInfoDetail(props: { timeInfo: TimeInfo; detailLoading: boole
           ? `<img src="${fileUrl(
               `${environment.assetsPath}/${
                 environment.theme === "light" ? "clock-icons" : "clock-icons@dark"
-              }/${new Date(timeInfo.datetime).getHours()}.svg`
+              }/${new Date(timeInfo.original_datetime).getHours()}.svg`
             )}" alt="${timeInfo.timezone}" height="190" />`
           : undefined
       }
