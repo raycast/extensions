419825236bdba8b58392027ba4a411ec83e6c244
Louis <huynguyennbk@gmail.com>
2023-04-29 19:10:10 +0700

    Update tmux-sessioner extension
    - rename tmux session
    - delete tmux session
    - create new session command
    - create new session command

--- extensions/tmux-sessioner/src/index.tsx
+++ extensions/tmux-sessioner/src/index.tsx
@@ -1,49 +1,16 @@
 import { useState, useEffect } from "react";
-import { List, Icon, Action, ActionPanel, Toast, showToast, Detail } from "@raycast/api";
-import { exec, execSync } from "child_process";
-import { LocalStorage, showHUD } from "@raycast/api";
+import { List, Icon, Action, ActionPanel, Toast, showToast, Detail, useNavigation } from "@raycast/api";
+import { LocalStorage } from "@raycast/api";
 import { SelectTerminalApp } from "./SelectTermnialApp";
-
-const env = Object.assign({}, process.env, { PATH: "/usr/local/bin:/usr/bin:/opt/homebrew/bin" });
-
-async function openTerminal() {
-  const localTerminalAppName = await LocalStorage.getItem<string>("terminalAppName");
-  execSync(`open -a ${localTerminalAppName}`);
-}
-
-async function switchToSession(session: string) {
-  const toast = await showToast({ style: Toast.Style.Animated, title: "Permission Checking" });
-
-  exec(`tmux switch -t ${session}`, { env }, async (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-
-      toast.style = Toast.Style.Failure;
-      toast.title = "No tmux client found 😢";
-      toast.message = error.message;
-
-      return;
-    }
-
-    try {
-      await openTerminal();
-
-      toast.style = Toast.Style.Success;
-      toast.title = `Switched to session ${session}`;
-      await showHUD(`Switched to session ${session}`);
-    } catch (e) {
-      toast.style = Toast.Style.Failure;
-      toast.title = "Terminal not supported 😢";
-    }
-    return;
-  });
-}
+import { deleteSession, getAllSession, switchToSession } from "./sessionUtils";
+import { RenameTmuxSession } from "./RenameTmuxSession";
 
 export default function Command() {
   const [sessions, setSessions] = useState<Array<string>>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isTerminalSetup, setIsTerminalSetup] = useState(false);
 
+  const { push } = useNavigation();
   async function checkTerminalSetup(): Promise<boolean> {
     const localTerminalAppName = await LocalStorage.getItem<string>("terminalAppName");
 
@@ -66,6 +33,24 @@ export default function Command() {
     }
   }
 
+  const setupListSesssions = () => {
+    getAllSession((error, stdout) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        setIsLoading(false);
+        return;
+      }
+
+      const lines = stdout.trim().split("\n");
+
+      if (lines?.length > 0) {
+        setSessions(lines);
+      }
+
+      setIsLoading(false);
+    });
+  };
+
   useEffect(() => {
     (async () => {
       setIsLoading(true);
@@ -85,21 +70,8 @@ export default function Command() {
     }
 
     // List down all tmux session
-    exec(`tmux list-sessions | awk '{print $1}' | sed 's/://'`, { env }, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`exec error: ${error}`);
-        setIsLoading(false);
-        return;
-      }
-
-      const lines = stdout.trim().split("\n");
-
-      if (lines?.length > 0) {
-        setSessions(lines);
-      }
-
-      setIsLoading(false);
-    });
+    setIsLoading(true);
+    setupListSesssions();
   }, [isTerminalSetup]);
 
   return (
@@ -112,7 +84,21 @@ export default function Command() {
             title={session}
             actions={
               <ActionPanel>
-                <Action title="Switch To Selected Session" onAction={() => switchToSession(session)} />
+                <Action title="Switch To Selected Session" onAction={() => switchToSession(session, setIsLoading)} />
+                <Action
+                  title="Delete This Session"
+                  onAction={() =>
+                    deleteSession(session, setIsLoading, () => setSessions(sessions.filter((s) => s !== session)))
+                  }
+                  shortcut={{ modifiers: ["cmd"], key: "d" }}
+                />
+                <Action
+                  title="Rename This Session"
+                  onAction={() => {
+                    push(<RenameTmuxSession session={session} callback={() => setupListSesssions()} />);
+                  }}
+                  shortcut={{ modifiers: ["cmd"], key: "r" }}
+                />
               </ActionPanel>
             }
           />
