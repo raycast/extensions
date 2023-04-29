ba413a97dcc7a743c3c29e0871a9c76fd70ea167
the-dream-machine <20072792+the-dream-machine@users.noreply.github.com>
2023-04-29 14:36:16 +0300

    Remove trailing forward-slash

--- extensions/saba/src/utils/logoUrl.ts
+++ extensions/saba/src/utils/logoUrl.ts
@@ -1,3 +1,3 @@
 import { environment } from "@raycast/api";
 
-export const logoUrl = `https://res.cloudinary.com/dxe761tbf/image/upload/v1/tv/${environment.theme}/`;
+export const logoUrl = `https://res.cloudinary.com/dxe761tbf/image/upload/v1/tv/${environment.theme}`;
