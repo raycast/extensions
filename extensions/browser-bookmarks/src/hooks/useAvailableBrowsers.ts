516142bd2452cf6572c20e317a8cbe9eda3a4cf0
mathieudutour <mathieu@dutour.me>
2023-04-29 07:17:20 +0200

    Browser Bookmarks: Open source the extension

---
+++ extensions/browser-bookmarks/src/hooks/useAvailableBrowsers.ts
@@ -0,0 +1,24 @@
+import { getApplications } from "@raycast/api";
+import { useCachedPromise } from "@raycast/utils";
+
+export const BROWSERS_BUNDLE_ID = {
+  brave: "com.brave.browser",
+  chrome: "com.google.chrome",
+  firefox: "org.mozilla.firefox",
+  safari: "com.apple.safari",
+};
+
+export const availableBrowsers = Object.values(BROWSERS_BUNDLE_ID);
+
+export default function useBrowsers() {
+  return useCachedPromise(async () => {
+    const apps = await getApplications();
+
+    return (
+      apps
+        // The default macOS browser's bundle ID is lowercased, so let's lowercase all bundleIds
+        .map((app) => ({ ...app, bundleId: app.bundleId?.toLowerCase() }))
+        .filter((app) => availableBrowsers.includes(app.bundleId?.toLowerCase() as string))
+    );
+  });
+}
