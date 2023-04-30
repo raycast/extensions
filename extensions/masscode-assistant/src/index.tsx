ed44e27de827376ce0c039492bab8db3436d5225
genericuser256 <scottadam23@gmail.com>
2023-04-30 08:06:07 -0400

    MassCode Assistant: Allow setting paste as the default action (#5820)
    Co-authored-by: Adam Scott <adam.scott@oncorps.io>

--- extensions/masscode-assistant/src/index.tsx
+++ extensions/masscode-assistant/src/index.tsx
@@ -1,9 +1,10 @@
-import { Action, ActionPanel, Icon, List, showToast, Toast, open, getPreferenceValues } from "@raycast/api";
+import { Icon, List, showToast, Toast, open, getPreferenceValues } from "@raycast/api";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import type { Snippet, State } from "./types";
+import { Preferences, Snippet, State } from "./types";
 import SnippetContent from "./components/snippet-content";
 import { storeLastCopied, getLastCopiedMap, clearUnusedSnippets, orderSnippets } from "./utils/localStorageHelper";
+import { ItemActions } from "./components/item-actions";
 
 export default function Command() {
   const [state, setState] = useState<State>({ snippets: [], isLoading: true });
@@ -11,9 +12,9 @@ export default function Command() {
   const handleAction = async function (snippet: Snippet) {
     await storeLastCopied(snippet);
 
-    const preferences = await getPreferenceValues();
+    const preferences = getPreferenceValues<Preferences>();
     const orderMap = await getLastCopiedMap();
-    const orderedSnippets = orderSnippets(state.snippets!, orderMap, preferences);
+    const orderedSnippets = orderSnippets(state.snippets || [], orderMap, preferences);
 
     setState((previous) => ({ ...previous, snippets: orderedSnippets }));
   };
@@ -24,7 +25,7 @@ export default function Command() {
         const { data } = await axios.get<Snippet[]>("http://localhost:3033/snippets/embed-folder");
         const snippets = data.filter((i) => !i.isDeleted);
 
-        const preferences = await getPreferenceValues();
+        const preferences = getPreferenceValues<Preferences>();
         const lastCopiedMap = await getLastCopiedMap();
         await clearUnusedSnippets(snippets, lastCopiedMap);
         const orderedSnippets = orderSnippets(snippets, lastCopiedMap, preferences);
@@ -63,7 +64,7 @@ export default function Command() {
     <List
       searchBarPlaceholder="Type to search snippets"
       isLoading={state.isLoading}
-      selectedItemId={state.snippets && state.snippets!.length > 0 ? state.snippets[0].id : "0"}
+      selectedItemId={state.snippets && state.snippets.length > 0 ? state.snippets[0].id : "0"}
       isShowingDetail
     >
       {state.snippets?.map((i) => {
@@ -76,25 +77,8 @@ export default function Command() {
             subtitle={i.content[0].language}
             icon={Icon.TextDocument}
             detail={<SnippetContent snippet={i} selectedFragment={0} />}
-            actions={
-              <ActionPanel title="Actions">
-                <ActionPanel.Section>
-                  <Action.CopyToClipboard
-                    content={i.content[0].value}
-                    onCopy={() => {
-                      handleAction(i);
-                    }}
-                  />
-                  <Action.Paste
-                    content={i.content[0].value}
-                    onPaste={() => {
-                      handleAction(i);
-                    }}
-                  />
-                </ActionPanel.Section>
-              </ActionPanel>
-            }
-          ></List.Item>
+            actions={<ItemActions snippet={i} handleAction={handleAction} />}
+          />
         );
       })}
     </List>
