2bd42f3751bf54a362272cadd84f52258de49a1e
j0 <joel@joellee.org>
2023-04-30 23:33:48 +0800

    fix: run prettier

--- extensions/french-verb-conjugation/src/index.tsx
+++ extensions/french-verb-conjugation/src/index.tsx
@@ -1,22 +1,35 @@
 import { Grid, Detail, Form, showToast, Toast, LaunchProps } from "@raycast/api";
-import conjugationFR from "conjugation-fr"
-
+import conjugationFR from "conjugation-fr";
 
 interface ConjugationArguments {
   verb: string;
   mode?: ModeInput;
   tense?: TenseInput;
-};
+}
 
-type ModeInput = "infinitive" | "conditional" | "subjunctive" | "imperative" | "participle"
-type TenseInput = "present" | "imperfect" | "future" | "simple-past" | "perfect-tense" | "pluperfect" | "anterior-past" | "anterior-future" |"conditional-past" |"subjunctive-past"| "subjunctive-pluperfect" | "imperative-present" | "imperative-past" |"present-participle" | "past-participle"
-
+type ModeInput = "infinitive" | "conditional" | "subjunctive" | "imperative" | "participle";
+type TenseInput =
+  | "present"
+  | "imperfect"
+  | "future"
+  | "simple-past"
+  | "perfect-tense"
+  | "pluperfect"
+  | "anterior-past"
+  | "anterior-future"
+  | "conditional-past"
+  | "subjunctive-past"
+  | "subjunctive-pluperfect"
+  | "imperative-present"
+  | "imperative-past"
+  | "present-participle"
+  | "past-participle";
 
 function generateTenseTable(args: ConjugationArguments) {
-  const {verb, mode, tense} = args
-  let tenseTable = ''
+  const { verb, mode, tense } = args;
+  let tenseTable = "";
   try {
-    const body = conjugationFR.conjugate(verb, mode ?? '', tense ?? '')
+    const body = conjugationFR.conjugate(verb, mode ?? "", tense ?? "");
     tenseTable = `| Pronoun | Verb  |
 | ------- | ----- |
 | ${body[0].pronoun} | ${body[0].verb}|
@@ -26,27 +39,22 @@ function generateTenseTable(args: ConjugationArguments) {
 | ${body[4].pronoun} | ${body[4].verb}|
 | ${body[5].pronoun} | ${body[5].verb}|`;
   } catch (error) {
-      if (error instanceof Error) {
-     showToast({
-       style: Toast.Style.Failure,
-       title: "Something went wrong",
-       message: error.message,
-     });
-          return error.message
-      } else {
-          return 'unknown error has occurred'
-
-      }
-
-
+    if (error instanceof Error) {
+      showToast({
+        style: Toast.Style.Failure,
+        title: "Something went wrong",
+        message: error.message,
+      });
+      return error.message;
+    } else {
+      return "unknown error has occurred";
+    }
   }
-    return tenseTable
-
+  return tenseTable;
 }
 
-export default function Command(props: LaunchProps<{ arguments: ConjugationArguments}>) {
+export default function Command(props: LaunchProps<{ arguments: ConjugationArguments }>) {
   const markdown = generateTenseTable(props.arguments);
 
   return <Detail markdown={markdown} />;
-
 }
