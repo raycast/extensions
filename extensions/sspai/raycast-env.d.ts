266fefb171192e6a11eff26ca98dd511e9274d7e
sgaxun <sgaxun@gmail.com>
2023-04-30 10:55:09 +0800

    - Change the screenshot resolution. - Change the Command's title. - run prettier

--- extensions/sspai/raycast-env.d.ts
+++ extensions/sspai/raycast-env.d.ts
@@ -7,17 +7,17 @@
 
 /* eslint-disable @typescript-eslint/ban-types */
 
-type ExtensionPreferences = {}
+type ExtensionPreferences = {};
 
 /** Preferences accessible in all the extension's commands */
-declare type Preferences = ExtensionPreferences
+declare type Preferences = ExtensionPreferences;
 
 declare namespace Preferences {
   /** Preferences accessible in the `index` command */
-  export type Index = ExtensionPreferences & {}
+  export type Index = ExtensionPreferences & {};
 }
 
 declare namespace Arguments {
   /** Arguments passed to the `index` command */
-  export type Index = {}
+  export type Index = {};
 }
