f1c45187fc127585eb912a9fd966eb7ec6088b58
Per Nielsen Tikær <per@raycast.com>
2023-05-01 18:04:03 +0200

    Update exit.tsx

--- extensions/tailscale/src/exit.tsx
+++ extensions/tailscale/src/exit.tsx
@@ -67,7 +67,7 @@ function ExitNodeList() {
           title="Turn off exit node"
           actions={
             <ActionPanel>
-              <Action title="Turn Off Exit Node" onAction={() => setExitNode("", false)} />
+              <Action title="Turn off Exit Node" onAction={() => setExitNode("", false)} />
             </ActionPanel>
           }
         />
