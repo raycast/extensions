b091f7233d6fbfcc6fe100fb775d783b5defaf36
Willian Ferreira <willianhf15@gmail.com>
2023-05-02 11:31:18 +0100

    Updates Brasileirão Série A extension (#6274)
    * Add Brasileirão Série A extension
    
    * Add Brasileirão Série A extension
    
    * Fix linting
    
    * Update CHANGELOG.md
    
    * Update CHANGELOG.md
    
    * Update package.json
    
    * wip improvements
    
    * fix(standings): displayed team name
    
    * fix: lock file wrong npm version
    
    * Changelog entry
    
    ---------
    
    Co-authored-by: Per Nielsen Tikær <per@raycast.com>
    Co-authored-by: Willian Ferreira <willian.ferreira@rank.com>

--- extensions/brasileirao-serie-a/src/hooks/index.ts
+++ extensions/brasileirao-serie-a/src/hooks/index.ts
@@ -2,3 +2,4 @@ export { useMatches } from "./useMatches";
 export { useTeams } from "./useTeams";
 export { useSelectedTeam } from "./useSelectedTeam";
 export { useTable } from "./useTable";
+export { useSeasons } from "./useSeasons";
