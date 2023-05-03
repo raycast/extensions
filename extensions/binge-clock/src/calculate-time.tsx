012657ed8cacdbb19f93c9f67ccb069dc08e5a18
Aayush <aayushpokharel36@gmail.com>
2023-05-03 13:06:34 -0400

    Update calculate-time.tsx

--- extensions/binge-clock/src/calculate-time.tsx
+++ extensions/binge-clock/src/calculate-time.tsx
@@ -5,10 +5,13 @@ import { SearchResult } from "./interface/search-result";
 import { SearchResultListItem } from "./components/SearchResultListItem";
 
 export default function Command() {
+  const [searchTerm, setSearchTerm] = useState("");
+
   const [isLoading, setLoading] = useState(false);
   const [searchResults, setSearchResults] = useState<SearchResult[]>([]);
 
   function onSearchTextChange(searchTerm: string) {
+    setSearchTerm(searchTerm);
     if (searchTerm.length === 0) {
       setSearchResults([]);
       return;
@@ -25,15 +28,19 @@ export default function Command() {
   return (
     <List
       isLoading={isLoading}
-      searchBarPlaceholder="Search for torrent..."
+      searchBarPlaceholder="Search for movie / tv show name"
       throttle
       onSearchTextChange={onSearchTextChange}
     >
-      <List.Section title="Results" subtitle={searchResults.length + ""}>
-        {searchResults.map((result: SearchResult) => (
-          <SearchResultListItem key={result.url} result={result} />
-        ))}
-      </List.Section>
+      {searchTerm === "" && searchResults.length === 0 ? (
+        <List.EmptyView title="Search for movie / tv show name" description="Minimum of 3 characters" />
+      ) : (
+        <List.Section title="Results" subtitle={searchResults.length + ""}>
+          {searchResults.map((result: SearchResult) => (
+            <SearchResultListItem key={result.url} result={result} />
+          ))}
+        </List.Section>
+      )}
     </List>
   );
 }
