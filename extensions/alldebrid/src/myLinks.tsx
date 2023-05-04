dab4d1b5e31f9df93d31b5de94e0246bb69404ec
Thomas Deconinck <thomas.deconinck@colisweb.com>
2023-05-04 10:01:54 +0200

    get rid of ts-ignore

--- extensions/alldebrid/src/myLinks.tsx
+++ extensions/alldebrid/src/myLinks.tsx
@@ -1,5 +1,5 @@
 import { Action, ActionPanel, List, Toast, open, showToast } from "@raycast/api";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { AsyncData, Result } from "@swan-io/boxed";
 import { formatBytes } from "./utils/helpers";
 import { Link, debridUrl, deleteSavedLink, getSavedLinks } from "./utils/api";
@@ -48,51 +48,54 @@ export default function Command() {
         Loading: () => <List.EmptyView />,
         Done: (results) => {
           return results.match({
-            // @ts-ignore
             Ok: (links) => {
-              return links.map((link) => {
-                return (
-                  <List.Item
-                    key={link.filename}
-                    title={link.filename}
-                    detail={
-                      <List.Item.Detail
-                        metadata={
-                          <List.Item.Detail.Metadata>
-                            <List.Item.Detail.Metadata.Label title="Filename" text={link.filename} />
-                            <List.Item.Detail.Metadata.Separator />
-                            <List.Item.Detail.Metadata.Label title="Host" text={link.host} />
-                            <List.Item.Detail.Metadata.Separator />
-                            <List.Item.Detail.Metadata.Label
-                              title="Date"
-                              text={new Date(link.date * 1000).toDateString()}
+              return (
+                <Fragment>
+                  {links.map((link) => {
+                    return (
+                      <List.Item
+                        key={link.filename}
+                        title={link.filename}
+                        detail={
+                          <List.Item.Detail
+                            metadata={
+                              <List.Item.Detail.Metadata>
+                                <List.Item.Detail.Metadata.Label title="Filename" text={link.filename} />
+                                <List.Item.Detail.Metadata.Separator />
+                                <List.Item.Detail.Metadata.Label title="Host" text={link.host} />
+                                <List.Item.Detail.Metadata.Separator />
+                                <List.Item.Detail.Metadata.Label
+                                  title="Date"
+                                  text={new Date(link.date * 1000).toDateString()}
+                                />
+                                <List.Item.Detail.Metadata.Separator />
+                                <List.Item.Detail.Metadata.Label title="Size" text={`${formatBytes(link.size)}`} />
+                              </List.Item.Detail.Metadata>
+                            }
+                          />
+                        }
+                        actions={
+                          <ActionPanel>
+                            <Action.OpenInBrowser url={link.link} />
+                            <Action
+                              title="Download"
+                              onAction={() => {
+                                downloadLink(link.link);
+                              }}
                             />
-                            <List.Item.Detail.Metadata.Separator />
-                            <List.Item.Detail.Metadata.Label title="Size" text={`${formatBytes(link.size)}`} />
-                          </List.Item.Detail.Metadata>
+                            <Action
+                              title="Delete This Link"
+                              onAction={() => {
+                                deleteLink(link.link);
+                              }}
+                            />
+                          </ActionPanel>
                         }
                       />
-                    }
-                    actions={
-                      <ActionPanel>
-                        <Action.OpenInBrowser url={link.link} />
-                        <Action
-                          title="Download"
-                          onAction={() => {
-                            downloadLink(link.link);
-                          }}
-                        />
-                        <Action
-                          title="Delete This Link"
-                          onAction={() => {
-                            deleteLink(link.link);
-                          }}
-                        />
-                      </ActionPanel>
-                    }
-                  />
-                );
-              });
+                    );
+                  })}
+                </Fragment>
+              );
             },
             Error: () => {
               return <List.EmptyView title="An error occured while loading links" />;
--- extensions/alldebrid/src/myMagnets.tsx
+++ extensions/alldebrid/src/myMagnets.tsx
@@ -1,5 +1,5 @@
 import { Action, ActionPanel, List, Toast, open, showToast } from "@raycast/api";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { AsyncData, Result } from "@swan-io/boxed";
 import { formatBytes } from "./utils/helpers";
 import { Magnet, deleteSavedMagnet, getSavedMagnets, saveLink } from "./utils/api";
@@ -64,57 +64,60 @@ export default function Command() {
         Loading: () => <List.EmptyView />,
         Done: (results) => {
           return results.match({
-            // @ts-ignore
             Ok: (magnets) => {
-              return magnets.map((magnet) => {
-                return (
-                  <List.Item
-                    key={magnet.filename}
-                    title={magnet.filename}
-                    detail={
-                      <List.Item.Detail
-                        metadata={
-                          <List.Item.Detail.Metadata>
-                            <List.Item.Detail.Metadata.Label title="Filename" text={magnet.filename} />
-                            <List.Item.Detail.Metadata.Separator />
-                            <List.Item.Detail.Metadata.Label title="Size" text={`${formatBytes(magnet.size)}`} />
-                          </List.Item.Detail.Metadata>
+              return (
+                <Fragment>
+                  {magnets.map((magnet) => {
+                    return (
+                      <List.Item
+                        key={magnet.filename}
+                        title={magnet.filename}
+                        detail={
+                          <List.Item.Detail
+                            metadata={
+                              <List.Item.Detail.Metadata>
+                                <List.Item.Detail.Metadata.Label title="Filename" text={magnet.filename} />
+                                <List.Item.Detail.Metadata.Separator />
+                                <List.Item.Detail.Metadata.Label title="Size" text={`${formatBytes(magnet.size)}`} />
+                              </List.Item.Detail.Metadata>
+                            }
+                          />
+                        }
+                        actions={
+                          <ActionPanel>
+                            {magnet.links.length > 0 ? (
+                              <>
+                                <Action.OpenInBrowser url={magnet.links[0].link} />
+                                <Action
+                                  title={"Save To My Links Folder"}
+                                  onAction={() => {
+                                    saveMagnetLink(magnet.links[0].link);
+                                  }}
+                                />
+                                <Action
+                                  title={"Download"}
+                                  onAction={() => {
+                                    downloadMagnetLink(magnet.links[0].link);
+                                  }}
+                                />
+                              </>
+                            ) : null}
+
+                            <ActionPanel.Section>
+                              <Action
+                                title="Delete This Magnet"
+                                onAction={() => {
+                                  deleteMagnet(magnet.id.toString());
+                                }}
+                              />
+                            </ActionPanel.Section>
+                          </ActionPanel>
                         }
                       />
-                    }
-                    actions={
-                      <ActionPanel>
-                        {magnet.links.length > 0 ? (
-                          <>
-                            <Action.OpenInBrowser url={magnet.links[0].link} />
-                            <Action
-                              title={"Save To My Links Folder"}
-                              onAction={() => {
-                                saveMagnetLink(magnet.links[0].link);
-                              }}
-                            />
-                            <Action
-                              title={"Download"}
-                              onAction={() => {
-                                downloadMagnetLink(magnet.links[0].link);
-                              }}
-                            />
-                          </>
-                        ) : null}
-
-                        <ActionPanel.Section>
-                          <Action
-                            title="Delete This Magnet"
-                            onAction={() => {
-                              deleteMagnet(magnet.id.toString());
-                            }}
-                          />
-                        </ActionPanel.Section>
-                      </ActionPanel>
-                    }
-                  />
-                );
-              });
+                    );
+                  })}
+                </Fragment>
+              );
             },
             Error: () => {
               return <List.EmptyView title="An error occured while loading links" />;
