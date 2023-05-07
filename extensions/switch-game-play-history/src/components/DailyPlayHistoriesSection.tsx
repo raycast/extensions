172f190922688a66d8f356f2145840e95efaa63d
daikaiyu <1007167752@qq.com>
2023-05-07 13:02:49 +0800

    feat: add colorful time bar

--- extensions/switch-game-play-history/src/components/DailyPlayHistoriesSection.tsx
+++ extensions/switch-game-play-history/src/components/DailyPlayHistoriesSection.tsx
@@ -1,22 +1,33 @@
-import { List } from "@raycast/api";
+import { Color, List } from "@raycast/api";
 import { IRecentPlayHistory } from "../types/nintendo";
 import dayjs from "dayjs";
 
-export const DailyPlayHistoriesSection = ({ history }: { history: IRecentPlayHistory }) => {
+export const DailyPlayHistoriesSection = ({
+  history,
+  timeBarMaxValue,
+}: {
+  history: IRecentPlayHistory;
+  timeBarMaxValue: number;
+}) => {
   const date = dayjs(history.playedDate).format("YYYY-MM-DD");
   const totalPlayedMinutes = history.dailyPlayHistories.reduce((acc, cur) => acc + cur.totalPlayedMinutes, 0);
   const sectionSubtitle = `${history.dailyPlayHistories.length} Games · ${totalPlayedMinutes} Minutes`;
   return (
     <List.Section title={date} subtitle={sectionSubtitle}>
       {history.dailyPlayHistories.map((item) => {
-        return (
-          <List.Item
-            key={item.titleId}
-            title={item.titleName}
-            icon={item.imageUrl}
-            accessories={[{ tag: item.totalPlayedMinutes + " mins" }]}
-          />
-        );
+        const timeBar = {
+          tag: {
+            value: [
+              "\u200B",
+              "\u3000".repeat(Math.floor((item.totalPlayedMinutes / timeBarMaxValue) * 10)),
+              item.totalPlayedMinutes + " minutes",
+              "\u200B",
+            ].join(""),
+            color:
+              item.totalPlayedMinutes < 60 ? Color.Green : item.totalPlayedMinutes < 120 ? Color.Orange : Color.Red,
+          },
+        };
+        return <List.Item key={item.titleId} title={item.titleName} icon={item.imageUrl} accessories={[timeBar]} />;
       })}
     </List.Section>
   );
--- extensions/switch-game-play-history/src/components/RecentPlayHistoriesList.tsx
+++ extensions/switch-game-play-history/src/components/RecentPlayHistoriesList.tsx
@@ -5,11 +5,19 @@ import { SessionTokenGuard } from "./SessionTokenGuard";
 
 export const RecentPlayHistoriesList = () => {
   const history = usePlayHistories();
+  const longestPlayTimeThisWeek = history.data?.recentPlayHistories.reduce(
+    (acc, cur) =>
+      Math.max(
+        acc,
+        cur.dailyPlayHistories.reduce((acc, cur) => Math.max(acc, cur.totalPlayedMinutes), 0)
+      ),
+    0
+  );
   return (
     <List navigationTitle="Recent Play Histories" isLoading={history.isLoading}>
       <SessionTokenGuard type="list">
         {history.data?.recentPlayHistories.map((history) => (
-          <DailyPlayHistoriesSection history={history} />
+          <DailyPlayHistoriesSection history={history} timeBarMaxValue={longestPlayTimeThisWeek} />
         ))}
       </SessionTokenGuard>
     </List>
