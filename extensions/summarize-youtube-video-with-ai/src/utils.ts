25c87983ad563833f3badd0b92df27194fa5aecb
Kim Schneider <web@schneider.kim>
2023-05-10 18:45:29 +0200

    create utils file

---
+++ extensions/summarize-youtube-video-with-ai/src/utils.ts
@@ -0,0 +1,15 @@
+export const summarizeTranscription = (transcript: string, max_chars: number) =>
+  transcript?.split(/(?<=\.)/).reduce(
+    (acc, curr) => {
+      if (acc[acc.length - 1].length + curr.length > max_chars) {
+        const splitTranscription = curr.match(new RegExp(`.{1,${max_chars}}`, "g"));
+        splitTranscription?.forEach((split) => {
+          acc.push(split);
+        });
+      } else {
+        acc[acc.length - 1] += curr;
+      }
+      return acc;
+    },
+    [""]
+  );
