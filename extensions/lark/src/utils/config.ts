f2e1d9e3d7c331ef8b1112b9d977ba26f5945efd
Congling XIA <xiacongling@126.com>
2023-05-10 12:03:32 +0800

    feat: support custom lark domain

--- extensions/lark/src/utils/config.ts
+++ extensions/lark/src/utils/config.ts
@@ -1,7 +1,7 @@
 import { getPreferenceValues } from '@raycast/api';
 
 export interface Preference {
-  type: 'feishu' | 'lark';
+  domain: string;
   recentListCount: number;
 }
 
@@ -13,7 +13,7 @@ export function getPreference(): Preference {
 
 export const preference = getPreference();
 
-export const DOMAIN = preference.type === 'feishu' ? 'feishu.cn' : 'larksuite.com';
+export const DOMAIN = preference.domain.trim();
 
 export function getDomain(sub?: string): string {
   return `https://${sub ? `${sub}.` : ''}${DOMAIN}`;
