b74895b69833bad9ebaa44823cb4c9d0e5ecaa74
Thomas Lombart <lombart.thomas@icloud.com>
2023-05-12 11:50:30 +0200

    Todoist: Revamp extension (#6498)

--- extensions/todoist/src/create-task.tsx
+++ extensions/todoist/src/create-task.tsx
@@ -1,14 +1,19 @@
-import { AddTaskArgs, getColorByKey, Task } from "@doist/todoist-api-typescript";
 import { ActionPanel, Form, Icon, useNavigation, open, Toast, Action, Color } from "@raycast/api";
-import { FormValidation, MutatePromise, useCachedPromise, useForm } from "@raycast/utils";
+import { FormValidation, useForm } from "@raycast/utils";
 
-import { handleError, todoist } from "./api";
+import { addComment, addTask, AddTaskArgs, handleError, uploadFile } from "./api";
+import RefreshAction from "./components/RefreshAction";
 import TaskDetail from "./components/TaskDetail";
 import View from "./components/View";
-import { priorities } from "./constants";
+import { getCollaboratorIcon, getProjectCollaborators } from "./helpers/collaborators";
+import { getColorByKey } from "./helpers/colors";
 import { getAPIDate } from "./helpers/dates";
 import { isTodoistInstalled } from "./helpers/isTodoistInstalled";
+import { priorities } from "./helpers/priorities";
+import { getPriorityIcon } from "./helpers/priorities";
 import { getProjectIcon } from "./helpers/projects";
+import { getTaskAppUrl, getTaskUrl } from "./helpers/tasks";
+import useSyncData from "./hooks/useSyncData";
 
 type CreateTaskValues = {
   content: string;
@@ -18,45 +23,29 @@ type CreateTaskValues = {
   projectId: string;
   sectionId: string;
   labels: string[];
+  parentId: string;
+  responsibleUid: string;
+  files: string[];
 };
 
 type CreateTaskProps = {
+  fromLabel?: string;
   fromProjectId?: string;
-  mutateTasks?: MutatePromise<Task[] | undefined>;
+  fromTodayEmptyView?: boolean;
   draftValues?: CreateTaskValues;
 };
 
-export default function CreateTask({ fromProjectId, mutateTasks, draftValues }: CreateTaskProps) {
+function CreateTask({ fromProjectId, fromLabel, fromTodayEmptyView, draftValues }: CreateTaskProps) {
   const { push, pop } = useNavigation();
 
-  const {
-    data: projects,
-    isLoading: isLoadingProjects,
-    error: getProjectsError,
-  } = useCachedPromise(() => todoist.getProjects());
+  const { data, setData, isLoading, error } = useSyncData();
 
-  const {
-    data: sections,
-    isLoading: isLoadingSections,
-    error: getSectionsError,
-  } = useCachedPromise(() => todoist.getSections());
+  const labels = data?.labels;
+  const projects = data?.projects;
+  const sections = data?.sections;
 
-  const {
-    data: labels,
-    isLoading: isLoadingLabels,
-    error: getLabelsError,
-  } = useCachedPromise(() => todoist.getLabels());
-
-  if (getProjectsError) {
-    handleError({ error: getProjectsError, title: "Unable to get projects" });
-  }
-
-  if (getSectionsError) {
-    handleError({ error: getSectionsError, title: "Unable to get sections" });
-  }
-
-  if (getLabelsError) {
-    handleError({ error: getLabelsError, title: "Unable to get labels" });
+  if (error) {
+    handleError({ error, title: "Unable to get Todoist data" });
   }
 
   const lowestPriority = priorities[priorities.length - 1];
@@ -66,7 +55,7 @@ export default function CreateTask({ fromProjectId, mutateTasks, draftValues }: 
       const body: AddTaskArgs = { content: values.content, description: values.description };
 
       if (values.dueDate) {
-        body.dueDate = getAPIDate(values.dueDate);
+        body.due = { date: getAPIDate(values.dueDate) };
       }
 
       if (values.priority) {
@@ -74,41 +63,60 @@ export default function CreateTask({ fromProjectId, mutateTasks, draftValues }: 
       }
 
       if (values.projectId) {
-        body.projectId = values.projectId;
+        body.project_id = values.projectId;
       }
 
       if (values.sectionId) {
-        body.sectionId = values.sectionId;
+        body.section_id = values.sectionId;
+      }
+      if (values.responsibleUid) {
+        body.responsible_uid = values.responsibleUid;
       }
 
       if (values.labels && values.labels.length > 0) {
         body.labels = values.labels;
       }
 
+      if (values.parentId) {
+        body.parent_id = values.parentId;
+      }
+
       const toast = new Toast({ style: Toast.Style.Animated, title: "Creating task" });
       await toast.show();
 
       try {
-        const { url, id } = await todoist.addTask(body);
+        const { id, data: newData } = await addTask(body, { data, setData });
         toast.style = Toast.Style.Success;
         toast.title = "Task created";
 
-        toast.primaryAction = {
-          title: "Open Task",
-          shortcut: { modifiers: ["cmd"], key: "o" },
-          onAction: () => push(<TaskDetail taskId={id} />),
-        };
+        if (id) {
+          toast.primaryAction = {
+            title: "Open Task",
+            shortcut: { modifiers: ["cmd"], key: "o" },
+            onAction: () => push(<TaskDetail taskId={id} />),
+          };
 
-        toast.secondaryAction = {
-          title: `Open Task ${isTodoistInstalled ? "in Todoist" : "in Browser"}`,
-          shortcut: { modifiers: ["cmd", "shift"], key: "o" },
-          onAction: async () => {
-            open(isTodoistInstalled ? `todoist://task?id=${id}` : url);
-          },
-        };
+          toast.secondaryAction = {
+            title: `Open Task ${isTodoistInstalled ? "in Todoist" : "in Browser"}`,
+            shortcut: { modifiers: ["cmd", "shift"], key: "o" },
+            onAction: async () => {
+              open(isTodoistInstalled ? getTaskAppUrl(id) : getTaskUrl(id));
+            },
+          };
 
-        if (fromProjectId && mutateTasks) {
-          mutateTasks();
+          if (values.files.length > 0) {
+            try {
+              toast.message = "Uploading file and adding to comment…";
+              const file = await uploadFile(values.files[0]);
+              await addComment({ item_id: id, file_attachment: file, content: "" }, { data: newData, setData });
+              toast.message = "File uploaded and added to comment";
+            } catch (error) {
+              toast.message = `Failed uploading file and adding to comment`;
+            }
+          }
+        }
+
+        if (fromProjectId) {
           pop();
         }
 
@@ -119,7 +127,10 @@ export default function CreateTask({ fromProjectId, mutateTasks, draftValues }: 
           priority: String(lowestPriority.value),
           projectId: "",
           sectionId: "",
+          responsibleUid: "",
           labels: [],
+          files: [],
+          parentId: "",
         });
 
         focus("content");
@@ -130,105 +141,139 @@ export default function CreateTask({ fromProjectId, mutateTasks, draftValues }: 
     initialValues: {
       content: draftValues?.content,
       description: draftValues?.description,
-      dueDate: draftValues?.dueDate,
+      dueDate: draftValues?.dueDate ?? (fromTodayEmptyView ? new Date() : null),
       priority: draftValues?.priority || String(lowestPriority.value),
-      projectId: draftValues?.projectId
-        ? String(draftValues?.projectId)
-        : "" || fromProjectId
-        ? String(fromProjectId)
-        : "",
-      sectionId: draftValues?.sectionId ? String(draftValues?.sectionId) : "",
-      labels: draftValues?.labels,
+      projectId: draftValues?.projectId ? draftValues.projectId : "" || fromProjectId ? fromProjectId : "",
+      sectionId: draftValues?.sectionId ? draftValues.sectionId : "",
+      responsibleUid: draftValues?.responsibleUid ? draftValues.responsibleUid : "",
+      labels: draftValues?.labels ?? (fromLabel ? [fromLabel] : []) ?? [],
     },
     validation: {
       content: FormValidation.Required,
     },
   });
 
-  const projectSections = sections?.filter((section) => String(section.projectId) === values.projectId);
+  const projectSections = sections?.filter((section) => section.project_id === values.projectId);
+
+  const collaborators = getProjectCollaborators(values.projectId, data);
 
   return (
-    <View>
-      <Form
-        isLoading={isLoadingProjects || isLoadingSections || isLoadingLabels}
-        actions={
-          <ActionPanel>
-            <Action.SubmitForm title="Create Task" onSubmit={handleSubmit} icon={Icon.Plus} />
-          </ActionPanel>
-        }
-        enableDrafts={!fromProjectId}
-      >
-        <Form.TextField {...itemProps.content} title="Title" placeholder="Buy fruits" />
+    <Form
+      isLoading={isLoading}
+      actions={
+        <ActionPanel>
+          <Action.SubmitForm title="Create Task" onSubmit={handleSubmit} icon={Icon.Plus} />
 
-        <Form.TextArea
-          {...itemProps.description}
-          title="Description"
-          placeholder="Apples, pears, and strawberries (Markdown supported)"
-          enableMarkdown
-        />
+          <RefreshAction />
+        </ActionPanel>
+      }
+      enableDrafts={!fromProjectId && !fromTodayEmptyView && !fromLabel}
+    >
+      <Form.TextField {...itemProps.content} title="Title" placeholder="Buy fruits" />
 
-        <Form.Separator />
+      <Form.TextArea
+        {...itemProps.description}
+        title="Description"
+        placeholder="Apples, pears, and strawberries (Markdown supported)"
+        enableMarkdown
+      />
 
-        <Form.DatePicker {...itemProps.dueDate} title="Due date" type={Form.DatePicker.Type.Date} />
+      <Form.Separator />
 
-        <Form.Dropdown {...itemProps.priority} title="Priority">
-          {priorities.map(({ value, name, color, icon }) => (
+      <Form.DatePicker {...itemProps.dueDate} title="Due date" type={Form.DatePicker.Type.Date} />
+
+      <Form.Dropdown {...itemProps.priority} title="Priority">
+        {priorities.map(({ value, name, color, icon }) => (
+          <Form.Dropdown.Item
+            value={String(value)}
+            title={name}
+            key={value}
+            icon={{ source: icon ? icon : Icon.Dot, tintColor: color }}
+          />
+        ))}
+      </Form.Dropdown>
+
+      {projects && projects.length > 0 ? (
+        <Form.Dropdown {...itemProps.projectId} title="Project">
+          <Form.Dropdown.Item title="No project" value="" icon={Icon.List} />
+
+          {projects.map((project) => (
             <Form.Dropdown.Item
-              value={String(value)}
-              title={name}
-              key={value}
-              icon={{ source: icon ? icon : Icon.Dot, tintColor: color }}
+              key={project.id}
+              value={project.id}
+              icon={getProjectIcon(project)}
+              title={project.name}
             />
           ))}
         </Form.Dropdown>
+      ) : null}
 
-        {projects && projects.length > 0 ? (
-          <Form.Dropdown {...itemProps.projectId} title="Project">
-            <Form.Dropdown.Item title="No project" value="" icon={Icon.List} />
+      {projectSections && projectSections.length > 0 ? (
+        <Form.Dropdown {...itemProps.sectionId} title="Section">
+          <Form.Dropdown.Item
+            value=""
+            title="No section"
+            icon={{ source: "section.svg", tintColor: Color.PrimaryText }}
+          />
 
-            {projects.map((project) => (
-              <Form.Dropdown.Item
-                key={project.id}
-                value={String(project.id)}
-                icon={getProjectIcon(project)}
-                title={project.name}
-              />
-            ))}
-          </Form.Dropdown>
-        ) : null}
-
-        {projectSections && projectSections.length > 0 ? (
-          <Form.Dropdown {...itemProps.sectionId} title="Section">
+          {projectSections.map(({ id, name }) => (
             <Form.Dropdown.Item
-              value=""
-              title="No section"
+              key={id}
+              value={id}
+              title={name}
               icon={{ source: "section.svg", tintColor: Color.PrimaryText }}
             />
+          ))}
+        </Form.Dropdown>
+      ) : null}
 
-            {projectSections.map(({ id, name }) => (
-              <Form.Dropdown.Item
-                key={id}
-                value={String(id)}
-                title={name}
-                icon={{ source: "section.svg", tintColor: Color.PrimaryText }}
-              />
-            ))}
-          </Form.Dropdown>
-        ) : null}
+      {collaborators && collaborators.length > 0 ? (
+        <Form.Dropdown {...itemProps.responsibleUid} title="Assignee">
+          <Form.Dropdown.Item icon={Icon.Person} value="" title="Unassigned" />
 
-        {labels && labels.length > 0 ? (
-          <Form.TagPicker {...itemProps.labels} title="Labels">
-            {labels.map(({ id, name, color }) => (
-              <Form.TagPicker.Item
-                key={id}
-                value={name}
-                title={name}
-                icon={{ source: Icon.Tag, tintColor: getColorByKey(color).hexValue }}
-              />
-            ))}
-          </Form.TagPicker>
-        ) : null}
-      </Form>
+          {collaborators.map((collaborator) => (
+            <Form.Dropdown.Item
+              key={collaborator.id}
+              value={collaborator.id}
+              title={collaborator.full_name}
+              icon={getCollaboratorIcon(collaborator)}
+            />
+          ))}
+        </Form.Dropdown>
+      ) : null}
+
+      {labels && labels.length > 0 ? (
+        <Form.TagPicker {...itemProps.labels} title="Labels">
+          {labels.map(({ id, name, color }) => (
+            <Form.TagPicker.Item
+              key={id}
+              value={name}
+              title={name}
+              icon={{ source: Icon.Tag, tintColor: getColorByKey(color).value }}
+            />
+          ))}
+        </Form.TagPicker>
+      ) : null}
+
+      <Form.Separator />
+
+      <Form.FilePicker {...itemProps.files} title="File" canChooseDirectories={false} allowMultipleSelection={false} />
+
+      <Form.Dropdown {...itemProps.parentId} title="Parent Task">
+        <Form.Dropdown.Item value="" title="None" />
+
+        {data?.items.map((item) => {
+          return <Form.Dropdown.Item key={item.id} title={item.content} icon={getPriorityIcon(item)} value={item.id} />;
+        })}
+      </Form.Dropdown>
+    </Form>
+  );
+}
+
+export default function Command(props: CreateTaskProps) {
+  return (
+    <View>
+      <CreateTask {...props} />
     </View>
   );
 }
