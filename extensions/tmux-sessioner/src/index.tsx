8a05265a75ced9f5ee888f21aaa436879816dcb0
Louis Huy Nguyen <40130936+louishuyng@users.noreply.github.com>
2023-05-12 15:08:53 +0700

    Add tmux-sessioner extension (#6200)
    * Add tmux-sessioner extension
    
    - add feature default terminal app
    - fix PATH is hard code
    - Better error message
    - add command icon
    - simple version
    
    * better description form
    
    * implement new command to change default terminal app
    
    * Update tmux-sessioner extension
    
    - filter only terminal apps
    - remove unused code
    
    * update the flow to avoid laggy rendering
    
    * Update tmux-sessioner extension
    
    - Merge pull request #1 from louishuyng/chore/show-hud
    - chore: should hud after selecting session
    
    * better description and title command
    
    * Update tmux-sessioner extension
    
    - rename tmux session
    - delete tmux session
    - create new session command
    - create new session command
    
    * Update tmux-sessioner extension
    
    - Update README.md
    - Merge pull request #3 from louishuyng/manipulate-basic-tmux-actions
    
    * Update tmux-sessioner extension
    
    - Better icon display
    - Update How to Use section in README.md
    
    * Update index.tsx
    
    * Update create_new_session.tsx
    
    * metadata
    
    ---------
    
    Co-authored-by: Per Nielsen Tikær <per@raycast.com>

---
+++ extensions/tmux-sessioner/src/index.tsx
@@ -0,0 +1,120 @@
+import { useState, useEffect } from "react";
+import { List, Icon, Action, ActionPanel, Toast, showToast, Detail, useNavigation } from "@raycast/api";
+import { LocalStorage } from "@raycast/api";
+import { SelectTerminalApp } from "./SelectTermnialApp";
+import { deleteSession, getAllSession, switchToSession } from "./sessionUtils";
+import { RenameTmuxSession } from "./RenameTmuxSession";
+
+export default function Command() {
+  const [sessions, setSessions] = useState<Array<string>>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isTerminalSetup, setIsTerminalSetup] = useState(false);
+
+  const { push } = useNavigation();
+  async function checkTerminalSetup(): Promise<boolean> {
+    const localTerminalAppName = await LocalStorage.getItem<string>("terminalAppName");
+
+    const toast = await showToast({
+      style: Toast.Style.Animated,
+      title: "Checking terminal App setup",
+    });
+
+    if (!localTerminalAppName) {
+      toast.style = Toast.Style.Failure;
+      toast.title = "No default terminal setup for tmux sessioner";
+      setIsTerminalSetup(false);
+
+      return false;
+    } else {
+      toast.hide();
+      setIsTerminalSetup(true);
+
+      return true;
+    }
+  }
+
+  const setupListSesssions = () => {
+    getAllSession((error, stdout) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        setIsLoading(false);
+        return;
+      }
+
+      const lines = stdout.trim().split("\n");
+
+      if (lines?.length > 0) {
+        setSessions(lines);
+      }
+
+      setIsLoading(false);
+    });
+  };
+
+  useEffect(() => {
+    (async () => {
+      setIsLoading(true);
+
+      const isSetup = await checkTerminalSetup();
+
+      if (!isSetup) {
+        setIsLoading(false);
+        return;
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (!isTerminalSetup) {
+      return;
+    }
+
+    // List down all tmux session
+    setIsLoading(true);
+    setupListSesssions();
+  }, [isTerminalSetup]);
+
+  return (
+    <>
+      <List isLoading={isLoading}>
+        {sessions.map((session, index) => (
+          <List.Item
+            key={index}
+            icon={Icon.Terminal}
+            title={session}
+            actions={
+              <ActionPanel>
+                <Action title="Switch to Selected Session" onAction={() => switchToSession(session, setIsLoading)} />
+                <Action
+                  title="Delete This Session"
+                  onAction={() =>
+                    deleteSession(session, setIsLoading, () => setSessions(sessions.filter((s) => s !== session)))
+                  }
+                  shortcut={{ modifiers: ["cmd"], key: "d" }}
+                />
+                <Action
+                  title="Rename This Session"
+                  onAction={() => {
+                    push(<RenameTmuxSession session={session} callback={() => setupListSesssions()} />);
+                  }}
+                  shortcut={{ modifiers: ["cmd"], key: "r" }}
+                />
+              </ActionPanel>
+            }
+          />
+        ))}
+      </List>
+
+      {!isTerminalSetup && !isLoading && (
+        <Detail
+          markdown="**Setup Default Terminal App Before Usage** `Go to Actions or using Cmd + k`"
+          actions={
+            <ActionPanel>
+              <Action.Push title="Setup Here" target={<SelectTerminalApp setIsTerminalSetup={setIsTerminalSetup} />} />
+            </ActionPanel>
+          }
+        />
+      )}
+    </>
+  );
+}
