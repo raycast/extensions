96cbc8896aa1bece553b847dcf6ff8869b9ece25
victorGPT <wuxuebin1993@gmail.com>
2023-05-13 20:10:23 +0800

    Update md-to-excel extension
    - update
    - Pull contributions
    - New features
    - add new file
    - add package
    - add package
    - Update README.md
    - New feature
    - Update Description

--- extensions/md-to-excel/src/md-to-excel.tsx
+++ extensions/md-to-excel/src/md-to-excel.tsx
@@ -1,6 +1,10 @@
 import { Action, ActionPanel, Clipboard, Form, Icon, popToRoot, showHUD, showToast, Toast } from "@raycast/api";
 import { useState } from "react";
 
+function isExcelFormat(input: string): boolean {
+  return !/^\|(.+\|)+$/m.test(input); // 如果输入不是Markdown表格，则将其视为Excel表格
+}
+
 function convertMarkdownTableToExcel(markdown: string): Promise<string> {
   const lines = markdown.trim().split("\n");
   const tableData: string[][] = lines
@@ -20,18 +24,42 @@ function convertMarkdownTableToExcel(markdown: string): Promise<string> {
   return Promise.resolve(result);
 }
 
+function createMarkdownTable(tableData: string[][]): string {
+  const separator = tableData[0].map(() => "---").join("|");
+  const result = tableData
+    .map((row, index) => {
+      const markdownRow = `|${row.join("|")}|`;
+      return index === 0 ? `${markdownRow}\n|${separator}|` : markdownRow;
+    })
+    .join("\n");
+
+  return result;
+}
+
+function convertExcelToMarkdown(excel: string): Promise<string> {
+  const lines = excel.trim().split("\n");
+  const tableData = lines.map((line) => line.split("\t").map((cell) => cell.trim()));
+
+  const result = createMarkdownTable(tableData);
+
+  return Promise.resolve(result);
+}
+
 export default function MD2Excel() {
   const [input, setInput] = useState("");
 
   const handleSubmit = async () => {
     try {
-      const plain = await convertMarkdownTableToExcel(input);
-      await Clipboard.copy(plain);
-      await showHUD("Converted to Plain and copied to clipboard");
+      const result = isExcelFormat(input)
+        ? await convertExcelToMarkdown(input)
+        : await convertMarkdownTableToExcel(input);
+
+      await Clipboard.copy(result);
+      await showHUD("Converted and copied to clipboard");
       await popToRoot();
     } catch (error) {
       console.error(error);
-      showToast(Toast.Style.Failure, "Failed to convert and copy Plain", "See console for more details");
+      showToast(Toast.Style.Failure, "Failed to convert and copy", "See console for more details");
     }
   };
 
@@ -39,14 +67,14 @@ export default function MD2Excel() {
     <Form
       actions={
         <ActionPanel>
-          <Action icon={Icon.Clipboard} title="Convert to Plain and Copy to Clipboard" onAction={handleSubmit} />
+          <Action icon={Icon.Clipboard} title="Convert and Copy to Clipboard" onAction={handleSubmit} />
         </ActionPanel>
       }
     >
       <Form.TextArea
         id="input"
-        title="Markdown Table"
-        placeholder="Paste your Markdown table here"
+        title="Table"
+        placeholder="Paste your Markdown table or Excel data here"
         onChange={setInput}
       />
     </Form>
