35a50cacfddf554309b04274d27f54d172884888
Kim Schneider <web@schneider.kim>
2023-05-15 01:52:14 +0200

    ♻️ refactor chatGPT hook

--- extensions/summarize-youtube-video-with-ai/src/hooks/useChatGPTSummary.ts
+++ extensions/summarize-youtube-video-with-ai/src/hooks/useChatGPTSummary.ts
@@ -2,7 +2,6 @@ import React from "react";
 import { CHATGPT_SUMMARY_MAX_CHARS } from "../const/max_chars";
 import { Configuration, OpenAIApi } from "openai";
 import { Toast, getPreferenceValues, showToast } from "@raycast/api";
-import { usePromise } from "@raycast/utils";
 import splitTranscript from "../utils/splitTranscript";
 
 type GetChatGPTSummaryProps = {
@@ -11,7 +10,7 @@ type GetChatGPTSummaryProps = {
   setSummary: React.Dispatch<React.SetStateAction<string | undefined>>;
 };
 
-const useChatGPTSummary = ({ transcript, setSummaryIsLoading, setSummary }: GetChatGPTSummaryProps) => {
+const useChatGPTSummary = async ({ transcript, setSummaryIsLoading, setSummary }: GetChatGPTSummaryProps) => {
   const preferences = getPreferenceValues();
 
   if (preferences.chosenAi !== "chatgpt") {
@@ -25,94 +24,81 @@ const useChatGPTSummary = ({ transcript, setSummaryIsLoading, setSummary }: GetC
   let temporarySummary = "";
   let openAiInstructions = "";
 
-  const { isLoading } = usePromise(
-    async () => {
-      showToast({
-        style: Toast.Style.Animated,
-        title: "💡",
-        message: "Summarizing the video",
-      });
+  setSummaryIsLoading(true);
 
-      if (transcript && transcript?.length > CHATGPT_SUMMARY_MAX_CHARS) {
-        showToast({
-          style: Toast.Style.Animated,
-          title: "❗",
-          message: "That's a long video, hold on.",
+  if (transcript && transcript?.length > CHATGPT_SUMMARY_MAX_CHARS) {
+    showToast({
+      style: Toast.Style.Animated,
+      title: "❗",
+      message: "That's a long video, hold on.",
+    });
+
+    const transcriptionSummary = splitTranscript(transcript, CHATGPT_SUMMARY_MAX_CHARS);
+
+    for (const summaryBlock of transcriptionSummary) {
+      const index = transcriptionSummary.indexOf(summaryBlock) + 1;
+      const openAiInstructionBlock = `
+        Summarize this transcription of a youtube video.
+        The transcription is split into parts and this is part ${index} of ${transcriptionSummary.length}.
+        Be as concise as possible.
+        Do not use more then ${CHATGPT_SUMMARY_MAX_CHARS / transcriptionSummary.length} characters.
+
+        Here is the transcript: ${summaryBlock}`;
+
+      try {
+        const result = await openai.createChatCompletion({
+          model: "gpt-3.5-turbo",
+          messages: [{ role: "user", content: openAiInstructionBlock }],
         });
-
-        const transcriptionSummary = splitTranscript(transcript, CHATGPT_SUMMARY_MAX_CHARS);
-
-        for (const summaryBlock of transcriptionSummary) {
-          const index = transcriptionSummary.indexOf(summaryBlock) + 1;
-          const openAiInstructionBlock = `
-          Summarize this transcription of a youtube video.
-          The transcription is split into parts and this is part ${index} of ${transcriptionSummary.length}.
-          Be as concise as possible.
-          Do not use more then ${CHATGPT_SUMMARY_MAX_CHARS / transcriptionSummary.length} characters.
-
-          Here is the transcript: ${summaryBlock}`;
-
-          openAiInstructionBlock &&
-            (await openai
-              .createChatCompletion({
-                model: "gpt-3.5-turbo",
-                messages: [{ role: "user", content: openAiInstructionBlock }],
-              })
-              .then((result) => {
-                temporarySummary += result.data.choices[0].message?.content;
-              })
-              .catch((error) => {
-                console.error(error.request.res);
-                showToast({
-                  style: Toast.Style.Failure,
-                  title: "🚨",
-                  message: error.message,
-                });
-              }));
-        }
+        temporarySummary += result.data.choices[0].message?.content;
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch (error: any) {
+        showToast({
+          style: Toast.Style.Failure,
+          title: "🚨",
+          message: error.message,
+        });
       }
-
-      openAiInstructions +=
-        transcript &&
-        `Summarize the following transcription of a youtube video as a list of the most important points each starting with a fitting emoji. Ignore mentions of video sponsors.
-
-        Format:
-        [Emoji] [List Item] [\n\n]
-
-        Here is the transcript: ${temporarySummary.length > 0 ? temporarySummary : transcript}`;
-
-      openAiInstructions &&
-        (await openai
-          .createChatCompletion({
-            model: "gpt-3.5-turbo",
-            messages: [{ role: "user", content: openAiInstructions }],
-          })
-          .then((result) => {
-            showToast({
-              style: Toast.Style.Success,
-              title: "📝",
-              message: "Video summarized!",
-            });
-            setSummary(result.data.choices[0].message?.content);
-          })
-          .catch((error) => {
-            showToast({
-              style: Toast.Style.Failure,
-              title: "🚨",
-              message: error.message,
-            });
-            console.error(error.request.res);
-          }));
-    },
-    [],
-    {
-      execute: Boolean(transcript),
     }
-  );
+  }
 
-  setSummaryIsLoading(isLoading);
+  openAiInstructions +=
+    transcript &&
+    `Summarize the following transcription of a youtube video as a list of the most important points each starting with a fitting emoji. Ignore mentions of video sponsors.
 
-  return null;
+    Format:
+    [Emoji] [List Item] [\n\n]
+
+    Here is the transcript: ${temporarySummary.length > 0 ? temporarySummary : transcript}`;
+
+  try {
+    showToast({
+      style: Toast.Style.Animated,
+      title: "💡",
+      message: "Summarizing video",
+    });
+
+    const result = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: openAiInstructions }],
+    });
+    showToast({
+      style: Toast.Style.Success,
+      title: "📝",
+      message: "Video summarized!",
+    });
+    setSummary(result.data.choices[0].message?.content);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    showToast({
+      style: Toast.Style.Failure,
+      title: "🚨",
+      message: error.message,
+    });
+    console.error(error.request.res);
+  }
+
+  setSummaryIsLoading(false);
 };
 
 export default useChatGPTSummary;
