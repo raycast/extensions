1c5e4b57b120068a8055fb08ed21f00153dd9707
chuyun <chuyun@users.noreply.github.com>
2023-05-16 03:55:12 +0800

    Update chatgo extension (#6381)
    * Update chatgo extension
    
    - fix
    - chore: fix eslint error
    - chore: update @raycast/api
    - chore: update @raycast/api
    - fix: fix some bugs and optimize user experience
    
    * Update chatgo extension
    
    - feat: Add more theme store functions,etc
    - docs: README.md
    - Merge remote-tracking branch 'chuyun/main'
    - doc: zh-cn README-cn.md
    - Update README-cn.md
    - doc: zh-cn README-cn.md
    - doc: zh-cn README-cn.md
    
    * Add files via upload
    
    * Delete command-icon-default.png
    
    * Delete list-icon.png
    
    * Delete wechat.jpg
    
    * Add files via upload
    
    ---------
    
    Co-authored-by: J <info_together@aliyun.com>

--- extensions/chatgo/src/hooks/useChatGo.tsx
+++ extensions/chatgo/src/hooks/useChatGo.tsx
@@ -1,10 +1,8 @@
-// import { getPreferenceValues } from "@raycast/api";
 import { useState } from "react";
 import ChatGoAPi from "../utils/chatGoApi";
 
 export const useChatGo = () => {
   const [chatGo] = useState(() => {
-    // const email = getPreferenceValues<{ email: string }>().email;
     return new ChatGoAPi();
   });
   return chatGo;
