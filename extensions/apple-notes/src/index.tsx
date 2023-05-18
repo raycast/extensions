140b9621b0f37dec056c7399aefb94c867d2c12e
Tyler Wince <tylerwince@gmail.com>
2023-05-18 16:59:54 -0700

    formatted

--- extensions/apple-notes/src/index.tsx
+++ extensions/apple-notes/src/index.tsx
@@ -131,8 +131,6 @@ export default function Command() {
       let tagString = note.tags as unknown as string;
       note.tags = tagString.split(" ");
     }
-    console.log([note.noteBody.replace(/\n/g, "")][0].split(" "))
-    console.log(typeof([note.noteBody.replace(/\n/g, "")][0].split(" ")))
   });
 
   return (
@@ -264,9 +262,9 @@ export default function Command() {
                   title={note.title || ""}
                   subtitle={note.snippet}
                   keywords={[`${note.folder}`, `${note.account}`]
-                  .concat(note.tags ? note.tags.map((tag) => tag.replace("#", "")) : [])
-                  .concat(note.noteBody ? note.noteBody.replace(/\n/g, "").split(" ") : [])
-                  .concat(note.ocrText ? note.ocrText.replace(/\n/g, "").split(" ") : [])}
+                    .concat(note.tags ? note.tags.map((tag) => tag.replace("#", "")) : [])
+                    .concat(note.noteBody ? note.noteBody.replace(/\n/g, "").split(" ") : [])
+                    .concat(note.ocrText ? note.ocrText.replace(/\n/g, "").split(" ") : [])}
                   accessories={([] as List.Item.Accessory[])
                     .concat(
                       preferences.tags && note.tags
