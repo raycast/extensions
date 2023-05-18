35f01fd1c061c33d9fbd8f30c266d201476ee1b2
José Pereira <jmfp-b@live.com.pt>
2023-05-18 01:12:24 +0100

    refactor: call showToast instead of modifying existing one

--- extensions/bitwarden/src/lock-vault.tsx
+++ extensions/bitwarden/src/lock-vault.tsx
@@ -4,13 +4,11 @@ import { VAULT_LOCK_MESSAGES } from "~/constants/general";
 import { SessionStorage } from "~/context/session/utils";
 
 async function lockVaultCommand() {
-  const toast = await showToast(Toast.Style.Animated, "Locking vault...", "Please wait");
   try {
+    await showToast(Toast.Style.Animated, "Locking vault...", "Please wait");
     const [token] = await SessionStorage.getSavedSession();
     if (!token) {
-      toast.style = Toast.Style.Failure;
-      toast.title = "No session found";
-      toast.message = "Already locked or not logged in";
+      await showToast(Toast.Style.Failure, "No session found", "Already locked or not logged in");
       return;
     }
 
@@ -18,11 +16,9 @@ async function lockVaultCommand() {
     await bitwarden.withSession(token).lock(VAULT_LOCK_MESSAGES.MANUAL);
     await SessionStorage.clearSession();
 
-    toast.style = Toast.Style.Success;
-    toast.title = "Vault successfully locked";
+    await showToast(Toast.Style.Success, "Vault successfully locked");
   } catch (error) {
-    toast.style = Toast.Style.Failure;
-    toast.title = "Failed to lock vault";
+    await showToast(Toast.Style.Failure, "Failed to lock vault");
   }
 }
 
