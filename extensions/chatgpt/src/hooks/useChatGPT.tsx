1d0c05e5c680fd2661e37d2f15ba553eadcb7958
湯遒之 <nauxscript@gmail.com>
2023-05-19 14:33:43 +0800

    Update useChatGPT.tsx

--- extensions/chatgpt/src/hooks/useChatGPT.tsx
+++ extensions/chatgpt/src/hooks/useChatGPT.tsx
@@ -2,22 +2,16 @@ import { getPreferenceValues } from "@raycast/api";
 import { Configuration, OpenAIApi } from "openai";
 import { useState } from "react";
 import { ConfigurationPreferences } from "../type";
+import { getConfigUrl } from "../utils";
 
 export function useChatGPT(): OpenAIApi {
   const [chatGPT] = useState(() => {
     const preferences = getPreferenceValues<ConfigurationPreferences>();
     const getConfig = function (params: ConfigurationPreferences) {
-      if (params.useAzure) {
-        return new Configuration({
-          apiKey: params.apiKey,
-          basePath: params.azureEndpoint + "/openai/deployments/" + params.azureDeployment,
-        });
-      } else {
-        return new Configuration({
-          apiKey: params.apiKey,
-          basePath: params.apiEndpoint ?? "https://api.openai.com/v1",
-        });
-      }
+      return new Configuration({
+        apiKey: params.apiKey,
+        basePath: getConfigUrl(params)
+      });
     };
     const config = getConfig(preferences);
     return new OpenAIApi(config);
