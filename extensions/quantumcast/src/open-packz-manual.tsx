c8e3628084962bc84c9fcb7de6027d58f7582a38
Per Nielsen Tikær <per@raycast.com>
2023-05-19 08:16:28 +0200

    Update open-packz-manual.tsx

--- extensions/quantumcast/src/open-packz-manual.tsx
+++ extensions/quantumcast/src/open-packz-manual.tsx
@@ -1,4 +1,4 @@
-import { List, Action, ActionPanel, getPreferenceValues, open, icon } from '@raycast/api';
+import { List, Action, ActionPanel, getPreferenceValues, open, Icon } from '@raycast/api';
 import { useState, useEffect } from 'react';
 import { packz, env } from 'quantumlib';
 
