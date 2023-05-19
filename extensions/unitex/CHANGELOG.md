7d3b3b256f4b179dd2a76d6e51c33b78dea85f1c
jaymody <jaykmody@gmail.com>
2023-05-18 20:34:23 -0400

    Update unitex extension
    - Fix code styling errors.
    - Fix linting issues.

--- extensions/unitex/CHANGELOG.md
+++ extensions/unitex/CHANGELOG.md
@@ -1,4 +1,5 @@
 # UniTex Changelog
 
 ## [Initial Version] - 2023-04-18
-* LaTeX to unicode with copy to clipboard functionality.
+
+- LaTeX to unicode with copy to clipboard functionality.
--- extensions/unitex/src/index.tsx
+++ extensions/unitex/src/index.tsx
@@ -1,22 +1,30 @@
 import { ActionPanel, Form, Action } from "@raycast/api";
 import { useState } from "react";
-var unicodeit = require('unicodeit');
+import { replace } from "unicodeit";
 
 export default function Command() {
   const [inputText, setInputText] = useState<string>("");
   const [outputText, setOutputText] = useState<string>("");
 
-  let update = (text: string) => {
+  const update = (text: string) => {
     setInputText(text);
-    setOutputText(unicodeit.replace(text))
-  }
+    setOutputText(replace(text));
+  };
 
   return (
     <Form
       actions={
         <ActionPanel>
-          <Action.CopyToClipboard title="Copy Text to Clipboard" content={outputText} shortcut={{ modifiers: ["cmd"], key: "enter" }} />
-          <Action.CopyToClipboard title="Copy Input Text to Clipboard" content={inputText} shortcut={{ modifiers: ["cmd", "shift"], key: "enter" }} />
+          <Action.CopyToClipboard
+            title="Copy Text to Clipboard"
+            content={outputText}
+            shortcut={{ modifiers: ["cmd"], key: "enter" }}
+          />
+          <Action.CopyToClipboard
+            title="Copy Input Text to Clipboard"
+            content={inputText}
+            shortcut={{ modifiers: ["cmd", "shift"], key: "enter" }}
+          />
         </ActionPanel>
       }
     >
