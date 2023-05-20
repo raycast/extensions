e61a05daa08eb5d1b3c3a48c78ee66125f73bf78
j0 <joel@joellee.org>
2023-05-20 23:35:36 +0800

    fix: add single display

--- extensions/french-verb-conjugation/src/index.tsx
+++ extensions/french-verb-conjugation/src/index.tsx
@@ -7,80 +7,54 @@ interface ConjugationArguments {
   tense?: TenseInput;
 }
 
-const modes = ["infinitive", "conditional", "subjunctive",  "imperative", "participle"];
-const tenses = ["present"
-  , "imperfect"
-  , "future"
-  , "simple-past"
-  , "perfect-tense"
-  , "pluperfect"
-  , "anterior-past"
-  , "anterior-future"
-  , "conditional-past"
-  , "subjunctive-past"
-  , "subjunctive-pluperfect"
-  , "imperative-present"
-  , "imperative-past"
-  , "present-participle"
-  , "past-participle"];
+const modes = ["infinitive", "conditional", "subjunctive", "imperative", "participle"];
+const tenses = [
+  "present",
+  "imperfect",
+  "future",
+  "simple-past",
+  "perfect-tense",
+  "pluperfect",
+  "anterior-past",
+  "anterior-future",
+  "conditional-past",
+  "subjunctive-past",
+  "subjunctive-pluperfect",
+  "imperative-present",
+  "imperative-past",
+  "present-participle",
+  "past-participle",
+];
 
 const structure = {
-	infinitive: [
-		 "infinitive-present"
-	   ],
-	indicative: [
-		"present",
-		"imperfect",
-		"future",
-		"simple-past",
-		"perfect-tense",
-		"pluperfect",
-		"anterior-past",
-		"anterior-future"
-	],
-	conditional: [
-		"present",
-		"conditional-past"
-	],
-	subjunctive: [
-		"present",
-		"imperfect",
-		"subjunctive-past",
-		"subjunctive-pluperfect"
-	],
-	imperative: [
-		"imperative-present",
-		"imperative-past"
-	],
-	participle: [
-		 "present-participle",
-		 "past-participle"
-	 ]
+  infinitive: ["infinitive-present"],
+  indicative: [
+    "present",
+    "imperfect",
+    "future",
+    "simple-past",
+    "perfect-tense",
+    "pluperfect",
+    "anterior-past",
+    "anterior-future",
+  ],
+  conditional: ["present", "conditional-past"],
+  subjunctive: ["present", "imperfect", "subjunctive-past", "subjunctive-pluperfect"],
+  imperative: ["imperative-present", "imperative-past"],
+  participle: ["present-participle", "past-participle"],
 };
 
-type ModeInput = typeof modes[number];
-type TenseInput = typeof tenses[number];
+type ModeInput = (typeof modes)[number];
+type TenseInput = (typeof tenses)[number];
 
 function generateTenseTable(args: ConjugationArguments) {
-  const { verb } = args;
-  let tenseTable = "";
+  const { verb, tense, mode } = args;
+  let output = "";
   try {
-    for (const [mode, tenses] of Object.entries(structure)) {
-      for (const tense of tenses) {
-        const body = conjugationFR.conjugate(verb, mode ?? "", tense ?? "").filter(record => record.pronounIndex !== -1 );
-        if (body.length  === 0) {
-          continue
-        }
-    tenseTable = tenseTable + `**Tense**: ${mode}, ${tense}` + '\n' + `| Pronoun | Verb  |
-| ------- | ----- |`
-for (const record of body) {
-  tenseTable = tenseTable + '\n' + `| ${record.pronoun} | ${record.verb}|`
-
-}
-        tenseTable += '\n\n'
-
-      }
-      tenseTable +='\n'
+    if (tense !== "" || mode !== "") {
+      return generateTenseConjugationTable({ verb, mode, tense });
+    } else {
+      output = displayAllTenses({ verb, tense, mode });
     }
   } catch (error) {
     if (error instanceof Error) {
@@ -94,11 +68,41 @@ for (const record of body) {
       return "unknown error has occurred";
     }
   }
-  return tenseTable;
+  return output;
 }
 
 export default function Command(props: LaunchProps<{ arguments: ConjugationArguments }>) {
   const markdown = generateTenseTable(props.arguments);
 
   return <Detail markdown={markdown} />;
+}
+
+function displayAllTenses({ verb, mode, tense }) {
+  let output = "";
+  for (const [mode, tenses] of Object.entries(structure)) {
+    for (const tense of tenses) {
+      output += generateTenseConjugationTable({ verb, mode, tense });
+    }
+    output += "\n";
+  }
+  return output;
+}
+
+function generateTenseConjugationTable({ verb, mode, tense }) {
+  let tenseTable = "";
+  const body = conjugationFR.conjugate(verb, mode ?? "", tense ?? "").filter((record) => record.pronounIndex !== -1);
+  if (body.length === 0) {
+    return "";
+  }
+  tenseTable =
+    tenseTable +
+    `**Tense**: ${mode}, ${tense}` +
+    "\n" +
+    `| Pronoun | Verb  |
+| ------- | ----- |`;
+  for (const record of body) {
+    tenseTable = tenseTable + "\n" + `| ${record.pronoun} | ${record.verb}|`;
+  }
+  tenseTable += "\n\n";
+  return tenseTable;
 }
