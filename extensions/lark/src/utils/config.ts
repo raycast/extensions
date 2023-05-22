17d809fd425a12f3392746141c72cf8f06d53bf1
Tmk <i@tmk.im>
2023-05-22 22:15:42 +0800

    feat(lark): polish lark extension (#6715)
    * feat(lark): polish lark extension
    
    * Changelog entry
    
    ---------
    
    Co-authored-by: Per Nielsen Tikær <per@raycast.com>

--- extensions/lark/src/utils/config.ts
+++ extensions/lark/src/utils/config.ts
@@ -1,15 +1,18 @@
 import { getPreferenceValues } from '@raycast/api';
 
-export interface Preference {
-  type: 'feishu' | 'lark' | 'self-hosted';
-  selfHostedDomain?: string;
-  recentListCount: number;
-}
+export type Preference = Override<
+  ExtensionPreferences,
+  {
+    recentListCount: number;
+  }
+>;
 
-export function getPreference(): Preference {
-  const preference = getPreferenceValues<Preference>();
-  preference.recentListCount = Number(preference.recentListCount) || 15;
-  return preference;
+function getPreference(): Preference {
+  const preference = getPreferenceValues<ExtensionPreferences>();
+  return {
+    ...preference,
+    recentListCount: Number(preference.recentListCount) || 15,
+  };
 }
 
 export const preference = getPreference();
