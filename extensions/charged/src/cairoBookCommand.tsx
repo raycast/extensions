849eabe4d74c701b544f9b648980c6b9f56457d9
apoorveth <95699312+apoorveth@users.noreply.github.com>
2023-05-23 12:06:12 +0530

    Add charged extension (#6638)
    * Add charged extension
    
    - removed owner from extension
    - fixed extension icon
    - circle logos for the ecosystem
    - init
    - Initial commit
    
    * added bn types
    
    * Update charged extension
    
    - changed commands subtitle
    - changed extension name
    
    * Update extensions/charged/src/cairoBookCommand.tsx
    
    Co-authored-by: Per Nielsen Tikær <per@raycast.com>
    
    * Update charged extension
    
    - Merge branch 'contributions/merge-1684744466191143000'
    - Pull contributions
    - removed unused variables, fixed titles and added screenshots
    
    * Small fixes
    
    ---------
    
    Co-authored-by: Per Nielsen Tikær <per@raycast.com>

---
+++ extensions/charged/src/cairoBookCommand.tsx
@@ -0,0 +1,46 @@
+import { List, ActionPanel, Action } from "@raycast/api";
+import { useEffect, useState } from "react";
+import fs from "fs";
+
+type ChapterDetails = {
+  name: string;
+  markdown: string;
+};
+
+export default function Command() {
+  const [listLoading, setListLoading] = useState<boolean>(true);
+  const [listItems, setListItems] = useState<ChapterDetails[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const files = await fs.promises.readdir(__dirname + "/assets/cairoBook");
+      let mappedFiles: ChapterDetails[] = await Promise.all(
+        files.map(async (file) => {
+          return {
+            name: file,
+            markdown: await fs.promises.readFile(__dirname + "/assets/cairoBook/" + file, "utf8"),
+          };
+        })
+      );
+      mappedFiles = mappedFiles.filter((file) => file.markdown && file.name !== "SUMMARY.md");
+      setListItems(mappedFiles);
+      setListLoading(false);
+    })();
+  }, []);
+  return (
+    <List isShowingDetail isLoading={listLoading}>
+      {listItems.map((item) => (
+        <List.Item
+          key={item.name}
+          title={item.markdown.trim().split("\n")[0].replaceAll("#", "").trim()}
+          detail={<List.Item.Detail markdown={item.markdown} />}
+          actions={
+            <ActionPanel>
+              <Action.OpenInBrowser url={`https://cairo-book.github.io/${item.name.replace(".md", ".html")}`} />
+            </ActionPanel>
+          }
+        />
+      ))}
+    </List>
+  );
+}
