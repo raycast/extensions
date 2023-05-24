6ff9ddea8fbb3c8c436ed05df5beb7fc43912064
Fatpandac <i@fatpandac.com>
2023-05-24 11:25:17 +0800

    fix: fix typo

--- extensions/docsearch/src/apollo.tsx
+++ extensions/docsearch/src/apollo.tsx
@@ -2,5 +2,4 @@ import { SearchDocumentation } from "./components";
 
 export default function Command(props: { arguments: { search?: string } }) {
   return <SearchDocumentation id="48dd6e88-fd12-4e55-8d0d-e9c2982ba924" quickSearch={props.arguments?.search} />;
-  Apollo;
 }
