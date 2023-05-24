3acd2d9c6e1400f59d666eff5d5dfe5af3e4b43f
Fatpandac <i@fatpandac.com>
2023-05-24 11:07:14 +0800

    feat: support Meilisearch

--- extensions/docsearch/src/flet.tsx
+++ extensions/docsearch/src/flet.tsx
@@ -1,5 +1,5 @@
 import { SearchDocumentation } from "./components";
 
 export default function Command(props: { arguments: { search?: string } }) {
-  return <SearchDocumentation id="a7cc37c2-cd7c-4d1b-9db2-5dd3b25bc96e" quickSearch={props.arguments?.search} />;
+  return <SearchDocumentation id="8c01f070-0f5b-4b1c-a45e-36ac52967484" quickSearch={props.arguments?.search} />;
 }
