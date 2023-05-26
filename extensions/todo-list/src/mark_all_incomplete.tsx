8e65e5dddb61f79ba67d1571a593aff6f4e758c5
Josh Korol <jkorol3@gatech.edu>
2023-05-26 13:42:28 -0400

    Update todo-list extension
    - Updated icon to rotateAntiClockwise for the Mark All Incomplete command
    - Update readme, useTodo, todo_item, create mark_all_incomplete

---
+++ extensions/todo-list/src/mark_all_incomplete.tsx
@@ -0,0 +1,36 @@
+import { useAtom } from "jotai";
+import { todoAtom } from "./atoms";
+import { TodoSections } from "./atoms";
+import { Action, Color, Icon } from "@raycast/api";
+
+const MarkAllIncompleteAction = () => {
+  const [todoItems, setTodoItems] = useAtom(todoAtom);
+
+  const handleMarkAllIncomplete = () => {
+    // update completed and pinned items
+    const updatedCompletedItems= todoItems.completed.map((item) => ({
+      ...item,
+      completed: false,
+    }));
+    const updatedPinnedItems = todoItems.pinned.map((item) => ({
+      ...item,
+      completed: false,
+    }));
+    const updatedTodoItems: TodoSections = {
+      pinned: updatedPinnedItems,
+      todo: todoItems.todo,
+      completed: updatedCompletedItems,
+    }
+    setTodoItems(updatedTodoItems)
+  }
+  return (
+    <Action
+      title="Mark All Incomplete"
+      onAction={ handleMarkAllIncomplete }
+      shortcut={{ modifiers: ["cmd"], key: "r" }}
+      icon={{ source: Icon.RotateAntiClockwise, tintColor: Color.Magenta}}
+    />
+  );
+};
+
+export default MarkAllIncompleteAction;
