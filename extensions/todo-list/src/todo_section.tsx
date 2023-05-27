02be1f414628026ac3e6fe921892e8a502faf17c
Josh Korol <jkorol3@gatech.edu>
2023-05-27 09:38:36 -0400

    Basic Setup

--- extensions/todo-list/src/todo_section.tsx
+++ extensions/todo-list/src/todo_section.tsx
@@ -5,14 +5,8 @@ import { SECTIONS_DATA } from "./config";
 import SingleTodoItem from "./todo_item";
 const TodoSection = ({ sectionKey }: { sectionKey: keyof TodoSections }) => {
   const [todoSections] = useAtom(todoAtom);
-  const total_items = todoSections.pinned.length + todoSections.completed.length + todoSections.todo.length;
-  const ratio = todoSections.todo.length / total_items * 100;
-  // const calculateRatio = () => {
-  //   const total_items = todoSections.pinned.length + todoSections.completed.length + todoSections.todo.length;
-  //   const ratio = todoSections.todo.length / total_items;
-  const string_title: string = (sectionKey === "completed") ? `Completed ${ratio}%` : SECTIONS_DATA[sectionKey].name
   return (
-    <List.Section title={string_title}>
+    <List.Section title={SECTIONS_DATA[sectionKey].name}>
       {todoSections[sectionKey].map((item, i) => (
         <SingleTodoItem item={item} key={i} idx={i} sectionKey={sectionKey} />
       ))}
