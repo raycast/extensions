e0778980ddf4beca5b430fee8f091ba01710d972
gebeto <slavik.nychkalo@gmail.com>
2023-05-30 11:41:39 +0300

    remove unused preference

--- extensions/google-translate/src/types.ts
+++ extensions/google-translate/src/types.ts
@@ -3,7 +3,6 @@ import { LanguageCode } from "./languages";
 export type LanguageCodeSet = { langFrom: LanguageCode; langTo: LanguageCode };
 
 export type TranslatePreferences = {
-  autoPasteClipboardText?: boolean;
   lang1: LanguageCode;
   lang2: LanguageCode;
 };
