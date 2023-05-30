e87abc66c0f32934bfe1941dda637f56275d783b
apoorveth <95699312+apoorveth@users.noreply.github.com>
2023-05-30 17:38:35 +0530

    fixed extension name (#6798)

--- extensions/charged/src/cairoBookCommand.tsx
+++ extensions/charged/src/cairoBookCommand.tsx
@@ -1,6 +1,7 @@
 import { List, ActionPanel, Action } from "@raycast/api";
 import { useEffect, useState } from "react";
 import fs from "fs";
+import * as analytics from "./utils/analytics";
 
 type ChapterDetails = {
   name: string;
@@ -10,6 +11,10 @@ type ChapterDetails = {
 export default function Command() {
   const [listLoading, setListLoading] = useState<boolean>(true);
   const [listItems, setListItems] = useState<ChapterDetails[]>([]);
+
+  useEffect(() => {
+    analytics.trackEvent("OPEN_CAIRO_BOOK");
+  }, []);
 
   useEffect(() => {
     (async () => {
@@ -36,7 +41,14 @@ export default function Command() {
           detail={<List.Item.Detail markdown={item.markdown} />}
           actions={
             <ActionPanel>
-              <Action.OpenInBrowser url={`https://cairo-book.github.io/${item.name.replace(".md", ".html")}`} />
+              <Action.OpenInBrowser
+                onOpen={() =>
+                  analytics.trackEvent("CAIRO_BOOK_PAGE_OPEN", {
+                    page: item.name,
+                  })
+                }
+                url={`https://cairo-book.github.io/${item.name.replace(".md", ".html")}`}
+              />
             </ActionPanel>
           }
         />
