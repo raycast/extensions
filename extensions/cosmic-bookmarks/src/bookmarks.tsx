08f5aa6ee1ff4ab62739e9961b3e97957feb279b
Karl Koch <karl@kejk.tech>
2023-06-02 10:34:54 +0100

    fix lint

--- extensions/cosmic-bookmarks/src/bookmarks.tsx
+++ extensions/cosmic-bookmarks/src/bookmarks.tsx
@@ -1,7 +1,13 @@
-import { Action, ActionPanel, List, getPreferenceValues, openExtensionPreferences } from '@raycast/api';
-import { createBucketClient } from '@cosmicjs/sdk';
-import { useFetch } from '@raycast/utils';
-import React from 'react';
+import {
+  Action,
+  ActionPanel,
+  List,
+  getPreferenceValues,
+  openExtensionPreferences,
+} from "@raycast/api";
+import { createBucketClient } from "@cosmicjs/sdk";
+import { useFetch } from "@raycast/utils";
+import React from "react";
 
 interface Preferences {
   bucketSlug: string;
@@ -42,13 +48,20 @@ interface SingularObject extends React.ComponentPropsWithoutRef<typeof List> {
 
 export default function Command() {
   const { bucketSlug, readKey } = getPreferenceValues<Preferences>();
-  const { data, isLoading } = useFetch<ObjectsList>(`https://api.cosmicjs.com/v3/buckets/${bucketSlug}/objects?query=%7B%22type%22:%22bookmarks%22%7D&read_key=${readKey}&depth=1&props=`);
+  const { data, isLoading } = useFetch<ObjectsList>(
+    `https://api.cosmicjs.com/v3/buckets/${bucketSlug}/objects?query=%7B%22type%22:%22bookmarks%22%7D&read_key=${readKey}&depth=1&props=`
+  );
 
   return (
     <>
       <List isLoading={isLoading} isShowingDetail>
         {data?.objects.map((object) => (
-          <List.Item key={object.id} title={object.title} detail={<ObjectDetail id={object.id ?? ''} />} actions={<Actions id={object.id ?? ''} />} />
+          <List.Item
+            key={object.id}
+            title={object.title}
+            detail={<ObjectDetail id={object.id ?? ""} />}
+            actions={<Actions id={object.id ?? ""} />}
+          />
         ))}
       </List>
     </>
@@ -58,24 +71,36 @@ export default function Command() {
 function ObjectDetail(props: { id: string }) {
   const { bucketSlug, readKey } = getPreferenceValues<Preferences>();
 
-  const { data, isLoading } = useFetch<SingularObject>(`https://api.cosmicjs.com/v3/buckets/${bucketSlug}/objects/${props.id}?read_key=${readKey}&depth=1&props=`);
+  const { data, isLoading } = useFetch<SingularObject>(
+    `https://api.cosmicjs.com/v3/buckets/${bucketSlug}/objects/${props.id}?read_key=${readKey}&depth=1&props=`
+  );
 
   if (!data?.object) {
     return null; // Return null when data is not available
   }
 
   const regex = /.*https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/|\/.*$/gm;
-  const trimmedURL = data?.object.metadata.url.replace(regex, '');
+  const trimmedURL = data?.object.metadata.url.replace(regex, "");
 
   return (
     data?.object && (
       <List.Item.Detail
         isLoading={isLoading}
-        markdown={`## ${data?.object.title}` + '\n\n' + `${data?.object.metadata?.snippet ?? ''}`}
+        markdown={
+          `## ${data?.object.title}` +
+          "\n\n" +
+          `${data?.object.metadata?.snippet ?? ""}`
+        }
         metadata={
           <List.Item.Detail.Metadata>
-            <List.Item.Detail.Metadata.Label title='Slug' text={trimmedURL ?? ''} />
-            <List.Item.Detail.Metadata.Label title='Status' text={data?.object.metadata.read ? 'Read' : 'Unread'} />
+            <List.Item.Detail.Metadata.Label
+              title="Slug"
+              text={trimmedURL ?? ""}
+            />
+            <List.Item.Detail.Metadata.Label
+              title="Status"
+              text={data?.object.metadata.read ? "Read" : "Unread"}
+            />
           </List.Item.Detail.Metadata>
         }
       />
@@ -86,7 +111,9 @@ function ObjectDetail(props: { id: string }) {
 function Actions(props: { id: string }) {
   const { bucketSlug, readKey, writeKey } = getPreferenceValues<Preferences>();
 
-  const { data } = useFetch<SingularObject>(`https://api.cosmicjs.com/v3/buckets/${bucketSlug}/objects/${props.id}?read_key=${readKey}&depth=1&props=`);
+  const { data } = useFetch<SingularObject>(
+    `https://api.cosmicjs.com/v3/buckets/${bucketSlug}/objects/${props.id}?read_key=${readKey}&depth=1&props=`
+  );
 
   if (!data?.object || data === null) {
     return null; // Return null when data is not available
@@ -103,16 +130,30 @@ function Actions(props: { id: string }) {
             }}
           />
         )}
-        {data?.object.slug && <Action.CopyToClipboard content={data?.object.metadata?.url} title='Copy Link' shortcut={{ modifiers: ['cmd'], key: '.' }} />}
+        {data?.object.slug && (
+          <Action.CopyToClipboard
+            content={data?.object.metadata?.url}
+            title="Copy Link"
+            shortcut={{ modifiers: ["cmd"], key: "." }}
+          />
+        )}
       </ActionPanel.Section>
       <ActionPanel.Section>
-        <Action title='Open Extension Preferences' onAction={openExtensionPreferences} />
+        <Action
+          title="Open Extension Preferences"
+          onAction={openExtensionPreferences}
+        />
       </ActionPanel.Section>
     </ActionPanel>
   );
 }
 
-async function markRead(id: string, bucketSlug: string, readKey: string, writeKey: string) {
+async function markRead(
+  id: string,
+  bucketSlug: string,
+  readKey: string,
+  writeKey: string
+) {
   const cosmic = createBucketClient({
     bucketSlug: bucketSlug,
     readKey: readKey,
@@ -125,7 +166,7 @@ async function markRead(id: string, bucketSlug: string, readKey: string, writeKe
         read: true,
       },
     });
-    console.log('Marked as read:', id);
+    console.log("Marked as read:", id);
     const data = await response.object;
     console.log(data);
   } catch (err) {
