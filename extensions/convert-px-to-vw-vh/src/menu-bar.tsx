befe8c425deef82b5cdadba0710bb42a4e542594
atiipik <jeremy.giardella@gmail.com>
2023-06-06 09:39:12 +0200

    :bug: Fix bug Icon in light/dark theme and remove clear history in environment

--- extensions/convert-px-to-vw-vh/src/menu-bar.tsx
+++ extensions/convert-px-to-vw-vh/src/menu-bar.tsx
@@ -1,44 +1,45 @@
-import { Clipboard, environment, MenuBarExtra, openCommandPreferences, showHUD } from "@raycast/api";
+import { Clipboard, MenuBarExtra, openCommandPreferences, showHUD } from "@raycast/api";
 import { useHistory } from "./history";
 
 export default function Command() {
-  const { history, remove, clear } = useHistory();
+    const { history, remove, clear } = useHistory();
+    const icon = "icon.png";
 
-  return (
-    <MenuBarExtra icon="../assets/logo.svg">
-      <MenuBarExtra.Section>
-        <MenuBarExtra.Item
-          title="Configure Command"
-          shortcut={{ modifiers: ["cmd"], key: "," }}
-          onAction={openCommandPreferences}
-        />
-      </MenuBarExtra.Section>
-      <MenuBarExtra.Section>
-        {history.length > 0 ? (
-          history.map((valueHist, index) => (
-            <MenuBarExtra.Item
-              key={index}
-              title={valueHist.data.label}
-              onAction={async (event) => {
-                if (event.type === "left-click") {
-                  await Clipboard.copy(valueHist.data.value);
-                  await showHUD(`${valueHist.data.value} Copied color to clipboard`);
-                } else {
-                  remove(valueHist);
-                  await showHUD("Deleted color from history");
-                }
-              }}
-            />
-          ))
-        ) : (
-          <MenuBarExtra.Item title="No history" />
-        )}
-      </MenuBarExtra.Section>
-      {history.length > 0 && (
-        <MenuBarExtra.Section>
-          {environment.isDevelopment && <MenuBarExtra.Item title="🗑️ Clear History" onAction={clear} />}
-        </MenuBarExtra.Section>
-      )}
-    </MenuBarExtra>
-  );
+    return (
+        <MenuBarExtra icon={icon}>
+            <MenuBarExtra.Section>
+                <MenuBarExtra.Item
+                    title="Configure Command"
+                    shortcut={{ modifiers: ["cmd"], key: "," }}
+                    onAction={openCommandPreferences}
+                />
+            </MenuBarExtra.Section>
+            <MenuBarExtra.Section>
+                {history.length > 0 ? (
+                    history.map((valueHist, index) => (
+                        <MenuBarExtra.Item
+                            key={index}
+                            title={valueHist.data.label}
+                            onAction={async (event) => {
+                                if (event.type === "left-click") {
+                                    await Clipboard.copy(valueHist.data.value);
+                                    await showHUD(`${valueHist.data.value} Copied color to clipboard`);
+                                } else {
+                                    remove(valueHist);
+                                    await showHUD("Deleted color from history");
+                                }
+                            }}
+                        />
+                    ))
+                ) : (
+                    <MenuBarExtra.Item title="No history" />
+                )}
+            </MenuBarExtra.Section>
+            {history.length > 0 && (
+                <MenuBarExtra.Section>
+                    <MenuBarExtra.Item title="🗑️ Clear History" onAction={clear} />
+                </MenuBarExtra.Section>
+            )}
+        </MenuBarExtra>
+    );
 }
