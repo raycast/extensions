cd8d5d86f1e8f4ba609a922fb48eac31a689bd39
gandli <gandli@qq.com>
2023-06-08 09:47:11 +0800

    🎨 style: 优化 useAria2

--- extensions/aria2/src/hooks/useAria2.ts
+++ extensions/aria2/src/hooks/useAria2.ts
@@ -2,7 +2,7 @@ import { useState, useEffect, useCallback } from "react";
 import Aria2, { Aria2Options } from "aria2";
 import ws from "ws";
 import nodefetch from "node-fetch";
-import { Preferences, Task } from "../types";
+import { Preferences, Task, Status } from "../types";
 import { formatTasks } from "../utils/utils";
 import { getPreferenceValues } from "@raycast/api";
 
@@ -73,8 +73,23 @@ const useAria2 = (): Aria2Client => {
       }
 
       try {
-        await client.call("addUri", [url]);
-        console.log("Added download task:", url);
+        // 判断 url 的类型
+        const isTorrent = url.endsWith(".torrent");
+        const isMetalink = url.endsWith(".metalink");
+
+        if (isTorrent) {
+          // 如果是 Torrent 文件，使用 addTorrent 方法添加任务
+          await client.call("addTorrent", Buffer.from(url).toString("base64"));
+          console.log("Added torrent download task:", url);
+        } else if (isMetalink) {
+          // 如果是 Metalink 文件，使用 addMetalink 方法添加任务
+          await client.call("addMetalink", Buffer.from(url).toString("base64"));
+          console.log("Added metalink download task:", url);
+        } else {
+          // 否则，使用 addUri 方法添加任务
+          await client.call("addUri", [url]);
+          console.log("Added download task:", url);
+        }
       } catch (error) {
         console.error("Failed to add download task:", error);
       }
@@ -89,7 +104,19 @@ const useAria2 = (): Aria2Client => {
       }
 
       try {
-        await client.call("remove", taskId);
+        const taskStatus = await client.call("tellStatus", taskId);
+
+        if (taskStatus.status === Status.Active) {
+          // 任务正在进行中，使用 `forceRemove` 方法停止并删除任务
+          await client.call("forceRemove", taskId);
+        } else if (taskStatus.status === Status.Paused || taskStatus.status === Status.Waiting) {
+          // 任务已暂停或正在等待中，使用 `remove` 方法删除任务
+          await client.call("remove", taskId);
+        } else {
+          // 任务已完成或已停止，使用 `purgeDownloadResult` 方法彻底删除任务结果
+          await client.call("purgeDownloadResult", taskId);
+        }
+
         console.log("Removed task:", taskId);
       } catch (error) {
         console.error("Failed to remove task:", error);
@@ -105,8 +132,15 @@ const useAria2 = (): Aria2Client => {
       }
 
       try {
-        await client.call("pause", taskId);
-        console.log("Paused task:", taskId);
+        const taskStatus = await client.call("tellStatus", taskId);
+
+        if (taskStatus.status === Status.Active) {
+          // 只有当任务状态为 "active" 时，才执行暂停操作
+          await client.call("pause", taskId);
+          console.log("Paused task:", taskId);
+        } else {
+          console.log("Task is not active. Skipping pause operation.");
+        }
       } catch (error) {
         console.error("Failed to pause task:", error);
       }
@@ -121,10 +155,17 @@ const useAria2 = (): Aria2Client => {
       }
 
       try {
-        await client.call("unpause", taskId);
-        console.log("Restart task:", taskId);
+        const taskStatus = await client.call("tellStatus", taskId);
+
+        if (taskStatus.status === Status.Paused) {
+          // 只有当任务状态为 "paused" 时，才执行重新启动操作
+          await client.call("unpause", taskId);
+          console.log("Restarted task:", taskId);
+        } else {
+          console.log("Task is not paused. Skipping restart operation.");
+        }
       } catch (error) {
-        console.error("Failed to Restart task:", error);
+        console.error("Failed to restart task:", error);
       }
     },
     [client]
