87a49034aaaa64c9f35d6c6a51101f34ce933a3a
Сергей Павлюк <spavlyuk@nic.ru>
2023-06-13 14:03:51 +0300

    [iTerm] added split horizontally and verically commands

---
+++ extensions/iterm/src/split-iterm-horizontally.tsx
@@ -0,0 +1,54 @@
+import { runAppleScript } from "run-applescript";
+import { closeMainWindow, Detail, popToRoot, showToast, Toast } from "@raycast/api";
+import { isPermissionError, PermissionErrorScreen } from "./core/permission-error-screen";
+import { useEffect, useState } from "react";
+
+const scriptToSplitHorizontally = `
+  tell application "iTerm"
+    activate
+    repeat until application "iTerm" is running
+        delay 0.1
+    end repeat
+    
+    tell current session of current window
+        split horizontally with default profile
+    end tell
+    activate
+  end tell`;
+
+export default function Command() {
+  const [hasPermissionError, setHasPermissionError] = useState<boolean>(false);
+
+  const loadNewTab = () => {
+    runAppleScript(scriptToSplitHorizontally)
+      .then(async () => {
+        await closeMainWindow();
+        await popToRoot();
+      })
+      .catch(async (e) => {
+        if (isPermissionError(e.message)) {
+          setHasPermissionError(true);
+          return;
+        }
+
+        await showToast({
+          style: Toast.Style.Failure,
+          title: "Cannot split horizontally",
+          message: e.message,
+        });
+      });
+  };
+
+  useEffect(() => {
+    loadNewTab();
+  }, []);
+
+  return (
+    <>
+      {hasPermissionError && <PermissionErrorScreen />}
+      {!hasPermissionError && (
+        <Detail isLoading={true} navigationTitle="Splitting iTerm..." markdown={"Splitting iTerm..."} />
+      )}
+    </>
+  );
+}
