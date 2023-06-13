7dbf9da81e695265fa9f4cbaaa21cfb3843bcff3
Michael Aigner <tonka3100@gmail.com>
2023-06-13 23:38:04 +0200

    add timeout for mDNS request

--- extensions/homeassistant/src/lib/mdns.ts
+++ extensions/homeassistant/src/lib/mdns.ts
@@ -1,11 +1,12 @@
 import multicast_dns from "multicast-dns";
 
-export function queryMdns(address: string) {
+export function queryMdns(address: string, timeout = 5000) {
   return new Promise<string | undefined>((resolve, reject) => {
     const mdns = multicast_dns();
     mdns.on("response", (response) => {
       const homeassistantData = response.answers.find((e) => e.name === address);
       if (homeassistantData) {
+        clearTimeout(timer);
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         const data = (homeassistantData as any).data as string | undefined;
         if (data) {
@@ -18,8 +19,14 @@ export function queryMdns(address: string) {
 
     mdns.query(address, (error, bytes) => {
       if (error) {
+        clearTimeout(timer);
         resolve(undefined);
       }
     });
+
+    const timer = setTimeout(() => {
+      mdns.destroy();
+      reject(new Error("mDNS request timeout"));
+    }, timeout);
   });
 }
