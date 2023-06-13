7aef984e10ed9e6d5621aae8ca34bfd8f3faee74
Noam Koren <noamko25@gmail.com>
2023-06-14 00:28:33 +0300

    Added quick-latex to the extention store

---
+++ extensions/quick-latex/src/quick-latex.tsx
@@ -0,0 +1,56 @@
+import { Action, ActionPanel, environment, List, popToRoot, showHUD } from "@raycast/api";
+import { useState, useEffect } from "react";
+import { existsSync, mkdirSync } from "fs";
+import { resolve } from "path";
+import { runAppleScript } from "run-applescript";
+import { image } from "image-downloader";
+
+const downloadDir = resolve(environment.supportPath, "download");
+const latexUrl = "https://latex.codecogs.com/png.image?" + encodeURIComponent("\\dpi{512}");
+const latexUrlDark = "https://latex.codecogs.com/png.image?" + encodeURIComponent("\\dpi{512}\\bg{white}");
+console.log(latexUrl);
+export default function CommandWithCustoEmptyView() {
+  const [state, setState] = useState({ searchText: "LaTeX", items: [] });
+  useEffect(() => {
+    // perform an API call that eventually populates `items`.
+    if (!existsSync(downloadDir)) {
+      mkdirSync(downloadDir, { recursive: true });
+    }
+  }, [state.searchText]);
+  
+  return (
+    <List onSearchTextChange={(newValue) => setState((previous) => ({ ...previous, searchText: newValue }))}>
+      {state.searchText != ""
+        ? (downloadlatex(latexUrl + encodeURIComponent(state.searchText)),
+          (
+            <List.EmptyView
+              icon={{
+                source: {
+                  light: latexUrl + encodeURIComponent(state.searchText),
+                  dark: latexUrlDark + encodeURIComponent(state.searchText),
+                  
+                },
+              }}
+              actions={
+                <ActionPanel>
+                  <Action
+                    title="Copy LaTeX imagee to Clipboard"
+                    onAction={() => {
+                      runAppleScript(`set the clipboard to POSIX file "${downloadDir}/img.jpg"`),
+                        popToRoot(),
+                        showHUD("Copied");
+                    }}
+                  />
+                </ActionPanel>
+              }
+            />
+          ))
+        : state.items.map((item) => <List.Item key={item} title={item} />)}
+    </List>
+  );
+  function downloadlatex(url: string) {
+    image({ url: url, dest: downloadDir + "/img.jpg" }).catch(() => {
+      console.log("Error check your internet connection.");
+    });
+  }
+}
