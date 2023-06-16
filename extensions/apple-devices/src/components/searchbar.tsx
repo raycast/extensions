b9380996275fa80e1d780d7d58797da73acd14f8
Nemanja M <nemanja@madza.rs>
2023-06-17 01:24:50 +0200

    Lint fix

--- extensions/apple-devices/src/components/searchbar.tsx
+++ extensions/apple-devices/src/components/searchbar.tsx
@@ -3,23 +3,23 @@ import { Preferences } from "../types";
 import React from "react";
 
 function SearchBar(props: { onChange: (value: string) => void }) {
-    const preferences = getPreferenceValues<Preferences>();
-    const isLatest = preferences.device === "latest";
+  const preferences = getPreferenceValues<Preferences>();
+  const isLatest = preferences.device === "latest";
 
-    return (
-        <Grid.Dropdown
-            tooltip="Devices"
-            onChange={props.onChange}
-            storeValue={isLatest}
-            defaultValue={!isLatest ? preferences.device : undefined}
-        >
-            <Grid.Dropdown.Item title="All" value="all" />
-            <Grid.Dropdown.Item title="iPhone" value="iPhone" />
-            <Grid.Dropdown.Item title="iPad" value="iPad" />
-            <Grid.Dropdown.Item title="iPod touch" value="iPod" />
-            <Grid.Dropdown.Item title="Apple Watch" value="Apple Watch" />
-        </Grid.Dropdown>
-    );
+  return (
+    <Grid.Dropdown
+      tooltip="Devices"
+      onChange={props.onChange}
+      storeValue={isLatest}
+      defaultValue={!isLatest ? preferences.device : undefined}
+    >
+      <Grid.Dropdown.Item title="All" value="all" />
+      <Grid.Dropdown.Item title="iPhone" value="iPhone" />
+      <Grid.Dropdown.Item title="iPad" value="iPad" />
+      <Grid.Dropdown.Item title="iPod touch" value="iPod" />
+      <Grid.Dropdown.Item title="Apple Watch" value="Apple Watch" />
+    </Grid.Dropdown>
+  );
 }
 
 export default React.memo(SearchBar);
