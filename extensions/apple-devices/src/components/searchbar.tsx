652597f5e87959569602772e218b83a61fd1703c
Nemanja M <nemanja@madza.rs>
2023-06-17 01:18:56 +0200

    Added preferences for default device type

--- extensions/apple-devices/src/components/searchbar.tsx
+++ extensions/apple-devices/src/components/searchbar.tsx
@@ -1,14 +1,25 @@
-import { Grid } from "@raycast/api";
+import { getPreferenceValues, Grid } from "@raycast/api";
+import { Preferences } from "../types";
 import React from "react";
 
-export default function SearchBar(props: { onChange: (value: string) => void }) {
-  return (
-    <Grid.Dropdown onChange={props.onChange} tooltip="Devices">
-      <Grid.Dropdown.Item title="All" value="all" />
-      <Grid.Dropdown.Item title="iPhone" value="iPhone" />
-      <Grid.Dropdown.Item title="iPad" value="iPad" />
-      <Grid.Dropdown.Item title="iPod touch" value="iPod" />
-      <Grid.Dropdown.Item title="Apple Watch" value="Apple Watch" />
-    </Grid.Dropdown>
-  );
+function SearchBar(props: { onChange: (value: string) => void }) {
+    const preferences = getPreferenceValues<Preferences>();
+    const isLatest = preferences.device === "latest";
+
+    return (
+        <Grid.Dropdown
+            tooltip="Devices"
+            onChange={props.onChange}
+            storeValue={isLatest}
+            defaultValue={!isLatest ? preferences.device : undefined}
+        >
+            <Grid.Dropdown.Item title="All" value="all" />
+            <Grid.Dropdown.Item title="iPhone" value="iPhone" />
+            <Grid.Dropdown.Item title="iPad" value="iPad" />
+            <Grid.Dropdown.Item title="iPod touch" value="iPod" />
+            <Grid.Dropdown.Item title="Apple Watch" value="Apple Watch" />
+        </Grid.Dropdown>
+    );
 }
+
+export default React.memo(SearchBar);
