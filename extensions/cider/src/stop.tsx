d62f2d7c08163184ada046bbc71638bca800ed7f
Amaru8 <52407090+Amaru8@users.noreply.github.com>
2023-06-19 19:18:23 +0200

    Misc Changes

---
+++ extensions/cider/src/stop.tsx
@@ -0,0 +1,17 @@
+import fetch from "node-fetch";
+import { closeMainWindow, showToast, Toast } from "@raycast/api";
+
+export default async function main() {
+  try {
+    await fetch("http://localhost:10769/stop");
+  } catch (error) {
+    console.error(error);
+    showToast({
+      style: Toast.Style.Failure,
+      title: "Error",
+      message: "Failed to connect to Cider",
+    });
+    return;
+  }
+  await closeMainWindow({ clearRootSearch: true });
+}
