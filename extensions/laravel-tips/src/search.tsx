9b4a5951e6c437b337a401374d55571730835d35
Per Nielsen Tikær <per@raycast.com>
2023-06-19 22:22:15 +0200

    Update search.tsx

--- extensions/laravel-tips/src/search.tsx
+++ extensions/laravel-tips/src/search.tsx
@@ -1,5 +1,5 @@
 import { ReactElement, useState } from "react";
-import { Action, ActionPanel, List, showToast, Toast } from "@raycast/api";
+import { Action, ActionPanel, Icon, List, showToast, Toast } from "@raycast/api";
 import { usePromise } from "@raycast/utils";
 import { search } from "./laravel-tip";
 
@@ -23,23 +23,34 @@ export default function Search(): ReactElement {
       isLoading={isLoading}
       isShowingDetail={showingDetail}
       onSearchTextChange={setSearchText}
-      navigationTitle={results?.data?.length ? `Found ${results.data.length} tips` : "No tips found"}
     >
-      {results?.data?.map((tip) => {
-        return (
-          <List.Item
-            key={tip.id}
-            title={tip.title}
-            subtitle={`#${tip.title}`}
-            detail={<List.Item.Detail markdown={`## ${tip.title}\n\n${tip.content}`} />}
-            actions={
-              <ActionPanel>
-                <Action title="Toggle Detail" onAction={() => setShowingDetail(!showingDetail)} />
-              </ActionPanel>
-            }
-          />
-        );
-      })}
+      <List.Section
+        title={
+          results?.data?.length
+            ? `Found ${results.data.length} tip${results.data.length > 1 ? "s" : ""}`
+            : "No tips found"
+        }
+      >
+        {results?.data?.map((tip) => {
+          return (
+            <List.Item
+              key={tip.id}
+              title={tip.title}
+              subtitle={`#${tip.title}`}
+              detail={<List.Item.Detail markdown={`## ${tip.title}\n\n${tip.content}`} />}
+              actions={
+                <ActionPanel>
+                  <Action
+                    title="Toggle Details"
+                    icon={Icon.AppWindowSidebarLeft}
+                    onAction={() => setShowingDetail(!showingDetail)}
+                  />
+                </ActionPanel>
+              }
+            />
+          );
+        })}
+      </List.Section>
     </List>
   );
 }
