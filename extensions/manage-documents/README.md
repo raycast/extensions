a5db3282c11cc6e39e31d035bcc5b38401fa4b79
qlery49 <borgardt.sven@icloud.com>
2023-06-21 22:58:40 +0200

    Add manage-documents extension
    - updated images
    - added aditional features

---
+++ extensions/manage-documents/README.md
@@ -0,0 +1,9 @@
+# Manage documents
+
+This extension enables you to swiftly create Pages, Keynote, Numbers, Scriptedit, and Textedit documents. You can also search for existing files, which only searches the appropriate iCloud folder of the app.
+
+![CleanShot 2023-06-21 at 22 53 00@2x](https://github.com/qlery49/manage-documents/assets/127060121/53363e41-fdd4-4022-ac07-e9dc9f5108b2)
+
+![CleanShot 2023-06-21 at 22 52 31@2x](https://github.com/qlery49/manage-documents/assets/127060121/a9da07c4-b885-496b-90f2-d7be9a68b7e1)
+
+![CleanShot 2023-06-21 at 22 52 41@2x](https://github.com/qlery49/manage-documents/assets/127060121/26743e22-6671-4f8a-9e2d-9d194b290b54)
---
+++ extensions/manage-documents/src/create_document.tsx
@@ -0,0 +1,137 @@
+import { useEffect, useState } from "react";
+import { environment, showToast, Action, ActionPanel, List } from "@raycast/api";
+import { exec } from "child_process";
+import fs from "fs";
+import os from "os";
+
+const currentDate: Date = new Date();
+const formattedDate = `${currentDate.getDate()}.${currentDate.getMonth() + 1}.${currentDate
+  .getFullYear()
+  .toString()
+  .slice(-2)}`;
+const formattedTime = `${currentDate.getHours().toString().padStart(2, "0")}-${currentDate
+  .getMinutes()
+  .toString()
+  .padStart(2, "0")}`;
+const currentUser = os.userInfo().username;
+const basePath = `/Users/${currentUser}/Library/Mobile Documents/`;
+
+const Pages_Template_DST = `${basePath}com~apple~Pages/Documents/PagesDoc ${formattedTime} ${formattedDate}.pages`;
+const Keynote_Template_DST = `${basePath}com~apple~Keynote/Documents/KeynoteDoc ${formattedTime} ${formattedDate}.key`;
+const Textedit_Template_DST = `${basePath}com~apple~TextEdit/Documents/TextEditDoc ${formattedTime} ${formattedDate}.rtf`;
+const Scriptedit_Template_DST = `${basePath}com~apple~ScriptEditor2/Documents/ScriptEditDoc ${formattedTime} ${formattedDate}.scpt`;
+const Numbers_Template_DST = `${basePath}com~apple~Numbers/Documents/NumbersDoc ${formattedTime} ${formattedDate}.numbers`;
+
+const Pages_Template_SRC = `${environment.assetsPath}/PagesOhneTitel.pages`;
+const Keynote_Template_SRC = `${environment.assetsPath}/KeynoteOhneTitel.key`;
+const Textedit_Template_SRC = `${environment.assetsPath}/TextEditOhneTitel.rtf`;
+const Scriptedit_Template_SRC = `${environment.assetsPath}/ScriptEditOhneTItel.scpt`;
+const Numbers_Template_SRC = `${environment.assetsPath}/NumbersOhneTitel.numbers`;
+
+const items = [
+  {
+    keywords: ["numbers"],
+    name: "Numbers",
+    tag: "iCloud Drive/Numbers",
+    imgsrc: `${environment.assetsPath}/numbers_icon.png`,
+    SRC: Numbers_Template_SRC,
+    DST: Numbers_Template_DST,
+  },
+  {
+    keywords: ["keynote"],
+    name: "Keynote",
+    tag: "iCloud Drive/Keynote",
+    imgsrc: `${environment.assetsPath}/keynote_icon.png`,
+    SRC: Keynote_Template_SRC,
+    DST: Keynote_Template_DST,
+  },
+  {
+    keywords: ["pages"],
+    name: "Pages",
+    tag: "iCloud Drive/Pages",
+    imgsrc: `${environment.assetsPath}/pages_icon.png`,
+    SRC: Pages_Template_SRC,
+    DST: Pages_Template_DST,
+  },
+  {
+    keywords: ["scriptedit", "script"],
+    name: "ScriptEdit",
+    tag: "iCloud Drive/ScriptEdit",
+    imgsrc: `${environment.assetsPath}/scriptedit_icon.png`,
+    SRC: Scriptedit_Template_SRC,
+    DST: Scriptedit_Template_DST,
+  },
+  {
+    keywords: ["textedit", "txt"],
+    name: "TextEdit",
+    tag: "iCloud Drive/TextEdit",
+    imgsrc: `${environment.assetsPath}/textedit_icon.png`,
+    SRC: Textedit_Template_SRC,
+    DST: Textedit_Template_DST,
+  },
+];
+
+export default function Command() {
+  const [searchText, setSearchText] = useState("");
+  const [filteredList, filterList] = useState(items);
+
+  useEffect(() => {
+    filterList(items.filter((item) => item.keywords.some((keyword) => keyword.includes(searchText))));
+  }, [searchText]);
+
+  function terminalBefehlausführen(befehl: string) {
+    exec(befehl, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error opening file: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.error(`Error running command: ${stderr}`);
+        return;
+      }
+      console.log("File opened.");
+    });
+  }
+
+  function copyFile(source: string, destination: string) {
+    fs.copyFile(source, destination, (error) => {
+      if (error) {
+        showToast({ title: "Error", message: error.message });
+        return;
+      }
+      terminalBefehlausführen(`open "${destination}"`);
+    });
+  }
+
+  const handleItemAction = (src: string, dst: string) => {
+    copyFile(src, dst);
+  };
+
+  return (
+    <List
+      filtering={false}
+      onSearchTextChange={setSearchText}
+      navigationTitle="Document type"
+      searchBarPlaceholder="Choose your document type"
+    >
+      {filteredList.map((item) => (
+        <List.Item
+          key={item.name}
+          title={item.name}
+          accessories={[
+            {
+              tag: item.tag,
+              tooltip: `The file will be created in ${item.tag}`
+            },
+          ]}
+          icon={item.imgsrc}
+          actions={
+            <ActionPanel>
+              <Action title="Create" onAction={() => handleItemAction(item.SRC, item.DST)} />
+            </ActionPanel>
+          }
+        />
+      ))}
+    </List>
+  );
+}
