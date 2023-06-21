d93cd6200de6395d3e3d030fc1bf1762facd37b2
liunnnanbing <exformation@icloud.com>
2023-06-21 19:51:02 +0800

    refactor: format code by Prettier

--- extensions/seedsnote/src/imgseed.tsx
+++ extensions/seedsnote/src/imgseed.tsx
@@ -1,10 +1,10 @@
-import { Action, ActionPanel, Form, showToast, Toast, getPreferenceValues } from "@raycast/api"
+import { Action, ActionPanel, Form, showToast, Toast, getPreferenceValues } from "@raycast/api";
 import Fs from "fs";
-import { readChunk } from 'read-chunk';
-import imageType, { minimumBytes } from 'image-type';
+import { readChunk } from "read-chunk";
+import imageType, { minimumBytes } from "image-type";
 import { useState } from "react";
 import got from "got";
-import FormData from 'form-data';
+import FormData from "form-data";
 
 interface Preferences {
   endpoint: string;
@@ -17,21 +17,21 @@ export default function Command() {
   const preferences = getPreferenceValues<Preferences>();
 
   async function onFileChange(values: string[]) {
-    setFiles(values)
-    const file = values[0]
+    setFiles(values);
+    const file = values[0];
     if (Fs.existsSync(file) && Fs.lstatSync(file).isFile()) {
       const buffer = await readChunk(file, { length: minimumBytes });
       const fileType = await imageType(buffer);
       if (!fileType) {
-        setFile(undefined)
-        setFileError("Please select a valid image file")
-        return
+        setFile(undefined);
+        setFileError("Please select a valid image file");
+        return;
       }
-      setFile(file)
-      setFileError(undefined)
+      setFile(file);
+      setFileError(undefined);
     } else {
-      setFile(undefined)
-      setFileError("Please select at least one file")
+      setFile(undefined);
+      setFileError("Please select at least one file");
     }
   }
   async function handleSubmit() {
@@ -50,9 +50,9 @@ export default function Command() {
     });
     try {
       const form = new FormData();
-      form.append('file', Fs.createReadStream(file));
+      form.append("file", Fs.createReadStream(file));
       const options = {
-        method: 'POST',
+        method: "POST",
         isStream: true,
         body: form,
       } as const;
@@ -72,20 +72,16 @@ export default function Command() {
     <Form
       actions={
         <ActionPanel>
-          <Action.SubmitForm
-            onSubmit={handleSubmit}
-            title="Submit"
-          />
+          <Action.SubmitForm onSubmit={handleSubmit} title="Submit" />
         </ActionPanel>
       }
     >
-      <Form.Description
-        title="Upload Image"
-        text="Only jpg / png / gif are allowed, one file at a time."
-      />
-      <Form.FilePicker id="image"
+      <Form.Description title="Upload Image" text="Only jpg / png / gif are allowed, one file at a time." />
+      <Form.FilePicker
+        id="image"
         title="Image"
-        value={files} error={fileError}
+        value={files}
+        error={fileError}
         onChange={onFileChange}
         allowMultipleSelection={false}
         storeValue={false}
