8a115462f4076aa5b1754e95c9a3d830862cd4c4
Luke Broad <lbroad@wonde.com>
2023-06-22 19:23:04 +0100

    Update one-time-secret extension
    - Add form validation and make it clearer what fields are required
    - Add screenshot

--- extensions/one-time-secret/src/index.tsx
+++ extensions/one-time-secret/src/index.tsx
@@ -1,5 +1,6 @@
 import { Form, ActionPanel, Action, Clipboard, showToast, Toast } from "@raycast/api";
 import got from "got";
+import { useState } from "react";
 
 type Values = {
   lifetime: number;
@@ -9,6 +10,14 @@ type Values = {
 };
 
 export default function Command() {
+  const [secretError, setSecretError] = useState<string | undefined>();
+
+  function dropSecretErrorIfNeeded() {
+    if (secretError && secretError.length > 0) {
+      setSecretError(undefined);
+    }
+  }
+
   async function handleSubmit(values: Values) {
     console.log(values);
 
@@ -62,10 +71,34 @@ export default function Command() {
         </ActionPanel>
       }
     >
-      <Form.TextArea id="secret" title="Secret*" placeholder="The secret to be sent" />
+      <Form.TextArea
+        id="secret"
+        title="Secret*"
+        placeholder="The secret to be sent"
+        info="Required"
+        error={secretError}
+        onChange={dropSecretErrorIfNeeded}
+        onBlur={(event) => {
+          const value = event.target.value;
+
+          if (value?.length === 0) {
+            setSecretError("You must provide a secret");
+          }
+        }}
+      />
       <Form.Separator />
-      <Form.TextField id="passphrase" title="Passphrase" placeholder="Something top sneaky" />
-      <Form.Dropdown id="lifetime" title="Lifetime" storeValue>
+      <Form.TextField
+        id="passphrase"
+        title="Passphrase"
+        placeholder="Something top sneaky"
+        info="Optional. Encrypt the secret with this value."
+      />
+      <Form.Dropdown
+        id="lifetime*"
+        title="Lifetime"
+        info="Required. How long should the secret be available for?"
+        storeValue
+      >
         <Form.Dropdown.Item value="300" title="5 minutes" />
         <Form.Dropdown.Item value="1800" title="30 minutes" />
         <Form.Dropdown.Item value="3600" title="1 hour" />
