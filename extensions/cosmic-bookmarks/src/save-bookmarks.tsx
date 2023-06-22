c4bc7297b54e1afc832a8eba6e724c874023d346
Karl Koch <karl@kejk.tech>
2023-06-22 19:52:07 +0100

    Fix: Update to use selection rather than clipboard

--- extensions/cosmic-bookmarks/src/save-bookmarks.tsx
+++ extensions/cosmic-bookmarks/src/save-bookmarks.tsx
@@ -1,7 +1,7 @@
-import { getPreferenceValues, Clipboard, showToast, Toast } from "@raycast/api";
-import { createBucketClient } from "@cosmicjs/sdk";
-import { load } from "cheerio";
-import fetch from "node-fetch";
+import { getPreferenceValues, Clipboard, showToast, Toast, getSelectedText } from '@raycast/api';
+import { createBucketClient } from '@cosmicjs/sdk';
+import { load } from 'cheerio';
+import fetch from 'node-fetch';
 
 interface Preferences {
   bucketSlug: string;
@@ -12,58 +12,45 @@ interface Preferences {
 export default async function Command() {
   const { bucketSlug, readKey, writeKey } = getPreferenceValues<Preferences>();
   try {
-    const url = (await Clipboard.readText())?.trim();
+    const selectedText = await getSelectedText();
+    const selectedURL = new URL(selectedText);
 
-    if (!url) {
-      console.error("Clipboard does not contain a URL");
+    if (!selectedURL) {
+      console.error('Clipboard does not contain a URL');
       await showToast({
         style: Toast.Style.Failure,
-        title: "Clipboard does not contain a URL",
-        message: "Copy a URL to your clipboard and try again",
+        title: 'Clipboard does not contain a URL',
+        message: 'Copy a URL to your clipboard and try again',
       });
       return;
     }
-    const data = await fetch(url);
+    const data = await fetch(selectedURL);
     if (!data) return null; // add this line to check if data is undefined
     const html = await data.text();
     const $ = load(html);
 
     const metaTitle = $('meta[name="title"]');
     const metaDescription = $('meta[name="description"]');
-    const pageTitle = $("title");
+    const pageTitle = $('title');
 
-    const title = metaTitle.attr("content") || pageTitle.text();
-    const description = metaDescription.attr("content") || "";
+    const title = metaTitle.attr('content') || pageTitle.text();
+    const description = metaDescription.attr('content') || '';
     const extractedUrl = data.url;
 
     console.log(title, description, extractedUrl);
 
-    await addBookmark(
-      bucketSlug,
-      readKey,
-      writeKey,
-      title ?? "",
-      description ?? "",
-      extractedUrl ?? ""
-    );
+    await addBookmark(bucketSlug, readKey, writeKey, title ?? '', description ?? '', extractedUrl ?? '');
     return {
       title,
       description,
       url: extractedUrl,
     };
   } catch (error) {
-    console.error("Error:", error);
+    console.error('Error:', error);
   }
 }
 
-async function addBookmark(
-  bucketSlug: string,
-  readKey: string,
-  writeKey: string,
-  title: string,
-  snippet: string,
-  url: string
-) {
+async function addBookmark(bucketSlug: string, readKey: string, writeKey: string, title: string, snippet: string, url: string) {
   const cosmic = createBucketClient({
     bucketSlug: bucketSlug,
     readKey: readKey,
@@ -78,12 +65,12 @@ async function addBookmark(
     if (isLoading) {
       await showToast({
         style: Toast.Style.Animated,
-        title: "Saving bookmark",
+        title: 'Saving bookmark',
         message: `Saving ${title}`,
       });
     }
     const create = await cosmic.objects.insertOne({
-      type: "bookmarks",
+      type: 'bookmarks',
       title: title,
       metadata: {
         snippet: snippet,
@@ -96,16 +83,16 @@ async function addBookmark(
     isLoading = false && success;
 
     if (!isLoading && success) {
-      await showToast({ title: "Saved bookmark", message: `Added ${title}` });
+      await showToast({ title: 'Saved bookmark', message: `Added ${title}` });
     } else {
       await showToast({
         style: Toast.Style.Failure,
         title: "Couldn't save bookmark",
-        message: "Try again later",
+        message: 'Try again later',
       });
     }
     Clipboard.clear();
   } catch (err) {
-    console.error("Error:", err);
+    console.error('Error:', err);
   }
 }
