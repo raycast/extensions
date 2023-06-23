d998f72aa32418988b106c776671ce797d96f8b3
Luis H. Moreno <l.mattoscapelmoreno@celonis.com>
2023-06-23 15:30:31 +0200

    Fixing issues

--- extensions/nuki/src/nuki-models.ts
+++ extensions/nuki/src/nuki-models.ts
@@ -1,7 +1,6 @@
 export interface Preferences {
   APIKey: string;
-};
-
+}
 
 export interface SmartLock {
   name: string;
--- extensions/nuki/src/nuki.tsx
+++ extensions/nuki/src/nuki.tsx
@@ -1,44 +1,44 @@
-import { Action, ActionPanel, List, closeMainWindow, getPreferenceValues } from '@raycast/api';
-import { useFetch } from '@raycast/utils';
-import { Preferences, SmartLock } from './nuki-models';
-import fetch from 'node-fetch';
+import { Action, ActionPanel, List, closeMainWindow, getPreferenceValues } from "@raycast/api";
+import { useFetch } from "@raycast/utils";
+import { Preferences, SmartLock } from "./nuki-models";
+import fetch from "node-fetch";
 
 export default function Nuki() {
-    const preferences = getPreferenceValues<Preferences>();
+  const preferences = getPreferenceValues<Preferences>();
 
-    const headers = {
-        Authorization: `Bearer ${preferences.APIKey}`,
-        'Content-Type': 'application/json',
-    };
+  const headers = {
+    Authorization: `Bearer ${preferences.APIKey}`,
+    "Content-Type": "application/json",
+  };
 
-    const { isLoading, data } = useFetch<SmartLock[]>('https://api.nuki.io/smartlock', { headers });
+  const { isLoading, data } = useFetch<SmartLock[]>("https://api.nuki.io/smartlock", { headers });
 
-    const openSmartlock = async (smartlockId: number) => {
-        await fetch(`https://api.nuki.io/smartlock/${smartlockId}/action`, {
-            headers,
-            method: 'POST',
-            body: JSON.stringify({
-                action: 3,
-                option: 0,
-            }),
-        });
-        closeMainWindow();
-    };
+  const openSmartlock = async (smartlockId: number) => {
+    await fetch(`https://api.nuki.io/smartlock/${smartlockId}/action`, {
+      headers,
+      method: "POST",
+      body: JSON.stringify({
+        action: 3,
+        option: 0,
+      }),
+    });
+    closeMainWindow();
+  };
 
-    return (
-        <List isLoading={ isLoading }>
-            { data?.map(({ name, smartlockId }) => (
-                <List.Item
-                    key={ smartlockId }
-                    icon="list-item-icon.png"
-                    title={ name }
-                    actions={
-                        <ActionPanel>
-                            <Action title={ `Open ${name}`} onAction={ () => openSmartlock(smartlockId) } />
-                        </ActionPanel>
-                    }
-                />
-            ))}
-        </List>
-    )
+  return (
+    <List isLoading={isLoading}>
+      {data?.map(({ name, smartlockId }) => (
+        <List.Item
+          key={smartlockId}
+          icon="list-item-icon.png"
+          title={name}
+          actions={
+            <ActionPanel>
+              <Action title={`Open ${name}`} onAction={() => openSmartlock(smartlockId)} />
+            </ActionPanel>
+          }
+        />
+      ))}
+    </List>
+  );
 }
