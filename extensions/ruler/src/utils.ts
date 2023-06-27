7d17dd2e7205a002a3bd6f8fd3b13b836dff37e2
Anwarul Islam <anwaarulislaam@gmail.com>
2023-06-27 21:58:41 +0600

    chore: removed development code

--- extensions/ruler/src/utils.ts
+++ extensions/ruler/src/utils.ts
@@ -8,17 +8,6 @@ export async function runRuler() {
   await chmod(command, "755");
 
   try {
-    if (environment.isDevelopment) {
-      const filePath = "/Users/anwar/Documents/projects/personal/ruler/Sources/ruler/main.swift";
-      // const filePath = "";
-      if (filePath) {
-        const { stdout } = await execa("swift", [filePath]);
-        return stdout;
-      }
-
-      throw new Error("No file path");
-    }
-
     const { stdout } = await execa(command);
     return stdout;
   } catch (error) {
