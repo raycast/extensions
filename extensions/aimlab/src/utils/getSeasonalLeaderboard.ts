4c3b47ad276a849c79230bebd7d265575a23b8a0
Joeri <verstraete@duck.com>
2023-06-29 12:57:52 +0200

    Run prettier

--- extensions/aimlab/src/utils/getSeasonalLeaderboard.ts
+++ extensions/aimlab/src/utils/getSeasonalLeaderboard.ts
@@ -1,14 +1,13 @@
 import { APIFetch, GET_SEASONAL_COMBINED_LEADERBOARD } from "./queries.js";
 
 type PropTypes = {
-  seasonId: string,
-  taskId: string,
-  weaponId: string,
-  modeId: number,
-}
+  seasonId: string;
+  taskId: string;
+  weaponId: string;
+  modeId: number;
+};
 
 const getSeasonalLeaderboard = async ({ seasonId, taskId, weaponId, modeId }: PropTypes) => {
-
   const res = await APIFetch(GET_SEASONAL_COMBINED_LEADERBOARD, {
     leaderboardInput: {
       clientId: "aimlab",
