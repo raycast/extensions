609f3a1091862d0c2e8714a490d5b9ed3b10a1c4
Joe Tannenbaum <joe@joe.codes>
2023-06-30 07:41:19 -0400

    fallback for when no items are explicitly selected in finder

--- extensions/warp-launch-configuration/src/open-selected-finder-item.tsx
+++ extensions/warp-launch-configuration/src/open-selected-finder-item.tsx
@@ -1,18 +1,43 @@
-import { getSelectedFinderItems, showToast, Toast, open } from "@raycast/api";
+import { getSelectedFinderItems, showToast, Toast, open, getFrontmostApplication } from "@raycast/api";
+import { runAppleScript } from "@raycast/utils";
 import fs from "node:fs/promises";
 import path from "node:path";
 import { newTab } from "./uri";
+
+const fallback = async (): Promise<boolean> => {
+  const app = await getFrontmostApplication();
+
+  if (app.name !== "Finder") {
+    return false;
+  }
+
+  const currentDirectory = await runAppleScript(
+    `tell application "Finder" to get POSIX path of (target of front window as alias)`
+  );
+
+  if (!currentDirectory) {
+    return false;
+  }
+
+  await open(newTab(currentDirectory));
+
+  return true;
+};
 
 export default async function Command() {
   try {
     const selectedItems = await getSelectedFinderItems();
 
     if (selectedItems.length === 0) {
-      await showToast({
-        style: Toast.Style.Failure,
-        title: "No directory selected",
-        message: "Please select a directory in Finder first",
-      });
+      const ranFallback = await fallback();
+
+      if (ranFallback === false) {
+        await showToast({
+          style: Toast.Style.Failure,
+          title: "No directory selected",
+          message: "Please select a directory in Finder first",
+        });
+      }
 
       return;
     }
