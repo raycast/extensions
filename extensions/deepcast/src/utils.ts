1da3aaa7583a59cb1b744b4d96e6623f6f87d224
Yamen Sharaf <hi@yamen.dev>
2023-07-03 14:40:09 +0200

    Adds a loading indicator toast

--- extensions/deepcast/src/utils.ts
+++ extensions/deepcast/src/utils.ts
@@ -59,6 +59,7 @@ export async function sendTranslateRequest({
 
     const { key, onTranslateAction } = getPreferences();
 
+    showToast(Toast.Style.Animated, "Fetching translation...");
     try {
       const {
         translations: [{ text: translation, detected_source_language: detectedSourceLanguage }],
