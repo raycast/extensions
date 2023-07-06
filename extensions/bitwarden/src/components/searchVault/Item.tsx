5a7b5bdcf1768fb7aa7e323f9b577847d71ff4cf
José Pereira <jmfp-b@live.com.pt>
2023-07-06 23:48:23 +0100

    refactor: move item icon and accessories hooks to a separate file

--- extensions/bitwarden/src/components/searchVault/Item.tsx
+++ extensions/bitwarden/src/components/searchVault/Item.tsx
@@ -1,16 +1,11 @@
-import { Color, getPreferenceValues, Icon, List } from "@raycast/api";
+import { List } from "@raycast/api";
 import { useMemo } from "react";
 import VaultItemActionPanel from "~/components/searchVault/ItemActionPanel";
 import VaultItemContext from "~/components/searchVault/context/vaultItem";
-import { ITEM_TYPE_TO_ICON_MAP } from "~/constants/general";
-import { ITEM_TYPE_TO_LABEL } from "~/constants/labels";
-import { useFavoritesContext } from "~/context/favorites";
-import { Folder, Item, ItemType, Reprompt } from "~/types/vault";
-import { getCardImageUrl } from "~/utils/cards";
-import { captureException } from "~/utils/development";
-import { extractKeywords, faviconUrl } from "~/utils/search";
-
-const { fetchFavicons } = getPreferenceValues();
+import { useItemAccessories } from "~/components/searchVault/utils/useItemAccessories";
+import { useItemIcon } from "~/components/searchVault/utils/useItemIcon";
+import { Folder, Item } from "~/types/vault";
+import { extractKeywords } from "~/utils/search";
 
 export type VaultItemProps = {
   item: Item;
@@ -18,9 +13,8 @@ export type VaultItemProps = {
 };
 
 const VaultItem = ({ item, folder }: VaultItemProps) => {
-  const icon = useIcon(item);
-  const accessories = useAccessories(item, folder);
-
+  const icon = useItemIcon(item);
+  const accessories = useItemAccessories(item, folder);
   const keywords = useMemo(() => extractKeywords(item), [item]);
 
   return (
@@ -37,89 +31,5 @@ const VaultItem = ({ item, folder }: VaultItemProps) => {
     </VaultItemContext.Provider>
   );
 };
-
-const ITEM_TYPE_TO_IMAGE_OR_ICON_MAP: Record<ItemType, (item: Item) => string | Icon> = {
-  [ItemType.LOGIN]: (item: Item) => {
-    const iconUri = item.login?.uris?.[0]?.uri;
-    if (fetchFavicons && iconUri) return faviconUrl(iconUri);
-    return ITEM_TYPE_TO_ICON_MAP[ItemType.LOGIN];
-  },
-  [ItemType.CARD]: (item: Item) => {
-    const { brand } = item.card ?? {};
-    if (brand) {
-      const cardBrandImage = getCardImageUrl(brand);
-      if (cardBrandImage) return cardBrandImage;
-    }
-    return ITEM_TYPE_TO_ICON_MAP[ItemType.CARD];
-  },
-  [ItemType.IDENTITY]: () => ITEM_TYPE_TO_ICON_MAP[ItemType.IDENTITY],
-  [ItemType.NOTE]: () => ITEM_TYPE_TO_ICON_MAP[ItemType.NOTE],
-};
-
-function useIcon(item: Item) {
-  return useMemo(() => {
-    const imageOrIcon = ITEM_TYPE_TO_IMAGE_OR_ICON_MAP[item.type]?.(item);
-    if (imageOrIcon) return imageOrIcon;
-    return Icon.QuestionMark;
-  }, [item.type, item.card?.brand, item.login?.uris?.[0]?.uri]);
-}
-
-type ListItemAccessory = NonNullable<List.Item.Props["accessories"]>[number];
-const TYPE_TO_ACCESSORY_MAP: Record<ItemType, ListItemAccessory> = {
-  [ItemType.LOGIN]: {
-    icon: { source: ITEM_TYPE_TO_ICON_MAP[ItemType.LOGIN], tintColor: Color.Blue },
-    tooltip: ITEM_TYPE_TO_LABEL[ItemType.LOGIN],
-  },
-  [ItemType.CARD]: {
-    icon: { source: ITEM_TYPE_TO_ICON_MAP[ItemType.CARD], tintColor: Color.Green },
-    tooltip: ITEM_TYPE_TO_LABEL[ItemType.CARD],
-  },
-  [ItemType.IDENTITY]: {
-    icon: { source: ITEM_TYPE_TO_ICON_MAP[ItemType.IDENTITY], tintColor: Color.Orange },
-    tooltip: ITEM_TYPE_TO_LABEL[ItemType.IDENTITY],
-  },
-  [ItemType.NOTE]: {
-    icon: { source: ITEM_TYPE_TO_ICON_MAP[ItemType.NOTE], tintColor: Color.PrimaryText },
-    tooltip: ITEM_TYPE_TO_LABEL[ItemType.NOTE],
-  },
-};
-
-function useAccessories(item: Item, folder: Folder | undefined) {
-  const { favoriteOrder } = useFavoritesContext();
-
-  return useMemo(() => {
-    try {
-      const accessories: ListItemAccessory[] = [];
-
-      if (folder?.id) {
-        accessories.push({
-          icon: { source: Icon.Folder, tintColor: Color.SecondaryText },
-          tag: { value: folder.name, color: Color.SecondaryText },
-          tooltip: `${folder.name} Folder`,
-        });
-      }
-
-      if (item.favorite) {
-        accessories.push({ icon: { source: Icon.Star, tintColor: Color.Blue }, tooltip: "Bitwarden Favorite" });
-      } else if (favoriteOrder.includes(item.id)) {
-        accessories.push({ icon: { source: Icon.Star, tintColor: Color.Yellow }, tooltip: "Favorite" });
-      }
-
-      if (item.reprompt === Reprompt.REQUIRED) {
-        accessories.push({
-          icon: { source: Icon.Lock, tintColor: Color.SecondaryText },
-          tooltip: "Reprompt Required",
-        });
-      }
-
-      accessories.push(TYPE_TO_ACCESSORY_MAP[item.type]);
-
-      return accessories;
-    } catch (error) {
-      captureException("Failed to get item accessories", error);
-      return [];
-    }
-  }, [favoriteOrder, item.favorite, item.reprompt, item.type, folder?.id, folder?.name]);
-}
 
 export default VaultItem;
