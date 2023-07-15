d3eab4eff9f24e412f79a9cbad0061d966c3738f
Yamen Sharaf <hi@yamen.dev>
2023-07-15 14:48:30 +0200

    Adds updated screenshots

--- extensions/real-debrid-manager/src/account-info.tsx
+++ extensions/real-debrid-manager/src/account-info.tsx
@@ -15,34 +15,39 @@ export const UserInfo = () => {
       isLoading={isLoading}
       markdown={userInfo ? readTrafficInfo(trafficInfo) : ""}
       metadata={
-        <Detail.Metadata>
-          <Detail.Metadata.Label title="Username" text={userInfo?.username} />
-          <Detail.Metadata.Separator />
-          <Detail.Metadata.Label title="Email" text={userInfo?.email} />
-          <Detail.Metadata.Separator />
-          <Detail.Metadata.TagList title="Premium Status">
-            <Detail.Metadata.TagList.Item
-              text={isPremium ? "Premium" : "Free"}
-              color={isPremium ? Color.Green : Color.Red}
-            />
-          </Detail.Metadata.TagList>
-          <Detail.Metadata.Label title="Premium Expiration Date" text={formatDateTime(userInfo?.expiration, "date")} />
-          {premiumRemainingDays && (
-            <Detail.Metadata.TagList title="Days Remaining">
+        userInfo && (
+          <Detail.Metadata>
+            <Detail.Metadata.Label title="Username" text={userInfo?.username} />
+            <Detail.Metadata.Separator />
+            <Detail.Metadata.Label title="Email" text={userInfo?.email} />
+            <Detail.Metadata.Separator />
+            <Detail.Metadata.TagList title="Premium Status">
               <Detail.Metadata.TagList.Item
-                text={premiumRemainingDays.toString()}
-                color={premiumRemainingDays > 30 ? Color.Green : Color.Red}
+                text={isPremium ? "Premium" : "Free"}
+                color={isPremium ? Color.Green : Color.Red}
               />
             </Detail.Metadata.TagList>
-          )}
-          <Detail.Metadata.Separator />
-          <Detail.Metadata.TagList title="Points">
-            <Detail.Metadata.TagList.Item
-              text={remainingPoints || "0"}
-              color={remainingPoints ? Color.Green : Color.PrimaryText}
+            <Detail.Metadata.Label
+              title="Premium Expiration Date"
+              text={formatDateTime(userInfo?.expiration, "date")}
             />
-          </Detail.Metadata.TagList>
-        </Detail.Metadata>
+            {premiumRemainingDays && (
+              <Detail.Metadata.TagList title="Days Remaining">
+                <Detail.Metadata.TagList.Item
+                  text={premiumRemainingDays.toString()}
+                  color={premiumRemainingDays > 30 ? Color.Green : Color.Red}
+                />
+              </Detail.Metadata.TagList>
+            )}
+            <Detail.Metadata.Separator />
+            <Detail.Metadata.TagList title="Points">
+              <Detail.Metadata.TagList.Item
+                text={remainingPoints || "0"}
+                color={remainingPoints ? Color.Green : Color.PrimaryText}
+              />
+            </Detail.Metadata.TagList>
+          </Detail.Metadata>
+        )
       }
     />
   );
