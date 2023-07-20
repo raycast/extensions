fe32a09b9c62e01fa33d8a3dd6964600a8017867
Stefano <s.cislaghi@gmail.com>
2023-07-20 13:51:20 +0100

    [Github For Enterprise] Dependencies (#7519)
    * chore: raycast API 1.55
    
    * chore: dependencies

--- extensions/github-for-enterprise/src/utils/index.ts
+++ extensions/github-for-enterprise/src/utils/index.ts
@@ -17,16 +17,19 @@ export async function fetcher({ document, variables = {}, headers = {} }: any) {
     {
       Authorization: `Bearer ${token}`,
       ...headers,
-    }
+    },
   );
 }
 
 export const groupBy = <T, K extends keyof T>(arr: T[], key: K) => {
-  return arr.reduce((r, a) => {
-    const v = a[key];
-    (r as any)[v] = [...((r as any)[v] || []), a];
-    return r;
-  }, {} as Record<string, T[]>);
+  return arr.reduce(
+    (r, a) => {
+      const v = a[key];
+      (r as any)[v] = [...((r as any)[v] || []), a];
+      return r;
+    },
+    {} as Record<string, T[]>,
+  );
 };
 
 export const partition = <T>(arr: T[], predicate: (item: T) => boolean) => {
