213ea58b5791d11e2cfd742cd5560b1a9bd9e80c
koin <36128970+koinzhang@users.noreply.github.com>
2023-07-25 00:46:02 +0800

    [Bugfix] Menubar  Weather (#7633)
    * [Bugfix] Menubar  Weather
    
    * Updated dependencies
    
    * Update CHANGELOG.md and optimise images
    
    ---------
    
    Co-authored-by: Per Nielsen Tikær <per@raycast.com>
    Co-authored-by: raycastbot <bot@raycast.com>

--- extensions/menubar-weather/src/hooks/hooks.ts
+++ extensions/menubar-weather/src/hooks/hooks.ts
@@ -1,7 +1,7 @@
 import { useCallback, useEffect, useState } from "react";
 import { getCurWeather } from "../utils/weather-utils";
-import { Cache, environment, LaunchType } from "@raycast/api";
-import { CacheKey, isEmpty, preferencesChanged, shouldRefresh } from "../utils/common-utils";
+import { Cache, environment, LaunchType, updateCommandMetadata } from "@raycast/api";
+import { CacheKey, getMenuItem, isEmpty, preferencesChanged, shouldRefresh } from "../utils/common-utils";
 import { GeoLocation, OpenMeteoWeather } from "../types/types";
 
 export const getCurrentWeather = () => {
@@ -18,7 +18,10 @@ export const getCurrentWeather = () => {
     let oldRefreshTime = 0;
     if (typeof cacheWeather === "string" && !isEmpty(cacheWeather)) {
       try {
-        setWeather(JSON.parse(cacheWeather) as OpenMeteoWeather);
+        const oldWeather = JSON.parse(cacheWeather) as OpenMeteoWeather;
+        setWeather(oldWeather);
+        const menuItems: string[] = getMenuItem(oldWeather);
+        updateCommandMetadata({ subtitle: menuItems.join(" | ") });
       } catch (e) {
         console.debug(`Could not parse cached weather: ${cacheWeather}`, e);
       }
@@ -38,7 +41,8 @@ export const getCurrentWeather = () => {
       }
     }
     const newRefreshTime = Date.now();
-    const isRefresh = shouldRefresh(oldRefreshTime, newRefreshTime) || preferencesChanged();
+    const isPreferencesChanged = preferencesChanged();
+    const isRefresh = shouldRefresh(oldRefreshTime, newRefreshTime) || isPreferencesChanged;
 
     if (isRefresh || environment.launchType === LaunchType.Background) {
       try {
@@ -46,6 +50,8 @@ export const getCurrentWeather = () => {
         if (typeof weather !== "undefined") {
           setWeather(weather);
           cache.set(CacheKey.CURRENT_WEATHER, JSON.stringify(weather));
+          const menuItems: string[] = getMenuItem(weather);
+          updateCommandMetadata({ subtitle: menuItems.join(" | ") });
         } else {
           cache.set(CacheKey.CURRENT_WEATHER, JSON.stringify(""));
         }
