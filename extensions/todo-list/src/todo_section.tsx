4964056e8e98df9bee093893b66d8c053d050c2a
Ben Keys <bkeys818@gmail.com>
2023-07-24 15:27:09 -0400

    ✨ [todo-list] Add preference for sorting items

--- extensions/todo-list/src/todo_section.tsx
+++ extensions/todo-list/src/todo_section.tsx
@@ -1,13 +1,20 @@
 import { List } from "@raycast/api";
 import { useAtom } from "jotai";
-import { todoAtom, TodoSections } from "./atoms";
-import { SECTIONS_DATA } from "./config";
+import { todoAtom, TodoSections, TodoItem } from "./atoms";
+import { SECTIONS_DATA, preferences } from "./config";
 import SingleTodoItem from "./todo_item";
 const TodoSection = ({ sectionKey }: { sectionKey: keyof TodoSections }) => {
   const [todoSections] = useAtom(todoAtom);
+
+  function sort(items: TodoItem[]) {
+    const { sortOrder } = preferences;
+    if (sortOrder == "creation_date_accending") return items;
+    else return items.sort((a, b) => b.timeAdded - a.timeAdded);
+  }
+
   return (
     <List.Section title={SECTIONS_DATA[sectionKey].name}>
-      {todoSections[sectionKey].map((item, i) => (
+      {sort(todoSections[sectionKey]).map((item, i) => (
         <SingleTodoItem item={item} key={i} idx={i} sectionKey={sectionKey} />
       ))}
     </List.Section>
