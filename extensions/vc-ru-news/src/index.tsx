6cb473c672340aadc4400423f344e1cfdcac181b
Gleb Shalimov <sbgleb10@gmail.com>
2023-07-26 15:35:20 +0300

    fix code style error

--- extensions/vc-ru-news/src/index.tsx
+++ extensions/vc-ru-news/src/index.tsx
@@ -33,8 +33,7 @@ export default function Command() {
   });
 
   return (
-    <List
-      isLoading={isLoading}>
+    <List isLoading={isLoading}>
       {data && data.items?.map((item, index) => <StoryListItem key={item.id} item={item} index={index} />)}
     </List>
   );
