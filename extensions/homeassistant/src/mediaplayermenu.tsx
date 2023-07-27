124606086b9a1672b5bdd26005c0bdb01a84054a
Michael Aigner <tonka3100@gmail.com>
2023-07-27 15:45:02 +0200

    fix wrong excludes

--- extensions/homeassistant/src/mediaplayermenu.tsx
+++ extensions/homeassistant/src/mediaplayermenu.tsx
@@ -3,16 +3,7 @@ import { getErrorMessage, getFriendlyName } from "./utils";
 import { useHAStates } from "./hooks";
 import { LaunchCommandMenubarItem, MenuBarItemConfigureCommand } from "./components/menu";
 import { MediaPlayerMenubarItem } from "./components/mediaplayer/menu";
-import { filterStates } from "./components/states/utils";
-
-function hiddenEntitiesPreferences(): string[] {
-  const prefs = getPreferenceValues();
-  const hidden: string | undefined = prefs.hiddenEntities;
-  if (!hidden) {
-    return [];
-  }
-  return (hidden.split(",").map((h) => h.trim()) || []).filter((h) => h.length > 0);
-}
+import { filterStates, hiddenEntitiesPreferences } from "./components/states/utils";
 
 export default function MediaPlayerMenuCommand(): JSX.Element {
   const { states, error, isLoading } = useHAStates();
