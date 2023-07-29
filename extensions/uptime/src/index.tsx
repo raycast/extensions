f181fbf511071606dd21248122f01950553aabde
yarnaid <yarnaid@gmail.com>
2023-07-29 15:50:43 +0400

    fix code style

--- extensions/uptime/src/index.tsx
+++ extensions/uptime/src/index.tsx
@@ -1,15 +1,15 @@
 import { Detail, ActionPanel, Action } from "@raycast/api";
-import util from 'util';
-import { exec } from 'child_process';
+import util from "util";
+import { exec } from "child_process";
 import { usePromise } from "@raycast/utils";
 import { useRef } from "react";
 
 const execp = util.promisify(exec);
 
 function highlightNumbersInMarkdown(markdown: string): string {
-  const numberRegex = /(\b(\d|\:|\.)+\b)/g;
+  const numberRegex = /(\b(\d|:|\.)+\b)/g;
 
-  const highlighted = markdown.replace(numberRegex, '**$1**');
+  const highlighted = markdown.replace(numberRegex, "**$1**");
 
   return highlighted;
 }
@@ -18,23 +18,25 @@ export default function Command() {
   const abortable = useRef<AbortController>();
   const { isLoading, data, revalidate } = usePromise(
     async () => {
-      const response = await execp("uptime")
+      const response = await execp("uptime");
       const result = highlightNumbersInMarkdown(await response.stdout.toString());
       return result;
     },
     [],
     {
       abortable,
-    }
+    },
   );
 
-  return <Detail
-  isLoading={isLoading}
-  markdown={`Uptime: ${data}`}
-  actions={
-    <ActionPanel>
-      <Action title="Reload" onAction={() => revalidate()} />
-    </ActionPanel>
-  }
-/>;
+  return (
+    <Detail
+      isLoading={isLoading}
+      markdown={`Uptime: ${data}`}
+      actions={
+        <ActionPanel>
+          <Action title="Reload" onAction={() => revalidate()} />
+        </ActionPanel>
+      }
+    />
+  );
 }
