0048f9d009cb8e35a1112713ba31a7ef12b17e03
maximilianzuern <max.zuern@gmail.com>
2023-08-04 23:19:59 +0200

    added List.Dropdown selector

--- extensions/reclaim-ai/src/list-tasks.tsx
+++ extensions/reclaim-ai/src/list-tasks.tsx
@@ -2,16 +2,43 @@ import { Color, Icon, List } from "@raycast/api";
 import { useTask } from "./hooks/useTask";
 import { Task } from "./types/task";
 import { useEffect, useMemo, useState } from "react";
+        
+// Dropdown Menu for Status types -> https://developers.raycast.com/api-reference/user-interface/list#list.dropdown
+type StatusDropdownProps = {
+  tasks: Task[];
+  onStatusChange: (newValue: string) => void;
+};
+
+const StatusDropdown = (props: StatusDropdownProps) => {
+  const { tasks, onStatusChange } = props;
+  const statusTypes = useMemo(() => Array.from(new Set(tasks.map((task) => task.status))), [tasks]); // using useMemo() to avoid re-rendering the list every time the status changes
+
+  return (
+    <List.Dropdown
+      tooltip="Select Status"
+      storeValue={true}
+      onChange={(newValue) => {
+        onStatusChange(newValue);
+      }}
+    >
+      <List.Dropdown.Section title="Status">
+        {statusTypes.map((statusType) => (
+          <List.Dropdown.Item key={statusType} title={statusType} value={statusType} />
+        ))}
+      </List.Dropdown.Section>
+    </List.Dropdown>
+  );
+};
+
 
 const TaskList = () => {
-  // const { data } = useTask().getAllTasks();
-  // const tasks = data;
-  
   const { getAllTasks } = useTask();
   const [tasks, setTasks] = useState<Task[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
+  const [selectedStatus, setSelectedStatus] = useState("");
 
   useEffect(() => {
+
     const fetchTasks = async () => {
       const tasks = await getAllTasks();
       setTasks(tasks);
@@ -20,21 +47,32 @@ const TaskList = () => {
     fetchTasks();
   }, [getAllTasks]);
 
+  const filteredTasks = useMemo(() => {
+    if (!selectedStatus) {
+      return tasks;
+    }
+
+    return tasks.filter((task) => task.status === selectedStatus);
+  }, [tasks, selectedStatus]);
+
   return (
-    <List
-    isLoading={isLoading}
+    <List 
+    // isLoading={isLoading}
+    navigationTitle="Search Tasks"
+    searchBarAccessory={
+      <StatusDropdown tasks={tasks} onStatusChange={setSelectedStatus} />
+    }
     >
-      {tasks?.map((task: Task) => (
+      {filteredTasks?.map((task: Task) => (
         <List.Item 
           key={task.id} 
           title={task.title} 
-          // subtitle={task.status}
           accessories={[
             { text: { value: task.status, color: Color.PrimaryText } },
             { tag: { value: (task.timeChunksRemaining / 4).toString() + "h", color: Color.Green }, tooltip: "Time remaining"},
             { tag: { value: new Date(task.due), color: Color.Red }, tooltip: "Due date" },
           ]}
-          />
+        />
       ))}
     </List>
   );
@@ -45,3 +83,4 @@ export default function Command() {
     <TaskList />
   );
 }
+
