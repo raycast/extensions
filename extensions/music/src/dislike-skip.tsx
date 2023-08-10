4af8b5ba6a3299ce306eb3692e269b544b93c0fa
Xan <octoxan@users.noreply.github.com>
2023-08-10 17:09:50 -0400

    change the code style and formatting of dislike + skip track to match the love + add to library command

--- extensions/music/src/dislike-skip.tsx
+++ extensions/music/src/dislike-skip.tsx
@@ -1,15 +1,11 @@
 import { pipe } from "fp-ts/lib/function";
+import * as TE from "fp-ts/TaskEither";
 
 import * as music from "./util/scripts";
 import { handleTaskEitherError } from "./util/utils";
 
-const dislikeAndSkipTrack = async () => {
-  await pipe(
-    music.currentTrack.dislike,
-    handleTaskEitherError("Could not dislike the track", "Disliked")
-  )();
-
-  await pipe(music.player.next, handleTaskEitherError("Failed to skip track", "Track skipped"))();
-};
-
-export default dislikeAndSkipTrack;
+export default pipe(
+  music.currentTrack.dislike,
+  TE.chain(() => pipe(music.player.next)),
+  handleTaskEitherError("Failed to dislike/skip the track", "Disliked & skipped")
+);
