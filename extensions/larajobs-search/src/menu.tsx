deaf27a91c8f5213cf0df5b2826630bf8e1a8ecd
Steve McDougall <juststevemcd@gmail.com>
2023-08-11 10:31:28 +0100

    Fixing linting issues

--- extensions/larajobs-search/src/menu.tsx
+++ extensions/larajobs-search/src/menu.tsx
@@ -22,29 +22,18 @@ export default function JobList() {
   const { data: jobs, error } = useFetch<Job[]>("https://larajobs.com/api/jobs");
 
   if (error) {
-    return (
-      <MenuBarExtra icon={Icon.Warning} title="Failed to fetch jobs" />
-    )
+    return <MenuBarExtra icon={Icon.Warning} title="Failed to fetch jobs" />;
   }
 
   if (!jobs) {
-    return (
-      <MenuBarExtra icon={Icon.MagnifyingGlass} title="Fetching Jobs"></MenuBarExtra>
-    )
+    return <MenuBarExtra icon={Icon.MagnifyingGlass} title="Fetching Jobs"></MenuBarExtra>;
   }
 
   return (
     <MenuBarExtra icon={Icon.Rocket} title="Larajobs" tooltip="See latest Larajobs">
-
       {jobs.map((job) => (
-        <MenuBarExtra.Item
-          key={job.id}
-          title={job.title}
-          icon={getFavicon(job.url)}
-          onAction={() => open(job.url)}
-        />
+        <MenuBarExtra.Item key={job.id} title={job.title} icon={getFavicon(job.url)} onAction={() => open(job.url)} />
       ))}
-
     </MenuBarExtra>
   );
 }
