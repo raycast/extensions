0f52627bf267f7272ee573ef5b378f09b2e95669
vaibhav <vaibhavguy40@gmail.com>
2023-08-10 18:20:13 -0700

    Add url-shortener extension
    - Commit 3
    - Commit 2
    - Commit 1
    - first commit

---
+++ extensions/url-shortener/README.md
@@ -0,0 +1,3 @@
+# URL Shortener
+
+URL Shortener shortens URLS for free with no API-Keys or other prior setup necessary.
---
+++ extensions/url-shortener/src/shorten-url.tsx
@@ -0,0 +1,42 @@
+import { getSelectedText, Clipboard, showToast, Toast, showHUD, getPreferenceValues } from "@raycast/api";
+import fetch from "node-fetch";
+
+interface Preferences {
+  domain: string;
+}
+
+export default async function Command() {
+  const preferences = getPreferenceValues<Preferences>();
+  try {
+    const url = await getSelectedText();
+    const regexURL = /^((ftp|smtp|file|data):\/\/)?[^\s$.?#].[^\s]*$/;
+
+    if (url === "") {
+      await showHUD("No text selected.");
+    } else if (!regexURL.test(url)) {
+      await showHUD("Selected text isn't a URL.");
+    } else {
+      showToast({
+        style: Toast.Style.Animated,
+        title: "Shortening URL",
+      });
+      const request = await fetch(`https://api.shrtco.de/v2/shorten?url=${encodeURIComponent(url)}`);
+      const response = await request.json();
+      console.log(url);
+      console.log(preferences.domain);
+      if (preferences.domain == "1") {
+        await Clipboard.paste(response.result.full_short_link);
+      } else if (preferences.domain == "2") {
+        await Clipboard.paste(response.result.full_short_link2);
+      } else {
+        await Clipboard.paste(response.result.full_short_link3);
+      }
+    }
+  } catch (error) {
+    await showToast({
+      style: Toast.Style.Failure,
+      title: "Cannot transform text",
+      message: String(error),
+    });
+  }
+}
