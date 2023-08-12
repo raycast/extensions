95d9792bda3e2e989f5fa35d526c953b70f12abc
Michael Aigner <tonka3100@gmail.com>
2023-08-12 21:14:23 +0200

    show more client infos

--- extensions/unifi/src/components/client/list.tsx
+++ extensions/unifi/src/components/client/list.tsx
@@ -1,4 +1,4 @@
-import { Action, ActionPanel, List } from "@raycast/api";
+import { Action, ActionPanel, Color, List } from "@raycast/api";
 import { useCachedPromise, useCachedState } from "@raycast/utils";
 import { Client, Site } from "unifi-client";
 import { showErrorToast } from "../../utils";
@@ -22,6 +22,14 @@ function ToggleDetailsAction(props: { showDetails?: boolean; setShowDetails?: (n
   );
 }
 
+function SingleDetailTag(props: { title: string; text: string; color?: Color.ColorLike | null | undefined }) {
+  return (
+    <List.Item.Detail.Metadata.TagList title={props.title}>
+      <List.Item.Detail.Metadata.TagList.Item text={props.text} color={props.color} />
+    </List.Item.Detail.Metadata.TagList>
+  );
+}
+
 function ClientListItem(props: {
   client: Client;
   showDetails?: boolean;
@@ -37,9 +45,30 @@ function ClientListItem(props: {
           <List.Item.Detail
             metadata={
               <List.Item.Detail.Metadata>
-                <List.Item.Detail.Metadata.Label title="IP Address" text={c.ip || "-"} />
                 <List.Item.Detail.Metadata.Label title="Hostname" text={c.hostname || "-"} />
+                <List.Item.Detail.Metadata.Label title="IP Address" text={c.ip || "-"} />
+                <SingleDetailTag title="IP Allocation" text={c.useFixedIp === true ? "Fixed" : "Dynamic"} />
+                <List.Item.Detail.Metadata.Label title="MAC Address" text={c.mac ? c.mac : "-"} />
                 <List.Item.Detail.Metadata.Label title="Vendor" text={c.oui || "-"} />
+                <List.Item.Detail.Metadata.Label
+                  title="Last Seen"
+                  text={c.lastSeen ? new Date(c.lastSeen).toLocaleString() : "-"}
+                />
+                <SingleDetailTag title="Guest" text={c.isGuest === true ? "Yes" : "No"} />
+                <List.Item.Detail.Metadata.Label
+                  title="Channel"
+                  text={c.channel === undefined ? "-" : c.channel.toString()}
+                />
+                <List.Item.Detail.Metadata.Label
+                  title="Noise"
+                  text={c.noise === undefined ? "?" : c.noise.toString()}
+                />
+
+                <SingleDetailTag
+                  title="Access"
+                  text={c.blocked === true ? "Blocked" : "Granted"}
+                  color={c.blocked === true ? Color.Red : Color.Green}
+                />
               </List.Item.Detail.Metadata>
             }
           />
