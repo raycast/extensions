731a772d48d685662e336782b0614ed533d1b8a5
Sean Nilan <snilan@method.actor>
2023-08-14 14:28:13 -0400

    fix: removed id by mistake

--- extensions/warp/src/workflow-util.ts
+++ extensions/warp/src/workflow-util.ts
@@ -10,6 +10,7 @@ export const WORKFLOW_QUERY = `SELECT id, data FROM workflows`;
 export const EXTENSION_URI = "raycast://extensions/warpdotdev/warp";
 
 export interface Workflow {
+  id: number;
   name: string;
   description: string | null;
   command: string;
@@ -33,6 +34,32 @@ export const showError = async (title: string, message: string) => {
     title,
     message,
   });
+};
+
+interface Marker {
+  index: number;
+  variable: string;
+}
+
+export const getWorkflowMarkers = (workflow: Workflow): Marker[] => {
+  const command = workflow.command;
+  const markers = workflow.arguments.flatMap((arg) => {
+    const matches = [...command.matchAll(new RegExp(`{{${arg.name}}}`, "g"))];
+    return matches.map((m) => ({ index: m.index!, variable: arg.name }));
+  });
+
+  return markers.sort((a, b) => a.index - b.index);
+};
+
+export const fillWorkflowCommand = (command: string, markers: Marker[], values: Record<string, string>) => {
+  return markers.length > 0
+    ? markers.reduce((text, marker, i) => {
+        const { index, variable } = marker;
+        const value = values[variable] || `{{${variable}}}`;
+        const after = command.slice(index + variable.length + 4, markers[i + 1]?.index);
+        return text + value + after;
+      }, command.slice(0, markers[0].index))
+    : command;
 };
 
 export const executeShellCommand = async (command: string) => {
