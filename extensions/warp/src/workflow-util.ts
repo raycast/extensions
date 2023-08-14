e0ea3280849a4cd99055a8b9d23cdcb5a48faef2
Sean Nilan <snilan@method.actor>
2023-08-14 14:30:35 -0400

    Update warp extension
    - fix: removing code
    - feat: added run workflow action and reorganized code
    - fix: removed id by mistake
    - fix: removing unnecessary logic - simple regex replace should be okay
    - Merge branch 'contributions/merge-1692026609172591000'
    - Pull contributions
    - feat: added run workflow action and reorganized code

--- extensions/warp/src/workflow-util.ts
+++ extensions/warp/src/workflow-util.ts
@@ -36,32 +36,6 @@ export const showError = async (title: string, message: string) => {
   });
 };
 
-interface Marker {
-  index: number;
-  variable: string;
-}
-
-export const getWorkflowMarkers = (workflow: Workflow): Marker[] => {
-  const command = workflow.command;
-  const markers = workflow.arguments.flatMap((arg) => {
-    const matches = [...command.matchAll(new RegExp(`{{${arg.name}}}`, "g"))];
-    return matches.map((m) => ({ index: m.index!, variable: arg.name }));
-  });
-
-  return markers.sort((a, b) => a.index - b.index);
-};
-
-export const fillWorkflowCommand = (command: string, markers: Marker[], values: Record<string, string>) => {
-  return markers.length > 0
-    ? markers.reduce((text, marker, i) => {
-        const { index, variable } = marker;
-        const value = values[variable] || `{{${variable}}}`;
-        const after = command.slice(index + variable.length + 4, markers[i + 1]?.index);
-        return text + value + after;
-      }, command.slice(0, markers[0].index))
-    : command;
-};
-
 export const executeShellCommand = async (command: string) => {
   let stdout = "",
     stderr = "";
