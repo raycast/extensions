a26942bc268aa62458a7d943600da8f566778be4
Per Nielsen Tikær <per@raycast.com>
2023-08-16 09:21:39 +0200

    Changed code a bit

--- extensions/url-shortener/src/shorten-url.jsx
+++ extensions/url-shortener/src/shorten-url.jsx
@@ -4,7 +4,7 @@ import fetch from "node-fetch";
 export default async function Command() {
   const preferences = getPreferenceValues();
   try {
-    const url = await getSelectedText();
+    let url = await getSelectedText();
     const regexURL = /^((ftp|smtp|file|data):\/\/)?[^\s$.?#].[^\s]*$/;
 
     if (url === "") {
@@ -16,41 +16,48 @@ export default async function Command() {
         style: Toast.Style.Animated,
         title: "Shortening URL",
       });
-      const request = await fetch(`https://api.shrtco.de/v2/shorten?url=${encodeURIComponent(url)}`);
-      const response = await request.json();
-      let urlString = "";
 
-      switch (preferences.domain) {
-        case "1":
-        case "2":
-        case "3":
-          if (preferences.domain == "1") {
-            await Clipboard.paste(response.result.full_short_link);
-          } else if (preferences.domain == "2") {
-            await Clipboard.paste(response.result.full_short_link2);
-          } else if (preferences.domain == "3") {
-            await Clipboard.paste(response.result.full_short_link3);
-          }
-          break;
-        default:
-          urlString = async (url) => {
-            const response = await fetch(`https://tinyurl.com/api-create.php?url=${url}`);
-            return response.text();
-          };
-          if (preferences.clipboard == "1") {
-            await Clipboard.paste(await urlString(url));
-          } else {
-            await Clipboard.copy(await urlString(url));
-            await showHUD("Copied url to clipboard");
-          }
-          break;
+      let URLString = "";
+
+      if (["1", "2", "3"].includes(preferences.domain)) {
+        const request = await fetch(`https://api.shrtco.de/v2/shorten?url=${encodeURIComponent(url)}`);
+        const response = await request.json();
+
+        if (preferences.domain == "1") {
+          URLString = response.result.full_short_link;
+        } else if (preferences.domain == "2") {
+          URLString = response.result.full_short_link2;
+        } else if (preferences.domain == "3") {
+          URLString = response.result.full_short_link3;
+        }
+      } else {
+        const getURLString = async (url) => {
+          const response = await fetch(`https://tinyurl.com/api-create.php?url=${url}`);
+          return response.text();
+        };
+
+        URLString = await getURLString(url);
+        if (URLString.includes("Error")) {
+          await showToast({
+            style: Toast.Style.Failure,
+            title: "Cannot transform text",
+          });
+          return;
+        }
+      }
+
+      if (preferences.clipboard == "1") {
+        await Clipboard.paste(URLString);
+        await showHUD("Pasted url to active window");
+      } else {
+        await Clipboard.copy(URLString);
+        await showHUD("Copied url to clipboard");
       }
     }
   } catch (error) {
     await showToast({
       style: Toast.Style.Failure,
-      title: "Cannot transform text",
-      message: String(error),
+      title: "Can't get selected text",
     });
   }
 }
