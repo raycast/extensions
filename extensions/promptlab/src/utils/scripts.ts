af13af9c2be3428c3c1c2f555022911d27b0fc94
Stephen Kaplan <stephen.kaplan@maine.edu>
2023-08-17 00:14:37 -0400

    Remove debug logging ;)

--- extensions/promptlab/src/utils/scripts.ts
+++ extensions/promptlab/src/utils/scripts.ts
@@ -329,7 +329,6 @@ export const ScriptRunner = {
    */
   ScreenCapture: async (windowOnly = false) => {
     const tempPath = path.join(os.tmpdir(), "screenshot.png");
-    console.log("fuck");
     await (runScript("ScreenCapture", ReturnType.STRING, "JavaScript", tempPath, windowOnly) as Promise<string>);
     const data = await ScriptRunner.ImageFeatureExtractor(tempPath, true, true, true, true, false, false, 0.7);
     await fs.promises.rm(tempPath);
