4e083c4d68c46be414ba2e3a473ef80b76163ab2
Vaibhav Satishkumar <78756662+Visual-Studio-Coder@users.noreply.github.com>
2023-08-18 13:49:13 -0700

    Update spoiler-converter extension
    - adasda
    - emoji support + app detection

--- extensions/spoiler-converter/src/index.tsx
+++ extensions/spoiler-converter/src/index.tsx
@@ -1,10 +1,9 @@
 import { getSelectedText, showToast, Toast, Clipboard, getFrontmostApplication, showHUD } from "@raycast/api";
-import { title } from "process";
 
 export default async function Command() {
   const frontmostApplication = await getFrontmostApplication();
   if (frontmostApplication.name == "Discord") {
-  try {
+    try {
       const selectedText = await getSelectedText();
       //selectedText = paper
       // ||p
@@ -21,27 +20,26 @@ export default async function Command() {
     }
 
     function sandwichCharacters(str: string): string {
-      const parts = str.split(':'); // Split the string into parts containing "::"
+      const parts = str.split(":"); // Split the string into parts containing "::"
       const wrappedParts = parts.map((part, index) => {
         if (index % 2 === 0) {
           // Even-indexed parts are outside the "::"
           const characters = Array.from(part); // Convert part to an array of characters
-          return characters.map((char) => `||${char}||`).join(''); // Wrap and combine characters
+          return characters.map((char) => `||${char}||`).join(""); // Wrap and combine characters
         } else {
           // Odd-indexed parts are inside the "::"
-          if (part.includes(' ')) {
-            const parts2 = str.split('');
+          if (part.includes(" ")) {
+            const parts2 = str.split("");
             const partArr = Array.from(parts2);
-            return partArr.map((char) => `||${char}||`).join(''); // Wrap the part with spoilers if it contains a space
+            return partArr.map((char) => `||${char}||`).join(""); // Wrap the part with spoilers if it contains a space
           } else {
             return `||:${part}:||`; // Leave the part unchanged without wrapping
           }
         }
       });
-      return wrappedParts.join(''); // Combine wrapped parts into a single string using double colons "::"
+      return wrappedParts.join(""); // Combine wrapped parts into a single string using double colons "::"
     }
   } else {
     await showHUD("This extension only works on Discord");
   }
-
 }
