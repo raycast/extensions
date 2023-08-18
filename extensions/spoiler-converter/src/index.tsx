83f44fa0c643232ed4142402628b36d11df5cba6
Vaibhav Satishkumar <78756662+Visual-Studio-Coder@users.noreply.github.com>
2023-08-18 13:58:18 -0700

    bug fix

--- extensions/spoiler-converter/src/index.tsx
+++ extensions/spoiler-converter/src/index.tsx
@@ -5,11 +5,6 @@ export default async function Command() {
   if (frontmostApplication.name == "Discord") {
     try {
       const selectedText = await getSelectedText();
-      //selectedText = paper
-      // ||p
-      //  p||||e
-      // r||
-      //||||
       Clipboard.paste(sandwichCharacters(selectedText));
     } catch (error) {
       await showToast({
@@ -18,28 +13,27 @@ export default async function Command() {
         message: String(error),
       });
     }
-
-    function sandwichCharacters(str: string): string {
-      const parts = str.split(":"); // Split the string into parts containing "::"
-      const wrappedParts = parts.map((part, index) => {
-        if (index % 2 === 0) {
-          // Even-indexed parts are outside the "::"
-          const characters = Array.from(part); // Convert part to an array of characters
-          return characters.map((char) => `||${char}||`).join(""); // Wrap and combine characters
-        } else {
-          // Odd-indexed parts are inside the "::"
-          if (part.includes(" ")) {
-            const parts2 = str.split("");
-            const partArr = Array.from(parts2);
-            return partArr.map((char) => `||${char}||`).join(""); // Wrap the part with spoilers if it contains a space
-          } else {
-            return `||:${part}:||`; // Leave the part unchanged without wrapping
-          }
-        }
-      });
-      return wrappedParts.join(""); // Combine wrapped parts into a single string using double colons "::"
-    }
   } else {
     await showHUD("This extension only works on Discord");
+  }
+  function sandwichCharacters(str: string): string {
+    const parts = str.split(":"); // Split the string into parts containing "::"
+    const wrappedParts = parts.map((part, index) => {
+      if (index % 2 === 0) {
+        // Even-indexed parts are outside the "::"
+        const characters = Array.from(part); // Convert part to an array of characters
+        return characters.map((char) => `||${char}||`).join(""); // Wrap and combine characters
+      } else {
+        // Odd-indexed parts are inside the "::"
+        if (part.includes(" ")) {
+          const parts2 = str.split("");
+          const partArr = Array.from(parts2);
+          return partArr.map((char) => `||${char}||`).join(""); // Wrap the part with spoilers if it contains a space
+        } else {
+          return `||:${part}:||`; // Leave the part unchanged without wrapping
+        }
+      }
+    });
+    return wrappedParts.join(""); // Combine wrapped parts into a single string using double colons "::"
   }
 }
