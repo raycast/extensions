d429d147c155da625e8be13f618d10339fd7a0e9
mathieudutour <mathieu@dutour.me>
2023-08-22 15:22:37 +0200

    Fix posting comment in reward script

--- scripts/reward.js
+++ scripts/reward.js
@@ -30,7 +30,7 @@ module.exports = async ({ github, context, fetch }) => {
 
     if (!result.user_id) {
       await github.rest.issues.createComment({
-        issue_number: context.issue.number,
+        issue_number: pr.number,
         owner: context.repo.owner,
         repo: context.repo.repo,
         body: ":tada: :tada: :tada:\n\nSuch a great contribution deserves a reward, but unfortunately we couldn't find your Raycast account based on your GitHub username.\nPlease [link your GitHub account to your Raycast account](https://www.raycast.com/settings/account) to receive your credits and soon be able to exchange them for some swag.",
