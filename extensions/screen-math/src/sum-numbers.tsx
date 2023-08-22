494a0e1b9ebb776766fda5c04caa731c6172d90e
Can Celik <celik@me.com>
2023-08-22 13:17:32 +0300

    Change to no-view logic

--- extensions/screen-math/src/sum-numbers.tsx
+++ extensions/screen-math/src/sum-numbers.tsx
@@ -1,16 +1,10 @@
-import { Detail, environment, showHUD, useNavigation } from "@raycast/api";
+import { Clipboard, environment, showHUD } from "@raycast/api";
 import { exec } from "child_process";
-import { useState, useEffect } from "react";
 import fs from "fs";
 import { chmod } from "fs/promises";
 import { join } from "path";
 
-export default function command() {
-  const { pop } = useNavigation();
-  const [isLoading, setLoading] = useState(false);
-  const [markdown, setMarkdown] = useState("");
-  const [formattedTotal, setFormattedTotal] = useState("0.00");
-
+export default async function command() {
   const locale = Intl.DateTimeFormat().resolvedOptions().locale;
 
   const handleError = (error: Error, message: string): void => {
@@ -21,9 +15,6 @@ export default function command() {
     if (message) {
       showHUD(message).then();
     }
-
-    setLoading(false);
-    pop();
   };
 
   const formatNumber = (n: number): string => {
@@ -53,47 +44,28 @@ export default function command() {
     return numbersFound;
   };
 
-  const displayNumbers = (numbers: number[]): void => {
-    const markdown = `
-| **Numbers Found** |
-| ------ |
-${numbers.map((n) => `| ${formatNumber(n)} |`).join("\n")}`;
+  const getScreenshot = async (filename: string): Promise<void> => {
+    return new Promise((resolve, reject) => {
+      exec(`/usr/sbin/screencapture -i -x ${filename}`, async (error) => {
+        if (error) {
+          handleError(error, "❌ There was an error while taking the screenshot.");
 
-    setMarkdown(markdown);
+          reject(error);
+        }
+
+        resolve();
+      });
+    });
   };
 
-  /**
-   * This is a workaround until Raycast API support openMainWindow()
-   */
-  const reopenRayCast = (): void => {
-    exec("open raycast://reopen");
-  };
-
-  useEffect(() => {
-    setLoading(true);
-
-    showHUD("Select an area with numbers to sum them up.").then();
-
-    const filename = `${environment.assetsPath}/sum-numbers-${Date.now()}.png`;
-
-    exec(`/usr/sbin/screencapture -i -x ${filename}`, async (error) => {
-      if (error) {
-        handleError(error, "❌ There was an error while taking the screenshot.");
-
-        return;
-      }
-
-      const command = join(environment.assetsPath, "screen-math");
-      await chmod(command, "755");
-
+  const findNumbers = async (filename: string): Promise<number[]> => {
+    return new Promise((resolve, reject) => {
       exec(`${command} ${filename}`, (error, stdout) => {
         if (error) {
           handleError(error, "❌ There was an error while processing the screenshot.");
 
-          return;
+          reject(error);
         }
-
-        reopenRayCast();
 
         // Remove surrounding brackets and split into an array
         const stringWithoutBrackets = stdout.slice(1, -1);
@@ -105,33 +77,30 @@ ${numbers.map((n) => `| ${formatNumber(n)} |`).join("\n")}`;
         // const numbers = JSON.parse(stdout);
         const numbersFound = processNumbers(trimmedArray);
 
-        displayNumbers(numbersFound);
-
-        const total = numbersFound.reduce((a, b) => a + b, 0);
-        const formattedTotal = formatNumber(total);
-
-        setFormattedTotal(formattedTotal);
-
-        showHUD(`✅ Total is ${formattedTotal}`);
-
-        if (fs.existsSync(filename)) {
-          fs.unlinkSync(filename);
-        }
-
-        setLoading(false);
+        resolve(numbersFound);
       });
     });
-  }, []);
+  };
 
-  return (
-    <Detail
-      isLoading={isLoading}
-      markdown={markdown}
-      metadata={
-        <Detail.Metadata>
-          <Detail.Metadata.Label title="Total" text={formattedTotal} />
-        </Detail.Metadata>
-      }
-    />
-  );
+  await showHUD("Select an area with numbers to sum them up.");
+
+  const filename = `${environment.assetsPath}/sum-numbers-${Date.now()}.png`;
+
+  await getScreenshot(filename);
+
+  const command = join(environment.assetsPath, "screen-math");
+  await chmod(command, "755");
+
+  const numbersFound = await findNumbers(filename);
+
+  const total = numbersFound.reduce((a, b) => a + b, 0);
+  const formattedTotal = formatNumber(total);
+
+  await showHUD(`✅ Total is ${formattedTotal}`);
+
+  await Clipboard.copy(`${numbersFound.join("\n")}\n\nTotal\n${total}`);
+
+  if (fs.existsSync(filename)) {
+    fs.unlinkSync(filename);
+  }
 }
