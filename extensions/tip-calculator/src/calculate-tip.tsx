554843a0ce138ad18a3f8d54e65b99ce70711d7c
Per Nielsen Tikær <per@raycast.com>
2023-08-22 11:54:01 +0200

    Update calculate-tip.tsx

--- extensions/tip-calculator/src/calculate-tip.tsx
+++ extensions/tip-calculator/src/calculate-tip.tsx
@@ -1,9 +1,9 @@
-import { Detail, LaunchProps, showToast, Toast, List, popToRoot, Icon, Color, getPreferenceValues } from "@raycast/api";
+import { LaunchProps, showToast, Toast, List, popToRoot, Icon, Color, getPreferenceValues } from "@raycast/api";
 
 export default function Command(props: LaunchProps) {
   const { bill, tip, people } = props.arguments;
 
-  const { currency } = getPreferenceValues();
+  const { currency, currencyPosition } = getPreferenceValues<ExtensionPreferences>();
   if (!Number.isNaN(bill) && bill >= 0 && !Number.isNaN(tip) && tip >= 0 && !Number.isNaN(people) && people > 0) {
     const billAmount = parseFloat(bill);
     const tipPercentage = parseFloat(tip);
@@ -12,23 +12,31 @@ export default function Command(props: LaunchProps) {
     const tipPerPerson = ((billAmount * tipPercentage) / 100 / peopleCount).toFixed(2);
     const totalPerPerson = ((billAmount * tipPercentage) / 100 / peopleCount + billAmount / peopleCount).toFixed(2);
 
+    const currencyFormatter = (amount: string) => {
+      return currencyPosition === "prepend" ? `${currency}${amount}` : `${amount}${currency}`;
+    };
+
     return (
       <List navigationTitle="Calculate Tip" searchBarPlaceholder="Search Information...">
         <List.Section title="Inputted Values">
           <List.Item
             title="Bill (Total)"
             icon={Icon.Receipt}
-            accessories={[{ text: `${currency}${billAmount.toFixed(2)}` }]}
+            accessories={[{ text: currencyFormatter(billAmount.toFixed(2)) }]}
           />
           <List.Item title="Tip %" icon={Icon.Coin} accessories={[{ text: `${tip}%` }]} />
           <List.Item title="Number of People" icon={Icon.TwoPeople} accessories={[{ text: `${people} people` }]} />
         </List.Section>
         <List.Section title="Results">
-          <List.Item title="Tip Per Person" icon={Icon.Coins} accessories={[{ text: `${currency}${tipPerPerson}` }]} />
+          <List.Item
+            title="Tip Per Person"
+            icon={Icon.Coins}
+            accessories={[{ text: currencyFormatter(tipPerPerson) }]}
+          />
           <List.Item
             title="Total Per Person"
             icon={Icon.Coins}
-            accessories={[{ text: `${currency}${totalPerPerson}` }]}
+            accessories={[{ text: currencyFormatter(totalPerPerson) }]}
           />
         </List.Section>
       </List>
