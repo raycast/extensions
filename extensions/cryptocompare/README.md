5e48d382fd417cdd137cd8dfa6c608e2dbac8166
Xiaokang Fan <xiaokang.fan@eigen.capital>
2023-08-23 10:38:26 +0800

    Update cryptocompare extension
    - Usage
    - getList
    - getActions

--- extensions/cryptocompare/README.md
+++ extensions/cryptocompare/README.md
@@ -4,7 +4,7 @@ Use cryptocompare to check the price of cryptocurrencies.
 
 cryptocompare API Doc: https://min-api.cryptocompare.com/documentation?key=Price&cat=multipleSymbolsFullPriceEndpoint
 
-### 使用
+### Usage
 
 1. Type `price` in the input box and enter to search for price. Based on the input in the search box, the cryptocompare plugin will automatically go to cryptocompare to look up the price and return it as a list.
 2. You can also use `Add Favorite` to add currency favorites, and then directly search the prices of the favorite coins to reduce the trouble of searching. Use `Remove Favorite` to remove favorites;
--- extensions/cryptocompare/src/price.tsx
+++ extensions/cryptocompare/src/price.tsx
@@ -26,6 +26,75 @@ export default function Command() {
     fetchDataAsync();
   }, []);
 
+  function getActions(item: Data) {
+    const actions = [
+      <Action.CopyToClipboard title="Copy Price" content={item.price} onCopy={() => item.price} />,
+      <Action
+        title={item.favorite ? "Remove From Favorite" : "Add To Favorite"}
+        icon={item.favorite ? "remove.png" : "favorite.png"}
+        onAction={async () => {
+          setPriceData((priceData) =>
+            priceData.map((i) => {
+              if (i.name === item.name) {
+                return { ...i, favorite: !item.favorite };
+              }
+              return i;
+            })
+          );
+          if (item.favorite) {
+            await removeFavoriteCrypto(item.name);
+          } else {
+            addFavoriteCrypto(item.name);
+          }
+        }}
+      />,
+      <Action
+        title="Refresh"
+        icon="refresh.png"
+        onAction={async () => {
+          setIsLoading(true);
+          setPriceData(await fetchData(""));
+          setIsLoading(false);
+        }}
+      />,
+    ];
+    return <ActionPanel>{...actions}</ActionPanel>;
+  }
+
+  function getList() {
+    const listItemList: Array<any> = [];
+    const listSectionList: Array<any> = [];
+    priceData.map((item: Data) => {
+      if (item.favorite) {
+        listSectionList.push(
+          <List.Item
+            key={item.name}
+            title={item.name}
+            icon={{ source: item.icon }}
+            accessories={item.favorite ? [{ icon: "favorited.png", tooltip: "Favorited" }] : []}
+            subtitle={{ value: item.price, tooltip: item.markets }}
+            actions={getActions(item)}
+          />
+        );
+      } else {
+        listItemList.push(
+          <List.Item
+            key={item.name}
+            title={item.name}
+            icon={{ source: item.icon }}
+            accessories={item.favorite ? [{ icon: "favorited.png", tooltip: "Favorited" }] : []}
+            subtitle={{ value: item.price, tooltip: item.markets }}
+            actions={getActions(item)}
+          />
+        );
+      }
+    });
+    return [
+      <List.Section title="Favorited">{...listSectionList}</List.Section>,
+      <List.Section title="Not Favorited">{...listItemList}</List.Section>,
+    ];
+  }
+
   return (
     <List
       isLoading={isLoading}
@@ -52,49 +121,7 @@ export default function Command() {
       throttle={true}
       searchBarPlaceholder="Search for prices of digital coins"
     >
-      {priceData.map((item) => (
-        <List.Item
-          key={item.name}
-          title={item.name}
-          icon={{ source: item.icon }}
-          accessories={item.favorite ? [{ icon: "favorited.png", tooltip: "Favorited" }] : []}
-          subtitle={{ value: item.price, tooltip: item.markets }}
-          actions={
-            <ActionPanel>
-              <Action.CopyToClipboard title="Copy Price" content={item.price} onCopy={() => item.price} />
-              <Action
-                title={item.favorite ? "Remove From Favorite" : "Add To Favorite"}
-                icon={item.favorite ? "remove.png" : "favorite.png"}
-                onAction={async () => {
-                  setPriceData((priceData) =>
-                    priceData.map((i) => {
-                      if (i.name === item.name) {
-                        return { ...i, favorite: !item.favorite };
-                      }
-                      return i;
-                    })
-                  );
-                  if (item.favorite) {
-                    await removeFavoriteCrypto(item.name);
-                  } else {
-                    addFavoriteCrypto(item.name);
-                  }
-                }}
-              />
-
-              <Action
-                title="Refresh"
-                icon="refresh.png"
-                onAction={async () => {
-                  setIsLoading(true);
-                  setPriceData(await fetchData(""));
-                  setIsLoading(false);
-                }}
-              />
-            </ActionPanel>
-          }
-        />
-      ))}
+      {...getList()}
     </List>
   );
 }
