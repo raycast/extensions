b4ce77047d4614b699e8559cba75f7f89dcb84bb
Per Nielsen Tikær <per@raycast.com>
2023-08-23 10:17:07 +0200

    Added try catch

--- extensions/counter/src/decrement-counter.jsx
+++ extensions/counter/src/decrement-counter.jsx
@@ -1,12 +1,15 @@
-import { updateCommandMetadata } from "@raycast/api";
-import { LocalStorage, launchCommand, LaunchType } from "@raycast/api";
+import { updateCommandMetadata, LocalStorage, launchCommand, LaunchType } from "@raycast/api";
 
 export default async function Command({ launchContext }) {
   let counter = parseInt((await LocalStorage.getItem("counter")) ?? 0);
   if (!launchContext?.noUpdate) {
     counter--;
     await LocalStorage.setItem("counter", counter);
-    await launchCommand({ name: "increment-counter", type: LaunchType.Background, context: { noUpdate: true } });
+    try {
+      await launchCommand({ name: "increment-counter", type: LaunchType.Background, context: { noUpdate: true } });
+    } catch (error) {
+      console.log(error);
+    }
   }
   await updateCommandMetadata({ subtitle: `Value: ${counter}` });
 }
