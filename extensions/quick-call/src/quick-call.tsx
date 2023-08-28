3a2f037a4764f97ebc950149e739d5bc3a1393f8
Ali Karbassi <ali@karbassi.com>
2023-08-28 09:02:55 -0500

    Change mode from view to no-view. Remove US-centric validation

--- extensions/quick-call/src/quick-call.tsx
+++ extensions/quick-call/src/quick-call.tsx
@@ -1,69 +1,39 @@
-import { Action, ActionPanel, closeMainWindow, Form, popToRoot, showToast, Toast } from "@raycast/api";
+import { getSelectedText, LaunchProps, showHUD } from "@raycast/api";
 import open from "open";
 
-interface CommandForm {
-  number: string;
-}
+export default async function Command(props: LaunchProps<{ arguments: Arguments.QuickCall }>) {
+  const { fallbackText } = props;
+  const { number } = props.arguments;
 
-export default function Command() {
-  async function handleSubmit(values: CommandForm) {
-    // Remove all non-numeric characters from the input
-    values.number = values.number.replace(/\D/g, "");
+  let dialNumber = fallbackText || number;
 
-    // If the input is empty, show an error toast and return
-    if (values.number.length === 0) {
-      await showToast({
-        style: Toast.Style.Failure,
-        title: "Input Error",
-        message: "You must enter a number before setting up a call",
-      });
-      return;
+  if (dialNumber === "") {
+    try {
+      const selectedText = await getSelectedText();
+      if (selectedText !== "") {
+        dialNumber = selectedText;
+      }
+    } catch (error) {
+      console.error(error);
     }
-
-    // If the input is less than 10 digits, show an error toast and return
-    if (values.number.length < 10) {
-      await showToast({
-        style: Toast.Style.Failure,
-        title: "Input Error",
-        message: "You must enter a 10 digit number before setting up a call",
-      });
-      return;
-    }
-
-    // If the input is 10 digits, add a +1 to the beginning
-    if (values.number.length === 10) {
-      values.number = `+1${values.number}`;
-    }
-
-    // If the input is 11 digits, add a + to the beginning
-    if (values.number.length === 11) {
-      values.number = `+${values.number}`;
-    }
-
-    // If the input is more than 12 digits, show an error toast and return
-    if (values.number.length > 12) {
-      await showToast({
-        style: Toast.Style.Failure,
-        title: "Input Error",
-        message: "You must enter a 10 to 12 digit number before setting up a call",
-      });
-      return;
-    }
-
-    await open(`tel:${encodeURIComponent(values.number)}`);
-    await popToRoot({ clearSearchBar: true });
-    await closeMainWindow();
   }
 
-  return (
-    <Form
-      actions={
-        <ActionPanel>
-          <Action.SubmitForm onSubmit={handleSubmit} title="Make a Phone Call" />
-        </ActionPanel>
-      }
-    >
-      <Form.TextField id="number" title="Phone Number" placeholder="Enter a phone number" defaultValue="" />
-    </Form>
-  );
+  // This line removes any non-numeric characters (except for the plus sign)
+  // from the dialNumber string.
+  dialNumber = dialNumber.replace(/[^0-9+]/g, "").trim();
+
+  // If the input is empty, show an error toast and return
+  if (dialNumber.length === 0) {
+    await showHUD("You must enter a number before setting up a call");
+    return;
+  }
+
+  try {
+    await open(`tel://${dialNumber}`);
+    await showHUD(`Opening facetime calling ${dialNumber}...`);
+  } catch (error) {
+    if (error instanceof Error) {
+      await showHUD(error.message);
+    }
+  }
 }
