97dc32547eff35998ab088f41de8a7de13308859
Anish Patel <anish.h.patel@gmail.com>
2023-08-30 02:51:29 -0400

    Update pipedrive extension
    - updated search to now include deals, people and organizations
    - updated search to now include deals, people and organizations

---
+++ extensions/pipedrive/CHANGELOG.md
@@ -0,0 +1,6 @@
+# Pipedrive Search Changelog
+
+## [New Actions] - 2023-08-30
+
+- Added ability to search people and organizations, in addition to the previous deals search.
+
--- extensions/pipedrive/src/index.tsx
+++ extensions/pipedrive/src/index.tsx
@@ -13,14 +13,15 @@ import fetch, { AbortError } from "node-fetch";
 interface Preferences {
   domain: string;
   apiToken: string;
+  limit: string;
 }
 
 export default function Command() {
   const { state, search } = useSearch();
 
   return (
-    <List isLoading={state.isLoading} onSearchTextChange={search} searchBarPlaceholder="Search by name..." throttle>
-      <List.Section title="Results" subtitle={state.results.length + ""}>
+    <List isLoading={state.isLoading} onSearchTextChange={search} searchBarPlaceholder="Search by name of deal, person or organization..." throttle>
+      <List.Section title="Results" subtitle={state.results.length + " "}>
         {state.results.map((searchResult) => (
           <SearchListItem key={searchResult.id} searchResult={searchResult} />
         ))}
@@ -32,21 +33,41 @@ export default function Command() {
 function SearchListItem({ searchResult }: { searchResult: SearchResult }) {
   const preferences: Preferences = getPreferenceValues();
   const itemUrl = `https://${preferences.domain}/${searchResult.type}/${searchResult.id}`;
-  const subtitle = `${searchResult.status} ${searchResult.type}`;
+  //const resultType = `${searchResult.type}`;
+  const subtitle = `${searchResult.subtitle}`;
+  const accessoryTitleValue = `${searchResult.accessoryTitle}`;
 
   return (
     <List.Item
       title={searchResult.title}
       subtitle={subtitle}
-      accessoryTitle={searchResult.stage}
+      accessoryTitle={accessoryTitleValue}
       actions={
         <ActionPanel>
           <ActionPanel.Section>
             <OpenInBrowserAction title="Open in Browser" url={itemUrl} />
             <CopyToClipboardAction
               title="Copy Deal Email"
-              content={searchResult.ccEmail}
+              content={searchResult.ccEmail as string}
+            />
+            <CopyToClipboardAction
+              title="Copy Person Email"
+              content={searchResult.email as string}
+            />
+          </ActionPanel.Section>
+          <ActionPanel.Section>
+            <CopyToClipboardAction
+              title="Copy Person Name"
+              content={searchResult.name as string}
+            />
+            <CopyToClipboardAction
+              title="Copy Person Email"
+              content={searchResult.email as string}
               shortcut={{ modifiers: ["cmd"], key: "." }}
+            />
+            <CopyToClipboardAction
+              title="Copy Person Phone"
+              content={searchResult.phone as string}
             />
           </ActionPanel.Section>
         </ActionPanel>
@@ -107,9 +128,11 @@ async function performSearch(searchText: string, signal: AbortSignal): Promise<S
   const params = new URLSearchParams();
   params.append("api_token", preferences.apiToken);
   params.append("include_fields", "deal.cc_email");
+  params.append("item_types", "deal,person,organization");
   params.append("term", searchText);
+  params.append("limit", preferences.limit);
 
-  const response = await fetch(`https://${preferences.domain}/v1/deals/search` + "?" + params.toString(), {
+  const response = await fetch(`https://${preferences.domain}/api/v1/itemSearch` + "?" + params.toString(), {
     method: "get",
     signal: signal,
   });
@@ -123,20 +146,65 @@ async function performSearch(searchText: string, signal: AbortSignal): Promise<S
   const json = (await response.json()) as Json;
   const jsonData = (json?.data as Json) ?? {};
   const jsonResults = (jsonData?.items as Json[]) ?? [];
-  return jsonResults.map((jsonResult) => {
-    const deal = jsonResult.item as Json;
-    const dealStage = deal.stage as Json;
-    const dealOrganization = deal.organization as Json;
 
-    return {
-      id: deal.id as string,
-      title: deal.title as string,
-      type: deal.type as string,
-      stage: dealStage.name as string,
-      organization: dealOrganization.name as string,
-      status: deal.status as string,
-      ccEmail: deal.cc_email as string,
-    };
+  return jsonResults.map((jsonResult) => {
+    const results = jsonResult.item as Json;
+    const resultsOrganization = results.organization as Json || "" as string;
+
+    if (results.type as string === "deal") {
+      const resultsStage = results.stage as Json || "" as string;
+      return {
+        id: results.id as string,
+        title: results.title as string,
+        subtitle: results.status + " " + results.type as string,
+        accessoryTitle: resultsOrganization.name as string || "" as string,
+        type: results.type as string,
+        stage: resultsStage.name as string,
+        organization: resultsOrganization.name as string || "" as string,
+        status: results.status as string,
+        ccEmail: results.cc_email as string,
+      };
+    }
+    else if (results.type as string === "person") {
+      const resultsName = results.name as Json;
+      //      const primaryEmail = results.primary_email as string || "" as string;
+      //      const resultsPhone = results.phones as Json[];
+      //      const primaryPhone = resultsPhone[0] || "" as string;    
+      const primaryEmail = typeof results.primary_email === 'string' ? results.primary_email : "" as string;
+      const resultsPhone = Array.isArray(results.phones) ? results.phones : [];
+      const primaryPhone = typeof resultsPhone[0] === 'string' ? resultsPhone[0] : "" as string;
+      return {
+        id: results.id as string,
+        title: results.name as string,
+        subtitle: primaryEmail as string + " " + primaryPhone as string,
+        accessoryTitle: resultsOrganization.name as string || "" as string,
+        type: results.type as string,
+        name: results.name as string,
+        organization: resultsOrganization.name as string || "" as string,
+        email: primaryEmail as string,
+        phone: primaryPhone as string,
+        status: results.status as string,
+      };
+    }
+    else if (results.type as string === "organization") {
+      return {
+        id: results.id as string,
+        title: results.name as string,
+        subtitle: "org" as string,
+        accessoryTitle: "" as string,
+        type: results.type as string,
+      };
+    }
+    else return {
+      id: results.id as string,
+      title: results.title as string,
+      type: results.type as string,
+      organization: resultsOrganization.name as string,
+      status: results.status as string,
+      ccEmail: results.cc_email as string,
+      email: "no-email" as string,
+      phone: "no-phone" as string,
+    }
   });
 }
 
@@ -148,9 +216,14 @@ interface SearchState {
 interface SearchResult {
   id: string;
   title: string;
+  subtitle?: string;
+  accessoryTitle?: string;
   type: string;
-  stage: string;
-  organization: string;
-  status: string;
-  ccEmail: string;
+  stage?: string;
+  organization?: string;
+  status?: string;
+  ccEmail?: string;
+  email?: string;
+  name?: string;
+  phone?: string;
 }
