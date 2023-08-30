0be04f5164e882ffa87f5648533d0aa016934d07
José Pereira <jmfp-b@live.com.pt>
2023-08-30 23:10:26 +0100

    feat: display right click menu with quit on indicator menu bar item

--- extensions/aperture/Sources/aperture-indicator/aperture-indicator/AppDelegate.swift
+++ extensions/aperture/Sources/aperture-indicator/aperture-indicator/AppDelegate.swift
@@ -11,13 +11,40 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
         if let button = self.statusBarItem.button {
             button.image = NSImage(named: "Icon")
-            button.action = #selector(openStopRcCommand)
-            button.attributedTitle = NSAttributedString(string: "Recording", attributes: [NSAttributedString.Key.foregroundColor: NSColor.red])
             button.toolTip = "Aperture (Raycast)"
+            button.attributedTitle = NSAttributedString(string: "Recording", attributes: [NSAttributedString.Key.foregroundColor: NSColor.red])
+            button.action = #selector(handleButtonClick(_:))
+            button.sendAction(on: [.leftMouseUp, .rightMouseUp])
         }
     }
     
-    @objc func openStopRcCommand() {
+    @objc func handleButtonClick(_ sender: NSStatusItem) {
+        let event = NSApp.currentEvent!
+        
+        if event.type == NSEvent.EventType.rightMouseUp {
+            let rightClickMenu = NSMenu()
+            
+            let titleMenuItem = NSMenuItem(title: "Aperture (Raycast)", action: nil, keyEquivalent: "")
+            titleMenuItem.isEnabled = false;
+            
+            let menuItems = [
+                titleMenuItem,
+                NSMenuItem.separator(),
+                NSMenuItem(title: "Stop Recording", action: #selector(openStopCommand), keyEquivalent: "s"),
+                NSMenuItem.separator(),
+                NSMenuItem(title: "Quit", action: #selector(quit(_:)), keyEquivalent: "q")
+            ]
+            rightClickMenu.items = menuItems
+            statusBarItem.menu = rightClickMenu
+            statusBarItem.button?.performClick(nil)
+            statusBarItem.menu = nil
+        } else {
+            openStopCommand()
+        }
+
+    }
+    
+    @objc func openStopCommand() {
         if let url = URL(string: "raycast://extensions/jomifepe/aperture/stop.command") {
             NSWorkspace.shared.open(url)
         }
