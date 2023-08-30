b743fe4857a39f0da505b47253d0d2b8d0ab6dba
Anish Patel <anish.h.patel@gmail.com>
2023-08-30 10:29:19 -0400

    prettier

--- extensions/pipedrive/src/index.tsx
+++ extensions/pipedrive/src/index.tsx
@@ -20,7 +20,12 @@ export default function Command() {
   const { state, search } = useSearch();
 
   return (
-    <List isLoading={state.isLoading} onSearchTextChange={search} searchBarPlaceholder="Search by name of deal, person or organization..." throttle>
+    <List
+      isLoading={state.isLoading}
+      onSearchTextChange={search}
+      searchBarPlaceholder="Search by name of deal, person or organization..."
+      throttle
+    >
       <List.Section title="Results" subtitle={state.results.length + " "}>
         {state.results.map((searchResult) => (
           <SearchListItem key={searchResult.id} searchResult={searchResult} />
@@ -46,29 +51,17 @@ function SearchListItem({ searchResult }: { searchResult: SearchResult }) {
         <ActionPanel>
           <ActionPanel.Section>
             <OpenInBrowserAction title="Open in Browser" url={itemUrl} />
-            <CopyToClipboardAction
-              title="Copy Deal Email"
-              content={searchResult.ccEmail as string}
-            />
-            <CopyToClipboardAction
-              title="Copy Person Email"
-              content={searchResult.email as string}
-            />
+            <CopyToClipboardAction title="Copy Deal Email" content={searchResult.ccEmail as string} />
+            <CopyToClipboardAction title="Copy Person Email" content={searchResult.email as string} />
           </ActionPanel.Section>
           <ActionPanel.Section>
-            <CopyToClipboardAction
-              title="Copy Person Name"
-              content={searchResult.name as string}
-            />
+            <CopyToClipboardAction title="Copy Person Name" content={searchResult.name as string} />
             <CopyToClipboardAction
               title="Copy Person Email"
               content={searchResult.email as string}
               shortcut={{ modifiers: ["cmd"], key: "." }}
             />
-            <CopyToClipboardAction
-              title="Copy Person Phone"
-              content={searchResult.phone as string}
-            />
+            <CopyToClipboardAction title="Copy Person Phone" content={searchResult.phone as string} />
           </ActionPanel.Section>
         </ActionPanel>
       }
@@ -149,40 +142,38 @@ async function performSearch(searchText: string, signal: AbortSignal): Promise<S
 
   return jsonResults.map((jsonResult) => {
     const results = jsonResult.item as Json;
-    const resultsOrganization = results.organization as Json || "" as string;
+    const resultsOrganization = (results.organization as Json) || ("" as string);
 
-    if (results.type as string === "deal") {
-      const resultsStage = results.stage as Json || "" as string;
+    if ((results.type as string) === "deal") {
+      const resultsStage = (results.stage as Json) || ("" as string);
       return {
         id: results.id as string,
         title: results.title as string,
-        subtitle: results.status + " " + results.type as string,
-        accessoryTitle: resultsOrganization.name as string || "" as string,
+        subtitle: (results.status + " " + results.type) as string,
+        accessoryTitle: (resultsOrganization.name as string) || ("" as string),
         type: results.type as string,
         stage: resultsStage.name as string,
-        organization: resultsOrganization.name as string || "" as string,
+        organization: (resultsOrganization.name as string) || ("" as string),
         status: results.status as string,
         ccEmail: results.cc_email as string,
       };
-    }
-    else if (results.type as string === "person") {
-      const primaryEmail = typeof results.primary_email === 'string' ? results.primary_email : "" as string;
+    } else if ((results.type as string) === "person") {
+      const primaryEmail = typeof results.primary_email === "string" ? results.primary_email : ("" as string);
       const resultsPhone = Array.isArray(results.phones) ? results.phones : [];
-      const primaryPhone = typeof resultsPhone[0] === 'string' ? resultsPhone[0] : "" as string;
+      const primaryPhone = typeof resultsPhone[0] === "string" ? resultsPhone[0] : ("" as string);
       return {
         id: results.id as string,
         title: results.name as string,
-        subtitle: primaryEmail as string + " " + primaryPhone as string,
-        accessoryTitle: resultsOrganization.name as string || "" as string,
+        subtitle: ((primaryEmail as string) + " " + primaryPhone) as string,
+        accessoryTitle: (resultsOrganization.name as string) || ("" as string),
         type: results.type as string,
         name: results.name as string,
-        organization: resultsOrganization.name as string || "" as string,
+        organization: (resultsOrganization.name as string) || ("" as string),
         email: primaryEmail as string,
         phone: primaryPhone as string,
         status: results.status as string,
       };
-    }
-    else if (results.type as string === "organization") {
+    } else if ((results.type as string) === "organization") {
       return {
         id: results.id as string,
         title: results.name as string,
@@ -190,17 +181,17 @@ async function performSearch(searchText: string, signal: AbortSignal): Promise<S
         accessoryTitle: "" as string,
         type: results.type as string,
       };
-    }
-    else return {
-      id: results.id as string,
-      title: results.title as string,
-      type: results.type as string,
-      organization: resultsOrganization.name as string,
-      status: results.status as string,
-      ccEmail: results.cc_email as string,
-      email: "no-email" as string,
-      phone: "no-phone" as string,
-    }
+    } else
+      return {
+        id: results.id as string,
+        title: results.title as string,
+        type: results.type as string,
+        organization: resultsOrganization.name as string,
+        status: results.status as string,
+        ccEmail: results.cc_email as string,
+        email: "no-email" as string,
+        phone: "no-phone" as string,
+      };
   });
 }
 
