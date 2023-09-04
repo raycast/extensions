6b768098f2a96b1584253b9b13abaf5d5469fb7c
emiliobool <boolemilio@gmail.com>
2023-09-03 18:01:49 -0700

    feat(FormItemsComponent.tsx): allow 'required' prop to be undefined to handle optional form fields refactor(FormItemsComponent.tsx): convert defaultValue to string to ensure consistent data type in form fields feat(types.ts): add 'draft_only' property to WindmillListItem type to support draft-only items

---
+++ extensions/windmill/README.md
@@ -0,0 +1,9 @@
+# Windmill
+
+Unofficial Windmill Extension to run flows and scripts directly
+
+Before you can use this extension you need to setup your workspace, you will need to get your workspace id, instance url (if using a self hosted environement) and your access token (go to user settings and create token), then go to manage workspaces.
+
+
+
+
---
+++ extensions/windmill/src/viewScripts.tsx
@@ -0,0 +1,11 @@
+import { useFetchWorkspaces } from "./hooks/useFetchWorkspaces";
+import { useFetchList } from "./hooks/useFetchList";
+import { ItemList } from "./components/ItemList";
+
+const kind = "script";
+
+export default function ListFlowsCommand() {
+  const { workspaces } = useFetchWorkspaces();
+  const { items, refreshItems } = useFetchList(kind, workspaces);
+  return <ItemList kind={kind} items={items} workspaces={workspaces} refreshItems={refreshItems} />;
+}
