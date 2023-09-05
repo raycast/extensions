3a2038b66f1b2b7ced8d6ed8915849992aed0f44
Kaan <63533083+kaanrkaraman@users.noreply.github.com>
2023-09-06 02:45:20 +0300

    Give it a one final try

--- extensions/twitter-video-downloader/raycast-env.d.ts
+++ extensions/twitter-video-downloader/raycast-env.d.ts
@@ -7,26 +7,25 @@
 
 /* eslint-disable @typescript-eslint/ban-types */
 
-type ExtensionPreferences = {}
+type ExtensionPreferences = {};
 
 /** Preferences accessible in all the extension's commands */
-declare type Preferences = ExtensionPreferences
+declare type Preferences = ExtensionPreferences;
 
 declare namespace Preferences {
   /** Preferences accessible in the `index` command */
-  export type Index = ExtensionPreferences & {}
+  export type Index = ExtensionPreferences & {};
 }
 
 declare namespace Arguments {
   /** Arguments passed to the `index` command */
-  export type Index = {}
+  export type Index = {};
 }
-
 
 declare module "swift:*" {
   function run<T = unknown, U = any>(command: string, input?: U): Promise<T>;
   export default run;
-	export class SwiftError extends Error {
+  export class SwiftError extends Error {
     stderr: string;
     stdout: string;
   }
