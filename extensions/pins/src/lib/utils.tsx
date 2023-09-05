90c3e48ec072c3d696155a3a714ad026bf228e10
Stephen Kaplan <stephen.kaplan@maine.edu>
2023-09-05 03:13:55 -0400

    Pins 1.4.0 Update

--- extensions/pins/src/lib/utils.tsx
+++ extensions/pins/src/lib/utils.tsx
@@ -1,10 +1,6 @@
-import { LocalStorage, Icon, Clipboard, showToast, Toast } from "@raycast/api";
-import { runAppleScript } from "run-applescript";
+import { LocalStorage } from "@raycast/api";
 import { exec, execSync } from "child_process";
-import { StorageKey } from "./constants";
-import { getFavicon } from "@raycast/utils";
-import { Pin } from "./Pins";
-import { Group } from "./Groups";
+import { runAppleScript } from "@raycast/utils";
 
 /**
  * Preferences for the entire extension.
@@ -24,12 +20,12 @@ export interface ExtensionPreferences {
    * Whether or not to show the recent applications section in lists of pins.
    */
   showRecentApplications: boolean;
-}
 
-/**
- * A map of icon names to icon objects.
- */
-export const iconMap: { [index: string]: Icon } = Icon;
+  /**
+   * The default sort strategy for lists of pins outside of groups.
+   */
+  defaultSortStrategy: string;
+}
 
 /**
  * Sets the value of a local storage key.
@@ -89,152 +85,20 @@ export const runCommandSync = (command: string) => {
  */
 export const runCommandInTerminal = async (command: string): Promise<string> => {
   const output = await runAppleScript(`tell application "Terminal"
-    activate
-    do script "${command.replaceAll('"', '\\"')}"
+    try
+      activate
+      do script "${command.replaceAll('"', '\\"')}"
+    end try
   end tell`);
   return output;
 };
 
 /**
- * Copies the pin data to the clipboard.
- * @returns A promise resolving to the JSON string of the pin data.
+ * Cuts off a string at a certain length, adding an ellipsis if necessary.
+ * @param str The string to modify.
+ * @param cutoff The maximum length of the string.
+ * @returns The modified string.
  */
-export const copyPinData = async () => {
-  const pins = await getStorage(StorageKey.LOCAL_PINS);
-  const groups = await getStorage(StorageKey.LOCAL_GROUPS);
-
-  const data = {
-    groups: groups,
-    pins: pins,
-  };
-
-  const jsonData = JSON.stringify(data);
-  await Clipboard.copy(jsonData);
-  return jsonData;
-};
-
-/**
- * Converts a vague icon reference to an icon object.
- * @param iconRef The icon reference to convert.
- * @returns The icon object.
- */
-export const getIcon = (iconRef: string) => {
-  if (iconRef in iconMap) {
-    return iconMap[iconRef];
-  } else if (iconRef.startsWith("/") || iconRef.startsWith("~")) {
-    return { fileIcon: iconRef };
-  } else if (iconRef.match(/^[a-zA-Z0-9]*?:.*/g)) {
-    return getFavicon(iconRef);
-  } else if (iconRef == "None") {
-    return "";
-  }
-  return Icon.Terminal;
-};
-
-/**
- * Imports default pins and groups into local storage.
- */
-export const installExamples = async () => {
-  const examplePins: Pin[] = [
-    {
-      id: 1,
-      name: "Google",
-      url: "https://google.com",
-      icon: "Favicon / File Icon",
-      group: "None",
-      application: "None",
-    },
-    {
-      id: 2,
-      name: "GitHub",
-      url: "https://github.com",
-      icon: "Favicon / File Icon",
-      group: "Dev Utils",
-      application: "None",
-    },
-    {
-      id: 3,
-      name: "Regex 101",
-      url: "https://regex101.com",
-      icon: "Favicon / File Icon",
-      group: "Dev Utils",
-      application: "None",
-    },
-    {
-      id: 4,
-      name: "Terminal",
-      url: "/System/Applications/Utilities/Terminal.app",
-      icon: "Favicon / File Icon",
-      group: "Dev Utils",
-      application: "None",
-    },
-    {
-      id: 5,
-      name: "New Folder Here",
-      url: `osascript -e 'tell application "Finder"' -e 'set dirPath to folder (POSIX file "{{currentDirectory}}" as alias)' -e 'set newFolder to make new folder at dirPath' -e 'select newFolder' -e 'end tell'`,
-      icon: "NewFolder",
-      group: "Scripts",
-      application: "None",
-      execInBackground: true,
-    },
-    {
-      id: 6,
-      name: "New File Here",
-      url: `osascript -e 'tell application "Finder"' -e 'set dirPath to folder (POSIX file "{{currentDirectory}}" as alias)' -e 'set newFile to make new file at dirPath' -e 'select newFile' -e 'end tell'`,
-      icon: "NewDocument",
-      group: "Scripts",
-      application: "None",
-      execInBackground: true,
-    },
-    {
-      id: 7,
-      name: "New Terminal Here",
-      url: `cd {{currentDirectory}}`,
-      icon: "Terminal",
-      group: "Scripts",
-      application: "None",
-    },
-    {
-      id: 8,
-      name: "ChatGPT",
-      url: "https://chat.openai.com",
-      icon: "Favicon / File Icon",
-      group: "None",
-      application: "None",
-    },
-    {
-      id: 9,
-      name: "Random Duck",
-      url: "https://random-d.uk",
-      icon: "Favicon / File Icon",
-      group: "None",
-      application: "None",
-    },
-    {
-      id: 10,
-      name: "Search On Google",
-      url: "https://www.google.com/search?q={{selectedText}}",
-      icon: "Favicon / File Icon",
-      group: "None",
-      application: "None",
-    },
-  ];
-
-  const exampleGroups: Group[] = [
-    {
-      id: 1,
-      name: "Dev Utils",
-      icon: "CodeBlock",
-    },
-    {
-      id: 2,
-      name: "Scripts",
-      icon: "Text",
-    },
-  ];
-
-  await setStorage(StorageKey.LOCAL_PINS, examplePins);
-  await setStorage(StorageKey.LOCAL_GROUPS, exampleGroups);
-  await LocalStorage.setItem(StorageKey.EXAMPLES_INSTALLED, true);
-  await showToast({ title: "Examples Installed!", style: Toast.Style.Success });
+export const cutoff = (str: string, cutoff: number) => {
+  return `${str.substring(0, cutoff)}${str.length > cutoff ? "..." : ""}`;
 };
