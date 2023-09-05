0d453aa4b6438cdf2658b397d20356d28de9469e
erics118 <52634785+erics118@users.noreply.github.com>
2023-09-05 16:24:11 -0400

    feat: add new commands and args
    added add-quick-access-overlay command, which as a *required* arg
    added filepath arg to open-annotate
    added hide-desktop-icons command
    added show-desktop-icons command

--- extensions/cleanshotx/src/annotate.tsx
+++ extensions/cleanshotx/src/annotate.tsx
@@ -1,7 +1,12 @@
-import { closeMainWindow, open } from "@raycast/api";
+import { LaunchProps, closeMainWindow, open } from "@raycast/api";
 
-export default async function Command() {
-  const url = "cleanshot://open-annotate";
+export default async function Command(props: LaunchProps<{ arguments: Arguments.Annotate }>) {
+  let url = "cleanshot://open-annotate";
+
+  if (props?.arguments?.filepath) {
+    url += `?filepath=${encodeURIComponent(props.arguments.filepath)}`;
+  }
+
   open(url);
   await closeMainWindow();
 }
