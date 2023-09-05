e9c009437acbe5bd2e8934ffcad49faebf7f1855
José Pereira <jmfp-b@live.com.pt>
2023-09-05 09:56:03 +0100

    refactor: destructure import

--- extensions/aperture/src/utils/crypto.ts
+++ extensions/aperture/src/utils/crypto.ts
@@ -1,8 +1,7 @@
-import crypto from "crypto";
+import { randomBytes } from "crypto";
 
 export function getRandomString(length = 16) {
-  return crypto
-    .randomBytes(Math.ceil(length / 2))
+  return randomBytes(Math.ceil(length / 2))
     .toString("hex")
     .slice(0, length);
 }
