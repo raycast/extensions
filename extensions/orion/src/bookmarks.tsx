9388b6a90cfe98dec7bed50c503282eca12b1ea2
erics118 <52634785+erics118@users.noreply.github.com>
2023-09-06 20:48:31 -0400

    revert: remove bookmarks folder filtering
    it's actually redundant, as you can filter directly from the search bar
    also, this allows the searchBarAccessory to be used for profies later

--- extensions/orion/src/bookmarks.tsx
+++ extensions/orion/src/bookmarks.tsx
@@ -1,23 +1,15 @@
 import { List } from "@raycast/api";
 import useBookmarks from "./hooks/useBookmarks";
 import BookmarkListItem from "./components/BookmarkListItem";
-import BookmarksFolderDropdown from "src/components/BookmarksFolderDropdown";
-import { useState } from "react";
 
 export default function Command() {
-  const { folders, bookmarks, isLoading } = useBookmarks();
-  const [folder, setFolder] = useState<string>("");
-
-  const filteredBookmarks = folder ? bookmarks?.filter((bookmark) => bookmark.folders.includes(folder)) : bookmarks;
+  const { bookmarks, isLoading } = useBookmarks();
 
   return (
-    <List
-      isLoading={isLoading}
-      searchBarPlaceholder="Search by title, domain name, or folder"
-      searchBarAccessory={<BookmarksFolderDropdown folders={folders} onChange={setFolder} />}
-    >
-      {filteredBookmarks &&
-        filteredBookmarks.map((bookmark) => <BookmarkListItem key={bookmark.uuid} bookmark={bookmark} />)}
+    <List isLoading={isLoading} searchBarPlaceholder="Search by title, domain name, or folder">
+      {bookmarks.map((bookmark) => (
+        <BookmarkListItem key={bookmark.uuid} bookmark={bookmark} />
+      ))}
     </List>
   );
 }
