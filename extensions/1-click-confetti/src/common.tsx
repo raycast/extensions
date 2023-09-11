1c587224feaad51a4cc75b06afd6dbac9c766353
velios <prostozapomnit@gmail.com>
2023-09-11 13:40:06 +0300

    [1-Click-Confetti] Add confetti with sound command (#8192)
    * Add confetti with sound command
    
    I love the sound in this plugin and dont want use standart confetti.
    But without command i cant set hotkey to confetti with sound.
    
    * Lint with prettier
    
    * Add entry to Changelog
    
    ---------
    
    Co-authored-by: Flocktory <flocktory@MacBook-Pro-Flocktory.local>

--- extensions/1-click-confetti/src/common.tsx
+++ extensions/1-click-confetti/src/common.tsx
@@ -1,19 +1,10 @@
-import { MenuBarExtra, environment, open, getPreferenceValues } from "@raycast/api";
+import { environment, open } from "@raycast/api";
 import { exec } from "child_process";
-
-export default function Command() {
-  const { confettiSound } = getPreferenceValues();
-  return (
-    <MenuBarExtra isLoading={false} icon="🎉">
-      <Shoot playSound={confettiSound} />
-    </MenuBarExtra>
-  );
-}
 
 const sound = "mixkit-happy-crowd-cheer-975.wav";
 const command = `afplay "${environment.assetsPath + "/" + sound}"`;
 
-function Shoot({ playSound }: { playSound: boolean }) {
+export function Shoot({ playSound }: { playSound: boolean }) {
   open("raycast://confetti");
 
   if (playSound) {
