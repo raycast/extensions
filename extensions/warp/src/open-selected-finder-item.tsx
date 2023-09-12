78b3a9049a2992d7e03ba4018231ec059dff89af
Hakob.Nersesyan <hakob.nersesian@gmail.com>
2023-09-13 00:08:44 +0200

    add pathfinder support

--- extensions/warp/src/open-selected-finder-item.tsx
+++ extensions/warp/src/open-selected-finder-item.tsx
@@ -4,41 +4,40 @@ import fs from "node:fs/promises";
 import path from "node:path";
 import { newTab } from "./uri";
 
-const fallback = async (): Promise<boolean> => {
-  const app = await getFrontmostApplication();
+const getSelectedPathFinderItems = async () => {
+  const script = `
+    tell application "Path Finder"
+      set thePaths to {}
+      repeat with pfItem in (get selection)
+        set the end of thePaths to POSIX path of pfItem
+      end repeat
+      return thePaths
+    end tell
+  `;
 
-  if (app.name !== "Finder") {
-    return false;
-  }
-
-  const currentDirectory = await runAppleScript(
-    `tell application "Finder" to get POSIX path of (target of front window as alias)`
-  );
-
-  if (!currentDirectory) {
-    return false;
-  }
-
-  await open(newTab(currentDirectory));
-
-  return true;
+  const paths = await runAppleScript(script);
+  return paths.split(","); // Assuming the paths are comma-separated
 };
 
 export default async function Command() {
   try {
-    const selectedItems = await getSelectedFinderItems();
+    let selectedItems: { path: string }[] = [];
+
+    const app = await getFrontmostApplication();
+
+    if (app.name === "Finder") {
+      selectedItems = await getSelectedFinderItems();
+    } else if (app.name === "Path Finder") {
+      const paths = await getSelectedPathFinderItems();
+      selectedItems = paths.map((p) => ({ path: p }));
+    }
 
     if (selectedItems.length === 0) {
-      const ranFallback = await fallback();
-
-      if (ranFallback === false) {
-        await showToast({
-          style: Toast.Style.Failure,
-          title: "No directory selected",
-          message: "Please select a directory in Finder first",
-        });
-      }
-
+      await showToast({
+        style: Toast.Style.Failure,
+        title: "No directory selected",
+        message: "Please select a directory in Finder or Path Finder first",
+      });
       return;
     }
 
