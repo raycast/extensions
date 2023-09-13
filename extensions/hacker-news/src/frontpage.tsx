6ef12f16a4330ca4a514f4e79326fdf37d899fa3
Hakob.Nersesyan <hakob.nersesian@gmail.com>
2023-09-14 00:52:50 +0200

    Update hacker-news extension
    - add shortcut for window mod
    - add summarizer functionality

--- extensions/hacker-news/src/frontpage.tsx
+++ extensions/hacker-news/src/frontpage.tsx
@@ -1,4 +1,4 @@
-import { Action, ActionPanel, List } from "@raycast/api";
+import { Action, ActionPanel, List, showToast, Toast } from "@raycast/api";
 import { startCase } from "lodash";
 import { getStories } from "./hackernews";
 import { Topic } from "./types";
@@ -6,14 +6,21 @@ import { useState } from "react";
 import { usePromise } from "@raycast/utils";
 import Parser from "rss-parser";
 import { getIcon, getAccessories } from "./utils";
+import { OpenAIModule } from "./utils/summarizer";
+import { getAccessToken } from "./utils/preferences";
+
+const openAIKey = getAccessToken();
+
+const openAI = openAIKey ? new OpenAIModule(openAIKey) : null;
 
 export default function Command() {
   const [topic, setTopic] = useState<Topic | null>(null);
   const { data, isLoading } = usePromise(getStories, [topic], { execute: !!topic });
-
+  const [isShowingDetail, setIsShowingDetail] = useState(false);
   return (
     <List
       isLoading={isLoading}
+      isShowingDetail={isShowingDetail}
       searchBarAccessory={
         <List.Dropdown
           tooltip="Select Page"
@@ -28,7 +35,14 @@ export default function Command() {
       }
     >
       {data?.map((item, index) => (
-        <StoryListItem key={item.guid} item={item} index={index} topic={topic} />
+        <StoryListItem
+          key={item.guid}
+          item={item}
+          index={index}
+          topic={topic}
+          setIsShowingDetail={setIsShowingDetail}
+          isShowingDetail={isShowingDetail}
+        />
       ))}
     </List>
   );
@@ -47,19 +61,69 @@ function setTitle(title: string, topic: Topic) {
   return title;
 }
 
-function StoryListItem(props: { item: Parser.Item; index: number; topic: Topic | null }) {
+function getWindowContent(title: string, topic: Topic) {
+  const _title = setTitle(title, topic);
+  const _informationContent = "To summarize the article, please press *'Cmd(⌘) + ]'*.";
+  return "# " + _title + "  \n" + _informationContent;
+}
+
+function StoryListItem(props: {
+  item: Parser.Item;
+  index: number;
+  topic: Topic | null;
+  setIsShowingDetail: React.Dispatch<React.SetStateAction<boolean>>;
+  isShowingDetail: boolean;
+}) {
+  const [summary, setSummary] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchSummary = async () => {
+    if (props.item.link) {
+      setIsLoading(true); // Show loading state
+      try {
+        const result = await openAI.summarizeArticle(props.item.link);
+        setSummary(result);
+      } catch (error) {
+        console.error("Failed to fetch summary:", error);
+      } finally {
+        setIsLoading(false); // Hide loading state
+      }
+    }
+  };
+
   return (
     <List.Item
+      detail={
+        <List.Item.Detail
+          markdown={
+            isLoading
+              ? "*Loading...*"
+              : summary || getWindowContent(props.item.title || "No Title", props.topic || Topic.FrontPage)
+          }
+        />
+      }
       icon={getIcon(props.index + 1)}
       title={setTitle(props.item.title || "No Title", props.topic || Topic.FrontPage)}
       subtitle={props.item.creator}
       accessories={getAccessories(props.item)}
-      actions={<Actions item={props.item} />}
+      actions={
+        <Actions
+          item={props.item}
+          setIsShowingDetail={props.setIsShowingDetail}
+          isShowingDetail={props.isShowingDetail}
+          fetchSummary={fetchSummary}
+        />
+      }
     />
   );
 }
 
-function Actions(props: { item: Parser.Item }) {
+function Actions(props: {
+  item: Parser.Item;
+  setIsShowingDetail: React.Dispatch<React.SetStateAction<boolean>>;
+  isShowingDetail: boolean;
+  fetchSummary: () => Promise<void>;
+}) {
   return (
     <ActionPanel title={props.item.title}>
       <ActionPanel.Section>
@@ -74,6 +138,37 @@ function Actions(props: { item: Parser.Item }) {
             shortcut={{ modifiers: ["cmd"], key: "." }}
           />
         )}
+      </ActionPanel.Section>
+
+      <ActionPanel.Section>
+        <Action
+          title="Toggle summarize window"
+          shortcut={{ modifiers: ["cmd"], key: "[" }}
+          onAction={async () => {
+            if (openAI != null) props.setIsShowingDetail((prev) => !prev);
+            else
+              await showToast({
+                style: Toast.Style.Failure,
+                title: "Cannot execute command",
+                message: "To enable the summarize function, you should provide an openAI key on the preferences page.",
+              });
+          }}
+        />
+        <Action
+          title="Summarize article"
+          onAction={async () => {
+            if (props.isShowingDetail) {
+              props.fetchSummary();
+            } else {
+              await showToast({
+                style: Toast.Style.Failure,
+                title: "Cannot execute command",
+                message: "You should enable summarize window first",
+              });
+            }
+          }}
+          shortcut={{ modifiers: ["cmd"], key: "]" }}
+        />
       </ActionPanel.Section>
     </ActionPanel>
   );
---
+++ extensions/hacker-news/src/utils/summarizer.ts
@@ -0,0 +1,31 @@
+import OpenAI from "openai";
+
+class OpenAIModule {
+  private openai: OpenAI;
+
+  constructor(apiKey: string) {
+    this.openai = new OpenAI({ apiKey });
+  }
+
+  async summarizeArticle(url: string): Promise<string> {
+    const prompt = `Please summarize the article and provide a only short summary with markdown. ${url}`;
+    return await this.gptRequest(prompt);
+  }
+
+  private async gptRequest(prompt: string): Promise<string> {
+    const response = await this.openai.chat.completions.create({
+      messages: [{ role: "user", content: prompt }],
+      model: "gpt-3.5-turbo",
+      temperature: 0.5,
+    });
+
+    let output = "";
+    if (response.choices[0]?.message.content != null) {
+      const regex = new RegExp(`^${'"'}|${'"'}$`, "g");
+      output = response.choices[0].message.content.replace(regex, "");
+    }
+    return output;
+  }
+}
+
+export { OpenAIModule };
