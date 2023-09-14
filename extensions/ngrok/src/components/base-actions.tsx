40b5a48be1678c47ee667b1b1199476f911c3fff
ubinatus <ja.castrocabrera@gmail.com>
2023-09-14 09:31:44 -0500

    fix: resolve linting

--- extensions/ngrok/src/components/base-actions.tsx
+++ extensions/ngrok/src/components/base-actions.tsx
@@ -8,7 +8,12 @@ type Props = {
 export default function BaseActions({ goToCreate, reload }: Props) {
   return (
     <>
-      <Action icon={Icon.Plus} title="Create Tunnel" shortcut={{ modifiers: ["cmd"], key: "n" }} onAction={goToCreate} />
+      <Action
+        icon={Icon.Plus}
+        title="Create Tunnel"
+        shortcut={{ modifiers: ["cmd"], key: "n" }}
+        onAction={goToCreate}
+      />
       <Action icon={Icon.ArrowClockwise} title="Reload List" onAction={reload} />
     </>
   );
