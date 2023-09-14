01833264b013630c7059ba366f464d2f9d9d5ee2
Per Nielsen Tikær <per@raycast.com>
2023-09-14 09:51:22 +0200

    Update index.ts

--- extensions/developer-excuse/src/index.ts
+++ extensions/developer-excuse/src/index.ts
@@ -5,7 +5,7 @@ import excuses from "./excuses";
 export default async function main() {
   const randomExcuse: string = getRandomElement(excuses);
   await Clipboard.paste(randomExcuse);
-  await showHUD(randomExcuse);
+  await showHUD("✅ Pasted excuse");
 }
 
 const getRandomElement = (arr: any[]) => arr[Math.floor(Math.random() * arr.length)];
