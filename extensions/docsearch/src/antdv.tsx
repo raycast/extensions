1011867f0a1abf0a9951674543ac123b37da3279
Fatpandac <i@fatpandac.com>
2023-09-17 12:39:06 +0800

    Update docsearch extension
    - chore: generate entry file
    - chore: remove tsx
    - chore: update package.json
    - feat(docs): support new docs
    - init

--- extensions/docsearch/src/antdv.tsx
+++ extensions/docsearch/src/antdv.tsx
@@ -1,5 +1,5 @@
 import { SearchDocumentation } from "./components";
 
 export default function Command(props: { arguments: { search?: string } }) {
-  return <SearchDocumentation id="885d77e7-300b-4713-8755-6b5f69fc1ac0" quickSearch={props.arguments?.search} />;
+  return <SearchDocumentation id="43cd38be-82b8-4d54-9764-349fa2b7d6a3" quickSearch={props.arguments?.search} />;
 }
