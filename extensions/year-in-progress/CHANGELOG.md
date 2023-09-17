1442f986a7e75aa11b7a7b6d73455fccd8c89c14
Danylo Zalizchuk <danilmail0110@gmail.com>
2023-09-17 11:40:38 +0300

    Make progress icon dynamic

--- extensions/year-in-progress/CHANGELOG.md
+++ extensions/year-in-progress/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Year in Progress Changelog
 
+## [Improvements] - {PR_MERGE_DATE}
+
+- Make progress icon dynamic
+
 ## [Added X In Progress] - 2023-03-29
 
 - Add X In Progress command
--- extensions/year-in-progress/src/utils/icon.ts
+++ extensions/year-in-progress/src/utils/icon.ts
@@ -1,14 +1,7 @@
-import { Icon } from "@raycast/api";
+import { Color } from "@raycast/api";
+import { getProgressIcon } from "@raycast/utils";
 
 export function getIcon(progressNumber: number) {
-  if (progressNumber >= 0 && progressNumber <= 25) {
-    return Icon.Circle;
-  } else if (progressNumber > 25 && progressNumber <= 50) {
-    return Icon.CircleProgress25;
-  } else if (progressNumber > 50 && progressNumber <= 75) {
-    return Icon.CircleProgress50;
-  } else if (progressNumber > 75 && progressNumber < 100) {
-    return Icon.CircleProgress75;
-  }
-  return Icon.CircleProgress100;
+  const icon = getProgressIcon(progressNumber / 100, Color.PrimaryText);
+  return icon;
 }
