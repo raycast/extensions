557d82eaf79cb4da1115460522194f237de3fae3
AbdulAlim Rajjoub <allomi.raj@gmail.com>
2023-09-21 23:23:48 +0300

    Update open-in-android-studio extension (#8481)
    * Initial Commit
    
    * resized icon
    
    * Fixed Lint
    
    * Update CHANGELOG.md and optimise images
    
    * Update open-in-android-studio extension
    
    - Merge branch \'contributions/merge-1695222227664756000\'
    - Pull contributions
    - Added Favorites
    
    * Update CHANGELOG.md and optimise images
    
    ---------
    
    Co-authored-by: raycastbot <bot@raycast.com>

---
+++ extensions/open-in-android-studio/src/common/util.ts
@@ -0,0 +1,56 @@
+import { Application, Toast, getApplications, open, showToast } from "@raycast/api";
+import { exec } from "child_process";
+
+const asPackageName = "com.google.android.studio";
+
+export async function getAndroidStudioApp(): Promise<Application | undefined> {
+  const applications = await getApplications();
+  const androidStudioApplication = applications.find((app) => app.bundleId === asPackageName);
+  return androidStudioApplication;
+}
+
+export async function getFinderApp(): Promise<Application | undefined> {
+  const applications = await getApplications();
+  const finderApp = applications.find((app) => app.bundleId === "com.apple.finder");
+  return finderApp;
+}
+
+export async function showAndroidStudioAppNotInstalled() {
+  await showToast({
+    style: Toast.Style.Failure,
+    title: "Android Studio is not installed",
+    primaryAction: {
+      title: "Install Android Studio",
+      onAction: () => open("https://developer.android.com/studio"),
+    },
+  });
+}
+
+/**
+ * Gets the selected Finder window.
+ * @throws — An error when Finder is not the frontmost application.
+ * @returns A Promise that resolves with the selected Finder window's path.
+ */
+export const getSelectedFinderWindow = (): Promise<string> => {
+  const appleScript = `
+    if application "Finder" is running and frontmost of application "Finder" then
+      tell app "Finder"
+        set finderWindow to window 1
+        set finderWindowPath to (POSIX path of (target of finderWindow as alias))
+        return finderWindowPath
+      end tell
+    else 
+      error "Could not get the selected Finder window"
+    end if
+   `;
+  return new Promise((resolve, reject) => {
+    const child = exec(`osascript -e '${appleScript}'`, (error, stdout, stderr) => {
+      if (error || stderr) reject(Error("Could not get the selected Finder window"));
+      resolve(stdout.trim());
+    });
+
+    child.on("close", () => {
+      child.kill();
+    });
+  });
+};
