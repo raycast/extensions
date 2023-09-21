f813c0ecadfbe3014d23562ccae0e833ecfd3e2e
Vaibhav Satishkumar <78756662+Visual-Studio-Coder@users.noreply.github.com>
2023-09-20 21:40:42 -0700

    useFetch

--- extensions/perchance-generator/src/index.tsx
+++ extensions/perchance-generator/src/index.tsx
@@ -1,69 +1,50 @@
-import { showToast, Toast, getPreferenceValues, LaunchProps, Detail, ActionPanel, Action } from "@raycast/api";
-import fetch from "node-fetch"; // v2.6.1
+import {
+  Detail,
+  ActionPanel,
+  Action,
+  LaunchProps,
+  getPreferenceValues,
+  Toast,
+  showToast,
+  popToRoot,
+} from "@raycast/api";
 import { useEffect, useState } from "react";
+import { useFetch } from "@raycast/utils";
 
 interface Preferences {
   project: string;
 }
 
 export default function Command(props: LaunchProps) {
-  showToast(Toast.Style.Animated, "Generating... (the first generation takes a bit longer)");
   const preferences = getPreferenceValues<Preferences>();
   const { project } = props.arguments;
-  const [generatedText, setGeneratedText] = useState<string>("");
-  const [selectedGenerator, setSelectedGenerator] = useState<string>(project === "" ? preferences.project : project);
+  const selectedGenerator = project === "" ? preferences.project : project;
 
-  async function regenerateText(generatorName: string) {
-    try {
-      const response = await fetch(`https://cat-sequoia-parcel.glitch.me/api?generator=${generatorName}&list=output`);
-      const html = await response.text();
-      setGeneratedText(html);
-      showToast(Toast.Style.Success, "Successfully Regenerated!");
-    } catch {
-      showToast({
-        style: Toast.Style.Failure,
-        title: "Error",
-        message: "Something went wrong. Please try again.",
-      });
+  try {
+    const { isLoading, data, revalidate } = useFetch(
+      "https://cat-sequoia-parcel.glitch.me/api?generator=" + selectedGenerator + "&list=output"
+    );
+
+    if (data === undefined) {
+      showToast({ style: Toast.Style.Failure, title: "Error", message: "That generator might not exist 🙁" });
+      popToRoot();
     }
+
+    return (
+      <Detail
+        isLoading={isLoading}
+        markdown={`# ${data}`}
+        actions={
+          <ActionPanel>
+            <Action title="Reload" onAction={() => revalidate()} />
+            <Action.Paste title="Copy to Clipboard" content={`${data}`} />
+            <Action.CopyToClipboard title="Copy to Clipboard" content={`${data}`} />
+            <Action.OpenInBrowser title="Open in Browser" url={`https://perchance.org/${selectedGenerator}`} />
+          </ActionPanel>
+        }
+      />
+    );
+  } catch (error) {
+    showToast({ style: Toast.Style.Failure, title: "Error", message: "Something went wrong" });
   }
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          `https://cat-sequoia-parcel.glitch.me/api?generator=${selectedGenerator}&list=output`
-        );
-        const html = await response.text();
-        setGeneratedText(html);
-        showToast(Toast.Style.Success, "Successfully Generated!");
-      } catch {
-        showToast({
-          style: Toast.Style.Failure,
-          title: "Error",
-          message: "Something went wrong. Please try again.",
-        });
-      }
-    };
-
-    fetchData();
-  }, [selectedGenerator]);
-  showToast(Toast.Style.Success, "Successfully Regenerated!");
-  return (
-    <Detail
-      markdown={`# ${generatedText}`}
-      navigationTitle={`Generating from ${project || preferences.project}`}
-      actions={
-        <ActionPanel>
-          <Action title="Regenerate" onAction={() => regenerateText(selectedGenerator)} />
-          <Action.OpenInBrowser
-            title="Open in Browser"
-            url={`https://perchance.org/${project || preferences.project}`}
-          />
-          <Action.CopyToClipboard title="Copy to Clipboard" content={generatedText} />
-          <Action.Paste title="Paste" content={generatedText} />
-        </ActionPanel>
-      }
-    />
-  );
 }
