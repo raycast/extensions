a454014abe8bc63c227413baa1ec2b481898f832
Olav Gjerde <olav.gjerde@miles.no>
2023-09-21 16:38:10 +0200

    Pull contributions

---
+++ extensions/looksee/src/lookup.tsx
@@ -0,0 +1,26 @@
+import { LaunchProps } from "@raycast/api";
+import { useFetch } from "@raycast/utils";
+import { LookupResponse } from "./common/types/lookupResponse";
+import { VendorInformation } from "./lookup/VendorInformation";
+import { FetchError } from "./lookup/FetchError";
+import { EmptyError } from "./lookup/EmptyError";
+import { useLookupHistoryStorage } from "./lookup/useLookupHistoryStorage";
+
+export default function lookup(props: LaunchProps<{ arguments: Arguments.Lookup }>) {
+  const { address } = props.arguments;
+  const { isLoading, data, error } = useFetch<LookupResponse[]>(`https://www.macvendorlookup.com/api/v2/${address}`);
+
+  const showFetchError = !isLoading && !!error;
+  const showEmptyError = !isLoading && !showFetchError && !data;
+  const showVendorInfo = !showFetchError && !showEmptyError;
+
+  useLookupHistoryStorage(address, isLoading, data);
+
+  return (
+    <>
+      {showFetchError && <FetchError targetAddress={address} error={error} />}
+      {showEmptyError && <EmptyError targetAddress={address} />}
+      {showVendorInfo && <VendorInformation isLoading={isLoading} targetAddress={address} lookupResponse={data?.[0]} />}
+    </>
+  );
+}
