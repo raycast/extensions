37e9f333c7aebc55c71934470a90e006113b5004
Robin <robinsrk3@gmail.com>
2023-09-23 05:28:06 +0600

    fix format with prettier

--- extensions/wifi-share/src/share-wifi.tsx
+++ extensions/wifi-share/src/share-wifi.tsx
@@ -4,119 +4,117 @@ import { exec } from "child_process";
 import QRCode from "qrcode";
 
 const DetailPassword = ({
-	networkName,
-	setIsLoading,
+  networkName,
+  setIsLoading,
 }: {
-	networkName: string;
-	setIsLoading: (loading: boolean) => void;
+  networkName: string;
+  setIsLoading: (loading: boolean) => void;
 }) => {
-	const [password, setPassword] = useState("");
-	const [url, setUrl] = useState("");
+  const [password, setPassword] = useState("");
+  const [url, setUrl] = useState("");
 
-	const markdown = `
+  const markdown = `
 ![](${url})
 `;
+  useEffect(() => {
+    (async () => {
+      const toast = await showToast({ style: Toast.Style.Animated, title: "Permission Checking" });
+      exec(
+        `security find-generic-password -D "AirPort network password" -a "${networkName}" -w`,
+        async (error, password, stderr) => {
+          if (error) {
+            console.error(`exec error: ${error}`);
 
-	useEffect(() => {
-		(async () => {
-			const toast = await showToast({ style: Toast.Style.Animated, title: "Permission Checking" });
+            toast.style = Toast.Style.Failure;
+            toast.title = "Permission checked failed ❌";
+            toast.message = error.message;
 
-			exec(
-				`security find-generic-password -D "AirPort network password" -a "${networkName}" -w`,
-				async (error, password, stderr) => {
-					if (error) {
-						console.error(`exec error: ${error}`);
+            setIsLoading(false);
+            return;
+          }
+          QRCode.toDataURL(
+            `WIFI:S:${networkName};T:WPA;P:${password.trim()};;`,
+            {
+              scale: 9,
+              rendererOpts: { quality: 1 },
+            },
+            function (err, u) {
+              setUrl(u);
+            }
+          );
+          exec("open /Applications/Raycast.app", (error, stdout, stderr) => {
+            toast.style = Toast.Style.Success;
+            toast.title = "Permission checked successed ✅";
 
-						toast.style = Toast.Style.Failure;
-						toast.title = "Permission checked failed ❌";
-						toast.message = error.message;
+            setPassword(password.trim());
+            setIsLoading(false);
+          });
+        }
+      );
+    })();
+  }, []);
 
-						setIsLoading(false);
-						return;
-					}
-					QRCode.toDataURL(
-						`WIFI:S:${networkName};T:WPA;P:${password.trim()};;`,
-						{
-							scale: 9,
-							rendererOpts: { quality: 1 },
-						},
-						function (err, u) {
-							setUrl(u);
-						}
-					);
-					exec("open /Applications/Raycast.app", (error, stdout, stderr) => {
-						toast.style = Toast.Style.Success;
-						toast.title = "Permission checked successed ✅";
-
-						setPassword(password.trim());
-						setIsLoading(false);
-					});
-				}
-			);
-		})();
-	}, []);
-
-	return (
-		<Detail
-			navigationTitle="Wifi Share"
-			markdown={markdown}
-			metadata={
-				<Detail.Metadata>
-					<Detail.Metadata.Label title="Wifi name" text={networkName} />
-					<Detail.Metadata.TagList title="Password">
-						<Detail.Metadata.TagList.Item text={password} color={"#eed535"} />
-					</Detail.Metadata.TagList>
-					<Detail.Metadata.Separator />
-					<Detail.Metadata.Label title="Scanned text" text={`WIFI:S:${networkName};T:WPA;P:${password.trim()};;`} />
-				</Detail.Metadata>
-			}
-		/>
-	);
+  return (
+    <Detail
+      navigationTitle="Wifi Share"
+      markdown={markdown}
+      metadata={
+        <Detail.Metadata>
+          <Detail.Metadata.Label title="Wifi name" text={networkName} />
+          <Detail.Metadata.TagList title="Password">
+            <Detail.Metadata.TagList.Item text={password} color={"#eed535"} />
+          </Detail.Metadata.TagList>
+          <Detail.Metadata.Separator />
+          <Detail.Metadata.Label title="Scanned text" text={`WIFI:S:${networkName};T:WPA;P:${password.trim()};;`} />
+        </Detail.Metadata>
+      }
+    />
+  );
 };
 
 export default function Command() {
-	const [networks, setNetworks] = useState<Array<string>>([]);
-	const [isLoading, setIsLoading] = useState(false);
+  const [networks, setNetworks] = useState<Array<string>>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-	useEffect(() => {
-		setIsLoading(true);
+  useEffect(() => {
+    setIsLoading(true);
 
-		exec("/usr/sbin/networksetup -listpreferredwirelessnetworks en0", (error, stdout, stderr) => {
-			if (error) {
-				console.error(`exec error: ${error}`);
-				setIsLoading(false);
-				return;
-			}
+    exec("/usr/sbin/networksetup -listpreferredwirelessnetworks en0", (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        setIsLoading(false);
+        return;
+      }
 
-			const lines = stdout.trim().split("\n");
+      const lines = stdout.trim().split("\n");
 
-			// Extract the Wi-Fi network names from the lines
-			const networks = lines.slice(1).map((line) => line.trim());
+      // Extract the Wi-Fi network names from the lines
+      const networks = lines.slice(1).map((line) => line.trim());
 
-			if (networks?.length > 0) {
-				setNetworks(networks);
-			}
-			setIsLoading(false);
-		});
-	}, []);
+      if (networks?.length > 0) {
+        setNetworks(networks);
+      }
+      setIsLoading(false);
+    });
+  }, []);
 
-	return (
-		<List isLoading={isLoading}>
-			{networks.map((network, index) => (
-				<List.Item
-					key={index}
-					icon={Icon.Wifi}
-					title={network}
-					actions={
-						<ActionPanel>
-							<Action.Push
-								title="Show Details"
-								target={<DetailPassword networkName={network} setIsLoading={setIsLoading} />}
-							/>
-						</ActionPanel>
-					}
-				/>
-			))}
-		</List>
-	);
+  return (
+    <List isLoading={isLoading}>
+      {networks.map((network, index) => (
+        <List.Item
+          key={index}
+          icon={Icon.Wifi}
+          title={network}
+          actions={
+            <ActionPanel>
+              <Action.Push
+                title="Show Details"
+                target={<DetailPassword networkName={network} setIsLoading={setIsLoading} />}
+              />
+            </ActionPanel>
+          }
+        />
+      ))}
+    </List>
+  );
 }
