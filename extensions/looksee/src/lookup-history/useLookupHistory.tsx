2024b4df004cec12a97e56e0a388635e198c9f68
Per Nielsen Tikær <per@raycast.com>
2023-09-22 11:09:45 +0200

    Update the code

--- extensions/looksee/src/lookup-history/useLookupHistory.tsx
+++ extensions/looksee/src/lookup-history/useLookupHistory.tsx
@@ -1,22 +1,18 @@
-import { useEffect, useState } from "react";
 import { LookupHistoryItem } from "../common/types/lookupHistoryItem";
 import { LocalStorage } from "@raycast/api";
+import { usePromise } from "@raycast/utils";
 
 /**
  * Hook that returns lookups stored in localStorage.
  * Items are returned in descending order by time of lookup.
  */
 export const useLookupHistory = () => {
-  const [historyItems, setHistoryItems] = useState<LookupHistoryItem[]>([]);
+  const { isLoading, data } = usePromise(async () => {
+    const storageItems = await LocalStorage.allItems();
+    const historyItems: LookupHistoryItem[] = Object.keys(storageItems).map((key) => JSON.parse(storageItems[key]));
+    const descByTimestamp = historyItems.sort((a, b) => b.lookupTimestamp - a.lookupTimestamp);
+    return descByTimestamp;
+  });
 
-  useEffect(() => {
-    (async () => {
-      const storageItems = await LocalStorage.allItems();
-      const historyItems: LookupHistoryItem[] = Object.keys(storageItems).map((key) => JSON.parse(storageItems[key]));
-      const descByTimestamp = historyItems.sort((a, b) => b.lookupTimestamp - a.lookupTimestamp);
-      setHistoryItems(descByTimestamp);
-    })();
-  }, []);
-
-  return historyItems;
+  return { isLoading, data };
 };
--- extensions/looksee/src/lookupHistory.jsx
+++ extensions/looksee/src/lookupHistory.jsx
@@ -5,36 +5,37 @@ import { useLookupHistory } from "./lookup-history/useLookupHistory";
 
 export default function LookupHistory() {
   const [showingDetail, setShowingDetail] = useState(true);
-  const historyItems = useLookupHistory();
+  const { isLoading, data } = useLookupHistory();
 
   return (
-    <List searchBarPlaceholder={"Search Previous Lookups"} isShowingDetail={showingDetail}>
-      {historyItems?.map((item) => {
-        const props = {
-          detail: item.success ? (
-            <List.Item.Detail markdown={getLookupResponseMarkdown(item.lookupResponse)} />
-          ) : undefined,
-        };
+    <List searchBarPlaceholder={"Search Previous Lookups"} isShowingDetail={showingDetail} isLoading={isLoading}>
+      {data &&
+        data?.map((item) => {
+          const props = {
+            detail: item.success ? (
+              <List.Item.Detail markdown={getLookupResponseMarkdown(item.lookupResponse)} />
+            ) : undefined,
+          };
 
-        return (
-          <List.Item
-            key={item.address}
-            title={item.address}
-            accessories={[
-              {
-                icon: item.success ? Icon.Binoculars : Icon.Warning,
-                date: new Date(item.lookupTimestamp),
-              },
-            ]}
-            {...props}
-            actions={
-              <ActionPanel>
-                <Action title={"Toggle Details"} onAction={() => setShowingDetail(!showingDetail)} />
-              </ActionPanel>
-            }
-          />
-        );
-      })}
+          return (
+            <List.Item
+              key={item.address}
+              title={item.address}
+              accessories={[
+                {
+                  icon: item.success ? Icon.Binoculars : Icon.Warning,
+                  date: new Date(item.lookupTimestamp),
+                },
+              ]}
+              {...props}
+              actions={
+                <ActionPanel>
+                  <Action title={"Toggle Details"} onAction={() => setShowingDetail(!showingDetail)} />
+                </ActionPanel>
+              }
+            />
+          );
+        })}
     </List>
   );
 }
