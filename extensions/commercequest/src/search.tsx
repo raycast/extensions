8628c3a2563dd7849d388ef9256f1f469cdd1139
Sebastian K <github@skroll.me>
2023-09-23 17:38:17 +0200

    Update commercequest extension
    - Merge branch \'refs/heads/feature/item-type-filter\'
    - Pull contributions
    - added changelog-entry + updated screenshots
    - added working itemType filter
    - added a Detailview with a preview of a forum-thread

--- extensions/commercequest/src/search.tsx
+++ extensions/commercequest/src/search.tsx
@@ -1,12 +1,45 @@
-import { ActionPanel, Action, List, Icon, getPreferenceValues } from "@raycast/api";
+import { ActionPanel, Action, List, getPreferenceValues } from "@raycast/api";
 import { useFetch } from "@raycast/utils";
 import { useState } from "react";
 import { URLSearchParams } from "node:url";
+import { mapIconCode } from "./lib/utils";
 
-export default function Command() {
-  const [searchText, setSearchText] = useState("");
+import Preview from "./components/Preview";
+import { Breadcrumb, User, SearchResult, ItemType } from "./lib/types";
+
+const itemTypes: ItemType[] = [
+  { id: "", name: "All" },
+  { id: "article", name: "Articles" },
+  { id: "discussion", name: "Discussions" },
+  { id: "comment", name: "Comments" },
+  { id: "question", name: "Questions" },
+  { id: "answer", name: "Answers" },
+  { id: "group", name: "Groups" },
+  { id: "poll", name: "Polls" },
+];
+
+function ItemTypeDropdown(props: { itemTypes: ItemType[]; onItemTypeChange: (newValue: string) => void }) {
+  const { itemTypes, onItemTypeChange } = props;
+  return (
+    <List.Dropdown
+      tooltip="Select Type"
+      defaultValue=""
+      storeValue={true}
+      onChange={(newValue) => {
+        onItemTypeChange(newValue);
+      }}
+    >
+      <List.Dropdown.Section title="Item Types">
+        {itemTypes.map((itemType) => (
+          <List.Dropdown.Item key={itemType.id} title={itemType.name} value={itemType.id} />
+        ))}
+      </List.Dropdown.Section>
+    </List.Dropdown>
+  );
+}
+
+function fetchItems(searchText: string, filterItemType: string) {
   const searchParams = new URLSearchParams({
-    domain: "all_content",
     query: searchText.length === 0 ? "" : searchText,
     collapse: "true",
     limit: getPreferenceValues().numSearchResults,
@@ -14,12 +47,26 @@ export default function Command() {
   });
   searchParams.append("expand", "breadcrumbs");
   searchParams.append("expand", "-body");
+  searchParams.append("expand", "insertUser");
+  searchParams.append("types", filterItemType);
 
   const { data, isLoading } = useFetch("https://commercequest.space/api/v2/search?" + searchParams, {
     parseResponse: parseFetchResponse,
   });
 
+  return {
+    data,
+    isLoading,
+  };
+}
+
+export default function Command() {
+  const [searchText, setSearchText] = useState("");
+  const [filterItemType, setFilterItemType] = useState("");
+
   const emptyTitle = searchText.length === 0 ? "Recent topics" : "Search results";
+
+  const { data, isLoading } = fetchItems(searchText, filterItemType);
 
   return (
     <List
@@ -27,6 +74,7 @@ export default function Command() {
       onSearchTextChange={setSearchText}
       searchBarPlaceholder="Search CommerceQuest forum..."
       throttle
+      searchBarAccessory={<ItemTypeDropdown itemTypes={itemTypes} onItemTypeChange={setFilterItemType} />}
     >
       <List.Section title={emptyTitle} subtitle={data?.length + ""}>
         {data?.map((searchResult: SearchResult) => (
@@ -38,38 +86,22 @@ export default function Command() {
 }
 
 function SearchListItem({ searchResult }: { searchResult: SearchResult }) {
-  let iconCode;
-  switch (searchResult.type) {
-    case "discussion":
-      iconCode = Icon.SpeechBubbleActive;
-      break;
-    case "idea":
-      iconCode = Icon.LightBulb;
-      break;
-    case "question":
-      iconCode = Icon.QuestionMark;
-      break;
-    case "category":
-      iconCode = Icon.Layers;
-      break;
-    default:
-      iconCode = Icon.Circle;
-      break;
-  }
-
   const date = new Date(searchResult.dateUpdated ? searchResult.dateUpdated : searchResult.dateInserted);
 
   return (
     <List.Item
       title={searchResult.name}
       keywords={[searchResult.recordType]}
-      subtitle={searchResult.breadcrumbs}
+      subtitle={searchResult.breadcrumbsFormatted}
       accessories={[{ date: date, tooltip: date.toLocaleString() }]}
-      icon={iconCode}
+      icon={mapIconCode(searchResult.type)}
       actions={
         <ActionPanel>
           <ActionPanel.Section>
             <Action.OpenInBrowser url={searchResult.url} />
+            {searchResult.type != "category" ? (
+              <Action.Push title="Preview" target={<Preview {...searchResult} />} />
+            ) : null}
           </ActionPanel.Section>
         </ActionPanel>
       }
@@ -85,30 +117,49 @@ async function parseFetchResponse(response: Response) {
     throw new Error("message" in json ? json.message : response.statusText);
   }
 
+  // @TODO import tagIds and display on Detail
   return json.map(
     (result: {
-      breadcrumbs: { name: string }[];
-      recordID: any;
-      name: any;
-      recordType: any;
-      type: any;
-      dateUpdated: any;
-      dateInserted: any;
-      url: any;
+      breadcrumbs: Breadcrumb[];
+      recordID: string;
+      name: string;
+      recordType: string;
+      type: string;
+      dateUpdated: string;
+      dateInserted: string;
+      url: string;
+      body: string;
+      insertUser?: User;
     }) => {
-      let breadcrumbFormatted = "";
+      let breadcrumbsFormatted = "";
+      const breadcrumbs: Breadcrumb[] = [];
       if (result.breadcrumbs) {
         // remove first breadcrumb, it is always "home"
         result.breadcrumbs.shift();
 
         // remove second one from array and add as start for breadcrumbs
         const secondBreadcrumb = result.breadcrumbs.shift();
-        breadcrumbFormatted += secondBreadcrumb?.name;
+        if (secondBreadcrumb) {
+          breadcrumbsFormatted += secondBreadcrumb.name;
+          breadcrumbs.push(secondBreadcrumb);
+        }
 
         // suffix remaining breadcrumbs
-        result.breadcrumbs.forEach(function (item: { name: string }) {
-          breadcrumbFormatted = breadcrumbFormatted + " » " + item.name;
+        result.breadcrumbs.forEach(function (breadcrumb: Breadcrumb) {
+          breadcrumbsFormatted = breadcrumbsFormatted + " » " + breadcrumb.name;
+          breadcrumbs.push(breadcrumb);
         });
+      }
+
+      let insertUser = null;
+      if (result.insertUser) {
+        insertUser = {
+          name: result.insertUser.name,
+          photoUrl: result.insertUser.photoUrl,
+          url: result.insertUser.url,
+          label: result.insertUser.label,
+          title: result.insertUser.title,
+        } as User;
       }
 
       return {
@@ -119,19 +170,11 @@ async function parseFetchResponse(response: Response) {
         dateUpdated: result.dateUpdated,
         dateInserted: result.dateInserted,
         url: result.url,
-        breadcrumbs: breadcrumbFormatted,
+        breadcrumbsFormatted: breadcrumbsFormatted,
+        breadcrumbs: breadcrumbs,
+        body: result.body,
+        insertUser: insertUser,
       } as SearchResult;
     }
   );
-}
-
-interface SearchResult {
-  recordID: string;
-  name: string;
-  recordType: string;
-  type: string;
-  dateUpdated: string;
-  dateInserted: string;
-  url: string;
-  breadcrumbs: string;
 }
