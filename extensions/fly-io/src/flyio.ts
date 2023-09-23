7e5a050cf185b5cd54d2c081b760ff936e8fad8b
mie035 <hello@michitoshiyano.com>
2023-09-23 16:36:24 +0900

    add: search apps feat.

---
+++ extensions/fly-io/src/flyio.ts
@@ -0,0 +1,78 @@
+import fetch from "node-fetch";
+export const flyioGraphqlUrl = "https://api.fly.io/graphql";
+export const flyioBaseUrl = "https://fly.io/apps/";
+export interface AppNode {
+  id: string;
+  name: string;
+  hostname: string;
+}
+
+export interface OrganizationNode {
+  id: string;
+  name: string;
+  apps: AppConnection;
+}
+
+export interface AppEdge {
+  node: AppNode;
+}
+
+export interface OrganizationEdge {
+  node: OrganizationNode;
+}
+
+export interface AppConnection {
+  edges: AppEdge[];
+}
+
+export interface OrganizationConnection {
+  edges: OrganizationEdge[];
+}
+
+export interface QueryResult {
+  organizations: OrganizationConnection;
+}
+
+export const gqlRequest = async <T = any>(query: string, token?: string): Promise<T | null> => {
+  const res = await fetch("https://api.fly.io/graphql", {
+    method: "POST",
+    headers: { "Content-Type": "application/json", ...(token != null ? { Authorization: `Bearer ${token}` } : {}) },
+    body: JSON.stringify({ query }),
+  });
+
+  const json = (await res.json()) as any;
+  const data: T | null = json?.data;
+
+  return data;
+};
+
+export const fetchOrganizations = async (token: string): Promise<OrganizationConnection | null> => {
+  const res = await gqlRequest<QueryResult>(
+    `query { 
+        organizations{
+            edges{	
+              node{
+                id
+                name
+                apps{
+                  edges{
+                    node{
+                      id
+                      name
+                      hostname
+                    }
+                  }
+                }
+              }
+            }
+          }
+    }`,
+    token
+  );
+  const organizations = res?.organizations;
+  if (organizations == null) {
+    return null;
+  }
+
+  return organizations;
+};
---
+++ extensions/fly-io/src/search-apps.tsx
@@ -0,0 +1,78 @@
+import { ActionPanel, Detail, List, Action, getPreferenceValues } from "@raycast/api";
+import { useEffect, useState } from "react";
+import { OrganizationConnection, fetchOrganizations, flyioBaseUrl } from "./flyio";
+
+export interface Preferences {
+  flyioApiKey?: string;
+}
+
+export default function Command() {
+  const [organizations, setOrganizations] = useState<OrganizationConnection>();
+  useEffect(() => {
+    getOrgs();
+  }, []);
+
+  async function getOrgs() {
+    const preferences: Preferences = getPreferenceValues();
+    const organizations = await fetchOrganizations(preferences.flyioApiKey ?? "");
+    console.log(`Organizations: ${JSON.stringify(organizations)}`);
+    if (organizations) {
+      setOrganizations(organizations);
+    }
+  }
+
+  return (
+    <List>
+      {organizations?.edges?.map((edge) => {
+        return (
+          <List.Section key={edge?.node?.id} title={edge?.node?.name}>
+            {edge?.node?.apps?.edges?.map((app) => {
+              return (
+                <List.Item
+                  key={app?.node?.id}
+                  title={app?.node?.name}
+                  subtitle={app?.node?.hostname}
+                  actions={
+                    <ActionPanel>
+                      <Action.OpenInBrowser title="Go Overview" url={`${flyioBaseUrl}/${app?.node?.name}`} />
+                      <Action.CopyToClipboard
+                        title="Copy Hostname"
+                        content={app?.node?.hostname}
+                        shortcut={{ modifiers: ["cmd", "shift"], key: "c" }}
+                      />
+                      <Action.OpenInBrowser title="Go Machines" url={`${flyioBaseUrl}/${app?.node?.name}/machines`} />
+                      <Action.OpenInBrowser title="Go Volumes" url={`${flyioBaseUrl}/${app?.node?.name}/volumes`} />
+                      <ActionPanel.Section>
+                        <Action.OpenInBrowser title="Go Activity" url={`${flyioBaseUrl}/${app?.node?.name}/activity`} />
+                        <Action.OpenInBrowser title="Go Metrics" url={`${flyioBaseUrl}/${app?.node?.name}/metrics`} />
+                        <Action.OpenInBrowser
+                          title="Go Monitoring"
+                          url={`${flyioBaseUrl}/${app?.node?.name}/monitoring`}
+                        />
+                        <Action.OpenInBrowser
+                          title="Go Sentry_new"
+                          url={`${flyioBaseUrl}/${app?.node?.name}/sentry_new`}
+                        />
+                      </ActionPanel.Section>
+                      <ActionPanel.Section>
+                        <Action.OpenInBrowser
+                          title="Go Certificates"
+                          url={`${flyioBaseUrl}/${app?.node?.name}/certificates`}
+                        />
+                        <Action.OpenInBrowser title="Go Secrets" url={`${flyioBaseUrl}/${app?.node?.name}/secrets`} />
+                      </ActionPanel.Section>
+                      <ActionPanel.Section>
+                        <Action.OpenInBrowser title="Go Tokens" url={`${flyioBaseUrl}/${app?.node?.name}/secrets`} />
+                        <Action.OpenInBrowser title="Go Settings" url={`${flyioBaseUrl}/${app?.node?.name}/tokens`} />
+                      </ActionPanel.Section>
+                    </ActionPanel>
+                  }
+                />
+              );
+            })}
+          </List.Section>
+        );
+      })}
+    </List>
+  );
+}
