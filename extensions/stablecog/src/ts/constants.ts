f546d35545d245cb69dbd39e1f4e350e40cb527f
yekta <ngyekta@gmail.com>
2023-09-26 05:18:25 +0300

    Use local gif

--- extensions/stablecog/src/ts/constants.ts
+++ extensions/stablecog/src/ts/constants.ts
@@ -1,4 +1,4 @@
-export const loadingGif = "https://stablecog.com/gifs/stars-512w.gif";
+export const loadingGif = "stars-512w.gif";
 
 export const defaultGridColumns = 4;
 export const defaultGridColumnsForImagine = 2;
