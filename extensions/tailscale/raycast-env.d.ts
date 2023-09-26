raycast/extensions

commit bed2b201b1db1c405b70c8c5486674180e8827d1

feat(tailscale): add preferences for path and exec wrapper (#8287)
* feat: add preferences for path and exec wrapper

* chore: add changelog entry

* chore: more idiomatic typescript

* chore: fix CI

extensions/tailscale/raycast-env.d.ts
@@ -7,7 +7,10 @@
 
 /* eslint-disable @typescript-eslint/ban-types */
 
-type ExtensionPreferences = {}
+type ExtensionPreferences = {
+  /** Tailscale CLI Path - Location to the Tailscale CLI (Defaults to `/Applications/Tailscale.app/Contents/MacOS/Tailscale`) */
+  "tailscalePath"?: string
+}
 
 /** Preferences accessible in all the extension's commands */
 declare type Preferences = ExtensionPreferences

extensions/tailscale/src/account-switcher.tsx
@@ -11,7 +11,7 @@ import {
   Toast,
 } from "@raycast/api";
 import { useEffect, useState } from "react";
-import { execSync } from "child_process";
+import { tailscale } from "./shared";
 
 interface User {
   active: boolean;
@@ -39,7 +39,6 @@ function loadUsers(unparsedUsers: string[]) {
 
     users.push(user);
   }
-  console.log(users);
   return users;
 }
 
@@ -48,14 +47,12 @@ function AccountSwitchList() {
   useEffect(() => {
     async function fetch() {
       try {
-        const ret = execSync("/Applications/Tailscale.app/Contents/MacOS/Tailscale switch --list").toString().trim();
-        console.log(ret);
+        const ret = tailscale(`--list`);
         const data: string[] = ret.split("\n");
 
         const _list = loadUsers(data);
         setUsers(_list);
       } catch (error) {
-        console.log(error);
         showToast(Toast.Style.Failure, "Couldn't load users. Make sure Tailscale is connected.");
       }
     }
@@ -68,7 +65,7 @@ function AccountSwitchList() {
   // return a list of users, starting with all of the inactive users.
   // output the active user last.
   return (
-    <List isLoading={users === undefined}>
+    <List isLoading={!users}>
       {users
         ?.sort((a, b) => +a.active - +b.active)
         .map((user) => (
@@ -87,11 +84,9 @@ function AccountSwitchList() {
                       title: "Switching user account",
                       message: `${user.name}`,
                     });
-                    const command = `/Applications/Tailscale.app/Contents/MacOS/Tailscale switch ${user.name}`;
-                    console.log(command);
                     popToRoot();
                     closeMainWindow();
-                    const ret = execSync(command).toString().trim();
+                    const ret = tailscale(`switch ${user.name}`);
 
                     if (ret.includes("Success") || ret.includes("Already")) {
                       showHUD(`Active Tailscale user is ${user.name}`);

extensions/tailscale/src/devices.tsx
@@ -1,16 +1,13 @@
 import { ActionPanel, List, Action, Icon, showToast, Toast, Image } from "@raycast/api";
 import { useEffect, useState } from "react";
-import { execSync } from "child_process";
-import { Device, LooseObject, loadDevices } from "./shared";
+import { Device, LooseObject, loadDevices, tailscale } from "./shared";
 
 function DeviceList() {
   const [devices, setDevices] = useState<Device[]>();
   useEffect(() => {
     async function fetch() {
       try {
-        const ret = await execSync("/Applications/Tailscale.app/Contents/MacOS/Tailscale status --json")
-          .toString()
-          .trim();
+        const ret = tailscale(`status --json`)!;
         const data: LooseObject = JSON.parse(ret);
 
         if (!data.Self.Online) {
@@ -27,7 +24,7 @@ function DeviceList() {
   }, []);
 
   return (
-    <List isLoading={devices === undefined}>
+    <List isLoading={!devices}>
       {devices?.map((device) => (
         <List.Item
           title={device.name}

extensions/tailscale/src/exit.tsx
@@ -1,7 +1,6 @@
 import { ActionPanel, List, Action, showToast, popToRoot, closeMainWindow, Toast, Image } from "@raycast/api";
 import { useEffect, useState } from "react";
-import { execSync } from "child_process";
-import { LooseObject, loadDevices } from "./shared";
+import { LooseObject, loadDevices, tailscale } from "./shared";
 
 function loadExitNodes(self: LooseObject, data: LooseObject) {
   const devices = loadDevices(self, data);
@@ -22,14 +21,12 @@ function isExitNodeActive(devices: any) {
 }
 
 function setExitNode(host: string, allowLAN: boolean) {
-  const command = `/Applications/Tailscale.app/Contents/MacOS/Tailscale set --exit-node "${host}"`;
-  console.log(command);
   popToRoot();
   closeMainWindow();
-  execSync(command).toString().trim();
+  tailscale(`set --exit-node "${host}"`);
 
   if (allowLAN) {
-    execSync("/Applications/Tailscale.app/Contents/MacOS/Tailscale set --exit-node-allow-lan-access").toString().trim();
+    tailscale(`set --exit-node-allow-lan-access`);
   }
 }
 
@@ -39,7 +36,7 @@ function ExitNodeList() {
   useEffect(() => {
     async function fetch() {
       try {
-        const ret = execSync("/Applications/Tailscale.app/Contents/MacOS/Tailscale status --json").toString().trim();
+        const ret = tailscale(`status --json`)!;
         const data: LooseObject = JSON.parse(ret);
 
         if (!data.Self.Online) {
@@ -60,14 +57,14 @@ function ExitNodeList() {
   }, []);
 
   return (
-    <List isLoading={exitNodes === undefined}>
+    <List isLoading={!exitNodes}>
       {isActive && (
         <List.Item
           key="_disable"
           title="Turn off exit node"
           actions={
             <ActionPanel>
-              <Action title="Turn off Exit Node" onAction={() => setExitNode("", false)} />
+              <Action title="Turn Off Exit Node" onAction={() => setExitNode("", false)} />
             </ActionPanel>
           }
         />

extensions/tailscale/src/my-devices.tsx
@@ -1,16 +1,13 @@
 import { ActionPanel, List, Action, Icon, showToast, Toast, Image } from "@raycast/api";
 import { useEffect, useState } from "react";
-import { execSync } from "child_process";
-import { Device, LooseObject, loadDevices } from "./shared";
+import { Device, LooseObject, loadDevices, tailscale } from "./shared";
 
 function MyDeviceList() {
   const [devices, setDevices] = useState<Device[]>();
   useEffect(() => {
     async function fetch() {
       try {
-        const ret = await execSync("/Applications/Tailscale.app/Contents/MacOS/Tailscale status --json")
-          .toString()
-          .trim();
+        const ret = tailscale(`status --json`)!;
         const data: LooseObject = JSON.parse(ret);
 
         if (!data.Self.Online) {
@@ -18,7 +15,6 @@ function MyDeviceList() {
         }
         const me: string = data.Self.UserID;
         const _list = loadDevices(data.Self, data.Peer);
-        console.log(_list);
         const _mylist = _list.filter((device) => device.userid === me);
         setDevices(_mylist);
       } catch (error) {
@@ -29,7 +25,7 @@ function MyDeviceList() {
   }, []);
 
   return (
-    <List isLoading={devices === undefined}>
+    <List isLoading={!devices}>
       {devices?.map((device) => (
         <List.Item
           title={device.name}

extensions/tailscale/src/shared.tsx
@@ -1,3 +1,7 @@
+import { Toast, getPreferenceValues, showToast } from "@raycast/api";
+import { execSync } from "node:child_process";
+import fs from "node:fs";
+
 export interface Device {
   self: boolean;
   key: number;
@@ -55,6 +59,16 @@ export function loadDevices(self: LooseObject, data: LooseObject) {
     };
     devices.push(device);
   }
-  console.log(devices);
   return devices;
+}
+
+const prefs = getPreferenceValues();
+
+export const tailscalePath: string =
+  prefs.tailscalePath && prefs.tailscalePath.length > 0
+    ? prefs.tailscalePath
+    : "/Applications/Tailscale.app/Contents/MacOS/Tailscale";
+
+export function tailscale(parameters: string): string {
+  return execSync(`${tailscalePath} ${parameters}`).toString().trim();
 }

commit da9c3ffde0db9cc81674c63bdeeea7844c51ef85

Refine error handling
This commit refines error handling in the extension, showing a
consistent error message when the Tailscale CLI Path is incorrect, or
when Tailscale isn't running.

This commit also fixes a bug introduced in #8287 where the account
switcher wouldn't show accounts.

extensions/tailscale/raycast-env.d.ts
@@ -40,3 +40,13 @@ declare namespace Arguments {
   /** Arguments passed to the `admin` command */
   export type Admin = {}
 }
+
+
+declare module "swift:*" {
+  function run<T = unknown, U = any>(command: string, input?: U): Promise<T>;
+  export default run;
+	export class SwiftError extends Error {
+    stderr: string;
+    stdout: string;
+  }
+}

extensions/tailscale/src/account-switcher.tsx
@@ -11,7 +11,7 @@ import {
   Toast,
 } from "@raycast/api";
 import { useEffect, useState } from "react";
-import { tailscale } from "./shared";
+import { handleError, tailscale } from "./shared";
 
 interface User {
   active: boolean;
@@ -47,13 +47,12 @@ function AccountSwitchList() {
   useEffect(() => {
     async function fetch() {
       try {
-        const ret = tailscale(`--list`);
-        const data: string[] = ret.split("\n");
-
+        const ret = tailscale(`switch --list`);
+        const data = ret.split("\n");
         const _list = loadUsers(data);
         setUsers(_list);
       } catch (error) {
-        showToast(Toast.Style.Failure, "Couldn't load users. Make sure Tailscale is connected.");
+        handleError(error, "Couldn't load users. Make sure Tailscale is connected.");
       }
     }
     fetch();

extensions/tailscale/src/devices.tsx
@@ -1,23 +1,17 @@
-import { ActionPanel, List, Action, Icon, showToast, Toast, Image } from "@raycast/api";
+import { ActionPanel, List, Action, Icon, Image } from "@raycast/api";
 import { useEffect, useState } from "react";
-import { Device, LooseObject, loadDevices, tailscale } from "./shared";
+import { Device, getStatus, handleError, getDevices } from "./shared";
 
 function DeviceList() {
   const [devices, setDevices] = useState<Device[]>();
   useEffect(() => {
     async function fetch() {
       try {
-        const ret = tailscale(`status --json`)!;
-        const data: LooseObject = JSON.parse(ret);
-
-        if (!data.Self.Online) {
-          throw "Tailscale not connected";
-        }
-
-        const _list = loadDevices(data.Self, data.Peer);
+        const status = getStatus();
+        const _list = getDevices(status);
         setDevices(_list);
       } catch (error) {
-        showToast(Toast.Style.Failure, "Couldn't load devices. Make sure Tailscale is connected.");
+        handleError(error, "Couldn't load users. Make sure Tailscale is connected.");
       }
     }
     fetch();

extensions/tailscale/src/exit.tsx
@@ -1,10 +1,9 @@
 import { ActionPanel, List, Action, showToast, popToRoot, closeMainWindow, Toast, Image } from "@raycast/api";
 import { useEffect, useState } from "react";
-import { LooseObject, loadDevices, tailscale } from "./shared";
+import { StatusResponse, getStatus, getDevices, tailscale } from "./shared";
 
-function loadExitNodes(self: LooseObject, data: LooseObject) {
-  const devices = loadDevices(self, data);
-
+function loadExitNodes(status: StatusResponse) {
+  const devices = getDevices(status);
   return devices.filter((element) => {
     return element.exitnodeoption;
   });
@@ -36,21 +35,15 @@ function ExitNodeList() {
   useEffect(() => {
     async function fetch() {
       try {
-        const ret = tailscale(`status --json`)!;
-        const data: LooseObject = JSON.parse(ret);
-
-        if (!data.Self.Online) {
-          throw "Tailscale not connected";
-        }
-
-        const _list = loadExitNodes(data.Self, data.Peer);
+        const status = getStatus();
+        const _list = loadExitNodes(status);
         setExitNodes(_list);
 
         if (isExitNodeActive(_list)) {
           setIsActive(true);
         }
       } catch (error) {
-        showToast(Toast.Style.Failure, "Couldn't load exitNodes. Make sure Tailscale is connected.");
+        showToast(Toast.Style.Failure, "Couldn't load exit nodes. Make sure Tailscale is connected.");
       }
     }
     fetch();

extensions/tailscale/src/my-devices.tsx
@@ -1,24 +1,19 @@
-import { ActionPanel, List, Action, Icon, showToast, Toast, Image } from "@raycast/api";
+import { ActionPanel, List, Action, Icon, Image } from "@raycast/api";
 import { useEffect, useState } from "react";
-import { Device, LooseObject, loadDevices, tailscale } from "./shared";
+import { Device, getStatus, handleError, getDevices } from "./shared";
 
 function MyDeviceList() {
   const [devices, setDevices] = useState<Device[]>();
   useEffect(() => {
     async function fetch() {
       try {
-        const ret = tailscale(`status --json`)!;
-        const data: LooseObject = JSON.parse(ret);
-
-        if (!data.Self.Online) {
-          throw "Tailscale not connected";
-        }
-        const me: string = data.Self.UserID;
-        const _list = loadDevices(data.Self, data.Peer);
+        const status = getStatus();
+        const me: string = status.Self.UserID.toString();
+        const _list = getDevices(status);
         const _mylist = _list.filter((device) => device.userid === me);
         setDevices(_mylist);
       } catch (error) {
-        showToast(Toast.Style.Failure, "Couldn't load devices. Make sure Tailscale is connected.");
+        handleError(error, "Couldn't load devices. Make sure Tailscale is connected.");
       }
     }
     fetch();

extensions/tailscale/src/shared.tsx
@@ -1,10 +1,9 @@
 import { Toast, getPreferenceValues, showToast } from "@raycast/api";
 import { execSync } from "node:child_process";
-import fs from "node:fs";
 
 export interface Device {
   self: boolean;
-  key: number;
+  key: string;
   name: string;
   userid: string;
   dns: string;
@@ -17,19 +16,59 @@ export interface Device {
   exitnodeoption: boolean;
 }
 
-export interface LooseObject {
-  [key: string]: any;
+export class NotConnectedError extends Error {}
+export class InvalidPathError extends Error {}
+
+export type StatusDevice = {
+  Active: boolean;
+  ID: string;
+  DNSName: string;
+  ExitNode: boolean;
+  ExitNodeOption: boolean;
+  Online: boolean;
+  OS: string;
+  TailscaleIPs: string[];
+  LastSeen: string;
+  UserID: number;
+};
+
+/**
+ * StatusResponse is a subset of the fields returned by `tailscale status --json`.
+ */
+export type StatusResponse = {
+  Peer: Record<string, StatusDevice>;
+  Self: StatusDevice;
+  TailscaleIPs: string[];
+  User: Record<
+    string,
+    {
+      ID: number;
+      DisplayName: string;
+      LoginName: string;
+      ProfilePictureURL: string;
+    }
+  >;
+  Version: string;
+};
+
+export function getStatus() {
+  const resp = tailscale(`status --json`)!;
+  const data = JSON.parse(resp) as StatusResponse;
+  if (!data || !data.Self.Online) {
+    throw new NotConnectedError();
+  }
+  return data;
 }
 
-export function loadDevices(self: LooseObject, data: LooseObject) {
+export function getDevices(status: StatusResponse) {
   const devices: Device[] = [];
-  let theKey = 0;
+  const self = status.Self;
 
   const me = {
     self: true,
-    key: ++theKey,
+    key: self.ID,
     name: self.DNSName.split(".")[0],
-    userid: self.UserID,
+    userid: self.UserID.toString(),
     dns: self.DNSName,
     ipv4: self.TailscaleIPs[0],
     ipv6: self.TailscaleIPs[1],
@@ -42,20 +81,20 @@ export function loadDevices(self: LooseObject, data: LooseObject) {
 
   devices.push(me);
 
-  for (const [key, value] of Object.entries(data)) {
+  for (const [, peer] of Object.entries(status.Peer)) {
     const device = {
       self: false,
-      key: ++theKey,
-      name: value.DNSName.split(".")[0],
-      userid: value.UserID,
-      dns: value.DNSName,
-      ipv4: value.TailscaleIPs[0],
-      ipv6: value.TailscaleIPs[1],
-      os: value.OS == "linux" ? "Linux" : value.OS,
-      online: value.Online,
-      lastseen: new Date(value.LastSeen),
-      exitnode: value.ExitNode,
-      exitnodeoption: value.ExitNodeOption,
+      key: peer.ID,
+      name: peer.DNSName.split(".")[0],
+      userid: peer.UserID.toString(),
+      dns: peer.DNSName,
+      ipv4: peer.TailscaleIPs[0],
+      ipv6: peer.TailscaleIPs[1],
+      os: peer.OS == "linux" ? "Linux" : peer.OS,
+      online: peer.Online,
+      lastseen: new Date(peer.LastSeen),
+      exitnode: peer.ExitNode,
+      exitnodeoption: peer.ExitNodeOption,
     };
     devices.push(device);
   }
@@ -64,11 +103,40 @@ export function loadDevices(self: LooseObject, data: LooseObject) {
 
 const prefs = getPreferenceValues();
 
-export const tailscalePath: string =
+const tailscalePath: string =
   prefs.tailscalePath && prefs.tailscalePath.length > 0
     ? prefs.tailscalePath
     : "/Applications/Tailscale.app/Contents/MacOS/Tailscale";
 
+/**
+ * tailscale runs a command against the Tailscale CLI.
+ */
 export function tailscale(parameters: string): string {
-  return execSync(`${tailscalePath} ${parameters}`).toString().trim();
+  try {
+    return execSync(`${tailscalePath} ${parameters}`).toString().trim();
+  } catch (err) {
+    if (err instanceof Error) {
+      if (err.message.includes("No such file or directory")) {
+        throw new InvalidPathError();
+      }
+      if (err.message.includes("is Tailscale running?")) {
+        throw new NotConnectedError();
+      }
+    }
+    throw err;
+  }
+}
+
+export function handleError(err: unknown, fallbackMessage: string) {
+  if (err instanceof InvalidPathError) {
+    showToast(
+      Toast.Style.Failure,
+      "Your Tailscale CLI Path is invalid. Update your extension preferences to fix this.",
+    );
+    return;
+  } else if (err instanceof NotConnectedError) {
+    showToast(Toast.Style.Failure, "Can’t connect to Tailscale. Make sure Tailscale is open and running.");
+    return;
+  }
+  showToast(Toast.Style.Failure, fallbackMessage);
 }