a06832c6c1e49629457c1561ea1c4e015501e819
maximilianzuern <max.zuern@gmail.com>
2023-09-28 20:48:34 +0200

    working prototype

--- extensions/Splitwise/src/list-expenses.tsx
+++ extensions/Splitwise/src/list-expenses.tsx
@@ -3,21 +3,49 @@ import { Expense } from "./types/get_expenses.types";
 import { GetExpense, DeleteExpense, UpdateExpense } from "./hooks/useList";
 import { loadingLimit } from "./hooks/userPreferences";
 import { GetCurrentUser } from "./hooks/useCurrentUser";
+import { getGroups } from "./hooks/useFriends_Groups";
+
+import { getCurrency_code } from "./utils/utils";
 
 import { Action, ActionPanel, Icon, Image, List, Color, Form, useNavigation, Keyboard } from "@raycast/api";
+
+function getColor(expense: Expense, currentUserID: number) {
+  let netBalance = expense.users
+    .filter((user) => user.user.id === currentUserID)
+    .map((user) => Number(user.net_balance))[0];
+
+  if (netBalance > 0) {
+    return Color.Green;
+  } else if (netBalance < 0) {
+    return Color.Red;
+  } else {
+    return Color.PrimaryText;
+  }
+}
+
+function expenseSplitEqually(owes: string[]) {
+  const similarThreshold = 0.03;
+  const owedShareNumbers = owes.map((owedShare) => Number(owedShare));
+  const firstOwedShare = owedShareNumbers[0];
+  const isSimilarOwedShares = owedShareNumbers.every(
+    (owedShare) => Math.abs(owedShare - firstOwedShare) <= similarThreshold
+  );
+  return isSimilarOwedShares;
+}
 
 export default function Command() {
   const [expenses, loadingExpenses, revalidate, Mutate] = GetExpense(loadingLimit); // FETCH EXPENSES
 
   const currentUser = GetCurrentUser(); // FETCH CURRENT USER
-  const currentUserID = currentUser?.id;
+  const currentUserID = currentUser?.id as number;
+  const [groups, loadingGroups] = getGroups();
 
   const handleDeleteExpense = (expenseID: number) => {
     DeleteExpense(expenseID, Mutate);
   };
 
   return (
-    <List isShowingDetail searchBarPlaceholder="Search Expenses" isLoading={loadingExpenses}>
+    <List isShowingDetail searchBarPlaceholder="Search Expenses" isLoading={loadingExpenses || loadingGroups}>
       {expenses
         .filter((expense) => expense.deleted_at === null)
         .map((expense) => (
@@ -26,21 +54,20 @@ export default function Command() {
             keywords={expense.users.map((user) => user.user.first_name)}
             title={expense.description}
             accessories={[
-              { icon: expense.group_id ? Icon.TwoPeople : "", tooltip: "Group Expense" },
+              {
+                icon: expense.group_id ? Icon.TwoPeople : "",
+                tooltip: `Group: ${
+                  expense.group_id ? groups.filter((group) => group.id === expense.group_id)[0].name : ""
+                }`,
+              },
               { icon: expense.payment ? Icon.BankNote : "", tooltip: "Payment" },
               {
                 tag: {
-                  value: `${expense.cost} ${expense.currency_code}`,
-                  color:
-                    Number(
-                      expense.users
-                        .filter((user) => Number(user.net_balance) > 0 && user.user.id === currentUserID)
-                        .map((user) => user.net_balance)
-                    ) > 0
-                      ? Color.Green
-                      : Color.Red,
+                  value: `${Number(expense.cost).toFixed(2)} ${getCurrency_code(expense.currency_code)}`,
+                  color: getColor(expense, currentUserID),
+                  // color: currentUserID ? getColor(expense, currentUserID) : Color.PrimaryText,
                 },
-                tooltip: `Amount: ${expense.cost} ${expense.currency_code}`,
+                tooltip: `Amount: ${Number(expense.cost).toFixed(2)} ${getCurrency_code(expense.currency_code)}`,
               },
             ]}
             detail={
@@ -50,85 +77,93 @@ export default function Command() {
                   <List.Item.Detail.Metadata>
                     <List.Item.Detail.Metadata.TagList title={expense.description}>
                       <List.Item.Detail.Metadata.TagList.Item
-                        text={`${expense.cost} ${expense.currency_code}`}
-                        color={Color.PrimaryText}
+                        text={`${Number(expense.cost).toFixed(2)} ${getCurrency_code(expense.currency_code)}`}
+                        color={getColor(expense, currentUserID)}
                         key={expense.id}
                         icon={Icon.Coins}
                       />
                     </List.Item.Detail.Metadata.TagList>
-
+                    {expense.group_id && ( // GROUP EXPENSES
+                      <>
+                        <List.Item.Detail.Metadata.Separator />
+                        <List.Item.Detail.Metadata.Link
+                          title="Group Expense"
+                          text={`Open '${groups.filter((group) => group.id === expense.group_id)[0].name}'`}
+                          target={`https://secure.splitwise.com/#/groups/${expense.group_id}`}
+                        />
+                      </>
+                    )}
                     <List.Item.Detail.Metadata.Separator />
-
-                    <List.Item.Detail.Metadata.Label
-                      title={`Date`}
+                    <List.Item.Detail.Metadata.Label // DATE OF CREATION
+                      title={`Created by ${
+                        expense.created_by["id"] === currentUserID ? "You" : expense.created_by["first_name"]
+                      }`}
                       icon={Icon.Calendar}
-                      text={new Date(expense.date).toDateString()}
+                      text={new Date(expense.date).toLocaleString(undefined, { dateStyle: "full", timeStyle: "short" })}
                       key={expense.created_by["id"]}
                     />
                     {expense.updated_by && (
-                      <List.Item.Detail.Metadata.Label
-                        // title={`Last updated by ${expense.updated_by["first_name"]}`}
+                      <List.Item.Detail.Metadata.Label // DATE OF LAST UPDATE
                         title={`Last updated by ${
                           expense.updated_by["id"] === currentUserID ? "You" : expense.updated_by["first_name"]
                         }`}
                         icon={Icon.Pencil}
-                        text={new Date(expense.updated_at).toDateString()}
+                        text={new Date(expense.date).toLocaleString(undefined, {
+                          dateStyle: "full",
+                          timeStyle: "short",
+                        })}
                       />
                     )}
-
                     <List.Item.Detail.Metadata.Separator />
-
                     <List.Item.Detail.Metadata.TagList title={`Paid by`}>
                       {expense.users
                         .filter((user) => Number(user.paid_share) > 0)
                         .map((user) => (
                           <List.Item.Detail.Metadata.TagList.Item
-                            // text={`${user.user.first_name} paid ${user.paid_share} ${expense.currency_code}`}
-                            text={`${user.user.id === currentUserID ? "You" : user.user.first_name} paid ${
-                              user.paid_share
-                            } ${expense.currency_code}`}
+                            text={
+                              Number(user.owed_share) === 0
+                                ? `${user.user.first_name} paid ${Number(user.paid_share).toFixed(
+                                    2
+                                  )} ${getCurrency_code(expense.currency_code)}`
+                                : `${user.user.first_name} paid ${Number(user.paid_share).toFixed(
+                                    2
+                                  )} ${getCurrency_code(expense.currency_code)} and owes ${Number(
+                                    user.owed_share
+                                  ).toFixed(2)} ${getCurrency_code(expense.currency_code)}`
+                            }
                             icon={{ source: user.user.picture.medium, mask: Image.Mask.Circle }}
                             color={Color.Green}
                             key={user.user_id}
                           />
                         ))}
                     </List.Item.Detail.Metadata.TagList>
-
                     <List.Item.Detail.Metadata.Separator />
-
-                    <List.Item.Detail.Metadata.TagList title={`Pays back`}>
+                    <List.Item.Detail.Metadata.TagList title={expense.payment === true ? "Received by" : "Owed by"}>
                       {expense.users
                         .filter((user) => Number(user.net_balance) < 0)
                         .map((user) => (
                           <List.Item.Detail.Metadata.TagList.Item
-                            text={`${
-                              user.user.id === currentUserID ? "You owe" : user.user.first_name + " owes"
-                            } ${String(Number(user.net_balance) * -1)} ${expense.currency_code}`}
+                            text={
+                              expense.payment === true
+                                ? `${user.user.first_name} received ${(Number(user.net_balance) * -1).toFixed(
+                                    2
+                                  )} ${getCurrency_code(expense.currency_code)}`
+                                : `${user.user.first_name} owes ${(Number(user.net_balance) * -1).toFixed(
+                                    2
+                                  )} ${getCurrency_code(expense.currency_code)}`
+                            }
                             icon={{ source: user.user.picture.medium, mask: Image.Mask.Circle }}
                             color={Color.Red}
                             key={user.user_id}
                           />
                         ))}
                     </List.Item.Detail.Metadata.TagList>
-
-                    {expense.group_id && ( // GROUP EXPENSES
-                      <>
-                        <List.Item.Detail.Metadata.Separator />
-                        <List.Item.Detail.Metadata.Link
-                          title="Group Expense"
-                          text="View Group"
-                          target={`https://secure.splitwise.com/#/groups/${expense.group_id}`}
-                        />
-                      </>
-                    )}
-
                     {expense.repeats === true && ( // REPEATING EXPENSES
                       <>
                         <List.Item.Detail.Metadata.Separator />
                         <List.Item.Detail.Metadata.Label title="Repeating Expense" text={expense.repeat_interval} />
                       </>
                     )}
-
                     {expense.receipt.original !== null && ( // RECEIPT
                       <>
                         <List.Item.Detail.Metadata.Separator />
@@ -145,19 +180,24 @@ export default function Command() {
             }
             actions={
               <ActionPanel>
-                <Action.OpenInBrowser
-                  title="Open Expense in Splitwise"
-                  url={`https://secure.splitwise.com/#/all/expenses/${expense.id}`}
-                  shortcut={Keyboard.Shortcut.Common.Open}
-                />
-                {expense.users.filter((user) => Number(user.paid_share) > 0).length <= 1 && ( // if more than one person paid, don't allow to change values
-                  <Action.Push
-                    title="Change values"
-                    icon={Icon.Pencil}
-                    target={<ChangeValues expense={expense} />}
-                    shortcut={Keyboard.Shortcut.Common.Edit}
+                <ActionPanel.Section>
+                  <Action.OpenInBrowser
+                    title="Open Expense in Splitwise"
+                    url={`https://secure.splitwise.com/#/all/expenses/${expense.id}`}
+                    shortcut={Keyboard.Shortcut.Common.Open}
                   />
-                )}
+                  {expense.users.filter((user) => Number(user.paid_share) > 0).length <= 1 && expenseSplitEqually(expense.users.map((user) => user.owed_share)) === true && ( // if more than 1 person paid, don't allow to change values
+                    <Action.Push
+                      title="Change values"
+                      icon={Icon.Pencil}
+                      target={<ChangeValues expense={expense} />}
+                      shortcut={Keyboard.Shortcut.Common.Edit}
+                    />
+                  )}
+                  {expense.receipt.original !== null && (
+                    <Action.OpenInBrowser title="Open Receipt" url={expense.receipt.original} icon={Icon.Receipt} />
+                  )}
+                </ActionPanel.Section>
                 <Action
                   title="Reload"
                   icon={Icon.Repeat}
@@ -183,17 +223,33 @@ export default function Command() {
 // Comment out some lines due to updating costs not working at the moment
 import { useForm, FormValidation } from "@raycast/utils";
 import { getFriends } from "./hooks/useFriends_Groups";
+import { useState, useEffect } from "react";
 
 function ChangeValues(handedOverValues: { expense: Expense }) {
   const { expense } = handedOverValues;
-  const { pop } = useNavigation();
 
+  // const [splitEqually, setSplitEqually] = useState(false);
+  // useEffect(() => {
+  //   const similarThreshold = 0.03;
+  //   const checkSimilarOwedShares = (owedShares: string[]) => {
+  //     const owedShareNumbers = owedShares.map((owedShare) => Number(owedShare));
+  //     const firstOwedShare = owedShareNumbers[0];
+  //     const isSimilarOwedShares = owedShareNumbers.every(
+  //       (owedShare) => Math.abs(owedShare - firstOwedShare) <= similarThreshold
+  //     );
+  //     if (!isSimilarOwedShares) {
+  //       setSplitEqually(true);
+  //     }
+  //   };
+  //   checkSimilarOwedShares(expense.users.map((user) => user.owed_share));
+  // }, []);
+
+  const { pop } = useNavigation();
   const [friends] = getFriends();
   const currentUser = GetCurrentUser() as any; // FETCH CURRENT USER
   const friendsWithCurrentUser = [...friends, currentUser];
 
-  const { handleSubmit, itemProps } = useForm<Expense | any>({
-    //Expense
+  const { handleSubmit, itemProps, values, setValue, focus } = useForm<Expense | any>({
     onSubmit: (input) => {
       const numberShares = input.owes?.length;
       const cost = Number(input.cost);
@@ -222,16 +278,17 @@ function ChangeValues(handedOverValues: { expense: Expense }) {
         counter++;
       });
 
-      UpdateExpense(expense.id, paramsJson).then(() => pop());
-      // console.log(paramsJson, expense.id); // DEBUG
+      // UpdateExpense(expense.id, paramsJson).then(() => pop());
+      console.log(paramsJson); // DEBUG
     },
 
     initialValues: {
-      cost: expense.cost,
+      cost: Number(expense.cost).toFixed(2),
       description: expense.description,
       date: new Date(expense.date),
       paid: expense.users.filter((user) => Number(user.paid_share) > 0)[0].user.id.toString(),
       owes: expense.users.filter((user) => Number(user.owed_share) > 0).map((user) => String(user.user.id)),
+      // split: splitEqually,
     },
 
     validation: {
@@ -239,11 +296,10 @@ function ChangeValues(handedOverValues: { expense: Expense }) {
       date: FormValidation.Required,
       paid: FormValidation.Required,
       owes: (input) => {
-        if (input.length < 2) {
+        if (input.length < 2 && expense.payment === false) {
           return "Select at least 2 people";
         }
       },
-
       cost: (input) => {
         if (!input?.match(/^\d+(\.\d{1,2})?$/)) {
           // check if input is integer or float with 1 or 2 decimal places
@@ -264,11 +320,15 @@ function ChangeValues(handedOverValues: { expense: Expense }) {
     >
       <Form.TextField title="Description" {...itemProps.description} />
       <Form.DatePicker title="Date of Expense" {...itemProps.date} />
-      <Form.TextField title={`Cost in ${expense.currency_code}`} {...itemProps.cost} />
-
+      <Form.TextField title={`Cost in ${getCurrency_code(expense.currency_code)}`} {...itemProps.cost} />
       <Form.Separator />
-
-      <Form.Dropdown title="Who paid?" {...itemProps.paid}>
+      {/* <Form.Checkbox
+        label="Split expense equally"
+        value={splitEqually}
+        onChange={() => setSplitEqually(!splitEqually)}
+        id="split"
+      /> */}
+      <Form.Dropdown title="Who paid?" {...itemProps.paid} >
         {friendsWithCurrentUser.map((friend) => (
           <Form.Dropdown.Item
             key={friend.id}
@@ -278,6 +338,7 @@ function ChangeValues(handedOverValues: { expense: Expense }) {
           />
         ))}
       </Form.Dropdown>
+
       {/* <Form.Dropdown title="Who paid?" {...itemProps.paid}>
         {expense.users.map((user) => (
           <Form.Dropdown.Item
@@ -293,6 +354,12 @@ function ChangeValues(handedOverValues: { expense: Expense }) {
         title="Who owes?"
         {...itemProps.owes}
         info="Expense will be split equally among the involved people"
+        // onChange={() => setValue("owes", values.owes)}
+        // value={userOwe}
+        // onChange={(values) => {
+        //   setUserOwe(values);
+        // }}
+        // onChange={() => setUserOwe}
       >
         {friendsWithCurrentUser.map((friend) => (
           <Form.TagPicker.Item
@@ -304,20 +371,28 @@ function ChangeValues(handedOverValues: { expense: Expense }) {
         ))}
       </Form.TagPicker>
 
-      {/* <Form.TagPicker
-        title="Who owes?"
-        {...itemProps.owes}
-        info="Expense will be split equally among the involved people"
-      >
-        {expense.users.map((user) => (
-          <Form.TagPicker.Item
-            key={user.user.id}
-            value={String(user.user.id)}
-            title={[user.user.first_name, user.user.last_name].join(" ")}
-            icon={{ source: user.user.picture.medium, mask: Image.Mask.Circle }}
+      {/* {splitEqually === false &&
+        friendsWithCurrentUser
+        .filter((friend) => values.owes.includes(String(friend.id)))
+        .map((friend) => (
+          <Form.TextField
+            id={String(friend.id)}
+            key={friend.id}
+            title={friend.first_name}
+            defaultValue={expense.users.filter((user) => user.user.id === friend.id)[0].owed_share.toString()}
           />
-        ))}
-      </Form.TagPicker> */}
+        ))} */}
+
+      {/* {splitEqually === false &&
+        expense.users.map((user) => (
+          <Form.TextField
+            id={String(user.user.id)}
+            key={user.user.id}
+            title={user.user.first_name}
+            defaultValue={user.owed_share}
+          />
+        ))} */}
+
     </Form>
   );
 }
