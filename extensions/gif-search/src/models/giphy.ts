3d41dcc24d58320f69c5c5781ba60460879194e3
Thomas Lombart <lombart.thomas@icloud.com>
2023-10-02 09:51:56 +0200

    GIF Search improvements (#8369)
    * Remove List layout
    
    * Refactor Gif Search code
    
    * Add frecency sorting
    
    * Add support for Giphy Clips
    
    * Add download GIF action
    
    * GIF Search: Load more

--- extensions/gif-search/src/models/giphy.ts
+++ extensions/gif-search/src/models/giphy.ts
@@ -2,7 +2,6 @@ import formatRelative from "date-fns/formatRelative";
 
 import { GiphyFetch } from "@giphy/js-fetch-api";
 import type { IGif as GiphyGif } from "@giphy/js-types";
-import { environment } from "@raycast/api";
 
 import { getAPIKey, GIF_SERVICE } from "../preferences";
 
@@ -17,18 +16,18 @@ export async function getAPI(force?: boolean) {
   return gf;
 }
 
-export default async function giphy(force?: boolean) {
+export default async function giphy(force?: boolean, type?: "gifs" | "videos") {
   const api = await getAPI(force);
 
   return <IGifAPI>{
     async search(term: string, opt?: APIOpt) {
       const { offset = 0, limit } = opt || {};
-      return (await api.search(term, { offset, limit })).data.map(mapGiphyResponse);
+      return (await api.search(term, { type, offset, limit })).data.map(mapGiphyResponse);
     },
 
     async trending(opt?: APIOpt) {
       const { offset = 0, limit = 10 } = opt || {};
-      return (await api.trending({ offset, limit })).data.map(mapGiphyResponse);
+      return (await api.trending({ type, offset, limit })).data.map(mapGiphyResponse);
     },
 
     async gifs(ids: string[]) {
@@ -44,13 +43,24 @@ export default async function giphy(force?: boolean) {
 
 export function mapGiphyResponse(giphyResp: GiphyGif) {
   const title = giphyResp.title || giphyResp.slug;
+  const slug = slugify(title);
+
+  const isGiphyClip = giphyResp.type === "video";
+  const gif_url = giphyResp.images.fixed_height.url;
+  const download_url = isGiphyClip
+    ? giphyResp.video?.assets["720p"]?.url ?? giphyResp.video?.assets["360p"].url ?? gif_url
+    : gif_url;
+  const isMP4 = /\.mp4(\?|$)/.test(download_url);
+
   return <IGif>{
     id: giphyResp.id,
     title: title,
     url: giphyResp.url,
     slug: slugify(title),
-    preview_gif_url: giphyResp.images.preview_gif.url,
-    gif_url: giphyResp.images.fixed_height.url,
+    download_url,
+    download_name: `${slug}.${isGiphyClip && isMP4 ? "mp4" : "gif"}`,
+    preview_gif_url: isGiphyClip ? gif_url : giphyResp.images.preview_gif.url,
+    gif_url,
     metadata: {
       width: giphyResp.images.original.width,
       height: giphyResp.images.original.height,
@@ -63,5 +73,6 @@ export function mapGiphyResponse(giphyResp: GiphyGif) {
       tags: giphyResp.tags,
     },
     attribution: "giphy-logo-square-180.png",
+    video: giphyResp.video,
   };
 }
