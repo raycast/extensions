b1b81e6d284ebf2d6ddbe73ecc9aa3b8b199f1e8
David Molinero <doktor500@users.noreply.github.com>
2023-10-03 11:21:47 +0100

    Update spanish-tv-guide extension (#8654)
    * Update spanish-tv-guide extension
    
    - User friendly error handling
    - Set live stream icon
    
    * Update raycast username
    
    * Fix lint
    
    * Remove type

--- extensions/spanish-tv-guide/src/utils/dateUtils.ts
+++ extensions/spanish-tv-guide/src/utils/dateUtils.ts
@@ -1,8 +1,13 @@
-import { DateTime } from "luxon";
+import { DateTime, Duration } from "luxon";
 
 const TIME_ZONE = "Europe/Madrid";
 
-const parseDate = (date: string) => DateTime.fromISO(date, { zone: TIME_ZONE }).toJSDate();
-const getTime = (date: Date) => DateTime.fromJSDate(date).toLocaleString(DateTime.TIME_24_SIMPLE);
+const oneDay = Duration.fromObject({ day: 1 });
+const currentDate = () => now().toISOString().substring(0, 11);
 
-export { getTime, parseDate };
+const getTime = (date: Date) => DateTime.fromJSDate(date, { zone: TIME_ZONE }).toLocaleString(DateTime.TIME_24_SIMPLE);
+const now = () => DateTime.now().setZone(TIME_ZONE).toJSDate();
+const parseTime = (time: string) => DateTime.fromISO(`${currentDate()}${time}`, { zone: TIME_ZONE }).toJSDate();
+const plusOneDay = (date: Date) => DateTime.fromJSDate(date, { zone: TIME_ZONE }).plus(oneDay).toJSDate();
+
+export { getTime, now, parseTime, plusOneDay };
