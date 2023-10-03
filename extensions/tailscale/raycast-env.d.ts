raycast/extensions

commit bed2b201b1db1c405b70c8c5486674180e8827d1

feat(tailscale): add preferences for path and exec wrapper (#8287)
* feat: add preferences for path and exec wrapper

* chore: add changelog entry

* chore: more idiomatic typescript

* chore: fix CI

extensions/tailscale/raycast-env.d.ts
@@ -7,7 +7,10 @@
 
 /* eslint-disable @typescript-eslint/ban-types */
 
-type ExtensionPreferences = {}
+type ExtensionPreferences = {
+  /** Tailscale CLI Path - Location to the Tailscale CLI (Defaults to `/Applications/Tailscale.app/Contents/MacOS/Tailscale`) */
+  "tailscalePath"?: string
+}
 
 /** Preferences accessible in all the extension's commands */
 declare type Preferences = ExtensionPreferences

commit 8944bbc70e7f5501234b14475c9cadd21c49988d

Update Tailscale extension (#8577)
* Refine error handling

This commit refines error handling in the extension, showing a
consistent error message when the Tailscale CLI Path is incorrect, or
when Tailscale isn't running.

This commit also fixes a bug introduced in #8287 where the account
switcher wouldn't show accounts.

* Refine error messaging

Previously, errors were shown as a tiny toast at the bottom of the view.
This was easy to miss, since it was small and transient. Plus, the
loading state never went away, even when we weren't loading.

This commit updates the extension to show a full-screen error when we
can't connect.

I'm hoping we can later refine this view to include actions to refresh
the state, or one-click buttons to open/connect Tailscale.

* Ignore Raycast typings file

* Remove unnecessary `any` types

extensions/tailscale/.gitignore
@@ -1,2 +1,3 @@
 node_modules
 test_data.json
+raycast-env.d.ts

extensions/tailscale/raycast-env.d.ts
@@ -1,42 +1,0 @@
-/// <reference types="@raycast/api">
-
-/* 🚧 🚧 🚧
- * This file is auto-generated from the extension's manifest.
- * Do not modify manually. Instead, update the `package.json` file.
- * 🚧 🚧 🚧 */
-
-/* eslint-disable @typescript-eslint/ban-types */
-
-type ExtensionPreferences = {
-  /** Tailscale CLI Path - Location to the Tailscale CLI (Defaults to `/Applications/Tailscale.app/Contents/MacOS/Tailscale`) */
-  "tailscalePath"?: string
-}
-
-/** Preferences accessible in all the extension's commands */
-declare type Preferences = ExtensionPreferences
-
-declare namespace Preferences {
-  /** Preferences accessible in the `devices` command */
-  export type Devices = ExtensionPreferences & {}
-  /** Preferences accessible in the `my-devices` command */
-  export type MyDevices = ExtensionPreferences & {}
-  /** Preferences accessible in the `account-switcher` command */
-  export type AccountSwitcher = ExtensionPreferences & {}
-  /** Preferences accessible in the `exit` command */
-  export type Exit = ExtensionPreferences & {}
-  /** Preferences accessible in the `admin` command */
-  export type Admin = ExtensionPreferences & {}
-}
-
-declare namespace Arguments {
-  /** Arguments passed to the `devices` command */
-  export type Devices = {}
-  /** Arguments passed to the `my-devices` command */
-  export type MyDevices = {}
-  /** Arguments passed to the `account-switcher` command */
-  export type AccountSwitcher = {}
-  /** Arguments passed to the `exit` command */
-  export type Exit = {}
-  /** Arguments passed to the `admin` command */
-  export type Admin = {}
-}

extensions/tailscale/src/account-switcher.tsx
@@ -11,7 +11,7 @@ import {
   Toast,
 } from "@raycast/api";
 import { useEffect, useState } from "react";
-import { tailscale } from "./shared";
+import { ErrorDetails, getErrorDetails, tailscale } from "./shared";
 
 interface User {
   active: boolean;
@@ -44,16 +44,16 @@ function loadUsers(unparsedUsers: string[]) {
 
 function AccountSwitchList() {
   const [users, setUsers] = useState<User[]>();
+  const [error, setError] = useState<ErrorDetails>();
   useEffect(() => {
     async function fetch() {
       try {
-        const ret = tailscale(`--list`);
-        const data: string[] = ret.split("\n");
-
+        const ret = tailscale(`switch --list`);
+        const data = ret.split("\n");
         const _list = loadUsers(data);
         setUsers(_list);
       } catch (error) {
-        showToast(Toast.Style.Failure, "Couldn't load users. Make sure Tailscale is connected.");
+        setError(getErrorDetails(error, "Couldn't load users."));
       }
     }
     fetch();
@@ -65,40 +65,44 @@ function AccountSwitchList() {
   // return a list of users, starting with all of the inactive users.
   // output the active user last.
   return (
-    <List isLoading={!users}>
-      {users
-        ?.sort((a, b) => +a.active - +b.active)
-        .map((user) => (
-          <List.Item
-            title={user.name}
-            key={user.name}
-            icon={user.active ? activeUserIcon : inactiveUserIcon}
-            subtitle={user.active ? "Active user" : ""}
-            actions={
-              <ActionPanel>
-                <Action
-                  title="Switch to User"
-                  onAction={async () => {
-                    await showToast({
-                      style: Toast.Style.Animated,
-                      title: "Switching user account",
-                      message: `${user.name}`,
-                    });
-                    popToRoot();
-                    closeMainWindow();
-                    const ret = tailscale(`switch ${user.name}`);
+    <List isLoading={!users && !error}>
+      {error ? (
+        <List.EmptyView icon={Icon.Warning} title={error.title} description={error.description} />
+      ) : (
+        users
+          ?.sort((a, b) => +a.active - +b.active)
+          .map((user) => (
+            <List.Item
+              title={user.name}
+              key={user.name}
+              icon={user.active ? activeUserIcon : inactiveUserIcon}
+              subtitle={user.active ? "Active user" : ""}
+              actions={
+                <ActionPanel>
+                  <Action
+                    title="Switch to User"
+                    onAction={async () => {
+                      await showToast({
+                        style: Toast.Style.Animated,
+                        title: "Switching user account",
+                        message: `${user.name}`,
+                      });
+                      popToRoot();
+                      closeMainWindow();
+                      const ret = tailscale(`switch ${user.name}`);
 
-                    if (ret.includes("Success") || ret.includes("Already")) {
-                      showHUD(`Active Tailscale user is ${user.name}`);
-                    } else {
-                      showHUD(`Tailscale user failed to switch to ${user.name}`);
-                    }
-                  }}
-                />
-              </ActionPanel>
-            }
-          />
-        ))}
+                      if (ret.includes("Success") || ret.includes("Already")) {
+                        showHUD(`Active Tailscale user is ${user.name}`);
+                      } else {
+                        showHUD(`Tailscale user failed to switch to ${user.name}`);
+                      }
+                    }}
+                  />
+                </ActionPanel>
+              }
+            />
+          ))
+      )}
     </List>
   );
 }

extensions/tailscale/src/devices.tsx
@@ -1,75 +1,74 @@
-import { ActionPanel, List, Action, Icon, showToast, Toast, Image } from "@raycast/api";
+import { ActionPanel, List, Action, Icon, Image } from "@raycast/api";
 import { useEffect, useState } from "react";
-import { Device, LooseObject, loadDevices, tailscale } from "./shared";
+import { Device, getStatus, getDevices, getErrorDetails, ErrorDetails } from "./shared";
 
 function DeviceList() {
   const [devices, setDevices] = useState<Device[]>();
+  const [error, setError] = useState<ErrorDetails>();
   useEffect(() => {
     async function fetch() {
       try {
-        const ret = tailscale(`status --json`)!;
-        const data: LooseObject = JSON.parse(ret);
-
-        if (!data.Self.Online) {
-          throw "Tailscale not connected";
-        }
-
-        const _list = loadDevices(data.Self, data.Peer);
+        const status = getStatus();
+        const _list = getDevices(status);
         setDevices(_list);
       } catch (error) {
-        showToast(Toast.Style.Failure, "Couldn't load devices. Make sure Tailscale is connected.");
+        setError(getErrorDetails(error, "Couldn’t load device list."));
       }
     }
     fetch();
   }, []);
 
   return (
-    <List isLoading={!devices}>
-      {devices?.map((device) => (
-        <List.Item
-          title={device.name}
-          subtitle={device.ipv4 + "   " + device.os}
-          key={device.key}
-          icon={
-            device.online
-              ? {
-                  source: {
-                    light: "connected_light.png",
-                    dark: "connected_dark.png",
-                  },
-                  mask: Image.Mask.Circle,
-                }
-              : {
-                  source: {
-                    light: "lastseen_light.png",
-                    dark: "lastseen_dark.png",
-                  },
-                  mask: Image.Mask.Circle,
-                }
-          }
-          accessories={
-            device.self
-              ? [
-                  { text: "This device", icon: Icon.Person },
-                  {
-                    text: device.online ? `        Connected` : "Last seen " + formatDate(device.lastseen),
-                  },
-                ]
-              : [
-                  {
-                    text: device.online ? `        Connected` : "Last seen " + formatDate(device.lastseen),
-                  },
-                ]
-          }
-          actions={
-            <ActionPanel>
-              <Action.CopyToClipboard content={device.ipv4} title="Copy IPv4" />
-              <Action.CopyToClipboard content={device.dns} title="Copy MagicDNS" />
-              <Action.CopyToClipboard content={device.ipv6} title="Copy IPv6" />
-            </ActionPanel>
-          }
-        />
-      ))}
+    <List isLoading={!devices && !error}>
+      {error ? (
+        <List.EmptyView icon={Icon.Warning} title={error.title} description={error.description} />
+      ) : (
+        devices?.map((device) => (
+          <List.Item
+            title={device.name}
+            subtitle={device.ipv4 + "   " + device.os}
+            key={device.key}
+            icon={
+              device.online
+                ? {
+                    source: {
+                      light: "connected_light.png",
+                      dark: "connected_dark.png",
+                    },
+                    mask: Image.Mask.Circle,
+                  }
+                : {
+                    source: {
+                      light: "lastseen_light.png",
+                      dark: "lastseen_dark.png",
+                    },
+                    mask: Image.Mask.Circle,
+                  }
+            }
+            accessories={
+              device.self
+                ? [
+                    { text: "This device", icon: Icon.Person },
+                    {
+                      text: device.online ? `        Connected` : "Last seen " + formatDate(device.lastseen),
+                    },
+                  ]
+                : [
+                    {
+                      text: device.online ? `        Connected` : "Last seen " + formatDate(device.lastseen),
+                    },
+                  ]
+            }
+            actions={
+              <ActionPanel>
+                <Action.CopyToClipboard content={device.ipv4} title="Copy IPv4" />
+                <Action.CopyToClipboard content={device.dns} title="Copy MagicDNS" />
+                <Action.CopyToClipboard content={device.ipv6} title="Copy IPv6" />
+              </ActionPanel>
+            }
+          />
+        ))
+      )}
     </List>
   );
 }

extensions/tailscale/src/exit.tsx
@@ -1,23 +1,16 @@
-import { ActionPanel, List, Action, showToast, popToRoot, closeMainWindow, Toast, Image } from "@raycast/api";
+import { ActionPanel, List, Action, popToRoot, closeMainWindow, Image, Icon } from "@raycast/api";
 import { useEffect, useState } from "react";
-import { LooseObject, loadDevices, tailscale } from "./shared";
+import { StatusResponse, getStatus, getDevices, tailscale, ErrorDetails, getErrorDetails, Device } from "./shared";
 
-function loadExitNodes(self: LooseObject, data: LooseObject) {
-  const devices = loadDevices(self, data);
-
+function loadExitNodes(status: StatusResponse) {
+  const devices = getDevices(status);
   return devices.filter((element) => {
     return element.exitnodeoption;
   });
 }
 
-function isExitNodeActive(devices: any) {
-  for (const device of devices) {
-    if (device.exitnode) {
-      return true;
-    }
-  }
-
-  return false;
+function isExitNodeActive(devices: Device[]) {
+  return devices.some((d) => d.exitnode);
 }
 
 function setExitNode(host: string, allowLAN: boolean) {
@@ -32,79 +25,81 @@ function setExitNode(host: string, allowLAN: boolean) {
 
 function ExitNodeList() {
   const [isActive, setIsActive] = useState<boolean>(false);
-  const [exitNodes, setExitNodes] = useState<any>();
+  const [error, setError] = useState<ErrorDetails>();
+  const [exitNodes, setExitNodes] = useState<Device[]>([]);
   useEffect(() => {
     async function fetch() {
       try {
-        const ret = tailscale(`status --json`)!;
-        const data: LooseObject = JSON.parse(ret);
-
-        if (!data.Self.Online) {
-          throw "Tailscale not connected";
-        }
-
-        const _list = loadExitNodes(data.Self, data.Peer);
+        const status = getStatus();
+        const _list = loadExitNodes(status);
         setExitNodes(_list);
-
         if (isExitNodeActive(_list)) {
           setIsActive(true);
         }
       } catch (error) {
-        showToast(Toast.Style.Failure, "Couldn't load exitNodes. Make sure Tailscale is connected.");
+        setError(getErrorDetails(error, "Couldn't load exit nodes."));
       }
     }
     fetch();
   }, []);
 
   return (
-    <List isLoading={!exitNodes}>
-      {isActive && (
-        <List.Item
-          key="_disable"
-          title="Turn off exit node"
-          actions={
-            <ActionPanel>
-              <Action title="Turn Off Exit Node" onAction={() => setExitNode("", false)} />
-            </ActionPanel>
-          }
-        />
+    <List isLoading={!exitNodes && !error}>
+      {error ? (
+        <List.EmptyView icon={Icon.Warning} title={error.title} description={error.description} />
+      ) : (
+        <>
+          {isActive && (
+            <List.Item
+              key="_disable"
+              title="Turn off exit node"
+              actions={
+                <ActionPanel>
+                  <Action title="Turn Off Exit Node" onAction={() => setExitNode("", false)} />
+                </ActionPanel>
+              }
+            />
+          )}
+          {exitNodes?.map((exitNode) => (
+            <List.Item
+              title={exitNode.name}
+              subtitle={exitNode.ipv4 + "    " + exitNode.os}
+              key={exitNode.key}
+              icon={
+                exitNode.online
+                  ? {
+                      source: {
+                        light: "connected_light.png",
+                        dark: "connected_dark.png",
+                      },
+                      mask: Image.Mask.Circle,
+                    }
+                  : {
+                      source: {
+                        light: "lastseen_light.png",
+                        dark: "lastseen_dark.png",
+                      },
+                      mask: Image.Mask.Circle,
+                    }
+              }
+              accessories={[
+                {
+                  text: exitNode.exitnode ? `        Connected` : "",
+                },
+              ]}
+              actions={
+                <ActionPanel>
+                  <Action title="Use as Exit Node" onAction={() => setExitNode(exitNode.name, false)} />
+                  <Action
+                    title="Use as Exit Node and Allow LAN Access"
+                    onAction={() => setExitNode(exitNode.name, true)}
+                  />
+                </ActionPanel>
+              }
+            />
+          ))}
+        </>
       )}
-
-      {exitNodes?.map((exitNode: any) => (
-        <List.Item
-          title={exitNode.name}
-          subtitle={exitNode.ipv4 + "    " + exitNode.os}
-          key={exitNode.key}
-          icon={
-            exitNode.online
-              ? {
-                  source: {
-                    light: "connected_light.png",
-                    dark: "connected_dark.png",
-                  },
-                  mask: Image.Mask.Circle,
-                }
-              : {
-                  source: {
-                    light: "lastseen_light.png",
-                    dark: "lastseen_dark.png",
-                  },
-                  mask: Image.Mask.Circle,
-                }
-          }
-          accessories={[
-            {
-              text: exitNode.exitnode ? `        Connected` : "",
-            },
-          ]}
-          actions={
-            <ActionPanel>
-              <Action title="Use as Exit Node" onAction={() => setExitNode(exitNode.name, false)} />
-              <Action title="Use as Exit Node and Allow LAN Access" onAction={() => setExitNode(exitNode.name, true)} />
-            </ActionPanel>
-          }
-        />
-      ))}
     </List>
   );
 }

extensions/tailscale/src/my-devices.tsx
@@ -1,92 +1,92 @@
-import { ActionPanel, List, Action, Icon, showToast, Toast, Image } from "@raycast/api";
+import { ActionPanel, List, Action, Icon, Image } from "@raycast/api";
 import { useEffect, useState } from "react";
-import { Device, LooseObject, loadDevices, tailscale } from "./shared";
+import { Device, getStatus, getErrorDetails, getDevices, ErrorDetails } from "./shared";
 
 function MyDeviceList() {
   const [devices, setDevices] = useState<Device[]>();
+  const [error, setError] = useState<ErrorDetails>();
   useEffect(() => {
     async function fetch() {
       try {
-        const ret = tailscale(`status --json`)!;
-        const data: LooseObject = JSON.parse(ret);
-
-        if (!data.Self.Online) {
-          throw "Tailscale not connected";
-        }
-        const me: string = data.Self.UserID;
-        const _list = loadDevices(data.Self, data.Peer);
+        const status = getStatus();
+        const me: string = status.Self.UserID.toString();
+        const _list = getDevices(status);
         const _mylist = _list.filter((device) => device.userid === me);
         setDevices(_mylist);
       } catch (error) {
-        showToast(Toast.Style.Failure, "Couldn't load devices. Make sure Tailscale is connected.");
+        setError(getErrorDetails(error, "Couldn’t load device list."));
       }
     }
     fetch();
   }, []);
 
   return (
-    <List isLoading={!devices}>
-      {devices?.map((device) => (
-        <List.Item
-          title={device.name}
-          subtitle={device.ipv4 + "    " + device.os}
-          key={device.key}
-          icon={
-            device.online
-              ? {
-                  source: {
-                    light: "connected_light.png",
-                    dark: "connected_dark.png",
-                  },
-                  mask: Image.Mask.Circle,
-                }
-              : {
-                  source: {
-                    light: "lastseen_light.png",
-                    dark: "lastseen_dark.png",
-                  },
-                  mask: Image.Mask.Circle,
-                }
-          }
-          accessories={
-            device.self
-              ? [
-                  { text: "This device", icon: Icon.Person },
-                  {
-                    text: device.online
-                      ? `        Connected`
-                      : "Last seen " +
-                        device.lastseen.toLocaleString("en-US", {
-                          month: "short",
-                          day: "numeric",
-                          hour: "numeric",
-                          minute: "numeric",
-                        }),
-                  },
-                ]
-              : [
-                  {
-                    text: device.online
-                      ? `        Connected`
-                      : "Last seen " +
-                        device.lastseen.toLocaleString("en-US", {
-                          month: "short",
-                          day: "numeric",
-                          hour: "numeric",
-                          minute: "numeric",
-                        }),
-                  },
-                ]
-          }
-          actions={
-            <ActionPanel>
-              <Action.CopyToClipboard content={device.ipv4} title="Copy IPv4" />
-              <Action.CopyToClipboard content={device.dns} title="Copy MagicDNS" />
-              <Action.CopyToClipboard content={device.ipv6} title="Copy IPv6" />
-            </ActionPanel>
-          }
-        />
-      ))}
+    <List isLoading={!devices && !error}>
+      {error ? (
+        <List.EmptyView icon={Icon.Warning} title={error.title} description={error.description} />
+      ) : (
+        devices?.map((device) => (
+          <List.Item
+            title={device.name}
+            subtitle={device.ipv4 + "    " + device.os}
+            key={device.key}
+            icon={
+              device.online
+                ? {
+                    source: {
+                      light: "connected_light.png",
+                      dark: "connected_dark.png",
+                    },
+                    mask: Image.Mask.Circle,
+                  }
+                : {
+                    source: {
+                      light: "lastseen_light.png",
+                      dark: "lastseen_dark.png",
+                    },
+                    mask: Image.Mask.Circle,
+                  }
+            }
+            accessories={
+              device.self
+                ? [
+                    { text: "This device", icon: Icon.Person },
+                    {
+                      text: device.online
+                        ? `        Connected`
+                        : "Last seen " +
+                          device.lastseen.toLocaleString("en-US", {
+                            month: "short",
+                            day: "numeric",
+                            hour: "numeric",
+                            minute: "numeric",
+                          }),
+                    },
+                  ]
+                : [
+                    {
+                      text: device.online
+                        ? `        Connected`
+                        : "Last seen " +
+                          device.lastseen.toLocaleString("en-US", {
+                            month: "short",
+                            day: "numeric",
+                            hour: "numeric",
+                            minute: "numeric",
+                          }),
+                    },
+                  ]
+            }
+            actions={
+              <ActionPanel>
+                <Action.CopyToClipboard content={device.ipv4} title="Copy IPv4" />
+                <Action.CopyToClipboard content={device.dns} title="Copy MagicDNS" />
+                <Action.CopyToClipboard content={device.ipv6} title="Copy IPv6" />
+              </ActionPanel>
+            }
+          />
+        ))
+      )}
     </List>
   );
 }

extensions/tailscale/src/shared.tsx
@@ -1,10 +1,9 @@
-import { Toast, getPreferenceValues, showToast } from "@raycast/api";
+import { getPreferenceValues } from "@raycast/api";
 import { execSync } from "node:child_process";
-import fs from "node:fs";
 
 export interface Device {
   self: boolean;
-  key: number;
+  key: string;
   name: string;
   userid: string;
   dns: string;
@@ -12,24 +11,65 @@ export interface Device {
   ipv6: string;
   os: string;
   online: boolean;
-  lastseen: any;
+  lastseen: Date;
   exitnode: boolean;
   exitnodeoption: boolean;
 }
 
-export interface LooseObject {
-  [key: string]: any;
+export class InvalidPathError extends Error {}
+export class NotRunningError extends Error {}
+export class NotConnectedError extends Error {}
+
+export type StatusDevice = {
+  Active: boolean;
+  ID: string;
+  DNSName: string;
+  ExitNode: boolean;
+  ExitNodeOption: boolean;
+  Online: boolean;
+  OS: string;
+  TailscaleIPs: string[];
+  LastSeen: string;
+  UserID: number;
+};
+
+/**
+ * StatusResponse is a subset of the fields returned by `tailscale status --json`.
+ */
+export type StatusResponse = {
+  Peer: Record<string, StatusDevice>;
+  Self: StatusDevice;
+  TailscaleIPs: string[];
+  User: Record<
+    string,
+    {
+      ID: number;
+      DisplayName: string;
+      LoginName: string;
+      ProfilePictureURL: string;
+    }
+  >;
+  Version: string;
+};
+
+export function getStatus() {
+  const resp = tailscale(`status --json`);
+  const data = JSON.parse(resp) as StatusResponse;
+  if (!data || !data.Self.Online) {
+    throw new NotConnectedError();
+  }
+  return data;
 }
 
-export function loadDevices(self: LooseObject, data: LooseObject) {
+export function getDevices(status: StatusResponse) {
   const devices: Device[] = [];
-  let theKey = 0;
+  const self = status.Self;
 
   const me = {
     self: true,
-    key: ++theKey,
+    key: self.ID,
     name: self.DNSName.split(".")[0],
-    userid: self.UserID,
+    userid: self.UserID.toString(),
     dns: self.DNSName,
     ipv4: self.TailscaleIPs[0],
     ipv6: self.TailscaleIPs[1],
@@ -42,20 +82,20 @@ export function loadDevices(self: LooseObject, data: LooseObject) {
 
   devices.push(me);
 
-  for (const [key, value] of Object.entries(data)) {
+  for (const [, peer] of Object.entries(status.Peer)) {
     const device = {
       self: false,
-      key: ++theKey,
-      name: value.DNSName.split(".")[0],
-      userid: value.UserID,
-      dns: value.DNSName,
-      ipv4: value.TailscaleIPs[0],
-      ipv6: value.TailscaleIPs[1],
-      os: value.OS == "linux" ? "Linux" : value.OS,
-      online: value.Online,
-      lastseen: new Date(value.LastSeen),
-      exitnode: value.ExitNode,
-      exitnodeoption: value.ExitNodeOption,
+      key: peer.ID,
+      name: peer.DNSName.split(".")[0],
+      userid: peer.UserID.toString(),
+      dns: peer.DNSName,
+      ipv4: peer.TailscaleIPs[0],
+      ipv6: peer.TailscaleIPs[1],
+      os: peer.OS == "linux" ? "Linux" : peer.OS,
+      online: peer.Online,
+      lastseen: new Date(peer.LastSeen),
+      exitnode: peer.ExitNode,
+      exitnodeoption: peer.ExitNodeOption,
     };
     devices.push(device);
   }
@@ -64,11 +104,54 @@ export function loadDevices(self: LooseObject, data: LooseObject) {
 
 const prefs = getPreferenceValues();
 
-export const tailscalePath: string =
+const tailscalePath: string =
   prefs.tailscalePath && prefs.tailscalePath.length > 0
     ? prefs.tailscalePath
     : "/Applications/Tailscale.app/Contents/MacOS/Tailscale";
 
+/**
+ * tailscale runs a command against the Tailscale CLI.
+ */
 export function tailscale(parameters: string): string {
-  return execSync(`${tailscalePath} ${parameters}`).toString().trim();
+  try {
+    return execSync(`${tailscalePath} ${parameters}`).toString().trim();
+  } catch (err) {
+    if (err instanceof Error) {
+      if (err.message.includes("No such file or directory")) {
+        throw new InvalidPathError();
+      }
+      if (err.message.includes("is Tailscale running?")) {
+        throw new NotRunningError();
+      }
+    }
+    throw err;
+  }
+}
+
+export type ErrorDetails = {
+  title: string;
+  description: string;
+};
+
+export function getErrorDetails(err: unknown, fallbackMessage: string): ErrorDetails {
+  if (err instanceof InvalidPathError) {
+    return {
+      title: "Can’t find the Tailscale CLI",
+      description: "Your Tailscale CLI Path is invalid.\nUpdate your extension preferences to fix this.",
+    };
+  } else if (err instanceof NotRunningError) {
+    return {
+      title: "Can’t connect to Tailscale",
+      description: "Make sure Tailscale is running and try again.",
+    };
+  } else if (err instanceof NotConnectedError) {
+    return {
+      title: "Not connected to a tailnet",
+      description: "Tailscale is running, but you’re not connected to a tailnet.\nLog in and try again.",
+    };
+  }
+  return {
+    title: "Something went wrong",
+    description: fallbackMessage,
+  };
 }