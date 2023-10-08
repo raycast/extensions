0ef983485e0f87a473a80198095520decae83930
Andreas Elia <andreaselia@live.co.uk>
2023-10-08 16:42:21 +0100

    Update wayback-machine extension
    - Wayback Machine - update changelog
    - Wayback Machine - add multi url support

---
+++ extensions/wayback-machine/src/saveMultiple.tsx
@@ -0,0 +1,45 @@
+import { Form, ActionPanel, Action, showToast, Toast } from "@raycast/api";
+import { savePage, urlRegex } from "./lib";
+
+type Values = {
+  urls: string;
+};
+
+export default function Command() {
+  function handleSubmit(values: Values) {
+    console.log(values);
+
+    // split urls by new line
+    let urls = values.urls.split("\n");
+
+    // remove empty lines
+    urls = urls.filter((url) => url !== "");
+
+    // validate each url by urlRegex and remove invalid urls
+    urls = urls.filter((url) => urlRegex.test(url));
+
+    if (urls.length !== values.urls.split("\n").length) {
+      showToast({ style: Toast.Style.Failure, title: "Some URLs are invalid" });
+
+      return;
+    }
+
+    urls.forEach(async (url) => {
+      await savePage(url);
+    });
+
+    showToast({ title: `${urls.length} URLs saved` });
+  }
+
+  return (
+    <Form
+      actions={
+        <ActionPanel>
+          <Action.SubmitForm onSubmit={handleSubmit} />
+        </ActionPanel>
+      }
+    >
+      <Form.TextArea id="urls" title="URLs" placeholder="Enter one URL per line" />
+    </Form>
+  );
+}
