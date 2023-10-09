316557639e0722c67fc73a87a5c2ca404bf132f7
Danny Seidel <69514484+DannySeidel@users.noreply.github.com>
2023-10-09 12:29:13 +0200

    add new extension flight-miles-calculator

---
+++ extensions/flight-miles-calculator/src/components/SelectionFormView.tsx
@@ -0,0 +1,43 @@
+import { Action, ActionPanel, Form, useNavigation } from "@raycast/api"
+import { milesValues } from "../ressources/milesValues"
+import { AirportDropDownView } from "./AirportDropDownView"
+import { ResultView } from "./ResultView"
+
+interface submitValues {
+    origin: string
+    destination: string
+    miles: string
+}
+
+export const SelectionFormView = () => {
+    const { push } = useNavigation()
+
+    function handleFormSubmit(values: submitValues) {
+        const origin = JSON.parse(values.origin)
+        const destination = JSON.parse(values.destination)
+        const milesPercentage = parseFloat(values.miles)
+
+        push(<ResultView origin={origin} destination={destination} milesPercentage={milesPercentage} />)
+    }
+
+    return (
+        <Form
+            actions={
+                <ActionPanel>
+                    <Action.SubmitForm onSubmit={handleFormSubmit} />
+                </ActionPanel>
+            }
+        >
+            <AirportDropDownView dropdownId="origin" title="Origin Airport" autofocus />
+            <AirportDropDownView dropdownId="destination" title="Destination Airport" />
+
+            <Form.Separator />
+
+            <Form.Dropdown id="miles" title="Miles Percentage" defaultValue="1">
+                {milesValues.map((value) => (
+                    <Form.Dropdown.Item key={value} value={value.toString()} title={`${value * 100}%`} />
+                ))}
+            </Form.Dropdown>
+        </Form>
+    )
+}
