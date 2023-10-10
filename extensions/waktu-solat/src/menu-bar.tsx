6cf8485156c1575ab88829f156fa3763f01ae880
Per Nielsen Tikær <per@raycast.com>
2023-10-10 12:21:12 +0200

    Updated menubar

--- extensions/waktu-solat/src/menu-bar.tsx
+++ extensions/waktu-solat/src/menu-bar.tsx
@@ -1,32 +1,32 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
 import { Icon, MenuBarExtra, getPreferenceValues, openExtensionPreferences } from "@raycast/api";
 import { useEffect, useState } from "react";
+import { useCachedState } from "@raycast/utils";
 import { PrayerTime, loadTodaySolat } from "./lib/prayer-times";
 
 export default function Command() {
   const userPreference: Preferences = getPreferenceValues();
   const [isLoading, setLoading] = useState(true);
-  const [prayerTime, setPrayerTime] = useState<PrayerTime>();
-
-  async function onZoneChange() {
-    setPrayerTime(await loadTodaySolat(userPreference.zone));
-    setLoading(false);
-  }
+  const [prayerTime, setPrayerTime] = useCachedState<PrayerTime>("cache");
 
   useEffect(() => {
-    onZoneChange();
+    (async () => {
+      setPrayerTime(await loadTodaySolat(userPreference.zone));
+      setLoading(false);
+    })();
   }, []);
 
   return (
     <MenuBarExtra icon={Icon.Moon} title="Waktu Solat" tooltip="Waktu Solat" isLoading={isLoading}>
-      {/* <MenuBarExtra.Section title={userPreference.zone}> */}
       {prayerTime?.items?.map((item) => (
         <MenuBarExtra.Item key={item.label} title={`${item.label}: `} subtitle={item.value} />
       ))}
-      {/* </MenuBarExtra.Section> */}
       <MenuBarExtra.Section>
-        <MenuBarExtra.Item title="Change zone" onAction={() => openExtensionPreferences()} />
+        <MenuBarExtra.Item
+          title="Change Zone"
+          icon={Icon.Cog}
+          onAction={() => openExtensionPreferences()}
+          shortcut={{ modifiers: ["cmd"], key: "," }}
+        />
       </MenuBarExtra.Section>
     </MenuBarExtra>
   );
