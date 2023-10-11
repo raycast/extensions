d4a52d4e969aaff46501eef16a5762e0d3d1e345
3llomi <allomi.raj@gmail.com>
2023-10-11 22:53:50 +0300

    Verify Purchase

--- extensions/envato/CHANGELOG.md
+++ extensions/envato/CHANGELOG.md
@@ -1,5 +1,6 @@
 # Envato Changelog
-
+## [Verify Purchase] - 2023-10-11
+- Added Verify Purchase using Purchase Code
 ## [Update] - 2023-05-02
 
 - Tracking New Sales For Envato Author
---
+++ extensions/envato/src/verifyPurchase.tsx
@@ -0,0 +1,41 @@
+import { ActionPanel, Action, Form, showToast, Toast, useNavigation } from "@raycast/api";
+import { verifyPurchaseCode } from "./utils";
+import PurchaseDetails from "./purchaseDetails";
+type Values = {
+  pc: string;
+};
+export default function Command() {
+  const { push } = useNavigation();
+
+  async function submit(purchaseCode: string) {
+    if (purchaseCode.length === 0) {
+      await showToast({ title: "Purchase code cannot be empty.", style: Toast.Style.Failure });
+      return;
+    }
+    const loadingToast = await showToast({ title: "Verifiying...", style: Toast.Style.Animated });
+    try {
+      const result = await verifyPurchaseCode(purchaseCode);
+      loadingToast.hide();
+      if (!result) {
+        await showToast({ title: "Invalid Purchase Code", style: Toast.Style.Failure });
+        return;
+      }
+      push(<PurchaseDetails data={result} />);
+    } catch (error) {
+      loadingToast.hide();
+      await showToast({ title: "Error", style: Toast.Style.Failure });
+    }
+  }
+
+  return (
+    <Form
+      actions={
+        <ActionPanel>
+          <Action.SubmitForm onSubmit={(values: Values) => submit(values.pc)} />
+        </ActionPanel>
+      }
+    >
+      <Form.TextField title="Purchase Code" id="pc" />
+    </Form>
+  );
+}
