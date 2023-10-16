09953675c838d2f80767e302cccc911a2ec18ec8
Per Nielsen Tikær <per@raycast.com>
2023-10-16 13:36:53 +0200

    Update search-movies.tsx

--- extensions/douban/src/search-movies.tsx
+++ extensions/douban/src/search-movies.tsx
@@ -113,7 +113,7 @@ export default function Command() {
                   onAction={() => setShowingDetail(!showingDetail)}
                 />
                 <Action.CopyToClipboard
-                  title="Copy As Markdown"
+                  title="Copy as Markdown"
                   content={`[${movie.title}](${movie.url})`}
                   shortcut={{ modifiers: ["cmd", "shift"], key: "c" }}
                 />
