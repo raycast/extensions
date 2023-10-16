17b70f529f2d43d099b034a8f6c79bc6f8f073dc
Per Nielsen Tikær <per@raycast.com>
2023-10-16 13:35:59 +0200

    Update search-users.tsx

--- extensions/douban/src/search-users.tsx
+++ extensions/douban/src/search-users.tsx
@@ -100,7 +100,7 @@ export default function Command() {
                   onAction={() => setShowingDetail(!showingDetail)}
                 />
                 <Action.CopyToClipboard
-                  title="Copy As Markdown"
+                  title="Copy as Markdown"
                   content={`[${user.title}](${user.url})`}
                   shortcut={{ modifiers: ["cmd", "shift"], key: "c" }}
                 />
