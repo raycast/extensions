8355eccba90f231aabcc7178974bbbba38fc9134
Per Nielsen Tikær <per@raycast.com>
2023-10-18 13:29:11 +0200

    Update index.tsx
    Added so search and open always are showed last in the list

--- extensions/search-astro-docs/src/index.tsx
+++ extensions/search-astro-docs/src/index.tsx
@@ -6,43 +6,55 @@ import { documentationListV3 as docsList } from "./data/docs";
 export default function UserSearchRoot() {
   const [search, setSearch] = useState<string>();
   return (
-    <List searchBarPlaceholder="Search the Astro Documentation" onSearchTextChange={setSearch} filtering throttle>
-      {docsList.map((docsItem) => (
+    <List
+      searchBarPlaceholder="Search the Astro Documentation"
+      onSearchTextChange={setSearch}
+      filtering={{ keepSectionOrder: true }}
+      throttle
+    >
+      <List.Section>
+        {docsList.map((docsItem) => (
+          <List.Item
+            keywords={docsItem.keywords}
+            key={docsItem.title}
+            title={`${docsItem.title}`}
+            icon={docsItem.icon ?? "astro-search-icon.png"}
+            actions={
+              <ActionPanel>
+                <Action.OpenInBrowser
+                  title="Open the Astro Documentation"
+                  icon={"astro-search-icon.png"}
+                  url={docsItem.url ? docsItem.url : `http://a.stro.cc/${docsItem.title}`}
+                />
+                <Action onAction={openExtensionPreferences} title="Open Extension Preferences" icon={Icon.Gear} />
+              </ActionPanel>
+            }
+          />
+        ))}
+      </List.Section>
+      <List.Section>
         <List.Item
-          keywords={docsItem.keywords}
-          key={docsItem.title}
-          title={`${docsItem.title}`}
-          icon={docsItem.icon ?? "astro-search-icon.png"}
+          icon={"astro-search-icon.png"}
+          title={`Search '${search}' in the Astro Documentation`}
           actions={
             <ActionPanel>
-              <Action.OpenInBrowser
-                title="Open the Astro Documentation"
-                url={docsItem.url ? docsItem.url : `http://a.stro.cc/${docsItem.title}`}
-              />
+              <OpenSearchInBrowserAction search={search ?? ""} />
               <Action onAction={openExtensionPreferences} title="Open Extension Preferences" icon={Icon.Gear} />
             </ActionPanel>
           }
         />
-      ))}
-      <List.Item
-        icon={"astro-search-icon.png"}
-        title={`Search '${search}' in the Astro Documentation`}
-        actions={
-          <ActionPanel>
-            <OpenSearchInBrowserAction search={search ?? ""} />
-          </ActionPanel>
-        }
-      />
-      <List.Item
-        key={"open Astro documentation"}
-        title={"Open the Astro Documentation"}
-        icon={"astro-search-icon.png"}
-        actions={
-          <ActionPanel>
-            <Action.OpenInBrowser title="Open the Astro Documentation" url={"http://docs.astro.build/"} />
-          </ActionPanel>
-        }
-      />
+        <List.Item
+          key={"open Astro documentation"}
+          title={"Open the Astro Documentation"}
+          icon={"astro-search-icon.png"}
+          actions={
+            <ActionPanel>
+              <Action.OpenInBrowser title="Open the Astro Documentation" url={"http://docs.astro.build/"} />
+              <Action onAction={openExtensionPreferences} title="Open Extension Preferences" icon={Icon.Gear} />
+            </ActionPanel>
+          }
+        />
+      </List.Section>
     </List>
   );
 }
