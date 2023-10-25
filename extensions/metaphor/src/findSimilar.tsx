ac96012bd65c36654893c369881c795181668a49
Qiangua <15757826110@163.com>
2023-10-25 22:01:41 +0800

    Add metaphor extension
    - update package.json
    - update assets
    - update readme
    - update: search similar part in readme
    - update: add findSimilar support
    - add type.tsx
    - update readme and metaphor png
    - update icon and bounce input
    - capture searchText & ignore 400 error
    - Merge pull request #1 from jeffzwang/jw-autoprompt
    - default to autoprompt, comment to use searchtext
    - update readme install
    - update readme
    - init metaphor demo

---
+++ extensions/metaphor/src/findSimilar.tsx
@@ -0,0 +1,69 @@
+import { useEffect, useState, useRef } from "react";
+import { Action, ActionPanel, List, Image, getPreferenceValues, showToast, Toast } from "@raycast/api";
+import { getFavicon } from "@raycast/utils";
+import Metaphor from "metaphor-node";
+import { Preferences, Result } from "./type";
+
+const apikey = getPreferenceValues<Preferences>().MetaphorAPIKey;
+const metaphor = new Metaphor(apikey);
+// console.log(apikey)
+export default function Command() {
+  const [searchText, setSearchText] = useState("");
+  const [results, setResults] = useState<Result[]>([]);
+  const [loading, setLoading] = useState(false);
+  const timerRef = useRef<NodeJS.Timeout | null>(null);
+  useEffect(() => {
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+    }
+
+    timerRef.current = setTimeout(() => {
+      searchArticles();
+    }, 800);
+
+    const searchArticles = async () => {
+      setLoading(true); //
+      try {
+        const response = await metaphor.findSimilar(searchText, {
+          numResults: 10,
+        });
+        setResults(response.results);
+      } catch (error) {
+        const err = error as Error;
+        if (!err.message.includes("400")) {
+          console.log(err.message);
+          showToast({
+            style: Toast.Style.Failure,
+            title: "Something went wrong hhh",
+            message: err.message,
+          });
+        }
+      }
+      setLoading(false);
+    };
+  }, [searchText]);
+
+  return (
+    <List
+      isLoading={loading}
+      searchText={searchText}
+      navigationTitle="Search Similar Links"
+      onSearchTextChange={setSearchText}
+    >
+      {results.length > 0 &&
+        results.map((result) => (
+          <List.Item
+            key={result.id}
+            icon={getFavicon(result.url, { mask: Image.Mask.RoundedRectangle })}
+            title={result.title ? result.title : "Empty Title"}
+            subtitle={result.author}
+            actions={
+              <ActionPanel>
+                <Action.OpenInBrowser title="Open In Browser" url={result.url} />
+              </ActionPanel>
+            }
+          />
+        ))}
+    </List>
+  );
+}
