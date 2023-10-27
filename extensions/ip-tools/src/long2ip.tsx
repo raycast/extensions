1ac6ae88e3086c8a308e332736a2a69f7ac71281
meguoe <45955794+meguoe@users.noreply.github.com>
2023-10-27 15:43:15 +0800

    Fixes and Enhancements - Updated convert number to IP address - Add a new prompt icon color to display according to different states (#8989)

--- extensions/ip-tools/src/long2ip.tsx
+++ extensions/ip-tools/src/long2ip.tsx
@@ -1,20 +1,19 @@
 import { IPv4, IPv6 } from "ip-toolkit";
 import { useState } from "react";
-import { List, Icon, Action, ActionPanel } from "@raycast/api";
+import { List, Icon, Color, Action, ActionPanel } from "@raycast/api";
 import { drinkTypes, DrinkDropdown } from "./components/dropdown";
 
 export default function Command(props: { arguments: { keywork: string } }) {
   const { keywork } = props.arguments;
   const [version, setVersion] = useState<string>("IPv4");
   const [searchText, setSearchText] = useState<string>(keywork ? keywork : "");
-
   const isEmpty = searchText.trim() === "";
   const isValid = !isEmpty && /^\d+$/.test(searchText);
   const result = isValid
     ? version === "IPv4"
       ? IPv4.long2ip(parseInt(searchText))
       : IPv6.long2ip(BigInt(searchText))
-    : "";
+    : false;
 
   return (
     <List
@@ -25,9 +24,15 @@ export default function Command(props: { arguments: { keywork: string } }) {
       searchBarAccessory={<DrinkDropdown drinkTypes={drinkTypes} onDrinkTypeChange={setVersion} />}
     >
       {isEmpty ? (
-        <List.EmptyView icon={Icon.Info} title="Please enter the Number that needs to be converted！" />
+        <List.EmptyView
+          icon={{ source: Icon.Warning, tintColor: Color.Yellow }}
+          title="Please enter the Number that needs to be converted！"
+        />
       ) : result === false ? (
-        <List.EmptyView icon={Icon.Warning} title="Please enter a valid Number" />
+        <List.EmptyView
+          icon={{ source: Icon.XMarkCircle, tintColor: Color.Red }}
+          title="Please enter a valid Number！"
+        />
       ) : (
         <List.Item
           icon={Icon.Clipboard}
