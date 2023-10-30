5c36ff96f5fa48f5876ec36c5e5dc4124df1d164
Rafael <rafael.vidas1@gmail.com>
2023-10-30 13:28:14 +0100

    Removed Google vision OCR engine

--- extensions/easy-ocr/src/index.tsx
+++ extensions/easy-ocr/src/index.tsx
@@ -1,50 +1,25 @@
-import {
-  Clipboard,
-  closeMainWindow,
-  getPreferenceValues,
-  showToast,
-  Toast,
-  openExtensionPreferences,
-} from "@raycast/api";
-import ocr from "./ocr";
+import { Clipboard, closeMainWindow, showToast, Toast } from "@raycast/api";
+import tesseractOcr from "./ocr";
 import utils from "./utils";
 import takeScreenshot from "./screenshot";
-
-const ocrEngine = getPreferenceValues<Preferences>().ocrEngine;
 
 export default async function main() {
   const isTesseractInstalled = await utils.isTesseractInstalled();
 
-  if (ocrEngine === "googleCloudVision") {
-    const valid = await utils.checkGoogleCredentials();
-
-    if (!valid) {
-      await openExtensionPreferences();
-      await showToast({
-        style: Toast.Style.Failure,
-        title: "Missing Google Cloud info",
-        message: "Missing Google Cloud info",
-      });
-      return;
-    }
-  }
-
-  if (ocrEngine === "tesseractOcr") {
-    if (!isTesseractInstalled) {
-      await showToast({
-        style: Toast.Style.Failure,
-        title: "Tesseract not found, check README!",
-        message: "Tesseract path not found or it is not installed, check README for more info!",
-      });
-      return;
-    }
+  if (!isTesseractInstalled) {
+    await showToast({
+      style: Toast.Style.Failure,
+      title: "Tesseract not found, check README!",
+      message: "Tesseract path not found or it is not installed, check README for more info!",
+    });
+    return;
   }
 
   await closeMainWindow();
   const filePath = await takeScreenshot();
 
   try {
-    let text = await ocr[ocrEngine](filePath);
+    let text = await tesseractOcr(filePath);
     text = utils.handleNewLines(text);
 
     if (!text) {
--- extensions/easy-ocr/src/ocr.ts
+++ extensions/easy-ocr/src/ocr.ts
@@ -1,8 +1,7 @@
-import vision from "@google-cloud/vision";
 import { recognize } from "node-tesseract-ocr";
 import { getPreferenceValues } from "@raycast/api";
 
-async function tesseractOcr(imagePath: string) {
+export default async function tesseractOcr(imagePath: string) {
   const config = {
     lang: "eng",
     oem: 1,
@@ -12,28 +11,3 @@ async function tesseractOcr(imagePath: string) {
 
   return await recognize(imagePath, config);
 }
-
-async function googleCloudVision(filePath: string) {
-  const { private_key, client_email } = getPreferenceValues();
-
-  const config = {
-    credentials: { private_key: private_key.split(String.raw`\n`).join("\n"), client_email },
-  };
-
-  const client = new vision.ImageAnnotatorClient(config);
-
-  const [result] = await client.textDetection(filePath);
-  const detections: any = result.textAnnotations;
-
-  if (detections[0]) {
-    return detections[0].description;
-  }
-
-  return false;
-}
-
-const ocr = {
-  tesseractOcr,
-  googleCloudVision,
-};
-export default ocr;
