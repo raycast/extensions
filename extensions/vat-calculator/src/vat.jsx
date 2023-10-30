b2363c0182888e4a190f8e9c2abbf093e0268c8b
justiniscoding <nanxnan@proton.me>
2023-10-30 12:19:06 -0400

    Update vat-calculator extension
    - run prettier and ESLint
    - update icon

--- extensions/vat-calculator/src/vat.jsx
+++ extensions/vat-calculator/src/vat.jsx
@@ -6,52 +6,52 @@ export default function calculateVAT() {
   const [number, setNumber] = useState(null);
 
   return (
-    <List
-      searchBarPlaceholder="Enter the number you want VAT added to"
-      onSearchTextChange={(num) => setNumber(num)}
-    >
+    <List searchBarPlaceholder="Enter the number you want VAT added to" onSearchTextChange={(num) => setNumber(num)}>
       {number && (
         <List.Section title="Results">
-          <List.Item title={"Number with VAT: " + numberWithVAT(number, VAT)} actions={resultActions(numberWithVAT(number, VAT))}/>
-          <List.Item title={"VAT: " + getVAT(number, VAT)} actions={resultActions(getVAT(number, VAT))}/>
-        </List.Section> 
+          <List.Item
+            title={"Number with VAT: " + numberWithVAT(number, VAT)}
+            actions={resultActions(numberWithVAT(number, VAT))}
+          />
+          <List.Item title={"VAT: " + getVAT(number, VAT)} actions={resultActions(getVAT(number, VAT))} />
+        </List.Section>
       )}
     </List>
   );
 }
 
-function equationToNumber(equation){
-    try{
-        if(equation.match(/[a-zA-Z]/g) != null){
-            return 0;
-        }
-        return safeEval(equation);
-    }catch{
-        return 0;
+function equationToNumber(equation) {
+  try {
+    if (equation.match(/[a-zA-Z]/g) != null) {
+      return 0;
     }
+    return safeEval(equation);
+  } catch {
+    return 0;
+  }
 }
 
-function getVAT(number, vat, keepAsNumber){
-    number = equationToNumber(number);
-    if(keepAsNumber){
-        return ((vat / 100) * number);
-    }
-    return ((vat / 100) * number).toFixed(2);
+function getVAT(number, vat, keepAsNumber) {
+  number = equationToNumber(number);
+  if (keepAsNumber) {
+    return (vat / 100) * number;
+  }
+  return ((vat / 100) * number).toFixed(2);
 }
 
-function safeEval(code){
-    return Function(`return ${code}`)();
+function safeEval(code) {
+  return Function(`return ${code}`)();
 }
 
-function numberWithVAT(number, vat){
-    return (getVAT(number, vat, true) + equationToNumber(number)).toFixed(2);
+function numberWithVAT(number, vat) {
+  return (getVAT(number, vat, true) + equationToNumber(number)).toFixed(2);
 }
 
-function resultActions(result){
-    return (
-        <ActionPanel>
-            <Action.CopyToClipboard content={result}/>
-            <Action.Paste content={result}/>
-        </ActionPanel>
-    )
+function resultActions(result) {
+  return (
+    <ActionPanel>
+      <Action.CopyToClipboard content={result} />
+      <Action.Paste content={result} />
+    </ActionPanel>
+  );
 }
