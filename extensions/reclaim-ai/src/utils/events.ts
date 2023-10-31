095e8e0798b0197642240f1730701e6ec0263e77
Simon Hessel <simon.hessel@kreios.lu>
2023-10-31 21:15:14 +0100

    Update reclaim-ai extension
    - Merge branch \'contributions/merge-1698783304520759000\'
    - Pull contributions
    - 🔧 refactor(useEvent.tsx, notifications.tsx): use filterMultipleOutDuplicateEvents function to filter out duplicate events ✨ feat(events.ts): add filterMultipleOutDuplicateEvents function to filter out events that are synced, managed by Reclaim and part of multiple calendars

--- extensions/reclaim-ai/src/utils/events.ts
+++ extensions/reclaim-ai/src/utils/events.ts
@@ -23,3 +23,58 @@ export const truncateEventSize = (eventTitle: string) => {
   }
   return eventTitle;
 };
+
+const CalendarSyncTitleRegex = /(Personal|Work|Travel) Commitment$/;
+
+/**
+ * Group events based on their start-end time combination.
+ * @param events List of events to group.
+ * @returns Map of grouped events.
+ */
+const groupEventsByTime = (events: ApiResponseEvents) => {
+  return events.reduce((acc, event) => {
+    const key = `${event.eventStart}-${event.eventEnd}` as const;
+    if (acc.has(key)) acc.get(key)!.push(event);
+    else acc.set(key, [event]);
+    return acc;
+  }, new Map<string, ApiResponseEvents>());
+};
+
+/**
+ * Filter events based on the given criteria.
+ * @param events List of events to filter.
+ * @returns Filtered list of events.
+ */
+const filterRelevantEvents = (events: ApiResponseEvents) => {
+  return events.filter((event) => {
+    const isSyncedAndManaged = event.personalSync && event.reclaimManaged;
+    const matchesTitleCriteria =
+      CalendarSyncTitleRegex.test(event.title) ||
+      (event.title.endsWith("Travel") && events.some((e) => e !== event && e.title.endsWith("Travel")));
+
+    return !(isSyncedAndManaged && matchesTitleCriteria);
+  });
+};
+
+/**
+ * Filter out events that are synced, managed by Reclaim and part of multiple calendars
+ * @param events
+ * @returns
+ */
+export function filterMultipleOutDuplicateEvents(events: undefined): undefined;
+export function filterMultipleOutDuplicateEvents<Events extends ApiResponseEvents>(events: Events): Events;
+export function filterMultipleOutDuplicateEvents<Events extends ApiResponseEvents>(
+  events: Events | undefined
+): Events | undefined;
+export function filterMultipleOutDuplicateEvents<Events extends ApiResponseEvents>(
+  events: Events | undefined
+): Events | undefined {
+  if (!events) return events;
+
+  // Create a map to store events based on start-end combination
+  const eventTimeMap = groupEventsByTime(events);
+
+  return [...eventTimeMap.values()].flatMap((events) =>
+    events.length > 1 ? filterRelevantEvents(events) : events
+  ) as Events;
+}
