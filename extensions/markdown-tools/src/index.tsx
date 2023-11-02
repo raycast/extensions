1e068ab68bdc00b6170df070f7ac6314a268956a
Jakob Kjaer <j.kjaer@sap.com>
2023-11-02 15:19:59 +1000

    fixed icon and title

--- extensions/markdown-tools/src/index.tsx
+++ extensions/markdown-tools/src/index.tsx
@@ -1,73 +1,93 @@
 import { Form, ActionPanel, Action, Clipboard, showHUD } from "@raycast/api";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import csvToMarkdown from "csv-to-markdown-table";
 
-
 interface Values {
-  inputValue: string,
-  delimiter: string,
-  firstRowIsHeader: boolean
+  inputValue: string;
+  delimiter: string;
+  firstRowIsHeader: boolean;
 }
 
 export default function Command() {
   async function handleSubmit(values: Values) {
-    await Clipboard.copy(result)
-    
- await showHUD("Table copied to clipboard")
+    await Clipboard.copy(result);
 
+    await showHUD("Table copied to clipboard");
   }
   useEffect(() => {
-    const markDown = csvToMarkdown(input.inputValue, (input.delimiter == "tab") ? "\t" :input.delimiter, input.firstRowIsHeader )
-    setResult(markDown)
-  })
-  const [result, setResult] = useState<string>('');
+    const markDown = csvToMarkdown(
+      input.inputValue,
+      input.delimiter == "tab" ? "\t" : input.delimiter,
+      input.firstRowIsHeader
+    );
+    setResult(markDown);
+  });
+  const [result, setResult] = useState<string>("");
 
   const [input, setInput] = useState<Values>({
-    inputValue: '',
+    inputValue: "",
     delimiter: "tab",
-    firstRowIsHeader: true
+    firstRowIsHeader: true,
   });
 
   function pasteAsMarkdownTable(newValue: string): void {
-        setInput({
+    setInput({
       ...input,
-      inputValue:  newValue
-    })
+      inputValue: newValue,
+    });
   }
 
   function setDelimiter(delimiter: string): void {
     setInput({
       ...input,
-     delimiter: delimiter
-    })
+      delimiter: delimiter,
+    });
   }
 
   function setFirstRowIsHeader(firstRowIsHeader: boolean): void {
     setInput({
       ...input,
-      firstRowIsHeader: firstRowIsHeader
-    })
+      firstRowIsHeader: firstRowIsHeader,
+    });
   }
 
   return (
     <Form
       actions={
         <ActionPanel>
-          <Action.SubmitForm onSubmit={handleSubmit}  title="Copy to Clipboard" />
+          <Action.SubmitForm onSubmit={handleSubmit} title="Copy to Clipboard" />
         </ActionPanel>
       }
     >
       <Form.Description text="Copy the text into the area below" />
-      <Form.TextArea id="textarea" title="Text area"   value={input?.inputValue} placeholder="Paste your text here." onChange={pasteAsMarkdownTable} />
+      <Form.TextArea
+        id="textarea"
+        title="Text area"
+        value={input?.inputValue}
+        placeholder="Paste your text here."
+        onChange={pasteAsMarkdownTable}
+      />
       <Form.Separator />
-      <Form.Checkbox id="checkbox" title="Row Header" label="First row is not header value" value={input?.firstRowIsHeader} onChange={setFirstRowIsHeader}/>
-      <Form.Dropdown id="dropdown" title="Delimiter" value={input.delimiter}  onChange={setDelimiter} >
+      <Form.Checkbox
+        id="checkbox"
+        title="Row Header"
+        label="First row is not header value"
+        value={input?.firstRowIsHeader}
+        onChange={setFirstRowIsHeader}
+      />
+      <Form.Dropdown id="dropdown" title="Delimiter" value={input.delimiter} onChange={setDelimiter}>
         <Form.Dropdown.Item value="tab" title="Tab" />
         <Form.Dropdown.Item value="," title="Comma" />
         <Form.Dropdown.Item value=";" title="Semicolon" />
         <Form.Dropdown.Item value=" " title="Space" />
       </Form.Dropdown>
-            <Form.TextArea id="pasteArea" title="Text area"  value={result} placeholder="Enter multi-line text" onChange={setResult} />
+      <Form.TextArea
+        id="pasteArea"
+        title="Text area"
+        value={result}
+        placeholder="Enter multi-line text"
+        onChange={setResult}
+      />
     </Form>
   );
 }
