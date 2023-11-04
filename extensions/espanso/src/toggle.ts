b9f709a39cb49a1cd04d7525c138268343f08184
Nico Baier <nico.baier@gmail.com>
2023-11-03 20:38:27 -0500

    Remove unused const

--- extensions/espanso/src/toggle.ts
+++ extensions/espanso/src/toggle.ts
@@ -2,6 +2,6 @@ import { showHUD } from "@raycast/api";
 import { $ } from "zx";
 
 export default async function main() {
-  const result = await $`espanso cmd toggle`;
+  await $`espanso cmd toggle`;
   await showHUD("Espanso toggled");
 }
