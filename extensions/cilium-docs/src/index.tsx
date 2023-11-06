dbe96c7401e333eb3d96af68d37322b14f896885
Per Nielsen Tikær <per@raycast.com>
2023-11-06 13:38:49 +0100

    Updated

--- extensions/cilium-docs/src/index.tsx
+++ extensions/cilium-docs/src/index.tsx
@@ -1,51 +1,45 @@
 import { ActionPanel, Action, List } from "@raycast/api";
-import axios from "axios";
+import { useFetch } from "@raycast/utils";
 import cheerio from "cheerio";
-import { useEffect, useState } from "react"; // Import useState and useEffect
 
 export default function Command() {
   const websiteUrl: string = "https://docs.cilium.io/en/stable/";
   const navigationClass: string = "wy-nav-side";
 
-  // Initialize state to store the fetched items
-  const [items, setItems] = useState<{ key: number; title: string; link: string }[]>([]);
+  const { data, isLoading } = useFetch(websiteUrl, {
+    async parseResponse(response) {
+      if (!response.ok) {
+        throw new Error(`Failed to fetch feed: ${response.statusText}`);
+      }
 
-  useEffect(() => {
-    axios
-      .get(websiteUrl)
-      .then((response) => {
-        const $ = cheerio.load(response.data);
+      const $ = cheerio.load(await response.text());
 
-        // Find the navigation element by class name
-        const navigation = $(`.${navigationClass}`);
+      // Find the navigation element by class name
+      const navigation = $(`.${navigationClass}`);
 
-        let keyCounter: number = 0;
+      let keyCounter: number = 0;
 
-        // Extract items from the navigation
-        const fetchedItems: { key: number; title: string; link: string }[] = [];
+      // Extract items from the navigation
+      const fetchedItems: { key: number; title: string; link: string }[] = [];
 
-        navigation.find("li").each((index, element) => {
-          const link = $(element).find("a");
-          const title: string = link.text();
-          const href: string | undefined = link.attr("href");
+      navigation.find("li").each((index, element) => {
+        const link = $(element).find("a");
+        const title: string = link.text();
+        const href: string | undefined = link.attr("href");
 
-          if (title && href) {
-            fetchedItems.push({ key: keyCounter, title, link: websiteUrl + (href || "") });
-          }
-          keyCounter++;
-        });
-
-        // Update the state with the fetched items
-        setItems(fetchedItems);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
+        if (title && href) {
+          fetchedItems.push({ key: keyCounter, title, link: websiteUrl + (href || "") });
+        }
+        keyCounter++;
       });
-  }, []); // The empty dependency array ensures this effect runs only once when the component mounts
+
+      return fetchedItems;
+    },
+  });
 
   return (
-    <List>
-      {items.map((item) => (
+    <List isLoading={isLoading}>
+      {data?.map((item) => (
         <List.Item
           key={item.key}
           icon="list-icon.png"
