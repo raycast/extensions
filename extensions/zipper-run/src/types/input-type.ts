bc1acd8e2773aa546000398b0a017c37e6aad00e
Ariel Conti <arielconti10@gmail.com>
2023-11-06 17:00:13 +0000

    fix lint

--- extensions/zipper-run/src/types/input-type.ts
+++ extensions/zipper-run/src/types/input-type.ts
@@ -1,19 +1,14 @@
 export enum InputType {
-  string = 'string',
-  number = 'number',
-  boolean = 'boolean',
-  date = 'date',
-  array = 'array',
-  object = 'object',
-  any = 'any',
-  enum = 'enum',
-  file = 'FileUrl',
-  unknown = 'unknown',
+  string = "string",
+  number = "number",
+  boolean = "boolean",
+  date = "date",
+  array = "array",
+  object = "object",
+  any = "any",
+  enum = "enum",
+  file = "FileUrl",
+  unknown = "unknown",
 }
 
-export const JSONEditorInputTypes = [
-  InputType.array,
-  InputType.object,
-  InputType.any,
-  InputType.unknown,
-];
+export const JSONEditorInputTypes = [InputType.array, InputType.object, InputType.any, InputType.unknown];
