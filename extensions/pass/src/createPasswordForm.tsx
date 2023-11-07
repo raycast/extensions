2959f696492cea6776de2a74e39071fefbb718fa
Tafeen <taafeenn@gmail.com>
2023-11-07 23:55:40 +0100

    Update pass extension
    - Fixed linting
    - Add custom fields during entry creation
    - Removed logging

--- extensions/pass/src/createPasswordForm.tsx
+++ extensions/pass/src/createPasswordForm.tsx
@@ -13,17 +13,33 @@ const options: any = {
 interface CreatingPassValues {
   path?: string;
   password?: string;
+  custom_fields?: string;
 }
 
 export default function CreatePassForm() {
   const { pop } = useNavigation();
 
   const AddPASS = async (values: CreatingPassValues) => {
-    let command;
-    if (values.password) {
-      command = `echo ${values.password} | pass insert -e '${values.path}'`;
-    } else {
+    let command = "";
+
+    // Generate password without custom fields
+    if (!values.password && !values.custom_fields) {
       command = `pass generate '${values.path}'`;
+    }
+
+    // Generate password and use custom fields
+    if (!values.password && values.custom_fields) {
+      command = `export password=$(openssl rand -base64 12) && pass insert -m '${values.path}' << EOF\n$password\n${values.custom_fields}\nEOF`;
+    }
+
+    // Use only password
+    if (values.password && !values.custom_fields) {
+      command = `echo '${values.password}' | pass insert -e '${values.path}'`;
+    }
+
+    // Use password and custom fields
+    if (values.password && values.custom_fields) {
+      command = `pass insert -m '${values.path}' << EOF\n${values.password}\n${values.custom_fields}\nEOF`;
     }
 
     const cmd = exec(command, options);
@@ -40,7 +56,6 @@ export default function CreatePassForm() {
 
     cmd.on("close", (code) => {
       if (code != 0) {
-        console.log(code);
         toast.style = Toast.Style.Failure;
         toast.title = "Failed to create Pass Entry";
       }
@@ -60,18 +75,14 @@ export default function CreatePassForm() {
       }
     >
       <Form.Description title="Create Password" text={"Create new password"} />
-      <Form.TextField id="path" title="File path" onBlur={logEvent} onFocus={logEvent} />
-      <Form.PasswordField
-        id="password"
-        placeholder="If empty, password will be generated"
-        title="Password"
-        onBlur={logEvent}
-        onFocus={logEvent}
+      <Form.TextField id="path" title="File path" />
+      <Form.PasswordField id="password" placeholder="If empty, password will be generated" title="Password" />
+      <Form.TextArea
+        id="custom_fields"
+        title="Custom Fields"
+        placeholder="login:my_login
+my_field:my_field_value"
       />
     </Form>
   );
-}
-
-function logEvent(event: Form.Event<string[] | string>) {
-  console.log(`Event '${event.type}' has happened for '${event.target.id}'. Current 'value': '${event.target.value}'`);
 }
