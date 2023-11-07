c06927b89ca1ac8bc16e283202856efeadcc5076
Kevin Kipp <kevin.kipp@gmail.com>
2023-11-07 09:43:51 -0600

    Update icon, screenshot, and address feedback

--- extensions/perplexity/src/index.tsx
+++ extensions/perplexity/src/index.tsx
@@ -1,4 +1,5 @@
 import { Action, ActionPanel, Form, open, popToRoot } from "@raycast/api";
+import { useState } from "react";
 
 type Values = {
   query: string;
@@ -7,7 +8,13 @@ type Values = {
 };
 
 export default function Command() {
+  const [queryError, setQueryError] = useState<string>();
+
   function handleSubmit({ query: q, copilot, focus }: Values) {
+    if (!q) {
+      setQueryError("Query cannot be empty");
+      return;
+    }
     const params = new URLSearchParams({
       q,
       copilot: copilot.toString(),
@@ -26,7 +33,7 @@ export default function Command() {
       }
     >
       <Form.Description text="Where knowledge begins" />
-      <Form.TextArea id="query" title="Ask anything..." autoFocus />
+      <Form.TextArea id="query" title="Ask Anything..." autoFocus error={queryError} />
       <Form.Dropdown
         info="Focus allows you to fine tune your search by narrowing down the sources, for more targeted and relevant results. All follow-up questions in this Thread will focus the search on your chosen domain."
         id="focus"
@@ -40,7 +47,7 @@ export default function Command() {
         <Form.Dropdown.Item value="reddit" title="Reddit" />
       </Form.Dropdown>
       <Form.Separator />
-      <Form.Checkbox defaultValue={false} id="copilot" label="Co-pilot" storeValue />
+      <Form.Checkbox defaultValue={false} id="copilot" label="Copilot" storeValue />
     </Form>
   );
 }
