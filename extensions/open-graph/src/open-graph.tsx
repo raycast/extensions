90353ea96af68ff6d473edd6d951cf53c2faeb45
David Ho <davidho0403@gmail.com>
2023-11-13 01:52:55 +0800

    Update open-graph extension
    - refactor: improve performance and readability
    - fix: display twitter:image Link only when not null

--- extensions/open-graph/src/open-graph.tsx
+++ extensions/open-graph/src/open-graph.tsx
@@ -46,52 +46,56 @@ export default function Command(props: LaunchProps<{ arguments: Arguments.OpenGr
     fetchData();
   }, []);
 
-  const markdown = `
-  ![](${openGraph?.og.image})
-  ## ${openGraph?.title}
-  ${openGraph?.description}
-  `;
-
   if (!openGraph) return <Detail isLoading={isLoading} markdown="Loading..." />;
+
+  const markdown = `
+  ![](${openGraph.og.image})
+  ## ${openGraph.title}
+  ${openGraph.description}
+  `;
 
   return (
     <Detail
       markdown={markdown}
-      navigationTitle={openGraph?.title}
+      navigationTitle={openGraph.title}
       metadata={
         <Detail.Metadata>
-          <Detail.Metadata.Label title="title" text={openGraph?.title} />
-          <Detail.Metadata.Label title="description" text={openGraph?.description} />
+          <Detail.Metadata.Label title="title" text={openGraph.title} />
+          <Detail.Metadata.Label title="description" text={openGraph.description} />
           <Detail.Metadata.Separator />
-          <Detail.Metadata.Label title="og:title" text={openGraph?.og.title} />
-          <Detail.Metadata.Label title="og:description" text={openGraph?.og.description} />
-          {openGraph?.og.image !== "none" ? (
+          <Detail.Metadata.Label title="og:title" text={openGraph.og.title} />
+          <Detail.Metadata.Label title="og:description" text={openGraph.og.description} />
+          {openGraph.og.image !== "none" ? (
             <Detail.Metadata.Link
               title="og:image"
-              target={openGraph?.og.image as string}
-              text={openGraph?.og.image as string}
+              target={openGraph.og.image as string}
+              text={openGraph.og.image as string}
             />
           ) : (
-            <Detail.Metadata.Label title="og:image" text={openGraph?.og.image} />
+            <Detail.Metadata.Label title="og:image" text={openGraph.og.image} />
           )}
-          {openGraph?.og.url !== "none" ? (
+          {openGraph.og.url !== "none" ? (
             <Detail.Metadata.Link
               title="og:url"
-              target={openGraph?.og.url as string}
-              text={openGraph?.og.url as string}
+              target={openGraph.og.url as string}
+              text={openGraph.og.url as string}
             />
           ) : (
-            <Detail.Metadata.Label title="og:url" text={openGraph?.og.url} />
+            <Detail.Metadata.Label title="og:url" text={openGraph.og.url} />
           )}
           <Detail.Metadata.Separator />
-          <Detail.Metadata.Label title="twitter:title" text={openGraph?.twitter.title} />
-          <Detail.Metadata.Label title="twitter:description" text={openGraph?.twitter.description} />
-          <Detail.Metadata.Label title="twitter:card" text={openGraph?.twitter.card} />
-          <Detail.Metadata.Link
-            title="twitter:image"
-            target={openGraph?.twitter.image as string}
-            text={openGraph?.twitter.image as string}
-          />
+          <Detail.Metadata.Label title="twitter:title" text={openGraph.twitter.title} />
+          <Detail.Metadata.Label title="twitter:description" text={openGraph.twitter.description} />
+          <Detail.Metadata.Label title="twitter:card" text={openGraph.twitter.card} />
+          {openGraph.twitter.image !== "none" ? (
+            <Detail.Metadata.Link
+              title="twitter:image"
+              target={openGraph.twitter.image as string}
+              text={openGraph.twitter.image as string}
+            />
+          ) : (
+            <Detail.Metadata.Label title="twitter:image" text={openGraph.twitter.image} />
+          )}
         </Detail.Metadata>
       }
     />
