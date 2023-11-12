fb3a4b7a790e5f0329155ed99ffb3acd18a6ae8f
David Ho <davidho0403@gmail.com>
2023-11-13 01:38:16 +0800

    feat: add arguments to package.json and refactor OpenGraph component

--- extensions/open-graph/src/open-graph.tsx
+++ extensions/open-graph/src/open-graph.tsx
@@ -1,92 +1,99 @@
-import { Action, ActionPanel, List } from "@raycast/api";
+import { Detail, LaunchProps, Toast, popToRoot, showToast } from "@raycast/api";
 import fetch from "node-fetch";
 import { useEffect, useState } from "react";
-import OpenGraphDetail from "./open-graph-detail";
-import { parseOpenGraph } from "./util";
 import { OpenGraph } from "./type";
-import { showToast, Toast } from "@raycast/api";
+import { parseOpenGraph } from "./util";
 
-export default function Command() {
-  const [url, setUrl] = useState<string>("");
+export default function Command(props: LaunchProps<{ arguments: Arguments.OpenGraph }>) {
+  const { url } = props.arguments;
   const [openGraph, setOpenGraph] = useState<OpenGraph | null>(null);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    const abortController = new AbortController();
-    const { signal } = abortController;
+    const fullUrl = url.startsWith("http://") || url.startsWith("https://") ? url : `https://${url}`;
 
-    const debounceTimer = setTimeout(() => {
-      const fullUrl = url.startsWith("http://") || url.startsWith("https://") ? url : `https://${url}`;
-
+    async function fetchData() {
+      setIsLoading(true);
       try {
-        new URL(fullUrl);
-      } catch (error) {
-        setOpenGraph(null);
-        return;
-      }
+        const res = await fetch(fullUrl);
 
-      async function fetchData() {
-        setIsLoading(true);
-        try {
-          const res = await fetch(fullUrl, { signal });
-          if (signal.aborted) return;
-
-          if (res.ok) {
-            const text = await res.text();
-            const openGraph = parseOpenGraph(text);
-            setOpenGraph(openGraph);
-          } else {
-            console.error(`HTTP error: ${res.status}`);
-            await showToast({
-              style: Toast.Style.Failure,
-              title: "Loading Failed",
-              message: `Error ${res.status}: Check the URL and try again.`,
-            });
-            setOpenGraph(null);
-          }
-        } catch (error: unknown) {
-          if (error instanceof Error) {
-            if (error.name !== "AbortError") {
-              console.error(error.message);
-              await showToast({
-                style: Toast.Style.Failure,
-                title: "Can't get open graph information",
-                message: "Please check the url and try again.",
-              });
-              setOpenGraph(null);
-            }
-          } else {
-            console.error(error);
-          }
-        } finally {
-          setIsLoading(false);
+        if (res.ok) {
+          const text = await res.text();
+          const openGraph = parseOpenGraph(text);
+          setOpenGraph(openGraph);
+        } else {
+          await showToast({
+            style: Toast.Style.Failure,
+            title: "Loading Failed",
+            message: `Error ${res.status}: Check the URL and try again.`,
+          });
+          popToRoot();
+          return;
         }
+      } catch (error: unknown) {
+        await showToast({
+          style: Toast.Style.Failure,
+          title: "Can't get open graph information",
+          message: "Please check the url and try again.",
+        });
+        popToRoot();
+        return;
+      } finally {
+        setIsLoading(false);
       }
+    }
 
-      if (url) fetchData();
-    }, 300);
+    fetchData();
+  }, []);
 
-    return () => {
-      clearTimeout(debounceTimer);
-      abortController.abort();
-    };
-  }, [url]);
+  const markdown = `
+  ![](${openGraph?.og.image})
+  ## ${openGraph?.title}
+  ${openGraph?.description}
+  `;
+
+  if (!openGraph) return <Detail isLoading={isLoading} markdown="Loading" />;
 
   return (
-    <List onSearchTextChange={setUrl} isLoading={isLoading}>
-      {openGraph ? (
-        <List.Item
-          title={openGraph?.title || ""}
-          icon={{ source: openGraph?.og.image || "" }}
-          actions={
-            <ActionPanel>
-              <Action.Push title={openGraph?.title || ""} target={<OpenGraphDetail openGraph={openGraph} />} />
-            </ActionPanel>
-          }
-        />
-      ) : (
-        <List.EmptyView title="Enter a URL to fetch Open Graph data." />
-      )}
-    </List>
+    <Detail
+      markdown={markdown}
+      navigationTitle={openGraph?.title}
+      metadata={
+        <Detail.Metadata>
+          <Detail.Metadata.Label title="title" text={openGraph?.title} />
+          <Detail.Metadata.Label title="description" text={openGraph?.description} />
+          <Detail.Metadata.Separator />
+          <Detail.Metadata.Label title="og:title" text={openGraph?.og.title} />
+          <Detail.Metadata.Label title="og:description" text={openGraph?.og.description} />
+          {openGraph?.og.image !== "none" ? (
+            <Detail.Metadata.Link
+              title="og:image"
+              target={openGraph?.og.image as string}
+              text={openGraph?.og.image as string}
+            />
+          ) : (
+            <Detail.Metadata.Label title="og:image" text={openGraph?.og.image} />
+          )}
+          {openGraph?.og.url !== "none" ? (
+            <Detail.Metadata.Link
+              title="og:url"
+              target={openGraph?.og.url as string}
+              text={openGraph?.og.url as string}
+            />
+          ) : (
+            <Detail.Metadata.Label title="og:url" text={openGraph?.og.url} />
+          )}
+          <Detail.Metadata.Separator />
+          <Detail.Metadata.Label title="twitter:title" text={openGraph?.twitter.title} />
+          <Detail.Metadata.Label title="twitter:description" text={openGraph?.twitter.description} />
+          <Detail.Metadata.Label title="twitter:card" text={openGraph?.twitter.card} />
+          <Detail.Metadata.Link
+            title="twitter:image"
+            target={openGraph?.twitter.image as string}
+            text={openGraph?.twitter.image as string}
+          />
+        </Detail.Metadata>
+      }
+    />
   );
 }
