6d742b888b174d74a2bfcb502da71b8c450e7496
viduycheung <viduy@appest.com>
2023-11-14 10:37:16 +0800

    fix(ticktick): fix eslint error  Empty block statement  no-empty

--- extensions/ticktick/src/create.tsx
+++ extensions/ticktick/src/create.tsx
@@ -55,7 +55,9 @@ export default function TickTickCreate() {
           default:
             break;
         }
-      } catch (error) {}
+      } catch (error) {
+        // error
+      }
     })();
   }, [defaultTitle]);
 
