f00fff5f66796a6c1ca089a45545ed12a30b7054
Anton Niklasson <niklasson.anton@gmail.com>
2023-11-15 09:51:34 +0100

    clean up the other commands

--- extensions/lorem-ipsum/src/paragraphs.tsx
+++ extensions/lorem-ipsum/src/paragraphs.tsx
@@ -1,10 +1,8 @@
-import { getPreferenceValues, showHUD } from "@raycast/api";
+import { showHUD } from "@raycast/api";
 import { generateParagraphs, produceOutput, safeLoremIpsumNumberArg } from "./utils";
 import { LoremIpsumArguments } from "./types";
 
 export default async function ParagraphCommand(props?: { arguments: LoremIpsumArguments }) {
-  const { action = "clipboard" } = getPreferenceValues();
-
   const numberArg = props?.arguments.numberOfLoremIpsumsToGenerate;
 
   const { error, safeLoremIpsumNumber } = await safeLoremIpsumNumberArg(numberArg);
@@ -13,6 +11,6 @@ export default async function ParagraphCommand(props?: { arguments: LoremIpsumAr
     await showHUD(`❌ ${error.message}`);
   } else {
     const output = generateParagraphs(safeLoremIpsumNumber);
-    await produceOutput(action, output);
+    await produceOutput(output);
   }
 }
