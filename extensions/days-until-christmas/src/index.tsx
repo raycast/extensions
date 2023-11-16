109aded336ff2b589389aa1c0ba6b8b306ce1660
erics118 <52634785+erics118@users.noreply.github.com>
2023-11-16 18:09:06 -0500

    refactor: move logic into lib

--- extensions/days-until-christmas/src/index.tsx
+++ extensions/days-until-christmas/src/index.tsx
@@ -1,29 +1,6 @@
-import { calculate, format } from "./lib";
+import { getTitle } from "./lib";
 import { updateCommandMetadata } from "@raycast/api";
 
-const emojis = ["🎁", "🍪", "❄️", "🎄", "🔔", "🎅", "☃️", "⛄", "🤶", "🦌"];
-
 export default async function Command() {
-  const now = new Date();
-
-  const s = calculate({
-    currDate: now,
-    neededDate: new Date(now.getFullYear(), 11, 25),
-  });
-  const emoji = emojis[Math.floor(Math.random() * emojis.length)];
-
-  let subtitle = "";
-
-  if (s.isToday) {
-    subtitle = `Merry Christmas! ${emoji}`;
-  } else {
-    const parts = [];
-
-    if (s.days) parts.push(format(s.days, "day"));
-    if (s.hours) parts.push(format(s.hours, "hour"));
-
-    subtitle = `${parts.join(" and ")} until Christmas ${emoji}`;
-  }
-
-  await updateCommandMetadata({ subtitle });
+  await updateCommandMetadata({ subtitle: getTitle() });
 }
--- extensions/days-until-christmas/src/lib/index.ts
+++ extensions/days-until-christmas/src/lib/index.ts
@@ -1,3 +1,29 @@
+export const emojis = ["🎁", "🍪", "❄️", "🎄", "🔔", "🎅", "☃️", "⛄", "🤶", "🦌"];
+
+export const getTitle = () => {
+  const now = new Date();
+
+  const s = calculate({
+    currDate: now,
+    neededDate: new Date(now.getFullYear(), 11, 25),
+  });
+  const emoji = emojis[Math.floor(Math.random() * emojis.length)];
+
+  let subtitle = "";
+
+  if (s.isToday) {
+    subtitle = `Merry Christmas! ${emoji}`;
+  } else {
+    const parts = [];
+
+    if (s.days) parts.push(format(s.days, "day"));
+    if (s.hours) parts.push(format(s.hours, "hour"));
+
+    subtitle = `${parts.join(" and ")} until Christmas ${emoji}`;
+  }
+  return subtitle;
+}
+
 export const calculate = ({
   currDate,
   neededDate,
