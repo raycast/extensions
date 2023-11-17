7c9ad1bd68147cd0a97d556b676cb22e5686e927
eeee0717 <70054568+eeee0717@users.noreply.github.com>
2023-11-17 11:15:30 +0800

    chore: add delete style

--- extensions/workspace/src/components/DeleteWorkspaceAction.tsx
+++ extensions/workspace/src/components/DeleteWorkspaceAction.tsx
@@ -1,7 +1,15 @@
 import { Action, Icon } from "@raycast/api";
 
 function DeleteWorkspaceAction(props: { onDelete: () => void }) {
-  return <Action icon={Icon.Trash} title="Delete Workspace" onAction={props.onDelete} />;
+  return (
+    <Action
+      icon={Icon.Trash}
+      style={Action.Style.Destructive}
+      title="Delete Workspace"
+      onAction={props.onDelete}
+      shortcut={{ modifiers: ["ctrl"], key: "x" }}
+    />
+  );
 }
 
 export default DeleteWorkspaceAction;
--- extensions/workspace/src/components/OpenWorkspaceAction.tsx
+++ extensions/workspace/src/components/OpenWorkspaceAction.tsx
@@ -1,5 +1,12 @@
 import { Action, Icon } from "@raycast/api";
 function OpenWorkspaceAction(props: { onOpen: () => void }) {
-  return <Action icon={Icon.List} title="Open Workspace" onAction={props.onOpen} />;
+  return (
+    <Action
+      icon={Icon.List}
+      title="Open Workspace"
+      onAction={props.onOpen}
+      shortcut={{ modifiers: ["cmd"], key: "o" }}
+    />
+  );
 }
 export default OpenWorkspaceAction;
