638603614c34666a74c5fd5b145a31afff3e3cf6
Roy Smith <rsmith@veracode.com>
2023-11-19 20:03:01 -0500

    Update jira extension
    - Update minor versions of dependencies
    - Separate out the issue metadata per issuetype for optimization
    - Initial commit

--- extensions/jira/src/api/issues.ts
+++ extensions/jira/src/api/issues.ts
@@ -203,9 +203,19 @@ type GetCreateIssueMetadataResponse = {
   projects: { issuetypes: IssueTypeWithCustomFields[] }[];
 };
 
-export async function getCreateIssueMetadata(projectId: string) {
-  const params = { expand: "projects.issuetypes.fields,style", projectIds: projectId };
+export async function getCreateIssueMetadataSummary(projectId: string) {
+  const params = { projectIds: projectId };
 
+  return getCreateIssueMetadataWithParams(params);
+}
+
+export async function getCreateIssueMetadata(projectId: string, issueTypeId: string) {
+  const params = { expand: "projects.issuetypes.fields", projectIds: projectId, issuetypeIds: issueTypeId };
+
+  return getCreateIssueMetadataWithParams(params);
+}
+
+async function getCreateIssueMetadataWithParams(params: { projectIds: string; expand?: string; issuetypeIds?: string; }) {
   const result = await request<GetCreateIssueMetadataResponse>(`/issue/createmeta`, { params });
 
   if (!result?.projects) {
