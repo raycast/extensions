8a001a3ff9eba7b6be208deb66915a057041bf92
Brittany Carusone <brcarusone@gmail.com>
2023-11-21 13:22:59 -0500

    Allow edit and delete from card details

--- extensions/rapidcap/src/view-card.tsx
+++ extensions/rapidcap/src/view-card.tsx
@@ -1,14 +1,23 @@
-import { Detail, Icon, Action } from "@raycast/api";
+import { Detail, Icon, Action, ActionPanel, useNavigation } from "@raycast/api";
 import { Card } from "./types";
+import DeleteCardAction from "./delete-card";
+import EditCardAction from "./edit-card";
 
-function ViewCardAction(props: { card: Card }) {
+function ViewCardAction(props: { card: Card; onEdit: ({ question, answer, tag }: Card) => void; onDelete: () => void }) {
   const { card } = props;
 
-  return <Action.Push icon={Icon.AppWindowSidebarRight} title="View Card" target={<ViewCardDetail card={card} />} />;
+  return (
+    <Action.Push 
+      icon={Icon.AppWindowSidebarRight} 
+      title="View Card" 
+      target={<ViewCardDetail card={card} onEdit={props.onEdit} onDelete={props.onDelete} />}
+    />
+  );
 }
 
-function ViewCardDetail(props: { card: Card }) {
-  const { card } = props;
+function ViewCardDetail(props: { card: Card; onEdit: ({ question, answer, tag }: Card) => void; onDelete: () => void }) {
+  const { card, onEdit, onDelete } = props;
+  const { pop } = useNavigation();
 
   const markdown = `# ${card.question}\n\n${card.answer}`;
 
@@ -22,6 +31,18 @@ function ViewCardDetail(props: { card: Card }) {
             <Detail.Metadata.TagList.Item text={card.tag} />
           </Detail.Metadata.TagList>
         </Detail.Metadata>
+      }
+      actions={
+        <ActionPanel>
+          <EditCardAction card={card} onEdit={(updatedCard) => {
+            onEdit(updatedCard);
+            pop();
+          }} />
+          <DeleteCardAction onDelete={() => {
+            onDelete();
+            pop();
+          }} />
+        </ActionPanel>
       }
     />
   );
