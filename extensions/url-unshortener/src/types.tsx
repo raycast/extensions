eae7528c483fc6ed16b3543ecb92cac80907ee51
Sebastian <sebastian.danielsson@proton.me>
2023-11-29 19:11:42 +0000

    Rework favicon fetching, improve accessory UI
    * Rework favicon fetching to fix missing favicons
    * Favicon is now using the same data structure as the redirectionSteps
    * Show icons in accessory for more http status codes
    * Show error messages in a list item instead of a toast for improved visibility

--- extensions/url-unshortener/src/types.tsx
+++ extensions/url-unshortener/src/types.tsx
@@ -1,11 +1,7 @@
-import { Icon } from "@raycast/api";
-
 export interface RedirectionStep {
   url: string;
   statusCode: number;
   statusName: string;
-}
-
-export interface FaviconUrls {
-  [key: string]: string | Icon;
+  faviconUrl?: string;
+  errorMessage?: string;
 }
--- extensions/url-unshortener/src/unshorten-url-interactive.tsx
+++ extensions/url-unshortener/src/unshorten-url-interactive.tsx
@@ -1,59 +1,17 @@
 import { useState, useEffect } from "react";
-import { ActionPanel, Action, Icon, List, showToast, Toast, Color } from "@raycast/api";
-import { RedirectionStep, FaviconUrls } from "./types";
+import { ActionPanel, Action, Icon, List, Color } from "@raycast/api";
+import { RedirectionStep } from "./types";
 import { getUrlFromSelectionOrClipboard, isValidUrl, unshortenUrl, getFaviconUrl } from "./utils";
 
 export default function UrlRedirectionList() {
   const [redirectionSteps, setRedirectionSteps] = useState<RedirectionStep[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [initialUrl, setInitialUrl] = useState("");
-  const [faviconUrls, setFaviconUrls] = useState<FaviconUrls>({});
-
-  useEffect(() => {
-    const init = async () => {
-      const url = await getUrlFromSelectionOrClipboard();
-
-      if (url) {
-        fetchData(url);
-      }
-    };
-
-    const fetchData = async (url: string) => {
-      setIsLoading(true);
-
-      if (!isValidUrl(url)) {
-        setIsLoading(false);
-        return;
-      }
-
-      try {
-        const result = await unshortenUrl(url);
-        setRedirectionSteps(result.redirectionSteps);
-        fetchFavicons(result.redirectionSteps);
-      } catch (error) {
-        handleFetchError(error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    const handleFetchError = (error: unknown) => {
-      if (error instanceof Error) {
-        if (error.message.includes("getaddrinfo ENOTFOUND")) {
-          showToast(Toast.Style.Failure, "Cannot resolve host: " + error.message.split(" ")[2]);
-        } else {
-          showToast(Toast.Style.Failure, error.message);
-        }
-      } else {
-        showToast(Toast.Style.Failure, "An unknown error occurred");
-      }
-    };
-
-    init();
-  }, []);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [initialUrl, setInitialUrl] = useState<string>("");
 
   const fetchData = async (url: string) => {
     setIsLoading(true);
+    setRedirectionSteps([]);
+
     if (!isValidUrl(url)) {
       setIsLoading(false);
       return;
@@ -62,30 +20,57 @@ export default function UrlRedirectionList() {
     try {
       const result = await unshortenUrl(url);
       setRedirectionSteps(result.redirectionSteps);
-    } catch (error) {
+      await fetchFavicons(result.redirectionSteps);
+    } catch (error: unknown) {
+      let errorMessage = "An unknown error occurred";
       if (error instanceof Error) {
-        showToast(Toast.Style.Failure, error.message);
-      } else {
-        showToast(Toast.Style.Failure, "An unknown error occurred");
+        if (error.message.includes("getaddrinfo ENOTFOUND")) {
+          errorMessage = "Cannot resolve host";
+        } else {
+          errorMessage = error.message;
+        }
       }
+      setRedirectionSteps([{ url, statusCode: 0, statusName: "Error", errorMessage }]);
     } finally {
       setIsLoading(false);
     }
   };
 
-  const fetchFavicons = async (urlObjects: { url: string }[]) => {
-    const newFaviconUrls = { ...faviconUrls };
-
-    for (const urlObject of urlObjects) {
-      try {
-        const faviconUrl = getFaviconUrl(urlObject.url);
-        newFaviconUrls[urlObject.url] = faviconUrl || Icon.Globe;
-      } catch (error) {
-        newFaviconUrls[urlObject.url] = Icon.Globe;
+  useEffect(() => {
+    const init = async () => {
+      const url = await getUrlFromSelectionOrClipboard();
+      if (url) {
+        fetchData(url);
       }
-    }
+    };
 
-    setFaviconUrls(newFaviconUrls);
+    init();
+  }, []);
+
+  useEffect(() => {
+    const fetchFaviconsForSteps = async () => {
+      const updatedSteps = await fetchFavicons(redirectionSteps);
+      if (JSON.stringify(updatedSteps) !== JSON.stringify(redirectionSteps)) {
+        setRedirectionSteps(updatedSteps);
+      }
+    };
+
+    if (redirectionSteps.length > 0) {
+      fetchFaviconsForSteps();
+    }
+  }, [redirectionSteps]);
+
+  const fetchFavicons = async (steps: RedirectionStep[]) => {
+    return Promise.all(
+      steps.map(async (step: RedirectionStep) => {
+        try {
+          const faviconUrl = await getFaviconUrl(step.url);
+          return { ...step, faviconUrl: faviconUrl || Icon.Globe };
+        } catch (error: unknown) {
+          return { ...step, faviconUrl: Icon.Globe };
+        }
+      }),
+    );
   };
 
   const onSearchTextChange = async (newText: string) => {
@@ -93,9 +78,6 @@ export default function UrlRedirectionList() {
 
     if (isValidUrl(newText)) {
       await fetchData(newText);
-      if (redirectionSteps.length === 0) {
-        await fetchFavicons([{ url: newText }]);
-      }
     } else {
       setRedirectionSteps([]);
     }
@@ -116,16 +98,37 @@ export default function UrlRedirectionList() {
           <List.Item
             key={index}
             title={step.url}
-            icon={{ source: typeof faviconUrls[step.url] === "string" ? faviconUrls[step.url] : Icon.Globe }}
-            accessories={[
-              {
-                tag: {
-                  value: step.statusCode.toString(),
-                  color: step.statusCode >= 200 && step.statusCode < 300 ? Color.Green : Color.Yellow,
-                },
-                icon: step.statusCode >= 200 && step.statusCode < 300 ? Icon.CheckCircle : Icon.ArrowClockwise,
-              },
-            ]}
+            subtitle={step.errorMessage ? `Error: ${step.errorMessage}` : undefined}
+            icon={step.errorMessage ? Icon.XMarkCircle : step.faviconUrl || Icon.Globe}
+            accessories={
+              !step.errorMessage
+                ? [
+                    {
+                      tag: {
+                        value: step.statusCode.toString(),
+                        color:
+                          step.statusCode >= 200 && step.statusCode < 300
+                            ? Color.Green
+                            : step.statusCode >= 300 && step.statusCode < 400
+                              ? Color.Yellow
+                              : step.statusCode >= 400 && step.statusCode < 600
+                                ? Color.Red
+                                : Color.PrimaryText,
+                      },
+                      icon:
+                        step.statusCode >= 100 && step.statusCode < 200
+                          ? Icon.Info
+                          : step.statusCode >= 200 && step.statusCode < 300
+                            ? Icon.CheckCircle
+                            : step.statusCode >= 300 && step.statusCode < 400
+                              ? Icon.ArrowClockwise
+                              : step.statusCode >= 400 && step.statusCode < 600
+                                ? Icon.XMarkCircle
+                                : Icon.QuestionMark,
+                    },
+                  ]
+                : []
+            }
             actions={
               <ActionPanel>
                 <Action.CopyToClipboard content={step.url} />
