91f2d58a02830740aa4bd798e0a6f9ebf91f80a4
Macko++ <82177845+Macko-pp@users.noreply.github.com>
2023-12-03 15:00:39 -0500

    Update hyrule-compendium-search extension
    - Formatted
    - Fixed materials and creatures bug

--- extensions/hyrule-compendium-search/CHANGELOG.md
+++ extensions/hyrule-compendium-search/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Hyrule Compendium Changelog
 
-## [Added Hyrule Compendium] - 2023-12-01
+## [Fixed materials and creatures bug] - 2023-12-03
+
+    Fixed certain materials and creatures that had either 0 hearts recovered or no cooking effect displaying the 0 or "" cooking effect. They now dont display their respective empty Details
+
+## [Added Hyrule Compendium] - 2023-12-03
 
 Initial version code
--- extensions/hyrule-compendium-search/src/index.tsx
+++ extensions/hyrule-compendium-search/src/index.tsx
@@ -23,66 +23,76 @@ interface Entry {
   description: string; // short paragraph
   image: string; // URL of image
   common_locations: string[] | null; // where the entry is commonly seen
-}
-
-interface Monsters {
-  name: string; // entry name
-  id: number; // ID as shown in compendium
-  category: string; // "monsters"
-  description: string; // short paragraph
-  image: string; // URL of image
-  common_locations: string[] | null; // where the entry is commonly seen
-  drops: string[] | null; // recoverable materials from killing
-}
-
-interface Equipment {
-  name: string; // entry name
-  id: number; // ID as shown in compendium
-  category: string; // "equipment"
-  description: string; // short paragraph
-  image: string; // URL of image
-  common_locations: string[] | null; // array of strings or null for unknown; where the entry is commonly seen
+  //
+  drops: string[]; // recoverable materials from killing
   properties: {
     attack: number; // damage the entry does (0 for shields and arrows)
     defense: number; // defense the entry offers (0 for equipment that aren't shields)
   };
-}
-
-interface Material {
-  name: string; // entry name
-  id: number; // ID as shown in compendium
-  category: string; // "materials"
-  description: string; // short paragraph
-  image: string; // URL of image
-  common_locations: string[] | null; // where the entry is commonly seen
   hearts_recovered: number; // health recovered when eaten raw
-  cooking_effect: string | null; // special effect when used in a dish/elixir (e.g. "stamina recovery"), empty if none
-}
-
-interface Creature {
-  name: string; // entry name
-  id: number; // ID as shown in compendium
-  category: string; // "creatures"
-  description: string; // short paragraph
-  image: string; // URL of image
   cooking_effect: string; // special effect when used in a dish/elixir (e.g. "stamina recovery"), empty if none
-  common_locations: string[] | null; // where the entry is commonly seen
   edible: boolean; // whether the creature can be eaten or incorporated into a dish/elixir
-  hearts_recovered: number | undefined; // hearts recovered when eaten raw
-  drops: string[] | null; // array of strings or null for unknown; recoverable materials from killing
-  dlc: boolean; // whether the entry is from a DLC pack
 }
 
-interface Treasure {
-  name: string; // string
-  id: number; // integer; ID as shown in compendium
-  category: string; // string; "treasure"
-  description: string; // string; short paragraph
-  image: string; // string; URL of image
-  common_locations: string[] | null; // array of strings or null for unknown; where the entry is commonly seen
-  drops: string[] | null; // array of strings or null for unknown; recoverable materials when accessed
-  dlc: boolean; // boolean; whether the entry is from a DLC pack
-}
+// interface Monsters {
+//   name: string; // entry name
+//   id: number; // ID as shown in compendium
+//   category: string; // "monsters"
+//   description: string; // short paragraph
+//   image: string; // URL of image
+//   common_locations: string[] | null; // where the entry is commonly seen
+//   drops: string[] | null; // recoverable materials from killing
+//   hearts_recovered: number; // health recovered when eaten raw
+//   cooking_effect: string | null; // special effect when used in a dish/elixir (e.g. "stamina recovery"), empty if none
+// }
+
+// interface Equipment {
+//   name: string; // entry name
+//   id: number; // ID as shown in compendium
+//   category: string; // "equipment"
+//   description: string; // short paragraph
+//   image: string; // URL of image
+//   common_locations: string[] | null; // array of strings or null for unknown; where the entry is commonly seen
+//   properties: {
+//     attack: number; // damage the entry does (0 for shields and arrows)
+//     defense: number; // defense the entry offers (0 for equipment that aren't shields)
+//   };
+// }
+
+// interface Material {
+//   name: string; // entry name
+//   id: number; // ID as shown in compendium
+//   category: string; // "materials"
+//   description: string; // short paragraph
+//   image: string; // URL of image
+//   common_locations: string[] | null; // where the entry is commonly seen
+//   hearts_recovered: number; // health recovered when eaten raw
+//   cooking_effect: string | null; // special effect when used in a dish/elixir (e.g. "stamina recovery"), empty if none
+// }
+
+// interface Creature {
+//   name: string; // entry name
+//   id: number; // ID as shown in compendium
+//   category: string; // "creatures"
+//   description: string; // short paragraph
+//   image: string; // URL of image
+//   cooking_effect: string; // special effect when used in a dish/elixir (e.g. "stamina recovery"), empty if none
+//   common_locations: string[] | null; // where the entry is commonly seen
+//   edible: boolean; // whether the creature can be eaten or incorporated into a dish/elixir
+//   hearts_recovered: number | undefined; // hearts recovered when eaten raw
+//   drops: string[] | null; // array of strings or null for unknown; recoverable materials from killing
+// }
+
+// interface Treasure {
+//   name: string; // string
+//   id: number; // integer; ID as shown in compendium
+//   category: string; // string; "treasure"
+//   description: string; // string; short paragraph
+//   image: string; // string; URL of image
+//   common_locations: string[] | null; // array of strings or null for unknown; where the entry is commonly seen
+//   drops: string[] | null; // array of strings or null for unknown; recoverable materials when accessed
+//   dlc: boolean; // boolean; whether the entry is from a DLC pack
+// }
 
 const options = {
   includeScore: true,
@@ -115,21 +125,21 @@ export default function Command(props: LaunchProps<{ arguments: EntryArgument }>
   );
   // const entry = data?.data as Equipment;
   const category = data?.data as Category;
-  let entry: any;
+  const entry = data?.data as Entry;
 
-  if (category?.category == "monsters") {
-    entry = data?.data as Monsters;
-  } else if (category?.category == "equipment") {
-    entry = data?.data as Equipment;
-  } else if (category?.category == "material") {
-    entry = data?.data as Material;
-  } else if (category?.category == "creature") {
-    entry = data?.data as Creature;
-  } else if (category?.category == "treasure") {
-    entry = data?.data as Treasure;
-  } else {
-    entry = data?.data as Entry;
-  }
+  //   if (category?.category == "monsters") {
+  //     entry = data?.data as Monsters;
+  //   } else if (category?.category == "equipment") {
+  //     entry = data?.data as Equipment;
+  //   } else if (category?.category == "material") {
+  //     entry = data?.data as Material;
+  //   } else if (category?.category == "creature") {
+  //     entry = data?.data as Creature;
+  //   } else if (category?.category == "treasure") {
+  //     entry = data?.data as Treasure;
+  //   } else {
+  //     entry = data?.data as Entry;
+  //   }
 
   const markdown = `# ${entry?.name.replace(/(^|\s)\S/g, (match: string) => match.toUpperCase())}
 
@@ -212,27 +222,53 @@ export default function Command(props: LaunchProps<{ arguments: EntryArgument }>
           </>
         );
       } else {
-        return (
-          <>
-            <Detail.Metadata.Label
-              title="Cooking Effect"
-              text={entry?.cooking_effect.replace(/(^|\s)\S/g, (match: string) => match.toUpperCase())}
-            />
-            <Detail.Metadata.Label title="Hearts Recovered" text={entry?.hearts_recovered.toString()} />
-          </>
-        );
+        if (entry?.hearts_recovered == 0) {
+          return (
+            <>
+              <Detail.Metadata.Label
+                title="Cooking Effect"
+                text={entry?.cooking_effect.replace(/(^|\s)\S/g, (match: string) => match.toUpperCase())}
+              />
+            </>
+          );
+        } else {
+          return (
+            <>
+              <Detail.Metadata.Label
+                title="Cooking Effect"
+                text={entry?.cooking_effect.replace(/(^|\s)\S/g, (match: string) => match.toUpperCase())}
+              />
+              <Detail.Metadata.Label title="Hearts Recovered" text={entry?.hearts_recovered.toString()} />
+            </>
+          );
+        }
       }
     } else if (category?.category == "creatures") {
       if (entry?.edible == true) {
-        return (
-          <>
-            <Detail.Metadata.Label
-              title="Cooking Effect"
-              text={entry?.cooking_effect.replace(/(^|\s)\S/g, (match: string) => match.toUpperCase())}
-            />
-            <Detail.Metadata.Label title="Hearts Recovered" text={entry?.hearts_recovered.toString()} />
-          </>
-        );
+        if (entry?.hearts_recovered == 0) {
+          return (
+            <>
+              <Detail.Metadata.Label
+                title="Cooking Effect"
+                text={entry?.cooking_effect.replace(/(^|\s)\S/g, (match: string) => match.toUpperCase())}
+              />
+            </>
+          );
+        } else {
+          if (entry?.cooking_effect == "") {
+            return <Detail.Metadata.Label title="Hearts Recovered" text={entry?.hearts_recovered.toString()} />;
+          } else {
+            return (
+              <>
+                <Detail.Metadata.Label
+                  title="Cooking Effect"
+                  text={entry?.cooking_effect.replace(/(^|\s)\S/g, (match: string) => match.toUpperCase())}
+                />
+                <Detail.Metadata.Label title="Hearts Recovered" text={entry?.hearts_recovered.toString()} />
+              </>
+            );
+          }
+        }
       } else {
         if (entry?.drops.length == 0) {
           return (
