7a3bc25a6de63c37d64e4b59d76ba62ea3de9604
Karol Podżerek <35146988+KarolusD@users.noreply.github.com>
2023-12-07 12:32:18 +0100

    Add omnivore extension (#9266)
    * Add omnivore extension
    
    - forth commit
    - third initial commit
    - second initial commit
    - initial commit
    
    * Fix typo in SearchDropdown component
    
    * Initial improvements
    
    * Update CHANGELOG.md
    
    * Update package.json
    
    * Update omnivore extension
    
    - Better error handling when capturing URL
    - Pull contributions
    
    * Add URL safeguard in saveUrl function
    
    * Omnivore API key is required now
    
    * saveUrl uses graphql query
    
    ---------
    
    Co-authored-by: Per Nielsen Tikær <per@raycast.com>

---
+++ extensions/omnivore/CHANGELOG.md
@@ -0,0 +1,16 @@
+# Omnivore Changelog
+
+## [Initial Improvements] - 2023-11-20
+
+- Quick Lookup on article (markdown) added
+- Better toasts messages when archiving and deleting article
+- Quick Capture runs in "no-view" mode now
+- Article actions are now separated into re-usable component
+
+## [Initial Version] - 2023-11-15
+
+- Search Articles (in inbox, archive, continue reading, library, highlights)
+- Archive Article
+- Unarchive Article
+- Delete Article
+- Quick Capture URL with Labels
---
+++ extensions/omnivore/src/hooks/useArticles.ts
@@ -0,0 +1,48 @@
+import { getPreferenceValues } from '@raycast/api'
+import { useFetch } from '@raycast/utils'
+import { searchArticlesQuery } from '../graphql/queries'
+import { Article } from '../types'
+
+const apiUrl = 'https://api-prod.omnivore.app/api/graphql'
+const { apiKey } = getPreferenceValues<{ apiKey: string }>()
+
+/**
+ * Custom hook for fetching articles based on search criteria.
+ *
+ * @param {string} searchType - The type of search (e.g., "in:inbox", "in:archive").
+ * @param {string} searchText - The text to search for.
+ */
+export function useArticles(searchType: string, searchText: string) {
+  const { data, error, revalidate } = useFetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: apiKey,
+    },
+    body: JSON.stringify({
+      query: searchArticlesQuery,
+      variables: {
+        query: `${searchType}`,
+      },
+    }),
+    parseResponse: async (response) => {
+      const json = await response.json()
+      return json.data
+    },
+    keepPreviousData: true,
+  })
+
+  const transformedData = data?.search?.edges?.map((edge: { node: Article }) => edge.node) || []
+  const filteredData = transformedData.filter(
+    (article: Article) =>
+      article.title.toLowerCase().includes(searchText.toLowerCase()) ||
+      article.author.toLowerCase().includes(searchText.toLowerCase()) ||
+      article.labels.some((label) => label.name.toLowerCase().includes(searchText.toLowerCase())),
+  )
+
+  return {
+    data: filteredData as Article[],
+    error,
+    revalidate,
+  }
+}
