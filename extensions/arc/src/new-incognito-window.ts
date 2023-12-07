87bc0f5c009be7f791c88e6384cf7b80c527ff47
tleo19 <leotuil19@gmail.com>
2023-12-06 22:05:10 -0600

    Update arc extension
    - add new command for open boost gallery and open release notes
    - Add "tleo19" to contributors and new commands for Arc release notes and Boost Gallery
    - Add logic to create a new window if no windows exist before making a new tab
    - add url option to new incognito window

--- extensions/arc/src/new-incognito-window.ts
+++ extensions/arc/src/new-incognito-window.ts
@@ -1,33 +1,10 @@
-import { closeMainWindow, showHUD, LaunchProps } from "@raycast/api";
+import { closeMainWindow, showHUD } from "@raycast/api";
 import { makeNewWindow } from "./arc";
-import { validateURL } from "./utils";
-import { URLArguments } from "./types";
 
-const handleOpenNewIncognitoTab = async (newTabUrl: string) => {
-  try {
-    if (await validateURL(newTabUrl)) {
-      // Append https:// if protocol is missing
-      const openURL = !/^\S+?:\/\//i.test(newTabUrl) ? "https://" + newTabUrl : newTabUrl;
-      await closeMainWindow();
-      await makeNewWindow({ incognito: true, url: openURL });
-    }
-  } catch (e) {
-    console.error(e);
-
-    await showHUD("❌ Failed opening a new tab");
-  }
-};
-
-export default async function command(props: LaunchProps<{ arguments: URLArguments }>) {
-  const { url } = props.arguments;
-
+export default async function command() {
   try {
     await closeMainWindow();
-    if (!url) {
-      await makeNewWindow({ incognito: true });
-      return;
-    }
-    await handleOpenNewIncognitoTab(url);
+    await makeNewWindow({ incognito: true });
   } catch {
     await showHUD("❌ Failed opening a new incogntio window");
   }
