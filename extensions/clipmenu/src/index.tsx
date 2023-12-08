975e850022325a21a20cec21ac005b2e8c294ca4
GnimOay <yyaomingm@gmail.com>
2023-12-08 15:15:32 +0800

    Update extensions/clipmenu/src/index.tsx
    Co-authored-by: Per Nielsen Tikær <per@raycast.com>

--- extensions/clipmenu/src/index.tsx
+++ extensions/clipmenu/src/index.tsx
@@ -1,29 +1,32 @@
 import { Clipboard, Icon, MenuBarExtra, updateCommandMetadata } from "@raycast/api";
-import { useCachedState } from "@raycast/utils";
-import { iiafe } from "./utils";
+import { useCachedPromise } from "@raycast/utils";
+import { truncateText } from "./utils";
 
 export default function Command() {
-  const [history, setHistory] = useCachedState<string[]>("latest-clipboard-histories", [""]);
-
-  iiafe(async () => {
-    const _history = (
-      await Promise.all(
-        Array(6)
-          .fill(0)
-          .map((_, idx) => Clipboard.readText({ offset: idx })),
-      )
-    ).filter(Boolean) as string[];
+  const { isLoading, data } = useCachedPromise(async () => {
+    const _history = await Promise.all(
+      Array(6)
+        .fill(0)
+        .map((_, idx) => Clipboard.readText({ offset: idx })),
+    );
 
     updateCommandMetadata({ subtitle: _history[0] });
-    setHistory(_history);
+    return _history.filter(Boolean) as string[];
   });
 
   return (
-    <MenuBarExtra icon={Icon.Clipboard} title={history[0]}>
-      <MenuBarExtra.Item title="Clipboard History" />
-      {history.map((text, i) => (
-        <MenuBarExtra.Item key={text + i} title={text} onAction={() => Clipboard.paste(text)} icon={Icon.Clipboard} />
-      ))}
+    <MenuBarExtra isLoading={isLoading} icon={Icon.Clipboard} title={data ? truncateText(data[0], 50) : ""}>
+      <MenuBarExtra.Section title="Clipboard History">
+        {data &&
+          data.map((text, i) => (
+            <MenuBarExtra.Item
+              key={text + i}
+              title={truncateText(text, 50)}
+              onAction={() => Clipboard.paste(text)}
+              icon={Icon.Clipboard}
+            />
+          ))}
+      </MenuBarExtra.Section>
     </MenuBarExtra>
   );
 }
