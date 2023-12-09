b06aa4211d768ffb81b82eed5fb3c54593a937ad
Neil <bn0010100@gmail.com>
2023-12-09 21:56:26 +0800

    rename preference name of apiToken

--- extensions/tana/src/api/helpers.ts
+++ extensions/tana/src/api/helpers.ts
@@ -4,12 +4,12 @@ import { TanaAPIHelper } from "./TanaAPIClient";
 export const TANA_URL = "https://europe-west1-tagr-prod.cloudfunctions.net";
 
 type Preferences = {
-  nodeApiToken: string;
+  workspaceApiToken: string;
 };
 export const prefs = getPreferenceValues<Preferences>();
 
 export const createPlainNode = async (name: string): Promise<string> => {
-  const helper = new TanaAPIHelper(prefs.nodeApiToken);
+  const helper = new TanaAPIHelper(prefs.workspaceApiToken);
   const tananode = await helper.createNode(
     {
       name,
