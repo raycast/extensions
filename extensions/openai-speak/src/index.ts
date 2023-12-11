48bd6b97dfa36075e1e0155db586909f2daaf766
Jian Chao Man <me@chaomander.com>
2023-12-11 20:49:34 +1100

    Improved latency by implementing sentence and newline based chunking in text-to-speech processing.

--- extensions/openai-speak/src/index.ts
+++ extensions/openai-speak/src/index.ts
@@ -1,10 +1,12 @@
 import { getSelectedText, showToast, Toast, getPreferenceValues, closeMainWindow } from "@raycast/api";
-
 import { OpenAI } from "openai";
-import fs from "fs";
+import fs from "fs/promises";
 import path from "path";
 import { exec } from "child_process";
 import os from "os";
+import util from "util";
+
+const execAsync = util.promisify(exec);
 
 type Voice = "alloy" | "echo" | "fable" | "onyx" | "nova" | "shimmer";
 
@@ -13,48 +15,90 @@ interface Preferences {
   defaultVoice: Voice;
 }
 
-export default async function Command() {
-  await textToSpeech();
+class TextToSpeechProcessor {
+  private textToSpeechQueue: string[] = [];
+  private playAudioQueue: string[] = [];
+  private isPlaying = false;
+  private isConverting = false;
+  private openai: OpenAI;
+  private voice: Voice;
+
+  constructor(apiKey: string, voice: Voice) {
+    this.openai = new OpenAI({ apiKey });
+    this.voice = voice;
+  }
+
+  public async processSelectedText() {
+    const selectedText = await getSelectedText();
+    this.textToSpeechQueue = selectedText.split(/(?:\.\s|\.\n|\n)/).filter(Boolean);
+
+    if (!this.isConverting) {
+      await this.convertTextToSpeech();
+    }
+
+    await this.waitForQueuesToEmpty();
+  }
+
+  private async waitForQueuesToEmpty() {
+    while (this.textToSpeechQueue.length > 0 || this.playAudioQueue.length > 0 || this.isConverting || this.isPlaying) {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
+  }
+
+  private async convertTextToSpeech() {
+    this.isConverting = true;
+
+    for (const textPart of this.textToSpeechQueue) {
+      const speechFile = path.resolve(os.tmpdir(), `speech_${Date.now()}.mp3`);
+      try {
+        const mp3 = await this.openai.audio.speech.create({
+          model: "tts-1",
+          voice: this.voice,
+          input: textPart,
+        });
+        const buffer = Buffer.from(await mp3.arrayBuffer());
+        await fs.writeFile(speechFile, buffer);
+        this.playAudioQueue.push(speechFile);
+      } catch (error) {
+        console.error("Error converting text to speech:", error);
+      }
+
+      if (!this.isPlaying) {
+        this.playAudio();
+      }
+    }
+
+    this.textToSpeechQueue = [];
+    this.isConverting = false;
+  }
+
+  private async playAudio() {
+    while (this.playAudioQueue.length > 0) {
+      await closeMainWindow({ clearRootSearch: true });
+      this.isPlaying = true;
+      const speechFile = this.playAudioQueue.shift()!;
+      try {
+        await execAsync(`afplay ${speechFile}`);
+      } catch (error) {
+        await showToast({
+          style: Toast.Style.Failure,
+          title: "Error playing audio",
+          message: String(error),
+        });
+      } finally {
+        await fs.unlink(speechFile).catch((err) => console.error(`Error deleting file: ${speechFile}`, err));
+      }
+    }
+
+    this.isPlaying = false;
+    if (!this.isConverting) {
+      await closeMainWindow({ clearRootSearch: true });
+    }
+  }
 }
 
-async function textToSpeech() {
+export default async function Command() {
   const preferences = getPreferenceValues<Preferences>();
-  const apiKey = preferences.apiKey;
-  const voice = preferences.defaultVoice;
-  const openai = new OpenAI({
-    apiKey: apiKey,
-  });
-
-  try {
-    const selectedText = await getSelectedText();
-    const speechFile = path.resolve(os.tmpdir(), `speech_${Date.now()}.mp3`);
-    const mp3 = await openai.audio.speech.create({
-      model: "tts-1",
-      voice: voice,
-      input: selectedText,
-    });
-    console.log(speechFile);
-    const buffer = Buffer.from(await mp3.arrayBuffer());
-    await fs.promises.writeFile(speechFile, buffer);
-
-    // Play the audio file and close the command window
-    await new Promise((resolve, reject) => {
-      exec(`afplay ${speechFile}`, (error) => {
-        if (error) {
-          console.error(`exec error: ${error}`);
-          reject(error);
-          return;
-        }
-        resolve(null);
-      });
-    });
-
-    await closeMainWindow({ clearRootSearch: true });
-  } catch (error) {
-    await showToast({
-      style: Toast.Style.Failure,
-      title: "Cannot Speak text",
-      message: String(error),
-    });
-  }
+  const processor = new TextToSpeechProcessor(preferences.apiKey, preferences.defaultVoice);
+  await processor.processSelectedText();
 }
