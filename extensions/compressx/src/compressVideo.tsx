7dd17b9dfd1aa46f9a35143f6483f1df3116ea1a
dqhieu <dqhieu13@gmail.com>
2023-12-16 00:38:46 +0800

    Update compressx extension
    - Fix lint
    - Check if app is installed

--- extensions/compressx/src/compressVideo.tsx
+++ extensions/compressx/src/compressVideo.tsx
@@ -1,7 +1,13 @@
 import { showToast, Toast, getSelectedFinderItems } from "@raycast/api";
 import { exec } from "child_process";
+import { checkCompressXInstallation } from "./utils/checkInstall";
 
 export default async function main() {
+  const isInstalled = await checkCompressXInstallation();
+  if (!isInstalled) {
+    return;
+  }
+
   let filePaths: string[];
 
   try {
