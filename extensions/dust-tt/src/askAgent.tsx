61c307a54e6446fbbde2185aa6040b64eac5ad3b
Per Nielsen Tikær <per@raycast.com>
2023-12-15 05:24:00 +0100

    Update askAgent.tsx

--- extensions/dust-tt/src/askAgent.tsx
+++ extensions/dust-tt/src/askAgent.tsx
@@ -214,7 +214,7 @@ function AgentListItem({
           {selectedText ? (
             <>
               <Action.Push
-                title="Ask For Selected Text"
+                title="Ask for Selected Text"
                 icon={Icon.Message}
                 shortcut={{ key: "return", modifiers: [] }}
                 target={
