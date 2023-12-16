b7a79ee5da6e25c51e3311932e7513644cf8d983
朱建业 <borisdunk@sina.com>
2023-12-16 17:31:27 +0800

    Update send-to-flomo extension
    - feat: prevent submit during loading
    - Initial commit

--- extensions/send-to-flomo/src/send-to-flomo.tsx
+++ extensions/send-to-flomo/src/send-to-flomo.tsx
@@ -11,8 +11,10 @@ export default function Command() {
   const preferences: Preferences = getPreferenceValues();
   const [text, setText] = useState<string>("");
   const [tags, setTags] = useState<string>(preferences.defaultTags);
+  const [loading, setLoading] = useState<boolean>(false);
 
   async function handleSubmit(values: Record<string, FormValue>) {
+    if (loading) return;
     let content = values.memo;
 
     if (content !== "") {
@@ -21,44 +23,50 @@ export default function Command() {
       }
       showToast({
         style: Toast.Style.Animated,
-        title: "Sending",
+        title: "Sending"
       });
+      setLoading(true);
       const response = await axios.post(preferences.api, { content });
 
       if (response.data?.code === 0) {
         showToast({
           style: Toast.Style.Success,
           title: "Success",
-          message: "Successfully sent MEMO",
+          message: "Successfully sent MEMO"
         });
         setText("");
         setTags(preferences.defaultTags);
+        setLoading(false);
       } else {
         showToast({
           style: Toast.Style.Failure,
           title: "Failed",
-          message: "Check that API MEMO URL is correct",
+          message: "Check that API MEMO URL is correct"
         });
+        setLoading(false);
       }
     } else {
       showToast({
         style: Toast.Style.Failure,
         title: "Failed",
-        message: "MEMO can't be left empty",
+        message: "MEMO can't be left empty"
       });
+      setLoading(false);
     }
   }
 
   return (
-    <Form
-      actions={
-        <ActionPanel>
-          <Action.SubmitForm title="Send MEMO" onSubmit={handleSubmit} />
-        </ActionPanel>
-      }
-    >
-      <Form.TextArea id="memo" title="MEMO" placeholder="What's on your mind?" defaultValue={text} onChange={setText} />
-      <Form.TextField id="tags" title="Tags" defaultValue={tags} onChange={setTags} info="Separated by space" />
-    </Form>
+    <>
+      <Form
+        actions={
+          <ActionPanel>
+            <Action.SubmitForm title="Send MEMO" onSubmit={handleSubmit} />
+          </ActionPanel>
+        }
+      >
+        <Form.TextArea id="memo" title="MEMO" placeholder="What's on your mind?" defaultValue={text} onChange={setText} />
+        <Form.TextField id="tags" title="Tags" defaultValue={tags as string} onChange={setTags} info="Separated by space" />
+      </Form>
+    </>
   );
 }
