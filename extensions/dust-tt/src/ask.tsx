98bcf370e20097baa622019fb49025e752042b93
Vincent de Lagabbe <vincent.delagabbe@alan.eu>
2023-12-17 17:46:10 +0100

    Update dust-tt extension
    - Merge branch \'contributions/merge-1702831515456437000\'
    - Pull contributions
    - changelog update
    - fix: race condition on get selected text logic

--- extensions/dust-tt/src/ask.tsx
+++ extensions/dust-tt/src/ask.tsx
@@ -1,4 +1,4 @@
-import { LaunchProps } from "@raycast/api";
+import { Detail, LaunchProps } from "@raycast/api";
 import { AskDustQuestion } from "./answerQuestion";
 import { AgentType } from "./dust_api/agent";
 import { AskAgentQuestionForm, useGetSelectedText } from "./askAgent";
@@ -7,11 +7,16 @@ import { DUST_AGENT } from "./agents";
 export default function AskDustCommand(props: LaunchProps<{ arguments: { search: string; agent?: AgentType } }>) {
   const question = props.arguments.search;
   const agent = props.arguments.agent || DUST_AGENT;
+  const { selectedText, isLoading } = useGetSelectedText();
 
-  const selectedText = question ? undefined : useGetSelectedText();
+  if (isLoading) {
+    return <Detail isLoading={true} />;
+  }
+
+  const initialQuestion = question ? undefined : selectedText;
   return question ? (
     <AskDustQuestion question={question} agent={agent} />
   ) : (
-    <AskAgentQuestionForm initialQuestion={selectedText} agent={agent} />
+    <AskAgentQuestionForm initialQuestion={initialQuestion} agent={agent} />
   );
 }
