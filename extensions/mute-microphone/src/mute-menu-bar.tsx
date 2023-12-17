198de727f3390e68290bbb44e954ddc0fdac3db9
iamyeizi <37552350+iamyeizi@users.noreply.github.com>
2023-12-17 05:15:04 -0300

    added show menubar icon only if muted, issue #8832

--- extensions/mute-microphone/src/mute-menu-bar.tsx
+++ extensions/mute-microphone/src/mute-menu-bar.tsx
@@ -1,4 +1,4 @@
-import { Cache, Color, Icon, MenuBarExtra } from "@raycast/api";
+import { Cache, Color, Icon, MenuBarExtra, getPreferenceValues, openCommandPreferences } from "@raycast/api";
 import { useEffect, useState } from "react";
 import { getCurrentAudioInputLevel, toggleSystemAudioInputLevel } from "./shared/utils";
 
@@ -8,6 +8,7 @@ export default function muteMenuBar() {
   const cachedValueNumber = cachedValue == undefined ? 1 : Number(cachedValue);
   const disabledIcon = { source: Icon.MicrophoneDisabled, tintColor: Color.Red };
   const enabledIcon = { source: Icon.Microphone };
+  const preferences = getPreferenceValues<Preferences.MuteMenuBar>();
 
   const [currentAudioInputLevel, setCurrentAudioInputLevel] = useState<number>(cachedValueNumber);
   const icon = currentAudioInputLevel == 0 ? disabledIcon : enabledIcon;
@@ -21,7 +22,7 @@ export default function muteMenuBar() {
     }
   }, []);
 
-  return (
+  const CommonMenuItems = () => (
     <MenuBarExtra icon={icon} tooltip="Audio input volume">
       <MenuBarExtra.Item
         title={menuItemText}
@@ -30,6 +31,16 @@ export default function muteMenuBar() {
           setCurrentAudioInputLevel(Number(newLevel));
         }}
       />
+      <MenuBarExtra.Separator />
+      <MenuBarExtra.Item title="Settings..." onAction={openCommandPreferences} />
     </MenuBarExtra>
+  );
+
+  return preferences.hideIconWhenUnmuted ? (
+    currentAudioInputLevel == 0 ? (
+      <CommonMenuItems />
+    ) : null
+  ) : (
+    <CommonMenuItems />
   );
 }
