62106dda3c49596b22f51db083c88093a906ce4b
Per Nielsen Tikær <per@raycast.com>
2023-12-21 13:20:36 +0100

    Update PR bot with OOO message 🎄

--- scripts/bots/pr-bot.ts
+++ scripts/bots/pr-bot.ts
@@ -67,7 +67,7 @@ export default async ({ github, context }: API) => {
       await comment({
         github,
         context,
-        comment: `Congratulation on your new Raycast extension! :rocket:\n\nWe will review it shortly. Once the PR is approved and merged, the extension will be available on the Store.`,
+        comment: `Congratulation on your new Raycast extension! :rocket:\n\nThe team is on holiday, we'll review it once everyone is back. :christmas_tree:\n\nOnce the PR is approved and merged, the extension will be available on the Store.`,
       });
       return;
     }
@@ -104,7 +104,7 @@ export default async ({ github, context }: API) => {
       comment: `Thank you for your ${isFirstContribution ? "first " : ""} contribution! :tada:\n\n🔔 ${owners
         .filter((x) => x !== sender)
         .map((x) => `@${x}`)
-        .join(" ")} you might want to have a look.`,
+        .join(" ")} you might want to have a look.\n\nThe team is on holiday, we'll review it once everyone is back. :christmas_tree:`,
     });
 
     return;
