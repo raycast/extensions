83b46f480ba205d4c09d240fe5c9c22d990fc4db
Sheraff <fpellet@ensc.fr>
2023-12-21 23:30:08 +0100

    remove notification from menubar command, not allowed apparently

--- extensions/twitch/CHANGELOG.md
+++ extensions/twitch/CHANGELOG.md
@@ -8,7 +8,6 @@
   - Only requires users to input their Twitch username
 - Added menubar item
   - Show how many followed channels are live
-  - Optionally send a HUD notification when a followed channel goes live
 - Re-implemented `followed` command
   - Shows all followed channels, even when they are offline
   - Shows rich results, including screenshots and stream info
--- extensions/twitch/src/live.tsx
+++ extensions/twitch/src/live.tsx
@@ -1,39 +1,12 @@
-import {
-  Icon,
-  LaunchType,
-  MenuBarExtra,
-  getPreferenceValues,
-  launchCommand,
-  open,
-  openCommandPreferences,
-  showHUD,
-} from "@raycast/api";
+import { Icon, LaunchType, MenuBarExtra, launchCommand, open, openCommandPreferences } from "@raycast/api";
 import { formatDate } from "./helpers/datetime";
 import useFollowedStreams from "./helpers/useFollowedStreams";
 import { useUserId } from "./helpers/useUserId";
 import { userName } from "./helpers/auth";
-import { useRef } from "react";
-
-const preferences = getPreferenceValues<Preferences.Live>();
 
 export default function main() {
   const { data: userId, isLoading: isLoadingUserId } = useUserId();
   const { data: items = [], isLoading, updatedAt } = useFollowedStreams(userId, { loadingWhileStale: true });
-
-  const previous = useRef(items);
-  if (
-    preferences.notify &&
-    (previous.current.length !== items.length || previous.current.some((item) => !items.some((i) => i.id === item.id)))
-  ) {
-    const newItems = items.filter((item) => !previous.current.some((i) => i.id === item.id));
-    previous.current = items;
-
-    if (newItems.length === 1) {
-      showHUD(`${newItems[0].user_name} is live on Twitch`);
-    } else if (newItems.length > 1) {
-      showHUD(`${newItems.length} followed channels went live on Twitch`);
-    }
-  }
 
   if (!userName) {
     return (
