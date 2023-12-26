a97bb68c6bd3a770bb2dd68d086b9069ff380cd1
Neil Chudleigh <nchudleigh@users.noreply.github.com>
2023-12-25 20:46:19 -0500

    superwhisper extensions

---
+++ extensions/superwhisper/raycast-env.d.ts
@@ -0,0 +1,33 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `toggle-record` command */
+  export type ToggleRecord = ExtensionPreferences & {}
+  /** Preferences accessible in the `select-mode` command */
+  export type SelectMode = ExtensionPreferences & {}
+  /** Preferences accessible in the `open-settings` command */
+  export type OpenSettings = ExtensionPreferences & {}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `toggle-record` command */
+  export type ToggleRecord = {}
+  /** Arguments passed to the `select-mode` command */
+  export type SelectMode = {}
+  /** Arguments passed to the `open-settings` command */
+  export type OpenSettings = {}
+}
+
+
