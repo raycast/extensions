2dc966bc55e4184c0bfd7d0c7f793743aed12926
Michael Aigner <tonka3100@gmail.com>
2023-12-27 11:59:45 +0100

    fix crash when there is no metadata field in the extension manifest

--- extensions/visual-studio-code-recent-projects/src/index.tsx
+++ extensions/visual-studio-code-recent-projects/src/index.tsx
@@ -346,7 +346,7 @@ function RemoveActionSection(props: { entry: EntryLike } & RemoveMethods) {
     <ActionPanel.Section>
       <Action
         icon={Icon.Trash}
-        title="Remove from Recent Projects"
+        title="Remove From Recent Projects"
         style={Action.Style.Destructive}
         onAction={() => props.removeEntry(props.entry)}
         shortcut={{ modifiers: ["ctrl"], key: "x" }}
