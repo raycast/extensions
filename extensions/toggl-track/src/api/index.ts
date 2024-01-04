bdbe9538137c0685591d0f2d49c8088d4f31f67b
Ben Keys <bkeys818@gmail.com>
2024-01-04 17:40:05 -0500

    🎨 Change exporting method for api

--- extensions/toggl-track/src/api/index.ts
+++ extensions/toggl-track/src/api/index.ts
@@ -1,7 +1,7 @@
-export * from "./me";
-export * from "./workspaces";
-export * from "./projects";
-export * from "./clients";
-export * from "./tags";
-export * from "./tasks";
-export * from "./timeEntries";
+export { getMe, type Me } from "./me";
+export { getWorkspaces, type Workspace } from "./workspaces";
+export { getWorkspaceProjects, type Project } from "./projects";
+export { getWorkspaceClients, type Client } from "./clients";
+export { getWorkspaceTags, type Tag } from "./tags";
+export { getWorkspaceTasks, type Task } from "./tasks";
+export { getTimeEntries, createTimeEntry, stopTimeEntry, getRunningTimeEntry, type TimeEntry } from "./timeEntries";
