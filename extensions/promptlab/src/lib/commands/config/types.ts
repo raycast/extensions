1b77c563a99a79c5a40e630fdb79907c7c72e2c6
Stephen Kaplan <stephen.kaplan@maine.edu>
2024-01-09 18:45:20 -0500

    Refactor actions

--- extensions/promptlab/src/lib/commands/config/types.ts
+++ extensions/promptlab/src/lib/commands/config/types.ts
@@ -1,32 +1,33 @@
-type ConfigField = {
+
+interface ConfigField {
   name: string;
   description: string;
   guideText: string;
-};
+}
 
-export type StringConfigField = ConfigField & {
+export interface StringConfigField extends ConfigField {
   defaultValue: string;
   maxLength: string;
   minLength: string;
   regex: string;
   value?: string;
-};
+}
 
-export type BooleanConfigField = ConfigField & {
+export interface BooleanConfigField extends ConfigField {
   defaultValue: boolean;
   value?: boolean;
-};
+}
 
-export type NumberConfigField = ConfigField & {
+export interface NumberConfigField extends ConfigField {
   defaultValue: string;
   min: string;
   max: string;
   value?: string;
-};
+}
+
 /**
  * A PromptLab command setup configuration.
  */
-
 export type CommandConfig = {
   /**
    * The list of configuration fields.
