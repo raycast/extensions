0d99275d9b34e5cba88bab8d434817325f017d03
LitoMore <litomore@gmail.com>
2024-01-12 14:00:27 +0800

    Update simple-icons extension
    - Keep lockfile synced
    - Add more CDN selections

--- extensions/simple-icons/src/index.tsx
+++ extensions/simple-icons/src/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { ActionPanel, Action, Grid, Icon, Detail } from "@raycast/api";
 import * as simpleIcons from "simple-icons";
+import packageJson from "../package.json";
 
 export default function Command() {
   const [itemSize, setItemSize] = useState<Grid.ItemSize>(Grid.ItemSize.Small);
@@ -58,12 +59,26 @@ export default function Command() {
                       actions={
                         <ActionPanel>
                           <Action.CopyToClipboard title="Copy SVG" content={icon.svg} />
+                          <Action.CopyToClipboard title="Copy Color" content={icon.hex} />
+                          <Action.CopyToClipboard
+                            title="Copy Slug"
+                            content={icon.slug}
+                            shortcut={{ modifiers: ["opt"], key: "enter" }}
+                          />
                           <Action.CopyToClipboard
                             title="Copy CDN Link"
                             content={`https://cdn.simpleicons.org/${icon.slug}`}
+                            shortcut={{ modifiers: ["shift"], key: "enter" }}
                           />
-                          <Action.CopyToClipboard title="Copy Color" content={icon.hex} />
-                          <Action.CopyToClipboard title="Copy Slug" content={icon.slug} />
+                          <Action.CopyToClipboard
+                            title="Copy jsDelivr CDN Link"
+                            content={`https://cdn.jsdelivr.net/npm/simple-icons@${packageJson.dependencies["simple-icons"]}/icons/${icon.slug}.svg`}
+                          />
+                          <Action.CopyToClipboard
+                            // eslint-disable-next-line @raycast/prefer-title-case
+                            title="Copy unpkg CDN Link"
+                            content={`https://unpkg.com/simple-icons@${packageJson.dependencies["simple-icons"]}/icons/${icon.slug}.svg`}
+                          />
                         </ActionPanel>
                       }
                     />
