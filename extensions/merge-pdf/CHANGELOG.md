ac9c05a6b3fe123f1273c7d2f8e451b8ee5e1a01
Valentin de Brabandère <valentindebrabandere@gmail.com>
2024-01-16 14:06:16 +0100

    corrected for publishing on the store

--- extensions/merge-pdf/CHANGELOG.md
+++
@@ -1,9 +0,0 @@
-# Open in Visual Studio Code Changelog
-
-## [Update] - 2023-06-05
-
-- Allows you to open with Visual Studio Code Insiders
-
-## [Initial Version] - 2022-08-21
-
-- Open current Finder directory, selected folder or file in Visual Studio Code.
--- extensions/merge-pdf/src/index.ts
+++ extensions/merge-pdf/src/index.ts
@@ -1,27 +1,37 @@
 import { getSelectedFinderItems, showToast, Toast } from "@raycast/api";
-import { PDFDocument } from 'pdf-lib';
-import fs from 'fs';
-import { homedir } from 'os';
-import path from 'path';
+import { PDFDocument } from "pdf-lib";
+import fs from "fs";
+import { homedir } from "os";
+import path from "path";
 /**
  * Gets the selected Finder window.
  * @throws — An error when Finder is not the frontmost application.
  * @returns A Promise that resolves with the selected Finder window's path.
  */
 
-export default async  function Command(props: { arguments: { fileName:string, deleteOriginalFiles:string } }){
+export default async function Command(props: {
+  arguments: { fileName: string; deleteOriginalFiles: string };
+}) {
   const { fileName, deleteOriginalFiles } = props.arguments;
 
   try {
     const selectedFinderItems = await getSelectedFinderItems();
 
-    const pdfFiles = selectedFinderItems.filter(item => path.extname(item.path).toLowerCase() === '.pdf');
-    const nonPdfFiles = selectedFinderItems.filter((item) => path.extname(item.path).toLowerCase() !== '.pdf');
+    const pdfFiles = selectedFinderItems.filter(
+      (item) => path.extname(item.path).toLowerCase() === ".pdf",
+    );
+    const nonPdfFiles = selectedFinderItems.filter(
+      (item) => path.extname(item.path).toLowerCase() !== ".pdf",
+    );
     if (nonPdfFiles.length > 0) {
       await showToast({
         style: Toast.Style.Failure,
         title: "Error",
-        message: nonPdfFiles.length + " non-PDF files selected :" + nonPdfFiles.map(item => path.basename(item.path)).join(", ") + ". Please select only PDF files.",
+        message:
+          nonPdfFiles.length +
+          " non-PDF files selected :" +
+          nonPdfFiles.map((item) => path.basename(item.path)).join(", ") +
+          ". Please select only PDF files.",
       });
       return;
     }
@@ -34,12 +44,18 @@ export default async  function Command(props: { arguments: { fileName:string, de
       return;
     }
 
-    const mergedPdfBytes = await mergePdfs(pdfFiles.map(item => item.path));
-    const outputPath = getOutputPath(pdfFiles.map(item => item.path), deleteOriginalFiles, fileName);
+    const mergedPdfBytes = await mergePdfs(pdfFiles.map((item) => item.path));
+    const outputPath = getOutputPath(
+      pdfFiles.map((item) => item.path),
+      deleteOriginalFiles,
+      fileName,
+    );
     fs.writeFileSync(outputPath, mergedPdfBytes);
-    
-    await showToast({ title: "PDFs merged successfully!", message: `Saved to ${outputPath}` });
 
+    await showToast({
+      title: "PDFs merged successfully!",
+      message: `Saved to ${outputPath}`,
+    });
   } catch (error: unknown) {
     await showToast({
       style: Toast.Style.Failure,
@@ -47,7 +63,7 @@ export default async  function Command(props: { arguments: { fileName:string, de
       message: String(error),
     });
   }
-};
+}
 
 async function mergePdfs(filePaths: string[]): Promise<Uint8Array> {
   // Create a new PDFDocument
@@ -55,44 +71,48 @@ async function mergePdfs(filePaths: string[]): Promise<Uint8Array> {
 
   // Loop through all file paths and merge them into the new document
   for (const filePath of filePaths) {
-      const pdfBytes = fs.readFileSync(filePath);
-      const pdf = await PDFDocument.load(pdfBytes);
-      const copiedPages = await mergedPdf.copyPages(pdf, pdf.getPageIndices());
-      copiedPages.forEach((page) => mergedPdf.addPage(page));
+    const pdfBytes = fs.readFileSync(filePath);
+    const pdf = await PDFDocument.load(pdfBytes);
+    const copiedPages = await mergedPdf.copyPages(pdf, pdf.getPageIndices());
+    copiedPages.forEach((page) => mergedPdf.addPage(page));
   }
 
   // Serialize the PDFDocument to bytes
   return mergedPdf.save();
 }
 
-function getOutputPath(filePaths: string[], deleteOriginalFiles:string, fileName:string): string {
-  let finalFileName:string = 'merged';
-  
+function getOutputPath(
+  filePaths: string[],
+  deleteOriginalFiles: string,
+  fileName: string,
+): string {
+  let finalFileName = "merged";
+
   if (filePaths.length === 0) {
-      return path.join(homedir(), 'Downloads', finalFileName +'.pdf');
+    return path.join(homedir(), "Downloads", finalFileName + ".pdf");
   }
-  
-  
+
   if (fileName) {
     finalFileName = fileName;
   }
-  
+
   const firstFilePath = filePaths[0];
   const firstDirPath = path.dirname(firstFilePath);
-  
+
   // Check if all files are in the same directory
-  const sameDirectory = filePaths.every((filePath) => path.dirname(filePath) === firstDirPath);
-  
-  if (deleteOriginalFiles == '') {
+  const sameDirectory = filePaths.every(
+    (filePath) => path.dirname(filePath) === firstDirPath,
+  );
+
+  if (deleteOriginalFiles == "") {
     // Delete original files
     for (const filePath of filePaths) {
       fs.unlinkSync(filePath);
     }
   }
   if (sameDirectory) {
-    return path.join(firstDirPath, finalFileName+'.pdf');
+    return path.join(firstDirPath, finalFileName + ".pdf");
   } else {
-    return path.join(homedir(), 'Downloads', finalFileName + '.pdf');
-  
+    return path.join(homedir(), "Downloads", finalFileName + ".pdf");
   }
 }
