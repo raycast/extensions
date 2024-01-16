dcd77b6cf98a197e679fdb1564cfa21de38742bc
Felix Jen <fjen@felixjen.com>
2024-01-16 11:27:54 -0600

    Update extensions/convert-3d-models/src/utilities/preferences.ts
    Co-authored-by: Per Nielsen Tikær <per@raycast.com>

--- extensions/convert-3d-models/src/utilities/preferences.ts
+++ extensions/convert-3d-models/src/utilities/preferences.ts
@@ -11,18 +11,6 @@
 /**
  * Preferences for the extension as a whole.
  */
-export interface ExtensionPreferences {
-  /**
-   * The source of input models -- either "Clipboard" or the name of a file manager (e.g. "Finder" or "Path Finder").
-   */
-  inputMethod: string;
-  freeCADPath: string;
-
-  /**
-   * The strategy for handling the result of the model processing, i.e. where to save or display the result. One of {@link ModelResultHandling}.
-   */
-  modelResultHandling: string;
-}
 
 /**
  * Preferences for the convert command. Specifies which model formats to show in the conversion formats list.
