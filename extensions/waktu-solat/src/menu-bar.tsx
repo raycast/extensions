deceab5283002a9d08eaf2cb9d74c5f810957a4c
msms <me@msms.work>
2024-01-16 21:45:24 +0800

    - Fix some issue and logic on menubar - Monotone icon in menubar

--- extensions/waktu-solat/src/menu-bar.tsx
+++ extensions/waktu-solat/src/menu-bar.tsx
@@ -1,17 +1,18 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 import {
-  getPreferenceValues,
+  getPreferenceValues, Icon,
   launchCommand,
   LaunchType,
   LocalStorage,
   MenuBarExtra,
-  openExtensionPreferences,
+  openExtensionPreferences
 } from "@raycast/api";
 import { useEffect, useState } from "react";
 import { loadTodaySolat, PrayerTime } from "./lib/prayer-times";
 import { now } from "moment/moment";
 
+// noinspection JSUnusedGlobalSymbols
 export default function Command() {
   const { menuTemplate, afterOffset, beforeOffset, showIcon }: Preferences = getPreferenceValues();
   const [isLoading, setLoading] = useState(true);
@@ -28,6 +29,7 @@ export default function Command() {
   useEffect(() => {
     onLoad();
   }, []);
+
   const current = prayerTime?.items?.find((p) => p.isCurrent);
   const nextPrayer = prayerTime?.items?.find((p) => p.isNext);
   const nextDiff = Math.abs(nextPrayer?.time.diff(now(), "minutes", true) || 0);
@@ -39,51 +41,56 @@ export default function Command() {
       ? current
       : null;
   const title = menuTemplate?.replace("$name", menuPrayer?.label).replace("$time", menuPrayer?.value);
-  const icon = showIcon ? "🕌 " : "";
+  // const icon = showIcon ? "🕌 " : "";
+  const _title = (!isLoading || undefined) && menuPrayer && `${title}`;
+  const shouldHide = !_title && !showIcon;
+  const upcomingPrayers = prayerTime?.items?.filter((p) => p.time.isAfter(now()));
+  const pastPrayers = prayerTime?.items?.filter((p) => p.time.isBefore(current.time));
   return (
     <MenuBarExtra
-      // icon={"🕌"}
-      title={(!isLoading || undefined) && menuPrayer && `${icon}${title}`}
+      icon={(!_title && showIcon && "mosque01.svg") || undefined}
+      title={_title}
       tooltip={`${current?.label} since ${current?.value}, Next: ${nextPrayer?.label} at ${nextPrayer?.value}`}
-      isLoading={isLoading}
+      isLoading={isLoading || shouldHide}
     >
       <MenuBarExtra.Section title={`Current [${zoneId}]`}>
         <MenuBarExtra.Item
-          icon={"🕌"}
+          // icon={"mosque-color.svg"}
+          icon={Icon.CircleProgress50}
           key={current?.label}
           title={`${current?.label}: `}
           subtitle={`${current?.value}`}
           onAction={() => launchCommand({ name: "index", type: LaunchType.UserInitiated })}
         />
       </MenuBarExtra.Section>
-      <MenuBarExtra.Section title="Upcoming">
-        {prayerTime?.items
-          ?.filter((p) => p.time.isAfter(now()))
-          .map((item) => (
+      {upcomingPrayers?.length > 0 && (
+        <MenuBarExtra.Section title="Upcoming">
+          {upcomingPrayers?.map((item) => (
             <MenuBarExtra.Item
-              icon={"🕌"}
+              icon={Icon.Circle}
               key={item.label}
               title={`${item.label}: `}
               subtitle={`${item.value}`}
               onAction={() => launchCommand({ name: "index", type: LaunchType.UserInitiated })}
             />
           ))}
-      </MenuBarExtra.Section>
-      <MenuBarExtra.Section title="Past">
-        {prayerTime?.items
-          ?.filter((p) => p.time.isBefore(current.time))
-          .map((item) => (
+        </MenuBarExtra.Section>
+      )}
+      {pastPrayers?.length > 0 && (
+        <MenuBarExtra.Section title="Past">
+          {pastPrayers?.map((item) => (
             <MenuBarExtra.Item
-              icon={"🕌"}
+              icon={Icon.CircleProgress100}
               key={item.label}
               title={`${item.label}: `}
               subtitle={`${item.value}`}
               onAction={() => launchCommand({ name: "index", type: LaunchType.UserInitiated })}
             />
           ))}
-      </MenuBarExtra.Section>
+        </MenuBarExtra.Section>
+      )}
       <MenuBarExtra.Section>
-        <MenuBarExtra.Item icon="⚙️" title="Settings" onAction={() => openExtensionPreferences()} />
+        <MenuBarExtra.Item icon={Icon.Cog} title="Settings" onAction={() => openExtensionPreferences()} />
       </MenuBarExtra.Section>
     </MenuBarExtra>
   );
