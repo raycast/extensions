129bd39dac00d219a2afa42a187733bb0ef843e0
kevin-pek <76090938+kevin-pek@users.noreply.github.com>
2024-01-18 20:43:21 +0800

    Clean old files

--- extensions/pdfsearch/compile-model.sh
+++
@@ -1,4 +0,0 @@
-xcrun coremlcompiler compile EmbeddingModel.mlpackage Sources/DocumentIndexer/Resources --platform macos --deployment-target 12.0
-
-# generate Swift class
-xcrun coremlcompiler generate --language Swift EmbeddingModel.mlpackage Sources/DocumentIndexer --platform macos --deployment-target 12.0
