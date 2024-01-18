31e2bb62682b35059131f253963931e78cfe3864
jaredliu <jaredliu233@gmail.com>
2024-01-18 11:24:08 +0800

    chore: add docs action

--- extensions/tidyread---streamline-your-daily-reading/README.md
+++ extensions/tidyread---streamline-your-daily-reading/README.md
@@ -1,6 +1,6 @@
 # TidyRead - Streamline Your Daily Reading
 
-You can schedule your daily reading, and through a digest powered by AI, we provide you with an efficient and tidy news reading experience.
+You can schedule your daily reading, and through a digest powered by AI, we provide you with an efficient and tidy reading experience.
 
 For more information, visit [Tidyread](https://tidyread.info).
 
--- extensions/tidyread---streamline-your-daily-reading/src/components/CommonActions.tsx
+++ extensions/tidyread---streamline-your-daily-reading/src/components/CommonActions.tsx
@@ -3,6 +3,7 @@ import { Action, Icon, openExtensionPreferences } from "@raycast/api";
 export default function CommonActions() {
   return (
     <>
+      <Action.OpenInBrowser icon={Icon.Document} title="View Docs" url="https://tidyread.info/docs" />
       <Action.OpenInBrowser icon={"twitter_logo_white.svg"} title="DM On X" url="https://x.com/jaredliu_bravo" />
       <Action icon={Icon.Gear} title="Open Extension Preferences" onAction={openExtensionPreferences} />
       <Action.OpenInBrowser icon={Icon.Message} title="Feature Request" url="https://tally.so/r/w8x6dr" />
