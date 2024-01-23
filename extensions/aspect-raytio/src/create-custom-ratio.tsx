dd9327030d5d818caaed8b9fb9851b067a22448c
Henrique Macedo <hey@ickas.dev>
2024-01-23 23:53:16 +0000

    feat: use `useForm` to handle validation form

--- extensions/aspect-raytio/src/create-custom-ratio.tsx
+++ extensions/aspect-raytio/src/create-custom-ratio.tsx
@@ -1,72 +1,52 @@
-import { useState } from "react";
 import { Action, ActionPanel, Form, useNavigation } from "@raycast/api";
+import { useForm } from "@raycast/utils";
 import { useCreateCustomRatio } from "../hooks/useCustomRatios";
 import { RatioType } from ".";
+
+interface CustomRatioFormValues {
+  widthField: string;
+  heightField: string;
+}
 
 export default function CreateCustomRatio(props: { totalCustomRatios: number; onCreate: (ar: RatioType) => void }) {
   const { totalCustomRatios, onCreate } = props;
   const { pop } = useNavigation();
-  const [widthError, setWidthError] = useState<string | undefined>();
-  const [heightError, setHeightError] = useState<string | undefined>();
 
-  function dropWidthError() {
-    if (widthError && widthError.length > 0) {
-      setWidthError(undefined);
-    }
-  }
-
-  function dropHeightError() {
-    if (heightError && heightError.length > 0) {
-      setHeightError(undefined);
-    }
-  }
+  const { handleSubmit, itemProps } = useForm<CustomRatioFormValues>({
+    onSubmit: (values) => {
+      useCreateCustomRatio(Number(values.widthField), Number(values.heightField));
+      onCreate({
+        key: `${totalCustomRatios + 1}`,
+        width: Number(values.widthField),
+        height: Number(values.heightField),
+      });
+      pop();
+    },
+    validation: {
+      widthField: (value) => {
+        if (!value || isNaN(Number(value))) {
+          return "The field should be a number!";
+        }
+      },
+      heightField: (value) => {
+        if (!value || isNaN(Number(value))) {
+          return "The field should be a number!";
+        }
+      },
+    },
+  });
 
   return (
     <Form
       navigationTitle="Create Custom Ratio"
       actions={
         <ActionPanel>
-          <Action.SubmitForm
-            title="Create"
-            onSubmit={(values) => {
-              useCreateCustomRatio(values.widthField, values.heightField);
-              onCreate({
-                key: `${totalCustomRatios + 1}`,
-                width: Number(values.widthField),
-                height: Number(values.heightField),
-              });
-              pop();
-            }}
-          />
+          <Action.SubmitForm title="Create" onSubmit={handleSubmit} />
         </ActionPanel>
       }
     >
-      <Form.TextField
-        id="widthField"
-        placeholder="16"
-        error={widthError}
-        onChange={dropWidthError}
-        onBlur={(event) => {
-          if (!event.target.value || isNaN(Number(event.target.value))) {
-            setWidthError("The field should be a number!");
-          } else {
-            dropWidthError();
-          }
-        }}
-      />
-      <Form.TextField
-        id="heightField"
-        placeholder="9"
-        error={heightError}
-        onChange={dropHeightError}
-        onBlur={(event) => {
-          if (!event.target.value || isNaN(Number(event.target.value))) {
-            setHeightError("The field should be a number!");
-          } else {
-            dropHeightError();
-          }
-        }}
-      />
+      <Form.TextField placeholder="16" {...itemProps.widthField} />
+      <Form.TextField placeholder="9" {...itemProps.heightField} />
     </Form>
   );
 }
