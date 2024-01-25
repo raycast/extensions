1f2568252a41391ec7539d1ca8a8ec9acaaf990e
Ullrich Schäfer <hi+github@ullrich.is>
2024-01-25 14:14:02 +0100

    Types for GIPHY API response

--- extensions/is-ullrich-raycast-giphy/TODOs.md
+++ extensions/is-ullrich-raycast-giphy/TODOs.md
@@ -3,7 +3,10 @@
 Things that I didn't get to yet:
 
 - [ ] Error handling, lol
+- [ ] Pagination (via `@giphy/js-fetch-api` ?)
 - [ ] Use AbortController to cancel running fetch requests before starting a new one
+- [X] Typescript types for API responses
+  - Note: could use the `@giphy/js-fetch-api` package, but it's quite a heavy dependency, does not explicitly require `node-fetch` and also does not support AbortController (Giphy/giphy-js#284)
 - [x] An empty view that gives GIPHY some more attribution and love
   - [x] Also need "Powered by GIPHY" attribution to get a production API Key
 - [ ] Configurable settings, i.e. for filtering differently rated GIFs
--- extensions/is-ullrich-raycast-giphy/src/index.tsx
+++ extensions/is-ullrich-raycast-giphy/src/index.tsx
@@ -1,45 +1,62 @@
 import { Action, ActionPanel, Icon, Grid } from "@raycast/api";
 
+import type { IGif, IProfileUser } from "@giphy/js-types";
 import { useState, useEffect } from "react";
 import fetch from "node-fetch";
+
+// Could be used via `@giphy/js-fetch-api`, but I'm not bringing in the entire depency just for this type alone.
+// https://github.com/Giphy/giphy-js/blob/c413823339234acda693d90ba87cc7fc4c4e9a29/packages/fetch-api/src/result-types.ts#L26-L28
+interface GifSearchResult {
+  data: IGif[];
+}
 
 // It's easy to get a Giphy API key: https://developers.giphy.com/docs/api#quick-start-guide
 // Get your own and don't steal mine, please.
 const giphyAPIKey = "Cw3z0ImWiQZsmi6ZbPwzmfkmkhiMolqZ";
 
-const GifItem = (item) => (
-  <Grid.Item
-    key={item.id}
-    content={{ source: item.images.preview_gif.url }}
-    title={item.title}
-    actions={
-      <ActionPanel title="Giphy GIF search">
-        <Action.Paste content={item.images.original.url} />
-        <Action.OpenInBrowser title="View on GIPHY" url={item.url} icon={"command-icon.png"} />
-        {item.user && (
-          <Action.OpenInBrowser
-            icon={{ source: Icon.Crown }}
-            title={`More by @${item.user.display_name}`}
-            url={item.user.profile_url}
-          />
-        )}
-        <Action.CopyToClipboard title="Copy URL" content={item.images.original.url} />
-      </ActionPanel>
-    }
-  />
-);
+const GifItem = (item: IGif) => {
+  const user = item.user as IProfileUser | undefined;
 
-const EmptyView = () => (
+  return (
+    <Grid.Item
+      key={item.id}
+      content={{ source: item.images.preview_gif.url }}
+      title={item.title}
+      actions={
+        <ActionPanel title="Giphy GIF search">
+          <Action.Paste content={item.images.original.url} />
+          <Action.OpenInBrowser title="View on GIPHY" url={item.url} icon={"command-icon.png"} />
+          {user && (
+            <Action.OpenInBrowser
+              icon={{ source: Icon.Crown }}
+              title={`More by @${user.display_name}`}
+              url={user.profile_url}
+            />
+          )}
+          <Action.CopyToClipboard title="Copy URL" content={item.images.original.url} />
+        </ActionPanel>
+      }
+    />
+  );
+};
+
+const EmptyView = ({ search, isLoading }: { search: string; isLoading: boolean }) => (
   <Grid.EmptyView
     title="GIF search powered by GIPHY"
-    message="Search for something that has results"
+    description={
+      search.length === 0
+        ? "Start typing your search and results will follow."
+        : isLoading
+          ? `Searching for "${search}"...`
+          : `No results for "${search}". Try something else.`
+    }
     icon={"giphy-logo.png"}
   />
 );
 
 const Command = () => {
   const [search, setSearch] = useState("");
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState([] as IGif[]);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -51,9 +68,9 @@ const Command = () => {
     // TODO: Use AbortController to cancel previous requests before starting a new one
     //       Also make sure to keep the loading state intact
     fetch(`https://api.giphy.com/v1/gifs/search?q=${search}&api_key=${giphyAPIKey}`)
-      .then((response) => response.json())
-      .then((data) => {
-        setItems(data.data);
+      .then((response) => response.json() as Promise<GifSearchResult>)
+      .then(({ data }) => {
+        setItems(data);
       })
       .finally(() => {
         setLoading(false);
@@ -71,7 +88,7 @@ const Command = () => {
       isLoading={loading}
       navigationTitle="Giphy Search"
     >
-      {items.length === 0 ? <EmptyView /> : items.map(GifItem)}
+      {items.length === 0 ? <EmptyView search={search} isLoading={loading} /> : items.map(GifItem)}
     </Grid>
   );
 };
