8dbbb8491141b0b11059c3414df20d28e522c460
Ben Keys <bkeys818@gmail.com>
2024-01-25 15:04:20 -0500

    🚧 Add missing API call methods for tags

--- extensions/toggl-track/src/api/index.ts
+++ extensions/toggl-track/src/api/index.ts
@@ -3,6 +3,6 @@ export { getMyOrganizations, type Organization } from "./organizations";
 export { getMyWorkspaces, type Workspace } from "./workspaces";
 export { getMyProjects, type Project } from "./projects";
 export { getMyClients, type Client } from "./clients";
-export { getMyTags, type Tag } from "./tags";
+export { getMyTags, createTag, updateTag, deleteTag, type Tag } from "./tags";
 export { getMyTasks, type Task } from "./tasks";
 export { getMyTimeEntries, createTimeEntry, stopTimeEntry, getRunningTimeEntry, type TimeEntry } from "./timeEntries";
