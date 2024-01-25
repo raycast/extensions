75833f91da4cab42ee4df70d5b8a6f08713d0db7
Thomas Lombart <lombart.thomas@icloud.com>
2024-01-25 14:18:43 +0100

    Messages: Create extension (#9828)
    * Messages: Create extension
    
    * Delete .vscode/launch.json

---
+++ extensions/messages/swift/contacts/Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "contacts",
+    platforms: [.macOS(.v12)],
+    dependencies: [
+        .package(url: "https://github.com/raycast/raycast-extension-macro", from: "0.1.0")
+    ],
+    targets: [
+        .executableTarget(
+            name: "contacts",
+            dependencies: [
+                .product(name: "RaycastExtensionMacro", package: "raycast-extension-macro"),
+            ]
+        ),
+    ]
+)
