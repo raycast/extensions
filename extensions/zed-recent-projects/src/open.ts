54ce95fd2291b570297255773cfb60a3058a3d4b
Evgenii Khramkov <hey@ewgenius.me>
2024-01-26 22:18:25 +0900

    Update zed-recent-projects extension
    - Open With Zed - support opening current finder folder if nothing selected
    - Initial commit

--- extensions/zed-recent-projects/src/open.ts
+++ extensions/zed-recent-projects/src/open.ts
@@ -1,22 +1,48 @@
 import { closeMainWindow, getSelectedFinderItems, open, showToast, Toast } from "@raycast/api";
+import { runAppleScript } from "@raycast/utils";
 import { saveZedEntries } from "./lib/zedEntries";
 import { ZED_BUNDLE_ID } from "./lib/zed";
 
+const getCurrentFinderPathScript = `
+try
+  tell application "Finder"
+    return POSIX path of (insertion location as alias)
+  end tell
+on error
+  return ""
+end try
+`;
+
+export const getCurrentFinderPath = async () => {
+  return await runAppleScript(getCurrentFinderPathScript);
+};
+
 export default async function openWithZed() {
   try {
+    let selectedItems: { path: string }[] = [];
+
     const finderItems = await getSelectedFinderItems();
     if (finderItems.length === 0) {
-      throw new Error("No Finder item selected");
+      console.log("Test");
+      const currentPath = await getCurrentFinderPath();
+      console.log(currentPath);
+      if (currentPath) {
+        selectedItems = [{ path: currentPath }];
+      } else {
+        throw new Error("No Finder item selected");
+      }
+    } else {
+      selectedItems = finderItems.map((i) => ({ path: i.path }));
     }
 
     await saveZedEntries(
-      finderItems.map((finderItem) => ({
-        uri: `file://${finderItem.path.endsWith("/") ? finderItem.path.slice(0, -1) : finderItem.path}`,
+      selectedItems.map((item) => ({
+        uri: `file://${item.path}`,
         lastOpened: Date.now(),
       }))
     );
 
-    for (const finderItem of finderItems) {
+    for (const finderItem of selectedItems) {
       await open(finderItem.path, ZED_BUNDLE_ID);
     }
 
