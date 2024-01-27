1c5ed2a52428450fc6ffe6b30ace9eb8724785f5
yannglt <helloyannglt@gmail.com>
2024-01-27 21:13:00 +0100

    Add new fetch util to browse device

--- extensions/fathom-analytics-stats/src/browse-devices.tsx
+++ extensions/fathom-analytics-stats/src/browse-devices.tsx
@@ -1,50 +1,59 @@
-import { getPreferenceValues, List, Icon } from "@raycast/api";
-import { useFetch } from "@raycast/utils";
+import { List, Icon, Detail, showToast, Toast } from "@raycast/api";
 import { useState } from "react";
+import FathomRequest from "./utils/api";
 import PeriodDropdown from "./components/PeriodDropdown";
+import { Device } from "./types/Device";
 
 export default function Command() {
-  const preferences = getPreferenceValues<Preferences>();
   const [dateFrom, setDateFrom] = useState<string>("");
 
-  const { data, isLoading } = useFetch<Data>(
-    `https://api.usefathom.com/v1/aggregations?entity_id=${
-      preferences.siteId
-    }&entity=pageview&aggregates=pageviews&field_grouping=device_type&sort_by=pageviews:desc${
-      dateFrom ? `&date_from=${dateFrom}` : ""
-    }`,
-    {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${preferences.apiToken}`,
-      },
-    },
-  );
+  const { data, isLoading, error } = FathomRequest({
+    endpoint: "/aggregations",
+    entity: "pageview",
+    aggregates: "pageviews",
+    groupBy: "device_type",
+    sortBy: "pageviews:desc",
+    dateFrom: dateFrom,
+  }) as {
+    data: Device[] | undefined;
+    isLoading: boolean;
+    error: { title: string; message: string; markdown: string } | undefined;
+  };
 
-  const totalPageviews = data?.reduce((total, page) => total + parseInt(page.pageviews), 0) || 0;
+  if (data) {
+    const totalPageviews = data?.reduce((total, page) => total + parseInt(page.pageviews), 0) || 0;
 
-  return (
-    <List
-      isLoading={isLoading}
-      navigationTitle="Choose a time period"
-      searchBarPlaceholder="Search devices"
-      searchBarAccessory={<PeriodDropdown setDateFrom={setDateFrom} />}
-    >
-      {data?.map((device) => {
-        const relativePageviews = ((parseInt(device.pageviews) / totalPageviews) * 100).toFixed(1);
-        return (
-          <List.Item
-            key={device.device_type}
-            title={device.device_type}
-            accessories={[
-              {
-                text: `${device.pageviews.toLocaleString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} (${relativePageviews}%)`,
-              },
-              { icon: Icon.TwoPeople },
-            ]}
-          />
-        );
-      })}
-    </List>
-  );
+    return (
+      <List
+        isLoading={isLoading}
+        navigationTitle="Choose a time period"
+        searchBarPlaceholder="Search devices"
+        searchBarAccessory={<PeriodDropdown setDateFrom={setDateFrom} />}
+      >
+        {data?.map((device) => {
+          const relativePageviews = ((parseInt(device.pageviews) / totalPageviews) * 100).toFixed(1);
+          return (
+            <List.Item
+              key={device.device_type}
+              title={device.device_type}
+              accessories={[
+                {
+                  text: `${device.pageviews.toLocaleString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} (${relativePageviews}%)`,
+                },
+                { icon: Icon.TwoPeople },
+              ]}
+            />
+          );
+        })}
+      </List>
+    );
+  } else if (error) {
+    showToast({
+      style: Toast.Style.Failure,
+      title: error.title,
+      message: error.message,
+    });
+
+    return <Detail markdown={error.markdown} />;
+  }
 }
