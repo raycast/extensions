5bda4a2288908cfd64ac2c8a235f594dbc67c3ad
Ivan Frolov <frolvanya@gmail.com>
2024-01-27 23:28:26 -0500

    fix: linting

--- extensions/ai-humanizer/src/index.tsx
+++ extensions/ai-humanizer/src/index.tsx
@@ -4,49 +4,49 @@ import { useState } from "react";
 import axios from "axios";
 
 interface Values {
-    originalText: string;
-    humanizedText: string;
+  originalText: string;
+  humanizedText: string;
 }
 
 export default function Command() {
-    const [humanizedText, setHumanizedText] = useState<string>("");
-    const [loading, setLoading] = useState<boolean>(false);
+  const [humanizedText, setHumanizedText] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
 
-    async function humanize(text: string): Promise<string | undefined> {
-        try {
-            const url = "https://aitohumanconverter.com/v3/process.php";
-            const data = `text=${text}`;
+  async function humanize(text: string): Promise<string | undefined> {
+    try {
+      const url = "https://aitohumanconverter.com/v3/process.php";
+      const data = `text=${text}`;
 
-            const response = await axios.post(url, data);
+      const response = await axios.post(url, data);
 
-            showToast({ title: "Text Humanized", message: "Text successfully humanized!" });
+      showToast({ title: "Text Humanized", message: "Text successfully humanized!" });
 
-            return response.data.data;
-        } catch (error) {
-            showToast({ title: "Error", message: "Failed to humanize text" });
-        }
+      return response.data.data;
+    } catch (error) {
+      showToast({ title: "Error", message: "Failed to humanize text" });
     }
+  }
 
-    const handleSubmit = async (values: Values) => {
-        console.log(values);
-        setLoading(true);
-        setHumanizedText((await humanize(values.originalText)) || "");
-        setLoading(false);
-    };
+  const handleSubmit = async (values: Values) => {
+    console.log(values);
+    setLoading(true);
+    setHumanizedText((await humanize(values.originalText)) || "");
+    setLoading(false);
+  };
 
-    return (
-        <Form
-            isLoading={loading}
-            actions={
-                <ActionPanel>
-                    <Action.SubmitForm title="Humanize" onSubmit={handleSubmit} />
-                    <Action.CopyToClipboard content={humanizedText} />
-                </ActionPanel>
-            }
-        >
-            <Form.TextArea autoFocus={true} id="originalText" title="Original text" />
-            <Form.Separator />
-            <Form.TextArea id="humanizedText" title="Humanized text" value={humanizedText} />
-        </Form>
-    );
+  return (
+    <Form
+      isLoading={loading}
+      actions={
+        <ActionPanel>
+          <Action.SubmitForm title="Humanize" onSubmit={handleSubmit} />
+          <Action.CopyToClipboard content={humanizedText} />
+        </ActionPanel>
+      }
+    >
+      <Form.TextArea autoFocus={true} id="originalText" title="Original text" />
+      <Form.Separator />
+      <Form.TextArea id="humanizedText" title="Humanized text" value={humanizedText} />
+    </Form>
+  );
 }
