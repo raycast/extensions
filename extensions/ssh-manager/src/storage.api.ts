86751577b85a8d32a13c263f80dc6de5df32bdfa
Alessandro Benassi <plasmax@gmail.com>
2024-01-29 10:52:08 +0100

    Update storage.api.ts

--- extensions/ssh-manager/src/storage.api.ts
+++ extensions/ssh-manager/src/storage.api.ts
@@ -25,7 +25,6 @@ function parseSSHConfig(configFilePath: string): ISSHConnection[] {
       }
       currentConnection = { id: connections.length.toString(), address: "", name: trimmedLine.substring(5), user: "" };
     } else if (currentConnection !== null) {
-
       const firstSpaceIndex = trimmedLine.indexOf(" ");
       const key = trimmedLine.substring(0, firstSpaceIndex);
       const value = trimmedLine.substring(firstSpaceIndex + 1);
