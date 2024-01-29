c5cd189944d9dc6cadcebca9b48877918123b2ee
Lanre Adelowo <yo@lanre.wtf>
2024-01-29 01:00:25 +0100

    Update dub extension
    - run npx ray lint and fix
    - use title case

--- extensions/dub/src/create.tsx
+++ extensions/dub/src/create.tsx
@@ -1,73 +1,79 @@
-import { Clipboard, Detail, LaunchProps, PopToRootType, Toast, getPreferenceValues, popToRoot, showHUD, showToast } from "@raycast/api";
+import {
+  Clipboard,
+  Detail,
+  LaunchProps,
+  PopToRootType,
+  Toast,
+  getPreferenceValues,
+  popToRoot,
+  showHUD,
+  showToast,
+} from "@raycast/api";
 import axios from "axios";
 import { useEffect } from "react";
 
 type ShortenedURLResponse = {
-  shortLink: string
-}
+  shortLink: string;
+};
 
-export default function CreateShortURL(
-  props: LaunchProps<{ arguments: { url: string; } }>,
-): JSX.Element {
-
-  const { projectSlug, baseURL, apiKey } = getPreferenceValues()
+export default function CreateShortURL(props: LaunchProps<{ arguments: { url: string } }>): JSX.Element {
+  const { projectSlug, baseURL, apiKey } = getPreferenceValues();
 
   const getURL = () => {
-    return `${baseURL}/links?projectSlug=${projectSlug}`
-  }
+    return `${baseURL}/links?projectSlug=${projectSlug}`;
+  };
 
   const shortenURL = () => {
-
     try {
-
       // just here to validate the url really
-      new URL(props.arguments.url)
+      new URL(props.arguments.url);
 
+      axios
+        .post<ShortenedURLResponse>(
+          getURL(),
+          {
+            url: props.arguments.url,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${apiKey}`,
+            },
+          },
+        )
+        .then((res) => {
+          Clipboard.copy(res.data.shortLink);
 
-      axios.post<ShortenedURLResponse>(getURL(), {
-        url: props.arguments.url,
-      }, {
-        headers: {
-          Authorization: `Bearer ${apiKey}`
-        }
-      }).then(res => {
-
-        Clipboard.copy(res.data.shortLink)
-
-        showHUD("Copied URL to your clipboard", {
-          clearRootSearch: true,
-          popToRootType: PopToRootType.Immediate,
+          showHUD("Copied URL to your clipboard", {
+            clearRootSearch: true,
+            popToRootType: PopToRootType.Immediate,
+          });
         })
-
-      }).catch(err => {
-        showToast({
-          style: Toast.Style.Failure,
-          title: "An error occurred while generating short URL",
-          message: err,
-        }).then(() => {
-          setTimeout(() => {
-            popToRoot({ clearSearchBar: false })
-          }, 1000)
-        })
-      })
+        .catch((err) => {
+          showToast({
+            style: Toast.Style.Failure,
+            title: "An error occurred while generating short URL",
+            message: err,
+          }).then(() => {
+            setTimeout(() => {
+              popToRoot({ clearSearchBar: false });
+            }, 1000);
+          });
+        });
     } catch (err) {
-
       showToast({
         style: Toast.Style.Failure,
         title: "Please provide a valid URL instead",
       }).then(() => {
         setTimeout(() => {
-          popToRoot({ clearSearchBar: false })
-        }, 1000)
-      })
+          popToRoot({ clearSearchBar: false });
+        }, 1000);
+      });
     }
-
-  }
+  };
 
   useEffect(() => {
-    shortenURL()
-  }, [])
+    shortenURL();
+  }, []);
 
-
-  return <Detail isLoading={true} />
+  return <Detail isLoading={true} />;
 }
