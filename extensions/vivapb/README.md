ce9222a524f7be959c3b8853758d856d04745bd2
codrkoaz <codrkoazie@gmail.com>
2024-01-30 00:25:38 -0500

    Update vivapb extension
    - Updated Changelog, fixed code and cleaned up unnecessary lines of code
    - Pull contributions
    - Updated Changelog, fixed code and cleaned up unnecessary lines of code
    - Updated Changelog, fixed code and cleaned up unnecessary lines of code
    - Update README.md

--- extensions/vivapb/README.md
+++ extensions/vivapb/README.md
@@ -2,6 +2,7 @@
 
 VivaPB is a Vivaldi browser that opens up a incognito window with ease in mind. Must have Vivaldi Installed for it to work.
 
-![Screenshot 1](https://github.com/codrkoaz/VivaPB/assets/51058620/e7430199-c200-4977-9020-ce692582ef98)
-![Screenshot 3](https://github.com/codrkoaz/VivaPB/assets/51058620/f193d371-1fc5-4937-be8f-e5f482f2b401)
-![Screenshot 2](https://github.com/codrkoaz/VivaPB/assets/51058620/202bdb0e-7be5-4a67-9c4d-194095edb92b)
+
+![Metadata-1](https://github.com/codrkoaz/VivaPB/assets/51058620/24ddb38a-11c0-44bb-9f69-05695b740133)
+![Metadata-2](https://github.com/codrkoaz/VivaPB/assets/51058620/9df06fff-7b48-41e0-b68c-4c0561b5396d)
+![Metadata-3](https://github.com/codrkoaz/VivaPB/assets/51058620/bba16b86-6007-41bd-a2c4-59958d081138)
--- extensions/vivapb/src/vivapb.tsx
+++ extensions/vivapb/src/vivapb.tsx
@@ -1,13 +1,46 @@
+import { getApplications, showToast, Toast } from "@raycast/api";
 import { exec } from "child_process";
 
-exec('open -na "Vivaldi" --args -incognito "duckduckgo.com"', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Error: ${error.message}`);
-    return;
+async function isVivaldiInstalled(): Promise<boolean> {
+  const applications = await getApplications();
+  return applications.some((app) => app.bundleId === "com.vivaldi.Vivaldi");
+}
+
+async function openVivaldiIncognito(): Promise<void> {
+  try {
+    await exec(' open -na "Vivaldi" --args -incognito "https://www.duckduckgo.com"');
+  } catch (error) {
+    console.error(`Error: Not working`);
+
+    showOpenVivaldiErrorToast();
   }
-  if (stderr) {
-    console.error(`Error: ${stderr}`);
-    return;
+}
+
+function showVivaldiNotInstalledErrorToast(): void {
+  const options: Toast.Options = {
+    style: Toast.Style.Failure,
+    title: "Vivaldi is not installed",
+    message: "Please install Vivaldi from https://vivaldi.com",
+  };
+  showToast(options);
+}
+
+function showOpenVivaldiErrorToast(): void {
+  const options: Toast.Options = {
+    style: Toast.Style.Failure,
+    title: "Failed to open Vivaldi incognito",
+    message: "Please ensure that Vivaldi is installed and try again.",
+  };
+  showToast(options);
+}
+
+export async function openVivaldiIncognitoIfInstalled(): Promise<void> {
+  const vivaldiInstalled = await isVivaldiInstalled();
+  if (vivaldiInstalled) {
+    await openVivaldiIncognito();
+  } else {
+    showVivaldiNotInstalledErrorToast();
   }
-  console.log(`Command executed successfully: ${stdout}`);
-});
+}
+
+openVivaldiIncognitoIfInstalled();
