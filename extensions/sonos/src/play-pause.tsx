8512ddbaaa01597ec1f0c59e5d0dba6475b59d8f
Anton Niklasson <niklasson.anton@gmail.com>
2024-01-31 20:36:12 +0100

    avoid multiple intervalled commands, play/pause is now the driver

--- extensions/sonos/src/play-pause.tsx
+++ extensions/sonos/src/play-pause.tsx
@@ -1,26 +1,38 @@
-import { LaunchProps, LaunchType, launchCommand, updateCommandMetadata } from "@raycast/api";
+import { LaunchProps, LaunchType, launchCommand, showHUD, updateCommandMetadata } from "@raycast/api";
 import { formatPlayingState, getLatestState, getActiveCoordinator } from "./core/sonos";
 
 export default async function Command({ launchType }: LaunchProps) {
   const state = await getLatestState();
   const coordinator = await getActiveCoordinator();
+  const userInitiated = launchType === LaunchType.UserInitiated;
 
-  if (coordinator === undefined) {
+  if (!coordinator && userInitiated) {
+    await showHUD("Failed to resolve coordinator, choose an explicit group first");
+
     await launchCommand({
       name: "set-group",
       type: LaunchType.UserInitiated,
     });
   } else {
+    if (coordinator && userInitiated) {
+      await coordinator.TogglePlayback();
+    }
+
     const title = await formatPlayingState(state);
 
     if (title !== null) {
       updateCommandMetadata({
         subtitle: title,
       });
-    }
 
-    if (launchType === LaunchType.UserInitiated) {
-      await coordinator.TogglePlayback();
+      try {
+        await launchCommand({
+          name: "now-playing",
+          type: LaunchType.Background,
+        });
+      } catch (error) {
+        // This means the menu bar command hasn't been loaded yet. We just ignore that here.
+      }
     }
   }
 }
