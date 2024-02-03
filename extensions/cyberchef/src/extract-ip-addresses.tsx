712908d1e40e51501223f42155a6cd2dca205c3b
Matthew Blode <m@blode.co>
2024-02-03 13:22:46 +1100

    Fix

--- extensions/cyberchef/src/extract-ip-addresses.tsx
+++ extensions/cyberchef/src/extract-ip-addresses.tsx
@@ -1,4 +1,4 @@
 import { Container } from "./container";
 export default function Command() {
-return <Container recipe="Extract_IP_addresses(true,false,false,false,false,false)" />;
+  return <Container recipe="Extract_IP_addresses(true,false,false,false,false,false)" />;
 }
--- extensions/cyberchef/src/pseudo.tsx
+++ extensions/cyberchef/src/pseudo.tsx
@@ -1,4 +1,4 @@
 import { Container } from "./container";
 export default function Command() {
-return <Container recipe="Pseudo-Random_Number_Generator(32,'Hex')" />;
+  return <Container recipe="Pseudo-Random_Number_Generator(32,'Hex')" />;
 }
