5476ff2d6fea9f422e64cf88e38f8d11fe9ea40a
Ben Keys <bkeys818@gmail.com>
2024-02-04 13:33:14 -0500

    Create `Manage Clients` command (bkeys818/raycast-extensions#17)
    * ♻️ Refactor ClientList into ClientListItem
    
    * ✨ Add workspaces field to ClientForm
    
    * ✨ Create new `Manage Clients` command

---
+++ extensions/toggl-track/src/components/ClientListItem.tsx
@@ -0,0 +1,119 @@
+import { List, ActionPanel, Action, Icon, confirmAlert, Alert } from "@raycast/api";
+import { Workspace, Client, deleteClient, archiveClient, restoreClient } from "../api";
+import ClientForm from "./ClientForm";
+import { withToast, Verb } from "../helpers/withToast";
+
+interface ClientListItemProps {
+  workspace: Workspace;
+  client: Client;
+  revalidateClients: () => void;
+  SharedActions: React.ReactNode;
+}
+
+export default function ClientListItem({ workspace, client, revalidateClients, SharedActions }: ClientListItemProps) {
+  const canModifyClients =
+    !workspace.only_admins_may_create_projects || workspace.role == "admin" || workspace.role == "projectlead";
+
+  return (
+    <List.Item
+      title={client.name}
+      key={client.id}
+      accessories={client.archived ? [{ tag: "archived", icon: Icon.Tray }] : undefined}
+      actions={
+        <ActionPanel>
+          {canModifyClients && (
+            <ActionPanel.Section>
+              <Action.Push
+                title="Rename Tag"
+                icon={Icon.Pencil}
+                shortcut={{ key: "e", modifiers: ["cmd", "shift"] }}
+                target={<ClientForm {...{ client, revalidateClients }} />}
+              />
+              <Action
+                title="Delete Tag"
+                icon={Icon.Trash}
+                shortcut={{ key: "x", modifiers: ["ctrl"] }}
+                style={Action.Style.Destructive}
+                onAction={async () => {
+                  if (
+                    await confirmAlert({
+                      title: "Delete Client",
+                      primaryAction: { title: "Delete", style: Alert.ActionStyle.Destructive },
+                      message:
+                        "Deleting a client will permanently remove the client from all associated time entries and projects.",
+                    })
+                  )
+                    await withToast({
+                      noun: "Client",
+                      verb: Verb.Delete,
+                      message: client.name,
+                      action: async () => {
+                        await deleteClient(client.wid, client.id);
+                        revalidateClients();
+                      },
+                    });
+                }}
+              />
+              {client.archived ? (
+                <ActionPanel.Submenu title="Restore Client..." icon={Icon.Undo}>
+                  <Action
+                    title={`Restore Client and Projects`}
+                    onAction={() =>
+                      withToast({
+                        noun: "Client",
+                        verb: Verb.Restore,
+                        message: client.name,
+                        action: async () => {
+                          await restoreClient(client.wid, client.id, true);
+                          revalidateClients();
+                        },
+                      })
+                    }
+                  />
+                  <Action
+                    title={`Restore Only Client`}
+                    onAction={() =>
+                      withToast({
+                        noun: "Client",
+                        verb: Verb.Restore,
+                        message: client.name,
+                        action: async () => {
+                          await restoreClient(client.wid, client.id, false);
+                          revalidateClients();
+                        },
+                      })
+                    }
+                  />
+                </ActionPanel.Submenu>
+              ) : (
+                <Action
+                  title={`Archive Client`}
+                  icon={Icon.Tray}
+                  onAction={async () => {
+                    if (
+                      await confirmAlert({
+                        title: "Archive Client",
+                        primaryAction: { title: "Archive" },
+                        message: "Any projects associated with this client will also be archived.",
+                      })
+                    )
+                      await withToast({
+                        noun: "Client",
+                        verb: Verb.Archive,
+                        message: client.name,
+                        action: async () => {
+                          await archiveClient(client.wid, client.id);
+                          revalidateClients();
+                        },
+                      });
+                  }}
+                />
+              )}
+            </ActionPanel.Section>
+          )}
+          {SharedActions}
+        </ActionPanel>
+      }
+    />
+  );
+}
