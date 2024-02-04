95e0ab8f290bb700676bd82318dad7f15d2b486a
Abiel Zulio M <7030944+abielzulio@users.noreply.github.com>
2024-02-04 15:13:28 +0700

    refine version list

--- extensions/klu-ai/src/views/action/action-version-list.tsx
+++ extensions/klu-ai/src/views/action/action-version-list.tsx
@@ -1,11 +1,11 @@
 import klu from "@/libs/klu";
-import { Icon, List } from "@raycast/api";
+import { Action, ActionPanel, Color, Icon, List } from "@raycast/api";
 import { useCachedPromise } from "@raycast/utils";
 import { intlFormatDistance } from "date-fns";
 import { ActionViewDropdown, ActionViewState } from "./action-view";
 
 const ActionVersionList = ({ guid, onChange }: { guid: string; onChange: (value: ActionViewState) => void }) => {
-  const { data, isLoading } = useCachedPromise(
+  const { data, isLoading, revalidate } = useCachedPromise(
     async (actionGuid: string) => {
       const data = await klu.actions.getVersions(actionGuid);
       return data;
@@ -29,15 +29,35 @@ const ActionVersionList = ({ guid, onChange }: { guid: string; onChange: (value:
         <List.Item
           key={a.guid}
           id={a.guid}
-          title={a.environment ?? ""}
-          subtitle={a.guid}
+          title={a.guid}
           accessories={[
+            {
+              tag: {
+                value: a.environment,
+                color:
+                  a.environment === "Production"
+                    ? Color.Green
+                    : a.environment === "Staging"
+                      ? Color.Orange
+                      : Color.Yellow,
+              },
+            },
             {
               icon: Icon.Clock,
               text: intlFormatDistance(new Date(a.updatedAt), new Date()),
               tooltip: "Last updated",
             },
           ]}
+          actions={
+            <ActionPanel title={a.guid}>
+              <ActionPanel.Section title="Copy">
+                <Action.CopyToClipboard title="Copy ID" content={a.guid ?? ""} />
+              </ActionPanel.Section>
+              <ActionPanel.Section title="Miscellaneous">
+                <Action icon={Icon.RotateClockwise} title="Refresh" onAction={() => revalidate()} />
+              </ActionPanel.Section>
+            </ActionPanel>
+          }
         />
       ))}
     </List>
