ccb1cad0a640b8f4ef9af77906bbc7a621c3a1cb
Anirudh Ganwal <anirudhganwal06@gmail.com>
2024-02-04 13:15:26 +0530

    Update bitwarden extension
    - Merge branch \'contributions/merge-1707032425977035000\'
    - Pull contributions
    - Refactors create folder & changes its icon

--- extensions/bitwarden/src/create-folder.tsx
+++ extensions/bitwarden/src/create-folder.tsx
@@ -1,8 +1,8 @@
-import { Form, Action, showHUD, ActionPanel, showToast, Toast } from "@raycast/api";
+import { Form, Action, showHUD, ActionPanel, showToast, Toast, popToRoot } from "@raycast/api";
 import { BitwardenProvider, useBitwarden } from "~/context/bitwarden";
 import RootErrorBoundary from "~/components/RootErrorBoundary";
-import { useState } from "react";
-import { SessionProvider } from "~/context/session";
+import { SessionProvider } from "./context/session";
+import { FormValidation, useForm } from "@raycast/utils";
 
 const CreateFolderCommand = () => (
   <RootErrorBoundary>
@@ -21,54 +21,33 @@ interface FormData {
 function CreateFolderComponent() {
   const bitwarden = useBitwarden();
 
-  const [nameError, setNameError] = useState<string | undefined>();
-
-  const dropNameErrorIfNeeded = () => {
-    if (nameError && nameError.length > 0) {
-      setNameError(undefined);
-    }
-  };
-
-  const validateName = (name: string | undefined) => {
-    if (name?.length === 0) {
-      setNameError("Name should't be empty!");
-      return false;
-    } else {
-      dropNameErrorIfNeeded();
-      return true;
-    }
-  };
-
-  const handleCreateFolder = async (formData: FormData) => {
-    if (!validateName(formData.name)) return;
-    const toast = await showToast({ title: "Creating Folder", style: Toast.Style.Animated });
-    try {
-      const { error } = await bitwarden.createFolder(formData.name);
-      if (error) throw error;
-      await showHUD("Folder Created");
-    } catch (error) {
-      toast.title = "Failed to create folder";
-      toast.style = Toast.Style.Failure;
-    }
-  };
+  const { handleSubmit, itemProps } = useForm<FormData>({
+    onSubmit: async (formData) => {
+      const toast = await showToast({ title: "Creating Folder", style: Toast.Style.Animated });
+      try {
+        const { error } = await bitwarden.createFolder(formData.name);
+        if (error) throw error;
+        await showHUD(`${formData.name} folder created`);
+        await popToRoot();
+      } catch (error) {
+        toast.title = "Failed to create folder";
+        toast.style = Toast.Style.Failure;
+      }
+    },
+    validation: {
+      name: FormValidation.Required,
+    },
+  });
 
   return (
     <Form
       actions={
         <ActionPanel>
-          <Action.SubmitForm title="Create Folder" onSubmit={handleCreateFolder} />
+          <Action.SubmitForm title="Create Folder" onSubmit={handleSubmit} />
         </ActionPanel>
       }
     >
-      <Form.TextField
-        id="name"
-        title="Folder Name"
-        placeholder="eg: Personal, Work"
-        autoFocus
-        error={nameError}
-        onChange={dropNameErrorIfNeeded}
-        onBlur={(event) => validateName(event.target.value)}
-      />
+      <Form.TextField title="Folder Name" placeholder="eg: Personal, Work" autoFocus={true} {...itemProps.name} />
     </Form>
   );
 }
