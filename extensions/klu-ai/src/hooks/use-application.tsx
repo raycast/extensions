b35f11ac72a1fd50ee6a573848549a5d88dd59d0
Abiel Zulio M <7030944+abielzulio@users.noreply.github.com>
2024-02-04 15:13:13 +0700

    use unique key

--- extensions/klu-ai/src/hooks/use-application.tsx
+++ extensions/klu-ai/src/hooks/use-application.tsx
@@ -3,7 +3,10 @@ import { environment } from "@raycast/api";
 import { useCachedState } from "@raycast/utils";
 
 export const useSelectedApplication = () => {
-  const [selectedApp, setSelectedApp] = useCachedState<PersistedApp | undefined>(environment.extensionName, undefined);
+  const [selectedApp, setSelectedApp] = useCachedState<PersistedApp | undefined>(
+    `${environment.extensionName}-selected-app`,
+    undefined,
+  );
 
   return { selectedApp, setSelectedApp };
 };
