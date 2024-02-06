0924006c200efd0b7a7964453dd063b1eb819a7d
Per Nielsen Tikær <per@raycast.com>
2024-02-06 12:40:40 +0100

    Update

--- extensions/youtube-subscriber-count/src/index.tsx
+++ extensions/youtube-subscriber-count/src/index.tsx
@@ -1,16 +1,15 @@
-import { Icon, MenuBarExtra, open, LocalStorage } from "@raycast/api";
+import { Icon, MenuBarExtra, open, LocalStorage, Color, openExtensionPreferences } from "@raycast/api";
 import { useFetch } from "@raycast/utils";
 import { getPreferenceValues } from "@raycast/api";
-import { ActionPanel, Action, Detail, openExtensionPreferences } from "@raycast/api";
 
-type Item = {
-  statistics: {
-    subscriberCount: string;
-  };
-};
-
-type Data = {
-  items: Item[];
+type Items = {
+  items: [
+    {
+      statistics: {
+        subscriberCount: string;
+      };
+    },
+  ];
 };
 
 export default function Command() {
@@ -18,62 +17,63 @@ export default function Command() {
   const youtubeApiKey = preferences.youtubeApiKey;
   const youtubeChannelId = preferences.youtubeChannelId;
   const celebrationThreshold = preferences.celebrationThreshold;
+  const localeFormat = preferences.localeFormat;
 
-  if (isNaN(celebrationThreshold)) {
-    const markdown =
-      "Celebration subscribers threshold is not a number. Please update it in extension preferences and try again.";
-
-    return (
-      <Detail
-        markdown={markdown}
-        actions={
-          <ActionPanel>
-            <Action title="Open Extension Preferences" onAction={openExtensionPreferences} />
-          </ActionPanel>
-        }
-      />
-    );
-  }
-
-  if (youtubeApiKey === undefined || youtubeChannelId === undefined) {
-    const markdown = "API key incorrect. Please update it in extension preferences and try again.";
-
-    return (
-      <Detail
-        markdown={markdown}
-        actions={
-          <ActionPanel>
-            <Action title="Open Extension Preferences" onAction={openExtensionPreferences} />
-          </ActionPanel>
-        }
-      />
-    );
-  }
-  const { data, isLoading } = useFetch<Data>(
+  const { data, isLoading, error } = useFetch(
     `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${youtubeChannelId}&key=${youtubeApiKey}`,
+    {
+      async parseResponse(response) {
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+
+        const data = (await response.json()) as Items;
+
+        if (data.items) {
+          const currentCount = Number(data.items[0].statistics.subscriberCount);
+          const countStr = localeFormat ? Number(currentCount).toLocaleString() : currentCount;
+
+          if (parseInt(celebrationThreshold) > 0) {
+            LocalStorage.getItem<number>("celebrated-at-count").then((celebratedAtCount) => {
+              const diff = currentCount - (celebratedAtCount ?? 0);
+              if (diff <= Number(celebrationThreshold)) {
+                return;
+              }
+
+              open("raycast://extensions/raycast/raycast/confetti");
+              LocalStorage.setItem("celebrated-at-count", currentCount);
+            });
+          }
+
+          return countStr;
+        } else {
+          throw new Error("Check Credentials");
+        }
+      },
+    },
   );
-  if (!data || data.items.length === 0) {
-    return <MenuBarExtra isLoading={isLoading} />;
-  }
-  const currentCount = Number(data.items[0].statistics.subscriberCount);
-  const countStr = Number(currentCount).toLocaleString();
-  if (celebrationThreshold != 0) {
-    LocalStorage.getItem<number>("celebrated-at-count").then((celebratedAtCount) => {
-      const diff = currentCount - (celebratedAtCount ?? 0);
-      if (diff < celebrationThreshold) {
-        console.log("Threshold not reached yet");
-        return;
-      }
-      open("raycast://extensions/raycast/raycast/confetti");
-      LocalStorage.setItem("celebrated-at-count", currentCount);
-    });
-  }
+
   return (
-    <MenuBarExtra icon={Icon.TwoPeople} title={countStr} isLoading={isLoading}>
-      <MenuBarExtra.Item
-        title="Open in YouTube Studio"
-        onAction={() => open(`https://studio.youtube.com/channel/${youtubeChannelId}`)}
-      />
+    <MenuBarExtra
+      icon={{ source: Icon.TwoPeople, tintColor: error ? Color.Red : Color.PrimaryText }}
+      title={error ? "Check Credentials" : data?.toString()}
+      isLoading={isLoading}
+    >
+      {!error && (
+        <MenuBarExtra.Item
+          title="Open in YouTube Studio"
+          icon={Icon.Globe}
+          onAction={() => open(`https://studio.youtube.com/channel/${youtubeChannelId}`)}
+        />
+      )}
+      {error && (
+        <MenuBarExtra.Item
+          title="Open Extension Preferences"
+          shortcut={{ modifiers: ["cmd", "shift"], key: "," }}
+          icon={Icon.Gear}
+          onAction={openExtensionPreferences}
+        />
+      )}
     </MenuBarExtra>
   );
 }
