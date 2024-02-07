2068d5557027a48ce86a9771d193476ae30bf112
maximilianzuern <max.zuern@gmail.com>
2024-02-07 18:10:30 +0100

    updated summarize command

--- extensions/perplexity-api/src/summarize.jsx
+++ extensions/perplexity-api/src/summarize.jsx
@@ -3,6 +3,6 @@ import usePerplexity from "./api/perplexity";
 export default function Summarize(props) {
   return usePerplexity(props, {
     context:
-      "Extract all facts from the following text and summarize it in all relevant aspects. Start with a 1-liner summary. Pick a good machting emoji for every bullet point",
+      "Extract all facts from the following text and summarize it in all relevant aspects. Start with a 1-liner summary. Start every bullet point with a good machting emoji",
   });
 }
