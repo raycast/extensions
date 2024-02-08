a78f09446904fcb4402e97b482be73efe7934bcc
Ann <songya@autoai.com>
2024-02-08 16:47:41 +0800

    fix: warning and Icon

--- extensions/paste-from-apple-books/CHANGELOG.md
+++ extensions/paste-from-apple-books/CHANGELOG.md
@@ -1,3 +1,10 @@
 # Paste From Apple Books Changelog
 
+## [Pass CI Check] - 2024-02-07
+
+- change code rm warnings
+- change Icon to need size
+
 ## [Initial Version] - 2024-02-07
+
+- Initial Version
--- extensions/paste-from-apple-books/src/index.tsx
+++ extensions/paste-from-apple-books/src/index.tsx
@@ -1,15 +1,14 @@
-import { Clipboard, Detail, PopToRootType, closeMainWindow, popToRoot, showHUD } from "@raycast/api";
-import { setTimeout } from "timers/promises";
+import { Clipboard, popToRoot } from "@raycast/api";
 
 export default function Command() {
   (async () => {
     const text = await Clipboard.readText() || "";
-    let languages = ['Excerpt From', '摘录来自'];
-    let re = new RegExp('“([\\s\\S]+)”\\n\\n' + `(${languages.join("|")})`);
+    const languages = ['Excerpt From', '摘录来自'];
+    const re = new RegExp('“([\\s\\S]+)”\\n\\n' + `(${languages.join("|")})`);
     console.log(re);
-    let groups = text.match(re);
+    const groups = text.match(re);
     if (groups) {
-      let content = groups[1];
+      const content = groups[1];
       Clipboard.paste(content);
     } else {
       Clipboard.paste(text);
