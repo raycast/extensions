0c2353db58209ba2e8925666e1b20ff6c9e10ed2
cyxn <nick.sukhov@eze.tech>
2024-02-10 21:14:19 +0100

    Add query-chatgpt extension
    - improve README.md
    - add screenshots
    - improve feedback from running command
    - remove unnecessary check
    - add logo and fill in changelog
    - add explanation of the extension to README.md
    - code cleanup, improve labels and placeholders
    - improve sanitise function; remove console logs
    - improve extension
    - first working e2e version
    - progress
    - template

---
+++ extensions/query-chatgpt/src/query-chatgpt-execute-custom-command.tsx
@@ -0,0 +1,15 @@
+import { Clipboard, Application, getPreferenceValues } from "@raycast/api";
+import { ExecuteCustomCommand } from "./types";
+import { openBrowserTab } from "./run-applescript";
+
+export default async function main(props: ExecuteCustomCommand) {
+  const browser = getPreferenceValues<{ browser: Application }>().browser;
+  await openBrowserTab({
+    browserName: browser.name,
+    prompt: props.arguments.prompt,
+    gptUrl: props.arguments.gptUrl,
+    query: props.arguments.query,
+  });
+  await Clipboard.copy(JSON.stringify(props));
+  return null;
+}
