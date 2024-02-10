2ce510e822d28dc6a2155641147ddb785c6ff967
Ben Keys <bkeys818@gmail.com>
2024-02-10 17:33:23 -0500

    🛂 Only show `Archive` and `Restore` actions on appropriate clients.
    Clients in workspaces with without a premium subscription can't be archive.

--- extensions/toggl-track/src/components/ClientListItem.tsx
+++ extensions/toggl-track/src/components/ClientListItem.tsx
@@ -53,62 +53,63 @@ export default function ClientListItem({ workspace, client, revalidateClients, S
                     });
                 }}
               />
-              {client.archived ? (
-                <ActionPanel.Submenu title="Restore Client..." icon={Icon.Undo} shortcut={Shortcut.Archive}>
+              {workspace.business_ws &&
+                (client.archived ? (
+                  <ActionPanel.Submenu title="Restore Client..." icon={Icon.Undo} shortcut={Shortcut.Archive}>
+                    <Action
+                      title={`Restore Client and Projects`}
+                      onAction={() =>
+                        withToast({
+                          noun: "Client",
+                          verb: Verb.Restore,
+                          message: client.name,
+                          action: async () => {
+                            await restoreClient(client.wid, client.id, true);
+                            revalidateClients();
+                          },
+                        })
+                      }
+                    />
+                    <Action
+                      title={`Restore Only Client`}
+                      onAction={() =>
+                        withToast({
+                          noun: "Client",
+                          verb: Verb.Restore,
+                          message: client.name,
+                          action: async () => {
+                            await restoreClient(client.wid, client.id, false);
+                            revalidateClients();
+                          },
+                        })
+                      }
+                    />
+                  </ActionPanel.Submenu>
+                ) : (
                   <Action
-                    title={`Restore Client and Projects`}
-                    onAction={() =>
-                      withToast({
-                        noun: "Client",
-                        verb: Verb.Restore,
-                        message: client.name,
-                        action: async () => {
-                          await restoreClient(client.wid, client.id, true);
-                          revalidateClients();
-                        },
-                      })
-                    }
+                    title={`Archive Client`}
+                    icon={Icon.Tray}
+                    shortcut={Shortcut.Archive}
+                    onAction={async () => {
+                      if (
+                        await confirmAlert({
+                          title: "Archive Client",
+                          primaryAction: { title: "Archive" },
+                          message: "Any projects associated with this client will also be archived.",
+                        })
+                      )
+                        await withToast({
+                          noun: "Client",
+                          verb: Verb.Archive,
+                          message: client.name,
+                          action: async () => {
+                            await archiveClient(client.wid, client.id);
+                            revalidateClients();
+                          },
+                        });
+                    }}
                   />
-                  <Action
-                    title={`Restore Only Client`}
-                    onAction={() =>
-                      withToast({
-                        noun: "Client",
-                        verb: Verb.Restore,
-                        message: client.name,
-                        action: async () => {
-                          await restoreClient(client.wid, client.id, false);
-                          revalidateClients();
-                        },
-                      })
-                    }
-                  />
-                </ActionPanel.Submenu>
-              ) : (
-                <Action
-                  title={`Archive Client`}
-                  icon={Icon.Tray}
-                  shortcut={Shortcut.Archive}
-                  onAction={async () => {
-                    if (
-                      await confirmAlert({
-                        title: "Archive Client",
-                        primaryAction: { title: "Archive" },
-                        message: "Any projects associated with this client will also be archived.",
-                      })
-                    )
-                      await withToast({
-                        noun: "Client",
-                        verb: Verb.Archive,
-                        message: client.name,
-                        action: async () => {
-                          await archiveClient(client.wid, client.id);
-                          revalidateClients();
-                        },
-                      });
-                  }}
-                />
-              )}
+                ))}
             </ActionPanel.Section>
           )}
           {SharedActions}
