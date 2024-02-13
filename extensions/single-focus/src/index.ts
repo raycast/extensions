9ae3024f6f51f03be0585f2f08df947cd05079d2
Mehdi Merkachi <merkachimehdi@gmail.com>
2024-02-13 10:54:27 +0100

    Update single-focus extension
    - Merge branch \'contributions/merge-1707818036456606000\'
    - Pull contributions
    - Update command icon and add confirmation dialog

--- extensions/single-focus/src/index.ts
+++ extensions/single-focus/src/index.ts
@@ -1,10 +1,19 @@
-import { showHUD, getFrontmostApplication } from "@raycast/api";
+import { showHUD, getFrontmostApplication, Alert, confirmAlert } from "@raycast/api";
 import { quitButFront } from "swift:../QuitAllButFocused";
 
 export default async function main() {
   const frontMostApp = await getFrontmostApplication();
 
-  await quitButFront();
+  const options: Alert.Options = {
+    title: "Quit all apps but " + frontMostApp.name,
+    message: "You will not be able to undo this action.",
+    rememberUserChoice: true,
+  };
 
-  await showHUD("Closing all apps except: " + frontMostApp.name);
+  if (await confirmAlert(options)) {
+    await quitButFront();
+    await showHUD("Closing all apps except: " + frontMostApp.name);
+  } else {
+    await showHUD("Canceled");
+  }
 }
