4f53e57b1d3b696eac9aa10dd0c5e8943cb9d000
Mateus Junges <mateus@junges.dev>
2024-02-13 11:40:26 -0300

    Update git-co-authors extension
    - Allow to add multiple co-authors
    - Initial commit

--- extensions/git-co-authors/src/choose-coauthor.tsx
+++ extensions/git-co-authors/src/choose-coauthor.tsx
@@ -1,11 +1,13 @@
 import { Action, ActionPanel, Icon, List, showToast, Toast, confirmAlert, Color, Alert } from "@raycast/api";
 import { useEffect, useState } from "react";
 import AddOrEditAuthor from "./add-or-edit-author";
-import { Authors } from "./types";
+import {Authors} from "./types";
 import { cache, getAuthorsArrFromCache, KEY, removeAuthorFromCache } from "./utils";
 
 export default function ChooseAuthor() {
   const [authors, setAuthors] = useState<Authors>(getAuthorsArrFromCache());
+  const [selectedAuthors, setSelectedAuthors] = useState<Authors>([]);
+
   useEffect(() => {
     return cache.subscribe((key, data) => {
       if (key === KEY && data) {
@@ -15,45 +17,72 @@ export default function ChooseAuthor() {
   }, []);
 
   return (
-    <List searchBarPlaceholder="Dana Scully">
-      {authors.map((author) => (
-        <List.Item
-          title={author.name}
-          subtitle={author.email}
-          key={author.email}
-          actions={
-            <ActionPanel>
-              <Action.CopyToClipboard content={`Co-authored-by: ${author.name} <${author.email}>`} />
-              <Action.Push
-                title={`Edit ${author.name}`}
-                target={<AddOrEditAuthor author={author} />}
-                icon={Icon.Pencil}
-              />
-              <Action
-                title={`Remove ${author.name}`}
-                icon={Icon.RemovePerson}
-                style={Action.Style.Destructive}
-                shortcut={{ modifiers: ["cmd"], key: "backspace" }}
-                onAction={async () => {
-                  await confirmAlert({
-                    title: "Remove Author",
-                    message: `Are you sure you want to remove ${author.name}?`,
-                    icon: { source: Icon.RemovePerson, tintColor: Color.Red },
-                    primaryAction: {
-                      title: "Remove",
-                      style: Alert.ActionStyle.Destructive,
-                      onAction: () => {
-                        removeAuthorFromCache(author.email);
-                        showToast(Toast.Style.Success, `Removed ${author.name}`);
-                      },
-                    },
-                  });
-                }}
-              />
-            </ActionPanel>
-          }
-        />
-      ))}
-    </List>
+      <List searchBarPlaceholder="Dana Scully">
+        {authors.map((author) => (
+            <List.Item
+                title={author.name}
+                subtitle={author.email}
+                icon={selectedAuthors.filter(_author => _author.email === author.email).length == 1 ? Icon.Check : Icon.Minus}
+                key={author.email}
+
+                actions={
+                  <ActionPanel>
+                    <Action
+                        title={`Select ${author.name}`}
+                        icon={Icon.Check}
+                        onAction={async () => {
+                          // If the author is already selected and the user clicks this action, we need to
+                          // remove this author from the selected authors array:
+                          if (selectedAuthors.filter((_author) => _author.email === author.email).length == 1) {
+                            setSelectedAuthors([
+                              ...selectedAuthors.filter((_author) => _author.email !== author.email)
+                            ])
+
+                            await showToast(Toast.Style.Success, `Author ${author.name} unselected`);
+                            return;
+                          }
+
+                          setSelectedAuthors([
+                            ...selectedAuthors.filter((_author) => _author.email !== author.email),
+                            author
+                          ]);
+
+                          await showToast(Toast.Style.Success, `Author ${author.name} selected`);
+
+                        }}
+                    />
+
+                    <Action.CopyToClipboard content={selectedAuthors.map((selectedAuthor) => `Co-authored-by: ${selectedAuthor.name} <${selectedAuthor.email}>`).join("\n")} />
+                    <Action.Push
+                        title={`Edit ${author.name}`}
+                        target={<AddOrEditAuthor author={author}/>}
+                        icon={Icon.Pencil}
+                    />
+                    <Action
+                        title={`Remove ${author.name}`}
+                        icon={Icon.RemovePerson}
+                        style={Action.Style.Destructive}
+                        shortcut={{ modifiers: ["cmd"], key: "backspace" }}
+                        onAction={async () => {
+                          await confirmAlert({
+                            title: "Remove Author",
+                            message: `Are you sure you want to remove ${author.name}?`,
+                            icon: { source: Icon.RemovePerson, tintColor: Color.Red },
+                            primaryAction: {
+                              title: "Remove",
+                              style: Alert.ActionStyle.Destructive,
+                              onAction: () => {
+                                removeAuthorFromCache(author.email);
+                                showToast(Toast.Style.Success, `Removed ${author.name}`);
+                              },
+                            },
+                          });
+                        }}
+                    />
+                  </ActionPanel>
+                }
+            />
+        ))}
+      </List>
   );
 }
