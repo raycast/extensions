afd39ec3bd26859c3cfb6ecf62c6272ebd0c718f
maximilianzuern <max.zuern@gmail.com>
2024-02-13 15:48:21 +0100

    added models to list

--- extensions/perplexity-api/src/hook/utils.ts
+++ extensions/perplexity-api/src/hook/utils.ts
@@ -2,9 +2,12 @@ import { encode } from "@nem035/gpt-3-encoder";
 
 export const allModels = [
   { name: "Follow global model", id: "global" },
-  { name: "Mistral 8x7b Instruct", id: "mixtral-8x7b-instruct" },
+  { name: "Mixtral 8x7b Instruct", id: "mixtral-8x7b-instruct" },
+  { name: "Mistral 7B Instruct", id: "mistral-7b-instruct" },
   { name: "Llama2 70B Chat", id: "llama-2-70b-chat" },
+  { name: "Perplexity 7B Chat", id: "pplx-7b-chat" },
   { name: "Perplexity 70B Chat", id: "pplx-70b-chat" },
+  { name: "CodeLlama 34B Instruct", id: "codellama-34b-instruct" },
   { name: "CodeLlama 70B Instruct", id: "codellama-70b-instruct" },
   { name: "Perplexity 7B Online", id: "pplx-7b-online" },
   { name: "Perplexity 70B Online", id: "pplx-70b-online" },
@@ -20,7 +23,7 @@ export const currentDate = new Date().toLocaleString("en-US", {
 export type ResultViewProps = {
   sys_prompt: string;
   selected_text?: string; // If defined, uses this as selected text
-  user_extra_msg?: string; // If not empty, appends this to the user message
+  user_extra_msg?: string; // Textfield in Form -> If not empty, appends this to the user message
   model_override: string;
   toast_title: string;
   temperature?: number;
