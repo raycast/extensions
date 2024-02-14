a590cd2efe4b3c6c391aa54385236acfc8a3a2f3
Maximilian <85677120+maximilianzuern@users.noreply.github.com>
2024-02-14 09:45:12 +0100

    [Perplexity API] New Features, Customization, and Streaming using OpenAI client (#10738)
    * reworked whole extension (openAI, typescript, etc.)
    
    * fixed ask commnand
    
    * Refactor getResult and fixed retry issue
    
    * cleaned code
    
    * updaded system prompt for online models, added query and fallback feature
    
    * updated README
    
    * added query, updated prompt
    
    * changed file name
    
    * added taskBreakdown
    
    * changed file name
    
    * changed CHANGELOG
    
    * Remove unused code, fixed temp.
    
    * changed file to academic, fixed manifest
    
    * added casual, changed file name
    
    * changed file structure, added shorter command
    
    * added coding command
    
    * changed shortcut keys
    
    * added synonyms command
    
    * added improve feature
    
    * added translate command
    
    * minor changes, rename functions
    
    * cleaned comments
    
    * added CHANGELOG
    
    * fixed README
    
    * updated screenshots
    
    * simplified prompt
    
    * added model dropdown and utils file
    
    * fixed filter issues, added date and type to utils
    
    * merged utils files
    
    * cleanup
    
    * lint and prettier
    
    * added models to list
    
    * fixed model names and improved prompts
    
    * changelog
    
    * fix Element issue
    
    * prettier
    
    * wrap ResultView in a fragment (<>...</>) to convert it to a ReactNode
    
    * moved types into new file
    
    * prettier
    
    * disabled types file
    
    * undo changes
    
    * ReactNode
    
    * updated packages
    
    * removed ReactNode
    
    * updated README
    
    * Update CHANGELOG.md and optimise images
    
    ---------
    
    Co-authored-by: raycastbot <bot@raycast.com>

--- extensions/perplexity-api/src/professional.jsx
+++
@@ -1,9 +0,0 @@
-import usePerplexity from "./api/perplexity";
-
-export default function Professional(props) {
-  return usePerplexity(props, {
-    context:
-      "Act as a professional content writer and editor. Rewrite the text to ensure: Professional tone of voice, formal language, accurate facts, concise phrasing, meaning unchanged, legth retained.",
-    allowPaste: true,
-  });
-}
