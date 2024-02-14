ee780dda2fcbd7d103cd64a46d6e85c2413c8af8
Pedro Duarte <oi@ped.ro>
2024-02-14 17:54:01 +0100

    Refactor

--- extensions/re-mind/src/view-re-mind.tsx
+++ extensions/re-mind/src/view-re-mind.tsx
@@ -10,22 +10,24 @@ type Bookmark = {
   url: string;
 };
 
-export default function ViewBookmarks() {
-  const { isLoading, data: api } = useFetch("https://re-mind.danilocampos.cc/api/remindapi");
+type Response = {
+  bookmarks: Bookmark[];
+};
 
-  const data = api as { bookmarks: Bookmark[] };
-  const [list] = React.useState(data.bookmarks);
+export default function ViewBookmarks() {
+  const { isLoading, data } = useFetch<Response>("https://re-mind.danilocampos.cc/api/remindapi");
+  const [selectedcategory, setSelectedCategory] = React.useState("All");
+
+  const list = data?.bookmarks || [];
 
   const categoriesList = [...new Set(list.map((item: Bookmark) => item.category))];
-
-  const [categories] = React.useState(categoriesList);
-  const [selectedcategory, setSelectedCategory] = React.useState("All");
 
   const sortedList = list.sort((a: Bookmark, b: Bookmark) =>
     a.title.trim().toLowerCase().localeCompare(b.title.trim().toLowerCase()),
   );
 
-  let filteredList: Bookmark[] = sortedList;
+  let filteredList = sortedList;
+
   if (selectedcategory !== "All") {
     filteredList = sortedList.filter((item: Bookmark) => item.category === selectedcategory);
   }
@@ -43,7 +45,7 @@ export default function ViewBookmarks() {
         >
           <Grid.Dropdown.Section title="Categories">
             <Grid.Dropdown.Item key="All" title="All" value="All" />
-            {categories.map((category) => (
+            {categoriesList.map((category) => (
               <Grid.Dropdown.Item key={category as string} title={category as string} value={category as string} />
             ))}
           </Grid.Dropdown.Section>
