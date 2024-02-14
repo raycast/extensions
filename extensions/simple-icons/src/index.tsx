97fc6d9abe4b3c541b89f37192a412c7c0711ffb
LitoMore <litomore@gmail.com>
2024-02-14 17:33:05 +0800

    Rewrite with CDN resources

--- extensions/simple-icons/src/index.tsx
+++ extensions/simple-icons/src/index.tsx
@@ -1,11 +1,48 @@
-import { useState } from "react";
-import { ActionPanel, Action, Grid, Icon, Detail, Color } from "@raycast/api";
-import * as simpleIcons from "simple-icons";
+import { useEffect, useState } from "react";
+import { ActionPanel, Action, Grid, Icon, Detail, Color, Clipboard, Cache, Toast, showToast } from "@raycast/api";
+import { titleToSlug } from "simple-icons/sdk";
+import { loadLatestVersion, loadJson, loadSvg } from "./utils";
+import { IconJson, IconData } from "./types";
 import packageJson from "../package.json";
 
 export default function Command() {
   const [itemSize, setItemSize] = useState<Grid.ItemSize>(Grid.ItemSize.Small);
   const [isLoading, setIsLoading] = useState(true);
+  const [version, setVersion] = useState("latest");
+  const [icons, setIcons] = useState<IconData[]>([]);
+
+  const cache = new Cache();
+
+  useEffect(() => {
+    (async () => {
+      await showToast({
+        style: Toast.Style.Animated,
+        title: "Loading Icons",
+      });
+      const version = await loadLatestVersion();
+      const cached = await cache.get(`json-${version}`);
+
+      const json: IconJson = cached ? JSON.parse(cached) : await loadJson(version);
+
+      if (!cached) {
+        cache.clear();
+        cache.set(`json-${version}`, JSON.stringify(json));
+      }
+
+      const icons = json.icons.map((icon) => ({
+        ...icon,
+        slug: icon.slug || titleToSlug(icon.title),
+      }));
+      setIsLoading(false);
+      setIcons(icons);
+      setVersion(version);
+      await showToast({
+        style: Toast.Style.Success,
+        title: "",
+        message: `Loaded ${icons.length} icons`,
+      });
+    })();
+  }, []);
 
   return (
     <Grid
@@ -18,7 +55,6 @@ export default function Command() {
           storeValue
           onChange={(newValue) => {
             setItemSize(newValue as Grid.ItemSize);
-            setIsLoading(false);
           }}
         >
           <Grid.Dropdown.Item title="Small" value={Grid.ItemSize.Small} />
@@ -28,73 +64,94 @@ export default function Command() {
       }
     >
       {!isLoading &&
-        Object.entries(simpleIcons).map(([name, icon]) => (
-          <Grid.Item
-            key={name}
-            content={{ value: { source: `icons/${icon.slug}.svg`, tintColor: Color.PrimaryText }, tooltip: name }}
-            title={icon.title}
-            actions={
-              <ActionPanel>
-                <Action.Push
-                  icon={Icon.Eye}
-                  title="See Detail"
-                  target={
-                    <Detail
-                      markdown={`<img src="icons/${icon.slug}.svg?raycast-width=325&raycast-height=325"/>`}
-                      navigationTitle={icon.title}
-                      metadata={
-                        <Detail.Metadata>
-                          <Detail.Metadata.Label title="Title" text={icon.title} />
-                          <Detail.Metadata.Label title="Slug" text={icon.slug} />
-                          <Detail.Metadata.TagList title="Brand color">
-                            <Detail.Metadata.TagList.Item text={icon.hex} color={`#${icon.hex}`} />
-                          </Detail.Metadata.TagList>
-                          <Detail.Metadata.Separator />
-                          <Detail.Metadata.Link title="Source" target={icon.source} text={icon.source} />
-                          {icon.guidelines && (
-                            <Detail.Metadata.Link title="Guidelines" target={icon.guidelines} text={icon.guidelines} />
-                          )}
-                          {icon.license && (
-                            <Detail.Metadata.Link
-                              title="License"
-                              target={icon.license.url}
-                              text={icon.license.type === "custom" ? icon.license.url : icon.license.type}
+        icons.map((icon) => {
+          const slug = icon.slug || titleToSlug(icon.title);
+
+          return (
+            <Grid.Item
+              key={slug}
+              content={{
+                value: {
+                  source: `https://cdn.jsdelivr.net/npm/simple-icons@${version}/icons/${slug}.svg`,
+                  tintColor: Color.PrimaryText,
+                },
+                tooltip: icon.title,
+              }}
+              title={icon.title}
+              actions={
+                <ActionPanel>
+                  <Action.Push
+                    icon={Icon.Eye}
+                    title="See Detail"
+                    target={
+                      <Detail
+                        markdown={`<img src="https://cdn.jsdelivr.net/npm/simple-icons@${version}/icons/${slug}.svg?raycast-width=235&raycast-height=235"  />`}
+                        navigationTitle={icon.title}
+                        metadata={
+                          <Detail.Metadata>
+                            <Detail.Metadata.Label title="Title" text={icon.title} />
+                            <Detail.Metadata.Label title="Slug" text={slug} />
+                            <Detail.Metadata.TagList title="Brand color">
+                              <Detail.Metadata.TagList.Item text={icon.hex} color={`#${icon.hex}`} />
+                            </Detail.Metadata.TagList>
+                            <Detail.Metadata.Separator />
+                            <Detail.Metadata.Link title="Source" target={icon.source} text={icon.source} />
+                            {icon.guidelines && (
+                              <Detail.Metadata.Link
+                                title="Guidelines"
+                                target={icon.guidelines}
+                                text={icon.guidelines}
+                              />
+                            )}
+                            {icon.license && (
+                              <Detail.Metadata.Link
+                                title="License"
+                                target={icon.license.url ?? `https://spdx.org/licenses/${icon.license.type}`}
+                                text={icon.license.url ? icon.license.url : icon.license.type}
+                              />
+                            )}
+                          </Detail.Metadata>
+                        }
+                        actions={
+                          <ActionPanel>
+                            <Action
+                              title="Copy SVG"
+                              onAction={async () => {
+                                const svg = await loadSvg(version, slug);
+                                Clipboard.copy(svg);
+                              }}
+                              icon={Icon.Clipboard}
                             />
-                          )}
-                        </Detail.Metadata>
-                      }
-                      actions={
-                        <ActionPanel>
-                          <Action.CopyToClipboard title="Copy SVG" content={icon.svg} />
-                          <Action.CopyToClipboard title="Copy Color" content={icon.hex} />
-                          <Action.CopyToClipboard
-                            title="Copy Slug"
-                            content={icon.slug}
-                            shortcut={{ modifiers: ["opt"], key: "enter" }}
-                          />
-                          <Action.CopyToClipboard
-                            title="Copy CDN Link"
-                            content={`https://cdn.simpleicons.org/${icon.slug}`}
-                            shortcut={{ modifiers: ["shift"], key: "enter" }}
-                          />
-                          <Action.CopyToClipboard
-                            title="Copy jsDelivr CDN Link"
-                            content={`https://cdn.jsdelivr.net/npm/simple-icons@${packageJson.dependencies["simple-icons"]}/icons/${icon.slug}.svg`}
-                          />
-                          <Action.CopyToClipboard
-                            // eslint-disable-next-line @raycast/prefer-title-case
-                            title="Copy unpkg CDN Link"
-                            content={`https://unpkg.com/simple-icons@${packageJson.dependencies["simple-icons"]}/icons/${icon.slug}.svg`}
-                          />
-                        </ActionPanel>
-                      }
-                    />
-                  }
-                />
-              </ActionPanel>
-            }
-          />
-        ))}
+                            <Action.CopyToClipboard title="Copy Color" content={icon.hex} />
+                            <Action.CopyToClipboard
+                              title="Copy Slug"
+                              content={slug}
+                              shortcut={{ modifiers: ["opt"], key: "enter" }}
+                            />
+                            <Action.CopyToClipboard
+                              title="Copy CDN Link"
+                              content={`https://cdn.simpleicons.org/${slug}`}
+                              shortcut={{ modifiers: ["shift"], key: "enter" }}
+                            />
+                            <Action.CopyToClipboard
+                              title="Copy jsDelivr CDN Link"
+                              content={`https://cdn.jsdelivr.net/npm/simple-icons@${packageJson.dependencies["simple-icons"]}/icons/${slug}.svg`}
+                            />
+                            <Action.CopyToClipboard
+                              // eslint-disable-next-line @raycast/prefer-title-case
+                              title="Copy unpkg CDN Link"
+                              content={`https://unpkg.com/simple-icons@${packageJson.dependencies["simple-icons"]}/icons/${slug}.svg`}
+                            />
+                          </ActionPanel>
+                        }
+                      />
+                    }
+                  />
+                </ActionPanel>
+              }
+            />
+          );
+        })}
     </Grid>
   );
 }
