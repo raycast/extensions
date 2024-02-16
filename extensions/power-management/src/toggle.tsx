528c8f88f0199059fb73f5a298f6f1bc7fcdfee2
erics118 <52634785+erics118@users.noreply.github.com>
2024-02-15 21:24:25 -0500

    del

--- extensions/power-management/src/toggle.tsx
+++
@@ -1,7 +0,0 @@
-import { PowerManagement } from "./PowerManagement";
-
-export default async function main() {
-  const powerManagement = new PowerManagement();
-
-  await powerManagement.toggleLowPowerMode();
-}
