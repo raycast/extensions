ea94a76ecc7f36a17e9a0281b0f4b8673a7de2cf
Ann <songya@autoai.com>
2024-02-19 10:29:31 +0800

    Update paste-from-apple-books extension
    - Merge branch \'contributions/merge-1708309755281686000\'
    - Pull contributions
    - change to no-view mode

--- extensions/paste-from-apple-books/CHANGELOG.md
+++ extensions/paste-from-apple-books/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Paste From Apple Books Changelog
 
+## [Pass CI Check] - 2024-02-19
+
+- change to no-view mode
+
 ## [Pass CI Check] - 2024-02-07
 
 - run Prettier with version 3.2.5
--- extensions/paste-from-apple-books/src/index.tsx
+++ extensions/paste-from-apple-books/src/index.tsx
@@ -1,11 +1,10 @@
 import { Clipboard, popToRoot } from "@raycast/api";
 
 export default function Command() {
-  (async () => {
+  return (async () => {
     const text = (await Clipboard.readText()) || "";
     const languages = ["Excerpt From", "摘录来自"];
     const re = new RegExp("“([\\s\\S]+)”\\n\\n" + `(${languages.join("|")})`);
-    console.log(re);
     const groups = text.match(re);
     if (groups) {
       const content = groups[1];
@@ -13,9 +12,5 @@ export default function Command() {
     } else {
       Clipboard.paste(text);
     }
-    popToRoot();
   })();
-  // popToRoot();
-  // closeMainWindow();
-  // return <Detail markdown= />;
 }
