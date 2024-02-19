44758259617b55e8a0ff7dbf501c3bc077886101
tleo19 <leotuil19@gmail.com>
2024-02-18 19:58:37 -0600

    Add new-note.ts file for opening a new note in Arc

---
+++ extensions/arc/src/new-note.ts
@@ -0,0 +1,31 @@
+import { showHUD } from "@raycast/api";
+import { runAppleScript } from "run-applescript";
+
+function runAppleScriptAction() {
+  return runAppleScript(`
+    tell application "Arc"
+	    activate
+    end tell
+
+     delay 1
+
+    tell application "System Events"
+	    tell process "Arc"
+          set frontmost to true
+      
+          keystroke "o" using {command down}
+          delay 1
+      
+          click menu item "New Note" of menu 1 of menu bar item "File" of menu bar 1
+	      end tell
+      end tell
+  `);
+}
+
+export default async function command() {
+  try {
+    await runAppleScriptAction();
+  } catch {
+    await showHUD("❌ Failed opening a new note");
+  }
+}
