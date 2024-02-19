2217418f9fa8366894cc2b7cbbff9634d30e03be
Pedro Duarte <oi@ped.ro>
2024-02-19 11:51:56 +0100

    Merge

--- extensions/bmrks/src/search-bookmarks.tsx
+++ extensions/bmrks/src/search-bookmarks.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { User } from "@supabase/supabase-js";
-import { Action, ActionPanel, Detail, Icon, List, openExtensionPreferences } from "@raycast/api";
+import { Action, ActionPanel, Detail, Icon, Image, List, openExtensionPreferences } from "@raycast/api";
 import { useAuth } from "../lib/use-auth";
 import { useGroups } from "../lib/use-groups";
 import { useBookmarks } from "../lib/use-bookmarks";
@@ -31,7 +31,8 @@ export function SearchBookmarks({ user }: { user: User }) {
       {bookmarks.length > 0 &&
         bookmarks.map((bookmark) => {
           const hostname = bookmark.type === "link" ? getHostname(bookmark.url || "") : "";
-          let icon;
+          let icon: Image.ImageLike = Icon.Bookmark;
+
           if (bookmark.favicon_url) {
             icon = bookmark.favicon_url;
           }
