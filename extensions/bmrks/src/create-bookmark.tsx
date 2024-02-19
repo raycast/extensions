bf7c44857cf8ec625a67ed92dcd5b80168b5e3a3
Per Nielsen Tikær <per@raycast.com>
2024-02-19 13:12:46 +0100

    Using useForm instead

--- extensions/bmrks/src/create-bookmark.tsx
+++ extensions/bmrks/src/create-bookmark.tsx
@@ -1,11 +1,12 @@
-import React from "react";
+import { useEffect } from "react";
 import fetch from "node-fetch";
-import { getFavicon } from "@raycast/utils";
+import { FormValidation, getFavicon, useForm } from "@raycast/utils";
 import {
   Action,
   ActionPanel,
   Detail,
   Form,
+  Icon,
   PopToRootType,
   Toast,
   openExtensionPreferences,
@@ -28,119 +29,115 @@ interface MicrolinkResponse {
 }
 
 function Bookmark({ user }: { user: User }) {
-  const [activeGroupId, setActiveGroupId] = React.useState<string | undefined>();
-  const [value, setValue] = React.useState("");
-  const [titleValue, setTitleValue] = React.useState<string | null>(null);
+  interface BookmarkValues {
+    groupId: string;
+    value: string;
+    title?: string;
+  }
+
+  const { handleSubmit, itemProps, setValue, values } = useForm<BookmarkValues>({
+    async onSubmit(values) {
+      const isValidColor = Boolean(colorString.get(values.value));
+
+      if (isValidColor) {
+        const res = await db.insertBookmark({
+          type: "color",
+          group_id: values.groupId,
+          title: values.value,
+        });
+
+        if (res.error) {
+          await showToast({ style: Toast.Style.Failure, title: "Something went wrong", message: res.error.message });
+          return;
+        }
+      } else if (valueIsUrl) {
+        const validUrl = ensureValidUrl(values.value);
+        const favicon = getFavicon(validUrl);
+
+        let title: string | undefined;
+
+        if (!values.title) {
+          const response = await fetch(`https://api.microlink.io?url=${encodeURIComponent(validUrl)}&data.title`);
+          if (response.ok) {
+            const data = (await response.json()) as MicrolinkResponse;
+            title = data.data.title;
+          }
+        } else {
+          title = "";
+        }
+
+        const res = await db.insertBookmark({
+          type: "link",
+          group_id: values.groupId,
+          url: validUrl,
+          title,
+          // @ts-expect-error: looks like source is missing
+          favicon_url: favicon.source || "",
+        });
+
+        if (res.error) {
+          await showToast({ style: Toast.Style.Failure, title: "Something went wrong", message: "res.error.message" });
+          return;
+        }
+      } else {
+        const res = await db.insertBookmark({
+          type: "text",
+          group_id: values.groupId,
+          title: values.value,
+        });
+
+        if (res.error) {
+          await showToast({ style: Toast.Style.Failure, title: "Something went wrong", message: "res.error.message" });
+          return;
+        }
+      }
+
+      await showHUD(`Saved to ${activeGroup!.name}`, {
+        clearRootSearch: true,
+        popToRootType: PopToRootType.Immediate,
+      });
+    },
+    validation: {
+      value: FormValidation.Required,
+    },
+  });
 
   const activeTab = useActiveTab();
   const { data: groups, isLoading: isLoadingGroups } = useGroups(user);
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (activeTab) {
-      setValue(activeTab.url);
-      setTitleValue(activeTab.title);
+      setValue("value", activeTab.url);
+      setValue("title", activeTab.title);
     }
   }, [activeTab]);
 
-  const valueIsUrl = isUrlLike(value);
-
-  const activeGroup = groups.find((group) => group.id === activeGroupId);
-
-  async function handleSubmit({ groupId, value }: { groupId: string; value: string; title: string }) {
-    if (!value) {
-      await showToast({ style: Toast.Style.Failure, title: "Missing URL", message: "Please provide one" });
-      return;
-    }
-
-    const isValidColor = Boolean(colorString.get(value));
-
-    if (isValidColor) {
-      const res = await db.insertBookmark({
-        type: "color",
-        group_id: groupId,
-        title: value,
-      });
-
-      if (res.error) {
-        await showToast({ style: Toast.Style.Failure, title: "Something went wrong", message: res.error.message });
-        return;
-      }
-    } else if (valueIsUrl) {
-      const validUrl = ensureValidUrl(value);
-      const favicon = await getFavicon(validUrl);
-
-      let title: string | undefined;
-
-      if (!titleValue) {
-        const response = await fetch(`https://api.microlink.io?url=${encodeURIComponent(validUrl)}&data.title`);
-        if (response.ok) {
-          const data = (await response.json()) as MicrolinkResponse;
-          title = data.data.title;
-        }
-      } else {
-        title = titleValue;
-      }
-
-      const res = await db.insertBookmark({
-        type: "link",
-        group_id: groupId,
-        url: validUrl,
-        title,
-        // @ts-expect-error: looks like source is missing
-        favicon_url: favicon.source || "",
-      });
-
-      if (res.error) {
-        await showToast({ style: Toast.Style.Failure, title: "Something went wrong", message: "res.error.message" });
-        return;
-      }
-    } else {
-      const res = await db.insertBookmark({
-        type: "text",
-        group_id: groupId,
-        title: value,
-      });
-
-      if (res.error) {
-        await showToast({ style: Toast.Style.Failure, title: "Something went wrong", message: "res.error.message" });
-        return;
-      }
-    }
-
-    await showHUD(`Saved to ${activeGroup!.name}`, {
-      clearRootSearch: true,
-      popToRootType: PopToRootType.Immediate,
-    });
-  }
+  const valueIsUrl = isUrlLike(values.value);
+  const activeGroup = groups.find((group) => group.id === values.groupId);
 
   return (
     <Form
       isLoading={isLoadingGroups}
       actions={
         <ActionPanel>
-          <Action.SubmitForm title="Save Bookmark" onSubmit={handleSubmit} />
+          <Action.SubmitForm title="Save Bookmark" icon={Icon.CheckCircle} onSubmit={handleSubmit} />
           {activeGroup && user && (
             <Action.OpenInBrowser
+              title={`Open ${activeGroup.name} in bmrks.com`}
               url={`https://bmrks.com/${user.user_metadata["username"]}/${activeGroup.name.toLowerCase()}`}
             />
           )}
         </ActionPanel>
       }
     >
-      <Form.Dropdown id="groupId" title="Group" onChange={setActiveGroupId}>
+      <Form.Dropdown title="Group" {...itemProps.groupId}>
         {groups.map((group) => (
           <Form.Dropdown.Item key={group.id} value={group.id} title={group.name} />
         ))}
       </Form.Dropdown>
       <Form.Separator />
-      <Form.TextField
-        id="value"
-        title="Bookmark"
-        placeholder="Link, color, or text"
-        value={value}
-        onChange={setValue}
-      />
-      {valueIsUrl && <Form.TextField id="title" title="Link Title" value={titleValue || ""} onChange={setTitleValue} />}
+      <Form.TextField title="Bookmark" placeholder="Link, color, or text" {...itemProps.value} />
+      {valueIsUrl && <Form.TextField title="Link Title" {...itemProps.title} />}
     </Form>
   );
 }
