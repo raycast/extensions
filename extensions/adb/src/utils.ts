a7204f36d3c7a2eef7b800a2be33be652351902d
Quentin23Soleil <dommer.q@gmail.com>
2024-02-20 14:18:03 +0100

    Add new commands

--- extensions/adb/src/utils.ts
+++ extensions/adb/src/utils.ts
@@ -1,5 +1,6 @@
 import * as fs from "fs";
 import { execSync } from "child_process";
+import { Cache } from "@raycast/api";
 
 export default async function checkAdbExists() {
   const adb = `${process.env.HOME}/Library/Android/sdk/platform-tools/adb`;
@@ -8,10 +9,28 @@ export default async function checkAdbExists() {
     throw new Error(`❗️ADB not found here: ${adb}`);
   } else {
     const device = execSync(`${adb} devices`).toString().trim().split("\n");
-    console.log(device);
     if (device.length == 1) {
       throw new Error(`❗No device seem to be connected.`);
     }
     return `${adb} -s ${device[1].split("\t")[0]}`;
   }
+}
+
+export function getAppIdFromParamsOrCache(paramAppId: string | undefined): string | undefined {
+  const cache = new Cache();
+  const cacheAppId = cache.get("appId");
+  let appId: string | undefined;
+  if (paramAppId != undefined && paramAppId.length > 0) {
+    appId = paramAppId;
+  } else if (cacheAppId?.length ?? 0 > 0) {
+    appId = cacheAppId;
+  } else {
+    appId = undefined;
+  }
+  return appId;
+}
+
+export function saveAppIdInCache(appId: string) {
+  const cache = new Cache();
+  cache.set("appId", appId);
 }
