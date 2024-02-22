609c70c2654e651faad0e653e04d6b67cbf26437
pradeepb28 <pradeepboorgu@gmail.com>
2024-02-22 22:20:56 +0530

    add extension

--- extensions/audio-writer/src/start-recording.tsx
+++ extensions/audio-writer/src/start-recording.tsx
@@ -5,7 +5,7 @@ export default async () => {
   try {
     const isInstalled = await isAudioWriterInstalled();
     if (isInstalled) {
-      const url = "audiowriter://transcripts";
+      const url = "audiowriter://record";
       open(url);
       await closeMainWindow();
     } else {
