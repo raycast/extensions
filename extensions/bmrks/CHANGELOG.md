949fe71ec3d90a219a5206c58f9619fb12e2eac0
rauno <freiberggg@gmail.com>
2024-02-22 17:00:25 +0200

    Add bmrks extension (#9975)
    * init
    
    * New
    
    * oops
    
    * Update README.md
    
    * Update README.md
    
    * Update bmrks-0.png
    
    * Custom `useUser`
    
    * Update icon.png
    
    * Refactor
    
    * conditionally show title if is link
    
    * search
    
    * Fix Search Bookmarks command
    
    * update metadata
    
    * Update README.md
    
    * tidying up
    
    * Update extensions/bmrks/src/search-bookmarks.tsx
    
    Co-authored-by: Per Nielsen Tikær <per@raycast.com>
    
    * Update extensions/bmrks/src/create-bookmark.tsx
    
    Co-authored-by: Per Nielsen Tikær <per@raycast.com>
    
    * Merge
    
    * Update create-bookmark.tsx
    
    * Using useForm instead
    
    * Update create-bookmark.tsx
    
    * Update create-bookmark.tsx
    
    * Refactor
    
    * Removed stale function
    
    * Respect overriden title
    
    * Better user feedback
    
    * Delete bookmark
    
    * Add Move action
    
    * Update CHANGELOG.md
    
    * Update README.md
    
    * Update icon.png
    
    * tooling
    
    * Abstract AuthenticatedView and make it faster
    
    * Consistent casing
    
    * Rename to authenticated-view
    
    ---------
    
    Co-authored-by: Pedro Duarte <oi@ped.ro>
    Co-authored-by: Per Nielsen Tikær <per@raycast.com>

---
+++ extensions/bmrks/CHANGELOG.md
@@ -0,0 +1,3 @@
+# bmrks Changelog
+
+## [Initial Version] - 2024-02-20
---
+++ extensions/bmrks/src/search-bookmarks.tsx
@@ -0,0 +1,117 @@
+import React from "react";
+import { User } from "@supabase/supabase-js";
+import { Action, ActionPanel, Icon, Image, List, Toast, showToast } from "@raycast/api";
+import { useGroups } from "../lib/use-groups";
+import { useBookmarks } from "../lib/use-bookmarks";
+import { getHostname } from "../lib/get-hostname";
+import { deleteBookmark, moveBookmarkToGroup } from "../lib/db";
+import AuthenticatedView from "./components/authenticated-view";
+
+export function SearchBookmarks({ user }: { user: User }) {
+  const { data: groups, isLoading: isLoadingGroups } = useGroups(user);
+  const [groupId, setGroupId] = React.useState<string | undefined>("");
+  const { data: bookmarks, isLoading: isLoadingBookmarks, revalidate } = useBookmarks(groupId);
+
+  React.useEffect(() => {
+    if (groups && groups.length > 0) {
+      setGroupId(groups[0].id);
+    }
+  }, [groups]);
+
+  return (
+    <List
+      isLoading={isLoadingGroups || isLoadingBookmarks}
+      searchBarAccessory={
+        <List.Dropdown tooltip="Groups" value={groupId} onChange={setGroupId}>
+          {groups &&
+            groups.map((group) => {
+              return <List.Dropdown.Item key={group.id} title={group.name || ""} value={group.id} />;
+            })}
+        </List.Dropdown>
+      }
+    >
+      {bookmarks &&
+        bookmarks.map((bookmark) => {
+          const hostname = bookmark.type === "link" ? getHostname(bookmark.url || "") : "";
+          let icon: Image.ImageLike = Icon.Bookmark;
+
+          if (bookmark.favicon_url) {
+            icon = bookmark.favicon_url;
+          }
+          if (bookmark.type === "color") {
+            icon = { source: Icon.CircleFilled, tintColor: bookmark.title };
+          }
+          if (bookmark.type === "text") {
+            icon = Icon.Paragraph;
+          }
+
+          return (
+            <List.Item
+              key={bookmark.id}
+              title={bookmark.title ? bookmark.title : hostname}
+              icon={icon}
+              subtitle={hostname}
+              actions={
+                <ActionPanel>
+                  {bookmark.type === "link" && bookmark.url && (
+                    <>
+                      <Action.OpenInBrowser url={bookmark.url} />
+                      <Action.CopyToClipboard title="Copy URL to Clipboard" content={bookmark.url} />
+                    </>
+                  )}
+                  {bookmark.type !== "link" && bookmark.title && <Action.CopyToClipboard content={bookmark.title} />}
+
+                  <Action
+                    style={Action.Style.Destructive}
+                    title="Delete Bookmark"
+                    icon={Icon.Trash}
+                    shortcut={{ modifiers: ["cmd"], key: "backspace" }}
+                    onAction={async () => {
+                      if (bookmark.id) {
+                        showToast({ title: "Deleting bookmark...", style: Toast.Style.Animated });
+                        const res = await deleteBookmark(bookmark.id);
+                        revalidate();
+                        res.error
+                          ? showToast({ title: "Failed to delete bookmark", style: Toast.Style.Failure })
+                          : showToast({ title: "Bookmark deleted", style: Toast.Style.Success });
+                      }
+                    }}
+                  />
+                  <ActionPanel.Section>
+                    <ActionPanel.Submenu title="Move to Group" icon={Icon.ArrowRight}>
+                      {groups &&
+                        groups.map((group) => {
+                          if (group.id === groupId) {
+                            return null;
+                          }
+                          return (
+                            <Action
+                              key={group.id}
+                              title={group.name}
+                              onAction={async () => {
+                                if (bookmark.id) {
+                                  showToast({ title: "Moving bookmark...", style: Toast.Style.Animated });
+                                  const res = await moveBookmarkToGroup(bookmark.id, group.id);
+                                  revalidate();
+                                  res.error
+                                    ? showToast({ title: "Failed to move bookmark", style: Toast.Style.Failure })
+                                    : showToast({ title: `Moved to ${group.name}`, style: Toast.Style.Success });
+                                }
+                              }}
+                            />
+                          );
+                        })}
+                    </ActionPanel.Submenu>
+                  </ActionPanel.Section>
+                </ActionPanel>
+              }
+            />
+          );
+        })}
+    </List>
+  );
+}
+
+export default function Command() {
+  return <AuthenticatedView component={SearchBookmarks} />;
+}
