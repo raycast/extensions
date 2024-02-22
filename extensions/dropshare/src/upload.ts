b60cbd82ead1832d8cdb97e55b9879042b8c37d6
Timo Josten <tj@mkswap.net>
2024-02-22 20:35:45 +0100

    Remove namespace

--- extensions/dropshare/src/upload.ts
+++ extensions/dropshare/src/upload.ts
@@ -1,16 +1,14 @@
 import { closeMainWindow, open, LaunchProps } from "@raycast/api";
 
-namespace Dropshare {
-    export type Upload = {
-        file: string;
-    }
+interface Upload {
+  file: string;
 }
 
-export default async function Upload(props: LaunchProps<{ arguments: Dropshare.Upload }>) {
-    const { file } = props.arguments;
-    console.log(`file: ${file}`);
+export default async function Upload(props: LaunchProps<{ arguments: Upload }>) {
+  const { file } = props.arguments;
+  console.log(`file: ${file}`);
 
-    const url = "dropshare5:///action/upload?file=" + file;
-    open(url);
-    await closeMainWindow();
+  const url = "dropshare5:///action/upload?file=" + file;
+  open(url);
+  await closeMainWindow();
 }
