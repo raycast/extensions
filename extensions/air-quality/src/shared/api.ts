5c4e3840bdd0806e8aab863441cb20130a67de20
Preeti Yuankrathok <preetisatit@gmail.com>
2024-02-23 13:02:25 +0700

    Update air-quality extension
    - Updated screnshot
    - Better cleaning city name
    - Reformat
    - Add support for station without forecast data
    - Extract error as functions
    - Remove redundant error message
    - Better error handle
    - Add reload interval
    - No sun icon
    - Initial commit

--- extensions/air-quality/src/shared/api.ts
+++ extensions/air-quality/src/shared/api.ts
@@ -1,21 +1,15 @@
 import axios from "axios";
 import { getPreferenceValues } from "@raycast/api";
 import { AirQualityData, Preferences } from "./types";
+import { cleanCityName, extractErrorMessage } from "./utils";
 
 const preferences: Preferences = getPreferenceValues();
-const cityName = preferences.city
-  ? preferences.city
-      .replace(/\s/g, "")
-      .replace("https://aqicn.org/city/", "")
-      .replace(/^\//, "")
-      .replace(/\/$/, "")
-      .toLowerCase()
-  : "here";
+const cityName = cleanCityName(preferences.city);
 
 export async function fetchAirQuality() {
   const response = await axios.get(`https://api.waqi.info/feed/${cityName}/?token=${preferences.apiToken}`);
-  if (response.status !== 200 || response.data.status !== "ok") {
-    throw new Error(response.data?.data || "Failed to fetch air quality data");
+  if (response.status !== 200 || response.data.status !== "ok" || response.data.data?.status === "error") {
+    throw new Error(extractErrorMessage(response));
   }
   return response.data.data as unknown as AirQualityData;
 }
