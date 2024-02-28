026bda0bfaec24c07132c5dc7c2e63d06efee2e9
LitoMore <litomore@gmail.com>
2024-02-28 11:49:03 +0800

    Update simple-icons extension
    - Bump changes
    - Fix changelog
    - Increase icon size in markdown
    - Fix version in CDN links

--- extensions/simple-icons/src/index.tsx
+++ extensions/simple-icons/src/index.tsx
@@ -21,7 +21,9 @@ export default function Command() {
       });
 
       const version = await loadLatestVersion();
+      console.log(version);
       const cached = cache.get(`json-${version}`);
+      console.log(Boolean(cached));
       const json: IconJson = cached ? JSON.parse(cached) : await loadJson(version);
 
       if (!cached) {
@@ -87,7 +89,7 @@ export default function Command() {
                     title="See Detail"
                     target={
                       <Detail
-                        markdown={`<img src="https://cdn.simpleicons.org/${slug}?raycast-width=235&raycast-height=235"  />`}
+                        markdown={`<img src="https://cdn.simpleicons.org/${slug}?raycast-width=325&raycast-height=325"  />`}
                         navigationTitle={icon.title}
                         metadata={
                           <Detail.Metadata>
@@ -137,12 +139,12 @@ export default function Command() {
                             />
                             <Action.CopyToClipboard
                               title="Copy jsDelivr CDN Link"
-                              content={`https://cdn.jsdelivr.net/npm/simple-icons@${packageJson.dependencies["simple-icons"]}/icons/${slug}.svg`}
+                              content={`https://cdn.jsdelivr.net/npm/simple-icons@${version}/icons/${slug}.svg`}
                             />
                             <Action.CopyToClipboard
                               // eslint-disable-next-line @raycast/prefer-title-case
                               title="Copy unpkg CDN Link"
-                              content={`https://unpkg.com/simple-icons@${packageJson.dependencies["simple-icons"]}/icons/${slug}.svg`}
+                              content={`https://unpkg.com/simple-icons@${version}/icons/${slug}.svg`}
                             />
                           </ActionPanel>
                         }
