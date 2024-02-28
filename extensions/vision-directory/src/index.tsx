68151f7138374fa36c7a6da87b764aed0ac32229
Marc Köhlbrugge <subscriptions@marckohlbrugge.com>
2024-02-28 16:11:12 +0700

    Update vision-directory extension
    - Merge branch \'contributions/merge-1709111405874571000\'
    - Pull contributions
    - Update changelog
    - Update screenshot
    - Show icons, category and group by day

--- extensions/vision-directory/src/index.tsx
+++ extensions/vision-directory/src/index.tsx
@@ -1,17 +1,25 @@
-import { ActionPanel, List, Action } from "@raycast/api";
+import { ActionPanel, List, Action, Image } from "@raycast/api";
 import { useFetch } from "@raycast/utils";
+import { useEffect, useState } from "react";
 
 interface Category {
   name: string;
   url: string;
 }
 
+interface AppIcon {
+  small: string;
+  medium: string;
+  large: string;
+}
+
 interface App {
   name: string;
   subtitle: string;
-  date: string;
+  featured_at: string;
   url: string;
   categories: Category[];
+  icon: AppIcon;
 }
 
 export default function Command() {
@@ -26,30 +34,65 @@ export default function Command() {
     },
   });
 
+  // Use useState to manage the apps state based on fetched data
+  const [apps, setApps] = useState<App[]>([]);
+
+  // Update apps state when data changes
+  useEffect(() => {
+    if (data) {
+      setApps(data);
+    }
+  }, [data]);
+
+  const formatDate = (date: Date) => date.toISOString().split("T")[0];
+
+  const filterAppsByDate = (apps: App[], date: Date) =>
+    apps?.filter((app) => formatDate(new Date(app.featured_at)) === formatDate(date)) || [];
+
+  const today = new Date();
+  const yesterday = new Date(today);
+  yesterday.setDate(yesterday.getDate() - 1);
+
+  const appsToday = filterAppsByDate(apps, today);
+  const appsYesterday = filterAppsByDate(apps, yesterday);
+  const appsEarlier = apps?.filter((app) => formatDate(new Date(app.featured_at)) < formatDate(yesterday)) || [];
+
   return (
     <List isLoading={isLoading}>
-      <List.EmptyView icon="no-view.png" title="No Results" />
-      {data?.map((app, index) => <AppListItem key={index} app={app} />)}
+      <List.Section title="Today">
+        {appsToday.map((app, index) => (
+          <AppListItem key={index} app={app} />
+        ))}
+      </List.Section>
+      <List.Section title="Yesterday">
+        {appsYesterday.map((app, index) => (
+          <AppListItem key={index} app={app} />
+        ))}
+      </List.Section>
+      <List.Section title="Earlier">
+        {appsEarlier.map((app, index) => (
+          <AppListItem key={index} app={app} />
+        ))}
+      </List.Section>
     </List>
   );
 }
 
-function AppListItem(props: { app: App }) {
-  const { app } = props;
-
+function AppListItem({ app }: { app: App }) {
   return (
     <List.Item
+      icon={{
+        source: app.icon.small,
+        fallback: "circle-progress.png",
+        mask: Image.Mask.RoundedRectangle,
+      }}
       title={app.name ?? "No name"}
       subtitle={app.subtitle ?? "No subtitle"}
-      // accessories={[{ text: useRelativeTime(new Date(app.featured_at)) }]}
+      accessories={app.categories.length > 0 ? [{ text: app.categories[0].name }] : []}
       actions={
         <ActionPanel title={app.name}>
-          <ActionPanel.Section>
-            {app.url && <Action.OpenInBrowser url={app.url} />}
-            {app.url && (
-              <Action.CopyToClipboard content={app.url} title="Copy Link" shortcut={{ modifiers: ["cmd"], key: "." }} />
-            )}
-          </ActionPanel.Section>
+          <Action.OpenInBrowser url={app.url} title="Open in Browser" />
+          <Action.CopyToClipboard content={app.url} title="Copy Link" shortcut={{ modifiers: ["cmd"], key: "c" }} />
         </ActionPanel>
       }
     />
