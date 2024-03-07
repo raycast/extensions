2fe181d7a898308d28bf7a705c94f6054f898ee7
Timo Josten <tj@mkswap.net>
2024-03-07 17:27:19 +0100

    Add URL shortener action

---
+++ extensions/dropshare/src/utils/clipboard.ts
@@ -0,0 +1,17 @@
+import { Clipboard } from "@raycast/api";
+
+export const getUrlFromClipboard = async () => {
+    let content = await Clipboard.readText();
+    content = typeof content == "undefined" ? "" : content;
+
+    console.log(content);
+
+    if (isUrl(content)) {
+        return content;
+    }
+    return "";
+};
+
+export function isUrl(text: string): boolean {
+    return text.startsWith("http://") || text.startsWith("https://");
+}
