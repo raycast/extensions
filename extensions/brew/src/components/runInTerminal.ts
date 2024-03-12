1cda4049b245fcb2f0e267dbc74a0ce6a123101e
Tyler Miller <tyemiller4@gmail.com>
2024-03-12 07:44:26 -0400

    [Brew] Add Warp  (#11180)
    * add warp
    * [Brew] add warp
    * update CHANGELOG
    * Ran npm exec ray lint  --fix  fixed issues
    * fix issue with keystroking return
    * make sure a new tab is used
    * fix formatting issue

--- extensions/brew/src/components/runInTerminal.ts
+++ extensions/brew/src/components/runInTerminal.ts
@@ -15,6 +15,16 @@ const appleScripts: { [key in typeof terminalApp]: (c: string) => string } = {
       set newWindow to create window with default profile command "bash -c '${c}; read -n 1 -s -r -p \\"Press any key to exit - will not quit\\" ; echo' ; exit"
     end tell
   `,
+  warp: (c: string) => `
+    tell application "Warp" to activate
+    tell application "System Events" to tell process "Warp"
+        keystroke "t" using command down
+        keystroke "${c}"
+        delay 1.0
+        key code 36
+    end tell
+`,
+  /// warp does not provide an URI or anyway to pass commands so this was a workaround
 };
 
 export function runCommandInTerminal(command: string): void {
