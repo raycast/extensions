1095282b61d7a09b0a13232c3db3faad806188fd
Bevis Zhao <i@bevis.me>
2024-03-14 12:00:35 +0800

    fix lint issues

--- extensions/pangu-for-raycast/src/pangu.tsx
+++ extensions/pangu-for-raycast/src/pangu.tsx
@@ -2,16 +2,16 @@ import pangu from "pangu";
 import { getSelectedText, Clipboard, showHUD } from "@raycast/api";
 
 export default async () => {
-    let text
-    try {
-      text = await getSelectedText()
-    } finally {
-      if ((text || '').trim() === '') {
-        text = await Clipboard.readText() || ''
-      }
+  let text;
+  try {
+    text = await getSelectedText();
+  } finally {
+    if ((text || "").trim() === "") {
+      text = (await Clipboard.readText()) || "";
     }
-    const result = pangu.spacing(text)
-    Clipboard.paste(result)
+  }
+  const result = pangu.spacing(text);
+  Clipboard.paste(result);
 
-    await showHUD(`Formated with spaces.👋`);
-}
+  await showHUD(`Formated with spaces.👋`);
+};
