9682dddf0b66b4a321a6f36139e9a47333af92bc
Bevis Zhao <i@bevis.me>
2024-03-14 09:16:36 +0800

    init repo

---
+++ extensions/pangu-for-raycast/src/pangu.tsx
@@ -0,0 +1,17 @@
+import pangu from "pangu";
+import { getSelectedText, Clipboard, showHUD } from "@raycast/api";
+
+export default async () => {
+    let text
+    try {
+      text = await getSelectedText()
+    } finally {
+      if ((text || '').trim() === '') {
+        text = await Clipboard.readText() || ''
+      }
+    }
+    const result = pangu.spacing(text)
+    Clipboard.paste(result)
+
+    await showHUD(`Formated with spaces.👋`);
+}
