30d5d8ce68f3f844c4459bf168ae494b65e2c655
Branden Wagner <branden@mercury.com>
2024-03-14 11:13:11 -0400

    add connect and disconnect feature

---
+++ extensions/tailscale/src/disconnect.tsx
@@ -0,0 +1,15 @@
+import { updateCommandMetadata } from "@raycast/api";
+import { getErrorDetails, tailscale } from "./shared";
+
+export default async function Connect() {
+  let subtitle: string;
+  subtitle = "Tailscale"
+  try {
+    tailscale("down")
+    
+  } catch (err) {
+    subtitle = getErrorDetails(err, "").title;
+  
+  }
+  await updateCommandMetadata({ subtitle });
+}
