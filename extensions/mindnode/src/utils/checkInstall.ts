9d5bca3cdc268b8d8e447ecf1cb91582c3c18b0f
spike <57598529+natsustan@users.noreply.github.com>
2024-03-19 14:03:57 +0800

    Update mindnode extension (#11368)

--- extensions/mindnode/src/utils/checkInstall.ts
+++ extensions/mindnode/src/utils/checkInstall.ts
@@ -1,8 +1,26 @@
 import { getApplications, showToast, Toast, open } from "@raycast/api";
+import { readdir } from "fs/promises";
+
+// 新增检查 Setapp 目录的函数
+async function isMindNodeInstalledInSetapp() {
+  const setappApplicationsPath = "/Applications/Setapp";
+  try {
+    const files = await readdir(setappApplicationsPath);
+    return files.some((file) => file.includes("MindNode"));
+  } catch (error) {
+    console.error("Error reading Setapp applications directory:", error);
+    return false;
+  }
+}
 
 async function isMindNodeInstalled() {
   const applications = await getApplications();
-  return applications.some(({ bundleId }) => bundleId === "com.ideasoncanvas.mindnode.macos");
+  const isInstalledRegular = applications.some(
+    ({ bundleId }) =>
+      bundleId === "com.ideasoncanvas.mindnode.macos-setapp" || bundleId === "com.ideasoncanvas.mindnode.macos",
+  );
+  const isInstalledSetapp = await isMindNodeInstalledInSetapp();
+  return isInstalledRegular || isInstalledSetapp;
 }
 
 export async function checkMindnodeInstallation(): Promise<boolean> {
@@ -11,18 +29,18 @@ export async function checkMindnodeInstallation(): Promise<boolean> {
     const options: Toast.Options = {
       style: Toast.Style.Failure,
       title: "MindNode is not installed.",
-      message: "Install it from: https://mindnode.com",
+      message: "Check it more from: https://mindnode.com",
       primaryAction: {
-        title: "Go to https://mindnode.com",
+        title: "Go to App Store",
         onAction: (toast) => {
-          open("https://mindnode.com");
+          open("https://apps.apple.com/app/apple-store/id1289197285?pt=14265&ct=web-main&mt=8");
           toast.hide();
         },
       },
       secondaryAction: {
-        title: "Go to App Store",
+        title: "Go to Setapp",
         onAction: (toast) => {
-          open("https://apps.apple.com/app/apple-store/id1289197285?pt=14265&ct=web-main&mt=8");
+          open("https://setapp.com/apps/mindnode");
           toast.hide();
         },
       },
