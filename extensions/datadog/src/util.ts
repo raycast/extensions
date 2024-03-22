68813e96e3b943bf272ebd9e3d5f859a5560a5c9
Tiago Cussiol <tcwk27+github@gmail.com>
2024-03-22 06:32:32 -0300

    Update raycast-datadog extension (#11331)
    - Add RUM Applications and other improvements
    - Initial commit

--- extensions/datadog/src/util.ts
+++ extensions/datadog/src/util.ts
@@ -1,6 +1,10 @@
-import { showToast, ToastStyle, getPreferenceValues } from "@raycast/api";
+import { showToast, Toast, getPreferenceValues } from "@raycast/api";
 
-export const showError = (e: Error) => showToast(ToastStyle.Failure, e.message);
+export const showError = (e: Error) => showToast(Toast.Style.Failure, e.message);
 export const linkDomain = () => {
-  return getPreferenceValues()["domain"] || "app." + getPreferenceValues()["server"];
+  const preferences = getPreferenceValues();
+  return preferences.domain || "app." + preferences.server;
 };
+export function notEmpty<TValue>(value: TValue): value is NonNullable<TValue> {
+  return value !== null && value !== undefined && value != "";
+}
