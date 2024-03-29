397f3d5bc206edba9b642ca35acbd9b300cd20c5
José Pereira <jmfp-b@live.com.pt>
2024-03-29 23:18:24 +0000

    feat: add copy url action and add some accessories depending on options

--- extensions/bitwarden/src/list-sends.tsx
+++ extensions/bitwarden/src/list-sends.tsx
@@ -1,4 +1,4 @@
-import { Color, Icon, List } from "@raycast/api";
+import { Action, ActionPanel, Color, Icon, List } from "@raycast/api";
 import { usePromise } from "@raycast/utils";
 import RootErrorBoundary from "~/components/RootErrorBoundary";
 import { BitwardenProvider, useBitwarden } from "~/context/bitwarden";
@@ -17,12 +17,37 @@ const ListSendCommand = () => (
   </RootErrorBoundary>
 );
 
+const getItemIcon = (send: Send): NonNullable<List.Item.Props["icon"]> => {
+  if (send.type === SendType.File) {
+    return { source: Icon.Document, tintColor: Color.Orange, tooltip: "File Send" };
+  }
+  return { source: Icon.Text, tintColor: Color.Blue, tooltip: "Text Send" };
+};
+
 const getItemAccessories = (send: Send): NonNullable<List.Item.Props["accessories"]> => {
   const accessories: NonNullable<List.Item.Props["accessories"]> = [];
-  if (send.type === SendType.File) {
-    accessories.push({ tag: { value: "File", color: Color.Orange }, icon: Icon.Document });
-  } else {
-    accessories.push({ tag: { value: "Text", color: Color.Blue }, icon: Icon.Text });
+  if (send.passwordSet) {
+    accessories.push({ icon: Icon.Lock, tooltip: "Password protected." });
+  }
+  if (send.maxAccessCount) {
+    accessories.push({ icon: Icon.Person, tooltip: `Has a max Access Count of ${send.maxAccessCount}.` });
+  }
+  if (send.disabled) {
+    accessories.push({ icon: Icon.EyeDisabled, tooltip: "Deactivated and no one can access it." });
+  }
+  if (send.expirationDate) {
+    accessories.push({
+      icon: Icon.Clock,
+      date: { value: new Date(send.expirationDate) },
+      tooltip: `Will expire on ${new Date(send.expirationDate).toLocaleString()}.`,
+    });
+  }
+  if (send.deletionDate) {
+    accessories.push({
+      icon: Icon.Trash,
+      date: { value: new Date(send.deletionDate) },
+      tooltip: `Will be deleted on ${new Date(send.deletionDate).toLocaleString()}.`,
+    });
   }
   return accessories;
 };
@@ -36,7 +61,17 @@ function ListSendCommandContent() {
   return (
     <List isLoading={isLoading}>
       {sends.map((send) => (
-        <List.Item key={send.id} title={send.name} accessories={getItemAccessories(send)} />
+        <List.Item
+          key={send.id}
+          title={send.name}
+          icon={getItemIcon(send)}
+          accessories={getItemAccessories(send)}
+          actions={
+            <ActionPanel>
+              <Action.CopyToClipboard title="Copy URL" content={send.accessUrl} />
+            </ActionPanel>
+          }
+        />
       ))}
     </List>
   );
