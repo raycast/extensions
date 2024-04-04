7c061c19efada8c0d52582320679ee7a7fa7e5a5
Willem van Lent <w.vanlent@rhea.infosupport.net>
2024-04-04 15:13:41 +0200

    Add ado-search extension
    - Added extra info for .http file
    - Project startup

---
+++ extensions/ado-search/CHANGELOG.md
@@ -0,0 +1,4 @@
+# Azure DevOps Repository Search
+
+## [Initial Version] - 2024-03-31
+The first version of the Azure DevOps Repository search extensions. This will help you find repo's quickly, open them or copy the GIT remote.
---
+++ extensions/ado-search/src/search-ado-repos.tsx
@@ -0,0 +1,34 @@
+import { Action, ActionPanel, List, Icon } from "@raycast/api";
+import { useFetch } from "@raycast/utils";
+import { personalAccessToken, organizationName } from "./preferences";
+import { AdoGitrepostitoriesResponse } from "./types";
+
+export default () => {
+  const url = 'https://dev.azure.com/' + organizationName + '/_apis/git/repositories?api-version=1.0';
+  const { data, isLoading } = useFetch<AdoGitrepostitoriesResponse>(url, {
+    headers: { Accept: "application/json", Authorization: `Basic ${personalAccessToken}` }
+  });
+
+  return <List isLoading={isLoading} >
+    {data?.value
+      .filter((repository) => !repository.isDisabled)
+      .map((repository) => (
+        <List.Item
+          key={repository.id}
+          title={repository.name}
+          subtitle={repository.project.name}
+          accessories={[
+            { text: repository.defaultBranch?.replace("refs/heads/", ""), icon: Icon.Code },
+          ]}
+          actions={
+            <ActionPanel>
+              <Action.OpenInBrowser title="Open in Browser" url={repository.webUrl} />
+              <Action.CopyToClipboard title="Copy GIT SSH URL" content={repository.sshUrl} />
+              <Action.CopyToClipboard title="Copy GIT Remote URL" content={repository.remoteUrl} />
+            </ActionPanel>
+          }
+        />
+      )
+      )}
+  </List>;
+}
