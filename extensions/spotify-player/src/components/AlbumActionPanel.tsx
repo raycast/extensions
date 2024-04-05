ca6e18d99288a1d6efc86b3cc6f02994cd62add5
Pavel Mikhaylov <mikhailov.sztu@gmail.com>
2024-04-05 21:38:08 +0400

    add new actions in the album panel: `Add To Library` and `Remove From Library`

--- extensions/spotify-player/src/components/AlbumActionPanel.tsx
+++ extensions/spotify-player/src/components/AlbumActionPanel.tsx
@@ -1,8 +1,11 @@
-import { Action, ActionPanel, Icon } from "@raycast/api";
+import { Action, ActionPanel, Icon, popToRoot, showHUD } from "@raycast/api";
 import { SimplifiedAlbumObject } from "../helpers/spotify.api";
 import { FooterAction } from "./FooterAction";
 import { PlayAction } from "./PlayAction";
 import { TracksList } from "./TracksList";
+import { getErrorMessage } from "../helpers/getError";
+import { addToMySavedAlbums } from "../api/addToMySavedAlbums";
+import { removeFromMySavedAlbums } from "../api/removeFromMySavedAlbums";
 
 type AlbumActionPanelProps = { album: SimplifiedAlbumObject };
 
@@ -17,6 +20,42 @@ export function AlbumActionPanel({ album }: AlbumActionPanelProps) {
         title="Show Songs"
         shortcut={{ modifiers: ["cmd", "shift"], key: "a" }}
         target={<TracksList album={album} showGoToAlbum={false} />}
+      />
+      <Action
+        icon={Icon.Plus}
+        title="Add To Library"
+        shortcut={{ modifiers: ["cmd", "shift"], key: "s" }}
+        onAction={async () => {
+          try {
+            await addToMySavedAlbums({
+              albumIds: album.id ? [album.id] : [],
+            });
+            await showHUD("Album added to the library");
+            await popToRoot();
+            return;
+          } catch (err) {
+            const error = getErrorMessage(err);
+            await showHUD(error);
+          }
+        }}
+      />
+      <Action
+        icon={Icon.Minus}
+        title="Remove From Library"
+        shortcut={{ modifiers: ["cmd", "shift"], key: "d" }}
+        onAction={async () => {
+          try {
+            await removeFromMySavedAlbums({
+              albumIds: album.id ? [album.id] : [],
+            });
+            await showHUD("Album removed from the library");
+            await popToRoot();
+            return;
+          } catch (err) {
+            const error = getErrorMessage(err);
+            await showHUD(error);
+          }
+        }}
       />
       <FooterAction url={album?.external_urls?.spotify} uri={album.uri} title={title} />
     </ActionPanel>
