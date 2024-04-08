dbcb29b1d8f33ce7e9096dcb5a6849ef9063debb
Thomas Lombart <lombart.thomas@icloud.com>
2024-04-08 11:57:49 +0200

    Small refactoring

--- extensions/jira/src/helpers/withJiraCredentials.tsx
+++ extensions/jira/src/helpers/withJiraCredentials.tsx
@@ -4,9 +4,7 @@ import { withAccessToken } from "@raycast/utils";
 import { jira, jiraWithApiToken } from "../api/jiraCredentials";
 
 export function withJiraCredentials<T>(Component: React.ComponentType<T>) {
-  const prefs = getPreferenceValues();
-  if (prefs.token && prefs.email && prefs.siteUrl) {
-    return withAccessToken<T>(jiraWithApiToken)(Component);
-  }
-  return withAccessToken<T>(jira)(Component);
+  const { token, email, siteUrl } = getPreferenceValues<Preferences>();
+
+  return withAccessToken<T>(token && email && siteUrl ? jiraWithApiToken : jira)(Component);
 }
