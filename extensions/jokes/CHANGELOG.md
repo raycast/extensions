ec3f7d72f9fe0337a2ceb4095fac11cce4289e73
xmok <30526133+xmok@users.noreply.github.com>
2024-04-10 16:26:42 +0500

    [Jokes] Only Safe Jokes

--- extensions/jokes/CHANGELOG.md
+++ extensions/jokes/CHANGELOG.md
@@ -1,3 +1,5 @@
 # Jokes Changelog
 
+## [Only `Safe` Jokes] - {PR_MERGE_DATE}
+
 ## [Initial Version] - 2024-04-10
--- extensions/jokes/src/constants.ts
+++
@@ -1,7 +0,0 @@
-import { getPreferenceValues } from "@raycast/api";
-
-const preferences = getPreferenceValues<Preferences>();
-
-export const ENABLE_SAFE_MODE = preferences.enable_safe_mode;
-export const FLAGS = Object.entries(preferences).filter(([key]) => key.startsWith("allow_"));
-export const API_URL = "https://v2.jokeapi.dev/";
