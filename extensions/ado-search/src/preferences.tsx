c4260da5e404a4be7ec898de9e0a30e6adebb409
Willem van Lent <willemvanlent@live.nl>
2024-04-11 14:24:19 +0200

    Update ado-search extension (#11753)
    * Update ado-search extension
    
    - Changed PAT
    - Pull contributions
    
    * Changed Changelog
    
    * Update CHANGELOG.md and optimise images
    
    ---------
    
    Co-authored-by: Willem van Lent <w.vanlent@rhea.infosupport.net>
    Co-authored-by: raycastbot <bot@raycast.com>

--- extensions/ado-search/src/preferences.tsx
+++ extensions/ado-search/src/preferences.tsx
@@ -1,3 +1,8 @@
 import { getPreferenceValues } from "@raycast/api";
 
-export const { personalAccessToken, organizationName } = getPreferenceValues();
+export function preparedPersonalAccessToken(): string {
+  const personalAccessToken = getPreferenceValues().personalAccessToken;
+  return Buffer.from(":" + personalAccessToken, "binary").toString("base64");
+}
+
+export const { organizationName } = getPreferenceValues();
