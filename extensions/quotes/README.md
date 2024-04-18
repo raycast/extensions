4065e1d3bee61566f6b4103c568c9c76a0c22b6a
Shane Kennedy <shane.kennedy19@gmail.com>
2024-04-18 19:48:23 +0200

    Update quotes extension
    - Fix the icon
    - Add quick action in detail view to open on yf
    - Update the readme with images
    - Implement a basic detail view
    - Update yahoo-finance2
    - fix diff, should use percent not abs value
    - Show price diff
    - Show quick quote in list view and purge form
    - list view to start, no forms

--- extensions/quotes/README.md
+++ extensions/quotes/README.md
@@ -1,3 +1,7 @@
-# quotes
+# Stock quotes in raycast
 
 Get live stock quotes right from raycast
+
+Get a quick quote in the list view or hit enter on a symbol to see it's details
+![list view](./assets/list.png)
+![detail view](./assets/detail.png)
--- extensions/quotes/src/index.tsx
+++ extensions/quotes/src/index.tsx
@@ -1,25 +1,52 @@
 import React, { useEffect, useState } from "react";
 import yahooFinance from "yahoo-finance2";
 import { Quote } from "yahoo-finance2/dist/esm/src/modules/quote";
+import { Ticker, tickers } from "./tickers";
 
-import { ActionPanel, Form, Action, Detail, useNavigation } from "@raycast/api";
+import { ActionPanel, Action, Detail, List, Color } from "@raycast/api";
 
 export default function Command() {
-  const { push } = useNavigation();
-  const [symbol, setSymbol] = useState("NVDA");
+  const [quickQuote, setQuickQuote] = useState<Quote | null>();
+  const handleNewSelection = async (symbol: string | null) => {
+    if (symbol === null) {
+      return;
+    } else if (tickers.findIndex((ticker) => ticker.symbol === symbol) >= 0) {
+      const q = await yahooFinance.quote(symbol);
+      setQuickQuote(q);
+    }
+  };
+  function getListItemTitle(t: Ticker) {
+    let title = `${t.symbol} ${t.name}`;
+    if (t.symbol === quickQuote?.symbol) {
+      title += ` ${quickQuote ? quickQuote?.regularMarketPrice : ""}`;
+      title += ` ${quickQuote ? "(" + quickQuote.regularMarketChangePercent?.toPrecision(3) + "%)" : ""}`;
+    }
+    return title;
+  }
 
   return (
-    <>
-      <Form
-        actions={
-          <ActionPanel>
-            <Action.SubmitForm title="Get Quote" onSubmit={() => push(<QuoteView symbol={symbol} />)} />
-          </ActionPanel>
-        }
-      >
-        <Form.TextField id="symbol" value={symbol} onChange={setSymbol} />
-      </Form>
-    </>
+    <List
+      onSelectionChange={handleNewSelection}
+      navigationTitle="Search for quotes"
+      searchBarPlaceholder="Search for quotes"
+    >
+      {tickers.map((t: Ticker, index) => (
+        <List.Item
+          id={t.symbol}
+          key={index}
+          title={getListItemTitle(t)}
+          actions={
+            <ActionPanel title="Quote menu">
+              <Action.Push icon={"📈"} title="Get Quote" target={<QuoteView symbol={t.symbol} />} />
+              <Action.OpenInBrowser
+                title="See on finance.yahoo.com"
+                url={`https://finance.yahoo.com/quote/${t.symbol}`}
+              />
+            </ActionPanel>
+          }
+        />
+      ))}
+    </List>
   );
 }
 
@@ -33,17 +60,53 @@ const QuoteView: React.FC<QuoteViewProps> = ({ symbol }) => {
     async function fetchQuote() {
       const q = await yahooFinance.quote(symbol);
       if (q.symbol != quote?.symbol) {
-        console.log(q);
         setQuote(q);
       }
     }
     fetchQuote();
   }, [quote]);
 
+  const markdown = (quote: Quote) => `
+  # ${quote.symbol.toUpperCase()} ${quote.regularMarketPrice}
+  #### ${new Date().toDateString()}
+  ### ${quote.longName} (${quote.fullExchangeName})
+  `;
   return (
     <>
       {quote ? (
-        <Detail markdown={`# ${symbol.toUpperCase()} ${quote.regularMarketPrice}`} />
+        <Detail
+          markdown={markdown(quote)}
+          navigationTitle={`${quote.symbol}`}
+          actions={
+            <ActionPanel title="Quote menu">
+              <Action.OpenInBrowser
+                title="See on finance.yahoo.com"
+                url={`https://finance.yahoo.com/quote/${quote.symbol}`}
+              />
+            </ActionPanel>
+          }
+          metadata={
+            <Detail.Metadata>
+              <Detail.Metadata.Label
+                title="Today's change"
+                text={{
+                  value: `${quote.regularMarketChangePercent?.toPrecision(4)}%`,
+                  color: quote.regularMarketChangePercent! > 0 ? Color.Green : Color.Red,
+                }}
+              />
+              <Detail.Metadata.Label title="Market cap" text={`$${quote.marketCap?.toLocaleString("en-US")}`} />
+              <Detail.Metadata.Label title="Previous close" text={`${quote.regularMarketPreviousClose}`} />
+              <Detail.Metadata.Label title="Open" text={`${quote.regularMarketOpen}`} />
+              <Detail.Metadata.Label title="Earnings date" text={`${quote.earningsTimestamp?.toLocaleDateString()}`} />
+              <Detail.Metadata.Separator />
+              <Detail.Metadata.Link
+                text={`${quote.symbol}`}
+                target={`https://finance.yahoo.com/quote/${quote.symbol}`}
+                title="Open in yahoo finance"
+              />
+            </Detail.Metadata>
+          }
+        />
       ) : (
         <Detail markdown="Fetching quote..." />
       )}
