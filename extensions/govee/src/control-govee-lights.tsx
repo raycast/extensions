b225d06d364ff1fac1863365a33e41725a2bbfba
j3lte <jwlagendijk@gmail.com>
2024-04-20 00:21:35 +0200

    Add govee extension
    - README
    - Update screenshots
    - Add screenshots
    - Readme
    - Duplicate
    - Refactor
    - Refactor
    - Icon for scenario
    - Updates
    - WIP
    - Execute
    - WIP
    - Clear custom name
    - Custom Naming
    - Branding
    - First setup

---
+++ extensions/govee/src/control-govee-lights.tsx
@@ -0,0 +1,62 @@
+import { useEffect, useMemo } from "react";
+
+import { List } from "@raycast/api";
+
+import useGoveeController from "@/hooks/useGoveeController";
+import useNameMapping from "@/hooks/useNameMapping";
+import useScenarios from "@/hooks/useScenarios";
+
+import ListItem from "@/components/DeviceListItem";
+import ScenarioListItem from "@/components/ScenarioListItem";
+
+export default function ControlGoveeLights() {
+  const { devices, isLoading: isLoadingController, error, executeScenario } = useGoveeController();
+  const { getName, setName } = useNameMapping();
+
+  const scenariosHook = useScenarios();
+  const { scenarios } = scenariosHook;
+
+  const availableDeviceMappings = useMemo(() => {
+    return devices.reduce(
+      (acc, device) => {
+        acc[device.id] = getName(device.id) ?? device.name;
+        return acc;
+      },
+      {} as Record<string, string>,
+    );
+  }, [devices, getName]);
+
+  useEffect(() => {
+    if (error) {
+      console.error("Error with Govee controller", error);
+    }
+  }, [error]);
+
+  return (
+    <List isLoading={isLoadingController} searchBarPlaceholder="Search for Govee lights...">
+      <List.Section title="Scenarios">
+        {scenarios.map((scenario) => (
+          <ScenarioListItem
+            key={scenario.id}
+            scenario={scenario}
+            scenariosHook={scenariosHook}
+            availableDeviceNames={availableDeviceMappings}
+            execute={() => executeScenario({ scenario })}
+          />
+        ))}
+      </List.Section>
+      <List.Section title="Devices">
+        {devices.map((device) => (
+          <ListItem
+            key={device.id}
+            device={device}
+            displayName={getName(device.id)}
+            setName={setName}
+            scenariosHook={scenariosHook}
+            availableDeviceNames={availableDeviceMappings}
+          />
+        ))}
+      </List.Section>
+    </List>
+  );
+}
