ce4229f057f5a7cfb205a9e19d4f762b86159a10
Javier Arce <4933+javierarce@users.noreply.github.com>
2024-04-19 14:54:09 +0200

    Add color-studio-picker extension
    - Removes duplicated colors. Removes console log messages
    - Adds argument
    - Searchs the selected text in the frontmost app
    - Updates asset
    - Display the color as subtitles
    - Adds updated assets
    - Removes comments
    - Adds dropdown
    - Uses types
    - Removes space
    - Adds types
    - Adds missing Blue list of colors
    - Adds metadata folder
    - Replaces screenshot
    - Display the colors in uppercase
    - Adds screenshot and lints code
    - Fixes warnings. Display a HUD instead of a Toast
    - Updates description
    - New icon
    - Removes unused command
    - Sorts the colors
    - Adds description
    - First import
    - first commit

---
+++ extensions/color-studio-picker/src/index.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import { getSelectedText, List } from "@raycast/api";
+import { findColor, useColorSections } from "./hooks";
+import colors from "./colors";
+import { LaunchProps, DropdownProps } from "./types";
+
+const DropdownCategories = ({ categories, setSection }: DropdownProps) => (
+  <List.Dropdown tooltip="Select Category" onChange={setSection}>
+    <List.Dropdown.Item key="all_categories" title="All palettes" value="" />
+    {categories.map((category: string) => (
+      <List.Dropdown.Item key={category} title={category} value={category} />
+    ))}
+  </List.Dropdown>
+);
+
+export default function Command(props: LaunchProps) {
+  const { color } = props.arguments;
+  const [searchText, setSearchText] = useState("");
+  const [section, setSection] = useState("");
+  const { filteredSections, categories } = useColorSections(colors, searchText, section);
+
+  useEffect(() => {
+    async function fetchAndSetSearchText() {
+      let selectedText = "";
+      try {
+        selectedText = await getSelectedText();
+      } catch (error) {
+        /* no-op */
+      }
+
+      const searchTextToUse = color || selectedText || "";
+      setSearchText(searchTextToUse);
+    }
+
+    fetchAndSetSearchText();
+  }, [color]);
+
+  return (
+    <List 
+      searchText={searchText}
+      searchBarPlaceholder="Filter colors by name or HEX code"
+      onSearchTextChange={setSearchText}
+      searchBarAccessory={<DropdownCategories categories={categories} setSection={setSection} />}
+    >
+      {filteredSections.map((section, index) => (
+        <List.Section key={index} title={section.title}>
+          {section.items}
+        </List.Section>
+      ))}
+    </List>
+  );
+}
