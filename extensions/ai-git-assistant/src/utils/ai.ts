2991ee9af0c6ef1e8bc901a768fb117d13600a07
Denis Nykyforov <popalay1@gmail.com>
2024-04-19 14:44:23 +0300

    Fix code style

--- extensions/ai-git-assistant/src/utils/ai.ts
+++ extensions/ai-git-assistant/src/utils/ai.ts
@@ -7,21 +7,21 @@ export async function generateCommitMessage(repositoryPath: string, prompt: stri
   }
 
   const input = buildInputForCommitMessageGeneration(repositoryPath, prompt);
-  return AI.ask(input, { creativity: "none"});
+  return AI.ask(input, { creativity: "none" });
 }
 
 export async function generatePrDescription(
   repositoryPath: string,
   currentBranch: string,
   baseBranch: string,
-  prompt: string
+  prompt: string,
 ) {
   if (!environment.canAccess(AI)) {
     throw Error("You don't have access to AI :(");
   }
 
   const input = buildInputForPrDescriptionGeneration(repositoryPath, currentBranch, baseBranch, prompt);
-  return AI.ask(input, { creativity: "none"});
+  return AI.ask(input, { creativity: "none" });
 }
 
 function buildInputForCommitMessageGeneration(repositoryPath: string, prompt: string) {
@@ -35,7 +35,7 @@ function buildInputForPrDescriptionGeneration(
   repositoryPath: string,
   currentBranch: string,
   baseBranch: string,
-  prompt: string
+  prompt: string,
 ) {
   const diffOutput = getPrDiff(repositoryPath, currentBranch, baseBranch);
 
