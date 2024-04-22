834800c44010a070fdcc066b3778661b31955cb6
Maximilian <85677120+maximilianzuern@users.noreply.github.com>
2024-04-22 13:35:09 +0200

    updated utils and pricing

--- extensions/perplexity-api/src/hook/utils.ts
+++ extensions/perplexity-api/src/hook/utils.ts
@@ -2,13 +2,16 @@ import { encode } from "@nem035/gpt-3-encoder";
 
 export const allModels = [
   { name: "Follow global model", id: "global" },
-  { name: "Mixtral 8x7b 16k", id: "mixtral-8x7b-instruct" },
   { name: "Mistral 7B 16k", id: "mistral-7b-instruct" },
-  { name: "Sonar small 7B 16k", id: "sonar-small-chat" },
-  { name: "Sonar medium 8x7B 16k", id: "sonar-medium-chat" },
+  { name: "Mixtral 8x7b 16k", id: "mixtral-8x7b-instruct" },
+  { name: "Mixtral 8x22b 16k", id: "mixtral-8x22b-instruct" },
+  { name: "Sonar Small 7B 16k", id: "sonar-small-chat" },
+  { name: "Sonar Medium 8x7B 16k", id: "sonar-medium-chat" },
+  { name: "Llama3 8B 8k", id: "llama-3-8b-instruct" },
+  { name: "Llama3 70B 8k", id: "llama-3-70b-instruct" },
   { name: "CodeLlama 70B 16k", id: "codellama-70b-instruct" },
-  { name: "Sonar small 7B Online", id: "sonar-small-online" },
-  { name: "Sonar medium 8x7B Online", id: "sonar-medium-online" },
+  { name: "Sonar Small 7B Online", id: "sonar-small-online" },
+  { name: "Sonar Medium 8x7B Online", id: "sonar-medium-online" },
 ];
 
 export const currentDate = new Date().toLocaleString("en-US", {
@@ -32,6 +35,7 @@ export function estimatePrice(prompt_token: number, output_token: number, model:
   switch (model) {
     case "sonar-small-chat":
     case "mistral-7b-instruct":
+    case "llama-3-8b-instruct":
       price = ((prompt_token * 0.2) / 1_000_000 + (output_token * 0.2) / 1_000_000) * 100;
       break;
     case "mixtral-8x7b-instruct":
@@ -39,6 +43,8 @@ export function estimatePrice(prompt_token: number, output_token: number, model:
       price = ((prompt_token * 0.6) / 1_000_000 + (output_token * 0.6) / 1_000_000) * 100;
       break;
     case "codellama-70b-instruct":
+    case "llama-3-70b-instruct":
+    case "mixtral-8x22b-instruct":
       price = ((prompt_token * 1) / 1_000_000 + (output_token * 1) / 1_000_000) * 100;
       break;
     case "sonar-small-online":
