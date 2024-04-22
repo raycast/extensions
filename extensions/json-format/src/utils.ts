0ba0303ed092a813532d34140276d9b7f568faa5
Benoit Durand <benoit.durand.mail@gmail.com>
2024-04-22 22:58:09 +0200

    Update json-format extension
    - Add format to JSONLines
    - Initial commit

--- extensions/json-format/src/utils.ts
+++ extensions/json-format/src/utils.ts
@@ -90,6 +90,20 @@ function parse(input: string) {
   }
 }
 
+export function formatToJSONLines(input: string) {
+  if (!isJson(input) || !isArray(input)) {
+    showToast({
+      style: Toast.Style.Failure,
+      title: 'Please enter a valid JSON/JS Array Object',
+    });
+    return;
+  }
+
+  let jsonVal = JSON.parse(`{"data":${input}}`);
+  return jsonVal.data.map(JSON.stringify).join('\n');
+}
+
+
 function isJson(str: string) {
   try {
     JSON.parse(str);
@@ -97,4 +111,8 @@ function isJson(str: string) {
     return false;
   }
   return true;
+}
+
+function isArray(str: string) {
+  return str.startsWith('[') && str.endsWith(']');
 }
