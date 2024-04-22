afb36249dd656e0d35a11ae63e234528c4867e7e
Sebastian <sebastian.danielsson@proton.me>
2024-04-22 14:33:36 +0200

    Use the correct preference pane for customization

--- extensions/background-sounds/src/customize.ts
+++ extensions/background-sounds/src/customize.ts
@@ -5,7 +5,7 @@ export default async function main() {
   const res = await runAppleScript(
     `
 tell application "System Events"
-  do shell script "open 'x-apple.systempreferences:com.apple.preference.universalaccess?Hearing'"
+  do shell script "open 'x-apple.systempreferences:com.apple.preference.universalaccess?Audio'"
   return true
 end tell
 `,
