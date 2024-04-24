96d5fc58afeb002840e201e5193fbc8574dbd7b7
Samuel Kraft <samuelkraft@me.com>
2024-04-24 14:15:13 +0200

    Update raycast-explorer extension
    - Add preset command
    - Initial commit

--- extensions/raycast-explorer/src/helpers.ts
+++ extensions/raycast-explorer/src/helpers.ts
@@ -1,6 +1,10 @@
+import { environment } from "@raycast/api";
+
 export const CONTRIBUTE_URL = "https://github.com/raycast/extensions/edit/main/extensions/raycast-explorer";
 
 export function wrapInCodeBlock(text: string, language = "sh") {
   const backticks = "```";
   return `${backticks}${language}\n${text}\n${backticks}`;
 }
+
+export const raycastProtocol = environment.raycastVersion.includes("alpha") ? "raycastinternal://" : "raycast://";
