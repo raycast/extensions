ec7e70ba16adac6e17d32558a95b0926508c0361
Alleluia23 <58318775+Alleluia23@users.noreply.github.com>
2024-04-26 15:50:12 +0100

    change icon size and prettier

--- extensions/libray-occupancy/src/index.tsx
+++ extensions/libray-occupancy/src/index.tsx
@@ -33,44 +33,80 @@ interface RoomDetal {
 }
 
 export default function Command() {
-  const { data: dataPeople, isLoading: loadingPeople } = useFetch<OverviewResponse>("https://www.st-andrews.ac.uk/library/sentry-api/current-occupancy");
-  const { data: dataTime, isLoading: loadingTime } = useFetch<TimeResponse>("https://www.st-andrews.ac.uk/library/sentry-api/average-visit");
-  const { data: dataPC } = useFetch<PCResponse>("https://www.st-andrews.ac.uk/pc-availability/rest/4d8283a9-213d-30be-e054-00144ffbdcf6");
-  
+  const { data: dataPeople, isLoading: loadingPeople } = useFetch<OverviewResponse>(
+    "https://www.st-andrews.ac.uk/library/sentry-api/current-occupancy",
+  );
+  const { data: dataTime, isLoading: loadingTime } = useFetch<TimeResponse>(
+    "https://www.st-andrews.ac.uk/library/sentry-api/average-visit",
+  );
+  const { data: dataPC } = useFetch<PCResponse>(
+    "https://www.st-andrews.ac.uk/pc-availability/rest/4d8283a9-213d-30be-e054-00144ffbdcf6",
+  );
+
   const numberEmojis = ["1️⃣", "2️⃣", "2️⃣", "3️⃣", "4️⃣"];
 
   return (
     <List navigationTitle="Main Library 📖">
       <List.Section title="People in the main library now">
-        <List.Item 
+        <List.Item
           title={"🧑🏻‍🎓  Student"}
           accessories={[
-            { tag: { value: loadingPeople? "Loading..." : `Total Student: ${dataPeople?.student}`, color: Color.Magenta }, tooltip: `Total Student: ${dataPeople?.student}` },
+            {
+              tag: {
+                value: loadingPeople ? "Loading..." : `Total Student: ${dataPeople?.student}`,
+                color: Color.Magenta,
+              },
+              tooltip: `Total Student: ${dataPeople?.student}`,
+            },
           ]}
         />
         <List.Item
           title={"🧑🏻‍💻  Staff"}
           accessories={[
-            { tag: { value: loadingPeople ? "Loading..." : `Total Staff: ${dataPeople?.staff}`, color: Color.Magenta }, tooltip: `Total Staff: ${dataPeople?.staff}` },
+            {
+              tag: { value: loadingPeople ? "Loading..." : `Total Staff: ${dataPeople?.staff}`, color: Color.Magenta },
+              tooltip: `Total Staff: ${dataPeople?.staff}`,
+            },
           ]}
         />
         <List.Item
           title={"🚶🏻  Visitor"}
           accessories={[
-            { tag: { value: loadingPeople ? "Loading..." : `Total Visitor: ${dataPeople?.other}`, color: Color.Magenta }, tooltip: `Total Visitor: ${dataPeople?.other}` },
+            {
+              tag: {
+                value: loadingPeople ? "Loading..." : `Total Visitor: ${dataPeople?.other}`,
+                color: Color.Magenta,
+              },
+              tooltip: `Total Visitor: ${dataPeople?.other}`,
+            },
           ]}
         />
         <List.Item
           title={"👥  Current Total"}
           accessories={[
-            { tag: { value: loadingPeople ? "Loading..." : `Capacity: ${dataPeople?.capacity}`, color: Color.Blue }, tooltip: "Capacity" },
-            { tag: { value: loadingPeople ? "Loading..." : `Current Total: ${dataPeople?.total}`, color: Color.Magenta }, tooltip: "Current Occupancy" },
+            {
+              tag: { value: loadingPeople ? "Loading..." : `Capacity: ${dataPeople?.capacity}`, color: Color.Blue },
+              tooltip: "Capacity",
+            },
+            {
+              tag: {
+                value: loadingPeople ? "Loading..." : `Current Total: ${dataPeople?.total}`,
+                color: Color.Magenta,
+              },
+              tooltip: "Current Occupancy",
+            },
           ]}
         />
         <List.Item
           title={"🕒  Average visit length in last 7 days"}
           accessories={[
-            { tag: { value: loadingTime ? "Loading..." : `${dataTime?.hours}h ${dataTime?.minutes}min`, color: Color.Purple }, tooltip: "Average Time" },
+            {
+              tag: {
+                value: loadingTime ? "Loading..." : `${dataTime?.hours}h ${dataTime?.minutes}min`,
+                color: Color.Purple,
+              },
+              tooltip: "Average Time",
+            },
           ]}
         />
       </List.Section>
@@ -81,8 +117,14 @@ export default function Command() {
             title={`${numberEmojis[index % numberEmojis.length]} ${room.name}`}
             accessories={[
               { tag: { value: `Total: ${room.total}`, color: Color.Blue }, tooltip: `Total PCs: ${room.total}` },
-              { tag: { value: `In Use: ${room.inuse.count}`, color: Color.Magenta }, tooltip: `In Use: ${room.inuse.count}` },
-              { tag: { value: `Available: ${room.available.count}`, color: Color.Green }, tooltip: `Available: ${room.available.count}` },
+              {
+                tag: { value: `In Use: ${room.inuse.count}`, color: Color.Magenta },
+                tooltip: `In Use: ${room.inuse.count}`,
+              },
+              {
+                tag: { value: `Available: ${room.available.count}`, color: Color.Green },
+                tooltip: `Available: ${room.available.count}`,
+              },
             ]}
           />
         ))}
