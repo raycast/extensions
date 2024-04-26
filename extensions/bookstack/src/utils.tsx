11cbc958aca974458bf86b8d059bf0ff285cf04d
Christophe Vanleeuw <christophe.vanleeuw@expliciet.be>
2024-04-27 00:35:47 +0200

    Format code with Prettier

--- extensions/bookstack/src/utils.tsx
+++ extensions/bookstack/src/utils.tsx
@@ -1,5 +1,5 @@
 // utils.tsx
 export const stripHtmlTags = (html: string): string => {
-    const regex = /(<([^>]+)>)/gi;
-    return html.replace(regex, '');
+  const regex = /(<([^>]+)>)/gi;
+  return html.replace(regex, "");
 };
