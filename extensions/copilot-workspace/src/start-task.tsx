454a4b6bbe377c810ed408ae771a69245e021df7
Per Nielsen Tikær <per@raycast.com>
2024-04-29 11:55:30 +0200

    Updated to useForm

--- extensions/copilot-workspace/src/start-task.tsx
+++ extensions/copilot-workspace/src/start-task.tsx
@@ -1,81 +1,56 @@
-import { Action, ActionPanel, Form, open } from "@raycast/api";
-import { useCachedState } from "@raycast/utils";
-import { useState } from "react";
+import { Action, ActionPanel, Form, open, popToRoot } from "@raycast/api";
+import { FormValidation, useCachedState, useForm } from "@raycast/utils";
 import { validateRepo } from "./validate-repo";
 
 export default function Command() {
   const [repo, setRepo] = useCachedState("repo", "");
-  const [taskError, setTaskError] = useState<string | undefined>();
-  const [repoError, setRepoError] = useState<string | undefined>();
 
-  function dropTaskErrorIfNeeded() {
-    if (taskError && taskError.length > 0) {
-      setTaskError(undefined);
-    }
+  interface FormValues {
+    task: string;
+    repo: string;
   }
 
-  function dropRepoErrorIfNeeded() {
-    if (repoError && repoError.length > 0) {
-      setRepoError(undefined);
-    }
-  }
+  const { handleSubmit, itemProps } = useForm<FormValues>({
+    onSubmit(values) {
+      const repo = values.repo || "githubnext/workspace-blank";
+
+      open(
+        `https://copilot-workspace.githubnext.com/${repo}?task=${encodeURIComponent(
+          values.task
+        )}`
+      );
+
+      setRepo(repo);
+      popToRoot();
+    },
+    validation: {
+      task: FormValidation.Required,
+      repo: (value) => {
+        if (value && !validateRepo(value)) {
+          return "Invalid repository. Please provide a valid repository name in the format owner/repo";
+        }
+      },
+    },
+    initialValues: { repo: repo },
+  });
 
   return (
     <Form
       actions={
         <ActionPanel>
-          <Action.SubmitForm
-            onSubmit={(values) => {
-              if (!values.task) {
-                setTaskError("Task cannot be empty");
-                return;
-              }
-
-              const repo = values.repo || "githubnext/workspace-blank";
-
-              open(
-                `https://copilot-workspace.githubnext.com/${repo}?task=${encodeURIComponent(
-                  values.task,
-                )}`,
-              );
-            }}
-          />
+          <Action.SubmitForm title="Submit" onSubmit={handleSubmit} />
         </ActionPanel>
       }
     >
       <Form.TextArea
-        id="task"
         title="Task"
         placeholder="Describe a task..."
-        error={taskError}
-        onChange={dropTaskErrorIfNeeded}
-        onBlur={(event) => {
-          if (!event.target.value) {
-            setTaskError("Task cannot be empty");
-          } else {
-            dropTaskErrorIfNeeded();
-          }
-        }}
+        {...itemProps.task}
       />
       <Form.TextField
-        id="repo"
         title="Repository"
         placeholder="e.g. owner/repo"
-        value={repo}
-        error={repoError}
-        onChange={(value) => {
-          setRepo(value);
-          dropRepoErrorIfNeeded();
-        }}
-        onBlur={(event) => {
-          if (event.target.value && !validateRepo(event.target.value)) {
-            setRepoError(
-              "Invalid repository. Please provide a valid repository name in the format owner/repo",
-            );
-          } else {
-            dropRepoErrorIfNeeded();
-          }
-        }}
+        {...itemProps.repo}
       />
       <Form.Description text="Leave the repository field empty to create a new repository." />
     </Form>
