b8e17b239bff1091b6f2205275a15b5035fe0c75
Talha Eryasar <talha.eryasar@virginmediao2.co.uk>
2024-05-06 14:47:09 +0100

    prettier

--- extensions/medre-se/src/types.tsx
+++ extensions/medre-se/src/types.tsx
@@ -1,75 +1,79 @@
 export enum ResponseType {
-    Definition = "definition",
-    Verse = "verse",
-    Chapter = "chapter",
-    Book = "book",
-    Page = "page",
-    VerseRange = "verse-range",
+  Definition = "definition",
+  Verse = "verse",
+  Chapter = "chapter",
+  Book = "book",
+  Page = "page",
+  VerseRange = "verse-range",
 }
 
 export interface Definition {
-    title: string;
-    summary: string;
-    meaning: string;
-    example: string;
-    reference: string;
+  title: string;
+  summary: string;
+  meaning: string;
+  example: string;
+  reference: string;
 }
 
 export interface Chapter {
-    name: string;
-    info: string;
-    revelation: string;
+  name: string;
+  info: string;
+  revelation: string;
 }
 
 export interface Verse {
-    name: string;
-    chapter: number;
-    order: number;
-    original: string;
-    translation: string;
+  name: string;
+  chapter: number;
+  order: number;
+  original: string;
+  translation: string;
 }
 
 export interface VerseRange {
-    name: string;
-    chapter: number;
-    from: number;
-    to: number;
-    verses: [{
-        original: string;
-        translation: string;
-    }]
+  name: string;
+  chapter: number;
+  from: number;
+  to: number;
+  verses: [
+    {
+      original: string;
+      translation: string;
+    },
+  ];
 }
 
 export interface Book {
-    order: number;
-    fromPage: number;
-    toPage: number;
-    numberOfChapters: number;
-    numberOfVerses: number;
+  order: number;
+  fromPage: number;
+  toPage: number;
+  numberOfChapters: number;
+  numberOfVerses: number;
 }
 
 export interface Page {
-    no: number;
-    content: [{
-        chapter: number;
-        name: string;
-        numberOfVerses: number;
-    }]
+  no: number;
+  content: [
+    {
+      chapter: number;
+      name: string;
+      numberOfVerses: number;
+    },
+  ];
 }
 
 export interface Entry {
-    type: ResponseType;
-    data: Definition | Chapter | Verse | Book | Page | VerseRange;
+  type: ResponseType;
+  data: Definition | Chapter | Verse | Book | Page | VerseRange;
 }
 
 export interface APIResponse {
-    entries: Entry[];
-    limit: number;
-    meta: {
-        hits: number;
-        pages: number;
-        current: number;
-        next?: string;
-        previous?: string;
-    };
+  entries: Entry[];
+  limit: number;
+  meta: {
+    hits: number;
+    pages: number;
+    current: number;
+    next?: string;
+    previous?: string;
+  };
 }
