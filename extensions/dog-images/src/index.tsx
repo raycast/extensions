11ea2f3b4ddb9fefbb2f71c5519e854dd5072d78
Elias Bruhn <elias@lfpost.dk>
2024-05-09 07:06:01 +0200

    Enable fetching image for specific breed

--- extensions/dog-images/src/index.tsx
+++ extensions/dog-images/src/index.tsx
@@ -1,5 +1,7 @@
-import { Action, ActionPanel, Detail } from "@raycast/api";
+import { Action, ActionPanel, List } from "@raycast/api";
 import { useFetch } from "@raycast/utils";
+import { useState } from "react";
+import { breeds } from "./breeds";
 
 export interface State {
   message: string;
@@ -7,19 +9,51 @@ export interface State {
 }
 
 export default () => {
-  const { isLoading, data, revalidate } = useFetch<State>("https://dog.ceo/api/breeds/image/random");
+  const [breed, setBreed] = useState("random");
 
-  const md = !isLoading && data?.message ? `${data.message} \n\n![](${data.message})` : "# Loading...";
+  const apiUrl =
+    breed && breed !== "random"
+      ? `https://dog.ceo/api/breed/${breed}/images/random`
+      : "https://dog.ceo/api/breeds/image/random";
+  const { isLoading, data, revalidate } = useFetch<State>(apiUrl);
+
+  const md = !isLoading && data?.message ? `![](${data.message})\n\n${data.message}` : "# Loading...";
+
+  const setMarkDown = (id: string) => {
+    setBreed((prevValue) => id || prevValue);
+    revalidate();
+  };
 
   return (
-    <Detail
+    <List
+      isShowingDetail={true}
+      searchBarPlaceholder="Search breeds..."
+      onSelectionChange={(id: string | null) => setMarkDown(id || "")}
       isLoading={isLoading}
-      markdown={md}
-      actions={
-        <ActionPanel>
-          <Action title="New Image" onAction={revalidate} />
-        </ActionPanel>
-      }
-    />
+    >
+      <List.Item
+        id="random"
+        title="Random"
+        detail={<List.Item.Detail markdown={md} />}
+        actions={
+          <ActionPanel>
+            <Action title="New Image" onAction={revalidate} />
+          </ActionPanel>
+        }
+      />
+      {breeds.map(({ id, name }) => (
+        <List.Item
+          key={id}
+          id={id}
+          title={name}
+          detail={<List.Item.Detail markdown={md} />}
+          actions={
+            <ActionPanel>
+              <Action title="New Image" onAction={revalidate} />
+            </ActionPanel>
+          }
+        />
+      ))}
+    </List>
   );
 };
