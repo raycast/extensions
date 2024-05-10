e03a006cce4dcd559958ab08ee8584906804d464
Ion Ostafi <ion.ostafi@crunchyroll.com>
2024-05-10 09:24:39 +0300

    Update to the new evolution API

--- extensions/swift-evolution/src/api.ts
+++ extensions/swift-evolution/src/api.ts
@@ -1,34 +1,74 @@
 import fetch from "node-fetch";
 
-const SWIFT_EVOLUTION_PROPOSALS_ENDPOINT = "https://download.swift.org/swift-evolution/proposals.json";
+const SWIFT_EVOLUTION_PROPOSALS_ENDPOINT = "https://download.swift.org/swift-evolution/v1/evolution.json";
 
 export type ProposalJson = {
+  authors: { link: string; name: string }[];
+  discussions: { link: string; name: string }[];
   id: string;
-  title: string;
-  summary: string;
+  link: string;
+  reviewManager: { link: string; name: string };
+  reviewManagers: { link: string; name: string }[];
+  sha: string;
   status: {
     end: string | undefined;
     start: string | undefined;
     state: string;
     version: string | undefined;
   };
-  sha: string;
-  reviewManager: { link: string; name: string };
-  link: string;
-  implementation:
-    | {
-        account: string;
-        id: string;
-        repository: string;
-        type: string;
-      }[]
-    | null;
-  authors: { link: string; name: string }[];
+  summary: string;
+  title: string;
+  trackingBugs: {
+    asignee: string;
+    id: string;
+    link: string;
+    status: string;
+    title: string;
+    updated: string;
+  }[];
+  implementation: {
+    account: string;
+    id: string;
+    repository: string;
+    type: string;
+  }[];
 };
+
+export type EvolutionJson = {
+  commit: string;
+  creationDate: string;
+  implementationVersions: string[];
+  proposals: ProposalJson[];
+};
+
+// export type ProposalJson = {
+//   id: string;
+//   title: string;
+//   summary: string;
+//   status: {
+//     end: string | undefined;
+//     start: string | undefined;
+//     state: string;
+//     version: string | undefined;
+//   };
+//   sha: string;
+//   reviewManager: { link: string; name: string };
+//   link: string;
+//   implementation:
+//     | {
+//         account: string;
+//         id: string;
+//         repository: string;
+//         type: string;
+//       }[]
+//     | null;
+//   authors: { link: string; name: string }[];
+// };
 
 export async function fetchProposals(): Promise<ProposalJson[]> {
   const response = await fetch(SWIFT_EVOLUTION_PROPOSALS_ENDPOINT);
-  return (await response.json()) as ProposalJson[];
+  let evolution = (await response.json()) as EvolutionJson;
+  return evolution.proposals;
 }
 
 export {};
