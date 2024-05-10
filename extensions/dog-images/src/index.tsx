6fbfc466e6bf3a73e68179c4826bb30d80650bfb
Elias Bruhn <elias@lfpost.dk>
2024-05-10 23:05:54 +0200

    Remove URL from markdown, add action to copy URL

--- extensions/dog-images/src/index.tsx
+++ extensions/dog-images/src/index.tsx
@@ -1,4 +1,4 @@
-import { Action, ActionPanel, List } from "@raycast/api";
+import { Action, ActionPanel, Icon, List } from "@raycast/api";
 import { useFetch } from "@raycast/utils";
 import { useState } from "react";
 import { breeds } from "./breeds";
@@ -17,7 +17,7 @@ export default () => {
       : "https://dog.ceo/api/breeds/image/random";
   const { isLoading, data, revalidate } = useFetch<State>(apiUrl);
 
-  const md = !isLoading && data?.message ? `![](${data.message})\n\n${data.message}` : "# Loading...";
+  const md = !isLoading && data?.message ? `![](${data.message}?raycast-height=325)` : "# Loading...";
 
   const setMarkDown = (id: string) => {
     setBreed((prevValue) => id || prevValue);
@@ -37,7 +37,12 @@ export default () => {
         detail={<List.Item.Detail markdown={md} />}
         actions={
           <ActionPanel>
-            <Action title="New Image" onAction={revalidate} />
+            <Action title="New Image" onAction={revalidate} icon={Icon.Image} />
+            <Action.CopyToClipboard
+              title="Copy URL to Clipboard"
+              content={data ? data.message : ""}
+              icon={Icon.CopyClipboard}
+            />
           </ActionPanel>
         }
       />
