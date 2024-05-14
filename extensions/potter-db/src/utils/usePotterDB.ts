dcb55d6b58e72a288f3c1b7024313ae7497c112e
xmok <30526133+xmok@users.noreply.github.com>
2024-05-14 19:54:19 +0500

    [PotterDB] Icon + lint + initial name search

--- extensions/potter-db/src/utils/usePotterDB.ts
+++ extensions/potter-db/src/utils/usePotterDB.ts
@@ -3,12 +3,13 @@ import { SuccessResponse } from "../types";
 import { API_HEADERS, API_URL } from "../constants";
 import { Toast, showToast } from "@raycast/api";
 
-export function usePotterDB<T>(endpoint: string, pluralItem = "") {
-    const { isLoading, data, pagination } = useFetch(
-        (options) =>
-            API_URL + endpoint + "?" +
-        `page[number]=${options.page + 1}`, {
-         headers: API_HEADERS,
+export function usePotterDB<T>(endpoint: string, pluralItem = "", filter?: string) {
+  // We do this instead of URLSearchParams since we explicitly need this type of format: filter[name_cont]=Weasley
+  const filterParam = filter ? `&filter[name_cont]=${filter}` : "";
+  const { isLoading, data, pagination } = useFetch(
+    (options) => API_URL + endpoint + "?" + `page[number]=${options.page + 1}` + filterParam,
+    {
+      headers: API_HEADERS,
       async onWillExecute() {
         await showToast({
           title: `Fetching ${pluralItem}`,
@@ -17,17 +18,18 @@ export function usePotterDB<T>(endpoint: string, pluralItem = "") {
       },
       mapResult(result: SuccessResponse<T>) {
         return {
-            data: result.data,
-            hasMore: ("next" in result.meta.pagination),
+          data: result.data,
+          hasMore: "next" in result.meta.pagination,
         };
       },
-    async onData(data: T[]) {
+      async onData(data: T[]) {
         await showToast({
           title: `Fetched ${data.length} ${pluralItem}`,
           style: Toast.Style.Success,
         });
       },
-    });
+    },
+  );
 
-    return { isLoading, data, pagination };
+  return { isLoading, data, pagination };
 }
