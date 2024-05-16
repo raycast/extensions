d9c18f756e649870ae6275e07914089a0a70aa89
xmok <30526133+xmok@users.noreply.github.com>
2024-05-16 10:03:27 +0500

    [Nobel] 🚀 Initial Commit w/ mostly complete

---
+++ extensions/the-nobel-prize/CHANGELOG.md
@@ -0,0 +1,3 @@
+# The Nobel Prize Changelog
+
+## [Initial Version] - 2024-04-15
---
+++ extensions/the-nobel-prize/src/constants.ts
@@ -0,0 +1,14 @@
+import { Icon } from "@raycast/api";
+
+export const API_BASE_URL = "https://api.nobelprize.org/2.1/";
+export const DEAULT_LIMIT = 25;
+
+export const ICONS = {
+    Chemistry: Icon.Syringe,
+    Economics: Icon.LineChart,
+    Literature: Icon.Book,
+    Peace: Icon.Globe,
+    Physics: Icon.Ruler,
+    Medicine: Icon.Dna,
+    "Physiology or Medicine": Icon.Dna,
+}
