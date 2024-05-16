ce6fe94bac80e0a7820fe7b8727fb847d91206c5
Per Nielsen Tikær <per@raycast.com>
2024-05-16 10:59:52 +0200

    Update upload-files.tsx

--- extensions/uploadthing/src/upload-files.tsx
+++ extensions/uploadthing/src/upload-files.tsx
@@ -1,4 +1,4 @@
-import { Form, ActionPanel, Action } from "@raycast/api";
+import { Form, ActionPanel, Action, showToast, Toast } from "@raycast/api";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useUpload } from "./lib/hooks";
 import { filePathsToFile, guardInvalidApiKey } from "./lib/utils";
@@ -20,6 +20,11 @@ const Command = () => {
   if (keyCheck) return keyCheck;
 
   const handleSubmit = async (values: { files: string[] }) => {
+    if (values.files.length === 0) {
+      showToast(Toast.Style.Failure, "Please select at least one file");
+      return;
+    }
+
     const files = await filePathsToFile(values.files);
     upload(files);
   };
@@ -36,7 +41,7 @@ const Command = () => {
         </ActionPanel>
       }
     >
-      <Form.FilePicker id="files" title="Select file" />
+      <Form.FilePicker id="files" title="Select Files" />
     </Form>
   );
 };
