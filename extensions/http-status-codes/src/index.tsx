6b333637067088eec4f83671e4e1c3af3455c95b
Lucas Mezêncio <lucas.mezencio@gmail.com>
2024-05-17 13:04:03 +0100

    feat: show the http.cat or http.dog detail based on the configuration

--- extensions/http-status-codes/src/index.tsx
+++ extensions/http-status-codes/src/index.tsx
@@ -1,8 +1,9 @@
-import { ActionPanel, Action, Icon, List } from "@raycast/api";
+import { ActionPanel, Action, Icon, List, getPreferenceValues } from "@raycast/api";
 import { Code } from "./types";
 import http from "http";
 import { copyFile, getCodeDocsUrl, getCodeGroupDescription, statusCodeToColor } from "./utils";
 
+const typeOfPerson = getPreferenceValues<ExtensionPreferences>().typeOfPerson;
 
 export default function Command() {
   const codeGroups = Object.entries(http.STATUS_CODES).reduce(
@@ -15,29 +16,52 @@ export default function Command() {
     },
     {},
   );
+  const isNone = typeOfPerson === "none";
+  let searchBarPlaceholder = "Filter by code or description";
+
+  if (!isNone) {
+    searchBarPlaceholder += ` (you're a ${typeOfPerson} person!)`;
+  }
 
   return (
-    <List isLoading={false} searchBarPlaceholder="Filter by code or description...">
+    <List isLoading={false} searchBarPlaceholder={searchBarPlaceholder} isShowingDetail={!isNone}>
       {Object.entries(codeGroups).map(([firstDigit, codes]) => (
         <List.Section key={firstDigit} title={`${firstDigit}xx`} subtitle={getCodeGroupDescription(firstDigit)}>
-          {codes.map((code) => (
-            <List.Item
-              key={code.code}
-              title={code.code}
-              subtitle={code.description}
-              keywords={[code.description || ""]} // make subtitle searchable
-              icon={{
-                source: Icon.Dot,
-                tintColor: statusCodeToColor(code.code),
-              }}
-              actions={
-                <ActionPanel>
-                  <Action.OpenInBrowser url={getCodeDocsUrl(code.code)} />
-                  <Action.CopyToClipboard content={code.code} />
-                </ActionPanel>
-              }
-            />
-          ))}
+          {codes.map((code) => {
+            const fileName = `${code.code}.jpg`;
+            const url = `https://http.${typeOfPerson}/${fileName}`;
+
+            return (
+              <List.Item
+                key={code.code}
+                title={code.code}
+                subtitle={code.description}
+                keywords={[code.description || ""]} // make subtitle searchable
+                icon={{
+                  source: Icon.Dot,
+                  tintColor: statusCodeToColor(code.code),
+                }}
+                detail={!isNone ? <List.Item.Detail markdown={`![Illustration](${url})`} /> : null}
+                actions={
+                  <ActionPanel>
+                    <Action.OpenInBrowser url={getCodeDocsUrl(code.code)} />
+                    <Action.CopyToClipboard title="Copy Code" content={code.code} />
+                    {!isNone ? (
+                      <Action
+                        icon={Icon.Clipboard}
+                        key="copyFile"
+                        title="Copy Image"
+                        onAction={() => {
+                          copyFile(url, fileName);
+                        }}
+                        shortcut={{ modifiers: ["cmd", "opt"], key: "c" }}
+                      />
+                    ) : null}
+                  </ActionPanel>
+                }
+              />
+            );
+          })}
         </List.Section>
       ))}
     </List>
--- extensions/http-status-codes/src/utils.ts
+++ extensions/http-status-codes/src/utils.ts
@@ -1,4 +1,70 @@
-import { Color } from "@raycast/api";
+import { Clipboard, Color, Toast, showHUD, showToast } from "@raycast/api";
+import { runAppleScript } from "@raycast/utils";
+import tempy, { FileOptions } from "tempy";
+import fetch from "node-fetch";
+import path from "path";
+
+const copyFileToClipboard = async (url: string, name: string) => {
+  const response = await fetch(url);
+
+  if (response.status !== 200) {
+    throw new Error(`File download failed. Server responded with ${response.status} status.`);
+  }
+
+  if (response.body === null) {
+    throw new Error("Unable to read image response.");
+  }
+
+  const tempyOpt: FileOptions = { name };
+  let file: string;
+
+  try {
+    file = await tempy.write(await response.body, tempyOpt);
+  } catch (e) {
+    const error = e as Error;
+
+    throw new Error(`Failed to download image: "${error.message}".`);
+  }
+
+  try {
+    await runAppleScript(`tell app "Finder" to set the clipboard to ( POSIX file "${file}" )`);
+  } catch (e) {
+    const error = e as Error;
+
+    throw new Error(`Failed to copy image: "${error.message}"`);
+  }
+
+  return path.basename(file);
+};
+
+export const copyFile = (url: string, fileName: string) => {
+  showToast({
+    style: Toast.Style.Animated,
+    title: "Copying...",
+  })
+    .then(async (toast) => {
+      return await copyFileToClipboard(url, fileName).then((file) => {
+        toast.hide();
+        showHUD(`Copied "${file}" to clipboard`);
+      });
+    })
+    .catch((e: Error) => {
+      console.error(e);
+
+      showToast({
+        style: Toast.Style.Failure,
+        title: "Error, please try again",
+        message: e?.message,
+        primaryAction: {
+          title: "Copy Error Message",
+          onAction: (toast) => {
+            Clipboard.copy(toast.message ?? "");
+          },
+          shortcut: { modifiers: ["cmd"], key: "c" },
+        },
+      });
+    });
+};
 
 export const statusCodeToColor = (status: string): Color => {
   return (
