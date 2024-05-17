e55579c77161d47f9d8095491620ffbefbaf1fe3
Ben Keys <bkeys818@gmail.com>
2024-05-17 11:40:02 -0400

    🐛 Default quicklinks to show all properties

--- extensions/notion/src/components/forms/CreatePageForm.tsx
+++ extensions/notion/src/components/forms/CreatePageForm.tsx
@@ -159,7 +159,7 @@ export function CreatePageForm({ mutate, launchContext, defaults }: CreatePageFo
 
   function getQuicklink(): Quicklink {
     const url = "raycast://extensions/HenriChabrand/notion/create-database-page";
-    const launchContext: LaunchContext = { defaults: values, visiblePropIds: visiblePropIds ?? [] };
+    const launchContext: LaunchContext = { defaults: values, visiblePropIds: visiblePropIds ?? databasePropertyIds };
     let name: string | undefined;
     const databaseTitle = databases.find((d) => d.id == databaseId)?.title;
     if (databaseTitle) name = "Create new page in " + databaseTitle;
