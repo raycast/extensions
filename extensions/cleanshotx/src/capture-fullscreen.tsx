0b9ad53227749926ef96e447954e800a451190a7
erics118 <52634785+erics118@users.noreply.github.com>
2024-05-17 11:07:51 -0400

    chore: lint

--- extensions/cleanshotx/src/capture-fullscreen.tsx
+++ extensions/cleanshotx/src/capture-fullscreen.tsx
@@ -3,7 +3,6 @@ import { LaunchProps, closeMainWindow, open } from "@raycast/api";
 export default async function Command(props: LaunchProps<{ arguments: Arguments.CaptureFullscreen }>) {
   const url = "cleanshot://capture-fullscreen";
   await closeMainWindow();
-  if (props.arguments?.action)
-    open(url + "?action=" + props.arguments.action)
-  else open(url)
+  if (props.arguments?.action) open(url + "?action=" + props.arguments.action);
+  else open(url);
 }
