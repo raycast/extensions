ac2a9d9ab41fd557cbf7147dac2c1352efd246f8
xmok <30526133+xmok@users.noreply.github.com>
2024-05-17 16:09:58 +0500

    [Horoscope] feat: add DEFAULT_SIGN in Preferences

--- extensions/horoscope/src/monthly-horoscope.tsx
+++ extensions/horoscope/src/monthly-horoscope.tsx
@@ -1,12 +1,12 @@
 import { Detail, LaunchProps } from "@raycast/api";
 import { useFetch } from "@raycast/utils";
 import { type MonthlyHoroscope } from "./types";
-import { API_URL } from "./constants";
+import { API_URL, DEFAULT_SIGN } from "./constants";
 import generateSignMarkdown from "./utils/generateSignMarkdown";
 import HoroscopeActions from "./components/HoroscopeActions";
 
 export default function MonthlyHoroscope(props: LaunchProps<{ arguments: Arguments.MonthlyHoroscope }>) {
-  const { sign } = props.arguments;
+  const sign = props.arguments.sign || DEFAULT_SIGN;
 
   const params = new URLSearchParams({ sign });
   const { data, isLoading } = useFetch<MonthlyHoroscope>(API_URL + `monthly?${params}`);
@@ -15,6 +15,6 @@ export default function MonthlyHoroscope(props: LaunchProps<{ arguments: Argumen
     generateSignMarkdown(sign) +
     (!data
       ? ""
-      : `${data.data.month} \n\n Challenging Days: ${data.data.challenging_days} \n\n Standout Days: ${data.data.standout_days} \n\n ${data.data.horoscope_data}`);
+      : `## ${data.data.month} \n\n Challenging Days: ${data.data.challenging_days} \n\n Standout Days: ${data.data.standout_days} \n\n ${data.data.horoscope_data}`);
   return <Detail isLoading={isLoading} markdown={markdown} actions={<HoroscopeActions data={data} />} />;
 }
