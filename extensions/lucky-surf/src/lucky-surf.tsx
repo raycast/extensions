d0aaa63e28ffdb2ed7a639fe5659f6c4b1b6c1c2
Denis Nykyforov <popalay1@gmail.com>
2024-05-18 12:22:52 +0300

    Open url with a default browser

--- extensions/lucky-surf/src/lucky-surf.tsx
+++ extensions/lucky-surf/src/lucky-surf.tsx
@@ -1,15 +1,13 @@
-import { Toast, getPreferenceValues, LaunchProps, open, showToast } from "@raycast/api";
+import { Toast, LaunchProps, open, showToast } from "@raycast/api";
 import { getUrl } from "./utils";
 
 export default async function Command(props: LaunchProps<{ arguments: Arguments.LuckySurf }>) {
-  const { preferredBrowser } = getPreferenceValues<ExtensionPreferences>();
-
   const queryArgument = props.arguments.query;
   try {
     const url = await getUrl(queryArgument);
 
-    // Open the URL with the preferred browser
-    await open(url, preferredBrowser);
+    // Open the URL with the default browser
+    await open(url);
   } catch (error) {
     await showToast({
       style: Toast.Style.Failure,
