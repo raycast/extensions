808ce2c060d78cfeb16e7067460227bd358da5e8
xilopaint <arthurpinheiro@protonmail.com>
2024-05-21 12:12:12 -0300

    Updare Coffee extension

--- extensions/coffee/src/caffeinateFor.tsx
+++ extensions/coffee/src/caffeinateFor.tsx
@@ -2,14 +2,13 @@ import { showToast, Toast } from "@raycast/api";
 import { startCaffeinate } from "./utils";
 
 interface Arguments {
-  hours?: string;
-  minutes?: string;
-  seconds?: string;
+  hours: string;
+  minutes: string;
+  seconds: string;
 }
 
 export default async function Command(props: { arguments: Arguments }) {
   const { hours, minutes, seconds } = props.arguments;
-
   const hasValue = hours || minutes || seconds;
 
   if (!hasValue) {
@@ -17,29 +16,22 @@ export default async function Command(props: { arguments: Arguments }) {
     return;
   }
 
-  const parsedHours = parseInt(hours || "0", 10);
-  const parsedMinutes = parseInt(minutes || "0", 10);
-  const parsedSeconds = parseInt(seconds || "0", 10);
-
   const validInput =
-    (!hours || (Number.isInteger(parsedHours) && parsedHours >= 0)) &&
-    (!minutes || (Number.isInteger(parsedMinutes) && parsedMinutes >= 0)) &&
-    (!seconds || (Number.isInteger(parsedSeconds) && parsedSeconds >= 0));
+    (!hours || (Number.isInteger(Number(hours)) && Number(hours) >= 0)) &&
+    (!minutes || (Number.isInteger(Number(minutes)) && Number(minutes) >= 0)) &&
+    (!seconds || (Number.isInteger(Number(seconds)) && Number(seconds) >= 0));
 
   if (!validInput) {
     await showToast(Toast.Style.Failure, "Please ensure all arguments are whole numbers");
     return;
   }
 
-  const totalSeconds = parsedHours * 3600 + parsedMinutes * 60 + parsedSeconds;
-  const caffeinateString =
-    `${parsedHours > 0 ? `${parsedHours}h` : ""}` +
-    `${parsedMinutes > 0 ? `${parsedMinutes}m` : ""}` +
-    `${parsedSeconds > 0 ? `${parsedSeconds}s` : ""}`;
+  const totalSeconds = Number(hours) * 3600 + Number(minutes) * 60 + Number(seconds);
+  const formattedTime = `${hours ? `${hours}h` : ""}${minutes ? `${minutes}m` : ""}${seconds ? `${seconds}s` : ""}`;
 
   await startCaffeinate(
     { menubar: true, status: true },
-    `Caffeinating your Mac for ${caffeinateString}`,
+    `Caffeinating your Mac for ${formattedTime}`,
     `-t ${totalSeconds}`,
   );
 }
