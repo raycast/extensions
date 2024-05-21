13cab3bd4efc191be42383473267dfad4c4f81f1
Shun Tedokon <shun@tedokon.com>
2024-05-22 00:11:59 +0900

    feat: add support for retrieving active tab URL in Firefox

--- extensions/raindrop-io/src/hooks/useBrowserLink.ts
+++ extensions/raindrop-io/src/hooks/useBrowserLink.ts
@@ -13,6 +13,20 @@ export function useBrowserLink() {
           return runAppleScript(`tell application "Google Chrome" to return URL of active tab of front window`);
         case "com.apple.Safari":
           return runAppleScript(`tell application "Safari" to return URL of front document`);
+        case "org.mozilla.firefox":
+          return runAppleScript(`
+            tell application "System Events"
+              set firefox to application process "Firefox"
+
+              -- HACK: It is important to get the list of UI elements; otherwise, we get an error
+              get properties of firefox
+
+              set frontWindow to front window of firefox
+              set firstGroup to first group of frontWindow
+              set navigation to toolbar "Navigation" of firstGroup
+              get value of UI element 1 of combo box 1 of navigation
+            end tell
+          `);
         default:
           throw new Error(`Unsupported App: ${app.name}`);
       }
