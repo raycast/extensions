6b6c3e243822ee400744916f2a49bf19cd4df677
sitogi <takano162@gmail.com>
2024-05-21 21:54:23 +0900

    Refactor genre selection to use major categories and checkboxes
    -  Updated `search-program-by-genre.tsx` to group genres by major categories and use checkboxes for selection.
    -  Modified `onSubmit` function to handle the new genre selection method.
    -  Simplified genre major category labels in `types.ts`.

--- extensions/nhk-program-search/src/search-program-by-genre.tsx
+++ extensions/nhk-program-search/src/search-program-by-genre.tsx
@@ -1,7 +1,7 @@
 import { Action, ActionPanel, Form, useNavigation } from "@raycast/api";
 import React from "react";
 import { ProgramList } from "./components/ProgramList";
-import { genreLabels } from "./types";
+import { genreLabels, genreMajorCategories, GenreMajorCategory, genreMajorCategoryLabels } from "./types";
 
 export default function Command() {
   const { push } = useNavigation();
@@ -12,20 +12,34 @@ export default function Command() {
         <ActionPanel>
           <Action.SubmitForm
             title="Search Programs"
-            onSubmit={async (input) => {
+            onSubmit={(values) => {
+              const selectedGenres = Object.entries(values)
+                .filter(([, value]) => value)
+                .map(([key]) => key);
               push(
-                <ProgramList customFilters={[(p) => p.genres.some((g) => input.genres.includes(g))]} canSelectAll />,
+                <ProgramList customFilters={[(p) => p.genres.some((g) => selectedGenres.includes(g))]} canSelectAll />,
               );
             }}
           />
         </ActionPanel>
       }
     >
-      <Form.TagPicker id="genres" title="Genres" defaultValue={[]} storeValue>
-        {Array.from(genreLabels).map(([key, label]) => (
-          <Form.TagPicker.Item key={key} value={key} title={label} />
-        ))}
-      </Form.TagPicker>
+      {genreMajorCategories.map((majorCategory) => (
+        <GenreDescriptionAndList key={majorCategory} majorCategory={majorCategory} />
+      ))}
     </Form>
+  );
+}
+
+function GenreDescriptionAndList({ majorCategory }: { majorCategory: GenreMajorCategory }) {
+  return (
+    <>
+      <Form.Description text={genreMajorCategoryLabels.get(majorCategory) ?? ""} />
+      {Array.from(genreLabels)
+        .filter(([key]) => key.startsWith(majorCategory))
+        .map(([key, label]) => (
+          <Form.Checkbox key={key} id={key} label={label} defaultValue={false} />
+        ))}
+    </>
   );
 }
