32cf4965a92594d67f99fcb83a0ff6f618a6829b
LitoMore <litomore@gmail.com>
2024-05-25 04:06:11 +0800

    Fix port

--- extensions/pm2/assets/pm2-wrapper/example.js
+++ extensions/pm2/assets/pm2-wrapper/example.js
@@ -5,6 +5,6 @@ const server = createServer((req, res) => {
   res.end("Hello Raycast PM2!\n");
 });
 
-server.listen(3000, "0.0.0.0", () => {
+server.listen(3000, "127.0.0.1", () => {
   console.log("Listening on 127.0.0.1:3000");
 });
