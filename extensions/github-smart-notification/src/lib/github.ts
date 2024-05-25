519e7ad22a8a04fc486c5777beeebead92256fab
ShoppingJaws <25874783+shoppingjaws@users.noreply.github.com>
2024-05-25 18:22:58 +0900

    update query

--- extensions/github-smart-notification/src/lib/github.ts
+++ extensions/github-smart-notification/src/lib/github.ts
@@ -4,6 +4,6 @@ import { execAsync } from "./util";
 
 export const toHtmlUrl = async (n: Notification) => {
   const url = n.subject.latest_comment_url ?? n.subject.url;
-  if (url === null) return "https://github.com/notifications?query=is%3Aunread";
+  if (url === null) return "https://github.com/notifications";
   return (await execAsync(`${ghCommandPath} api ${url} --jq '.html_url'`)).stdout;
 };
