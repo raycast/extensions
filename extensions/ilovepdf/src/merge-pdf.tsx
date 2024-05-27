5a8d02e5491a3f2f66f54d9c0de0d8045e78f2e4
Mohamed Mahmoud <mohamedkhaled.kh98@gmail.com>
2024-05-28 00:08:09 +0100

    feat: add merge pdf command

---
+++ extensions/ilovepdf/src/merge-pdf.tsx
@@ -0,0 +1,137 @@
+import {
+  Form,
+  ActionPanel,
+  Action,
+  showToast,
+  getPreferenceValues,
+  Toast,
+  closeMainWindow,
+  open,
+  openExtensionPreferences,
+} from "@raycast/api";
+import ILovePDFApi from "@ilovepdf/ilovepdf-nodejs";
+import MergeTask from "@ilovepdf/ilovepdf-js-core/tasks/MergeTask";
+import ILovePDFFile from "@ilovepdf/ilovepdf-nodejs/ILovePDFFile";
+import { useState } from "react";
+import fs from "fs";
+import path from "path";
+import { getFilePath } from "./common/utils";
+
+type Values = {
+  files: string[];
+};
+
+type Status = "init" | "success" | "failure";
+
+const { APIPublicKey: publicKey, APISecretKey: secretKey, OpenNow: openNow } = getPreferenceValues<Preferences>();
+
+function getDestinationFile(files: string[]): string {
+  if (!files.length) {
+    return "";
+  }
+  const file = files[0];
+  const directory = path.dirname(file);
+  return getFilePath(directory, `merged.pdf`);
+}
+
+export default function Command() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [status, setStatus] = useState<Status>("init");
+  const [destinationFilePath, setDestinationFilePath] = useState<string>("");
+
+  async function handleSubmit(values: Values) {
+    setIsLoading(true);
+    if (!values.files.length) {
+      await showToast(Toast.Style.Failure, "You must select at least a single pdf file", "Please select a file");
+      setStatus("failure");
+      setIsLoading(false);
+      return;
+    }
+
+    const toast = await showToast(Toast.Style.Animated, "Processing", "Merging PDF...");
+
+    let destinationFile = "";
+    const instance = new ILovePDFApi(publicKey, secretKey);
+    const task = instance.newTask("merge") as MergeTask;
+    const addedFilesPromises = [];
+    try {
+      await task.start();
+      for (const file of values.files) {
+        const fileExtension = path.extname(file);
+        if (fileExtension != ".pdf") {
+          toast.style = Toast.Style.Failure;
+          toast.title = "failure";
+          toast.message = "You must select PDF files.";
+          setStatus("failure");
+          setIsLoading(false);
+          console.log(`file is not a PDF received extension is ${fileExtension}`);
+          return;
+        }
+        const iLovePdfFile = new ILovePDFFile(file);
+        addedFilesPromises.push(task.addFile(iLovePdfFile));
+      }
+      destinationFile = getDestinationFile(values.files);
+
+      await Promise.all(addedFilesPromises);
+      setDestinationFilePath(destinationFile);
+      await task.process();
+      const data = await task.download();
+      fs.writeFileSync(destinationFile, data);
+
+      toast.style = Toast.Style.Success;
+      toast.title = "success";
+      toast.message = "Files merged successfully.";
+      setStatus("success");
+      setIsLoading(false);
+    } catch (error) {
+      toast.style = Toast.Style.Failure;
+      toast.title = "failure";
+      toast.message = "Error happened during merging the files.";
+      setStatus("failure");
+      setIsLoading(false);
+      console.log(error);
+      return;
+    }
+
+    if (openNow) {
+      await closeMainWindow();
+      open(destinationFile);
+    } else {
+      toast.primaryAction = {
+        title: "Open File",
+        onAction: () => {
+          open(destinationFile);
+        },
+      };
+    }
+  }
+
+  return (
+    <Form
+      enableDrafts
+      actions={
+        <ActionPanel>
+          <Action.SubmitForm title="Merge PDFs" onSubmit={handleSubmit} />
+          {status == "success" ? <Action.ShowInFinder title="Show in Finder" path={destinationFilePath} /> : null}
+          {status == "success" ? (
+            <Action.CopyToClipboard title="Copy Path to Clipboard" content={destinationFilePath} />
+          ) : null}
+          {status == "success" ? (
+            <Action
+              title="Open File"
+              onAction={() => {
+                open(destinationFilePath);
+              }}
+            />
+          ) : null}
+          {status == "failure" ? (
+            <Action title="Open Extension Preferences" onAction={openExtensionPreferences} />
+          ) : null}
+        </ActionPanel>
+      }
+      isLoading={isLoading}
+    >
+      <Form.FilePicker id="files" title="Choose PDF Files" allowMultipleSelection={true} />
+    </Form>
+  );
+}
