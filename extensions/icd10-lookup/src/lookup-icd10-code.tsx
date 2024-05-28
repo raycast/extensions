bcfd3e44d8123b38ba1cf5e4ca389409a625b8cb
Per Nielsen Tikær <per@raycast.com>
2024-05-28 10:39:53 +0200

    Update

--- extensions/icd10-lookup/src/lookup-icd10-code.tsx
+++ extensions/icd10-lookup/src/lookup-icd10-code.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import { ActionPanel, List, Action, showToast, ToastStyle } from "@raycast/api";
-import axios from "axios";
+import { ActionPanel, List, Action } from "@raycast/api";
+import { useFetch } from "@raycast/utils";
 
 interface ICD10Response {
   [index: number]: [string, string][];
@@ -9,50 +9,45 @@ interface ICD10Response {
 
 export default function Command() {
   const [searchText, setSearchText] = useState("");
-  const [results, setResults] = useState<{ code: string; desc: string }[]>([]);
+  const { isLoading, data } = useFetch(
+    `https://clinicaltables.nlm.nih.gov/api/icd10cm/v3/search?sf=code,name&terms=${searchText}&maxList=1000`,
+    {
+      execute: searchText.length > 0,
+      keepPreviousData: false,
+      async parseResponse(response) {
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
 
-  const handleSearch = async (query: string) => {
-    if (query.length < 3) {
-      setResults([]);
-      return;
-    }
+        const data = (await response.json()) as ICD10Response;
+        const codes = data[3].map((item) => ({
+          code: item[0],
+          desc: item[1],
+        }));
 
-    try {
-      const response = await axios.get<ICD10Response>(
-        `https://clinicaltables.nlm.nih.gov/api/icd10cm/v3/search?sf=code,name&terms=${query}&maxList=1000`,
-      );
-      const data = response.data[3];
-      const codes = data.map((item) => ({
-        code: item[0],
-        desc: item[1],
-      }));
-      setResults(codes);
-    } catch (error: unknown) {
-      if (axios.isAxiosError(error)) {
-        showToast(ToastStyle.Failure, "Failed to fetch data", error.message);
-      } else {
-        showToast(ToastStyle.Failure, "Failed to fetch data", "An unknown error occurred.");
-      }
-    }
-  };
+        return codes;
+      },
+    },
+  );
 
   return (
     <List
       searchBarPlaceholder="Type to search for ICD-10 codes..."
-      onSearchTextChange={(query) => {
-        setSearchText(query);
-        handleSearch(query);
-      }}
-      isLoading={searchText.length >= 3 && results.length === 0}
+      isLoading={isLoading}
+      searchText={searchText}
+      onSearchTextChange={setSearchText}
+      throttle
     >
-      {results.map((result) => (
+      <List.EmptyView title="Type to Search (e.g. E11.9)" />
+      {data?.map((result) => (
         <List.Item
           key={result.code}
           title={result.code}
           subtitle={result.desc}
           actions={
             <ActionPanel>
-              <Action.CopyToClipboard content={result.code} />
+              <Action.CopyToClipboard title="Copy Code to Clipboard" content={result.code} />
+              <Action.CopyToClipboard title="Copy Description to Clipboard" content={result.desc} />
             </ActionPanel>
           }
         />
