7f3c491a58793c31f9e5e98ff439ecbc3fe83f41
fabien-ch <fabienchanfreau@hotmail.com>
2024-05-28 23:10:55 +0200

    update description

--- extensions/keyboard-win-mac-switch/src/switch-mac-keyboard.ts
+++ extensions/keyboard-win-mac-switch/src/switch-mac-keyboard.ts
@@ -2,16 +2,16 @@ import { exec } from "child_process";
 import { showHUD } from "@raycast/api";
 
 export default function command() {
-    exec(`hidutil property --set '{"UserKeyMapping": []}'`, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.error(`Error: ${stderr}`);
-            return;
-        }
-        console.log(`Output: ${stdout}`);
-    });
-    showHUD(`✅ Mac keyboard`);
+  exec(`hidutil property --set '{"UserKeyMapping": []}'`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`Error: ${stderr}`);
+      return;
+    }
+    console.log(`Output: ${stdout}`);
+  });
+  showHUD(`✅ Mac keyboard`);
 }
--- extensions/keyboard-win-mac-switch/src/switch-window-keyboard.ts
+++ extensions/keyboard-win-mac-switch/src/switch-window-keyboard.ts
@@ -1,8 +1,8 @@
 import { exec } from "child_process";
 import { showHUD } from "@raycast/api";
 export default function command() {
-
-  exec(`hidutil property --set '{"UserKeyMapping":[
+  exec(
+    `hidutil property --set '{"UserKeyMapping":[
   {
     "HIDKeyboardModifierMappingSrc": 0x7000000E3,
     "HIDKeyboardModifierMappingDst": 0x7000000E2
@@ -11,16 +11,18 @@ export default function command() {
     "HIDKeyboardModifierMappingSrc": 0x7000000E2,
     "HIDKeyboardModifierMappingDst": 0x7000000E3
   }
-]}'`, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      console.error(`Error: ${stderr}`);
-      return;
-    }
-    console.log(`Output: ${stdout}`);
-  });
+]}'`,
+    (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.error(`Error: ${stderr}`);
+        return;
+      }
+      console.log(`Output: ${stdout}`);
+    },
+  );
   showHUD(`✅ Window keyboard`);
 }
