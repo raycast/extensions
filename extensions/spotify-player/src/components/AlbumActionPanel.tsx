757631ce332a05f6b31545ccb6502a841099623f
Pavel Mikhaylov <mikhailov.sztu@gmail.com>
2024-05-28 21:30:31 +0400

    improve actions

--- extensions/spotify-player/src/components/AlbumActionPanel.tsx
+++ extensions/spotify-player/src/components/AlbumActionPanel.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import { Action, ActionPanel, Icon, popToRoot, showHUD } from "@raycast/api";
 import { SimplifiedAlbumObject } from "../helpers/spotify.api";
 import { FooterAction } from "./FooterAction";
@@ -6,11 +7,37 @@ import { TracksList } from "./TracksList";
 import { getErrorMessage } from "../helpers/getError";
 import { addToMySavedAlbums } from "../api/addToMySavedAlbums";
 import { removeFromMySavedAlbums } from "../api/removeFromMySavedAlbums";
+import { getMySavedAlbums } from "../api/getMySavedAlbums";
 
 type AlbumActionPanelProps = { album: SimplifiedAlbumObject };
 
 export function AlbumActionPanel({ album }: AlbumActionPanelProps) {
-  const title = album.name;
+  const [isAlbumSaved, setIsAlbumSaved] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function checkIfAlbumSaved() {
+      try {
+        const { items: savedAlbums } = await getMySavedAlbums();
+        const isSaved = savedAlbums.some((savedAlbum) => savedAlbum.id === album.id);
+        setIsAlbumSaved(isSaved);
+      } catch (error) {
+        await showHUD(getErrorMessage(error));
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    checkIfAlbumSaved().then();
+  }, [album.id]);
+
+  if (isLoading) {
+    return (
+      <ActionPanel title="Loading...">
+        <Action title="Loading..." />
+      </ActionPanel>
+    );
+  }
 
   return (
     <ActionPanel>
@@ -21,43 +48,45 @@ export function AlbumActionPanel({ album }: AlbumActionPanelProps) {
         shortcut={{ modifiers: ["cmd", "shift"], key: "a" }}
         target={<TracksList album={album} showGoToAlbum={false} />}
       />
-      <Action
-        icon={Icon.Plus}
-        title="Add To Library"
-        shortcut={{ modifiers: ["cmd", "shift"], key: "s" }}
-        onAction={async () => {
-          try {
-            await addToMySavedAlbums({
-              albumIds: album.id ? [album.id] : [],
-            });
-            await showHUD("Album added to the library");
-            await popToRoot();
-            return;
-          } catch (err) {
-            const error = getErrorMessage(err);
-            await showHUD(error);
-          }
-        }}
-      />
-      <Action
-        icon={Icon.Minus}
-        title="Remove From Library"
-        shortcut={{ modifiers: ["cmd", "shift"], key: "d" }}
-        onAction={async () => {
-          try {
-            await removeFromMySavedAlbums({
-              albumIds: album.id ? [album.id] : [],
-            });
-            await showHUD("Album removed from the library");
-            await popToRoot();
-            return;
-          } catch (err) {
-            const error = getErrorMessage(err);
-            await showHUD(error);
-          }
-        }}
-      />
-      <FooterAction url={album?.external_urls?.spotify} uri={album.uri} title={title} />
+      {!isAlbumSaved && (
+        <Action
+          icon={Icon.Plus}
+          title="Add To Library"
+          shortcut={{ modifiers: ["cmd", "shift"], key: "s" }}
+          onAction={async () => {
+            try {
+              await addToMySavedAlbums({ albumIds: [album.id] });
+              await showHUD("Album added to the library");
+              setIsAlbumSaved(true);
+              await popToRoot();
+              return;
+            } catch (err) {
+              const error = getErrorMessage(err);
+              await showHUD(error);
+            }
+          }}
+        />
+      )}
+      {isAlbumSaved && (
+        <Action
+          icon={Icon.Minus}
+          title="Remove From Library"
+          shortcut={{ modifiers: ["cmd", "shift"], key: "d" }}
+          onAction={async () => {
+            try {
+              await removeFromMySavedAlbums({ albumIds: [album.id] });
+              await showHUD("Album removed from the library");
+              setIsAlbumSaved(false);
+              await popToRoot();
+              return;
+            } catch (err) {
+              const error = getErrorMessage(err);
+              await showHUD(error);
+            }
+          }}
+        />
+      )}
+      <FooterAction url={album?.external_urls?.spotify} uri={album.uri} title={album.name} />
     </ActionPanel>
   );
 }
