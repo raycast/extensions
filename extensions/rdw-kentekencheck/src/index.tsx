1bd5f56c84db06e29322214ddaff2b11dbf6fcd7
Jeffrey van Hees <jeffrey@emeals.nl>
2024-05-31 14:04:24 +0200

    Linting

--- extensions/rdw-kentekencheck/src/index.tsx
+++ extensions/rdw-kentekencheck/src/index.tsx
@@ -29,28 +29,32 @@ interface Car {
  * Function to convert a date string to a locale date string
  * @param value
  */
-const toDate = (value?: string) => value && !isNaN(Date.parse(value)) ? new Date(value).toLocaleDateString() : "Unknown";
+const toDate = (value?: string) =>
+  value && !isNaN(Date.parse(value)) ? new Date(value).toLocaleDateString() : "Unknown";
 
 /**
  * Function to convert a number to a centimeter
  * @param value
  */
-const toCentimeters = (value?: number) => value && !isNaN(value) ? `${value.toLocaleString()}cm` : "Unknown";
+const toCentimeters = (value?: number) => (value && !isNaN(value) ? `${value.toLocaleString()}cm` : "Unknown");
 
 /**
  * Function to convert a number to a kilogram
  * @param value
  */
-const toKilograms = (value?: number) => value && !isNaN(value) ? `${value.toLocaleString()}kg` : "Unknown";
+const toKilograms = (value?: number) => (value && !isNaN(value) ? `${value.toLocaleString()}kg` : "Unknown");
 
 /**
  * Function to convert a number to a euro currency
  * @param value
  */
-const toEuro = (value?: number) => value && !isNaN(value) ? new Intl.NumberFormat("nl-NL", {
-  style: "currency",
-  currency: "EUR"
-}).format(value) : "Unknown";
+const toEuro = (value?: number) =>
+  value && !isNaN(value)
+    ? new Intl.NumberFormat("nl-NL", {
+        style: "currency",
+        currency: "EUR",
+      }).format(value)
+    : "Unknown";
 
 export default function Command() {
   const [searchText, setSearchText] = useState("");
@@ -68,22 +72,30 @@ export default function Command() {
     const licence = keyword.replace(/\s/g, "");
 
     // Return the URL to fetch the data from
-    return `https://opendata.rdw.nl/resource/m9d7-ebf2.json?`
-      + `$select=merk,kenteken,handelsbenaming,datum_eerste_toelating_dt,datum_eerste_tenaamstelling_in_nederland_dt,vervaldatum_apk_dt,eerste_kleur,bruto_bpm,catalogusprijs,aantal_cilinders,cilinderinhoud,`
-      + `lengte,breedte,massa_ledig_voertuig,toegestane_maximum_massa_voertuig,massa_rijklaar,maximum_massa_trekken_ongeremd,maximum_trekken_massa_geremd,`
-      + `wam_verzekerd,tellerstandoordeel`
-      + `&$where=kenteken LIKE '${licence}%25' or merk LIKE '%25${keyword}%25' or handelsbenaming LIKE '%25${keyword}%25'`
-      + `&$limit=25`;
+    return (
+      `https://opendata.rdw.nl/resource/m9d7-ebf2.json?` +
+      `$select=merk,kenteken,handelsbenaming,datum_eerste_toelating_dt,datum_eerste_tenaamstelling_in_nederland_dt,vervaldatum_apk_dt,eerste_kleur,bruto_bpm,catalogusprijs,aantal_cilinders,cilinderinhoud,` +
+      `lengte,breedte,massa_ledig_voertuig,toegestane_maximum_massa_voertuig,massa_rijklaar,maximum_massa_trekken_ongeremd,maximum_trekken_massa_geremd,` +
+      `wam_verzekerd,tellerstandoordeel` +
+      `&$where=kenteken LIKE '${licence}%25' or merk LIKE '%25${keyword}%25' or handelsbenaming LIKE '%25${keyword}%25'` +
+      `&$limit=25`
+    );
   }, [keyword]);
 
   const { isLoading, data } = useFetch<Car[]>(url, {
     // to make sure the screen isn't flickering when the searchText changes
-    keepPreviousData: true
+    keepPreviousData: true,
   });
 
   return (
-    <List isLoading={isLoading} searchText={searchText} onSearchTextChange={setSearchText} isShowingDetail={true}
-          searchBarPlaceholder="Search on licence plate, brand or type" throttle>
+    <List
+      isLoading={isLoading}
+      searchText={searchText}
+      onSearchTextChange={setSearchText}
+      isShowingDetail={true}
+      searchBarPlaceholder="Search on licence plate, brand or type"
+      throttle
+    >
       <List.Section title="Found licences:">
         {(data || []).map((item: Car) => (
           <List.Item
@@ -94,22 +106,10 @@ export default function Command() {
               <List.Item.Detail
                 metadata={
                   <List.Item.Detail.Metadata>
-                    <List.Item.Detail.Metadata.Label
-                      title="License plate"
-                      text={item.kenteken}
-                    />
-                    <List.Item.Detail.Metadata.Label
-                      title="Brand"
-                      text={item.merk}
-                    />
-                    <List.Item.Detail.Metadata.Label
-                      title="Trade name"
-                      text={item.handelsbenaming}
-                    />
-                    <List.Item.Detail.Metadata.Label
-                      title="Color"
-                      text={item.eerste_kleur ?? "Unknown"}
-                    />
+                    <List.Item.Detail.Metadata.Label title="License plate" text={item.kenteken} />
+                    <List.Item.Detail.Metadata.Label title="Brand" text={item.merk} />
+                    <List.Item.Detail.Metadata.Label title="Trade name" text={item.handelsbenaming} />
+                    <List.Item.Detail.Metadata.Label title="Color" text={item.eerste_kleur ?? "Unknown"} />
                     <List.Item.Detail.Metadata.Separator />
                     <List.Item.Detail.Metadata.Label
                       title="Number of cylinders"
@@ -120,14 +120,8 @@ export default function Command() {
                       text={toCentimeters(item.cilinderinhoud)}
                     />
                     <List.Item.Detail.Metadata.Separator />
-                    <List.Item.Detail.Metadata.Label
-                      title="Length"
-                      text={toCentimeters(item.lengte)}
-                    />
-                    <List.Item.Detail.Metadata.Label
-                      title="Width"
-                      text={toCentimeters(item.breedte)}
-                    />
+                    <List.Item.Detail.Metadata.Label title="Length" text={toCentimeters(item.lengte)} />
+                    <List.Item.Detail.Metadata.Label title="Width" text={toCentimeters(item.breedte)} />
                     <List.Item.Detail.Metadata.Label
                       title="Empty vehicle mass"
                       text={toKilograms(item.massa_ledig_voertuig)}
@@ -154,14 +148,8 @@ export default function Command() {
                       text={toDate(item.vervaldatum_apk_dt)}
                     />
                     <List.Item.Detail.Metadata.Separator />
-                    <List.Item.Detail.Metadata.Label
-                      title="Catalog price"
-                      text={toEuro(item.catalogusprijs)}
-                    />
-                    <List.Item.Detail.Metadata.Label
-                      title="Gross BPM"
-                      text={toEuro(item.bruto_bpm)}
-                    />
+                    <List.Item.Detail.Metadata.Label title="Catalog price" text={toEuro(item.catalogusprijs)} />
+                    <List.Item.Detail.Metadata.Label title="Gross BPM" text={toEuro(item.bruto_bpm)} />
                   </List.Item.Detail.Metadata>
                 }
               />
@@ -169,8 +157,8 @@ export default function Command() {
             accessories={[
               {
                 tag: item.merk,
-                tooltip: "Brand"
-              }
+                tooltip: "Brand",
+              },
             ]}
           />
         ))}
