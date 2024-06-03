366343d076cf4c14fa61d891d3bc6aebfddc4d18
Massimiliano Pasquini <62497822+MassimilianoPasquini97@users.noreply.github.com>
2024-06-03 14:48:34 +0200

    Update raycast-ollama extension (#12734)
    - CHANGELOG.md updated
    - Fixed model error when deleting last chat.
    - ChatView UI Fix
    - README.md updated.
    - Configured signal timeout at 300ms
    - README.md changes
    - keep_alive implementation on chat
    - moved keep_alive from extension preferences
    - keep_alive implementation on prebuild command.
    - keep_alive implementation on custom commands.
    - debug message removed
    - Model and Server value combined for lint reason
    - README.md changes
    - refactor with this new features:
    - Pull contributions

--- extensions/raycast-ollama/src/lib/ollama/errors.ts
+++ extensions/raycast-ollama/src/lib/ollama/errors.ts
@@ -1,12 +1,10 @@
-export const ErrorRaycastModelNotConfiguredOnLocalStorage = new Error(
-  "Preferred model is not configured on LocalStorage."
-);
-export const ErrorOllamaVersion = new Error("Ollama version not supported, update at least at v0.1.14.");
-export const ErrorOllamaNotInstalledOrRunning = new Error("Verify Ollama is Installed and Currently Running.");
-export const ErrorOllamaModelRegistryUnreachable = new Error("Ollama Models Registry is Unreachable.");
-export const MessageOllamaModelNotInstalled = new Error("Model is not Installed.");
+export const OllamaMessageModelNotInstalled = "Model is not Installed.";
 
-export class ErrorOllamaCustomModel extends Error {
+export const OllamaVersion = new Error("Ollama version not supported, update at least at v0.1.14.");
+export const OllamaNotInstalledOrRunning = new Error("Verify Ollama is Installed and Currently Running.");
+export const OllamaModelRegistryUnreachable = new Error("Ollama Models Registry is Unreachable.");
+
+export class OllamaCustomModel extends Error {
   public readonly model?: string;
   public readonly file?: string;
 
@@ -17,7 +15,7 @@ export class ErrorOllamaCustomModel extends Error {
   }
 }
 
-export class ErrorOllamaModelNotInstalled extends Error {
+export class OllamaModelNotInstalled extends Error {
   public readonly model?: string;
   public readonly suggest?: string;
 
@@ -28,7 +26,7 @@ export class ErrorOllamaModelNotInstalled extends Error {
   }
 }
 
-export class ErrorOllamaModelNotMultimodal extends Error {
+export class OllamaModelNotMultimodal extends Error {
   public readonly model?: string;
   public readonly suggest?: string;
 
