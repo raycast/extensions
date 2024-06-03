dd09ae57a5be6d341b2495cd374a0d1275e1b58a
Mohamed Mahmoud <mohamedkhaled.kh98@gmail.com>
2024-06-03 23:27:14 +0100

    refactor: get file path to avoid recursion

--- extensions/ilovepdf/src/common/utils.ts
+++ extensions/ilovepdf/src/common/utils.ts
@@ -1,14 +1,20 @@
 import fs from "fs";
 import path from "path";
 
-export const getFilePath = (directory: string, filename: string, increment = 0): string => {
-  const name = `${path.basename(filename, path.extname(filename))}${increment || ""}${path.extname(filename)}`;
-  const initialPath = path.join(directory, name);
-  if (!fs.existsSync(initialPath)) {
-    return initialPath;
-  }
+export const getFilePath = (directory: string, filename: string, ): string => {
+  const files = fs.readdirSync(directory);
+  const fileSet = new Set(files);
 
-  return getFilePath(directory, filename, increment + 1);
+  for(let increment=0; increment<MaxInt32;increment++)
+  {
+    const name = `${path.basename(filename, path.extname(filename))}${increment || ""}${path.extname(filename)}`;
+    const initialPath = path.join(directory, name);
+    if (!fileSet.has(name)) {
+      return initialPath;
+    }
+  }
+  return path.join(directory, filename);
+
 };
 
 export const MaxInt32 = 2147483647;
