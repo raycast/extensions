a348589b5221d1eb06e46d680ef5bee63765c6f2
LitoMore <litomore@gmail.com>
2024-06-05 00:27:25 +0800

    Close window asap

--- extensions/say/src/selectedTextToSay.tsx
+++ extensions/say/src/selectedTextToSay.tsx
@@ -3,8 +3,8 @@ import { say } from "mac-say";
 import { getSaySettings, parseSaySettings } from "./utils.js";
 
 export default async function SelectionToSay() {
+  await closeMainWindow();
   const selectedText = await getSelectedText().catch((error) => error.toString());
   const saySettings = parseSaySettings(getSaySettings());
-  await closeMainWindow();
   await say(selectedText, saySettings);
 }
