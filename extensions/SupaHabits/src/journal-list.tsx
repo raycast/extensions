823839ceb10037232482869fd9c892214660639e
Gonzalo Barba <zalo250@gmail.com>
2024-06-06 16:11:12 +0200

    Add SupaHabits extension
    - Rename extension name
    - LaunchCommand after adding a new journal entry
    - Fix lint
    - update details and readme
    - add journla entry list
    - add journal entry
    - update list after creation
    - Fix completed type
    - fix initial loading
    - first commit

---
+++ extensions/SupaHabits/src/journal-list.tsx
@@ -0,0 +1,65 @@
+import { Action, ActionPanel, Cache, List, showToast, Toast, useNavigation } from "@raycast/api";
+
+import SetSecret from "./components/set-secret";
+import { useEffect, useState } from "react";
+import fetch from "node-fetch";
+
+import { JournalEntry } from "./models/journal-entry";
+import JournalCommand from "./journal";
+
+const cache = new Cache();
+
+export default function JournalListCommand() {
+  const secret = cache.get("secret");
+  const navigation = useNavigation();
+
+  const [loading, setLoading] = useState<boolean>(false);
+  const [entries, setEntries] = useState<JournalEntry[] | undefined>();
+
+  useEffect(() => {
+    if (secret) {
+      const fetchData = async () => {
+        await retrieveJournalEntries();
+      };
+
+      fetchData();
+    }
+  }, []);
+
+  const retrieveJournalEntries = async () => {
+    try {
+      setLoading(true);
+      const res = await fetch(`https://www.supahabits.com/api/journal?secret=${secret}`).then((res) => res.json());
+      setEntries(res as JournalEntry[]);
+    } catch (e) {
+      showToast({ style: Toast.Style.Failure, title: "Failed to retrieve habits" });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  if (!secret) {
+    return <SetSecret />;
+  }
+
+  if (loading) {
+    return <List isLoading={true} />;
+  }
+
+  return (
+    <List filtering={true} navigationTitle="Search Journal Entries" searchBarPlaceholder="Filter entries by content">
+      {entries && entries.length === 0 && (
+        <List.Item
+          title="No entries found"
+          actions={
+            <ActionPanel>
+              <Action title="New Entry" onAction={() => navigation.push(<JournalCommand />)} />
+            </ActionPanel>
+          }
+        />
+      )}
+
+      {entries && entries.map((item) => <List.Item key={item.id} title={item.content} />)}
+    </List>
+  );
+}
---
+++ extensions/SupaHabits/src/journal.tsx
@@ -0,0 +1,72 @@
+import {
+  Action,
+  ActionPanel,
+  Cache,
+  Form,
+  launchCommand,
+  LaunchType,
+  List,
+  showToast,
+  Toast,
+  useNavigation
+} from "@raycast/api";
+import { useState } from "react";
+import fetch from "node-fetch";
+
+import SetSecret from "./components/set-secret";
+import JournalListCommand from "./journal-list";
+
+const cache = new Cache();
+
+export default function JournalCommand() {
+  const secret = cache.get("secret");
+  const [loading, setLoading] = useState<boolean>(false);
+  const navigation = useNavigation();
+
+  if (!secret) {
+    return <SetSecret />;
+  }
+
+  const submitEntry = async (values: { content: string }) => {
+    if (!values.content || !values.content.trim()) {
+      showToast({ style: Toast.Style.Failure, title: "Content is required" });
+      return;
+    }
+
+    setLoading(true);
+    try {
+      await fetch(`https://www.supahabits.com/api/journal`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ secret, content: values.content }),
+      });
+      showToast({ style: Toast.Style.Success, title: "✅ Entry submitted!" });
+
+      await launchCommand( {
+        name: 'journal-list',
+        type: LaunchType.UserInitiated,
+      });
+    } catch (error) {
+      showToast({ style: Toast.Style.Failure, title: "Failed to submit entry" });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  if (loading) {
+    return <List isLoading={true} />;
+  }
+
+  return (
+    <Form
+      actions={
+        <ActionPanel>
+          <Action.SubmitForm title="Submit Entry" onSubmit={submitEntry} />
+          <Action.OpenInBrowser title="View All Entries" url="https://www.supahabits.com/dashboard/journal" />
+        </ActionPanel>
+      }
+    >
+      <Form.TextArea id="content" title="New Journal Entry" defaultValue="" />
+    </Form>
+  );
+}
