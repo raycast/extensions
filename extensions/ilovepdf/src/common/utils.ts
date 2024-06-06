c78e860d126e3c74aa500b060844cb91a599d971
Mohamed Khaled <41074872+MohamedK1@users.noreply.github.com>
2024-06-06 07:59:52 +0100

    Refactoring  (#12747)
    * refactor: get file path to avoid recursion
    
    * refactor: consistently adding dot at end of message
    
    * refactor: add error info in the error message
    
    * refactor: add changelog entry
    
    * Update CHANGELOG.md and optimise images
    
    ---------
    
    Co-authored-by: Per Nielsen Tikær <per@raycast.com>
    Co-authored-by: raycastbot <bot@raycast.com>

--- extensions/ilovepdf/src/common/utils.ts
+++ extensions/ilovepdf/src/common/utils.ts
@@ -2,14 +2,18 @@ import fs from "fs";
 import path from "path";
 import filetype from "magic-bytes.js";
 
-export const getFilePath = (directory: string, filename: string, increment = 0): string => {
-  const name = `${path.basename(filename, path.extname(filename))}${increment || ""}${path.extname(filename)}`;
-  const initialPath = path.join(directory, name);
-  if (!fs.existsSync(initialPath)) {
-    return initialPath;
-  }
+export const getFilePath = (directory: string, filename: string): string => {
+  const files = fs.readdirSync(directory);
+  const fileSet = new Set(files);
 
-  return getFilePath(directory, filename, increment + 1);
+  for (let increment = 0; increment < MaxInt32; increment++) {
+    const name = `${path.basename(filename, path.extname(filename))}${increment || ""}${path.extname(filename)}`;
+    const initialPath = path.join(directory, name);
+    if (!fileSet.has(name)) {
+      return initialPath;
+    }
+  }
+  return path.join(directory, filename);
 };
 
 export const MaxInt32 = 2147483647;
