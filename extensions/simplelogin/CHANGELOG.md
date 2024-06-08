2536c081e534bd601b5407b76b7bb0e1055b4fd7
Marcel Ruhf <hey@mruhf.dev>
2024-06-09 01:09:52 +0200

    Update simplelogin extension
    - Prefill Alias Description feature; Raycast API package bump
    - Merge branch \'contributions/merge-1717886972176571000\'
    - Pull contributions
    - Pull contributions

--- extensions/simplelogin/CHANGELOG.md
+++ extensions/simplelogin/CHANGELOG.md
@@ -1,5 +1,9 @@
 # SimpleLogin Changelog
 
+[Enable Alias Description Prefill Based on Active Browser Tab URL] - {PR_MERGE_DATE}
+* Added a new preference option: "Prefill Alias Description".
+* When enabled, the default alias description will be automatically populated using the hostname of the active browser tab.
+* (Side Note) Bump Raycast API packag version
 
 ## [Updates to List Aliases & Create Random Alias commands] - 2024-06-05
 - Update List Aliases command to cache previously fetched aliases so that aliases can be viewed before refetch
---
+++ extensions/simplelogin/src/utils/browser.tsx
@@ -0,0 +1,65 @@
+/**
+ * CREDIT: The AppleScript snippets were taken from the whois extension
+ */
+import { runAppleScript } from "@raycast/utils";
+
+const CHROMIUM_BROWSERS = ["Google Chrome", "Opera", "Brave Browser", "Microsoft Edge", "Vivaldi"];
+
+export default async (): Promise<string | undefined> => {
+  const browser = await getFrontmostApp();
+  let url: string | undefined;
+
+  if (browser.match(/Safari/i)) {
+    url = await getSafariURL();
+  } else if (CHROMIUM_BROWSERS.some((b) => browser.startsWith(b))) {
+    url = await getChromiumURL(browser);
+  } else if (browser.match(/Arc/i)) {
+    url = await getArcURL();
+  }
+
+  if (!url) {
+    return;
+  }
+
+  try {
+    return new URL(url).hostname;
+  } catch (errror) {
+    console.error("Failed to get hostname", errror);
+    return;
+  }
+};
+
+const getFrontmostApp = () => {
+  return runAppleScript(`
+    tell application "System Events"
+      set frontmostApp to name of first application process whose frontmost is true
+      return frontmostApp
+    end tell
+  `);
+};
+
+const getSafariURL = () => {
+  return runAppleScript(`
+    tell application "Safari" to get URL of front document
+  `);
+};
+
+const getChromiumURL = (browser = "Google Chrome") => {
+  return runAppleScript(`
+    tell application "${browser}"
+      set currentTab to active tab of front window
+      set currentURL to URL of currentTab
+      return currentURL
+      end tell
+  `);
+};
+
+const getArcURL = () => {
+  return runAppleScript(`
+    tell application "Arc"
+      tell front window
+        get the URL of active tab
+      end tell
+    end tell
+  `);
+};
