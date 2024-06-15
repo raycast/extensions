9a5a0e3695e13304a1a83fa5dfd614d6fd323e87
🥷⚔️💀👻 <contact@stelo.dev>
2024-06-15 06:28:09 -0700

    refactor(secrets): lint fix
    closes https://github.com/raycast/extensions/issues/12958
    
    closes https://github.com/raycast/extensions/issues/12966

--- extensions/amazon-aws/src/secrets.tsx
+++ extensions/amazon-aws/src/secrets.tsx
@@ -2,7 +2,7 @@ import { Action, ActionPanel, Color, Image, Icon, Keyboard, List } from "@raycas
 import { useCachedState } from "@raycast/utils";
 import { useState } from "react";
 import AWSProfileDropdown from "./components/searchbar/aws-profile-dropdown";
-import { resourceToConsoleLink, uniqBy } from './util';
+import { resourceToConsoleLink, uniqBy } from "./util";
 import { AwsAction } from "./components/common/action";
 import { useSecrets } from "./hooks/use-secrets";
 import { SecretValueDetails } from "./components/secrets/secret-value-details";
@@ -37,138 +37,138 @@ export default function Secrets() {
       {uniqBy(secrets || [], (secret) => secret.ARN)
         .sort((a, b) => a.Name.localeCompare(b.Name))
         .map((secret) => (
-        <List.Item
-          key={secret.secretKey}
-          icon={{ source: "aws-icons/secretsmanager.png", mask: Image.Mask.RoundedRectangle }}
-          title={secret.Name || ""}
-          detail={
-            <List.Item.Detail
-              markdown={
-                secret.resourcePolicy
-                  ? `**Policy:**\n \`\`\`json\n${JSON.stringify(JSON.parse(secret.resourcePolicy), undefined, 4)}\n\`\`\``
-                  : undefined
-              }
-              metadata={
-                <List.Item.Detail.Metadata>
-                  <List.Item.Detail.Metadata.Label title="Name" text={secret.Name} />
-                  <List.Item.Detail.Metadata.Label title="Description" text={secret.Description} />
-                  {secret.PrimaryRegion && (
-                    <List.Item.Detail.Metadata.Label title="Primary Region" text={secret.PrimaryRegion} />
-                  )}
-                  {secret.OwningService && (
-                    <List.Item.Detail.Metadata.Label title="Owning Service" text={secret.OwningService} />
-                  )}
-                  <List.Item.Detail.Metadata.Separator />
-                  <List.Item.Detail.Metadata.Label
-                    title="Creation Date"
-                    text={secret.CreatedDate?.toISOString()}
-                    icon={Icon.Calendar}
-                  />
-                  <List.Item.Detail.Metadata.Label
-                    title="Last Accessed"
-                    text={secret.LastAccessedDate?.toISOString()}
-                    icon={Icon.Calendar}
-                  />
-                  {secret.LastRotatedDate && (
+          <List.Item
+            key={secret.secretKey}
+            icon={{ source: "aws-icons/secretsmanager.png", mask: Image.Mask.RoundedRectangle }}
+            title={secret.Name || ""}
+            detail={
+              <List.Item.Detail
+                markdown={
+                  secret.resourcePolicy
+                    ? `**Policy:**\n \`\`\`json\n${JSON.stringify(JSON.parse(secret.resourcePolicy), undefined, 4)}\n\`\`\``
+                    : undefined
+                }
+                metadata={
+                  <List.Item.Detail.Metadata>
+                    <List.Item.Detail.Metadata.Label title="Name" text={secret.Name} />
+                    <List.Item.Detail.Metadata.Label title="Description" text={secret.Description} />
+                    {secret.PrimaryRegion && (
+                      <List.Item.Detail.Metadata.Label title="Primary Region" text={secret.PrimaryRegion} />
+                    )}
+                    {secret.OwningService && (
+                      <List.Item.Detail.Metadata.Label title="Owning Service" text={secret.OwningService} />
+                    )}
+                    <List.Item.Detail.Metadata.Separator />
                     <List.Item.Detail.Metadata.Label
-                      title="Last Rotated"
-                      text={secret.LastRotatedDate?.toISOString()}
+                      title="Creation Date"
+                      text={secret.CreatedDate?.toISOString()}
                       icon={Icon.Calendar}
                     />
-                  )}
-                  {secret.LastChangedDate && (
                     <List.Item.Detail.Metadata.Label
-                      title="Last Changed"
-                      text={secret.LastChangedDate?.toISOString()}
+                      title="Last Accessed"
+                      text={secret.LastAccessedDate?.toISOString()}
                       icon={Icon.Calendar}
                     />
-                  )}
-                  {secret.DeletedDate && (
+                    {secret.LastRotatedDate && (
+                      <List.Item.Detail.Metadata.Label
+                        title="Last Rotated"
+                        text={secret.LastRotatedDate?.toISOString()}
+                        icon={Icon.Calendar}
+                      />
+                    )}
+                    {secret.LastChangedDate && (
+                      <List.Item.Detail.Metadata.Label
+                        title="Last Changed"
+                        text={secret.LastChangedDate?.toISOString()}
+                        icon={Icon.Calendar}
+                      />
+                    )}
+                    {secret.DeletedDate && (
+                      <List.Item.Detail.Metadata.Label
+                        title="Deleted"
+                        text={secret.DeletedDate?.toISOString()}
+                        icon={Icon.Calendar}
+                      />
+                    )}
+                    <List.Item.Detail.Metadata.Separator />
+                    {secret.SecretVersionsToStages &&
+                      Object.keys(secret.SecretVersionsToStages).map((version) => (
+                        <List.Item.Detail.Metadata.TagList title={version} key={version}>
+                          {(secret.SecretVersionsToStages?.[version] ?? []).map((stage: string) => (
+                            <List.Item.Detail.Metadata.TagList.Item
+                              text={stage}
+                              icon={Icon.Tag}
+                              color={Color.Orange}
+                              key={`${version}-${stage}`}
+                            />
+                          ))}
+                        </List.Item.Detail.Metadata.TagList>
+                      ))}
+                    <List.Item.Detail.Metadata.Separator />
                     <List.Item.Detail.Metadata.Label
-                      title="Deleted"
-                      text={secret.DeletedDate?.toISOString()}
-                      icon={Icon.Calendar}
+                      title="Rotation"
+                      icon={
+                        secret.RotationEnabled
+                          ? { source: Icon.Checkmark, tintColor: Color.Green }
+                          : { source: Icon.Xmark, tintColor: Color.Red }
+                      }
                     />
-                  )}
-                  <List.Item.Detail.Metadata.Separator />
-                  {secret.SecretVersionsToStages &&
-                    Object.keys(secret.SecretVersionsToStages).map((version) => (
-                      <List.Item.Detail.Metadata.TagList title={version} key={version}>
-                        {(secret.SecretVersionsToStages?.[version] ?? []).map((stage: string) => (
-                          <List.Item.Detail.Metadata.TagList.Item
-                            text={stage}
-                            icon={Icon.Tag}
-                            color={Color.Orange}
-                            key={`${version}-${stage}`}
-                          />
-                        ))}
-                      </List.Item.Detail.Metadata.TagList>
-                    ))}
-                  <List.Item.Detail.Metadata.Separator />
-                  <List.Item.Detail.Metadata.Label
-                    title="Rotation"
-                    icon={
-                      secret.RotationEnabled
-                        ? { source: Icon.Checkmark, tintColor: Color.Green }
-                        : { source: Icon.Xmark, tintColor: Color.Red }
-                    }
+                    {secret.RotationRules?.Duration && (
+                      <List.Item.Detail.Metadata.Label
+                        title="Rotation Duration"
+                        text={secret.RotationRules.Duration}
+                        icon={{ source: Icon.Clock, tintColor: Color.Blue }}
+                      />
+                    )}
+                    {secret.RotationRules?.AutomaticallyAfterDays && (
+                      <List.Item.Detail.Metadata.Label
+                        title="Rotation After Days"
+                        text={`${secret.RotationRules.AutomaticallyAfterDays}`}
+                        icon={{ source: Icon.Clock, tintColor: Color.Blue }}
+                      />
+                    )}
+                    {secret.RotationRules?.ScheduleExpression && (
+                      <List.Item.Detail.Metadata.Label
+                        title="Rotation Schedule"
+                        text={secret.RotationRules.ScheduleExpression}
+                        icon={{ source: Icon.Clock, tintColor: Color.Blue }}
+                      />
+                    )}
+                  </List.Item.Detail.Metadata>
+                }
+              />
+            }
+            actions={
+              <ActionPanel>
+                <AwsAction.Console url={resourceToConsoleLink(secret.Name, "AWS::SecretsManager::Secret")} />
+                <ActionPanel.Section title={"Secret Actions"}>
+                  <Action.Push
+                    target={<SecretValueDetails {...{ secret }} />}
+                    title={"Show Secret Value"}
+                    icon={Icon.LockUnlocked}
+                    shortcut={Keyboard.Shortcut.Common.Open}
                   />
-                  {secret.RotationRules?.Duration && (
-                    <List.Item.Detail.Metadata.Label
-                      title="Rotation Duration"
-                      text={secret.RotationRules.Duration}
-                      icon={{ source: Icon.Clock, tintColor: Color.Blue }}
-                    />
-                  )}
-                  {secret.RotationRules?.AutomaticallyAfterDays && (
-                    <List.Item.Detail.Metadata.Label
-                      title="Rotation After Days"
-                      text={`${secret.RotationRules.AutomaticallyAfterDays}`}
-                      icon={{ source: Icon.Clock, tintColor: Color.Blue }}
-                    />
-                  )}
-                  {secret.RotationRules?.ScheduleExpression && (
-                    <List.Item.Detail.Metadata.Label
-                      title="Rotation Schedule"
-                      text={secret.RotationRules.ScheduleExpression}
-                      icon={{ source: Icon.Clock, tintColor: Color.Blue }}
-                    />
-                  )}
-                </List.Item.Detail.Metadata>
-              }
-            />
-          }
-          actions={
-            <ActionPanel>
-              <AwsAction.Console url={resourceToConsoleLink(secret.Name, "AWS::SecretsManager::Secret")} />
-              <ActionPanel.Section title={"Secret Actions"}>
-                <Action.Push
-                  target={<SecretValueDetails {...{ secret }} />}
-                  title={"Show Secret Value"}
-                  icon={Icon.LockUnlocked}
-                  shortcut={Keyboard.Shortcut.Common.Open}
-                />
-                <Action.CopyToClipboard title="Copy Name" content={secret.Name || ""} />
-                <Action.CopyToClipboard title="Copy ARN" content={secret.ARN || ""} />
-                <Action
-                  title={`${isDetailsEnabled ? "Hide" : "Show"} Details`}
-                  onAction={() => setDetailsEnabled(!isDetailsEnabled)}
-                  icon={isDetailsEnabled ? Icon.EyeDisabled : Icon.Eye}
-                />
-              </ActionPanel.Section>
-            </ActionPanel>
-          }
-          accessories={[
-            { date: secret.LastAccessedDate, tooltip: "Last Access", icon: Icon.Calendar },
-            {
-              icon: secret.RotationEnabled
-                ? { source: Icon.Checkmark, tintColor: Color.Green }
-                : { source: Icon.Warning, tintColor: Color.Orange },
-              tooltip: secret.RotationEnabled ? "Rotation Enabled" : "Rotation Disabled",
-            },
-          ]}
-        />
-      ))}
+                  <Action.CopyToClipboard title="Copy Name" content={secret.Name || ""} />
+                  <Action.CopyToClipboard title="Copy ARN" content={secret.ARN || ""} />
+                  <Action
+                    title={`${isDetailsEnabled ? "Hide" : "Show"} Details`}
+                    onAction={() => setDetailsEnabled(!isDetailsEnabled)}
+                    icon={isDetailsEnabled ? Icon.EyeDisabled : Icon.Eye}
+                  />
+                </ActionPanel.Section>
+              </ActionPanel>
+            }
+            accessories={[
+              { date: secret.LastAccessedDate, tooltip: "Last Access", icon: Icon.Calendar },
+              {
+                icon: secret.RotationEnabled
+                  ? { source: Icon.Checkmark, tintColor: Color.Green }
+                  : { source: Icon.Warning, tintColor: Color.Orange },
+                tooltip: secret.RotationEnabled ? "Rotation Enabled" : "Rotation Disabled",
+              },
+            ]}
+          />
+        ))}
     </List>
   );
 }
