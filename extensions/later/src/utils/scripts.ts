45f1137f97c968815667a108b1643baa137fa944
Jim Luo <528266131@qq.com>
2024-06-19 10:53:12 +0800

    fix: rewrite all command scripts

--- extensions/later/src/utils/scripts.ts
+++ extensions/later/src/utils/scripts.ts
@@ -2,81 +2,95 @@ import { splitter } from "../common/config";
 
 export const Scripts = {
   chrome: `
-  	tell application "Google Chrome"
-		if not application "Google Chrome" is running then
+	tell application "System Events"
+		set isRunning to (name of processes) contains "Google Chrome"
+	end tell
+
+		if isRunning then
+			tell application "Google Chrome"
+			set activeTabIndex to active tab index of window 1
+			set activeTabURL to URL of tab activeTabIndex of window 1
+			set activeTabTitle to title of tab activeTabIndex of window 1
+			return (activeTabURL & "${splitter}" & activeTabTitle)
+		end tell
+	else
+		return ""
+	end if
+	`,
+  arc: `
+  	tell application "System Events"
+  		set isRunning to (name of processes) contains "Arc"
+	end tell
+
+	if isRunning then
+		tell application "Arc"
+			set currentURL to URL of active tab of window 1
+			set currentTitle to title of active tab of window 1
+			return (currentURL & "${splitter}" & currentTitle)
+		end tell
+	else
+		return ""
+	end if
+  `,
+  safari: `
+  	tell application "Safari"
+		if not application "Safari" is running then
 			return ""
 		end if
-		set activeTabIndex to active tab index of window 1
-		set activeTabURL to URL of tab activeTabIndex of window 1
-		set activeTabTitle to title of tab activeTabIndex of window 1
+		set activeTab to current tab of window 1
+		set activeTabURL to URL of activeTab
+		set activeTabTitle to name of activeTab
 		return (activeTabURL & "${splitter}" & activeTabTitle)
 	end tell
 	`,
-  arc: `
-	tell application "Arc"
-		if not application "Arc" is running then
-			return ""
-		end if
-		activate
-		 tell application "System Events" to tell process "Arc"
-		    set frontmost to true
-		    set the_title to name of windows's item 1
-		    set the_title to (do shell script "echo " & quoted form of the_title & " | tr '[' ' '")
-		    set the_title to (do shell script "echo " & quoted form of the_title & " | tr ']' ' '")
-	    	end tell
 
-		    tell application "System Events"
-		    keystroke "l" using command down
-		    keystroke "c" using command down
-		    key code 53 
-			end tell
-			delay 0.5
-			set activeTabURL to the clipboard
-  		return (activeTabURL & "${splitter}" & the_title)
-	end tell 
-  `,
-  safari: `tell application "Safari"
-	if not application "Safari" is running then
+  firefox: `
+  	tell application "System Events"
+  		set isRunning to (name of processes) contains "firefox"
+	end tell
+
+	if isRunning then
+		tell application "Firefox"
+			activate
+			delay 0.5 -- Give Firefox some time to activate
+		end tell
+
+	tell application "System Events"
+		tell process "firefox"
+			set frontmost to true
+			set the_title to name of window 1
+			set the_title to (do shell script "echo " & quoted form of the_title & " | tr '[' ' '")
+			set the_title to (do shell script "echo " & quoted form of the_title & " | tr ']' ' '")
+		end tell
+		keystroke "l" using command down
+		delay 0.2 -- Give some time for the keystroke
+		keystroke "c" using command down
+		delay 0.5 -- Give some time for the clipboard to update
+		set activeTabURL to the clipboard
+		key code 53
+	end tell
+	
+		return (activeTabURL & "${splitter}" & the_title)
+	else
 		return ""
 	end if
-	set activeTab to current tab of window 1
-	set activeTabURL to URL of activeTab
-	set activeTabTitle to name of activeTab
-	return (activeTabURL & "${splitter}" & activeTabTitle)
-end tell`,
-
-  firefox: `tell application "Firefox"
-    		if not application "Firefox" is running then
-  			return ""
-  		end if
-    		activate
-		 tell application "System Events" to tell process "firefox"
-		    set frontmost to true
-		    set the_title to name of windows's item 1
-		    set the_title to (do shell script "echo " & quoted form of the_title & " | tr '[' ' '")
-		    set the_title to (do shell script "echo " & quoted form of the_title & " | tr ']' ' '")
-	    	end tell
-
-		    tell application "System Events"
-		    keystroke "l" using command down
-		    keystroke "c" using command down
-		    key code 53 
-		    end tell
-			delay 0.5
-			set activeTabURL to the clipboard
-  		return (activeTabURL & "${splitter}" & the_title)
-    	end tell`,
+	`,
 
   edge: `
-  	tell application "Microsoft Edge"
-		if not application "Microsoft Edge" is running then
-			return ""
-		end if
-		set activeTabIndex to active tab index of window 1
-		set activeTabURL to URL of tab activeTabIndex of window 1
-		set activeTabTitle to title of tab activeTabIndex of window 1
-		return (activeTabURL & "${splitter}" & activeTabTitle)
+  	tell application "System Events"
+  		set isRunning to (name of processes) contains "Microsoft Edge"
 	end tell
+
+	if isRunning then
+		tell application "Microsoft Edge"
+			set activeTabIndex to active tab index of window 1
+			set activeTabURL to URL of tab activeTabIndex of window 1
+			set activeTabTitle to title of tab activeTabIndex of window 1
+			return (activeTabURL & "${splitter}" & activeTabTitle)
+		end tell
+	else
+		return ""
+	end if
 	`,
 };
 
