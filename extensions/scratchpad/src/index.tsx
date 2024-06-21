f065c737843cc304c6bfc9db57737b5566ed0a69
Asnim P Ansari <asnim96@gmail.com>
2024-06-21 22:55:43 +0530

    adding support for folder picker and saving previously used editor

--- extensions/scratchpad/src/index.tsx
+++ extensions/scratchpad/src/index.tsx
@@ -1,20 +1,56 @@
-import { Form, ActionPanel, Action, Application } from "@raycast/api";
+import { Form, ActionPanel, Action, Application, Detail } from "@raycast/api";
 import { fileTypes } from "./constants";
-import { createScratchPadFile, getDefaultHomeDir, isEmpty } from "./utils/utils";
+import {
+  isEmpty,
+  getLastUsedEditorId,
+  getLastUsedScratchpadFolder,
+  getDesktopPath,
+  isFolderSelectionValid,
+  processSubmitAction,
+} from "./utils/utils";
 import { useEffect, useState } from "react";
+
 import { getValidApplicationsForScratchFile } from "./utils/utils";
 
 export default function Command() {
   const [fileNamePrefixError, setFileNamePrefixError] = useState<string | undefined>(undefined);
-  const [folderPathError, setFolderPathError] = useState<string | undefined>(undefined);
   const [applicationsList, setApplicationsList] = useState<Application[]>([]);
+  const [defaultEditorBundleId, setDefaultEditorBundleId] = useState<string | undefined>(undefined);
+  const [lastUsedScratchpadFolder, setLastUsedScratchpadFolder] = useState<string[] | undefined>(undefined);
+  const [hasInitDataLoaded, setHasInitDataLoaded] = useState<boolean>(false);
+  const [folderSelectionError, setFolderSelectionError] = useState<string | undefined>(undefined);
+  const [defaultAppPickerError, setDefaultAppPickerError] = useState<string | undefined>(undefined);
 
   useEffect(() => {
     (async () => {
+      // stores a list of default application
       const installedApplications = await getValidApplicationsForScratchFile();
       setApplicationsList(installedApplications);
+
+      const lastUsedEditorBundleId = await getLastUsedEditorId();
+
+      const firstApplication = installedApplications[0].bundleId;
+
+      const defaultEditor =
+        lastUsedEditorBundleId && installedApplications.find((app) => app.bundleId === lastUsedEditorBundleId)
+          ? lastUsedEditorBundleId
+          : firstApplication;
+      setDefaultEditorBundleId(defaultEditor);
+      if (!defaultEditor) {
+        setDefaultAppPickerError("No default application found. Please Install an editor and try again.");
+      }
+
+      const lastUsedFolder = await getLastUsedScratchpadFolder();
+      const desktopPath = getDesktopPath();
+      const lastUsedScratchpadFolder = lastUsedFolder ? [lastUsedFolder] : [desktopPath];
+      setLastUsedScratchpadFolder(lastUsedScratchpadFolder);
+      setHasInitDataLoaded(true);
     })();
   }, []);
+
+  if (!hasInitDataLoaded) {
+    return <Detail markdown="Please wait while we load the application." />;
+  }
 
   return (
     <Form
@@ -23,8 +59,16 @@ export default function Command() {
           <Action.SubmitForm
             title="Create Scratchpad File"
             onSubmit={async (values) => {
-              const { folder, fileNamePrefix, fileType, applicationBundleId } = values;
-              await createScratchPadFile(folder, fileNamePrefix, fileType, applicationBundleId);
+              const { folders, fileNamePrefix, fileType, applicationBundleId } = values;
+              const folder = folders[0];
+              const folderSelectionError =
+                isEmpty(folder) || !isFolderSelectionValid(folder) ? "folder path is not valid" : undefined;
+              setFolderSelectionError(folderSelectionError);
+              if (folderSelectionError) {
+                // abort if there is some folder selection related error
+                return;
+              }
+              await processSubmitAction(folder, fileNamePrefix, fileType, applicationBundleId);
             }}
           />
         </ActionPanel>
@@ -40,36 +84,42 @@ export default function Command() {
         ))}
       </Form.Dropdown>
 
-      <Form.TextField
-        id="folder"
+      <Form.FilePicker
+        id="folders"
         title="Folder"
-        placeholder="Enter Scratchpad Folder Path"
-        defaultValue={getDefaultHomeDir()}
-        error={folderPathError}
+        allowMultipleSelection={false}
+        canChooseFiles={false}
+        canChooseDirectories={true}
+        defaultValue={lastUsedScratchpadFolder}
+        storeValue={true}
+        error={folderSelectionError}
         onBlur={(event) => {
-          if (isEmpty(event.target.value)) {
-            setFolderPathError("Folder Path should not be empty.");
-          } else {
-            setFolderPathError(undefined);
-          }
+          const folderSelectionError =
+            isEmpty(event.target.value) || !isFolderSelectionValid(event.target.value![0])
+              ? "folder path is not valid"
+              : undefined;
+          setFolderSelectionError(folderSelectionError);
         }}
       />
+
       <Form.TextField
         id="fileNamePrefix"
         title="File Name Prefix"
         placeholder="Enter File Name Prefix"
         error={fileNamePrefixError}
         onBlur={(event) => {
-          if (isEmpty(event.target.value)) {
-            setFileNamePrefixError("File Name Prefix should not be empty.");
-          } else {
-            setFileNamePrefixError(undefined);
-          }
+          const namePrefixErr = isEmpty(event.target.value) ? "File Name Prefix should not be empty." : undefined;
+          setFileNamePrefixError(namePrefixErr);
         }}
         defaultValue="scratch_"
       />
 
-      <Form.Dropdown id="applicationBundleId" title="Application">
+      <Form.Dropdown
+        id="applicationBundleId"
+        title="Application"
+        defaultValue={defaultEditorBundleId}
+        error={defaultAppPickerError}
+      >
         {applicationsList.map((entry) => (
           <Form.Dropdown.Item value={entry.bundleId!} title={entry.name} key={entry.bundleId} />
         ))}
--- extensions/scratchpad/src/utils/utils.ts
+++ extensions/scratchpad/src/utils/utils.ts
@@ -1,11 +1,15 @@
 import fse from "fs-extra";
 import moment from "moment";
 import { homedir } from "os";
-import { getApplications, Application, open, showToast, Toast } from "@raycast/api";
+import { getApplications, Application, open, showToast, Toast, LocalStorage } from "@raycast/api";
 import { editorBundleIds } from "../constants";
 
 export const getDefaultHomeDir = () => {
   return `${homedir()}/Desktop/Scratchpad`;
+};
+
+export const getDesktopPath = () => {
+  return `${homedir()}/Desktop`;
 };
 
 export const createScratchPadFile = async (
@@ -52,11 +56,46 @@ export const createScratchPadFile = async (
   });
 };
 
-export const isEmpty = (ip: string | null | undefined) => {
+export const isEmpty = (ip: string | null | undefined | string[]) => {
+  if (Array.isArray(ip)) {
+    return ip.length === 0;
+  }
   return ip === null || ip === undefined || ip === "";
 };
 
 export const getValidApplicationsForScratchFile = async (): Promise<Application[]> => {
   const allApplications = await getApplications();
   return allApplications.filter((app) => app.bundleId !== undefined && editorBundleIds.includes(app.bundleId));
+};
+
+export const getLastUsedEditorId = async (): Promise<string | undefined> => {
+  return LocalStorage.getItem("editorId");
+};
+export const storeLastUsedEditorId = async (editorId: string) => {
+  return LocalStorage.setItem("editorId", editorId);
+};
+
+export const getLastUsedScratchpadFolder = async (): Promise<string | undefined> => {
+  return LocalStorage.getItem("scratchpadFolder");
+};
+
+export const storeLastUsedScratchpadFolder = async (folderPath: string) => {
+  return LocalStorage.setItem("scratchpadFolder", folderPath);
+};
+
+// Validates the existance of a folder
+// param: folderPath - path to the folder
+// returns: boolean
+export const isFolderSelectionValid = (folderPath: string): boolean => {
+  return fse.existsSync(folderPath) && fse.lstatSync(folderPath).isDirectory();
+};
+
+export const processSubmitAction = async (
+  folderPath: string,
+  filePrefix: string,
+  fileType: string,
+  application: string,
+) => {
+  await createScratchPadFile(folderPath, filePrefix, fileType, application);
+  await storeLastUsedEditorId(application);
 };
