f30fb365cab1c0426ea2b7c9e77f3c3ecb53cf5a
Mortada Sarheed <mortadasarheed@gmail.com>
2024-06-23 08:27:31 +0200

    Update plex extension
    - merge
    - Pull contributions
    - changelog updated
    - new screenshots
    - sessiosns command done
    - sessions ok
    - changed file name
    - images + detail views works
    - grid images but search still not working

--- extensions/plex/utils/constants.tsx
+++ extensions/plex/utils/constants.tsx
@@ -1,12 +1,18 @@
 import { getPreferenceValues } from "@raycast/api";
 
 const preferences = getPreferenceValues<Preferences>();
+<<<<<<< HEAD
+
+export const plexBaseUrl = preferences.plexBaseUrl;
+=======
 const plexBaseUrl = preferences.plexBaseUrl;
 
+>>>>>>> contributions/merge-1719123929787840000
 export const plex_token = preferences.plexToken;
 
 export const ENDPOINTS = {
   librarySections: `${plexBaseUrl}/library/sections/`,
+  activeSessions: `${plexBaseUrl}/status/sessions`,
 };
 
 export const SECTION_ICONS = {
