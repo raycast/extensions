fb083886dd33e3e8700f9ac33adfbbae618d34f2
Ahmed Abdullah <ahmadabdullah4919@gmail.com>
2024-06-26 17:25:09 +0500

    remove unnecessary types

--- extensions/python/src/types.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "turndown-plugin-gfm" {
-  import { Options } from "turndown";
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  export function gfm(turndownService: any, options?: Options): void;
-}
