ab857ff34dadfa47d166b7f36a6511519e6ac8e6
zhsama <a602693793@gmail.com>
2024-06-27 15:49:44 +0800

    Update baidu-screenshot-ocr extension
    - perf: use "@raycast/eslint-config": "^1.0.8"
    - fix: run ray lint --fix;

--- extensions/baidu-screenshot-ocr/src/baidu-ocr.tsx
+++ extensions/baidu-screenshot-ocr/src/baidu-ocr.tsx
@@ -1,33 +1,33 @@
-import { Clipboard, closeMainWindow, showHUD } from '@raycast/api'
-import { screenshot } from './screenshot'
-import { recognizeText } from './ocr'
+import { Clipboard, closeMainWindow, showHUD } from "@raycast/api";
+import { screenshot } from "./screenshot";
+import { recognizeText } from "./ocr";
 
 export default async function Command() {
-
-  await closeMainWindow()
+  await closeMainWindow();
 
   try {
-    const image = await screenshot()
+    const image = await screenshot();
     if (!image) {
-      return await showHUD('❌ No Image detected!')
+      return await showHUD("❌ No Image detected!");
     }
 
-    const { file } = await Clipboard.read()
+    const { file } = await Clipboard.read();
 
-    let filepath = file
-    if (file.substring(0, 7) === 'file://') { filepath = file.substring(7, file.length) }
+    let filepath = file;
+    if (file.substring(0, 7) === "file://") {
+      filepath = file.substring(7, file.length);
+    }
 
-    const ocrResult = await recognizeText(filepath)
+    const ocrResult = await recognizeText(filepath);
 
     // console.log(`result = ${ocrResult}`)
 
-    await Clipboard.copy(ocrResult)
-    await closeMainWindow()
+    await Clipboard.copy(ocrResult);
+    await closeMainWindow();
 
-    return await showHUD('✅ Copied to clipboard')
-  }
-  catch (e) {
-    console.error(e)
-    await showHUD('❌ Failed to detect text')
+    return await showHUD("✅ Copied to clipboard");
+  } catch (e) {
+    console.error(e);
+    await showHUD("❌ Failed to detect text");
   }
 }
--- extensions/baidu-screenshot-ocr/src/screenshot.ts
+++ extensions/baidu-screenshot-ocr/src/screenshot.ts
@@ -1,25 +1,23 @@
-import { join } from 'path'
-import { chmod } from 'fs/promises'
-import { environment } from '@raycast/api'
-import type { ExecaError } from 'execa'
-import { execa } from 'execa'
+import { join } from "path";
+import { chmod } from "fs/promises";
+import { environment } from "@raycast/api";
+import type { ExecaError } from "execa";
+import { execa } from "execa";
 
-const screenshot = async() => {
-  const command = join(environment.assetsPath, 'screenshot')
-  await chmod(command, '755')
+const screenshot = async () => {
+  const command = join(environment.assetsPath, "screenshot");
+  await chmod(command, "755");
   try {
-    const { stdout } = await execa(command)
+    const { stdout } = await execa(command);
 
-    return stdout
-  }
-  catch (error) {
-    if ((error as ExecaError).stdout === 'No text selected') {
-      return undefined
-    }
-    else {
-      throw error
+    return stdout;
+  } catch (error) {
+    if ((error as ExecaError).stdout === "No text selected") {
+      return undefined;
+    } else {
+      throw error;
     }
   }
-}
+};
 
-export { screenshot }
+export { screenshot };
