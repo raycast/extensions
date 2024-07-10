3a5df943ab2b51a2218ab4c96730d7b7628ba2e4
Lukas Baranauskas <taztdaras@gmail.com>
2024-07-10 20:07:01 +0300

    fix: new time entry inheriting old parameters

--- extensions/toggl-track/src/hooks/useProcessedTimeEntries.ts
+++ extensions/toggl-track/src/hooks/useProcessedTimeEntries.ts
@@ -9,20 +9,33 @@ export function useProcessedTimeEntries() {
   const isLoading = isLoadingTimeEntries || isLoadingRunningTimeEntry;
 
   const timeEntriesWithUniqueProjectAndDescription = useMemo(() => {
-    return timeEntries.reduce<typeof timeEntries>((acc, timeEntry) => {
+    const seenEntries = new Set();
+    const result = [];
+
+    for (const timeEntry of timeEntries) {
+      const uniqueKey = `${timeEntry.project_id}-${timeEntry.description}`;
+
       if (
-        (timeEntry.description == runningTimeEntry?.description &&
-          timeEntry.project_id == runningTimeEntry?.project_id) ||
-        acc.find((t) => t.description === timeEntry.description && t.project_id === timeEntry.project_id)
-      )
-        return acc;
-      return [...acc, timeEntry];
-    }, []);
+        (timeEntry.description === runningTimeEntry?.description &&
+          timeEntry.project_id === runningTimeEntry?.project_id) ||
+        seenEntries.has(uniqueKey)
+      ) {
+        continue;
+      }
+
+      seenEntries.add(uniqueKey);
+      result.push(timeEntry);
+    }
+
+    return result;
   }, [timeEntries, runningTimeEntry]);
 
-  const runningTimeWithUniqueProjectAndDescription = timeEntries.find(
+  const runningTimeEntryMetadata = timeEntries.find(
     (entry) => entry.description === runningTimeEntry?.description && entry.project_id === runningTimeEntry.project_id,
   );
+
+  const runningTimeWithUniqueProjectAndDescription =
+    runningTimeEntryMetadata && runningTimeEntry ? { ...runningTimeEntryMetadata, ...runningTimeEntry } : null;
 
   return {
     timeEntries,
