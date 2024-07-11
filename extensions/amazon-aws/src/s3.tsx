0bd3a429d44ea655993835b439c0c59829dd0d54
Julian Frenzel <26822719+b0lle@users.noreply.github.com>
2024-07-11 22:18:53 +0200

    Update s3.tsx
    Co-authored-by: stelo <42366677+jfkisafk@users.noreply.github.com>

--- extensions/amazon-aws/src/s3.tsx
+++ extensions/amazon-aws/src/s3.tsx
@@ -86,10 +86,7 @@ function S3BucketObjects({ bucket, prefix = "" }: { bucket: Bucket; prefix?: str
                   />
                   <Action
                     title={`${isReversedOrder ? "Standard" : "Reversed"} Order`}
-                    onAction={() => {
-                      setReversedOrder(!isReversedOrder);
-                      mutate();
-                    }}
+                    onAction={() => setReversedOrder(!isReversedOrder)}
                     icon={Icon.Switch}
                     shortcut={{ modifiers: ["cmd"], key: "r" }}
                   />
