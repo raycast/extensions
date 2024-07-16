149ba7b4179cc1689c8ad24a1491527fe78ffb18
devjiwonchoi <devjiwonchoi@gmail.com>
2024-07-17 04:08:14 +0900

    Update git-branch-name-generator extension
    - Merge branch \'contributions/merge-1721156877150651000\'
    - Pull contributions
    - feat: add custom branch type

--- extensions/git-branch-name-generator/src/index.tsx
+++ extensions/git-branch-name-generator/src/index.tsx
@@ -1,5 +1,6 @@
-import { Form, ActionPanel, Action, Clipboard, showHUD, PopToRootType } from "@raycast/api";
+import { Form, ActionPanel, Action, Clipboard, showHUD, PopToRootType, LocalStorage } from "@raycast/api";
 import { useForm } from "@raycast/utils";
+import { useEffect, useState } from "react";
 
 type Values = {
   branchType: string;
@@ -8,6 +9,7 @@ type Values = {
   snakeCase: boolean;
   prefixDate: boolean;
   includeGitCommand: boolean;
+  hideDefaultBranchTypes: boolean;
 };
 
 function slugify(content: string, snakeCase: boolean) {
@@ -29,8 +31,22 @@ function slugify(content: string, snakeCase: boolean) {
 }
 
 export default function Command() {
+  const [customDropdownValue, setCustomDropdownValue] = useState("/");
+  const [viewDefaultBranchTypes, setViewDefaultBranchTypes] = useState(true);
+  const [customBranchTypes, setCustomBranchTypes] = useState<string[]>([]);
+
+  useEffect(() => {
+    (async function () {
+      const itemsFromLocalStorage = await LocalStorage.allItems();
+      setCustomBranchTypes(Object.values(itemsFromLocalStorage));
+    })();
+  }, []);
+
   const { handleSubmit, itemProps } = useForm<Values>({
     async onSubmit({ branchType, prefix, description, snakeCase, prefixDate, includeGitCommand }) {
+      // save custom branch type to local storage
+      await LocalStorage.setItem(branchType, branchType);
+
       // leave trailing space for slugify to add separator
       const date = prefixDate ? `${new Date().toISOString().split("T")[0]}-` : "";
       const content = `${date}${prefix}${branchType}${description}`;
@@ -48,22 +64,57 @@ export default function Command() {
     },
   });
 
+  function handleSearchText(searchText: string) {
+    setCustomDropdownValue(searchText);
+  }
+
+  function handleHideDefaultBranchTypes(isHidden: boolean) {
+    setViewDefaultBranchTypes(!isHidden);
+  }
+
+  async function clearCustomBranchTypes() {
+    await LocalStorage.clear();
+    setCustomBranchTypes([]);
+  }
+
   return (
     <Form
       actions={
         <ActionPanel>
           <Action.SubmitForm title="Branch Name to Clipboard" onSubmit={handleSubmit} />
+          <Action.SubmitForm title="Clear Custom Branch Types" onSubmit={clearCustomBranchTypes} />
         </ActionPanel>
       }
     >
-      <Form.Dropdown id="branchType" title="Branch Type" storeValue>
-        <Form.Dropdown.Item value="" title="-" />
-        <Form.Dropdown.Item value="fix/" title="fix (bugs or issues)" />
-        <Form.Dropdown.Item value="feat/" title="feat (add new features)" />
-        <Form.Dropdown.Item value="refactor/" title="refactor (improve efficiency)" />
-        <Form.Dropdown.Item value="chore/" title="chore (non-production changes)" />
-        <Form.Dropdown.Item value="docs/" title="docs (add or modify documentation)" />
+      <Form.Dropdown
+        id="branchType"
+        title="Branch Type"
+        placeholder="Custom... (Enter existing branch type to delete)"
+        onSearchTextChange={handleSearchText}
+        storeValue
+      >
+        <Form.Dropdown.Section title="Custom Branch Types">
+          <Form.Dropdown.Item value={customDropdownValue} title={customDropdownValue} />
+          {customBranchTypes.map((branchType, index) => (
+            <Form.Dropdown.Item key={index} value={branchType} title={branchType} />
+          ))}
+        </Form.Dropdown.Section>
+        {viewDefaultBranchTypes ? (
+          <Form.Dropdown.Section title="Default Branch Types">
+            <Form.Dropdown.Item value="fix/" title="fix/ (bugs or issues)" />
+            <Form.Dropdown.Item value="feat/" title="feat/ (add new features)" />
+            <Form.Dropdown.Item value="refactor/" title="refactor/ (improve efficiency)" />
+            <Form.Dropdown.Item value="chore/" title="chore/ (non-production changes)" />
+            <Form.Dropdown.Item value="docs/" title="docs/ (add or modify documentation)" />
+          </Form.Dropdown.Section>
+        ) : null}
       </Form.Dropdown>
+      <Form.Checkbox
+        id="hideDefaultBranchTypes"
+        label="Hide Default Branch Types"
+        onChange={handleHideDefaultBranchTypes}
+        storeValue
+      />
       <Form.TextField
         {...itemProps.description}
         title="Description"
