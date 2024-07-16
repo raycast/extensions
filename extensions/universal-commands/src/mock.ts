5f0359244d52844d709ed1374b6a861fbfb0efd1
cyxn <nick.sukhov@eze.tech>
2024-07-16 19:22:52 +0200

    Update universal-commands extension
    - Merge branch \'contributions/merge-1721150201782028000\'
    - Pull contributions
    - cleanup mock.ts file

--- extensions/universal-commands/src/mock.ts
+++ extensions/universal-commands/src/mock.ts
@@ -23,15 +23,10 @@ export const applicationShortcutRecordToggleMute: CommandRecord = {
     {
       type: "app",
       applicationName: "zoom.us",
-      // this one is default
       shortcutToRun: {
         key: "a",
         modifiers: ["command", "shift"],
       },
-      // shortcutToRun: {
-      //   key: "d",
-      //   modifiers: ["command"],
-      // },
     },
   ],
   isPredefined: true,
@@ -60,24 +55,8 @@ export const applicationShortcutRaiseHandForMeet: CommandRecord = {
   ],
   isPredefined: true,
 };
-export const toggleHandForDiagramApp: CommandRecord = {
-  id: "3",
-  name: "Toggle Hand For Diagram",
-  shortcuts: [
-    {
-      type: "web",
-      shortcutToRun: {
-        key: "h",
-        modifiers: [""],
-      },
-      websiteUrl: "https://www.tldraw.com/",
-    },
-  ],
-  isPredefined: true,
-};
 
 export const idToCommandMap: Record<CommandRecord["id"], CommandRecord> = {
   1: applicationShortcutRecordToggleMute,
   2: applicationShortcutRaiseHandForMeet,
-  3: toggleHandForDiagramApp,
 };
