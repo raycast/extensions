54b569c676c8b71ba70e3e2a726330d188f70371
🥷⚔️💀👻 <contact@stelo.dev>
2024-07-21 20:34:23 -0700

    refactor(disk-util): remove redundant captureException

--- extensions/disk-util/src/hooks/use-volumes.ts
+++ extensions/disk-util/src/hooks/use-volumes.ts
@@ -1,7 +1,6 @@
 import { useExec } from "@raycast/utils";
 import { parse } from "plist";
 import { ListResult } from "@components/volumes";
-import { captureException } from "@raycast/api";
 
 export const useVolumes = (isScriptsLoading: boolean) => {
   const { isLoading: isLoadingVolumes, data: volumes } = useExec("/usr/sbin/diskutil", ["list", "-plist"], {
@@ -9,8 +8,7 @@ export const useVolumes = (isScriptsLoading: boolean) => {
     parseOutput: (args) => {
       try {
         return parse(args.stdout) as ListResult;
-      } catch (err) {
-        captureException(err);
+      } catch {
         return {} as ListResult;
       }
     },
