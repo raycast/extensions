914122b621d5e478d2ab062151393ad9d57aaa1e
j3lte <jwlagendijk@gmail.com>
2024-07-28 14:39:21 +0200

    Update raindrop-io extension
    - Updates
    - First updates
    - Initial commit

--- extensions/raindrop-io/src/hooks/useBrowserLink.ts
+++ extensions/raindrop-io/src/hooks/useBrowserLink.ts
@@ -9,8 +9,12 @@ export function useBrowserLink() {
       switch (app.bundleId) {
         case "company.thebrowser.Browser":
           return runAppleScript(`tell application "Arc" to return URL of active tab of front window`);
+        case "com.vivaldi.Vivaldi":
+          return runAppleScript(`tell application "Vivaldi" to return URL of active tab of front window`);
         case "com.google.Chrome":
           return runAppleScript(`tell application "Google Chrome" to return URL of active tab of front window`);
+        case "com.brave.Browser":
+          return runAppleScript(`tell application "Brave Browser" to return URL of active tab of front window`);
         case "com.apple.Safari":
           return runAppleScript(`tell application "Safari" to return URL of front document`);
         case "org.mozilla.firefox":
@@ -28,8 +32,15 @@ export function useBrowserLink() {
             end tell
           `);
         default:
-          throw new Error(`Unsupported App: ${app.name}`);
+          break;
       }
+
+      // Fallback for Vivaldi Browser not recognized by bundleId
+      if (app?.name === "Vivaldi.app") {
+        return runAppleScript(`tell application "Vivaldi" to return URL of active tab of front window`);
+      }
+
+      throw new Error(`Unsupported App: ${app.name}`);
     },
     [],
     {
