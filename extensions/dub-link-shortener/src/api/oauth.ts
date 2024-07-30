4aaa75ec6d51202c582a623160498602025a1f3a
Kiran K <kiran@dub.co>
2024-07-30 12:04:13 +0530

    update the scopes

--- extensions/dub-link-shortener/src/api/oauth.ts
+++ extensions/dub-link-shortener/src/api/oauth.ts
@@ -15,7 +15,7 @@ const client = new OAuth.PKCEClient({
 export const dubOAuth = new OAuthService({
   client,
   clientId: "dub_app_d70ed8f23a7eb32a0532776e5de0be08796cc82e12a3c806",
-  scope: ["workspaces.read", "links.write", "tags.read", "domains.read"].join(" "),
+  scope: ["links.write", "tags.write", "workspaces.read", "domains.read"].join(" "),
   authorizeUrl: "https://app.dub.co/oauth/authorize",
   tokenUrl: "https://api.dub.co/oauth/token",
   refreshTokenUrl: "https://api.dub.co/oauth/token",
