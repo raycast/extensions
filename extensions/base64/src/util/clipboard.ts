1e3ca43a9ebbd0674e55a56f2b7b9dba2bbd79bf
VitorLostadaC <vitorlostada@hotmail.com>
2024-07-30 11:28:53 -0300

    Update base64 extension
    - Merge branch \'contributions/merge-1722349582507375000\'
    - Pull contributions
    - Merge branch \'contributions/merge-1722349451016079000\'
    - Pull contributions
    - Merge branch \'contributions/merge-1722349297039045000\'
    - Merge branch \'contributions/merge-1722349123447001000\'
    - Pull contributions
    - feat: update changelog
    - feat: fork from base64
    - feat: update changelog
    - feat: update images
    - fix: texts
    - fix: texts
    - feat: finished the ui
    - feat: ui improviments
    - feat: improve ui
    - feat: add more features
    - fix: lint
    - feat: first commit

--- extensions/base64/src/util/clipboard.ts
+++ extensions/base64/src/util/clipboard.ts
@@ -1,17 +1,6 @@
-import { runAppleScript } from "run-applescript";
-import { copyTextToClipboard, showHUD } from "@raycast/api";
-
-const isEmpty = (string: string) => {
-  return string.length === 0;
-};
-
-export const contents = async () => {
-  const clipboard = await runAppleScript("the clipboard");
-  if (isEmpty(clipboard)) throw "Clipboard is empty";
-  else return clipboard;
-};
+import { Clipboard, showHUD } from "@raycast/api";
 
 export const update = async (contents: string) => {
-  await copyTextToClipboard(contents);
+  await Clipboard.copy(contents);
   showHUD("Copied to clipboard");
 };
