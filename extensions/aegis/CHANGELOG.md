cbb2643cd9fcc62968152997eb198babb46624f2
bfayers <bfayers@users.noreply.github.com>
2024-08-04 02:02:22 +0100

    Add first version of Aegis extension

---
+++ extensions/aegis/CHANGELOG.md
@@ -0,0 +1,7 @@
+# Aegis Extension Changelog
+
+## [Aegis Backup Integration] - 2024-08-04
+
+- Search OTP code
+- Load accounts from encrypted Aegis backup file
+- Copy OTP to clipboard
---
+++ extensions/aegis/src/component/otp/OtpList.tsx
@@ -0,0 +1,35 @@
+import { ActionPanel, Icon, List } from "@raycast/api";
+import { useEffect, useState } from "react";
+import OtpListItems from "./OtpListItems";
+import { Service } from "../../util/service";
+import { checkError, loadData } from "./otp-helpers";
+
+export function OtpList() {
+  const [items, setItems] = useState<{ otpList: Service[]; isLoading: boolean }>({
+    otpList: [],
+    isLoading: true,
+  });
+
+  useEffect(() => {
+    loadData(setItems);
+  }, []);
+
+  // error checking
+  useEffect(() => {
+    checkError(items.otpList, items.isLoading);
+  }, [items]);
+
+  return (
+    <List searchBarPlaceholder="Search" isLoading={items.isLoading}>
+      {items.otpList.length == 0 ? (
+        <List.EmptyView
+          icon={Icon.SpeechBubbleImportant}
+          title={"Add Services with Aegis"}
+          description={"Then export a backup from Aegis and use it here"}
+        />
+      ) : (
+        <OtpListItems items={items.otpList} setItems={setItems} />
+      )}
+    </List>
+  );
+}
