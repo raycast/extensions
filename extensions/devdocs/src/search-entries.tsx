ecc2e41a4dbab68545f9bc4ce85a17f9ba1dd9ea
Dave Powers <djpowers89@gmail.com>
2024-08-07 20:16:08 -0400

    Use URL scheme to open in app instead of HTTP

--- extensions/devdocs/src/search-entries.tsx
+++ extensions/devdocs/src/search-entries.tsx
@@ -65,7 +65,7 @@ function EntryItem({ entry, slug }: { entry: Entry; slug: string }) {
           <Action.Open
             icon="devdocs.png"
             title="Open in DevDocs"
-            target={`https://devdocs.io/${slug}/${entry.path}`}
+            target={`devdocs-macos://search?doc=${slug}&term=${entry.path}`}
             application="DevDocs"
           />
         </ActionPanel>
