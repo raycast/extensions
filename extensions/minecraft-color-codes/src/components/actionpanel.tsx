7550812f2b48c15282c2875bfd93ce9c89a63dd1
lafkpages <luisafk@proton.me>
2024-08-08 23:42:13 +0100

    Update minecraft-color-codes extension
    - feat: fix #13467
    - Pull contributions

--- extensions/minecraft-color-codes/src/components/actionpanel.tsx
+++ extensions/minecraft-color-codes/src/components/actionpanel.tsx
@@ -1,8 +1,56 @@
-import { Action, ActionPanel } from "@raycast/api";
+import { Action, ActionPanel, Cache, getPreferenceValues, showToast } from "@raycast/api";
 
-export function generateActionPanel(chatCode: string, chatCodeEscaped: string, hexCode?: string) {
+export function generateActionPanel({
+  cache,
+  setPrefix,
+  chatCode,
+  chatCodeEscaped,
+  hexCode,
+}: {
+  cache?: Cache;
+  setPrefix?: (prefix: string) => void;
+
+  chatCode: string;
+  chatCodeEscaped: string;
+  hexCode?: string;
+}) {
+  const prefs = getPreferenceValues<Preferences>();
+
   return (
     <ActionPanel>
+      <ActionPanel.Section>
+        <Action
+          title="Use primary prefix"
+          onAction={() => {
+            cache?.set("prefix", prefs.prefix1);
+            setPrefix?.(prefs.prefix1);
+
+            showToast({
+              title: `Prefix changed to ${prefs.prefix1}`,
+            });
+          }}
+          shortcut={{
+            modifiers: ["cmd", "shift"],
+            key: "1",
+          }}
+        />
+        <Action
+          title="Use secondary prefix"
+          onAction={() => {
+            cache?.set("prefix", prefs.prefix2);
+            setPrefix?.(prefs.prefix2);
+
+            showToast({
+              title: `Prefix changed to ${prefs.prefix2}`,
+            });
+          }}
+          shortcut={{
+            modifiers: ["cmd", "shift"],
+            key: "2",
+          }}
+        />
+      </ActionPanel.Section>
+
       <ActionPanel.Section>
         <Action.CopyToClipboard content={chatCode} title="Copy Chat Code" />
         <Action.CopyToClipboard content={chatCodeEscaped} title="Copy Escaped Chat Code" />
--- extensions/minecraft-color-codes/src/index.tsx
+++ extensions/minecraft-color-codes/src/index.tsx
@@ -1,16 +1,29 @@
-import { Icon, List, getPreferenceValues } from "@raycast/api";
+import { Action, ActionPanel, Cache, Icon, List, getPreferenceValues } from "@raycast/api";
 import { generateActionPanel } from "./components/actionpanel";
-import { COLORS, ESCAPED_PREFIX, FORMATTING, PREFIX } from "./constants";
+import { COLORS, FORMATTING } from "./constants";
+import { escapeChar } from "./utils";
+import { useState } from "react";
+
+const cache = new Cache({ capacity: 1 });
+// Use a 1-byte cache to store the prefix character,
+// idk where else to store it
+
+if (!cache.has("prefix")) {
+  cache.set("prefix", getPreferenceValues<Preferences>().prefix1);
+}
 
 export default function Command() {
   const prefs = getPreferenceValues<Preferences>();
+
+  const [prefix, setPrefix] = useState(cache.get("prefix") || prefs.prefix1);
+  const prefixEscaped = escapeChar(prefix);
 
   return (
     <List>
       <List.Section title="Colors">
         {COLORS.map(({ name, chatCode, hexCode, keywords }) => {
-          const fullChatCode = `${PREFIX}${chatCode}`;
-          const chatCodeEscaped = `${ESCAPED_PREFIX}${chatCode}`;
+          const fullChatCode = `${prefix}${chatCode}`;
+          const chatCodeEscaped = `${prefixEscaped}${chatCode}`;
           const formattedHexCode = prefs.hexUpperCase ? hexCode : hexCode.toLowerCase();
 
           return (
@@ -20,7 +33,14 @@ export default function Command() {
               title={name}
               subtitle={fullChatCode}
               keywords={keywords}
-              actions={generateActionPanel(fullChatCode, chatCodeEscaped, formattedHexCode)}
+              actions={generateActionPanel({
+                cache,
+                setPrefix,
+
+                chatCode: fullChatCode,
+                chatCodeEscaped,
+                hexCode: formattedHexCode,
+              })}
             />
           );
         })}
@@ -28,8 +48,8 @@ export default function Command() {
 
       <List.Section title="Formatting">
         {FORMATTING.map(({ name, icon, chatCode }) => {
-          const fullChatCode = `${PREFIX}${chatCode}`;
-          const chatCodeEscaped = `${ESCAPED_PREFIX}${chatCode}`;
+          const fullChatCode = `${prefix}${chatCode}`;
+          const chatCodeEscaped = `${prefixEscaped}${chatCode}`;
 
           return (
             <List.Item
@@ -37,7 +57,13 @@ export default function Command() {
               icon={icon}
               title={name}
               subtitle={fullChatCode}
-              actions={generateActionPanel(fullChatCode, chatCodeEscaped)}
+              actions={generateActionPanel({
+                cache,
+                setPrefix,
+
+                chatCode: fullChatCode,
+                chatCodeEscaped,
+              })}
             />
           );
         })}
