2a56ffa4739eb85c5ed174081e187541ff21dfdf
bfayers <bfayers@users.noreply.github.com>
2024-08-09 12:26:09 +0100

    remove unused code

--- extensions/aegis/src/constants.js
+++ extensions/aegis/src/constants.js
@@ -1,10 +1,1 @@
 exports.icondir = "icons";
-
-exports.genericColors = [
-  { name: "black", value: "#464646" },
-  { name: "blue", value: "#098FCE" },
-  { name: "green", value: "#3DA329" },
-  { name: "orange", value: "#F59E27" },
-  { name: "purple", value: "#7408B4" },
-  { name: "red", value: "#DF1112" },
-];
