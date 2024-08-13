4cdedc3acce9baccd0c60c3f968491a7b81d57af
Chris Messina <chris.messina@gmail.com>
2024-08-13 11:48:34 -0700

    Initial commit for @ Profile extension.

---
+++ extensions/at-profile/src/sites.ts
@@ -0,0 +1,57 @@
+import { LocalStorage } from "@raycast/api";
+
+export interface Site {
+  name: string;
+  value: string;
+  urlTemplate: string;
+}
+
+export const defaultSites: Site[] = [
+  {
+    name: "Raycast",
+    value: "raycast",
+    urlTemplate: "https://www.raycast.com/{profile}",
+  },
+  {
+    name: "Threads",
+    value: "threads",
+    urlTemplate: "https://www.threads.net/@{profile}",
+  },
+  { name: "X", value: "x", urlTemplate: "https://x.com/{profile}" },
+  {
+    name: "GitHub",
+    value: "github",
+    urlTemplate: "https://github.com/{profile}",
+  },
+  {
+    name: "Facebook",
+    value: "facebook",
+    urlTemplate: "https://www.facebook.com/{profile}",
+  },
+  {
+    name: "Reddit",
+    value: "reddit",
+    urlTemplate: "https://www.reddit.com/user/{profile}",
+  },
+  {
+    name: "YouTube",
+    value: "youtube",
+    urlTemplate: "https://www.youtube.com/user/{profile}",
+  },
+  {
+    name: "Instagram",
+    value: "instagram",
+    urlTemplate: "https://www.instagram.com/{profile}",
+  },
+  {
+    name: "LinkedIn",
+    value: "linkedin",
+    urlTemplate: "https://www.linkedin.com/in/{profile}",
+  },
+];
+
+export async function getAllSites(): Promise<Site[]> {
+  const customSitesJson = await LocalStorage.getItem<string>("customSites");
+  const customSites: Site[] = customSitesJson ? JSON.parse(customSitesJson) : [];
+  return [...defaultSites, ...customSites];
+}
