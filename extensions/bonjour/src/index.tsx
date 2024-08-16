98ece41abd61686ddcb0cb9416fac300474e4daf
Chris Bailey <chris@bailey.mx>
2024-08-16 13:52:21 -0400

    add list item details

--- extensions/bonjour/src/index.tsx
+++ extensions/bonjour/src/index.tsx
@@ -15,15 +15,46 @@ export default function Command() {
   }, []);
 
   return (
-    <List isLoading={!items}>
+    <List isLoading={!items} isShowingDetail={true}>
       {items?.map((service: Service, index: number) => {
-        const url = `http://${service.host}`;
+        const url = `${service.type}://${service.host}:${service.port}`;
 
         return (
           <List.Item
             key={index}
             icon={Icon.Globe}
             title={service.name}
+            detail={
+              <List.Item.Detail
+                metadata={
+                  <List.Item.Detail.Metadata>
+                    <List.Item.Detail.Metadata.Link
+                      target={url}
+                      text={url}
+                      title="Location"
+                    />
+                    <List.Item.Detail.Metadata.Label
+                      title="Host"
+                      text={service.fqdn}
+                    />
+                    <List.Item.Detail.Metadata.Separator />
+                    <List.Item.Detail.Metadata.TagList title="Addresses">
+                      {service.addresses?.map((address, index) => (
+                        <List.Item.Detail.Metadata.TagList.Item
+                          key={index}
+                          text={address}
+                        />
+                      ))}
+                    </List.Item.Detail.Metadata.TagList>
+                    <List.Item.Detail.Metadata.Separator />
+                    <List.Item.Detail.Metadata.Label
+                      title="Referer"
+                      text={service.referer?.address}
+                    />
+                  </List.Item.Detail.Metadata>
+                }
+              />
+            }
             actions={
               <ActionPanel>
                 <Action.OpenInBrowser url={url} />
