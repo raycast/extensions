0aaf8ea2c08052bb1d1d0ad2e07efbe4f8de701b
James Turnbull <james@lovedthanlost.net>
2024-08-16 09:16:21 -0400

    Update extensions/nba-game-viewer/src/views/schedule.tsx
    Co-authored-by: stelo <42366677+jfkisafk@users.noreply.github.com>

--- extensions/nba-game-viewer/src/views/schedule.tsx
+++ extensions/nba-game-viewer/src/views/schedule.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 const Schedule = () => {
   const [selectedLeague, setSelectedLeague] = useState<string>("nba"); // Default to NBA
-  const { data, isLoading, setSelectedLeague: updateLeague } = useSchedule(selectedLeague);
+  const { data, isLoading } = useSchedule(selectedLeague);
 
   return (
     <List
