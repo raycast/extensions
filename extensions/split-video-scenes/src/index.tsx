b2fd37b9f6a50539e3700d9a875706742a1d8ab2
remorses <beats.by.morse@gmail.com>
2024-08-17 15:41:09 +0200

    Update split-video-scenes extension
    - index:
    - added cancel process logic
    - added detection method dropdown
    - add sponsor link

--- extensions/split-video-scenes/src/index.tsx
+++ extensions/split-video-scenes/src/index.tsx
@@ -5,11 +5,12 @@ import {
   Detail,
   Form,
   getSelectedFinderItems,
+  Icon,
   showToast,
   Toast,
 } from "@raycast/api";
 import { usePromise } from "@raycast/utils";
-import { exec, execSync } from "child_process";
+import { ChildProcess, ChildProcessWithoutNullStreams, exec, execSync } from "child_process";
 import os from "os";
 import path from "path";
 import { useState } from "react";
@@ -39,8 +40,12 @@ function isCommandInstalled(command: string) {
     return false;
   }
 }
+
+let childProcess: ChildProcess | null = null;
+
 export default function Command() {
   const [logs, setLogs] = useState("");
+  const [detectionMethod, setDetectionMethod] = useState("detect-content");
   const finderItems = usePromise(() => getSelectedFinderItems(), [], {
     onData(data) {
       const els = data?.map((item) => item.path) || [];
@@ -67,21 +72,31 @@ export default function Command() {
             console.log(`Executing install command: ${installCommand}`);
             setLogs((prevLogs) => `Executing install command: ${installCommand}\n${prevLogs}`);
 
-            const childProcess = exec(installCommand, {
+            if (childProcess) {
+              childProcess.kill();
+            }
+            childProcess = exec(installCommand, {
               env: { ...process.env },
             });
 
             childProcess.stdout?.on("data", (data) => {
+              if (childProcess?.killed) {
+                return;
+              }
               console.error(data.toString());
               setLogs((prevLogs) => `${data.toString()}\n${prevLogs}`);
             });
 
             childProcess.stderr?.on("data", (data) => {
+              if (childProcess?.killed) {
+                return;
+              }
               console.error(data.toString());
               setLogs((prevLogs) => `${data.toString()}\n${prevLogs}`);
             });
 
-            const exitCode = await new Promise((resolve) => childProcess.on("exit", (code) => resolve(code || 0)));
+            const exitCode = await new Promise((resolve) => childProcess!.on("exit", (code) => resolve(code || 0)));
+
             if (exitCode === 0) {
               setLogs("");
             } else {
@@ -101,25 +116,31 @@ export default function Command() {
     for (const videoPath of filePaths) {
       const outputDir = path.join(path.dirname(videoPath), path.basename(videoPath, path.extname(videoPath)));
 
-      const command = `scenedetect -i "${videoPath}" -o "${outputDir}" detect-content split-video`;
+      let command = `scenedetect -i "${videoPath}" -o "${outputDir}" ${detectionMethod} split-video`;
 
       console.log(`Executing command: ${command}`);
       setLogs((prevLogs) => `Executing command: ${command}\n${prevLogs}`);
 
-      const childProcess = exec(command, {
+      childProcess = exec(command, {
         env: { ...process.env },
       });
 
       childProcess.stdout?.on("data", (data) => {
+        if (childProcess?.killed) {
+          return;
+        }
         console.error(data.toString());
         setLogs((prevLogs) => `${data.toString()}\n${prevLogs}`);
       });
 
       childProcess.stderr?.on("data", (data) => {
+        if (childProcess?.killed) {
+          return;
+        }
         console.error(data.toString());
         setLogs((prevLogs) => `${data.toString()}\n${prevLogs}`);
       });
-      exitCode = await new Promise((resolve) => childProcess.on("exit", (code) => resolve(code || 0)));
+      exitCode = await new Promise((resolve) => childProcess!.on("exit", (code) => resolve(code || 0)));
     }
     if (exitCode === 0) {
       closeMainWindow();
@@ -128,22 +149,40 @@ export default function Command() {
 
   const [filePaths, setFilePaths] = useState(finderItems?.data?.map((x) => x.path) || []);
 
-  if (finderItems.isLoading) {
+  async function handleCancel() {
+    if (!childProcess) {
+      return;
+    }
+    childProcess.kill();
+    await new Promise((resolve) => childProcess!.on("exit", (code) => resolve(code || 0)));
+    setLogs("");
+  }
+
+  if (logs && filePaths.length > 0) {
     return (
-      <Form>
-        <Form.Description text="Loading..." />
-      </Form>
+      <Detail
+        actions={
+          <ActionPanel>
+            <Action icon={Icon.Trash} title="Cancel" onAction={handleCancel} />
+          </ActionPanel>
+        }
+        isLoading={true}
+        navigationTitle="logs"
+        key="markdown logs"
+        markdown={"```\n" + logs + "\n```"}
+      />
     );
   }
-  if (logs && filePaths.length > 0) {
-    return <Detail navigationTitle="logs" key="markdown logs" markdown={"```\n" + logs + "\n```"} />;
-  }
+
   return (
     <Form
       isLoading={finderItems.isLoading}
+      searchBarAccessory={
+        <Form.LinkAccessory target="https://github.com/sponsors/remorses" text="Sponsor Extension Creator" />
+      }
       actions={
         <ActionPanel>
-          <Action.SubmitForm onSubmit={handleSubmit} title="Detect Scenes" />
+          <Action.SubmitForm onSubmit={handleSubmit} title="Split Video Into Scenes" />
         </ActionPanel>
       }
     >
@@ -156,6 +195,16 @@ export default function Command() {
         value={filePaths}
         onChange={setFilePaths}
       />
+      <Form.Dropdown
+        id="detectionMethod"
+        title="Detection Method"
+        value={detectionMethod}
+        onChange={setDetectionMethod}
+      >
+        <Form.Dropdown.Item value="detect-content" title="Content Detection" />
+        <Form.Dropdown.Item value="detect-threshold" title="Threshold Detection" />
+        <Form.Dropdown.Item value="detect-adaptive" title="Adaptive Detection" />
+      </Form.Dropdown>
     </Form>
   );
 }
