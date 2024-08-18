6a24c4af30ed96b1ae8f6e4e542dcb39183e9253
Chris Bailey <chris@bailey.mx>
2024-08-18 03:47:57 -0400

    make more reliable

--- extensions/bonjour/src/index.tsx
+++ extensions/bonjour/src/index.tsx
@@ -3,70 +3,71 @@ import Bonjour, { Service } from "bonjour-service";
 import { useState } from "react";
 
 const bonjour = new Bonjour();
+const services: { [key: string]: Service } = {};
 
 export default function Command() {
-  const [items, setItems] = useState<Service[]>();
-  const services: Service[] = [];
+  const [items, setItems] = useState<{ [key: string]: Service }>();
 
   bonjour.find({ type: "http" }, (service) => {
-    services.push(service);
+    services[service.name] = service;
     setItems(services);
   });
 
   return (
     <List isLoading={!items} isShowingDetail={true}>
-      {items
-        ?.sort((a, b) => {
-          return a.name > b.name ? 1 : -1;
-        })
-        .map((service: Service, index: number) => {
-          const url = `${service.type}://${service.host}:${service.port}`;
+      {items &&
+        Object.values(items)
+          ?.sort((a, b) => {
+            return a.name > b.name ? 1 : -1;
+          })
+          .map((service: Service, index: number) => {
+            const url = `${service.type}://${service.host}:${service.port}`;
 
-          return (
-            <List.Item
-              key={index}
-              icon={Icon.Globe}
-              title={service.name}
-              detail={
-                <List.Item.Detail
-                  metadata={
-                    <List.Item.Detail.Metadata>
-                      <List.Item.Detail.Metadata.Link
-                        target={url}
-                        text={url}
-                        title="Location"
-                      />
-                      <List.Item.Detail.Metadata.Label
-                        title="Host"
-                        text={service.fqdn}
-                      />
-                      <List.Item.Detail.Metadata.Separator />
-                      <List.Item.Detail.Metadata.TagList title="Addresses">
-                        {service.addresses?.map((address, index) => (
-                          <List.Item.Detail.Metadata.TagList.Item
-                            key={index}
-                            text={address}
-                          />
-                        ))}
-                      </List.Item.Detail.Metadata.TagList>
-                      <List.Item.Detail.Metadata.Separator />
-                      <List.Item.Detail.Metadata.Label
-                        title="Referer"
-                        text={service.referer?.address}
-                      />
-                    </List.Item.Detail.Metadata>
-                  }
-                />
-              }
-              actions={
-                <ActionPanel>
-                  <Action.OpenInBrowser url={url} />
-                  <Action.CopyToClipboard content={url} />
-                </ActionPanel>
-              }
-            />
-          );
-        })}
+            return (
+              <List.Item
+                key={index}
+                icon={Icon.Globe}
+                title={service.name}
+                detail={
+                  <List.Item.Detail
+                    metadata={
+                      <List.Item.Detail.Metadata>
+                        <List.Item.Detail.Metadata.Link
+                          target={url}
+                          text={url}
+                          title="Location"
+                        />
+                        <List.Item.Detail.Metadata.Label
+                          title="Host"
+                          text={service.fqdn}
+                        />
+                        <List.Item.Detail.Metadata.Separator />
+                        <List.Item.Detail.Metadata.TagList title="Addresses">
+                          {service.addresses?.map((address, index) => (
+                            <List.Item.Detail.Metadata.TagList.Item
+                              key={index}
+                              text={address}
+                            />
+                          ))}
+                        </List.Item.Detail.Metadata.TagList>
+                        <List.Item.Detail.Metadata.Separator />
+                        <List.Item.Detail.Metadata.Label
+                          title="Referer"
+                          text={service.referer?.address}
+                        />
+                      </List.Item.Detail.Metadata>
+                    }
+                  />
+                }
+                actions={
+                  <ActionPanel>
+                    <Action.OpenInBrowser url={url} />
+                    <Action.CopyToClipboard content={url} />
+                  </ActionPanel>
+                }
+              />
+            );
+          })}
     </List>
   );
 }
