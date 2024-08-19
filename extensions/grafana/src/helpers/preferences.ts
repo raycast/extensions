a5ff477b7e58f8d80a6fb2e5ebe4c0362d091a11
Francois Le Pape <francois@ninetailed.com>
2024-08-19 17:54:59 +0200

    fix: typo

--- extensions/grafana/src/helpers/preferences.ts
+++ extensions/grafana/src/helpers/preferences.ts
@@ -5,6 +5,6 @@ export const preferences: Preferences = getPreferenceValues();
 export interface Preferences {
   // apiKey is the same as a service account token
   // It is not renamed to `serviceAccountToken` to avoid breaking users' existing configurations
-  apiKey: string;
+  apikey: string;
   rootApiUrl: string;
 }
