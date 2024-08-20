aaf40cde0865c46013810b6123d8ff8768e012d7
Chris Bailey <chris@bailey.mx>
2024-08-19 21:37:22 -0400

    add menu bar command

---
+++ extensions/bonjour/src/list.tsx
@@ -0,0 +1,62 @@
+import { Action, ActionPanel, List } from "@raycast/api";
+import { useCachedState } from "@raycast/utils";
+import { cache, HttpService, KEY } from "./service";
+
+HttpService.fetch();
+export default function Command() {
+  const [items, setItems] = useCachedState<HttpService[]>(KEY);
+
+  cache.subscribe(() => {
+    setItems(HttpService.services);
+  });
+
+  return (
+    <List isLoading={!items} isShowingDetail={true}>
+      {items?.map((service: HttpService, index: number) => {
+        return (
+          <List.Item
+            key={index}
+            title={service.name}
+            detail={
+              <List.Item.Detail
+                metadata={
+                  <List.Item.Detail.Metadata>
+                    <List.Item.Detail.Metadata.Link
+                      title="Location"
+                      target={service.origin}
+                      text={service.origin}
+                    />
+                    <List.Item.Detail.Metadata.Label
+                      title="Host"
+                      text={service.fqdn}
+                    />
+                    <List.Item.Detail.Metadata.Separator />
+                    <List.Item.Detail.Metadata.TagList title="Addresses">
+                      {service.addresses?.map((address, index) => (
+                        <List.Item.Detail.Metadata.TagList.Item
+                          key={index}
+                          text={address}
+                        />
+                      ))}
+                    </List.Item.Detail.Metadata.TagList>
+                    <List.Item.Detail.Metadata.Separator />
+                    <List.Item.Detail.Metadata.Label
+                      title="Referer"
+                      text={service.referer?.address}
+                    />
+                  </List.Item.Detail.Metadata>
+                }
+              />
+            }
+            actions={
+              <ActionPanel>
+                <Action.OpenInBrowser url={service.origin} />
+                <Action.CopyToClipboard content={service.origin} />
+              </ActionPanel>
+            }
+          />
+        );
+      })}
+    </List>
+  );
+}
---
+++ extensions/bonjour/src/menu.tsx
@@ -0,0 +1,38 @@
+import { MenuBarExtra, open } from "@raycast/api";
+import { useCachedState } from "@raycast/utils";
+import { cache, HttpService, KEY } from "./service";
+
+HttpService.fetch();
+export default function Command() {
+  const [items, setItems] = useCachedState<HttpService[]>(KEY);
+
+  cache.subscribe(() => {
+    setItems(HttpService.services);
+  });
+
+  return (
+    <MenuBarExtra
+      isLoading={!items}
+      icon="menubar_icon.png"
+      tooltip="View local services"
+    >
+      {!items ? (
+        <MenuBarExtra.Item title="loading..." />
+      ) : !items.length ? (
+        <MenuBarExtra.Item title="No services found" />
+      ) : (
+        items.map((service: HttpService, index: number) => {
+          return (
+            <MenuBarExtra.Item
+              key={index}
+              title={service.name}
+              onAction={() => {
+                open(service.origin);
+              }}
+            />
+          );
+        })
+      )}
+    </MenuBarExtra>
+  );
+}
