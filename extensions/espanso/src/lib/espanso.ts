ede322751c8660b88787da44c8ab2b1fde8ef7bb
erics118 <52634785+erics118@users.noreply.github.com>
2024-08-23 11:47:30 -0400

    fix: use arch() instead of os() to get apple silicon/intel

--- extensions/espanso/src/lib/espanso.ts
+++ extensions/espanso/src/lib/espanso.ts
@@ -1,5 +1,5 @@
 import { getPreferenceValues } from "@raycast/api";
-import { cpus } from "os";
+import { arch } from "os";
 import { exec } from "child_process";
 
 function execAsync(command: string): Promise<{ stdout: string; stderr: string }> {
@@ -19,7 +19,7 @@ export async function espanso(args: string) {
 
   const espansoPath: string = preferences.espansoPath?.length
     ? preferences.espansoPath
-    : cpus()[0].model.includes("Apple")
+    : arch() === "arm64"
       ? "/opt/homebrew/bin/espanso"
       : "/usr/local/bin/espanso";
 
