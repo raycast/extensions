7b2058a5231f5d3b22075d0b3e754e9828fb2a22
tonngw <1336572985@qq.com>
2024-08-24 10:09:28 +0800

    fix: optimize code

--- extensions/sql-format/CHANGELOG.md
+++ extensions/sql-format/CHANGELOG.md
@@ -1,5 +1,5 @@
 # Changelog
 
-## [Added Format SQL] - 2024-08-23
+## [Added Format SQL] - {PR_MERGE_DATE}
 
 - Initial version, support format a SQL cluase string stored in clipboard and copies it back
--- extensions/sql-format/src/index.tsx
+++ extensions/sql-format/src/index.tsx
@@ -1,10 +1,18 @@
-import { Clipboard } from '@raycast/api';
-import { getPreferenceValues, showHUD, showToast, Toast } from '@raycast/api';
+import {
+  Clipboard,
+  getPreferenceValues,
+  showHUD,
+  showToast,
+  Toast,
+  closeMainWindow,
+  environment,
+  LaunchType,
+} from "@raycast/api";
 
-import { format } from 'sql-formatter';
+import { format } from "sql-formatter";
 
 export default async () => {
-  const output = formatSQL((await Clipboard.readText()) || '');
+  const output = formatSQL((await Clipboard.readText()) || "");
   if (output) {
     await copyFormattedSQL(output);
   }
@@ -13,17 +21,17 @@ export default async () => {
 // format sql
 export function formatSQL(text: string) {
   const trimmedText = text.trim();
-  let output = '';
+  let output = "";
   try {
     if (!validateSQL(trimmedText)) {
       showToastMessage();
       return;
     }
 
-    const tabWidth = getTabWidth();
-    const keywordCase = getKeywordCase();
+    const { tabWidth } = getPreferenceValues<Preferences>();
+    const { keywordCase } = getPreferenceValues<Preferences>();
     const options = {
-      tabWidth: tabWidth === 'tab' ? 8 : parseInt(tabWidth),
+      tabWidth: tabWidth === "tab" ? 8 : parseInt(tabWidth as string),
       keywordCase: keywordCase,
     };
 
@@ -37,121 +45,107 @@ export function formatSQL(text: string) {
 
 // copy or auto paste output
 export async function copyFormattedSQL(result: string) {
-  if (autoPasteEnabled()) {
+  const { autopaste } = getPreferenceValues<Preferences>();
+  if (autopaste) {
     await Clipboard.paste(result);
-    await showHUD('✅ Pasted succesfully!');
+    if (environment.launchType === LaunchType.Background) {
+      await showHUD("✅ Pasted to foremost application");
+    } else {
+      await closeMainWindow();
+      await showToast(Toast.Style.Success, "Pasted to foremost application");
+    }
   } else {
     await Clipboard.copy(result);
-    await showHUD('✅ Copied succesfully!');
+    if (environment.launchType === LaunchType.Background) {
+      await showHUD("✅ Copied to clipboard");
+    } else {
+      await closeMainWindow();
+      await showToast(Toast.Style.Success, "Copied to clipboard");
+    }
   }
-}
-
-// user preferences settings
-interface Preferences {
-  tabWidth: tabWidthType;
-  keywordCase: keywordCaseType;
-  autopaste: boolean;
-}
-
-type tabWidthType = '2' | '4' | 'tab';
-type keywordCaseType = 'preserve' | 'upper' | 'lower';
-
-function getKeywordCase(): keywordCaseType {
-  const { keywordCase } = getPreferenceValues<Preferences>();
-  return keywordCase;
-}
-
-function getTabWidth(): tabWidthType {
-  const { tabWidth } = getPreferenceValues<Preferences>();
-  return tabWidth;
-}
-
-function autoPasteEnabled(): boolean {
-  const { autopaste } = getPreferenceValues<Preferences>();
-  return autopaste;
 }
 
 // sql keywords set
 const sqlKeywords = new Set([
-  'ADD',
-  'ADD CONSTRAINT',
-  'ALL',
-  'ALTER',
-  'ALTER COLUMN',
-  'ALTER TABLE',
-  'AND',
-  'ANY',
-  'AS',
-  'ASC',
-  'BACKUP DATABASE',
-  'BETWEEN',
-  'CASE',
-  'CHECK',
-  'COLUMN',
-  'CONSTRAINT',
-  'CREATE',
-  'CREATE DATABASE',
-  'CREATE INDEX',
-  'CREATE OR REPLACE VIEW',
-  'CREATE TABLE',
-  'CREATE PROCEDURE',
-  'CREATE UNIQUE INDEX',
-  'CREATE VIEW',
-  'DATABASE',
-  'DEFAULT',
-  'DELETE',
-  'DESC',
-  'DISTINCT',
-  'DROP',
-  'DROP COLUMN',
-  'DROP CONSTRAINT',
-  'DROP DATABASE',
-  'DROP DEFAULT',
-  'DROP INDEX',
-  'DROP TABLE',
-  'DROP VIEW',
-  'EXEC',
-  'EXISTS',
-  'FOREIGN KEY',
-  'FROM',
-  'FULL OUTER JOIN',
-  'GROUP BY',
-  'HAVING',
-  'IN',
-  'INDEX',
-  'INNER JOIN',
-  'INSERT INTO',
-  'INSERT INTO SELECT',
-  'IS NULL',
-  'IS NOT NULL',
-  'JOIN',
-  'LEFT JOIN',
-  'LIKE',
-  'LIMIT',
-  'NOT',
-  'NOT NULL',
-  'OR',
-  'ORDER BY',
-  'OUTER JOIN',
-  'PRIMARY KEY',
-  'PROCEDURE',
-  'RIGHT JOIN',
-  'ROWNUM',
-  'SELECT',
-  'SELECT DISTINCT',
-  'SELECT INTO',
-  'SELECT TOP',
-  'SET',
-  'TABLE',
-  'TOP',
-  'TRUNCATE TABLE',
-  'UNION',
-  'UNION ALL',
-  'UNIQUE',
-  'UPDATE',
-  'VALUES',
-  'VIEW',
-  'WHERE',
+  "ADD",
+  "ADD CONSTRAINT",
+  "ALL",
+  "ALTER",
+  "ALTER COLUMN",
+  "ALTER TABLE",
+  "AND",
+  "ANY",
+  "AS",
+  "ASC",
+  "BACKUP DATABASE",
+  "BETWEEN",
+  "CASE",
+  "CHECK",
+  "COLUMN",
+  "CONSTRAINT",
+  "CREATE",
+  "CREATE DATABASE",
+  "CREATE INDEX",
+  "CREATE OR REPLACE VIEW",
+  "CREATE TABLE",
+  "CREATE PROCEDURE",
+  "CREATE UNIQUE INDEX",
+  "CREATE VIEW",
+  "DATABASE",
+  "DEFAULT",
+  "DELETE",
+  "DESC",
+  "DISTINCT",
+  "DROP",
+  "DROP COLUMN",
+  "DROP CONSTRAINT",
+  "DROP DATABASE",
+  "DROP DEFAULT",
+  "DROP INDEX",
+  "DROP TABLE",
+  "DROP VIEW",
+  "EXEC",
+  "EXISTS",
+  "FOREIGN KEY",
+  "FROM",
+  "FULL OUTER JOIN",
+  "GROUP BY",
+  "HAVING",
+  "IN",
+  "INDEX",
+  "INNER JOIN",
+  "INSERT INTO",
+  "INSERT INTO SELECT",
+  "IS NULL",
+  "IS NOT NULL",
+  "JOIN",
+  "LEFT JOIN",
+  "LIKE",
+  "LIMIT",
+  "NOT",
+  "NOT NULL",
+  "OR",
+  "ORDER BY",
+  "OUTER JOIN",
+  "PRIMARY KEY",
+  "PROCEDURE",
+  "RIGHT JOIN",
+  "ROWNUM",
+  "SELECT",
+  "SELECT DISTINCT",
+  "SELECT INTO",
+  "SELECT TOP",
+  "SET",
+  "TABLE",
+  "TOP",
+  "TRUNCATE TABLE",
+  "UNION",
+  "UNION ALL",
+  "UNIQUE",
+  "UPDATE",
+  "VALUES",
+  "VIEW",
+  "WHERE",
 ]);
 
 // validate input string whether it is a sql
@@ -174,6 +168,6 @@ function validateSQL(sql: string) {
 function showToastMessage() {
   showToast({
     style: Toast.Style.Failure,
-    title: 'Please copy a valid SQL clause',
+    title: "Please copy a valid SQL clause",
   });
 }
