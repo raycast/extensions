2e9b020095579c2abf3eab75d2ef03029612c6dd
anon <>
2024-08-26 17:23:22 +0000

    Code Review: Refactoring

--- extensions/prowlarr/src/search/utils.tsx
+++ extensions/prowlarr/src/search/utils.tsx
@@ -1,21 +1,14 @@
 import { ReleaseResource } from "../prowlarrApi";
 import { SearchFormValues, Sort } from "./types";
 
-export function formatBytes(bytes: number) {
-  const marker = 1024; // Change to 1000 if required
-  const decimal = 1; // Change as required
-  const kiloBytes = marker; // One Kilobyte is 1024 bytes
-  const megaBytes = marker * marker; // One MB is 1024 KB
-  const gigaBytes = marker * marker * marker; // One GB is 1024 MB
+export function formatBytes(bytes: number, precision: number = 1): string {
+  const units = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+  if (bytes === 0) return "0 Bytes";
 
-  // return bytes if less than a KB
-  if (bytes < kiloBytes) return bytes + " Bytes";
-  // return KB if less than a MB
-  else if (bytes < megaBytes) return (bytes / kiloBytes).toFixed(decimal) + " KB";
-  // return MB if less than a GB
-  else if (bytes < gigaBytes) return (bytes / megaBytes).toFixed(decimal) + " MB";
-  // return GB if less than a TB
-  else return (bytes / gigaBytes).toFixed(decimal) + " GB";
+  const k = 1024;
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(precision)) + " " + units[i];
 }
 
 export function getSearchResultSortPredicate(values: SearchFormValues) {
