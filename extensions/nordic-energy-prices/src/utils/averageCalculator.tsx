1ccfb125c3ac0f49ff8efd765e4f61cf99ff42ca
Per Nielsen Tikær <per@raycast.com>
2024-08-27 12:19:08 +0200

    Update averageCalculator.tsx

--- extensions/nordic-energy-prices/src/utils/averageCalculator.tsx
+++ extensions/nordic-energy-prices/src/utils/averageCalculator.tsx
@@ -3,6 +3,6 @@ import { PriceCurrency, PriceType } from "../types/energyData";
 export const averageCalculator = (data: PriceType[]) => {
   if (data.length === 0) return 0;
 
-  const sum = data.reduce((acc, item) => acc += item[PriceCurrency], 0);
+  const sum = data.reduce((acc, item) => (acc += item[PriceCurrency]), 0);
   return sum / data.length;
 };
