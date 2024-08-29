9e3d0fa15c82c82d4de12b5cbaeec5d4b22c3a02
stelo <42366677+jfkisafk@users.noreply.github.com>
2024-08-29 01:31:44 -0700

    [Format JSON] Updates to extension (#14155)
    * fix(json-format): json line command fixes and used toasts instead of huds
    
    * feat(json-format): adds command to format selected text
    
    * refactor(json-format): changed view result icon
    
    * refactor(json-format): adds detail view to see formatted json as decorated markdown
    
    * Update CHANGELOG.md and optimise images
    
    ---------
    
    Co-authored-by: raycastbot <bot@raycast.com>

--- extensions/json-format/src/formatSelection.tsx
+++ extensions/json-format/src/formatSelection.tsx
@@ -1,8 +1,8 @@
-import { Clipboard } from "@raycast/api";
+import { getSelectedText } from "@raycast/api";
 import { formatJS, copyFormattedJs } from "./utils";
 
 export default async () => {
-  const output = await formatJS((await Clipboard.readText()) || "");
+  const output = await formatJS(await getSelectedText());
 
   if (output) {
     await copyFormattedJs(output);
