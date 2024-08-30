a140f8a22c08632f8ec75533641e50b4b0242316
Per Nielsen Tikær <per@raycast.com>
2024-08-30 14:12:02 +0200

    Fixed errorView

--- extensions/nextdns/src/components/empty-view.tsx
+++ extensions/nextdns/src/components/empty-view.tsx
@@ -1,23 +1,40 @@
-import { Action, ActionPanel, Icon, Image, Keyboard, List } from "@raycast/api";
+import { Action, ActionPanel, Icon, Image, Keyboard, List, openExtensionPreferences } from "@raycast/api";
 import AddDomain from "./add-domain";
 import { Mutate } from "../types";
 
-export function EmptyView(props: { title: string; icon: Icon | Image.ImageLike; type?: string; mutate?: Mutate }) {
-  const { title, icon, mutate, type } = props;
+export function EmptyView(props: {
+  title: string;
+  description: string;
+  icon: Icon | Image.ImageLike;
+  type?: string;
+  mutate?: Mutate;
+}) {
+  const { title, description, icon, mutate, type } = props;
 
   //TODO: Can we clearsearchbar after running AddComain here?
   return (
     <List.EmptyView
       title={title}
+      description={description}
       icon={icon}
       actions={
         <ActionPanel>
-          <Action.Push
-            icon={Icon.Plus}
-            title="Add New Domain"
-            shortcut={Keyboard.Shortcut.Common.New}
-            target={<AddDomain type={type ?? ""} mutate={mutate ?? undefined} />}
-          />
+          {mutate && (
+            <Action.Push
+              icon={Icon.Plus}
+              title="Add New Domain"
+              shortcut={Keyboard.Shortcut.Common.New}
+              target={<AddDomain type={type ?? ""} mutate={mutate ?? undefined} />}
+            />
+          )}
+          <ActionPanel.Section>
+            <Action
+              title="Open Extension Preferences"
+              onAction={openExtensionPreferences}
+              icon={Icon.Gear}
+              shortcut={{ modifiers: ["cmd"], key: "," }}
+            />
+          </ActionPanel.Section>
         </ActionPanel>
       }
     />
--- extensions/nextdns/src/components/error-view.tsx
+++ extensions/nextdns/src/components/error-view.tsx
@@ -1,10 +1,14 @@
-import { Icon, List } from "@raycast/api";
+import { Color, Icon, List } from "@raycast/api";
 import { EmptyView } from "./empty-view";
 
 export function ErrorView() {
   return (
     <List>
-      <EmptyView title="Failed to load domains" icon={Icon.Warning}></EmptyView>
+      <EmptyView
+        title="Failed to load data"
+        description="Please check credentials and try again"
+        icon={{ source: Icon.Warning, tintColor: Color.Red }}
+      ></EmptyView>
     </List>
   );
 }
