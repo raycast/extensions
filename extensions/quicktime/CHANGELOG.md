09bcee74d412d31e0da4c4891bda4c3f66bdaff1
xmok <30526133+xmok@users.noreply.github.com>
2024-08-30 18:00:01 +0500

    [Quicktime] new movie recording command + use ray/utils applescript

--- extensions/quicktime/CHANGELOG.md
+++ extensions/quicktime/CHANGELOG.md
@@ -1,4 +1,9 @@
-# Brew Changelog
+# QuickTime Recording Changelog
+
+## [Enhancements] - {PR_MERGE_DATE}
+- New command to `Start Movie Recording`
+- Use Raycast `runAppleScript`
+- Add Error Handling
 
 ## [New commands] - 2022-04-02
 - Added one click "Start Audio Recording"
--- extensions/quicktime/src/start-movie-recording.tsx
+++ extensions/quicktime/src/start-movie-recording.tsx
@@ -3,9 +3,9 @@ import { runAppleScript, showFailureToast } from "@raycast/utils";
 
 export default async () => {
   try {
-    await runAppleScript('try \n tell application "QuickTime Player" to start (new screen recording) \n end try');
+    await runAppleScript('try \n tell application "QuickTime Player" to start (new movie recording) \n end try');
     await closeMainWindow();
-    await showHUD("Started screen recording");
+    await showHUD("Started movie recording");
   } catch (error) {
     showFailureToast(error, { title: "Could not run AppleScript" });
   }
