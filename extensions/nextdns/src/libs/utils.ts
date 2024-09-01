668dcbc7695ee9f8fcc292665f1a5c69496a6b9a
Per Nielsen Tikær <per@raycast.com>
2024-09-02 01:00:07 +0200

    fix

--- extensions/nextdns/src/libs/utils.ts
+++ extensions/nextdns/src/libs/utils.ts
@@ -1,4 +1,5 @@
-import { Icon } from "@raycast/api";
+import { Color, Icon } from "@raycast/api";
+import { Log } from "../types";
 
 export function getIdHex(id: string) {
   return Buffer.from(id).toString("hex");
@@ -8,5 +9,23 @@ export function getIconById(id: string) {
   return {
     source: `https://favicons.nextdns.io/hex:${getIdHex(id)}@2x.png`,
     fallback: Icon.Globe,
+  };
+}
+
+export function getStatusTag(status: string) {
+  switch (status) {
+    case "blocked":
+      return { value: "Blocked", color: Color.Red };
+    case "allowed":
+      return { value: "Allowed", color: Color.Green };
+    default:
+      return null;
+  }
+}
+
+export function getDeviceTag(device: Log["device"] | null) {
+  return {
+    tag: device ? device.name : "Unknown",
+    tooltip: device ? device.model : "No model",
   };
 }
--- extensions/nextdns/src/logs.tsx
+++ extensions/nextdns/src/logs.tsx
@@ -1,35 +1,34 @@
-import { Action, ActionPanel, Color, Icon, List } from "@raycast/api";
+import { Action, ActionPanel, getPreferenceValues, Icon, List, openCommandPreferences } from "@raycast/api";
 import { getLogs } from "./libs/api";
-import { getIconById } from "./libs/utils";
+import { getDeviceTag, getIconById, getStatusTag } from "./libs/utils";
 import { Log } from "./types";
 
 export default function Logs() {
   const { data, isLoading, revalidate } = getLogs();
+  const { hideDeviceNames } = getPreferenceValues<Preferences.Logs>();
 
   return (
     <List isLoading={isLoading}>
       {data.map((log: Log) => {
-        console.log(log);
         const date = new Date(log.timestamp);
         return (
           <List.Item
             title={log.domain}
             icon={getIconById(log.domain)}
             accessories={[
-              {
-                tag:
-                  log.status === "blocked"
-                    ? { value: "Blocked", color: Color.Red }
-                    : log.status === "allowed"
-                      ? { value: "Allowed", color: Color.Green }
-                      : null,
-              },
-              { tag: log.device ? log.device.name : "Unknown", tooltip: log.device ? log.device.model : "No model" },
+              { tag: getStatusTag(log.status) },
+              ...(hideDeviceNames ? [] : [getDeviceTag(log.device)]),
               { date: date, tooltip: date.toLocaleDateString() },
             ]}
             actions={
               <ActionPanel>
                 <Action title="Reload" icon={Icon.RotateClockwise} onAction={revalidate} />
+                <Action
+                  title="Open Command Preferences"
+                  onAction={openCommandPreferences}
+                  icon={Icon.Gear}
+                  shortcut={{ modifiers: ["cmd", "shift"], key: "," }}
+                />
               </ActionPanel>
             }
           />
