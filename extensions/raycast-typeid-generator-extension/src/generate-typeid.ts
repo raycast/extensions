00fcf6952950e00f59876ac5a4332825fa9ece60
Lukas Runge <login@lukasrunge.de>
2024-09-01 20:56:26 +0200

    ✨ add option for inserting new id at curor position

--- extensions/raycast-typeid-generator-extension/src/generate-typeid.ts
+++ extensions/raycast-typeid-generator-extension/src/generate-typeid.ts
@@ -1,11 +1,29 @@
-import { Clipboard, LaunchProps, showHUD } from "@raycast/api";
+import { Clipboard, LaunchProps, showHUD, getPreferenceValues } from "@raycast/api";
 import { typeid } from "typeid-js";
 
+interface Preferences {
+  disableCopyToClipboard: boolean;
+  insert: boolean;
+}
+
 export default async function generateTypeid(props: LaunchProps<{ arguments: Arguments.GenerateTypeid }>) {
+  const preferences = getPreferenceValues<Preferences>();
+
+  if (preferences.disableCopyToClipboard && !preferences.insert) {
+    await showHUD("Doing nothing.");
+    return;
+  }
+
   const { prefix } = props.arguments;
   const uuid = typeid(prefix).toString();
 
-  await Clipboard.copy(uuid);
+  if (!preferences.disableCopyToClipboard) {
+    await Clipboard.copy(uuid);
+    await showHUD(`✅ Copied new UUID: ${uuid}`);
+  }
 
-  await showHUD(`✅ Copied new UUID: ${uuid}`);
+  if (preferences.insert) {
+    await Clipboard.paste(uuid);
+    await showHUD(`✅ Inserted new UUID: ${uuid}`);
+  }
 }
