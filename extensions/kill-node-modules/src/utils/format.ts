8b4aed95230106da9ced39c9f2683f3bbd890cc9
NicolasMontone <montonenicolas01@gmail.com>
2024-09-03 12:08:57 -0300

    fix: improve format function.

--- extensions/kill-node-modules/src/utils/format.ts
+++ extensions/kill-node-modules/src/utils/format.ts
@@ -1,10 +1,7 @@
 export function formatSize(bytes: number): string {
   const units = ["B", "KB", "MB", "GB", "TB"];
   let size = bytes;
-  let unitIndex = 0;
-  while (size >= 1024 && unitIndex < units.length - 1) {
-    size /= 1024;
-    unitIndex++;
-  }
+  const unitIndex = Math.floor(Math.log(size) / Math.log(1024));
+  size /= Math.pow(1024, unitIndex);
   return `${size.toFixed(2)} ${units[unitIndex]}`;
 }
