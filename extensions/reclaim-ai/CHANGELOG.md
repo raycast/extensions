51fc827b301548986265a1aab313ec2c73db38a0
Clayton Knapp <80793319+clayton-knapp@users.noreply.github.com>
2024-09-03 17:01:47 -0700

    cleanup + changelog

--- extensions/reclaim-ai/CHANGELOG.md
+++ extensions/reclaim-ai/CHANGELOG.md
@@ -1,5 +1,8 @@
 # reclaim Changelog
 
+## [Fixes] - 2024-09-03
+- Break out separate components and hook for Scheduling Links
+
 ## [Fixes] - 2024-08-23
 - Stopped rapid-fire calls to `GET:/api/smart-habits`
     - New hook `useSyncCachedPromise`
--- extensions/reclaim-ai/src/scheduling-links.tsx
+++ extensions/reclaim-ai/src/scheduling-links.tsx
@@ -6,64 +6,6 @@ import { ListItemDetailMetadataField } from "./components/ListItemDetailMetadata
 import { SchedulingLinkActionPanel } from "./components/SchedulingLinkActionPanel";
 import { resolveTimePolicy } from "./utils/time-policy";
 
-// const SLActions = ({ link }: { link: SchedulingLink }) => {
-//   const url = `https://app.reclaim.ai/m/${link.pageSlug}/${link.slug}`;
-//   const { currentUser } = useUser();
-
-//   const shareTimesEnabled = useMemo(() => {
-//     return !!currentUser?.features.schedulingLinks.shareTimesEnabled;
-//   }, [currentUser]);
-
-//   const createOneOffLink = async () => {
-//     const [oneOff, error] = await axiosPromiseData<SchedulingLink>(
-//       fetcher("/scheduling-link/derivative", {
-//         method: "POST",
-//         data: {
-//           parentId: link.id,
-//         },
-//       })
-//     );
-
-//     if (!error && oneOff) {
-//       open(`https://app.reclaim.ai/scheduling-links?personalize=${oneOff.id}`);
-//     } else {
-//       await showToast({
-//         style: Toast.Style.Failure,
-//         title: "Error creating one-off link. Try using the reclaim.ai web app.",
-//       });
-//     }
-//   };
-
-//   const createShareLink = async () => {
-//     open(`https://app.reclaim.ai/quick-forms/scheduling-links/${link.id}/available-times`);
-//   };
-
-//   return (
-//     <ActionPanel>
-//       <Action.CopyToClipboard title="Copy Link to Clipboard" content={url} />
-//       {shareTimesEnabled && <Action icon={Icon.AddPerson} title="Personalize and Share" onAction={createShareLink} />}
-//       {!shareTimesEnabled && <Action icon={Icon.AddPerson} title="Create One Off Link" onAction={createOneOffLink} />}
-//       <Action.Open title="Open in Browser" target={url} />
-//     </ActionPanel>
-//   );
-// };
-
-// const ListDetailMetadataField = ({
-//   title,
-//   value,
-//   description,
-// }: {
-//   title: string;
-//   value: string;
-//   description?: string;
-// }) => (
-//   <>
-//     <List.Item.Detail.Metadata.Label title={title} />
-//     <List.Item.Detail.Metadata.Label title={value} text={description} />
-//     <List.Item.Detail.Metadata.Separator />
-//   </>
-// );
-
 export default function Command() {
   const [searchText, setSearchText] = useState("");
 
