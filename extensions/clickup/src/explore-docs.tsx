46cc0bc2ffdf0237bfab0d8c821222e6f1b45765
Syed M Mokaarim Bin Razi <30526133+xmok@users.noreply.github.com>
2024-09-10 19:29:28 +0500

    [ClickUp] close #14111 (new command) + utilize `usePromise` + remove use of "any" + add List Sections + migrate (#14312)
    * [ClickUp] hook and docs working - need to remove any
    
    * [ClickUp] removed any and now using the new hook for GET
    
    * [ClickUp] modify extension Title
    
    * [ClickUp] make hooks non default export + improve Task types
    
    * Update CHANGELOG.md and optimise images
    
    ---------
    
    Co-authored-by: raycastbot <bot@raycast.com>

--- extensions/clickup/src/explore-docs.tsx
+++ extensions/clickup/src/explore-docs.tsx
@@ -1,6 +1,6 @@
 import { ActionPanel, Icon, List, Action } from "@raycast/api";
 import { useTeams } from "./hooks/useTeams";
-import { TeamSpaces } from "./views/TeamSpaces";
+import { ListDocs } from "./views/DocList/ListDocs";
 
 export default function Teams() {
   const { isLoading, teams } = useTeams();
@@ -10,13 +10,14 @@ export default function Teams() {
         {teams.map((team, index) => (
           <List.Item
             key={index}
-            icon={Icon.TwoPeople}
+            icon={Icon.Person}
             title={team.name}
             actions={
               <ActionPanel title="Team Actions">
                 <Action.Push
-                  title="Projects Page"
-                  target={<TeamSpaces teamId={team?.id ?? ""} teamName={team?.name ?? ""} />}
+                  icon={Icon.Eye}
+                  title="Docs Page"
+                  target={<ListDocs workspaceId={team.id} workspaceName={team.name} />}
                 />
               </ActionPanel>
             }
