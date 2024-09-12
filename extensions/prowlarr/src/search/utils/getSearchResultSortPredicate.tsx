539758e42f7058640449764c1fa80bdd38e3464e
Andrei Lineishchikov <quick.gate0982@fastmail.com>
2024-09-12 06:51:28 +0200

    Update prowlarr extension (#14419)
    * Update prowlarr extension
    
    - Update screenshots
    - Update changelog
    - Display action error in toast
    - Change details format
    - Bugfix: fix adding to download client
    - Refactoring
    - Add "keep category selection" and "keep indexer selection" preferences
    - Refactoring. Add Category filter to SearchForm
    - Add indexer picker to search form
    - Simplify default sort - use storeValue prop
    - Initial commit
    
    * Fix linter errors
    
    * Update CHANGELOG.md and optimise images
    
    ---------
    
    Co-authored-by: anon <>
    Co-authored-by: raycastbot <bot@raycast.com>

--- extensions/prowlarr/src/search/utils/getSearchResultSortPredicate.tsx
+++ extensions/prowlarr/src/search/utils/getSearchResultSortPredicate.tsx
@@ -1,15 +1,5 @@
-import { ReleaseResource } from "../prowlarrApi";
-import { SearchFormValues, Sort } from "./types";
-
-export function formatBytes(bytes: number, precision: number = 1): string {
-  const units = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
-  if (bytes === 0) return "0 Bytes";
-
-  const k = 1024;
-  const i = Math.floor(Math.log(bytes) / Math.log(k));
-
-  return parseFloat((bytes / Math.pow(k, i)).toFixed(precision)) + " " + units[i];
-}
+import { ReleaseResource } from "../../prowlarrApi";
+import { SearchFormValues, Sort } from "../types";
 
 export function getSearchResultSortPredicate(values: SearchFormValues) {
   switch (values.sort) {
