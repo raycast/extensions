3bb2cd258e80efdfcb08150487b36be4eac8beba
Mortada Sarheed <mortadasarheed@gmail.com>
2024-09-28 08:54:52 +0200

    flcikering fixed

--- extensions/nordic-energy-prices/src/today.tsx
+++ extensions/nordic-energy-prices/src/today.tsx
@@ -1,6 +1,6 @@
-import { List, showToast, Toast } from "@raycast/api";
+import { Detail, List, showToast, Toast } from "@raycast/api";
 import { useFetch } from "@raycast/utils";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { BASEURL, HEADERS, PriceCurrency } from "./constants/preferences";
 import { generateUrl } from "./utils/queryGenerator";
 import { PriceSuffix, PriceType } from "./types/energyData";
@@ -13,7 +13,7 @@ export default function Command() {
   const url = generateUrl(new Date(), BASEURL);
   const [selectedItemId, setSelectedItemId] = useState<string>();
 
-  const { data, isLoading } = useFetch(url, {
+  const { data, isLoading, error } = useFetch(url, {
     headers: HEADERS,
     async onWillExecute() {
       await showToast({
@@ -32,13 +32,21 @@ export default function Command() {
         title: `Successfully fetched energy prices`,
         style: Toast.Style.Success,
       });
-      setAverage(averageCalculator(data));
       setSelectedItemId(getCurrentTime());
     },
     initialData: [],
     keepPreviousData: true,
   });
-  return (
+
+  useEffect(() => {
+    if (data.length > 0) {
+      setAverage(averageCalculator(data));
+    }
+  }, [data]);
+
+  return error ? (
+    <Detail markdown={error.message} />
+  ) : (
     <List
       isLoading={isLoading}
       searchBarPlaceholder={"Search for a time period..."}
@@ -46,7 +54,7 @@ export default function Command() {
     >
       <List.Section
         title={"Today's energy prices"}
-        subtitle={`Daily Average: ${isLoading ? "Loading..." : averageCalculator(data).toFixed(2) + " " + PriceSuffix + " " + PriceCurrency}`}
+        subtitle={`Daily Average: ${isLoading ? "Loading..." : average.toFixed(2) + " " + PriceSuffix + " " + PriceCurrency}`}
       >
         {data.map((price: PriceType) => (
           <PriceEntry allData={data} price={price} average={average} key={price.time_start} />
