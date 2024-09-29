12b6916018a227deda8008d70a92b991c81e47ca
xmok <30526133+xmok@users.noreply.github.com>
2024-09-30 01:12:27 +0500

    [Rebrandly] separate the API Headers

--- extensions/rebrandly/src/hooks.ts
+++ extensions/rebrandly/src/hooks.ts
@@ -1,25 +1,20 @@
 import { useFetch } from "@raycast/utils";
-import { API_KEY, API_URL, MAX_PAGE_SIZE } from "./config";
+import { API_HEADERS, API_URL, MAX_PAGE_SIZE } from "./config";
 import { BrandedLink } from "./types";
 
-const API_PARAMS = {
-  apikey: API_KEY,
-  Accept: "application/json",
-  "Content-Type": "application/json",
-};
 export const useGetLinks = () =>
   useFetch(
     (options) =>
       API_URL +
       "links?" +
       new URLSearchParams({
-        ...API_PARAMS,
         orderBy: "slashtag",
         orderDir: "asc",
         limit: String(MAX_PAGE_SIZE),
         last: options.cursor || "",
       }).toString(),
     {
+      headers: API_HEADERS,
       mapResult(result: BrandedLink[]) {
         return {
           data: result,
