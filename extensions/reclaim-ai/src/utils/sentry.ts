4c70a8f41fb4282e59b80c8445ee155d4d2d1c67
Sandy <sandy@reclaim.ai>
2024-10-01 12:56:09 -0400

    Better typing and return type

--- extensions/reclaim-ai/src/utils/sentry.ts
+++ extensions/reclaim-ai/src/utils/sentry.ts
@@ -1,28 +1,58 @@
 import { startSpanManual, StartSpanOptions, captureException, ExclusiveEventHintOrCaptureContext } from "@sentry/node";
 
 export type ErrorCoverageOptions = {
-  onError?: (e: unknown) => boolean | undefined;
+  onError?: (e: unknown) => Error | boolean | undefined;
   noReport?: boolean;
   hint?: ExclusiveEventHintOrCaptureContext;
+  rethrowExceptions?: boolean;
 };
 
-export const errorCoverage = (spanOptions: StartSpanOptions, cb: () => unknown, options: ErrorCoverageOptions = {}) => {
-  const { onError, noReport, hint } = options;
+export type ErrorCoverageReturnType<T> =
+  | { status: "DATA"; data: T; error: never }
+  | { status: "ERROR"; data: never; error: unknown };
 
-  const handleError = (e: unknown) => {
-    const result = onError?.(e);
-    if (result !== false && !noReport) captureException(e, hint);
-  };
+export function errorCoverage<T>(
+  spanOptions: StartSpanOptions,
+  cb: () => Promise<T>,
+  options?: ErrorCoverageOptions
+): Promise<ErrorCoverageReturnType<T>>;
+export function errorCoverage<T>(
+  spanOptions: StartSpanOptions,
+  cb: () => T,
+  options?: ErrorCoverageOptions
+): ErrorCoverageReturnType<T>;
+export function errorCoverage<T>(
+  spanOptions: StartSpanOptions,
+  cb: () => T | Promise<T>,
+  options: ErrorCoverageOptions = {}
+): ErrorCoverageReturnType<T> | Promise<ErrorCoverageReturnType<T>> {
+  const { onError, noReport, hint, rethrowExceptions } = options;
 
-  startSpanManual(spanOptions, (span) => {
-    let result: unknown;
+  return startSpanManual(spanOptions, (span) => {
+    const handleError = (error: unknown): ErrorCoverageReturnType<T> => {
+      const result = onError?.(error);
+      error = result instanceof Error ? result : error;
+      if (result !== false && !noReport) captureException(error, hint);
+      if (rethrowExceptions) throw error;
+      return { status: "ERROR", data: undefined as never, error };
+    };
+
+    let data: T | Promise<T> | undefined;
+
     try {
-      result = cb();
+      data = cb();
     } catch (e) {
-      handleError(e);
-    } finally {
-      if (result instanceof Promise) result.catch((e) => handleError(e)).finally(() => span.end());
-      else span.end();
+      return handleError(e);
+    }
+
+    if (data instanceof Promise)
+      return data
+        .then<ErrorCoverageReturnType<T>>((data) => ({ status: "DATA", data, error: undefined as never }))
+        .catch((e) => handleError(e))
+        .finally(() => span.end());
+    else {
+      span.end();
+      return { status: "DATA", data, error: undefined as never };
     }
   });
-};
+}
