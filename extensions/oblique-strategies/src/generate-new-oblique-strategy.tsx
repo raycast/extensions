a4c4ef05020251b76efa87674aac1f557f4a6675
Dennis Muensterer <dennismuensterer@gmail.com>
2024-10-04 19:08:31 +0200

    Update oblique-strategies extension
    - Merge branch \'contributions/merge-1728061702356217000\'
    - Pull contributions
    - Check for AI access

--- extensions/oblique-strategies/src/generate-new-oblique-strategy.tsx
+++ extensions/oblique-strategies/src/generate-new-oblique-strategy.tsx
@@ -1,16 +1,16 @@
-import { Action, ActionPanel, Detail } from "@raycast/api";
+import { AI, Action, ActionPanel, Detail, environment } from "@raycast/api";
 import { useAI } from "@raycast/utils";
 
 const OBLIQUE_PROMPT =
   "Create a new oblique strategy in the vain of Brian Eno and Peter Schmidt. Only reply with the short suggestion, nothing else.";
 
 export default function Command() {
-  const { data, isLoading } = useAI(OBLIQUE_PROMPT);
+  const { data, isLoading } = useAI(OBLIQUE_PROMPT, { execute: environment.canAccess(AI) });
 
   return (
     <Detail
       isLoading={isLoading}
-      markdown={data}
+      markdown={data || "You don't have AI access :("}
       actions={
         <ActionPanel>
           <Action.CopyToClipboard content={data} shortcut={{ modifiers: ["cmd"], key: "c" }} />
