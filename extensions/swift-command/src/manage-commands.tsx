c598e493244891abbcf76bbd80ff9cffa9f58372
AaronLin <1546848781@qq.com>
2024-10-04 17:26:21 +0800

    Update swift-command extension
    - docs: update screencast
    - chore: update command title
    - chore: remove --skip-types in build script
    - fix: empty state flicker
    - fix: add style to delete command

--- extensions/swift-command/src/manage-commands.tsx
+++ extensions/swift-command/src/manage-commands.tsx
@@ -1,12 +1,10 @@
 import { ActionPanel, List, Action, Icon, showToast, Toast } from "@raycast/api";
 
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { getDatasourceFolderPath, getDatasourcePath } from "./perference";
 import { createDataSource, DataItem, DataSource } from "./datasource";
 import { commandFilter } from "./filter";
 import { ActionDataItem, ArgumentForm, CommandForm } from "./form";
-
-const cmdItems: DataItem[] = [];
 
 export function filterCommand(cmds: DataItem[], searchKey: string) {
   if (searchKey === "") {
@@ -20,46 +18,55 @@ export function filterCommand(cmds: DataItem[], searchKey: string) {
 
 export default function Command() {
   const [searchBarText, setSearchBarText] = useState("");
-  const [items, setItems] = useState<DataItem[]>(cmdItems);
+  const [fullItems, setFullItems] = useState<DataItem[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [dataSource, setDataSource] = useState<DataSource | null>(null);
 
   const filePath = getDatasourcePath();
 
-  let dataSource: DataSource;
-  let cmds: DataItem[] = [];
-  try {
-    dataSource = createDataSource(filePath);
-    cmds = dataSource.getAll();
-  } catch (error) {
-    showToast({
-      style: Toast.Style.Failure,
-      title: "Failed to load data source",
-      message: error as string,
-    });
-  }
-
   useEffect(() => {
-    searchBarTextChange();
-  }, [searchBarText]);
+    try {
+      const dataSource = createDataSource(filePath);
+      setDataSource(dataSource);
+      setFullItems(dataSource.getAll());
+    } catch (error) {
+      showToast({
+        style: Toast.Style.Failure,
+        title: "Failed to load data source",
+        message: error as string,
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
 
-  function searchBarTextChange() {
-    setItems(filterCommand(cmds, searchBarText));
-  }
+  const items = useMemo(() => {
+    // searchBarText changed
+    return filterCommand(fullItems, searchBarText);
+  }, [fullItems, searchBarText]);
 
   function refreshList() {
+    if (!dataSource) return;
+
     setSearchBarText("");
+    setIsLoading(true);
 
     try {
-      setItems(dataSource.getAll());
+      setFullItems(dataSource.getAll());
     } catch (error) {
       showToast({
         style: Toast.Style.Failure,
         title: "Failed to refresh list",
         message: error as string,
       });
+    } finally {
+      setIsLoading(false);
     }
   }
 
   function handleCreate(cmd: ActionDataItem) {
+    if (!dataSource) return;
+
     try {
       dataSource.add(cmd.data, cmd.remark, cmd.args || []);
     } catch (error) {
@@ -73,6 +80,8 @@ export default function Command() {
   }
 
   function handleDelete(id: string): void {
+    if (!dataSource) return;
+
     try {
       dataSource.delete(id);
     } catch (error) {
@@ -86,6 +95,8 @@ export default function Command() {
   }
 
   function handleUpdate(updatedCmd: ActionDataItem) {
+    if (!dataSource) return;
+
     try {
       dataSource.update(updatedCmd.id, updatedCmd.data, updatedCmd.remark, updatedCmd.args || []);
     } catch (error) {
@@ -99,6 +110,8 @@ export default function Command() {
   }
 
   function handlePaste(cmd: ActionDataItem) {
+    if (!dataSource) return;
+
     try {
       dataSource.updateLastUsedTime(cmd.id);
     } catch (error) {
@@ -120,6 +133,7 @@ export default function Command() {
           <OpenDataFileFolderAction />
         </ActionPanel>
       }
+      isLoading={isLoading}
     >
       {items.map((item) => (
         <CommandListItem
@@ -193,17 +207,18 @@ function CommandListItem(props: {
           )}
           <Action.CopyToClipboard title="Copy Command" content={props.item.data} />
           <CreateCommandAction onCreate={props.onCreate} command={props.searchBarText} />
-          <Action
-            title="Delete Command"
-            onAction={() => props.onDelete(props.item.id)}
-            icon={Icon.Trash}
-            shortcut={{ modifiers: ["ctrl"], key: "x" }}
-          />
           <Action.Push
             title="Edit Command"
             target={<CommandForm onCreate={props.onUpdate} cmd={props.item} />}
             icon={Icon.Pencil}
             shortcut={{ modifiers: ["cmd"], key: "e" }}
+          />
+          <Action
+            title="Delete Command"
+            onAction={() => props.onDelete(props.item.id)}
+            icon={Icon.Trash}
+            shortcut={{ modifiers: ["ctrl"], key: "x" }}
+            style={Action.Style.Destructive}
           />
           <OpenDataFileFolderAction />
         </ActionPanel>
