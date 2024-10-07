7960b9807b071d3abdf65796618635552e99e973
xmok <30526133+xmok@users.noreply.github.com>
2024-10-08 00:33:03 +0500

    [Must] cache + metadata images + filter

--- extensions/must/src/hooks/useMust.ts
+++ extensions/must/src/hooks/useMust.ts
@@ -1,13 +1,11 @@
-import { useEffect, useMemo, useState } from "react";
-import fetch from "node-fetch";
-import useSWR from "swr";
-
 // Constants
 const USERINFO_URL = "https://mustapp.com/api/users/uri";
 
 // Types
-import { UserResponse } from "@/types/UserResponse";
-import { ListResponse, Product } from "@/types/ListResponse";
+import { UserResponse } from "../types/UserResponse";
+import { ListResponse, Product } from "../types/ListResponse";
+import { useFetch } from "@raycast/utils";
+import { useMemo } from "react";
 
 interface WantList {
   series: Product[];
@@ -15,74 +13,60 @@ interface WantList {
 }
 
 const useMust = (username: string) => {
-  try {
-    // State values
-    const [want, setWant] = useState<WantList>({ series: [], movies: [] });
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState<unknown>(null);
+  // User data fetching
+  const { isLoading: isLoadingUser, data: user } = useFetch(`${USERINFO_URL}/${username}`, {
+    async parseResponse(response) {
+      const result = (await response.json()) as UserResponse;
+      if (result.error) throw new Error(result.error.message);
+      return result;
+    },
+    failureToastOptions: {
+      title: "User Error",
+    },
+  });
 
-    // User data fetching
-    const userFetcher = (url: string) => fetch(url).then((r) => r.json() as Promise<UserResponse>);
-    const { data: user, error: userError } = useSWR<UserResponse>(`${USERINFO_URL}/${username}`, userFetcher);
+  const listsWant = useMemo(() => user?.lists?.want || [], [user?.lists]);
 
-    useEffect(() => {
-      if (userError || user?.error?.message) {
-        setLoading(false);
-        setError(new Error(userError || user?.error?.message));
-      }
-    }, [userError, user?.error]);
+  // Series info fetching
+  const { isLoading: isLoadingList, data: list } = useFetch<ListResponse[], unknown, WantList>(
+    `https://mustapp.com/api/users/id/${user?.id}/products?embed=product`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "User-Agent": "RaycastApp",
+      },
+      body: JSON.stringify({
+        ids: listsWant,
+      }),
+      mapResult(result) {
+        const products = result.map((item) => item.product);
 
-    const listsWant = useMemo(() => user?.lists?.want || [], [user?.lists]);
+        const series = products.filter((item) => item.items_count);
+        const movies = products.filter((item) => !item.items_count);
 
-    // Series info fetching
-    const listFetcher = (url: string) => {
-      if (listsWant.length === 0) return [];
-
-      return fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "User-Agent": "RaycastApp",
-        },
-        body: JSON.stringify({
-          ids: listsWant,
-        }),
-      }).then((r) => r.json() as Promise<ListResponse[]>);
-    };
-
-    useSWR<ListResponse[]>(`https://mustapp.com/api/users/id/${user?.id}/products?embed=product`, listFetcher, {
-      onSuccess: (data) => {
-        if (data?.length) {
-          const products = data?.map((item) => item.product);
-
-          const series = products.filter((item) => item.items_count);
-          const movies = products.filter((item) => !item.items_count);
-
-          setWant({
+        return {
+          data: {
             series,
             movies,
-          });
-          setLoading(false);
-        }
+          },
+        };
       },
-      onError: (err) => {
-        setError(err);
-        setLoading(false);
+      initialData: {
+        series: [],
+        movies: [],
       },
-    });
+      execute: listsWant.length > 0,
+      failureToastOptions: {
+        title: "List Error",
+      },
+    },
+  );
 
-    return {
-      isLoading: loading && !error,
-      list: want,
-      error,
-    };
-  } catch (error) {
-    return {
-      isLoading: false,
-      list: { series: [], movies: [] },
-      error,
-    };
-  }
+  return {
+    isLoading: isLoadingUser || isLoadingList,
+    list,
+  };
 };
 
 export default useMust;
--- extensions/must/src/index.tsx
+++ extensions/must/src/index.tsx
@@ -1,65 +1,78 @@
-import React from "react";
-import {
-  getPreferenceValues,
-  ActionPanel,
-  showToast,
-  ToastStyle,
-  List,
-  ListItem,
-  OpenInBrowserAction,
-  CopyToClipboardAction,
-  ListSection,
-  Icon,
-} from "@raycast/api";
+import React, { useState } from "react";
+import { getPreferenceValues, ActionPanel, List, Icon, Action } from "@raycast/api";
 
 // Types
-import { Preferences } from "@/types/Preferences";
-import { Product } from "@/types/ListResponse";
+import { Product } from "./types/ListResponse";
 
 // Hooks
 import useMust from "./hooks/useMust";
+import { getFavicon } from "@raycast/utils";
 
 const Raycast: React.FC = () => {
   const { username }: Preferences = getPreferenceValues();
-  const { isLoading, error, list } = useMust(username);
-
-  if (error !== null) {
-    showToast(ToastStyle.Failure, "Something went wrong.", String(error));
-  }
+  const { isLoading, list } = useMust(username);
+  const [filter, setFilter] = useState("");
 
   // Render
   return (
-    <List isLoading={isLoading} searchBarPlaceholder="Search entries...">
-      <ListSection title="Series" subtitle={String(list?.series?.length)}>
-        {list?.series?.map((show) => (
-          <MustListItem key={show.id} item={show} />
-        ))}
-      </ListSection>
-
-      <ListSection title="Movies" subtitle={String(list?.movies?.length)}>
-        {list?.movies?.map((movie) => (
-          <MustListItem key={movie.id} item={movie} />
-        ))}
-      </ListSection>
+    <List
+      isLoading={isLoading}
+      isShowingDetail
+      searchBarPlaceholder="Search entries..."
+      searchBarAccessory={
+        <List.Dropdown tooltip="Filter" onChange={setFilter}>
+          <List.Dropdown.Item icon={Icon.Circle} title="All" value="" />
+          <List.Dropdown.Item icon={Icon.List} title="Series" value="series" />
+          <List.Dropdown.Item icon={Icon.FilmStrip} title="Movies" value="movies" />
+        </List.Dropdown>
+      }
+    >
+      {filter !== "movies" && (
+        <List.Section title="Series" subtitle={String(list?.series.length)}>
+          {list?.series.map((show) => <MustListItem key={show.id} item={show} />)}
+        </List.Section>
+      )}
+      {filter !== "series" && (
+        <List.Section title="Movies" subtitle={String(list?.movies.length)}>
+          {list?.movies.map((movie) => <MustListItem key={movie.id} item={movie} />)}
+        </List.Section>
+      )}
     </List>
   );
 };
 
 const MustListItem: React.FC<{ item: Product }> = ({ item }) => (
-  <ListItem
+  <List.Item
     title={item.title}
     icon={`https://image.tmdb.org/t/p/w342${item.poster_file_path}`}
-    accessoryTitle={item.items_count ? `${item.items_count} episodes` : undefined}
-    accessoryIcon={item.items_count ? Icon.Calendar : undefined}
     actions={
       <ActionPanel>
-        <OpenInBrowserAction url={`https://mustapp.com/p/${item.id}`} title="Open on Must" />
-        {item.trailer_url && <OpenInBrowserAction url={item.trailer_url} title="Open Trailer on YouTube" />}
+        <Action.OpenInBrowser url={`https://mustapp.com/p/${item.id}`} icon="command-icon.png" title="Open on Must" />
+        {item.trailer_url && (
+          <Action.OpenInBrowser
+            url={item.trailer_url}
+            icon={getFavicon(item.trailer_url)}
+            title="Open Trailer on Youtube"
+          />
+        )}
 
-        <CopyToClipboardAction title="Copy Name to Clipboard" content={item.title} />
-        <CopyToClipboardAction title="Copy Must URL to Clipboard" content={`https://mustapp.com/p/${item.id}`} />
-        {item.trailer_url && <CopyToClipboardAction title="Copy Trailer URL to Clipboard" content={item.trailer_url} />}
+        <Action.CopyToClipboard title="Copy Name to Clipboard" content={item.title} />
+        <Action.CopyToClipboard title="Copy Must URL to Clipboard" content={`https://mustapp.com/p/${item.id}`} />
+        {item.trailer_url && (
+          <Action.CopyToClipboard title="Copy Trailer URL to Clipboard" content={item.trailer_url} />
+        )}
       </ActionPanel>
+    }
+    accessories={[
+      {
+        text: item.items_count ? `${item.items_count} episodes` : undefined,
+        icon: item.items_count ? Icon.Calendar : undefined,
+      },
+    ]}
+    detail={
+      <List.Item.Detail
+        markdown={`![Poster](https://media.mustapp.me/must/posters/w342${item.poster_file_path}.jpg)`}
+      />
     }
   />
 );
