bbc9e868582e78b9650fc25eae35f6444843ebc2
Fernando Barrios <fer.b.1990@gmail.com>
2024-10-12 12:54:54 -0600

    fix: run linting tool

--- extensions/shottr/src/ssap.tsx
+++ extensions/shottr/src/ssap.tsx
@@ -1,7 +1,7 @@
 import { closeMainWindow, open } from "@raycast/api";
 import { withShottrCheck } from "./utils/checkInstall";
 
-export default withShottrCheck(async function() {
+export default withShottrCheck(async function () {
   const url = "shottr://grab/append";
   await closeMainWindow();
   open(url);
