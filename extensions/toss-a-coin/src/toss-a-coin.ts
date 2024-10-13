36c049168206ccf1b87f4ff248829e0f8f083d02
Advaith U <45172876+advaith-unnikrishnan@users.noreply.github.com>
2024-10-13 23:22:13 +0530

    fix: lint

--- extensions/toss-a-coin/src/toss-a-coin.ts
+++ extensions/toss-a-coin/src/toss-a-coin.ts
@@ -1,5 +1,5 @@
 import { showHUD } from "@raycast/api";
 export default async function Command() {
-  const result = Math.ceil(Math.random()*10)%2 === 0 ? "Heads!" : "Tails!";
+  const result = Math.ceil(Math.random() * 10) % 2 === 0 ? "Heads!" : "Tails!";
   await showHUD(`🪙 ${result}`);
 }
