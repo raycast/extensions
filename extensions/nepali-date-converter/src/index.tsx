ade2edba42c0aa0d04b9db00429b2078f0be34b4
Beedhan <bhuwaibidhan@gmail.com>
2024-10-16 12:54:03 +0545

    update: converted command to no-view and returned a method which updates a metadata

--- extensions/nepali-date-converter/src/index.tsx
+++ extensions/nepali-date-converter/src/index.tsx
@@ -1,7 +1,7 @@
-import { Detail } from "@raycast/api";
+import { updateCommandMetadata } from "@raycast/api";
 import NepaliDate from "nepali-date-converter";
 
-export default function convertToBS() {
+export default async function convertToBS() {
   const bsDate = new NepaliDate().format("ddd, DD MMMM YYYY");
-  return <Detail markdown={`# ${bsDate}`} />;
+  await updateCommandMetadata({ subtitle: bsDate });
 }
