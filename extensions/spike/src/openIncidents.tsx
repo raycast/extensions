8e6048731b0e7852e830fadf40b00e7a4120eca2
Damanpreet singh <dmnprt4@gmail.com>
2024-10-18 12:59:51 +0530

    Add spike extension
    - compess images
    - Feat: add Oncall url in list view and oncall view
    - fix: acknowledge and resolve bug, fix menu bar
    - feat: add favorites command and make extension public
    - Update readme
    - Update README
    - chore: remove favorites command
    - chore: remove \`cmd\` + \`enter\` shortcut
    - update owner in package.json
    - chore: update contributers
    - fix: types in who is oncall command
    - fix: lint and types issues
    - feat: add shortcuts and fix types
    - feat: directly add override from list view
    - Refactor oncall API and incident components
    - adding description for each command
    - Copy changes on some commands
    - feat: add priority and severity label
    - Update README.md
    - Update README.md
    - fix: remove icon from menu item
    - feat: add readme file
    - feat: add oncall and teams API endpoints
    - feat: add assets
    - feat: add API client
    - feat: add config
    - Feat: add favorites, oncall, incidents command
    - Initial commit

---
+++ extensions/spike/src/openIncidents.tsx
@@ -0,0 +1,96 @@
+import { MenuBarExtra, open } from "@raycast/api";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import api from "./api";
+import config from "./config";
+
+interface Incident {
+  counterId: string;
+  message: string;
+  status: "NACK" | "ACK" | "RES";
+}
+
+interface ActiveOncall {
+  isCurrentlyOncall: boolean;
+}
+
+export default function Command() {
+  const [incidents, setIncidents] = useState<Incident[]>([]);
+  const [activeOncall, setActiveOncall] = useState<ActiveOncall | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchIncidents = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      const response = await api.incidents.getOpenIncidents(1, 20);
+      const oncall = await api.oncall.amIOncall();
+      setActiveOncall(oncall.oncallData);
+
+      // activeOncall && activeOncall.isCurrentlyOncall
+      setIncidents([...response.NACK_Incidents, ...response.ACK_Incidents]);
+    } catch (err) {
+      console.error("Error fetching incidents:", err);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchIncidents();
+  }, [fetchIncidents]);
+
+  const triggeredIncidents = useMemo(() => incidents.filter((i) => i.status === "NACK"), [incidents]);
+  const acknowledgedIncidents = useMemo(() => incidents.filter((i) => i.status === "ACK"), [incidents]);
+
+  const truncate = (str: string, n: number) => (str && str.length > n ? str.substring(0, n - 1) + "..." : str);
+
+  return (
+    <MenuBarExtra isLoading={isLoading} icon={"spike-logo-white.png"} tooltip="Open incidents">
+      <MenuBarExtra.Item
+        icon={{
+          source: activeOncall && activeOncall.isCurrentlyOncall ? "green-dot.png" : "gray-dot.png",
+        }}
+        title={activeOncall && activeOncall.isCurrentlyOncall ? "You are on-call" : "You are not on-call"}
+        onAction={() => {
+          open(`${config?.spike}/on-calls?includes=me`);
+        }}
+      />
+      <MenuBarExtra.Section title={`Triggered (${triggeredIncidents.length})`}>
+        {triggeredIncidents.map((incident, index) => (
+          <MenuBarExtra.Item
+            key={index}
+            title={
+              `[${incident.counterId}] ${truncate(incident.message, 35)}` || `[${incident.counterId}] Parsing failed`
+            }
+            onAction={() => {
+              open(`${config?.spike}/incidents/${incident.counterId}`);
+            }}
+          />
+        ))}
+      </MenuBarExtra.Section>
+      <MenuBarExtra.Item title={`Acknowledged (${acknowledgedIncidents.length})`} />
+      {acknowledgedIncidents.map((incident, index) => (
+        <MenuBarExtra.Item
+          key={index}
+          title={
+            `[${incident.counterId}] ${truncate(incident.message, 35)}` || `[${incident.counterId}] Parsing failed`
+          }
+          onAction={() => {
+            open(`${config?.spike}/incidents/${incident.counterId}`);
+          }}
+        />
+      ))}
+      <MenuBarExtra.Section>
+        <MenuBarExtra.Item
+          icon={{
+            source: "spike-logo-white.png",
+          }}
+          title="Open Dashboard"
+          shortcut={{ modifiers: ["cmd"], key: "o" }}
+          onAction={() => {
+            open(`${config?.spike}`);
+          }}
+        />
+      </MenuBarExtra.Section>
+    </MenuBarExtra>
+  );
+}
