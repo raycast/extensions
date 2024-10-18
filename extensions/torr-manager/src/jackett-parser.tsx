8752a66d252f07225cd56eac2f98f11d3e3e4a17
danniuz <dan.sldv@gmail.com>
2024-10-19 00:59:12 +0300

    Fix save to db

--- extensions/torr-manager/src/jackett-parser.tsx
+++ extensions/torr-manager/src/jackett-parser.tsx
@@ -85,10 +85,9 @@ export default function Command() {
       const fileBlob = await fileResponse.blob();
 
       const formData = new FormData();
-      formData.append("save", "true");
+      formData.append("save", saveToDb);
       formData.append("file", fileBlob, title);
       formData.append("title", title);
-      formData.append("save_to_db", saveToDb);
 
       const serverUrl = `${handleDomain(torrserverUrl)}/torrent/upload`;
       const uploadResponse = await timeoutFetch(handleDomain(serverUrl), {
