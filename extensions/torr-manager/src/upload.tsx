c81f94947d12fcb9cc1f6ec57f22f156c8d3b713
danniuz <dan.sldv@gmail.com>
2024-10-18 16:54:38 +0300

    Update torr-manager extension
    - Merge branch \'contributions/merge-1729259439923229000\'
    - Pull contributions
    - Improve uploading, add useForm

--- extensions/torr-manager/src/upload.tsx
+++ extensions/torr-manager/src/upload.tsx
@@ -1,10 +1,9 @@
 import { Form, ActionPanel, Action, showToast, Toast, getPreferenceValues } from "@raycast/api";
-
-import { FormData } from "formdata-node";
 import { fileFromPath } from "formdata-node/file-from-path";
 import { getAuthHeaders, handleDomain, timeoutFetch } from "./utils";
 import { Preferences } from "./models";
-import { useState } from "react";
+import { useForm, FormValidation } from "@raycast/utils";
+import { FormData } from "formdata-node";
 
 interface SubmitFormValues {
   torrentFile: string[];
@@ -14,63 +13,71 @@ interface SubmitFormValues {
 
 export default function Command() {
   const { torrserverUrl } = getPreferenceValues<Preferences>();
-  const [title, setTitle] = useState<string>("");
 
-  async function handleSubmit(values: SubmitFormValues) {
-    const { torrentFile, posterUrl } = values;
+  const { handleSubmit, itemProps, setValue } = useForm<SubmitFormValues>({
+    onSubmit: async (values) => {
+      const { torrentFile, posterUrl, title } = values;
 
-    if (!torrentFile || torrentFile.length === 0) {
-      showToast(Toast.Style.Failure, "No file selected", "Please select a torrent file.");
-      return;
-    }
-
-    const filePath = torrentFile[0];
-
-    if (!filePath) {
-      showToast(Toast.Style.Failure, "File path not available", "Unable to get the file path.");
-      return;
-    }
-
-    const formData = new FormData();
-    formData.append("save", "true");
-
-    try {
-      const file = await fileFromPath(filePath);
-      formData.append("file", file, file.name);
-      formData.append("title", title);
-
-      if (posterUrl) {
-        formData.append("poster", posterUrl);
+      if (!torrentFile || torrentFile.length === 0) {
+        showToast(Toast.Style.Failure, "No file selected", "Please select a torrent file.");
+        return false;
       }
 
-      const serverUrl = `${handleDomain(torrserverUrl)}/torrent/upload`;
-      showToast(Toast.Style.Animated, "Processing...");
+      const filePath = torrentFile[0];
 
-      const response = await timeoutFetch(handleDomain(serverUrl), {
-        method: "POST",
-        body: formData,
-        headers: {
-          ...getAuthHeaders(),
-        },
-      });
-
-      if (!response.ok) {
-        showToast(Toast.Style.Failure, `Failed to upload torrent: ${response.status} ${response.statusText}`);
-        return;
+      if (!filePath) {
+        showToast(Toast.Style.Failure, "File path not available", "Unable to get the file path.");
+        return false;
       }
 
-      showToast(Toast.Style.Success, "Torrent added successfully");
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    } catch (error) {
-      showToast(Toast.Style.Failure, "Error", `Could not connect to ${torrserverUrl}`);
-    }
-  }
+      const formData = new FormData();
+      formData.append("save", "true");
+
+      try {
+        const file = await fileFromPath(filePath);
+        formData.append("file", file, file.name);
+        formData.append("title", title);
+
+        if (posterUrl) {
+          formData.append("poster", posterUrl);
+        }
+
+        const serverUrl = `${handleDomain(torrserverUrl)}/torrent/upload`;
+        showToast(Toast.Style.Animated, "Processing...");
+
+        const response = await timeoutFetch(handleDomain(serverUrl), {
+          method: "POST",
+          body: formData,
+          headers: {
+            ...getAuthHeaders(),
+          },
+        });
+
+        if (!response.ok) {
+          showToast(Toast.Style.Failure, `Failed to upload torrent: ${response.status} ${response.statusText}`);
+          return false;
+        }
+
+        showToast(Toast.Style.Success, "Torrent added successfully");
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      } catch (error) {
+        showToast(Toast.Style.Failure, "Error", `Could not connect to ${torrserverUrl}`);
+        return false;
+      }
+
+      return true;
+    },
+    validation: {
+      torrentFile: FormValidation.Required,
+      title: FormValidation.Required,
+    },
+  });
 
   const handleFileChange = (files: string[]) => {
     if (files.length > 0) {
       const selectedFilePath = files[0];
       const fileName = selectedFilePath.split("/").pop();
-      setTitle(fileName || "");
+      setValue("title", fileName || "");
     }
   };
 
@@ -84,19 +91,13 @@ export default function Command() {
     >
       <Form.Description text="Upload new torrent to TorrServer." />
       <Form.FilePicker
-        id="torrentFile"
+        {...itemProps.torrentFile}
         title="Torrent File"
         allowMultipleSelection={false}
         onChange={handleFileChange}
       />
-      <Form.TextField
-        id="title"
-        title="Title"
-        value={title}
-        onChange={setTitle}
-        placeholder="Auto-filled with file name"
-      />
-      <Form.TextField id="posterUrl" title="Poster URL" placeholder="Enter poster URL" />
+      <Form.TextField {...itemProps.title} title="Title" placeholder="Auto-filled with file name" />
+      <Form.TextField {...itemProps.posterUrl} title="Poster URL" placeholder="Enter poster URL" />
     </Form>
   );
 }
