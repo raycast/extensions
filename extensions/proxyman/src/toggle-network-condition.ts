e9de6b35305bb978291d8ea0ed4ed6425b73e00e
Nghia Tran <vinhnghiatran@gmail.com>
2024-10-19 17:04:11 +0700

    Proxyman Ext v1.0.0

---
+++ extensions/proxyman/src/toggle-network-condition.ts
@@ -0,0 +1,5 @@
+import { performAction, ProxymanActions } from "./utils/actions";
+
+export default async function Main() {
+  await performAction(ProxymanActions.ToggleNetworkCondition, "Toggle Network Condition Success", "Failed to Toggle Network Condition");
+}
---
+++ extensions/proxyman/src/toggle-ssl-proxying-list.ts
@@ -0,0 +1,5 @@
+import { performAction, ProxymanActions } from "./utils/actions";
+
+export default async function Main() {
+  await performAction(ProxymanActions.ToggleSSLProxyingList, "Toggle SSL Proxying List Success", "Failed to Toggle SSL Proxying List");
+}
