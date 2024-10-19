5f5e4dde09afa61dadf51269d0b895b00c8f37cf
Nghia Tran <vinhnghiatran@gmail.com>
2024-10-19 20:49:00 +0700

    Proxyman Ext v1.0.0

---
+++ extensions/proxyman/src/toggle-external-proxy.ts
@@ -0,0 +1,5 @@
+import { performAction, ProxymanActions } from "./utils/actions";
+
+export default async function Main() {
+  await performAction(ProxymanActions.ToggleExternalProxy, "Toggle External Proxy Success", "Failed to Toggle External Proxy");
+}
---
+++ extensions/proxyman/src/toggle-record-traffic.ts
@@ -0,0 +1,5 @@
+import { performAction, ProxymanActions } from "./utils/actions";
+
+export default async function Main() {
+  await performAction(ProxymanActions.ToggleRecordTraffic, "Toggle Record Traffic Success", "Failed to Toggle Record Traffic");
+}
