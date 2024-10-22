3a14f5473b9aaebeba23e3df13c7fb685e1d8c10
Aryan Thatte <aryan.thatte@gmail.com>
2024-10-22 17:07:48 -0400

    removes newline from title

--- extensions/remember-the-date/src/menu-bar.tsx
+++ extensions/remember-the-date/src/menu-bar.tsx
@@ -22,8 +22,7 @@ export default function Command() {
   const difference = showCountdownByDay
     ? "in " + moment(nextDate.date).diff(new Date(), "days") +  " days"
     : moment(nextDate.date).fromNow()
-  const untilNextDate = `${nextDate.name.length > 15 ? nextDate.name.substring(0, 15) + "..." : nextDate.name}
-    ${difference}`;
+  const untilNextDate = `${nextDate.name.length > 15 ? nextDate.name.substring(0, 15) + "..." : nextDate.name} ${difference}`;
 
   return (
     <MenuBarExtra icon={nextDate.icon} title={untilNextDate}>
