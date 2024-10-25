8f667aa354dd4813bd34114cba8219cfdb2fed7b
Per Nielsen Tikær <per@raycast.com>
2024-10-25 09:51:01 +0200

    Update serch-monokakido-dictionary.ts

--- extensions/monokido-dictionary-search/src/serch-monokakido-dictionary.ts
+++ extensions/monokido-dictionary-search/src/serch-monokakido-dictionary.ts
@@ -1,19 +1,19 @@
 import { getSelectedText, open, showToast, Toast } from "@raycast/api";
 
 export default async function Command() {
-    try {
-        const selectedText = await getSelectedText();
-    
-        if (selectedText !== "") {
-            await open(`mkdictionaries:///?text=${selectedText}`);
-        } else {
-            throw new Error("No text selected");
-        } 
-    } catch (error) {
-        await showToast({
-            style: Toast.Style.Failure,
-            title: "Cannot seach dictionary",
-            message: String(error),
-        });
+  try {
+    const selectedText = await getSelectedText();
+
+    if (selectedText !== "") {
+      await open(`mkdictionaries:///?text=${selectedText}`);
+    } else {
+      throw new Error("No text selected");
     }
+  } catch (error) {
+    await showToast({
+      style: Toast.Style.Failure,
+      title: "Cannot seach dictionary",
+      message: String(error),
+    });
+  }
 }
