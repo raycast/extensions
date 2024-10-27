ea0642db39c6c6cb14d34470d21c97969001b21d
Philip <4144601+philipbrembeck@users.noreply.github.com>
2024-10-27 11:55:35 +0100

    Update veganify-application extension
    - feat: Upgrade to Veganify API v1
    - Merge branch \'contributions/merge-1730025091113261000\' into ext/veganify-app
    - Pull contributions
    - Pull contributions

--- extensions/veganify-application/src/components/EmptyList.tsx
+++ extensions/veganify-application/src/components/EmptyList.tsx
@@ -4,7 +4,7 @@ export default function EmptyList({ searchText }: { searchText: string }) {
   return (
     <List.EmptyView
       icon={"vgc.svg"}
-      title={searchText.trim() === "" ? "Loading..." : "Welcome to Veganify"}
+      title={searchText.trim() === "" ? "Welcome to Veganify" : "Loading..."}
       description="Just enter the ingredients you want to check, separated by commas. Veganify will instantly show you the vegan status of each ingredient."
     />
   );
--- extensions/veganify-application/src/models/IngredientResult.ts
+++ extensions/veganify-application/src/models/IngredientResult.ts
@@ -2,6 +2,8 @@ export interface IngredientResult {
   name: string;
   isVegan: boolean | null;
   surelyVegan: boolean;
-  maybeVegan: boolean;
+  notVegan: boolean;
+  maybeNotVegan: boolean;
+  unknown: boolean;
   error?: string;
 }
