927db13844d3c94dee106ed0a322a23ce095ca5a
talbx <dev@talbx.de>
2024-10-27 12:56:30 +0100

    Add recent-logic-pro-projects extension
    - chore: updated setup image
    - chore: enhanced readme, assets and prepared changelog
    - fix: ray lint --fix
    - feat: set project base path in settings, handle no findings
    - chore: init raycast extension: recent logic pro projects

---
+++ extensions/recent-logic-pro-projects/src/recent-logic-pro-projects.tsx
@@ -0,0 +1,129 @@
+import { Action, ActionPanel, getPreferenceValues, Icon, List, open, openCommandPreferences } from "@raycast/api";
+import { useEffect, useState } from "react";
+import { exec } from "node:child_process";
+import { promisify } from "node:util";
+import * as crypto from "node:crypto";
+import { showFailureToast } from "@raycast/utils";
+
+export interface LogicProject {
+  name: string;
+  id: string;
+  path: string;
+  lastModified: Date;
+}
+
+export default function Command() {
+  const [projects, setProjects] = useState<LogicProject[]>([]);
+  const [, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<Error>();
+  // default location
+  const preferences = getPreferenceValues<Preferences>();
+  useEffect(() => {
+    const location = preferences["projects-location"];
+    const execAsync = promisify(exec);
+    setIsLoading(true);
+    setError(undefined);
+
+    execAsync(
+      // `ls -A1 ${path} | grep .logicx`
+      `find ${location} -regex ".*\\.\\(logicx\\)" | head -n 200 | xargs -I{} stat -f "%N,%m" "{}"`,
+    )
+      .then((result) => {
+        if (result.stderr !== null && result.stderr !== "") {
+          setError(new Error(result.stderr));
+          return [];
+        }
+        const projects: LogicProject[] = result.stdout
+          .trim()
+          .split("\n")
+          .filter((entry) => entry.includes(","))
+          .map((entry) => {
+            const [p, lastModified] = entry.split(",");
+            const lastModifiedDate = new Date(Number(lastModified) * 1000);
+            const lastSlashIndex = p.lastIndexOf("/");
+            const afterLastSlash = p.substring(lastSlashIndex + 1);
+            return {
+              name: afterLastSlash,
+              lastModified: isNaN(lastModifiedDate.getTime()) ? new Date(0) : lastModifiedDate, // Handle invalid dates
+              id: crypto.createHash("md5").update(entry).digest("hex"),
+              path: p,
+            };
+          })
+          .sort((a, b) => {
+            if (a.lastModified < b.lastModified) {
+              return 1;
+            }
+            return -1;
+          });
+        setError(undefined);
+        setProjects(projects);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setError(new Error(err));
+        return [];
+      });
+  }, [setProjects, setIsLoading, setError]);
+
+  useEffect(() => {
+    if (error !== undefined) {
+      showFailureToast(error, {
+        title: "Something went wrong",
+      });
+    }
+  }, [error, setError]);
+
+  return (
+    <List
+      filtering={true}
+      throttle={true}
+      searchBarPlaceholder={"Filter projects..."}
+      actions={
+        <ActionPanel>
+          <Action title="Open Extension Preferences" onAction={openCommandPreferences} />
+        </ActionPanel>
+      }
+    >
+      {projects.map((project) => {
+        return (
+          <List.Item
+            key={project.id}
+            accessories={[
+              {
+                date: {
+                  value: project.lastModified,
+                },
+              },
+            ]}
+            icon={{
+              source:
+                "https://help.apple.com/assets/654E7F8CD472768668095520/654E7F9560B6B45E960FE823/de_DE/390711ce08c61bf054d3dc4dfb9080ae.png",
+            }}
+            title={project.name}
+            actions={
+              <ActionPanel>
+                <Action title="Open Project" onAction={() => open(project.path)} />
+                <Action title="Open Extension Preferences" onAction={openCommandPreferences} />
+              </ActionPanel>
+            }
+            detail={<List.Item.Detail markdown={"#Hello!"} />}
+          />
+        );
+      })}
+      {projects.length == 0 && (
+        <List.EmptyView
+          actions={
+            <ActionPanel>
+              <Action title="Open Extension Preferences" onAction={openCommandPreferences} />
+            </ActionPanel>
+          }
+          icon={Icon.EmojiSad}
+          title={"No Logic projects found."}
+          description={`Are you sure you picked the right directory?
+${preferences["projects-location"]} is currently selected.
+Open the extension preferences (Enter) to change the directory.`}
+        ></List.EmptyView>
+      )}
+    </List>
+  );
+}
