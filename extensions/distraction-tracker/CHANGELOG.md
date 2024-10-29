7460fc6d1d56db5020f7f5f7b972f923789addbe
Daniel Deichfuß <d.deichfuss@gmail.com>
2024-10-29 09:58:42 +0100

    Add distraction-tracker extension
    - Update readme and add screenshots
    - Improve details view
    - Sort list by date
    - Display date time in details view
    - Add icon
    - Edit distraction
    - Add delete from detail view
    - Extract form component
    - Cleanup
    - Delete Distraction
    - Rename to list distractions
    - Add Details view
    - Add description for my distractions
    - Fix ts error
    - Show all infos in list
    - Extract feelings
    - List distractions
    - Rename to title
    - Close form after success
    - Persist distractions
    - Add form validation
    - Create form fields
    - Initial commit

---
+++ extensions/distraction-tracker/CHANGELOG.md
@@ -0,0 +1,3 @@
+# Distraction Tracker Changelog
+
+## [Initial Version] - {PR_MERGE_DATE}
---
+++ extensions/distraction-tracker/src/track-distraction.tsx
@@ -0,0 +1,28 @@
+import { showHUD, PopToRootType } from "@raycast/api";
+import { useLocalStorage } from "@raycast/utils";
+import { randomUUID } from "crypto";
+import { Distraction, DistractionFormValues } from "./lib/types";
+import { feelings } from "./lib/feelings";
+import { DistractionForm } from "./components/distraction-form";
+
+export default function Command() {
+  const { setValue: setDistractions, value: distractions } = useLocalStorage<Distraction[]>("distractions", []);
+
+  return (
+    <DistractionForm
+      onSubmitHandler={async (values: DistractionFormValues) => {
+        const previousDistractions = distractions ?? [];
+        await setDistractions([
+          ...previousDistractions,
+          {
+            ...values,
+            feeling: values.feeling as keyof typeof feelings,
+            id: randomUUID(),
+          },
+        ]);
+        showHUD("Distraction saved", { clearRootSearch: true, popToRootType: PopToRootType.Immediate });
+        return;
+      }}
+    />
+  );
+}
