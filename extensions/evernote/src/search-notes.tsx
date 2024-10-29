569c99a9bf84c18628aa8a36f0127ccf38a36923
artpi <artur.piszek@gmail.com>
2024-10-29 13:40:41 +0100

    Update evernote extension
    - Add screenshots
    - lint
    - Better error handling
    - icon

--- extensions/evernote/src/search-notes.tsx
+++ extensions/evernote/src/search-notes.tsx
@@ -1,6 +1,6 @@
 import { resolve } from "path";
 import { homedir } from "os";
-import { List, ActionPanel, Action } from "@raycast/api";
+import { showToast, Toast, open, getApplications } from "@raycast/api";
 import { useState, useEffect } from "react";
 import path from "path";
 import fs from "fs";
@@ -14,37 +14,46 @@ export default function Command() {
   );
 
   useEffect(() => {
-    if (evernoteDB) {
-      return;
-    }
-    if (!fs.existsSync(baseDir)) {
-      return;
-    }
-    const files = fs.readdirSync(baseDir);
-    console.log(files);
-    const dbFile = files.find((file) => file.endsWith("+RemoteGraph.sql"));
-    if (dbFile) {
-      setEvernoteDB(path.join(baseDir, dbFile));
-    }
+    getApplications().then((applications) => {
+      const isEvernoteInstalled = applications.find(({ bundleId }) => bundleId === "com.evernote.Evernote");
+      if (!isEvernoteInstalled) {
+        showToast({
+          style: Toast.Style.Failure,
+          title: "Evernote client is not installed.",
+          message: "Download",
+          primaryAction: {
+            title: "Go to https://evernote.com/download",
+            onAction: (toast) => {
+              open("https://evernote.com/download");
+              toast.hide();
+            },
+          },
+        });
+        return;
+      }
+      if (evernoteDB) {
+        return;
+      }
+      if (!fs.existsSync(baseDir)) {
+        showToast({
+          style: Toast.Style.Failure,
+          title: "Cannot find Evernote database.",
+          message:
+            "The database should be in ~/Library/Application Support/Evernote/conduit-storage/https%3A%2F%2Fwww.evernote.com",
+        });
+        return;
+      }
+      const files = fs.readdirSync(baseDir);
+      console.log(files);
+      const dbFile = files.find((file) => file.endsWith("+RemoteGraph.sql"));
+      if (dbFile) {
+        setEvernoteDB(path.join(baseDir, dbFile));
+      }
+    });
   }, [evernoteDB]);
 
   if (!evernoteDB) {
-    return (
-      <List
-        isLoading={true}
-        searchBarPlaceholder="Searching for Evernote database in ~/Library/Application Support/Evernote/conduit-storage/https%3A%2F%2Fwww.evernote.com"
-      >
-        <List.Item
-          title="Evernote database not found"
-          subtitle="Please check if you have Evernote installed and if the database file exists."
-          actions={
-            <ActionPanel>
-              <Action.ShowInFinder path={baseDir} />
-            </ActionPanel>
-          }
-        />
-      </List>
-    );
+    return;
   }
 
   return <NotesList evernoteDB={evernoteDB} />;
