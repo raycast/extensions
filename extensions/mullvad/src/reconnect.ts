b3c8389d73ca217ceee1c162a2c892a11cdd2d3d
svenhofman <svenhofman25@gmail.com>
2024-10-29 09:48:10 +0100

    Update mullvad extension
    - Support Reconnecting via the Mullvad CLI, Fixed HUD message display
    - Initial commit

--- extensions/mullvad/src/reconnect.ts
+++ extensions/mullvad/src/reconnect.ts
@@ -5,13 +5,17 @@ import { verifyIsMullvadInstalled } from "./utils";
 export default async function Command() {
   const isMullvadInstalled = await verifyIsMullvadInstalled();
   if (!isMullvadInstalled) return;
-  
-  const priorStatus = execSync("mullvad status").toString().split('\n')[0];
-  execSync("mullvad connect");
 
-  // `mullvad connect` doesn't change the relay,
-  // so if the status is anything except 'Disconnected' we can assume that nothing has changed.
-  const message = priorStatus === "Disconnected" ? "Connected" : "Already Connected";
+  const priorStatus = execSync("mullvad status").toString().split('\n')[0];
+  // `mullvad reconnect` doesn't do anything if disconnected.
+  let message;
+  if (priorStatus === "Connected") {
+    execSync("mullvad reconnect");
+    message = "Reconnected";
+  } else {
+    message = "Not Connected";
+  }
+
 
   await showHUD(message, {
     clearRootSearch: true,
