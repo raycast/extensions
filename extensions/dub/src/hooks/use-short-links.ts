3e02d4974da41c4279285f429206b288497d117c
Teddy ASSIH <110066080+Ionfinisher@users.noreply.github.com>
2024-10-29 23:36:12 +0000

    Add dynamic acceptance of a query parameter

--- extensions/dub/src/hooks/use-short-links.ts
+++ extensions/dub/src/hooks/use-short-links.ts
@@ -1,16 +1,21 @@
 import { useCachedPromise } from "@raycast/utils";
 import { getAllShortLinks } from "@/api";
 
-export const useShortLinks = () => {
+export const useShortLinks = (query: string = "") => {
   const {
-    data: shortLinks,
+    data: data,
     isLoading,
     error,
     mutate,
-  } = useCachedPromise(getAllShortLinks, [], {
-    initialData: [],
+  } = useCachedPromise(getAllShortLinks, [query], {
+    initialData: { links: [], hasMoreLinks: false },
     failureToastOptions: { title: "❗ Failed to fetch short links" },
   });
 
-  return { shortLinks, mutate, isLoading: (!shortLinks && !error) || isLoading, error };
+  return { 
+    shortLinks: data?.links, 
+    mutate, isLoading: (!data?.links && !error) || isLoading, 
+    error,
+    supportsLinkTypeahead: query.trim().length > 0 || data?.hasMoreLinks,
+  };
 };
