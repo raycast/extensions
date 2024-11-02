4b3a051ebcb9a800d7195a35d8e0430a1c030d05
Rahul Pandey <rpandey1234@gmail.com>
2024-11-02 01:50:35 -0700

    Initial commit of bare bones raycast app

---
+++ extensions/hello-world/src/rkp-hello.tsx
@@ -0,0 +1,11 @@
+import { List } from "@raycast/api";
+
+export default function Command() {
+  return (
+    <List>
+      <List.Item title="Hello Item 1!" />
+      <List.Item title="Hello Item 2!" />
+      <List.Item title="Hello Item 3!" />
+    </List>
+  );
+}
