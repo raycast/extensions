29cb163458ed1fdc5155e40e0962130946f68050
Mehdi <pyrexrj@gmail.com>
2024-11-06 01:25:25 +0330

    Update rehooks extension (#15158)
    * Update rehooks extension
    
    - Merge branch \'contributions/merge-1730225560891785000\'
    - Pull contributions
    - fix: copy hook instead of import statement
    
    * Update rehooks extension
    
    - chore: make separated type
    - chore: update description and readme
    - chore: update changelog
    
    * chore: improve reusability
    
    * Update rehooks extension
    
    - chore: add documentation url action
    - chore: rename command file
    
    * Update rehooks extension
    
    - chore: update metadata
    - chore: update changelog
    - feat: add documentation command
    
    * fix: remove unused import
    
    * Update rehooks extension
    
    - chore: update extension description
    - chore: rewrite docs command description
    - ref: rename command name
    
    * Update CHANGELOG.md and optimise images
    
    ---------
    
    Co-authored-by: raycastbot <bot@raycast.com>

---
+++ extensions/rehooks/src/docs.tsx
@@ -0,0 +1,24 @@
+import { OpenInBrowserAction } from "./components/actions";
+import { ActionPanel, List } from "@raycast/api";
+import { DOCS_PAGES } from "./utils/docs";
+
+export default function Command() {
+  return (
+    <List>
+      {DOCS_PAGES.map((item, index) => (
+        <List.Item
+          key={index}
+          title={item.title}
+          detail={item.description}
+          subtitle={item.description}
+          keywords={[item.title]}
+          actions={
+            <ActionPanel>
+              <OpenInBrowserAction title={item.title} url={item.url} />
+            </ActionPanel>
+          }
+        />
+      ))}
+    </List>
+  );
+}
--- extensions/rehooks/src/search-hooks.tsx
+++ extensions/rehooks/src/search-hooks.tsx
@@ -1,20 +1,11 @@
-import { ActionPanel, Action, Icon, List } from "@raycast/api";
-import { useFetch } from "@raycast/utils";
+import { CopyToClipboardAction, ReloadAction } from "./components/actions";
+import { ActionPanel, List } from "@raycast/api";
+import { useSearch } from "./hooks/useSearch";
 import { useState } from "react";
-
-type Hook = Readonly<{
-  id: string;
-  title: string;
-  description: string;
-  import: string;
-  url: string;
-}>;
 
 export default function Command() {
   const [searchText, setSearchText] = useState("");
-  const { isLoading, data, revalidate } = useFetch<Hook[]>(`https://rehooks.pyr33x.ir/api/hooks?search=${searchText}`, {
-    keepPreviousData: true,
-  });
+  const { isLoading, data, revalidate } = useSearch(searchText);
 
   return (
     <List isLoading={isLoading} searchText={searchText} onSearchTextChange={setSearchText} throttle>
@@ -27,9 +18,8 @@ export default function Command() {
           keywords={[item.title]}
           actions={
             <ActionPanel>
-              <Action.CopyToClipboard title={`Import ${item.title}`} content={item.import} />
-              <Action.OpenInBrowser title="Open in Browser" url={item.url} />
-              <Action title="Reload" icon={Icon.ArrowClockwise} onAction={() => revalidate()} />
+              <CopyToClipboardAction title={item.title} content={item.content} />
+              <ReloadAction onReload={revalidate} />
             </ActionPanel>
           }
         />
