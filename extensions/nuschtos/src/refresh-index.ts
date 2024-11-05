75777c00485f1164500f84010ebe2e4784321dca
Lucas <me@keyruu.de>
2024-11-05 13:10:30 +0100

    fix wrong ixxPath for refresh index

--- extensions/nuschtos/src/refresh-index.ts
+++ extensions/nuschtos/src/refresh-index.ts
@@ -1,4 +1,4 @@
-import { environment, LaunchType, LocalStorage, showHUD } from "@raycast/api";
+import { environment, getPreferenceValues, LaunchType, LocalStorage, showHUD } from "@raycast/api";
 import { writeFileSync } from "fs";
 import fetch from "node-fetch";
 import { execSync } from "node:child_process";
@@ -17,6 +17,8 @@ export interface Scope {
 export default async function main() {
   const indexPath = join(environment.supportPath, "index.ixx");
 
+  const { ixxPath } = getPreferenceValues();
+
   try {
     // Fetch and write index to disk
     const response = await fetch(`https://search.nüschtos.de/index.ixx`, { method: "GET" });
@@ -24,7 +26,7 @@ export default async function main() {
     writeFileSync(indexPath, Buffer.from(data));
 
     // Get and store meta results
-    const metaCommand = `/Users/lucas.rott/git/private/raycast/nuschtos/ixx meta -f json -i "${indexPath}"`;
+    const metaCommand = `${ixxPath} meta -f json -i "${indexPath}"`;
     const cliMetaResult: CliMetaResult = JSON.parse(execSync(metaCommand).toString());
     await LocalStorage.setItem("cliMetaResult", JSON.stringify(cliMetaResult));
 
