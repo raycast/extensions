086eb51746e24be2b064c411e6b7feee6077f01f
Reza Jafar <admin@rezajafar.com>
2024-11-05 12:29:58 -0800

    Merge remote changes

--- extensions/shell-optimizer/.eslintrc.js
+++ extensions/shell-optimizer/.eslintrc.js
@@ -1,9 +1,7 @@
 module.exports = {
-    extends: [
-        "@raycast",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint"],
-    root: true
+    extends: ["@raycast"],
+    rules: {
+        "@typescript-eslint/no-explicit-any": "warn",
+        "react/jsx-key": "warn"
+    }
 }; 
