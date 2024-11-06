31f68f95df5ca49e1507f3f5a872a353b2e8847e
Cole Bemis <colebemis@github.com>
2024-11-06 09:07:28 -0800

    Update github-spark extension
    - Add model picker and variants checkbox
    - Ignore raycast
    - Run prettier

--- extensions/github-spark/src/create-new-spark.ts
+++
@@ -1,6 +0,0 @@
-import { LaunchProps, open } from "@raycast/api";
-
-export default async function main(props: LaunchProps<{ arguments: { topic: string } }>) {
-  const topic = props.arguments.topic;
-  open(`https://spark.githubnext.com/new/${encodeURIComponent(topic)}`);
-}
