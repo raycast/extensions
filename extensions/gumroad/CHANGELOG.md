5e45df142102611191aae5219c88fdd3ab8ffc8b
Eduardo Gomez <this.eduardo@gmail.com>
2024-11-09 17:12:47 +0100

    Add gumroad extension
    - Updated readme
    - Added assets and metadata
    - Fixed linting issues
    - Fixed flickering issue
    - Persist state
    - Implemented pagination and filters for sales
    - Implemented sale and product details
    - Moved types to their own file
    - Remove .idea directory from repository
    - first commit

---
+++ extensions/gumroad/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Gumroad Sales Changelog
+
+## [Initial Version] - {PR_MERGE_DATE}
+
+Initial version of the Gumroad Sales extension
---
+++ extensions/gumroad/src/utils.ts
@@ -0,0 +1,13 @@
+export const formatDate = (dateString: string) => {
+  const date = new Date(dateString);
+  const options: Intl.DateTimeFormatOptions = { year: "numeric", month: "short", day: "numeric" };
+  return date.toLocaleDateString(undefined, options);
+};
+
+export const formatNumber = (number: string): string => {
+  return Number(number).toLocaleString(undefined, { maximumFractionDigits: 2 });
+};
+
+export const removeParentheses = (str: string): string => {
+  return str.replace(/[()]/g, "");
+};
