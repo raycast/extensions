999cba609f6123c7d619220e9f7bc394c45daab2
albertxwz <1017981912@qq.com>
2024-11-14 09:54:53 +0800

    Prepare the extension

--- extensions/move-windows/src/move-to-next-desktop.ts
+++ extensions/move-windows/src/move-to-next-desktop.ts
@@ -2,10 +2,9 @@ import { showToast, Toast } from "@raycast/api";
 import { moveWindowToSpace } from "swift:../swift";
 
 export default async function main() {
-    try {
-        await moveWindowToSpace("next");
-    }
-    catch (error) {
-        showToast({ title: "Could not move window", message: error.message, style: Toast.Style.Failure });
-    }
+  try {
+    await moveWindowToSpace("next");
+  } catch (error) {
+    showToast({ title: "Could not move window", message: error.message, style: Toast.Style.Failure });
+  }
 }
