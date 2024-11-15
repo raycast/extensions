cf7df7df6a5dfdb6c42355bdc2660cc9fcd1fe10
Silvan Egger <silvan.egger@buhlergroup.com>
2024-11-15 14:22:54 +0100

    Fix code style issues

--- extensions/remove-window-from-set/src/remove-window-from-set.ts
+++ extensions/remove-window-from-set/src/remove-window-from-set.ts
@@ -3,16 +3,13 @@ import { showHUD } from "@raycast/api";
 
 export default async function Command() {
   try {
-    await runAppleScript(
-      `
+    await runAppleScript(`
 tell application "System Events" to tell (process 1 where frontmost is true)
     click menu item "Remove Window from Set" of menu 1 of menu bar item "Window" of menu bar 1
 end tell
-    `
-    );
+    `);
     await showHUD("Successfully clicked `Remove Window from Set`");
-  }
-  catch (error) {
+  } catch (error) {
     await showHUD("Error when trying to click `Remove Window from Set`");
   }
 }
