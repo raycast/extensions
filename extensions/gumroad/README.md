5fd2458b1253c4a7d087144db76cd2da51508124
Eduardo Gómez <this.eduardo@gmail.com>
2024-11-17 07:52:08 +0100

    Add gumroad extension (#15295)
    * Add gumroad extension
    
    - Updated readme
    - Added assets and metadata
    - Fixed linting issues
    - Fixed flickering issue
    - Persist state
    - Implemented pagination and filters for sales
    - Implemented sale and product details
    - Moved types to their own file
    - Remove .idea directory from repository
    - first commit
    
    * Added icon to details action, added tags and url to product metadata and fixed issue where the loading indicator did not stop after an error
    
    * Update CHANGELOG.md and optimise images
    
    ---------
    
    Co-authored-by: raycastbot <bot@raycast.com>

---
+++ extensions/gumroad/README.md
@@ -0,0 +1,15 @@
+# Gumroad Sales
+
+View your sales and products from Gumroad.
+
+## Obtaining an Access Token
+
+In order to use this extension, you need to obtain a Gumroad access token. To do so, follow these steps:
+
+1. Go to [Gumroad](https://gumroad.com) and log in to your account.
+
+2. In the left sidebar, click "Settings" and then click on "Advanced". The following link can be used as a shortcut: https://app.gumroad.com/settings/advanced
+
+4. In the "Applications" section you will need to create a new application. To do this you will need to provide a name and a redirect URI, which can be any valid URL, for example `https://localhost/`.
+
+5. Once the app is created, click "Generate Access Token", copy the generated token and paste it into the corresponding field in the extension settings.
---
+++ extensions/gumroad/src/view-products.tsx
@@ -0,0 +1,39 @@
+import { getPreferenceValues, List, ActionPanel, Action, Icon, Color } from "@raycast/api";
+import { useFetch } from "@raycast/utils";
+import { ProductsResponse } from "./types";
+import { ProductDetails } from "./product-details";
+import { formatNumber } from "./utils";
+import { BASE_URL, PRODUCTS_ENDPOINT } from "./const";
+
+const token = getPreferenceValues<Preferences>().token;
+
+export default function Command() {
+  const { data, isLoading } = useFetch<ProductsResponse>(`${BASE_URL}${PRODUCTS_ENDPOINT}?access_token=${token}`);
+
+  return (
+    <List isLoading={isLoading}>
+      {data?.products.map((product) => (
+        <List.Item
+          key={product.id}
+          title={product.name}
+          subtitle={product.formatted_price}
+          icon={
+            product.thumbnail_url ? { source: product.thumbnail_url } : { source: Icon.Image, tintColor: Color.Magenta }
+          }
+          accessories={[
+            {
+              text: `${formatNumber(product.sales_count)} Sales`,
+              icon: { source: Icon.Cart, tintColor: Color.Green },
+            },
+          ]}
+          actions={
+            <ActionPanel>
+              <Action.Push title="Show Details" target={<ProductDetails product={product} />} icon={Icon.Sidebar} />
+              <Action.OpenInBrowser url={product.short_url} />
+            </ActionPanel>
+          }
+        />
+      ))}
+    </List>
+  );
+}
