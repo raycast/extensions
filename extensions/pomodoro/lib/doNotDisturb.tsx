697686bbc73688147ef2bb6ed17d47fa8edf9c22
Mikey <46366395+mikikiv@users.noreply.github.com>
2024-11-18 23:18:36 -0800

    Add DND extension check and prompt for installation

--- extensions/pomodoro/lib/doNotDisturb.tsx
+++ extensions/pomodoro/lib/doNotDisturb.tsx
@@ -1,4 +1,4 @@
-import { LaunchType } from "@raycast/api";
+import { confirmAlert, LaunchType, open } from "@raycast/api";
 import { crossLaunchCommand } from "raycast-cross-extension";
 
 export const dndLaunchOptions = {
@@ -17,5 +17,23 @@ export async function setDND(enabled: boolean) {
     false
   ).catch(() => {
     // Do nothing here because we're going to check when mounting the extension
+  });
+}
+
+export async function checkDNDExtensionInstall() {
+  await crossLaunchCommand({
+    ...dndLaunchOptions,
+    name: "status",
+    context: { suppressHUD: true },
+  }).catch(async () => {
+    const installDND = await confirmAlert({
+      title: "Need to Install Additional Extension",
+      message:
+        'The "Enable Do Not Disturb mode while focused" feature requires the "Do Not Distrub" extension, do you want to move to the install page now?',
+    });
+    if (installDND) {
+      // Open the store view
+      await open("raycast://extensions/yakitrak/do-not-disturb");
+    }
   });
 }
