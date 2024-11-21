f0daf4ed5a1134f87074d6abb6f433333af1fab0
xmok <30526133+xmok@users.noreply.github.com>
2024-11-22 02:58:09 +0500

    [changedetection] add icon + view basic details

--- extensions/changedetection-io/src/list-watches.tsx
+++ extensions/changedetection-io/src/list-watches.tsx
@@ -1,24 +1,40 @@
-import { Action, ActionPanel, Icon, List, getPreferenceValues } from "@raycast/api";
+import { Action, ActionPanel, Detail, Icon, Keyboard, List, getPreferenceValues, openExtensionPreferences } from "@raycast/api";
 import { getFavicon, useFetch } from "@raycast/utils";
 
 interface Watch {
   last_changed: number;
   last_checked: number;
-  last_error: boolean;
+  last_error: false | string;
   title: string | null;
   url: string;
+  viewed: boolean;
 }
 interface WatchesResponse {
   [key: string]: Watch;
 }
-export default function ListWatches() {
-  const { instance_url, api_key } = getPreferenceValues<Preferences>();
-  const url = new URL(instance_url).toString();
-  const { isLoading, data } = useFetch<WatchesResponse>(url + "api/v1/watch", {
+
+const { instance_url, api_key } = getPreferenceValues<Preferences>();
+
+function useApi<T>(endpoint: string) {
+  const url = new URL(`api/v1/${endpoint}`, instance_url).toString();
+  const { isLoading, data } = useFetch<T>(url, {
     headers: {
       "x-api-key": api_key
     }
-  })
+  });
+  return { isLoading, data };
+}
+
+export default function ListWatches() {
+
+  try {
+    new URL(instance_url);
+  } catch (error) {
+    return <Detail markdown={"# Error \n\n Invalid URL"} actions={<ActionPanel><Action icon={Icon.Gear} title="Open Extension Preferences" onAction={openExtensionPreferences} /></ActionPanel>} />
+  }
+  const url = new URL(instance_url).toString();
+
+  const { isLoading, data } = useApi<WatchesResponse>("watch");
 
   return (
     <List isLoading={isLoading}>
@@ -29,14 +45,44 @@ export default function ListWatches() {
         const icon = getFavicon(watch.url, { fallback: Icon.Globe });
 
         return <List.Item key={id} icon={icon} title={watch.url} subtitle={watch.title ?? undefined} keywords={keywords} accessories={[
+          {icon: watch.viewed ? Icon.Eye : Icon.EyeDisabled, tooltip: watch.viewed ? "Viewed" : "Not Viewed"},
           { date: new Date(watch.last_checked * 1000), tooltip: "Last Checked", icon: Icon.MagnifyingGlass },
           watch.last_changed ? { date: new Date(watch.last_changed * 1000), tooltip: "Last Changed", icon: Icon.Pencil } : { text: "Not yet", tooltip: "Last Changed", icon: Icon.Pencil }
         ]} actions={<ActionPanel>
-          <Action.OpenInBrowser icon={Icon.ArrowNe} title="Edit" url={`${url}edit/${id}#general`} />
+          <Action.Push title="View Details" target={<WatchDetails id={id} />} />
           <Action.OpenInBrowser icon={Icon.ArrowNe} title="Preview" url={`${url}preview/${id}#text`} />
-          <Action.OpenInBrowser icon={icon} url={watch.url} />
+          <Action.OpenInBrowser icon={Icon.ArrowNe} title="Edit" url={`${url}edit/${id}#general`} shortcut={Keyboard.Shortcut.Common.Edit} />
+          <Action.OpenInBrowser icon={icon} url={watch.url} shortcut={Keyboard.Shortcut.Common.Open} />
         </ActionPanel>} />
       })}
     </List>
   );
+}
+
+function WatchDetails({id}: {id: string}) {
+  interface WatchDetails extends Watch {
+  check_count: number
+  date_created: number
+  last_viewed: number
+  method: "OPTIONS" | "GET" | "DELETE" | "PATCH" | "POST" | "PUT";
+  notification_alert_count:number,
+  paused: boolean
+  processor: "restock_diff" | "text_json_diff";
+  sort_text_alphabetically: boolean,
+  tags: string[];
+uuid: string
+last_notification_error: false | string;
+history_n: number
+  }
+  const { isLoading, data } = useApi<WatchDetails>(`watch/${id}`);
+  return <Detail isLoading={isLoading} metadata={data && <Detail.Metadata>
+    <Detail.Metadata.Label title="Check Count" text={data.check_count.toString()} />
+    <Detail.Metadata.Label title="Date Created" text={new Date(data.date_created*1000).toString()} />
+    <Detail.Metadata.Label title="Last Viewed" text={new Date(data.last_viewed*1000).toString()} />
+    <Detail.Metadata.Label title="Method" text={data.method} />
+    <Detail.Metadata.Label title="Notification Alert Count" text={data.notification_alert_count.toString()} />
+    <Detail.Metadata.Label title="Paused" icon={data.paused ? Icon.Check : Icon.Xmark} />
+    <Detail.Metadata.Label title="Processor" text={data.processor==="restock_diff" ? "Re-stock & Price detection for single product pages" : "Re-stock & Price detection for single product pages"} />
+    <Detail.Metadata.Label title="Sort Text Alphabetically" icon={data.paused ? Icon.Check : Icon.Xmark} />
+  </Detail.Metadata>} />
 }
