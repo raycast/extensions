5eb6f62eae77b2a55490ed3859a22bbf41b9a965
yen-cheng <lin0905580802@gmail.com>
2024-11-25 10:24:53 +0800

    Update port-manager extension

--- extensions/port-manager/src/open-ports.tsx
+++ extensions/port-manager/src/open-ports.tsx
@@ -1,4 +1,4 @@
-import { Action, ActionPanel, Color, Icon, List, showToast } from "@raycast/api";
+import { Action, ActionPanel, Color, Icon, List, showToast, getPreferenceValues } from "@raycast/api";
 import { useCachedState } from "@raycast/utils";
 import { CopyCommandsActionsMenu } from "./actions/CopyCommandsActionMenu";
 import CopyInfoActionsMenu from "./actions/CopyInfoActionsMenu";
@@ -12,6 +12,7 @@ import useProcesses from "./hooks/useProcesses";
 import { getProcessAccessories } from "./utilities/getProcessAccessories";
 
 export default function Command() {
+  const { primaryPortAction } = getPreferenceValues();
   const { processes, revalidateProcesses, isLoadingProcesses } = useProcesses();
   const { getNamedPort } = useNamedPorts();
 
@@ -19,53 +20,12 @@ export default function Command() {
 
   return (
     <List isShowingDetail={isShowingDetail} isLoading={isLoadingProcesses} searchBarPlaceholder="Search Open Ports">
-      {processes?.map((p) => (
-        <List.Item
-          key={p.pid}
-          title={p.name ?? "Untitled Process"}
-          subtitle={isShowingDetail ? "" : p.user ?? ""}
-          keywords={p.portInfo
-            ?.map((i) => `${i.port}`)
-            .concat(p.portInfo?.map((i) => `${i.host}`))
-            .concat(p.portInfo?.map((i) => `${i.name}`))}
-          detail={
-            <List.Item.Detail
-              metadata={
-                <List.Item.Detail.Metadata>
-                  <List.Item.Detail.Metadata.Label title="Name" text={p.name} />
-                  <List.Item.Detail.Metadata.Label title="User" text={`${p.user} (${p.uid})`} />
-                  <List.Item.Detail.Metadata.Label title="PID" text={`${p.pid}`} />
-                  {p.path !== undefined && <List.Item.Detail.Metadata.Label title="Path" text={p.path} />}
-                  <List.Item.Detail.Metadata.Label title="Parent PID" text={`${p.parentPid}`} />
-                  {p.parentPath !== undefined && (
-                    <List.Item.Detail.Metadata.Label title="Parent Path" text={p.parentPath} />
-                  )}
-                  <List.Item.Detail.Metadata.Label title="Protocol" text={`${p.protocol}`} />
-                  {p.portInfo && (
-                    <List.Item.Detail.Metadata.TagList title="Ports">
-                      {p.portInfo.map((i, index) => {
-                        const name = getNamedPort(i.port)?.name;
-                        if (name !== undefined) {
-                          return (
-                            <List.Item.Detail.Metadata.TagList.Item
-                              key={index}
-                              text={`${i.port} (${name})`}
-                              color={Color.Green}
-                            />
-                          );
-                        }
-
-                        return <List.Item.Detail.Metadata.TagList.Item key={index} text={`${i.port}`} />;
-                      })}
-                    </List.Item.Detail.Metadata.TagList>
-                  )}
-                </List.Item.Detail.Metadata>
-              }
-            />
-          }
-          actions={
-            <ActionPanel>
+      {processes?.map((p) => {
+        const actions = [
+          {
+            action: (
               <KillActions
+                key="kill"
                 process={p}
                 onKilled={() => {
                   showToast(Toasts.KillProcess.Success(p));
@@ -76,7 +36,13 @@ export default function Command() {
                   revalidateProcesses();
                 }}
               />
+            ),
+            id: "kill",
+          },
+          {
+            action: (
               <KillAllActions
+                key="killAll"
                 process={p}
                 onKilled={() => {
                   showToast(Toasts.KillProcess.Success(p));
@@ -87,7 +53,13 @@ export default function Command() {
                   revalidateProcesses();
                 }}
               />
+            ),
+            id: "killAll",
+          },
+          {
+            action: (
               <KillParentActions
+                key="killParent"
                 process={p}
                 onKilled={() => {
                   showToast(Toasts.KillProcess.Success(p));
@@ -98,21 +70,94 @@ export default function Command() {
                   revalidateProcesses();
                 }}
               />
+            ),
+            id: "killParent",
+          },
+          {
+            action: (
               <Action
+                key="showDetails"
                 title="Show Details"
                 icon={Icon.QuestionMark}
                 shortcut={{ modifiers: ["cmd", "shift"], key: "d" }}
                 onAction={() => setIsShowingDetail((prev) => !prev)}
               />
-              <ShowInFinderActionMenu process={p} />
-              <CopyInfoActionsMenu process={p} />
-              <CopyCommandsActionsMenu process={p} />
-              <Action title="Reload" onAction={revalidateProcesses} icon={Icon.ArrowClockwise} />
-            </ActionPanel>
-          }
-          accessories={isShowingDetail ? undefined : getProcessAccessories(p)}
-        />
-      ))}
+            ),
+            id: "showDetails",
+          },
+          {
+            action: <ShowInFinderActionMenu key="showInFinder" process={p} />,
+            id: "showInFinder",
+          },
+          {
+            action: <CopyInfoActionsMenu key="copyInfo" process={p} />,
+            id: "copyInfo",
+          },
+          {
+            action: <CopyCommandsActionsMenu key="copyCommands" process={p} />,
+            id: "copyCommands",
+          },
+          {
+            action: <Action key="reload" title="Reload" icon={Icon.ArrowClockwise} onAction={revalidateProcesses} />,
+            id: "reload",
+          },
+        ];
+
+        const primaryActionIndex = actions.findIndex((a) => a.id === primaryPortAction);
+        if (primaryActionIndex > -1) {
+          const [primaryAction] = actions.splice(primaryActionIndex, 1);
+          actions.unshift(primaryAction);
+        }
+
+        return (
+          <List.Item
+            key={p.pid}
+            title={p.name ?? "Untitled Process"}
+            subtitle={isShowingDetail ? "" : p.user ?? ""}
+            keywords={p.portInfo
+              ?.map((i) => `${i.port}`)
+              .concat(p.portInfo?.map((i) => `${i.host}`))
+              .concat(p.portInfo?.map((i) => `${i.name}`))}
+            detail={
+              <List.Item.Detail
+                metadata={
+                  <List.Item.Detail.Metadata>
+                    <List.Item.Detail.Metadata.Label title="Name" text={p.name} />
+                    <List.Item.Detail.Metadata.Label title="User" text={`${p.user} (${p.uid})`} />
+                    <List.Item.Detail.Metadata.Label title="PID" text={`${p.pid}`} />
+                    {p.path !== undefined && <List.Item.Detail.Metadata.Label title="Path" text={p.path} />}
+                    <List.Item.Detail.Metadata.Label title="Parent PID" text={`${p.parentPid}`} />
+                    {p.parentPath !== undefined && (
+                      <List.Item.Detail.Metadata.Label title="Parent Path" text={p.parentPath} />
+                    )}
+                    <List.Item.Detail.Metadata.Label title="Protocol" text={`${p.protocol}`} />
+                    {p.portInfo && (
+                      <List.Item.Detail.Metadata.TagList title="Ports">
+                        {p.portInfo.map((i, index) => {
+                          const name = getNamedPort(i.port)?.name;
+                          if (name !== undefined) {
+                            return (
+                              <List.Item.Detail.Metadata.TagList.Item
+                                key={index}
+                                text={`${i.port} (${name})`}
+                                color={Color.Green}
+                              />
+                            );
+                          }
+
+                          return <List.Item.Detail.Metadata.TagList.Item key={index} text={`${i.port}`} />;
+                        })}
+                      </List.Item.Detail.Metadata.TagList>
+                    )}
+                  </List.Item.Detail.Metadata>
+                }
+              />
+            }
+            actions={<ActionPanel>{actions.map((a) => a.action)}</ActionPanel>}
+            accessories={isShowingDetail ? undefined : getProcessAccessories(p)}
+          />
+        );
+      })}
     </List>
   );
 }
