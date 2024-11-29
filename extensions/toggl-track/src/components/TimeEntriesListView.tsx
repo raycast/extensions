2705ef5d6b03dcfe8960c13ee937d69466537b96
AlanHuang <alan0988469026@gmail.com>
2024-11-29 18:31:40 +0800

    Update toggl-track extension
    - Fix lint and update changelog
    - Add UpdateTimeEntry command and update and remove time entry functionality
    - Initial commit

--- extensions/toggl-track/src/components/TimeEntriesListView.tsx
+++ extensions/toggl-track/src/components/TimeEntriesListView.tsx
@@ -1,69 +1,31 @@
-import { Action, ActionPanel, Alert, Color, confirmAlert, Icon, List } from "@raycast/api";
-import dayjs from "dayjs";
-import duration from "dayjs/plugin/duration";
+import { Action, ActionPanel, Alert, Color, Icon, List, confirmAlert } from "@raycast/api";
 
-import { removeTimeEntry } from "@/api/timeEntries";
-import TimeEntryForm from "@/components/CreateTimeEntryForm";
-import RunningTimeEntry from "@/components/RunningTimeEntry";
-import UpdateTimeEntryForm from "@/components/UpdateTimeEntryForm";
+import { removeTimeEntry } from "@/api";
 import { ExtensionContextProvider } from "@/context/ExtensionContext";
 import { formatSeconds } from "@/helpers/formatSeconds";
 import { Verb, withToast } from "@/helpers/withToast";
 import { useProcessedTimeEntries } from "@/hooks/useProcessedTimeEntries";
-import { useTimeEntryActions } from "@/hooks/useTimeEntryActions";
 import { useTotalDurationToday } from "@/hooks/useTotalDurationToday";
 
-dayjs.extend(duration);
+import UpdateTimeEntryForm from "./UpdateTimeEntryForm";
 
-function ListView() {
+export function TimeEntriesListView() {
   const {
     isLoading,
     mutateTimeEntries,
-    revalidateRunningTimeEntry,
-    revalidateTimeEntries,
-    runningTimeEntry,
     timeEntries,
     timeEntriesWithUniqueProjectAndDescription,
+    revalidateTimeEntries,
   } = useProcessedTimeEntries();
 
-  const totalDurationToday = useTotalDurationToday(timeEntries, runningTimeEntry);
-  const { resumeTimeEntry } = useTimeEntryActions(revalidateRunningTimeEntry, revalidateTimeEntries);
+  const totalDurationToday = useTotalDurationToday(timeEntries);
 
   return (
     <List
       isLoading={isLoading}
       throttle
-      navigationTitle={isLoading ? undefined : `Today: ${formatSeconds(totalDurationToday)}`}
+      navigationTitle={isLoading ? undefined : `Total: ${formatSeconds(totalDurationToday)}`}
     >
-      {runningTimeEntry && (
-        <RunningTimeEntry
-          runningTimeEntry={runningTimeEntry}
-          revalidateRunningTimeEntry={revalidateRunningTimeEntry}
-          revalidateTimeEntries={revalidateTimeEntries}
-        />
-      )}
-      <List.Section title="Actions">
-        <List.Item
-          title="Create a new time entry"
-          icon={"command-icon.png"}
-          actions={
-            <ActionPanel>
-              <Action.Push
-                title="Create Time Entry"
-                icon={{ source: Icon.Clock }}
-                target={
-                  <ExtensionContextProvider>
-                    <TimeEntryForm
-                      revalidateRunningTimeEntry={revalidateRunningTimeEntry}
-                      revalidateTimeEntries={revalidateTimeEntries}
-                    />
-                  </ExtensionContextProvider>
-                }
-              />
-            </ActionPanel>
-          }
-        />
-      </List.Section>
       {timeEntriesWithUniqueProjectAndDescription.length > 0 && (
         <List.Section title="Recent time entries">
           {timeEntriesWithUniqueProjectAndDescription.map((timeEntry) => (
@@ -80,30 +42,12 @@ function ListView() {
               icon={{ source: Icon.Circle, tintColor: timeEntry.project_color }}
               actions={
                 <ActionPanel>
-                  <Action.SubmitForm
-                    title="Resume Time Entry"
-                    onSubmit={() => resumeTimeEntry(timeEntry)}
-                    icon={{ source: Icon.Clock }}
-                  />
                   <Action.Push
                     title="Edit Time Entry"
                     icon={Icon.Pencil}
                     target={
                       <ExtensionContextProvider>
                         <UpdateTimeEntryForm timeEntry={timeEntry} revalidateTimeEntries={revalidateTimeEntries} />
-                      </ExtensionContextProvider>
-                    }
-                  />
-                  <Action.Push
-                    title="Create Similar Time Entry"
-                    icon={{ source: Icon.Plus }}
-                    target={
-                      <ExtensionContextProvider>
-                        <TimeEntryForm
-                          initialValues={timeEntry}
-                          revalidateRunningTimeEntry={revalidateRunningTimeEntry}
-                          revalidateTimeEntries={revalidateTimeEntries}
-                        />
                       </ExtensionContextProvider>
                     }
                   />
@@ -117,10 +61,7 @@ function ListView() {
                         await confirmAlert({
                           title: "Delete Time Entry",
                           message: "Are you sure you want to delete this time entry?",
-                          icon: {
-                            source: Icon.Trash,
-                            tintColor: Color.Red,
-                          },
+                          icon: { source: Icon.Trash, tintColor: Color.Red },
                           primaryAction: {
                             title: "Delete",
                             style: Alert.ActionStyle.Destructive,
@@ -148,10 +89,4 @@ function ListView() {
   );
 }
 
-export default function Command() {
-  return (
-    <ExtensionContextProvider>
-      <ListView />
-    </ExtensionContextProvider>
-  );
-}
+export default TimeEntriesListView;
