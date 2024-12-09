60537905c1b6669c76eefdd7b428ea0328805012
zcsabbag <zane@Zanes-MacBook-Pro.local>
2024-12-08 21:02:43 -0800

    bug fixes

--- extensions/toggle-grayscale/toggle-grayscale/src/toggle-grayscale.ts
+++ extensions/toggle-grayscale/toggle-grayscale/src/toggle-grayscale.ts
@@ -19,23 +19,22 @@ const appleScript = `
       -- Click Accessibility
       click menu item "Accessibility" of menu "View" of menu bar 1
       
-      delay 0.2
+      delay 0.3
       
-      -- Click Display under Vision section
-      click button 3 of group 1 of scroll area 1 of group 1 of group 2 of splitter group 1 of group 1 of window 1
+      -- Click Display under Vision section (updated selector)
+      click button 4 of group 2 of scroll area 1 of group 1 of group 2 of splitter group 1 of group 1 of window 1
       
       delay 0.3
       
-      -- Click the Color Filters toggle (updated selector)
       click checkbox "Color filters" of group 5 of scroll area 1 of group 1 of group 2 of splitter group 1 of group 1 of window 1
     end tell
   end tell
 
-  -- Quit System Settings
+    -- Quit System Settings
   tell application "System Settings" to quit
 `;
 
 export default async function main() {
-  const res = await runAppleScript(appleScript, [], { language: "AppleScript" });
-  await showHUD(res);
+  await runAppleScript(appleScript, [], { language: "AppleScript" });
+  await showHUD("✨ Color filters toggled ✨");
 }
