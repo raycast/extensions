16704bf7cb00bc83dc0c75df08e60c46a75b2e4c
xmok <30526133+xmok@users.noreply.github.com>
2024-12-10 11:29:07 +0500

    [MyIdlers] items + more summary

--- extensions/my-idlers/src/dashboard.tsx
+++ extensions/my-idlers/src/dashboard.tsx
@@ -11,6 +11,20 @@ export default function Dashboard() {
   
   const { isLoading: isLoadingPricing, data: pricing } = useGetPricing();
   
+  const sum = servers.filter(server => server.active).reduce(
+    (acc, server) => {
+      acc.cpu += server.cpu;
+      acc.ram += server.ram_as_mb;
+      acc.disk += server.disk_as_gb;
+      acc.bandwidth += server.bandwidth;
+      acc.locations.add(server.location_id);
+      acc.providers.add(server.provider_id);
+      return acc;
+    },
+    { cpu: 0, ram: 0, disk: 0, bandwidth: 0, locations: new Set(), providers: new Set() }
+  );
+  const averages = { ...sum, ram: (sum.ram/1024).toFixed(2), bandwidth: (sum.bandwidth/1024).toFixed(2), locations: sum.locations.size, providers: sum.providers.size };
+  
   const isLoading = isLoadingServers || isLoadingShared || isLoadingReseller || isLoadingDomains || isLoadingMisc || isLoadingPricing;
 
   return <List isLoading={isLoading} isShowingDetail>
@@ -25,6 +39,14 @@ export default function Dashboard() {
       <List.Item.Detail.Metadata.Label title="Weekly Cost" text={pricing.weekly} />
       <List.Item.Detail.Metadata.Label title="Monthly Cost" text={pricing.monthly} />
       <List.Item.Detail.Metadata.Label title="Yearly Cost" text={pricing.yearly} />
+    </List.Item.Detail.Metadata>} />} />
+    <List.Item title="Averages" detail={<List.Item.Detail metadata={<List.Item.Detail.Metadata>
+      <List.Item.Detail.Metadata.Label title="CPU" text={averages.cpu.toString()} />
+      <List.Item.Detail.Metadata.Label title="RAM" text={`${averages.ram} GB`} />
+      <List.Item.Detail.Metadata.Label title="DISK" text={`${averages.disk} GB`} />
+      <List.Item.Detail.Metadata.Label title="Bandwidth" text={`${averages.bandwidth} TB`} />
+      <List.Item.Detail.Metadata.Label title="Locations" text={averages.locations.toString()} />
+      <List.Item.Detail.Metadata.Label title="Providers" text={averages.providers.toString()} />
     </List.Item.Detail.Metadata>} />} />
   </List>
 }
--- extensions/my-idlers/src/providers.tsx
+++ extensions/my-idlers/src/providers.tsx
@@ -1,16 +1,20 @@
 import { Icon, List } from "@raycast/api";
 import useGet from "./hooks";
+import { useState } from "react";
+import { Item } from "./types";
 
-type Provider = {
-  id: number
-  name: string
-}
 export default function Providers() {
-  const { isLoading, data: providers } =useGet<Provider>("providers");
+  const [item, setItem] = useState("");
   
-  return <List isLoading={isLoading}>
-    <List.Section title="Providers" subtitle={providers.length.toString()}>
-    {providers.map(provider => <List.Item key={provider.id} icon={Icon.Dot} title={provider.name} />)}
+  const { isLoading, data: items } = useGet<Item>(item);
+
+  return <List isLoading={isLoading} searchBarAccessory={<List.Dropdown tooltip="Item" onChange={setItem} storeValue>
+    <List.Dropdown.Item title="Locations" value="locations" />
+    <List.Dropdown.Item title="OS" value="os" />
+    <List.Dropdown.Item title="Providers" value="providers" />
+  </List.Dropdown>}>
+    <List.Section title={item} subtitle={items.length.toString()}>
+    {items.map(item => <List.Item key={item.id} icon={Icon.Dot} title={item.name} />)}
     </List.Section>
   </List>
 }
