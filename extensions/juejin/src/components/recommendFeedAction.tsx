e0a9de443e78b3ee1fcbebb75430d0643c83d62c
Sharker <1548742234@qq.com>
2024-12-10 21:51:37 +0800

    chore: clean up code

--- extensions/juejin/src/components/recommendFeedAction.tsx
+++ extensions/juejin/src/components/recommendFeedAction.tsx
@@ -3,7 +3,7 @@ import { Action, ActionPanel } from "@raycast/api";
 export function RecommendFeedAction({ articleId }: { articleId: string }) {
   return (
     <ActionPanel>
-      {articleId && <Action.OpenInBrowser url={`https://juejin.cn/post/${articleId}`} title="打开文章" />}
+      {articleId && <Action.OpenInBrowser url={`https://juejin.cn/post/${articleId}`} title="Open In Browser" />}
       {articleId && (
         <Action.CopyToClipboard
           title="Copy Link"
