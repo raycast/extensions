d014051b57edde23efe528445903658d95d42aee
xmok <30526133+xmok@users.noreply.github.com>
2024-12-11 21:56:17 +0500

    [Aiven] lint + add metadata

--- extensions/aiven/src/interfaces.ts
+++ extensions/aiven/src/interfaces.ts
@@ -1,34 +1,45 @@
 interface Project {
-    project_name: string;
-    tags: {
-        [key: string]: string;
-    }
+  project_name: string;
+  tags: {
+    [key: string]: string;
+  };
 }
 export interface ListProjectsResult {
-    projects: Project[];
+  projects: Project[];
 }
 
 export enum State {
-    POWEROFF="POWEROFF",
-    REBALANCING="REBALANCING",
-    REBUILDING="REBUILDING",
-    RUNNING="RUNNING",
+  POWEROFF = "POWEROFF",
+  REBALANCING = "REBALANCING",
+  REBUILDING = "REBUILDING",
+  RUNNING = "RUNNING",
+}
+interface Component {
+  component: string;
+  host: string;
+  port: number;
+  route: "dynamic" | "public" | "private" | "privatelink";
+  usage: "disaster_recovery" | "primary" | "replica";
 }
 interface Service {
-    create_time: string;
-    node_count: number;
-    service_name: string;
-    service_type: string;
-    state: State;
+  cloud_description: string;
+  components: Component[];
+  create_time: string;
+  metadata: { [key: string]: string | boolean };
+  node_count: number;
+  service_name: string;
+  service_type: string;
+  state: State;
 }
 export interface ListServicesResult {
-    services: Service[];
+  services: Service[];
 }
 
+interface AivenError {
+  message: string;
+  status: number;
+}
 export interface ErrorResult {
-    message: string;
-    errors?: Array<{
-        message: string;
-        status: number;
-    }>;
+  message: string;
+  errors: AivenError[];
 }
