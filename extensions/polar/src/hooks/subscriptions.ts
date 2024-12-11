b6098697bb9e6095154867aeb76b4730e9bc1d22
Emil Widlund <hello@emilwidlund.com>
2024-12-11 17:41:52 +0100

    Update polar extension
    - lint
    - implement pagination

--- extensions/polar/src/hooks/subscriptions.ts
+++ extensions/polar/src/hooks/subscriptions.ts
@@ -1,15 +1,22 @@
-import { OrdersListRequest } from "@polar-sh/sdk/models/operations";
 import { useInfiniteQuery } from "@tanstack/react-query";
-import { useContext } from "react";
+import { SubscriptionsListRequest } from "@polar-sh/sdk/models/operations";
 import { PolarContext } from "../providers";
+import { useContext } from "react";
 
-export const useOrders = (parameters: OrdersListRequest, limit: number) => {
+export const useListSubscriptions = (
+  parameters: SubscriptionsListRequest,
+  limit: number,
+) => {
   const polar = useContext(PolarContext);
 
   return useInfiniteQuery({
-    queryKey: ["orders", parameters],
+    queryKey: ["subscriptions", parameters],
     queryFn: ({ pageParam = 1 }) =>
-      polar.orders.list({ ...parameters, page: pageParam, limit: limit }),
+      polar.subscriptions.list({
+        ...parameters,
+        page: pageParam,
+        limit: limit,
+      }),
     initialPageParam: 1,
     getNextPageParam: (lastPage, pages) => {
       const currentPage = pages.length;
