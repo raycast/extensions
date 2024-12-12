a4c8d792958b787e0e4b0236427b2096d43296d7
yen-cheng <lin0905580802@gmail.com>
2024-12-12 17:08:35 +0800

    Update search-npm extension

--- extensions/search-npm/src/components/PackagListItem.tsx
+++ extensions/search-npm/src/components/PackagListItem.tsx
@@ -22,7 +22,7 @@ import {
   removeItemFromFavorites,
 } from '../utils/favorite-storage'
 import Favorites from '../favorites'
-import gitUrlParse from 'git-url-parse';
+import gitUrlParse from 'git-url-parse'
 
 interface PackageListItemProps {
   result: Package
@@ -77,7 +77,7 @@ export const PackageListItem = ({
     openRepository: pkg.links?.repository ? (
       <Action.OpenInBrowser
         key="openRepository"
-        url={gitUrlParse(pkg.links.repository).toString("https")}
+        url={gitUrlParse(pkg.links.repository).toString('https')}
         title="Open Repository"
         onOpen={handleAddToHistory}
       />
