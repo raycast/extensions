b9a7c5b37a65f01dbe860ac430474d363ba56c6c
Evan Zhou <evanzhoudev@gmail.com>
2024-12-17 13:54:43 -0800

    Update raycast-apple-intelligence extension (#15869)
    * Update raycast-apple-intelligence extension
    
    - Merge branch \'contributions/merge-1734238380067\'
    - Pull contributions
    - ✨ Some new commands
    - 🔧 Fix package-lock.json
    - first commit
    
    * 📝 Update CHANGELOG
    
    * 🔥 Decoupled Siri command
    
    * 📝 Slight change to description
    
    * 🔥 Remove Siri file
    
    * Update CHANGELOG.md and optimise images
    
    ---------
    
    Co-authored-by: raycastbot <bot@raycast.com>

--- extensions/raycast-apple-intelligence/src/api.ts
+++ extensions/raycast-apple-intelligence/src/api.ts
@@ -1,24 +1,25 @@
 import { closeMainWindow, showToast, Toast } from "@raycast/api";
 import { runAppleScript } from "@raycast/utils";
 
-export async function executeWritingToolCommand(command: string) {
+export async function executeWritingToolCommand(command: string | number) {
   await closeMainWindow();
   try {
     await runAppleScript(`
-    tell application "System Events"
-      try
-        tell (first process whose frontmost is true)
-          click menu bar item "Edit" of menu bar 1
-          click menu item "Writing Tools" of menu "Edit" of menu bar item "Edit" of menu bar 1
-          click menu item "${command}" of menu 1 of menu item "Writing Tools" of menu "Edit" of menu bar item "Edit" of menu bar 1
-        end tell
-      on error
-        key code 53 -- Press "escape" key
-        error "Failed to execute the command"
-      end try
-    end tell
-    `);
+      tell application "System Events"
+        try
+          tell (first process whose frontmost is true)
+            click menu bar item "Edit" of menu bar 1
+            click menu item "Writing Tools" of menu "Edit" of menu bar item "Edit" of menu bar 1
+            click menu item ${typeof command == "number" ? `${command}` : `"${command}"`} of menu 1 of menu item "Writing Tools" of menu "Edit" of menu bar item "Edit" of menu bar 1
+          end tell
+        on error
+          key code 53 -- Press "escape" key
+          error "Failed to execute the command"
+        end try
+      end tell
+      `);
   } catch (error) {
+    console.error(error);
     await showToast({ title: "Failed to execute " + command, style: Toast.Style.Failure });
   }
 }
