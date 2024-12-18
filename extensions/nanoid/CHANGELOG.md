d0062bcf9de117dda179634d48684135f6160072
Mike Valstar <mvalstar@konradgroup.com>
2024-12-18 09:45:48 -0500

    Update nanoid extension
    - added newer publish command
    - updated to use placeholder date
    - aded date to changelog
    - Added optional id length argument
    - Initial commit

--- extensions/nanoid/CHANGELOG.md
+++ extensions/nanoid/CHANGELOG.md
@@ -1,3 +1,7 @@
 # nanoid Changelog
 
+## Added length as an optional argument - {PR_MERGE_DATE}
+
+Added length for an optional argument to get different length of id
+
 ## [Initial Version] - 2022-06-08
--- extensions/nanoid/src/nanoid.tsx
+++ extensions/nanoid/src/nanoid.tsx
@@ -1,8 +1,19 @@
-import { Clipboard, showHUD } from "@raycast/api";
+import { Clipboard, showHUD, LaunchProps } from "@raycast/api";
 import { nanoid } from "nanoid";
 
-export default async () => {
-  const id = nanoid();
+interface Arguments {
+  len?: string;
+}
+
+export default async (props: LaunchProps<{ arguments: Arguments }>) => {
+  const len = props?.arguments.len;
+  let id: string | null = null;
+  if (len && !isNaN(parseInt(len))) {
+    id = nanoid(parseInt(len));
+  } else {
+    id = nanoid();
+  }
+
   await Clipboard.copy(id);
   await showHUD(`✅ Copied ${id}`);
 };
