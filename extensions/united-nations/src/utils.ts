6a6eb61b354abd0921b9d7ad30fd44447ac01b4b
LitoMore <LitoMore@users.noreply.github.com>
2024-12-19 14:05:13 +0800

    [United Nations] Enhance search experience (#15943)
    * [United Nations] Enhance search experience
    
    * Update CHANGELOG.md and optimise images
    
    ---------
    
    Co-authored-by: raycastbot <bot@raycast.com>

--- extensions/united-nations/src/utils.ts
+++ extensions/united-nations/src/utils.ts
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { LaunchType, confirmAlert, getPreferenceValues, open } from "@raycast/api";
 import { getVoices, killRunningSay, say } from "mac-say";
+import { match } from "pinyin-pro";
 import { crossLaunchCommand } from "raycast-cross-extension";
 import { useCachedState } from "@raycast/utils";
 import { recommendVoices } from "./constants.js";
@@ -62,4 +63,40 @@ export const useVoice = (languageCode: LanguageCode) => {
     loadVoices();
   }, []);
   return voice || undefined;
+};
+
+export const rightToLeftLanguages = new Set([LanguageCode.Arabic, LanguageCode.Urdu]);
+
+export const lantinSearchLanguages = new Set([
+  // Pinyin match
+  LanguageCode.Chinese,
+
+  // Contains match
+  LanguageCode.Arabic,
+  LanguageCode.Russian,
+  LanguageCode.Hindi,
+  LanguageCode.Urdu,
+]);
+
+export const latinSearchFilter = (languageCode: LanguageCode, text: string, searchText: string) => {
+  switch (languageCode) {
+    case LanguageCode.Chinese: {
+      const matched = match(text, searchText) || [];
+      return matched.length > 0;
+    }
+
+    case LanguageCode.Arabic:
+    case LanguageCode.Russian:
+    case LanguageCode.Hindi:
+    case LanguageCode.Urdu: {
+      const replacePattern = /[\s-_/\\:+%&.]/g;
+      const textWithoutSpaces = text.replace(replacePattern, "").toLowerCase();
+      const searchTextWithoutSpaces = searchText.replace(replacePattern, "").toLowerCase();
+      return textWithoutSpaces.includes(searchTextWithoutSpaces);
+    }
+
+    // Other language are using Raycast built-in search functionality
+    default:
+      return true;
+  }
 };
