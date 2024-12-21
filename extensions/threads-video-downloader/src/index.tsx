2f32b3906c765105a8beb245cd8b9c7243b3ce7f
ridemountainpig <lin0905580802@gmail.com>
2024-12-21 19:27:28 +0800

    Add crossLaunchCommand for Threads extension

--- extensions/threads-video-downloader/src/index.tsx
+++ extensions/threads-video-downloader/src/index.tsx
@@ -1,4 +1,5 @@
-import { Action, ActionPanel, Color, Icon, List, openExtensionPreferences } from "@raycast/api";
+import { Action, ActionPanel, Color, Icon, List, openExtensionPreferences, LaunchType, open } from "@raycast/api";
+import { crossLaunchCommand } from "raycast-cross-extension";
 
 export default function Command() {
   return (
@@ -6,13 +7,26 @@ export default function Command() {
       <List.EmptyView
         icon={{ source: Icon.Warning, tintColor: Color.SecondaryText }}
         title="Deprecated extension"
-        description="This extension is now part of the Threads extension. Install Threads to access its previous functionality. You can uninstall this extension from Preferences."
+        description="This extension is now included in the Threads extension. You can use Threads to access its previous functionality. If you wish, you can uninstall this extension from Preferences."
         actions={
           <ActionPanel>
-            <Action.OpenInBrowser
-              title="Install Threads Extension"
-              url="https://www.raycast.com/chrismessina/threads"
-              icon={Icon.Download}
+            <Action
+              title="Open Threads Extension"
+              onAction={() =>
+                crossLaunchCommand({
+                  name: "threads",
+                  type: LaunchType.UserInitiated,
+                  extensionName: "threads",
+                  ownerOrAuthorName: "chrismessina",
+                  context: {
+                    foo: "foo",
+                    bar: "bar",
+                  },
+                }).catch(() => {
+                  open("raycast://extensions/chrismessina/threads");
+                })
+              }
+              icon={Icon.ArrowRight}
             />
             <Action
               onAction={openExtensionPreferences}
