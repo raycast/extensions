361fc160a83e80b6b34a464b1bdde4e6dd28594f
Menahi Shayan <menahi.shayan@gmail.com>
2024-12-24 14:01:07 -0500

    Update battery-health extension
    - chore: :wrench: add publish script
    - feat: :sparkles: added battery health information
    - Initial commit

--- extensions/battery-health/CHANGELOG.md
+++ extensions/battery-health/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Battery Health Check Changelog
 
+## [Added Battery Health Percentage] - {PR_MERGE_DATE}
+
+ - Extension now shows the health of your battery relative to when it was new
+
 ## [Add Battery Health Check] - 2022-07-05
 
 Initial version code
--- extensions/battery-health/src/index.tsx
+++ extensions/battery-health/src/index.tsx
@@ -1,19 +1,26 @@
 import { List } from "@raycast/api";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { execa } from "execa";
 import plist from "plist";
 import TimeRemainingItem from "./components/TimeRemainingItem";
-import PercentageItem from "./components/PercentageItem";
 import PowerUsageItem from "./components/PowerUsageItem";
 import CycleCountItem from "./components/CycleCountItem";
 import TemperatureItem from "./components/TemperatureItem";
 import ChargeItem from "./components/ChargeItem";
 import PowerSourceItem from "./components/PowerSourceItem";
 import ConditionItem from "./components/ConditionItem";
+import StatsListItem from "./components/StatsListItem";
 
 type State = {
   batteryRegistry: any;
   isLoading: boolean;
+};
+
+type TValues = Record<"health" | "percentage", string>;
+const EMPTY_PLACEHOLDER = "--";
+const DEFAULT_VALUES: TValues = {
+  health: EMPTY_PLACEHOLDER,
+  percentage: EMPTY_PLACEHOLDER,
 };
 
 export default function Command() {
@@ -36,12 +43,25 @@ export default function Command() {
     })();
   }, []);
 
+  const { health, percentage } = useMemo(() => {
+    const values: TValues = DEFAULT_VALUES;
+    if (state?.batteryRegistry && !state.isLoading) {
+      const { AppleRawMaxCapacity, DesignCapacity, CurrentCapacity } = state.batteryRegistry;
+      if (!isNaN(AppleRawMaxCapacity / DesignCapacity)) {
+        values["health"] = ((AppleRawMaxCapacity / DesignCapacity) * 100).toFixed(1) + "%";
+      }
+      if (CurrentCapacity !== undefined && CurrentCapacity <= 100) values["percentage"] = `${CurrentCapacity}%`;
+    }
+
+    return DEFAULT_VALUES;
+  }, [state.isLoading, state?.batteryRegistry]);
+
   return (
     <List isLoading={state.isLoading}>
       {!state.isLoading ? (
         <>
           <TimeRemainingItem timeRemaining={state.batteryRegistry["TimeRemaining"]} />
-          <PercentageItem percentage={state.batteryRegistry["CurrentCapacity"]} />
+          <StatsListItem label="Percentage" value={percentage} />
           <PowerUsageItem voltage={state.batteryRegistry["Voltage"]} amperage={state.batteryRegistry["Amperage"]} />
           <ConditionItem pfStatus={state.batteryRegistry["PermanentFailureStatus"]} />
           <ChargeItem
@@ -50,6 +70,7 @@ export default function Command() {
             }
             maxCapacity={state.batteryRegistry["AppleRawMaxCapacity"] || state.batteryRegistry["MaxCapacity"]}
           />
+          <StatsListItem label="Health" value={health} />
           <CycleCountItem cycles={state.batteryRegistry["CycleCount"]} />
           <PowerSourceItem
             externalConnected={state.batteryRegistry["ExternalConnected"]}
