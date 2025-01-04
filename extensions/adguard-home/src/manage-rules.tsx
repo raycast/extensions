43fc34b4c6621c24069b75a24374e5cbe3d27d81
Ben Williams <ben@plgeek.com>
2025-01-04 23:18:09 +0000

    fix imports

--- extensions/adguard-home/src/manage-rules.tsx
+++ extensions/adguard-home/src/manage-rules.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { getCustomRules, CustomRule } from "./api";
 import { CustomRules } from "./components/CustomRules";
 
 export default function Command() {
