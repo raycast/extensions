8ef28079415c452a2566f4dbf4f20f45fe97a882
tiga <tigazhuang@futunn.com>
2025-01-08 17:28:17 +0800

    fix: fixed Ghostty becoming unresponsive when in the foreground

--- extensions/ghostty/src/new-ghostty-tab.ts
+++ extensions/ghostty/src/new-ghostty-tab.ts
@@ -3,12 +3,22 @@ import { popToRoot } from "@raycast/api";
 
 export default async function Command() {
   const script = `
+  tell application "System Events"
+    set isGhosttyRunning to exists (processes where name is "Ghostty")
+  end tell
+
   tell application "Ghostty"
-    if it is running then
+    if not isGhosttyRunning then
       activate
-      tell application "System Events" to keystroke "t" using {command down}
     else
+      -- If Ghostty is already running, activate Finder first then activate Ghostty and send Cmd+N to create new window
+      tell application "Finder" to activate
       activate
+      tell application "System Events"
+        tell process "Ghostty"
+          keystroke "t" using {command down}
+        end tell
+      end tell
     end if
   end tell`;
 
