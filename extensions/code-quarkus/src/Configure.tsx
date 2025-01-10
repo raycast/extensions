adb581e021a50bd8b6754ae88a7b9639a0dae4e7
Loïc Magnette <loicmagnette@gmail.com>
2025-01-10 07:05:35 +0100

    Update code-quarkus extension (#16248)
    * Update code-quarkus extension
    
    - shore(merging changes)
    - Pull contributions
    - adding script
    - shore(deps): update package.json
    - release(release 0.2.0): releasing new version
    - doc(update readme): update readme with the latest features
    - doc(update readme): update readme with the latest features
    - doc(update readme): update readme with the latest features
    - feature(configurable download directory): add configurable download directory
    - feature(add find extension): Add feature to be able to find an extension
    - shore(linting): fix linting
    - refactor(simplify routing): simplify routing using the native routing mechanism
    - feature(confort featues): Add some small features
    - shore(release): update version for next release
    
    * shore(changelog): update changelog file.
    
    * Update CHANGELOG.md and optimise images
    
    ---------
    
    Co-authored-by: Loic Magnette <loic.magnette@gmail.com>
    Co-authored-by: raycastbot <bot@raycast.com>

--- extensions/code-quarkus/src/Configure.tsx
+++ extensions/code-quarkus/src/Configure.tsx
@@ -1,23 +1,20 @@
 import { useEffect, useState } from "react";
-import fetch from "node-fetch";
-import { Action, ActionPanel, Form } from "@raycast/api";
+import { Action, ActionPanel, Form, openCommandPreferences, useNavigation } from "@raycast/api";
 import { QuarkusVersion } from "./models/QuarkusVersion";
 import { Configuration } from "./models/Configuration";
 import { BUILD_TOOLS, JAVA_VERSIONS } from "./models/Constants";
+import { Dependencies } from "./Dependencies";
+import { getQuarkusVersion } from "./api";
 
-export function Configure({
-  onVersionChange,
-  onConfigurationChange,
-}: {
-  onVersionChange: (newValue: QuarkusVersion) => void;
-  onConfigurationChange: (configuration: Configuration) => void;
-}) {
+export function Configure() {
+  const { push } = useNavigation();
   const [isLoading, setIsLoading] = useState(true);
   const [versions, setVersions] = useState<QuarkusVersion[]>([]);
+  const [version, setVersion] = useState<QuarkusVersion | null>(null);
 
   async function fetchQuarkusVersions() {
     setIsLoading(true);
-    const response = await fetch("https://code.quarkus.io/api/streams", {});
+    const response = await getQuarkusVersion();
     if (!response.ok) {
       setIsLoading(false);
       throw new Error(`Failed to fetch quarkus version: ${response.status} ${response.statusText}`);
@@ -28,11 +25,11 @@ export function Configure({
   }
 
   function handleVersionChange(key: string) {
-    onVersionChange(versions.filter((v) => v.key === key)[0]);
+    setVersion(versions.filter((v) => v.key === key)[0]);
   }
 
   function handleSubmit(configuration: Configuration) {
-    onConfigurationChange(configuration);
+    push(<Dependencies version={version || versions[0]} configuration={configuration} />);
   }
 
   useEffect(() => {
@@ -66,10 +63,11 @@ export function Configure({
       actions={
         <ActionPanel>
           <Action.SubmitForm onSubmit={handleSubmit} title="Add Dependencies" />
+          <Action title="Open Extension Preferences" onAction={openCommandPreferences} />
         </ActionPanel>
       }
+      navigationTitle={"Configure your new Quarkus project"}
     >
-      <Form.Description text="Configure your new Quarkus project" />
       <Form.Dropdown id="quarkusVersion" title="Quarkus version" onChange={handleVersionChange}>
         {versions.map((v) => (
           <Form.Dropdown.Item key={v.key} value={v.key} title={v?.platformVersion + (v?.lts ? " [LTS]" : "")} />
@@ -84,6 +82,7 @@ export function Configure({
       <Form.Separator />
       <Form.TextField id="group" title="Group" placeholder="org.acme" defaultValue="org.acme" />
       <Form.TextField id="artifact" title="Artifact" placeholder="code.with.quarkus" defaultValue="code.with.quarkus" />
+      <Form.TextField id="version" title="Version" placeholder="1.0.0-SNAPSHOT" defaultValue="1.0.0-SNAPSHOT" />
       <Form.Dropdown id="javaVersion" title="Java version">
         {JAVA_VERSIONS.map((javaVersion) => (
           <Form.Dropdown.Item key={javaVersion} value={javaVersion} title={javaVersion} />
