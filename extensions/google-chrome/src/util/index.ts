bb44aec1df0ff5de07b8f2930bff694cf265ccce
Santiago Perez <sperez@atlassian.com>
2025-01-11 19:53:51 +1100

    Add configurable profile path

--- extensions/google-chrome/src/util/index.ts
+++ extensions/google-chrome/src/util/index.ts
@@ -1,5 +1,7 @@
 import path from "path";
 import { DEFAULT_CHROME_PROFILE_ID, defaultChromeProfilePath, defaultChromeStatePath } from "../constants";
+import { getPreferenceValues } from "@raycast/api";
+import { Preferences } from "../interfaces";
 
 const userLibraryDirectoryPath = () => {
   if (!process.env.HOME) {
@@ -8,11 +10,21 @@ const userLibraryDirectoryPath = () => {
 
   return path.join(process.env.HOME, "Library");
 };
+const getChromeFilePath = (fileName: string, profile?: string) => {
+  const { profilePath } = getPreferenceValues<Preferences>();
+  if (profilePath) {
+    return path.join(profilePath, fileName);
+  }
+  return path.join(
+    userLibraryDirectoryPath(),
+    ...defaultChromeProfilePath,
+    profile ?? DEFAULT_CHROME_PROFILE_ID,
+    fileName
+  );
+};
 
-export const getHistoryDbPath = (profile?: string) =>
-  path.join(userLibraryDirectoryPath(), ...defaultChromeProfilePath, profile ?? DEFAULT_CHROME_PROFILE_ID, "History");
+export const getHistoryDbPath = (profile?: string) => getChromeFilePath("History", profile);
 
 export const getLocalStatePath = () => path.join(userLibraryDirectoryPath(), ...defaultChromeStatePath);
 
-export const getBookmarksFilePath = (profile?: string) =>
-  path.join(userLibraryDirectoryPath(), ...defaultChromeProfilePath, profile ?? DEFAULT_CHROME_PROFILE_ID, "Bookmarks");
+export const getBookmarksFilePath = (profile?: string) => getChromeFilePath("Bookmarks", profile);
