408d4ac4b834ae1816c609acbd6e3ba3992733ef
xmok <30526133+xmok@users.noreply.github.com>
2025-01-12 15:08:31 +0500

    [Cloudinary] ye olde lint

--- extensions/cloudinary/src/upload-clipboard.tsx
+++ extensions/cloudinary/src/upload-clipboard.tsx
@@ -8,23 +8,28 @@ import ViewResource from "./components/ViewResource";
 import { showFailureToast, usePromise } from "@raycast/utils";
 
 export default function main() {
-  const { isLoading, data: asset } = usePromise(async () => {
-    const { file } = await Clipboard.read();
+  const { isLoading, data: asset } = usePromise(
+    async () => {
+      const { file } = await Clipboard.read();
 
-    if (typeof file === "undefined") {
-      throw new Error("Missing image data.");
-    }
+      if (typeof file === "undefined") {
+        throw new Error("Missing image data.");
+      }
 
-    const filePath = fileUriToPath(file);
+      const filePath = fileUriToPath(file);
 
-    const resource = await uploadImage(filePath);
-    return resource as Asset;
-  }, [], {
-    onError(error) {
-      const message = typeof error.message === "string" ? error.message : "Failed to upload clipboard data to Cloudinary";
-      showFailureToast(message, { title: "Error" });
+      const resource = await uploadImage(filePath);
+      return resource as Asset;
     },
-  })
+    [],
+    {
+      onError(error) {
+        const message =
+          typeof error.message === "string" ? error.message : "Failed to upload clipboard data to Cloudinary";
+        showFailureToast(message, { title: "Error" });
+      },
+    },
+  );
 
   return <ViewResource resource={asset} isLoading={isLoading} />;
 }
--- extensions/cloudinary/src/upload-url.tsx
+++ extensions/cloudinary/src/upload-url.tsx
@@ -7,14 +7,18 @@ import ViewResource from "./components/ViewResource";
 import { showFailureToast, usePromise } from "@raycast/utils";
 
 export default function main(props: LaunchProps<{ arguments: Arguments.UploadUrl }>) {
-  const { isLoading, data: asset } = usePromise(async () => {
-    const resource = await uploadImage(props.arguments.url);
-    return resource as Asset;
-  }, [], {
-    onError() {
-      showFailureToast("Failed to upload url data to Cloudinary", { title: "Error" });
+  const { isLoading, data: asset } = usePromise(
+    async () => {
+      const resource = await uploadImage(props.arguments.url);
+      return resource as Asset;
     },
-  })
-  
+    [],
+    {
+      onError() {
+        showFailureToast("Failed to upload url data to Cloudinary", { title: "Error" });
+      },
+    },
+  );
+
   return <ViewResource resource={asset} isLoading={isLoading} />;
 }
