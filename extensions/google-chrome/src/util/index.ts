cbe30d361494fd462a90e2b2ea46f1879f6e9cd4
Santiago Perez <sperez@atlassian.com>
2025-01-13 11:07:00 +1100

    Improve message messed up after fixing linting

--- extensions/google-chrome/src/util/index.ts
+++ extensions/google-chrome/src/util/index.ts
@@ -28,7 +28,7 @@ const getChromeFilePath = (fileName: ChromeFile, profile?: string) => {
 
   if (!fs.existsSync(resolvedProfilePath)) {
     throw new Error(
-      `The path ${resolvedProfilePath} does not exist. Please check your Chrome profile location by visiting chrome://version -> Profile Path.`
+      `The profile path ${resolvedProfilePath} does not exist. Please check your Chrome profile location by visiting chrome://version -> Profile Path. Then update it in Extension Settings -> Profile Path.`
     );
   }
 
