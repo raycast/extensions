59c63aea4aec1b4dff50fe90047957dfa2d11dc3
Wyatt Johnson <accounts+github@wyattjoh.ca>
2025-01-16 14:28:19 -0700

    feat: added project dropdown

--- extensions/project-manager/src/search-projects.tsx
+++ extensions/project-manager/src/search-projects.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from "react";
+import { startTransition, useCallback, useMemo, useState } from "react";
 import { Color, getPreferenceValues, Keyboard } from "@raycast/api";
 import { ActionPanel, Action, Icon, List, showToast, Toast } from "@raycast/api";
 import { showFailureToast, useCachedPromise } from "@raycast/utils";
@@ -9,11 +9,16 @@ import { archiveProject } from "./lib/archive-project";
 import type { Project } from "./types/project";
 import { unarchiveProject } from "./lib/unarchive-project";
 
+type View = Preferences.SearchProjects["defaultView"];
+
 export default function Command() {
-  const { directory, openWith, excludePatterns } = getPreferenceValues<Preferences.SearchProjects>();
+  const { directory, openWith, excludePatterns, defaultView } = getPreferenceValues<Preferences.SearchProjects>();
   const { data, isLoading, revalidate } = useCachedPromise(getProjects, [directory, excludePatterns.split(",")], {
-    // keepPreviousData: true,
+    keepPreviousData: true,
   });
+
+  const [selectedItemId, setSelectedItemId] = useState<string | undefined>(undefined);
+  const [view, setView] = useState<View>(defaultView);
 
   const onUnarchiveProject = useCallback(
     async (project: Project) => {
@@ -27,6 +32,13 @@ export default function Command() {
 
         revalidate();
 
+        if (view === "archived") {
+          startTransition(() => {
+            setView("all");
+            setSelectedItemId(project.id);
+          });
+        }
+
         showToast({
           title: "Project unarchived",
           message: `Project ${project.filename} has been unarchived`,
@@ -35,7 +47,7 @@ export default function Command() {
         showFailureToast(err);
       }
     },
-    [revalidate],
+    [revalidate, view],
   );
 
   const onArchiveProject = useCallback(
@@ -50,6 +62,13 @@ export default function Command() {
 
         revalidate();
 
+        if (view === "active") {
+          startTransition(() => {
+            setView("all");
+            setSelectedItemId(project.id);
+          });
+        }
+
         showToast({
           title: "Project archived",
           message: `Project ${project.filename} has been archived to ${archivePath}`,
@@ -58,12 +77,34 @@ export default function Command() {
         showFailureToast(err);
       }
     },
-    [revalidate, excludePatterns],
+    [revalidate, excludePatterns, view],
   );
 
+  const filtered = useMemo(() => {
+    if (view === "all") {
+      return data;
+    }
+
+    if (view === "archived") {
+      return data?.filter((item) => item.archived);
+    }
+
+    return data?.filter((item) => !item.archived);
+  }, [data, view]);
+
   return (
-    <List isLoading={isLoading}>
-      {data?.map((item) => (
+    <List
+      isLoading={isLoading}
+      selectedItemId={selectedItemId}
+      searchBarAccessory={
+        <List.Dropdown tooltip="Dropdown With Items" onChange={(value) => setView(value as View)}>
+          <List.Dropdown.Item title="All" value="all" />
+          <List.Dropdown.Item title="Active" value="active" />
+          <List.Dropdown.Item title="Archived" value="archived" />
+        </List.Dropdown>
+      }
+    >
+      {filtered?.map((item) => (
         <List.Item
           key={item.id}
           title={item.filename}
