55f13b70d8913494c74eeca2e4204540dbd9234f
Wei GENG <wgeng6@gmail.com>
2025-01-16 14:21:38 +0100

    keep existing command names

--- extensions/raycast-gemini/src/translate.jsx
+++ extensions/raycast-gemini/src/translate.jsx
@@ -3,11 +3,11 @@ import useGemini from "./api/gemini";
 
 export default function Translate(props) {
   // we allow user to override the prompt of translation
-  const { forceLanguage } = props["arguments"];
-  let { prompt, firstTargetLanguage, secondTargetLanguage } = getPreferenceValues();
-  let prompts = forceLanguage
-    ? `Translate following text to ${forceLanguage}. ` + prompt
-    : `If the following text is in ${firstTargetLanguage} then translate it to ${secondTargetLanguage}, otherwise Translate following text to ${firstTargetLanguage}. ` +
+  const { TranslateLanguage } = props["arguments"];
+  let { prompt, defaultTargetLanguage, secondTargetLanguage } = getPreferenceValues();
+  let prompts = TranslateLanguage
+    ? `Translate following text to ${TranslateLanguage}. ` + prompt
+    : `If the following text is in ${defaultTargetLanguage} then translate it to ${secondTargetLanguage}, otherwise Translate following text to ${defaultTargetLanguage}. ` +
       prompt;
 
   return useGemini(props, {
