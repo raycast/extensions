f1c0c09bab1940b677598017cf75ba325bcf0c74
Kim Schneider <web@schneider.kim>
2025-01-17 18:13:13 +0100

    adjust copy

--- extensions/summarize-youtube-video-with-ai/src/summarizeVideo.tsx
+++ extensions/summarize-youtube-video-with-ai/src/summarizeVideo.tsx
@@ -3,13 +3,13 @@ import { Detail } from "@raycast/api";
 const markdown = `
 # The Extension has changed
 
-We introduced new commands to summarize YouTube videos with AI. The one you used to use is now deprecated and split into three. You can now run the following commands:
+We introduced new commands to summarize YouTube videos with AI. The one you are used to is now deprecated and split into three. You can run the following commands:
 
 - Summarize with RaycastAI: \`summarizeVideoWithRaycastAI\`
 - Summarize with OpenAI: \`summarizeVideoWithOpenAI\`
 - Summarize with Anthropic: \`summarizeVideoWithAnthropic\`
 
-You have to migrate your settings to the new commands. Open the extensions poreferences and update the command accordingly. You can find your old settings in the deprecated command.
+You have to migrate your settings to the new commands. Open the extensions preferences and update the command accordingly. You can find your old settings in the deprecated command.
 `;
 
 export default function DeprecationNote() {
