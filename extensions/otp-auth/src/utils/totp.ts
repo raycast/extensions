fe1db5244dad2ea92a0a52eee1471913c7cb5d08
neo773 <62795688+neo773@users.noreply.github.com>
2025-01-17 18:38:12 +0530

    Update totp.ts

--- extensions/otp-auth/src/utils/totp.ts
+++ extensions/otp-auth/src/utils/totp.ts
@@ -1,15 +1,15 @@
-import { Algorithm } from "decrypt-otpauth-ts/dist/decryptor";
-import { TOTP } from "totp-generator";
+import { Algorithm } from 'decrypt-otpauth-ts/dist/decryptor';
+import { TOTP } from 'totp-generator';
 
 const generateOtp = (secret: string, period: number, algorithm: Algorithm) => {
   const algorithmString =
     algorithm === Algorithm.SHA1
-      ? "SHA-1"
+      ? 'SHA-1'
       : algorithm === Algorithm.SHA256
-        ? "SHA-256"
+        ? 'SHA-256'
         : algorithm === Algorithm.SHA512
-          ? "SHA-512"
-          : "SHA-1";
+          ? 'SHA-512'
+          : 'SHA-1';
   return TOTP.generate(secret, {
     period: period,
     algorithm: algorithmString,
