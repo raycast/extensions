6056f47aca8433e8f0af06c632d560dae060fb84
moojing <starts789@gmail.com>
2025-01-18 10:32:34 +0000

    refactor: create useCryptoList hook.

--- extensions/coinmarketcap-crypto-crawler/src/index.tsx
+++ extensions/coinmarketcap-crypto-crawler/src/index.tsx
@@ -1,11 +1,10 @@
-import { showToast, Toast, List } from "@raycast/api";
-import { useState, useEffect, useMemo } from "react";
+import { List } from "@raycast/api";
+import { useState, useMemo } from "react";
 import fuzzysort from "fuzzysort";
-import fs from "fs";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 
-import { getListFromFile, CRYPTO_LIST_PATH, refreshExistingCache } from "./utils";
+import useCryptoList from "./utils/useCryptoList";
 import useCoinWatchList from "./utils/useCoinWatchList";
 import { CryptoCurrency } from "./types";
 import CoinListItem from "./components/CoinListItem";
@@ -15,7 +14,7 @@ dayjs.extend(relativeTime);
 
 export default function SearchCryptoList() {
   const [isLoading, setIsLoading] = useState(false);
-  const [cryptoList, setCryptoList] = useState<CryptoCurrency[]>([]);
+
   const [searchResult, setSearchResult] = useState<CryptoCurrency[]>([]);
 
   const [selectedSlug, setSelectedSlug] = useState<string>("");
@@ -23,50 +22,12 @@ export default function SearchCryptoList() {
   const { store: coinPriceStore, refresh: refreshCoinPrice } = useCoinPriceStore(selectedSlug);
 
   const { watchList, addToWatchList, removeFromWatchList, loading: favoriteLoading } = useCoinWatchList();
+  const { cryptoList } = useCryptoList();
 
   const favoriteCoinSlugs = useMemo(() => watchList.map(({ slug }) => slug), [watchList]);
   const displayedSearchResult = useMemo<CryptoCurrency[]>(() => {
     return searchResult.filter(({ slug }) => !favoriteCoinSlugs.includes(slug));
   }, [watchList, searchResult]);
-
-  useEffect(() => {
-    getListFromFile((err, data) => {
-      if (err) {
-        console.error("ReadListError:" + err);
-        return;
-      }
-
-      if (!data) {
-        // fetch crypto list mapping if there's no data exist in the local file
-        // the api has an limit num per request.
-        refreshExistingCache((err, cryptoList) => {
-          if (err) {
-            console.error("WriteFileError:" + err);
-            showToast(Toast.Style.Failure, "Refresh failed", (err as Error)?.message);
-            return;
-          }
-
-          setCryptoList(cryptoList);
-        });
-      } else {
-        const now = dayjs();
-        const { cryptoList: cryptoListFromFile, timestamp } = JSON.parse(data);
-        const fileCachedTimeDiff = now.diff(dayjs(timestamp), "day");
-
-        //Remove cache file if it has been more than 15 days since last time saved.
-        if (fileCachedTimeDiff >= 15) {
-          fs.unlink(CRYPTO_LIST_PATH, (err) => {
-            if (err) throw err;
-            console.log("Crypto list cache has been cleared.");
-          });
-        }
-
-        if (cryptoListFromFile) {
-          setCryptoList(cryptoListFromFile);
-        }
-      }
-    });
-  }, []);
 
   const onSearchChange = (search: string) => {
     setIsLoading(true);
