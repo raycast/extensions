5f9a418910358fa18f8f142588e228f03f9efe6e
Aleksey Petrunnik <petrunnik.a@gmail.com>
2025-01-19 23:25:24 +0200

    Remove pre-created links to AI commands

--- extensions/chatgpt/src/fix-spelling-and-grammar.tsx
+++
@@ -1,7 +0,0 @@
-import CommandView from "./views/command/command-view";
-import { FIX_SPELLING_AND_GRAMMAR_COMMAND_ID } from "./hooks/useCommand";
-import { LaunchProps } from "@raycast/api";
-
-export default function FixSpellingAndGrammar(props: LaunchProps) {
-  return <CommandView {...props} launchContext={{ commandId: FIX_SPELLING_AND_GRAMMAR_COMMAND_ID }} />;
-}
--- extensions/chatgpt/src/improve-writing.tsx
+++
@@ -1,7 +0,0 @@
-import CommandView from "./views/command/command-view";
-import { IMPROVE_WRITING_COMMAND_ID } from "./hooks/useCommand";
-import { LaunchProps } from "@raycast/api";
-
-export default function ImproveWriting(props: LaunchProps) {
-  return <CommandView {...props} launchContext={{ commandId: IMPROVE_WRITING_COMMAND_ID }} />;
-}
