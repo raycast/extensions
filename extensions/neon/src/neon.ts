f268715d9b22bb84ff60310e47ce61cb8e80dca6
xmok <30526133+xmok@users.noreply.github.com>
2025-01-20 15:36:24 +0500

    [Neon] add metadata images + ye olde lint

--- extensions/neon/src/neon.ts
+++ extensions/neon/src/neon.ts
@@ -2,8 +2,11 @@ import { createApiClient } from "@neondatabase/api-client";
 import { getPreferenceValues } from "@raycast/api";
 
 export const neon = createApiClient({
-    apiKey: getPreferenceValues<Preferences>().api_key,
+  apiKey: getPreferenceValues<Preferences>().api_key,
 });
-neon.instance.interceptors.response.use(res => res, err => {
-    return Promise.reject(err.response.data.message)
-})
+neon.instance.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    return Promise.reject(err.response.data.message);
+  },
+);
