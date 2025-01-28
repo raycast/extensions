7d70122fda8717e24a66e02b359b4f8faa8f0d9e
Maximilian <85677120+maximilianzuern@users.noreply.github.com>
2025-01-28 12:18:15 +0100

    [perplexity-api] Updated models + added citations (#16656)
    * updated models
    
    * Reorder model entries in allModels for consistency
    
    * Update dependencies in perplexity-api package.json
    
    * simplified code
    
    * Add citation handling + extend OpenAI types
    
    * removed deprecated llama 3.3
    
    * updated changelog
    
    * Update perplexity.types.ts
    
    * Update perplexity.types.ts
    
    * fixed namespace lint issue
    
    * updated icon
    
    * updated screenshots
    
    * Update CHANGELOG.md and optimise images
    
    ---------
    
    Co-authored-by: Per Nielsen Tikær <per@raycast.com>
    Co-authored-by: raycastbot <bot@raycast.com>

--- extensions/perplexity-api/src/hook/utils.ts
+++ extensions/perplexity-api/src/hook/utils.ts
@@ -2,12 +2,8 @@ import { encode } from "gpt-tokenizer";
 
 export const allModels = [
   { name: "Follow global model", id: "global" },
-  { name: "Llama 3.3 70B 128k", id: "llama-3.3-70b-instruct" },
-  { name: "Sonar Large 70B 128k", id: "llama-3.1-sonar-large-128k-chat" },
-  { name: "Sonar Small 8B 128k", id: "llama-3.1-sonar-small-128k-chat" },
-  { name: "Sonar Huge 405B Online", id: "llama-3.1-sonar-huge-128k-online" },
-  { name: "Sonar Large 70B Online", id: "llama-3.1-sonar-large-128k-online" },
-  { name: "Sonar Small 8B Online", id: "llama-3.1-sonar-small-128k-online" },
+  { name: "Sonar 128k", id: "sonar" },
+  { name: "Sonar Pro 200k", id: "sonar-pro" },
 ];
 
 // format: Wednesday, April 24, 2024 at 5:14:26 PM GMT+2.
@@ -28,21 +24,11 @@ export function countToken(content: string) {
 export function estimatePrice(prompt_token: number, output_token: number, model: string) {
   let price = 0;
   switch (model) {
-    case "llama-3.1-sonar-small-128k-chat":
-      price = ((prompt_token * 0.2) / 1_000_000 + (output_token * 0.2) / 1_000_000) * 100;
-      break;
-    case "llama-3.1-sonar-large-128k-chat":
-    case "llama-3.3-70b-instruct":
-      price = ((prompt_token * 1) / 1_000_000 + (output_token * 1) / 1_000_000) * 100;
-      break;
-    case "llama-3.1-sonar-small-128k-online":
-      price = (5 / 1000 + (prompt_token * 0.2) / 1_000_000 + (output_token * 0.2) / 1_000_000) * 100;
-      break;
-    case "llama-3.1-sonar-large-128k-online":
+    case "sonar":
       price = (5 / 1000 + (prompt_token * 1) / 1_000_000 + (output_token * 1) / 1_000_000) * 100;
       break;
-    case "llama-3.1-sonar-huge-128k-online":
-      price = (5 / 1000 + (prompt_token * 5) / 1_000_000 + (output_token * 5) / 1_000_000) * 100;
+    case "sonar-pro":
+      price = (5 / 1000 + (prompt_token * 3) / 1_000_000 + (output_token * 15) / 1_000_000) * 100;
       break;
   }
   return naiveRound(price, 5);
