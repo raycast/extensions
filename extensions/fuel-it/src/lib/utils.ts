7af442b7a1b2925bb6a93195e557922f3aee094f
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2025-01-29 16:29:44 +0530

    Update fuel-it extension
    - add metadata
    - add metadata screenshots
    - update icon name
    - make requested changes

--- extensions/fuel-it/src/lib/utils.ts
+++ extensions/fuel-it/src/lib/utils.ts
@@ -1,5 +1,3 @@
-import * as dotenv from "dotenv";
-
 export const TESTNET_URL = "https://testnet.fuel.network/v1/graphql";
 export const MAINNET_URL = "https://mainnet.fuel.network/v1/graphql";
 
@@ -7,17 +5,4 @@ type Network = "testnet" | "mainnet";
 
 export const getFuelUrl = (network: Network) => {
   return network === "testnet" ? TESTNET_URL : MAINNET_URL;
-};
-
-interface EnvVars {
-  OPENAI_API_KEY: string;
-  FUEL_WALLET_PRIVATE_KEY: string;
-}
-
-export const getEnvVars = async (): Promise<EnvVars> => {
-  dotenv.config();
-  return {
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
-    FUEL_WALLET_PRIVATE_KEY: process.env.FUEL_WALLET_PRIVATE_KEY || "",
-  };
 };
