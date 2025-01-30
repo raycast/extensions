204bf96638c22e0cc3340c982264af2e9b12d5ad
Stephen Kaplan <s.kaplan@sdf.org>
2025-01-29 21:58:06 -0500

    Add new filters, bug fixes
    Add 28 new filters (mostly distortions) and improve memory management & error handling.

--- extensions/sips/src/apply-filter.tsx
+++ extensions/sips/src/apply-filter.tsx
@@ -5,72 +5,96 @@
  * @author Stephen Kaplan <skaplanofficial@gmail.com>
  *
  * Created at     : 2023-07-06 14:52:33
- * Last modified  : 2024-06-26 21:37:46
  */
 
-import { Action, ActionPanel, Grid } from "@raycast/api";
+import { statSync } from "fs";
+import { useRef, useState } from "react";
+
+import { Action, ActionPanel, getPreferenceValues, Grid, Icon, Image } from "@raycast/api";
 
 import applyFilter from "./operations/filterOperation";
-import { filters, getFilterThumbnail } from "./utilities/filters";
+import runOperation from "./operations/runOperation";
+import { getActiveFilters, getFilterThumbnail } from "./utilities/filters";
 import { Filter } from "./utilities/types";
 import { cleanup, getSelectedImages } from "./utilities/utils";
-import { useState } from "react";
-import runOperation from "./operations/runOperation";
+
+type FilterGridItemProps = {
+  filter: Filter;
+  content?: Image.ImageLike;
+};
+
+function FilterGridItem({ filter, content }: FilterGridItemProps) {
+  return (
+    <Grid.Item
+      title={filter.name}
+      id={filter.CIFilterName}
+      accessory={{ icon: { source: Icon.Info }, tooltip: filter.description }}
+      subtitle={filter.category}
+      key={filter.CIFilterName}
+      content={content || { source: filter.thumbnail }}
+      actions={
+        <ActionPanel>
+          <Action
+            title={`Apply ${filter.name} Filter`}
+            onAction={async () => {
+              const selectedImages = await getSelectedImages();
+              await runOperation({
+                operation: () => applyFilter(selectedImages, filter),
+                selectedImages,
+                inProgressMessage: "Filtering in progress...",
+                successMessage: "Applied filter to",
+                failureMessage: "Failed to apply filter to",
+              });
+            }}
+          />
+        </ActionPanel>
+      }
+    />
+  );
+}
 
 export default function Command() {
   const [selectedFilter, setSelectedFilter] = useState<Filter>();
   const [preview, setPreview] = useState<string>("");
-
-  const gridItems = filters.map((filter) => {
-    const isSelected = selectedFilter?.name === filter.name;
-    const itemContent = { source: isSelected ? (preview == "" ? filter.thumbnail : preview) : filter.thumbnail };
-    return (
-      <Grid.Item
-        title={filter.name}
-        id={filter.name}
-        subtitle={filter.description}
-        key={filter.name}
-        content={itemContent}
-        actions={
-          <ActionPanel>
-            <Action
-              title={`Apply ${filter.name} Filter`}
-              onAction={async () => {
-                const selectedImages = await getSelectedImages();
-                await runOperation({
-                  operation: () => applyFilter(selectedImages, filter),
-                  selectedImages,
-                  inProgressMessage: "Filtering in progress...",
-                  successMessage: "Applied filter to",
-                  failureMessage: "Failed to apply filter to",
-                });
-              }}
-            />
-          </ActionPanel>
-        }
-      />
-    );
-  });
-
+  const previewQueue = useRef([] as string[]);
+  const preferences = getPreferenceValues<Preferences.ApplyFilter>();
   return (
     <Grid
       searchBarPlaceholder="Search filters..."
-      throttle={true}
       onSelectionChange={async (id) => {
-        const filter = filters.find((filter) => filter.name === id);
-        if (filter && filter.name !== selectedFilter?.name) {
-          setPreview("");
-          setSelectedFilter(filter);
-          const selection = await getSelectedImages();
-          if (selection.length > 0 && selection[0].trim() !== "") {
-            const preview = await getFilterThumbnail(filter, selection[0]);
-            setPreview(preview);
-          }
-          await cleanup();
+        if (!preferences.showPreviews || !id) {
+          return;
         }
+        previewQueue.current.push(id);
+        setTimeout(async () => {
+          if (previewQueue.current.at(-1) == id) {
+            const filter = getActiveFilters().find((filter) => filter.CIFilterName === id);
+            if (filter && filter.name !== selectedFilter?.name) {
+              setPreview("");
+              setSelectedFilter(filter);
+              const filePath = (await getSelectedImages()).at(0);
+              if (filePath && filePath.trim() !== "") {
+                if (statSync(filePath).size < 800000) {
+                  const preview = await getFilterThumbnail(filter, filePath);
+                  setPreview(preview);
+                }
+              }
+              await cleanup();
+            }
+          }
+          previewQueue.current = previewQueue.current.filter((item) => item !== id);
+        }, 500);
       }}
     >
-      {gridItems}
+      {getActiveFilters().map((filter) => {
+        return (
+          <FilterGridItem
+            key={filter.CIFilterName}
+            filter={filter}
+            content={selectedFilter?.name === filter.name ? preview : undefined}
+          />
+        );
+      })}
     </Grid>
   );
 }
