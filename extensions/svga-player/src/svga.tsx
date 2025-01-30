d2aacfd1ec108fd02ea1fb913b6941f49ee97eab
Liaoyuan <stu0615@163.com>
2025-01-30 14:26:21 +0800

    Update extensions/svga-player/src/svga.tsx
    Co-authored-by: LitoMore <LitoMore@users.noreply.github.com>

--- extensions/svga-player/src/svga.tsx
+++ extensions/svga-player/src/svga.tsx
@@ -52,10 +52,10 @@ export default async function Main() {
           throw new Error("Not a valid SVGA file");
         }
       }
-    } catch (error: any) {
+    } catch (error) {
       showToast({
         style: Toast.Style.Failure,
-        title: error.message || "Failed to read current focus",
+        title: error instanceof Error ? error.message : "Failed to read current focus",
       });
       return true;
     }
