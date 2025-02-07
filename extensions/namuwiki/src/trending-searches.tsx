8ee267681b48ae1c928e2fdc17054f883a8a91f1
Song Jonggil <contact@123p.me>
2025-02-07 12:39:36 +0900

    feat: Initial Commit

---
+++ extensions/namuwiki/src/trending-searches.tsx
@@ -0,0 +1,41 @@
+import { ActionPanel, Action, List } from "@raycast/api";
+import { useFetch } from "@raycast/utils";
+
+async function parseFetchResponse(response: Response) {
+  const json = (await response.json()) as string[];
+
+  return json.map((result) => ({
+    rank: json.indexOf(result) + 1,
+    document: result,
+    url: `https://namu.wiki/w/${encodeURIComponent(result)}`,
+  })) as SearchResult[];
+}
+
+export default function Command() {
+  const { data = [] } = useFetch("https://search.namu.wiki/api/ranking", { parseResponse: parseFetchResponse });
+
+  return (
+    <List>
+      <List.Section title="Current Trending Documents" subtitle={new Date().toLocaleString()}>
+        {data.map((item) => (
+          <List.Item
+            key={item.document}
+            title={item.document}
+            accessories={[{ text: item.rank.toString() }]}
+            actions={
+              <ActionPanel>
+                <Action.OpenInBrowser url={item.url} />
+              </ActionPanel>
+            }
+          />
+        ))}
+      </List.Section>
+    </List>
+  );
+}
+
+interface SearchResult {
+  rank: number;
+  document: string;
+  url: string;
+}
