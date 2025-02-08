41581df553d51a08be50d483c33abb5cfb15b388
Chad Walters <chad.walters@gmail.com>
2025-02-08 12:59:02 -0600

    fix(harmony-control): disable ban-types rule for generated file

--- extensions/harmony-hub-control/src/services/harmony/harmonyMachine.typegen.ts
+++ extensions/harmony-hub-control/src/services/harmony/harmonyMachine.typegen.ts
@@ -1,4 +1,6 @@
 /* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable @typescript-eslint/ban-types */
 // This file was automatically generated. Edits will be overwritten
 
 export interface Typegen0 {
