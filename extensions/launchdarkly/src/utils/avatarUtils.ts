dc427f82d962397f8bf62627fe25da61a33e340e
Raul Martin <luarmr@gmail.com>
2025-02-08 17:41:19 -0800

    unecesary hassle on getFullName

--- extensions/launchdarkly/src/utils/avatarUtils.ts
+++ extensions/launchdarkly/src/utils/avatarUtils.ts
@@ -2,7 +2,7 @@ import { LDMaintainer } from "../types";
 
 export const getFullName = (maintainer: LDMaintainer): string => {
   if (maintainer.firstName || maintainer.lastName) {
-    return `${maintainer.firstName?.charAt(0) ?? ""} ${maintainer.lastName?.charAt(0) ?? ""}`.toUpperCase().trim();
+    return `${maintainer.firstName ?? ""} ${maintainer.lastName ?? ""}`.trim();
   }
-  return maintainer.email.charAt(0).toUpperCase();
+  return maintainer.email;
 };
