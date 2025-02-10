b512029786ef35fef45e6f6616f64e3a81971cf9
LitoMore <LitoMore@users.noreply.github.com>
2025-02-10 21:05:39 +0800

    [Raycast Port] Add port for Browser Extension (#16817)
    * [Raycast Port] Add port for Browser Extension
    
    * Update CHANGELOG.md and optimise images
    
    ---------
    
    Co-authored-by: raycastbot <bot@raycast.com>

--- extensions/raycast-port/src/browser-extension.tsx
+++ extensions/raycast-port/src/browser-extension.tsx
@@ -1,41 +1,50 @@
-import { AI, getPreferenceValues, open } from "@raycast/api";
 import { execSync } from "node:child_process";
+import { BrowserExtension, getPreferenceValues, open } from "@raycast/api";
 import { LaunchOptions, callbackLaunchCommand } from "raycast-cross-extension";
 import { appendError } from "./utils.js";
 
 type LaunchContext = {
-  askPrompt?: string;
-  askOptions?: AI.AskOptions;
+  getContent?: boolean | Parameters<typeof BrowserExtension.getContent>;
+  getTabs?: boolean;
   callbackOpen?: Parameters<typeof open>;
   callbackExec?: Parameters<typeof execSync>;
   callbackLaunchOptions?: LaunchOptions;
 };
 
 type Result = {
-  answer?: string;
+  content?: string;
+  tabs?: string | Awaited<ReturnType<typeof BrowserExtension.getTabs>>;
   errors?: string[];
 };
 
 export default async function Api({ launchContext = {} }: { launchContext: LaunchContext }) {
-  const { askPrompt, askOptions, callbackOpen, callbackExec, callbackLaunchOptions } = launchContext;
+  const { getContent = true, getTabs, callbackOpen, callbackExec, callbackLaunchOptions } = launchContext;
   const { enableExecuteShellSupport } = getPreferenceValues<ExtensionPreferences>();
-  if (!askPrompt) return;
 
   const result: Result = {};
 
-  try {
-    const answer = await AI.ask(askPrompt, askOptions);
-    result.answer = answer;
-  } catch (error) {
-    appendError(result, "AI.ask()", error);
+  if (getContent) {
+    try {
+      const content = await BrowserExtension.getContent(...(getContent === true ? [] : getContent));
+      result.content = content;
+    } catch (error) {
+      appendError(result, "BrowserExtension.getContent()", error);
+    }
+  }
+
+  if (getTabs) {
+    try {
+      const tabs = await BrowserExtension.getTabs();
+      result.tabs = tabs;
+    } catch (error) {
+      appendError(result, "BrowserExtension.getTabs()", error);
+    }
   }
 
   if (callbackOpen) {
     try {
       await open(
-        callbackOpen[0]
-          .replace("RAYCAST_PORT_AI_ANSWER", encodeURIComponent(result.answer || "")) // [Deprecated]
-          .replace("RAYCAST_PORT_AI_ASK_RESULT", encodeURIComponent(JSON.stringify(result))),
+        callbackOpen[0].replace("RAYCAST_PORT_BROWSER_EXTENSION_RESULT", encodeURIComponent(JSON.stringify(result))),
         callbackOpen[1],
       );
     } catch (error) {
@@ -46,8 +55,7 @@ export default async function Api({ launchContext = {} }: { launchContext: Launc
   if (callbackExec) {
     if (enableExecuteShellSupport) {
       try {
-        process.env.RAYCAST_PORT_AI_ANSWER = result.answer || ""; // [Deprecated]
-        process.env.RAYCAST_PORT_AI_ASK_RESULT = JSON.stringify(result);
+        process.env.RAYCAST_PORT_BROWSER_EXTENSION_RESULT = JSON.stringify(result);
         execSync(...callbackExec);
       } catch (error) {
         appendError(result, "execSync()", error);
@@ -59,10 +67,7 @@ export default async function Api({ launchContext = {} }: { launchContext: Launc
 
   if (callbackLaunchOptions) {
     try {
-      await callbackLaunchCommand(callbackLaunchOptions, {
-        answer: result.answer, // [Deprecated]
-        result,
-      });
+      await callbackLaunchCommand(callbackLaunchOptions, { result });
     } catch (error) {
       appendError(result, "callbackLaunchCommand()", error);
     }
