0587a0dbfc7b97c62868de4ab94ac994d2ecb198
xmok <30526133+xmok@users.noreply.github.com>
2025-02-11 00:51:59 +0500

    [OCI] do instance actions

--- extensions/oci/CHANGELOG.md
+++ extensions/oci/CHANGELOG.md
@@ -1,8 +1,13 @@
 # Oracle Cloud Changelog
 
-## [] - {PR_MERGE_DATE}
+## [Perform Instance Actions] - {PR_MERGE_DATE}
 
+- Add instance actions:
+    - Reset
+    - Start
+    - Stop
 - Show a proper `Detail` view with information when "provider" fails. Previously, extension would crash (ref: Issue #).
+- Show instance "Created" `Accessory`
 
 ## [Initial Version] - 2024-12-20
 
--- extensions/oci/src/core.tsx
+++ extensions/oci/src/core.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable @raycast/prefer-title-case */
-import { Action, ActionPanel, Color, Detail, Icon, List } from "@raycast/api";
+import { Action, ActionPanel, Color, Detail, Icon, List, showToast, Toast } from "@raycast/api";
 import { getFavicon, showFailureToast, useCachedPromise, useCachedState } from "@raycast/utils";
 import common = require("oci-common");
 import * as core from "oci-core";
 import { Instance } from "oci-core/lib/model";
 import { mapObjectToMarkdownTable } from "./utils";
+import { InstanceActionRequest } from "oci-core/lib/request";
 
 const onError = (error: Error) => {
   const err = error.message as string | common.OciError;
@@ -16,19 +17,36 @@ const onError = (error: Error) => {
 export default function CheckProvider() {
   try {
     const provider = new common.ConfigFileAuthenticationDetailsProvider();
-    if (!provider) return <Detail isLoading />
-    return <Core provider={provider} />
+    if (!provider) return <Detail isLoading />;
+    return <Core provider={provider} />;
   } catch (error) {
-    return <Detail navigationTitle="Oracle Cloud - Provider Error" markdown={`## Error: \n\n Can't load the default config from \`~/.oci/config\` or \`~/.oraclebmc/config\` because it does not exists or it's not a file. For more info about config file and how to get required information, see https://docs.oracle.com/en-us/iaas/Content/API/Concepts/sdkconfig.htm for more info on OCI configuration files. \n\n > TIP: Check extension README!`} actions={<ActionPanel>
-      <Action.OpenInBrowser icon={getFavicon("https://docs.oracle.com/en-us/iaas/Content/API/Concepts/sdkconfig.htm", { fallback: Icon.Globe })} url="https://docs.oracle.com/en-us/iaas/Content/API/Concepts/sdkconfig.htm" />
-    </ActionPanel>} />
+    return (
+      <Detail
+        navigationTitle="Oracle Cloud - Provider Error"
+        markdown={`## Error: \n\n Can't load the default config from \`~/.oci/config\` or \`~/.oraclebmc/config\` because it does not exists or it's not a file. For more info about config file and how to get required information, see https://docs.oracle.com/en-us/iaas/Content/API/Concepts/sdkconfig.htm for more info on OCI configuration files. \n\n > TIP: Check extension README!`}
+        actions={
+          <ActionPanel>
+            <Action.OpenInBrowser
+              icon={getFavicon("https://docs.oracle.com/en-us/iaas/Content/API/Concepts/sdkconfig.htm", {
+                fallback: Icon.Globe,
+              })}
+              url="https://docs.oracle.com/en-us/iaas/Content/API/Concepts/sdkconfig.htm"
+            />
+          </ActionPanel>
+        }
+      />
+    );
   }
 }
 
 function Core({ provider }: { provider: common.ConfigFileAuthenticationDetailsProvider }) {
   const [isShowingDetail, setIsShowingDetail] = useCachedState("show-instance-details", false);
-  
-  const { isLoading, data: instances } = useCachedPromise(
+
+  const {
+    isLoading,
+    data: instances,
+    mutate,
+  } = useCachedPromise(
     async () => {
       const computeClient = new core.ComputeClient({ authenticationDetailsProvider: provider });
       const instances = await computeClient.listInstances({ compartmentId: provider.getTenantId() });
@@ -46,6 +64,7 @@ function Core({ provider }: { provider: common.ConfigFileAuthenticationDetailsPr
         return Color.Yellow;
       case core.models.Instance.LifecycleState.Provisioning:
         return Color.Blue;
+      case core.models.Instance.LifecycleState.Starting:
       case core.models.Instance.LifecycleState.Stopping:
       case core.models.Instance.LifecycleState.Terminating:
         return Color.Orange;
@@ -56,10 +75,31 @@ function Core({ provider }: { provider: common.ConfigFileAuthenticationDetailsPr
     }
   }
 
+  async function doInstanceAction(instance: Instance, action: InstanceActionRequest["action"]) {
+    const toast = await showToast(Toast.Style.Animated, action, instance.displayName ?? "");
+    try {
+      const computeClient = new core.ComputeClient({ authenticationDetailsProvider: provider });
+      await mutate(
+        computeClient.instanceAction({
+          instanceId: instance.id,
+          action,
+        }),
+      );
+      toast.style = Toast.Style.Success;
+      toast.title = `${action} ✅`;
+    } catch (error) {
+      toast.style = Toast.Style.Failure;
+      toast.title = `${action} ❌`;
+      toast.message = `${error}`;
+    }
+  }
+
   return (
     <List isLoading={isLoading} isShowingDetail={isShowingDetail} searchBarPlaceholder="Search instances">
       {instances.map((instance) => {
-        const accessories: List.Item.Accessory[] = [];
+        const accessories: List.Item.Accessory[] = [
+          { date: new Date(instance.timeCreated), tooltip: `Created ${new Date(instance.timeCreated).toString()}` },
+        ];
         if (instance.systemTags && instance.systemTags["orcl-cloud"]?.["free-tier-retained"] === "true")
           accessories.push({ tag: "Always Free" });
 
@@ -68,7 +108,7 @@ function Core({ provider }: { provider: common.ConfigFileAuthenticationDetailsPr
         return (
           <List.Item
             key={instance.id}
-            icon={{ source: Icon.CircleFilled, tintColor: getInstanceColor(instance.lifecycleState) }}
+            icon={{ value: {source: Icon.CircleFilled, tintColor: getInstanceColor(instance.lifecycleState)}, tooltip: instance.lifecycleState}}
             title={instance.displayName ?? ""}
             subtitle={isShowingDetail ? undefined : instance.shape}
             accessories={isShowingDetail ? undefined : accessories}
@@ -105,6 +145,17 @@ function Core({ provider }: { provider: common.ConfigFileAuthenticationDetailsPr
                   title="View VNIC Attachments"
                   target={<ListInstanceVnicAttachments instanceId={instance.id} provider={provider} />}
                 />
+                <ActionPanel.Section>
+                  {instance.lifecycleState === Instance.LifecycleState.Running && (
+                    <>
+                      <Action icon={Icon.Redo} title="Reboot" onAction={() => doInstanceAction(instance, "RESET")} />
+                      <Action icon={Icon.Stop} title="Stop" onAction={() => doInstanceAction(instance, "STOP")} />
+                    </>
+                  )}
+                  {instance.lifecycleState === Instance.LifecycleState.Stopped && (
+                    <Action icon={Icon.Play} title="Start" onAction={() => doInstanceAction(instance, "START")} />
+                  )}
+                </ActionPanel.Section>
               </ActionPanel>
             }
           />
@@ -114,7 +165,13 @@ function Core({ provider }: { provider: common.ConfigFileAuthenticationDetailsPr
   );
 }
 
-function ListInstanceVnicAttachments({ instanceId, provider }: { instanceId: string, provider: common.ConfigFileAuthenticationDetailsProvider }) {
+function ListInstanceVnicAttachments({
+  instanceId,
+  provider,
+}: {
+  instanceId: string;
+  provider: common.ConfigFileAuthenticationDetailsProvider;
+}) {
   const { isLoading, data: VNICs } = useCachedPromise(
     async () => {
       const computeClient = new core.ComputeClient({ authenticationDetailsProvider: provider });
@@ -136,7 +193,11 @@ function ListInstanceVnicAttachments({ instanceId, provider }: { instanceId: str
           actions={
             <ActionPanel>
               {vnic.vnicId && (
-                <Action.Push icon={Icon.Eye} title="View VNIC" target={<ViewVnic vnicId={vnic.vnicId} provider={provider} />} />
+                <Action.Push
+                  icon={Icon.Eye}
+                  title="View VNIC"
+                  target={<ViewVnic vnicId={vnic.vnicId} provider={provider} />}
+                />
               )}
             </ActionPanel>
           }
@@ -146,7 +207,7 @@ function ListInstanceVnicAttachments({ instanceId, provider }: { instanceId: str
   );
 }
 
-function ViewVnic({ vnicId, provider }: { vnicId: string, provider: common.ConfigFileAuthenticationDetailsProvider }) {
+function ViewVnic({ vnicId, provider }: { vnicId: string; provider: common.ConfigFileAuthenticationDetailsProvider }) {
   const { isLoading, data: vnic } = useCachedPromise(
     async () => {
       const vnicClient = new core.VirtualNetworkClient({ authenticationDetailsProvider: provider });
