53d8e3ae73629020e19ebc63cc2991f8522edae2
Liam Neville <lineville@github.com>
2025-02-11 10:55:57 -0500

    Implement skip 15 seconds, back 15 seconds

---
+++ extensions/spotify-player/src/back15.ts
@@ -0,0 +1,29 @@
+import { showHUD } from "@raycast/api";
+import { setSpotifyClient } from "./helpers/withSpotifyClient";
+import { getCurrentlyPlaying } from "./api/getCurrentlyPlaying";
+import { safeLaunchCommandInBackground } from "./helpers/safeCommandLauncher";
+import { seek } from "./api/seek";
+
+export default async function Command() {
+  await setSpotifyClient();
+
+  const currentlyPlayingData = await getCurrentlyPlaying();
+  const nothingIsPlaying = !currentlyPlayingData || !currentlyPlayingData.item;
+
+  if (nothingIsPlaying) {
+    return await showHUD("Nothing is currently playing");
+  }
+
+  if (currentlyPlayingData.currently_playing_type === "track") {
+    return await showHUD("Can't skip in a song, only available for episodes");
+  }
+
+  try {
+    const currentPositionSeconds = (currentlyPlayingData?.progress_ms || 0) / 1000;
+    await seek(Math.max(currentPositionSeconds - 15, 0));
+    await showHUD("Skipped back 15 seconds");
+    await safeLaunchCommandInBackground("current-track");
+  } catch (error) {
+    await showHUD("Nothing is currently playing");
+  }
+}
