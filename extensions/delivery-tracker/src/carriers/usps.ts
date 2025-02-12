3a65e93ab9197868a3160d7909b5dd42c28ae1ba
halprin <halprin@users.noreply.github.com>
2025-02-11 20:02:54 -0700

    Add delivery-tracker extension

---
+++ extensions/delivery-tracker/src/carriers/usps.ts
@@ -0,0 +1,27 @@
+import { Package } from "../package";
+import { Delivery } from "../delivery";
+
+export async function ableToTrackUspsRemotely(): Promise<boolean> {
+  // doesn't support remote tracking yet.
+  return false;
+}
+
+export function urlToUspsTrackingWebpage(delivery: Delivery): string {
+  return `https://tools.usps.com/go/TrackConfirmAction_input?qtc_tLabels1=${delivery.trackingNumber}`;
+}
+
+export async function updateUspsTracking(delivery: Delivery): Promise<Package[]> {
+  const trackingNumber = delivery.trackingNumber;
+
+  console.log(`Updating tracking for ${trackingNumber}`);
+
+  console.log(`Updated tracking for ${trackingNumber}`);
+
+  return [
+    {
+      delivered: new Date() === delivery.manualDeliveryDate,
+      deliveryDate: delivery.manualDeliveryDate,
+      activity: [],
+    },
+  ];
+}
