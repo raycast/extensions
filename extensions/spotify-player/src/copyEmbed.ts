ea43246b7983bd9660d94d97bcccfee60af1316d
Daniel Andersson <dandersson23@gmail.com>
2025-02-17 00:30:01 +0100

    style: apply linting fixes to copyEmbed command

--- extensions/spotify-player/src/copyEmbed.ts
+++ extensions/spotify-player/src/copyEmbed.ts
@@ -3,27 +3,22 @@ import { setSpotifyClient } from "./helpers/withSpotifyClient";
 import { getCurrentlyPlaying } from "./api/getCurrentlyPlaying";
 
 export default async function Command() {
-    await setSpotifyClient();
+  await setSpotifyClient();
 
-    const currentlyPlayingData = await getCurrentlyPlaying();
-    const nothingIsPlaying = !currentlyPlayingData ||
-        !currentlyPlayingData?.item;
+  const currentlyPlayingData = await getCurrentlyPlaying();
+  const nothingIsPlaying = !currentlyPlayingData || !currentlyPlayingData?.item;
 
-    if (nothingIsPlaying) {
-        return await showHUD("Nothing is currently playing");
-    }
+  if (nothingIsPlaying) {
+    return await showHUD("Nothing is currently playing");
+  }
 
-    const external_urls = currentlyPlayingData.item.external_urls;
-    const spotifyUrl = external_urls?.spotify;
+  const external_urls = currentlyPlayingData.item.external_urls;
+  const spotifyUrl = external_urls?.spotify;
 
-    const embedUrl = spotifyUrl?.replace(
-        "open.spotify.com/",
-        "open.spotify.com/embed/",
-    );
+  const embedUrl = spotifyUrl?.replace("open.spotify.com/", "open.spotify.com/embed/");
 
-    const embedCode =
-        `<iframe style="border-radius:12px" src="${embedUrl}?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`;
+  const embedCode = `<iframe style="border-radius:12px" src="${embedUrl}?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`;
 
-    await Clipboard.copy(embedCode);
-    return await showHUD("Copied embed code to clipboard");
+  await Clipboard.copy(embedCode);
+  return await showHUD("Copied embed code to clipboard");
 }
