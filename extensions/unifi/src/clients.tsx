8d279913ec0f758252fd9ad95ee9cbf3a6ce96eb
lewjuh <lewj92@gmail.com>
2025-02-17 11:01:04 +0000

    chore: remove dead files

--- extensions/unifi/src/clients.tsx
+++
@@ -1,6 +0,0 @@
-import { SelectSite } from "./components/site/list";
-import { SiteClientsList } from "./components/client/list";
-
-export default function Command() {
-  return <SelectSite onSiteSelectAction={(site) => <SiteClientsList site={site} />} />;
-}
--- extensions/unifi/src/devices.tsx
+++
@@ -1,6 +0,0 @@
-import { SelectSite } from "./components/site/list";
-import { SiteDevicesList } from "./components/device/list";
-
-export default function Command() {
-  return <SelectSite onSiteSelectAction={(site) => <SiteDevicesList site={site} />} />;
-}
