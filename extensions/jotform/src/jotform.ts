6ab3fc47c53dcae111489aeb77f9a1a94978a423
xmok <30526133+xmok@users.noreply.github.com>
2025-02-22 07:22:40 +0500

    [Jotform] modify submissions format + lint

--- extensions/jotform/src/jotform.ts
+++ extensions/jotform/src/jotform.ts
@@ -4,19 +4,20 @@ import { ErrorResponse, SuccessResponse } from "./types";
 
 const { api_key } = getPreferenceValues<Preferences>();
 
-export const useJotform = <T>(endpoint: string) => useFetch("https://api.jotform.com/" + endpoint, {
+export const useJotform = <T>(endpoint: string) =>
+  useFetch("https://api.jotform.com/" + endpoint, {
     headers: {
-        Accept: "application/json",
-        APIKEY: api_key
+      Accept: "application/json",
+      APIKEY: api_key,
     },
     async parseResponse(response) {
-        const result = await response.json();
-        if (!response.ok) throw new Error((result as ErrorResponse).message || response.statusText);
-        return result as SuccessResponse<T>;
+      const result = await response.json();
+      if (!response.ok) throw new Error((result as ErrorResponse).message || response.statusText);
+      return result as SuccessResponse<T>;
     },
     mapResult(result) {
-        return {
-            data: result.content
-        }
+      return {
+        data: result.content,
+      };
     },
-})
+  });
