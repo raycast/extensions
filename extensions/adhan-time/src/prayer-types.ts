5a9c50188286fddcf6db4db9677cc183cd553730
menisy <elmenisy@gmail.com>
2025-02-24 00:34:50 -0800

    empty commit to force publish

--- extensions/adhan-time/src/prayer-types.ts
+++ extensions/adhan-time/src/prayer-types.ts
@@ -1,9 +1,12 @@
+import { Icon } from "@raycast/api";
+
 export interface Preferences {
   country: string;
   city: string;
   calculation_methods: string;
   hanfi: boolean;
   twelve_hours_system: boolean;
+  menu_bar_icon_only: boolean;
 }
 export type Prayers = {
   Fajr: string;
@@ -23,3 +26,22 @@ export type PrayerType = {
     timings: Prayers;
   };
 };
+
+export type PrayerPeriod = {
+  current: string;
+  next: string;
+  currentTime: string;
+  nextTime: string;
+  compareTime: string;
+};
+
+export type PrayerProperty = {
+  sort: number;
+  name: string;
+  icon: Icon;
+  nextPrayer: string | null;
+  isPrayer: boolean;
+  section: "prayers" | "times";
+};
+
+export type PrayerProperties = Record<keyof Prayers, PrayerProperty>;
