def6a5872098a12330b08535613d7bf1d2e656bb
Thomas Paul Mann <hello@thomaspaulmann.com>
2025-02-26 17:29:04 +0000

    Perplexity: Add regular search tool plus evals

--- extensions/perplexity-api/src/tools/search.ts
+++ extensions/perplexity-api/src/tools/search.ts
@@ -1,3 +1,4 @@
+import { getPreferenceValues } from "@raycast/api";
 import { openai } from "../hook/configAPI";
 
 type Input = {
@@ -5,6 +6,14 @@ type Input = {
    * The query for the deep research
    */
   query: string;
+  /**
+   * The model to use for the search.
+   *
+   * @remarks Use "reasoning" for more detailed and nuanced responses.
+   *
+   * @defaultValue The user's global model set in the extension preferences.
+   */
+  model?: "sonar" | "sonar-pro" | "sonar-reasoning" | "sonar-reasoning-pro";
   /**
    * Given a list of domains, limit the citations used by the online model to URLs from the specified domains.
    * Currently limited to only 3 domains for whitelisting and blacklisting.
@@ -17,16 +26,11 @@ type Input = {
   searchRecencyFilter?: "month" | "week" | "day" | "hour";
 };
 
-/**
- * Call this tool when the user wants to research a specific query very deeply.
- *
- * @remarks
- * When you ask a Deep Research question, Perplexity performs dozens of searches, reads hundreds of sources, and reasons through the material to autonomously deliver a comprehensive report.
- */
 export default async function tool(input: Input) {
+  const preferences: Preferences = getPreferenceValues();
   const response = await openai.chat.completions.create({
     messages: [{ role: "user", content: input.query }],
-    model: "sonar-deep-research",
+    model: input.model ?? preferences.model,
     search_domain_filter: input.searchDomainFilter,
     search_recency_filter: input.searchRecencyFilter,
   });
