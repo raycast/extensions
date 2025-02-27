db2cf574f093eb6141dbad33db5e158eb40fcb69
halprin <halprin@users.noreply.github.com>
2025-02-26 17:25:33 -0700

    USPS correctly determine whether they are delivered or not

--- extensions/delivery-tracker/src/carriers/usps.ts
+++ extensions/delivery-tracker/src/carriers/usps.ts
@@ -19,7 +19,9 @@ export async function updateUspsTracking(delivery: Delivery): Promise<Package[]>
 
   return [
     {
-      delivered: new Date() === delivery.manualDeliveryDate,
+      delivered: delivery.manualDeliveryDate
+        ? new Date().setHours(0, 0, 0, 0) > delivery.manualDeliveryDate.setHours(0, 0, 0, 0)
+        : false, // truncate the time from both now and the manual delivery date
       deliveryDate: delivery.manualDeliveryDate,
       activity: [],
     },
