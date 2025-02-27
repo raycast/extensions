fca0f66b8bc3783c504fe7128b39d0a752e812d7
xmok <30526133+xmok@users.noreply.github.com>
2025-02-27 22:02:19 +0500

    [Canva] remove generated SDK

--- extensions/canva/src/designs.tsx
+++ extensions/canva/src/designs.tsx
@@ -1,33 +1,56 @@
 import { authorize, client } from "./oauth";
-import { DesignService } from "../client/ts";
-import { Action, ActionPanel, Icon, Grid } from "@raycast/api";
-import { createClient } from "@hey-api/client-fetch";
-import { useCachedPromise } from "@raycast/utils";
+import { Action, ActionPanel, Grid, Icon } from "@raycast/api";
+import { useCachedState, useFetch } from "@raycast/utils";
+import { useEffect } from "react";
+
+interface Design {
+  id: string;
+  title: string;
+  owner: {
+    user_id: string;
+    team_id: string;
+  }
+  thumbnail: {
+    width: number;
+    height: number;
+    url: string;
+  }
+  urls: {
+    edit_url: string;
+    view_url: string;
+  }
+  created_at: number;
+  updated_at: number;
+  page_count: number;
+}
 
 export default function Index() {
-  const { isLoading, data } = useCachedPromise(async () => {
-    await authorize();
-    const token = (await client.getTokens())?.accessToken;
-    const cnvClient = createClient({
-      headers: {
-        Authorization: `Bearer ${token}`
-      },
-      baseUrl: "https://api.canva.com/rest"
-    })
-    
-    const { data, error } = await DesignService.listDesigns({ client: cnvClient });
-    if (error) throw new Error(error.message);
-    return data.items;
-  }, [], {
-    initialData: []
+  const [token, setToken] = useCachedState<string | undefined>("token");
+  useEffect(() => {
+    (async () => {
+      await authorize();
+      const tokenSet = await client.getTokens();
+      setToken(tokenSet?.accessToken);
+    })()
+  }, [])
+
+  const { isLoading, data } = useFetch("https://api.canva.com/rest/v1/designs", {
+    headers: {
+      Authorization: `Bearer ${token}`
+    },
+    execute: !!token,
+    initialData: [],
+    mapResult(result: { items: Design[] }) {
+      return {
+        data: result.items
+      }
+    },
   })
   
   return <Grid isLoading={isLoading}>
-    {data.map(design => <Grid.Item key={design.id} content={design.thumbnail?.url} title={design.title || ""} accessories={[
-      { icon: Icon.Document, text: design.page_count?.toString() },
-      {date: new Date(design.updated_at * 1000)}
-    ]} actions={<ActionPanel>
-      <Action.OpenInBrowser title="Open View URL" url={design.urls.view_url} />
+    {data.map(design => <Grid.Item key={design.id} content={design.thumbnail?.url} title={design.title || ""} actions={<ActionPanel>
+      <Action.OpenInBrowser icon={Icon.Eye} title="Open View URL" url={design.urls.view_url} />
+      <Action.OpenInBrowser icon={Icon.Pencil} title="Open Edit URL" url={design.urls.view_url} />
     </ActionPanel>} />)}
   </Grid>
 }
