7c28db78efb7d7515ebcb566400465cda896b162
Sven Tiigi <sven.tiigi@gmail.com>
2025-02-27 20:03:52 +0100

    Add search-recent-projects tool

---
+++ extensions/xcode/src/tools/search-recent-projects.ts
@@ -0,0 +1,19 @@
+import { XcodeProjectService } from "../services/xcode-project.service";
+import { XcodeProjectType } from "../models/xcode-project/xcode-project-type.model";
+
+type Input = {
+  query?: string;
+  filter?: XcodeProjectType;
+};
+
+export default async (input: Input) => {
+  let xcodeProjects = await XcodeProjectService.xcodeProjects();
+  if (input.filter) {
+    xcodeProjects = xcodeProjects.filter((xcodeProject) => xcodeProject.type === input.filter);
+  }
+  if (input.query) {
+    const query = input.query;
+    xcodeProjects = xcodeProjects.filter((xcodeProject) => xcodeProject.name.includes(query));
+  }
+  return xcodeProjects;
+};
