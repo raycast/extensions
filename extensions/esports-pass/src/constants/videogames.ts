6201737d2d65db1f990503b112cc8f457769f4d5
Vicio <rafaelvicio@icloud.com>
2025-03-01 21:04:33 +0000

    fix: lint erro

--- extensions/esports-pass/src/constants/videogames.ts
+++ extensions/esports-pass/src/constants/videogames.ts
@@ -1,5 +1,6 @@
 export const videogames = [
-    { name: "League of Legends", slug: "league-of-legends", icon: "./lol.png" },
+    { name: "League of legends", slug: "league-of-legends", icon: "./lol.png" },
     { name: "VALORANT", slug: "valorant", icon: "./valorant.png" },
     { name: "Counter-Strike", slug: "cs-go", icon: "./cs.png" },
-];
+  ];
+  
