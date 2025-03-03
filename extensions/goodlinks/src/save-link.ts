424ecd11b6563f718260f8013de70f4c0584beb9
Nicolas Marien <mariennicolas@icloud.com>
2025-03-03 16:43:09 +0100

    fix: better error handling

--- extensions/goodlinks/src/save-link.ts
+++ extensions/goodlinks/src/save-link.ts
@@ -1,6 +1,7 @@
 import { Clipboard, showToast, Toast } from "@raycast/api";
 import { saveLink } from "./api/save-link";
 import { isApplicationInstalled, showMustBeInstalledToast } from "./utils/isApplicationInstalled";
+import { showFailureToast } from "@raycast/utils";
 
 async function getURLFromClipboard() {
   const possibleUrl = await Clipboard.readText();
@@ -15,7 +16,7 @@ async function getURLFromClipboard() {
 
   try {
     new URL(possibleUrl);
-    return possibleUrl;
+    return encodeURI(possibleUrl);
   } catch {
     await showToast({
       title: "Your clipboard does not contain a valid URL",
@@ -25,22 +26,26 @@ async function getURLFromClipboard() {
   }
 }
 export default async function command() {
-  const isInstalled = await isApplicationInstalled();
+  try {
+    const isInstalled = await isApplicationInstalled();
 
-  if (!isInstalled) {
-    await showMustBeInstalledToast();
-    return;
+    if (!isInstalled) {
+      await showMustBeInstalledToast();
+      return;
+    }
+
+    const linkURL = await getURLFromClipboard();
+
+    if (!linkURL) {
+      return;
+    }
+
+    await saveLink(linkURL);
+    await showToast({
+      title: "Link saved!",
+      style: Toast.Style.Success,
+    });
+  } catch (error) {
+    await showFailureToast(error);
   }
-
-  const linkURL = await getURLFromClipboard();
-
-  if (!linkURL) {
-    return;
-  }
-
-  await saveLink(linkURL);
-  await showToast({
-    title: "Link saved!",
-    style: Toast.Style.Success,
-  });
 }
