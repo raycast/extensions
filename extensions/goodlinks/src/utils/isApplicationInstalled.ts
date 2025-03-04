cc03a7727ff9a7a6077ca88110537a2bc425e775
Nicolas Marien <mariennicolas@icloud.com>
2025-03-04 09:32:27 +0100

    chore: improve app installation detection logic

--- extensions/goodlinks/src/utils/isApplicationInstalled.ts
+++ extensions/goodlinks/src/utils/isApplicationInstalled.ts
@@ -1,16 +1,18 @@
-import { getApplications, showToast, Toast } from "@raycast/api";
+import { getApplications } from "@raycast/api";
+import { showFailureToast } from "@raycast/utils";
 
 export async function isApplicationInstalled() {
-  const applications = await getApplications();
+  try {
+    const applications = await getApplications();
 
-  const goodlinks = applications.find((app) => app.name === "GoodLinks");
+    const goodlinks = applications.find((app) => app.name.toLowerCase() === "goodlinks");
 
-  return !!goodlinks;
+    return !!goodlinks;
+  } catch {
+    return false;
+  }
 }
 
 export async function showMustBeInstalledToast() {
-  await showToast({
-    title: "GoodLinks is not installed on your computer.",
-    style: Toast.Style.Failure,
-  });
+  await showFailureToast("GoodLinks is not installed on your computer.");
 }
