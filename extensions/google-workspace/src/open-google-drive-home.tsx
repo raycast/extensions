fe5ab30237bcfdb209926d10d5d85efb46ace4ac
Wei GENG <wgeng6@gmail.com>
2025-03-04 16:58:10 +0100

    Update extensions/google-workspace/src/open-google-drive-home.tsx
    Co-authored-by: Thomas Lombart <lombart.thomas@icloud.com>

--- extensions/google-workspace/src/open-google-drive-home.tsx
+++ extensions/google-workspace/src/open-google-drive-home.tsx
@@ -1,8 +1,16 @@
+import { getUserEmail } from "./api/googleAuth";
 import { withGoogleAuth } from "./components/withGoogleAuth";
-import { openURL } from "./helpers/docs";
+import { open } from "@raycast/api";
 
 async function Command() {
-  await openURL("https://drive.google.com/drive/u/0/my-drive");
+  const email = await getUserEmail();
+
+  const searchParams = new URLSearchParams();
+  if (email) {
+    searchParams.append("authuser", email);
+  }
+
+  await open(`https://drive.google.com/drive/u/0/my-drive?${searchParams.toString()}`);
 }
 
 export default withGoogleAuth(Command);
