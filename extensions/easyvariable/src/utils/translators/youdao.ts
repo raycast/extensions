d40ed416a61c273626581b81b7530b45f4b082b9
LitoMore <litomore@gmail.com>
2025-03-05 10:01:10 +0800

    [Easy Variable] Enhancements and tweaks

--- extensions/easyvariable/src/utils/translators/youdao.ts
+++ extensions/easyvariable/src/utils/translators/youdao.ts
@@ -1,6 +1,8 @@
+import { getPreferenceValues } from "@raycast/api";
 import axios from "axios";
 import * as cheerio from "cheerio";
-import { preferences } from "../preferences";
+
+const preferences = getPreferenceValues<ExtensionPreferences>();
 
 const USER_AGENT =
   "Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1";
