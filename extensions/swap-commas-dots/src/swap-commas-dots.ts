d9aa28f45685de6974b410228a95a25070caa253
Bruno Picinin Cardoso <brunopicinin@gmail.com>
2025-03-06 09:17:15 -0300

    Update swap-commas-dots extension
    - Add unit tests
    - Improve error handling

--- extensions/swap-commas-dots/src/swap-commas-dots.ts
+++ extensions/swap-commas-dots/src/swap-commas-dots.ts
@@ -1,15 +1,16 @@
 import { getSelectedText, Clipboard, showToast, Toast } from "@raycast/api";
+import { swapCommasAndDots } from "./utils";
 
 export default async () => {
   try {
     const selectedText = await getSelectedText();
-    const transformedText = selectedText.replace(/[.,]/g, (val) => (val === "." ? "," : "."));
+    const transformedText = swapCommasAndDots(selectedText);
     await Clipboard.paste(transformedText);
   } catch (error) {
     await showToast({
       style: Toast.Style.Failure,
       title: "Cannot transform text",
-      message: String(error),
+      message: error instanceof Error ? error.message : String(error),
     });
   }
 };
