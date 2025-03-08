1fb66e4a8f591af03ff057c4230ca580c0417f13
ramah <ramah.guedegbe@gmail.com>
2025-03-08 12:30:34 +0100

    Update replace extension
    - Remove unnecessary blank lines in replace.ts
    - Add error handling and improve clipboard text replacement

--- extensions/replace/src/replace.ts
+++ extensions/replace/src/replace.ts
@@ -1,19 +1,34 @@
 import { Clipboard, showHUD } from "@raycast/api";
+import { showFailureToast } from "@raycast/utils";
 /**
  *
  * "Hello World" -> "Hello_world"
  */
 export default async function main(props: { arguments: { prefix?: string } }) {
-  const input = await Clipboard.readText();
-  if (!input) {
-    await showHUD("No text in clipboard");
+  let input: string | undefined;
+  try {
+    input = await Clipboard.readText();
+    if (!input) {
+      await showHUD("No text in clipboard");
+      return;
+    }
+    const result = input.split(" ").filter(Boolean).join("_");
+    const resultWithPrefix = props.arguments.prefix ? `${prefixValue(props.arguments.prefix)}${result}` : result;
+    await Clipboard.copy(resultWithPrefix);
+    await showHUD(resultWithPrefix);
+  } catch (error) {
+    await showFailureToast("Failed to read clipboard", {
+      title: "Failed to read clipboard",
+      message: "Please try again",
+      primaryAction: {
+        title: "Try again",
+        onAction: () => {
+          main(props);
+        },
+      },
+    });
     return;
   }
-
-  const result = input.split(" ").filter(Boolean).join("_");
-  const resultWithPrefix = props.arguments.prefix ? `${prefixValue(props.arguments.prefix)}${result}` : result;
-  await Clipboard.copy(resultWithPrefix);
-  await showHUD(result);
 }
 
 function prefixValue(prefix: string): string {
