e58b3d97241fe5da07e6e2620ecf184c47649842
ridemountainpig <lin0905580802@gmail.com>
2025-03-09 20:14:06 +0800

    Update ColorConvert.tsx

--- extensions/color-picker/src/components/ColorConvert.tsx
+++ extensions/color-picker/src/components/ColorConvert.tsx
@@ -30,7 +30,7 @@ export const ColorConvertListItem = ({ text, title, subtitle, value }: ColorForm
       actions={
         <ActionPanel>
           <Action
-            title="Convet Color"
+            title="Convert Color"
             onAction={async () => {
               const convertedColor = await getConvertedColor(text, value as ColorFormatType);
               if (convertedColor) {
