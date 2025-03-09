de20a281b575239ebe65e410506a11a13cc80b33
Emilio Aranda <ara.emi44@gmail.com>
2025-03-10 00:46:55 +0100

    v1.

--- src/show.tsx
+++ src/show.tsx
@@ -42,6 +42,84 @@ async function runTerminal(item: ISSHConnection) {
     command = ["ssh", interactive, identity, address, customPort, customCommand].filter(Boolean).join(" ");
   }
 
+  const scriptGhostty = `
+      -- For the latest version:
+      -- https://github.com/DavidMChan/custom-alfred-warp-scripts
+
+      -- Set this property to true to always open in a new window
+      property open_in_new_window: ${openIn == "newWindow"}
+
+      -- Set this property to true to always open in a new tab
+      property open_in_new_tab: ${openIn == "newTab"}
+
+      -- Don't change this :)
+      property opened_new_window: false
+
+      -- Handlers
+      on new_window()
+          tell application "System Events" to tell process "Ghostty"
+              click menu item "New Window" of menu "File" of menu bar 1
+              set frontmost to true
+          end tell
+          delay 0.5
+      end new_window
+
+      on new_tab()
+          tell application "System Events" to tell process "Ghostty"
+              click menu item "New Tab" of menu "File" of menu bar 1
+              set frontmost to true
+          end tell
+      end new_tab
+
+      on call_forward()
+          tell application "Ghostty" to activate
+      end call_forward
+
+      on is_running()
+          application "Ghostty" is running
+      end is_running
+
+      on has_windows()
+          if not is_running() then return false
+          tell application "System Events"
+              if windows of process "Ghostty" is {} then return false
+          end tell
+          return true
+      end has_windows
+
+      on send_text(custom_text)
+          tell application "System Events"
+              keystroke custom_text
+          end tell
+      end send_text
+
+      -- Main
+      if not is_running() then
+          call_forward()
+          set opened_new_window to true
+      else
+          call_forward()
+          set opened_new_window to false
+      end if
+
+      delay 0.1
+
+      if has_windows() then
+          if open_in_new_window and not opened_new_window then
+              new_window()
+          else if open_in_new_tab and not opened_new_window then
+              new_tab()
+          end if
+      else
+          new_window()
+      end if
+
+      -- Make sure a window exists before we continue, or the write may fail
+      delay 0.1
+
+      send_text("${command}\n")
+      call_forward()
+  `;
   const scriptWarp = `
       -- For the latest version:
       -- https://github.com/DavidMChan/custom-alfred-warp-scripts
@@ -364,6 +442,7 @@ async function runTerminal(item: ISSHConnection) {
   call_forward()
   `;
 
+  await closeMainWindow(); // neccessary when alacritty already in fullscreen, or when Ghostty is minimized
   if (terminal == "iTerm") {
     try {
       await runAppleScript(scriptIterm);
@@ -380,7 +459,6 @@ async function runTerminal(item: ISSHConnection) {
     }
   } else if (terminal == "Alacritty") {
     try {
-      await closeMainWindow(); // neccessary when alacritty already in fullscreen
       await runAppleScript(scriptAlacritty);
     } catch (error) {
       await runAppleScript(scriptTerminal);
@@ -393,7 +471,15 @@ async function runTerminal(item: ISSHConnection) {
       await runAppleScript(scriptTerminal);
       console.log(error);
     }
-  } else {
+  } else if (terminal == "Ghostty") {
+    try {
+      await runAppleScript(scriptGhostty);
+    } catch (error) {
+      await runAppleScript(scriptTerminal);
+      console.log(error);
+    }
+  }
+  else {
     await runAppleScript(scriptTerminal);
   }
 
@@ -495,7 +581,6 @@ function GetAction({
 }
 
 function getSubtitle(item: ISSHConnection) {
-  return `${item.user ? item.user + "@" : ""}${item.address}${item.port ? " Port: " + item.port : ""}${
-    item.sshKey ? " SSH Key: " + item.sshKey : ""
-  } ${item.command ? ' Command: "' + item.command + '"' : ""}`;
+  return `${item.user ? item.user + "@" : ""}${item.address}${item.port ? " Port: " + item.port : ""}${item.sshKey ? " SSH Key: " + item.sshKey : ""
+    } ${item.command ? ' Command: "' + item.command + '"' : ""}`;
 }
