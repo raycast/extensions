3d4c783eb5884796b7c5c3181db0536f3facb68d
Mikey <46366395+mikikiv@users.noreply.github.com>
2025-03-11 21:46:13 -0700

    Update homeassistant extension
    - Update Dashboard command to allow custom pathing
    - Initial commit

--- extensions/homeassistant/src/dashboard.tsx
+++ extensions/homeassistant/src/dashboard.tsx
@@ -1,9 +1,24 @@
 import { ha } from "@lib/common";
-import { popToRoot, showHUD } from "@raycast/api";
+import { getPreferenceValues, popToRoot, showHUD } from "@raycast/api";
 import open from "open";
 
+function dashboardPath() {
+  const prefs = getPreferenceValues();
+  const path: string | undefined = prefs.dashboardpath;
+
+  if (!path || path.trim().length <= 0) {
+    return "/";
+  }
+  if (!path.startsWith("/")) {
+    return `/${path}`;
+  }
+  return path;
+}
+
 async function main() {
-  const url = ha.preferCompanionApp ? ha.navigateUrl("") : await ha.nearestDefinedURL();
+  const path = dashboardPath();
+  const baseUrl = ha.preferCompanionApp ? ha.navigateUrl("") : await ha.nearestDefinedURL();
+  const url = baseUrl + path;
   open(url);
   showHUD("Open Dashboard");
   popToRoot();
