6759913ad606771866eb69ba7ab6f2bbc99c429f
K.K. <m@mnm.mn>
2025-03-13 08:50:29 +0000

    chore: apply suggestions

--- extensions/aave-search/src/utils/useAddresses.ts
+++ extensions/aave-search/src/utils/useAddresses.ts
@@ -8,22 +8,20 @@ export type ListItem = {
 };
 
 export const useAddresses = () => {
-  const { isLoading, data, error } = useFetch<string>(DATA_CSV_URL);
-
-  const parsedData: ListItem[] =
-    !error && data
-      ? data
-          .split("\n")
-          .filter(Boolean)
-          .map((line: string) => {
-            const [address, fullPath, chainId] = line.split(",");
-            return {
-              value: address,
-              path: fullPath.trim().split(" "),
-              chainId: parseInt(chainId, 10),
-            };
-          })
-      : [];
-
-  return { isLoading, data: parsedData, error };
+  return useFetch<ListItem[]>(DATA_CSV_URL, {
+    parseResponse: async (response) => {
+      const text = await response.text();
+      return text
+        .split("\n")
+        .filter(Boolean)
+        .map((line: string) => {
+          const [address, fullPath, chainId] = line.split(",");
+          return {
+            value: address,
+            path: fullPath.trim().split(" "),
+            chainId: parseInt(chainId, 10),
+          };
+        });
+    },
+  });
 };
