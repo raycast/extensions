ae5d2e578ada103dc981cabd5538d90c0d89438a
José Pereira <7235666+jomifepe@users.noreply.github.com>
2025-03-14 23:47:54 +0000

    feat: update cli to 2025.2.0 and remove arm64 exception checks

--- extensions/bitwarden/src/api/bitwarden.ts
+++ extensions/bitwarden/src/api/bitwarden.ts
@@ -1,5 +1,5 @@
-import { Cache as RCCache, environment, getPreferenceValues, LocalStorage, open, showToast, Toast } from "@raycast/api";
-import { execa, ExecaChildProcess, ExecaError, ExecaReturnValue, execaSync } from "execa";
+import { environment, getPreferenceValues, LocalStorage, open, showToast, Toast } from "@raycast/api";
+import { execa, ExecaChildProcess, ExecaError, ExecaReturnValue } from "execa";
 import { existsSync, unlinkSync, writeFileSync, accessSync, constants, chmodSync } from "fs";
 import { dirname } from "path/posix";
 import { LOCAL_STORAGE_KEY, DEFAULT_SERVER_URL, CACHE_KEYS } from "~/constants/general";
@@ -78,7 +78,7 @@ type ReceiveSendOptions = {
 
 const { supportPath } = environment;
 
-const Δ = "2"; // changing this forces a new bin download for people that had a failed one
+const Δ = "3"; // changing this forces a new bin download for people that had a failed one
 const BinDownloadLogger = (() => {
   /* The idea of this logger is to write a log file when the bin download fails, so that we can let the extension crash,
    but fallback to the local cli path in the next launch. This allows the error to be reported in the issues dashboard. It uses files to keep it synchronous, as it's needed in the constructor.
@@ -93,8 +93,8 @@ const BinDownloadLogger = (() => {
 })();
 
 export const cliInfo = {
-  version: "2024.2.0",
-  sha256: "fd80ffefd4686e677d7c8720258b3c92559b65b890519b1327cd4bb45887dde8",
+  version: "2025.2.0",
+  sha256: "fade51012a46011c016a2e5aee2f2e534c1ed078e49d1178a69e2889d2812a96",
   downloadPage: "https://github.com/bitwarden/clients/releases",
   path: {
     arm64: "/opt/homebrew/bin/bw",
@@ -106,25 +106,6 @@ export const cliInfo = {
       return process.arch === "arm64" ? this.arm64 : this.x64;
     },
     get bin() {
-      // TODO: Remove this when the issue is resolved
-      // CLI bin download is off for arm64 until bitwarden releases arm binaries
-      // https://github.com/bitwarden/clients/pull/2976
-      // https://github.com/bitwarden/clients/pull/7338
-      if (process.arch === "arm64") {
-        const cache = new RCCache();
-        try {
-          if (!existsSync(this.downloadedBin)) throw new Error("No downloaded bin");
-          if (cache.get("downloadedBinWorks") === "true") return this.downloadedBin;
-
-          execaSync(this.downloadedBin, ["--version"]);
-          cache.set("downloadedBinWorks", "true");
-          return this.downloadedBin;
-        } catch {
-          cache.set("downloadedBinWorks", "false");
-          return this.installedBin;
-        }
-      }
-
       return !BinDownloadLogger.hasError() ? this.downloadedBin : this.installedBin;
     },
   },
