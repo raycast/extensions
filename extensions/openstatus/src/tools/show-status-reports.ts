9372ae11e41eaa8914b3e2b34d6538d651d9e3d6
xmok <30526133+xmok@users.noreply.github.com>
2025-03-16 01:45:47 +0500

    [OpenStatus] tools: status_report_update

---
+++ extensions/openstatus/src/tools/show-status-reports.ts
@@ -0,0 +1,15 @@
+import { getPreferenceValues } from "@raycast/api";
+import fetch from "node-fetch";
+import { StatusReport } from "../api/schema";
+
+export default async function () {
+  const { access_token } = getPreferenceValues<Preferences>();
+  const response = await fetch("https://api.openstatus.dev/v1/status_report", {
+    headers: {
+      "x-openstatus-key": `${access_token}`,
+    }
+  })
+  if (!response.ok) throw new Error(response.statusText);
+  const result = await response.json();
+  return result as StatusReport;
+}
