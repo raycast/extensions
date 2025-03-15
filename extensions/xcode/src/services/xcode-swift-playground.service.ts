7811899a537ee8aaeaab3a990f36a25e773d8167
Sven Tiigi <sven.tiigi@gmail.com>
2025-03-15 10:02:44 +0100

    Add option to select the Swift version when creating a playground

--- extensions/xcode/src/services/xcode-swift-playground.service.ts
+++ extensions/xcode/src/services/xcode-swift-playground.service.ts
@@ -8,6 +8,7 @@ import { existsAsync, makeDirectoryAsync, removeDirectoryAsync, writeFileAsync }
 import untildify from "untildify";
 import * as Path from "path";
 import { createSwiftPlaygroundCommandPreferences } from "../shared/preferences";
+import { XcodeSwiftPlaygroundSwiftVersion } from "../models/swift-playground/xcode-swift-playground-swift-version.model";
 
 /**
  * XcodeSwiftPlaygroundService
@@ -18,24 +19,13 @@ export class XcodeSwiftPlaygroundService {
    */
   private static scaffoldTemplateFiles: TemplateFile[] = [
     {
-      name: "timeline",
-      extension: "xctimeline",
-      contents: `
-      <?xml version="1.0" encoding="UTF-8"?>
-      <Timeline version="3.0">
-         <TimelineItems>
-         </TimelineItems>
-      </Timeline>
-      `,
-    },
-    {
       path: "playground.xcworkspace",
       name: "contents",
       extension: "xcworkspacedata",
       contents: `
       <?xml version="1.0" encoding="UTF-8"?>
       <Workspace version="1.0">
-        <FileRef location="group:self:">
+        <FileRef location="group:">
         </FileRef>
       </Workspace>
       `,
@@ -79,7 +69,7 @@ export class XcodeSwiftPlaygroundService {
     const templateFiles = [
       ...XcodeSwiftPlaygroundService.scaffoldTemplateFiles,
       XcodeSwiftPlaygroundService.swiftSourceContentsTemplateFile(parameters.template),
-      XcodeSwiftPlaygroundService.contentsTemplateFile(parameters.platform),
+      XcodeSwiftPlaygroundService.contentsTemplateFile(parameters.platform, parameters.swiftVersion),
     ];
     try {
       // Create TemplateFiles in parallel
@@ -154,20 +144,24 @@ export class XcodeSwiftPlaygroundService {
   /**
    * The XCPlayground contents TemplateFile
    * @param platform The XcodeSwiftPlaygroundPlatform
+   * @param swiftVersion The XcodeSwiftPlaygroundSwiftVersion
    * @private
    */
-  private static contentsTemplateFile(platform: XcodeSwiftPlaygroundPlatform): TemplateFile {
+  private static contentsTemplateFile(
+    platform: XcodeSwiftPlaygroundPlatform,
+    swiftVersion: XcodeSwiftPlaygroundSwiftVersion
+  ): TemplateFile {
     return {
       name: "contents",
       extension: "xcplayground",
       contents: `
       <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-      <playground version='5.0' 
-                  target-platform='${platform.toLocaleLowerCase()}' 
+      <playground version='7.0' 
+                  target-platform='${platform.toLowerCase()}' 
+                  swift-version='${swiftVersion}'
                   buildActiveScheme='true' 
                   executeOnSourceChanges='false' 
                   importAppTypes='true'>
-          <timeline fileName='timeline.xctimeline'/>
       </playground>
       `,
     };
