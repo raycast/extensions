7b5a4e124fe6501ada33d14dfa58035998de4380
José Pereira <7235666+jomifepe@users.noreply.github.com>
2025-03-15 01:11:30 +0000

    feat: add cli version to debugging section title

--- extensions/bitwarden/src/components/actions/DebuggingBugReportingActionSection.tsx
+++ extensions/bitwarden/src/components/actions/DebuggingBugReportingActionSection.tsx
@@ -1,9 +1,12 @@
 import { ActionPanel } from "@raycast/api";
 import { BugReportCollectDataAction, BugReportOpenAction, CopyRuntimeErrorLog } from "~/components/actions";
+import { useCliVersion } from "~/utils/hooks/useCliVersion";
 
 export function DebuggingBugReportingActionSection() {
+  const cliVersion = useCliVersion();
+
   return (
-    <ActionPanel.Section title="Debugging & Bug Reporting">
+    <ActionPanel.Section title={`Debugging & Bug Reporting (CLI v${cliVersion})`}>
       <CopyRuntimeErrorLog />
       <BugReportOpenAction />
       <BugReportCollectDataAction />
