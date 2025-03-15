eaacb4ad9d41b77b25bb7d3a140c0f4b4ca3d88c
xmok <30526133+xmok@users.noreply.github.com>
2025-03-15 20:17:57 +0500

    [OpenStatus] tools: incidents,monitors,status_page

---
+++ extensions/openstatus/src/tools/show-incidents.ts
@@ -0,0 +1,15 @@
+import { getPreferenceValues } from "@raycast/api";
+import fetch from "node-fetch";
+import { Incident } from "../api/schema";
+
+export default async function () {
+  const { access_token } = getPreferenceValues<Preferences>();
+  const response = await fetch("https://api.openstatus.dev/v1/incident", {
+    headers: {
+      "x-openstatus-key": `${access_token}`,
+    }
+  })
+  if (!response.ok) throw new Error(response.statusText);
+  const result = await response.json();
+  return result as Incident;
+}
---
+++ extensions/openstatus/src/tools/show-monitors.ts
@@ -0,0 +1,15 @@
+import { getPreferenceValues } from "@raycast/api";
+import fetch from "node-fetch";
+import { Monitor } from "../api/schema";
+
+export default async function () {
+  const { access_token } = getPreferenceValues<Preferences>();
+  const response = await fetch("https://api.openstatus.dev/v1/monitor", {
+    headers: {
+      "x-openstatus-key": `${access_token}`,
+    }
+  })
+  if (!response.ok) throw new Error(response.statusText);
+  const result = await response.json();
+  return result as Monitor;
+}
