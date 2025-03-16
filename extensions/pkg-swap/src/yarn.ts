3ddc4cf8749a229ce27ef3c704ca44d22dcad62f
Josh Dunsterville <neversitdull@icloud.com>
2025-03-16 14:06:01 -0700

    Update yarn.ts

--- extensions/pkg-swap/src/yarn.ts
+++ extensions/pkg-swap/src/yarn.ts
@@ -1,10 +1,5 @@
 import { convertCommand } from "./lib/converter";
 
 export default async function command() {
-  try {
-    await convertCommand("npm", "yarn");
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+  await convertCommand("npm", "yarn");
 }
