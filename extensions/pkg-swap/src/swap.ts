efa7b157b759bc750fc30c99affabbd7d02b7e29
Josh Dunsterville <neversitdull@icloud.com>
2025-03-16 14:05:33 -0700

    Update swap.ts

--- extensions/pkg-swap/src/swap.ts
+++ extensions/pkg-swap/src/swap.ts
@@ -1,19 +1,14 @@
 import { getPreferenceValues } from "@raycast/api";
 import { convertCommand, PackageManager } from "./lib/converter";
-import { showFailureToast } from "@raycast/utils";
 
 interface Preferences {
   defaultPackageManager: PackageManager;
 }
 
 export default async function command() {
-  try {
-    // Get user's preferred package manager from preferences
-    const preferences = getPreferenceValues<Preferences>();
-    const packageManager = preferences.defaultPackageManager;
+  // Get user's preferred package manager from preferences
+  const preferences = getPreferenceValues<Preferences>();
+  const packageManager = preferences.defaultPackageManager;
 
-    await convertCommand("npm", packageManager);
-  } catch (error) {
-    showFailureToast("Failed to convert package manager command", error);
-  }
+  await convertCommand("npm", packageManager);
 }
