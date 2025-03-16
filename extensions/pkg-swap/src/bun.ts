d1df728ce6700be0935391fde72353deb103549a
Josh Dunsterville <neversitdull@icloud.com>
2025-03-16 14:03:30 -0700

    Update bun.ts

--- extensions/pkg-swap/src/bun.ts
+++ extensions/pkg-swap/src/bun.ts
@@ -1,10 +1,5 @@
 import { convertCommand } from "./lib/converter";
-import { showFailureToast } from "@raycast/utils";
 
 export default async function command() {
-  try {
-    await convertCommand("npm", "bun");
-  } catch (error) {
-    await showFailureToast("Failed to convert to bun", error);
-  }
+  await convertCommand("npm", "bun");
 }
