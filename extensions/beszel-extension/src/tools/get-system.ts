7a1da5cccc821148688cb531433860790f49468c
Wyatt Johnson <accounts+github@wyattjoh.ca>
2025-03-17 10:34:19 -0600

    feat: reduce tools to one

--- extensions/beszel-extension/src/tools/get-system.ts
+++ extensions/beszel-extension/src/tools/get-system.ts
@@ -1,3 +1,4 @@
+import type PocketBase from "pocketbase";
 import { getClient } from "../helpers/get-client";
 import type { BeszelSystem } from "../types/beszel";
 
@@ -5,13 +6,10 @@ type Input = {
   /**
    * The name of the system to get
    */
-  name: string;
+  name?: string;
 };
 
-export default async function tool(input: Input) {
-  const client = await getClient();
-  const system = await client.collection("systems").getFirstListItem<BeszelSystem>(`name="${input.name}"`);
-
+function formatSystem(system: BeszelSystem) {
   return {
     name: system.name,
     host: system.host,
@@ -26,4 +24,27 @@ export default async function tool(input: Input) {
     threads: system.info.t,
     uptime: system.info.u,
   };
+}
+
+export default async function tool(input: Input) {
+  let client: PocketBase;
+  try {
+    client = await getClient();
+  } catch (error) {
+    throw new Error("Failed to get client", { cause: error });
+  }
+
+  try {
+    if (input.name) {
+      const system = await client.collection("systems").getFirstListItem<BeszelSystem>(`name="${input.name}"`);
+
+      return formatSystem(system);
+    }
+
+    const systems = await client.collection("systems").getFullList<BeszelSystem>();
+
+    return systems.map(formatSystem);
+  } catch (error) {
+    throw new Error("Failed to get system(s)", { cause: error });
+  }
 }
