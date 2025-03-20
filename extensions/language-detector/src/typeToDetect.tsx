704b0f65e7f0b9874d5f8096d97e65b0b9e7fac3
LitoMore <LitoMore@users.noreply.github.com>
2025-03-20 23:16:32 +0800

    [Language Detector] Add support for choosing AI models (#18006)
    * [Language Detector] Add support for choosing AI models
    
    * Update CHANGELOG.md and optimise images
    
    ---------
    
    Co-authored-by: raycastbot <bot@raycast.com>

--- extensions/language-detector/src/typeToDetect.tsx
+++ extensions/language-detector/src/typeToDetect.tsx
@@ -1,6 +1,6 @@
 import { LaunchProps, Toast, showHUD } from "@raycast/api";
 import { LaunchOptions, callbackLaunchCommand } from "raycast-cross-extension";
-import { detectLanguage } from "./utils.js";
+import { detectLanguage, getCachedDetector, getCachedModel } from "./utils.js";
 
 type LaunchContext = {
   displayHUD?: boolean;
@@ -13,14 +13,16 @@ export default async function TypeToDetect({
 }: LaunchProps<{ arguments: Arguments.TypeToDetect; launchContext?: LaunchContext }>) {
   if (!args.content) return;
   const { displayHUD, callbackLaunchOptions } = launchContext;
+  const detector = getCachedDetector();
+  const model = getCachedModel();
   if (callbackLaunchOptions) {
-    const { languageCode, languageName } = await detectLanguage(args.content);
+    const { languageCode, languageName } = await detectLanguage(args.content, detector, model);
     if (displayHUD) await showHUD(languageName);
     callbackLaunchCommand(callbackLaunchOptions, { languageCode, languageName });
   } else {
     const toast = new Toast({ style: Toast.Style.Animated, title: "Detecting language..." });
     toast.show();
-    const { languageName } = await detectLanguage(args.content);
+    const { languageName } = await detectLanguage(args.content, detector, model);
     toast.style = Toast.Style.Success;
     toast.title = "Detected language";
     toast.message = languageName;
