ccfa5822e6d833272b902b6582987c8f4b44e6f2
Daniele Gargano <daniele.gargano.office@gmail.com>
2025-03-20 12:12:43 +0100

    Add openrouter extension
    - chore: downgrade @raycast/api to v1.93 for compatibility
    - feat(ask): persist last selected model ID in LocalStorage
    - feat: migrate from Google Gemini to OpenRouter API
    - ray lint --fix
    - Removes unused getConfiguration
    - feat: migrate from OpenAI ChatGPT to Google Gemini
    - refactor(vision): removes unsupported Temperature parameter
    - refactor: remove unused imports and update token pricing
    - Updates screenshot
    - chore: update asset size to be compatible with raycast (512x512)
    - Updates assets to match Gemini
    - Fixees 400 no body on ask
    - Basic reformat from GPT to Gemini (OpenAI Compatible)
    - Clone ChatGPT Extension
    - Raycast Extension Create

---
+++ extensions/openrouter/README.md
@@ -0,0 +1,101 @@
+<p align="center">
+<img width=100 src="assets/icon@dark.png">
+</p>
+
+<h1 align="center">OpenRouter</h1>
+
+<h3 align="center">
+Interact with OpenRouter right from your command bar
+</h3>
+
+![Conversation View](metadata/1.png)
+
+# Features
+
+### Ask anything, from your favourite thing
+
+Straight from your command bar, ask anything that you wanted and get an AI-generated answer without any effort.
+
+![Ask anything](metadata/2.png)
+
+### Personalized for you, really
+
+Customize the model to your liking. Create and edit custom engines beyond your creativity.
+
+![Custom model](metadata/3.png)
+
+### Keep continue, with you
+
+Continue talking about everything right where you left off. Be pro without from zero.
+
+![Initial set-up](metadata/7.png)
+
+### Save the answer, for later
+
+Got the answer that you wanted? Great. Now you can save it without asking again.
+
+![Saving the answer](metadata/4.png)
+
+### Look-up your past, fast
+
+Automatically save all the question and answer so you can go back digging for the answer you're looking, quickly.
+
+![Looking through the question history](metadata/5.png)
+
+### Use AI commands and create your own
+
+Process text taken from anywhere (selected text, clipboard text, opened web page) and 
+insert the result into the frontmost application or copy it to the clipboard.
+
+![Search AI commands and create a quicklink to the command and use it easily](metadata/8.png)
+
+![AI command in action](metadata/9.png)
+
+# Models
+
+GPT-4o model supports vision capabilities, which can be enabled in the Models Command when creating or editing a model.
+
+### Custom Models
+
+Modify the preferences properties to configure the API Endpoint and use custom models.
+
+# How to use
+
+This extension requires a valid `Secret Key` as your API Key from [OpenRouter](https://openrouter.ai/settings/keys).
+
+![Initial set-up](metadata/6.png)
+
+> All the preferences value will be stored locally using [Preferences API](https://developers.raycast.com/api-reference/preferences)
+
+# Preferences
+
+All preferences properties list that can be customize through `Raycast Settings > Extensions > OpenRouter`
+
+| Properties               | Label                  | Value                               | Required | Default                     | Description                                                                                                      |
+| ------------------------ | ---------------------- | ----------------------------------- | -------- | --------------------------- | ---------------------------------------------------------------------------------------------------------------- |
+| `apiKey`                 | API Key                | `string`                            | `true`   | `empty`                     | Your personal OpenRouter API key                                                                              |
+| `useStream`              | Stream Completion      | `boolean`                           | `true`   | `true`                      | Stream the completions of the generated answer                                                                   |
+| `isAutoSaveConversation` | Auto-save Conversation | `boolean`                           | `true`   | `true`                      | Auto-save every conversation that you had with the model                                                         |
+| `isHistoryPaused`        | Pause History          | `boolean`                           | `false`  | `false`                     | Pause the history of the conversation                                                                            |
+| `isAutoLoadText`         | Auto-load              | `boolean`                           | `false`  | `false`                     | Load selected text from your frontmost application to the `question bar` or `full text input form` automatically |
+| `isAutoFullInput`        | Use Full Text Input    | `boolean`                           | `false`  | `false`                     | Switch to `full text input form` from `question bar` automatically whenever you want to ask or type a question   |
+| `isAutoTTS`              | Text-to-Speech         | `boolean`                           | `false`  | `false`                     | Enable auto text-to-speech everytime you get a generated answer                                                  |
+| `useApiEndpoint`         | Use API Endpoint       | `boolean`                           | `false`  | `false`                     | Change the OpenRouter's default API endpoint to custom endpoint                                                      |
+| `apiEndpoint`            | API Endpoint           | `string`                            | `false`  | `empty`                     | Custom API endpoint                                                                                              |
+| `useProxy`               | Use Proxy              | `boolean`                           | `false`  | `false`                     | Each question request will be passed through the proxy                                                           |
+| `proxyProtocol`          | Proxy Protocol         | `http`, `https`, `socks4`, `socks5` | `false`  | `http`                      | Proxy protocol option                                                                                            |
+| `proxyHost`              | Proxy Host             | `string`                            | `false`  | `empty`                     | Proxy host value                                                                                                 |
+| `proxyUsername`          | Proxy Username         | `string`                            | `false`  | `empty`                     | Proxy username value                                                                                             |
+| `proxyPassword`          | Proxy Password         | `string`                            | `false`  | `empty`                     | Proxy password value                                                                                             |
+
+# Support
+
+Donate the original's extension developer to support the development. Thank you!
+
+[![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/abielzulio)
+
+---
+
+<p align="right">
+Made with ♥ from Indonesia and Italy
+</p>
---
+++ extensions/openrouter/src/views/command/command-view.tsx
@@ -0,0 +1,315 @@
+import {
+  Action,
+  ActionPanel,
+  Application,
+  Detail,
+  getFrontmostApplication,
+  Icon,
+  LaunchProps,
+  List,
+  Navigation,
+  open,
+  useNavigation,
+} from "@raycast/api";
+import { useChat } from "../../hooks/useChat";
+import React, { useEffect, useState } from "react";
+import { canAccessBrowserExtension } from "../../utils/browser";
+import { PrimaryAction } from "../../actions";
+import { Command, ChatHook } from "../../type";
+import { fetchContent } from "../../utils/cmd-input";
+import { getAppIconPath } from "../../utils/icon";
+import Ask from "../../ask";
+import { v4 as uuidv4 } from "uuid";
+import { mapCommandToModel, useCommand } from "../../hooks/useCommand";
+
+type CommandLaunchContext = { commandId?: string };
+export type CommandLaunchProps = LaunchProps<{ launchContext?: CommandLaunchContext }>;
+
+export default function CommandView(props: CommandLaunchProps) {
+  const navigation = useNavigation();
+  const commands = useCommand();
+  const chat = useChat([]);
+  const [aiAnswer, setAiAnswer] = useState<string | null>(null);
+  const [userInput, setUserInput] = useState<string | null>(null);
+  const [userInputError, setUserInputError] = useState<string | null>(null);
+  const [frontmostApp, setFrontmostApp] = useState<Application | null>(null);
+
+  const requestedCommandId = props.launchContext?.commandId || "";
+  const requestedCommand = commands.data[requestedCommandId];
+
+  useEffect(() => {
+    getFrontmostApplication().then(setFrontmostApp);
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      if (!requestedCommand) {
+        return;
+      }
+      const { content, error } = await fetchContent(requestedCommand.contentSource);
+      setUserInput(content);
+      setUserInputError(error);
+    })();
+  }, [requestedCommand]);
+
+  useEffect(() => {
+    if (userInput && requestedCommand) {
+      setAiAnswer(null);
+      chat.ask(userInput, [], mapCommandToModel(requestedCommand));
+    }
+  }, [userInput, requestedCommand]);
+
+  useEffect(() => {
+    if (!chat.streamData && !chat.isLoading && chat.data.length > 0) {
+      const lastChat = chat.data[chat.data.length - 1];
+      setAiAnswer(lastChat.answer);
+    } else {
+      setAiAnswer(chat.streamData?.answer || null);
+    }
+  }, [chat.streamData, chat.isLoading, chat.data]);
+
+  if (!commands.isLoading && !requestedCommand) {
+    return buildCommandNotFoundView(requestedCommandId);
+  }
+  if (!requestedCommand) {
+    return <Detail markdown="" />;
+  }
+  if (requestedCommand.contentSource === "browserTab" && !canAccessBrowserExtension()) {
+    return BROWSER_EXTENSION_NOT_AVAILABLE_VIEW;
+  }
+
+  const viewBuilder = new CommandViewBuilder(
+    requestedCommand,
+    chat,
+    navigation,
+    frontmostApp,
+    userInput,
+    aiAnswer,
+    userInputError || chat.errorMsg,
+  );
+
+  return <Detail markdown={viewBuilder.buildContent()} actions={viewBuilder.buildActionPanel()} />;
+}
+
+class CommandViewBuilder {
+  iconSizePx: number;
+  totalViewWidthPx: number;
+
+  command: Command;
+  chat: ChatHook;
+  navigation: Navigation;
+  frontmostApp: Application | null;
+  userInput: string | null;
+  aiAnswer: string | null;
+  error: string | null;
+
+  constructor(
+    command: Command,
+    chat: ChatHook,
+    navigation: Navigation,
+    frontmostApp: Application | null,
+    userInput: string | null,
+    aiAnswer: string | null,
+    error: string | null,
+  ) {
+    this.totalViewWidthPx = 710;
+    this.iconSizePx = 17;
+
+    this.command = command;
+    this.chat = chat;
+    this.navigation = navigation;
+    this.frontmostApp = frontmostApp;
+    this.userInput = userInput;
+    this.aiAnswer = aiAnswer;
+    this.error = error;
+  }
+
+  buildContent(): string {
+    let inputTemplate = "";
+    if (this.command.isDisplayInput) {
+      // Show user input as preformatted text because it allows the text to be displayed
+      // exactly as it is. If we don't wrap it or format it as a quote, some symbols may be
+      // rendered as Markdown markup.
+      inputTemplate = `\`\`\`\n${(this.userInput || "...").trim()}\n\`\`\``;
+    }
+
+    let footerMessage = "";
+    let chatWidthPx = 7;
+    let footerMessageColor: "gray" | "yellow" | "red" = "gray";
+    if (this.chat.isAborted) {
+      footerMessage = "Canceled";
+      footerMessageColor = "yellow";
+    } else if (this.error) {
+      chatWidthPx = 6;
+      footerMessage = "An unexpected error occurred; the command execution failed.";
+      footerMessageColor = "red";
+    } else if (this.aiAnswer !== null && !this.chat.isLoading) {
+      chatWidthPx = 6.1;
+      footerMessage = "Continue in Chat ⌘ + 🅹";
+    }
+
+    return `${this.generateTitleSvg(this.command.name)}
+
+${inputTemplate}
+
+${this.aiAnswer || "..."}
+
+${this.generateStatFooterSvg(this.command.model, footerMessage, chatWidthPx, footerMessageColor)}
+
+${this.error ? "---" : ""}
+${this.error || ""}`;
+  }
+
+  generateTitleSvg(title: string): string {
+    const width = this.totalViewWidthPx - this.iconSizePx;
+    const titleImage = `
+<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${this.iconSizePx}" style="background: transparent;">
+  <style>
+    .text { 
+      font-size: 14px; 
+      fill: grey; 
+      font-family: Arial, sans-serif;
+      font-weight: bold;
+    }
+  </style>
+  <text x="0" y="16" class="text">${title}</text>
+</svg>`;
+
+    return `${this.getFrontmostAppIcon()}![CommandName](data:image/svg+xml;base64,${Buffer.from(
+      titleImage,
+      "utf-8",
+    ).toString("base64")})`;
+  }
+
+  generateStatFooterSvg(
+    model: string,
+    message: string,
+    charWidthPx: number = 7,
+    color: "gray" | "yellow" | "red" = "gray",
+  ) {
+    // charWidthPx is a workaround to align the message to the right edge.
+    // I couldn't find a better solution, so for each value that will be sent
+    // to the message, you need to calculate charWidthPx manually.
+    const width = this.totalViewWidthPx - this.iconSizePx;
+    const messageWidth = message.length * charWidthPx;
+
+    const statImage = `
+<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${this.iconSizePx}" style="background: transparent;">
+  <style>
+    .text { 
+      font-size: 13px; 
+      fill: grey; 
+      font-family: Arial, sans-serif; 
+    }
+  </style>
+  
+  <text x="5" y="14.5" class="text">${model}</text>
+  <text x="${width - messageWidth}" y="14.5" class="text" style="fill: ${color};">${message}</text>
+</svg>`;
+
+    const modelIcon = `&#x200b;![ModelIcon](icon.png?raycast-width=${this.iconSizePx}&raycast-height=${this.iconSizePx})`;
+    return `${modelIcon}![CommandFooter](data:image/svg+xml;base64,${Buffer.from(statImage, "utf-8").toString(
+      "base64",
+    )})`;
+  }
+
+  getFrontmostAppIcon(): string {
+    let appIconPath = "";
+    if (this.command.contentSource === "clipboard") {
+      appIconPath = "clipboard.svg";
+    } else if (this.frontmostApp?.path) {
+      try {
+        appIconPath = getAppIconPath(this.frontmostApp.path).replace(/ /g, "%20");
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    const markdownIcon = `&#x200b;![AppIcon](${appIconPath}?raycast-width=${this.iconSizePx}&raycast-height=${this.iconSizePx}) `;
+    return appIconPath ? markdownIcon : "";
+  }
+
+  buildActionPanel(): React.JSX.Element {
+    const cancel = <Action key="cancel" title="Cancel" icon={Icon.Stop} onAction={this.chat.abort} />;
+    const pasteToActiveApp = (
+      <Action.Paste
+        key="pasteToActiveApp"
+        title={`Paste Response to ${this.frontmostApp ? this.frontmostApp.name : "Active App"}`}
+        content={this.aiAnswer || ""}
+        icon={this.frontmostApp ? { fileIcon: this.frontmostApp.path } : Icon.AppWindow}
+      />
+    );
+    const copyToClipboard = (
+      <Action.CopyToClipboard key="copyToClipboard" title={`Copy Response`} content={this.aiAnswer || ""} />
+    );
+    const continueInChat = (
+      <Action
+        key="continueInChat"
+        title="Continue in Chat"
+        icon={Icon.Message}
+        onAction={() => {
+          this.navigation.push(
+            <Ask
+              conversation={{
+                id: uuidv4(),
+                chats: this.chat.data,
+                model: mapCommandToModel(this.command),
+                created_at: new Date().toISOString(),
+                updated_at: new Date().toISOString(),
+                pinned: false,
+              }}
+            />,
+          );
+        }}
+        shortcut={{ modifiers: ["cmd"], key: "j" }}
+      />
+    );
+
+    const mainActions: React.JSX.Element[] = [];
+    const extraActions: React.JSX.Element[] = [];
+    if (this.chat.isLoading) {
+      mainActions.push(cancel);
+    } else {
+      if (this.command.contentSource === "selectedText") {
+        mainActions.push(pasteToActiveApp);
+        mainActions.push(copyToClipboard);
+      } else {
+        mainActions.push(copyToClipboard);
+      }
+      extraActions.push(continueInChat);
+    }
+
+    return (
+      <ActionPanel>
+        <ActionPanel.Section>{mainActions}</ActionPanel.Section>
+        {extraActions ? <ActionPanel.Section>{extraActions}</ActionPanel.Section> : null}
+      </ActionPanel>
+    );
+  }
+}
+
+function buildCommandNotFoundView(commandId: string) {
+  return (
+    <Detail
+      markdown={
+        `AI command with id=${commandId} not found. This AI command may have been deleted.` +
+        `You need to remove this quicklink, create the AI command again, and then create the quicklink once more.`
+      }
+    />
+  );
+}
+
+const BROWSER_EXTENSION_NOT_AVAILABLE_VIEW = (
+  <List
+    actions={
+      <ActionPanel>
+        <PrimaryAction title="Install" onAction={() => open("https://www.raycast.com/browser-extension")} />
+      </ActionPanel>
+    }
+  >
+    <List.EmptyView
+      icon={Icon.BoltDisabled}
+      title={"Browser Extension Required"}
+      description={"This command need install Raycast browser extension to work. Please install it first"}
+    />
+  </List>
+);
