367248bd01ed6442e61bab1e8b8f4b31409f28d9
sinsky <60662802+sinsky@users.noreply.github.com>
2025-03-22 01:10:55 +0900

    feat(copy-markdown.ts): add generateMarkdown utility to create markdown links from title and URL for improved functionality

--- extensions/create-link/src/copy-markdown.ts
+++ extensions/create-link/src/copy-markdown.ts
@@ -1,19 +1,6 @@
 import { showHUD, Clipboard } from "@raycast/api";
 import { isExtensionInstalled, getActiveTab } from "./utils/browser";
-
-function sanitizeForMarkdown(text: string): string {
-  // Escape markdown special characters: [ ] ( ) ` * _ { } # + - . !
-  return text.replace(/([[\]()"`*_{}\\#+\-.!])/g, "\\$1");
-}
-
-function sanitizeUrl(url: string): string {
-  try {
-    new URL(url);
-    return url;
-  } catch {
-    return "about:blank";
-  }
-}
+import { generateMarkdown } from "./utils/formatter";
 
 export default async function copyMarkdown() {
   if (!isExtensionInstalled()) {
@@ -28,9 +15,7 @@ export default async function copyMarkdown() {
   }
 
   const { url, title } = activeTab;
-  const safeTitle = sanitizeForMarkdown(title || "");
-  const safeUrl = sanitizeUrl(url);
-  const markdownLink = `[${safeTitle}](${safeUrl})`;
+  const markdownLink = generateMarkdown(title || "", url);
   await Clipboard.copy(markdownLink);
   await showHUD(`Copied Markdown link for "${title || ""}" to clipboard`);
 }
