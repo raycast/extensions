c88533039bd7b0fe72047190c36ef5d79fafb4f2
sinsky <60662802+sinsky@users.noreply.github.com>
2025-03-22 01:12:07 +0900

    feat(show-tabs.tsx): add loading state and user feedback for no tabs found to enhance user experience

---
+++ extensions/create-link/src/show-tabs.tsx
@@ -0,0 +1,42 @@
+import { showHUD, BrowserExtension, List } from "@raycast/api";
+import { isExtensionInstalled, getTabs } from "./utils/browser";
+import TabList from "./components/TabList";
+import { Suspense, useEffect, useState } from "react";
+
+async function checkExtension() {
+  if (!isExtensionInstalled()) {
+    await showHUD("Extension not installed");
+    return false;
+  }
+  return true;
+}
+
+export default function CopyPlainText() {
+  const [tabs, setTabs] = useState<BrowserExtension.Tab[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const initialize = async () => {
+      const isExtensionOk = await checkExtension();
+      if (isExtensionOk) {
+        const tabData = await getTabs();
+        setTabs(tabData);
+      }
+      setIsLoading(false);
+    };
+
+    initialize();
+  }, []);
+
+  useEffect(() => {
+    if (!isLoading && tabs.length === 0) {
+      showHUD("No tabs found");
+    }
+  }, [tabs, isLoading]);
+
+  return (
+    <Suspense fallback={<List isLoading={isLoading}>Loading...</List>}>
+      {(  !isLoading && tabs.length === 0) ? <List.EmptyView title="No tabs found" /> : <TabList tabs={tabs} />}
+    </Suspense>
+  );
+}
