75d5cde84f30199393b5236e7916148808bca4c7
karita <hayue0228@icloud.com>
2025-03-23 22:13:53 +0900

    fix try catch error

--- extensions/todoisttoggl/src/api/togglClient.ts
+++ extensions/todoisttoggl/src/api/togglClient.ts
@@ -39,6 +39,7 @@ async function togglFetch<T>(method: string, endpoint: string, body?: unknown): 
     const json = (await res.json()) as T | null;
     if (json !== null) return json;
   } catch (error) {
+    console.error(error);
     if (!(error instanceof SyntaxError)) throw error;
   }
 }
