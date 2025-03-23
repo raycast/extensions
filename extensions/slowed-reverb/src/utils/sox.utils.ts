23bc501762929542bba2feb74439884b9bc13504
Tomo <105588693+neontomo@users.noreply.github.com>
2025-03-23 16:38:47 +0100

    escaping sox path properly with node

--- extensions/slowed-reverb/src/utils/sox.utils.ts
+++ extensions/slowed-reverb/src/utils/sox.utils.ts
@@ -1,6 +1,11 @@
 import { execFileSync, execSync } from "child_process";
+import path from "path";
 import { errorUtils } from "./errors.utils";
 import { preferenceUtils } from "./preference.utils";
+
+const escapePath = (filePath: string): string => {
+  return path.resolve(path.normalize(filePath));
+};
 
 const getSoxPath = () => {
   const { getPreference } = preferenceUtils;
@@ -31,7 +36,7 @@ const getSoxPath = () => {
     .toString()
     .trim();
 
-  if (commandFolderPath) return commandFolderPath.replace(/\n/gi, "");
+  if (commandFolderPath) return escapePath(commandFolderPath);
   return "";
 };
 
