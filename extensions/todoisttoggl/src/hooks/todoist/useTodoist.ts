11a2577c86c4a805ba9d55368e9bffc15046bdcf
karita <hayue0228@icloud.com>
2025-03-25 12:07:03 +0900

    delete type and fix default data

--- extensions/todoisttoggl/src/hooks/todoist/useTodoist.ts
+++ extensions/todoisttoggl/src/hooks/todoist/useTodoist.ts
@@ -17,7 +17,7 @@ export function useGetProject() {
     },
     [],
     {
-      initialData: null,
+      initialData: [],
     },
   );
   return { isLoading, data };
@@ -36,7 +36,7 @@ export function useGetTasks() {
     },
     [],
     {
-      initialData: null,
+      initialData: [],
     },
   );
   return { isLoading, data, mutate };
@@ -55,7 +55,7 @@ export function useGetLabels() {
     },
     [],
     {
-      initialData: null,
+      initialData: [],
     },
   );
   return { isLoading, data, mutate };
