3f0f843f23115d551012ccdeb3fdfe1156eedae7
gabriel-schmid <gabriel.schmid321@gmail.com>
2025-03-25 17:26:37 +0100

    comments

--- extensions/hazu/src/paste-hazuid-with-prefix-swiss.ts
+++ extensions/hazu/src/paste-hazuid-with-prefix-swiss.ts
@@ -1,20 +1,20 @@
 import { Clipboard } from "@raycast/api";
 
 export default async function pasteClipboard() {
-  // Liest den aktuellen Inhalt der Zwischenablage
+  // Read the current clipboard content
   const clipboardContent = await Clipboard.readText();
 
   if (clipboardContent) {
-    // Überprüfen, ob der Prefix "hazu.swiss/" bereits vorhanden ist
+    // check if the clipboard content starts with the prefix
     const prefix = "hazu.swiss/";
     let newString = clipboardContent;
 
     if (!clipboardContent.startsWith(prefix)) {
-      // Wenn der Prefix nicht vorhanden ist, füge ihn hinzu
+      // if the clipboard content does not start with the prefix, add it
       newString = prefix + clipboardContent;
     }
 
-    // Fügt den (gegebenenfalls modifizierten) Inhalt an der aktuellen Cursor-Position ein
+    // Paste the new string
     await Clipboard.paste(newString);
   }
 }
