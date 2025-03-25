6044c617b50bd2a1e405a1bfda05df691186418e
Jonatas Walker <jonatasswwalker@gmail.com>
2025-03-24 22:12:54 -0300

    fix: export ServiceHub

--- extensions/g-cloud/src/services/servicehub/index.ts
+++ extensions/g-cloud/src/services/servicehub/index.ts
@@ -1,4 +1,4 @@
 export { default as ServiceHubView } from "./ServiceHubView";
-export { MarketplaceService } from "./ServiceHubService";
+export { ServiceHubService } from "./ServiceHubService";
 export type { GCPService } from "./ServiceHubService";
 export * from "./components";
