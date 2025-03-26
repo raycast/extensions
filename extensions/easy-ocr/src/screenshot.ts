98d3b891528eba2d791d8352cc448de349477d2e
Rafael <rafael.vidas1@gmail.com>
2025-03-26 12:21:02 +0100

    added toast on screenshot failure used path.join instead of regular string concatenation

--- extensions/easy-ocr/src/screenshot.ts
+++ extensions/easy-ocr/src/screenshot.ts
@@ -1,17 +1,19 @@
 import { environment } from "@raycast/api";
+import { showFailureToast } from "@raycast/utils";
 import util from "util";
 import { exec } from "child_process";
+import path from 'path';
 
 const execPromise = util.promisify(exec);
 
-const filePath = environment.assetsPath + "/" + Date.now() + ".png";
+const filePath = path.join(environment.assetsPath, `${Date.now()}.png`);
 const command = "/usr/sbin/screencapture -i " + filePath;
 export default async function takeScreenshot() {
   try {
     await execPromise(command);
-import { environment } from "@raycast/api";
-import { showFailureToast } from "@raycast/utils";
-import util from "util";
-import { exec } from "child_process";
+  } catch (e) {
+    showFailureToast(e, { title: "Failed to capture screenshot" });
+  }
+
   return filePath;
 }
