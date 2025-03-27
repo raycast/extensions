c47ad4534b876a8f4739204e217c76bd2ac387a2
moojing <starts789@gmail.com>
2025-03-27 13:56:40 +0000

    fix: remove unnecessary type

--- extensions/flashlang/src/utils/prompts.ts
+++ extensions/flashlang/src/utils/prompts.ts
@@ -1,12 +1,8 @@
 import { LocalStorage } from "@raycast/api";
-import { SelectedText } from "@raycast/api";
 import { translationPrompt } from "../constants/prompts";
 import { LanguageOption } from "./useLanguageList";
 
-export const translateVocabularyPrompt = async (
-  vocabulary: string | SelectedText,
-  languageOptions: LanguageOption[],
-) => {
+export const translateVocabularyPrompt = async (vocabulary: string, languageOptions: LanguageOption[]) => {
   const learningLanguageCode: string = (await LocalStorage.getItem("learning-language")) || "";
   const nativeLanguageCode: string = (await LocalStorage.getItem("native-language")) || "";
   const learningLanguage: string = languageOptions.find((option) => option.value === learningLanguageCode)?.label || "";
