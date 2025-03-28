e8f52169fa47a9df4da6d76185ef862bbf7d95aa
Dante Issaias <dante@issaias.com>
2025-03-28 19:54:26 +0000

    Invalidate cache

--- extensions/mail/src/utils/cache.ts
+++ extensions/mail/src/utils/cache.ts
@@ -10,6 +10,8 @@ export enum ExpirationTime {
   Week = 7 * Day,
 }
 
+const CACHE_VERSION = 1;
+
 const isCacheExpired = (time: number, limit = ExpirationTime.Day): boolean => {
   return Date.now() - time > limit;
 };
@@ -24,8 +26,8 @@ const getAccounts = (): Account[] | undefined => {
   if (accounts.has("accounts")) {
     const response = accounts.get("accounts");
     if (response) {
-      const { time, data } = JSON.parse(response);
-      if (!isCacheExpired(time)) {
+      const { time, data, version } = JSON.parse(response);
+      if (!isCacheExpired(time) && version === CACHE_VERSION) {
         return data;
       }
     }
@@ -39,7 +41,7 @@ const getAccount = (idOrName: string): Account | undefined => {
 };
 
 const setAccounts = (data: Account[]) => {
-  accounts.set("accounts", JSON.stringify({ time: Date.now(), data: data }));
+  accounts.set("accounts", JSON.stringify({ time: Date.now(), data: data, version: CACHE_VERSION }));
 };
 
 const messages = new RaycastCache();
@@ -53,8 +55,8 @@ const getMessages = (account: string, mailbox: string): Message[] => {
   if (messages.has(key)) {
     const response = messages.get(key);
     if (response) {
-      const { time, data } = JSON.parse(response);
-      if (!isCacheExpired(time)) {
+      const { time, data, version } = JSON.parse(response);
+      if (!isCacheExpired(time) && version === CACHE_VERSION) {
         return data.slice(0, messageLimit);
       }
     }
@@ -65,7 +67,7 @@ const getMessages = (account: string, mailbox: string): Message[] => {
 
 const setMessages = (data: Message[], account: string, mailbox: string) => {
   const key = `${account}-${mailbox}`;
-  messages.set(key, JSON.stringify({ time: Date.now(), data: data }));
+  messages.set(key, JSON.stringify({ time: Date.now(), data: data, version: CACHE_VERSION }));
 };
 
 const addMessage = (data: Message, account: string, mailbox: string) => {
