54621617d8612c59e170e23e984c116f2b756f4b
Kiran K <kiran@dub.co>
2025-04-01 19:27:29 +0530

    Bump `dub` version

--- extensions/dub/src/polyfills/fetch.ts
+++ extensions/dub/src/polyfills/fetch.ts
@@ -1,14 +1,10 @@
 import { fetch, Headers, Request, Response } from "undici";
 
-if (!Object.keys(global).includes("fetch")) {
-  Object.defineProperty(global, "fetch", { value: fetch });
-}
-if (!Object.keys(global).includes("Headers")) {
-  Object.defineProperty(global, "Headers", { value: Headers });
-}
-if (!Object.keys(global).includes("Request")) {
-  Object.defineProperty(global, "Request", { value: Request });
-}
-if (!Object.keys(global).includes("Response")) {
-  Object.defineProperty(global, "Response", { value: Response });
-}
+// @ts-expect-error
+global.fetch = fetch;
+// @ts-expect-error
+global.Headers = Headers;
+// @ts-expect-error
+global.Request = Request;
+// @ts-expect-error
+global.Response = Response;
