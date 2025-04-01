9e3eee8dc456063c98dc5df17e5ac035aafd2340
Cory Hayes <corymhayes@gmail.com>
2025-03-31 21:43:23 -0500

    Add movie-quicklook extension
    - fixed package-lock
    - initial commit

---
+++ extensions/movie-quicklook/README.md
@@ -0,0 +1,14 @@
+# Movie Quicklook
+
+## Steps for getting a TMDb API key
+
+- Head over to [https://themoviedb.org](https://themoviedb.org)
+- Click "Join TMDB" in the top right corner
+- After you have joined and logged in
+  - Click your _avatar_ in the top right corner (probably the first letter of your username) and select _Settings_ from the drop down menu
+  - Click _API_ from the menu on the left
+  - Under **Request an API Key**, click _click here_
+  - Click _Yes_ to indicate that using the API is for personal use
+  - Fill out the information
+  - After submission you should be taken to another page, then click _Access your API key details here_
+  - Finally, you should be back at the Settings page, from here copy the _API Read Access Token_ and paste it into Raycast
---
+++ extensions/movie-quicklook/src/movie-quicklook.tsx
@@ -0,0 +1,177 @@
+import React from "react";
+import { Action, ActionPanel, Detail, Grid, useNavigation, getPreferenceValues } from "@raycast/api";
+import { useFetch } from "@raycast/utils";
+import { useState } from "react";
+
+interface Preferences {
+  api_key: string;
+}
+
+interface Movie {
+  id: number;
+  genres: Genre[];
+  overview: string;
+  poster_path: string;
+  release_date: string;
+  runtime: number;
+  title: string;
+  vote_average: number;
+  vote_count: number;
+}
+
+interface Genre {
+  name: string;
+}
+
+interface Movies {
+  results: Movie[];
+}
+
+export default function Command() {
+  const preferences = getPreferenceValues<Preferences>();
+  const [searchText, setSearchText] = useState("");
+  const { push } = useNavigation();
+
+  const { isLoading, data } = useFetch(
+    () =>
+      `https://api.themoviedb.org/3/search/movie?` + new URLSearchParams({ query: searchText, page: "1" }).toString(),
+    {
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${preferences.api_key}`,
+      },
+      mapResult(result: Movies) {
+        return {
+          data: result.results,
+        };
+      },
+      keepPreviousData: true,
+      initialData: [],
+    },
+  );
+
+  return (
+    <Grid
+      columns={4}
+      inset={Grid.Inset.Zero}
+      filtering={false}
+      isLoading={isLoading}
+      onSearchTextChange={setSearchText}
+      searchBarPlaceholder="Search for a movie"
+      throttle
+    >
+      {searchText === "" && data.length === 0 ? (
+        <Grid.EmptyView
+          icon={"../assets/empty-view-icon.png"}
+          title="No movie entered"
+          description="Type a movie to get started"
+        />
+      ) : (
+        data.map((movie: Movie) => (
+          <Grid.Item
+            key={movie.id}
+            title={`${movie.title} (${movie.release_date.substring(0, 4)})`}
+            subtitle={`${movie.vote_average > 0 ? movie.vote_average.toFixed(2) : "N/A"}`}
+            content={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+            actions={
+              <ActionPanel>
+                <Action
+                  title="Movie Quicklook"
+                  onAction={() => push(<MovieDetails movieId={movie.id} api_key={preferences.api_key} />)}
+                />
+              </ActionPanel>
+            }
+          />
+        ))
+      )}
+    </Grid>
+  );
+}
+
+function MovieDetails({ movieId, api_key }: { movieId: number; api_key: string }) {
+  const { pop } = useNavigation();
+
+  const { isLoading, data } = useFetch(() => `https://api.themoviedb.org/3/movie/${movieId}`, {
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${api_key}`,
+    },
+    mapResult(result: Movie) {
+      return {
+        data: [result],
+      };
+    },
+    keepPreviousData: true,
+    initialData: [],
+  });
+
+  return (data || []).map((detail) => (
+    <React.Fragment key={movieId}>
+      <Detail
+        isLoading={isLoading}
+        actions={
+          <ActionPanel>
+            <Action title="Search" onAction={pop} />
+          </ActionPanel>
+        }
+        markdown={`![Image](https://image.tmdb.org/t/p/w185/${detail.poster_path})&NewLine;${detail.overview}`}
+        metadata={
+          <Detail.Metadata>
+            <Detail.Metadata.Label title="Title" text={`${detail.title}`} />
+            <Detail.Metadata.Label
+              title="Rating"
+              text={`⭐ ${detail.vote_average > 0 ? detail.vote_average.toFixed(2) : "N/A"} (${detail.vote_count})`}
+            />
+            <Detail.Metadata.Label title="Release Year" text={`${detail.release_date.substring(0, 4)}`} />
+            <Detail.Metadata.Label title="Runtime" text={`${detail.runtime} min`} />
+            <Detail.Metadata.Separator />
+            <Detail.Metadata.TagList title="Genres">
+              {(detail.genres || []).map((genre) => (
+                <Detail.Metadata.TagList.Item key={`${genre.name}`} text={`${genre.name}`} />
+              ))}
+            </Detail.Metadata.TagList>
+            <Detail.Metadata.Separator />
+            <MovieReviews movieId={movieId} api_key={api_key} />
+          </Detail.Metadata>
+        }
+      />
+    </React.Fragment>
+  ));
+}
+
+interface Reviews {
+  results: Review[];
+}
+
+interface Review {
+  author: string;
+  author_details: { rating: number };
+  content: string;
+}
+
+function MovieReviews({ movieId, api_key }: { movieId: number; api_key: string }) {
+  const { data } = useFetch(() => `https://api.themoviedb.org/3/movie/${movieId}/reviews`, {
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${api_key}`,
+    },
+    mapResult(result: Reviews) {
+      return {
+        data: result.results,
+      };
+    },
+    keepPreviousData: true,
+    initialData: [],
+  });
+
+  return (data || []).map((review) => (
+    <React.Fragment key={review.author}>
+      <Detail.Metadata.Label
+        title="Rating"
+        text={`${review.author} (⭐ ${review.author_details.rating !== null ? review.author_details.rating : "N/A"})`}
+      />
+      <Detail.Metadata.Label title="Review" text={`${review.content}`} />
+      <Detail.Metadata.Separator />
+    </React.Fragment>
+  ));
+}
