d33ac5fa833e663845c561637e6221fa86898158
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2025-04-01 19:28:36 +0100

    feat: add paystack documentation URL to utils for easy access

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,1 +1,2 @@
-export const paystackDashboardUrl = "https://dashboard.paystack.com/#";
+export const paystackDashboardUrl = 'https://dashboard.paystack.com/#'
+export const paystackDocsUrl = 'https://paystack.com/docs/api'
