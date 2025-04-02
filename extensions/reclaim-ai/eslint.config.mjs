ac4722ee3a8035b675e263e4a217007cb2731cdf
Sanford Gifford <sanford.r.gifford@gmail.com>
2025-04-02 15:51:58 -0400

    migrate to new eslint

---
+++ extensions/reclaim-ai/eslint.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig } from "eslint/config";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([{
+    extends: compat.extends("@raycast"),
+
+    rules: {
+        "no-restricted-imports": ["error", {
+            paths: [{
+                name: "@raycast/utils",
+                importNames: ["useFetch"],
+                message: "Use useApi instead.",
+            }],
+        }],
+    },
+}]);
