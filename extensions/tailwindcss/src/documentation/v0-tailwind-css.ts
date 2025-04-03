a8103340447280a1959256a5c2a290ee3cb7e9bb
Olaf van Midden <olaf@dorpzicht.nl>
2025-04-04 00:15:40 +0200

    Update tailwindcss extension
    - Removed double / inside urls
    - Updated CHANGELOG.md
    - Improved typing
    - Merge branch 'feature/naming-changes'
    - Updated naming conventions
    - Merge branch 'feature/moved-preferences'
    - Removed getPreferenceValues
    - Moved preferences inside the command itself
    - Updated to useMemo and replaced switch with documentationMap
    - Adjusted preference naming
    - Added more typing for the DocumentationItem
    - Added V0 documentation
    - Added V1 documentation
    - Update v4 documentation
    - Added V2 documentation
    - Updated v3 documentation
    - Added version 3 as preference to change versions
    - Added contributor

--- extensions/tailwindcss/src/documentation/v0-tailwind-css.ts
+++ extensions/tailwindcss/src/documentation/v0-tailwind-css.ts
@@ -1,317 +1,317 @@
 export default {
   Introduction: [
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/what-is-tailwind",
+      url: "https://tailwindcss-v0.netlify.app/docs/what-is-tailwind",
       title: "What is Tailwind?",
     },
   ],
   "Getting Started": [
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/installation",
+      url: "https://tailwindcss-v0.netlify.app/docs/installation",
       title: "Installation",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/configuration",
+      url: "https://tailwindcss-v0.netlify.app/docs/configuration",
       title: "Configuration",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/colors",
+      url: "https://tailwindcss-v0.netlify.app/docs/colors",
       title: "Colors",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/responsive-design",
+      url: "https://tailwindcss-v0.netlify.app/docs/responsive-design",
       title: "Responsive Design",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/state-variants",
+      url: "https://tailwindcss-v0.netlify.app/docs/state-variants",
       title: "State Variants",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/adding-new-utilities",
+      url: "https://tailwindcss-v0.netlify.app/docs/adding-new-utilities",
       title: "Adding New Utilities",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/extracting-components",
+      url: "https://tailwindcss-v0.netlify.app/docs/extracting-components",
       title: "Extracting Components",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/functions-and-directives",
+      url: "https://tailwindcss-v0.netlify.app/docs/functions-and-directives",
       title: "Functions & Directives",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/controlling-file-size",
+      url: "https://tailwindcss-v0.netlify.app/docs/controlling-file-size",
       title: "Controlling File Size",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/plugins",
+      url: "https://tailwindcss-v0.netlify.app/docs/plugins",
       title: "Plugins",
     },
   ],
   Layout: [
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/container",
+      url: "https://tailwindcss-v0.netlify.app/docs/container",
       title: "Container",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/display",
+      url: "https://tailwindcss-v0.netlify.app/docs/display",
       title: "Display",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/floats",
+      url: "https://tailwindcss-v0.netlify.app/docs/floats",
       title: "Floats",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/object-fit",
+      url: "https://tailwindcss-v0.netlify.app/docs/object-fit",
       title: "Object Fit",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/object-position",
+      url: "https://tailwindcss-v0.netlify.app/docs/object-position",
       title: "Object Position",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/overflow",
+      url: "https://tailwindcss-v0.netlify.app/docs/overflow",
       title: "Overflow",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/positioning",
+      url: "https://tailwindcss-v0.netlify.app/docs/positioning",
       title: "Position",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/visibility",
+      url: "https://tailwindcss-v0.netlify.app/docs/visibility",
       title: "Visibility",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/z-index",
+      url: "https://tailwindcss-v0.netlify.app/docs/z-index",
       title: "Z-Index",
     },
   ],
   Typography: [
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/text-color",
+      url: "https://tailwindcss-v0.netlify.app/docs/text-color",
       title: "Color",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/fonts",
+      url: "https://tailwindcss-v0.netlify.app/docs/fonts",
       title: "Font Family",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/text-sizing",
+      url: "https://tailwindcss-v0.netlify.app/docs/text-sizing",
       title: "Font Size",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/font-weight",
+      url: "https://tailwindcss-v0.netlify.app/docs/font-weight",
       title: "Font Weight",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/letter-spacing",
+      url: "https://tailwindcss-v0.netlify.app/docs/letter-spacing",
       title: "Letter Spacing",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/line-height",
+      url: "https://tailwindcss-v0.netlify.app/docs/line-height",
       title: "Line Height",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/lists",
+      url: "https://tailwindcss-v0.netlify.app/docs/lists",
       title: "Lists",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/text-style",
+      url: "https://tailwindcss-v0.netlify.app/docs/text-style",
       title: "Style & Decoration",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/text-alignment",
+      url: "https://tailwindcss-v0.netlify.app/docs/text-alignment",
       title: "Text Alignment",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/vertical-alignment",
+      url: "https://tailwindcss-v0.netlify.app/docs/vertical-alignment",
       title: "Vertical Alignment",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/whitespace-and-wrapping",
+      url: "https://tailwindcss-v0.netlify.app/docs/whitespace-and-wrapping",
       title: "Whitespace & Wrapping",
     },
   ],
   Backgrounds: [
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/background-attachment",
+      url: "https://tailwindcss-v0.netlify.app/docs/background-attachment",
       title: "Background Attachment",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/background-color",
+      url: "https://tailwindcss-v0.netlify.app/docs/background-color",
       title: "Background Color",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/background-position",
+      url: "https://tailwindcss-v0.netlify.app/docs/background-position",
       title: "Background Position",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/background-repeat",
+      url: "https://tailwindcss-v0.netlify.app/docs/background-repeat",
       title: "Background Repeat",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/background-size",
+      url: "https://tailwindcss-v0.netlify.app/docs/background-size",
       title: "Background Size",
     },
   ],
   Borders: [
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/border-color",
+      url: "https://tailwindcss-v0.netlify.app/docs/border-color",
       title: "Border Color",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/border-style",
+      url: "https://tailwindcss-v0.netlify.app/docs/border-style",
       title: "Border Style",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/border-width",
+      url: "https://tailwindcss-v0.netlify.app/docs/border-width",
       title: "Border Width",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/border-radius",
+      url: "https://tailwindcss-v0.netlify.app/docs/border-radius",
       title: "Border Radius",
     },
   ],
   Flexbox: [
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/flexbox-display",
+      url: "https://tailwindcss-v0.netlify.app/docs/flexbox-display",
       title: "Display",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/flexbox-direction",
+      url: "https://tailwindcss-v0.netlify.app/docs/flexbox-direction",
       title: "Flex Direction",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/flexbox-wrapping",
+      url: "https://tailwindcss-v0.netlify.app/docs/flexbox-wrapping",
       title: "Flex Wrapping",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/flexbox-align-items",
+      url: "https://tailwindcss-v0.netlify.app/docs/flexbox-align-items",
       title: "Align Items",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/flexbox-align-content",
+      url: "https://tailwindcss-v0.netlify.app/docs/flexbox-align-content",
       title: "Align Content",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/flexbox-align-self",
+      url: "https://tailwindcss-v0.netlify.app/docs/flexbox-align-self",
       title: "Align Self",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/flexbox-justify-content",
+      url: "https://tailwindcss-v0.netlify.app/docs/flexbox-justify-content",
       title: "Justify Content",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/flexbox-flex-grow-shrink",
+      url: "https://tailwindcss-v0.netlify.app/docs/flexbox-flex-grow-shrink",
       title: "Flex, Grow, & Shrink",
     },
   ],
   Spacing: [
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/spacing",
+      url: "https://tailwindcss-v0.netlify.app/docs/spacing",
       title: "Margin & Padding",
     },
   ],
   Sizing: [
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/width",
+      url: "https://tailwindcss-v0.netlify.app/docs/width",
       title: "Width",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/min-width",
+      url: "https://tailwindcss-v0.netlify.app/docs/min-width",
       title: "Min-Width",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/max-width",
+      url: "https://tailwindcss-v0.netlify.app/docs/max-width",
       title: "Max-Width",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/height",
+      url: "https://tailwindcss-v0.netlify.app/docs/height",
       title: "Height",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/min-height",
+      url: "https://tailwindcss-v0.netlify.app/docs/min-height",
       title: "Min-Height",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/max-height",
+      url: "https://tailwindcss-v0.netlify.app/docs/max-height",
       title: "Max-Height",
     },
   ],
   Tables: [
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/border-collapse",
+      url: "https://tailwindcss-v0.netlify.app/docs/border-collapse",
       title: "Border Collapse",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/table-layout",
+      url: "https://tailwindcss-v0.netlify.app/docs/table-layout",
       title: "Table Layout",
     },
   ],
   Effects: [
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/shadows",
+      url: "https://tailwindcss-v0.netlify.app/docs/shadows",
       title: "Box Shadow",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/opacity",
+      url: "https://tailwindcss-v0.netlify.app/docs/opacity",
       title: "Opacity",
     },
   ],
   Interactivity: [
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/appearance",
+      url: "https://tailwindcss-v0.netlify.app/docs/appearance",
       title: "Appearance",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/cursor",
+      url: "https://tailwindcss-v0.netlify.app/docs/cursor",
       title: "Cursor",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/outline",
+      url: "https://tailwindcss-v0.netlify.app/docs/outline",
       title: "Outline",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/pointer-events",
+      url: "https://tailwindcss-v0.netlify.app/docs/pointer-events",
       title: "Pointer Events",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/resize",
+      url: "https://tailwindcss-v0.netlify.app/docs/resize",
       title: "Resize",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/user-select",
+      url: "https://tailwindcss-v0.netlify.app/docs/user-select",
       title: "User Select",
     },
   ],
   SVG: [
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/svg",
+      url: "https://tailwindcss-v0.netlify.app/docs/svg",
       title: "Fill & Stroke",
     },
   ],
   "Component Examples": [
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/examples/alerts",
+      url: "https://tailwindcss-v0.netlify.app/docs/examples/alerts",
       title: "Alerts",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/examples/buttons",
+      url: "https://tailwindcss-v0.netlify.app/docs/examples/buttons",
       title: "Buttons",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/examples/cards",
+      url: "https://tailwindcss-v0.netlify.app/docs/examples/cards",
       title: "Cards",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/examples/forms",
+      url: "https://tailwindcss-v0.netlify.app/docs/examples/forms",
       title: "Forms",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/examples/grids",
+      url: "https://tailwindcss-v0.netlify.app/docs/examples/grids",
       title: "Grids",
     },
     {
-      url: "https://tailwindcss-v0.netlify.app//docs/examples/navigation",
+      url: "https://tailwindcss-v0.netlify.app/docs/examples/navigation",
       title: "Navigation",
     },
   ],
