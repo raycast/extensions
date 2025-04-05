56153214217f5b879bb3ce64b74f2b08f24a5dda
xmok <30526133+xmok@users.noreply.github.com>
2025-04-05 20:25:34 +0500

    [Pastery] modernize + docs

--- extensions/pastery/src/types.ts
+++ extensions/pastery/src/types.ts
@@ -1,14 +1,16 @@
 export type Paste = {
-    duration: number | null;
-    id: string;
-    language: string;
-    title: string;
-    url: string;
-}
-export type ErrorResult = {
-    result: "error";
-    error_msg: string;
+  duration: number | null;
+  id: string;
+  language: string;
+  title: string;
+  url: string;
 };
-export type ActionResult = {
-    result: "success"
-} | ErrorResult;
+export type ErrorResult = {
+  result: "error";
+  error_msg: string;
+};
+export type ActionResult =
+  | {
+      result: "success";
+    }
+  | ErrorResult;
