79c8dd4874d5cb0563af68b76293966addce0426
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2025-04-05 19:10:20 +0300

    [Connect to VPN] feat: implement toggle action
    This new action will allow users to toggle their last used VPN in a matter of seconds.

---
+++ extensions/connect-to-vpn/src/toggle.ts
@@ -0,0 +1,38 @@
+import { LocalStorage, PopToRootType, showHUD, showToast, Toast } from "@raycast/api";
+import {
+  getNetworkServices,
+  LAST_USED_KEY,
+  loadFavoriteOrder,
+  loadFavorites,
+  setServiceStatus,
+} from "./network-services";
+import { getVpnStatus } from "./store";
+
+export default async () => {
+  const lastUsedName = await LocalStorage.getItem(LAST_USED_KEY);
+  const test = await getVpnStatus();
+  console.log(test);
+  await showToast({
+    style: Toast.Style.Animated,
+    title: `Toggling ${lastUsedName}`,
+  });
+
+  const favs = await loadFavorites();
+  const order = await loadFavoriteOrder();
+  const networkServices = await getNetworkServices(favs, order);
+  const ids = Object.keys(networkServices);
+  const lastUsed = ids.find((id) => networkServices[id].name === lastUsedName);
+  if (lastUsed) {
+    const service = networkServices[lastUsed];
+    if (service) {
+      const status = networkServices[lastUsed].status;
+      const newStatus = status === "connected" ? "disconnected" : "connecting";
+      const newStatusMessage = status === "connected" ? "off" : "on";
+      await setServiceStatus(service, newStatus);
+      await showHUD(`Turned ${lastUsedName} ${newStatusMessage}`, {
+        clearRootSearch: true,
+        popToRootType: PopToRootType.Immediate,
+      });
+    }
+  }
+};
