bdd358cedf0d30110f3ae4985cae2b8c9cfaba07
José Pereira <7235666+jomifepe@users.noreply.github.com>
2025-04-08 23:30:15 +0100

    feat: add primary action option
    refactor: reorder search primary action for consistency

--- extensions/bitwarden/src/authenticator.tsx
+++ extensions/bitwarden/src/authenticator.tsx
@@ -1,6 +1,17 @@
 import * as OTPAuth from "otpauth";
 import { useEffect, useMemo, useState } from "react";
-import { Action, ActionPanel, BrowserExtension, Clipboard, Color, Icon, List, showToast, Toast } from "@raycast/api";
+import {
+  Action,
+  ActionPanel,
+  BrowserExtension,
+  Clipboard,
+  Color,
+  getPreferenceValues,
+  Icon,
+  List,
+  showToast,
+  Toast,
+} from "@raycast/api";
 import { usePromise } from "@raycast/utils";
 
 import RootErrorBoundary from "~/components/RootErrorBoundary";
@@ -21,6 +32,7 @@ import { ActionWithReprompt, DebuggingBugReportingActionSection, VaultActionsSec
 import { Err, Ok, Result, tryCatch } from "~/utils/errors";
 import { useVaultSearch } from "~/utils/search";
 import ListFolderDropdown from "~/components/ListFolderDropdown";
+import ComponentReverser from "~/components/ComponentReverser";
 
 const AuthenticatorCommand = () => (
   <RootErrorBoundary>
@@ -102,6 +114,7 @@ function ListItem({ item }: { item: Item }) {
   const icon = useItemIcon(item);
   const [canGenerate, setCanGenerate] = useState(!item.reprompt);
   const { code, time, error, isLoading } = authenticator.useCode(item, canGenerate);
+  const { primaryAction } = getPreferenceValues<Preferences.Authenticator>();
 
   function getAccessories(): List.Item.Props["accessories"] {
     if (!canGenerate) {
@@ -138,10 +151,10 @@ function ListItem({ item }: { item: Item }) {
         actions={
           <ActionPanel>
             {canGenerate ? (
-              <>
+              <ComponentReverser reverse={primaryAction === "paste"}>
                 <CopyCodeAction />
                 <PasteCodeAction />
-              </>
+              </ComponentReverser>
             ) : (
               <ConfirmAction onConfirm={() => setCanGenerate(true)} />
             )}
