36dd740364a37ae83ae3959f47f696c7fd15550a
xmok <30526133+xmok@users.noreply.github.com>
2025-04-10 23:25:23 +0500

    [Spacship] adding is mostly working

--- extensions/spaceship/src/manage-domains.tsx
+++ extensions/spaceship/src/manage-domains.tsx
@@ -1,6 +1,7 @@
 import { Action, ActionPanel, Alert, Color, confirmAlert, Form, getPreferenceValues, Icon, Keyboard, List, showToast, Toast, useNavigation } from "@raycast/api";
 import { getFavicon, useCachedState, useFetch, useForm } from "@raycast/utils";
 import { DomainInfo, ErrorResult, SuccessResult, ResourceRecord, DomainClientEPPStatus, ResourceRecordsListCreateOrUpdateItem } from "./types";
+import { useState } from "react";
 
 const { apiKey, apiSecret } = getPreferenceValues<Preferences>();
 const API_URL = "https://spaceship.dev/api/v1/";
@@ -178,17 +179,57 @@ function ManageDNSRecords({ domain }: { domain: DomainInfo }) {
 }
 
 function CreateDNSRecord({domain, onRecordAdded}: { domain: DomainInfo, onRecordAdded: () => void }) {
+  const [isLoading, setIsLoading] = useState(false);
   const {pop} = useNavigation();
-  const { handleSubmit, itemProps } = useForm<{
+  const { handleSubmit, itemProps, values } = useForm<{
     type: string;
+    name: string;
+    value: string;
+    ttl: string;
+    force: boolean;
+    // MX
+    preference: string;
   }>({
     async onSubmit(values) {
+      setIsLoading(true);
       const toast = await showToast(Toast.Style.Animated, "Adding record");
+      let item: ResourceRecordsListCreateOrUpdateItem | undefined;
+      switch (values.type) {
+        case "A":
+          item = {
+            type: "A",
+            address: values.value,
+            name: values.name,
+            ttl: +values.ttl
+          }    
+          break;
+          case "MX":
+          item = {
+            type: "MX",
+            exchange: values.value,
+            name: values.name,
+            preference: +values.preference,
+            ttl: +values.ttl
+          }
+        break;
+          case "TXT":
+          item = {
+            type: "TXT",
+            value: values.value,
+            name: values.name,
+            ttl: +values.ttl
+          }
+        break;
+      }
+      
       try {
         const response = await fetch(`${API_URL}dns/records/${domain.name}`, {
           method: "PUT",
           headers: API_HEADERS,
-          body: JSON.stringify(values)
+          body: JSON.stringify({
+            force: values.force,
+            items: [item]
+          })
         });
         if (!response.ok) {
           const res: ErrorResult = await response.json();
@@ -202,10 +243,50 @@ function CreateDNSRecord({domain, onRecordAdded}: { domain: DomainInfo, onRecord
         toast.style = Toast.Style.Failure;
         toast.title = "Could not add record";
         toast.message = `${error}`;
+      } finally {
+        setIsLoading(false);
       }
     },
+    initialValues: {
+      type: "A",
+      ttl: "1800",
+      force: false
+    },
+    validation: {
+      name(value) {
+        if (!value) return "Invalid host value";
+      },
+      value(value) {
+        if (["A", "MX", "TXT"].includes(values.type) && !value) return "The item is required";
+      },
+      preference(value) {
+        if (values.type==="MX" && (!value || !Number(value) || +value<0 || +value>65535)) return "Enter a value between 0 and 65535, only integer";
+      },
+    }
   })
-  return <Form actions={<ActionPanel>
+
+  const TYPE_SPECIFIC_FIELDS: { [type: string]: { value: { placeholder: string, info: string } } } = {
+    A: {
+      value: {
+        placeholder: "IP V4 Address",
+        info: "IPv4 address"
+      }
+    },
+    MX: {
+      value: {
+        placeholder: "mail exchange server",
+        info: "Mail server that accepts mail"
+      }
+    },
+    TXT: {
+      value: {
+        placeholder: "text data value",
+        info: "Text value"
+      }
+    }
+  }
+
+  return <Form isLoading={isLoading} actions={<ActionPanel>
     <Action.SubmitForm icon={Icon.Plus} title="Add" onSubmit={handleSubmit} />
   </ActionPanel>}>
     <Form.Dropdown title="Type" {...itemProps.type}>
@@ -213,5 +294,18 @@ function CreateDNSRecord({domain, onRecordAdded}: { domain: DomainInfo, onRecord
       <Form.Dropdown.Item title="MX" value="MX" />
       <Form.Dropdown.Item title="TXT" value="TXT" />
     </Form.Dropdown>
+    <Form.Separator />
+    <Form.TextField title="Host" placeholder="@" info="Enter @ to create a record for yourdomain.com. Enter blog to create a record for blog.yourdomain.com." {...itemProps.name} />
+    <Form.TextField title="Value" placeholder={TYPE_SPECIFIC_FIELDS[values.type].value.placeholder} info={TYPE_SPECIFIC_FIELDS[values.type].value.info} {...itemProps.value} />
+    {values.type==="MX" && <Form.TextField title="Preference" placeholder="0 - 65535" info="Preference (distance) number of mail server" {...itemProps.preference} />}
+    <Form.Dropdown title="Time To Live (TTL)" info="Specifies the amount of time in seconds that a DNS record should be cached by a resolver or a caching server before it expires and needs to be refreshed from the authoritative DNS servers" {...itemProps.ttl}>
+      <Form.Dropdown.Item title="60 min" value="3600" />
+      <Form.Dropdown.Item title="30 min" value="1800" />
+      <Form.Dropdown.Item title="20 min" value="1200" />
+      <Form.Dropdown.Item title="5 min" value="300" />
+      <Form.Dropdown.Item title="1 min" value="60" />
+    </Form.Dropdown>
+    <Form.Separator />
+    <Form.Checkbox label="Turn-off conflicts resolution checker and force zone update" {...itemProps.force} />
   </Form>
 }
