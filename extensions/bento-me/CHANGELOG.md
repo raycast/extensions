3e1e165283688a3a7a4301be9a2804ab7da68fa5
pabroux <pierre-alexandrebroux@live.fr>
2025-04-11 12:15:09 +0200

    Update bento-me extension
    - style: add extra line.
    - chore(CHANGELOG): add v1.1.0.
    - chore: update screenshots.
    - chore: update packages.
    - feat: use top-level domain to retrieve favicon and filter on subtitles as well.
    - feat: sort links by top-level domain and add bento profile url.
    - Initial commit

--- extensions/bento-me/CHANGELOG.md
+++ extensions/bento-me/CHANGELOG.md
@@ -1,5 +1,19 @@
 # Bento Extension Changelog
 
+## [1.1.0] - {PR_MERGE_DATE}
+
+### Added
+
+- Filter links on subtitles as well.
+- Use `tldts` to get the domain of a link.
+- Use the top-level domain instead of the full domain to retrieve favicons.
+- Bento-profile url in the `Search Links` command.
+
+### Fixed
+
+- Update screenshots.
+- Sort links by top-level domain.
+
 ## [1.0.0] - 2025-04-04
 
 ### Added
--- extensions/bento-me/src/search-links.tsx
+++ extensions/bento-me/src/search-links.tsx
@@ -14,6 +14,7 @@ import {
   Toast,
 } from "@raycast/api";
 import { getFavicon } from "@raycast/utils";
+import { getDomain } from "tldts";
 import { getLinks } from "./utils/bento-loader";
 import { isValidUrl } from "./utils/url-checker";
 
@@ -30,59 +31,80 @@ const userInterfaceFavicon = Boolean(preferences.userInterfaceFavicon);
  */
 export default function Command() {
   const [entries, setEntries] = useState<string[]>([]);
+  const [filteredEntries, setFilterdEntries] = useState<string[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
     getLinks()
-      .then(setEntries, (e: { message: string }) => {
-        showToast(Toast.Style.Failure, "Error", e.message);
-      })
+      .then(
+        (links) => {
+          setEntries(links);
+          setFilterdEntries(links);
+        },
+        (e: { message: string }) => {
+          showToast(Toast.Style.Failure, "Error", e.message);
+        },
+      )
       .then(() => setIsLoading(false));
   }, []);
 
+  useEffect(() => {
+    setFilterdEntries(entries.filter((item) => item.includes(searchText)));
+  }, [searchText]);
+
   return (
-    <List isLoading={isLoading} searchBarPlaceholder="Search Links in Bento">
-      {entries.map((entry, i) => (
-        <List.Item
-          key={i}
-          icon={
-            userInterfaceFavicon
-              ? isValidUrl(entry)
-                ? getFavicon(entry, { fallback: Icon.QuestionMarkCircle })
-                : { source: Icon.QuestionMarkCircle, tintColor: Color.SecondaryText }
-              : undefined
-          }
-          title={entry}
-          actions={
-            <ActionPanel>
-              <Action
-                title="Paste Link"
-                icon={Icon.BlankDocument}
-                onAction={() => {
-                  Clipboard.paste(entry).then(() => closeMainWindow());
-                }}
-              />
-              <Action
-                title="Copy Link"
-                icon={Icon.Clipboard}
-                shortcut={{ modifiers: ["cmd"], key: "c" }}
-                onAction={() => {
-                  Clipboard.copy(entry);
-                  showHUD("Link has been copied to clipboard");
-                }}
-              />
-              <Action
-                title="Open URL"
-                icon={Icon.Globe}
-                shortcut={{ modifiers: ["cmd"], key: "o" }}
-                onAction={() => {
-                  open(entry);
-                }}
-              />
-            </ActionPanel>
-          }
-        />
-      ))}
+    <List
+      filtering={false}
+      onSearchTextChange={setSearchText}
+      isLoading={isLoading}
+      searchBarPlaceholder="Search Links in Bento"
+    >
+      {filteredEntries.map((entry, i) => {
+        const domain = getDomain(entry);
+        return (
+          <List.Item
+            key={i}
+            icon={
+              userInterfaceFavicon
+                ? isValidUrl(entry)
+                  ? getFavicon(`http://${domain}`, { fallback: Icon.QuestionMarkCircle })
+                  : { source: Icon.QuestionMarkCircle, tintColor: Color.SecondaryText }
+                : undefined
+            }
+            title={domain || entry}
+            subtitle={domain ? entry : undefined}
+            actions={
+              <ActionPanel>
+                <Action
+                  title="Paste Link"
+                  icon={Icon.BlankDocument}
+                  onAction={() => {
+                    Clipboard.paste(entry).then(() => closeMainWindow());
+                  }}
+                />
+                <Action
+                  title="Copy Link"
+                  icon={Icon.Clipboard}
+                  shortcut={{ modifiers: ["cmd"], key: "c" }}
+                  onAction={() => {
+                    Clipboard.copy(entry);
+                    showHUD("Link has been copied to clipboard");
+                  }}
+                />
+                <Action
+                  title="Open URL"
+                  icon={Icon.Globe}
+                  shortcut={{ modifiers: ["cmd"], key: "o" }}
+                  onAction={() => {
+                    open(entry);
+                  }}
+                />
+              </ActionPanel>
+            }
+          />
+        );
+      })}
       <List.EmptyView
         title="No Links Found"
         description={entries.length === 0 ? "Your Bento seems to have no links" : "Try adjusting your search"}
