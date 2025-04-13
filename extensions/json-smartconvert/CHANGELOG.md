0b1bc6713fa92896d59ddfd0af1d19876f609dd0
Sachin Chavan <chavansachin295@gmail.com>
2025-04-13 14:16:57 +0400

    Add json-smartconvert extension
    - add npm script to pull change
    - update  changelog

---
+++ extensions/json-smartconvert/CHANGELOG.md
@@ -0,0 +1,13 @@
+# JSON-SmartConvert Changelog
+
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased] - 2025-04-13
+
+### Added
+
+- v1.0 added initial functionality to convert json (parse/stringify)
---
+++ extensions/json-smartconvert/src/json-smart-convert.tsx
@@ -0,0 +1,26 @@
+import { showToast, Toast, Clipboard } from "@raycast/api";
+
+export default async function Command() {
+  const clipboardText = await Clipboard.readText();
+  if (!clipboardText?.trim()) {
+    await showToast({ style: Toast.Style.Failure, title: "Clipboard is empty" });
+    return;
+  }
+  let result: string;
+  try {
+    if (clipboardText.startsWith("{") || clipboardText.startsWith("[")) {
+      result = JSON.stringify(clipboardText, null, 2);
+      await showToast({ style: Toast.Style.Success, title: "Stringified JSON" });
+    } else {
+      result = JSON.parse(clipboardText);
+      await showToast({ style: Toast.Style.Success, title: "Parsed JSON" });
+    }
+    await Clipboard.copy(result);
+  } catch {
+    await showToast({
+      style: Toast.Style.Failure,
+      title: "Invalid JSON format",
+    });
+    return;
+  }
+}
