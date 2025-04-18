5db377d4a078fbbecd8d59c03298dec74a8aec26
pewpeo <2847698+pewpeo@users.noreply.github.com>
2025-04-18 17:11:16 +0200

    Update raindrop-io extension
    - Update changelog
    - Add option to fetch all results
    - Update changelog
    - Set maximum items for raindrop requests
    - Fix changelog layout
    - Fix private github handle
    - Initial commit

--- extensions/raindrop-io/src/hooks/useBookmarks.ts
+++ extensions/raindrop-io/src/hooks/useBookmarks.ts
@@ -1,21 +1,68 @@
 import { getPreferenceValues, showToast, Toast } from "@raycast/api";
-import { useFetch } from "@raycast/utils";
-import { BookmarksParams, BookmarksResponse } from "../types";
+import { Bookmark, BookmarksParams, BookmarksResponse } from "../types";
+import { useCallback, useEffect, useState } from "react";
 
 export function useBookmarks({ collection, search = "" }: BookmarksParams) {
   const preferences: Preferences = getPreferenceValues();
   const url = new URL(`https://api.raindrop.io/rest/v1/raindrops/${collection}`);
+  const perPage = 50; // maximum value `perpage`: https://developer.raindrop.io/v1/raindrops/multiple#common-parameters
 
   url.searchParams.set("sort", preferences.sortBy ? preferences.sortBy : "-created");
   url.searchParams.set("search", search);
+  url.searchParams.set("perpage", perPage.toString());
 
-  return useFetch<BookmarksResponse>(url.href, {
-    headers: {
-      Authorization: `Bearer ${preferences.token}`,
-    },
-    keepPreviousData: true,
-    onError: () => {
-      showToast(Toast.Style.Failure, "Cannot search bookmark");
-    },
-  });
+  const [bookmarks, setBookmarks] = useState<BookmarksResponse>({ items: [] });
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchAllPages = async () => {
+    let page = 0;
+    let allBookmarks: Bookmark[] = [];
+    let hasMore = true;
+
+    try {
+      while (hasMore) {
+        url.searchParams.set("page", page.toString());
+
+        const response = await fetch(url.href, {
+          headers: {
+            Authorization: `Bearer ${preferences.token}`,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP Error: ${response.status} (${response.statusText})`);
+        }
+
+        const data: BookmarksResponse = await response.json();
+        allBookmarks = allBookmarks.concat(data.items);
+
+        hasMore = data.items.length === perPage && preferences.fetchAllResults;
+        page++;
+      }
+
+      setBookmarks({ items: allBookmarks });
+    } catch (err) {
+      showToast(Toast.Style.Failure, "Cannot fetch bookmarks");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const fetchCallback = useCallback(fetchAllPages, [
+    collection,
+    search,
+    preferences.sortBy,
+    preferences.token,
+    preferences.fetchAllResults,
+  ]);
+
+  useEffect(() => {
+    fetchCallback();
+  }, [fetchCallback]);
+
+  return {
+    isLoading,
+    data: bookmarks,
+    revalidate: fetchAllPages,
+  };
 }
