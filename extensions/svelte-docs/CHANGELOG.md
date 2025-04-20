d424431de026d47543c5afbd16cc6e70c7f4e5dc
Paolo Ricciuti <ricciutipaolo@gmail.com>
2025-04-20 20:59:43 +0200

    Update svelte-docs extension (#18686)
    * Update svelte-docs extension
    
    - Merge branch \'contributions/merge-1745099195515\'
    - Pull contributions
    - feat: add svelte docs AI tool
    
    * chore: add changelog
    
    * chore: add suggestions from code review

--- extensions/svelte-docs/CHANGELOG.md
+++ extensions/svelte-docs/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Svelte Docs Changelog
 
+## [AI command to search the svelte docs] - 2025-04-19
+
+Adds a new AI tool to the svelte extension, allowing users to query the svelte docs through LLMs
+
 ## [Support for new Svelte Omnisite] - 2021-11-04
 
 The new Svelte Omnisite is live and with it the docs are all in one neat place. As of today the extension is updated to fetch from the right place and link to the right place. And even better, you now have the ability to search for the docs of the CLI too, as well as take a look at the Tutorial or some example REPL...all with a couple of clicks within Raycast!
---
+++ extensions/svelte-docs/src/tools/search-svelte-docs.ts
@@ -0,0 +1,18 @@
+import { parse } from "valibot";
+import { response_schema } from "../schema";
+
+type Input = {
+  /**
+   * A space separated list of words to search for in the Svelte documentation. A record will be returned if any of the keyword is found in the text.
+   */
+  query: string;
+};
+
+export default async function search_svelte_docs(input: Input) {
+  const unparsed_svelte_items = await fetch(`https://svelte.dev/content.json`).then((res) => res.json());
+  const svelte_items = parse(response_schema, unparsed_svelte_items);
+  const keywords = input.query.split(" ");
+  return svelte_items?.blocks.filter((block) =>
+    keywords.some((keyword) => block.content.toLowerCase().includes(keyword.toLowerCase())),
+  );
+}
