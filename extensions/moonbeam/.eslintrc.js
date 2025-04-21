2a4fa36fcf42da2e5879149d139b33b7a1a174d9
afroviking <themoseadan@gmail.com>
2025-04-22 00:10:38 +0900

    fix: update ESLint and Prettier configuration

--- extensions/moonbeam/.eslintrc.js
+++ extensions/moonbeam/.eslintrc.js
@@ -1,4 +1,30 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["@raycast/eslint-config"]
+  root: true,
+  extends: [
+    "@raycast/eslint-config",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier"
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "react", "react-hooks"],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off"
+  }
 };
