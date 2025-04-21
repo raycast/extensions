e336875471bc2d4014ebdedf4df8b7346919e0dc
afroviking <themoseadan@gmail.com>
2025-04-22 00:14:46 +0900

    chore: simplify ESLint configuration

--- extensions/moonbeam/.eslintrc.js
+++ extensions/moonbeam/.eslintrc.js
@@ -1,30 +1,4 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  root: true,
-  extends: [
-    "@raycast/eslint-config",
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "prettier"
-  ],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "react", "react-hooks"],
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true
-    }
-  },
-  settings: {
-    react: {
-      version: "detect"
-    }
-  },
-  rules: {
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off"
-  }
+  extends: "@raycast/eslint-config"
 };
