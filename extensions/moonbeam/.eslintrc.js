db256baf8e9b845b86fd1630fc126ccd4c1c1651
afroviking <themoseadan@gmail.com>
2025-04-22 13:09:24 +0900

    chore: update ESLint configuration and dependencies

--- extensions/moonbeam/.eslintrc.js
+++ extensions/moonbeam/.eslintrc.js
@@ -2,13 +2,24 @@
 module.exports = {
   extends: [
     "@raycast/eslint-config",
-    "plugin:@typescript-eslint/recommended"
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended"
   ],
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "react", "react-hooks"],
   rules: {
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }]
+    "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
   }
 };
