f2dc9de89d5d36f976aca1715d9e12f89f0f7ce0
afroviking <themoseadan@gmail.com>
2025-04-22 13:30:50 +0900

    Revert to prev. config

--- extensions/moonbeam/.eslintrc.js
+++ extensions/moonbeam/.eslintrc.js
@@ -1,33 +1,14 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  root: true,
   extends: [
     "@raycast/eslint-config",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended"
+    "plugin:@typescript-eslint/recommended"
   ],
   parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true
-    }
-  },
-  plugins: ["@typescript-eslint", "react", "react-hooks"],
+  plugins: ["@typescript-eslint"],
   rules: {
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off",
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn"
-  },
-  settings: {
-    react: {
-      version: "detect"
-    }
+    "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }]
   }
 };
