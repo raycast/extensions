4f55b89735eac02592a81674507a2b47d2b57ccf
Kevin Batdorf <kbat82@gmail.com>
2025-04-26 13:18:31 +0700

    Update hacker-news-top-stories extension
    - update changelog
    - Merge pull request #2 from KevinBatdorf/add-notification-for-new-story
    - revert
    - remove code commaent
    - revert to 10m
    - remove swift for apple script
    - merge main
    - Pull contributions
    - Merge branch \'main\' into add-notification-for-new-story
    - Add native notification for new stories

--- extensions/hacker-news-top-stories/CHANGELOG.md
+++ extensions/hacker-news-top-stories/CHANGELOG.md
@@ -1,5 +1,10 @@
 # Hacker News Top Stories Changelog
 
+## [Add Notifications Support] - {PR_MERGE_DATE}
+
+Adds an option to show native Mac notifications for new stories.
+Adds an option to show the icon from the story, or the hacker new icon.
+
 ## [Update Mark As Read button] - 2025-04-24
 
 Hides "mark all as read" if there are no stories.
---
+++ extensions/hacker-news-top-stories/src/lib/show-notification.ts
@@ -0,0 +1,56 @@
+import { runAppleScript } from "@raycast/utils";
+import { exec } from "child_process";
+import { promisify } from "util";
+import { Image, environment } from "@raycast/api";
+
+const execAsync = promisify(exec);
+
+type NotificationOptions = {
+  title: string;
+  message: string;
+  icon?: Image.ImageLike;
+  url?: string;
+};
+
+const PATH = [
+  ".",
+  "/bin",
+  "/usr/bin",
+  "/usr/gnu/bin",
+  "/usr/local/bin",
+  "/opt/homebrew/bin",
+  "/opt/homebrew/sbin",
+].join(":");
+
+function shellEscape(str: string) {
+  return `'${str.replace(/'/g, `'\\''`)}'`;
+}
+function extractUrlFromImageLike(favicon?: Image.ImageLike) {
+  if (typeof favicon === "string") return favicon;
+  if (favicon instanceof URL) return favicon.toString();
+  if (typeof favicon === "object" && favicon !== null && "source" in favicon) {
+    const src = (favicon as Image).source;
+    if (typeof src === "string") return src;
+    if (src instanceof URL) return src.toString();
+    if (typeof src === "object" && src !== null && "light" in src && src.light) {
+      // Return light or dark depending on user env
+      return environment.appearance === "dark" ? src.dark : src.light;
+    }
+  }
+  return undefined;
+}
+
+export async function showNotification({ title, message, icon, url }: NotificationOptions) {
+  try {
+    const image = extractUrlFromImageLike(icon);
+    const args = ["-title", shellEscape(title), "-message", shellEscape(message)];
+    if (image) args.push("-contentImage", shellEscape(image));
+    if (url) args.push("-open", shellEscape(url));
+
+    await execAsync(`terminal-notifier ${args.join(" ")}`, { env: { ...process.env, PATH } });
+  } catch {
+    await runAppleScript(`
+      display notification "${message.replace(/"/g, '\\"')}" with title "${title.replace(/"/g, '\\"')}"
+    `);
+  }
+}
