6bad0925867bb9c027ebe86e5481f846d30a0ca8
Matheus Chein <matheuschein@gmail.com>
2025-04-29 10:25:42 -0300

    Apply getHsPath - call hammerspoon cli according to path obtained from user environment

--- extensions/animated-window-manager/src/move-right.ts
+++ extensions/animated-window-manager/src/move-right.ts
@@ -1,11 +1,12 @@
 import { execSync } from "child_process";
 import { closeMainWindow } from "@raycast/api";
 import { showFailureToast } from "@raycast/utils";
+import { getHsPath } from "./utils/hasPath";
 
 export default async function main() {
   try {
     await closeMainWindow();
-    execSync('hs -c "moveWindowRightAnimated()"');
+    execSync(`${getHsPath()} -c "moveWindowRightAnimated()"`);
   } catch (error) {
     await showFailureToast(error, { title: "Is Hammerspoon running?" });
   }
