bfaca714ed6515fd0936edc179e7051f1e2a35dc
Hasan Akay <hasanakay444@gmail.com>
2025-04-30 22:03:27 +0300

    Update search-domain extension
    - Fix code style issues and metadata format
    - Fix screenshot naming format in metadata folder
    - Fix screenshot naming format in metadata folder

--- extensions/search-domain/src/constants.ts
+++ extensions/search-domain/src/constants.ts
@@ -1,3 +1,6 @@
-// Environment-specific configuration
+// API ENDPOINTS
+// Note: This should be moved to environment variables when officially deploying
+// For Raycast Store, we need to keep it here for review purposes
 export const API_ENDPOINT =
+  process.env.DOMAIN_API_ENDPOINT ||
   "https://5qbo4f2ir7.execute-api.eu-west-1.amazonaws.com/default/Search_Domain_For_Raycast_Extension";
