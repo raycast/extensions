b9da06ae8a8aa4ba3205fd6aafd50793186f7321
Hasan Akay <hasanakay444@gmail.com>
2025-05-01 11:36:54 +0300

    Update search-domain extension
    - Fix package.json and dependency versions
    - Fix package.json and dependency versions
    - Fix ESLint dependency and configuration
    - Fix dependency versions and TypeScript compatibility issues
    - Fix: Synchronize package-lock.json with package.json

---
+++ extensions/search-domain/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    extends: ["@raycast/eslint-config"],
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: "module",
+      ecmaFeatures: {
+        jsx: true
+      }
+    },
+    rules: {}
+  }
+  
