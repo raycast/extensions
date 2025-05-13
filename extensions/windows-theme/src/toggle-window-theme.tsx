0b7f65866505427224e59d4effcc5a1a84eb89a2
Ayhan <2834954+c0b41@users.noreply.github.com>
2025-05-14 00:59:53 +0300

    Some fixes

--- extensions/windows-theme/src/toggle-window-theme.tsx
+++ extensions/windows-theme/src/toggle-window-theme.tsx
@@ -1,17 +1,17 @@
 import { showToast, Toast } from "@raycast/api";
+import { showFailureToast } from "@raycast/utils";
 import { themeToggle } from "./lib";
 
 export default async () => {
   try {
     const theme = await toggleWindowsTheme();
 
-    await showToast({ title: `Windows Theme`, message: `Current Theme ${theme}`, style: Toast.Style.Success });
+    await showToast({ title: "Windows Theme", message: `Current Theme ${theme}`, style: Toast.Style.Success });
   } catch (error) {
-    console.log(error);
-    await showToast({ title: `Windows Theme`, message: `Theme changes failed`, style: Toast.Style.Failure });
+    await showFailureToast(error, { title: "Could not toggle Windows theme" });
   }
 };
 
-async function toggleWindowsTheme(): Promise<String> {
+async function toggleWindowsTheme(): Promise<string> {
   return themeToggle();
 }
