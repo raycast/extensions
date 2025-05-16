61449f1094fc8d2bb4f7c1cd4e6cf71ffb10a297
y0ngha <dev.y0ngha@gmail.com>
2025-05-16 09:51:53 +0900

    fix: fixing problems that fail to import all slack channels

--- extensions/slack/src/tools/get-channels.ts
+++ extensions/slack/src/tools/get-channels.ts
@@ -1,24 +1,37 @@
-import { getSlackWebClient } from "../shared/client/WebClient";
-import { withSlackClient } from "../shared/withSlackClient";
+import {getSlackWebClient} from "../shared/client/WebClient";
+import {withSlackClient} from "../shared/withSlackClient";
 
 async function getChannels() {
   const slackWebClient = getSlackWebClient();
-  const channels = await slackWebClient.conversations.list({
-    exclude_archived: true,
-    types: "public_channel,private_channel",
-    limit: 1000,
-  });
+  let allChannels = [];
+  let cursor = null;
 
-  if (channels.error) {
-    throw new Error(channels.error);
-  }
+  do {
+    const response = await slackWebClient.conversations.list({
+      exclude_archived: true,
+      types: "public_channel,private_channel",
+      limit: 1000,
+      cursor: cursor || undefined,
+    });
 
-  return channels.channels?.map((c) => ({
-    id: c.id,
-    name: c.name,
-    purpose: c.purpose?.value,
-    topic: c.topic?.value,
-  }));
+    if (response.error) {
+      throw new Error(response.error);
+    }
+
+    const currentChannels = response.channels?.map((channel) => ({
+      id: channel.id,
+      name: channel.name,
+      purpose: channel.purpose?.value,
+      topic: channel.topic?.value,
+    })) || [];
+
+    allChannels = allChannels.concat(currentChannels);
+    cursor = response.response_metadata?.next_cursor;
+  } while (cursor);
+
+  return allChannels;
 }
+
+
 
 export default withSlackClient(getChannels);
