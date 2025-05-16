9732999b139e0cc091db58f58c246e6a41c9edb5
y0ngha <dev.y0ngha@gmail.com>
2025-05-16 09:50:58 +0900

    feat: add slack channel id validation function

--- extensions/slack/src/shared/utils.ts
+++ extensions/slack/src/shared/utils.ts
@@ -92,10 +92,17 @@ const handleError = async (error: CodedError | Error | unknown, title?: string) 
   return showFailureToast(error, { title: title ?? "Something unexpected happened" });
 };
 
+const isValidChannelId = (channelId) => {
+  const channelIdRegex = /^C[a-zA-Z0-9]{8,}$/;
+
+  return channelIdRegex.test(channelId.trim());
+}
+
 export {
   timeDifference,
   convertTimestampToDate,
   buildScriptEnsuringSlackIsRunning,
   handleError,
   convertSlackEmojiToUnicode,
+  isValidChannelId
 };
