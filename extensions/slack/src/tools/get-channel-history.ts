b2c2e8b6bd1188bb588084be935c03cd5d85f3dd
y0ngha <dev.y0ngha@gmail.com>
2025-05-16 09:52:59 +0900

    fix: fails to find channelId when using get-channel-history when using AI

--- extensions/slack/src/tools/get-channel-history.ts
+++ extensions/slack/src/tools/get-channel-history.ts
@@ -1,5 +1,38 @@
-import { getSlackWebClient } from "../shared/client/WebClient";
-import { withSlackClient } from "../shared/withSlackClient";
+import {getSlackWebClient} from "../shared/client/WebClient";
+import {withSlackClient} from "../shared/withSlackClient";
+import {isValidChannelId} from "../shared/utils";
+
+async function getChannelIdByChannelName(channelName: string) {
+  const slackWebClient = getSlackWebClient();
+  let cursor = null;
+
+  do {
+    const response = await slackWebClient.conversations.list({
+      exclude_archived: true,
+      types: "public_channel,private_channel",
+      limit: 1000,
+      cursor: cursor || undefined,
+    });
+
+    if (response.error) {
+      throw new Error(response.error);
+    }
+
+    if (response.channels) {
+      for (let i = 0; i < response.channels?.length ?? 0; i++) {
+        const channel = response.channels[i];
+        if (channel.name?.includes(channelName)) {
+          return channel.id
+        }
+      }
+
+      cursor = response.response_metadata?.next_cursor;
+    }
+
+  } while (cursor);
+
+  return undefined;
+}
 
 async function getChannelHistory(input: {
   /**
@@ -17,8 +50,25 @@ async function getChannelHistory(input: {
 
   const unixTimestamp = input.after ? Math.floor(new Date(input.after).getTime() / 1000).toString() : undefined;
 
+  /**
+   * There are cases where the channelId is not the channelId, so I will add that condition.
+   * To test for the opposite condition, if you ask @ask slack to 'Summarize the messages posted on May 12th in the #general channel,' this will occur.
+   * If you ask again, you will run getChannels, so the error will occur only the first time.
+   */
+  const channelId = await (async () => {
+    if (isValidChannelId(input.channelId)) {
+      return input.channelId;
+    } else {
+      return await getChannelIdByChannelName(input.channelId)
+    }
+  })()
+
+  if (!channelId) {
+    throw new Error('Not found Channel Id');
+  }
+
   const messages = await slackWebClient.conversations.history({
-    channel: input.channelId,
+    channel: channelId,
     oldest: unixTimestamp,
     limit: 30,
   });
@@ -27,14 +77,12 @@ async function getChannelHistory(input: {
     throw new Error(messages.error);
   }
 
-  const response = messages.messages?.map((message) => ({
+  return messages.messages?.map((message) => ({
     text: message.text,
     user: message.user,
     ts: message.ts,
     date: message.ts ? new Date(parseInt(message.ts, 10) * 1000).toISOString() : undefined,
   }));
-
-  return response;
 }
 
 export default withSlackClient(getChannelHistory);
