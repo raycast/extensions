b82e4717d55153e082e0439840c4b661a28b9555
Dominic Kloecker <d.kloecker@icloud.com>
2025-05-18 23:45:00 +0100

    Update polymarket extension
    - Merge branch \'contributions/merge-1747608287813\'
    - Pull contributions
    - Fix image size
    - Add Market Details
    - Add Market Details ability
    - it do be working
    - show raw json data
    - Initial commit

--- extensions/polymarket/src/constants.ts
+++ extensions/polymarket/src/constants.ts
@@ -1,4 +1,5 @@
 const POLY_GAMMA_URL = "https://gamma-api.polymarket.com/";
 const POLY_REST_URL = "https://polymarket.com/api/";
+const POLY_CLOB_URL = "https://clob.polymarket.com/";
 
-export { POLY_GAMMA_URL, POLY_REST_URL };
+export { POLY_GAMMA_URL, POLY_REST_URL, POLY_CLOB_URL };
