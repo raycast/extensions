451987e208bb73b04b5f3fbdc0d78b91f298acf6
Jack Gardner <gardnerjack11@gmail.com>
2025-05-20 16:42:29 +1000

    Dovetail extension: Pagination with query filtering (#19163)
    * Raycast Dovetail extension: Filtering and pagination
    
    * Remove console.log
    
    * Update CHANGELOG.md and optimise images
    
    ---------
    
    Co-authored-by: raycastbot <bot@raycast.com>

--- extensions/dovetail/src/api/endpoints.ts
+++ extensions/dovetail/src/api/endpoints.ts
@@ -1,76 +1,100 @@
 import { z } from "zod";
 
-export const tokenInfo = {
-  path: "/v1/token-info",
-  method: "GET",
-  schema: z.object({
-    id: z.string(),
-    scopes: z.array(z.string()),
-    expires_at: z.string().datetime(),
-  }),
-};
+export const PageSize = 50;
+export const BaseUrl = "https://dovetail.com/api";
 
-export const insights = {
-  path: "/v1/insights",
-  method: "GET",
-  schema: z.object({
-    data: z.array(
-      z.object({
-        id: z.string(),
-        title: z.string(),
-        type: z.string(),
-        created_at: z.string(),
-        published: z.boolean(),
-      }),
-    ),
-  }),
-};
+export const buildHeaders = (token: string) => ({
+  "Content-Type": "application/json",
+  Authorization: `Bearer ${token}`,
+});
 
-export const contacts = {
-  path: "/v1/contacts",
-  method: "GET",
-  schema: z.object({
-    data: z.array(
-      z.object({
-        id: z.string(),
-        name: z.string().nullable(),
-        created_at: z.string().optional(),
-        deleted: z.boolean().optional(),
-      }),
-    ),
-  }),
-};
+export type EndpointReturnType<Endpoint extends ApiEndpoint> = z.infer<Endpoint["schema"]>;
+export type EndpointData<Endpoint extends ApiEndpoint> = EndpointReturnType<Endpoint>["data"][number];
 
-export const contactDetails = {
-  path: (id: string) => `/v1/contacts/${id}`,
-  method: "GET",
-  schema: z.object({
-    data: z.object({
-      id: z.string(),
-      name: z.string().nullable(),
-      email: z.string().nullable().optional(),
-      created_at: z.string().optional(),
-      deleted: z.boolean().optional(),
-      fields: z.array(z.record(z.any())).optional(),
+const paginationSchemaFactory = <T extends z.ZodTypeAny>(dataSchema: z.ZodArray<T>) =>
+  z.object({
+    data: dataSchema,
+    page: z.object({
+      total_count: z.number(),
+      has_more: z.boolean(),
+      next_cursor: z.string().nullable(),
     }),
-  }),
-};
+  });
 
-export const data = {
-  path: "/v1/data",
-  method: "GET",
-  schema: z.object({
-    data: z.array(
-      z.object({
-        id: z.string(),
-        type: z.string(),
-        title: z.string(),
-        created_at: z.string(),
-        deleted: z.boolean(),
-      }),
+const FieldValueSchema = z.union([z.string(), z.boolean(), z.number(), z.array(z.string())]).nullable();
+const FieldSchema = z.object({ label: z.string(), value: FieldValueSchema });
+
+export type Field = z.infer<typeof FieldSchema>;
+
+export const endpoints = {
+  insights: {
+    path: "/v1/insights",
+    method: "GET",
+    buildFilter: (query: string) => ({
+      title: {
+        contains: query,
+      },
+    }),
+    schema: paginationSchemaFactory(
+      z.array(
+        z.object({
+          id: z.string(),
+          title: z.string(),
+          type: z.literal("insight"),
+          created_at: z.string(),
+          published: z.boolean(),
+        }),
+      ),
     ),
-  }),
+  },
+  contacts: {
+    path: "/v1/contacts",
+    method: "GET",
+    buildFilter: (query: string) => ({
+      name: {
+        contains: query,
+      },
+    }),
+    schema: paginationSchemaFactory(
+      z.array(
+        z.object({
+          id: z.string(),
+          name: z.string().nullable(),
+          created_at: z.string(),
+          fields: z.array(FieldSchema),
+        }),
+      ),
+    ),
+  },
+  data: {
+    path: "/v1/data",
+    method: "GET",
+    buildFilter: (query: string) => ({
+      title: {
+        contains: query,
+      },
+    }),
+    schema: paginationSchemaFactory(
+      z.array(
+        z.object({
+          id: z.string(),
+          type: z.literal("data"),
+          title: z.string(),
+          created_at: z.string(),
+          deleted: z.boolean(),
+        }),
+      ),
+    ),
+  },
 };
 
-export const endpoints = { tokenInfo, insights, contacts, contactDetails, data };
-export type EndpointKey = keyof typeof endpoints;
+export type ApiEndpoint = (typeof endpoints)[keyof typeof endpoints];
+
+export const ExportDataResponse = z.object({
+  id: z.string(),
+  type: z.literal("data"),
+  title: z.string(),
+  created_at: z.string(),
+  deleted: z.boolean(),
+  content_markdown: z.string(),
+});
