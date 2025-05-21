b56fcf75bd3166b89625cffbaf7792af7532ff5e
Andreas Elia <andreas@raycast.com>
2025-05-21 17:39:38 +0100

    fix type

--- extensions/apple-notes/src/tools/search-notes.ts
+++ extensions/apple-notes/src/tools/search-notes.ts
@@ -3,7 +3,7 @@ import { getPreferenceValues } from "@raycast/api";
 import { getNotes } from "../api/getNotes";
 
 export default async function () {
-  const { maxQueryResults } = getPreferenceValues<Preferences.MenuBar>();
+  const { maxQueryResults } = getPreferenceValues();
   const max = parseInt(maxQueryResults) ?? 250;
   const notes = await getNotes(max);
   return notes;
