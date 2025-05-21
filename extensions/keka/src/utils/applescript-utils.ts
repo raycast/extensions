7bb7218c25a05dc7ee3db9f313b3fa16f21d1a11
aone <aone.jga@gmail.com>
2025-05-21 21:12:11 +0200

    Missing showFailureToast

--- extensions/keka/src/utils/applescript-utils.ts
+++ extensions/keka/src/utils/applescript-utils.ts
@@ -1,4 +1,4 @@
-import { runAppleScript } from "@raycast/utils";
+import { showFailureToast, runAppleScript } from "@raycast/utils";
 
 export const scriptCompressFiles = async (filePaths: string[]) => {
   scriptExecuteFiles("compress", filePaths);
