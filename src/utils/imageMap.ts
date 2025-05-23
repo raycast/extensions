207e7ae2977a74a6211250eb1736f447b8aaa493
Abdullah Nettoor <abdullahnettoor@gmail.com>
2025-05-23 11:19:09 +0530

    chore: use local images instead of cdn

---
+++ src/utils/imageMap.ts
@@ -0,0 +1,8 @@
+import { Image } from "@raycast/api";
+
+// utils/imageMap.ts
+export const logoMap: { [key: string]: string } = {
+  go: "../assets/icons/go-original.svg",
+  javascript: "../assets/icons/javascript-original.svg",
+  python: "../assets/icons/python-original.svg",
+};
