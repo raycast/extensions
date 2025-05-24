98e295de79bd731414080eb6ad4b087cc972cfc0
Shashank RM <ugs22047_ece.shashank@cbit.org.in>
2025-05-24 17:51:02 +0530

    Implemented view specific handlers

--- extensions/deepcast/src/components/TranslationView.tsx
+++ extensions/deepcast/src/components/TranslationView.tsx
@@ -1,12 +1,44 @@
-import { Action, ActionPanel, Detail } from "@raycast/api";
+import {
+  Action,
+  ActionPanel,
+  Detail,
+  getPreferenceValues,
+  closeMainWindow,
+  Clipboard,
+  showToast,
+  Toast,
+} from "@raycast/api";
 import { SourceLanguage, source_languages } from "../utils";
 
 export const TranslationView = (props: { translation: string | null; sourceLanguage?: string }) => {
   const translation = props.translation;
   const sourceLanguage = source_languages[props.sourceLanguage as SourceLanguage] ?? "unknown language";
   const sourceLanguageMessage = `Translated from ${sourceLanguage}`;
+  const { closeRaycastAfterTranslation } = getPreferenceValues<Preferences>();
 
   if (!translation) return null;
+
+  const handleCopyToClipboard = async () => {
+    await Clipboard.copy(translation);
+    await showToast(Toast.Style.Success, "Translation copied to clipboard!");
+    if (closeRaycastAfterTranslation) {
+      // Add a short delay so users can see the success message
+      setTimeout(async () => {
+        await closeMainWindow();
+      }, 1000);
+    }
+  };
+
+  const handlePasteInFrontmostApp = async () => {
+    await Clipboard.paste(translation);
+    await showToast(Toast.Style.Success, "Translation pasted!");
+    if (closeRaycastAfterTranslation) {
+      // Add a short delay so users can see the success message
+      setTimeout(async () => {
+        await closeMainWindow();
+      }, 1000);
+    }
+  };
 
   return (
     <Detail
@@ -14,8 +46,8 @@ export const TranslationView = (props: { translation: string | null; sourceLangu
       markdown={translation}
       actions={
         <ActionPanel>
-          <Action.CopyToClipboard title="Copy to Clipboard" content={translation} />
-          <Action.Paste title="Paste in Frontmost App" content={translation} />
+          <Action title="Copy to Clipboard" onAction={handleCopyToClipboard} />
+          <Action title="Paste in Frontmost App" onAction={handlePasteInFrontmostApp} />
         </ActionPanel>
       }
     />
