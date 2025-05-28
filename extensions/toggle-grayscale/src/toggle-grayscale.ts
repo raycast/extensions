7a14705e24a17d502a017e8bf93086ca7eb99432
Erlend Tryti Lindseth <etl@dhcp-10-24-0-149.wlan.ntnu.no>
2025-05-28 16:53:06 +0200

    Fix: Learn Danish

--- extensions/toggle-grayscale/src/toggle-grayscale.ts
+++ extensions/toggle-grayscale/src/toggle-grayscale.ts
@@ -21,7 +21,7 @@ tell application "System Events"
 		end repeat
 		
 		-- Toggle Color filters
-		click checkbox "Color filters" of group 5 of scroll area 1 of group 1 of group 2 of splitter group 1 of group 1 of window 1
+		click checkbox 1 of group 5 of scroll area 1 of group 1 of group 2 of splitter group 1 of group 1 of window 1
 	end tell
 end tell
 
