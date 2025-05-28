c848aca79a83fb519b004cb8a5c0329aa69b89b5
Per Nielsen Tikær <per@raycast.com>
2025-05-28 08:11:56 +0200

    Update vpnHelper.ts

--- extensions/mozilla-vpn/src/utils/vpnHelper.ts
+++ extensions/mozilla-vpn/src/utils/vpnHelper.ts
@@ -6,7 +6,7 @@ export const isMozillaVpnInstalled = (): boolean => {
   try {
     return fs.existsSync(MOZILLA_VPN_PATH);
   } catch (error) {
-    console.error('Error checking Mozilla Vpn installation:', error);
+    console.error('Error checking Mozilla VPN installation:', error);
     return false;
   }
 };
