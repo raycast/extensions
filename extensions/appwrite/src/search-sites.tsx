375658e129de0ecf00b7719c98604da5cba76342
xmok <30526133+xmok@users.noreply.github.com>
2025-05-28 07:38:57 +0500

    [Appwrite] add icon

--- extensions/appwrite/src/search-sites.tsx
+++ extensions/appwrite/src/search-sites.tsx
@@ -1,7 +1,7 @@
 import { getPreferenceValues, List } from "@raycast/api";
 import { useCachedPromise } from "@raycast/utils";
-// const sdk = require('node-appwrite');
-import sdk from 'node-appwrite';
+const sdk = require('node-appwrite');
+// import sdk from 'node-appwrite';
 
 const { endpoint, project, key } = getPreferenceValues<Preferences>();
 export default function SearchSites() {
@@ -13,9 +13,10 @@ export default function SearchSites() {
                 .setKey(key)
                 // .setSelfSigned() // Use only on dev mode with a self-signed SSL cert
             ;
-            const databases = new sdk.Databases(client)
-            const res = await databases.list();
-            return res.databases;
+            const sites = new sdk.Sites(client);
+            // const databases = new sdk.Databases(client)
+            const res = await sites.list();
+            return res.sites;
     }, [], {
         initialData: []
     })
