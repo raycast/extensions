2826daf1f339e49e6da5ea9e37866274c4f18b26
xmok <30526133+xmok@users.noreply.github.com>
2025-05-30 09:56:25 +0500

    [Appwrite] update ray deps

--- extensions/appwrite/src/sdk.tsx
+++ extensions/appwrite/src/sdk.tsx
@@ -7,12 +7,14 @@ const databases = new sdk.Databases(client);
 const storage = new sdk.Storage(client);
 const users = new sdk.Users(client);
 const sites = new sdk.Sites(client);
+
 export const SDKContext = createContext<{
   databases: sdk.Databases;
   storage: sdk.Storage;
   users: sdk.Users;
   sites: sdk.Sites;
 }>({ databases, storage, users, sites });
+
 export function SDKProvider({ project, children }: { project: Project; children: JSX.Element }) {
   client.setEndpoint(project.endpoint).setProject(project.id).setKey(project.key);
 
