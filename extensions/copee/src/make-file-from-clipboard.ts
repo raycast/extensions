f73e3e6cb5e08ea1642d76af17a957fde613ec84
Gianpiero <gianpispi@me.com>
2025-06-01 15:31:39 +0200

    Re-add make file

--- extensions/copee/src/make-file-from-clipboard.ts
+++
@@ -1,6 +0,0 @@
-import { Clipboard } from "@raycast/api";
-import { handleTextToFile } from "./api/helpers";
-
-export default async function main() {
-  await handleTextToFile(Clipboard.readText, Clipboard.copy);
-}
