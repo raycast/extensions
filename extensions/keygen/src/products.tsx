f69f03ad973d37fdf42c9e5f586bfcc004c3904f
xmok <30526133+xmok@users.noreply.github.com>
2025-06-05 08:18:54 +0500

    [Keygen] icon + basic List

--- extensions/keygen/src/products.tsx
+++ extensions/keygen/src/products.tsx
@@ -1,11 +1,35 @@
-import { getPreferenceValues, List } from "@raycast/api";
+import { getPreferenceValues, Icon, List } from "@raycast/api";
 import { useFetch } from "@raycast/utils";
 
+enum LicenseStatus {
+  ACTIVE="ACTIVE",
+  INACTIVE="INACTIVE",
+  EXPIRING="EXPIRING",
+  EXPIRED="EXPIRED",
+  SUSPENDED="SUSPENDED",
+  BANNED="BANNED",
+}
 interface License {
   id: string;
   attributes: {
+    name: string | null;
     key: string;
+    status: LicenseStatus;
+    created: string;
+    updated: string;
   }
+}
+interface Result<T> {
+  data: T;
+}
+
+interface Error {
+  title: string;
+  detail: string;
+  code?: string;
+}
+interface ErrorResult {
+  errors: Error[];
 }
 
 export default function Command() {
@@ -18,21 +42,31 @@ export default function Command() {
     },
     async parseResponse(response) {
       if (!response.ok) {
-        const err: { errors: Array<{ title: string; detail: string; code: string }> } = await response.json();
+        const err: ErrorResult = await response.json();
         throw new Error(err.errors[0].detail);
       }
       const result = await response.json();
       return result;
     },
-    mapResult(result: { data: License[] }) {
+    mapResult(result: Result<License[]>) {
       return {
         data: result.data
       }
     },
-    initialData: []
-  })
+    initialData: [],
+    execute: false 
+  }) 
 
-  return <List isLoading={isLoading}>
-    {licenses.map(license => <List.Item key={license.id} title={license.attributes.key} />)}
+  return <List isLoading={isLoading} isShowingDetail>
+    {licenses.map(license => <List.Item key={license.id} icon={Icon.Dot} title={license.id.slice(0, 8)} detail={<List.Item.Detail metadata={<List.Item.Detail.Metadata>
+      <List.Item.Detail.Metadata.Label title="Resource" />
+      <List.Item.Detail.Metadata.Label title="ID" text={license.id} />
+      <List.Item.Detail.Metadata.Label title="Created" text={license.attributes.created} />
+      <List.Item.Detail.Metadata.Label title="Updated" text={license.attributes.updated} />
+      <List.Item.Detail.Metadata.Separator />
+
+      <List.Item.Detail.Metadata.Label title="Attributes" />
+      <List.Item.Detail.Metadata.Label title="Name" text={license.attributes.name || "--"} />
+    </List.Item.Detail.Metadata>} />} />)}
   </List>
 }
