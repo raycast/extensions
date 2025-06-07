86c55c29cbba419421c56ef07eb5bf45d8107baf
Kamil Drzastwa <drzastwakamil@gmail.com>
2025-06-07 18:35:26 +0200

    Update clean-keyboard extension
    - Refactor keyboard-status component by removing unused import and clean up utils.ts by removing unnecessary whitespace.
    - Update package.json to add contributor and new keyboard status command; refactor clean-keyboard.tsx to use mock handlers and integrate keyboard locking functionality; implement setKeyboardLocked utility in utils.ts for managing keyboard lock state.
    - Initial commit

--- extensions/clean-keyboard/src/clean-keyboard.tsx
+++ extensions/clean-keyboard/src/clean-keyboard.tsx
@@ -1,6 +1,14 @@
 import { Action, ActionPanel, Icon, List, Toast } from "@raycast/api";
 import { useEffect, useState } from "react";
-import { handler, stopHandler } from "swift:../swift/MyExecutable";
+// import { handler, stopHandler } from "swift:../swift/MyExecutable";
+import { setKeyboardLocked } from "./utils";
+// we need this mocks for now because we don't have a swift executable
+const handler = (seconds: number) => {
+  console.log("handler", seconds);
+};
+const stopHandler = () => {
+  console.log("stopHandler");
+};
 
 interface Duration {
   display: string;
@@ -56,12 +64,13 @@ export default function Command() {
   const [timeLeft, setTimeLeft] = useState<number | null>(null);
   const [icon, setIcon] = useState<string | null>(null);
 
-  const selectDuration = (duration: Duration) => {
+  const selectDuration = async (duration: Duration) => {
     const lockToast = new Toast({ title: "Keyboard locked" });
     handler(duration.seconds);
     setTimeLeft(duration.seconds);
     setIcon(duration.icon);
     setIsRunning(true);
+    await setKeyboardLocked(true);
     lockToast.show();
   };
 
@@ -71,11 +80,20 @@ export default function Command() {
         setTimeLeft(timeLeft - 1);
         if (timeLeft - 1 === 0) {
           setIsRunning(false);
+          setKeyboardLocked(false);
         }
       }
     }, 1000);
     return () => clearInterval(interval);
   }, [isRunning, timeLeft]);
+
+  useEffect(() => {
+    return () => {
+      if (isRunning) {
+        setKeyboardLocked(false);
+      }
+    };
+  }, [isRunning]);
 
   if (isRunning) {
     return (
@@ -91,9 +109,10 @@ export default function Command() {
                 autoFocus={false}
                 title={"Unlock Keyboard"}
                 shortcut={{ modifiers: ["ctrl"], key: "u" }}
-                onAction={() => {
+                onAction={async () => {
                   stopHandler();
                   setIsRunning(false);
+                  await setKeyboardLocked(false);
                 }}
               />
             </ActionPanel>
