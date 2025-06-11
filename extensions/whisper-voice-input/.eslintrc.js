7957ee2c578959c9b12468db05a3e43ae970d238
KOCHI <fantasybz@gmail.com>
2025-06-11 21:43:04 +0800

    Update extensions/whisper-voice-input/.eslintrc.js
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>

--- extensions/whisper-voice-input/.eslintrc.js
+++ extensions/whisper-voice-input/.eslintrc.js
@@ -1,20 +1,4 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended'
-  ],
-  env: {
-    node: true,
-    es2021: true,
-  },
-  parserOptions: {
-    ecmaVersion: 2021,
-    sourceType: 'module',
-    ecmaFeatures: { jsx: true },
-  },
-  rules: {},
-  ignorePatterns: ['dist/', 'node_modules/'],
-}; 
+  extends: ["@raycast"]
+};
