b3aa6c8fd0619b53dfce1d5a6cbe470f2f0255d1
xmok <30526133+xmok@users.noreply.github.com>
2025-06-13 17:58:54 +0500

    [ClickUp] update metadata image

--- extensions/clickup/src/components/OpenInClickupAction.tsx
+++ extensions/clickup/src/components/OpenInClickupAction.tsx
@@ -4,7 +4,8 @@ function OpenInClickUpAction({ route, override = false }: { route: string; overr
   return (
     <Action.OpenInBrowser
       icon="clickup-icon.png"
-      title="Open in Clickup"
+      // eslint-disable-next-line @raycast/prefer-title-case
+      title="Open in ClickUp"
       url={override ? route : `https://app.clickup.com/${route}`}
       shortcut={Keyboard.Shortcut.Common.Open}
     />
