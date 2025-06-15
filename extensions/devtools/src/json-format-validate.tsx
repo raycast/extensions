5dca7ff4cc0c8379b71bc75c2eaee51b60d87816
Huy Tran <tranquanghuy341@gmail.com>
2025-06-15 21:37:48 +0700

    Update extensions/devtools/src/json-format-validate.tsx
    Co-authored-by: Yen Cheng Lin <lin0905580802@gmail.com>

--- extensions/devtools/src/json-format-validate.tsx
+++ extensions/devtools/src/json-format-validate.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { ActionPanel, Action, Form, showToast, Toast } from "@raycast/api";
 
 export default function Command() {
