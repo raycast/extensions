121caf3014f4cc667807dec7bb3037504eff6b3e
Olivier Gilquin <olivier.gilquin@infosupport.com>
2025-06-15 02:23:23 +0200

    Update docker extension
    - Group containers by state in the container list view
    - Initial commit

--- extensions/docker/src/container_list.tsx
+++ extensions/docker/src/container_list.tsx
@@ -7,6 +7,7 @@ import { useDockerode } from './docker/dockerode';
 import { formatContainerError } from './docker/error';
 import ErrorDetail from './error_detail';
 import { withToast } from './ui/toast';
+import { groupByToArray } from './utils/group_by';
 
 export default function ContainerList(props: { projectFilter?: string }) {
   const docker = useDockerode();
@@ -24,115 +25,118 @@ export default function ContainerList(props: { projectFilter?: string }) {
     stopAndRemoveContainer,
   } = useContainers();
 
-  if (error) {
-    return <ErrorDetail error={error} />;
-  }
+  if (error) return <ErrorDetail error={error} />;
 
   return (
     <List isLoading={isLoading}>
-      {filterContainers(containers, props.projectFilter)?.map((containerInfo) => {
-        const cName = containerName(containerInfo);
-        return (
-          <List.Item
-            keywords={[containerInfo.Id, cName, containerInfo.Image]}
-            key={containerInfo.Id}
-            title={cName}
-            subtitle={containerInfo.Image}
-            accessories={[{ text: { value: containerInfo.State } }]}
-            icon={
-              isContainerRunning(containerInfo)
-                ? { source: 'icon-container-running.png', tintColor: Color.Green }
-                : { source: 'icon-container.png', tintColor: Color.SecondaryText }
-            }
-            actions={
-              <ActionPanel>
-                {isContainerRunning(containerInfo) && (
-                  <Action
-                    title="Stop Container"
-                    shortcut={{ modifiers: ['cmd', 'shift'], key: 'w' }}
-                    icon={{ source: 'icon-stop.png', tintColor: Color.PrimaryText }}
-                    onAction={withToast({
-                      action: () => stopContainer(containerInfo),
-                      onSuccess: () => `Container ${cName} stopped`,
-                      onFailure: (error) => formatContainerError(error, containerInfo),
-                    })}
+      {groupByToArray(filterContainers(containers, props.projectFilter), (x) => x.State)
+        .reverse()
+        .map(([state, entries]) => {
+          return (
+            <List.Section key={state} title={state}>
+              {entries.map((containerInfo) => {
+                const cName = containerName(containerInfo);
+                return (
+                  <List.Item
+                    key={containerInfo.Id}
+                    keywords={[containerInfo.Id, cName, containerInfo.Image]}
+                    title={cName}
+                    accessories={[{ text: { value: containerInfo.Image } }]}
+                    icon={
+                      isContainerRunning(containerInfo)
+                        ? { source: 'icon-container-running.png', tintColor: Color.Green }
+                        : { source: 'icon-container.png', tintColor: Color.SecondaryText }
+                    }
+                    actions={
+                      <ActionPanel>
+                        {isContainerRunning(containerInfo) && (
+                          <Action
+                            title="Stop Container"
+                            shortcut={{ modifiers: ['cmd', 'shift'], key: 'w' }}
+                            icon={{ source: 'icon-stop.png', tintColor: Color.PrimaryText }}
+                            onAction={withToast({
+                              action: () => stopContainer(containerInfo),
+                              onSuccess: () => `Container ${cName} stopped`,
+                              onFailure: (error) => formatContainerError(error, containerInfo),
+                            })}
+                          />
+                        )}
+                        {isContainerRunning(containerInfo) && (
+                          <Action
+                            title="Restart Container"
+                            icon={Icon.ArrowClockwise}
+                            shortcut={{ modifiers: ['opt'], key: 'r' }}
+                            onAction={withToast({
+                              action: () => restartContainer(containerInfo),
+                              onSuccess: () => `Container ${cName} restarted`,
+                              onFailure: (error) => formatContainerError(error, containerInfo),
+                            })}
+                          />
+                        )}
+                        {isContainerRunning(containerInfo) && (
+                          <Action.CopyToClipboard
+                            title="Copy Container Id"
+                            shortcut={{ modifiers: ['cmd', 'shift'], key: 'c' }}
+                            content={containerInfo.Id}
+                          />
+                        )}
+                        {!isContainerRunning(containerInfo) && (
+                          <Action
+                            title="Start Container"
+                            shortcut={{ modifiers: ['cmd', 'shift'], key: 'r' }}
+                            icon={{ source: 'icon-start.png', tintColor: Color.PrimaryText }}
+                            onAction={withToast({
+                              action: () => startContainer(containerInfo),
+                              onSuccess: () => `Container ${cName} started`,
+                              onFailure: (error) => formatContainerError(error, containerInfo),
+                            })}
+                          />
+                        )}
+                        <Action.Push
+                          title="Inspect"
+                          icon={{ source: Icon.Binoculars }}
+                          shortcut={{ modifiers: ['cmd'], key: 'i' }}
+                          target={<ContainerDetail docker={dockerState} containerId={containerInfo.Id} />}
+                        />
+                        {!isContainerRunning(containerInfo) && (
+                          <Action
+                            title="Remove Container"
+                            icon={Icon.Trash}
+                            style={Action.Style.Destructive}
+                            shortcut={Keyboard.Shortcut.Common.Remove}
+                            onAction={withToast({
+                              action: () => removeContainer(containerInfo),
+                              onSuccess: () => `Container ${cName} removed`,
+                              onFailure: (error) => formatContainerError(error, containerInfo),
+                            })}
+                          />
+                        )}
+                        {isContainerRunning(containerInfo) && (
+                          <Action
+                            title="Stop and Remove Container"
+                            icon={Icon.Trash}
+                            style={Action.Style.Destructive}
+                            shortcut={Keyboard.Shortcut.Common.Remove}
+                            onAction={withToast({
+                              action: () => stopAndRemoveContainer(containerInfo),
+                              onSuccess: () => `Container ${cName} stopped and removed`,
+                              onFailure: (error) => formatContainerError(error, containerInfo),
+                            })}
+                          />
+                        )}
+                      </ActionPanel>
+                    }
                   />
-                )}
-                {isContainerRunning(containerInfo) && (
-                  <Action
-                    title="Restart Container"
-                    icon={Icon.ArrowClockwise}
-                    shortcut={{ modifiers: ['opt'], key: 'r' }}
-                    onAction={withToast({
-                      action: () => restartContainer(containerInfo),
-                      onSuccess: () => `Container ${cName} restarted`,
-                      onFailure: (error) => formatContainerError(error, containerInfo),
-                    })}
-                  />
-                )}
-                {isContainerRunning(containerInfo) && (
-                  <Action.CopyToClipboard
-                    title="Copy Container Id"
-                    shortcut={{ modifiers: ['cmd', 'shift'], key: 'c' }}
-                    content={containerInfo.Id}
-                  />
-                )}
-                {!isContainerRunning(containerInfo) && (
-                  <Action
-                    title="Start Container"
-                    shortcut={{ modifiers: ['cmd', 'shift'], key: 'r' }}
-                    icon={{ source: 'icon-start.png', tintColor: Color.PrimaryText }}
-                    onAction={withToast({
-                      action: () => startContainer(containerInfo),
-                      onSuccess: () => `Container ${cName} started`,
-                      onFailure: (error) => formatContainerError(error, containerInfo),
-                    })}
-                  />
-                )}
-                <Action.Push
-                  title="Inspect"
-                  icon={{ source: Icon.Binoculars }}
-                  shortcut={{ modifiers: ['cmd'], key: 'i' }}
-                  target={<ContainerDetail docker={dockerState} containerId={containerInfo.Id} />}
-                />
-                {!isContainerRunning(containerInfo) && (
-                  <Action
-                    title="Remove Container"
-                    icon={Icon.Trash}
-                    style={Action.Style.Destructive}
-                    shortcut={Keyboard.Shortcut.Common.Remove}
-                    onAction={withToast({
-                      action: () => removeContainer(containerInfo),
-                      onSuccess: () => `Container ${cName} removed`,
-                      onFailure: (error) => formatContainerError(error, containerInfo),
-                    })}
-                  />
-                )}
-                {isContainerRunning(containerInfo) && (
-                  <Action
-                    title="Stop and Remove Container"
-                    icon={Icon.Trash}
-                    style={Action.Style.Destructive}
-                    shortcut={Keyboard.Shortcut.Common.Remove}
-                    onAction={withToast({
-                      action: () => stopAndRemoveContainer(containerInfo),
-                      onSuccess: () => `Container ${cName} stopped and removed`,
-                      onFailure: (error) => formatContainerError(error, containerInfo),
-                    })}
-                  />
-                )}
-              </ActionPanel>
-            }
-          />
-        );
-      })}
+                );
+              })}
+            </List.Section>
+          );
+        })}
     </List>
   );
 }
 
 const filterContainers = (containers: ContainerInfo[] | undefined, projectFilter?: string) => {
-  if (projectFilter === undefined || containers === undefined) {
-    return containers;
-  }
+  if (projectFilter === undefined || containers === undefined) return containers ?? [];
   return containers.filter((container) => container.Labels['com.docker.compose.project'] === projectFilter);
 };
