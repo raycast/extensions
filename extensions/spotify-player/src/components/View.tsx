0283c5ed747b4595f45abad07e47e67194379807
Karthik <73416366+Karthikxp@users.noreply.github.com>
2025-06-21 14:11:49 +0530

    Update extensions/spotify-player/src/components/View.tsx
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>

--- extensions/spotify-player/src/components/View.tsx
+++ extensions/spotify-player/src/components/View.tsx
@@ -3,7 +3,7 @@ import { checkSpotifyApp } from "../helpers/isSpotifyInstalled";
 import { withSpotifyClient } from "../helpers/withSpotifyClient";
 
 /**
- * Makes sure that we have a authenticated Spotify client available in the children
+ * Makes sure that we have an authenticated Spotify client available in the children
  */
 export const View = withSpotifyClient(({ children }: { children: React.ReactNode }) => {
   useEffect(() => {
