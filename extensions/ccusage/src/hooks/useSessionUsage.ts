bf8a1ed8c9166e208268a2b677ea4f55fddb8952
nyatinte <takumi.062757@gmail.com>
2025-06-25 01:50:48 +0900

    refactor: replace magic number 5 with DEFAULT_DISPLAY_LIMIT constant

--- extensions/ccusage/src/hooks/useSessionUsage.ts
+++ extensions/ccusage/src/hooks/useSessionUsage.ts
@@ -2,6 +2,8 @@ import { useCCUsageSessionCli } from "./useCCUsageSessionCli";
 import { SessionData, ModelUsage } from "../types/usage-types";
 import { getRecentSessions, calculateModelUsage, getTopModels } from "../utils/usage-calculator";
 
+const DEFAULT_DISPLAY_LIMIT = 5;
+
 type SessionUsageResult = {
   sessions: SessionData[];
   recentSessions: SessionData[];
@@ -15,8 +17,8 @@ export const useSessionUsage = (): SessionUsageResult => {
   const { data: rawData, isLoading, error, revalidate } = useCCUsageSessionCli();
 
   const sessions = rawData?.sessions ?? [];
-  const recentSessions = getRecentSessions(sessions, 5);
-  const topModels = sessions.length > 0 ? getTopModels(calculateModelUsage(sessions), 5) : [];
+  const recentSessions = getRecentSessions(sessions, DEFAULT_DISPLAY_LIMIT);
+  const topModels = sessions.length > 0 ? getTopModels(calculateModelUsage(sessions), DEFAULT_DISPLAY_LIMIT) : [];
 
   return {
     sessions,
