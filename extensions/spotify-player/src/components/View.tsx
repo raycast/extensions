4049e6b6b8cfacb68b92f061b9f016b53fc11d0f
Karthik <73416366+Karthikxp@users.noreply.github.com>
2025-06-26 16:24:47 +0530

    Update View.tsx

--- extensions/spotify-player/src/components/View.tsx
+++ extensions/spotify-player/src/components/View.tsx
@@ -3,9 +3,9 @@ import { checkSpotifyApp } from "../helpers/isSpotifyInstalled";
 import { withSpotifyClient } from "../helpers/withSpotifyClient";
 
 /**
- * Makes sure that we have an authenticated Spotify client available in the children
+ * Makes sure that we have a authenticated Spotify client available in the children
  */
-export const View = withSpotifyClient(({ children }: { children: React.ReactNode }) => {
+export const View = withSpotifyClient(({ children }: { children: JSX.Element }) => {
   useEffect(() => {
     checkSpotifyApp();
   }, []);
