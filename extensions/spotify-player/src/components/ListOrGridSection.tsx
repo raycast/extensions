481bbcc16839642b8cfabfccdafe0a6e561e7c92
Karthik <73416366+Karthikxp@users.noreply.github.com>
2025-06-26 16:31:56 +0530

    Update ListOrGridSection.tsx

--- extensions/spotify-player/src/components/ListOrGridSection.tsx
+++ extensions/spotify-player/src/components/ListOrGridSection.tsx
@@ -3,11 +3,9 @@ import { Grid, List } from "@raycast/api";
 type ListOrGridSectionProps = (List.Section.Props | Grid.Section.Props) & {
   type: "list" | "grid";
 };
-export function ListOrGridSection(props: ListOrGridSectionProps) {
+export function ListOrGridSection<T>(props: ListOrGridSectionProps, context?: T) {
   const { type, ...sectionProps } = props;
-  return type === "list" ? (
-    <List.Section {...(sectionProps as List.Section.Props)} />
-  ) : (
-    <Grid.Section {...(sectionProps as Grid.Section.Props)} />
-  );
+  return type === "list"
+    ? List.Section(sectionProps as List.Section.Props, context)
+    : Grid.Section(sectionProps as Grid.Section.Props, context);
 }
