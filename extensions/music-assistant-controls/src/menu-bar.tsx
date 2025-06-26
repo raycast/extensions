69be3a45b73ed15d6a0268392f367cebc6d5051e
yoerivd <yoeri.vandamme@gmail.com>
2025-06-26 14:46:25 +0200

    Update music-assistant-controls extension
    - linting
    - remove test id
    - more robut api

--- extensions/music-assistant-controls/src/menu-bar.tsx
+++ extensions/music-assistant-controls/src/menu-bar.tsx
@@ -2,19 +2,32 @@ import { getPreferenceValues, Icon, MenuBarExtra } from "@raycast/api";
 import { Prefs } from "./preferences";
 import executeApiCommand from "./api-command";
 import { usePromise } from "@raycast/utils";
-import { PlayerMedia, PlayerState } from "./interfaces";
+import { PlayerState } from "./interfaces";
 
 export default function Command() {
   const { host, playerId } = getPreferenceValues<Prefs>();
-
   const { isLoading, data, revalidate } = usePromise(
     async (host: string, playerId: string) =>
-      await executeApiCommand(host, async (api) => await api.getPlayer(playerId)),
+      await executeApiCommand(host, async (api) => {
+        const player = await api.getPlayer(playerId);
+        const artist = player.current_media?.artist;
+        const song = player.current_media?.title;
+        let title = `${artist} - ${song}`;
+        if (!artist || !song) {
+          const queues = await api.getPlayerQueues();
+          const queue = queues.filter((q) => q.queue_id == playerId)[0];
+          if (queue.current_item) {
+            title = queue.current_item?.name;
+          }
+        }
+
+        return {
+          player,
+          title,
+        };
+      }),
     [host, playerId],
   );
-
-  const format = (current: PlayerMedia | undefined) =>
-    current?.artist && current.title ? `${current.artist} - ${current.title}` : "";
 
   const next = async () =>
     await executeApiCommand(host, async (api) => {
@@ -29,12 +42,12 @@ export default function Command() {
     });
 
   return (
-    <MenuBarExtra icon={data?.icon ?? "logo.png"} isLoading={isLoading} title={format(data?.current_media)}>
+    <MenuBarExtra icon="logo.png" isLoading={isLoading} title={data?.title}>
       <MenuBarExtra.Section title="Controls">
         <MenuBarExtra.Item title="Next" icon={Icon.ArrowRight} onAction={next}></MenuBarExtra.Item>
         <MenuBarExtra.Item
-          title={data?.state == PlayerState.PLAYING ? "Pause" : "Play"}
-          icon={data?.state == PlayerState.PLAYING ? Icon.Pause : Icon.Play}
+          title={data?.player.state == PlayerState.PLAYING ? "Pause" : "Play"}
+          icon={data?.player.state == PlayerState.PLAYING ? Icon.Pause : Icon.Play}
           onAction={pause}
         ></MenuBarExtra.Item>
       </MenuBarExtra.Section>
