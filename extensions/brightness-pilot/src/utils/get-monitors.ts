a7882c4f03db3013ea27a190f2308fccb422ed81
giova <100043265+giovacalle@users.noreply.github.com>
2025-06-27 22:17:13 +0200

    fix(brightness-pilot/get-monitors): pr review

--- extensions/brightness-pilot/src/utils/get-monitors.ts
+++ extensions/brightness-pilot/src/utils/get-monitors.ts
@@ -1,15 +1,19 @@
 import { environment } from "@raycast/api";
-import { execSync } from "child_process";
+import { exec } from "child_process";
+import { promisify } from "util";
 import { chmod } from "fs/promises";
 import { join } from "path";
 import { Monitor } from "../types";
+
+const execAsync = promisify(exec);
 
 export async function getMonitors() {
   try {
     const cliUtilPath = join(environment.assetsPath, "brightness-cli");
     await chmod(cliUtilPath, "755");
-    const result = execSync(`${cliUtilPath} detect-displays`).toString();
-    return JSON.parse(result) as Monitor[];
+    const { stdout, stderr } = await execAsync(`${cliUtilPath} detect-displays`);
+    if (stderr) throw new Error(stderr);
+    return JSON.parse(stdout) as Monitor[];
   } catch {
     throw new Error("Something went wrong while trying to get monitors. Please try again later.");
   }
