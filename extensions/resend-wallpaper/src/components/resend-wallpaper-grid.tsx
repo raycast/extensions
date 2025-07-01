8394f0e8537d97e980cea4a8c1923e6b8ea9069e
João Melo <jopcmelo@gmail.com>
2025-07-01 12:08:41 -0300

    feat: add resend wallpapers extension

---
+++ extensions/resend-wallpaper/src/components/resend-wallpaper-grid.tsx
@@ -0,0 +1,62 @@
+import { Color, Grid, Icon } from "@raycast/api";
+import type React from "react";
+import { columns, layout } from "../types/preferences";
+import type { ResendWallpaperWithInfo } from "../types/types";
+import { ActionOnResendWallpaper } from "./action-on-resend-wallpaper";
+import { ResendWallpaperEmptyView } from "./resend-wallpaper-empty-view";
+
+export function ResendWallpaperGrid(props: {
+  resendWallpapers: ResendWallpaperWithInfo[];
+  setRefresh: React.Dispatch<React.SetStateAction<number>>;
+  selectedItem: string;
+  setSelectedItem: React.Dispatch<React.SetStateAction<string>>;
+}) {
+  const { resendWallpapers, setRefresh, selectedItem, setSelectedItem } = props;
+
+  return (
+    <Grid
+      isLoading={resendWallpapers.length === 0}
+      columns={Number.parseInt(columns)}
+      aspectRatio={"16/9"}
+      fit={Grid.Fit.Fill}
+      selectedItemId={selectedItem}
+      onSelectionChange={(selected) => {
+        if (selected) {
+          setSelectedItem(selectedItem);
+        }
+      }}
+      searchBarPlaceholder={"Search wallpapers..."}
+    >
+      <ResendWallpaperEmptyView layout={layout} />
+      {resendWallpapers.map((value, index) => {
+        return (
+          <Grid.Item
+            id={`${index}`}
+            key={`${index}-${value.title}`}
+            content={value.url}
+            title={value.title}
+            actions={
+              <ActionOnResendWallpaper
+                index={index}
+                resendWallpapers={resendWallpapers}
+                setRefresh={setRefresh}
+                setSelectedItem={setSelectedItem}
+              />
+            }
+            accessory={
+              value.exclude
+                ? {
+                    icon: {
+                      source: Icon.XMarkTopRightSquare,
+                      tintColor: Color.SecondaryText,
+                    },
+                    tooltip: "Excluded From Auto Switch",
+                  }
+                : undefined
+            }
+          />
+        );
+      })}
+    </Grid>
+  );
+}
