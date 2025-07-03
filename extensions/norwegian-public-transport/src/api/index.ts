2bc64be675f7aa4def0563636c87a26056114148
Johannes Røsvik <j.rosvik@gmail.com>
2025-07-03 22:34:04 +0200

    Fix ESLint error

--- extensions/norwegian-public-transport/src/api/index.ts
+++ extensions/norwegian-public-transport/src/api/index.ts
@@ -82,6 +82,7 @@ async function fetchJourneyPlannerData<T>(
 }
 
 function mapDepartureQueryKeys(
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   data: any,
   favorites: QuayLineFavorites[],
 ): StopPlaceQuayDeparturesQuery | undefined {
