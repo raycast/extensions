5270f09ee04b20d7198e483edc9dafdf9cd43801
José Rodríguez <jrodalo@gmail.com>
2025-07-11 11:43:26 +0100

    Update Tasklink extension (#20375)
    * Update dependencies
    
    * Migrate to new Eslint config file
    
    * Fix eslint warnings
    
    * Add Vitest to devDependencies
    
    * Test current behaviour
    
    * Extract error handler from commands
    
    * Remove redundant Preference interface
    
    It looks like we can use the generated definition from
    
    * Update Prettier config
    
    * Keep line breaks when replacing the content as HTML
    
    * Update the changelog
    
    * Update Prettier config according to Raycast recommendations
    
    * Update CHANGELOG.md and optimise images
    
    ---------
    
    Co-authored-by: raycastbot <bot@raycast.com>

--- extensions/tasklink/src/issue_id_to_html_link.ts
+++ extensions/tasklink/src/issue_id_to_html_link.ts
@@ -1,5 +1,6 @@
-import { showToast, Clipboard, Toast } from "@raycast/api";
+import { Clipboard } from "@raycast/api";
 import { getPreferences } from "./preferences";
+import { handleErrors } from "./error_handler";
 import { getSelectedTextOfFrontmostApplication, HtmlText, SelectedText } from "./selected_text";
 
 export default async function Command() {
@@ -11,17 +12,9 @@ export default async function Command() {
 
 const convertIssueIdsIntoHtmlLinks = (text: SelectedText): HtmlText => {
   const { format, url } = getPreferences();
-
   return text.convertIssueIdsIntoHtmlLinks(format, url);
 };
 
 const replaceSelectedText = (text: HtmlText): Promise<void> => {
   return Clipboard.paste(text.toClipboardContent());
-};
-
-const handleErrors = () => {
-  return showToast({
-    style: Toast.Style.Failure,
-    title: "No text selected",
-  });
 };
