96a98326298f22b0d4f9c6838ceb94a8c928edba
Per Nielsen Tikær <per@raycast.com>
2025-07-11 15:33:01 +0200

    Update pr-bot.ts (#20376)

--- scripts/bots/pr-bot.ts
+++ scripts/bots/pr-bot.ts
@@ -11,7 +11,7 @@ type API = {
 };
 
 export default async ({ github, context }: API) => {
-  const assignReadyForReviewTo = "pernielsentikaer";
+  const assignReadyForReviewTo = "andreaselia";
 
   if (context.payload.action === "ready_for_review" && !context.payload.pull_request.draft) {
     try {
@@ -45,8 +45,8 @@ export default async ({ github, context }: API) => {
     return;
   }
 
-  // Due to our current reduced availability, the initial review may take up to 10-15 business days.
-  const expectations = "You can expect an initial review within five business days.";
+  // You can expect an initial review within five business days.
+  const expectations = "Due to our current reduced availability during summer, the initial review may take up to 10-15 business days.";
 
   const codeowners = await getCodeOwners({ github, context });
 
