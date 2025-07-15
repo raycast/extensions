91ff7fe847b2dc02629257e61393feae50f70901
d.zachwieja <d.zachwieja@iceo.co>
2025-07-15 06:42:52 +0200

    Change a way to get subscribers count, due to issue

--- extensions/beehiiv/src/menubar-count-subscribers.tsx
+++ extensions/beehiiv/src/menubar-count-subscribers.tsx
@@ -1,32 +1,51 @@
 import { Icon, MenuBarExtra, open, getPreferenceValues } from "@raycast/api";
 import { useFetch } from "@raycast/utils";
 
-interface Preferences {
+type Preferences = {
   apiKey: string;
   publicationId: string;
-}
+};
 
-interface SubscriptionResponse {
-  total_results: number;
-}
+type PublicationResponse = {
+  data: {
+    id: string;
+    name: string;
+    organization_name: string;
+    referral_program_enabled: boolean;
+    created: number;
+    stats: {
+      active_subscriptions: number;
+      active_premium_subscriptions: number;
+      active_free_subscriptions: number;
+      average_open_rate: number;
+      average_click_rate: number;
+      total_sent: number;
+      total_unique_opened: number;
+      total_clicked: number;
+    };
+  };
+};
 
 const preferences = getPreferenceValues<Preferences>();
 
-const BASE_URL = `https://api.beehiiv.com/v2/publications/${preferences.publicationId}/subscriptions?limit=1&status=active`;
+const BASE_URL = `https://api.beehiiv.com/v2/publications/${preferences.publicationId}?expand=stats`;
 
 export default function SubscriberCount() {
-  const { data, isLoading, mutate } = useFetch<SubscriptionResponse>(BASE_URL, {
+  const { data, isLoading, mutate } = useFetch<PublicationResponse>(BASE_URL, {
     headers: {
       Authorization: `Bearer ${preferences.apiKey}`,
       "Content-Type": "application/json",
     },
   });
+
+  console.log(data);
+
   return (
     <MenuBarExtra
       isLoading={isLoading}
       icon={Icon.TwoPeople}
       tooltip="Your Subscribers Count"
-      title={data?.total_results.toLocaleString() ?? ""}
+      title={data?.data.stats.active_subscriptions.toLocaleString() ?? ""}
     >
       <MenuBarExtra.Item
         title="Dashboard"
