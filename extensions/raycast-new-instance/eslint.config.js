f6d7b9892ac413a661311a571c8198b5c54528e7
Caramel <git@caramelfur.dev>
2025-07-15 16:34:32 +0200

    Update raycast-new-instance extension
    - add raycast esconfig
    - update deps
    - Fix comments

--- extensions/raycast-new-instance/eslint.config.js
+++ extensions/raycast-new-instance/eslint.config.js
@@ -1,28 +1,4 @@
-/* eslint-disable no-undef */
-const js = require('@eslint/js');
-const tseslint = require('@typescript-eslint/eslint-plugin');
-const tsparser = require('@typescript-eslint/parser');
+const { defineConfig } = require("eslint/config");
+const raycastConfig = require("@raycast/eslint-config");
 
-module.exports = [
-  js.configs.recommended,
-  {
-    files: ['src/**/*.{ts,tsx}'],
-    languageOptions: {
-      parser: tsparser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
-    },
-    plugins: {
-      '@typescript-eslint': tseslint,
-    },
-    rules: {
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': ['error'],
-    },
-  },
-]; 
+module.exports = defineConfig([...raycastConfig]);
--- extensions/raycast-new-instance/src/utils.ts
+++ extensions/raycast-new-instance/src/utils.ts
@@ -1,27 +1,24 @@
 import { Application, closeMainWindow, showToast, Toast } from "@raycast/api";
 import { showFailureToast } from "@raycast/utils";
-import { exec } from "child_process";
+import { execFile } from "child_process";
 import { promisify } from "util";
 
-export const execAsync = promisify(exec);
+export const execFileAsync = promisify(execFile);
 
 export async function launchNewInstance(application: Application) {
   try {
-    await showToast({
+    await closeMainWindow({ clearRootSearch: true });
+
+    const toast = await showToast({
       style: Toast.Style.Animated,
       title: `Launching new instance of ${application.name}...`,
     });
 
-    // Use shell to execute open -n command for new instance
-    await execAsync(`open -n "${application.path}"`);
+    await execFileAsync("open", ["-n", application.path]);
 
-    await showToast({
-      style: Toast.Style.Success,
-      title: `Launched new instance of ${application.name}`,
-    });
+    toast.style = Toast.Style.Success;
+    toast.title = `Launched new instance of ${application.name}`;
   } catch (error) {
     await showFailureToast(error, { title: "Failed to launch application" });
-  } finally {
-    await closeMainWindow({ clearRootSearch: true });
   }
 }
