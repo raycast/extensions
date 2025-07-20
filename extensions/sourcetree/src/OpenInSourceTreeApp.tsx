7dc1ece349ad7d8b54269836b7fb304311cea6b9
Frankie <1426203851@qq.com>
2025-07-20 17:59:44 +0800

    chore: update deprecated api

--- extensions/sourcetree/src/OpenInSourceTreeApp.tsx
+++ extensions/sourcetree/src/OpenInSourceTreeApp.tsx
@@ -1,4 +1,4 @@
-import { ActionPanel, popToRoot, showToast, Toast } from "@raycast/api";
+import { Action, popToRoot, showToast, Toast } from "@raycast/api";
 import { execSync } from "child_process";
 import { Repository } from "./lib/repository";
 import { Preferences } from "./lib/preferences";
@@ -24,5 +24,5 @@ export function OpenInSourceTreeApp({ repo }: OpenInSourceTreeAppProps) {
     }
   }
 
-  return <ActionPanel.Item title={`Open with Sourcetree`} icon="sourcetree_256x256x32.png" onAction={handleAction} />;
+  return <Action title={`Open with Sourcetree`} icon="sourcetree_256x256x32.png" onAction={handleAction} />;
 }
