9cc340112be42775e21a33164568f289dbcabf23
Frankie <1426203851@qq.com>
2025-07-20 18:25:54 +0800

    chore: update message

--- extensions/sourcetree/src/BinNotAvailable.tsx
+++ extensions/sourcetree/src/BinNotAvailable.tsx
@@ -7,7 +7,7 @@ interface BinNotAvailableProps {
 export function BinNotAvailable({ bin }: BinNotAvailableProps) {
   const message = [
     "# Unable to find Sourcetree command line tools",
-    `The path for the command line tools are currently set to:`,
+    `The path for the command line tools is currently set to:`,
     `\`${bin}\``,
     `If you have installed Sourcetree via Homebrew, you can set the path to \`/opt/homebrew/bin/stree\` in the extension settings.`,
   ];
