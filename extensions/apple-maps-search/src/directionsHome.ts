47b98c39c428320091e961de18f76337b7f82254
Sebastian <sebastian.danielsson@proton.me>
2025-07-30 01:49:32 +0200

    Add Windows support and update dependencies
    Added support for Windows users to open Apple Maps links in the browser. Updated dependencies, switched ESLint config to use @raycast/eslint-config, and updated tsconfig to latest recommended settings. Removed old .eslintrc.json and added eslint.config.js. Minor code changes to support cross-platform compatibility.
    
    Delete test-urls.js

--- extensions/apple-maps-search/src/directionsHome.ts
+++ extensions/apple-maps-search/src/directionsHome.ts
@@ -1,9 +1,8 @@
-import open from "open";
-import { getPreferenceValues } from "@raycast/api";
-import { Preferences, makeDirectionsURL } from "./utils";
+import { getPreferenceValues, open } from "@raycast/api";
+import { Preferences, makePlatformDirectionsURL } from "./utils";
 
 export default async () => {
   const preferences: Preferences = getPreferenceValues();
-  const dirURL = makeDirectionsURL("", preferences.homeAddress, preferences.preferredMode);
-  open(dirURL);
+  const dirURL = makePlatformDirectionsURL("", preferences.homeAddress, preferences.preferredMode);
+  await open(dirURL);
 };
