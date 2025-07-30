9ea75503598b67ab7807e151e1ac95e23757bf55
LAZIC Erwan <115077240+MonsPropre@users.noreply.github.com>
2025-07-30 17:01:49 +0200

    Update extensions/cobalt-for-raycast/src/about.tsx
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>

--- extensions/cobalt-for-raycast/src/about.tsx
+++ extensions/cobalt-for-raycast/src/about.tsx
@@ -6,7 +6,7 @@ export default function Command() {
 Cobalt for Raycast is an extension that allows you to use [cobalt.tools](https://cobalt.tools) through Raycast.
 
 ## Licensing
-Cobalt for Raycast is available under the [MIT License](https://github.com/MonsPropre/cobalt-for-raycast/blob/main/LICENSE) and use the cobalt.tools API [by imput.net](imput.net) which is available under the [AGPL-3.0 License](https://github.com/imputnet/cobalt/blob/main/LICENSE).`
+Cobalt for Raycast is available under the [MIT License](https://github.com/MonsPropre/cobalt-for-raycast/blob/main/LICENSE) and uses the cobalt.tools API [by imput.net](https://imput.net) which is available under the [AGPL-3.0 License](https://github.com/imputnet/cobalt/blob/main/LICENSE).`
 
   return <Detail markdown={markdown} />
 }
