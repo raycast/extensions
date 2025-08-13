25352e823cc5accb6948c91b87f0a7d5abd1d96d
Ben Drucker <bvdrucker@gmail.com>
2025-08-13 03:21:42 -0700

    tabler: add download actions (#20704)
    * trigger actions re-run
    
    * tabler: add download actions
    
    - Add download actions for outline and filled SVG icons to ~/Downloads
    - Breaking: Cmd+Enter now downloads the outline SVG icon
    - Add Option to keyboard shortcuts for filled version when available
    - Fixed an old exact version dependency on React types
    
    * update screenshot
    
    * use cached SVG data for downloads
    
    - Update downloadSVG function to use already fetched SVG content
    - Remove unnecessary HTTP requests during downloads
    - More efficient and faster downloads
    
    * lint
    
    * Update CHANGELOG.md and optimise images
    
    ---------
    
    Co-authored-by: raycastbot <bot@raycast.com>

--- extensions/tabler/src/index.tsx
+++ extensions/tabler/src/index.tsx
@@ -1,5 +1,8 @@
-import { Action, ActionPanel, Grid, Color } from "@raycast/api";
-import { useFetch } from "@raycast/utils";
+import { Action, ActionPanel, Grid, Color, showToast, Toast, showInFinder } from "@raycast/api";
+import { useFetch, showFailureToast } from "@raycast/utils";
+import { writeFile } from "fs/promises";
+import { homedir } from "os";
+import { join } from "path";
 
 type Styles = {
   outline?: { version: string; unicode: string; svg: string };
@@ -19,6 +22,25 @@ type TablerIcon = {
 
 const Outline = "https://raw.githubusercontent.com/tabler/tabler-icons/master/icons/outline/";
 const Filled = "https://raw.githubusercontent.com/tabler/tabler-icons/master/icons/filled/";
+
+const downloadSVG = async (svgContent: string, name: string) => {
+  const filename = `${name}.svg`;
+  const path = join(homedir(), "Downloads", filename);
+
+  try {
+    const toast = await showToast(Toast.Style.Animated, "Downloading Icon", "Please wait...");
+
+    await writeFile(path, svgContent);
+
+    toast.title = "Downloaded";
+    toast.message = filename;
+    toast.style = Toast.Style.Success;
+
+    await showInFinder(path);
+  } catch (error) {
+    await showFailureToast(error, { title: "Download Failed" });
+  }
+};
 
 export default function Command() {
   const { isLoading, data } = useFetch("https://tabler.io/api/icons", {
@@ -65,11 +87,27 @@ export default function Command() {
                       icon={Outline + tablerIcon.name + ".svg"}
                     />
                   )}
+                  {outline && (
+                    <Action
+                      title="Download Outline SVG"
+                      icon={Outline + tablerIcon.name + ".svg"}
+                      onAction={() => downloadSVG(outline, tablerIcon.name)}
+                    />
+                  )}
                   {filled && (
                     <Action.CopyToClipboard
                       title="Copy Filled SVG"
                       content={filled}
                       icon={Filled + tablerIcon.name + ".svg"}
+                      shortcut={{ modifiers: ["opt"], key: "return" }}
+                    />
+                  )}
+                  {filled && (
+                    <Action
+                      title="Download Filled SVG"
+                      icon={Filled + tablerIcon.name + ".svg"}
+                      onAction={() => downloadSVG(filled, tablerIcon.name)}
+                      shortcut={{ modifiers: ["cmd", "opt"], key: "return" }}
                     />
                   )}
                   <Action.CopyToClipboard
