e791d80f737f9e7685e53f69a46ccce5d39cb19a
xmok <30526133+xmok@users.noreply.github.com>
2025-08-19 05:14:24 +0500

    [Console Dev] rewrite

--- extensions/consoledev/CHANGELOG.md
+++ extensions/consoledev/CHANGELOG.md
@@ -1,5 +1,14 @@
 # Consoledev changelog
 
+## [Major Rewrite + Support Windows] - {PR_MERGE_DATE}
+- Description shown in Detail panel w/ Markdown formatting
+- Simplify parser to directly expose objects
+- Use Raycast hook (`useCachedPromise`) to benefit from caching
+- Remove many deps (_adios_ `axios`)
+- Modernize to use latest Raycast configuration
+- Add **metadata** images
+- Add **README.md**
+
 ## [Bugfix] - 2024-05-16
 - Fix parsing process
 - Show raycast icons for the list items
--- extensions/consoledev/src/xml.ts
+++ extensions/consoledev/src/xml.ts
@@ -1,40 +1,14 @@
-import { promisify } from "util";
-import { parseString } from "xml2js";
-import { Feed, FeedItemInterface } from "./responseTypes";
+import { parseStringPromise } from "xml2js";
+import { Feed, FeedItemInterface, FeedResponse } from "./responseTypes";
 
-const parseXml = promisify(parseString) as (xml: string) => Promise<any>;
-
-type RSSObject<T> = {
-  [key in keyof T]: [T[key]];
-};
-
-const parseRSSObject = <T>(rssObject: RSSObject<T>): T =>
-  (Object.keys(rssObject) as Array<keyof T>).reduce((acc, k) => {
-    const data = rssObject[k][0];
-    return {
-      ...acc,
-      [k]: data,
-    };
-  }, {} as T);
+const parseXml = (xml: string) => parseStringPromise(xml, { explicitArray: false }) as Promise<FeedResponse>;
 
 export const buildFeed = async (xml: string): Promise<Feed<FeedItemInterface>> => {
   const {
-    rss: {
-      channel: [data],
-    },
+    rss: { channel },
   } = await parseXml(xml);
   return {
-    title: data.title[0],
-    link: data.link[0],
-    description: data.description[0],
-    image: {
-      link: data.image[0].url[0],
-      title: data.image[0].title[0],
-      url: data.image[0].url[0],
-    },
-    language: data.language[0],
-    webMaster: data.webMaster[0],
-    items: data.item.map((item: RSSObject<FeedItemInterface>) => parseRSSObject<FeedItemInterface>(item)),
-    feedUrl: data.link[0],
+    ...channel,
+    items: channel.item,
   };
 };
