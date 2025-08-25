53d05cb2077e9ec714c2c713119e758382b17854
yannglt <helloyannglt@gmail.com>
2025-08-25 11:07:51 +0200

    update icon and add shortcut

--- extensions/fathom-analytics-stats/CHANGELOG.md
+++ extensions/fathom-analytics-stats/CHANGELOG.md
@@ -1,3 +1,8 @@
 # Fathom Analytics Changelog
 
+## Updated icon and shortcut - {PR_MERGE_DATE}
+
+- Updated menu bar icon
+- Added shortcut to open in Fathom
+
 ## [Initial Version] - 2024-01-20
--- extensions/fathom-analytics-stats/src/current-visitors-menu-bar.tsx
+++ extensions/fathom-analytics-stats/src/current-visitors-menu-bar.tsx
@@ -1,9 +1,10 @@
+/// <reference path="../raycast-env.d.ts" />
 import { getPreferenceValues, MenuBarExtra, Icon, open } from "@raycast/api";
 import FathomRequest from "./utils/api";
 import { LiveData } from "./types/LiveData";
 
 export default function Command() {
-  const preferences = getPreferenceValues<Preferences>();
+  const preferences = getPreferenceValues<Preferences.CurrentVisitorsMenuBar>();
 
   const { data, isLoading } = FathomRequest({
     endpoint: "/current_visitors",
@@ -20,10 +21,8 @@ export default function Command() {
   const totalReferrers = data.referrers.reduce((sum, referrer) => sum + referrer.total, 0);
 
   return (
-    <MenuBarExtra icon={Icon.TwoPeople} title={data.total.toLocaleString()} isLoading={isLoading}>
-      <MenuBarExtra.Section
-        title={"Visitors (" + data.total.toLocaleString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + ")"}
-      >
+    <MenuBarExtra icon={Icon.BarChart} title={data.total.toLocaleString()} isLoading={isLoading}>
+      <MenuBarExtra.Section title={"Visitors: " + data.total.toLocaleString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}>
         {data.content.map((page, index) => (
           <MenuBarExtra.Item
             key={index}
@@ -31,7 +30,7 @@ export default function Command() {
           />
         ))}
       </MenuBarExtra.Section>
-      <MenuBarExtra.Section title={`Referrers (${totalReferrers.toLocaleString()})`}>
+      <MenuBarExtra.Section title={`Referrers: ${totalReferrers.toLocaleString()}`}>
         {data.referrers.map((page, index) => (
           <MenuBarExtra.Item
             key={index}
@@ -42,8 +41,9 @@ export default function Command() {
       <MenuBarExtra.Section>
         <MenuBarExtra.Item
           title="Open in Fathom"
-          icon={Icon.BarChart}
+          icon={Icon.Link}
           onAction={() => open(`https://app.usefathom.com?range=today&site=${preferences.siteId}`)}
+          shortcut={{ modifiers: ["cmd"], key: "o" }}
         />
       </MenuBarExtra.Section>
     </MenuBarExtra>
