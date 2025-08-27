b74756cc8251c27c61ec2da81f92b4bd63ec6e55
mathieudutour <mathieu@dutour.me>
2025-08-27 15:13:02 +0200

    notion: Fix opening page on windows

--- extensions/notion/src/components/actions/ActionCreateQuicklink.tsx
+++ extensions/notion/src/components/actions/ActionCreateQuicklink.tsx
@@ -2,11 +2,10 @@ import { Action } from "@raycast/api";
 import { usePromise } from "@raycast/utils";
 
 import { getPageName, Page } from "../../utils/notion";
-import { urlForPreferredMethod, checkedDefaultOpenMethod } from "../../utils/openPage";
+import { urlForPreferredMethod } from "../../utils/openPage";
 
 export default function ActionCreateQuicklink({ page }: { page: Page }) {
-  const { isLoading } = usePromise(checkedDefaultOpenMethod);
-  if (!page.url || isLoading) return null;
+  if (!page.url) return null;
   const link = urlForPreferredMethod(page.url);
 
   return (
