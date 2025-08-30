8932b57af1faf1b69f414cba240cb2163d6d05df
Jorge Ramírez Serrato <jramirezserrato@gmail.com>
2025-08-30 21:11:31 +0300

    Implement caching to reduce request number

--- extensions/tallinn-transport/src/service/all-routes.ts
+++ extensions/tallinn-transport/src/service/all-routes.ts
@@ -1,9 +1,19 @@
 import { fetchRoutes, fetchStops, type RouteRaw, type StopRaw } from "@/api";
-import { extractAllRoutes } from "@/lib/routes";
-import { extractAllStops } from "@/lib/stops";
+import { extractAllRoutes, type Route } from "@/lib/routes";
+import { extractAllStops, type Stop } from "@/lib/stops";
 import Papa from "papaparse";
+import { CacheManager } from "@/utils/cache";
+
+const cache = new CacheManager<{ routes: Route[]; serializedStops: [string, Stop][] }>({ key: "all-routes" });
 
 export const getAllRoutesData = async () => {
+  const cachedData = cache.get();
+
+  if (cachedData) {
+    const { routes, serializedStops } = cachedData;
+    return { routes, stops: new Map(serializedStops) };
+  }
+
   const [routesRaw, stopsRaw] = await Promise.all([fetchRoutes(), fetchStops()]);
 
   const parsedRoutes = Papa.parse<RouteRaw>(routesRaw, { header: true, delimiter: ";" });
@@ -18,6 +28,8 @@ export const getAllRoutesData = async () => {
 
   const { routes, relevantStopIds } = extractAllRoutes(parsedRoutes.data);
   const { stops } = extractAllStops(parsedStops.data, relevantStopIds);
+
+  cache.set({ routes, serializedStops: Array.from(stops) });
 
   return { routes, stops };
 };
--- extensions/tallinn-transport/src/service/announcements.ts
+++ extensions/tallinn-transport/src/service/announcements.ts
@@ -1,5 +1,18 @@
 import { fetchAnnouncements, type AnnouncementRaw } from "@/api";
+import { CacheManager } from "@/utils/cache";
 
-export const getAnnouncements = () => {
-  return fetchAnnouncements() as Promise<AnnouncementRaw[]>;
+const cache = new CacheManager<AnnouncementRaw[]>({ key: "announcements" });
+
+export const getAnnouncements = async () => {
+  const cachedData = cache.get();
+
+  if (cachedData) {
+    return cachedData;
+  }
+
+  const announcements = await fetchAnnouncements();
+
+  cache.set(announcements);
+
+  return announcements;
 };
