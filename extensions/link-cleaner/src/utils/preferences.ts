b1a51501554f78bb6d1621a9e09892ce97bf82f4
cupcakearmy <hi@nicco.io>
2025-08-31 22:41:09 +0200

    typos

--- extensions/link-cleaner/src/utils/preferences.ts
+++ extensions/link-cleaner/src/utils/preferences.ts
@@ -1,6 +1,6 @@
 import { getPreferenceValues } from "@raycast/api";
 
 export function getPreferences() {
-  const preferences: Preferences.CleanSelectedText | Preferences.CleanSelectedText = getPreferenceValues();
+  const preferences: Preferences.CleanSelectedText | Preferences.CleanClipboardText = getPreferenceValues();
   return preferences;
 }
