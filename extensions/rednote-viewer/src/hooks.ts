6520efe91a1f1a7995d77ca2e3b6605adc9cdbd2
LitoMore <litomore@gmail.com>
2025-09-04 01:48:04 +0800

    Handle error while translating

--- extensions/rednote-viewer/src/hooks.ts
+++ extensions/rednote-viewer/src/hooks.ts
@@ -1,7 +1,12 @@
-import { useAI } from "@raycast/utils";
+import { showFailureToast, useAI } from "@raycast/utils";
 import { TRANSLATE_PROMPT } from "./constants.js";
 import { shouldTranslate } from "./utils.js";
 
 export function useTranslate(text: string, prompt = TRANSLATE_PROMPT) {
-  return useAI(`${prompt}${text}`, { execute: shouldTranslate });
+  return useAI(`${prompt}${text}`, {
+    execute: shouldTranslate,
+    onError: (error) => {
+      showFailureToast(error);
+    },
+  });
 }
