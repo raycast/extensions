510668930a5138bfafb7d0c579658f619afec1d5
Per Nielsen Tikær <per@raycast.com>
2025-09-04 10:27:13 +0200

    Update configService.ts

--- extensions/flibusta-search/src/services/configService.ts
+++ extensions/flibusta-search/src/services/configService.ts
@@ -1,9 +1,5 @@
 import { getPreferenceValues } from "@raycast/api";
 
-interface Preferences {
-  baseSiteUrl: string;
-}
-
 export function getBaseSiteUrl(): string {
   const preferences = getPreferenceValues<Preferences>();
   return preferences.baseSiteUrl || "https://flibusta.is";
