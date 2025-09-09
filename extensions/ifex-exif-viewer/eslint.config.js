908f7283ae15f289901deacfa785c23eea9329ba
Daniel Filho <daniel@dnl.dev>
2025-09-09 09:49:40 +0200

    Update ifex-exif-viewer extension
    - Merge branch \'main\' of github.com:danielfilho/ifex-raycast
    - fix(lint): fix import according to automate code review
    - fix(lint): fix import

--- extensions/ifex-exif-viewer/eslint.config.js
+++ extensions/ifex-exif-viewer/eslint.config.js
@@ -1,4 +1,3 @@
-const { defineConfig } = require("eslint/config");
 const raycastConfig = require("@raycast/eslint-config");
 
-module.exports = defineConfig([...raycastConfig]);
+module.exports = raycastConfig.flat();
