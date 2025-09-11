0d1c98cd3389d250f8cf6e6a978809339f6bd7f7
Per Nielsen Tikær <per@raycast.com>
2025-09-11 15:27:19 +0200

    Update clock.ts implementation

--- extensions/yr-weather-forecast/src/clock.ts
+++ extensions/yr-weather-forecast/src/clock.ts
@@ -3,6 +3,6 @@ import { getPreferenceValues } from "@raycast/api";
 export type ClockFormat = "12h" | "24h";
 
 export function getClockFormat(): ClockFormat {
-  const prefs = getPreferenceValues<Preferences.Yr>();
+  const prefs = getPreferenceValues<{ clockFormat?: ClockFormat }>();
   return (prefs.clockFormat as ClockFormat) ?? "24h";
 }
