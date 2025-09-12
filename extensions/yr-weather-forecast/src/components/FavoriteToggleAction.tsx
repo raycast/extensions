a54b296ef746b859b835a8210aac09bf6447a9d7
Per Nielsen Tikær <per@raycast.com>
2025-09-12 15:37:47 +0200

    Update FavoriteToggleAction component

--- extensions/yr-weather-forecast/src/components/FavoriteToggleAction.tsx
+++ extensions/yr-weather-forecast/src/components/FavoriteToggleAction.tsx
@@ -1,4 +1,4 @@
-import { Action, Icon } from "@raycast/api";
+import { Action, Icon, Keyboard } from "@raycast/api";
 
 interface FavoriteToggleActionProps {
   isFavorite: boolean;
@@ -10,7 +10,7 @@ export function FavoriteToggleAction({ isFavorite, onToggle }: FavoriteToggleAct
     <Action
       title={`${isFavorite ? "Remove from" : "Add to"} Favorites`}
       icon={isFavorite ? Icon.StarDisabled : Icon.Star}
-      shortcut={isFavorite ? { modifiers: ["cmd", "shift"], key: "f" } : { modifiers: ["cmd"], key: "f" }}
+      shortcut={Keyboard.Shortcut.Common.Pin}
       onAction={onToggle}
     />
   );
