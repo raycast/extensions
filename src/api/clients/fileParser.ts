e5c0e4c1a0f615700a052114574b05d0478537b9
Botterson <botterson@Bottersons-MacBook-Air.local>
2025-09-18 16:02:00 +0300

    fileParser client

---
+++ src/api/clients/fileParser.ts
@@ -0,0 +1,85 @@
+import fs from "fs";
+import path from "path";
+import { execSync } from "child_process";
+import plist from "plist";
+import xml2js from "xml2js";
+import { SoundSet } from "../../types";
+import { expandTilde } from "../../utils/helpers";
+import { Clipboard } from "@raycast/api";
+
+export class FileParser {
+  farragoDataDirpath: string;
+  lastParsedSets: number | null;
+
+  constructor(options: { farragoDataDir: string }) {
+    this.farragoDataDirpath = expandTilde(options.farragoDataDir);
+    this.lastParsedSets = null;
+  }
+
+  get soundSetsDir() {
+    return path.join(this.farragoDataDirpath, "Sound Sets");
+  }
+
+  _getSetsFilepaths() {
+    if (!fs.existsSync(this.soundSetsDir)) {
+      throw new Error("Sound Sets directory does not exist: " + this.soundSetsDir);
+    }
+    const files = fs.readdirSync(this.soundSetsDir);
+
+    return files.filter((file) => !file.includes(".")).map((file) => path.join(this.soundSetsDir, file));
+  }
+
+  _parseSetFile(fp: string): SoundSet {
+    let result: SoundSet;
+
+    try {
+      // Convert and capture the output as a string
+      const xmlString = execSync(`plutil -convert xml1 -o - "${fp}"`, { encoding: "utf8" });
+
+      // Parse the XML string
+      const parser = new xml2js.Parser({ explicitArray: false, mergeAttrs: true });
+      parser.parseString(xmlString, (err, _result) => {
+        if (err) {
+          throw new Error("Error parsing XML: " + err);
+        }
+
+        result = plist.parse(xmlString) as SoundSet;
+      });
+    } catch (error) {
+      throw new Error("Error during conversion: " + error);
+    }
+
+    return result!;
+  }
+
+  _debugCopyParsedSets() {
+    const setsParsed = this.getFreshSetsParsed();
+    Clipboard.copy(JSON.stringify(setsParsed, null, 2));
+  }
+
+  getFreshSetsParsed() {
+    const result = this._getSetsFilepaths().map((fp) => this._parseSetFile(fp));
+    this.lastParsedSets = Date.now();
+    return result;
+  }
+
+  hasFreshSets() {
+    const lastTimeEdited = this.getLastTimeEdited();
+    return this.lastParsedSets && this.lastParsedSets > lastTimeEdited;
+  }
+
+  getLastTimeEdited() {
+    const files = fs.readdirSync(this.soundSetsDir);
+    let lastModifiedTime = 0;
+
+    files.forEach((file) => {
+      const filePath = path.join(this.soundSetsDir, file);
+      const stat = fs.statSync(filePath);
+      if (stat.isFile() && stat.mtimeMs > lastModifiedTime) {
+        lastModifiedTime = stat.mtimeMs;
+      }
+    });
+
+    return lastModifiedTime;
+  }
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,98 @@ export type Preferences = {
   oscHost: string;
   oscPort: string;
 };
+
+export type SoundSetTile = {
+  automations: Automation;
+  colorIndex: number;
+  editorModelState: EditorModelState;
+  editorWindowState: Record<string, any>;
+  favorite: boolean;
+  fileName: string;
+  fileUUID: string;
+  gridPositionX: number;
+  gridPositionY: number;
+  holdToPlay: boolean;
+  lastPlayStart: number;
+  listPositionY: number;
+  notes: string;
+  notesShown: boolean;
+  playbackEnabled: boolean;
+  playerSettings: PlayerSettings;
+  selected: number;
+  solo: boolean;
+  tileIcon: string[];
+  tileUUID: string;
+  title: string;
+};
+
+export type SoundSet = {
+  automations: Automation;
+  crossfadeDuration: number;
+  formatMinSupportedVersion: number;
+  formatVersion: number;
+  maxRowsGrid: number;
+  maxRowsList: number;
+  mode: number;
+  notes: string;
+  playbackMode: number;
+  position: number;
+  security: Security;
+  setWindowState: WindowState;
+  splitViewState: SplitViewState[];
+  tiles: SoundSetTile[];
+  title: string;
+  type: string;
+  uuid: string;
+};
+
+type Automation = {
+  modelItems: any[];
+};
+
+type Clip = {
+  clipUUID: string;
+  fileUUID: string;
+  name: string;
+  originalUUID: string;
+  selectedPacketRange: string;
+};
+
+type EditorModelState = {
+  clip?: Clip;
+  zoom?: number;
+};
+
+type PlayerSettings = {
+  channels: [number, number];
+  defaultToPause: boolean;
+  ducked: boolean;
+  duckedVolume: number;
+  duration: number;
+  endPackets: number;
+  fadeInTime: number;
+  fadeOutTime: number;
+  fullVolume: number;
+  loop: boolean;
+  muted: boolean;
+  packetCount: number;
+  startPackets: number;
+};
+
+type SplitViewState = {
+  collapsed: boolean;
+  size: number;
+};
+
+type Security = {
+  hint: string;
+  locked: boolean;
+  password: string;
+  passwordRequired: boolean;
+};
+
+type WindowState = {
+  frame: string;
+  fullscreen: boolean;
+  open: boolean;
+};
