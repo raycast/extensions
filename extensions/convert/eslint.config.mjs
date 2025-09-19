4b60ec292202fbf1754a83c77329df0b786291ee
Yusif Aliyev <114253515+YusifAliyevPro@users.noreply.github.com>
2025-09-19 15:25:50 +0400

    Refactor ESLint configuration to use Raycast's ESLint config and remove TypeScript support

--- extensions/convert/eslint.config.mjs
+++ extensions/convert/eslint.config.mjs
@@ -1,27 +1,4 @@
 import { defineConfig } from "eslint/config";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import raycastConfig from "@raycast/eslint-config";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
-
-export default defineConfig([
-  {
-    extends: compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"),
-    plugins: {
-      "@typescript-eslint": typescriptEslint,
-    },
-    languageOptions: {
-      parser: tsParser,
-    },
-  },
-]);
+export default defineConfig([...raycastConfig]);
