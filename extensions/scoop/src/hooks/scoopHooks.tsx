bc2430f0bf66afba4ba978b3046fb7162f987824
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2025-09-20 09:33:55 +0530

    fix: unsued imports

--- extensions/scoop/src/hooks/scoopHooks.tsx
+++ extensions/scoop/src/hooks/scoopHooks.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { ScoopManager } from "../scoop";
-import { ScoopPackage, InstalledScoopPackage } from "../types/index.types";
 
 function useScoop() {
   const [scoop, setScoop] = useState<ScoopManager>(ScoopManager.getInstance());
