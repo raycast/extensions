3e08ab3cf19ec6609c13b87d4ae9a0e0667656f1
Botterson <botterson@Bottersons-MacBook-Air.local>
2025-09-25 23:36:15 +0300

    show HUD instead of toast in fade-all

--- src/fade-all.tsx
+++ src/fade-all.tsx
@@ -1,10 +1,10 @@
-import { closeMainWindow, showToast } from "@raycast/api";
+import { closeMainWindow, showHUD } from "@raycast/api";
 import { ClientManager } from "./api/clientManager";
 import { setTimeout } from "node:timers/promises";
 
 export default async function Command() {
   closeMainWindow();
-  showToast({ title: "Fading all sounds..." });
+  showHUD("Fading all sounds...");
   const oscClient = ClientManager.initializeOscClient();
   oscClient.fadeAll();
   await setTimeout(5); // just to ensure the message is sent before closing the port
