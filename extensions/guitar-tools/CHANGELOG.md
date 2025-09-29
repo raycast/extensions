8d417dccef9beef52f218b8f111c18e42baf0622
narghev <narghev@protonmail.com>
2025-09-29 12:44:20 +0400

    Add guitar-tools extension
    - lint and format
    - add better error handling for to useTuner
    - update package-lock.json for renaming the extensions
    - revert changes to eslint config, remove problematic dependencies from useEffect
    - improve error handling and cleanup of single chunk analysis
    - rename the extension to guitar tools
    - update README, extension-icon and add screenshots
    - show error in list view
    - change clarity threshold
    - display the detected note
    - fix categories and platforms
    - wip: add cents to detected note
    - wip: add utils for sox, note recognition and a very basic UI to render detected notes
    - install the packages, generate package-lock
    - init the extension with a simple command

---
+++ extensions/guitar-tools/CHANGELOG.md
@@ -0,0 +1,3 @@
+# Tuner Changelog
+
+## [Initial Version] - {PR_MERGE_DATE}
---
+++ extensions/guitar-tools/src/tune.tsx
@@ -0,0 +1,49 @@
+import { List, Icon, Color } from "@raycast/api";
+import { useEffect } from "react";
+import { useTuner } from "./hooks/useTuner";
+import { centsToDisplayObject } from "./utils/display.utils";
+
+export default function Command() {
+  const { startContinuousListening, stopContinuousListening, detectedNote, error } = useTuner();
+
+  useEffect(() => {
+    startContinuousListening();
+
+    return () => {
+      stopContinuousListening();
+    };
+  }, []);
+
+  if (error) {
+    return (
+      <List>
+        <List.Item
+          title={error.title}
+          subtitle={error.subTitle}
+          icon={{ source: Icon.ExclamationMark, tintColor: Color.Red }}
+        />
+      </List>
+    );
+  }
+
+  if (detectedNote) {
+    const { noteName, cents } = detectedNote;
+
+    const displayObject = centsToDisplayObject(cents);
+
+    return (
+      <List>
+        <List.Item
+          title={noteName}
+          subtitle={`${cents > 0 ? "+" : ""}${cents} cents`}
+          icon={{ source: displayObject.icon, tintColor: displayObject.color }}
+        />
+      </List>
+    );
+  }
+  return (
+    <List>
+      <List.Item title="Play your instrument" icon={{ source: Icon.Heartbeat, tintColor: Color.Blue }} />
+    </List>
+  );
+}
