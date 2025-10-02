2584097f5c594f2f19b5faf9de5a22ba8360f278
Morgan Titcher <50208304+MorganTitcher@users.noreply.github.com>
2025-10-02 10:01:35 -0400

    Update constants.ts
    To implement in lastOcrResult

--- extensions/screenshot-ocr/src/constants.ts
+++ extensions/screenshot-ocr/src/constants.ts
@@ -6,5 +6,6 @@
 export const LS_AUTO_PASTE_OVERRIDE = "autoPasteOverride"
 export const LS_LAST_SHOT_PATH = "lastShotPath"
 export const LS_LAST_OCR_TEXT = "lastOcrText"
+export const LS_LAST_OCR_META = "lastOcrMeta"
 export const LS_LAST_OCR_LANGUAGE = "lastOcrLanguage"
 export const LS_LANG_OVERRIDE = "ocrLanguageOverride"
