4e99ee80b661f4f08d735f27c3bf0ec8b1b756b5
narghev <narghev@protonmail.com>
2025-10-06 01:09:19 +0400

    Add ipinfo extension
    - change extension title casing
    - prettier
    - change title and command name casing
    - prepare the extension for store
    - allow full ip lookup from my ip command
    - history command
    - ip info command
    - handle API errors
    - add actions for copying the IP address or its extended information
    - show IP country name and flag
    - add my-ip command to get and render the user IP information
    - init the extension with a simple command

---
+++ extensions/ipinfo/CHANGELOG.md
@@ -0,0 +1,3 @@
+# IPInfo Changelog
+
+## [Initial Version] - {PR_MERGE_DATE}
---
+++ extensions/ipinfo/src/utils/country-emoji.ts
@@ -0,0 +1,7 @@
+export const getFlagEmoji = (countryCode: string) => {
+  const codePoints = countryCode
+    .toUpperCase()
+    .split("")
+    .map((char) => 127397 + char.charCodeAt(0));
+  return String.fromCodePoint(...codePoints);
+};
