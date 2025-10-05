92b9d50ef8e41f59aaaad519249b019613dce0f1
xmok <30526133+xmok@users.noreply.github.com>
2025-10-05 13:29:18 +0500

    [OVH] fix delete + ye olde lint

--- extensions/ovh/src/types.ts
+++ extensions/ovh/src/types.ts
@@ -1,32 +1,51 @@
 export type Domain = {
-  domain: string
-  dnssecState: "disabled"|"enabled"|"not_supported";
-  nameServerType: "anycast"|"dedicated"|"empty"|"external"|"hold"|"hosted"|"hosting"|"mixed"|"parking"
-  nameServers: Array<{nameServer: string}>
+  domain: string;
+  dnssecState: "disabled" | "enabled" | "not_supported";
+  nameServerType: "anycast" | "dedicated" | "empty" | "external" | "hold" | "hosted" | "hosting" | "mixed" | "parking";
+  nameServers: Array<{ nameServer: string }>;
   renewalDate: string;
-  renewalState: "automatic_renew"|"cancellation_complete"|"cancellation_requested"|"manual_renew"|"unpaid"
+  renewalState: "automatic_renew" | "cancellation_complete" | "cancellation_requested" | "manual_renew" | "unpaid";
   serviceId: number;
-  state: "autorenew_in_progress"
-|"autorenew_registry_in_progress"
-|"deleted"
-|"dispute"
-|"expired"
-|"ok"
-|"outgoing_transfer"
-|"pending_create"
-|"pending_delete"
-|"pending_incoming_transfer"
-|"pending_installation"
-|"registry_suspended"
-|"restorable"
-|"technical_suspended";
-  transferLockStatus: "locked"|"locking"|"unavailable"|"unlocked"|"unlocking";
-}
+  state:
+    | "autorenew_in_progress"
+    | "autorenew_registry_in_progress"
+    | "deleted"
+    | "dispute"
+    | "expired"
+    | "ok"
+    | "outgoing_transfer"
+    | "pending_create"
+    | "pending_delete"
+    | "pending_incoming_transfer"
+    | "pending_installation"
+    | "registry_suspended"
+    | "restorable"
+    | "technical_suspended";
+  transferLockStatus: "locked" | "locking" | "unavailable" | "unlocked" | "unlocking";
+};
 export type DNSRecord = {
-  fieldType: "A"|"AAAA"|"CAA"|"CNAME"|"DKIM"|"DMARC"|"DNAME"|"LOC"|"MX"|"NAPTR"|"NS"|"PTR"|"RP"|"SPF"|"SRV"|"SSHFP"|"TLSA"|"TXT"
-  id: number
-  subDomain: string | null
+  fieldType:
+    | "A"
+    | "AAAA"
+    | "CAA"
+    | "CNAME"
+    | "DKIM"
+    | "DMARC"
+    | "DNAME"
+    | "LOC"
+    | "MX"
+    | "NAPTR"
+    | "NS"
+    | "PTR"
+    | "RP"
+    | "SPF"
+    | "SRV"
+    | "SSHFP"
+    | "TLSA"
+    | "TXT";
+  id: number;
+  subDomain: string | null;
   target: string;
-  ttl: number | null
+  ttl: number | null;
   zone: string;
-}
+};
