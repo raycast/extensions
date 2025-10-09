2c9a605cd12c89ca9b79a98675ee5660d032b42c
Tucker <6243879+YourMCGeek@users.noreply.github.com>
2025-10-09 16:31:26 -0500

    Feat: Add IPv6 Support

--- extensions/ipinfo/src/utils/ip-validator.ts
+++ extensions/ipinfo/src/utils/ip-validator.ts
@@ -1,5 +1,14 @@
-export const isValidIpV4 = (ip: string): boolean => {
+const isValidIpV4 = (ip: string): boolean => {
   const ipv4Regex =
     /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
   return ipv4Regex.test(ip);
+};
+
+const isValidIpV6 = (ip: string): boolean => {
+  const ipv6Regex = /^((([0-9A-Fa-f]{1,4}:){1,6}:)|(([0-9A-Fa-f]{1,4}:){7}))([0-9A-Fa-f]{1,4})$/;
+  return ipv6Regex.test(ip);
+};
+
+export const isValidIp = (ip: string): boolean => {
+  return isValidIpV4(ip) || isValidIpV6(ip);
 };
