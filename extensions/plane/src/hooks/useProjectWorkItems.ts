ff808e5d6d29bdf90a8ae5de4bc471d9ec0bbe70
xmok <30526133+xmok@users.noreply.github.com>
2025-10-11 20:24:11 +0500

    [Plane] re-run linnt

--- extensions/plane/src/hooks/useProjectWorkItems.ts
+++ extensions/plane/src/hooks/useProjectWorkItems.ts
@@ -5,7 +5,7 @@ import { Issue } from "@makeplane/plane-node-sdk";
 export function useProjectWorkItems(projectId: string) {
   const { data, error, isLoading, mutate, pagination } = useCachedPromise(
     (projectId: string) => async (pagination: PaginationOptions<Issue[]>) =>
-      getProjectWorkItems({ projectId, cursor: pagination.cursor}),
+      getProjectWorkItems({ projectId, cursor: pagination.cursor }),
     [projectId],
   );
   return {
