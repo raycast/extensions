f3cf06994975c29387c13efd3cf10495f971a083
Tucker <6243879+YourMCGeek@users.noreply.github.com>
2025-10-13 17:58:23 -0500

    Refactor: Use IP-Address package for validation

--- extensions/ipinfo/src/utils/ip-validator.ts
+++ extensions/ipinfo/src/utils/ip-validator.ts
@@ -1,14 +1,3 @@
-const isValidIpV4 = (ip: string): boolean => {
-  const ipv4Regex =
-    /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-  return ipv4Regex.test(ip);
-};
+import { Address4, Address6 } from "ip-address";
 
-const isValidIpV6 = (ip: string): boolean => {
-  const ipv6Regex = /^((([0-9A-Fa-f]{1,4}:){1,6}:)|(([0-9A-Fa-f]{1,4}:){7}))([0-9A-Fa-f]{1,4})$/;
-  return ipv6Regex.test(ip);
-};
-
-export const isValidIp = (ip: string): boolean => {
-  return isValidIpV4(ip) || isValidIpV6(ip);
-};
+export const isValidIp = (ip: string): boolean => Address4.isValid(ip) || Address6.isValid(ip);
