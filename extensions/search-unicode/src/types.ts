dc0475a0d3ceb1ebbab62137adea9eb999238a3b
Eana Hufwe <eana@1a23.com>
2025-10-18 13:16:07 -0700

    Add search-unicode extension

---
+++ extensions/search-unicode/src/types.ts
@@ -0,0 +1,33 @@
+export interface Entry {
+  aliases: string;
+  bin: string;
+  block: string;
+  cat: string;
+  cells: string;
+  char: string;
+  cpoint: string;
+  dec: string;
+  digraph: string;
+  hex: string;
+  html: string;
+  json: string;
+  keysym: string;
+  name: string;
+  oct: string;
+  plane: string;
+  props: string;
+  refs: string;
+  script: string;
+  unicode: string;
+  utf16be: string;
+  utf16le: string;
+  utf8: string;
+  width: string;
+  xml: string;
+}
+
+export interface PreferenceValues {
+  execSource: "wasm" | "bundled" | "path" | "custom";
+  execPath: string;
+  showEncodings?: boolean;
+}
