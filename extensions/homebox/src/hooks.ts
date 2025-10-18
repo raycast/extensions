657654a6f04755ba8a63a181a7dce640059cd211
xmok <30526133+xmok@users.noreply.github.com>
2025-10-18 17:56:57 +0500

    [HomeBox] ye olde lint + images

--- extensions/homebox/src/hooks.ts
+++ extensions/homebox/src/hooks.ts
@@ -1,50 +1,52 @@
-import { useCachedPromise, useLocalStorage } from "@raycast/utils";
+import { useCachedPromise } from "@raycast/utils";
 import { homebox } from "./homebox";
 
-export function useToken() {
-  const {isLoading, value} = useLocalStorage<string>("HOMEBOX-TOKEN", "");
-  if (!isLoading) console.log(value)
-  // const token =value?.replaceAll("\"", "").replaceAll("Bearer ", "");
+export function useItem(id: string) {
+  const { data, ...rest } = useCachedPromise(async (id: string) => await homebox.items.get(id), [id]);
   return {
-    isLoadingToken: isLoading,
-    token: value
-  }
+    item: data,
+    ...rest,
+  };
+}
+export function useItemMaintenanceLog(id: string) {
+  const { data, ...rest } = useCachedPromise(async () => await homebox.items.getMaintenanceLog(id));
+  return {
+    maintenance: data,
+    ...rest,
+  };
 }
 
-export function useItem(id: string) {
-  const {data, ...rest} = useCachedPromise(async() =>await homebox.items.get(id))
-    return {
-      item: data,
-      ...rest
-    }
-}
 export function useItems(query: string) {
-    const {data, ...rest} = useCachedPromise((query: string) => async (options) => {
-        const result = await homebox.items.search({query, page: options.page+1});
-        return {
-          data: result.items,
-          hasMore: (result.page*result.pageSize)<result.total
-        }
-      }, [query], {
-        initialData: []
-      })
+  const { data, ...rest } = useCachedPromise(
+    (query: string) => async (options) => {
+      const result = await homebox.items.search({ query, page: options.page + 1 });
       return {
-        items: data,
-        ...rest
-      }
+        data: result.items,
+        hasMore: result.page * result.pageSize < result.total,
+      };
+    },
+    [query],
+    {
+      initialData: [],
+    },
+  );
+  return {
+    items: data,
+    ...rest,
+  };
 }
 
 export function useLabels() {
-    const {data, ...rest} = useCachedPromise(homebox.labels.list,[],{initialData:[]})
-    return {
-        labels: data,
-        ...rest
-    }
+  const { data, ...rest } = useCachedPromise(homebox.labels.list, [], { initialData: [] });
+  return {
+    labels: data,
+    ...rest,
+  };
 }
 export function useLocations() {
-    const {data, ...rest} = useCachedPromise(homebox.locations.list,[],{initialData:[]})
-    return {
-        locations: data,
-        ...rest
-    }
+  const { data, ...rest } = useCachedPromise(homebox.locations.list, [], { initialData: [] });
+  return {
+    locations: data,
+    ...rest,
+  };
 }
--- extensions/homebox/src/types.ts
+++ extensions/homebox/src/types.ts
@@ -1,70 +1,75 @@
 export type Label = {
-          "id": string;
-          "name": string;
-          "description": string;
-          "color": string;
-          "createdAt": string;
-          "updatedAt": string;
-}
+  id: string;
+  name: string;
+  description: string;
+  color: string;
+  createdAt: string;
+  updatedAt: string;
+};
 export type CreateLabelRequest = {
-    name: string;
-    description: string;
-}
+  name: string;
+  description: string;
+};
 export type Item = {
-    id: string;
-    assetId: string;
-    name: string;
-    description: string;
-    "quantity": number
-    "insured": boolean
-    "archived": boolean
-    "createdAt": string
-    "updatedAt": string
-    "purchasePrice": number
-    labels: Label[]
-}
+  id: string;
+  assetId: string;
+  name: string;
+  description: string;
+  quantity: number;
+  insured: boolean;
+  archived: boolean;
+  createdAt: string;
+  updatedAt: string;
+  purchasePrice: number;
+  labels: Label[];
+};
 type Attachment = {
-    id: string
-}
+  id: string;
+};
 export type DetailedItem = Item & {
-        "serialNumber": string;
-        "modelNumber": string;
-        "manufacturer": string;
-        "lifetimeWarranty": boolean
-        "warrantyExpires": string
-        "warrantyDetails": string
-        "purchaseTime": string
-        "purchaseFrom": string
-        "soldTime": string
-        "soldTo": string
-        "soldPrice": number
-        "soldNotes":string
-        "notes":string
-        "attachments": Attachment[],
-        // "fields": []
-}
+  serialNumber: string;
+  modelNumber: string;
+  manufacturer: string;
+  lifetimeWarranty: boolean;
+  warrantyExpires: string;
+  warrantyDetails: string;
+  purchaseTime: string;
+  purchaseFrom: string;
+  soldTime: string;
+  soldTo: string;
+  soldPrice: number;
+  soldNotes: string;
+  notes: string;
+  attachments: Attachment[];
+};
 export type CreateItemRequest = {
-    locationId: string;
-    name: string;
-    quantity: number
-    description: string;
-    labelIds: string[]
-}
+  locationId: string;
+  name: string;
+  quantity: number;
+  description: string;
+  labelIds: string[];
+};
 
 export type Location = {
-    "id": string
-    "name": string
-    "description": string
-    "createdAt": string
-    "updatedAt": string
-    "itemCount": number
-}
+  id: string;
+  name: string;
+  description: string;
+  createdAt: string;
+  updatedAt: string;
+  itemCount: number;
+};
 
 export type GroupStatistics = {
-  "totalUsers": number
-  "totalItems": number
-  "totalLocations": number
-  "totalLabels": number
-  "totalItemPrice": number
-  "totalWithWarranty": number
-}
+  totalUsers: number;
+  totalItems: number;
+  totalLocations: number;
+  totalLabels: number;
+  totalItemPrice: number;
+  totalWithWarranty: number;
+};
+
+export type MaintenanceLog = {
+  completedDate: string;
+  scheduledDate: string;
+  name: string;
+};
