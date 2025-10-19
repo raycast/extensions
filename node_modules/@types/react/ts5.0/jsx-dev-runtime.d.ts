791dff90c0f7514cba368a788822751447846aef
Jungwoon Park <your-email@example.com>
2025-10-19 18:12:02 +0900

    Update @types/react to fix GitHub Actions type errors
    - Update @types/react from 18.0.9 to 18.3.12
    - Resolves JSX component type compatibility issues
    - All local validations passing

--- node_modules/@types/react/ts5.0/jsx-dev-runtime.d.ts
+++ node_modules/@types/react/ts5.0/jsx-dev-runtime.d.ts
@@ -2,7 +2,6 @@ import * as React from "./";
 export { Fragment } from "./";
 
 export namespace JSX {
-    type ElementType = React.JSX.ElementType;
     interface Element extends React.JSX.Element {}
     interface ElementClass extends React.JSX.ElementClass {}
     interface ElementAttributesProperty extends React.JSX.ElementAttributesProperty {}
