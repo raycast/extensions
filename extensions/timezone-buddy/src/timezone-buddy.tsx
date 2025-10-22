1991e9e59ff692ebb82a1505dcd012d4459fb5b4
Michael Reinhard <michael.reinhard.91@gmail.com>
2025-10-23 01:25:46 +0200

    Update timezone-buddy extension

--- extensions/timezone-buddy/src/timezone-buddy.tsx
+++ extensions/timezone-buddy/src/timezone-buddy.tsx
@@ -389,7 +389,7 @@ export default function Command() {
                   </ActionPanel.Section>
                   <ActionPanel.Section>
                     <Action
-                      title="Move Up"
+                      title="Move up"
                       icon={Icon.ArrowUp}
                       shortcut={{ modifiers: ["cmd", "opt"], key: "arrowUp" }}
                       onAction={() => moveUp(index)}
