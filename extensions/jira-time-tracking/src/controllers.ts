234f73c6da809880c36a37fbf6619e6b4d85e895
Bruno Gonçalves <bruno.m.goncalves@ist.utl.pt>
2025-10-22 11:24:03 +0100

    Replace search endpoint for non jira cloud (v2 api)

--- extensions/jira-time-tracking/src/controllers.ts
+++ extensions/jira-time-tracking/src/controllers.ts
@@ -23,7 +23,7 @@ function getApiPath(path: string): string {
     if (path.includes("/project/search")) {
       path = path.replace("/rest/api/3/project/search", "/rest/api/2/project");
     } else if (path.includes("/search")) {
-      path = path.replace("/rest/api/3/search/jql", "/rest/api/2/search");
+      path = path.replace("/rest/api/3/search/jql", "/rest/api/2/search/jql");
     } else {
       path = path.replace("/rest/api/3", `/rest/api/${version}`);
     }
