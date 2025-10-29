02cc9ed1e45ab105b2fb822bcf6b394016f26cb8
Aaron Johnson <ajohnson@cephas.net>
2025-10-28 18:57:03 -0700

    Refactor useTasks to simplify task URL construction

--- extensions/reclaim-ai/src/hooks/useTask.tsx
+++ extensions/reclaim-ai/src/hooks/useTask.tsx
@@ -6,20 +6,15 @@ import { CreateTaskProps, PlannerActionIntermediateResult } from "./useTask.type
 import { fetchPromise } from "../utils/fetcher";
 
 export const useTasks = () => {
-  const taskURL = (
+  const taskURL =
     "/tasks?instances=true" +
     "&status=NEW" +
     "&status=SCHEDULED" +
     "&status=COMPLETE" +
     "&status=ARCHIVED" +
-    "&status=IN_PROGRESS"
-  );
+    "&status=IN_PROGRESS";
 
-  const {
-    data: tasks,
-    error,
-    isLoading,
-  } = useApi<Task[]>(taskURL);
+  const { data: tasks, error, isLoading } = useApi<Task[]>(taskURL);
 
   return {
     tasks,
