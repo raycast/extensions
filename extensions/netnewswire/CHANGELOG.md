a7c69e80dd3663888baa7a4947e25debc75a750b
xmok <30526133+xmok@users.noreply.github.com>
2025-11-06 03:28:56 +0500

    [NetNewsWire] list articles + lint

--- extensions/netnewswire/CHANGELOG.md
+++ extensions/netnewswire/CHANGELOG.md
@@ -1,3 +1,8 @@
 # NetNewsWire Changelog
 
 ## [Initial Version] - {PR_MERGE_DATE}
+
+- View Feeds
+- View Articles
+    1. Toggle Article "Read"
+    2. Toggle Article "Starred"
---
+++ extensions/netnewswire/src/types.ts
@@ -0,0 +1,9 @@
+export interface Feed {
+  account: string;
+  name: string;
+  url: string;
+  totalArticles: number;
+  readArticles: number;
+  starredArticles: number;
+  icon: string | null;
+}
