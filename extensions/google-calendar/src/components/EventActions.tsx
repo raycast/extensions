54751bbbccd268585de211df3fad5f7b1d015968
Ed Weston <eddweston@me.com>
2025-11-11 12:16:27 +0000

    Variable primary and secondary commands based on the users preference and the available URL.

--- extensions/google-calendar/src/components/EventActions.tsx
+++ extensions/google-calendar/src/components/EventActions.tsx
@@ -25,12 +25,30 @@ const EventActions = ({
   calendar: calendar_v3.Schema$CalendarListEntry | null;
   revalidate: () => void;
 }) => {
+  const meetingAction = {
+    title: "Open Meeting",
+    url: event.conferenceData?.entryPoints?.[0]?.uri,
+    shortcut: { modifiers: ["cmd"], key: "m"} as Keyboard.Shortcut,
+  }
+  const eventAction = {
+    title: "Open in Google Calendar",
+    url: event.htmlLink,
+    shortcut: { modifiers: ["cmd"], key: "o"} as Keyboard.Shortcut,
+  }
+  const openCommandJoinsMeeting = preferences.openCommandJoinsMeeting && meetingAction.url;
+  const primaryAction = openCommandJoinsMeeting ? meetingAction : eventAction;
+  const secondaryAction = openCommandJoinsMeeting ? eventAction : meetingAction;
   return (
     <ActionPanel>
-      {preferences.openCommandJoinsMeeting && event.conferenceData?.entryPoints?.[0]?.uri ? (
-        <Action.OpenInBrowser title="Open Meeting" url={event.conferenceData?.entryPoints?.[0]?.uri} />
-      ) : (
-        event.htmlLink && <Action.OpenInBrowser title="Open in Google Calendar" url={event.htmlLink} />
+      {primaryAction.url && (
+        <Action.OpenInBrowser title={primaryAction.title} url={primaryAction.url} />
+      )}
+      {secondaryAction.url && (
+        <Action.OpenInBrowser
+          title={secondaryAction.title}
+          url={secondaryAction.url}
+          shortcut={secondaryAction.shortcut}
+        />
       )}
       <ActionPanel.Section>
         {event.id && (
