8d09ea34cd6657f23597a1be6ebccefb68bc77d2
Gustavo Muniz dos Anjos <muniz.gustavo@pm.me>
2025-11-18 09:08:28 -0300

    Update extensions/fotmob/eslint.config.js
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>

--- extensions/fotmob/eslint.config.js
+++ extensions/fotmob/eslint.config.js
@@ -1,20 +1,6 @@
-const js = require("@eslint/js");
-const prettier = require("eslint-config-prettier/flat");
-const typescript = require("typescript-eslint");
-const raycast = require("@raycast/eslint-plugin");
-const globals = require("globals");
+const { defineConfig } = require("eslint/config");
+const raycastConfig = require("@raycast/eslint-config");
 
-module.exports = [
-  js.configs.recommended,
-  ...typescript.configs.recommended,
-  {
-    languageOptions: {
-      ecmaVersion: 2022,
-      globals: {
-        ...globals.node,
-      },
-    },
-  },
-  ...raycast.configs.recommended,
-  prettier,
-];
+module.exports = defineConfig([
+  ...raycastConfig,
+]);
