a73ecc30d5b1eb3643887211947149b3bcb0739d
Fillipe Augusto <fillipe484@gmail.com>
2025-11-19 00:32:02 -0300

    Update extensions/xcode-switcher/.eslintrc.js
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>

--- extensions/xcode-switcher/.eslintrc.js
+++ extensions/xcode-switcher/.eslintrc.js
@@ -1,33 +1,6 @@
-module.exports = {
-  root: true,
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 2021,
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  env: {
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-  ],
-  plugins: ["@typescript-eslint", "react", "react-hooks"],
-  rules: {
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off",
-  },
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
-};
+const { defineConfig } = require("eslint/config");
+const raycastConfig = require("@raycast/eslint-config");
+
+module.exports = defineConfig([
+  ...raycastConfig,
+]);
