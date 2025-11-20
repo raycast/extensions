f83c42a18cfb93d3397daf559435f3a419879323
Yiwei Ho <davidho0403@gmail.com>
2025-11-20 23:54:23 +0800

    Update shadcn-ui extension
    - docs: update changelog
    - style: fix lint
    - feat: add search registries command
    - chore: update shadcn github link
    - Initial commit

--- extensions/shadcn-ui/src/constants.ts
+++ extensions/shadcn-ui/src/constants.ts
@@ -4,7 +4,7 @@ export const SHADCN_URL = {
   DOCS_COMPONENTS: "https://ui.shadcn.com/docs/components",
   EXAMPLES: "https://ui.shadcn.com/examples",
   GITHUB: "https://github.com/shadcn-ui/ui/tree/main",
-  RAW_GITHUB_COMPONENTS: "https://raw.githubusercontent.com/shadcn/ui/main/apps/www/content/docs/components",
+  RAW_GITHUB_COMPONENTS: "https://raw.githubusercontent.com/shadcn-ui/ui/main/apps/v4/content/docs/components",
 } as const;
 
 export const OCTOKIT_CONFIG = {
