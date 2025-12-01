c50335cc3682b79a147aaf9674878c3e6cdb5ac8
Max Blum <maxblum94@yahoo.de>
2025-12-01 23:01:34 +0100

    types types and more types

--- extensions/geoguesser/src/api/client.ts
+++ extensions/geoguesser/src/api/client.ts
@@ -1,12 +1,19 @@
 import { getPreferenceValues } from "@raycast/api";
-import { getPreferences } from "../preferences";
-import { StatsResponse, FeedResponse, GameDetails, DuelDetails } from "../types";
+import {
+  StatsResponse,
+  FeedResponse,
+  GameDetails,
+  DuelDetails,
+  ProfileResponse,
+  DailyChallengeResponse,
+  MyDailyChallengeScore,
+} from "../types";
 
 const BASE_URL = "https://www.geoguessr.com/api";
 const GAME_SERVER_URL = "https://game-server.geoguessr.com/api";
 
 export async function apiRequest<T>(endpoint: string): Promise<T> {
-  const { ncfaToken } = getPreferences();
+  const { ncfaToken } = getPreferenceValues<Preferences>();
 
   const response = await fetch(`${BASE_URL}${endpoint}`, {
     headers: {
@@ -24,31 +31,31 @@ export async function apiRequest<T>(endpoint: string): Promise<T> {
 }
 
 // API functions
-export async function getProfile() {
+export async function getProfile(): Promise<ProfileResponse> {
   return apiRequest("/v3/profiles");
 }
 
-export async function getProfileStats() {
+export async function getProfileStats(): Promise<StatsResponse> {
   return apiRequest<StatsResponse>("/v3/profiles/stats");
 }
 
-export async function getDailyChallenge() {
+export async function getDailyChallenge(): Promise<DailyChallengeResponse> {
   return apiRequest("/v3/challenges/daily-challenges/today");
 }
 
-export async function getMyDailyChallengeScore() {
+export async function getMyDailyChallengeScore(): Promise<MyDailyChallengeScore> {
   return apiRequest("/v3/challenges/daily-challenges/me");
 }
 
-export async function getFeed(count = 10, page = 0) {
+export async function getFeed(count = 10, page = 0): Promise<FeedResponse> {
   return apiRequest(`/v4/feed/private?count=${count}&page=${page}`);
 }
 
-export async function getPreviousDailyChallenge() {
+export async function getPreviousDailyChallenge(): Promise<DailyChallengeResponse> {
   return apiRequest("/v3/challenges/daily-challenges/previous");
 }
 
-export async function getRecentGames(count = 10) {
+export async function getRecentGames(count = 10): Promise<FeedResponse> {
   return apiRequest<FeedResponse>(`/v4/feed/private?count=${count}&page=0`);
 }
 
