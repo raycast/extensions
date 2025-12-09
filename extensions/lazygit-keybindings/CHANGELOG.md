fc1f453e10190476d53c0fb646d13cd215fd1a6b
Noam <noamifergan@gmail.com>
2025-12-09 19:21:25 +0000

    Add Lazygit Keybindings extension

---
+++ extensions/lazygit-keybindings/CHANGELOG.md
@@ -0,0 +1,4 @@
+# Changelog
+
+## 1.0.0 - 2025-12-09
+- Initial release of the Lazygit Keybindings Raycast extension.
---
+++ extensions/lazygit-keybindings/eslint.config.mjs
@@ -0,0 +1,4 @@
+import baseConfig from "@raycast/eslint-config";
+
+// Flatten to avoid nested array entries in the Raycast base config.
+export default baseConfig.flat();
