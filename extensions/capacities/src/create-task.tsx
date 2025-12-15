3840034eb3949198af0edeed2bbff2cb29f793cc
Steffen Bleher <steffen.bleher@gmail.com>
2025-12-15 13:29:32 +0100

    Update capacities extension
    - Moved from pnpm to npm
    - Updated README to new version of docs
    - New Screenshots and updated docs
    - Improve icon consistency and keywords to commands
    - Fix web link command access
    - Added task creation to extension
    - Added correct merge date to change log
    - Remove abort signal
    - fix linting issues
    - Offline support, search details, better open behavior
    - Initial commit

--- extensions/capacities/src/create-task.tsx
+++ extensions/capacities/src/create-task.tsx
@@ -1,23 +1,14 @@
-import { ActionPanel, Action, Form, Icon, closeMainWindow, Clipboard, showToast, Toast, showHUD } from "@raycast/api";
+import { ActionPanel, Action, Form, Icon, closeMainWindow, showToast, Toast, showHUD } from "@raycast/api";
 import { FormValidation, useForm } from "@raycast/utils";
 import { useEffect, useRef } from "react";
-import { useActiveTab } from "./helpers/useActiveTab";
 import { API_HEADERS, API_URL, handleAPIError, handleUnexpectedError, useCapacitiesStore } from "./helpers/storage";
 
 interface SaveWeblinkBody {
   spaceId: string;
-  value: string;
+  title: string;
   mdText?: string;
-  tags?: string;
-}
-
-function isValidURL(url: string) {
-  try {
-    new URL(url);
-    return true;
-  } catch {
-    return false;
-  }
+  priority?: string;
+  date?: Date;
 }
 
 export default function Command() {
@@ -35,15 +26,51 @@ export default function Command() {
         style: Toast.Style.Animated,
         title: "Saving",
       });
+
+      // date processing
+      const dateTime = values.date;
+      let dateObject:
+        | {
+            dateResolution: "time" | "day";
+            startTime: string;
+          }
+        | undefined = undefined;
+      if (dateTime) {
+        if (
+          (dateTime.getMilliseconds() === 0 || dateTime.getMilliseconds() === 1) &&
+          dateTime.getSeconds() === 0 &&
+          dateTime.getMinutes() === 0 &&
+          dateTime.getHours() === 0
+        ) {
+          const newDate = new Date();
+          newDate.setUTCHours(0, 0, 0, 0);
+          newDate.setUTCDate(dateTime.getDate());
+          newDate.setUTCMonth(dateTime.getMonth());
+          newDate.setUTCFullYear(dateTime.getFullYear());
+          dateObject = {
+            dateResolution: "day",
+            startTime: newDate.toISOString(),
+          };
+        } else {
+          dateObject = {
+            dateResolution: "time",
+            startTime: dateTime.toISOString(),
+          };
+        }
+      }
+
       const body = {
         spaceId: store?.spaces.length === 1 ? store.spaces[0].id : values.spaceId,
-        url: values.value,
-        mdText: values.mdText,
-        tags: values.tags ? values.tags.split(",") : [],
+        title: values.title,
+        mdText: values.mdText?.trim()?.length ? values.mdText.trim() : undefined,
+        priority: values.priority === "empty" ? undefined : values.priority,
+        date: dateObject,
       };
 
+      console.log(body);
+
       try {
-        const response = await fetch(`${API_URL}/save-weblink`, {
+        const response = await fetch(`${API_URL}/save-task`, {
           method: "POST",
           headers: API_HEADERS,
           body: JSON.stringify(body),
@@ -57,7 +84,7 @@ export default function Command() {
           style: Toast.Style.Success,
           title: "Saved",
         });
-        showHUD("Weblink created");
+        showHUD("Task created");
         closeMainWindow();
       } catch (e) {
         if (e instanceof Error) {
@@ -68,51 +95,21 @@ export default function Command() {
       }
     },
     validation: {
-      value(value) {
+      title(value) {
         if (!value || value.trim() === "") {
-          return "A link is required";
-        }
-        if (!isValidURL(value)) {
-          return "Invalid URL";
+          return "A title is required";
         }
       },
       spaceId: spacesDropdown.current ? FormValidation.Required : undefined,
-      tags(value) {
-        if (value && value.split(",").length > 10) {
-          return "Maximum of 10 tags allowed.";
-        }
-      },
     },
   });
-
-  const activeTab = useActiveTab();
-
-  useEffect(() => {
-    async function checkClipboard() {
-      try {
-        const { text } = await Clipboard.read();
-        if (text && isValidURL(text) && !itemProps.value.value) {
-          setValue("value", text);
-        }
-      } catch (e) {
-        console.error(e);
-      }
-    }
-    checkClipboard();
-  }, []);
-
-  useEffect(() => {
-    if (activeTab && !itemProps.value.value) {
-      setValue("value", activeTab.url);
-    }
-  }, [activeTab]);
 
   return (
     <Form
       isLoading={storeIsLoading}
       actions={
         <ActionPanel>
-          <Action.SubmitForm title="Create Weblink" icon={Icon.CheckCircle} onSubmit={handleSubmit} />
+          <Action.SubmitForm title="Create Task" icon={Icon.CheckCircle} onSubmit={handleSubmit} />
         </ActionPanel>
       }
     >
@@ -132,14 +129,15 @@ export default function Command() {
           </Form.Dropdown>
         </>
       )}
-      <Form.TextField title="Link" {...itemProps.value} autoFocus />
-      <Form.TextField
-        title="Tags"
-        placeholder="Use a comma separated list of tags."
-        {...itemProps.tags}
-        info="Optional. Tags added to your web link object. Tags need to exactly match your tag names in Capacities, otherwise they will be created. You can add a maximum of 10 tags."
-        storeValue
-      />
+      <Form.TextField title="Title" {...itemProps.title} autoFocus />
+      <Form.Separator />
+      <Form.Dropdown id="priority" title="Priority" defaultValue="empty">
+        <Form.Dropdown.Item value="empty" title="No Priority" icon={Icon.Minus} />
+        <Form.Dropdown.Item value="low" title="Low" icon={Icon.Exclamationmark} />
+        <Form.Dropdown.Item value="medium" title="Medium" icon={Icon.Exclamationmark2} />
+        <Form.Dropdown.Item value="high" title="High" icon={Icon.Exclamationmark3} />
+      </Form.Dropdown>
+      <Form.DatePicker id="date" title="Date" defaultValue={null} />
       <Form.TextArea title="Notes" {...itemProps.mdText} info="Optional. Notes can be formatted in markdown." />
     </Form>
   );
