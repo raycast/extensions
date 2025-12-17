b5164d7595f51feff5ee88a3773b384a1339d114
j3lte <jwlagendijk@gmail.com>
2025-12-17 10:17:13 +0100

    Add Keyboard shortcut to Open With when default is selected

--- extensions/forked-extensions/src/manage-forked-extensions.tsx
+++ extensions/forked-extensions/src/manage-forked-extensions.tsx
@@ -114,7 +114,7 @@ function ManageForkedExtensions() {
                     icon={{ fileIcon: openWith.path }}
                   />
                 ) : null}
-                <Action.OpenWith path={x.folderPath} />
+                <Action.OpenWith path={x.folderPath} shortcut={openWith ? { key: "enter", modifiers: ["shift"]} : undefined} />
                 <Action.CopyToClipboard
                   title="Copy Extension Path to Clipboard"
                   content={x.folderPath}
