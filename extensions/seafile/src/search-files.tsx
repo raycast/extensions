914ded70f9d5038c853599ead9f277bdf857d0f5
xmok <30526133+xmok@users.noreply.github.com>
2025-12-19 00:28:19 +0500

    [Seafile] move stuff around + custom url

--- extensions/seafile/src/search-files.tsx
+++ extensions/seafile/src/search-files.tsx
@@ -2,77 +2,36 @@ import {
   Action,
   ActionPanel,
   Detail,
-  getPreferenceValues,
   Icon,
   Keyboard,
   List,
-  LocalStorage,
   open,
+  openExtensionPreferences,
   showToast,
   Toast,
 } from "@raycast/api";
 import { useCachedPromise } from "@raycast/utils";
 import { useState } from "react";
 import { filesize } from "filesize";
-import { ErrorResult, type FileDetails, SearchResult } from "./types";
-
-const { username, password } = getPreferenceValues<Preferences>();
-const SEAFILE_URL = "https://plus.seafile.com/";
-const API_URL = SEAFILE_URL + "api2/";
-const API_HEADERS = {
-  accept: "application/json",
-  "content-type": "application/json",
-};
-
-const parseResponse = async <T,>(response: Response) => {
-  const result = await response.json();
-  if (!response.ok) {
-    const error = result as ErrorResult;
-    if ("detail" in error) throw new Error(error.detail as string);
-    if ("error_msg" in error) throw new Error(error.error_msg as string);
-    throw new Error(`${Object.keys(error)[0]}: ${error[Object.keys(error)[0]]}`);
-  }
-  return result as T;
-};
-const getToken = async () => {
-  const token = await LocalStorage.getItem<string>("SEAFILE-ACCOUNT-TOKEN");
-  if (token) return token;
-  const response = await fetch(API_URL + "auth-token/", {
-    method: "POST",
-    headers: API_HEADERS,
-    body: JSON.stringify({ username, password }),
-  });
-  const result = await parseResponse<{ token: string }>(response);
-  const newToken = result.token;
-  await LocalStorage.setItem("SEAFILE-ACCOUNT-TOKEN", newToken);
-  return newToken;
-};
-const makeRequest = async <T,>(endpoint: string) => {
-  try {
-    const token = await getToken();
-    const response = await fetch(API_URL + endpoint, {
-      headers: { ...API_HEADERS, Authorization: `Bearer ${token}` },
-    });
-    const result = await parseResponse<T>(response);
-    return result;
-  } catch (error) {
-    const err = error as Error;
-    if (err.message === "Invalid token") await LocalStorage.removeItem("SEAFILE-ACCOUNT-TOKEN");
-    throw new Error(err.message);
-  }
-};
+import { type FileDetails, SearchResult } from "./types";
+import { makeRequest, SEAFILE_URL } from "./seafile";
 
 const buildSearchResultUrl = (result: SearchResult) => {
   let url = SEAFILE_URL;
   url += result.is_dir ? "library" : "lib";
   url += `/${result.repo_id}`;
-  url += result.is_dir ? "" : "/file";
+  if (!result.is_dir) url += "/file";
   url += encodeURI(result.fullpath === "/" ? `/${result.name}` : result.fullpath);
   return url;
 };
+
 export default function SearchFiles() {
   const [searchText, setSearchText] = useState("");
-  const { isLoading, data: results } = useCachedPromise(
+  const {
+    isLoading,
+    data: results,
+    error,
+  } = useCachedPromise(
     (q: string) => async (options) => {
       const result = await makeRequest<{ total: number; results: SearchResult[]; has_more: boolean }>(
         `search/?q=${q}&page=${options.page + 1}&per_page=25`,
@@ -87,6 +46,18 @@ export default function SearchFiles() {
       initialData: [],
     },
   );
+
+  if (error?.message === "Invalid URL")
+    return (
+      <Detail
+        markdown={`# ERROR \n\n Invalid URL \n Please make sure URL in Preferences is valid`}
+        actions={
+          <ActionPanel>
+            <Action icon={Icon.Gear} title="Open Extension Preferences" onAction={openExtensionPreferences} />
+          </ActionPanel>
+        }
+      />
+    );
 
   return (
     <List isLoading={isLoading} searchBarPlaceholder="Search files" onSearchTextChange={setSearchText} throttle>
@@ -115,15 +86,15 @@ export default function SearchFiles() {
                   <Action.Push icon={Icon.Info} title="Properties" target={<FileDetails result={result} />} />
                   <Action
                     icon={Icon.Download}
-                    title="Open Download URL"
+                    title="Download"
                     onAction={async () => {
-                      const toast = await showToast(Toast.Style.Animated, "Generating URL", result.name);
+                      const toast = await showToast(Toast.Style.Animated, "Starting Download", result.name);
                       try {
                         const url = await makeRequest<string>(
                           `repos/${result.repo_id}/file/?p=${encodeURIComponent(result.fullpath)}`,
                         );
                         toast.style = Toast.Style.Success;
-                        toast.title = "Opening URL";
+                        toast.title = "Download Started";
                         await open(url);
                       } catch (error) {
                         toast.style = Toast.Style.Failure;
