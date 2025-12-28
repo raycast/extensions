c84262392fdf940e6ff30dfe9db58d6e85de7b56
Arthur Pinheiro <arthurpinheiro@protonmail.com>
2025-12-28 09:24:12 -0300

    Update urban-dictionary extension

--- extensions/urban-dictionary/src/index.tsx
+++ extensions/urban-dictionary/src/index.tsx
@@ -1,4 +1,4 @@
-import { Action, ActionPanel, Icon, List } from "@raycast/api";
+import { Action, ActionPanel, Icon, Keyboard, List } from "@raycast/api";
 import { useFetch } from "@raycast/utils";
 import { useState } from "react";
 
@@ -84,7 +84,7 @@ function ItemDetails({ term }: { term: string }) {
                   <Action.CopyToClipboard
                     title={`Copy "${term}" to Clipboard`}
                     content={term}
-                    shortcut={{ modifiers: ["cmd", "shift"], key: "c" }}
+                    shortcut={Keyboard.Shortcut.Common.Copy}
                   />
                 </ActionPanel.Section>
               </ActionPanel>
