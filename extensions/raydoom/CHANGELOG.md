9f57fb52d180aa6a90f6e5c1c78524e7d2bd9aae
Saketh Chandra <b.sakethchandra9@gmail.com>
2025-12-30 21:51:12 +0530

    Add raydoom extension
    - feat: Update metadata images and clean up whitespace in code files
    - Refactor code structure for improved readability and maintainability
    - updated the icon
    - feat: Refactor WAD file management and enhance validation checks
    - feat: Enhance DOOM game controls and improve state management with logging
    - feat: Implement DOOM game integration with Raycast

---
+++ extensions/raydoom/CHANGELOG.md
@@ -0,0 +1,3 @@
+# RayDoom Changelog
+
+## [Initial Version] - {PR_MERGE_DATE}
---
+++ extensions/raydoom/src/input-mapper.ts
@@ -0,0 +1,67 @@
+/**
+ * Input Mapper - Maps Raycast keyboard shortcuts to Doom key codes
+ */
+
+// Import DoomKey from raydoom-core
+import { DoomKey } from "raydoom-core";
+
+/**
+ * Available input actions for Doom
+ */
+export enum InputAction {
+  MOVE_FORWARD = "move_forward",
+  MOVE_BACKWARD = "move_backward",
+  TURN_LEFT = "turn_left",
+  TURN_RIGHT = "turn_right",
+  STRAFE_LEFT = "strafe_left",
+  STRAFE_RIGHT = "strafe_right",
+  USE = "use",
+  FIRE = "fire",
+  WEAPON_1 = "weapon_1",
+  WEAPON_2 = "weapon_2",
+  WEAPON_3 = "weapon_3",
+  WEAPON_4 = "weapon_4",
+  WEAPON_5 = "weapon_5",
+  WEAPON_6 = "weapon_6",
+  WEAPON_7 = "weapon_7",
+  ESCAPE = "escape",
+  ENTER = "enter",
+  YES = "yes",
+  NO = "no",
+  MAP = "map",
+  PAUSE = "pause",
+}
+
+/**
+ * Map of input actions to Doom key codes
+ */
+export const INPUT_ACTION_MAP: Record<InputAction, number> = {
+  [InputAction.MOVE_FORWARD]: DoomKey.KEY_UPARROW,
+  [InputAction.MOVE_BACKWARD]: DoomKey.KEY_DOWNARROW,
+  [InputAction.TURN_LEFT]: DoomKey.KEY_LEFTARROW,
+  [InputAction.TURN_RIGHT]: DoomKey.KEY_RIGHTARROW,
+  [InputAction.STRAFE_LEFT]: DoomKey.KEY_STRAFE_L,
+  [InputAction.STRAFE_RIGHT]: DoomKey.KEY_STRAFE_R,
+  [InputAction.USE]: DoomKey.KEY_USE,
+  [InputAction.FIRE]: DoomKey.KEY_FIRE,
+  [InputAction.WEAPON_1]: 49, // '1'
+  [InputAction.WEAPON_2]: 50, // '2'
+  [InputAction.WEAPON_3]: 51, // '3'
+  [InputAction.WEAPON_4]: 52, // '4'
+  [InputAction.WEAPON_5]: 53, // '5'
+  [InputAction.WEAPON_6]: 54, // '6'
+  [InputAction.WEAPON_7]: 55, // '7'
+  [InputAction.ESCAPE]: DoomKey.KEY_ESCAPE,
+  [InputAction.ENTER]: DoomKey.KEY_ENTER,
+  [InputAction.YES]: DoomKey.KEY_Y,
+  [InputAction.NO]: DoomKey.KEY_N,
+  [InputAction.MAP]: DoomKey.KEY_TAB,
+  [InputAction.PAUSE]: DoomKey.KEY_PAUSE,
+};
+
+/**
+ * Get Doom key code for an input action
+ */
+export function getDoomKey(action: InputAction): number {
+  return INPUT_ACTION_MAP[action];
+}
