27b265bedd9b9d73f1ce362ff115e82e004b6182
n0kovo <n0kovo@riseup.net>
2025-12-30 22:29:50 +0100

    feat: add 'close window on trigger' preference

--- extensions/soundboard/src/favorite6.tsx
+++ extensions/soundboard/src/favorite6.tsx
@@ -1,5 +1,11 @@
+import { closeMainWindow, getPreferenceValues } from "@raycast/api";
 import { playSoundFromIndex } from "./utils";
 
 export default async function Main() {
+  const { closeOnTrigger } = getPreferenceValues();
+
+  if (closeOnTrigger) {
+    await closeMainWindow();
+  }
   await playSoundFromIndex(6);
 }
