086ec54251ef109be1c69c0c5352bbcefac1efe5
kim-raaschou-nielsen <kim-raaschou-nielsen@users.noreply.github.com>
2026-01-06 19:56:29 +0100

    add filter

--- extensions/aerospace/src/switchApps.tsx
+++ extensions/aerospace/src/switchApps.tsx
@@ -58,24 +58,28 @@ export default function Command(
     >
       {Object.entries(groupedByWorkspace).map(([workspaceName, group]) => (
         <List.Section key={workspaceName} title={`Workspace ${workspaceName} - ${group.monitor}`}>
-          {group.windows.map((window) => (
-            <List.Item
-              key={window["window-id"]}
-              title={window["app-name"]}
-              subtitle={window["window-title"]}
-              icon={{ fileIcon: window["app-path"] }}
-              actions={
-                <ActionPanel>
-                  <Action
-                    title="Focus Window"
-                    onAction={() => {
-                      focusWindow(window["window-id"].toString());
-                    }}
-                  />
-                </ActionPanel>
-              }
-            />
-          ))}
+          {group.windows
+            .filter((window) =>
+              searchText ? window["app-name"].toLowerCase().startsWith(searchText?.toLowerCase()) : true,
+            )
+            .map((window) => (
+              <List.Item
+                key={window["window-id"]}
+                title={window["app-name"]}
+                subtitle={window["window-title"]}
+                icon={{ fileIcon: window["app-path"] }}
+                actions={
+                  <ActionPanel>
+                    <Action
+                      title="Focus Window"
+                      onAction={() => {
+                        focusWindow(window["window-id"].toString());
+                      }}
+                    />
+                  </ActionPanel>
+                }
+              />
+            ))}
         </List.Section>
       ))}
     </List>
