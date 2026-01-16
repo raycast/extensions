67ddd7c67b5653a1f8524a3a3ec8cb5abf0fc9ba
Josh Millgate <38218081+joshmillgate@users.noreply.github.com>
2026-01-16 23:36:57 +0000

    Update QuickLookup function argument type

--- extensions/zacks-stock-ranking/src/zacks-quick.tsx
+++ extensions/zacks-stock-ranking/src/zacks-quick.tsx
@@ -1,8 +1,6 @@
 import { LaunchProps } from "@raycast/api";
 import { StockDetailView } from "./components/StockDetailView";
 
-export default function QuickLookup(props: LaunchProps<{ arguments: Arguments.ZacksQuick }>) {
-
 export default function QuickLookup(props: LaunchProps<{ arguments: QuickLookupArguments }>) {
   return <StockDetailView ticker={props.arguments.ticker} />;
 }
