9be21e279e4403e5f72a7c333b01b9f065a44d2c
Michael Y. Qiu <altmickymike@outlook.com>
2026-01-18 10:06:36 +0800

    Update desktoprenamer extension
    - Merge branch \'contributions/merge-1768701926453\'
    - Pull contributions
    - Restyle
    - Merge branch \'contributions/merge-1768701779248\'
    - Pull contributions
    - Use black theme screenshots
    - Add metadata
    - Accept suggestion from greptile

--- extensions/desktoprenamer/src/rename-current-space.ts
+++ extensions/desktoprenamer/src/rename-current-space.ts
@@ -2,8 +2,6 @@ import { LaunchProps, showHUD } from "@raycast/api";
 import { runAppleScript } from "@raycast/utils";
 
 export default async function Command(props: LaunchProps<{ arguments: Arguments.RenameCurrentSpace }>) {
-
-export default async function Command(props: LaunchProps<{ arguments: RenameArguments }>) {
   const { newName } = props.arguments;
 
   try {
