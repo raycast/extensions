b0f5a0f7ca0e7bfeb5f41c9d0df4360e95ebba1b
Sorin Muntean <me@sorinmuntean.ro>
2026-01-26 19:40:06 +0100

    Fork Repositories: Add support for reading `repositories.toml`, falling back to the old `repositories.json`.

--- extensions/fork-repositories/CHANGELOG.md
+++ extensions/fork-repositories/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Fork Repositories Changelog
 
+## [Bug Fix] - 2026-01-26
+
+- Added support for Fork's `repositories.toml` file with fallback to `repositories.json`.
+
 ## [Bug Fix] - 2024-04-05
 
 - Fixed an issue where `Open in Fork` would properly open Fork, but not the the target repository.
--- extensions/fork-repositories/src/constants.ts
+++ extensions/fork-repositories/src/constants.ts
@@ -1,4 +1,6 @@
 import { homedir } from "node:os";
 
 export const FORK_BUNDLE_ID = "com.DanPristupov.Fork";
-export const REPO_FILE_PATH = `${homedir()}/Library/Application Support/${FORK_BUNDLE_ID}/repositories.json`;
+export const REPO_FILE_PATH_TOML = `${homedir()}/Library/Application Support/${FORK_BUNDLE_ID}/repositories.toml`;
+export const REPO_FILE_PATH_JSON = `${homedir()}/Library/Application Support/${FORK_BUNDLE_ID}/repositories.json`;
+export const REPO_FILE_PATHS = [REPO_FILE_PATH_TOML, REPO_FILE_PATH_JSON];
