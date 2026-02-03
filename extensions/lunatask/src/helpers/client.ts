460bbb6f216285ec2ffd45ad4ca47b7fe52f986c
otacakes <jedceladez20@gmail.com>
2026-02-03 13:11:49 +0800

    lunatask: Add Windows support

--- extensions/lunatask/src/helpers/client.ts
+++ extensions/lunatask/src/helpers/client.ts
@@ -1,22 +1,20 @@
 import { getPreferenceValues } from "@raycast/api";
-import fetch from "cross-fetch";
 
-const { accessToken } = getPreferenceValues();
+const { accessToken } = getPreferenceValues<{ accessToken: string }>();
 
 const prefixURL = "https://api.lunatask.app/v1";
 const headers = {
-  "Content-Type": "application/json",
-  Authorization: `bearer ${accessToken}`,
+    "Content-Type": "application/json",
+    Authorization: `bearer ${accessToken}`,
 };
 
 const client = {
-  post: (url: string, body: object) =>
-    fetch(`${prefixURL}/${url}`, {
-      method: "POST",
-      body: JSON.stringify(body),
-      headers,
-    }),
-  credentials: "same-origin",
+    post: (url: string, body: object) =>
+        fetch(`${prefixURL}/${url}`, {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers,
+        }),
 };
 
 export default client;
