e73fe76931114a4bddca6d964445794d0aad7e8d
ridemountainpig <lin0905580802@gmail.com>
2026-02-07 16:00:10 +0800

    Update search-movies-page.tsx

--- extensions/letterboxd/src/search-movies-page.tsx
+++ extensions/letterboxd/src/search-movies-page.tsx
@@ -64,12 +64,18 @@ function MovieItem(props: MovieItemProps) {
           </>
           <ActionPanel.Section>
             <Action.CopyToClipboard
-              shortcut={{ modifiers: ["cmd"], key: "." }}
+              shortcut={{
+                macOS: { modifiers: ["cmd"], key: "." },
+                Windows: { modifiers: ["ctrl"], key: "." },
+              }}
               title={STRINGS.copyTitle}
               content={title}
             />
             <Action.CopyToClipboard
-              shortcut={{ modifiers: ["cmd", "shift"], key: "," }}
+              shortcut={{
+                macOS: { modifiers: ["cmd", "shift"], key: "," },
+                Windows: { modifiers: ["ctrl", "shift"], key: "," },
+              }}
               title={STRINGS.copyUrl}
               content={getFullURL(detailsPage)}
             />
