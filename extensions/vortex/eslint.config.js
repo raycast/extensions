90b0cac7e307c37fdfe924f36a1824901ae83ca6
saunter <68239231+stackingsaunter@users.noreply.github.com>
2026-02-12 02:44:39 +0100

    vortex: fix CI icon/lint checks and update changelog

--- extensions/vortex/eslint.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "eslint/config";
-import raycastConfig from "@raycast/eslint-config";
-
-const config = Array.isArray(raycastConfig) ? raycastConfig.flat() : [raycastConfig];
-
-export default defineConfig(config);
-
