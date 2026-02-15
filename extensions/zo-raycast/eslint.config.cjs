c32b0f73e1c0f965cc868befb6c27b86ad95b131
Jordan Stella <jordanjstella@gmail.com>
2026-02-14 20:26:09 -0700

    Add zo-raycast extension
    - Prepare Raycast store submission
    - Review Raycast store readiness
    - Remove MCP tooling from extension
    - Update Zo app icon display
    - Implement Zo Chat thinking toggle
    - Fix Zo Chat model selector errors
    - Initial commit

---
+++ extensions/zo-raycast/eslint.config.cjs
@@ -0,0 +1,46 @@
+const js = require("@eslint/js");
+const { FlatCompat } = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+module.exports = [
+  ...compat.config({
+    root: true,
+    env: {
+      es2022: true,
+      node: true,
+    },
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      project: "./tsconfig.json",
+      tsconfigRootDir: __dirname,
+      ecmaVersion: "latest",
+      sourceType: "module",
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    plugins: ["@typescript-eslint"],
+    extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended-type-checked",
+      "plugin:@typescript-eslint/stylistic-type-checked",
+      "prettier",
+    ],
+    ignorePatterns: ["dist", "coverage", "node_modules"],
+    rules: {
+      "@typescript-eslint/consistent-type-imports": ["error", { fixStyle: "inline-type-imports" }],
+      "@typescript-eslint/consistent-type-definitions": "off",
+      "@typescript-eslint/array-type": "off",
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-unsafe-argument": "off",
+      "@typescript-eslint/no-unnecessary-type-assertion": "off",
+      "@typescript-eslint/no-misused-promises": "off",
+      "@typescript-eslint/no-floating-promises": "error",
+    },
+  }),
+];
