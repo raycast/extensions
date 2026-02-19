4636ed19f0e4ea9f9d46100284ea3a77f57225b9
lafkpages <luisafk@proton.me>
2026-02-19 19:26:56 +0100

    fix: use js-base64

--- extensions/base64/src/tools/base64-decode.ts
+++ extensions/base64/src/tools/base64-decode.ts
@@ -1,3 +1,5 @@
+import { decode } from "js-base64";
+
 export default async function (input: { input: string }) {
-  return atob(input.input);
+  return decode(input.input);
 }
--- extensions/base64/src/tools/base64-encode.ts
+++ extensions/base64/src/tools/base64-encode.ts
@@ -1,3 +1,5 @@
+import { encode } from "js-base64";
+
 export default function (input: { input: string }) {
-  return btoa(input.input);
+  return encode(input.input);
 }
