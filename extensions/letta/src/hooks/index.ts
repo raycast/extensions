33f6b37523051c23420493e8f161ba13a4acb882
John Wessel <john@agreeabledata.com>
2026-02-21 08:08:04 -0500

    Remove useChat export from hooks barrel (file deleted)

--- extensions/letta/src/hooks/index.ts
+++ extensions/letta/src/hooks/index.ts
@@ -4,5 +4,4 @@
 
 export { useLettaClient, useAccounts, getAccounts } from "./useLettaClient";
 export { useAgents, toAgentsWithAccount, type AgentSummary } from "./useAgents";
-export { useChat, type ToolCall } from "./useChat";
 export { useConversations } from "./useConversations";
