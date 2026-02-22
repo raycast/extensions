eb82fea7b6ef633681f4e399717ea7e2ea828da0
Sahil Gulihar <sahilgullihar@gmail.com>
2026-02-22 22:28:35 +0530

    wow

--- extensions/network-speed-monitor/eslint.config.mjs
+++ extensions/network-speed-monitor/eslint.config.mjs
@@ -1,3 +1,4 @@
+import { defineConfig } from "eslint/config"
 import raycastConfig from "@raycast/eslint-config"
 
-export default raycastConfig.flat()
+export default defineConfig([...raycastConfig])
