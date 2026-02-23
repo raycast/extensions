b1548f2b1ef62bb53bb9a6a48528a3ed6a550111
Abdullah Nettoor <abdullahnettoor@gmail.com>
2026-02-24 01:52:30 +0530

    Clean up redundant quran-audio after merge

--- extensions/quran-audio/CHANGELOG.md
+++
@@ -1,3 +0,0 @@
-# Quran Audio Changelog
-
-## [Initial Version] - {PR_MERGE_DATE}
--- extensions/quran-audio/src/lib/constants.ts
+++
@@ -1,8 +0,0 @@
-export const API_BASE_URL = "https://api.quran.com/api/v4";
-export const FAV_SURAH_KEY = "fav_surahs";
-export const FAV_RECITER_KEY = "fav_reciters";
-
-export const CACHE_KEYS = {
-  CHAPTERS: "quran_chapters",
-  RECITATIONS: "quran_recitations",
-};
