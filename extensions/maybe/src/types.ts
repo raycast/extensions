b1424830f65300bec1a7fdc0ae167b9467acf3b9
xmok <30526133+xmok@users.noreply.github.com>
2026-02-24 00:05:54 +0500

    [Maybe] full extension

--- extensions/maybe/src/types.ts
+++ extensions/maybe/src/types.ts
@@ -1,41 +1,47 @@
 export type Account = {
-    "id": string
-    "name": string
-    "balance": string
-    "currency": string
-    "classification": string
-    "account_type": string
-}
+  id: string;
+  name: string;
+  balance: string;
+  currency: string;
+  classification: string;
+  account_type: string;
+};
 type Category = {
-  "id": string
-  "name": string
-  "classification": string
-  "color": string
-  "icon": string
-}
+  id: string;
+  name: string;
+  classification: string;
+  color: string;
+  icon: string;
+};
 export type Transaction = {
-      "id": string
-      "date": string
-      "amount": string
-      "currency": string
-      "name": string
-      "notes": string
-      "classification": string
-      "account": Pick<Account, "id"|"name"|"account_type">
-      "category": Category | null,
-    //   "merchant": null,
-    //   "tags": [],
-    //   "transfer": null,
-      "created_at": string
-      "updated_at": string
-    }
-export type PaginationInfo ={
-    "pagination": {
-        "page": 1,
-        "per_page": 25,
-        "total_count": 3,
-        "total_pages": 1
-  }
-}
+  id: string;
+  date: string;
+  amount: string;
+  currency: string;
+  name: string;
+  notes: string;
+  classification: string;
+  account: Pick<Account, "id" | "name" | "account_type">;
+  category: Category | null;
+  created_at: string;
+  updated_at: string;
+};
+export type PaginationInfo = {
+  pagination: {
+    page: 1;
+    per_page: 25;
+    total_count: 3;
+    total_pages: 1;
+  };
+};
 
-export type ErrorResult = {status:number; error:string}| {error:string; message:string, errors?: string[]}
+export type TransactionCreateRequest = {
+  description: string;
+  account_id: string;
+  amount: string;
+  currency: string;
+  date: Date | null;
+  notes: string;
+};
+
+export type ErrorResult = { status: number; error: string } | { error: string; message: string; errors?: string[] };
