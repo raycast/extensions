821fd0db6842ca670466989da2c18b269b5a6f5d
gandli <gandli@qq.com>
2026-03-09 18:30:13 +0800

    Add raycast-env.d.ts

---
+++ extensions/reverse-shell-generator/raycast-env.d.ts
@@ -0,0 +1,24 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `generate-reverse-shell` command */
+  export type GenerateReverseShell = ExtensionPreferences & {}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `generate-reverse-shell` command */
+  export type GenerateReverseShell = {}
+}
+
