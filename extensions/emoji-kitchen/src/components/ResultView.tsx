4fe2c1d7a2765360de7ca7af13d4e11e910501f9
Corey <corey.oneal@gmail.com>
2026-03-12 11:14:04 -0700

    Refactor: Switch ResultView to Detail for better image scaling

--- extensions/emoji-kitchen/src/components/ResultView.tsx
+++ extensions/emoji-kitchen/src/components/ResultView.tsx
@@ -1,4 +1,4 @@
-import { ActionPanel, Action, Grid, useNavigation, Icon } from "@raycast/api";
+import { ActionPanel, Action, Detail, useNavigation, Icon } from "@raycast/api";
 import { copyImageToClipboard } from "../utils";
 
 interface ResultViewProps {
@@ -12,35 +12,40 @@ export function ResultView({ url, e1, e2, onReset }: ResultViewProps) {
   const { pop } = useNavigation();
   const name = `${e1}_${e2}_mashup`;
 
+  const markdown = `
+<div align="center">
+  <img src="${url}" alt="${e1} + ${e2}" width="250" />
+</div>
+`;
+
   return (
-    <Grid columns={1} searchBarPlaceholder="Result">
-      <Grid.Item
-        content={url}
-        title={`${e1} + ${e2}`}
-        actions={
-          <ActionPanel>
-            <Action
-              title="Copy Image"
-              icon={Icon.CopyClipboard}
-              onAction={() => copyImageToClipboard(url, name)}
-            />
-            <Action.CopyToClipboard title="Copy Image URL" content={url} />
-            <Action.OpenInBrowser title="Open in Browser" url={url} />
-            <Action.CopyToClipboard
-              title="Copy Emoji Combination"
-              content={`${e1}${e2}`}
-            />
-            <Action
-              title="Mix More"
-              onAction={() => {
-                onReset();
-                pop();
-              }}
-              icon={Icon.PlusCircle}
-            />
-          </ActionPanel>
-        }
-      />
-    </Grid>
+    <Detail
+      markdown={markdown}
+      navigationTitle={`${e1} + ${e2}`}
+      actions={
+        <ActionPanel>
+          <Action
+            title="Copy Image"
+            icon={Icon.CopyClipboard}
+            onAction={() => copyImageToClipboard(url, name)}
+          />
+          <Action.CopyToClipboard title="Copy Image URL" content={url} />
+          <Action.OpenInBrowser title="Open in Browser" url={url} />
+          <Action.CopyToClipboard
+            title="Copy Emoji Combination"
+            content={`${e1}${e2}`}
+          />
+          <Action
+            title="Mix More"
+            onAction={() => {
+              onReset();
+              pop();
+            }}
+            icon={Icon.PlusCircle}
+            shortcut={{ modifiers: ["cmd"], key: "m" }}
+          />
+        </ActionPanel>
+      }
+    />
   );
 }
