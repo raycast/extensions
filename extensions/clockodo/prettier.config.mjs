87267da83f7b4a8fa772fd056b09e419707f3b35
Leonhard Melzer <leonhard.melzer@peerigon.com>
2026-03-18 21:03:15 +0100

    fix: remove shared eslint config

--- extensions/clockodo/prettier.config.mjs
+++ extensions/clockodo/prettier.config.mjs
@@ -1,7 +1,9 @@
-import { config } from "@peerigon/configs/prettier";
+// TODO: Ideally we'd use the shared config from @peerigon/configs,
+// but this causes issues in the Raycast CI pipeline (think because of old Node.js version)
+// import { config } from "@peerigon/configs/prettier";
 
 export default {
-  ...config,
+//   ...config,
   printWidth: 120,
   singleQuote: false,
 };
