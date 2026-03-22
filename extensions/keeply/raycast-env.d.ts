5ed9461d0475355f0c078dfd64ed04d86533b679
Rolf Koenders <post@madebyrolf.nl>
2026-03-22 18:20:03 +0100

    Add auto-generated raycast-env.d.ts

---
+++ extensions/keeply/raycast-env.d.ts
@@ -0,0 +1,31 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {
+  /** API Key - Your Keeply API key. Generate one at app.keeply.tools/settings → Developer */
+  "apiKey": string
+}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `search-bookmarks` command */
+  export type SearchBookmarks = ExtensionPreferences & {}
+  /** Preferences accessible in the `add-bookmark` command */
+  export type AddBookmark = ExtensionPreferences & {}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `search-bookmarks` command */
+  export type SearchBookmarks = {}
+  /** Arguments passed to the `add-bookmark` command */
+  export type AddBookmark = {}
+}
+
