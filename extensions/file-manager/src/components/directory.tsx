74758985c7d1ca4a3eb5953a2f2e429640b1048e
erics118 <52634785+erics118@users.noreply.github.com>
2026-04-03 15:14:45 -0400

    fix ignore logic, state

--- extensions/file-manager/src/components/directory.tsx
+++ extensions/file-manager/src/components/directory.tsx
@@ -2,6 +2,7 @@ import fs from "node:fs";
 import { Action, ActionPanel, Detail, getPreferenceValues, List } from "@raycast/api";
 import { useState } from "react";
 import { execSync } from "node:child_process";
+import { homedir } from "node:os";
 import { join } from "node:path";
 import { getDirectoryData, createItem, getStartDirectory, iCloudDrivePath, escapeShellArg } from "../utils";
 import { FileDataType } from "../types";
@@ -13,42 +14,42 @@ export function Directory(props: { path: string; ignores: GitIgnoreHelper[]; ini
     return <Detail markdown={`# Error: \n\nThe directory \`${props.path}\` does not exist. `} />;
   }
 
-  const ignores = props.ignores;
+  // copy to avoid mutating the parent's array across renders
+  const ignores = [...props.ignores];
   const preferences = getPreferenceValues<Preferences>();
 
-  const [directoryData, setDirectoryData] = useState<FileDataType[]>(() => {
-    // .gitignore
-    if (preferences.respectGitignore) {
-      if (fs.existsSync(`${props.path}/.gitignore`)) {
-        ignores.push(parser.compile(fs.readFileSync(`${props.path}/.gitignore`, "utf8")));
-      }
-
-      // ~.config/git/ignore
-      if (props.initial) {
-        if (fs.existsSync(`${getStartDirectory()}/.config/git/ignore`)) {
-          ignores.push(parser.compile(fs.readFileSync(`${getStartDirectory()}/.config/git/ignore`, "utf8")));
-        }
-      }
+  // .gitignore
+  if (preferences.respectGitignore) {
+    if (fs.existsSync(`${props.path}/.gitignore`)) {
+      ignores.push(parser.compile(fs.readFileSync(`${props.path}/.gitignore`, "utf8")));
     }
 
-    // .rayignore
-    if (preferences.respectRayignore) {
-      if (fs.existsSync(`${props.path}/.rayignore`)) {
-        ignores.push(parser.compile(fs.readFileSync(`${props.path}/.rayignore`, "utf8")));
+    // ~.config/git/ignore
+    if (props.initial) {
+      if (fs.existsSync(`${homedir()}/.config/git/ignore`)) {
+        ignores.push(parser.compile(fs.readFileSync(`${homedir()}/.config/git/ignore`, "utf8")));
       }
     }
+  }
 
+  // .rayignore
+  if (preferences.respectRayignore) {
+    if (fs.existsSync(`${props.path}/.rayignore`)) {
+      ignores.push(parser.compile(fs.readFileSync(`${props.path}/.rayignore`, "utf8")));
+    }
+  }
+
+  function getFilteredData(): FileDataType[] {
     const data = getDirectoryData(props.path);
-
     return data.filter((f) => {
       const path = f.name + (f.type === "directory" ? "/" : "");
-      for (const ignore of props.ignores) {
+      for (const ignore of ignores) {
         if (ignore.denies(path)) {
           return false;
         }
       }
       if (!preferences.showHiddenFiles) {
-        const fullPath = `${props.path}/${f.name}`;
+        const fullPath = join(props.path, f.name);
         const escapedPath = escapeShellArg(fullPath);
         const statOutput = execSync(`stat -f%f ${escapedPath}`, {
           encoding: "utf8",
@@ -61,7 +62,9 @@ export function Directory(props: { path: string; ignores: GitIgnoreHelper[]; ini
       }
       return true;
     });
-  });
+  }
+
+  const [directoryData, setDirectoryData] = useState<FileDataType[]>(() => getFilteredData());
 
   if (preferences.directoriesFirst) {
     const directories = directoryData.filter((file) => file.type === "directory");
@@ -82,13 +85,11 @@ export function Directory(props: { path: string; ignores: GitIgnoreHelper[]; ini
           </List.Section>
         )}
         <List.Section title="Directories">
-          {directories.map((data) =>
-            createItem(data, () => setDirectoryData(getDirectoryData(props.path)), preferences, ignores),
-          )}
+          {directories.map((data) => createItem(data, () => setDirectoryData(getFilteredData()), preferences, ignores))}
         </List.Section>
         <List.Section title="Files">
           {nonDirectories.map((data) =>
-            createItem(data, () => setDirectoryData(getDirectoryData(props.path)), preferences, ignores),
+            createItem(data, () => setDirectoryData(getFilteredData()), preferences, ignores),
           )}
         </List.Section>
       </List>
@@ -108,9 +109,7 @@ export function Directory(props: { path: string; ignores: GitIgnoreHelper[]; ini
           />
         )}
 
-        {directoryData.map((data) =>
-          createItem(data, () => setDirectoryData(getDirectoryData(props.path)), preferences, ignores),
-        )}
+        {directoryData.map((data) => createItem(data, () => setDirectoryData(getFilteredData()), preferences, ignores))}
       </List>
     );
   }
