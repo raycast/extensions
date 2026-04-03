32b8c20b8984aecfaa3e3b389ea64e42bd041c6f
erics118 <52634785+erics118@users.noreply.github.com>
2026-04-03 15:22:55 -0400

    explicitly handle broken symlink

--- extensions/file-manager/src/components/symlink-item.tsx
+++ extensions/file-manager/src/components/symlink-item.tsx
@@ -1,5 +1,6 @@
 import fs from "node:fs";
 import { join, resolve } from "node:path";
+import { Icon, List } from "@raycast/api";
 import { DirectoryItem } from "./directory-item";
 import { FileItem } from "./file-item";
 import { FileDataType } from "../types";
@@ -15,7 +16,11 @@ export function SymlinkItem(props: {
   const a = fs.readlinkSync(filePath);
   const originalPath = resolve(props.fileData.path, a);
   const originalFileData = fs.lstatSync(originalPath, { throwIfNoEntry: false });
-  if (originalFileData?.isDirectory() ?? false) {
+  if (originalFileData === undefined) {
+    // handle broken symlink
+    return <List.Item title={props.fileData.name} subtitle="Broken symlink" icon={Icon.Warning} />;
+  }
+  if (originalFileData.isDirectory()) {
     return (
       <DirectoryItem
         fileData={props.fileData}
