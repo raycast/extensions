653fb1a763953e8031f34d7d6834529912eb0b07
RUBisco0211 <1205456072@qq.com>
2026-04-04 20:21:55 +0800

    fix: react rule and img filter

--- extensions/picgo/src/components/ImagesPreviewPage.tsx
+++ extensions/picgo/src/components/ImagesPreviewPage.tsx
@@ -9,11 +9,12 @@ interface Props {
 }
 
 export default function ImagesPreviewPage({ imgs }: Props) {
+    const [formatKey, setFormatKey] = useState<keyof typeof exportFormats>("url");
+    const format = useMemo(() => exportFormats[formatKey]!, [formatKey]);
+
     if (imgs.length === 0) {
         return <Grid.EmptyView title="No Content"></Grid.EmptyView>;
     }
-    const [formatKey, setFormatKey] = useState<keyof typeof exportFormats>("url");
-    const format = useMemo(() => exportFormats[formatKey]!, [formatKey]);
 
     return (
         <Grid
@@ -29,32 +30,34 @@ export default function ImagesPreviewPage({ imgs }: Props) {
                 </Grid.Dropdown>
             }
         >
-            {imgs.map((img) => (
-                <Grid.Item
-                    key={img.imgUrl}
-                    content={img.imgUrl!}
-                    title={img.fileName}
-                    subtitle={img.imgUrl}
-                    accessory={{ icon: Icon.Link, tooltip: img.imgUrl }}
-                    actions={
-                        <ActionPanel>
-                            <Action.CopyToClipboard
-                                title={`Copy as ${format.label} Format`}
-                                content={format.generate([img])}
-                            />
-                            <Action.CopyToClipboard
-                                title={`Copy All as ${format.label} Format`}
-                                content={format.generate(imgs)}
-                            />
-                            <Action.Push
-                                title="Switch to Format List View"
-                                icon={Icon.Switch}
-                                target={<FormatListPage result={imgs} />}
-                            />
-                        </ActionPanel>
-                    }
-                ></Grid.Item>
-            ))}
+            {imgs
+                .filter((i) => i.imgUrl)
+                .map((img) => (
+                    <Grid.Item
+                        key={img.imgUrl}
+                        content={img.imgUrl!}
+                        title={img.fileName}
+                        subtitle={img.imgUrl}
+                        accessory={{ icon: Icon.Link, tooltip: img.imgUrl }}
+                        actions={
+                            <ActionPanel>
+                                <Action.CopyToClipboard
+                                    title={`Copy as ${format.label} Format`}
+                                    content={format.generate([img])}
+                                />
+                                <Action.CopyToClipboard
+                                    title={`Copy All as ${format.label} Format`}
+                                    content={format.generate(imgs)}
+                                />
+                                <Action.Push
+                                    title="Switch to Format List View"
+                                    icon={Icon.Switch}
+                                    target={<FormatListPage result={imgs} />}
+                                />
+                            </ActionPanel>
+                        }
+                    ></Grid.Item>
+                ))}
         </Grid>
     );
 }
