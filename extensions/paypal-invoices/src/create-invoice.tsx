abef5298b2bcd736bf6cc0be14df959b3aee3aec
Ethan Luong <ethananderstandable@gmail.com>
2026-04-16 17:29:21 +0700

    Persist dueDate to local storage on invoice creation

--- extensions/paypal-invoices/src/create-invoice.tsx
+++ extensions/paypal-invoices/src/create-invoice.tsx
@@ -192,6 +192,7 @@ export default function CreateInvoiceCommand(
         currency,
         total,
         createdAt: new Date().toISOString(),
+        dueDate: dueDate ? dueDate.toISOString().split("T")[0] : undefined,
         status: hasValidEmail ? "SENT" : "UNPAID",
         payerViewUrl,
         invoicerViewUrl,
@@ -247,6 +248,7 @@ export default function CreateInvoiceCommand(
         currency,
         total,
         createdAt: new Date().toISOString(),
+        dueDate: dueDate ? dueDate.toISOString().split("T")[0] : undefined,
         status: "DRAFT",
         invoicerViewUrl,
       });
