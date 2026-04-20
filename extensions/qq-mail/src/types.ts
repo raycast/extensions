95ae92689c8ba8769d9946fa7df4018f507d95da
慕雨 <ly02065292@taobao.com>
2026-04-20 11:22:26 +0800

    Add qq-mail extension

---
+++ extensions/qq-mail/src/types.ts
@@ -0,0 +1,31 @@
+export interface Email {
+  uid: number;
+  messageId: string;
+  subject: string;
+  from: EmailAddress[];
+  to: EmailAddress[];
+  cc?: EmailAddress[];
+  date: Date;
+  flags: string[];
+  hasAttachment: boolean;
+  preview?: string;
+  body?: string;
+  htmlBody?: string;
+}
+
+export interface EmailAddress {
+  name?: string;
+  address: string;
+}
+
+export interface Folder {
+  path: string;
+  name: string;
+  delimiter: string;
+  flags: string[];
+  specialUse?: string;
+  messagesCount?: number;
+  unseenCount?: number;
+}
+
+export type EmailFilter = "all" | "unread" | "read" | "attachment";
