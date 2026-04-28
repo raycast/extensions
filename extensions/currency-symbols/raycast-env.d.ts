86b30f45e260f417ca67350b5e3f3b113e139fe6
MobileMage <aidelojejoshua@gmail.com>
2026-04-28 21:06:07 +0100

    Add raycast-env.d.ts

---
+++ extensions/currency-symbols/raycast-env.d.ts
@@ -0,0 +1,24 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `search-currencies` command */
+  export type SearchCurrencies = ExtensionPreferences & {}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `search-currencies` command */
+  export type SearchCurrencies = {}
+}
+
