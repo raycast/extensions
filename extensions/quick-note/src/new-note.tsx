a1fd216166844253066a60ce9e119493c2c92278
Kaunteya Suryawanshi <k.suryawanshi@gmail.com>
2026-05-03 23:11:35 +0530

    Added ComandArguments interface

--- extensions/quick-note/src/new-note.tsx
+++ extensions/quick-note/src/new-note.tsx
@@ -2,6 +2,12 @@ import { runAppleScript } from "run-applescript";
 import { LaunchProps, closeMainWindow } from "@raycast/api";
 import { escaped } from "./utils";
 
+// Arguments interface
+interface CommandArguments {
+  title?: string;
+  content?: string;
+}
+
 export default async function Command(props: LaunchProps<{ arguments: CommandArguments }>) {
   const { title, content } = props.arguments;
 
