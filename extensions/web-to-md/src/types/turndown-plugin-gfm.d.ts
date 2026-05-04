bb77f91a70ea464af9f4d57b5a9d620c1eb59332
Bryan Blanchot <Bryan21B@users.noreply.github.com>
2026-05-04 10:24:51 +0200

    fix: add type declaration for turndown-plugin-gfm

---
+++ extensions/web-to-md/src/types/turndown-plugin-gfm.d.ts
@@ -0,0 +1,6 @@
+declare module 'turndown-plugin-gfm' {
+  import TurndownService from 'turndown';
+  export function gfm(service: TurndownService): void;
+  export function tables(service: TurndownService): void;
+  export function strikethrough(service: TurndownService): void;
+}
