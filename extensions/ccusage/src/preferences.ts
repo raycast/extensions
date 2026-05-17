c774164a8aef7105ad2431ceb800bad2245c01a3
mehmet turac <mehmetturac@gmail.com>
2026-05-17 06:10:56 +0300

    Add monochrome ccusage menu bar icon option

--- extensions/ccusage/src/preferences.ts
+++ extensions/ccusage/src/preferences.ts
@@ -1,6 +1,8 @@
 import { getPreferenceValues } from "@raycast/api";
 import { homedir } from "os";
 import { sep } from "path";
+import type { Image } from "@raycast/api";
+import { Icon } from "@raycast/api";
 import type { ProgressBarStyle } from "./utils/usage-limits-formatter";
 
 export const preferences = getPreferenceValues<Preferences>();
@@ -29,6 +31,9 @@ export const getMenuBarTitle = ():
 
 export const getProgressBarStyle = (): ProgressBarStyle =>
   (menuBarPreferences.progressBarStyle as ProgressBarStyle) ?? "solid";
+
+export const getMenuBarIcon = (): Image.ImageLike =>
+  menuBarPreferences.menuBarIconStyle === "monochrome" ? Icon.BarChart : "extension-icon.png";
 
 export const getCustomNpxPath = (): string | undefined => {
   const customPath = preferences.customNpxPath?.trim();
