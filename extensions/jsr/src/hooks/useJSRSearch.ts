dc62c92503cb89712c7cd92937c9e3fdba4c2d75
j3lte <jwlagendijk@gmail.com>
2026-05-18 10:22:51 +0200

    Refactor useJSRSearch

--- extensions/jsr/src/hooks/useJSRSearch.ts
+++ extensions/jsr/src/hooks/useJSRSearch.ts
@@ -1,7 +1,7 @@
-import { useMemo, useRef } from "react";
+import { useMemo } from "react";
 
 import { captureException } from "@raycast/api";
-import { useCachedPromise } from "@raycast/utils";
+import { useFetch } from "@raycast/utils";
 
 import type { ErrorResult, SearchResult, SearchResults } from "@/types";
 
@@ -13,55 +13,33 @@ import { useSearchAPIData } from "@/hooks/useSearchAPIData";
 export const useJSRSearch = (queryString: string, scoped: string | null) => {
   const { query, scope, triggerQuery, runtimes, searchQueryURL } = useQueryParser(queryString, scoped);
   const { data: apiData, isLoading: isLoadingAPIData, error: apiDataError } = useSearchAPIData();
-  const abortable = useRef<AbortController | null>(null);
 
-  const searchURL = useMemo(() => {
-    if (!apiData || isLoadingAPIData) {
-      return null;
-    }
-    return `https://collections.orama.com/v1/collections/${apiData.projectId}/search`;
-  }, [apiData, isLoadingAPIData]);
+  const searchURL = apiData ? `https://collections.orama.com/v1/collections/${apiData.projectId}/search` : "";
 
-  const apiKey = apiData?.apiKey ?? null;
-
-  const body = useMemo(() => {
-    return generateSearchBody(query, scope, runtimes);
-  }, [query, scope, runtimes]);
+  const body = useMemo(() => generateSearchBody(query, scope, runtimes), [query, scope, runtimes]);
 
   const {
     isLoading,
     error: dataError,
     ...rest
-  } = useCachedPromise(
-    async (url: string | null, key: string | null, triggerQuery: string) => {
-      if (!url || !key || !triggerQuery) {
-        return [] as SearchResult[];
+  } = useFetch<SearchResults | ErrorResult, SearchResult[], SearchResult[]>(searchURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${apiData?.apiKey ?? ""}`,
+    },
+    body,
+    execute: !!apiData && !!triggerQuery && !!searchURL,
+    keepPreviousData: true,
+    initialData: [] as SearchResult[],
+    mapResult: (data) => {
+      if ("message" in data) {
+        captureException(data.message);
+        return { data: [] as SearchResult[] };
       }
-      return fetch(url, {
-        method: "POST",
-        signal: abortable.current?.signal,
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${key}`,
-        },
-        body,
-      })
-        .then((response) => response.json() as Promise<SearchResults | ErrorResult>)
-        .then((data) => {
-          if ("message" in data) {
-            captureException(data.message);
-            return [];
-          }
-
-          return data.hits.filter((h) => !!h.id && !!h.document.id);
-        });
+      return { data: data.hits.filter((h) => !!h.id && !!h.document.id) };
     },
-    [searchURL, apiKey, triggerQuery],
-    {
-      abortable,
-      initialData: [] as SearchResult[],
-    },
-  );
+  });
 
   return { isLoading: isLoading || isLoadingAPIData, error: dataError || apiDataError, ...rest, searchQueryURL };
 };
