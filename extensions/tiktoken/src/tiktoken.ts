a2537c14bd030d69c7804adadcce191eadf09b2a
Maximilian <85677120+maximilianzuern@users.noreply.github.com>
2026-05-29 23:40:37 +0200

    Use WASM tokenizer

--- extensions/tiktoken/src/tiktoken.ts
+++ extensions/tiktoken/src/tiktoken.ts
@@ -1,63 +1,53 @@
-import { getPreferenceValues } from "@raycast/api";
-import { Tiktoken } from "js-tiktoken/lite";
-import type { TiktokenBPE } from "js-tiktoken/lite";
+import { getPreferenceValues, PreferenceValues } from "@raycast/api";
+import type { TiktokenEncoding } from "tiktoken";
+import { get_encoding, init } from "tiktoken/init";
+import wasm from "tiktoken/tiktoken_bg.wasm";
 
-type EncodingName = "gpt2" | "r50k_base" | "p50k_base" | "p50k_edit" | "cl100k_base" | "o200k_base";
-type Preferences = { encoding: EncodingName };
+const encoders = new Map<TiktokenEncoding, ReturnType<typeof get_encoding>>();
+let tiktokenInitPromise: Promise<void> | undefined;
 
-const encoders = new Map<EncodingName, Tiktoken>();
-
-export function encode(str: string) {
-  const { encoding } = getPreferenceValues<Preferences>();
-  const encoder = getEncoder(encoding);
+export async function encode(str: string) {
+  const { encoding } = getPreferenceValues<PreferenceValues>();
+  const encoder = await getEncoder(encoding);
 
   return {
-    tokens: encoder.encode(str),
+    tokens: Array.from(encoder.encode(str)),
     encoding,
   };
 }
 
-export function decode(list: number[]) {
-  const { encoding } = getPreferenceValues<Preferences>();
-  const encoder = getEncoder(encoding);
+export async function decode(list: number[]) {
+  const { encoding } = getPreferenceValues<PreferenceValues>();
+  const encoder = await getEncoder(encoding);
 
   return {
-    text: encoder.decode(list),
+    text: new TextDecoder().decode(encoder.decode(new Uint32Array(list))),
     encoding,
   };
 }
 
-function getEncoder(encoding: EncodingName) {
+async function getEncoder(encoding: TiktokenEncoding) {
+  await initTiktoken();
+
   const existingEncoder = encoders.get(encoding);
 
   if (existingEncoder) {
     return existingEncoder;
   }
 
-  let ranks: TiktokenBPE;
-  switch (encoding) {
-    case "gpt2":
-      ranks = require("js-tiktoken/ranks/gpt2");
-      break;
-    case "r50k_base":
-      ranks = require("js-tiktoken/ranks/r50k_base");
-      break;
-    case "p50k_base":
-      ranks = require("js-tiktoken/ranks/p50k_base");
-      break;
-    case "p50k_edit":
-      ranks = require("js-tiktoken/ranks/p50k_edit");
-      break;
-    case "cl100k_base":
-      ranks = require("js-tiktoken/ranks/cl100k_base");
-      break;
-    case "o200k_base":
-      ranks = require("js-tiktoken/ranks/o200k_base");
-      break;
-  }
-
-  const encoder = new Tiktoken(ranks);
+  const encoder = get_encoding(encoding);
   encoders.set(encoding, encoder);
 
   return encoder;
+}
+
+function initTiktoken() {
+  // check if tiktoken is already initialized
+  if (tiktokenInitPromise) {
+    return tiktokenInitPromise;
+  }
+
+  tiktokenInitPromise = init((imports) => WebAssembly.instantiate(wasm, imports)).then(() => undefined);
+
+  return tiktokenInitPromise;
 }
