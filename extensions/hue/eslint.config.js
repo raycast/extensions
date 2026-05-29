f64c216e760c9b9a07bc457b45dfc75befcf586b
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2026-05-29 13:15:18 +0200

    Wrap ESLint flat config with defineConfig

--- extensions/hue/eslint.config.js
+++ extensions/hue/eslint.config.js
@@ -1,2 +1,3 @@
+const { defineConfig } = require("eslint/config");
 const raycast = require("@raycast/eslint-config");
-module.exports = raycast.flat(Infinity);
+module.exports = defineConfig([...raycast]);
