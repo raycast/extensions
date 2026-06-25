7462ef51b71eae9845e206835b43c4afc8208a9e
Ethan <ensucao@outlook.com>
2026-06-25 17:35:11 +0800

    style(otty): fix prettier formatting in ssh.tsx

--- extensions/otty/src/ssh.tsx
+++ extensions/otty/src/ssh.tsx
@@ -1,7 +1,5 @@
 import { openSshTarget } from "./lib/otty";
 
-export default async function Command(props: {
-  arguments: Arguments.Ssh;
-}) {
+export default async function Command(props: { arguments: Arguments.Ssh }) {
   await openSshTarget(props.arguments.target);
 }
