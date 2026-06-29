41bbf23dfda6a812054ee744bb0a2c2e0e413d94
xmok <30526133+xmok@users.noreply.github.com>
2026-06-29 11:16:09 +0500

    [dbtCloud] select endpoint

--- extensions/dbtCloud/CHANGELOG.md
+++ extensions/dbtCloud/CHANGELOG.md
@@ -1,3 +1,7 @@
 # dbtCloud Changelog
 
+## [Modernize + Specify Endpoint] - {PR_MERGE_DATE}
+
+- Add `README.md`
+
 ## [Initial Version] - 2022-03-26
--- extensions/dbtCloud/src/fetch_runs.tsx
+++ extensions/dbtCloud/src/fetch_runs.tsx
@@ -1,28 +1,23 @@
-import { getPreferenceValues, showToast, Toast } from "@raycast/api";
-import fetch from "node-fetch";
-import { RunsFetchResponse, Preferences } from "./types";
+import { getPreferenceValues } from "@raycast/api";
+import { dbtV2Response, RunsFetchResponse } from "./types";
 
-const preferences: Preferences = getPreferenceValues();
+const preferences = getPreferenceValues<Preferences>();
 const token = preferences.dbtCloudAPIToken;
 const account_id = preferences.dbtCloudAcountID;
+const account_prefix = preferences.dbtCloudAcountPrefix;
+const endpoint = preferences.dbtCloudEndpoint.replace("{account_prefix}", account_prefix ?? "");
 
 export const returnRuns = async (): Promise<RunsFetchResponse> => {
-  try {
-    const response = await fetch(
-      `https://cloud.getdbt.com/api/v2/accounts/${account_id}/runs?order_by=-finished_at&include_related=["job"]`,
-      {
-        method: "GET",
-        headers: {
-          Authorization: `Token ${token}`,
-        },
-      }
-    );
-
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const json: any = await response.json();
-    return json["data"] as RunsFetchResponse;
-  } catch (error) {
-    showToast(Toast.Style.Failure, "An error occured", "Could not fetch API, check your credentials");
-    return Promise.resolve([]);
-  }
+  const response = await fetch(
+    `${endpoint}/api/v2/accounts/${account_id}/runs?order_by=-finished_at&include_related=["job"]`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Token ${token}`,
+      },
+    },
+  );
+  const json = (await response.json()) as dbtV2Response<RunsFetchResponse>;
+  if (!json.status.is_success) throw new Error(json.status.user_message || response.statusText);
+  return json.data;
 };
