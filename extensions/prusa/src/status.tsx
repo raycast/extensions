35f556f8f7e438adee7ad367701e48619f7c6c2c
Dhruv Suthar <git@dhrv.pw>
2026-06-30 15:50:17 +0530

    chore(deps): update project dependencies
    - Bump versions of libraries to latest stable releases
    - Run tests to ensure compatibility with updated packages

--- extensions/prusa/src/status.tsx
+++ extensions/prusa/src/status.tsx
@@ -305,7 +305,11 @@ export default function Command() {
                 }}
                 shortcut={{ modifiers: ["cmd"], key: "r" }}
               />
-              <Action title="Open Preferences" icon={Icon.Gear} onAction={() => open(`${process.env.RAYCAST_SCHEME ?? "raycast"}://preferences`)} />
+              <Action
+                title="Open Preferences"
+                icon={Icon.Gear}
+                onAction={() => open(`${process.env.RAYCAST_SCHEME ?? "raycast"}://preferences`)}
+              />
             </ActionPanel.Section>
           </ActionPanel>
         }
@@ -325,7 +329,11 @@ export default function Command() {
                   fetchStatus();
                 }}
               />
-              <Action title="Open Preferences" icon={Icon.Gear} onAction={() => open(`${process.env.RAYCAST_SCHEME ?? "raycast"}://preferences`)} />
+              <Action
+                title="Open Preferences"
+                icon={Icon.Gear}
+                onAction={() => open(`${process.env.RAYCAST_SCHEME ?? "raycast"}://preferences`)}
+              />
             </ActionPanel>
           }
         />
