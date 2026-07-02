f0e2e25866c68e4263f993afc5a30054ded261c0
xmok <30526133+xmok@users.noreply.github.com>
2026-07-02 11:35:59 +0500

    [BTTV Emote] fix pagination

--- extensions/bttv-emote/src/api/bttv_api.tsx
+++ extensions/bttv-emote/src/api/bttv_api.tsx
@@ -1,11 +1,11 @@
-import { Emote } from "../components/emote";
+import { Emote, LIMIT } from "../components/emote";
 
 export async function performSearch(searchText: string, signal: AbortSignal, page: number): Promise<Emote[]> {
   const params = new URLSearchParams();
   const blankQuery = "    ";
   params.append("query", searchText.length > 2 ? searchText : blankQuery);
-  params.append("offset", `${32 * page}`);
-  params.append("limit", "32");
+  params.append("offset", String(LIMIT * page));
+  params.append("limit", String(LIMIT));
 
   const response = await fetch("https://api.betterttv.net/3/emotes/shared/search" + "?" + params.toString(), {
     method: "get",
--- extensions/bttv-emote/src/hooks/use_search.tsx
+++ extensions/bttv-emote/src/hooks/use_search.tsx
@@ -1,16 +1,17 @@
 import { useRef } from "react";
 import { performSearch } from "../api/bttv_api";
 import { useCachedPromise } from "@raycast/utils";
+import { LIMIT } from "../components/emote";
 
 export function useSearch(query: string) {
   const cancelRef = useRef<AbortController | null>(null);
   const { isLoading, data, pagination } = useCachedPromise(
     (searchText: string) => async (options) => {
-      if (searchText.length < 2) return { data: [] };
+      if (searchText.length < 2) return { data: [], hasMore: false };
       cancelRef.current?.abort();
       cancelRef.current = new AbortController();
       const results = await performSearch(searchText, cancelRef.current.signal, options.page);
-      return { data: results };
+      return { data: results, hasMore: results.length === LIMIT };
     },
     [query],
     {
