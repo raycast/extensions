d312b327a1c88c4b5b5aa1234ba5ce017439f1f6
xmok <30526133+xmok@users.noreply.github.com>
2026-07-02 11:30:21 +0500

    [BTTV Emote] win+cachee+pagination

--- extensions/bttv-emote/src/api/bttv_api.tsx
+++ extensions/bttv-emote/src/api/bttv_api.tsx
@@ -1,11 +1,10 @@
 import { Emote } from "../components/emote";
-import fetch from "node-fetch";
 
-export async function performSearch(searchText: string, signal: AbortSignal): Promise<Emote[]> {
+export async function performSearch(searchText: string, signal: AbortSignal, page: number): Promise<Emote[]> {
   const params = new URLSearchParams();
   const blankQuery = "    ";
   params.append("query", searchText.length > 2 ? searchText : blankQuery);
-  params.append("offset", "0");
+  params.append("offset", `${32 * page}`);
   params.append("limit", "32");
 
   const response = await fetch("https://api.betterttv.net/3/emotes/shared/search" + "?" + params.toString(), {
