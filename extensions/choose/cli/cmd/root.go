df009c39a7ed46c60998f48874e83c31171b1389
SOM <spe4pg@gmail.com>
2026-07-04 01:48:13 +0300

    Update choose extension
    - fix(package): changed titles
    - fix(formatting): Removed debug logging in react and cleaned up in go
    - fix(cleanup): some code cleanup

--- extensions/choose/cli/cmd/root.go
+++ extensions/choose/cli/cmd/root.go
@@ -17,95 +17,97 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
-	Use:   "choose",
-	Short: "Choose items from stdin via raycast",
-	Long: `Takes input from stdin, separates into items by \n and feeds to raycast. Choice is then piped to stdout:
+func receiver(listener net.Listener, items []string) {
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			errCh <- err
+			return
+		}
+		fmt.Fprintf(conn, "%v\n", len(items))
+		fmt.Fprint(conn, strings.Join(items, "\n")+"\n")
+		fmt.Fprintln(os.Stderr, "Acquired another connection...")
+		connCh <- conn
+	}
+}
+
+func reader(conn net.Conn) {
+	choice, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		errCh <- errors.New("Nothing was chosen")
+		return
+	}
+	selection <- choice
+}
+
+var (
+	connCh    = make(chan net.Conn)
+	errCh     = make(chan error)
+	selection = make(chan string)
+
+	rootCmd = &cobra.Command{
+		Use:   "choose",
+		Short: "Choose items from stdin via raycast",
+		Long: `Takes input from stdin, separates into items by \n and feeds to raycast. Choice is then piped to stdout:
 
 For now its just that, no extra flags or anything`,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		listener, err := net.Listen("tcp", "127.0.0.1:0")
-		if err != nil {
-			return err
-		}
-		addr := listener.Addr().String()
-		fmt.Fprintf(os.Stderr, "Will be listening on %v...", addr)
-		defer listener.Close()
-
-		stdin, err := io.ReadAll(cmd.InOrStdin())
-		if err != nil {
-			return err
-		}
-		if len(stdin) <= 1 {
-			return errors.New("Got empty stdin!")
-		}
-
-		host, port, _ := net.SplitHostPort(addr)
-		deepLinkURL := fmt.Sprintf(`raycast://extensions/anaritus/choose/choose?arguments={"host": "%s","port": "%s"}`, host, port)
-		fmt.Fprintf(os.Stderr, "Opening %v...", deepLinkURL)
-		err = exec.Command("open", deepLinkURL).Run()
-		if err != nil {
-			return err
-		}
-
-		items := strings.Split(string(stdin), "\n")
-
-		connCh := make(chan net.Conn)
-		errCh := make(chan error)
-
-		go func() {
-			for {
-				conn, err := listener.Accept()
-				if err != nil {
-					errCh <- err
-					return
-				}
-				connCh <- conn
-			}
-		}()
-
-		selection := make(chan string)
-		reader := func(conn net.Conn) {
-			choice, err := bufio.NewReader(conn).ReadString('\n')
+		RunE: func(cmd *cobra.Command, args []string) error {
+			listener, err := net.Listen("tcp", "127.0.0.1:0")
 			if err != nil {
-				errCh <- errors.New("Nothing was chosen")
-				return
+				return err
 			}
-			selection <- choice
-		}
+			addr := listener.Addr().String()
+			fmt.Fprintf(os.Stderr, "Will be listening on %v...\n", addr)
+			defer listener.Close()
 
-		select {
-		case err = <-errCh:
-			return err
-		case conn := <-connCh:
-			fmt.Fprintln(os.Stderr, "Accepted connection")
-			fmt.Fprintf(conn, "%v\n", len(items))
-			fmt.Fprint(conn, strings.Join(items, "\n")+"\n")
-			go reader(conn)
-		}
+			stdin, err := io.ReadAll(cmd.InOrStdin())
+			if err != nil {
+				return err
+			}
+			if len(stdin) <= 1 {
+				return errors.New("Got empty stdin!")
+			}
 
-		for {
+			host, port, _ := net.SplitHostPort(addr)
+			deepLinkURL := fmt.Sprintf(
+				`raycast://extensions/anaritus/choose/choose?arguments={"host": "%s","port": "%s"}`, host, port,
+			)
+			fmt.Fprintf(os.Stderr, "Opening %v...\n", deepLinkURL)
+			err = exec.Command("open", deepLinkURL).Run()
+			if err != nil {
+				return err
+			}
+
+			items := strings.Split(string(stdin), "\n")
+
+			go receiver(listener, items)
+
 			select {
 			case err = <-errCh:
-				grace := make(chan int)
-				go func() {
-					time.Sleep(50 * time.Millisecond)
-					grace <- 0
-				}()
-				select {
-				case conn := <-connCh:
-					go reader(conn)
-				case <-grace:
-					return errors.New("Failed to refresh connection after breaking")
-				}
-			case choice := <-selection:
-				fmt.Println(strings.TrimRight(choice, " \n"))
-				return nil
+				return err
+			case conn := <-connCh:
+				go reader(conn)
 			}
-		}
-	},
-}
+
+			for {
+				select {
+				case err = <-errCh:
+					timer := time.NewTimer(50 * time.Millisecond)
+					select {
+					case conn := <-connCh:
+						timer.Stop()
+						go reader(conn)
+					case <-timer.C:
+						return errors.New("Failed to refresh connection after breaking")
+					}
+				case choice := <-selection:
+					fmt.Println(strings.TrimRight(choice, " \n"))
+					return nil
+				}
+			}
+		},
+	}
+)
 
 func Execute() {
 	err := rootCmd.Execute()
--- extensions/choose/src/choose.tsx
+++ extensions/choose/src/choose.tsx
@@ -18,11 +18,10 @@ export default (props: Props) => {
     arguments: { host, port },
   } = props;
 
-  console.log(`Listening on host ${host} with port ${port}...`);
   useEffect(() => {
-    console.log("Started useEffect hook...");
     const newSocket = connect({ host, port: +port }, () => {
-      console.log("Acquired socket!");
+      setItems([]);
+      setTotal(1);
     });
 
     let buffer = "";
@@ -30,35 +29,28 @@ export default (props: Props) => {
     newSocket.on("data", (data) => {
       const chunk = data.toString();
       buffer += chunk;
-      console.log(`Received chunk ${chunk}, current buffer ${buffer}...`);
       if (!buffer.includes("\n")) {
         return;
       }
       const new_items = buffer.split("\n");
-      console.log(`New items are ${new_items}`);
-      console.log(buffer);
       if (!hasTotal) {
         hasTotal = true;
         const newTotal = +new_items.shift()!;
         setTotal(newTotal);
-        console.log(`there are ${newTotal} items in total!`);
+        setItems(new_items);
+      } else {
+        setItems((prevItems) => prevItems.concat(new_items));
       }
       buffer = new_items.pop() || "";
-      console.log(`Current buffer is ${buffer}`);
-      setItems((prevItems) => prevItems.concat(new_items));
     });
 
     newSocket.on("error", (err) => {
-      console.log(err);
       showToast({ message: err.message, title: "Connection error occured" });
     });
     setSocket(newSocket);
 
     return () => {
-      console.log("Called unmount");
-      newSocket.end(() => {
-        console.log("Closed connection socket");
-      });
+      newSocket.end(() => {});
     };
   }, []);
 
@@ -74,7 +66,6 @@ export default (props: Props) => {
                 title="Select"
                 onAction={() => {
                   socket!.write(`${item}\n`);
-                  console.log(`Choosing ${item}...`);
                   closeMainWindow({ clearRootSearch: true, popToRootType: PopToRootType.Immediate });
                 }}
               ></Action>
