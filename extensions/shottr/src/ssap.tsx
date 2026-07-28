b3bbf6ba2949caf4a72ce9850353df4687e0e362
UnnecTrycat <108726581+Caffeine19@users.noreply.github.com>
2026-07-28 15:53:12 +0800

    [Shottr] Add new "closeMainWindow" preference (#29794)
    * Update shottr extension
    
    - :sparkles: Add a close main window preference
    - Pull contributions
    
    * Update CHANGELOG.md
    
    ---------
    
    Co-authored-by: raycastbot <bot@raycast.com>

--- extensions/shottr/src/ssap.tsx
+++ extensions/shottr/src/ssap.tsx
@@ -1,9 +1,10 @@
-import { closeMainWindow } from "@raycast/api";
 import { withShottrCheck } from "./utils/checkInstall";
+import { withCloseMainWindow } from "./utils/withCloseMainWindow";
 import { execSync } from "child_process";
 
-export default withShottrCheck(async function () {
-  const url = "shottr://grab/append";
-  await closeMainWindow();
-  execSync(`open -g ${url}`);
-});
+export default withShottrCheck(
+  withCloseMainWindow(async function () {
+    const url = "shottr://grab/append";
+    execSync(`open -g ${url}`);
+  }),
+);
--- extensions/shottr/src/ssw.tsx
+++ extensions/shottr/src/ssw.tsx
@@ -1,9 +1,10 @@
-import { closeMainWindow } from "@raycast/api";
 import { withShottrCheck } from "./utils/checkInstall";
+import { withCloseMainWindow } from "./utils/withCloseMainWindow";
 import { execSync } from "child_process";
 
-export default withShottrCheck(async function () {
-  const url = "shottr://grab/repeat";
-  await closeMainWindow();
-  execSync(`open -g ${url}`);
-});
+export default withShottrCheck(
+  withCloseMainWindow(async function () {
+    const url = "shottr://grab/repeat";
+    execSync(`open -g ${url}`);
+  }),
+);
