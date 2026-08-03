a14d3ef5113c46e8c0229a015fd892dab8314c2d
Anton <81649116+isabhiii@users.noreply.github.com>
2026-08-03 11:53:16 +0200

    Add formatReminderTime function and update getDateString

--- extensions/apple-reminders/src/helpers.ts
+++ extensions/apple-reminders/src/helpers.ts
@@ -8,10 +8,23 @@ export function isFullDay(date: string) {
   return /^\d{4}-\d{2}-\d{2}$/.test(date);
 }
 
-export function getDateString(date: string) {
-  return isFullDay(date) ? date : formatISO(date, { representation: "date" });
+export function formatReminderTime(reminder: Reminder): string {
+  if (!reminder.dueDate) {
+    return "";
+  }
+
+  const date = new Date(reminder.dueDate);
+  if (isNaN(date.getTime())) {
+    return "";
+  }
+
+  return new Intl.DateTimeFormat(undefined, {
+    hour: "2-digit",
+    minute: "2-digit",
+  }).format(date);
 }
 
+export function getDateString(date: string) {
 export function getTodayInLocalTime() {
   return formatISO(new Date(), { representation: "date" });
 }
