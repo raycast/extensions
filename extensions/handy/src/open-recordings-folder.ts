9de2bc885832578d2555be6345a12c83168f2412
Maciej Piasecki <maciej.k.piasecki@gmail.com>
2026-08-04 13:08:13 +0200

    Handy: Improve Linux/Vicinae compatibility (#29224)
    * Handy: Improve Linux/Vicinae compatibility
    
    * Update CHANGELOG.md
    
    ---------
    
    Co-authored-by: raycastbot <bot@raycast.com>

--- extensions/handy/src/open-recordings-folder.ts
+++ extensions/handy/src/open-recordings-folder.ts
@@ -1,10 +1,9 @@
-import { showHUD, showToast, Toast } from "@raycast/api";
-import { execa } from "execa";
+import { open, showHUD, showToast, Toast } from "@raycast/api";
 import { RECORDINGS_DIR } from "./lib/constants";
 
 export default async function main() {
   try {
-    await execa("open", [RECORDINGS_DIR]);
+    await open(RECORDINGS_DIR);
     await showHUD("Opening recordings folder");
   } catch (err) {
     await showToast({
