340331c18bd65ae2d9efa9e9e947a636d8971071
Syed M Mokaarim Bin Razi <30526133+xmok@users.noreply.github.com>
2026-08-11 22:22:19 +0500

    [Browsers Profile] copy deeplink + upd8 images (#30143)
    * [Browsers Profile] copy deeplink + upd8 images
    
    * Update CHANGELOG.md and optimise images
    
    ---------
    
    Co-authored-by: raycastbot <bot@raycast.com>

--- extensions/browsers-profiles/src/index.tsx
+++ extensions/browsers-profiles/src/index.tsx
@@ -1,17 +1,30 @@
-import { ActionPanel, List, Icon, Action, closeMainWindow, getPreferenceValues } from "@raycast/api";
+import { ActionPanel, List, Icon, Action, closeMainWindow, getPreferenceValues, LaunchProps } from "@raycast/api";
 
 import { getFirefoxProfiles } from "./lib/firefox";
 import { getChromiumProfiles } from "./lib/chromium";
 import { launchBrowser } from "./lib/browsers";
+import { useEffect, useState } from "react";
 
-export default function Command() {
-  const preferences = getPreferenceValues();
+export default function Command(props: LaunchProps<{ launchContext: { type: string; app: string; profile: string } }>) {
+  const preferences = getPreferenceValues<Preferences.Index>();
   const enabledBrowsers = preferences["browsers.filter"].split(",");
 
   const chromiumProfiles = getChromiumProfiles(enabledBrowsers);
   const firefoxProfiles = getFirefoxProfiles(enabledBrowsers);
 
   const browsers = [...chromiumProfiles, ...firefoxProfiles];
+
+  const [launched, setLaunched] = useState(false);
+  useEffect(() => {
+    (() => {
+      if (!launched && props.launchContext) {
+        const { type, app, profile } = props.launchContext;
+        setLaunched(true);
+        launchBrowser(type, app, profile);
+        closeMainWindow({ clearRootSearch: true });
+      }
+    })();
+  }, []);
 
   return (
     <List>
@@ -26,11 +39,16 @@ export default function Command() {
               actions={
                 <ActionPanel>
                   <Action
+                    icon={`icons/${profile.icon}`}
                     title="Open Browser"
                     onAction={async () => {
                       launchBrowser(profile.type, profile.app, profile.path);
                       await closeMainWindow({ clearRootSearch: true });
                     }}
+                  />
+                  <Action.CopyToClipboard
+                    title="Copy Deeplink"
+                    content={`raycast://extensions/skydiver/browsers-profiles/index?context=${encodeURIComponent(JSON.stringify({ type: profile.type, app: profile.app, profile: profile.path }))}`}
                   />
                 </ActionPanel>
               }
