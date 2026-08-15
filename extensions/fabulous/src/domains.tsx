29b8b2d522131b11a36bbf703f848242a01caca1
xmok <30526133+xmok@users.noreply.github.com>
2026-08-15 18:08:50 +0500

    [Fabulous] cleanup

--- extensions/fabulous/src/domains.tsx
+++ extensions/fabulous/src/domains.tsx
@@ -3,7 +3,8 @@ import { getFavicon, useCachedPromise, useFetch } from "@raycast/utils";
 import { XMLParser } from "fast-xml-parser";
 
 interface Domain {
-  domain: string; exdate: string
+  domain: string;
+  exdate: string;
 }
 interface DomainInfo {
   expiry: string;
@@ -11,7 +12,12 @@ interface DomainInfo {
     nameserver: string[];
   };
 }
-interface DNSRecord { id: number; name: string; type: string; content: string }
+interface DNSRecord {
+  id: number;
+  name: string;
+  type: string;
+  content: string;
+}
 interface XMLResult<T> {
   fabulous: {
     response: {
@@ -37,63 +43,78 @@ function buildApiUrl(endpoint: string, params: Record<string, string> = {}) {
   return url.toString();
 }
 async function parseApiResponse<T>(response: Response) {
-      const txt = await response.text();
-      const obj: XMLResult<T> = await parser.parse(txt);
-      const { status } = obj.fabulous.response;
-      if (![200, 201, 202].includes(status)) throw new Error(obj.fabulous.response.reason);
-      return obj.fabulous.response.results?.result;
-    }
-    async function callApi<T>(endpoint: string, params: Record<string, string> = {}) {
-      const response = await fetch(buildApiUrl(endpoint, params));
-      const result = await parseApiResponse<T>(response);
-      return result;
-    }
-
-function useFabulous<T>(endpoint: string, params?: Record<string, string>) {
-  return useFetch(buildApiUrl(endpoint, params), {
-    parseResponse: parseApiResponse<T>,
-  });
+  const txt = await response.text();
+  const obj: XMLResult<T> = await parser.parse(txt);
+  const { status } = obj.fabulous.response;
+  if (![200, 201, 202].includes(status)) throw new Error(obj.fabulous.response.reason);
+  return obj.fabulous.response.results?.result;
+}
+async function callApi<T>(endpoint: string, params: Record<string, string> = {}) {
+  const response = await fetch(buildApiUrl(endpoint, params));
+  const result = await parseApiResponse<T>(response);
+  return result;
 }
 
 export default function Domains() {
-  // const { isLoading, data: domains = [] } = useFabulous<Domain[]>("listDomains");
-  const { isLoading, data: domains } = useCachedPromise(async() => {
-    const domains = await callApi<Domain[]>("listDomains") ?? [];
-    const infos = await Promise.all(domains.map(({domain}) => callApi<DomainInfo>("domainInfo", {domain: domain})))
-    return domains.map((domain, index) => ({...domain, ...infos[index]}));
-  }, [], {initialData:[]})
+  const {
+    isLoading,
+    data: domains,
+    error,
+  } = useCachedPromise(
+    async () => {
+      const domains = (await callApi<Domain[]>("listDomains")) ?? [];
+      const infos = await Promise.all(
+        domains.map(({ domain }) => callApi<DomainInfo>("domainInfo", { domain: domain })),
+      );
+      return domains.map((domain, index) => ({ ...domain, ...infos[index] }));
+    },
+    [],
+    { initialData: [] },
+  );
   return (
     <List isLoading={isLoading}>
-      {domains.map((domainItem) => (
-        <List.Item
-          key={domainItem.domain}
-          icon={getFavicon(`https://${domainItem.domain}`, {fallback: Icon.Globe})}
-          title={domainItem.domain}
-          accessories={[{ date: new Date(domainItem.exdate) }]}
+      {!isLoading && !domains.length && !error ? (
+        <List.EmptyView
+          title="No Results"
+          description="Go online to register a domain"
           actions={
             <ActionPanel>
-              <Action.Push icon={Icon.List} title="DNS Records" target={<DNSRecords domain={domainItem.domain} />} />
+              <Action.OpenInBrowser title="Register Domain" url="https://fabulous.com/search" />
             </ActionPanel>
           }
         />
-      ))}
+      ) : (
+        domains.map((domainItem) => (
+          <List.Item
+            key={domainItem.domain}
+            icon={getFavicon(`https://${domainItem.domain}`, { fallback: Icon.Globe })}
+            title={domainItem.domain}
+            accessories={[{ date: new Date(domainItem.exdate) }]}
+            actions={
+              <ActionPanel>
+                <Action.Push icon={Icon.List} title="DNS Records" target={<DNSRecords domain={domainItem.domain} />} />
+              </ActionPanel>
+            }
+          />
+        ))
+      )}
     </List>
   );
 }
 
 function DNSRecords({ domain }: { domain: string }) {
-  const { isLoading, data: records = [] } = useFabulous<
-    DNSRecord[]
-  >("listDNSrecords", {
-    domain,
+  const { isLoading, data: records = [] } = useFetch(buildApiUrl("listDNSrecords", { domain }), {
+    parseResponse: parseApiResponse<DNSRecord[]>,
   });
+
   return (
     <List isLoading={isLoading} isShowingDetail>
       {records.map((record, recordIndex) => (
         <List.Item
           key={recordIndex}
           icon={Icon.Text}
-          title={record.id.toString()}
+          title={record.name}
+          accessories={[{ tag: record.type }]}
           detail={
             <List.Item.Detail
               markdown={record.content}
