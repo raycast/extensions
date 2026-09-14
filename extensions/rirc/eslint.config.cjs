452a5be520dd9e9c1f6f7633c5f5b97e58ec62ca
2Ahmad5 <chahmad2005@gmail.com>
2026-09-14 17:08:02 -0400

    Use Raycast ESLint config convention

--- extensions/rirc/eslint.config.cjs
+++ extensions/rirc/eslint.config.cjs
@@ -1,1 +1,4 @@
-module.exports = require("@raycast/eslint-config");
+const { defineConfig } = require("eslint/config");
+const raycastConfig = require("@raycast/eslint-config");
+
+module.exports = defineConfig([...raycastConfig]);
