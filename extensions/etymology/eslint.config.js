11c6455a45df335d8d4981e95df9a632593749fe
Chiarandini <zak.katz123@gmail.com>
2026-09-17 23:09:57 -0400

    Second review pass

--- extensions/etymology/eslint.config.js
+++ extensions/etymology/eslint.config.js
@@ -1,6 +1,9 @@
-module.exports = [
-  ...require("@raycast/eslint-config").flat(),
+const { defineConfig } = require("eslint/config");
+const raycastConfig = require("@raycast/eslint-config");
+
+module.exports = defineConfig([
+  ...raycastConfig,
   {
     ignores: ["dist/**", "node_modules/**", "raycast-env.d.ts", "assets/langcodes.json"],
   },
-];
+]);
