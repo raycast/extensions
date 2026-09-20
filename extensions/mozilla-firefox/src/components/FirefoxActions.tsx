f7f57a9a8770f0f856b34d63251cd59fa872adaf
Bryan Guano <alejoguano@hotmail.com>
2026-09-20 12:06:24 -0500

    Pull contributions

--- extensions/mozilla-firefox/src/components/FirefoxActions.tsx
+++ extensions/mozilla-firefox/src/components/FirefoxActions.tsx
@@ -1,26 +1,11 @@
 import { Action, ActionPanel, closeMainWindow, Icon } from "@raycast/api";
-import { buildNewTabUrl, openHistoryTab, openInNewWindow, openNewTab, setActiveTab } from "../actions";
+import { openHistoryTab, openNewTab, setActiveTab } from "../actions";
 import { HistoryEntry, Tab } from "../interfaces";
-
-function OpenInNewWindowAction({ url }: { url?: string }) {
-  if (process.platform !== "win32") return null;
-  return (
-    <Action
-      title="Open in New Window"
-      shortcut={{
-        macOS: { modifiers: ["ctrl"], key: "enter" },
-        Windows: { modifiers: ["ctrl"], key: "enter" },
-      }}
-      onAction={() => openInNewWindow(url)}
-    />
-  );
-}
 
 export function NewTabAction({ query }: { query?: string }) {
   return (
     <ActionPanel title="New Tab">
       <ActionPanel.Item onAction={() => openNewTab(query)} title={query ? `Search "${query}"` : "Open Empty Tab"} />
-      <OpenInNewWindowAction url={buildNewTabUrl(query)} />
     </ActionPanel>
   );
 }
@@ -29,7 +14,6 @@ export function HistoryItemAction({ entry: { title, url } }: { entry: HistoryEnt
   return (
     <ActionPanel title={title}>
       <MozillaFirefoxHistoryTab url={url} />
-      {url ? <OpenInNewWindowAction url={url} /> : null}
       <Action.OpenInBrowser title="Open in Default Browser" url={url} shortcut={{ modifiers: ["opt"], key: "enter" }} />
       <Action.CopyToClipboard title="Copy URL" content={url} shortcut={{ modifiers: ["cmd", "shift"], key: "c" }} />
     </ActionPanel>
@@ -40,7 +24,6 @@ export function TabListItemAction(props: { tab: Tab }) {
   return (
     <ActionPanel title={props.tab.title}>
       <MozillaFirefoxGoToTab tab={props.tab} />
-      {props.tab.url ? <OpenInNewWindowAction url={props.tab.url} /> : null}
       <Action.CopyToClipboard title="Copy URL" content={props.tab.url} />
     </ActionPanel>
   );
