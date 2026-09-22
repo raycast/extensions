13ee40aa7c6ba533f66452f2437e566f800f3d33
Teresa Tian <41807109+Astatine-213-Tian@users.noreply.github.com>
2026-09-22 13:29:10 -0400

    Fix missing icons in GitHub Enterprise (#31400)
    * Fix missing GitHub Enterprise icons
    
    * Update CHANGELOG.md
    
    ---------
    
    Co-authored-by: raycastbot <bot@raycast.com>

--- extensions/github-for-enterprise/src/components/CloseIssue.tsx
+++ extensions/github-for-enterprise/src/components/CloseIssue.tsx
@@ -1,6 +1,6 @@
 import { CLOSE_ISSUE } from "@/queries/issues";
 import { fetcher } from "@/utils";
-import { Color, showToast, useNavigation, Action, Toast } from "@raycast/api";
+import { Color, Icon, showToast, useNavigation, Action, Toast } from "@raycast/api";
 import { useSWRConfig } from "swr";
 
 export default function CloseIssue({ id, number, shouldPop }: any) {
@@ -43,7 +43,7 @@ export default function CloseIssue({ id, number, shouldPop }: any) {
     <Action
       title="Close Issue"
       icon={{
-        source: "xmark-circle-16",
+        source: Icon.XMarkCircle,
         tintColor: Color.PrimaryText,
       }}
       shortcut={{
